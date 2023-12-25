26b626bf7211bfd1c14a60e476ef0b4e4dd9b2e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:30 +0000

    noise passenger fashion schedule comfort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
