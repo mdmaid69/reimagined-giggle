e26dd68dd279b9a5ab12b82e0deff65129f121e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:50:19 +0000

    condition moral accident very market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
