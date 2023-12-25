d5070bb43dd1fc5af6fb9c0bbbafdfc81ee7c2f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:13 +0000

    thick 567enemy extra wash unlucky

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
