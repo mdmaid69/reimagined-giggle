05f964bdf08b10e806513976e5295cd9c71f52bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:26:07 +0000

    fold fail sell bank afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
