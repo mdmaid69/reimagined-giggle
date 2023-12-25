d4be9151c8c54225599063200965f7f6013c5632
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:21:10 +0000

    design regret thankyou thank weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
