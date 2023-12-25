9c32099e1ca76125e45361f0f823327c6cee3dbd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:43 +0000

    simple recover announce bye runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def convert_to_hex(n):
-        return hex(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
