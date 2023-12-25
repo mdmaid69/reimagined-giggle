791bc276f82f18bd1a13d146e7bbb49d7b69deb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:07 +0000

    opinion dinner lucky there Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
