dead1d59f48ae6ec5eb76720a5af0fbb5049594f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:25:35 +0000

    classroom wild service soft anything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
