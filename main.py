f92b2f6dbcc2d2b1be480fd8fd22463a6895b671
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:19 +0000

    bringup plan kind lift finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
