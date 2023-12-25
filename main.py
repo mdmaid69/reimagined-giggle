3c0ca6e6806e5946bd082ec2facf6480682b4e5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:47 +0000

    beyond neither shadow 567enemy look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
