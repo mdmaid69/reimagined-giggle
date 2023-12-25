757e2ec133142c8688fb93fc6d21816fd19989da
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:21 +0000

    future platform base lookup be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
