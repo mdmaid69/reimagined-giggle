2db6bcc383ced15cff4d0d34d85260e0091d1a1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:52 +0000

    bored fear opponent callout cost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
