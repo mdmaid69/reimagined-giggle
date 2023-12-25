ede58a64b58ff9ce140f9587306c8df3b2efd7f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:20 +0000

    familiar air become winter challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
