1253f70f5628d03b4afeb70b698c4d299a2d62fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:58:08 +0000

    afraid that tread pinch steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
