ba23fec3e07eba37f1ac6ce16d0de1cc03a8b5e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:09 +0000

    decide train suppose same us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_current_datetime():
+        return datetime.datetime.now()
