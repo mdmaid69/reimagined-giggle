2cd07a219fc95327db09c27531250596c735ae20
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:52 +0000

    appeal clothes introduction nervous already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_as_set(array):
+        return set(array)
