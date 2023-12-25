97cebb3b3ac3f6312179962597059d8d11e5450e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:25 +0000

    broken everywhere me talk love

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
