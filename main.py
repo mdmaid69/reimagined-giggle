56f75c2c926d2b0b5c830cebb2437e41866e2e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:52 +0000

    surprising impatient special OKorokay kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
