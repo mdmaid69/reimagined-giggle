3d630fb64e59fa7da8e06ab10169307f027236a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:53:01 +0000

    wish tight answer be practice

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
