de5b74156d7cad812d76a8699e4d339e5fdcd1d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:27 +0000

    offer consult compete runout contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
