e3623c78e70add59252250658989873894d2ba23
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:02:04 +0000

    share shoulder arrange I pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
