df89aa461e9262d1acf8c32faaa85358efb0456d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:42:18 +0000

    collection ours typical detail live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
