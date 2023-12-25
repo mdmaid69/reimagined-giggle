0196e431bdeec85f6ef7cecc8620de953baec7c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:31 +0000

    chief ahead hold accept wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
