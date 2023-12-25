a180a83b6b0626c5f31c3bad7bf5724449c315fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:09 +0000

    offend fair degree judge backwards

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-print(random.randint(0, 100))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
