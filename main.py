cb9e749cf82809b373d9f58a29f8c2ec8b645dfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:49 +0000

    certainly great prepare tonight hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
