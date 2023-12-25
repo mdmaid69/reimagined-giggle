6481bf5976f94273eed197ff6d2e985f42be6541
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:29 +0000

    passenger opportunity sticky check broad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
