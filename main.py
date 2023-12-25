615b566c4d8274c3cad437bca837085e22f310db
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:21 +0000

    comeout complete wipe hospital section

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
