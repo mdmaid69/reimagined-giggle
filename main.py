d1997ff1892acea24858ab961f9284bada8d7ee3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:28 +0000

    grand minority past you traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import platform
+def get_os_info():
+        return platform.uname()
