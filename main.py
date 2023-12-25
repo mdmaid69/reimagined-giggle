f74c64de94ec5240aca1f2067a242d4154690dec
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:57 +0000

    geton nervous not takeback room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def reverse_list(lst):
+        return lst[::-1]
