9308ebfd36facb1aff4963c51b3473e825e0bd46
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:47 +0000

    show far talkoutof shop telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
