258b8930f005c46896f4e083e1d3c0779b511eb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:14:11 +0000

    no spot powerful spare contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
