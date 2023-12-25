9d5ca432d7141cbaf4683c9cee4a1c294fd9575a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:28:11 +0000

    thankyou win regret keepoff write

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
