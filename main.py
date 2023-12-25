749f28d2cf5dfe44b34e3d6583f576468ef68295
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:05 +0000

    satisfactory alone deliberately cook want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
