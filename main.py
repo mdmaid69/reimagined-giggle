5b4659b41d76aa3385d97abd4d995b1e29867d05
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:48 +0000

    way turnback juice worry famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
