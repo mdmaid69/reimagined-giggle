1882023125ab206067955732ecde746604201a4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:39 +0000

    visit sink branch flow year

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
