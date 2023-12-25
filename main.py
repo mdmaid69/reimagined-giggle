0222c11e3d3368170cf9bdd73b71e7f59915e0e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:21 +0000

    guilty sad depend drive ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
