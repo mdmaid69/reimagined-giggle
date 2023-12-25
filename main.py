5b98e77adb31534651e9d12428f8a1a2448309ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:41 +0000

    window crisis hospital clever under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
