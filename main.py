4ad221d20afc79971ff6d6818387231a35c8bad2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:38 +0000

    firm phone warn drawing not

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
