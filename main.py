4e9fd857b6fef0293abfe86a692164c8cdf99aa6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:41 +0000

    before list second very rich

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
