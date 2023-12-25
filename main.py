0e00c1620c82a3f88b4407196dbfe549f4492f74
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:47 +0000

    Jj confuse slice opposite lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def multiply_numbers(x, y):
+        return x * y
