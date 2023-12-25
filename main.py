075fa99b1955184ecbaf14a36bb50d76ecaa1f34
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:30 +0000

    spray square normal weather deliberately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
