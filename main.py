480ad3e44f22016ae64d7c89c064785c1956b437
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:11 +0000

    defend escape mine slow contact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
