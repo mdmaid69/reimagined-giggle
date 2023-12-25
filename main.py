3716743a96a81418eb7ec2da15b7db2f8dd03ef8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:39:48 +0000

    die practical mark satisfactory takeover

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
