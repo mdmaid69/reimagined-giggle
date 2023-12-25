6af5a4bd3f3d8dfa038d34ea07c6bd8442e12bdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:56:45 +0000

    with mail education language just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
