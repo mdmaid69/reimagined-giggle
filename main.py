73b6ee71a5c0ab93ee41f4af8657de291f349953
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:06 +0000

    convinced please unit switch confuse

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
