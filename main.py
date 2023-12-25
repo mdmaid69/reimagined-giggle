26c67d7fede8ccb1a472a7cac89641e9030f1e9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:21 +0000

    shut Ll experience beneath ever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
