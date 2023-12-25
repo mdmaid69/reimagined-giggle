2146073ecbe5991b8a8ea4ecc484e9d9e61e2aa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:26 +0000

    evening acceptable read awake forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
