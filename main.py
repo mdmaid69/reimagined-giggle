05fde3708a9a9cd6f0e764e84be2758933cee773
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:40 +0000

    chance raw please fall clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def sort_list(lst):
-        return sorted(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
