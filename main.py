480f5c618067cce53b61ebbe9a0daab87d08b708
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:50 +0000

    borrow callout peaceful runaway swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
