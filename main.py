480544a8e30e6592d25868561aced1b75350ede0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:05 +0000

    pain distant breath have in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_current_directory():
+        return os.getcwd()
