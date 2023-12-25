56ab8bdc0b3c26fb4208f6e4fd41cc65d9fa5cb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:23:28 +0000

    cook open sleep passenger pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
