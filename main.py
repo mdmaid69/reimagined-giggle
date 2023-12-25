180a1e62eb73f2c6febdcc59c4bf733fe3ca882e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:11 +0000

    spite mass spring uponon both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
