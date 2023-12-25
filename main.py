236152e18ebdacd3fec14b5bb130dca6781cba32
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:22 +0000

    how club name fill marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
