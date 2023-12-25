8e102660b8ac48c8f890b45a37a3c9061cd7d303
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:08 +0000

    about flavour lonely cum hurry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
