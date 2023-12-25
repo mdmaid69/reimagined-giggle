f2b5322c245cc47b521e30bc50cb8bb68f2825ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:46 +0000

    list lesson miss great swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
