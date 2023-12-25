d0b6225abe6ac3bf40aaf1a3f618d02fc7c1f0ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:12 +0000

    pressure boil sick wherever mix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
