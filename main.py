716297d34636b42ef56695c954cab5c109a11858
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:46 +0000

    control plan act nervous speech

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
