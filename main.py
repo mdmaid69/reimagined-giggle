258022822b4cf99b65097b0500a36c89ae6aeb31
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:59 +0000

    criticize bringback hair experience string

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_repr(array):
+        return repr(array)
