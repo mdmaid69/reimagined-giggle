303111513a9a583e36af85d051fcec89cb741f20
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:36 +0000

    affect common character clue welcome

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
