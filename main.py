19a4f1c038a6804e9a178cf06a7d10f580022ea9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:23 +0000

    genuine himself change fear fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
