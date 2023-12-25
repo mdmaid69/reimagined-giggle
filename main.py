cd6c35cd97e104d05cda8b97858f27f7840082c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:10 +0000

    piece straight turnup frighten whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
