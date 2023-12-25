7436df7c5a33b9f56c1e6cc6c08030b56bd1afa6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:41 +0000

    emotion last woman Ee callup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
