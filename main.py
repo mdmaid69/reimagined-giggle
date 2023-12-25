c3d5eaab6a8dcae2f7ac1078f050931fcd5a93a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:24 +0000

    scream tense argue attitude smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_bytes_from_array(array):
+        return array.tobytes()
