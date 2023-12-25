9510f2f382f010235d94ebb23f50376d9b9b9d7c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:56 +0000

    deep fight holdup expression ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
