bb8196904c54584eff4f31a509578f97f7471083
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:03 +0000

    meeting cause impatient awake change

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
