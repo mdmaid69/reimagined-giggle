35f69e09b648d5fcdcbeea0f2db2e9e428d1db7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:11 +0000

    cry near relation sit wet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
