e914dc0e9e07e46b156553d17529ff80f051f5e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:15 +0000

    circumstance concentration bringforward cut share

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
