2d957b1c4c00cbee3624cbef0fde8288f11921e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:21 +0000

    label away refuse wait build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
