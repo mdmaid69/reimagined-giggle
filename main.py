488855eae1fbf1882f1a80ec0af761afe5bc26c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:51:35 +0000

    gift valuable original noise turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
