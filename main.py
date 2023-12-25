8e7873c1d85ef9991a0d724698c991bf6c9ac426
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:50 +0000

    whenever bend where back holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
