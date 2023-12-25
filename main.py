c94b7d5c121a9cb7123f44e0a093d21b42c9b088
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:07 +0000

    mistake forever each deal street

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_rectangle(l, w):
+        return l * w
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
