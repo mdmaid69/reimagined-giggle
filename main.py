e08ea97ddf49442db698c4ec5fcfa8f984169ca9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:43 +0000

    available main yours skill pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
