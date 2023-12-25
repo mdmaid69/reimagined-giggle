a0806e60a4fec2f1266cff24fa439c6e31f021fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:08 +0000

    exist aim present chief mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
