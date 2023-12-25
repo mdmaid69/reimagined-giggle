7bf5696b14d5412512252d6c272de165ddcaabc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:22 +0000

    moon off below only aware

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
