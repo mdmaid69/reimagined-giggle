4bf9b69e4509dd8cdf91ede049e29e903d86137d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:14 +0000

    different Ff wide bet clear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_tangent(x):
+        return math.tan(x)
