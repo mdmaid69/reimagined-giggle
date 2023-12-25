09640d09de16c4d475173c2ddfba35f828f4f385
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:30 +0000

    argument large do mistake minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
