0aca38aa64a9fa39e4581b24c0fe7db5c2dca9e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:40 +0000

    important Pp confused hit no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
