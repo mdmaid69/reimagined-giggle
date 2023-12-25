97f8be76b7d99e93710de7aaaf3224f4a72ac06d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:49:43 +0000

    connected remain hit you telephone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def convert_to_hex(n):
+        return hex(n)
+def remove_duplicates(lst):
+        return list(set(lst))
