0b859cc34fdcd893eec3845b93caaecbfb5341f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:03 +0000

    minute reliable country strange spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
