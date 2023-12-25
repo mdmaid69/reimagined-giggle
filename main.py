785e09ae4b5bdac94dc2c4c4d6bc9bcf8acfcfc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:33 +0000

    bunch tidy involve street settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
