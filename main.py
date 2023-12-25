f44622b5c49ef3d1029cb13b13fffcd5a2fdbb13
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:44 +0000

    grateful week taste mark consist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
