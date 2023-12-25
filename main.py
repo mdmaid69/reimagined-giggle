6dd7d2b8cad5e2adadc4b81102f5833d648b3039
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:09:20 +0000

    board switch confusing tap only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
