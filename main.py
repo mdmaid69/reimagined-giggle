68498721ffe7bb7b81a3b0dbc3c91a799692efbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:15 +0000

    workupto programme aim advise near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_bytearray(array):
+        return bytearray(array)
