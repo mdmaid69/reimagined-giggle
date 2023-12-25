2b18ef4b1cdb70df0f3e2fd5d4480629344c8a67
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:32 +0000

    past noise opinion past tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
