3059b2df20c4e4c940b426ac07947edc185c21b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:31 +0000

    within refer from name against

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_acceleration(speed, time):
+        return speed / time
