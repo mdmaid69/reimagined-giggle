1bed1d0cd671c188cf647600760232a829980e6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:18 +0000

    bin somewhere balance book solve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def is_even(n):
+        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
