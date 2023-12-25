b8be5bb6dbf8960187d5b2c5f9b5d4d245f05c57
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:46 +0000

    land cook limit eager contact

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
