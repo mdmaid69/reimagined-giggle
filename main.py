34392c32855e58b85602cf9c14d71346f21dcc48
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:21 +0000

    complain schedule interview clean uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
