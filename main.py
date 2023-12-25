98fc998b07a8ebb5627727bf0c93ffda6f63ea41
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:04:49 +0000

    turnup row connect yourself thing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
