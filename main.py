7a2d29baa8bfeaf593b0d1d396f909927ee1881e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:43 +0000

    cover we complete heavy month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
