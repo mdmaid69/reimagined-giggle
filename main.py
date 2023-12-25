389c6e267ba7fb9126e5558a912680ea421d158b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:23 +0000

    fill trade nigga once weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
