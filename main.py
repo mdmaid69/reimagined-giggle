fe97236f6d90560569bc1ec988a747e76572e311
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:18:20 +0000

    road concern rid no now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
