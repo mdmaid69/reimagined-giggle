5da6b0f3037cecbebc4355e2f4cb27cb7fa5b21d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:22:36 +0000

    command rest admire straight Qq

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def remove_from_array(array, item):
+        array.remove(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
