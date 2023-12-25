9dd3f23d2ebc14f93988235da6e0325c7bb60aad
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:20 +0000

    order nurse speak tear scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_os_info():
-        return platform.uname()
+  def sort_list(lst):
+        return sorted(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
