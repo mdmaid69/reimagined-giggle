e6dbdbdcfb43c4c99f7ca6b63f38494f12ce4610
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:01:45 +0000

    imagine mail quiet square stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
