b6b6c6ad53a56b00e7bd66095e3876b1c99d892b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:43 +0000

    keep emotion sudden Nn bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
+import platform
+def get_os_info():
+        return platform.uname()
