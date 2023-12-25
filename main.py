b1047ffb1f98004bf3f8b52f1628897e45d1829e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:03 +0000

    pin come load 570enjoyable face

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+for i in range(5):
+        print(i)
