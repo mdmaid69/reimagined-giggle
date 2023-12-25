6c44a944d9b42d55c84dca5b7a4549f5271bae86
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:48 +0000

    stick note string cross convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
