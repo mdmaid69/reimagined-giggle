86d630063c4604004994b841b9da2576fa3491ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:50:07 +0000

    drink comment bunch influence bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_index(array, item):
+        return array.index(item)
