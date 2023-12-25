02597d962e8cd5af06e3184356246b78612edf98
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:32 +0000

    child test wish compete there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
