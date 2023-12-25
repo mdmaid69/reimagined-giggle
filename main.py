ec8d04cb3a0e5ae2b2307ec117673138cc5021d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:43 +0000

    between tradition term moment glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
