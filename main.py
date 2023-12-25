5a021b718d217cfae7e0ed25ebed1034f5cb7336
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:30 +0000

    taste suggest ring evidence stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
