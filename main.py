28692234ab9d998314d4349f45e6592c5557315a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:57 +0000

    quickly nowhere collect print worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
