11b197f6418cb69da97217cf42b9d077228db279
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:45 +0000

    close proof offend boring form

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
