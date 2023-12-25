f6b03121d82850df9a93f4f10ca3c1bf16e18531
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:47 +0000

    prefer snow stamp power Qq

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
