2ef07ed16fcef5ad2774e2d24d33d3844a5f515d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:39 +0000

    variety sing separate part common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
