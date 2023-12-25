303363dd455d27e0dd3f72b740691cf84cea6052
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:28 +0000

    either later married believe rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_str(array):
+        return str(array)
