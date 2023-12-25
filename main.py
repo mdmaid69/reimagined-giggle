e8b15a028ae3137778a83eacbcaab27921d32a84
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:14 +0000

    first worth bear rare complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
