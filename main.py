5cf14eae50145ce2dd1e2d8fff23b2ee2f25ed9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:24 +0000

    excited same talkback ear style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
