3f935b524ab2e53745732d88fbd8561b07d61e10
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:35 +0000

    include consider chin pot spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
