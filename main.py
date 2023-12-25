682a49f530221402b03a30648ce48edfc3bb7f2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:04 +0000

    commit Kk big below picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
