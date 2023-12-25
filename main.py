0bc27b3a0030d3db9fb3762d405691943a49084d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:15 +0000

    buy pretty fast hate child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import random
+print(random.randint(0, 100))
