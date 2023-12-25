1a6d8030fe54a9c692ee90763d6eea0c61a73c03
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:52 +0000

    female level build foot already

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
