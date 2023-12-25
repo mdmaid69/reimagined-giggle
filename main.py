17e018d281d8c85e4cc666c072968b237d882f88
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:27:09 +0000

    may professional answer typical imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
