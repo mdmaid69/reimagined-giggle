6c66c4074113fba942872ae6ffb4b2fb4279b192
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:01 +0000

    reply possibly speak turnback town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
