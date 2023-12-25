c0253c0f0dbd5fb07d3b89b127940f7a8f787d46
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:28 +0000

    balance safety information weigh smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
