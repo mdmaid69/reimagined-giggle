1b4a85527ad4ca0f61985128ede5b4811122867d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:08 +0000

    culture deaf confident hurt chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
