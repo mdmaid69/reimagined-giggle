6e94854c3f0daad08354f0184925c24ace9be426
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:29 +0000

    set pronounce far habit strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
