121b5e22b82e6f5c53e825da79f37695e30a18c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:31:14 +0000

    confidence holdout fail 567enemy slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
