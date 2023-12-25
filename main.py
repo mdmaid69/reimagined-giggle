8a44b295b55bf7e6c2a47fd82ca1209e1f8982f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:12 +0000

    criticism traffic turnoff themselves offer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
