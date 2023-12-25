ae35ef48a67941ede8677a8ee5fbb147b943762b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:34 +0000

    regret shop medium store gate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
