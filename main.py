9f072ca1730c3e92a68555a02f49870a9343e58f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:48 +0000

    pause smooth normal stretch immediately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
