775438707a08552c30148fd372ad7118937bddf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:52:30 +0000

    exchange ticket makeinto below total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import tensorflow as tf
+print(tf.__version__)
