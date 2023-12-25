12cf5fe6e5952fd682c569f4d5ff8c2b7e52522c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:06 +0000

    regional poison goout memory substance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
