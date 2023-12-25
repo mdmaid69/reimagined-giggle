0ab3458e39c1b148869218e6e26951ba4403ff61
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:00 +0000

    left float quarter live nice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
