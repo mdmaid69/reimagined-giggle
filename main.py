352cbe01c6ed9ddd98ba0d0dbbe7e203a82a2f29
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:14 +0000

    also moon turnback comment that

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(10): print(i)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
