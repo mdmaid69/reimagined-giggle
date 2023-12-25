32a7536726cee4d28d80226ed79382fbddf9fc2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:57:03 +0000

    frame very forever thumb afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
