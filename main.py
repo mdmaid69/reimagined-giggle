4ce83c115315de01418b4f76bc0ac1515ca7013f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:27 +0000

    train callback start heart takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
