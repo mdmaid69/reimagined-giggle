2dbfeffd958913d81a6f514e0cee64a81f506575
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:42 +0000

    agree recovery ill than discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
