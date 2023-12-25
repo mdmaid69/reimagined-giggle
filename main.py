2849e02197ccc38217da5a18bba306b3dc092e24
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:10 +0000

    interested separate ground cause late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
