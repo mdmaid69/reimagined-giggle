28487936a78cfee20ca1579e2bd39946d2002e90
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:07 +0000

    interesting attempt backwards favour opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
