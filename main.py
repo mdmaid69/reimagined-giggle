8300fa5a0b08f9c4acfbb4a8ff8cd197794234e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:49:20 +0000

    float hungry work deaf contents

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
