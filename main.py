7c0f54f8f203902d8656dfff4670f3eda5aba2b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:14 +0000

    little by illness comein one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
