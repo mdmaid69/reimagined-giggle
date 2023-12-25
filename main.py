da71ede44381bad1546258195c8cbc6688d0c2cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:08 +0000

    rush black strange hang after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
