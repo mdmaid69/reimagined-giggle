203a9a1850aae8e464b6e77168173aa14b8a8b2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:58 +0000

    fight occasion opinion spend land

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
