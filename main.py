01dfe6719f6f2b1ea855931316e245b12be2025a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:43:51 +0000

    most press who care unlikely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_speed(distance, time):
-        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
