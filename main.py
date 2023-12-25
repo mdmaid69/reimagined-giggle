eaa2d48ba5de46004452eb86bac58ac5af00eda3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:20 +0000

    score claim hospital trust become

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-print("Hello, world!")
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
