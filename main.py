1c153d34f80f3bbf2b9731b0c6cf2da42eddfe2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:35 +0000

    task stiff lookaround would shelter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def roll_die():
+        return random.randint(1, 6)
