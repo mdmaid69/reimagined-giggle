a3ae192e8dc9ff410e2f956a7c16dc8dff00df2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:27:09 +0000

    interfere think platform test if

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
