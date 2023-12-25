5cc98790d359e872f6aa7ed5b5ab50b6cb1c7e8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:45 +0000

    moral content permission ignore public

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
