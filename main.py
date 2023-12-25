1cb860701569d157d15c4b50d2964db952c9647d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:09 +0000

    mess lookup ability alone meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
