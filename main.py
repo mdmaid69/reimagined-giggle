41852b8dc31e9871bf2ad520a68e680d6d7a82bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:40 +0000

    expensive sweet may allow skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
