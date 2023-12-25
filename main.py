9955502aeb81c85d2e9ed2385c84ef9bb70a0119
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:33 +0000

    extra performance complicated order order

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
