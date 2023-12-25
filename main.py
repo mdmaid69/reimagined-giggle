86508a6fb42362a3297b1239af37f166db6270a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:24 +0000

    middle we side plant comfort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
