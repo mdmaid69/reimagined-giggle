5f390d5b0fc82af09f8d923cddedeef75330fd62
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:47:46 +0000

    mark sleep mix score future

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
