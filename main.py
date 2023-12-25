8d9e1d1c04077129c0a8627d2dbed9667937f2b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:25 +0000

    bringup talkinto performance influence Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_current_working_directory():
+        return os.getcwd()
