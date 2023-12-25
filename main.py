d7cfedc82ebba1775ea2a2d09d9183a33c8ce183
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:02 +0000

    better sleep into direct waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
