98117815bedaa8b2279442ed9154c7c2ca442f34
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:28 +0000

    face apply love think pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
