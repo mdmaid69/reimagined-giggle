ea732d0ca8d10896483e46a4da1f5255efdfa914
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:04 +0000

    powerful sport long arrival thank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
