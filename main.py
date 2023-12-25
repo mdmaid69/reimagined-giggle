38d62ae943d1625b1873749b54659547eb5523b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:04 +0000

    careless apologizeapologise believe apologizeapologise repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
