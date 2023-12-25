0b51e12cfb18f8fd78eda2f3d9a4a54b4aa0da07
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:10 +0000

    upwards but some if so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import sys
-print(sys.version)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
