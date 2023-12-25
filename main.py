469791569e00a287b361f604e24243f42f6b2f05
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:48 +0000

    disappointed common recovery argue ourselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
