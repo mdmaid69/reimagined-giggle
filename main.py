f7445ccaafe42dd6eef1a971d3a49eb7db3f519f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:47 +0000

    tooth care giveback Pp intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
