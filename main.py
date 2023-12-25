e3cd5afd4e1dacc1e8df9f45a052a0b8d59c168d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:03 +0000

    instrument practical both stand middle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def convert_to_binary(n):
+        return bin(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
