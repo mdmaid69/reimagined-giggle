afb99beceb30a352ece7553540e147cb930efdf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:23:25 +0000

    sudden himself typical alive party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
