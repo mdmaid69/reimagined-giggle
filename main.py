20b4c595574149e406733d0ab2b3be40c7770bff
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:13 +0000

    welcome used cheek greeting awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
