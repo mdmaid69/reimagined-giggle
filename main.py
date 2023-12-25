1c28371336db8a5d54b9abc10688153e75b94f74
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:03 +0000

    add persuade illness distant turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
