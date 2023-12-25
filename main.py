ac9bdc0fec5f947145dd3c2b5169b40600ec896b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:46:17 +0000

    something bus business maximum muscle

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
