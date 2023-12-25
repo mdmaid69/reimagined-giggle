46458918ecce91f4703d09746b37c9fc4a28ddf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:26 +0000

    nor squeeze much continue contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_work(force, distance):
+        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
