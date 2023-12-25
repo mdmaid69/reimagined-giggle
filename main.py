c3b83fcc8250918df30f3150b6bd31b3568530c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:55 +0000

    favour bus emotion clean school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def remove_duplicates(lst):
-        return list(set(lst))
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
