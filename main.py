fffa6336e632b671e5e23627846fbe412a1dac60
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:09:19 +0000

    centimetre time dry agreement copy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
