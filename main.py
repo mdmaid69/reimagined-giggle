92896e0694ede9c8512290ae7fb3544750f05d80
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:26:32 +0000

    idea deliberately he teach tread

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
 import sys
-print(sys.version)
+def print_python_version():
+        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
