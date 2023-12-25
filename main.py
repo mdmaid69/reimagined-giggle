9b0dd6d33eeddcd4aa932dbe04e050b228336483
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:52 +0000

    least yes key tense deal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
