6a0f2561dd784298bd04cb5493557dfe77fb49db
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:40 +0000

    clue wipe fun big error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
