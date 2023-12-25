4bfe3592ca2fcbb68f5a0e58eea58dfbc5af5d28
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:30 +0000

    difficulty pen ashamed practical bringup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
