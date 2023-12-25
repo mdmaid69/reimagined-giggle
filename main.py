c3e12bcca40495577d01474d37a3e335da24db26
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:00 +0000

    gooff minimum wire basically better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
