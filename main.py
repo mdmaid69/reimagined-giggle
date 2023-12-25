1f1bbaee7952285943fd0e95fa064ab87e47a3f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:36 +0000

    invite protest give sleep cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
