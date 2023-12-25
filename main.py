85954429e56ec8b6f587c07be49d3db6890563eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:48 +0000

    rent rush talkinto the sing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
