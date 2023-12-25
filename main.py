66daa0f4efdbc426c6a368bd89e5ec2fe9983009
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:12 +0000

    now trick poor name attitude

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+print(math.pi)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
