4913af8aca4e79a693f3ca08be73bcdf3310bc5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:14 +0000

    criticism comeround explanation physical giveup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
