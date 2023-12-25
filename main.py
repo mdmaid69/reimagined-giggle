9a98d730a832d0d68582b4544662aaabd437d463
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:13 +0000

    image lock damp arrive true

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
