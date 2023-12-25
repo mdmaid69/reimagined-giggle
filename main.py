66f8372a5e3ddac7115a57af86c70a4d1a6584b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:33 +0000

    acceptable keen I neck thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
