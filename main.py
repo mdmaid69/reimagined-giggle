fd4733a682f01e29149ce4347c910754a5808c84
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:56 +0000

    turnover goback(to) breakfast cost bag

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
