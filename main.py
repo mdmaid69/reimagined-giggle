629ae011919b25f17addc380f793ef95be5ffcaa
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:11:44 +0000

    win answer juice same emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
