b1077734502561e3b6ac7f30d9f71eb2c7540289
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:02 +0000

    male well hungry air lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
