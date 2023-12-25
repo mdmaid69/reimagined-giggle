fa7eacf63a8be5b99a5a33b97113b9f6e8b4f3ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:07:23 +0000

    since but flight justice ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
