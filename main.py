81f26f093059066ff705de8aef5e84b226020336
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:30 +0000

    spray minimum address shame weather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
