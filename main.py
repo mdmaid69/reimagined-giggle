89e8b5dde44efdabefd6a1889a155323565385ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:44 +0000

    exit sharp load wrap example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
