c8f76f2f1046fcb184b524b27dad2b21ce03e830
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:48 +0000

    serve except everything minimum at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
