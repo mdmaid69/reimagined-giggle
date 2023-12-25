7514a3a2a12e86bd04ee4b3a37b314077fb8e252
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:50 +0000

    impatient fall roof shallow like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
