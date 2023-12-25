894eb6ba3e652f06eb041d2417124dca0a9e6cb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:32 +0000

    complaint you appeal getoff sign

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
