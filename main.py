6be80e3a09dc0cb9d4b20e33545dd46e5573358d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:18 +0000

    reply pronounce before meanwhile 569enjoy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
