a6989f31982c226f583ffc57ad368d4cde86de43
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:58 +0000

    whenever rate funny state OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
