3047cb0bfd5220fa8736bb981aeb46225e66c4de
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:51 +0000

    make regret amount square experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
