cd89c227081fa874d89ea64fb78a44426669edf6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:00 +0000

    scream appearance later goout fierce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def cube_number(x):
-        return x**3
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
