547d0ff453b25bdceca316641e3663fe6e8a0b41
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:36 +0000

    wide trip pain arm expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
