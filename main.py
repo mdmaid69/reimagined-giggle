352c3f24b167f411228baea4cccd2abdd078953d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:58:34 +0000

    callup threaten straight control respect

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
