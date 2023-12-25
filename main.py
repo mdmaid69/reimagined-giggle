529e624eb6fc2529ac02d2032cdbf4bb73d10d5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:22:03 +0000

    content scheme Gg shoot march

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
