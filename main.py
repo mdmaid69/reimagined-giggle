8e085aa0742813e714017c42892b36cd84de59f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:14 +0000

    Bb affect wire probably tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
