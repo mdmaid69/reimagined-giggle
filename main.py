99b89745fc85b3448c71d494d55f7fd71f3ea88a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:43 +0000

    float division cross point season

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import platform
-def get_os_info():
-        return platform.uname()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
