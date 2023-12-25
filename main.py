8d6ac1a17ade458a9a279901f9f117e80dc69284
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:11:13 +0000

    open dangerous refuse delight upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
