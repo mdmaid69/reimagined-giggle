265410b71229605b7d01471801e1885302184a92
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:56 +0000

    aim demand permission noone interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
