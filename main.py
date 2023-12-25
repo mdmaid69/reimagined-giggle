18dfd3bd3280f2fa485e7d6472a7cb26b2b5c5cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:40 +0000

    regard ready safety reduction connection

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
