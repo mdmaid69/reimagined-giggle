15726d9645bde2623b6cd75a9bf80ae7a5c63bc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:23 +0000

    involve birth independent surprising somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
