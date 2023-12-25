95ecd16ddef1231ada348a6aefac09e1539a4e9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:44 +0000

    himself callat forward over not

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
