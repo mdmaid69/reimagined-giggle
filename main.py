1f8956aac4d6e5bec25fbfbfd1b4b033525dd9c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:47:11 +0000

    Kk would approve sort ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
