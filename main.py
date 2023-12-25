ef585d476615dca44bdc8f4008880b7eb7e82793
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:42 +0000

    fact threaten switch order exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
