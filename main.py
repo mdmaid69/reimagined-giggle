98c5746dfb273ed3add5ab3546f6fdfb0a34b84a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:07:33 +0000

    correct content wear involve perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
