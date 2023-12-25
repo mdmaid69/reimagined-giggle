555bee1900de8047cda724549b617638e5067522
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:02 +0000

    cry hour glass please related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
