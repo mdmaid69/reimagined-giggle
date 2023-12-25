b8ea2169d7fa184eb05d471e8ad26af534eb2d62
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:20 +0000

    nowhere rid for rush peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
