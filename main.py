843178c6b4ca334b50fc940e0c7e790f992fddee
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:23 +0000

    wire favourite dry true same

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_to_hex(n):
+        return hex(n)
+  def sort_list(lst):
+        return sorted(lst)
