e97276f6d78fc7eaee76c3cf514e3f24abfcb71f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:02 +0000

    theirs alive interrupt advanced match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_work(force, distance):
-        return force * distance
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
