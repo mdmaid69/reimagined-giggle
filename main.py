6c74d98365988a2992db67cb91e90db7bca2a5c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:26 +0000

    exit connection name room affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
