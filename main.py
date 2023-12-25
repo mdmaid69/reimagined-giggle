c887040f4be8a9119698eb36a49463510c9aac17
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:28:03 +0000

    fit production flat lesson well

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-print([x**2 for x in range(10)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
