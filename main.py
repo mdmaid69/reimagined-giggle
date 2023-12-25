8c8995e5c1fef58cd307f74964134fe3a2462dbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:35 +0000

    absolutely pause child turn supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
