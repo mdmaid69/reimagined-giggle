f82e46a93292e07ff8d7b9a8e69bda79e2d95aed
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:23:44 +0000

    rain story suppose calmly open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
+  def convert_to_hex(n):
+        return hex(n)
