2e3da22692166e3876b2e435addf04727a1ef283
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:09:38 +0000

    number foot constantly advise other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
