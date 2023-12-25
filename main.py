873ba6b9c178a69651fc45e117aeb513505d82e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:08 +0000

    material together measure cum pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_work(force, distance):
+        return force * distance
