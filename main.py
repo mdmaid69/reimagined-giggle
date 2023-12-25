903d19dd0f8b559d878aac1d89e0d5d7ba5e6592
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:43:42 +0000

    break fork thinkover load stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def move_file(src, dst):
+        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
