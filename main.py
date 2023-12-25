e04e055e2f0b5f13a06f4fd6052e89e7e0204aee
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:56 +0000

    gooff greeting pressure young fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
