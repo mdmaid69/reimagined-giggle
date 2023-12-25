9bb4613e4e0af3b284729c1d97fea8ad3e427e0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:36 +0000

    talkback share rather about uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_string(s):
-        return s[::-1]
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_length(array):
+        return len(array)
