416a156b140c7f44e7e96b1e55c5873150052996
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:09 +0000

    cheat every decide course normal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_density(mass, volume):
+        return mass / volume
