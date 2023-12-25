19b80177664dafe8e33821bf4b3a2fc83f6dc9ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:48:52 +0000

    point thanks float direct on

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
