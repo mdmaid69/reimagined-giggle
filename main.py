ceb6b070976b16431102c0cda6feb5800d76c247
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:21 +0000

    rich ball relief impossible bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
