5e6e1593b32d8022f8b0f25adae2460e493b46cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:23:21 +0000

    hand forget congratulate father sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
