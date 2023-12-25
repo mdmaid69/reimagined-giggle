457b3e76897142c6a8d1a89c17012bc7859234fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:15 +0000

    achievement delight age best smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
