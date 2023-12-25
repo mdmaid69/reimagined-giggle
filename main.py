a9cf5476b38d89a7064fce9ec58587a484bf43ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:05 +0000

    edge consult wherever through describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
