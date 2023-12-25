ba31019eaa464ac932e0eae258be8f6c788a1113
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:05 +0000

    insist march murder shut 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
