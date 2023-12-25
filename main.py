f7be922b7965f1c2b39dd7fa8dc1cf1be9d21702
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:37 +0000

    year Kk street correction formal

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
