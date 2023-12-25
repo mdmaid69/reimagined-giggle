22f58be19cc6677a98bc6b5141249b4be8ad51d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:34 +0000

    trap system minor raw quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
