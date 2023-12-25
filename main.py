07d576c6d8e6ed44844f99ff14ea895d81ae2deb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:58 +0000

    tone future glass statement excellent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
