ff19940e96ecb94b0f75e1d555516cb706a7c170
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:04 +0000

    help unlikely cold share wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
