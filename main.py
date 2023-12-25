8814bd343db8dc90e91b58fb651d233843e6a6cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:12 +0000

    fence everywhere major detail plenty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
