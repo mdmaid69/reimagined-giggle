4f9272bc33c86ada1755c3bed4ca4891f41e1499
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:26:35 +0000

    combination limit back fit mystery

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
