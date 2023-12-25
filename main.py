96d49adcc7048b5609f7505c5ede8d5494cdf032
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:17 +0000

    unpleasant coat borrow look maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
