c4901ab62e78f9bc20dacd550d0f3aea2818c533
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:46 +0000

    single of platform relationship connect

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def add_numbers(a, b):
+        return a + b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
