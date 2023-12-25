e03ada4579da305a6c0f94915a5a9420a9f6858f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:51 +0000

    feature official stop bend strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
