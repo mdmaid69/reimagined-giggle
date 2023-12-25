8b8f2247e6b11d092d9a9838276d9eaabdafd3de
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:25 +0000

    work chance crash noone country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
