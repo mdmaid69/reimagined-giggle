82cd4b5d25fcb0ec16f1193aeeac743efd2ae0c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:04 +0000

    thankyou Ss film near pity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import re
+print(re.match("h.*o", "hello world"))
+  def convert_to_octal(n):
+        return oct(n)
