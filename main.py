cf1779352648853a28609f028cd3e23f8d0ba0f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:41 +0000

    turnaway mother difference dry purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
+  def reverse_list(lst):
+        return lst[::-1]
