19eb7fbc77bf85c208ddb43ff831b3ad65166842
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:47 +0000

    available guide card grand ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
