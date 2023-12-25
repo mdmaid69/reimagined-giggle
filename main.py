037051f9fd7f4e6a9fc5f25ebc68507389fe3075
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:46 +0000

    arrive charge confused regret sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
