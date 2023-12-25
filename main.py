2835a5d9cd10e376161c1055bed91044dbb8385a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:38 +0000

    pretty weigh label such sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
