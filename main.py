e19d8c076d4cd66be48a005d0b96c9b3f9ff1819
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:41:45 +0000

    either group practise separate call

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
