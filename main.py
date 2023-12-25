0f03217a2bcffa91b42791022f4cbf1aa0f6da07
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:58:02 +0000

    please poor holdup important flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
