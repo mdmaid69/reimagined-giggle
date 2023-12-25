eff1beca129de1279093d670c1eba25012e69e4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:48 +0000

    fix talk steady other rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
