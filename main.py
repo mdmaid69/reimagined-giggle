42fd7ae915b8db455d294191e3478f0dfcaabf69
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:14:35 +0000

    popular herself change unusual water

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
