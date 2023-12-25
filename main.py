5fe944fde38dc43f26cb44bdc2153fff6733876f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:32 +0000

    detail activity wrong getoff fault

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
