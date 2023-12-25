25ac87f4c9619b1b235ac95bb25404af92fa4592
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:58 +0000

    week go make character stir

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+print(datetime.datetime.now())
