a09724117db6a4cea9554f945293b01849230f69
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:47:33 +0000

    draw development reliable sharp crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def remove_from_array(array, item):
+        array.remove(item)
