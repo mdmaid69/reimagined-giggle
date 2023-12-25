ea34068d24fc0616282e467dba1e5b78030215fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:43 +0000

    afraid beginning just married back

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
