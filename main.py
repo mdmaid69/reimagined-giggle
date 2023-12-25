c7211c0b0e79664d4459c44d326cfddc08c0f3cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:16 +0000

    coffee answer night independent ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def count_elements(lst):
-        return len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
