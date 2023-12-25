207e1a6f277cc9b1e2c57a2b52bafa4bdeee41d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:38 +0000

    backwards unhappy oil Kk bound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
