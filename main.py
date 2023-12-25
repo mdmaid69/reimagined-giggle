1bcdb4d80ba63f9fe3fcd8bea30cccebfddb012d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:31 +0000

    workon voice ability rest bottom

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
