0d6e97b76c5215a2544d115c7c77ca8aab5055c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:19 +0000

    only turnaway port nice clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
