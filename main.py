2ac63684e78945d5154ab94e13f84d2b627272e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:04 +0000

    quick immediately public godown practical

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_pressure(force, area):
+        return force / area
