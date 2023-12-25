eeb0ce939bd3d1072d6b897c53a066a8e91256fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:10:00 +0000

    wake draw thing welcome situation

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_array_to_list(array):
+        return array.tolist()
