6314cad352466b72a8e3a25640a6fa1c780a9a8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:11 +0000

    effect lookafter soil duty involve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_max(lst):
+        return max(lst)
