d300179d8cc8d386f43feb1d88473b75072cd500
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:51 +0000

    contents term balance television collect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-print(os.getcwd())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
