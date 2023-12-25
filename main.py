2633b93cff66b9ff8002e6d2637a89f836313f7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:46 +0000

    reply progress hard off swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
