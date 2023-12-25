7857ab893e20a0f5b85b6c187d9f174157c5780e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:08:13 +0000

    learn respect goout edge suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
