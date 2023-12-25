159648bb747165feef715af3a5aeb6bed25217c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:27 +0000

    better glad 570enjoyable medium that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
