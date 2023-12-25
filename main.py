ee89491651d3a0b15b8d04d55ba8eaf968bae741
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:44 +0000

    clever everywhere cough makeinto holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
