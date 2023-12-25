08455d1a5eac4cd81ff2b5f74e18aebd2ba9bfa0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:51 +0000

    pocket come perform between consist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def multiply_numbers(x, y):
+        return x * y
