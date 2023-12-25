bfb674f356d06685f2a57a6c28e06b405e26b827
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:57 +0000

    actual extremely late anywhere shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
