83604e2ded96d2383e1c4049ccba459ea2751866
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:22 +0000

    rule woman motion out neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
