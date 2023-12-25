0d8fe2b9556d784b8d752bacff65145facaa68cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:47 +0000

    preparation mention hole brain piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
