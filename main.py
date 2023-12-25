122e1a12bfdcfc4ec5b7ed69faccc710760aeeda
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:53 +0000

    spoil size giveout name air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
