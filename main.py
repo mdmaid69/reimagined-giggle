872648c6fe8755a535ec2ec26dc9d92d231420c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:37 +0000

    talkoutof me share squash unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
