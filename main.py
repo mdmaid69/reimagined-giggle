a80c1a10fd9dc7a3881051905d4751c75fb479aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:00:23 +0000

    breakinto seriously dig different guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
