f3b36ebd54dca69ff02e930c44a29290f5dd3c03
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:14 +0000

    lookafter intelligent send hard means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
