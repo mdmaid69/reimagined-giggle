19a142cda253ca52b4d8c7676cf05a93cf290e79
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:09 +0000

    fashion sound do necessary ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
