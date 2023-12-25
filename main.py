92d88fb77403dd1c5dbbd5a2450b34c732a90a0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:16 +0000

    excuse 564encourage transparent reaction plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
