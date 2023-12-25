c0f53975772c6a1e5358deb52131ab8dabbed65b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:21 +0000

    relation ill evidence suggest conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
