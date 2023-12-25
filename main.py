16200af46eb62d83431abe37dd26998e867f9ea8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:42 +0000

    confusing reach command gather bury

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
