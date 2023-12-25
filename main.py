3d9c4df6fb5de386f0e4ba7378f7048fa9861392
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:04 +0000

    tongue name catch less path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
