3279fe18c3c7231ff328b740ca2e2b79c5d834b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:05 +0000

    task rest fairly responsible general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
