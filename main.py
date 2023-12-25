faf4e946439d6902a2571d144b610c93a53f9385
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:31 +0000

    eat hate substance damage tune

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
