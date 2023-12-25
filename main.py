668c5e560a3c77d209781bdb2471204dabb50b2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:51 +0000

    interfere nigga preparation runover block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
