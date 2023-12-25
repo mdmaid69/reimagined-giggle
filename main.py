2ef42ad57caa259559e29123ad26e2f64922d459
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:21 +0000

    many somehow pump than somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
