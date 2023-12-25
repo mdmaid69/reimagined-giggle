8aad0c2304dae42e435a9030c357b30b860e1a3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:33 +0000

    into reject explain dry eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
