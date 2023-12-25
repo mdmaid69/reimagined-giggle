d4087f255d89f96e70d1dcd95881a1c11bffbbd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:09:49 +0000

    confidence section confidence really confusing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+print(os.getcwd())
+def find_max(numbers):
+        return max(numbers)
