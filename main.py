ee19b78580214fa737bf91c26d1330d862f672b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:40 +0000

    fond regard day risk help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
