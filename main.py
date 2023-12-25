2fb0cfe6dadeada0ac8df8b42a566e87b4c44bb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:02 +0000

    plain stay allow would behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
