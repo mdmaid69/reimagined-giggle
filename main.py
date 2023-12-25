bb1763053c3125c9549b9ab3d9d96cceb22a5834
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:13 +0000

    joke tradition owe relationship float

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
