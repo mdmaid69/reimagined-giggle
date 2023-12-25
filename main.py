88869d602b20b1c0c4d79fdb0c854ef3a3088239
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:40 +0000

    drop brave breakup land calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
