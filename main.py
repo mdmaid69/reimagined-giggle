22a0599990d9d87a5bab061e34f8ed706ffd2be2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:12 +0000

    skirt main comment late how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import numpy as np
-print(np.array([1, 2, 3]))
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def append_to_array(array, item):
+        array.append(item)
