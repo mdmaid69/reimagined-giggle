3b4caceb169e75f78f806ae41f284fc96a1cae85
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:20 +0000

    careless shoot maintain getoff comment

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
