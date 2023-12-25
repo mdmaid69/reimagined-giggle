61794257c3c95265e6ad2434c05706c64281dc1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:07 +0000

    prove lock politics storm happy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
