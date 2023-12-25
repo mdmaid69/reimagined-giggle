cb0584a33b2bc74e4a5839edaafb3e2872d46495
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:42 +0000

    peace instruction question moment spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
