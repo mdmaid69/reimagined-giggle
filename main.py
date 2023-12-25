0dea029d40c3b0f0af48370996a3cb5dddfcb0ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:04 +0000

    ring goout of soil throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
