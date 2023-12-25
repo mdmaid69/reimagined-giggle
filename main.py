70e27a813369df08259b806dab344e62770c6b3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:43 +0000

    Dd right bright material reasonable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
