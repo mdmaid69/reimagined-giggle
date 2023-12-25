650cc0e0cf851d2d6bbf3a68417b290601673bee
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:07 +0000

    backwards traffic drink future young

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
