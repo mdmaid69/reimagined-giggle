8b9870190b7c18303af6fcfb0d48693983c5fd25
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:08:23 +0000

    taste excellent new loud birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
