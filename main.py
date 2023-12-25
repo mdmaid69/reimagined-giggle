42c95b1ab927a4dd7c46507f5d2dab99e90b3fc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:41 +0000

    project affect cheek likely follow

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
