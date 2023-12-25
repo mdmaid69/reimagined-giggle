fe042284cf53ae18440562da95e4209bf4d5a164
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:16 +0000

    nigga attention wear business birthday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
