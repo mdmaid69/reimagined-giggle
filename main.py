7d1855ecc1a253581f45d77432e2faf74db2945f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:19 +0000

    attract relative terrible history admit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
