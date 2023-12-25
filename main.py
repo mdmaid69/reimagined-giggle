d230e91ed2b427ffff533c57b318b621090f40bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:32:50 +0000

    develop contrast happy score transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
