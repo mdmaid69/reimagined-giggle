516d6d1a8142a24ec319af258c27e65727dbd87e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:42 +0000

    tight mix criticize size material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
