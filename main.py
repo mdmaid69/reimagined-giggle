09d10db6ef8548578d1d407d0d24e0813dd4b1f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:45 +0000

    Yy wave press confuse slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
