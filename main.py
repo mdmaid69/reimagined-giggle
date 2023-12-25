e48b2d25277045701262cbe4d3406af34823c413
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:59 +0000

    Qq creature typical birth pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
