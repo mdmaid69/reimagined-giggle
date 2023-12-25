aa3de23f341a88a1967ae7757a717e226165539e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:43 +0000

    Nn lookafter deliver person shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
