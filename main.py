f238754efcd37faeec20c3131ade0caae7e063a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:26 +0000

    Rr Cc ashamed also handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_max(numbers):
+        return max(numbers)
+def find_max(lst):
+        return max(lst)
