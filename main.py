69bf4f3892be3ef8001d668749ede0cac70a6de1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:04 +0000

    suit contribute punishment unusual deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_volume(length, width, height):
+        return length * width * height
