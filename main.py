5c58d2604126fbd3138484ed24894b2a3dc262be
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:48 +0000

    speed normal basic Yy moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
