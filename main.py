aef035860a484002d58153cdd2b9ba9bcda7616d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:06 +0000

    grand breakfast manner table collect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-print(math.pi)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
