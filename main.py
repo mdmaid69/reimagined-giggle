f297112d319cce82a498155a99384ee696fea537
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:10 +0000

    surprised pronunciation noisy community something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
