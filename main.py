4de56ac7024c6c91fd336bbbb2eed5a8b0865180
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:33 +0000

    cross distance flat swallow or

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
