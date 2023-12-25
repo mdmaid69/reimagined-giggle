3b6344421052d7669740e7c058c478be9b5d656d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:15 +0000

    design statement only against contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
