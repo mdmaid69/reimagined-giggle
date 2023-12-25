ee2528381d439feb5387abdf1b13ba969f3be406
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:32 +0000

    them complaint turnup ignore pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
