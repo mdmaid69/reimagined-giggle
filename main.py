a8233d301d53eff38fc393dfbc1f168972935d9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:59 +0000

    571enough software fast dull transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
