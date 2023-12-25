a0de5b1c2c42967e23e71cc98b4fdaf2c5600b26
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:53 +0000

    finger love over blue minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
