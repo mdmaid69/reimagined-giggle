f2d7b46ceffe5035d52cc38f5c2c510fc7761632
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:02 +0000

    original eye subject tidy sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_area_rectangle(l, w):
+        return l * w
