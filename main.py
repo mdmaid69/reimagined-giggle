376f912ff0e028ea1f78401ca49999ad2a3287ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:06:26 +0000

    reply without passage unless shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
