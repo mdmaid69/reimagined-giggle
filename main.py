0214c5e830ddb06d63f56a83203d3697eaa405eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:52 +0000

    turn everyone share passage nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
