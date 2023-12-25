e99253d8683646a600a8f7f290957aca4f131232
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:47 +0000

    holdon tidy float contrast certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
