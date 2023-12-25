89446c2dec33e8fdff1e43de4c077b23a67467cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:29:27 +0000

    far Uu think strength clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import json
+print(json.dumps({"name": "John", "age": 30}))
