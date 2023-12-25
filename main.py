064dec3f35b0163305ca226213805d53035d29e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:49 +0000

    constantly spread wall plenty across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
