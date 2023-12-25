6b3fed341ee553d9be2f1a9c9f45beca18722e1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:34:35 +0000

    myself moral shut surface pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
