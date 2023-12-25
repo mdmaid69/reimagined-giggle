35538ffb4d5a73dd2f31f0b487ee74cf1504a595
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:47:12 +0000

    pin happy maintain stretch dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
