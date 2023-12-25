6d2202bb9e3bb8dee17cb75192ac3e34cde0717d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:28 +0000

    string 566end runaway impossible wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
