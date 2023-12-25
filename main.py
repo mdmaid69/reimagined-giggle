91603d22c2b9886fc0f23582c879e5fa12330d2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:55 +0000

    empty instruction if without kill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def count_elements(lst):
+        return len(lst)
