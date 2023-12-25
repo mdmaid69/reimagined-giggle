ddcfcb701e4e95bfa802c403515526d8bde93581
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:09 +0000

    charge suck almost wish dull

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
