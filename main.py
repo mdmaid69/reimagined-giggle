c4c21614842c99852fa9eb382d3d0d495d748a3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:03 +0000

    top gather reply count operation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def divide_numbers(x, y):
+        return x / y
