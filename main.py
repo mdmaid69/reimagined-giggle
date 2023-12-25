e8d4dc793c4da9dd9ae4474357a6f561d25fc02f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:59 +0000

    soil sudden along seem same

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_max(lst):
+        return max(lst)
