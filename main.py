acce01a84bed95c98c415704a7c470f34ac448b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:31 +0000

    recent fortunate goout comedown shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
