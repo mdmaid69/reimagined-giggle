cec2fbc746149d5f12ffde3549c5295e4a209657
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:15 +0000

    book getout aim play period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
