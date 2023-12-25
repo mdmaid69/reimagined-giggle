48c85f4aac71dad51af90f821500b2c63521e7f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:01 +0000

    danger breath able between Ss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+from collections import Counter
+print(Counter("hello world"))
+import sys
+print(sys.version)
