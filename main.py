75dc6a866fcc9e54fab24b61b0ace7215f46a0d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:01 +0000

    reasonable lower mistake style vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
