c1d9b580fe352847f74cb2aa8e52e7610c3cc6cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:36 +0000

    beyond base attractive sudden protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
