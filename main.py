4bb5109bc3837722bf6c42b3843713db767004e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:37 +0000

    match mine translate distance evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
