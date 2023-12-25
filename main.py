44b0257af8189d4267cccdf0252529052e029b60
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:12 +0000

    hurry cope protest touch butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def is_odd(n):
+        return n % 2 != 0
