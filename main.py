381c8742252bf2ce4e1daba86189f41f85f71162
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:39:09 +0000

    understand characteristic of crash rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_even(n):
+        return n % 2 == 0
