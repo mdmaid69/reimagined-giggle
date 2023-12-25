7f4b014efb97566cd10b1f79955dbc2c9fc950b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:15 +0000

    send market measure exhibition shake

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import threading
+import queue
 
-def print_hello():
-        print("Hello, world!")
+q = queue.Queue()
 
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
