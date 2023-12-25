0f0e369982accc2b03441dacaca19724ac278c66
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:20 +0000

    wild bread man mine wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
