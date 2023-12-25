c9d5d5dd23247b79bcb5cbe93c68b129e5026adb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:39 +0000

    567enemy seat away movement clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def convert_to_octal(n):
+        return oct(n)
