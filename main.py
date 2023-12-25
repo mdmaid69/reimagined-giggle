eb7a395fb7fb356d23bebc18ec408c258327f815
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:43 +0000

    climb on design emphasize drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
