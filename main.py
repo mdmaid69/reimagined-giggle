de3419d5ec84ff0fba907c403b4f1538c1e8fce4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:49 +0000

    snow result comfortable you common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
