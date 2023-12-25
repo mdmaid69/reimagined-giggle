64a19a0d9636e6c6a868726d8459db93c10418fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:59:45 +0000

    reaction picture father pin crowded

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_string(s):
-        return s[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import getpass
+def get_username():
+        return getpass.getuser()
