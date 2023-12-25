60e0a21f57068d891512e5a2ada6aa9dab8f7a5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:15:23 +0000

    marry estimate port praise frame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def subtract_numbers(x, y):
+        return x - y
