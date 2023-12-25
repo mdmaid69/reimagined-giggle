e259d9d5aa354fd44ff0fe1edeae7cfea8c3678c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:51 +0000

    round air content Oo quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_min(lst):
-        return min(lst)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
