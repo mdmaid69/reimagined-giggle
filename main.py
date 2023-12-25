96c881dbb78f141eaac7e9019d14420ec8718d95
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:33 +0000

    neither provide sleep pinch talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
