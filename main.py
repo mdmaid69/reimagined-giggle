efbaf29385e9e550f642109e7a8fc8377ceb7c95
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:55 +0000

    subject preparation convenient change straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
