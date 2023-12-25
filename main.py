6b4c0cb07405c37241f36e062fdd13f09d560e23
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:24 +0000

    proper exciting outside stamp border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
