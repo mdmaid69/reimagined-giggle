832e37ad1495150d726a4b97f0518ef93dcc6267
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:37:23 +0000

    proud often destroy why reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
