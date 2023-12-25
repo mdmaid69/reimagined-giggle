619820d77f99987acdaef7e48b7a2fbadc99b10e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:15 +0000

    scrap combine bend both none

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
