311d7d71e7210bafc29e7ba95c7794f56407443a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:49:43 +0000

    clean sea cross war window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
