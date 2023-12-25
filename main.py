ffd764f2cf8fb9f18fe8b157a6c19948976221e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:30 +0000

    embarrassed surprise giveback ride chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
