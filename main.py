005202b68c3a65ce221c71c443d294d057758846
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:49 +0000

    protest cheat along sentence lost

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
