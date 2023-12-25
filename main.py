ca957576846a1fb04063a2564ac3b83481ec6188
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:03 +0000

    goback(to) grand transparent quiet address

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
