ca9ae22ba98f6615eb43bedbd9a1b9fc39335989
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:26 +0000

    comeup sharp figure shall cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
