93064ba6d8859e713a832c4597c91ad3cbcc3d3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:55 +0000

    everybody step convenient defeat best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
