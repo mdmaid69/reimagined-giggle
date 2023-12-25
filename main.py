28dafd178ea6e3b71b9d08a178c88f80781a0c13
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:58 +0000

    record transparent movement popular continue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
