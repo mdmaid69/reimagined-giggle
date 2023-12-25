2497d89f2a2ee36cbd45b8a09699a282d635a974
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:00 +0000

    cut several breakout responsible recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
