ac4c6524e128d6d90ce88957d906c7425f8520c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:40 +0000

    sort embarrassing check success apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
