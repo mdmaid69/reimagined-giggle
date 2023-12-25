3a339915fecfd846b9e8227b1f994f9b42422c06
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:37 +0000

    fix mass join dear fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_complex(array):
+        return complex(array[0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
