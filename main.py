b9544b8eec2b52c995c5c0b2b6571b94db4758c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:09 +0000

    slow rub slip jump favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
