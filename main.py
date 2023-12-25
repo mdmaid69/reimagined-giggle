2a9151ec076e2b4424db61efd3b3c38456b051e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:25:26 +0000

    exist damp split half tie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
