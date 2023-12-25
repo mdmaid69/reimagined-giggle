216d20d9b4f717d8ed8adb184e762f3dfb871c32
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:49 +0000

    answer responsibility software public my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+print(os.getcwd())
