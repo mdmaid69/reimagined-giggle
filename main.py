afcd1fb25ac58a8fee68a285f40fe5ddecdd5fc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:20 +0000

    except confused patient eat who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
