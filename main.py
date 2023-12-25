3183ab9a46e0544f3a3aac24711aea0d4111635b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:13 +0000

    geton point oil plan easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_user_dict():
+        return collections.UserDict()
