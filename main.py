ac1edb2891476099dda6bda34d3e2921a4bcaa3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:24 +0000

    anywayanyhow innocent lot post tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
