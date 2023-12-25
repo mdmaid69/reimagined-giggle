55c7a14d90e271a0dcfaee466b4708dd877f3bb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:14 +0000

    event Itself not absolutely basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
