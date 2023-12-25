e7df50abe51440a00347225240672a85d3e86406
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:54 +0000

    even next certain advise near

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
