66248c3bdd24de22fc5701a4abf9ec95f3ac9470
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:12:09 +0000

    quit late about Cc pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
