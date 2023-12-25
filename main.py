706e0241705433ada2eefdabde58e84740d0f31c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:27:11 +0000

    night spring depend about professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
