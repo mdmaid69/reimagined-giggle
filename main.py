5e3a0a6063a84a2372de64a236d501118d7e08e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:59 +0000

    minor believe slice crowd whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+from collections import Counter
+print(Counter("hello world"))
