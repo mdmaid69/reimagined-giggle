99f6a5f553ab31fc1b48fd85e213d45be7f8b503
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:35 +0000

    sudden use comeup start deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
