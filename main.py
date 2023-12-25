858f585b6d532ac4036f1e87b28051e9692419f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:13:38 +0000

    gooff separate occasional cum reduce

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def greet(name):
-        print(f"Hello, {name}!")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
