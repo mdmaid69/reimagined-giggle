8e8d39fab4b20632d8d61621520d724e10c79ab7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:56 +0000

    shut flow book slightly disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
