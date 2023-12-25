ea430285d439a88f2f733652b3a4e003d42ccd49
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:52 +0000

    pot judge beginning room dress

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(10): print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
