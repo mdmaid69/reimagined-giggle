4c7ad46d1fd55cd5e995fc465e8353929e7a16f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:39 +0000

    speak black last part impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
