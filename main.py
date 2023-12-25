27f3d0b7e274dae994917aebb0d311df959c6a33
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:24 +0000

    exhibition direct keen over fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
