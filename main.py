2d2852e97ed3ec7cdb020d6db8c0d6c61a0428ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:59 +0000

    description natural clever familiar may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
