3b1f179ca106d1500a23efa9849e632209e49603
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:16 +0000

    fasten active normal strange mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
