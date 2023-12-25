5877dac1c66749f0fe1c8aff8aea69e162869849
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:37:37 +0000

    brain twist warm us art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
