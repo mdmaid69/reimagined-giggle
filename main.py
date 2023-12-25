680a77c5d92a82e85d67a73c7a294740003b3e6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:42 +0000

    satisfactory defeat yet body national

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import datetime
-print(datetime.datetime.now())
+print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
