b9a802722b6bc61c448a782597672a4d96435919
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:38 +0000

    serve splash protest quick building

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
