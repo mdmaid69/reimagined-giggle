409a59b3889953665316281a02cce24f971efecf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:29 +0000

    cry community object thick date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
