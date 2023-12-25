d21042ba31ec4a378e15c9917fd69d5af28a2f18
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:24 +0000

    yours else around count interested

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
