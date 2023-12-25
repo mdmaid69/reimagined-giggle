60e83b8777f2b14af5e814989dcf0b0efc1ec508
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:20 +0000

    mystery match when culture communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
