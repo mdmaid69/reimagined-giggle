d7cf295757c1ae7d66889d129c7a9515871a27e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:00 +0000

    company look impatient soil accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
