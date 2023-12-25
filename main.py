f38e75bc513a739ec1a4dd4534703860caf78558
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:08 +0000

    everything trade lucky exciting fortunately

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
