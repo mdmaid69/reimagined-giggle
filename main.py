0c5c37f565326bfb66ad561e590a45602116e397
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:19 +0000

    fight long deliver deliver 569enjoy

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def add_numbers(x, y):
+        return x + y
