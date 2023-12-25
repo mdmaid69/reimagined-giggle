434b179a65cb22dbaebc800f83676e028a2401a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:03 +0000

    tradition sky turnaway commit accordingto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
