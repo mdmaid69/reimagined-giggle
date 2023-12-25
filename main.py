10d39f56249d48d855d93003ea300a8c34b39944
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:25:34 +0000

    passenger lesson calloff improve circle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
