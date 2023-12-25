4a05087b6b07871f5d422178e189ef12f5a2a9a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:46 +0000

    few rest reply push miss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
