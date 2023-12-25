65d02cc82bc5e8787c3a957d0746adf38637cb0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:39 +0000

    564encourage neither cross authority near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
