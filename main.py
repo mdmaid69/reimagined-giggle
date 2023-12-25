6d15d423b32b42c8032bd4dec2a25571c335f9ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:06:55 +0000

    outside check giveup degree value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_typecode(array):
-        return array.typecode
+def remove_from_array(array, item):
+        array.remove(item)
