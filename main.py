dfd4a726f994f8db8cdb94f341c54acd01cb43bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:01 +0000

    book turn rubbish worth confuse

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import math
-print(math.pi)
-for i in range(10): print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
