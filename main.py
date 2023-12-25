aa22db1f99a12405e1b94407e7a43d9f1659b179
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:29:52 +0000

    real general cut bush holdup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
