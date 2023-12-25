e5cbd4432fac790c5268bd205c3e402076e5a1b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:18:49 +0000

    past block replace rain female

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
