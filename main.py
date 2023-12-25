9e2828ea1afc297b7eb06ea07a55191525185f84
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:50 +0000

    mistake apply length spot Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
