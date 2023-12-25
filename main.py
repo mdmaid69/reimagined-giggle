a3178ea0fdff526b11e2c0afbf7ef2091a9b23a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:19 +0000

    bet OKorokay make among confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
