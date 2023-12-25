079eda79be55e192473f8a7b09918d364b4e188f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:19:52 +0000

    slip waste leg refuse creature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
