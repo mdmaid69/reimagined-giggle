3686702acb8b501e07f03b956de0afedbd0a89d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:15 +0000

    place mysterious all identify proper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
