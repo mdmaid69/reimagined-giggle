fe706073beca129c9a35fc5801b7d836218be092
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:15 +0000

    sure only dull wish keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
