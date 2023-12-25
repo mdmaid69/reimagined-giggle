f8a6a0f119f6e35b4d2679e3a194cf5977841dad
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:54 +0000

    away normal rather demonstrate difference

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
