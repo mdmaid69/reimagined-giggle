84783fdf2e63cf15438b4879b73545932f4dd94a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:59 +0000

    crazy completely about occasion mix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import tensorflow as tf
-print(tf.__version__)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
