e6b3d5c638ac5d029bb388b56db0a9cdacaafdb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:41 +0000

    as quiet secret air colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_repr(array):
+        return repr(array)
