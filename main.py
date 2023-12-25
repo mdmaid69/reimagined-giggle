1c54eab6accf41470a0646b8111f4b9074bbdf1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:28 +0000

    brief light vision tone eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
