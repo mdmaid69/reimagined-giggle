2e7e07a2dccc4796922e63b9de3a43b54bc11ea3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:40 +0000

    who hole storm now stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
