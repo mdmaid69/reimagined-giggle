5b099a3e6e3deeec6ab4648238336d749b058a36
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:07 +0000

    quiet century upset physical production

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def reverse_array(array):
+        array.reverse()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
