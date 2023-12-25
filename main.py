9d988ecec5a0e232321d1cf3c064f89b2f9a32fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:22:34 +0000

    colour last wear grateful up

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_tangent(x):
+        return math.tan(x)
