062921d048f8db175dfd6c2ba2af9b9a1eb733ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:43 +0000

    cinema doctor bye constantly talkinto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
