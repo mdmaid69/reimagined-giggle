5b1061383222e012373a791ba6a419ed292c9d75
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:41 +0000

    relation pocket move remark limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
