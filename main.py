5b4aa5c0f038ba1076d568a0206ea8f373a55ad5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:16 +0000

    friendly also just which amount

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_min(numbers):
+        return min(numbers)
