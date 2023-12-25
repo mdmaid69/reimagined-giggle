b65af398c9767f84cc8faef8ed9b83e4228be58a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:35:40 +0000

    job challenge little flight lose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 text = "Hello, world!"
-print("Characters:", len(text))
+print("Uppercase:", text.upper())
