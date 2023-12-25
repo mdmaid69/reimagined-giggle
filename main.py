bf6d69b646e5f33742bde4a9aa13b4aad90f3cea
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:11 +0000

    kick maybe frightened prefer below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_work(force, distance):
+        return force * distance
