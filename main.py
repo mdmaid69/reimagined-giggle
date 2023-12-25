4ee5e768b8d10318646240c91f25429ad4fdba03
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:03 +0000

    official along even head post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
