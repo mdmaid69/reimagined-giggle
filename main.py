d96463dfde2224b010039dcc227fa4baa9e30607
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:09:30 +0000

    maybe bring top flow indeed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
