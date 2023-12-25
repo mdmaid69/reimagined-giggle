a8417e4c492ab19e6c5b68a0afe693caaad79632
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:30 +0000

    keepout oil sweep face him

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
