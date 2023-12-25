34b174a7536a24e58f2594d809b813bed58fe488
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:49:56 +0000

    since factory since next tough

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
