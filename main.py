e61a614d3347c18e5867957145f2acfe6e11bdd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:17:24 +0000

    clear treat complicated refer rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
