4166114c2087148ecc462ecf8f27aaf347ea4907
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:52 +0000

    smile yes limit clean explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def exit_program():
-        sys.exit()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
