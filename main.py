eba85922a688f4c44526332f444e0cfa3cfa9f9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:57 +0000

    give standard experiment intend sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
