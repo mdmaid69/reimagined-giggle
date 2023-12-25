5def9660839ffeda7aeee9207ee16b01a19aa087
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:03:24 +0000

    level apologizeapologise injury push measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
