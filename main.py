1a660806fdc68eb9df5c37a485321ab24e98504c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:33 +0000

    cruel method unusual actual story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
