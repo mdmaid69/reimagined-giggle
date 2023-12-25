4c4cbae828de18dc58226179049aa2df83447d7c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:31 +0000

    formal occasion boil just ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
