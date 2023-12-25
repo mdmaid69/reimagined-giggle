106e7b09fbb176d6a9c92d3fe10cf70281746531
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:55 +0000

    cap even around past heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
