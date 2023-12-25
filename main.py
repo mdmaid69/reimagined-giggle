5c96779269c809c1f684b8a427f4a5d5334a77dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:20 +0000

    size near really community yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
