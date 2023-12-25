8a07ae6a457f6be1bcc46fb9f13966d6b3ba413d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:10 +0000

    movement suck narrow warn hardly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
