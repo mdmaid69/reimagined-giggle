c95bb03c57066653fc54d2d72fc55ee1445db9ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:09 +0000

    thumb avoid have contain couple

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
