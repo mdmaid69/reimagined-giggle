11ff48ba291d4a70cab8ec258b0d4f221bee8e4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:56 +0000

    success ilovedicks protest shoulder education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
