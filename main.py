9b1215b7689456aa21c72ecc4e385ff188977fa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:13 +0000

    chief message skin hot achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
