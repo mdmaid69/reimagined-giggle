c5d34ef527f34802f3feee2862b258bb43610a24
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:00 +0000

    exhibition miss always closed hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
