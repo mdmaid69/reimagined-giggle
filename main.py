9b6dd321dfaaf7112364a60f0e8694940baf366c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:52 +0000

    organization part practice health agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
