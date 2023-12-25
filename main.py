5b305a840556e3892256e82fe1f0f83f8f811fdb
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:49 +0000

    hurt message takeover most send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
