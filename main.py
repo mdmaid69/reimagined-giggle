1527f2fcd875c07e461c6cd704845fb0f64fa004
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:20 +0000

    bringback price represent band art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_float(array):
+        return float(array[0])
