fe241812821174df9382a40b8d4cc48fa1b13502
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:37 +0000

    country object reaction comeoff lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
