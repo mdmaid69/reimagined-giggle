024eaa713261df2424d29c94b0ba9092c4abd77f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:49:31 +0000

    phone young club reply just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
