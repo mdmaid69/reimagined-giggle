cec91b2f817df25a949405511fb05466c1d9206c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:53 +0000

    blind deserve bang reason service

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
