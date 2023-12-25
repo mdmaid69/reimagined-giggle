06bef0daf9269665b9f7268d9c21e7bb53a0272c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:05 +0000

    hospital better nearly act suckme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
