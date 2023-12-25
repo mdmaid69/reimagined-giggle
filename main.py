7c06fad7e1a48906428f9bc57826914c745ffd20
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:04 +0000

    kick start disapprove tape too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
