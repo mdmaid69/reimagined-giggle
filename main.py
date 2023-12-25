c8a35f9a2a245cba8490cb086cbb7b9495bb02b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:15 +0000

    impression dance cry point design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
