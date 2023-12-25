88c16690b553727948bc127ac9956b153d11c5b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:16 +0000

    safe exhibition produce wipe turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
