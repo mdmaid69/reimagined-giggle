133b776590b9b3f493ffa83679f386d2f8be6ea2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:48 +0000

    tense stir rest whoever be

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def reverse_array(array):
-        array.reverse()
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
