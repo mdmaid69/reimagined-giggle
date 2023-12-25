64cdb519d582cbb2ecb1db984cd3197ad8e6aa6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:15 +0000

    study pure bunch harm again

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
