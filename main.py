30381b238a2a1fe81d7932b6093f333685642e17
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:08:58 +0000

    no see splash any finish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
