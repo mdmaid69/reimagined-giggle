ddb7832ba7e750320e25fdc3aba964aec8527a87
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:40 +0000

    supply pencil waste legal usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
