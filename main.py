2e694fee35110b358b6f541f914db3fe78e78bd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:00:11 +0000

    do far violent wash body

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
