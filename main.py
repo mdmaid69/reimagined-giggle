dd981a53e31fd69765355817ac21fc8805dff354
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:36 +0000

    back importance wear cost acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
