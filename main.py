48448a49e1b065b5fbfd65c49207c474e98a298b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:18 +0000

    tragedy attract race rely real

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
