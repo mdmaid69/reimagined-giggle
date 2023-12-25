ec02a3f5c86aeac7c9f1f8d0bde3e75604621ac0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:30 +0000

    storm hers realize kind future

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
