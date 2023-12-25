58003aad28149b1f47a1d2356983285a59612706
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:37 +0000

    properly cinema hour list damage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
