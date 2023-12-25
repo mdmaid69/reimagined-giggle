9219f6c8f5d8feeea1ac600994108c25fd506892
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:42 +0000

    comedown excited sort train pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
