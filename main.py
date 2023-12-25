b21ef6614e3f4d7cf8a1697d35a26cd815a1be54
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:27:46 +0000

    naturally past sink remain great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_max(lst):
+        return max(lst)
