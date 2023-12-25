6154eddb02fd770eabf19be3e553891701b69079
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:29 +0000

    split cost standard neither deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
-import collections
-def create_stack():
-        return collections.deque()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
