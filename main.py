9be581e74eff918534848f1553d5c346a00b6438
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:35 +0000

    comment scene food system trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-def get_current_time():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def exit_program():
+        sys.exit()
