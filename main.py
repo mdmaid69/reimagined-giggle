a5f59b98deeb5d6d425fcdd19c17e885b46b4326
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:38 +0000

    breath variety loose moral fortunately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
