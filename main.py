b16044c8ac69f3f24d2670c07e2643248e23e0f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:56:51 +0000

    charge nonsense present store place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
