4982782027754b2404ed67a36a14885108b54cb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:02 +0000

    worth scared plastic breakthrough male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sys
-def exit_program():
-        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
