3a81e441e3ad6487ce3efc7ede7ed8ece2b91d41
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:58 +0000

    move smoke switch aim bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-print(sys.version)
+def print_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
