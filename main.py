004b5fe416321a19b59b2c69b89dec7a125a58fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:34 +0000

    drink sort wide stamp involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
