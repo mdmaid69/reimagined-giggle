a1f4b645e8a29b617983f76764bfafddfa3c043d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:36 +0000

    doubt operation his wish field

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
