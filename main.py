4b2f718a73ebb6806beee21fd1a15ec6ff6c787c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:07 +0000

    period frightened strict impressive again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
