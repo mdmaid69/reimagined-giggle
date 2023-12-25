df1853bc8e7a175f891d0f5081d0b069368f15a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:55 +0000

    head ride live wall hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
