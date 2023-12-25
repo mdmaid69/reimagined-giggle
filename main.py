8533d5205b03e3d176d48abb0007d060a910a512
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:26:16 +0000

    eye underneath hungry introduction major

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
