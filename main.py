3a86e103a1fd6b18aec4f3f525f4e1fc2c3f93c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:31 +0000

    notice exaggerate pleasant innocent entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
