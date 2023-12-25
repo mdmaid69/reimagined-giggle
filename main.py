606a1b88afc53cbe2a03457d0afc23dccd684827
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:44:58 +0000

    land appeal man regret slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_floor(x):
+        return math.floor(x)
