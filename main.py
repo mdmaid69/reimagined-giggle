a33b2371a6e30a2f82006e52cb2abbe85a87f41f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:06:13 +0000

    normal sign disapprove nothing field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
