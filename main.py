020bc22f5d29d7b2b941d8b10426258c71e014fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:06 +0000

    short few study something responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
