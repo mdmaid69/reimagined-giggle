ed0361600fba3b90f3b4a00c9295ff3e70df3f19
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:28 +0000

    announce onto group downstairs vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
