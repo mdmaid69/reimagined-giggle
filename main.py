e0d991c9c8fcf4fadfab8d53df79a1a8d3ea9273
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:33 +0000

    callback row tend noone powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
