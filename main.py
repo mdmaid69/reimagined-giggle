7ef6aa46fc623d755d4ae286939e1f8fcffc26b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:56 +0000

    arm blame thinkthrough someone day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
