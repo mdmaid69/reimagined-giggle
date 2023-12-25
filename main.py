8946216c1077bb2d61618fed4e7eef9cf393e35c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:59:34 +0000

    guide limit advantage sort Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(numbers):
+        return min(numbers)
