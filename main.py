d4e8d0543547bcdff46477f7a169fd297ca1fbe0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:17 +0000

    train hers pity best round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
