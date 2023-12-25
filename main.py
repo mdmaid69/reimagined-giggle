0583f6d8d8cd0f84609b110b06ad6bc79514ad74
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:54 +0000

    happy in smile flat somewhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
