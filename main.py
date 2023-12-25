94abd5ab695b78d0b3d1cbb11d1cc005a9d44e70
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:42:15 +0000

    uponon call room rent introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
