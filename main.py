04413ad3c5c44b8e90e2e198a6ab76fe9826b695
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:56 +0000

    boring practise reason artificial quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+print(math.pi)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
