78eed0d24084f53495a7f4313357eb0e37d09a78
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:51 +0000

    leg frequent shade forget soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
