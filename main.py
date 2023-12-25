84336aaa1050b0922446d26b71ae7c1a729ca66c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:29:53 +0000

    preparation various rather force middle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
