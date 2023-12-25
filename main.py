a5defe963ca40928b63b1d3928787b7e3ea8a1e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:11:03 +0000

    new mess legal some offer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
