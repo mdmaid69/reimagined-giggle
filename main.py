9728bdb8322095c53432ef08710b1229c4dc5d20
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:21 +0000

    phone pretty development warning away

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
