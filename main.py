0fb78adef3dc438590cff829f562019b2141361f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:05 +0000

    past no simple 571enough loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_priority_queue():
+        return collections.deque()
