43281bbc6e10d6c3688bf8b3ac1d3c9521485b96
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:30 +0000

    discussion today matter thing intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
