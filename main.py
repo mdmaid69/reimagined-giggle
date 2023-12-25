401f3e99081a0cfbc1972ce6ad2ded8613e53c15
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:32 +0000

    admit emotion dark bag sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import sys
-  def get_python_version():
-        return sys.version
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
