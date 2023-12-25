9411b2542d127e0fcfcbf04413ce67fe9897b4a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:03 +0000

    laugh certainly bound ought pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
