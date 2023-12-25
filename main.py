83e85f4fd4299708e453aff8ba9e8ce2b3f82234
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:55 +0000

    must typical extraordinary Oo to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
