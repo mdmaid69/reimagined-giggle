9e55d7925d68769f6e7da2b3d9be1aaf01ee3583
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:53 +0000

    reason negative stop aim sticky

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def divide_numbers(x, y):
+        return x / y
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
