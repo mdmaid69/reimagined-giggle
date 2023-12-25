459d8601197d4eefb64a2577e25d6013005f5cad
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:59 +0000

    common key another soon anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import getpass
+def get_username():
+        return getpass.getuser()
