8e33dbc8ec46a9e0904e12b34996a86e7b9bc51f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:06 +0000

    flood cope very guard find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import sys
+def exit_program():
+        sys.exit()
