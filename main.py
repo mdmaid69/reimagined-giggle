555a8fa24e65fb40ed00853de15c7918079b7f4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:46:45 +0000

    capital cost freedom regional children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
