7a69ad8f61fb5c2d3c2d2274215e78ef10400980
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:41 +0000

    protection deal to comedown platform

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
