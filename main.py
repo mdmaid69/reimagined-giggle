ae4287f66e9bfbeb410592c7659cb8cb81286a6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:38 +0000

    pretty wide will anything impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
