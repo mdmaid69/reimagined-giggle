b3f93f5e54ee3cd812b39438d0da2027cc3685e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:28:18 +0000

    wind little pot school spare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
