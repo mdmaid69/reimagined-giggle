8895c7ae45e64f9afc0f04b0193d17b92456efb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:46 +0000

    anyone naturally both record pair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
