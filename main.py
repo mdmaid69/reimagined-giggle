21b5f9cabc6943d1e4c0ff10b964fe25e6c2cf94
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:49 +0000

    slight day curious hello purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
