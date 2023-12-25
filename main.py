45794ad7f5bc2f899c5856a4dd7df42c879fc715
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:36 +0000

    along unhappy rather test goback(to)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
