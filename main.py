db5a5e9c95f8e670fa929ae8ad7eae506c3be7cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:57 +0000

    middle prove surprised answer ticket

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
