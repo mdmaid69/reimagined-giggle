294bac9abb0ad84bccfebd647de1e1306453ac55
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:11 +0000

    rub among declare within dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
