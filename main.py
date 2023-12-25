dc52af60564d6e3f6ba18938a5fa66a1103ac7dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:12 +0000

    look song cost black amount

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
