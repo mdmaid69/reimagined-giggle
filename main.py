7ef4941165be08cb8fd792e9a9ae6d09de8adf14
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:12 +0000

    cook equipment argument soft phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
