6b793e9106705fef1050c03adea05bc815e4dba6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:58 +0000

    near limit between shallow hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
