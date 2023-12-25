5daa98cf0443836f9519e45810375b753c1c2e69
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:39:09 +0000

    total birth runout shake trip

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
