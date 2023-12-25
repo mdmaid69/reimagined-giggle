56a9be7e19061fc51daad547a7169d2cc06e2a1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:14 +0000

    town sing large art track

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_itemsize(array):
+        return array.itemsize
