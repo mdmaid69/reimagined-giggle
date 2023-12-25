9971f646032d1e426fed172156cf4ad10f0ece83
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:46 +0000

    forgive getoff giveaway relation Hh

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
