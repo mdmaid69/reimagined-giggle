5b46e35d6de1561ad15da7962c8e563c8fc98df7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:18 +0000

    hurt patient share smart right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
