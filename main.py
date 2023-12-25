afee410a8c2a56796e4f58e7c95ebadf465f7fed
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:28 +0000

    back feel large pretty heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
