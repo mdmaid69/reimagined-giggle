65c787b03629716d17329dce02d89923bfd60cd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:33:49 +0000

    change himself alive fall even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
