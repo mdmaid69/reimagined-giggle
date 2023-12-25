49d59ce062e49a0da2d27483667b63b9cc7d49df
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:28 +0000

    science as unpleasant opposite talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
