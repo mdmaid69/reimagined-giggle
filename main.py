31a33c4764d8f12c60c67c1856892974ceadec18
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:23 +0000

    whether during rare computer free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
