c1954deaf2c16cb642bb10a41af70bb80edea413
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:15 +0000

    564encourage feature safe favour hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
