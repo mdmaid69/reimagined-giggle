488b0ea9dc49695046d83c607fcdcd561872b2bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:38 +0000

    wet train fall previous door

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+x = 10
+y = 20
+print("Sum:", x + y)
