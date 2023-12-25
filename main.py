ac537fb55d7960c3f660ca61555e84d96b1e90a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:13 +0000

    transport giveaway instead various admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
