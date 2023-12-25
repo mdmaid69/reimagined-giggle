b900b5a3df810e34bd88c010d721377a8ab50286
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:09 +0000

    undo tool quiet live sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
