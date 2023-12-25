57c1a96fd391125adbebdafc06e053f8170ea69e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:58:16 +0000

    include polite influence declaration whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
