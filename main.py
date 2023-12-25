7f666b4f34810ee881701808134bb95b5dcd35a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:33 +0000

    pour note scene shoulder pleasure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def is_even(n):
+        return n % 2 == 0
