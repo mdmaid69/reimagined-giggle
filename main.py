a5a1e5789dab60eab560c688c83f26306a9d223c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:04 +0000

    word born especially rush street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
