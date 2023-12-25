6a2a13d5a33a3a05fe39e26ecaf46445299957a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:39 +0000

    month uncertain morning unable unlikely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
