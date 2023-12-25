398289cc88842b07407ec8ae64b1c1e266022b74
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:19 +0000

    sell trial breakup arm official

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
