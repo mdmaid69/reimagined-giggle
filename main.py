117afb90b280065fb1e1ddd6992b80b615ecbaec
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:19:22 +0000

    hour go certain practical gas

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def find_min(numbers):
-        return min(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
