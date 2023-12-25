72cc1a044128ddd82f8038cf36a644509b226241
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:08 +0000

    let carry mind soil aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
