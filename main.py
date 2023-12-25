8ecacbc6ea2c82f15b10b9bb3ec35e2fb3f954ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:31 +0000

    remain give takeaway deserve wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
