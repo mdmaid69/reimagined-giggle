7e8389684563230fad9f191dca604ea057ecf17b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:49:36 +0000

    moment mark violence hardly godown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
