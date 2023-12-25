7b1848d0aa03da3ffc8d696437a578770e478261
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:25 +0000

    anyone now special fierce belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
