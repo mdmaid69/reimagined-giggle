998575ab48f6925caceb99d5ec9002ebfbbf9065
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:28:35 +0000

    confuse wipe famous money curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
