6185f92e8a326961620979917f4269f55259be1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:40 +0000

    give school right have cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
