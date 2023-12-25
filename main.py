3901fb917aeb6d61429fc277c57362a122b1e33d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:00:52 +0000

    keepaway rush handle substance recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
