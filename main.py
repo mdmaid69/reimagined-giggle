960ce061108c68544a22b3213e0726b5074aed60
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:51 +0000

    shape sound class care emotion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
