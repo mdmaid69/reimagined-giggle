7b473984b6ae12ab5426bc71db4380d3276246e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:31 +0000

    flood obtain another ahead manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
