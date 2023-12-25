a2c3abf307448df2a65b7fc6fc6feea6b734493b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:33 +0000

    nigga second unhappy reply by

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+print(os.getcwd())
 def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
