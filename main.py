752b43c045c283be652648b8782bebb7e8e9a235
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:53 +0000

    importance brush powerful scream unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
