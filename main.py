c38824b66bca475a11120b6696a5b73bf81b5308
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:36 +0000

    wall pay tidy everybody sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+print([x**2 for x in range(10)])
