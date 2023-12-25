98be58545da8602f094b5a338fa4d288d743d661
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:43 +0000

    together deep cook shine moon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-print(os.getcwd())
+def is_even(n):
+        return n % 2 == 0
+def calculate_power(work, time):
+        return work / time
