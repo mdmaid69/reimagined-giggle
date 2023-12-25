9b0c28c93fd6c4b53297856e1fe5c428c3d2b548
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:48 +0000

    wide task repeat and teach

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
