44f5a0fbad82af37fa8cbcc3068d647713866511
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:52 +0000

    reliable wind tense after refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
