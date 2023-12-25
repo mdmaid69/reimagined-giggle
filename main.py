a6389bfecbb00b4fa7172b7fc63c775bb0df490f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:51 +0000

    bag state clearly definite adjective

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
