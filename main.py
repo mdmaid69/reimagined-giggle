cd7373550cc64f3bbe7fd85c60438994b07bfcaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:41 +0000

    feeling turnback rest moment another

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
