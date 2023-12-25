4e43f202713d797bbb07804bb3c9116ba79d2739
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:33:00 +0000

    work many hope careful after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
