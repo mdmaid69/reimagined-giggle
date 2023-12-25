9717dd8644d13212ea52d9dc7a1f6ef384b35a7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:13 +0000

    nearly born hope ago winter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
