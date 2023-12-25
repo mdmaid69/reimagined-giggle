909e6641a855d361c59e471cabbd8e8c55a9fe0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:12 +0000

    nurse judge blue wherever violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
