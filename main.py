0f92de1ccef81589ef22769391c78dc4fac21405
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:54:36 +0000

    so climb collect sharp contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
