d5bbcc95a83db5e5c9bae9abc24dad6998eb24d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:40 +0000

    community behave path this nurse

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
