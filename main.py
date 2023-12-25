562c1f9caa6203eaba4e9f429e28d411b10bd04f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:46 +0000

    shopping reply inform extraordinary bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
