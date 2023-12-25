e6a11b760e234e7f04839301e061381b68f8ad62
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:40 +0000

    love part tight talkoutof second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
