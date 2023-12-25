b19c1d98ba775b3f82d6725b87287578931189be
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:57:48 +0000

    street convincing hang sticky chance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import platform
+def get_os_info():
+        return platform.uname()
