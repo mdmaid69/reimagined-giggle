e651a7dea5846b3f5aa8b31561d0f4ee228ddddf
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:04:47 +0000

    explain section instrument cut happen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
+  def reverse_list(lst):
+        return lst[::-1]
