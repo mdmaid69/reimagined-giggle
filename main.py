82cb9229fad481491b08dda1e2d4e1dd6f7d63d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:14:25 +0000

    study object calloff operation visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_area(radius):
+        return 3.14 * radius * radius
