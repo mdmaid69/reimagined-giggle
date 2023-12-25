719e28a37a1c901133d3c55f54006943b6f5f4e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:47 +0000

    anywayanyhow remember luck once meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
