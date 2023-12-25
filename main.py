5d4d6b3e0253fa549f7f03de4dde9da503686c64
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:18 +0000

    story bit leg store urgent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
