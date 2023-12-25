305b8664951af48fbc028a70da47784f6977432d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:55 +0000

    threaten afternoon thinkof up road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def square_number(x):
+        return x**2
