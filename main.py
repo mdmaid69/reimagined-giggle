2cd851306227740750a5071291e4ee75b3b2571d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:44 +0000

    miss soft smile used environment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
