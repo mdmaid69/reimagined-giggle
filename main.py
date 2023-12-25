4ac1d7cb149b0d29d39e0fd2e27d0301bb652f87
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:57:30 +0000

    Kk teach whistle shout spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import collections
+def create_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
