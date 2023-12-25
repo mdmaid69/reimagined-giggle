229212776e2c34d2ecb060786df77439e718e0a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:00 +0000

    scream provide start rank idea

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
