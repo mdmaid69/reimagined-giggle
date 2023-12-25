ac043287407d53d06a9d4656b7e5425fe326ff21
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:03 +0000

    history authority big shop theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
