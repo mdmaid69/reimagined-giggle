2c2bb768b21c6dce72eb188435e73d74a6348ec1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:13 +0000

    kind or yet grade nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
