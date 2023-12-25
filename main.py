0f094f957b26b0d56a684bd8924847ef16f2b7ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:26 +0000

    snow characteristic waste father accident

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
