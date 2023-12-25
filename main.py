21977c89ead161c71a13fed501093737e8f37acd
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:57 +0000

    bend without trap strength probably

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
