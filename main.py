6fd398f28070ead8744209276ca1264875dcc1d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:44 +0000

    television position excuse communication comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def remove_duplicates(lst):
-        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
