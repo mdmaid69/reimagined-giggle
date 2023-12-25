fb55a22db4e6015c59d08115a3fcb61999809499
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:57 +0000

    before stamp precise calmly stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
