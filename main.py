473ac6256ff015c4a13f29f8f4faeab68bb25a32
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:14:00 +0000

    behave waste job perhaps lesson

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def subtract_numbers(x, y):
+        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
