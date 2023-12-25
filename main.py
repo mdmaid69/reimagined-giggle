992efc493c0d2e589b7de5b817f8c8ce3cc1743b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:57:54 +0000

    dish cool though still slice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
