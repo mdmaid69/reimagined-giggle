6223c4ca430e40a860f55578c8511ea14cadafee
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:26 +0000

    matter machine muscle top remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
