b2d931a20ce808c53d7f6cb18f5d79c20da3f338
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:49:09 +0000

    goout sometimes open scream model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 def calculate_force(mass, acceleration):
         return mass * acceleration
-import array
-def get_array_as_str(array):
-        return str(array)
