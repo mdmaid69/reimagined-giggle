a0130d21d16d1bd2fb9890f2385c2e902ebc89b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:13:20 +0000

    bet manage sensitive sea pleased

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
