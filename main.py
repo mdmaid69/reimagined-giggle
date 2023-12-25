35f748de4b8211b2388007d5c4e4056b85997804
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:07:19 +0000

    closely invitation after will minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
