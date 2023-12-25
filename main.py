6e9a365d8740ec057f0b363e219e4d811b7eff53
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:40:22 +0000

    instruction slice neither divide cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
