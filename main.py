c271936a005e8d0331c3e5542a85816d7f759099
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:58:29 +0000

    neither thank run sensitive present

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
