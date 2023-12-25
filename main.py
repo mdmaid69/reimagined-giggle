c5a0a8d96e439d67706ca983fb16fdedcf56e49c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:00 +0000

    picture Kk bright clearly slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
