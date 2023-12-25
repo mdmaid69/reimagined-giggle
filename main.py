0d3015dae7c689d258640ff8c1417dd03a276766
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:06 +0000

    both clue arrange condition favourite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def sort_list(lst):
+        return sorted(lst)
+  def count_elements(lst):
+        return len(lst)
