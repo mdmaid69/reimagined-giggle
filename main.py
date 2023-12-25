bb111167be962c3c45e696277b1cbc18775c0869
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:47 +0000

    lookup ability performance circle release

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
