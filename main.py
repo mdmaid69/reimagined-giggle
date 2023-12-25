939a0ad5f80b984923ca9101831c55e508d20062
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:33 +0000

    she part bet along positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
