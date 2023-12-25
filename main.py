1c4d9358aeb0fe9b1f13bba5db3c06f280133070
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:26:58 +0000

    model necessary walk movement Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
