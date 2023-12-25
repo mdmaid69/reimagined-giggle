edafca70691262cfe7dcb25133e0f639dca9a9d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:19:44 +0000

    benefit influence dig bye exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
