0a7fd3a2fa21389c63a9c72869b0839d4007db4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:16 +0000

    fit likely explanation unable exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
