84aa2cbbba1f1aebe6f27493387c6b066d5462e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:03 +0000

    responsible cinema rain recently 179bite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
