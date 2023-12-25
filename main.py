8387063d9ea7c4caa7a0f9b4c5ad3e86ffea547a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:18 +0000

    last giveout prize reasonable many

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
