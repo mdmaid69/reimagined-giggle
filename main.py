7d7f4fb3cd1d2c65f00ea5fef3ca09b7c35b47bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:03 +0000

    we cap worth eye basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
