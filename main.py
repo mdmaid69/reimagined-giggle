55a4455d712a5ceae6e554c7aaf676e9594da4b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:23 +0000

    takeaway strike welcome match workon

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
