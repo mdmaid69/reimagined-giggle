7f95fbb8d585263f273f1fb215908521b2d75477
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:39 +0000

    add such bringback inform only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
