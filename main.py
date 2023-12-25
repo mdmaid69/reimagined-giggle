f2ba5912eadeb7ffb011659fed147fa266c6adbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:36 +0000

    shopping vanish since level square

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def square_number(x):
-        return x**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def is_palindrome(s):
+        return s == s[::-1]
