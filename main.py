9829ca375da72d94b5c5e37508ae97f42249f672
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:02:03 +0000

    base gentle old straight bend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
