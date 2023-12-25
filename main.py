9b4386c057383941012bb0ba7bff9fa23ff43aa3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:42 +0000

    deserve eye ride blind embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
