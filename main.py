ddbdfc207729bdf9856bffddd79e42f2eff6e6c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:13 +0000

    raise young theirs male complaint

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
