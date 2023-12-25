66cbd89c0a5e0fcde9318be24abd37f2a8abac68
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:48 +0000

    walk story fast job combination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
