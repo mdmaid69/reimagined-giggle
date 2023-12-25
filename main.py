24b73fb8f3611eaabd93c7dce685f4c3f0ffce5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:51 +0000

    decide out goup similar transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def reverse_array(array):
+        array.reverse()
