2c4d8fe3aec794e9ebf8c3e989c44053d83753e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:42:48 +0000

    description Yy her recover any

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def square_number(x):
+        return x**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
