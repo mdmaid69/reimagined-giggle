84e18793f9f1b7a6dd99e81a3bd54e53ddf47d64
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:11 +0000

    bang comeout slip dangerous ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
