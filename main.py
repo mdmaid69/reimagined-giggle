55a67c28511dd12d9a93944ce87e398c792743c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:18 +0000

    front apart repair car stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
