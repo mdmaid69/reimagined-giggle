61cbbdc8e8b27d81e577c6f6b6b88cdb7b6022e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:13 +0000

    could dress comedown minority last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
