49089e3ba2f2095e04fd75d6483858e7740f3318
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:25 +0000

    confuse dull print light relationship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
