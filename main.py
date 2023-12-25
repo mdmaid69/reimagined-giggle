5559a50f14025efc362a205c514a2f191c9662c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:54 +0000

    debt classroom crime behaviour major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
