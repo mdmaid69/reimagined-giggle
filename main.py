08a3eec70d3edc495070542f838912e2f5e34108
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:24:49 +0000

    commit Ss comfortable hate model

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+def exit_program():
+        sys.exit()
+import array
+def clear_array(array):
+        array *= 0
