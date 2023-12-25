c346f2c75e415f06c8838b91a278ecc58324fe20
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:05 +0000

    criticism holdout father entrance preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
