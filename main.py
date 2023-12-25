3e60888236d1ea1f7ced41798585e72b69c1d729
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:41 +0000

    crime challenge mess ignore attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
