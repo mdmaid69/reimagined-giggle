2d286f7fbcbbfd1cae031c59bf622b39852aeecd
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:13 +0000

    warm picture die off in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
