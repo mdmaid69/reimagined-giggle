87efe76f9fceb0e6e42872d1a7b164402ec0d6a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:04 +0000

    distant callout towards fire abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
