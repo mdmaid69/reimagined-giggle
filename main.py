daea676bacff247b3e450d24e0347a5ea8ecb19e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:44 +0000

    minority sleep regret remain late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
