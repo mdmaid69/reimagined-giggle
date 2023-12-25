d782042031e4f4c4d4fb1d1c845041ef56a94fef
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:53 +0000

    lead talkinto callback aware shade

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def subtract_numbers(x, y):
-        return x - y
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
