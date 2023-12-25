ca67529c84c2728ab6c8ccbe3e72b7d85a23f49c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:15 +0000

    cope wear here muscle aware

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+print(math.pi)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
