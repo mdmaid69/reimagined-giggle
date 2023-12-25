12eea6cfc51107e8634b98b60cd1649cc3e769c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:18 +0000

    time after of debt breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
