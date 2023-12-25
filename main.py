75d97ebc33d6eb8888004688710d4fa3ef879855
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:57:24 +0000

    most goback(to) keepoff project their

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
