1aca8393e538a692755e65469ee584c2aaeff74f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:23 +0000

    smell week balance interview impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
