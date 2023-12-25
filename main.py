bc938f54b9372785c4fc9ff3c33dd9f69ec3f433
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:10:13 +0000

    suddenly difficulty sometimes easy number

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
