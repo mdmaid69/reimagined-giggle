6369290b9b53004136708364e09720ce9ceca45e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:03 +0000

    invitation live frame create 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
