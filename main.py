e7909bc6bf42ca571b131f2c70bf3bb2863a81d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:05 +0000

    weekend fence flight noisy frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
