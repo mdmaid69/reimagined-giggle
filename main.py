ef2ff5f25b6aca71f829a425da192f6c136b7d50
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:24 +0000

    force decide each meal talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
