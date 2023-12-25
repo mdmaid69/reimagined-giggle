ab8d519353b06816eda72af3d196117e2b05f4b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:00:04 +0000

    always tidy head at treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
