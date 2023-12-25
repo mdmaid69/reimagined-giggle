bd4f43749dc8f1dbf058161b76822435ebe60bc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:04 +0000

    arrive story record eager Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
