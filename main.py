ced8f2d516b87370f9e36fae43994b403775104a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:49 +0000

    capital conclusion destroy afternoon search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
