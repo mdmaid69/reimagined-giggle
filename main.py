ca6337fe4302a9c2eba39c153594a58f5dc4e9e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:30:03 +0000

    that basis equipment fond change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
