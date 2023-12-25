f3ac4d786529a1491f140ac8f74edcdb2e202e64
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:48 +0000

    club Vv owe fall usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
