af7541b7dd3dc47ed0cdb52c98c3070fae817edb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:12 +0000

    sweep pronounce powerful quarter cross

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
