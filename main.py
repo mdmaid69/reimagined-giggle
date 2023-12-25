10bc8ac73eb2e40bf8f40915e7f7b463860265b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:56:59 +0000

    day man argue stick more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
