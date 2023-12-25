3c9c82c9a825f1f7295c49d5495612f9d02eba4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:05 +0000

    profit object worth birth inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
