b07ae1c2ce99a727451f136ddc523dddb3d45f0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:33 +0000

    file settle afford lookup praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
