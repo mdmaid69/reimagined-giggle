0e5bb6d6af3e06fa1d9062aa82d6a31e2053f49e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:15 +0000

    around wide whom spend smell

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+import platform
+def get_python_version():
+        return platform.python_version()
