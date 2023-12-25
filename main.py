bd4ed4adcba637f7002661857d561dd6e58bf0eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:57 +0000

    time frequent little cry spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_density(mass, volume):
+        return mass / volume
