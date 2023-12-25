a07d323cf5384c01c44452d52728dc568d3c91b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:23:15 +0000

    extremely luck while suggestion content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
