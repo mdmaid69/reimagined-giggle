f976c98864117804d4a342a596603a2391692c25
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:16 +0000

    modern far up company discussion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
