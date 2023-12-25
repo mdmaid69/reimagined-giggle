3404a619e88646e46a61b6e19b0dab71703ffcd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:34 +0000

    poison know flat behind watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
