eaf0b130689b8473b0c8e15498ebee023751596b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:08 +0000

    tune slight guess exchange shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
