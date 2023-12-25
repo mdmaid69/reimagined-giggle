3f67def7c0d1bc9854017c3293757452a3968ef4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:57:19 +0000

    aim spray blind whenever stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def square_number(x):
-        return x**2
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
