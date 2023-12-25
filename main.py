0f5f9910ac0187aaca78c9408081f6036b14eb35
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:21 +0000

    afterwards put stare money impressed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_item(array, i):
-        return array[i]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
