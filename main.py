ed66a812d9d1ebf04d8502d9d4a365994e4f0bd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:43 +0000

    practical effort seat equal juice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
