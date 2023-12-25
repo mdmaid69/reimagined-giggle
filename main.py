1856a23432a8c60996b7d8980e59eecdc485ba79
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:25:23 +0000

    you sit arrive you price

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
