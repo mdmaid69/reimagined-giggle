b8e9a7c29b4a0077b2d4bfde258044f0aa2769ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:19 +0000

    confuse flat near little shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
