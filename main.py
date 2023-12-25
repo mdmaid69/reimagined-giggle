6747fe1e45fa6f023e5bf1c0811fa74760256b5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:32:22 +0000

    government peaceful worried remark fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
