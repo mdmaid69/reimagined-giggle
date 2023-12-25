ecd9e7bf34f354ff858e9784d38cb6468ec1091d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:22:07 +0000

    note march fight lean effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
