6bd786fea159f62da82479d9eccce8e38e583d45
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:40:12 +0000

    nervous willing hold breakinto live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
