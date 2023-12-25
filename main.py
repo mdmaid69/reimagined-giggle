4fc0fa5ea7b74147cfa182cd9610e412b271ffea
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:06:58 +0000

    tall beginning bringdown professional amount

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+print(os.getcwd())
