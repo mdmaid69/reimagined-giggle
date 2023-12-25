0fcc94e729ac382b0b02610e31b2d85083e8f4de
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:57 +0000

    wire pipe sure hour would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
