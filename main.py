7d7383bbf302acd35e78075ac62f3a2b0baaef34
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:59 +0000

    region arrival what floor flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
