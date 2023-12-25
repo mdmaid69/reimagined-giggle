a4d5744089ad089419d761b2267164d0c0d0f60a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:15 +0000

    principle road copy line fail

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+for i in range(10): print(i)
