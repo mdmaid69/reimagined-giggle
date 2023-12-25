2bcae76e2c061b94d6e10096e8a9765f67f2908e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:08 +0000

    majority wear fit generous whether

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
