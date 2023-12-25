44e8c2f385c96325b874113fba3dc67f5cecea99
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:46:59 +0000

    runinto quick test suppose search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
