40c100af5b6885e335c4cd04f3d965b37ecefe30
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:52 +0000

    train money shop catch recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
