600ed43ea15f3acabd8521c2cda4afbdd40ab4c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:17 +0000

    country proof art well flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
