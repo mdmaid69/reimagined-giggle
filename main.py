27b81cade9c069ed5e3afb9617d6ef06069f9eeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:27:52 +0000

    order geton shout normal please

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
