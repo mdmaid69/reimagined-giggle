ad06c847f4c1134d4279d2c0a66d687162e5bc43
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:30 +0000

    Bb sharp list successful prove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
