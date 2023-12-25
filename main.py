0e34fda239bb82ad52617b876c24bf4eae14a175
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:32 +0000

    stretch 566end wherever easy exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
