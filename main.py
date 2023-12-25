1f0312e24e180b98df8f83f30aa1e317500d4e34
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:14:37 +0000

    black along nor morning class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def add_numbers(x, y):
+        return x + y
