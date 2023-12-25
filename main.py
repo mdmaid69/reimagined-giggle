f15eee6d19f083a6d257bd564dec69816fb3245c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:56 +0000

    this play late method extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
