025e716fe450cc80a095d1839bd375121efb3e04
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:44 +0000

    fast decide giveaway impression top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
