8b30830c93d72e36248a5ad6ccb7f918502dbd60
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:56 +0000

    frame murder remain shoot worse

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
