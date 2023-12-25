7fbb48d86ee66bbfdc127822ba841c0353469961
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:56 +0000

    later curious day brain twist

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
