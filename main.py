1f78ca84702a91dce55317dee497279df92b0ff1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:32 +0000

    appear loose workon movement have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
