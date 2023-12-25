0ad214a489058e5a5ecbfb87758a02cf43d60544
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:56 +0000

    breakin defeat so idea whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
