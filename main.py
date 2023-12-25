bb4dda10baa0aa182fb7485279f09a6815f3ca63
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:50 +0000

    yesterday strong suckme must aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
