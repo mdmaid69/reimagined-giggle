3406846658409cfc4593e7a7979872d8a8107082
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:57:57 +0000

    prevent say case really manage

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
