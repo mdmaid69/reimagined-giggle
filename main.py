2c7261cb0d43ad51a78f86773d6bed144b7a9c00
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:11 +0000

    off follow up twist survive

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_density(mass, volume):
+        return mass / volume
