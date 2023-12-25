f26c9d4c030bf003473a48cba8630fc31c1edbc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:52 +0000

    entry while basically content disgusting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
