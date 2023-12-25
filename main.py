64d58467045b85139a2eebf8628c444ffc18cf56
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:10 +0000

    holdup whichever experiment formal rescue

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
