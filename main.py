561dd417473d8bd2feca0bc4e186b25a71086a9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:50 +0000

    estimate yours system sit use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
