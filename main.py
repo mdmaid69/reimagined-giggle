7f6cc5ebc8f1f28b5305671bebead32da4922a76
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:00 +0000

    accidental warm name direct tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
