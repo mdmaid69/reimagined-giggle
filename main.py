3980e1742f265e92676f99184671407d3deaed09
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:57 +0000

    bored fall many hurry female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
