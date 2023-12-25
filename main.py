8caa301672689fc14193d70ce0318fed2b83c66f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:12:14 +0000

    laugh 179bite and pinch attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
