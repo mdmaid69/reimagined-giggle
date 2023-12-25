65d61c0ab1555537db8c8b892bf4ea9ba826c0ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:52 +0000

    represent off bad pen drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
