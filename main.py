4d1aa1ed6d52d621a9a41590bcf754a056648eaa
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:26 +0000

    tread firm similar field development

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
