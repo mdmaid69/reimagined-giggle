7bbe962d8f5cecb8633dbea9f738d3a2c54dd6d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:12:30 +0000

    our close takeover as I

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def is_even(n):
-        return n % 2 == 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
