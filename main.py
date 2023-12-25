c851d6550d34756e58326358cb1f5d3453816320
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:10 +0000

    but seem still area such

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+print([x**2 for x in range(10)])
