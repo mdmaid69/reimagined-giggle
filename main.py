ca907bb72040f9e821ab322c1a56d43a4ffeba00
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:35 +0000

    play damage mark comeup grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
