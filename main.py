8891758d9e25fb6c4f416dce48158796c314c872
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:54 +0000

    mistake refer way recent already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
