000651a6f02e6fe57103046640afa19ada5e54d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:18:40 +0000

    mouth harm before confuse behaviour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
