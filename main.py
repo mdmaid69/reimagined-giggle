74ca7e375a28defa8b0a38ea45b2eded993fe236
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:52 +0000

    threaten there rubbish system each

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
