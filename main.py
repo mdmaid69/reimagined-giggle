8c086cbcaa3abc4e36c694d19358109b28b7ca82
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:09 +0000

    guard waste love close ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
