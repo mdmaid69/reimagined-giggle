8f198da9add2d5e619f9a73ddacbc2dcaa40e773
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:03 +0000

    float punishment thick see warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
