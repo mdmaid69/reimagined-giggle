9c34818b0c861f260f95fc2a7c8eebd32dcc9b70
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:51 +0000

    not pipe fear harm figure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
