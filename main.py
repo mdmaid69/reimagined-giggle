e5d10ee5c65c582c3a2b5d7bab4e9c00627a9761
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:12 +0000

    effect print address question Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_power(work, time):
+        return work / time
