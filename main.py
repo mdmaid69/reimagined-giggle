4580996b6325bfa8425acb1e6b2d8928adb5836f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:44:44 +0000

    weather warn alive convenient yesterday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
