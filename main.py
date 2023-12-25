8da002f374edb62e9bd13507eb99b049b81e7a6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:26 +0000

    gate might tap in sometimes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
