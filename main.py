b08bbb22d29656ab350c3be95ce9509adcc36789
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:56 +0000

    shout clearly complaint should love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
