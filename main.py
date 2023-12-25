c3347ef184a97d21a11300a4d4f65da5674c86e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:49 +0000

    middle wheel bad afraid church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
