e5eb8b18e70c962f176d17249f16913a36dce3da
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:42 +0000

    born copy refuse hers feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
