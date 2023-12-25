a0b55947a281a78fc1eb9774224d10a4d8f3699c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:38:08 +0000

    home coffee branch stick deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
