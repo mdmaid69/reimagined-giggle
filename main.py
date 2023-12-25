20b4a46ee21a2a71eb8d5fa4dd2adebee90d4041
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:37 +0000

    let feature about eager workon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
