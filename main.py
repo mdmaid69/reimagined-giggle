cd96e5f3f652c28c212cd10c696718a16d76fed3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:14:01 +0000

    cut disappear separate copy lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
