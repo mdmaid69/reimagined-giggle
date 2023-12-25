4ebcbca6a297aef15e9293364cc05bffdea6cc1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:10 +0000

    modern breathe radio profit body

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def convert_to_binary(n):
+        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
