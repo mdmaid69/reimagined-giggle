499991915a72fedd062e1bae1acdd39f4072f022
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:12:50 +0000

    clue dry soil protest nose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
