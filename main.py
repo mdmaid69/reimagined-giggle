8394503ab28f4f1055ed1d9c68dd28b0e9c734ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:08:46 +0000

    prove blow spread still soak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
+def find_min(numbers):
+        return min(numbers)
