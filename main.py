93000ebf7cb6d00dfa9e42d97e3541cd121e0962
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:41 +0000

    deliberate reply leave century away

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
