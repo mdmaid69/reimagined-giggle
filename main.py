cc4d182d53be2c22fb6cb1c64dc2a080f99ca210
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:36 +0000

    connect ought spot unfortunate description

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_odd(n):
+        return n % 2 != 0
