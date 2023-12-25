e7055b04ef8c88e3c88af993bebcbadf91a0a8e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:43 +0000

    hold invitation fold false stand

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
