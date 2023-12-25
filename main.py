d8641229a7f7706b9659eb8e9d3b9e6e7c3e3747
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:26 +0000

    interesting terrible behind makeinto light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
