318c84a3ac66e98926d19f71b8185e7133583dd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:36 +0000

    identify give sky terrible ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_area_rectangle(l, w):
+        return l * w
