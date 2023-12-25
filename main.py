ddb42024ae3eba21163c740d5073a22417274a41
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:25 +0000

    keepout need serve shock valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def cube_number(x):
+        return x**3
