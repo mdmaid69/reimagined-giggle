1a3fc3b4a38c07a2942d8ecc39216da4ccc61324
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:57 +0000

    work now name dare noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
