38c1bbb112e698fe142d422f9713038ecd05f608
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:59 +0000

    herself shape convinced comefrom Ss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def clear_array(array):
+        array *= 0
