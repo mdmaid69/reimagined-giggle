a8044364f9a32c0bbd72a22578411671c56fa21a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:44:33 +0000

    force them fault method steal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
