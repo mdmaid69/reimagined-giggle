a9d8da022cd1db0b9ef9f4a2cfcb4a46471d22b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:31:54 +0000

    display certain string set switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
