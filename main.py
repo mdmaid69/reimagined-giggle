73731eae07699f4f32d8ee3079abfde947ef523f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:35:09 +0000

    rid callup consult lead centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
