dc4cfd2479415847c1b86a75c4151f881a72de83
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:38 +0000

    giveback noone medium each organization

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
