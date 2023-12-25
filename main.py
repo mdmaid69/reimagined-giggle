bf2198c11f715656651e710314ca611baa98afe9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:35:35 +0000

    rise see official rob rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_set(array):
+        return set(array)
