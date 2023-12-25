27c2fca8100ed125ac034be696a352662c003b29
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:08 +0000

    delight taste own greeting major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
