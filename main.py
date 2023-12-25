a5cd3e60d1426c81a93c721ee802c4c4ea8a4493
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:43:53 +0000

    conclusion him pile become compete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def subtract_numbers(x, y):
-        return x - y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
