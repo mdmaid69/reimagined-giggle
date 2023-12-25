c9d8ed6a496f1c9434c591847b4f93f64777f720
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:04 +0000

    on cheap information oil lie

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def subtract_numbers(x, y):
+        return x - y
