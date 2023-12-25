000856cfc58bcd6804a78d9596ddb51300264bc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:28 +0000

    target poor period violence that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
