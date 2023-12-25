564459a690a8ed58a47d679dbf9f6c0646e7dc83
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:25 +0000

    key turnoff simple nor under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
