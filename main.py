1aa6e0c8644c9e0d5ce42e80225dd176f5d245cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:54 +0000

    collection succeed bet organize approval

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
