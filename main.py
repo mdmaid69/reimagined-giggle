b17dd35e589700c501a6a7b8ad3b0cf45d2831ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:57 +0000

    claim communicate terrible admit ready

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
