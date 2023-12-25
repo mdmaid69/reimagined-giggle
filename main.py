364f8636602dab25378f91a61ee06cad06be93c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:07 +0000

    ourselves travel constantly light worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
