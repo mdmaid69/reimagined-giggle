95c1e2932f1cefe090d8ae812f28d85556274c25
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:22:31 +0000

    talk destroy deliver exhibition sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_time_since_epoch():
+        return time.time()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
