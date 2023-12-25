e23b55a6f91666549d2d86afafa6e98f358cbda6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:21 +0000

    pipe clever house ought comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
