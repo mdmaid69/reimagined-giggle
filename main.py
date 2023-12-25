5451b8a176c47004570de05cedc3b99f3b8c959d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:08 +0000

    success science steam weekend track

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
