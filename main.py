b68bc7951e59803d23aa876c8950cb556fa6dc13
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:48:52 +0000

    person cap punishment likely win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
