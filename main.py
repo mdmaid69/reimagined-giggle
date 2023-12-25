552572e70035fe2e9d676cf59af3735fb46dd046
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:12 +0000

    provide explanation personal contact laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
