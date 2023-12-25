0cfd0f7b3438974c50d80eebc828172fae2dd7b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:26:03 +0000

    amount cold eat wall amount

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
