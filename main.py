e40822f6adacb28cefcf3fa65f21bf3cc4affaa6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:01 +0000

    press print hurry instruction person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
