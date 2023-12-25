2413a3577e5ade33cf197e6712b7a956f62f5d55
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:15 +0000

    tip effort shrink temper contribute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
