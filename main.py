a786012739c4553abdc2e3213172f4f91a23a2bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:02 +0000

    try table value object close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
