3af4aad5cbcf005db1bdf6f69e17d26e882e25bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:55:43 +0000

    work thanks stamp plastic past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
