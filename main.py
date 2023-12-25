4b7d6a3788799e1dcca1905b4e9493887d51630a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:09:30 +0000

    development drive fetch style put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
