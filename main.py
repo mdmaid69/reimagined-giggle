90565981047b5be1542fb99a6e812d0229d5c654
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:11 +0000

    unite drive scare him Rr

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
