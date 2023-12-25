e5e6051c960136597a22a51f778b51e3f6b0b280
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:22 +0000

    year bang sort broad smoke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
