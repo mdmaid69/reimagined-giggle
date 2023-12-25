2116390b3da80920d99c8e4e99bb0f688dc7bc2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:17 +0000

    yourself appeal guess sense noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
