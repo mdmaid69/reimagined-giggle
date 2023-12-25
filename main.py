c997e4dc9a00dacace0e733264d1f3d435cb365c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:20 +0000

    juice exercise makeinto sort word

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  def cube_number(x):
+        return x**3
   def convert_to_octal(n):
         return oct(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
