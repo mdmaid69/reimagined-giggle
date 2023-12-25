424f0c0416ed286266eebf92adc80330c9b58e73
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:38 +0000

    some reaction angry prevent but

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
+def calculate_volume(length, width, height):
+        return length * width * height
