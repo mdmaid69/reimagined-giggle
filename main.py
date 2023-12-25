0640c02e7ed0755d81efc39bdfb90e51242509c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:53 +0000

    family clearly delight alive content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
