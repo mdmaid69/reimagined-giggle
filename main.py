118a858e49c53408c100d42f9799360144a2dfe3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:36 +0000

    talk stare through quarter present

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def get_file_size(filename):
+        return os.path.getsize(filename)
