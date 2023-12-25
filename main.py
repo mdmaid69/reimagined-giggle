ff55d7875a4522f091e8a8e1e60fe8ce76393e21
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:10:24 +0000

    either world just wonder majority

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
