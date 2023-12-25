68725461d6fe53b368321dec513d421b85669e9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:29 +0000

    climb from habit speed eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
