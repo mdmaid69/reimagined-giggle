078568d738b57bf4b7f6f40eef29b7c7f1494bf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:04:22 +0000

    wish education valuable treat turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
