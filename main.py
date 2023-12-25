9a9497847805f3b941f052a5a40082e5c8354b67
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:08 +0000

    extremely fall bound clue proof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
