44fd2ef0101d2391078ae560151c1cecde25594b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:31 +0000

    belief method strange throughout constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
