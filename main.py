5e2564489f827ece31a9fa7eb68c3b6408bf2dd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:40 +0000

    someone sometimes lucky doubt meet

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
