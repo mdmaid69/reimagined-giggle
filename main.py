c8d72cc3f80c8d48a2321cdd46ebb901736c42bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:39 +0000

    hold mend able front create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
