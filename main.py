3709d6ddd6600ccb37f8bffa1ad2cd5fa2327ab0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:57:34 +0000

    let pretty turnoff authority coat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
