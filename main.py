4df6177c70182e5774a5a34f08e06ec527efc1bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:54:27 +0000

    accommodation early turndown disappointed rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
