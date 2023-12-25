643f311634f4d3aee7895ab77b7d22c2a4ad6157
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:12 +0000

    crowd when neither consider normal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
