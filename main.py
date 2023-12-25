e7e7e8103643d2d74c198a92a4c08f21fb81e33d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:08 +0000

    necessary obvious record swim contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
