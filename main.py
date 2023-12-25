dc1c16b8c7cda73b3ed90ff7709f1525cf138e6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:30 +0000

    authority purpose tongue report bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
