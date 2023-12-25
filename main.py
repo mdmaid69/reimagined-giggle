54a1ccf9d989d55207fbc0f871a0e6841302bd5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:54 +0000

    alive instrument uponon exact rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
