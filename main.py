036cbc5d3cab0b1450bdfce126dd8a729cb8f5aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:42 +0000

    subject depend total unlikely damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
