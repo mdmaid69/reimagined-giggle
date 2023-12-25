673c6ea5fd5b62b0484e1dacca64f0474d004103
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:17 +0000

    organize have medium container large

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
