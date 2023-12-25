cb3c91eed69b0486722c3190e4ad9d57a7450d5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:22 +0000

    space question exchange spill area

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import sys
+def print_python_version():
+        return sys.version
