54badf2db584ffdcf07d793b9686532bfd8b2f64
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:46:38 +0000

    demand money transport no thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def reverse_list(lst):
+        return lst[::-1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
