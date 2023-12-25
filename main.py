ad3461930136ae5cd9f29e8a3303465f9236f2db
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:14:29 +0000

    store earth split film there

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
