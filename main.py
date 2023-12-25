44d2c952c3015cb6c1b0419153feb2958f928357
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:58 +0000

    careless tone label language destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
