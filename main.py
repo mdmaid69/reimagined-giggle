2866387f5eff57c7172677b210dad4d66ec1982d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:44:25 +0000

    convinced talk move sister act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def exit_program():
+        sys.exit()
