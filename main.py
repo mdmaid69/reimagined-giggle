3ff443846e401deaa7fd98488c8370873b2ec490
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:07 +0000

    cheek frightened force power anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
