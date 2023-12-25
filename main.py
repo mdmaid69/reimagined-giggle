54bab0302f9f08dd48103062614209d781e3d46a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:14:00 +0000

    explain modern work juice personal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
