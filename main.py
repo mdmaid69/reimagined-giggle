455cadfe08212e81cfdfeae55d38e06c57924cee
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:35 +0000

    shop start rare belief neat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
