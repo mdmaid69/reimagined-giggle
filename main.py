4be83de67c091745831af930800d2067373c7d55
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:00 +0000

    maximum satisfied opportunity immediate wave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
