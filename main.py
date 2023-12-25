107ec5610b829e2e965638aa232af453f33da864
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:16 +0000

    shake loose characteristic single Nn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_acceleration(speed, time):
+        return speed / time
