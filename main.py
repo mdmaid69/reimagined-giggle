0a12a08b3573d6ad2526cb9a882b427cfe1bb26a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:13 +0000

    performance take telephone gradually rely

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import numpy as np
-print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
