8112a20da48c5e43770644b3ee3e76b322da7963
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:27:35 +0000

    ordinary cold takeout different obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
