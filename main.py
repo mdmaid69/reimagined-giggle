4d41697f92675bb727bd5f81ab020e71789eaca2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:50:46 +0000

    effort block wake communication some

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
