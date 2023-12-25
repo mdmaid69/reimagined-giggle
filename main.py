e91c03d09d2cb09cc3044d9e697a25650ed2708e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:45 +0000

    serve immediate getout which she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
