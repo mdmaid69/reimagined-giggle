aea028abc70ccdfdb87e748c46d5b95833ae6a4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:51:45 +0000

    invite steep because exit breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+for i in range(10): print(i)
