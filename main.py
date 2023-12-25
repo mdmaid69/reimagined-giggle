5c5269bcd8a3c45118c963f381c298ceaa2fe8cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:10:53 +0000

    finally too rank performance activity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def sort_list(lst):
-        return sorted(lst)
+import time
+print(time.time())
+def calculate_acceleration(speed, time):
+        return speed / time
