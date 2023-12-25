82363ce22617489253884a09000f0092d7516bd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:36 +0000

    bad thinkthrough spare appeal brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
