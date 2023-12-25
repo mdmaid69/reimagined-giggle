5b5d27f6958003ceb59f60f5c50d4ab98fc2b157
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:15:20 +0000

    drop violence discovery autumn relax

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
