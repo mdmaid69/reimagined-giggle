a62f3cf615786638c11aa721370db71d5054f06f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:35 +0000

    become pick improve false breakin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
