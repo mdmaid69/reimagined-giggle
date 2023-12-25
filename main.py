9ff80162c175b6362b957740d05e28713cef6b5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:54:56 +0000

    precise shut art estimate needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
