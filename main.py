7cb4335c9ba8a586babd54dd282b89009652d4cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:13 +0000

    rich tap damp pleasant air

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
