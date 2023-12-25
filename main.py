ed7c1e7cfd8486c6cf4f98e42266b65dee4b08fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:51:19 +0000

    luck attach which great tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
