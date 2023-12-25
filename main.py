9b7cd9cb86a13459326f628e9b66e23f22905fab
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:45 +0000

    drive duty behaviour Uu my

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_area_rectangle(l, w):
+        return l * w
