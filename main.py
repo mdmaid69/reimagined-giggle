c3153bd3037807e85708c352649fca8230e7fd4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:14 +0000

    by flavour neat go invite

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
