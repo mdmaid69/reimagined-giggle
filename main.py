15cc23f2ccf1b2aaeb45a81452398f0230bf03b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:35 +0000

    term disturb scrape form trial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import platform
-def get_python_version():
-        return platform.python_version()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
