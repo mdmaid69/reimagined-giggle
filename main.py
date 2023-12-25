abcd74382ef9f87b78c249f958ca3ac661bc075f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:24 +0000

    pure succeed silent he truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
