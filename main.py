a52b55a813ec4d30ec1bed42c5a43d83d9a709d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:07 +0000

    lunch tooth chance sense close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
