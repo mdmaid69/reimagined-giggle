183ed6304893c5b02e509c61a1eb2cca3ac97637
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:07:22 +0000

    extremely smile clean communicate much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import datetime
+print(datetime.datetime.now())
