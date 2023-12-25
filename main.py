d927e9d39003e888aa3ca2367eee3721712cd344
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:16 +0000

    murder cheat nice rule valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
