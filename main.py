38790484ce69c15ca8a87a5a8af4179dbf08fd56
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:48 +0000

    tip impressive good relax announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
