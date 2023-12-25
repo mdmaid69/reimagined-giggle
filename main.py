a785e22b1e108916f6e146076c05397b85e82758
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:11 +0000

    criticize car pick squash fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import sys
+  def get_python_version():
+        return sys.version
 import math
 def calculate_euclidean_norm(v):
         return math.hypot(*v)
