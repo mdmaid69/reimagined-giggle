adcaf5da782885c09b92ce296f628846f69d7310
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:35 +0000

    middle reasonable gooff release permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
