f6f2f0d8dae7474b07e71a3f46e6f31a3472b158
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:21 +0000

    big snow fond complaint special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
