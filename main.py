ff820c9476f9b26de0f7b82d5d6319a0ff4794fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:31:18 +0000

    pen offer black influence card

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def remove_duplicates(lst):
+        return list(set(lst))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
