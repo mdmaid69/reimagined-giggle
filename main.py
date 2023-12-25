5026c916a2527205679517b7a7618ec12cab50a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:42 +0000

    wave flight basis limit back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
