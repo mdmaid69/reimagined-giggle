3a7cb6fd6f9c3e9c02a351acb3aec79429b57218
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:45:47 +0000

    realize formal badly warn mood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
