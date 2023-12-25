bc0da20088ee027cb4d73f9a65ce9c45d735e9c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:27 +0000

    teach inquire glass rain minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_density(mass, volume):
+        return mass / volume
