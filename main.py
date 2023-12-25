402ac523bc4676b1588f131cf9f1f69e18656e7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:47:30 +0000

    available weak appeal contrast art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
