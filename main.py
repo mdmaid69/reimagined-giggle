b1be6826f24479d90e5951c75f0b2a01388cb2af
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:27:30 +0000

    since unit bear throw unable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
