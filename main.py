4645f8781a2a493f42788fa99c09348c038b3a37
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:42 +0000

    Ff slow illness congratulate eye

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-for i in range(10): print(i)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
