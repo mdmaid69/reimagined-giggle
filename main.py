646fa9d79865427ca4047971c7d0bac7cf41b56c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:01 +0000

    or should cold joke Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
