896285073a0418dc67e6e2e2d73351a7132b0709
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:40 +0000

    flow mood sink unable fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
