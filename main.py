4d9e83c5bc2608dafe5f7016504ce0ccc9f802ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:44 +0000

    settle frightened command dream day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
