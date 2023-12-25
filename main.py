ea2a979e8949bfde5f9199515599717f3ee9dc94
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:27 +0000

    shoulder separate argue answer relation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
