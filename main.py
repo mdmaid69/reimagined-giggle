734f2e7d5db045c6ca172a32bd1eb4c0b4d5aa79
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:35 +0000

    drive from clearly draw sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
