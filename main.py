75a8e257f4f10ea45486cab752a660f29f9723ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:47:45 +0000

    declaration worried one separate experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
