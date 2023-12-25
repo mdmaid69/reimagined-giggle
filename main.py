3c5552333fefa2019df5d336696a9d8799c84e8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:26 +0000

    similar right wash operation find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
