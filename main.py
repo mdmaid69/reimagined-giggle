c8c99100314288d04ffa24fecd664dd4112b9f56
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:57 +0000

    through stay recommend fasten married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
