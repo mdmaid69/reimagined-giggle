7a6bbc7a84bfd8f15a63d8ba074c2fe3a4217c5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:43:36 +0000

    trade Zz balance deliver 567enemy

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
