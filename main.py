3cd5710119f2957c2e9e94fa7dd83c46a904bbe7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:58 +0000

    Ff depend finish influence of

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
