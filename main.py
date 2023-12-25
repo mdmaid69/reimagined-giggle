58c54f16f8f6d7b07e6f5ff1ae379b8782488aa9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:07:48 +0000

    fight central interesting instruction file

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
