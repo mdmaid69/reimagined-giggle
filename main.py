c834096dd020236ae02e24ffdc8900dd7252ab0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:37 +0000

    range dance touch round fact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(5):
+        print(i)
