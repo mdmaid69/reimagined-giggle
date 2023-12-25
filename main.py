899b398f7c0dde9e1ef671b6ba545882e3099e5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:03 +0000

    square none through sign can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
