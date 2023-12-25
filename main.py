42802e78235623c94c07707418d493508b4f7a84
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:17 +0000

    nor active waste swimming back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
