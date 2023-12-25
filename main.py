eb225daab98c416916811b9645ff93935fb2b8fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:45 +0000

    your fine wear all number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
