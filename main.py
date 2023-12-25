8864c5097d857a5bc586b96d8ea18da871371d0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:15:36 +0000

    wonder accept split nothing right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
