072d5a1838d0ddc005e54a8f0755e6016716e639
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:47 +0000

    key memory thought decide undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
