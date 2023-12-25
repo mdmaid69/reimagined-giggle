350991e62acf4bc7779d380fcc1fd5b89512f54c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:27:19 +0000

    I poison judge fairly disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
