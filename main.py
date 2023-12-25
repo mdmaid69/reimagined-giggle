96e7b66d7304dc6783804000ff60c887ae326942
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:30 +0000

    track 179bite book land 572enter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-print("Hello, world!")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
