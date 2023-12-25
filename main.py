434055e729aa6007f4d019ad35da8a4d1eee75ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:03:03 +0000

    protect love describe card neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
