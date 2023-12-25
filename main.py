6fbd64936878468dd92f0f922404082dcc5116ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:30:17 +0000

    offend power bush hole previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
