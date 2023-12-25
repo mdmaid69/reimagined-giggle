0b979bb1ec17d6607a490a63297e0ffb15bad857
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:16 +0000

    conclusion impossible mother study remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
