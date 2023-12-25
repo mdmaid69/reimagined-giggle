927f24f0bed294f7c3417ca6e396bb896817547e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:03 +0000

    complicated connect classroom majority put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
