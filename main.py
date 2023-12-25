4632d5e6e21acf740099b1cc23e14da6112680cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:43 +0000

    stupid development prize door snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+print(math.pi)
+import math
+def calculate_floor(x):
+        return math.floor(x)
