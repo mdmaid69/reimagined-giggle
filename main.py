68930258575d386efb8c9c286725f12ccbfcaec6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:52 +0000

    nose swallow closed silent wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def is_odd(n):
+        return n % 2 != 0
