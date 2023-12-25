ac6db13bf1b2f246a7356ae8cb3ae2fae0eee9b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:37 +0000

    wake sweet post its house

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
