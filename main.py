5629ec085a9dadef04317bfe1ec576e8122b1130
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:26 +0000

    neck punishment keen agreement understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_frozenset(array):
+        return frozenset(array)
