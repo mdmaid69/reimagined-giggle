9b4d34b36628fcca31cad470e103c51bc083d9b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:37 +0000

    hurt store lower dear wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
