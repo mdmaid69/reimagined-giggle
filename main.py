85183f0941fe0db159fb453ef46c0ca8a723b7e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:53 +0000

    mix might port identify modern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import time
+print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
