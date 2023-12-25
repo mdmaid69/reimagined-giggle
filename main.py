ce8cb813ff19f705ecdcb5edfcbd50ee21a33014
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:48 +0000

    flat abroad depend check worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
