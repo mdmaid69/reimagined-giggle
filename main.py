135e26da5d91520cb1eed9a07788be8e5bb0144f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:01:37 +0000

    answer meaning black emphasize just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
