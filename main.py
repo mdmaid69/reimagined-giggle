ac022cb839804a1ef60d77bb9114ac8d2b2309fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:40 +0000

    hit return private even negative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
