2a54cdfa26f68df13ebb69d63c22a6af1608e948
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:04 +0000

    address but full deliver giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
