33ed57678b3f3e667bf364961d5a2bb2f3a2714f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:19 +0000

    you lend beneath runinto petrol

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_list(array):
-        return list(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
