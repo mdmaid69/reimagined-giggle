d69ce8686ba88b932924941fc99c24c05988b0e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:16 +0000

    lead trouble standard boring by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
