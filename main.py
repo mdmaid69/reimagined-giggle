8c0fab222fb5dad2d9a0e2c77c58760cdd9d7af5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:59 +0000

    now straight callat kind know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
