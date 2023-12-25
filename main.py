255e541a917371ed3b645835395288f5c1283ebf
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:06 +0000

    brother motion it clever other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
