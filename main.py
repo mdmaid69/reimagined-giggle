6374a5d57f780c3c529147dfd964257edfe7e94f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:12:40 +0000

    treat release glass risk health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
