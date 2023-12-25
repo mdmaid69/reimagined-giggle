bc821a0a6f1886358533e20f90bcb6386bf85949
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:28:30 +0000

    the regional exactly honest tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
