c392d63d2d5b125bc4637286589f603ad96366b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:06 +0000

    attention chief charge sleep present

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import os
-print(os.getcwd())
+import array
+def get_array_typecode(array):
+        return array.typecode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
