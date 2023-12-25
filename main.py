9dd1f5cfe093a14a991ced0c3e519a39e79dd034
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:30:46 +0000

    strike recovery speed trade strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
