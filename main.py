af3b10aada61a3bd1fcee9939e078948897a977d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:14 +0000

    flat however his act obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
