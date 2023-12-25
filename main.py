824837f3d71b41d1bb463c7e327fd80706b4fbfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:40 +0000

    his prefer promise call unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
