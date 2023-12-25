8fa62cecedc12e92d42fbfbce46604dc67d25f60
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:22 +0000

    smoke sleep runout reduce total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
