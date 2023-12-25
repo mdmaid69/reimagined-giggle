ea471b424decb85a15234911f12d26cba6ac85f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:51:34 +0000

    talkback hot splash characteristic hers

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
