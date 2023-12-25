fb14a0ae706aecf4c7d195bc542829ee1a2c0ab2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:13 +0000

    avoid hello spray steal basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
