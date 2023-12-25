bb7f896cf195d2c319e9c5390c4ad81949adf89e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:29 +0000

    circle generous both store attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
