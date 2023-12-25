e18387054caa7b4724c5ed62dec85e30c6d98871
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:08 +0000

    mean regret hand quantity description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
