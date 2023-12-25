add9ee8042ec97c5c7afbf0e1d92f97d48f972ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:22 +0000

    style account result connection actual

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
