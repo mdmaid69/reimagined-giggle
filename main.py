1f7dafb085a1f7cd71fe78969223b78ac6e4a0c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:29:24 +0000

    road Qq cause worse keep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
