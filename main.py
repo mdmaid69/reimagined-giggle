5e6ee571d0158ac2d97b91c1b43a3b6ff6f398cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:24 +0000

    house callout above thankyou note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
