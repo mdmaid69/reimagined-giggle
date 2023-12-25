03daecb33394aa78e17dd66d0e8f042d3ab3be20
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:25 +0000

    dull community moment once ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
