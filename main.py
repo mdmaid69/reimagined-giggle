9fe39b13335260a1da12bbe27576410927d5ac09
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:42 +0000

    over quit world situation try

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
