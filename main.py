420997feae8d8f7c0f17ce5a287eaabce845677c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:32:16 +0000

    pay read reaction gift effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
