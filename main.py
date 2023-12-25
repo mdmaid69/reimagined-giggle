26f19e55b0bc22816d1b0671bfa45ddc0e67f793
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:50 +0000

    tomorrow familiar ours getback round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import sys
-def exit_program():
-        sys.exit()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
