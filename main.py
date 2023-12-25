db8af7dd0726f120c0523b4bfd6048db74528976
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:21:31 +0000

    either ability bit knock fly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
