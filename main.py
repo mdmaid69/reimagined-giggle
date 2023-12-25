31cef5d9b9b91cada132bf92eed13fd76147e3f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:18 +0000

    train treat wrong my careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
