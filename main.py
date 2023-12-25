4aabccffb760987e69398f592eee0bd23fdb01d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:44 +0000

    dangerous quantity it no suck

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Reversed:", text[::-1])
