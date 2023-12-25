92049ff3be81e784937c5e700b649992b0fe5173
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:03 +0000

    comeout activity especially exact trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
