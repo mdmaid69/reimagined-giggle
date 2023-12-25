e4f0cf40d7c68b70dcde2bc725792f0325cb10fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:44:11 +0000

    press complete drop set window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
