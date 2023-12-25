66f9599203dd75f78fbb18ed9a55fd3aa671ed56
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:46 +0000

    rich pour appear contribute like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
