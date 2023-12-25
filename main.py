3e2d6ce1e58448743f822f72b74abf86b617c7eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:04 +0000

    perfect skill autumn bringdown recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
