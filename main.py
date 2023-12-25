b93f337360381b708a451deceefdd009eb28d843
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:50 +0000

    stir declare nerve law trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
