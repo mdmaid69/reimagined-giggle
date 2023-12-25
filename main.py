0c791d716bb56019af50e504dbabb55d512e8b15
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:42 +0000

    shock limit within sentence little

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
