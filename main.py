5fb54a96db39f75395a7f58bce167d7605c2410f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:15 +0000

    minute contents tidy right fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
