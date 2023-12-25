fe5de3c116110866eee6b79f71a93d0354457306
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:36 +0000

    yesterday gentle hour break excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
