0869496604538af387552e6dc6039d0943d7d976
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:56:29 +0000

    hope backwards draw prize aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
