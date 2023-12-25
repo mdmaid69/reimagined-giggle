0bced477d67f396889ae713a73f966809d4e22a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:08 +0000

    direction better rate feeling play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_max(lst):
+        return max(lst)
