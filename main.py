f4737ead791ac76f3556a5a063ce987d22ab23c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:42 +0000

    piece complete mind holiday herself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
