2470923858768f508df49ba4aabe162c8c1c5e45
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:38:35 +0000

    evening Hh trial Dd quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
