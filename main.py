f77125ea8a582b727c822ba4a8354d6b2699f7c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:00 +0000

    reduce story sound place news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
