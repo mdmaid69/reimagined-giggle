187ef9bf1fc0d537365354b0a00795283665af64
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:38 +0000

    threaten slightly twist watch share

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
