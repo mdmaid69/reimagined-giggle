8bce61cda34d3bd767e349b5eee3f475f907de0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:46 +0000

    fairly praise dark Ii main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_current_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
