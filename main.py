3951077ed066c2a261e0f157a12a38fb36766f4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:31 +0000

    herself word desperate chase date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
