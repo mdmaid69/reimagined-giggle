3abf4a3af47e8088989b7039b24f2ce151938476
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:55 +0000

    see arrival punishment visit wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
