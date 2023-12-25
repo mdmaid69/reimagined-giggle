7d10def6a6548d87e7bf14cd8de46ede169d8e4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:40 +0000

    wide trip Ii weak bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
