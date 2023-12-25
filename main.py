1d595616cab667671dc8375a0cb1d1646675a38c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:12 +0000

    there onto where because rather

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
