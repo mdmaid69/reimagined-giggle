b0cbe8c88e066ac696ea59896200bd305fc43af0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:03 +0000

    wrong however convincing give apply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
