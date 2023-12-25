f7da67ecb030e2fa8ba41943d0bd8eccbcf6c9a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:56 +0000

    its worst limit thumb hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
