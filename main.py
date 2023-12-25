239c1132f4ab093a30587b5dfac19461a90aa035
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:05:09 +0000

    breakin dry turn whenever cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-print(sys.version)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
