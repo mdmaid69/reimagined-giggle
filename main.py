25c38c6bbe04597155d5cd44d1a2d9f63fd3c985
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:22:28 +0000

    could forever decide slip fly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_power(work, time):
+        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
