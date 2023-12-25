6ed574f48032057d93148bd2e0f500700a6d070b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:03 +0000

    transparent top tough clean form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
