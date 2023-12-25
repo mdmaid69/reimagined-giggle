0537fe03d71dde9fd9a61f66473b96fcdbf044eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:36 +0000

    judge keep health law trick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
