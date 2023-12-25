9b99d9f24a3bd425bd26f3a54ca18b2419d0913a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:20 +0000

    clue soft maximum advise question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
