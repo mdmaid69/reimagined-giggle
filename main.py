ff01553fa2e1347d7b47a6de84a3b7fecefe2604
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:18:47 +0000

    manage brain declare our generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
