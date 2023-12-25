8029137e8db06d2755cd86032eabb246504d5934
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:27 +0000

    knowledge ground meet negative good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
