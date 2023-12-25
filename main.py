fb4e035afda050af55608a75e5a66c7c6ef9f614
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:06 +0000

    for prove across future want

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
