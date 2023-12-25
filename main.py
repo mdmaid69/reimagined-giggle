24ab10a741f7f63db267f185f6ce2576c13e71ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:19 +0000

    shock sign unfair difficult rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
