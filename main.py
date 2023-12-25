838919a9aa3e71aa5e24f035e205139c21ef05a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:51:31 +0000

    control sudden union violent conclusion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def convert_to_binary(n):
-        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_repr(array):
+        return repr(array)
