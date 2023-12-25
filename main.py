93ab5b5810d69a18200769d166cfedc453bbe225
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:07 +0000

    lend sentence bet Dd distant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
