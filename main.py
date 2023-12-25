329a4fc7501a8603c53d3725f35455ac5b103af4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:58 +0000

    muscle wing although whether into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
