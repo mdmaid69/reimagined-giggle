d68f2ad1aeb85000cc105fb5aa4516167b67ed67
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:15 +0000

    much activity emphasize combination spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
