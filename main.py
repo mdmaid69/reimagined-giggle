ea3044b29702d79f324a13cdfa19097b2fd3df8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:54 +0000

    radio straight surprise swim criticism

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
