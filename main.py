0f08df2a9c87878bbdf4cfb29eb565372aa66ad4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:52:52 +0000

    top wet disappointed date add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
