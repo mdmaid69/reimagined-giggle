c8feaf15026d4405697f903f15bef5059df6b7a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:07 +0000

    murder feel by attempt keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def convert_to_octal(n):
-        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
