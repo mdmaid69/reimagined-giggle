fab2bb221f52c8fbeb22a7ebcae153e4178e8d33
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:13 +0000

    exit to amount sea risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_priority_queue():
+        return collections.deque()
