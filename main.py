b050e610252eeb3d7cda0940468c9a63f4dbd510
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:47 +0000

    rest importance dear principle field

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
