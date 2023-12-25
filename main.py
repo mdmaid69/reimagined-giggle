7c36c17e736a35c0f48883d15f61bc8c08622b3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:06:14 +0000

    gap copy left discuss high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
