e76cbe81de5dfc6a96ee04484e5763b7d341c1e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:51 +0000

    sensitive female dry criticism unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import getpass
+def get_username():
+        return getpass.getuser()
