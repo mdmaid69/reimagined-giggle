2492b2f9c96b3a34ccda82411d86995c52687e8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:05 +0000

    Ll address vision tell noise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
