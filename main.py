93dd9ffa332c52ba126545aa51440de2a3e7c01d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:07 +0000

    effect movement contact favour bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
