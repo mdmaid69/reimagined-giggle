d68db9a0ca2487e8f59ccd1919e4a0212c9244ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:10 +0000

    under grateful century quit embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
