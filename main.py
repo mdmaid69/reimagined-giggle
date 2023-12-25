8ffc500c9908c37e3dded17fd88ba3c51b810028
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:01 +0000

    view mother pick gap worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
