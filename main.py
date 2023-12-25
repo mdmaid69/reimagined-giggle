086b1221a9c7f52728e956d273fd6d613928639c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:01:29 +0000

    strange much nowhere relation thinkover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
