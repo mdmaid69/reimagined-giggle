873f96de1283c102a004321faf415dc154ed7523
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:51 +0000

    damage choice equal feel slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
