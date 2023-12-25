0d109d184a6c902c53f19ad7001321d05bf9f1e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:52 +0000

    let then consist her case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
