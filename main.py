405426233503bda438c7de13eb49c083d6589a36
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:03 +0000

    evening flavour from within war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
