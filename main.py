28044f99f6de728aa66b424c0e38c4a1d93c9140
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:03 +0000

    breakout peace question test geton

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
