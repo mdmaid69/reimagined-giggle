01ce96c14e0ed42efdb094ef844364e515d80c35
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:11 +0000

    under left time smoke treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_typecode(array):
+        return array.typecode
