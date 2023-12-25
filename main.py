40223523f94e99e877a5b3b21588b10bf1b1269b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:38 +0000

    allow closed than mass happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.time()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
