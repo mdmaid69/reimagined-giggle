ff9aa9020f793c06dd83a3c1b14985a623bea93d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:57:28 +0000

    benefit pretty satisfied feel band

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
