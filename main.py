6711369963f931abd4e0a2e7f42f580dac1c212c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:41 +0000

    late pride comeout tool blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
