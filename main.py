d7a5cd8ccc63e112a1b42e2c6bc2b0eb6677fac8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:03 +0000

    successful loud exchange boil learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
