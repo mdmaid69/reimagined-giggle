f874ab20c869b5cc9111b7e09fe67a41dae0e55d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:41 +0000

    hers rely empty sea slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def subtract_numbers(x, y):
+        return x - y
