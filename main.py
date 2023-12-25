339de8adbfaf965affe5b5771d8d2a0994398903
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:23 +0000

    false closed command damage smile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
