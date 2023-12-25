60ad8c7625135e204a3657d9dc4c61def7e6e45b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:24:21 +0000

    Itself we bank callback down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
