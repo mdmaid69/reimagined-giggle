e3f8748172dbc523273831aafb18f27d96f194fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:58 +0000

    public always identify relax control

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
