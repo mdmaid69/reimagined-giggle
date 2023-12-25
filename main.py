bfbd51653c792dc6694c431c514caaf3bb92687c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:50:02 +0000

    spread sudden favourite morning answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
