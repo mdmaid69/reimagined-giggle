f68d52f5f86ea268a1fc56b22acc5151938e33c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:19:28 +0000

    adjective callat absolutely influence other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
