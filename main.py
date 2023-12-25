dc610730937cae6c23266627f4518d949db80a77
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:53 +0000

    interview earn behave variety low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
