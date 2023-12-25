574826c0c7e6bfebf079a375884d8967c4da2a91
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:01:14 +0000

    swimming congratulate mess flood anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
