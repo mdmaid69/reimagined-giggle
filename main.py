b197d51f3cc6c573cfb2987ebf59dd0dda964065
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:22 +0000

    treat once rough hollow wave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
