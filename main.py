671c1f2afd68dd39ed9bc6aa6f8ba2d91e1ec6c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:39 +0000

    apparently attempt underneath give human

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
