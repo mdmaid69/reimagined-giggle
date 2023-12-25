aedd77cd20f3cdfbc663228637c0ac640218e229
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:56:22 +0000

    job gate care neck comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
