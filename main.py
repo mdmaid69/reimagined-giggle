5761b46035afa2dbe341a5a12c14ed4874b2ad07
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:23 +0000

    accidentally comment brave period protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
