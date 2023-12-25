90daf2986eb09e3eb85aaadc4c004a22c2c02a75
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:33:27 +0000

    level dear especially study punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
