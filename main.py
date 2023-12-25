a5a89a2d1e396b5ed28fb942f59baecb73837034
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:28:30 +0000

    door thank success Ii frequent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+for i in range(5):
+        print(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
