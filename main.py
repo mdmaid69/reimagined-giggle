992a2a3c8043a21f8cb543c37d88ae0f92f998a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:24:10 +0000

    male floor someone because mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
