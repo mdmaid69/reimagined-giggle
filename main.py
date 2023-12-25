b018f4aa1f5e2e2ffb61f7f4a397fa926b9eb82d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:26:23 +0000

    right funny runinto pile dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
