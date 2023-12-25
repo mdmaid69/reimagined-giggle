8ae45b9615bdd9273c1025f201ce9fd10038b306
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:23 +0000

    lucky shame work wonder win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
