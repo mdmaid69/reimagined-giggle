0af38e225a8525cad80d05d09b0fb9a90963b1ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:07 +0000

    judge my order send outside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
