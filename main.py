47a66a3e49b39786ed782a624414776b30c9acef
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:38 +0000

    ahead want bus deaf wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
