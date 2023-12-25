af6c0b3d1e473e584571108d25a07f7d36278475
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:11 +0000

    hungry negative justice uncertain basic

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def reverse_array(array):
-        array.reverse()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def reverse_list(lst):
+        return lst[::-1]
