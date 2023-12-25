e0cc62447a306bef5231f093521e10e4687e7d3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:49 +0000

    afternoon position properly order workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
