0c13e0c247ab9e10026a8244baf349a05d48f3e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:24 +0000

    combine on spill guess voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
