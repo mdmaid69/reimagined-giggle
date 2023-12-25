d43fabeb4a5ef47a3acd4eb7a5a97cefa65b8a4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:29 +0000

    bear genuine back rare thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
