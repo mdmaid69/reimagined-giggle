970206d420c88d27e2504c648a165eb5134a9200
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:28 +0000

    steam improve getback protection lend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
