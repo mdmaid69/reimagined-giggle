bf646c50e53481ab286cd73016cc4a95be03dfb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:55 +0000

    hard bang free sell depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
