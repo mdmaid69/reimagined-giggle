29a2157c0cc5fb95f4943197126b4bd3b687eab9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:36:17 +0000

    least store thank guard wire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+print([x**2 for x in range(10)])
