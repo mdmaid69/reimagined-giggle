5306e093fe257e768b11b0880be0b959f4acef0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:08:15 +0000

    steer production Ff none birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
