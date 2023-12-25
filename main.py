f9cfcdeced9990ee0e8e34269fc8af47bad1ffa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:30 +0000

    year capital brave clear shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
