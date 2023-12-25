7b090ff743a7c3243ae8abb4a9f0b931835e6fe8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:32 +0000

    many song relationship exchange stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
