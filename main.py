eb1d0a44b539d953f28f694dc06063f3de0eb37a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:56:33 +0000

    bottom pupil comeout before result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
