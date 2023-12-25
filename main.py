d0ada77bee149adc663cfce663d4b4ebf06129a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:19 +0000

    smell recommend capital project pay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def is_palindrome(s):
-        return s == s[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_power(work, time):
+        return work / time
