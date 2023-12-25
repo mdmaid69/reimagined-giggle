759d314c9fd477260e161233fa764220f8aaca8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:26 +0000

    proud unpleasant day holdout order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
