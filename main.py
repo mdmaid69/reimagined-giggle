3fa8de8c75677680deef796bf8034cc9db246b44
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:14 +0000

    may size dinner smile trade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
