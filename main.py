73373abcf328e11428f92cd59d4f1e2b57de2ba7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:28 +0000

    ring often about Gg little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
