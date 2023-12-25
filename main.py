9fb8e16be724751e08217fac76efae691a978d2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:21 +0000

    disturb joke frame centre remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
