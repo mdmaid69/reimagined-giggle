699af86a508208013cc44035aebe9b397563790a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:27 +0000

    practice 567enemy dish proof stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
