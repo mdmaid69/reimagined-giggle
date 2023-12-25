eee84143c2cb55fc6a4149d87b4821b86cedc5e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:56 +0000

    discussion list reach loan friendly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
