eeecefad1767b907a35587894fedd63b6dc1f36f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:07 +0000

    flow often present appearance introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def sort_list(lst):
+        return sorted(lst)
