3785fd0c004d652c7ceb9ef3e06a709e629e48c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:22 +0000

    flavour every afraid weak benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
