71e3366b5b77d7e864b3fd5a470bec18258b2849
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:53 +0000

    reach miss cool past value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
