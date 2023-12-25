215efc6492b223f921281a5c27463dafb3f30519
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:12 +0000

    pair reply hurry bye chat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
