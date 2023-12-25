92130bbe16731d385ed808a9f27f9cc90ed062d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:48 +0000

    exercise shelter system guess sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
