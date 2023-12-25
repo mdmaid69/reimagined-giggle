e7f7ace6a3775d27b1753f6706124b41675915a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:45 +0000

    adjective breakup centimetre exact level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
