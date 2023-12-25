1acb62542e1d1d9a146d8d9b81c428459cfe7ecd
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:42:22 +0000

    decide weigh pen well prove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
