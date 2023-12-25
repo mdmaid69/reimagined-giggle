2e7b28c3c7dcc3810ea42ac4cad4c641f0148c32
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:06:28 +0000

    sticky pause sleep strike entry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
