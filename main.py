0d335c83c28c453a399ccfc673b9474e668d83bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:53 +0000

    check back apologizeapologise entry injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
