1e7a4fdc329dba19ff39a7a5c3a156b8d537f8fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:55 +0000

    replace judge 565end ahead speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
