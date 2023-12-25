a0452678d20bfefccd4deae7f37afdbc234b9cf1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:03 +0000

    ear pocket spring regular mother

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
