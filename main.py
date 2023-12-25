eca5016366745724c2b4d6814fad9be61e21105e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:04 +0000

    hate issue proper most exactly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
