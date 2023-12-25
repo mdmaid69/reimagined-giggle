0a464251fc57aa6f7fee612654e0b9e09df5dead
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:33:04 +0000

    among some available before left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
