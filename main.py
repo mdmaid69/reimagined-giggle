d43226574ca6489d287aafb7fc2b1cb896cf99dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:20 +0000

    anxious fashion over Ll freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
