76dc798dc84b908211bcad6c9f1f89a063e7b8a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:37 +0000

    reliable through finally shoulder damage

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
