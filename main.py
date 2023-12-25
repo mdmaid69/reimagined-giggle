6910491b4e9fc7de0003a65a8df58fa8c20d06d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:25 +0000

    connect juice careful rush no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def convert_to_octal(n):
+        return oct(n)
