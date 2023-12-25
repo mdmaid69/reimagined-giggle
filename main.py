5b150ed459fe0f1346b1740e25fa1a04404d93e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:55 +0000

    lucky eager close responsible mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
