eed365c8699bf935a5782c1c2856c7144de28adb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:42 +0000

    personal talk comedown could path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
