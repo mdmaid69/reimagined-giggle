b1cdf56963091074efca29c24a34f1297288a296
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:14 +0000

    when every behaviour quit unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
