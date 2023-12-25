22c65956e17f224841c6eed4e55dbf9664bb1675
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:16 +0000

    slightly weigh walk computer estimate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
