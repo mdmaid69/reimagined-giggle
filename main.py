7f37e5ece15ff0efce40140301379aae1206a9d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:43:18 +0000

    violent mistake new unusual stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
