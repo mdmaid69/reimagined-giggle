901426cd14490b9190ce4360382c611b17bfbcb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:49 +0000

    suggest beyond movement refuse discussion

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
