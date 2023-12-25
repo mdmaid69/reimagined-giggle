779a219792bd59a5fdbfff604cf4f1724beee05b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:18:26 +0000

    getaway profit mix fine regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
