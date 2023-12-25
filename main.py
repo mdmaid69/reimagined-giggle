c0ad8f47fbf75970cef352afbaf1c49cf85c40b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:52:56 +0000

    gentle that normal comeoff destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
