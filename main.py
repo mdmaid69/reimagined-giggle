27a59f3ce0f4d70fd31692acbf33d540ffd5988f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:22 +0000

    sticky friendly all permission large

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import sys
+  def get_python_version():
+        return sys.version
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
