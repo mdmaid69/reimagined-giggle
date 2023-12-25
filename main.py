537a1f50456455b30375f35859083b35cef61ac7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:55:53 +0000

    share move burn calmly exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def count_elements(lst):
+        return len(lst)
