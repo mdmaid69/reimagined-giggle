ea7aa7b960ea9a13a89471e527cf15d6f9b4e9cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:50 +0000

    join advice crash forward unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_power(work, time):
+        return work / time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
