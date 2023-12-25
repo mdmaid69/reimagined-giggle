c83d37e283690612a00863cbd2cb2abe4cd5664e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:58 +0000

    wrong leg state strike calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
