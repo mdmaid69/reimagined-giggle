9343ac7070f05baa885dad05a385d54a3addcf7c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:21 +0000

    morning spill strike agreement community

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
