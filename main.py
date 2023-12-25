6280f68ada69b79bd48ad3efd664884576fe76f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:45 +0000

    handle keepoff calloff actually shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
