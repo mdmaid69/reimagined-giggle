96aa0b24ec493114f2472e94df3514f3b208d4eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:24 +0000

    hurry past Ww keepup find

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
+for i in range(10): print(i)
