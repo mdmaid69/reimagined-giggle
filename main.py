6f0679efc6bd30003b8de71a32a9dd7a145d187e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:31 +0000

    special information brother middle swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
