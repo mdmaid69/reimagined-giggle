5abe314a6c21845da2ee9ce99ba41c29f2da84aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:56 +0000

    list child clever present pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
