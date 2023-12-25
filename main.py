9485b2c546dfa0aad66fe19258138bf862eb1d6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:49 +0000

    whom lookup absolutely otherwise mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def count_elements(lst):
-        return len(lst)
+  def get_current_working_directory():
+        return os.getcwd()
