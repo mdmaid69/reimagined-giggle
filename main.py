f55b03f2912e474c4c2602a98d432f6fe45ea76b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:56:20 +0000

    straight coffee wander material duty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
