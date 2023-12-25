f56484f4b994f2c253efdad9f899318213cb515b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:18:38 +0000

    either gift geton pinch pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
