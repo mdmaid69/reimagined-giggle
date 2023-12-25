5efd1e586eb0ae883c5b0eb475019e46e17e5520
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:01 +0000

    school sign dark home design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_base_name(path):
+        return os.path.basename(path)
