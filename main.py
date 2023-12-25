c03c745a038a99df30a18009a2259b04ca979a28
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:10:15 +0000

    born lunch part trust blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
