076e493414a5b93edbeb2d590369a0a21f940f5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:21 +0000

    hate use release uponon road

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
