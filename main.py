9d78b93fff9e849ce91de544bc45624edee02a4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:44:39 +0000

    think failure city mainly protest

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
