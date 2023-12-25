9dbc8590866847e38dc186d4513d8eefb0c07d04
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:44:44 +0000

    compare approve live slice improve

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
