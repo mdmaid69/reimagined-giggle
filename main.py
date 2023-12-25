09faf15fe54ac8e491790e18b44ac9aea87fef7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:33:37 +0000

    paper colour grow creature contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
