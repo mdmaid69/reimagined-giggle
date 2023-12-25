5fe369b27c94470c78afbd315ccee69ea42f3439
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:37 +0000

    influence zero dull tidy broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
