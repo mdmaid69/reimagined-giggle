89dde7839eb117fec24a549559ec49b749c13dbd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:03 +0000

    everywhere nerve pleasant influence gap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import os
-print(os.getcwd())
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
