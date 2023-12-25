914e22553086cf84c69d8b5e1e051c27a05cff8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:15 +0000

    fall spot belong plant point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print("Hello, world!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
