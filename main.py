aa03c22b1b7d2378a54635ebdd35195a83987aec
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:30 +0000

    mix genuine delicate bother course

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 from collections import Counter
 print(Counter("hello world"))
