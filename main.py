1bcd03f80a8e5f9c20763472922347af11adc26c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:26:20 +0000

    unable fill impatient something favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
