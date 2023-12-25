d3b6042adf06963e43bd1de3cbd9df81fed6e965
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:50 +0000

    improve workon why talk chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import random
+def roll_die():
+        return random.randint(1, 6)
