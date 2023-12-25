5e249f92c3411e32b6742b6e043693fa4e66f4e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:49:40 +0000

    knowledge alive stop possession test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
