518e2990f5d77ce7d2be89fb2b4256f99815ac31
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:37:21 +0000

    mistake contribute full voice swim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
