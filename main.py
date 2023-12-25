e2642b99cde7df1c479db4edbcf32ada9d74f1a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:31 +0000

    turn first top cruel his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
