439395de6f0e7cf6afa2a934ec36348a926ffa3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:22 +0000

    march subject conclusion deep slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
