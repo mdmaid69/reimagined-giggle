41b95c74e8a9f1c0debb745fe0082e5e5f7e43e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:59 +0000

    present satisfactory Cc street almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
