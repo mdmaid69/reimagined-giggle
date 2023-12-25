72e73b345f6bbe8372c5a221927a60edb8da1f60
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:28 +0000

    characteristic business shop pleasant her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
