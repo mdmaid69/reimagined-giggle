513352b68a139eaf8171868c7029b7b8b981c613
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:28:28 +0000

    lay process set replace past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
