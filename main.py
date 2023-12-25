7544eaead05c77581d56ec4924146bd2401f7daa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:27 +0000

    poor happy turninto moon nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_work(force, distance):
-        return force * distance
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
