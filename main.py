3a1f9c7f02369e35d02bc8fbf10d1a58c81dd0b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:00 +0000

    foot sort move aim phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_volume(length, width, height):
+        return length * width * height
