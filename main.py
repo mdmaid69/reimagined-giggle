604154932386be097b75782f69d539c75a676b76
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:15:54 +0000

    difference care nigga progress mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_hex(n):
+        return hex(n)
