58bc1fb969caa8a6bc2753f7ab97627e2480bd19
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:44 +0000

    makeup yours tense secret behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
