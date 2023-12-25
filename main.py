774f24374523b0e459f5d267782d9d3c831b3adf
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:15:19 +0000

    picture adjective only bringback frighten

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
