27eae4337427d5382db45e262b5e866a164072d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:39 +0000

    hers try emphasis theirs important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
