74a8fae2f5a06bebbc4cca8ad84cd045d1e82db2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:50:17 +0000

    clever unit confuse hurt answer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
