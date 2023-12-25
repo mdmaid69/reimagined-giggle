d8e37b8ad8b0a6d9bd761f5b67cf128eb83a7e7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:26:11 +0000

    getaway beneath example punish course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
