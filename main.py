d8586e9f525f43a95aa90a6d21a133a465af522d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:41 +0000

    worried question violent order where

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
