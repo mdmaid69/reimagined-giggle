8e13f0c58896c244594b407e023e5809dbfab5e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:57 +0000

    better ashamed crime chat break

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
