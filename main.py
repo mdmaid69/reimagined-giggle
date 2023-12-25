5f28525ac1372d926277a1603f2eaca60a5204e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:03 +0000

    lean guide scrap board satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
