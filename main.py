14d998ca61f01522a4af02101f1aad642d640dcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:04 +0000

    Ii mind maintain also appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
