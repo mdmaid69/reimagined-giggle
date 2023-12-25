da93ea195d8169f5a24636cf0cc504eb87f0004d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:22:42 +0000

    his snow eager show 565end

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
