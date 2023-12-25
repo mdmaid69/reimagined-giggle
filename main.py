1485da71ee84fa601f7827202060fe4aa4a8317e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:12:00 +0000

    truth curious splash fashion confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
