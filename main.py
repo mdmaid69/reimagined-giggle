833933a773736240f8be02ef89b0e9db349ffc45
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:48 +0000

    uniform bottom acceptable flat rank

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
