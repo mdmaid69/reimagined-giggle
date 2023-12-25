824d4cba3e4fab52c547bba71e4a7be797bbed1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:33:22 +0000

    respect 564encourage comeround same spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(5):
+        print(i)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
