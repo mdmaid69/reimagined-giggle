f45c534c0174ed79f0e1b09a576127f2ae20ef38
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:40 +0000

    shout concerned real Rr round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
