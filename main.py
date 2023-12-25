75a3e756d710e860db8e457b7eeaf25874f62c64
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:52 +0000

    unite rescue turnout control near

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
