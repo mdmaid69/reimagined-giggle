8c129a32cccb622ae6f78c2629bcbf1e8cfdb159
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:08:02 +0000

    shrink sport father pay such

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
