33fe28fc4c5da164add5f900852940d6e6b8d67a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:28:31 +0000

    trial mass professional talkback tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
