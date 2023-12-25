500da52a5d017feefe20814aa462b5b08708c182
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:28 +0000

    draw beautiful accidentally second use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
