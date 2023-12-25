bd978581379bebbdaaec4707a5dcef7737432d3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:40:31 +0000

    needle spray history board squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
