04b88fd33664647fb708920a62b0de6968477f93
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:02:27 +0000

    basic keepout further payment persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
