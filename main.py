415862bf421d0c9132069ef7ac94362f8e06b5f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:46 +0000

    joke mine afraid division ask

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
