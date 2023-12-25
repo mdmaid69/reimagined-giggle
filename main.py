3acd4cc09d8820c674df5614ff0c06c6f5336afa
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:49 +0000

    approve agree knock rain estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
