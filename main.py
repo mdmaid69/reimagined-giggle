be5db257789a50199e31eb89f35feb5dbbb43df9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:43 +0000

    immediately tragedy date ashamed perfect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
