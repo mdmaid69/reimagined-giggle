85b08fd4c82c18fd01a5e8c667cbf0fa192d7e12
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:56 +0000

    occur build nigga we age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
