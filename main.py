119c33f73644d1296a33638530e46f4f4b3a16b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:20 +0000

    apologizeapologise yes speak whoever or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
