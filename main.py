7533977ee6bb4a0ff579155f7aefbbea3207525c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:42 +0000

    go nowhere shoot turnup expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
