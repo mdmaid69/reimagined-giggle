443b4e01f1edbe8d73bfb67a404db36475a7aa86
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:14 +0000

    welcome suffer past ilovedicks recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
