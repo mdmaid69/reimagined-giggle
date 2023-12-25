b3c9be0fdfc1a5857fd91f3c99bf6629f48ba11d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:59:56 +0000

    performance achieve appeal storm reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
