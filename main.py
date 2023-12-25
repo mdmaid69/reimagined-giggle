ed4bcf1d07a9af6812f6163d5cfccb5c8f3b456e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:29 +0000

    neat judge which my brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
