d19fbbc10a4683ee724003e8ef63061a9cc76004
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:06 +0000

    clothes control exciting exchange attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_username():
+        return getpass.getuser()
