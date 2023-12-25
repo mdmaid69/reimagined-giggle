9000eb12b9ea772a5fa0636b775fef5cd41e11a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:47 +0000

    seat birth Pp understand rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
