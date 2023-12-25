15a470e23b72cf5f2256a06122444d28f84717cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:19 +0000

    reason act love try introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
