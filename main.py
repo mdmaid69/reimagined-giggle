0c53b359f468dac510309ee2eed91d73f409b097
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:18 +0000

    clear come emotion deep lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
