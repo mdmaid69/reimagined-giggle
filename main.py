a66db3f3fc2bf85d8e4f83a00b25d432fcc26f58
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:59:36 +0000

    union cap expensive children immediately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
