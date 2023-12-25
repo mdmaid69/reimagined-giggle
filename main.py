12bdd9e283be6804a8d270962b2687dfaa9eadde
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:30 +0000

    attempt delight hair morning Ff

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+print("Hello, world!")
