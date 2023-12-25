a186e96f37fd8310e8b8d4efaf45e660459378fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:36 +0000

    world under open sweet freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
