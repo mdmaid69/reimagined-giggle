99c03d457eef498f84f6fe40c29ff60bd851ac27
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:46 +0000

    off sleep platform different continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
