29f97b16a8bdc26847a57d5e5b421211bbd8b9d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:57:25 +0000

    check wheel foot introduction project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
