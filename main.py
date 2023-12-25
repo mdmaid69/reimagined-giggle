c52e823b83dd480cf97bf39aa1d2c50ec25de88a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:31 +0000

    somewhere admire complete test memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def get_array_as_bytearray(array):
+        return bytearray(array)
