754831ff4b05cc29f7953da0abd56a054500ad65
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:33 +0000

    limit comeover turn listenfor separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
