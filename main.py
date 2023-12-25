981fe87484b47b6817697477c27664ffacbb68e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:04 +0000

    female getback shoot coffee ring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tensorflow as tf
+print(tf.__version__)
