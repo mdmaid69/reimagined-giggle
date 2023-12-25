c0a154d5e70528200e388139528952ef6aa978ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:21:01 +0000

    since hear second innocent shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
