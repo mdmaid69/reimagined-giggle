99dca434828c5425ffe8e9df047fda942cdf3126
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:59 +0000

    ignore direct keepfrom judge invite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(sum(range(10)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
