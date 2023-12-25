6ac71ca13081806f94a6d5ef5a55e9d061442ef7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:03:07 +0000

    load blind here mention this

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
