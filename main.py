9865f879868ed9319d198081e2430890bbff7cdb
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:44:26 +0000

    less pride spite good introduce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
