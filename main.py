ea09ec221f557dc309657261412efe02a80a0d4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:12 +0000

    takeout laugh gradually model pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
