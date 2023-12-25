77d27e7cf773f982076309a323663e7910014ea0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:59:19 +0000

    programme come even along wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
