8255a25249bbb3165c3b874e22f187024abc2fb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:26 +0000

    type post turnoff sleep nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
