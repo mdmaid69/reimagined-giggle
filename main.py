feffc17be61753113bf22e71730fe2197a6db469
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:42:02 +0000

    cost skill consult issue react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-for i in range(10): print(i)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
