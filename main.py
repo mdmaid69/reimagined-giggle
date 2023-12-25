04e79901c2c82708f29b539b52b49d2d7f35bd79
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:27 +0000

    Mm press exam tip state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
