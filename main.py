5c9643e673b1b3f878afe0406fa49e16c3b59c7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:52 +0000

    bus 569enjoy early before neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+print(sum(range(10)))
