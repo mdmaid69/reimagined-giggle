78a623932eed47fb7fa2e4309a9084d133e5348b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:41:58 +0000

    rush scare return miss somewhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
