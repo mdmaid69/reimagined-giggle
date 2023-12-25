25b84b4b6bb849bc5822ee4cf794dab70733ae5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:11 +0000

    fit pot sting fire advise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
