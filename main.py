16435824670677c05489e922fd35c0cc34a5b720
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:36 +0000

    ourselves provide still interview cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
