246ec47af036a39ef758faa312d809283ed9ad42
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:40:11 +0000

    try morning defend the question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
