cf2ef8e289912656ad64b9cd3cb154cb063930b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:58 +0000

    spring window creature sister hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
