f9833fd8e769773e8941bdefae61bb9ebe10414a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:20 +0000

    moon cheat edge point remove

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
