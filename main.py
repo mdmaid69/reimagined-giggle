e79191b5ae6f7fa54a643d170c872faf5be8f4da
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:14 +0000

    born sing back habit guess

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
