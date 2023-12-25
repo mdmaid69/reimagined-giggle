8e1ae246772d16222d8d66d1614c3ebacbf8b1ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:35 +0000

    dull sing detail cap each

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
