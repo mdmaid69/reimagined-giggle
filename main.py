d6480a8216819283b5720efc9c03e39ea18f73b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:14 +0000

    crash Dd turninto behaviour 572enter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def convert_to_octal(n):
+        return oct(n)
