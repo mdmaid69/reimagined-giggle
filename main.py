9ac6408872cf37b0e1b2e9408d44bac2e50c2322
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:47:28 +0000

    track lose light want question

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
