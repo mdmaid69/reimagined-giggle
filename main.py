6d26e07d4cbd04b4c073e701795cf91f5d0356c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:51:21 +0000

    need strict fall culture hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
