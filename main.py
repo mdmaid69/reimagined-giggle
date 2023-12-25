c42049230dc375c6bd5bcaa7895357ebb271ef94
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:02:05 +0000

    fold interview wide careful rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_set(array):
+        return set(array)
+import collections
+def create_user_list():
+        return collections.UserList()
