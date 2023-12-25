37b9f2856d4f15778ccadb78027a037abe182d1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:54 +0000

    connect rare agree seem live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
