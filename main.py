6098ba595bf93809f5618d2eb41506a8dd402909
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:01:26 +0000

    win right past brown tomorrow

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
