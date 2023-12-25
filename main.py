c865ce6d47b0ca43120b5f3028b78eeccad6dafd
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:07 +0000

    passenger worth payment present cross

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
