f1018341632abd744dd3d7b258e46658b48d55d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:35 +0000

    Dd surprised wing shadow tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_string_from_array(array):
+        return array.tobytes()
