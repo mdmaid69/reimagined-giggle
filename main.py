ef11a83bf076d77501460cd8b479e11145140106
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:48 +0000

    right meet cross wing serve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
