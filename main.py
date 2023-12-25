1e17d665a0d74799c1fb6d7dd2d5ae15c7b0d9c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:47 +0000

    share post affect breakfast 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
