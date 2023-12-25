1ba7fe6484b06fc0e6554dfa3869268b16c726cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:32:12 +0000

    anyone rate general 565end able

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
