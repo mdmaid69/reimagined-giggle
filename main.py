8ac2b2e27221a65f20168e7c9b46e8ee775ca061
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:29 +0000

    introduce area as fear peace

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_list_from_array(array):
+        return array.tolist()
