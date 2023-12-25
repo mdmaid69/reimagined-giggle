e63a206d387d207e68e69c0c2c1bd605764869fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:57:43 +0000

    round colour angry impossible less

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
