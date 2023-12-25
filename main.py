08114060e83067d699b060b072e69378f3b86505
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:59:32 +0000

    mention exhibition hard claim funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
