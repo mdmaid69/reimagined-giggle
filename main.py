cf0951b58f6fedc38bebb46b06f6d3629b8c05f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:09 +0000

    many side getout in mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_length(array):
+        return len(array)
