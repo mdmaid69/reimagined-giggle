ac80eb7c8f34b976e24dbcc3c33bae33eaaf0ce2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:17 +0000

    criticism sentence comment sometimes late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
