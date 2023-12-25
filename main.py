e6f5561a82f1125aaabbf1907b359272d98fe42f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:54 +0000

    secret risk welcome reaction impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
