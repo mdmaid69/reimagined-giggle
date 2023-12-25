8bad5998c449df88fc108b4cf0931b58c60cef41
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:11 +0000

    sort year cost meet book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
