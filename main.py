6d4274ec7d98b6fc8ba0878b9d87e9eb14f7e2c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:36 +0000

    allow dance public convince right

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
