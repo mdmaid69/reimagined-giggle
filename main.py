76322244b55b72fc30dffdf821bf77f501cf9c72
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:37 +0000

    recognize frequent circumstance world defend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
