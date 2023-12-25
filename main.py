24a77d88bd21d90734a4452887bac9470b360a1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:41 +0000

    admire ride enthusiastic brush minority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
