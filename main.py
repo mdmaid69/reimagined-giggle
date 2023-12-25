504cc123c6a2c9f141ba3f4ce5937e85dc976043
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:33 +0000

    tour copy responsible loose bottom

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
