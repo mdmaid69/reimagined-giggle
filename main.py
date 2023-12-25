b09a69284e3310cb41038bceba4392ea77df5d99
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:37 +0000

    side influence health maybe sort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import platform
+def get_os_info():
+        return platform.uname()
