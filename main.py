8d63badd54f0c22bc7e89e757153a298628bd75c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:59 +0000

    outside thinkover write precise when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
