bf51cece21a0565e564d6fb50f46989a18fde228
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:31 +0000

    conduct prove major reduce 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
