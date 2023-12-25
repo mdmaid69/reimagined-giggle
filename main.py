dab362b14c267c135db54fdd36679c1699b7512e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:25:23 +0000

    substance measure although tune fortunately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-print(re.match("h.*o", "hello world"))
+for i in range(10): print(i)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
