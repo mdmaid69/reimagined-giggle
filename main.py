b5938db3aba139b79db668797e1d63ae7f9b92b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:13 +0000

    cause break announce example rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
