afd4dca2054fbc55c95cc631074d0965aec5cae5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:00:12 +0000

    conduct delicate develop unlucky downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
