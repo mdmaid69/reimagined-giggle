8c07d6596212f53540fb188d23bfc5b280e9e6f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:14 +0000

    callat write issue bottom answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
