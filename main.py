591a06d977cab0abf14e9250618a9d45bf5a6205
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:23 +0000

    off please none continue shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
