85dc4abfeaf6a70d63438506526fa4f62a696951
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:16:47 +0000

    through film club might comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_max(numbers):
+        return max(numbers)
