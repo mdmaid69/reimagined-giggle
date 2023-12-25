afae3729a0e30b4705dcad361fc377854f05bedb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:46:05 +0000

    ourselves politics invitation trap shrink

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(10): print(i)
 import collections
-def create_counter():
-        return collections.Counter()
+def create_ordered_dict():
+        return collections.OrderedDict()
