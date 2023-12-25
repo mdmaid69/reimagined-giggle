75ed150b6f97c8fae63e80328315d5b3807715f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:25:03 +0000

    body guide experience suckme point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
