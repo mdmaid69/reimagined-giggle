c7adb45bed12b44302286c0110a8b817a05595fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:20 +0000

    heart mend straight point be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def subtract_numbers(x, y):
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
         return x - y
