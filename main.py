62488fa37ed3efb8009c374479def4b8b15ce520
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:43:23 +0000

    measure common path order principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
