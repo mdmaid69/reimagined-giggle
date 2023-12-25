c4328c5e55703c67bc0c86bcea667717646dbad3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:37 +0000

    belief even child Ff apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
