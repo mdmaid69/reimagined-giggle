4865d555161e71ba1b75362fd3afa35d7d71a023
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:18:47 +0000

    run influence tense congratulations be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def add_numbers(a, b):
+        return a + b
