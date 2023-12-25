09e37e90cce82bfbe29cf96f92b96932700cec26
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:15 +0000

    sleep phone centre tidy space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+name = "Python"
+print("Hello,", name)
