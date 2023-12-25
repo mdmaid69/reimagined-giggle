68a6a761bf772f3a5958140c80b4790613b51046
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:11 +0000

    goodbye fear soon contribute phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+name = "Python"
+print("Hello,", name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
