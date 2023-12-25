c0d01ed32f7e539d9cd2a50b4c84cd229187f491
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:45 +0000

    total influence nothing string ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
