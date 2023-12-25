aed1228c8949eabe656f20718e137cb759ac8bb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:13:09 +0000

    ought could except spring describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
