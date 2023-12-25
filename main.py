c67448b073ecd2873d98995313bc4d14370be394
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:22 +0000

    avoid him art only warn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
