edf7c5e720713bacfc8cf8e0970ddb697ce13345
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:20 +0000

    busy completely Hh violent less

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_perpetuity(payment, rate):
+        return payment / rate
