fca6ddbd590d3a0c2807cba39e9b52dc1fdd8859
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:17 +0000

    turnon reject since pinch attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pressure(force, area):
-        return force / area
+def convert_to_hex(n):
+        return hex(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
