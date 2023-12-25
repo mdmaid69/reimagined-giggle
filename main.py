44d3206d35eff7dd6ea5c546f58b33752f64e379
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:08:49 +0000

    raise idea manage gas force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
