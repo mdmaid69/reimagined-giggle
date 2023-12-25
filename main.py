a926d301d02555d301e9d90d80d9643b9ea8dcb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:13 +0000

    punishment sleep cross skin chief

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print(sum(range(10)))
