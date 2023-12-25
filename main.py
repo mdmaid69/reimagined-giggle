6d6109e05f421b74e2f49dbe34547f0efda614b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:34 +0000

    lookup shade whatever afraid compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
