d9afbd8e2b0622acc5af7fa6d0a22b36eada2ecb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:09:59 +0000

    comein left regular pair wrong

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
