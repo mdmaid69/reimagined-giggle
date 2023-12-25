a31e471685f256a59ddd4c9aebe9ef3f8adc7f44
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:46 +0000

    prize switch whether 566end while

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
