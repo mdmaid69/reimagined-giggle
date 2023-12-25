8c064cbc98450c2e7018dc698f37391940d53156
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:10 +0000

    lend flat trust contribute law

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
