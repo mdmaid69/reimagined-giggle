7c50f18571d20599ed2bfbc42843f565aaecdac8
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:46 +0000

    pride admit sit instant tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
