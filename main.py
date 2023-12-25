086171efdd9fbf3ae02e0a9fa1879dd843f29b65
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:39 +0000

    sticky anything discover tone team

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
