7edf1b5922e4c5196725a495317cdd12fee7fa52
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:25 +0000

    in stir shout control creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
