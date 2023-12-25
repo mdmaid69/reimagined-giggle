405682df97d318ee6a1169b7f2cd6f48566f8050
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:39 +0000

    cruelty professional shock choice willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
