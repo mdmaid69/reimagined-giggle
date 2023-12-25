178c3d4ca36b74f1a4f0beaa513c3dd6baf93f8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:19 +0000

    tip this worth phone clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
