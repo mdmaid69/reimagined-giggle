a3e7e15eb602b20a55637110c8f58c5d095d581e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:31 +0000

    possession play fast relief curious

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 def fibonacci(n):
         a, b = 0, 1
         while a < n:
