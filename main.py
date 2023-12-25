0a5ee93eb8ee71cdaf2c580416cbcc4bafddd1e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:35 +0000

    continue action often perhaps provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
