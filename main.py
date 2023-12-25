f81503888dfcba99144b54b14a1431750a395d1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:58:39 +0000

    order worry waste contrast wealth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_item(array, i):
+        return array[i]
