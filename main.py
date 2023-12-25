52c8a3a95d3b41507003ba88f1aa92d065b03a28
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:27 +0000

    OKorokay reserve tense fashion before

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
