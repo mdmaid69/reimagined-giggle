28e91fd7dbe3311c20a29d7cbe85ca2589a92e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:32:31 +0000

    Itself score emphasize before clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
