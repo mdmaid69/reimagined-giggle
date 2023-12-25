120b12ef9123e6ea3d90f4183978fe72d8565782
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:47 +0000

    take that next clearly punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
