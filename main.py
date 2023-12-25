d1710ce748637e43ea0dfe6eef074bd1d7fd9dba
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:03 +0000

    dig play appear company runaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def reverse_list(lst):
+        return lst[::-1]
+def convert_to_octal(n):
+        return oct(n)
