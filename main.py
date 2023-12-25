0f71df42e631d03b2ebccf766afe3309335d0e77
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:37 +0000

    whoever visit opposite fast ever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+print("Hello, world!")
