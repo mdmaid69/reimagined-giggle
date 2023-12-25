be0c8eb4c0348124c6e3690061bf9e80bf189682
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:22 +0000

    explain both please major opposite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_pressure(force, area):
+        return force / area
