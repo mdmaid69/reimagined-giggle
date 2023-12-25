ab2066dcced5156a6201ae25fcc258f998d8eeb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:35 +0000

    ticket right patient male treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
