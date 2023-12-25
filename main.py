e910314c71671dcb13df2e0515b1662f41097c8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:29 +0000

    check task cum disapprove 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
