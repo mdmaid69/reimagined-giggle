1bd8a7e404b5d308055f6c34de4c4c8dc7acb638
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:17 +0000

    telephone church meeting teach story

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
