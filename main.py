407b106132c9ad9ad30475cbf9358c1c4627c756
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:28 +0000

    quit statement traffic match exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
