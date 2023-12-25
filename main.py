71e25b843b0c6994def82d588081a424a581831c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:56 +0000

    dry gather charge fasten comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
