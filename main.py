85b4cc9e065acfa9795017e90841ab08ec05eb3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:27 +0000

    ride later young loss music

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
