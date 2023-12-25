eebcb78a76e43fc4182c705dbf34740f81ef86cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:14 +0000

    runinto turnaway phone sting earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
