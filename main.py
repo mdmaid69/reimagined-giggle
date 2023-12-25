d40a8ff80e309adf7c63532f5999b86644cd11d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:35 +0000

    well yours repeat method brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
