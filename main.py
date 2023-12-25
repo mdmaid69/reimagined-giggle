49ce67753288fe793da230bcf44a19d18dc5c63a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:06 +0000

    harm nervous fasten solve blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def clear_array(array):
+        array *= 0
