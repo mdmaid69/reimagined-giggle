f05432f81ea24b7bcad63592bd3f4fd090bf9d19
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:13 +0000

    plan noone state later so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_length(array):
+        return len(array)
