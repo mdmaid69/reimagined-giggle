4666a280b4870eb3c7568c4a7e016d8d268622b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:10 +0000

    variety angry breakdown increase give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
