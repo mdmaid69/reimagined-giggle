d223aeae2499fc203d826bc380a6eabc128744c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:59 +0000

    amount complete speak polite base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
