fffe7421c374675d979db2d163ba7b080150c7d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:12 +0000

    listen point tap extremely rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
