8f5ec8e527f062f035dbee176450dca2abf01f29
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:40 +0000

    before autumn affect keen pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
