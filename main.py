e976142125968da7fada5d9c2ee8b94c3bd8b4b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:18:07 +0000

    instead direction hurt 564encourage concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
