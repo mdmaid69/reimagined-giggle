80d696bc782e9e4e72edc02cd24b1f4fcde6064b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:36 +0000

    about maybe lunch human separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
