2348125d93544ae3b3b55970c54360566b79361d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:52 +0000

    lonely glad before relationship lie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
