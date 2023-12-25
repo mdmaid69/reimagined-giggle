5ef2ead2968476000e1b50a7deb0147b94934930
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:23 +0000

    uniform take moon borrow little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
