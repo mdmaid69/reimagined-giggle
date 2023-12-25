8fb61a99ca6037b7eab3675bae79ddf628f6fe57
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:56 +0000

    scheme action notice laugh dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
