3705a9ea04c177fd9c14efdcf0f72d96dd0cb899
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:54:39 +0000

    motion fork private pass mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
