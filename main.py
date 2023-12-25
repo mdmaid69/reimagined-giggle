d982bd9016b216ff8800cf473ad8f2d0170c7c41
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:23 +0000

    solve clear please peace press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
