30be293f2f9abb3db86f60dbb0d9d8efb913678b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:39 +0000

    mind about team protect part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
