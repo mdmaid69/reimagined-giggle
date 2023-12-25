708aaef5db4f7ff9d72b2317142429dee30413d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:13 +0000

    attempt most stare tape begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
