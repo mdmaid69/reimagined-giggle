dd9babff5dfa75f4d4970eeb62cb1fdc51fe8ee2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:05 +0000

    ago late exhibition to argue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
