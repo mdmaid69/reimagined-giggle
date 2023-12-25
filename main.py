74cab9f755875a8745cde9a80d588cf22ed66369
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:49:28 +0000

    summer combine tip fashion surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
