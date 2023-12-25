dc6df2afd81f01ac0c26c74859fe3852e079a2ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:18 +0000

    yesterday difference afford now correction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
