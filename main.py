d0a12cde9eb6c1cd073e8144da8fbc6dc55bced5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:49 +0000

    juice history absolutely performance innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
