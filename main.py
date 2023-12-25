287031c7f2012f4a58262d9d2f029f6eed034df7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:18 +0000

    success medium for stupid few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import datetime
+print(datetime.datetime.now())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
