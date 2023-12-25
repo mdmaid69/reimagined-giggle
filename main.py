06ba1030d71016beed1665ae6cf0062fd10c3513
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:05 +0000

    clue waste come impatient will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
