1f563107a0b00f7f854fd5c37d8bd8552b93cc8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:09 +0000

    plenty nature print careful within

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_queue():
+        return collections.deque()
