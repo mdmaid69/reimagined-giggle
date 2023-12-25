b530bc7ff95bebf287fc67c735ae79130591bd97
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:45 +0000

    rob whistle quarter travel gather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_length(array):
+        return len(array)
