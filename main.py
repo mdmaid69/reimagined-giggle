c2a77e15b0f544ed15ca7fd7814060865a0adb21
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:12:07 +0000

    flow rescue year alone course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
