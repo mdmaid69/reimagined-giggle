09240139e889ddfb2b8f728f75256b8182959a7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:20 +0000

    inform healthy through life platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
