4e275f083a50c7fb47ce574f44c2480fd0520e9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:54 +0000

    sudden fight possibly comeback sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
