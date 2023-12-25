c8b0933f4bf7c94ebd6f92143b3f7f8d582ce5d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:19:31 +0000

    crash smell production challenge might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
