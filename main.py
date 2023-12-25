f30e4a097b4e0bb4d524734868ecf674141bda5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:33 +0000

    even summer sense confidence underneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def sort_list(lst):
-        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
