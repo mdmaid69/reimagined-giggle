04a8aab9f88aca5afd0b67e143e2552b0a5073a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:13 +0000

    score seem total concern consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
