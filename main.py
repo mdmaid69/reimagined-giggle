8ebeea546c301076ae68b63f6dfb82354cf05964
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:29:44 +0000

    cry bread swing form properly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
