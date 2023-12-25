64a466664de3d5af0637909a2500fe236cae0b72
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:24:42 +0000

    sky excuse giveout careful eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_min(lst):
-        return min(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
