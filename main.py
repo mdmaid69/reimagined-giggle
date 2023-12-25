29d38517ec5a0e6d9801ebcbaf03bdaaa428019a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:45 +0000

    sale now care turnup friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
