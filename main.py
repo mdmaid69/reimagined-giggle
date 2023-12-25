ef70012a479514c476492fb2e0b1e8dfb6198f27
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:58 +0000

    path dangerous bridge past draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
