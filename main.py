d04a9bfbd2b712bd7440528b7fb24b6da843edb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:14 +0000

    steep material either some maybe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
