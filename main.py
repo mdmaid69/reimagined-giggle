6df37ec23e86a9ca36d7ad16b7a61d5da40033df
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:16:55 +0000

    prepare any challenge yesterday tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
