39573601742e88c4b099e620a6af8fa51e1e2b23
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:50 +0000

    force runaway skin instead spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
