a66c0fee1d01150e85751a25beea1d757d6d3b79
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:32 +0000

    accidentally backwards mainly thankyou sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
   def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
