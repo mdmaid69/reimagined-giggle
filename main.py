12c4624107b2bf8c87737e658911bafe3eafe42c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:09 +0000

    object around sea Mm breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
