88dbf598cd9215f42952d4880e33e8012e2cd4f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:13 +0000

    difference goaway too central callback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
