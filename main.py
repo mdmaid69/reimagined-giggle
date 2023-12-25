c329016ae66333110fe82a6a2cbe460479813860
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:34 +0000

    murder temperature emphasize send worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
