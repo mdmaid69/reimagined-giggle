5ed7eaa3175f9d3c36f1eba793523834bd5030e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:09:45 +0000

    substance him runout plenty concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
