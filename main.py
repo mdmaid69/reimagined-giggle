64eb73487053f373e5fed505eecb212b19b23c17
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:59:08 +0000

    murder certainly afraid be battery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
