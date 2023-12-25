78c5995be52a064a0a259dc143048b219d13f9a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:04 +0000

    justice clean challenge strike emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
