88c6a3a7ab82866dbcf67cfadf1866e5db9b6882
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:33 +0000

    short subject over us steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
