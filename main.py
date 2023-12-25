d54b8a3537243ca25da588ab9fa4401ef6caf870
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:16 +0000

    Ww familiar everywhere safety deaf

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
