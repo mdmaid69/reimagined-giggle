0f3126a6ed4d4ffbf147fcd97ca193eff1eed10a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:46 +0000

    lucky total who wave slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
