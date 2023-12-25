8b04fe5efc0ad98590a2877ea8bdcec52d4b0872
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:41 +0000

    calloff sink far organization past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
