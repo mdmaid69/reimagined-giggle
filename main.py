780a12d0a15522a93c0c29cd802e86e4c070ebfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:41 +0000

    discover theirs watch than culture

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
