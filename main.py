7a9bd0595c3a25c5e1f507eeb879c939dbfe929d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:54 +0000

    score shut flow wide pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
