bbbbb1e1813cbb90f3601d7644e960062df33d90
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:04 +0000

    hard upset content snow kind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
