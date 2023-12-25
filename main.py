34148508524c3022b7f426c9804e9f561e0d38a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:45 +0000

    hate sorry skin inside part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
