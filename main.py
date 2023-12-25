844bc504325e11b583019118919ee18d938d1a74
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:35:19 +0000

    meanwhile run turnout kill note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
