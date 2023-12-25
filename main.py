da6af957cbbf1e94330d070f2ec9dca654a8ba30
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:18 +0000

    authority concerned disturbing correct roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_length(array):
-        return len(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
