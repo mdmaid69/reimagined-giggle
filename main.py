17b5eb9e50f590ab1556e727dd626193dd8bc8b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:48 +0000

    572enter defeat giveout thick future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
