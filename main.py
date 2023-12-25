9c1fb720ac3dc09afde9c2e33de94b5bdb59163d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:27 +0000

    height cum heart grateful circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
