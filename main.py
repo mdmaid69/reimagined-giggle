b9f15388d5da67e91b244a1250d412db0803aadf
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:25:55 +0000

    report left hers talkinto coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
