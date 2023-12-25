97e9a3b6fdf3ebf3d67c3c708e6af5c1cdf294f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:51 +0000

    design leave hungry distant abroad

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
