48b2fad44235117c6f83fea743f90583b595e76c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:32 +0000

    tradition greeting himself many switch

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
