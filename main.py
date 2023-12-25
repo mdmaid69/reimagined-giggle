640b55238cc69875349f0b3217a2e23e7436e2db
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:09 +0000

    doubt getaway theirs organize wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_odd(n):
+        return n % 2 != 0
