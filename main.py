24b3a8d7ce3d78e87a1c2eb67531bc2423c8e451
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:40 +0000

    what brave thirsty deep understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
