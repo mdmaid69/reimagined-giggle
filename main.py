ea7262989f9c825ed86a8081c74a570bcfa54430
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:38 +0000

    maybe pot within tape door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
