abb7d2e5118f06e1ad661825fd2ba2498b735a49
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:24 +0000

    appeal thankyou every something total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
