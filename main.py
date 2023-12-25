1b4684489b070265233827e61cc46c066c48b437
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:48:47 +0000

    reply depend human special divide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
