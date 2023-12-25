2f33e89ccb2d0e119ff53ec933f2714333e0a923
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:07:30 +0000

    forget excuse anywayanyhow scene mysterious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
