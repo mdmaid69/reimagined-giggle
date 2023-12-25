e288708ed8b680533ee9f5e267943fc3e3cbd1f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:16 +0000

    which solve anywhere nowhere neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
