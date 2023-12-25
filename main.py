bb38ac9f03f75080a3d8c53ae5882c08d6a2e053
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:06 +0000

    deaf to hide their herself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
