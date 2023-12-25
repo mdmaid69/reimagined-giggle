c4023308f7ab22a88640eba32b068384d6d95488
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:55 +0000

    loose lookafter be forever comefrom

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
