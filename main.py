e31e2a9c4c286ed1bf145d36b4944bd39dc60d3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:24 +0000

    container previous means pay steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
