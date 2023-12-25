45d856ac026a1519924018ef96004da7032ec40a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:17 +0000

    perform reply beyond plastic nonsense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
