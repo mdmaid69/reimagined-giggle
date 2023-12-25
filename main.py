5575d37446dce43615c44214b5e3222743235df0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:57 +0000

    keen tight consult profit nowhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import random
+print(random.randint(0, 100))
