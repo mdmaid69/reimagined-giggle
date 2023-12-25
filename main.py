d4d57313abaf85053b75382d1649b8d7ec549c17
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:44 +0000

    telephone pin traffic boil public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
