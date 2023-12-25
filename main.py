12ea27af85d3e03d2246092f1d4ab407f07f18b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:48:48 +0000

    problem soon water theirs what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_list(array):
+        return list(array)
