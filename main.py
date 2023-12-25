2c535c1cb6757ace8383631503811bbeb3170e30
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:18 +0000

    Hh original shrink exercise sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def convert_to_hex(n):
+        return hex(n)
