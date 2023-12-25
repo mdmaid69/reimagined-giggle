5241f3d5474a3fb4e68e8b5d6bd147d67a4650a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:14:03 +0000

    excuse water smoke excited pen

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def is_even(n):
-        return n % 2 == 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
