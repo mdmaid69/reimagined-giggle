9c1f7fcc7fddf350a9ec054b9b6b977d7ae8668c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:59 +0000

    live them centre rate point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
