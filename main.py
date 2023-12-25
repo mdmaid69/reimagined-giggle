8e21df7c9c074f1a1fc87861a66e668253e1cf86
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:32 +0000

    cry new mother bin ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
