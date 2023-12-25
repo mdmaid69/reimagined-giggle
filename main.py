d156fc3de0e29b4e901fc9b0cd2b038ac4f8b038
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:09 +0000

    cost apparently mistake slice recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
