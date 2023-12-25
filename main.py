43ff5888c9281d4683f9806546f28a7ed30ed171
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:47 +0000

    better comment fail exchange line

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def find_min(numbers):
-        return min(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
