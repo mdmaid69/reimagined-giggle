2901bb1b25632a14cc73ed771cdc11ba553b2078
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:44 +0000

    bag material limit closely copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
