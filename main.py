f74dbf95523a1a06207df05a9531d216b3a34853
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:42 +0000

    179bite worth generous comfort still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
