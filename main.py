792f656ef42675859beee6d6f4c7a4cba744e85e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:14:54 +0000

    hurt value weak plan whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
