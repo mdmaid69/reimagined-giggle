d596d485f48c029001b479adc01d2e62ba5b84f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:15 +0000

    record shout square scared unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
