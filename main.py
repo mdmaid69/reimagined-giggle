7b7127e07a9f773e51100b219c6f44c7fca2e156
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:01 +0000

    ready gift serious top majority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def count_elements(lst):
-        return len(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
