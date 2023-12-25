f3a688f3fc44b63992d09a0ea3240b96d771e56e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:58 +0000

    talkinto event likely hit violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
