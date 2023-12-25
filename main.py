83c996568f8f27ade521e16ed0b5292ef5bbbd6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:34:18 +0000

    skirt recently swallow poison power

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
