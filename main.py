54e059cf7790a91cf2fa7a8884166e8858fb9f83
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:22:24 +0000

    dead past exercise left short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
