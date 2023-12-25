0c042b63e4a96255a30fb044fa39a98395d7cff4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:25 +0000

    breath field fit admire limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
