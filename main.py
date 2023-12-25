41938e7b74be12dd2710053a381b0f26ecf0f0cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:10 +0000

    accident cough stamp 179bite laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
