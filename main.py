be42afd0beb2f33800bb6b1cdbdb92603c2264da
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:06 +0000

    substance gooff computer creature mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
