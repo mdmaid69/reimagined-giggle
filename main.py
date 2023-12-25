d32e2377d20555e177cf5dff92da2c84f4ddd211
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:54 +0000

    correct news discovery live divide

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
