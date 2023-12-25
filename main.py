19be1865e7afa4597597d17a3fcaab0a764d99f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:28 +0000

    comedown immediate wonder quantity telephone

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
