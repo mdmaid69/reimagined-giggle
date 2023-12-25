b8cb8bbc96ba4e414f2da2719fc4f525f0c22134
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:04 +0000

    suck clue distant moment success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
