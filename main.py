22eebad6c1717c6c3b36bb1cbfaf1741b6e9bab7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:26 +0000

    furniture company last round shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
