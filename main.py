929248b3f7e88bf290c01947765a958af59c3ae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:44 +0000

    professional sound complete release straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_typecode(array):
+        return array.typecode
