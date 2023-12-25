dd136d5c57712139a776021401f7fbd4a91eec19
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:29 +0000

    Itself last far describe business

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_length(array):
-        return len(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
