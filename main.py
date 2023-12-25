cc7be15f2cb3d3ae9ecd0553aeaebc7daee48d99
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:54 +0000

    her insist consider difficulty herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
