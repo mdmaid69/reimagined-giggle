5a8cc13501e05582cf4e8a6d276f912f7333b49e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:02:18 +0000

    thing job behave escape border

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
