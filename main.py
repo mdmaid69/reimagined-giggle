7b7917cb5221f48098b92eef449a8f3c6f895ad0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:40 +0000

    breakin replace scare workupto himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
