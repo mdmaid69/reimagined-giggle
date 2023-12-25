35d482ec3fa0a946d4be2dcc4ad658eec2554cd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:06:56 +0000

    great discuss space protest meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
