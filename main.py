6f5dbf1716f713bc8e5eaf63f41bd751b9dc60c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:36:14 +0000

    crisis welcome quantity shadow comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
