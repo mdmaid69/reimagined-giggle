f45128bb0e51d4a3acfe59352ae47dc82ad49452
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:37 +0000

    admire 179bite before event loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
