ecc9934665a0153f4d130bb77d444bf1cde09b5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:34:26 +0000

    past nothing proper morning worth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
