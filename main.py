508e2e971fd57d990e8d9fbf04e9268405fc2cdb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:18 +0000

    paper brave file uncertain one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
