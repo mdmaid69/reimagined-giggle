e98395cf08b6ea02d84b91846a0351f9ffcb48c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:27:33 +0000

    active copy fall pressure meal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import datetime
-def get_current_date():
-        return datetime.date.today()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
