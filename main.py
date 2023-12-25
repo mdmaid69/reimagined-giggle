4c68b3669e2dd9a4446e9f9731a85eca585ba3c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:01 +0000

    expression control tidy experiment mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(a, b):
+        return a + b
