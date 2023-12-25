7016d615f8e88c4a327311bbd637b10e701ecbeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:07 +0000

    react contact recommend question luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
