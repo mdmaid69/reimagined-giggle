8d6c28b1175db4fabb4bc041fa4525b177043e4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:33 +0000

    cheque still the bite married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
