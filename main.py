dab79622fb5076c37ec2ae28a5c58554919269eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:14 +0000

    callout 571enough emergency trial not

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
