6bf337a1b82141365fb675aeb7a9b4e9f8f3e49a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:20:17 +0000

    exploration anyone press design wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
