114a5a48d536916d5d387c6f05911b7fadc483d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:40 +0000

    ear forgive choice main somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
