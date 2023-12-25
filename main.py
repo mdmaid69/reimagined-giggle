df8f1f5487c6825283446f4309c6f605cc852914
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:14 +0000

    advice copy thing tidy outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
