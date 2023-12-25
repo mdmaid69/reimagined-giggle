485cffec022227ff5fc5ab7015fb2ba60f8a9ad5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:18 +0000

    plain own combine consist behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
