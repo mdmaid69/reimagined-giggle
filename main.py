cd681fdd3ba7d1b77b6853a55e56610f15a310b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:46 +0000

    interview shoot sea band hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
