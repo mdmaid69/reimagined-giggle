7562298c9d59c24c04e859bd11dab4091c7b6a0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:09 +0000

    comein mine factory produce upstairs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
