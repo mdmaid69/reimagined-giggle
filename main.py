1b207d59db16af09219e27369ad0c38a6a2a6156
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:45 +0000

    result mend attract thin powerful

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_min(lst):
-        return min(lst)
-  def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
