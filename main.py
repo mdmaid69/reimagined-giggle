25f6c7bcef20a5d71bd7d0499ddf64e21072d45a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:26 +0000

    wing apart squeeze before country

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
+  import sys
+  def get_python_version():
+        return sys.version
