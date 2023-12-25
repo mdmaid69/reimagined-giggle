954fd2c573779a134ccf1928eb7e58fe3f5ed52c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:59 +0000

    real keep duty fashion refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
