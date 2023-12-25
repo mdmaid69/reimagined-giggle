1ded5be0d6ca8b607ab4a3125951c84be5688305
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:58:22 +0000

    live hot bend stir recover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
