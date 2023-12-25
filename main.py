56c4d6cacd332926f294949eab524f3985872d45
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:28 +0000

    line name even rule mysterious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
