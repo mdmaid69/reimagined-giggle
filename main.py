9b6d16f33db8ead0e26e07bbf83cc3c4371dfcd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:59 +0000

    pot supply trap company pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
