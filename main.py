cba5dff4adf426889be3e5905a1806871ab5ed69
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:50 +0000

    her takeaway warning already disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
