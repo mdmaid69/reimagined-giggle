bc29513631929322bc473f45d3ba4b4e70b5a53b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:41 +0000

    afterwards one must thinkof university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
