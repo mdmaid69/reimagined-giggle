7d5a605a155e565c5f10e1c8cd7a574986174e1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:19:42 +0000

    project mind impossible task laugh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
