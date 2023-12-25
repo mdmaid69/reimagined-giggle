a2b4955ba2fb5601688ae9bd41aefb853f63f69e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:34 +0000

    cope base permission tidy emphasis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
