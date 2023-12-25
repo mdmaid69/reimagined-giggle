18a5f81b500fc23cc7872efe55313d6eaf810ba7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:08 +0000

    argue 564encourage money big high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
