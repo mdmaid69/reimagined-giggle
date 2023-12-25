26c97c18e2fe6cf4234e6766155fb84f06938fe0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:26 +0000

    stand wear fly nose themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
