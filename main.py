78fbcab38b8bd8a9f94967cc8b2d6a4d35304652
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:18 +0000

    medium excuse steady realize unless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
