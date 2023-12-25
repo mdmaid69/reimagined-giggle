ed794db983e0ff2ed86cb1ff711f798c7c402cad
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:00 +0000

    else plenty reduce injury close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
