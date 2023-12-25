29e97aaaba75bf955eb6faf538685144731f81a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:50:43 +0000

    fence offence rarely myself idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
