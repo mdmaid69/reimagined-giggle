f3716b238901ce52ef2c6352c001f9b178b1835e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:57:41 +0000

    turninto design unlucky live even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def convert_to_hex(n):
+        return hex(n)
+  import sys
+  def get_python_version():
+        return sys.version
