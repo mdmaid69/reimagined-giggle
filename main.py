9f34c4f266399b51a7eaabc41cdc46a7a0b6c666
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:59 +0000

    over especially press regional left

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_max(numbers):
+        return max(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
