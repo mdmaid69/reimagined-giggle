eba8e3a8cf9fd87581b2d067424de85d2be3ff55
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:51 +0000

    instrument hurry relative shopping shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
