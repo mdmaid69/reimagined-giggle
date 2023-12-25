1d31326f627df0e5e91af2588fb8546fba958e80
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:50 +0000

    limit by mass smell fit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
