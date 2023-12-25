3097c29db27c40a9f3d2d8caa812b459df111866
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:00 +0000

    reliable punish straight movement innocent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
