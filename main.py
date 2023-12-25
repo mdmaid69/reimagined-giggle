ec50cbba6da1cbb7c5e6c5a118ac9bd894bc120c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:53 +0000

    convince short live belief pencil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
