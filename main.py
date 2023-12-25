113797d933087b1201d13eca06708833c2a7ba00
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:26:14 +0000

    size mess address buy reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
