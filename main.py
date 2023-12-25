1433f28b530acc8747c60739ac7c807624400e8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:04 +0000

    respect intelligent runover turnback release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
