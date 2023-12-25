de256b05dffe05bc784af470ca746fa9c5bf5fba
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:57 +0000

    cause expression argument side post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
