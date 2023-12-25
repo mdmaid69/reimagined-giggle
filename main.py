d29751a191ca8d8064700b8fa46b8be4695753fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:24 +0000

    regional shame word anyone anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
