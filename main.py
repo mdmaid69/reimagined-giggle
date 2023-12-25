af895617588e509cb1ace93dc5526d2447ff8f5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:03 +0000

    fork near long the normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
