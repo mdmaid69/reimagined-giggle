2e6c336937d8cd22047b322b0a9f2fc6a66d54f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:23:49 +0000

    pair present colour radio just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+def reverse_string(s):
+        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
