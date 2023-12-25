334480d6b46b19e2bc4155d2f5926054bcd68dbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:07 +0000

    recently capital weigh difficulty circle

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
