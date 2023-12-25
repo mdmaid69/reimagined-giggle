9984939bea4a02906b9e066feed37b7af7452fdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:26:31 +0000

    extremely rain nose male rich

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_str(array):
+        return str(array)
+def calculate_acceleration(speed, time):
+        return speed / time
