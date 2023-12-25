fe7d63e384caab79fa9d71fea29f37f0763395f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:53 +0000

    stamp price declaration that part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
