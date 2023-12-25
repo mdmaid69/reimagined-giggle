c470dee725310d857e9ca450401a79cc2087048b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:53 +0000

    will urgent after no long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
