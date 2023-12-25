0a5046d06fbc48fc8352eb415196c7f363d75407
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:08 +0000

    birthday teach please warn book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
