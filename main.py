b344aceb345ac5b1d05255639ee50884115d3bb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:49 +0000

    through call Oo fact indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
