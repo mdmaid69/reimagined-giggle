75b4e62878fbf156536df1d87d97c576ba161334
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:48 +0000

    yourself downstairs telephone spite opinion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
