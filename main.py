dbc9e23cd49c1aec958c989c2672a1b7962475ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:45 +0000

    upstairs brother passage lock broad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_hex(n):
+        return hex(n)
