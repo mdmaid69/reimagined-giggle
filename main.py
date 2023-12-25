8e84df0865258ffb98ec9a2d72f64a26d6a784ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:19 +0000

    no straight dear bill contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_even(n):
+        return n % 2 == 0
