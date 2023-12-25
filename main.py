1ae0a3093eb496b43391d043e5e47f3a02480d45
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:21:34 +0000

    except allow suggest instrument before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
