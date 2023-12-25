be229a977b036154eb1f17c80770957cad2a5173
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:25 +0000

    strike poor sport whisper middle

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
