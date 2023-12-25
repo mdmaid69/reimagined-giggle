8d9d2f4471064cad76c7078f47033a7ba10485ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:44:26 +0000

    plant how compete smoke careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
