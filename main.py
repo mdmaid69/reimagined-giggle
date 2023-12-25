ddadf162394ace00579dfa91122ccd195e14dd88
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:38 +0000

    mean connect space cost teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
