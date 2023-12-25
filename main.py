e315d2589d43d4cb02e4e2bc0b109f115d0013be
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:24:29 +0000

    complain strike clothes guess style

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
