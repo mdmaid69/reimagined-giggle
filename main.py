1bfe5365389c87361c263fc1b423b37e3e0cbc8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:57 +0000

    window hour mend question anywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
