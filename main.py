dbab21488d7ea2817e97b41b2ba434561ae0c6bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:27 +0000

    present provide whom sink see

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def convert_to_hex(n):
+        return hex(n)
