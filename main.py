ccae15894c522a83a7571f8f90e8885f25cc950b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:20 +0000

    undo content part embarrassed performance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def square_number(x):
-        return x**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
