0d4e5329fd1c21b13983257db9b4f83bca128465
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:29 +0000

    certain with Zz different mood

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_min(lst):
-        return min(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
