116c98a6bd8e9dec9062b628240b2f7c555c832a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:13:26 +0000

    interview unhappy excuse glass measure

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
