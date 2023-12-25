fb1fc022def83747d8b71a35548d15955f1bac2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:24 +0000

    snow list fresh majority brush

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+for i in range(10): print(i)
