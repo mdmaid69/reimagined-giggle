bbb62aadeda104baf9350720023037b41391235e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:44 +0000

    round trip 573entertainment snow late

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def reverse_list(lst):
-        return lst[::-1]
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
