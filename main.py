7751236f0409ca2a81b6884752b163ea58513d81
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:23 +0000

    story help work take polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
