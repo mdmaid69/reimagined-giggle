900965e9461960ca4743bd6987f70be8c5d0ec0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:04 +0000

    promise happen debt rank wet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
