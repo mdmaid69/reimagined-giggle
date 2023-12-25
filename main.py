e8729915b9afadee137a9dbda0945aae418febc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:13 +0000

    comeout alive minute crisis order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
   import os
   def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
