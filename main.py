fbafb2b677262be318f6c6719bb9977535c481f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:35 +0000

    hurt here getback 179bite honest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
