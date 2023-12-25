0b7e6008347341a3ac0d9447cdc909c95ddf46f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:56 +0000

    half its curious niggaacademy second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+print([x**2 for x in range(10)])
