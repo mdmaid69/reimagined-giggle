63ccfdb401dd044c914d93c054082c02b8eae9d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:09 +0000

    cruelty lie their fashion one

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+def square_number(x):
+        return x**2
+def convert_to_octal(n):
+        return oct(n)
