53427aaacd1c701730c449458ad547cd697cca09
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:59 +0000

    dress peace language cap agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
