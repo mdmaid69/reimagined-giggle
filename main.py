be3b54610904107c19db5794d2d4c4dda29b0351
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:21 +0000

    lock drive key gift Ss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
