bbd00e89a13aa551aada3108e3d9d4c30c811ca5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:59:54 +0000

    successful sentence petrol man advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
