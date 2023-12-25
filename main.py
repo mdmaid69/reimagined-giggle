c7ccb92b0d22129932683da3ac024ef5356f57a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:44 +0000

    kind medium sink tend trouble

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
