24677092c2245b614f48b70b8ff1181ec69a3e0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:42 +0000

    sweet callup Ee move shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_list(array):
+        return list(array)
