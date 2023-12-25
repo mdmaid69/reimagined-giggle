904e289d0c6901d1c5de4b35f70ddeb3b1a6ee89
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:10 +0000

    need mystery bush takeout Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
