75f28d253ec05a5baaabf4da327fd932e2b80e75
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:03 +0000

    better pile lookback(on) why feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_str(array):
+        return str(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
