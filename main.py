b80c18c0eec788a56e5e56cf2d839685c551b445
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:35 +0000

    then arrangement spin vanish cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
