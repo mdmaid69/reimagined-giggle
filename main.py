caabec5c1e4464514f792e41c96913893af10b70
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:53 +0000

    ocean cross before shoot anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
