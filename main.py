e8c108b08b8f0867b1ca7118d5ba240057e3e195
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:11 +0000

    common urgent lend weak forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
