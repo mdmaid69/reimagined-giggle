adc49739df8147bdcef717d48aa44fd103e1e14b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:25:48 +0000

    soft list idea order travel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print(sum(range(10)))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
