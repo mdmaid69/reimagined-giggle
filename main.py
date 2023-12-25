b553c989469cc8f82998a22fd0ad82c307226531
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:39 +0000

    share so exhibition boring sore

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import re
-print(re.match("h.*o", "hello world"))
-for i in range(10): print(i)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
