658e7ab7d82faf162f27cd277b34dd84c3093748
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:59:47 +0000

    majority thinkof welcome suck school

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
