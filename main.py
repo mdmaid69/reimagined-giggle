17295969e87ab15088c6218d4a3f8b51376531b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:59 +0000

    dream tax battery seriously giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-print(math.pi)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
