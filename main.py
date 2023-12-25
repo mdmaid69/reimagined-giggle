d5a64208c7951da76b1c55743d7b547a2a42e710
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:09 +0000

    aim air damage head switch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_area_rectangle(l, w):
+        return l * w
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
