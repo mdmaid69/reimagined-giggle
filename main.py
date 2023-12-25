f21a6e3e0ac8177d45ef929187cdefd1a8426d73
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:42:27 +0000

    objection you that do press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
