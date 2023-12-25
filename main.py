91b081fd87def670025efc2836c0e9033361c4d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:37:29 +0000

    worried splash high smart example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def square_number(x):
-        return x**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
