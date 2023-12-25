8c1ff2afb2d61d64a056f82bdb50cbfa2a34fdf6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:30:43 +0000

    failure realize rush innocent none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
