0ae4d5a210d117796dfae6d2949c593dc35ca8da
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:55:12 +0000

    cruel least Ii steep event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
