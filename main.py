07621047c979cb582f43056668059b5451a23721
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:25 +0000

    present talkback trouble sharp which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
