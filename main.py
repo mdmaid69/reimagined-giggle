50e8c614a7d2234c2a66ac87db0e44a2f6a55088
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:18:14 +0000

    giveout dead arrival private until

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
