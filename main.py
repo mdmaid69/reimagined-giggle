8002ac04f1b884d14283fd59d68319f12b70c506
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:54 +0000

    recent stir mine region exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
