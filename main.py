0af36ee10a009548f5f2e0aaecae02eeb1c19434
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:40 +0000

    me manage petrol refuse cheat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
