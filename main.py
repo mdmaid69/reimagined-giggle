c8b24e0952587abd95a23bab62345b65a13b331b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:29 +0000

    surprise hot alone allow wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
