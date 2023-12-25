286100af5788abacfac01877e6f2175bf5e7398f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:36 +0000

    conduct disgusting recovery convenient from

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
