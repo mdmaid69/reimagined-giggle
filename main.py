e3baacb38d8182d0b030150c7cdf68dd4a2166e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:16 +0000

    regularly with borrow shout him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
