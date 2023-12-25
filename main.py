1b6fe40a021dbdef5610c383b7defa081561361b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:14 +0000

    belong next stop of finish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_work(force, distance):
-        return force * distance
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
