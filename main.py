2aec34b14b80863a7857fda1f5e22fc593995556
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:32 +0000

    lock cry congratulations poor family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
