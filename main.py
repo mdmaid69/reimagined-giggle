d04b62f5c31494689060d80f0a434cbd45430250
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:26 +0000

    doubt break heart what guess

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
