f3efc59c507c1b03cc2fc43f25d4bbd3ad46251c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:29 +0000

    runaway past contain worry quiet

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
