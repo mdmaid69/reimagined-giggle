9e0de1452aa752562402cc1b082532072aee0417
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:31 +0000

    persuade mail opposite demand sell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def add_numbers(x, y):
-        return x + y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+for i in range(10): print(i)
