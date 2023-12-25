0dbcca443b28ee0b98b1f7b902800e147ca21969
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:57 +0000

    holdup off father later breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
