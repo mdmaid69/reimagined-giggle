4fc5c72995507f7a2844e79c45385af7e52d73cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:41:24 +0000

    sting hurt account who tap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
