eb34274cc34176a49f7bc01889d7e7eb5c9e110a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:16 +0000

    love instrument weak disagree threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
