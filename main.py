13980c50843339a856b2f6f59f2c63fc18d4d823
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:32 +0000

    main pain step appear stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
