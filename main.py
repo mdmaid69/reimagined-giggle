e73b640f8be0cf9803ed0f714f06c391e6bdea88
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:04 +0000

    remain clearly butter scream offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
