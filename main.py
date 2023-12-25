32f2cd7a60c97a8c474052fe42082a4852904a1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:52 +0000

    snow mine common upset almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
