cb4282a6a974208fe9d55d0a487b29b0d7c89fba
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:59:13 +0000

    angry rubbish tidy week rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
