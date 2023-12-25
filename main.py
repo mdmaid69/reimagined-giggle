31d97d50b840a128f1657cc2dd1d457aa50c0ce6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:46:05 +0000

    by scrap fire load keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
