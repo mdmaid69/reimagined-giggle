112bdc628bde2db8f6c475663c66e2eb0a01adc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:17 +0000

    expect hungry realize onto brain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
