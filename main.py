8111ef3e2b36241ea025d00c3cff3db75528c0cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:46:37 +0000

    mood answer last short interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
