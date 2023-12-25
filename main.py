27ec4455fdec45b19996f2edce06852e8dca8c37
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:28:48 +0000

    beat care trust sudden fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
