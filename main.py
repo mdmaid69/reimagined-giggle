d1ec6d1d85ae33489f931e79b4ff837b7a1637a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:18:55 +0000

    frequent through inform reduction finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
