6ced1e9c7f2ae87630a3ea821f9dd298bf974113
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:10 +0000

    unlucky indeed open steer word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
