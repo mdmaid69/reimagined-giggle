698da8a1164d92067d05c4a302941cbe59f5724f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:23:30 +0000

    mail tax strength friend suddenly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
