8bfb0915e2cb920684f0c1f5d583e447958e130a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:18 +0000

    story lift guess about turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
