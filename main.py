0726dbf8f931b36a0ea16689837bbbb4a0dac8ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:41:46 +0000

    sell reasonable disappointed joke wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
