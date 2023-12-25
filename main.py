02e04a50f2a794b81a3c290a8f93742b42b3563b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:42 +0000

    breakthrough loose strike regret nothing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
