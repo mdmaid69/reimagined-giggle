5bd2469856d778b46ac2e2d71cf33719a3464b2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:14 +0000

    ordinary declare but hard degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+x = 10
+y = 20
+print("Sum:", x + y)
