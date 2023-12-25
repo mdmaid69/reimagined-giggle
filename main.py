a9be045178d0592644c6cec320fd0acc02729983
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:19:15 +0000

    badly chance emotion impressive makeinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_string():
+        return collections.UserString()
