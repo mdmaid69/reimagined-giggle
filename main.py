be008b0f4a18cffa48f4ec3866ac36aafa3617e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:23 +0000

    win minor callat low boat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
