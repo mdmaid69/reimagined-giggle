4be4421097acacd0eb164b01fb4ea415b7d1920b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:02 +0000

    spoil impressive scream authority operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
