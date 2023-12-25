6e70a056b2d07734b80cbf3afc3ccf5e4fc7ca22
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:36:20 +0000

    left store recommend runout pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
