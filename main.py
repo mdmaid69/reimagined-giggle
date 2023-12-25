edda0cbc798a49d14155ef9e68bbf0eaaae84e8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:38 +0000

    stop deaf comeup care sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import time
+def get_current_time():
+        return time.ctime()
+name = "Python"
+print("Hello,", name)
