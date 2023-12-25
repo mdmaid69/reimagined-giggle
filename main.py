e29fa86071321ad7403bc5c5db6f0861140ceb0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:42:56 +0000

    patience appeal uncertain treat rob

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
