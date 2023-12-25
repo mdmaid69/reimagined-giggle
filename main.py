a0af1f119a1b70dac8518f30f7e3301051c0419b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:24 +0000

    command half payment once compete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
