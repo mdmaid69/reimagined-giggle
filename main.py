2479eb3972d64769773a79dddf39b4fb79bd646c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:34 +0000

    nice seriously bill turnoff display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
+import os
+def get_current_working_directory():
+        return os.getcwd()
