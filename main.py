da97551917d8cc55b7d69134ed7ebde9096c1701
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:39:36 +0000

    thirsty comedown strike but happy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
