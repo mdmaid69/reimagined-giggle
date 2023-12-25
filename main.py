a3b005ed0b6b388b828533e4759678cdcfdd94c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:23 +0000

    dry left however another difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
