477ef57451f645017c7ebcba7f683042289839d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:25 +0000

    to walk throughout quarter freeze

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
