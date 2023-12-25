76f725e39b8f60d348b5ef1d42af5e1eecbec850
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:24 +0000

    frequent rank trip project below

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
