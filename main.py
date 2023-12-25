ea99eb63be54b0d822583879dc9680a37ba371b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:36 +0000

    wealth period profit stare shame

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
