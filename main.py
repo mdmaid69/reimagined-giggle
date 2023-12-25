ab47885f2a66cef91d7dec99a7602567ac628db0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:27 +0000

    cold chief slightly pencil spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
