5ed514694a4f6db776922b2634f34b097a78fa9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:21 +0000

    safe respect takeout circumstance dare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
