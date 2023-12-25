22ae94124f92895f2a3200f310183ec471ecdfe5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:44:35 +0000

    accidentally flat holiday wall turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
