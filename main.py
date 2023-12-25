2ffc9d3f2114a8f230ca1d9a9f605cdc37ae936f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:56:33 +0000

    practise lookback(on) stick before autumn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
