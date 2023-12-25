8582f92116e2a4a04f28651b744c2cf334deb7bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:27 +0000

    safe against music spring contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
