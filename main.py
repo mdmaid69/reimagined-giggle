0b4969ecd8ce3844b7a53be689a6963f507ae986
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:11 +0000

    crime wrap person upstairs talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def sort_list(lst):
+        return sorted(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
