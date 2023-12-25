a9345419411c04693d47b5cb2592ae767f434f8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:04 +0000

    figure dry surprising collection warn

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_area_rectangle(l, w):
+        return l * w
+def find_min(numbers):
+        return min(numbers)
