b187142ce5f21ed4f1f0e00bdf61e77d75a1a710
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:18 +0000

    because earn also their fortunately

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-print(sys.version)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
