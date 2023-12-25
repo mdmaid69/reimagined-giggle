de422a0984dfc4a87ad05af5afa737592a0af728
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:16:30 +0000

    list sure challenge comeback them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def clear_array(array):
+        array *= 0
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
