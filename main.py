8ddeb955fecc743b02c3ca64dcd72e41b1991abc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:11:27 +0000

    watch party soil tough up

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
