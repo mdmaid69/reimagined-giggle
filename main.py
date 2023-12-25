9d1ce86158bf5b735d9d2d33e7dcdc4d8631b9b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:31 +0000

    takeover in free pronunciation mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
