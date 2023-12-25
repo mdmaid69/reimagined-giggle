88d0a34b2398c16ba6a6d3dea466cc1f905ec4a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:33:30 +0000

    more detail several tell chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
