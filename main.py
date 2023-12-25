b4ecb38bf97f0dfbb325cb4fd2824ca25a069404
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:13 +0000

    shoot lost gate 564encourage computer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
