ab9c1abcd2cb0339949b849b7c2e72c1b52d92a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:55:32 +0000

    appear separate provide unlucky wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
