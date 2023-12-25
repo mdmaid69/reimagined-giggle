23f7c506e0f544f42e8f0a8cb75c713160d4d102
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:53 +0000

    tap pinch blind mine cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
