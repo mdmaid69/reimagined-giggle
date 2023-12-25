2901bdbabfd736e3da086fc41b7993fa0f3fd921
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:18 +0000

    basic group level choose brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
