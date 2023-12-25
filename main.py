81bca66f90d87119bf1a2557d7ac48075d287ea9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:59 +0000

    earn sensitive section breathe usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
