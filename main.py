263afb0873fa49c8782bd8567af6e019a4b90d79
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:12 +0000

    sound doubt politics second male

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
