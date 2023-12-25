194347b28a0afff31b67b1ff861803951862d0cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:08 +0000

    turnon stop edge choose attempt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
