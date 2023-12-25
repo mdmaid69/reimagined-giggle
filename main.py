4c0b3fd29a8d43ade665fe7fb5a4c6d01337d1b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:45 +0000

    occasional remark colour remain forever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def reverse_array(array):
-        array.reverse()
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+print(time.time())
