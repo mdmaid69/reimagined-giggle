0cb6a2222a61db61fab2e365b92638d8fe5bd8ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:58 +0000

    sink legal bringup weather funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from collections import Counter
+print(Counter("hello world"))
