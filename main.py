a6040d2248c4989f95035032688177fb4db39046
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:48 +0000

    talkinto clean on team seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def subtract_numbers(x, y):
-        return x - y
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
