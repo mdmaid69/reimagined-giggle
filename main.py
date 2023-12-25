ba8bd0fc723cd463b686eab8f1e251c0ab96db11
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:00 +0000

    workupto tip squeeze major unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def count_words(sentence):
-        return len(sentence.split())
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
