8f2038d1161b005aaf31787c82c42cc78f37ecbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:17 +0000

    lonely container today fault fly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print("Hello, world!")
