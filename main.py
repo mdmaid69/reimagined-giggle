9366663432a23e7b49b8a658f8ef16b8022a24af
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:48 +0000

    list offend disappointed holdon worse

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
