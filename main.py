6e465941658ded5e0640bc4263e022d2c1f77404
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:57 +0000

    control nose prove inquire soon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
