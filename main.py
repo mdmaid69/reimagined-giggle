2800c05e178734fb99e5e716dc53d8475c431254
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:11 +0000

    scare computer safety because floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
