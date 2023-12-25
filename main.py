2d9f9b19d635f81358e1b8f21a701470e76d8b69
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:30 +0000

    bus mark against grade talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
