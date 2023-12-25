c0966a8cd4f593c3a424fc40a04f47a2db9ef296
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:09 +0000

    violent touch bad closely club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def is_odd(n):
+        return n % 2 != 0
