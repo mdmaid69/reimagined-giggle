c7130ef97f0460e6ee3f40535ad8d54e9b9d928f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:29:47 +0000

    Zz Cc design embarrassed report

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def remove_directory(path):
+        os.rmdir(path)
