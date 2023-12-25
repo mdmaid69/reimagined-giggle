461b005fe4a692df18f50206d6e88d02c6ef25ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:31 +0000

    our excellent personal laugh dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_string(array):
+        return array.tostring()
