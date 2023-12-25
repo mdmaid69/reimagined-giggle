cdbbed302ffb7d1a4e0abc4e0514fd8a6db51616
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:09 +0000

    comment try cross angry division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
