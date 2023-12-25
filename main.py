75e6f801cbd667036af60a713c22f62bfc902adc
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:32 +0000

    eye live hurry subject persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
