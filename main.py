2a0ff9240576fe053b9a5173fdee194c12c78cf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:02 +0000

    discover callup borrow shrink guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
