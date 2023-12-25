39ab53be36b4d4967ad5f1bdca1decbe5f21c63a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:05 +0000

    unable aim party expensive breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+name = "Python"
+print("Hello,", name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
