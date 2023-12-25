5c461ee8bdfe33592d6469b3775b42e0564148c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:20 +0000

    Ii straight along different apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
