aff1b5b786f91e19e7987a5b31cb9d26b20fc0e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:45 +0000

    content unkind plastic high nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
