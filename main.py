50c122e328b345812ff98215ba90334abda824b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:48 +0000

    on sit culture no emergency

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
