ba1c348ac111fe0dad5c217096c3434ae93d9ef0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:44 +0000

    broad read cruel company spin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_force(mass, acceleration):
+        return mass * acceleration
