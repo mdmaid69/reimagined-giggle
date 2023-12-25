8f12828c47e2dca628cd055e25e1641a03c413f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:30:01 +0000

    manage progress violence float kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
