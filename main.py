fb3c48bfb706c91e06d632142d47dc0468b1ca6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:24 +0000

    doctor ago danger wonder success

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
