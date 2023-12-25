ce941286d6060d2d388d182dada351ea6430b24d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:14 +0000

    purpose someone back freedom pick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
+  def square_number(x):
+        return x**2
