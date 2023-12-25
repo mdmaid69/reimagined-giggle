5b4b55a9d5392b08002497a7f33d9d900fd0c1b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:41 +0000

    organization holdon willing unit face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
