c147a5253f80b8826fd33382e1bef6243f99b6f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:20 +0000

    clean false thought live advise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
