7b26b48878d0db67ccd434484350a32ced83e024
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:39 +0000

    community store born reasonable shock

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import time
-def get_current_time():
-        return time.ctime()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
