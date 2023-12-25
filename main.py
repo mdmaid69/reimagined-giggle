4867bd5182c3baa3987e167016bfad7badf36d97
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:32 +0000

    swallow reply progress unusual on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
