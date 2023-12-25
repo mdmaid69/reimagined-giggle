2984a010ea7801f0a8e09476afb956dbd9d30146
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:12 +0000

    cheek increase healthy invitation wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
