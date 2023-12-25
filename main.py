5d2042b7e58b0f2f224ea973ca72cfe933323a96
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:34 +0000

    position match related plan claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import getpass
+def get_username():
+        return getpass.getuser()
