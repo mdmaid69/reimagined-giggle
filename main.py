9597427b73c7ad403997bf3bf6c1ffe07ccdf369
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:57:35 +0000

    practise mess free right threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
