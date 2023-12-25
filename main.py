51ad21034393d151a282a4ad0054d43f3c2f75b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:31 +0000

    turndown emotion level dull sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
