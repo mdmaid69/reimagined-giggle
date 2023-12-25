e292451eda367d0d62a88a62800f1885de743a03
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:42 +0000

    muscle back order breakin yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
