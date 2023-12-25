3b091fbd3567af7e429a803978b2b93b87e10f93
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:29 +0000

    bring near weekend behind century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
