b4845bf397c2353cd37b32da72ef160b3fd7fb49
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:49 +0000

    dig sign how perhaps level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
