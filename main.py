e085c54b596ec85f6f7cb36d843c3e99e064685b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:17 +0000

    laugh holdout well earn broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
