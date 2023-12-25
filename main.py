c39bb5e883d175fc128f911057fae17c3e723afb
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:55 +0000

    explanation comeup case private lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
