171c07392f027a8b8b5fa0b3793ccd3ec3f36fca
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:52 +0000

    that tie famous opposite pupil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
