ca775da67ed567b7da5e31d6cab61552fff74c66
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:06 +0000

    bite contact copy notice start

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
