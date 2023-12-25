a5ad52213b619b19eefe87c569660b26a6d0c929
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:06:12 +0000

    juice basic run custom else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sys
-print(sys.version)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
