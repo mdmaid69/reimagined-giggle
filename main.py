0583f6ced51077f4d9cacf8f7026bfc9e9d28b7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:35 +0000

    active scheme giveup square accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
