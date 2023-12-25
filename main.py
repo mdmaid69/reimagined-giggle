68a2fd54caac43fadd01ff3c5f0eb39b8e2fe1c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:50:32 +0000

    towards lower bush sure connected

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
