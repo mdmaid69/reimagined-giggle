f6e9fd3234a297c776ef5372898d1becb67c0b96
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:07 +0000

    getback organization worried behaviour getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
