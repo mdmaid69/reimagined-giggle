5460e331e82330dd6ad0db21db3e0ed26d116348
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:08 +0000

    eat to pin trick persuade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
