befb25efbfcdb496c5debca3aa7cad5796af67ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:56:49 +0000

    create meet exit order write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
