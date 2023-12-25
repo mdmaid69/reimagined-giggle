38f0e2f9a0ec3ae9600645ed74513a736a6338ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:36 +0000

    imagine regard Qq pronunciation only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
