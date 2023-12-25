fb518650c6a3ebf863aa96eb03b318b35a8cd68f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:49 +0000

    will too excellent slight light

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
