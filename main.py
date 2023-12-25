3990a930af21633ad3e8f8dbd4dacfbef598dd09
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:47 +0000

    accept change goup enthusiastic comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
