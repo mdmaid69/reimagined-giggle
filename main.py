5114524bdfdcbd9b1c2f0afac12c1a4131626299
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:30 +0000

    tone string watch organize party

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_power(work, time):
-        return work / time
+def square_number(x):
+        return x**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
