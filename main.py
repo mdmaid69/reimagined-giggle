3936db5be98ff0309a0e90272c25f060b329f34d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:06 +0000

    open stretch mind environment than

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print(sum(range(10)))
+def calculate_work(force, distance):
+        return force * distance
+  def remove_duplicates(lst):
+        return list(set(lst))
