720d83245ff4402c101b67687ff40ac96b7faf58
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:47 +0000

    crowded exam importance argument he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
