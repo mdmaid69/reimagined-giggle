84af49b1a510ee6e976a36751bffaae2cc697db8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:27 +0000

    excuse breakfast visit dream base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
