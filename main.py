da44959aeda185b22ffabffd2ad3a769f5f8654b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:39:24 +0000

    head attitude lift table talkback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
