e96b0c0059f6103c757a253fedcec7fe50dcd26a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:29 +0000

    value plain us wonder clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
