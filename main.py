1784b583ebfdf3a3bdbdc73be7082d1670020d6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:35 +0000

    fair slow takeback glad anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_density(mass, volume):
-        return mass / volume
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
