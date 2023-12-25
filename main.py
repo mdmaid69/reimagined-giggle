6b2e959e4d408d2bd6c704796dab1652c0bc6bd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:40 +0000

    prove duty geton takeaway appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
