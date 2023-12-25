9a846abfe4f7f220168fd9376e04d3f7a676e3e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:29 +0000

    rest punish circumstance various relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
