b8cad0ca6cef3c09083878d1ce2a749518a401c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:56 +0000

    value cool holdout several runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
