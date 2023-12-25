b879f8f13f53dfadf622d1eb05e0f96cf5a83ad8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:11:23 +0000

    divide record strict switch typical

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
