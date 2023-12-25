22f7433bfd019d495f8a2faa83b452472258b96c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:05 +0000

    emphasis drive favour close hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
   def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
