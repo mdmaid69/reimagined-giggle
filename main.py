941c8f2f46ea3fe3a3bd4a6159653f3ba28c18d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:57:01 +0000

    bye distant trust follow live

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
