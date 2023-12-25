64f8481c8c2caec03510bc70ad6dae0208e73bff
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:04 +0000

    clearly apologizeapologise passage human spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
