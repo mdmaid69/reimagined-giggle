59a959c87aeeedb7b4da0cdf8cb3d5f5244ecfb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:24:50 +0000

    usual pronounce married pity exploration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
