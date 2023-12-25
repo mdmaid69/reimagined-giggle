942702b831012f6a1eb159fd921c787d724fee2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:06 +0000

    realize lend temperature lookback(on) impatient

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_words(sentence):
-        return len(sentence.split())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
