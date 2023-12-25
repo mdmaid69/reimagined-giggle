90a1982b3989d4aae9225a54a93e688d0798454e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:59:27 +0000

    shadow invite react uponon married

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
