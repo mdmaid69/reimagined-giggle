092d267f60ea1b3d9506f8530a772749a6b82db9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:33 +0000

    class form deep secret seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
