688f7dca0c74e408f96ecaad777f95b1f3506d76
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:11 +0000

    day criticize satisfied runaway handle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
