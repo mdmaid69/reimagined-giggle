940d12a3074d59c9d35783a53bb31e72676ca1cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:35 +0000

    matter damp hurry exaggerate there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
