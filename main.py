afe9461faf362fa08cf81a02bc260667e5232c28
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:48 +0000

    deserve cheque hour shut child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
