06cd4bd800070059ea32e8bb44029319cfc98393
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:52:36 +0000

    568energy ball uncomfortable habit frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
