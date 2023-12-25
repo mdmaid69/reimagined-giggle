44e1dec6786c3ae3a95c0a71b8f43b6818aae42d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:28 +0000

    pool married address invite continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
