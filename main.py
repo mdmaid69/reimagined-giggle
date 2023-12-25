cf30f3b6664542b47d8fdf022bb68b33dc9534dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:21:13 +0000

    row major lift muscle late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
