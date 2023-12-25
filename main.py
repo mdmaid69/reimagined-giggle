23207cb37013ef711b083879aacf61b9dba35652
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:53 +0000

    pick life first eye trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
