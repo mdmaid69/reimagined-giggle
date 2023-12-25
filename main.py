b8352bf31f100abedee27e32a4821b818f9d2184
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:10 +0000

    round ago culture road child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
