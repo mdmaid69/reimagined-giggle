fac787f820a5e48fcf1793d1c7b88d6cf3f829f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:19 +0000

    autumn hollow gentle influence worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
