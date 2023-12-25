2dcc559210a2dd06b6de687adf301baf7082d522
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:19:53 +0000

    listenfor figure this instant board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_pressure(force, area):
+        return force / area
