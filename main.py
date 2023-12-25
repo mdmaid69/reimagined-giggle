52f52eaa8c6ebe78ba888c7a8729ac8b83575162
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:56 +0000

    unable old burn real stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
