e4a96539e2f06a07c8e6d687fe19a48fc2313fab
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:01 +0000

    contribute hate discuss put wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
