5436bb2325e6ac921802a4041eed85fca230f439
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:00 +0000

    blue advanced wealth cheap thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
