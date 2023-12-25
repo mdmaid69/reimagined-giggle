e0d52009e7d15a40af807e36d758673c07566e11
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:03:51 +0000

    boil contents left uncomfortable keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
