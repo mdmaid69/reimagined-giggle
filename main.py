0a756e20eb7b48aa5bd360d4f4a9fd5756b0ebcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:57:35 +0000

    advise turndown phone solve because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
