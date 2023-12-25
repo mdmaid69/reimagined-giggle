4ab0e06e7ae49842b0e63d7b546e09b31fe92f79
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:49:18 +0000

    willing prepare steady ocean we

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
