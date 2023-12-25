6241899136841f62b9e77dbca14266d32230c595
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:37 +0000

    crazy pretty wheel vanish rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
