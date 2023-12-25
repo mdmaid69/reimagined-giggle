d81290939cde2bedff906a54a165026973bce167
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:32 +0000

    mother moral power pupil crisis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
