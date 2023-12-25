ed8b16e1a1bb3221784ddb009cd9cd0b3715d97a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:32:37 +0000

    close suggestion realize light comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
