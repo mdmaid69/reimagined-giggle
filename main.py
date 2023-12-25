7a0dbc51b86342972a4cd070471aec6418741ab2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:22 +0000

    unlucky lock swim tragedy crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
