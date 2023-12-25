de877dc54ba4d4f34175d2678d869646120beb0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:18 +0000

    call stick table string pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
