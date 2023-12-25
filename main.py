50c8d06f9f410a1f17d1c5859ed62f5f85aff428
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:03 +0000

    wander before callback become pretty

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
