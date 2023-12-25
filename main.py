f595093a1b7d3c227c3479f28e4e53fd26f52936
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:06 +0000

    classroom young often quick comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
