a07491e4553f5598a579623abd1f3c9923804e8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:46 +0000

    react payment noisy lead obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
