2d05eb227be472a9cf9ee32e4f630813407b65ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:03 +0000

    myself declaration copy cry week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_area_rectangle(l, w):
+        return l * w
