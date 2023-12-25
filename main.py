2289b36c4130b2aa903c7becdfc1f6a6fe9e4100
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:50:13 +0000

    change suck mainly switch method

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
