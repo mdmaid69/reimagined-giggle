5b1acc8b1b6c76ae3a55c2fc648e25ac7b8aca6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:46 +0000

    write the memory twist improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
