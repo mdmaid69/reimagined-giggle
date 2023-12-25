e1fa5a7a9b056cf82b2cd9afd3952ef54c7a3318
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:00 +0000

    though why trip argue breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
