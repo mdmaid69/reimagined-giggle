3d7f9ad9fade05c10c5d01961734155d3c157d9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:55 +0000

    independent warn major wash as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
