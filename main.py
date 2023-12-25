1a49d6e358595ea3030e07668fdd66ae03a81d8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:32 +0000

    children prefer use card gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
