b3aed86733f480aa91c6cad572e7edee500acf35
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:17 +0000

    proof attention lock possibility stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
