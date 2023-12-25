2ac6dc9577b268a0e9b6491a30b3e39ebb52ba77
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:47:00 +0000

    onto Pp sorry callup smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_power(work, time):
+        return work / time
