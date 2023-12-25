de9e96c9be606ff18e0f080af760c8857e03cb89
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:29 +0000

    side run on better prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def add_numbers(x, y):
+        return x + y
