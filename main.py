cd421abab671efeec18128943a8dd2771e5030f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:31 +0000

    long occasional inform set software

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def count_characters(sentence):
+        return len(sentence)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
