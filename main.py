947b38eb38b2a6065b12bef301031b9222acb692
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:38 +0000

    I content turn instrument thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
