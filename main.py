2da23d3a3473cfad5a8d24c90d8b070b87c48397
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:56 +0000

    content work sing owe share

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
