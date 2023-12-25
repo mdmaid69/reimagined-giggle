1a6e818002dc70327c0873509b20241d14ca37e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:52 +0000

    free increase satisfactory hot either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
