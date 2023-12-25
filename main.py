e13bb199ddc4effbfb39ebd481c9b4ce4cda2288
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:30 +0000

    rare tomorrow coat build team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
