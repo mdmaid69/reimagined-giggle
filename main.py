b7813e55042bbe65a6f8b2e86aada6375def29b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:04 +0000

    that unable heat base tear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def is_even(n):
-        return n % 2 == 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
