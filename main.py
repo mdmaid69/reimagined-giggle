73bd894d9450e06674a91f9019e8ebccfa010c66
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:22 +0000

    giveaway edge takeout love practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_string():
-        return collections.UserString()
+import random
+print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
