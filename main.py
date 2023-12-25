709667492351542cd98b3ae505fd8e54207b02a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:42 +0000

    cruel think exit cum scream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
