149e596f547e789f714446735f5f76c5829f757b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:50:27 +0000

    cough rise separate basis decision

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
