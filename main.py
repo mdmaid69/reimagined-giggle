2f0dd9a292285beb0fafff1041546a03073abfed
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:03 +0000

    brush pollution none dance size

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import numpy as np
+print(np.array([1, 2, 3]))
+def reverse_list(lst):
+        return lst[::-1]
