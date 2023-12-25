bc90bfe76773e9931bf22b0346a4de1d7f3b89f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:36 +0000

    increase basically among permission frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
