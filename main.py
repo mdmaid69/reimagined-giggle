fc0f1875b8f6b9ecdb572c44d3621898958fa7cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:25 +0000

    carry slip wave copy talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
