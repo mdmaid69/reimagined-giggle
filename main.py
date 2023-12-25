29c9bcdfd201d2b4fac563d6787834adc9885faa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:01 +0000

    admire smooth dig mass use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
