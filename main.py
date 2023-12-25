2c2899cca69be61ac4241a15321a9b1b9c20a4e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:02 +0000

    morning block very unusual job

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
