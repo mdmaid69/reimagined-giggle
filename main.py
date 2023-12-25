017e97c189e5a7a8946c7f2c43c28a72dfc8baae
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:10 +0000

    dare waste pain support line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import math
-print(math.pi)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Words:", len(text.split()))
