51e3b6026cccbd2cdf93d5e2221b31f616ab2dad
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:40 +0000

    together achievement skill gentle benefit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
