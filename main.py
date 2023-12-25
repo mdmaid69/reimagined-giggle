5497cf10b634320d2596fbde7d918a344f144c76
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:10:21 +0000

    cook please present relax information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
