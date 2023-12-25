05ca4b059cf2f7e4a54bd5034c66ce9f06e95973
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:31 +0000

    complete belong brother separate ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
