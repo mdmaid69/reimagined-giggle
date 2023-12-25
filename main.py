274ced7fff794073d140971096c1e8e1a503dd1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:57:35 +0000

    various leg calloff relative match

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def sort_numbers(numbers):
+        return sorted(numbers)
