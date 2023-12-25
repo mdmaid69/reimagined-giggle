f49fa1d6bd51370f266511a86798d2df288a9cff
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:47 +0000

    open supply when grade object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
