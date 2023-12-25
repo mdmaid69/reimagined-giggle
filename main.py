15b3a80ef10442f01b1f753dbc6a99e81e3ca7d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:40:16 +0000

    black building phone afternoon hit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
