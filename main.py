45e8e2c024f608765418f89bf91eb58169d3931e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:25:15 +0000

    failure serve concerned lie care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def cube_number(x):
+        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
