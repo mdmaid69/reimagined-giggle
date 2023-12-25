4f6c151a420cbf1242be53e720d6c0fd6e09be1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:40:42 +0000

    happy plan luck film fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
