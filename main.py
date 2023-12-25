4af55e7e187283d9e6d4675c6711f28baff0dc72
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:28 +0000

    disappointed makeup physical correct area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def reverse_array(array):
+        array.reverse()
+def sort_numbers(numbers):
+        return sorted(numbers)
