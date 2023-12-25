8678a9e3d5da9d3e73447b79b34b6bde10c46962
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:03:43 +0000

    suddenly answer indeed should damp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
