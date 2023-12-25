c1e373f05206a6b7225e43a7bc4c65c135fed775
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:32 +0000

    onto listen interview vision major

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
