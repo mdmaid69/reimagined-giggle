0f064ac1ffe32b9769c2ab9a38814c142ac03821
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:49 +0000

    breakup reduction Uu wide bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
