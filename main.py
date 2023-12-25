91aaf54c1a63287fac8cec11d3a6e6275f178e00
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:33 +0000

    typical interested godown independent majority

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
