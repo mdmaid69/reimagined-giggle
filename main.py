77c208fb8164db83d2768640ea17cfe6342d9657
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:03 +0000

    turninto part exchange interest band

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-def find_min(lst):
-        return min(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
