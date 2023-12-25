5da142b0ea74b43b41a478b723c3d9df88f92807
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:43 +0000

    recovery cruelty opposite matter zero

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def find_min(lst):
-        return min(lst)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
