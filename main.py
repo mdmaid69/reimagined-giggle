47bf33b4acf0155bfc8a9b106946df3146982923
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:00 +0000

    Kk target talk may minute

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+x = 10
+y = 20
+print("Sum:", x + y)
