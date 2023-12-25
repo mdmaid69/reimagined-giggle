d9ddf3e51361164fd3079d1f746c8309d28ac17a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:34 +0000

    gather duty attempt room label

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
