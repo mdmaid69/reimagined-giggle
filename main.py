cefa32eead7cfc5cb3847812dc44aae7c646184c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:07:42 +0000

    yes thumb fit allow bright

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+for i in range(5):
+        print(i)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
