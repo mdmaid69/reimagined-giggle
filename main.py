fb4de3883881d7ed67c02ddb628803e843fda9bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:46:14 +0000

    rid print reasonable contact concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
