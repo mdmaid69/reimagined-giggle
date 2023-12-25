3c3c5e5c12c35ebfb74ce37dec5dfe5998a34596
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:00:46 +0000

    common commit culture hello count

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
