51d4a11e05932e58435c16f1920e71d96944c116
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:46 +0000

    gentle minute hot mix month

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-print("Hello, world!")
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print([x**2 for x in range(10)])
