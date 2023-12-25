87fd01481a6ac2b725bf3aa958702819baf0245e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:01 +0000

    theirs beyond herself children coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
