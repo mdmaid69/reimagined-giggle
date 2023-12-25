447f2ad49b97449fa5de47ad01310ef5541f3167
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:40 +0000

    depend plan describe strike turninto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
