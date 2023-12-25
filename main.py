a42ab3717439359f750255a80137de15663728fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:57 +0000

    there command claim keen aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
