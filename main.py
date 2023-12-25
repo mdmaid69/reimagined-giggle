eb1300d067358878155ce565821a984d18799277
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:53 +0000

    pretty view child who free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def divide_numbers(x, y):
+        return x / y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
