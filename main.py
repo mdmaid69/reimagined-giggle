59166f9127282906b37e98fa321b7b39ff24d79b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:52 +0000

    fast play debt odd tape

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
