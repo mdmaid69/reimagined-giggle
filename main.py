1466bd852368d87126a99f5891f897e2b6aa5079
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:00 +0000

    give keepup shoot direct after

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
