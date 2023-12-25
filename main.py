bd4bc7355b795b9187365bb6dbac31498c8dd6ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:23:52 +0000

    boat disappear minority memory row

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
