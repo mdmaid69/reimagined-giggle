e9cb24b4bff5a8daaba3ecb63b9c69f6dee0c26f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:53 +0000

    watch dress foreign equal film

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
