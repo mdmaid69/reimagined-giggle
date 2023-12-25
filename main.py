17add1a533592ba2ce2af04519f786374fe57add
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:00 +0000

    light thank kill attempt flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
