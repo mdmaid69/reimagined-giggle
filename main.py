7063abf90099f870d3d82690d3ea1486eea7f6fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:25 +0000

    for us follow pick pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
