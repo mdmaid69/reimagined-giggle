92f9252f432dec1928f154b0a43b75c41cbd4418
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:01 +0000

    unfair tidy reserve ear for

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
