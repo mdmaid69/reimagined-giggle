a09c44e530a64b485c41761a149ac673731cbc05
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:05 +0000

    official report work praise live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
