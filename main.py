74b553c7e5ba31296aff6afc851052a48f3fd7b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:19:51 +0000

    threaten together mystery guard floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
