8cc4cb6a9dd57a7218317d3a3b1c3603fea9953c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:36 +0000

    should emergency mixture unite little

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-for i in range(10): print(i)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def convert_to_hex(n):
+        return hex(n)
