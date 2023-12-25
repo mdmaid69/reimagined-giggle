199fa4e3d3d99c0650cd03e5acb8d8ff1f07984c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:19 +0000

    consult business pattern form interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
