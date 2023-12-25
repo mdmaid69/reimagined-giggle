8e8869f8d1667df2d7f99840f40b8aea33aa7949
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:31:21 +0000

    Bb protect equal interview embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
