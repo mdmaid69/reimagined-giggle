a98a1c360405bb06ffde4c3db4e4473ba73fd180
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:42 +0000

    ordinary passenger base university uncertain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
