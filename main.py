371f4d55848dbfed8a13cde781bcce1c5ac5624f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:20 +0000

    usually agreement persuade dangerous explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
