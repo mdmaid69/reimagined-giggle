cf8d13f9e328383f00d8a66f0516617501587afd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:39 +0000

    take ride food doubt vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
