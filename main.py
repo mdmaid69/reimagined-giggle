1849b5eb4fd6ba07b71ea819925220b78c00a14c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:09 +0000

    less connect original care yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import time
+print(time.time())
