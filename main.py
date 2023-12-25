6628e4e07d32fe84b2cc0d3b735f1028f3b5348a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:22 +0000

    come hot stir getout family

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
