5a6ba44c726e63381ad61424ab6108d734c5d81b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:50:38 +0000

    generous miss variety right generous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
