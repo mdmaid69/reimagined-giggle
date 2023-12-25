55cca67a598ceaf503416706876c5050413d0e4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:38 +0000

    live shock apparently trial Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bytearray(array):
+        return bytearray(array)
