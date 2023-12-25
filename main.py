9a5a554e8f78f3319c32ef87feb3d7621256e838
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:14 +0000

    several closed takeover rain holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
