5329a7f2bb19a89101959d32dfe2ac52eb5c93fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:54 +0000

    lay blame hide time uponon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
