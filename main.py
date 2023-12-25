dd1fb01b8f91aef0caf3d935831154573898e570
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:38 +0000

    stage figure precise lower about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
