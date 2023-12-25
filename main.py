c3e9fb22385422531c2639eb94398bd3e79b232b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:23 +0000

    cause live stiff surprised classroom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
