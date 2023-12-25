99b59ad5133289bc57032d6a060698df6fcf67bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:17 +0000

    wind broad tour type die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
