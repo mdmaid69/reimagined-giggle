c2999e3db53805b0b46858b7eb91aad23f106695
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:12:10 +0000

    class really young gap fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
