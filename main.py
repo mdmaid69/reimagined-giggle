f95c2612f3032cd831f70bd90677f53c733289d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:12:26 +0000

    bound head 566end stamp sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
