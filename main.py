c311a5251aef18c6cc203189181c4732c80ec92a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:47 +0000

    goaway round conclusion teach certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
