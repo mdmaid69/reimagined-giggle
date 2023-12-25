b19e7a0f27e7961c55e2b896ed919ac83cc5381a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:30 +0000

    visit very by include quit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
