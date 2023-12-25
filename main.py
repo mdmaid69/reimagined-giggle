c19ad2614748e8d4fc6b01a35f27fcd8a4822397
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:27 +0000

    worth bringdown complaint mass space

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(x, y):
+        return x + y
