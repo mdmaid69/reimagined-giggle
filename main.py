80bb984b535a1052c82b1fd4b8f0c3334d03b1e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:10 +0000

    bringup undo proud warn tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
