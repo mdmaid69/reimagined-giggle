bfb08a2152e4c8cbd4f634955dd6a8ba84a05a6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:27:07 +0000

    wing risk attention admire fact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
