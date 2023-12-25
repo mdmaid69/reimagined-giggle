e68c9c733a2aae13a63c8b88c03d2b51cc5ea081
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:39 +0000

    but object tap comeround remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
