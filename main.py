420ba8eb2ff86a0d70a2e4deb5fb4d479e50426b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:23 +0000

    false to matter hide strong

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_user_list():
-        return collections.UserList()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
