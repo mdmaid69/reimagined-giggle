6764e0e45a2d6320461dfd96e5241448a08ac6dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:28 +0000

    pocket over harm mood wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+def count_characters(sentence):
+        return len(sentence)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
