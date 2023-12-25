5f78d5fda4a6583de88b18e252e462e7e3e46d76
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:47 +0000

    national hurry talk sell push

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
+  def add_numbers(x, y):
+        return x + y
