6029d2132501050610b8d88a5b5db5915ef71cb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:16 +0000

    ordinary calm matter welcome room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
