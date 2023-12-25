ba0a7de99dcfd773357afbe314772c4f3d60e26f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:12 +0000

    range get trade turnoff disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
