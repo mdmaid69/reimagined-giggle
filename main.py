fecf28148be0417abb1b3bbbb330ec63dfcf980a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:41 +0000

    announce read create carry month

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
