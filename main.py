810d818587acfd4994e16b0d003176bf308e35b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:48:56 +0000

    refuse comfortable disapprove card culture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
