2192f5034a6e55c4fdc37e6fe48b3920fc7a6d13
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:59:01 +0000

    content support rob degree talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
