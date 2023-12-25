526aebc3cbc1453454e945ddd9090bffaf36f2ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:18:38 +0000

    report callback enthusiastic goon direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_power(work, time):
+        return work / time
