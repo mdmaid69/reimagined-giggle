598906a9722cbffa9f2600ac07294f15683d512e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:23 +0000

    transport middle history thinkthrough compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def square_number(x):
-        return x**2
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
