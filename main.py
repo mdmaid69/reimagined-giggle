6ab9281940f1e458608f3a0ada576129fd103c83
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:30 +0000

    used law by final from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
