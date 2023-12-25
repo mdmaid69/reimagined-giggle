da50d24aed1fc2aad6ec1ccd44b9cde7517a57db
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:08 +0000

    discuss second burn show admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
