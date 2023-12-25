1f8f572b79939c89b0ef3ae9bd3cd8868ca3d4d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:56:54 +0000

    fairly which wide half declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_max(lst):
+        return max(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
