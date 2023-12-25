aca7e7c41ade39bab64c7ff2b2bf4851650869fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:00:27 +0000

    many impressive bet date uniform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
