3e5d6450157be1cfcbfbc7b7138cf2aebe4ba493
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:21:35 +0000

    breakdown extraordinary rare spare memory

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
