a7de472bb145ebf6be069c52a20e85ff0f0ff5b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:14 +0000

    snow field holdout exact without

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
