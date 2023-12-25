a46c2ab17a008d5cffb708188dbc8b8ad84e2c09
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:05:29 +0000

    quarter capital anyone Zz near

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
