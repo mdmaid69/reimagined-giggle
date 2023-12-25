fbcfd5f8b27f8225a424dbcefdf3ead4a6f608fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:48 +0000

    provide joke total scared negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
