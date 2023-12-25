bd6b6b6eb3c94fd19921d5a690cdfe8f41e103e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:40 +0000

    announce guide year extra build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
