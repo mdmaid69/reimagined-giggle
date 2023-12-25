c280518fb199b9c472eee058c66f5d41b6337ac6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:51:44 +0000

    stranger think wrong aim fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
