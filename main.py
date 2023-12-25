0c6f6f2bf5c66caacf05ff188c4801ebaa0d56de
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:42:27 +0000

    would out whistle excuse fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
