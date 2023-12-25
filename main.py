2fb48f4e96ddc27020bc931ef9b0f1b1fcbec619
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:39 +0000

    tax window takeover each late

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def add_numbers(a, b):
+        return a + b
+  def add_numbers(x, y):
+        return x + y
