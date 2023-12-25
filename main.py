db7585707ce3e7acaa26f360342c462a319da467
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:31 +0000

    hear neither fear future report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def clear_array(array):
+        array *= 0
