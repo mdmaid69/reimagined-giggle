70c8bbc8ebac580d0af53f2fcf93c0dc37727f45
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:10:13 +0000

    broken unlikely legal seriously talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
