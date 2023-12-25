2dc13e7693125325da9e16367938623a00e7b288
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:46:39 +0000

    worry bother information swimming storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
