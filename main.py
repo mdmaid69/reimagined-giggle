cd953cca421c0ab6708cb258bd92362d8e897146
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:28:35 +0000

    test read past pen fall

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
