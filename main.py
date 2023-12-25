c0bb6e90b2397b12ae68ba28cac79327583f89d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:24 +0000

    meet you thanks city defend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_odd(n):
-        return n % 2 != 0
+def is_even(n):
+        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
