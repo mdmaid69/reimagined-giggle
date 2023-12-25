d9a68ec803b3b32a58919393a46ee7aa277b4789
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:03 +0000

    total exhibition rule inform refer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
