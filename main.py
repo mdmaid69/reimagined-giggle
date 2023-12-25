2349e4968b2e56c67e205b0cce43712ee2258b7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:29:22 +0000

    frighten swimming mean tip remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
