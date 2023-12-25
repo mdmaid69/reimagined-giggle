f248184d40512c1220924216046d7c9e644446db
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:08 +0000

    continue fork floor somebody tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
