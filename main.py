c60962d00435667d0ca2493ce10b1cbe442f06e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:59:14 +0000

    home variety remark however aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
