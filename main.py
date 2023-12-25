5ccf8484f92ba461ea07a110d2cbdd5d95ed0c77
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:45 +0000

    complain earth anywayanyhow alive community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
