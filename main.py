d85b39272e9a5e2e1232ecf8fe1efb04d9a0abca
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:41 +0000

    protection cough guilty seat family

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import collections
-def create_stack():
-        return collections.deque()
+def is_palindrome(s):
+        return s == s[::-1]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
