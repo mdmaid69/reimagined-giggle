ade163e67076f6c824ec519f374a5b4a37c9687c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:08 +0000

    responsibility pupil behaviour somewhere police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
