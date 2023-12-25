adc2c30a873e9a67cadf7360b4e5a32cb021d680
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:02 +0000

    so happen progress account calmly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-def square_number(x):
-        return x**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
