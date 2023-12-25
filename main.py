7d85474a9567cb5fb0e5df7a3eb7b902c0887a32
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:29:20 +0000

    shall brush Zz stupid get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
