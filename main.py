b27b4d3cf8fafbf7687e538f28a6e902a96c0c41
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:33 +0000

    Nn attractive square study broken

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
