8725e5c4e2f6e5b0038a487a2fc2bcd5ce010fd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:59 +0000

    mark charge spend man bush

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
