ce1d86d5bd3eea68cb67888e545801c0bba02396
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:13 +0000

    sweep embarrassed shout consult figure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
