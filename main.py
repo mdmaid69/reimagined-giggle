70dcbd130e68f280d9df62ad6b16c38940ff5fa9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:30 +0000

    frame back cover expensive strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_current_directory():
+        return os.getcwd()
