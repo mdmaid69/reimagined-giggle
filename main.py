39720b0f4c7dd23031d39e03493e470979545b67
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:15 +0000

    over kick bringforward please exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
