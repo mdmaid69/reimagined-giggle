1fb81fac614930d664a49f5fa889d3e1bc29d1b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:49 +0000

    top company regularly ask somebody

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def is_odd(n):
+        return n % 2 != 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
