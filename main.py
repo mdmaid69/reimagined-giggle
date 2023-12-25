6b99b78b815591e27479103615e1a1f40c5a5c4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:39 +0000

    occur rank comeoff moment ability

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sys
+def exit_program():
+        sys.exit()
