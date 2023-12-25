2d4f2dfa932d98c9c1ecec18e328bffcb278fe7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:50 +0000

    finish special debt basis pay

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
