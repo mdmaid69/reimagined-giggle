1a5e9376f5db177f5ba5a159ef233be318b89fe9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:41 +0000

    suit hold good see giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
