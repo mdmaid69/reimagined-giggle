49bf40685357d680b74cc7a253442b8f57c84c0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:08:29 +0000

    increase year that 571enough attention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
