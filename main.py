5f13c76b6c68ce07d01576234aee737a3ced1d3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:37 +0000

    content wish prevent voice pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
