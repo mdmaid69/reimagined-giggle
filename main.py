77405e5ad58943b5190dbfda3472c18cd9a6f235
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:38 +0000

    prize comeover best poor tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
