60f68a14313d06c049e3aa4401816a0e21f07258
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:37:41 +0000

    likely cost once mend trust

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def find_max(lst):
+        return max(lst)
