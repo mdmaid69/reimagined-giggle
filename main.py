28b7fe48ca5d05354426e726f491217e78acf199
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:44 +0000

    free raise pencil come Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_max(numbers):
+        return max(numbers)
