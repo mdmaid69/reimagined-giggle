21637d5cf79650a1c2848eccca65fe87c5daff50
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:47:32 +0000

    doctor unpleasant well cheek hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+print(math.pi)
+import sys
+def exit_program():
+        sys.exit()
