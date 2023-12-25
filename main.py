4fbdf46965194b401f4f842cc272c22e2f5b9643
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:59 +0000

    change best ground spoil exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
