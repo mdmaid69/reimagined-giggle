45c93c7ae50a5ab34256b62e38a5bbc3ee85504c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:57 +0000

    bottom giveup boat course middle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_work(force, distance):
-        return force * distance
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
