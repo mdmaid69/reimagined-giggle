651b95a1816bde02d530a0af80b6f17fa47e69a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:41 +0000

    at hurry attract cover nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_length(array):
+        return len(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
