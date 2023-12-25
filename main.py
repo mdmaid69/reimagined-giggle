01376309bf04b9a73b3f910a214dc9cce358fd32
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:56 +0000

    classroom any big open joke

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
