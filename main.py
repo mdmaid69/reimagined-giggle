afe36441941ae3009a5b56a558d25a605e056aa3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:51 +0000

    bus lunch suggestion raise call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
