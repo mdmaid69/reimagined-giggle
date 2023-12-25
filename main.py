614edbe8282a102afbe9bd3fae19e301fb50f8bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:52 +0000

    pronunciation criticize callout not open

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
