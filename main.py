25dda1b17d972b50170ed04dbe7c5258ee069812
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:11:34 +0000

    word sleep possible describe their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
