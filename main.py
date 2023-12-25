dc3b93a1dc536de2341d738f3204c4fc88a22b1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:15 +0000

    pain inquire direct workout create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_length(array):
+        return len(array)
