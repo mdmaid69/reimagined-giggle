0bd633fafec7642555a58a8849fa107dc970227f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:36 +0000

    the mark false keepaway kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
