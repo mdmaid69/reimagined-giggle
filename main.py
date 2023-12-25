217ea70aafc2c31c50a09025750ba41b6178edf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:58 +0000

    keep scrap either straight Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
