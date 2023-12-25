597733b4f5e8769e554b900c32de7a9fdf617bf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:25 +0000

    knock fat real excellent hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_average(lst):
+        return sum(lst) / len(lst)
