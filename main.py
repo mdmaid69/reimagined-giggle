4c79dbb00737a569790dbc5c92c96d4ebf20569b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:49 +0000

    spite dance begin opportunity appeal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_work(force, distance):
-        return force * distance
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
