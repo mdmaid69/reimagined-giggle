c00ba315b2329e7fda3b78ec3fea4bb9c6d5f71b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:33 +0000

    market difference shame since argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
