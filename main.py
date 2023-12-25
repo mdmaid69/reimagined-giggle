09a0af5ae1ae71c6aebf7c47cee400d775ce5388
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:43 +0000

    this future entrance turnover trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
