84b69f72456b6711affbe11fc2b90354cc3daf89
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:28:57 +0000

    pollution serious month separate taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
