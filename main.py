73a16b386a60c9d20a2816647a172fb237e889c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:22 +0000

    justice step bottom stone glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
