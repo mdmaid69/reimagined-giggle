26f47e79a657bd4866306097150514861415977d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:41:52 +0000

    unpleasant somehow children finish occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
