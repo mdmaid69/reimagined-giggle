dc3e5dd283b3a9c0f5a1094210d0f5ed73f85803
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:50:50 +0000

    consist cruel defend reason discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
