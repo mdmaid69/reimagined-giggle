6d81cc73755167ed57dd08d64f3fcbd9a2173174
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:10 +0000

    because prepare clear your abroad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-print(os.getcwd())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def remove_from_array(array, item):
+        array.remove(item)
