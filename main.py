4c44dc1d0c499759a9a849a02b5516194b5e213a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:10 +0000

    party getaway mention offer stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_min(lst):
+        return min(lst)
