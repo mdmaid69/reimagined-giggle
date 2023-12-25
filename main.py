c08d628e8124adb19548718d96cf7673627a71db
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:58 +0000

    confidence thumb somebody claim scared

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
