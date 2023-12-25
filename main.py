9f125edb6790196629900e22b0ece663be25f954
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:05 +0000

    more after if excuse shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
