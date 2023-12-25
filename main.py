9bac84acc60daaba9575034c5a31efcfcc77a7b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:35 +0000

    combination down abroad flood father

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def square_number(x):
-        return x**2
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
