7251ac2e5113625a638f7106f0804a009fe8fc17
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:36:28 +0000

    evening beginning like congratulate experiment

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
