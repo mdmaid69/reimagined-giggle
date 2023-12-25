d27a41df00fffc728eb579b2c804b2a40054d452
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:16 +0000

    separate pen minimum shame history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
