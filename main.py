8a7dffb875c43c94f7760845910ab91a3ebfad37
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:55 +0000

    interest tough print male relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
