dd6bf91bd9e9f774dcc28ea6a977af5bca799004
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:25 +0000

    lift tidy below distant recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
