88cae72d0c06d8991e3042f82af870aa44afbaa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:52 +0000

    dangerous lookaround refuse pinch brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
