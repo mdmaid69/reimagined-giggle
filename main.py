0f0bea1c706708fd1817de963a13beb2f2eefe07
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:31 +0000

    home thank then thumb callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
