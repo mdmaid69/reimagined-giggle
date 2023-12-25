fb4423b349468193dbe4d87804d377394fb75668
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:13 +0000

    receive waste callback flow ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
