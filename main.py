a5154e7af8dbe04b087381d96554d32a28e9a330
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:55 +0000

    water decision shop command chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
