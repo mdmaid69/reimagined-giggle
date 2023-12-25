e66b8ebb7287500150d8fc6d44172d95021d52c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:07 +0000

    circle be pick total correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
