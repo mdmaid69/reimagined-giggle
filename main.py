1b7c6aa0d690c4e61a4f3398db446404bcc357fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:13 +0000

    air collection art persuade narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
