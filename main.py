0d4ea80063255c24abda858beb693ef1012e0438
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:12 +0000

    welcome law again print target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
