f23424b49aaf7ec694dbaa3b588eb8290e622295
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:55 +0000

    real protection morning equal equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
