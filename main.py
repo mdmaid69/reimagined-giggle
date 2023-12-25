b61c51b21af9115ae6ca2c939d8b1f0aceba1201
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:54 +0000

    explanation list yes plain risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import os
+print(os.getcwd())
