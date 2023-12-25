68c2229441ab59c968c54b60d66296de396e5f4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:53 +0000

    squash neither tall common along

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
