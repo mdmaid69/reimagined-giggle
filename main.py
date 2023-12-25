4e1f0b7e696d74bb23606520222263b95a317b26
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:56 +0000

    arrive breakinto perhaps stage exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
