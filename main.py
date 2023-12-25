41058e12e2437821fd68b6c0a9a7621a5af2142c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:42:16 +0000

    deliver order independent accept objection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_work(force, distance):
+        return force * distance
