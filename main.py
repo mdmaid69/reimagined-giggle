e87a87b92a1419f29910bd6c309b059c9fdbb6c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:20 +0000

    shine since narrow develop either

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
