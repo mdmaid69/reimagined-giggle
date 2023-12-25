866b664fdd0551504c44f5f03dc7051fa073afe1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:59:57 +0000

    mind goout forget object key

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
