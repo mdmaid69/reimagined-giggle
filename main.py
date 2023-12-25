0938f3ade4fb2908edbd17e457a755703fbe420d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:14:30 +0000

    date careful demonstrate greeting gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def cube_number(x):
+        return x**3
