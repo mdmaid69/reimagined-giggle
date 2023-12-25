86c65fc701ce2a9964e4975655cb121a46964246
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:35 +0000

    state tool section yes correction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
