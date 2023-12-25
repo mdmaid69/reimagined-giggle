0fa9dd01afe47926806b53611941d4805f4a0fc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:33:47 +0000

    name height fly round slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+def multiply_numbers(x, y):
+        return x * y
