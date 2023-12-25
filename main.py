b1a522bb4ac1ccb403d309c34c1ff6a8fa0ee787
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:44 +0000

    odd bound reduction afraid muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+print(os.getcwd())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
