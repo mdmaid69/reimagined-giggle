cfb298356bfa57f50059900d544546eeef359cff
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:38 +0000

    base plan average autumn separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
