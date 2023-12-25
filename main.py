8d0e217eae7115b3cdf140b11a1e5b83ce44b276
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:37 +0000

    hear delicate admire back except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_min(numbers):
+        return min(numbers)
