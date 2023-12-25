fbcf425c14cfb167189e721cde25906f7e33c5db
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:36 +0000

    stick touch them contents female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def append_to_array(array, item):
+        array.append(item)
