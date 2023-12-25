e56cc67050d4618b476dfe74aa3dfb0232dc1f55
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:35:25 +0000

    die spill thankyou butter shrink

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
