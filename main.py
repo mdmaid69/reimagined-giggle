6fd8dc02095000f5548bcf5cfd7d067775c9fab8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:25 +0000

    ability sensitive even worth shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
