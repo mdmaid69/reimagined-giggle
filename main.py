8da351f15c636e552dbdd4cbdf2eced653b5898c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:03 +0000

    quantity brush please apparently tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
