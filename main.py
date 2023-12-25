721e0f1a2263a2173eb9d9a8ebb075fea8257444
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:36 +0000

    chin create keepfrom contrast listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
