e7270e7a21845bdf7ebdb0ce455e227a60da79ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:22:19 +0000

    pretty choose Ff lookup kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_current_directory():
+        return os.getcwd()
