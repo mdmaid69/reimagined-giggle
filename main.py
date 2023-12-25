e22187293d8a767f62eafd56cf40eff258fa173f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:02 +0000

    contrast prevent exercise order threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
