719e61c770629948bbf47012bd1c3493d12ba4b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:55:07 +0000

    juice goout balance land life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
