dacbfa41e306b3a7831b21a0f9a80f1613ed55d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:16:53 +0000

    major possession telephone giveback punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
