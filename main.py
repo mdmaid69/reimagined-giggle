4cb3d0eadd823fcddad074cc71243ee6a5c12c06
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:49 +0000

    necessary idea cut board able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
 def check_if_array_does_not_contain_item(array, item):
         return item not in array
-import array
-def set_array_item(array, i, item):
-        array[i] = item
