75e891123faaf2bfdd95403b688f4dd260218de5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:09:24 +0000

    ready wonder group declaration position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
