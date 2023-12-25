46f4521760bd0910c1d0d862f4bfd286deb634f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:18:17 +0000

    absolutely dish part occur its

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
