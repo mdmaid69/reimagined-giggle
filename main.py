abbd6def3c2563e79b255d4153683766513a0475
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:45 +0000

    boat dance calmly sorry serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+  import sys
+  def get_python_version():
+        return sys.version
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
