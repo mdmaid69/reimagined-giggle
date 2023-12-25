b979e406653a41d365bbe53a0424eab311474cbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:51 +0000

    conduct search calm bury stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
