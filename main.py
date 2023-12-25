261dd8ffc5b9bafa32198fdecdc356d878d23d63
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:45 +0000

    permission surprise steam constantly distance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
