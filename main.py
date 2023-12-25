5769e2f3fdf178483ad9819db47eed20b1c96c47
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:24:35 +0000

    basically sort learn actually plain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import time
-print(time.time())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+text = "Hello, world!"
+print("Reversed:", text[::-1])
