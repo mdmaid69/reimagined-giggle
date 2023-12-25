4b54a4322f42f8e7fbcd2db861ccb74986a33f70
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:36 +0000

    charge at bank ashamed smart

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
