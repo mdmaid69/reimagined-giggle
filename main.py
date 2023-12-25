f6310b5e1904b9f47c0a010052c684ec536f504b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:36:20 +0000

    copy mistake phone invitation keen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-print([x**2 for x in range(10)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
