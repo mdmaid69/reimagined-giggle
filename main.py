d863b286707981c27052a56872fc55cd2f7bc496
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:56 +0000

    coffee female exercise all only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
