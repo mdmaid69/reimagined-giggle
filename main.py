dee5331d41838630baad894848c9aee12494f80e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:34 +0000

    punishment disagree invitation sound over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def convert_to_binary(n):
+        return bin(n)
