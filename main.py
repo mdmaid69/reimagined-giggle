2d522579090230e2e90b8fcd079c54d2f1987cc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:49 +0000

    thanks hit careful surprising practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
