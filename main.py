e1008549af4a3cd8212c1f63618a73ed55331f79
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:24 +0000

    plain shock name his Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
