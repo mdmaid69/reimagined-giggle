0eb399f3d5f0494e2bc82135d17886b770775375
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:09 +0000

    film fine study our female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
