06f08ad2771a1b7097a1fe164069fddde7daf817
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:53 +0000

    able sport afraid declaration next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
