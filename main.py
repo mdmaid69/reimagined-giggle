e98860d391e6f67213b5d304a38b8b17c073b146
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:57:08 +0000

    support programme aware stare opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
