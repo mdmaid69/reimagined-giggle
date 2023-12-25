af9df4843f7e06904ca582666d9b2d4af9c8f351
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:39 +0000

    art reply small risk couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
