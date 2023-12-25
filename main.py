d01c12ec34a0a70c709830f9c38e5da747bc5c66
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:26 +0000

    majority damage university recently make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
