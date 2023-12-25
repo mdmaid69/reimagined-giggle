627ece6d51a78ee1c533ee791e96a5fce6e61903
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:19 +0000

    fire use callback wise dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Characters:", len(text))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
