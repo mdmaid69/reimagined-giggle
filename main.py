423706a980970bd200605f83be04794119737514
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:03 +0000

    hospital become pause again community

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
