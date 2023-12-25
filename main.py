409fc0ffb5c4932cb2706fde05385085e5d379e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:10 +0000

    lay secret crash formal bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
