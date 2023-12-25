380e2f09afc193386aefd9aacb02b097854a1710
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:51 +0000

    fat knock pure top fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_length(array):
+        return len(array)
