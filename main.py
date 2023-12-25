cb7948f61ea9106fef9ca83b5c1fc0c559c88b79
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:53 +0000

    early recently ever protection stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Uppercase:", text.upper())
