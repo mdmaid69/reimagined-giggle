1a710f4f9bbbbe32a8a27f79017a76e72989fd83
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:13 +0000

    character contain store exist crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
