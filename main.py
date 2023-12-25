4e92d97e39ce222e4b29d077046593d4a6e1f09b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:00 +0000

    thorough hope maximum tidy commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import random
+print(random.randint(0, 100))
