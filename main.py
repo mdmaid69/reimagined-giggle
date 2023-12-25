322f124a7948df0bd8918f21bcece4e7eb845366
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:50 +0000

    bite pot look content risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import sys
-print(sys.version)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
