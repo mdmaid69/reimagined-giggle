4339f899e2820bc69f9f2816770d17cc694be101
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:34 +0000

    week promise skirt takeaway moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
