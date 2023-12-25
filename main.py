19acd6a6231cdf5659451094eef03e43978d4ca8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:48 +0000

    bush worried frequent evidence call

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
