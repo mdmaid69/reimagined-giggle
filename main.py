a8f166e5dd0efaee9ba351f6825d362d358c3803
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:24:36 +0000

    sensitive skirt Jj downstairs their

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
