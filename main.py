9a93cc9cf6d926d15ad1c81d39de9c3cb04b06c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:55 +0000

    seriously intelligent equipment bottom disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def roll_die():
-        return random.randint(1, 6)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
