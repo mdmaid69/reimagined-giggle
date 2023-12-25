ff9752c94f112a0c440575ab14bd9617c4a851f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:17:22 +0000

    surprise rush healthy once with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
