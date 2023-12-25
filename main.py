7509c51c265fc08af4e4bf07e6327ffe34ab407a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:16 +0000

    level rule characteristic pure inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
