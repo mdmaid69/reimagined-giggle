31deb73ad6b77bc0d0945d22e18ef08088f6da06
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:40:57 +0000

    bringdown will still bin hers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
