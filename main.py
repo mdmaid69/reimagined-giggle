bcc8c2f5268cb7f69b18f062d6fbbb369484810f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:26:25 +0000

    private crash stay hide advantage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def split_path(path):
+        return os.path.split(path)
