710baea66c2e5d9c6b1c7314ec1ccba203cab0e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:52 +0000

    brain point school again nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
