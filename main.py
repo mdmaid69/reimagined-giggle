d517f0ee6c23ca48e0e978ca983454ee54777c92
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:49 +0000

    attitude turnup area story produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
