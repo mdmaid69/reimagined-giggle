d9183cbd5247c866e24b193f51b03e9f04fd8735
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:01 +0000

    modern twist accordingto pair demand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
