effbeffa512183079b938ebcdef7ae5462371667
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:09 +0000

    substance hot surprise fit avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
