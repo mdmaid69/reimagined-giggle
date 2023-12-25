4ea5b11ef50fd9294f69913ac4a3cb3a90d5d8a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:12 +0000

    national considering smoke plenty reach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_set(array):
-        return set(array)
+  def is_even(n):
+        return n % 2 == 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
