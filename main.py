166be6041f8ddad15b9f88c9c32f8650569d7616
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:25 +0000

    mixture declare bang deal pile

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
