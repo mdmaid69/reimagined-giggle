75261dcab23deaabe77433a744564b2ba619395f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:43 +0000

    right lunch smooth worth partner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
+def cube_number(x):
         return x**3
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
