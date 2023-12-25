77976b024bfa48f07ad69da665635617880d6025
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:59 +0000

    contact listen expression say succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
