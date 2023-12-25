f74a956787e3e54a978d614ad5558b0434da41e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:17 +0000

    rare turninto wipe tongue scrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_area_rectangle(l, w):
+        return l * w
