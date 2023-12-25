0a7d6786fc96965675bd2eb2581c1dcec762913e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:12:45 +0000

    worst influence thought wide attach

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
