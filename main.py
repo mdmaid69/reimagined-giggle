d60aeacbc0e6b3e5ac9cb5e69dd85abfd552e99a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:16:12 +0000

    to how tone false leg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+print("Hello, world!")
