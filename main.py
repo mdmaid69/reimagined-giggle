7975f4e9d29de1e77d39ff461c537cba5055b0af
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:42 +0000

    throw solve protect grateful makeinto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import time
+print(time.time())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
