82434adf5d9ccadb57832a1996d8b94605ebfa32
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:25 +0000

    shadow bank company impatient difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
