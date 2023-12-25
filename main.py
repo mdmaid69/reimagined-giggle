345a6285aff9b78acba48a1b1a41bab966ad2c9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:27:56 +0000

    early deliberately murder turnon coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
