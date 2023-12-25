66b869faa7c7dc75e02b7963096fbb924aff0036
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:40 +0000

    television track him trust Gg

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+for i in range(10): print(i)
