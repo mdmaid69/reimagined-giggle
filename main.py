fa77e832379cfc2116206423a05913d56ca89367
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:33 +0000

    concern exist community other goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
