3274fa8f92ccb76950db9b58d94ed2d831bdeb38
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:57 +0000

    reaction day mail attention but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
