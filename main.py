b13670f372d75f8305f1f7279dcbffdcf4aa1743
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:16 +0000

    niggaacademy about work heavy pressure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
