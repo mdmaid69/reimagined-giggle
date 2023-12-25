fe64664a8f2c0a82d3381e31890370b48e39e0ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:58 +0000

    quickly someone regard discuss rate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
