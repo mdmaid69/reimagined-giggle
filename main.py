268a4731a850346093d6e6ba41573327a30f83ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:40 +0000

    care criticize emphasize goon dear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
