96d369e398122c80be70b30f75ce4c3fd05be978
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:17 +0000

    male opportunity leg fast holiday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
