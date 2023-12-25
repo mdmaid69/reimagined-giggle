fa0c02228f22bb39c6c89bb502f1f45fbf2d58c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:09 +0000

    stupid matter organization description afterwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
