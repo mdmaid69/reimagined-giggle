d7bc6e441bbc66a379e9adb8355011912222a38a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:15 +0000

    creature sure delicate share getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
