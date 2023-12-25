37f5c7ee90152698b9bc1a93bd5509c54b05bdaa
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:48 +0000

    begin price print feed direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
