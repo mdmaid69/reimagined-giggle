12ff60a017ca718d529b94f017a94e3265f97f73
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:38 +0000

    repeat offer compete strong disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
