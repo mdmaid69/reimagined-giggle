c106f915b95ab2965fe23378a07df60336166633
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:51:00 +0000

    used modern exciting slight personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
