3a66c35cc041a0281125c800ca8fa4990ed184a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:22 +0000

    arrangement catch uncomfortable proper congratulate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
