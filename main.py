ceb5d8922adfde32d220ec76955c6c7c7d9bd916
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:12 +0000

    brave murder pick 567enemy transparent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
