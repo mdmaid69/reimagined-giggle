86d554d3a4965d584117e942955406a62e7ea863
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:13 +0000

    hope Mm take boil wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def square_number(x):
-        return x**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
