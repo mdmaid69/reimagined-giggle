7a13b26a728cc20f9c5c5660873479cf132f378e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:50 +0000

    spring smooth do line every

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
