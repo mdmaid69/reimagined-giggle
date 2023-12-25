a1ce0fb611b5a4238913bcfc7d5836338c0c1bc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:19 +0000

    match 567enemy prize sell inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
