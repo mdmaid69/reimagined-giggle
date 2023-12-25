d7cbf9d96ffdc83ce70ead711eabd36d0774da6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:46 +0000

    male passage community like left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def print_python_version():
+        return sys.version
