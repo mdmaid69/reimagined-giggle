d84fbaf3abc379b1fd32c394fbe72265541d463e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:47 +0000

    estimate way cum criticism wish

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
