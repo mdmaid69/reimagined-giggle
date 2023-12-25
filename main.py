edf192f1695a88a18ff1173f402393e27e37541c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:43 +0000

    ignore agreement prepare exit beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
