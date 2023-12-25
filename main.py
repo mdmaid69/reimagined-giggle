96534aba33d4c7d2764806f9b2867a0f0d823e2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:45 +0000

    satisfied set design fault phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
