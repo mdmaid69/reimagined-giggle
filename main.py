e8ea6012ac9504c05034952f4a1a795cb621601d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:26:31 +0000

    grand minute in Itself coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_current_directory():
+        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
