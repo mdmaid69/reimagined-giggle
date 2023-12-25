d05a18a25734cbcf937c284cf6abdfbfd2498b9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:35:34 +0000

    speed clearly age gather label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
