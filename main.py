277dd3e2eddb41d04373ebf106864006b42c6671
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:57 +0000

    star hear 571enough private valuable

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import matplotlib.pyplot as plt
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
