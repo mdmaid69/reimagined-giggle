3e6fbdcecbdb406875c587db3a452b0ae32f0b9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:06 +0000

    do control big position mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
