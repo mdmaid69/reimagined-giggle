eab5f638087ef674c9d012945b6f5968795aa8b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:42 +0000

    Oo realize character themselves block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_max(numbers):
+        return max(numbers)
