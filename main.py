d6e60401abcfd46b92dc5e7d2ebb28b58cab8b68
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:58 +0000

    storm land swallow over under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
