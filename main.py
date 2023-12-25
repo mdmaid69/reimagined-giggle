fffe8844108fd5e51783d45675cfdba47f52ceb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:53 +0000

    sell aim trade journey cheque

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
