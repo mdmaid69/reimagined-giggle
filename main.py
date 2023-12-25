d20e0857c71010f749497ae3d8f3c6ccfde437ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:27:55 +0000

    Itself measure swallow bush artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
