46f9cf751be4ded0cc14c9135fa416cef0f14519
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:55 +0000

    you post perhaps delicate cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
