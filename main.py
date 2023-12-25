6c56bdd5149d4b9732db06d50b064d1b29a0317c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:12 +0000

    suppose store below yours loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
