942e4f756c0960142fdad7ad3f547c2b10a96c42
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:22 +0000

    stamp shade snow depend apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+print(re.match("h.*o", "hello world"))
