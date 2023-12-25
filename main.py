78827a9c3ff2860056c79424115ee1af7a65b0a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:13 +0000

    boat find capital drink pool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_min(numbers):
+        return min(numbers)
