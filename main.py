6e8febdf4cd9768b93ce8b4f00bf95a406fca995
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:22 +0000

    piece nigga match draw mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
