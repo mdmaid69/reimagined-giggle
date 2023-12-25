87e6b5f4e30754166b1a0fd1cfb8828e160ba413
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:13:22 +0000

    unkind actually fear answer whatever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
