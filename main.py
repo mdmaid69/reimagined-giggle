589ff25d7770d50ec0cf8204cb79f9a4a114652c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:54 +0000

    land result fashion figure theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
