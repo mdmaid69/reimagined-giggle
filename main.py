db83d4bd91e5589c0972574f2624d4567501e278
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:31 +0000

    thumb godown crowd chat belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
