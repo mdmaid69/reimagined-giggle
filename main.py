141c167b5e29233cbb1868ad7c16be054687ce32
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:24:07 +0000

    deliberately suit miss battery talkinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
