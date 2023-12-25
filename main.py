fa8e93e754480f7b0780e943e5cc3cb6421ede51
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:14 +0000

    cheap ordinary subject guilty compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
