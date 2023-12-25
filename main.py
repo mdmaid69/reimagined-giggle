2531a6ac5c9e34767aa0840c6228ba941e874060
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:05:47 +0000

    tool sharp course well mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
