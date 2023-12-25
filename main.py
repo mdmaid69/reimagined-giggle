e1077174920ab20f770c1266028465dce7a0a196
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:22:46 +0000

    confusing create pool damage fairly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.time()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
