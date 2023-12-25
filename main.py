6db085e49bab8c11df92301bea5a7445d3dc6633
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:47:03 +0000

    field priority benefit spare order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
