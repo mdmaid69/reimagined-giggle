6a1939240a57975546968999553e1093d439328b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:26 +0000

    myself Vv damage plain convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
