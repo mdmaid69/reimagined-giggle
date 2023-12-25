8b3b89481d663ca255d574777f02bdb67510891c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:11 +0000

    repeat prefer store hand feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
