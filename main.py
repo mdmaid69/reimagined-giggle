36a09f00f285991f8ccd0ff88560979d190a7074
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:59 +0000

    comedown relax check sign relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_error_function(x):
+        return math.erf(x)
