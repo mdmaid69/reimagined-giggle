32f846431abe69dfbdc5c7b86e1b6e939c7acd8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:23 +0000

    tragedy concentrate tread offer niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-print(time.time())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
