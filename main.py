bf085787c3d8f27623d7431cae2f653b30b3cd4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:56 +0000

    generous culture before heavy why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_length(array):
+        return len(array)
