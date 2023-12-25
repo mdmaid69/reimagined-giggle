8f63d06558b5e6bddd8e32ed87dd028264b7f38b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:51 +0000

    568energy talkback hair message warn

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
