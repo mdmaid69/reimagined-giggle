a3c0188ee7f8b282d8fa3698b9b435a100ca31c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:58 +0000

    performance who breakup anybody accident

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(10): print(i)
+def calculate_speed(distance, time):
+        return distance / time
