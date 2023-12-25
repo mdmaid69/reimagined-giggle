d435bad8875e8912487dc3fcd637338e4425063b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:21 +0000

    complain cruelty ought exit damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
