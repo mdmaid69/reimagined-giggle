d00609ee1be2f3ab80653e4f116df8340b877f99
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:52 +0000

    peaceful concerned welcome holdout blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
