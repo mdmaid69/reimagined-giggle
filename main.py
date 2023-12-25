1151266f15982f75b71ccc00c0cd84de71776217
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:02 +0000

    visit beside company fire promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
