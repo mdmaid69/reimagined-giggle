e4d169b296515891bf97ef6dee621acf545985c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:45 +0000

    fortunately central people will full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
