bd7a4ae64b2a13d90707d6bf424f97987144b365
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:34 +0000

    gentle sign friend future almost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
