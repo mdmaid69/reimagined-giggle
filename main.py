c9ec11a29cb4efdc1413b401239a1616d847973a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:36:10 +0000

    Qq report bang nature hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
