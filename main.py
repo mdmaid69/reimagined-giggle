09049c46d790c3f2631cf6127e5a94d8a8eba2f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:19:09 +0000

    tread frighten law custom slowly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
