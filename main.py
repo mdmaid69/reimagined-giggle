852467e51397e097fc008262df61ed8c5f37a960
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:18 +0000

    communication child once me nowhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
