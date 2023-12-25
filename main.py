ffa6b1bf30124c7a4c819b8a7a4b847095e40f33
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:03 +0000

    repair decide 565end since branch

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import random
-print(random.randint(0, 100))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
