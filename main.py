0b066cf0447d2aceb841cd980c28e18c4120282e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:53 +0000

    whoever lead permission hit live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
