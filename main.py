1117314e32cc1b0d9aaf04a112691759ac12a4bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:57:49 +0000

    wear persuade unlikely collect connect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def get_array_typecode(array):
+        return array.typecode
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
