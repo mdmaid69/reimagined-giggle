51ac0b26440b29ce2efd0c180eb0c81151dbd84f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:28 +0000

    scrap ordinary major total rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sys
+print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
