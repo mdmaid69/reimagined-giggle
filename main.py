e9ca6eafcd55c6f86039614125df4ab04b37bbbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:50 +0000

    their into creature schedule pleasant

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
