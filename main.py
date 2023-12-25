cdc4d9efd0105a02aa8e575b5c3338ad31950db1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:41 +0000

    uncomfortable build below wrong interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
