93f38892c655816f7138a8a5bd847cc3aafc8440
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:50:36 +0000

    check no party 573entertainment trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
