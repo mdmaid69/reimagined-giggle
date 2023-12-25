569639de2a5babb928dadb59f022bd2a6945b410
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:17 +0000

    pile the exploration giveout present

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def convert_to_binary(n):
-        return bin(n)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_power(work, time):
+        return work / time
