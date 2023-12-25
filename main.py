81aa43c09af9f9547b5c20a9cdafb10d9f6ddbd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:23 +0000

    church admit scheme gooff noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
