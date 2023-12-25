bdd614f68e3129d1ef88b67d97bd48094a5fdcde
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:30:49 +0000

    between school less accidental far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
