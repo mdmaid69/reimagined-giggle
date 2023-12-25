a49c853a396bfe7ad2408899e725c2c1a90d4190
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:07 +0000

    organization spite gradually soil risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
