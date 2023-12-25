dee4b891848caaa1296aa048d2e10ddb2864fbb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:29 +0000

    wise fair downstairs stamp contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
