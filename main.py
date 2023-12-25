cb0d0706972459cb7619de3977ce824e2ff8fc10
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:50 +0000

    tax confusing load welcome either

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
