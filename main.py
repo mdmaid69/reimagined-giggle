dab12a75a0c7ad9d2a241e0b377101d1dab3fae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:41 +0000

    accidentally colour interrupt shock personal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
