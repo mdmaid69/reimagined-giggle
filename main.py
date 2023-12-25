d39d5ae9e1d479ac35b5c8ecc0842e69d9ded41f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:23 +0000

    agree scale weather touch defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
