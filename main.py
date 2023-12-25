a4b601e8eb81c0a1e7a4db468b1a70731ce34ab0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:15 +0000

    say jump disappear cover solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
