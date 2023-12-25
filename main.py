7829edfd27d523512d89dcfc2ea7478fa6f073c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:03 +0000

    pretty commit sure activity traffic

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import time
+def get_current_time():
+        return time.time()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
