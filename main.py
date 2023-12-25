5f1fd2d8fb18a002f371da99a12a4f50ba25aec5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:49:28 +0000

    properly mess holdup fence wire

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
