9e5c9748efe13e70b00e398aa0884ef8bd63acb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:48 +0000

    treatment neither steal sweep frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
