3c11de0b86dff9b378ba04bce1da7f618dd89610
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:23:02 +0000

    exit kick rid spray patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
