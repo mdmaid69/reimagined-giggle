191bb57373bdae39ecff3cd1bdb38f33e2e263d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:47 +0000

    mine to this ready between

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
