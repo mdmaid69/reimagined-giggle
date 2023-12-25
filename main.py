593e2fc276737e6417f4e2f078595bcf1ce5f691
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:20:19 +0000

    wish circle pour punishment ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
