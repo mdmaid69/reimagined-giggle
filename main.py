8733c5ff7e65c785f08d45ada905ad7d99bb22f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:48 +0000

    power steep complaint once inquire

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-text = "Hello, world!"
-print("Characters:", len(text))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
