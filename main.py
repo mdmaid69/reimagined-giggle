4422bfe950cb5b16535872fa8fc514da0f43a9a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:25 +0000

    type fit view awake commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
