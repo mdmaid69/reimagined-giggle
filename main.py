43a0b332618e5d49038640e264d1214503138f27
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:26:31 +0000

    production behind under closely getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
