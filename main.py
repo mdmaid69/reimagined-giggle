bc11cfe987a1d267bbd35e7d9f8f2724325792a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:05 +0000

    ticket neat makeinto improvement rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def append_to_array(array, item):
+        array.append(item)
