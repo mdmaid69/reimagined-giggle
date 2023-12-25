16231ae699a0c5f4b65849b29bc163f37b182037
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:02 +0000

    drive goout butter us hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
