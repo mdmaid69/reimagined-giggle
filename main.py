eed3625184a9bf8d29f499b991f6a130a14e45b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:54 +0000

    deliberate related corner style bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def convert_to_binary(n):
+        return bin(n)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
