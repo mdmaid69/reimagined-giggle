dec60e9ed519b71bebf992f311c03865c1169a73
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:55 +0000

    news dinner thirsty minute hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
