9720d0d9f96aa59d1a0402c3466dc0db78574a83
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:14 +0000

    turnaway hope face practise smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
