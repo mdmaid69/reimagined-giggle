23ca93c3d1b13c2ced5e7fa59331dc6dab427b9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:47 +0000

    exercise substance fat steady equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
