2fe20f17ff6a582833bb0d3021640cd1133f0328
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:38:10 +0000

    watch relief repeat dish goon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
