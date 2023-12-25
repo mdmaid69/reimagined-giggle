e37ff99c9034ce2cb4ebcffc58ab65664355b651
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:54:00 +0000

    somebody family nonsense slight insist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
