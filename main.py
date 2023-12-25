f499573f25684ee73da125c66a6d5ac7889280ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:17 +0000

    some Ff weekend remember temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
