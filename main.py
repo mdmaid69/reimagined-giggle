3cdd6698287603353eddc2cd30e5f0764c6bca4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:31:17 +0000

    spray brain lookback(on) together deliberately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+from collections import Counter
+print(Counter("hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
