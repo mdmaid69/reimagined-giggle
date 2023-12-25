0ab42f924f51e041c97c429921757bf096c63904
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:18 +0000

    Jj hope warm light experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
