eb5845b0b6b589c9eabcef1f13a6106c3cfe86e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:09 +0000

    turnup tune getup speed worry

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
