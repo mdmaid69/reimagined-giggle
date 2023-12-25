4cf14babcea3c634e2273b07df566aaf857ad8a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:38 +0000

    behind fast bringforward future gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
