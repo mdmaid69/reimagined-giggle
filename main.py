5d47095adcac89e1845fd1e782ac5e6a1ce50bad
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:17 +0000

    dull listenfor month wear accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
