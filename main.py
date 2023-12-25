24b81cef2a245867fb538925658538f94823e56c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:32:08 +0000

    proper people remember which understand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
