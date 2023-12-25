7bca95a2aa3518042571885e7b4b4b1497a861f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:44 +0000

    software tune so matter experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
