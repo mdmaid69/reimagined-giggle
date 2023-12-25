beefdb877f8e34e90dcf80c6a09b369376e697be
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:11 +0000

    personal pot imagination runinto needle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
