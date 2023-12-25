24cdccc32dfe1d78fa9090a461d52ac70460820a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:30 +0000

    happy raw deliberate sing mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
