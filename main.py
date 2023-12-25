a4d98f8b44d6f97d6ffd22b557f42cc07461f8c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:18 +0000

    once record godown hair loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_time_since_epoch():
-        return time.time()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
