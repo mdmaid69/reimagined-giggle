c0b38f2658dcc7e2782a6a8e9449963552dfb3b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:36 +0000

    mention true underneath difference base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def add_numbers(x, y):
-        return x + y
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
