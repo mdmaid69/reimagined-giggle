5c9e8e06e3d39a96fec31fc2f50df5d6ac27e8fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:27:25 +0000

    unhappy close mistake protest maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
