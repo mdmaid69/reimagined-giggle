09b43acf21c6d7eb2fb8affcb3b81e68753f316e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:28 +0000

    spend amount absolutely yes trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
