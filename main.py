20868c8ffb4d2ae960a45a323f7fb027f6be7326
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:42 +0000

    return direction hurry confused coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
