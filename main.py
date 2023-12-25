65ff93d17f9ec29ab9042cf67f86020f518e9f2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:37:26 +0000

    father sweep moral pattern shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
