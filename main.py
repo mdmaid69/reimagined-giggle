0a598c54c05eb57e26071830e85c6f8ebca9607d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:04 +0000

    relationship away intend comfort apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
