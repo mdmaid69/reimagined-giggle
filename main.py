497addf34b553068246cc0252f1e3070d52bba88
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:09 +0000

    platform left flood than miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+def convert_to_binary(n):
+        return bin(n)
+  def convert_to_octal(n):
+        return oct(n)
