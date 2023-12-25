e8a6808441020d8ed71ee96117ad014f1768bfde
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:49:03 +0000

    afraid explanation load square emphasis

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
