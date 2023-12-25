2e8b0786ace6cabc0fb93c47d9a45d8209532791
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:19 +0000

    little opportunity takeback drop recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
