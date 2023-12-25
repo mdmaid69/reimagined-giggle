c21d81d3af56ff497dbecc639f8b7aa66487301b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:35:29 +0000

    only division bunch plastic holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_float(array):
+        return float(array[0])
