43572df2337c2d1963b021d06b31f83a4d0f55ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:43 +0000

    Kk live poison pass chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
