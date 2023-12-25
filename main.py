142104d187708c953526764eb730330b9c43da17
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:42 +0000

    heavy trick settle ring commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
