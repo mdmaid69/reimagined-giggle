5e994ae66ef143a6bfc87bb5edcf5404fd5587d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:39 +0000

    because simple start state to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
