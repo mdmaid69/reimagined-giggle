bd36b59c0743085c1832d0551d48eb74c7f8dccf
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:01 +0000

    cough great small hollow study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
