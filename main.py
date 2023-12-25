38e2bd71278969b32cb5679ec5436f34edbb71fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:47 +0000

    simple protect various past spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-name = "Python"
-print("Hello,", name)
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
