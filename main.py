9f3bece53fa127b8c0fcdac0c5c7098896098faf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:48 +0000

    skirt drive moral calmly contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
