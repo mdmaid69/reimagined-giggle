b702e3a40717c832148844f8f31d7cd36c57ebc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:05 +0000

    along deliberately upset practise catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_set(array):
+        return set(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
