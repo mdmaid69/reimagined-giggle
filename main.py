66ee5a77ff11d2af7b413abd8e60dd430229e817
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:41 +0000

    yourself skin fasten praise fence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
