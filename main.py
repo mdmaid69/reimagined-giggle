214ca3c9773afa47cd13b03379e1d5b42b597cfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:36 +0000

    aware drop year fair we

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
