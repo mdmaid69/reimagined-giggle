98646891d5781738d72b5fedfb9184438689339a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:51 +0000

    major crowded pride treatment correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import getpass
+def get_username():
+        return getpass.getuser()
+def convert_to_hex(n):
+        return hex(n)
