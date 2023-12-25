a302ec5dd59e87c409eb0e42ddcfcaca8abd3351
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:07 +0000

    mean the section declaration apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_length(array):
+        return len(array)
