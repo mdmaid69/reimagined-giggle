ec168aa13c99a5778bcef7ca9d21a324acea996e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:48 +0000

    discuss book payment borrow male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
