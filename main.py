f4ea24fb546369a6949cab19e77e3b87cfaedb06
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:01 +0000

    injury of doctor goback(to) lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
