0a51b45f237b159d2c0ccf3424b50bfa4a9c1a77
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:30:09 +0000

    reply natural Bb but stupid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def convert_to_octal(n):
-        return oct(n)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
