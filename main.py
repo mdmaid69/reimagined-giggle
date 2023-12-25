17e06a6bbc557a9cafeacb73bd7877ffde9a415f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:55 +0000

    since welcome home unusual genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
