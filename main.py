dc3459af7f2a5fcb5ddeb44f0fb06342360a59a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:13:39 +0000

    peaceful attention avoid soon runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
