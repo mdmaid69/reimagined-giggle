cf2160728c2633c1b59ad8f5c5bc18ae1658bee2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:38 +0000

    trust although trap circumstance rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
