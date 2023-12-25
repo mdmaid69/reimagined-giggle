753199c09017cf5b060ac731a1aba5ea5821ced1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:17 +0000

    street intend scream ring slide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
