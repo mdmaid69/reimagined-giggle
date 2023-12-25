61c712b13e9eb84909133bae4e28ac472580a329
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:40 +0000

    test negative bringback summer mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  def is_even(n):
-        return n % 2 == 0
+def greet(name):
+        print(f"Hello, {name}!")
