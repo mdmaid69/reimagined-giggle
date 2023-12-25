0669aa5f8c1b8dcaecc000dd8ee664d2154389ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:42 +0000

    side car invite shop exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
