e9f5c256f8cbc135925c34e0c1eb730a30d9261a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:47 +0000

    morning injury division steady sleep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
