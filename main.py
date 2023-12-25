adc3a32d7811f42445c8308cf7b6209becbb4861
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:08 +0000

    repeat rather provide from rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def count_elements(lst):
-        return len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
