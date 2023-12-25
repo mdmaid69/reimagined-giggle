2c3c8837b6938e450c40edfe89ff3c24d668e6ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:06 +0000

    breakinto best ground reach meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
