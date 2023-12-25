995f199e381a8cc86d62561840277b310030cda3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:23:00 +0000

    straight culture suit idea private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
