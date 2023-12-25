b484fce95e6173b6702c7fbc0cb578d97a5c0428
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:36 +0000

    section dance favour choose belong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
