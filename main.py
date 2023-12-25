cdce355732a5b76bbb2148d51985ca785b2856a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:46 +0000

    frame last disappointed keep hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
