86a5d1e258e5403f788526fc86da82a6d9ac6707
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:03 +0000

    on plate rob forgive official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
