81ee30f2710d1fcc6287510fcf3b60434b918e58
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:25 +0000

    surprised warning door sleep moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_floor(x):
+        return math.floor(x)
