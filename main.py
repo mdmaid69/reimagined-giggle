35c34f35f1c33f0418da9a6ef3a46c39a4d9e77e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:47 +0000

    sensitive myself war shut computer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
