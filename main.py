03fcb8855fc5606d495446ccee78120374fd0297
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:16:03 +0000

    certainly truth burst pay urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
