07c7476ff5db5f71e348e3ea2da445f8f2e1d301
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:51 +0000

    sale warning the crazy male

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def count_elements(lst):
-        return len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
