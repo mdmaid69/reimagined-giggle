9358514aa3f52d70b1fa59e85b7f4ae829a69199
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:29:54 +0000

    glass proof count peace with

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
