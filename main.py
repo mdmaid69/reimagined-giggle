049ff94101105df6ec0c6060863cc8b8ac2b128c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:04 +0000

    risk help snow soon appear

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_power(work, time):
+        return work / time
