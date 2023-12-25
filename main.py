3a6ba2986886ba88923e9535a81c87d0dc3cc5db
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:23 +0000

    this wear believe exam child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
