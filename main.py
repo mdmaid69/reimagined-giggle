596a6f360769fa9c07419bab65eabcbd898fe361
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:58:31 +0000

    rush yesterday demand closed onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
