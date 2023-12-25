14e69fc3bc41499a0b0668c23294e5d77eb329bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:56 +0000

    square polite thorough often centre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+print([x**2 for x in range(10)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
