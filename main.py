aa7c3cbe77f149927613d39f73710c82e920e96e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:38 +0000

    support prove lift wrap famous

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
