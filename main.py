b5ef36b6ddb410b52d1fbbfb62fca18cbde13f3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:47 +0000

    left away score deliver learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
