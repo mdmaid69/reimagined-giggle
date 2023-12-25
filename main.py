6343c95db76cbb418eca9ed10817b8a00b305cd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:00 +0000

    pupil check problem decide try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
