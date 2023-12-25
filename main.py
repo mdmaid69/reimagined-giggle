4ee220f808ba94ce2f1713b05b52733a70cf9cf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:41:32 +0000

    region wing complicated quick whichever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
