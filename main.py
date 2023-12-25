7628e7aec733c2b2980ba3c160f5da18f347a2d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:59:00 +0000

    may similar message bet Gg

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
