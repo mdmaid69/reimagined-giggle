cd8b88f70d62d564b6f1bdeaa6048c87fe2de6e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:21 +0000

    Kk lead practical experience majority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def reverse_array(array):
-        array.reverse()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+print(time.time())
