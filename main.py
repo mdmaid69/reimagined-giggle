b85dc4eb060a05d3edb4fddb15b45e8bb55ed071
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:07:31 +0000

    truth either firm record container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import getpass
-def get_username():
-        return getpass.getuser()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
