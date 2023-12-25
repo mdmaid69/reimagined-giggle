95c03cf62addd2663b60d5b6bf15d59e6580962e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:44 +0000

    OKorokay cheat remove poor spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
