137eaa47a02b6367a7edf4c29af62f284c1ba543
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:01 +0000

    normal machine doubt approve please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
