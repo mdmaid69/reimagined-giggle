4708d54c74c1275cb6d440dd30e4ba8be30f45e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:47 +0000

    since quiet consider dream Cc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
