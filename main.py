ccec9f17779cb224e56eb8e1037353705c91fb08
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:59 +0000

    simple by smell sure follow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
