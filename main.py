8d7bab72b50d044481dee8c83dc59aedce8e34ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:57 +0000

    tip characteristic accommodation quit just

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
