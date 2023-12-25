67baaebe65760b473b7d8a74ebc0cb5e5aea8a4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:43:09 +0000

    payment pretty thing catch beneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
