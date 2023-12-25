fccd3400afc76cf2fb3a952b7a886c9b3383a506
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:38 +0000

    substance achievement ear plate holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
