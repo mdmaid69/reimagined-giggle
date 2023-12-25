db707f5be18c4db11d61d839c1690b96a590ea81
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:48 +0000

    conduct classroom false range direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def check_if_array_contains_item(array, item):
+        return item in array
