c1568c228a94a47abd3479eb279367d0fcfc3ab7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:54 +0000

    organize nor difficult uncomfortable nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
