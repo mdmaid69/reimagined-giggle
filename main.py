055750d1b4893bfdd917c444391f4c673f0df990
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:43 +0000

    floor connected picture spill display

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
