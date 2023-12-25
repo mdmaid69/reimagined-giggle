93524ea515790efc82bf326e33928dd55fae5e7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:44:54 +0000

    pipe benefit 570enjoyable draw woman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
