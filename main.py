55606449327fb2f2a67c06787c7cf03cd6a9c285
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:01:04 +0000

    land grade fire hers heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
