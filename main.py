8609535fe2fd122ad3d65db1cc40806b7058b5f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:14 +0000

    narrow method emergency suck phone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def subtract_numbers(x, y):
-        return x - y
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
