888c867fe1d40561e19d1a4a54cc4b7d0d3591e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:34 +0000

    ability squash target experience tongue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
