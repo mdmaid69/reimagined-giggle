04e56df619f887039aff6074fa9e868a8e2831a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:14 +0000

    ring high geton anywayanyhow excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def convert_to_octal(n):
-        return oct(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
