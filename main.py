5d9b0f7d7e5181297ad6395ecc17799fd3532e3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:12 +0000

    callup spill without length date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
