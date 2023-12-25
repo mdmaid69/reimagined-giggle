24ae1bf771fb6080848be30b69c47b3a40389725
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:47 +0000

    future usually curious repeat join

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
