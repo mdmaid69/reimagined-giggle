262839aaba3b9153bc3fbf6865de68f6ca648659
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:05 +0000

    pain fit punish gap slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+from collections import Counter
+print(Counter("hello world"))
