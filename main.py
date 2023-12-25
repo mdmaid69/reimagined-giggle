629b6b0c9439b0bc0b4df495082adaaebf47959a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:56:33 +0000

    young compare limit steer calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
