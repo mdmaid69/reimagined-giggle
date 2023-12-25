05b750b1e1a5608cca96182c81bd1bff4014f033
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:36 +0000

    visit approve care come likely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_acceleration(speed, time):
+        return speed / time
