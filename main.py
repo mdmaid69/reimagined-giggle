c7ceab9b3c6006ffa474ea11792ff0b6edcca344
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:18:10 +0000

    finger rush intend shallow ought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def sort_list(lst):
+        return sorted(lst)
+def greet(name):
+        print(f"Hello, {name}!")
