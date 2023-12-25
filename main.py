6d4b48e7a16a9c8fd1aa6f71521775b6be999224
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:03 +0000

    complain backwards flood lose support

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
