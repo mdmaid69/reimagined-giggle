c2c640bd9c009ed3c18ea3c9ea0b540b30345469
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:56 +0000

    ever reserve nigga completely eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
