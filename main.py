efea55c221163c0cc4d451c961e338b8efd002f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:40:36 +0000

    walk people difficult breakout vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
