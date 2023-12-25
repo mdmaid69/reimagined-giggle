9dc8c17b1cc9bf36bb4dadeac9e85d0f40963e29
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:39 +0000

    position row heavy suddenly model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sys
+def exit_program():
+        sys.exit()
