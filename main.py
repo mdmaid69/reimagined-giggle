fe1180a67e45ed7fbd8fe50fac492b0026d487ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:51 +0000

    criticism serious live special impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
