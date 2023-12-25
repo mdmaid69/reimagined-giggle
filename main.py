07220670502c4facb086dfe33318409700aac614
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:53:59 +0000

    by notice should grade relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_min(numbers):
+        return min(numbers)
