5bef79eebf09d5624b98f61f14bd7a7ff4e73285
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:19 +0000

    only zero ordinary quiet agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
