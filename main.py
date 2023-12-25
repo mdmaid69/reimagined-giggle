0ce284e957ea77925274b64ed784919935df0226
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:14 +0000

    disgusting shake contain sticky dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
