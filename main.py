f5f25fd102b8f2ec940ceda9101a40e0042fa4a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:42 +0000

    admire goodbye back figure ball

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
