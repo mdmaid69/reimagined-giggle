7b09ae5f8c244f8fc5803d69dad5bf76694476df
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:17 +0000

    cover wipe fill pocket produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
