af93a17d727a824fe7e993b17dca7f430deab4cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:54 +0000

    belong narrow choice miss drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
