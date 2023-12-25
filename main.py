71a9fd2178aab46a57a77e4d97f56e3400cc19dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:23 +0000

    yet beside where tone compare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
