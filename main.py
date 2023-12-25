c73689764aaeaf1eb81dd04271a0227bf98ce420
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:59:56 +0000

    interfere dress place strike shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
