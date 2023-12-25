9e5955b2cf7a17f0911f82c873778eb3484ab258
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:20 +0000

    petrol organize match trip imagination

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
