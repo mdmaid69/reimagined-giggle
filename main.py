5b08554d13b122dd45d9047747b653bd79f6dcb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:39:36 +0000

    complain sea contents ring protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
