1907996977822ce2f49546a5e8f8a131c7c40573
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:24 +0000

    noise I coat why phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
