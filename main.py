0bd28d05472acbdd49965bac17604d1346ba3a23
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:23 +0000

    entry accept live fast spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
