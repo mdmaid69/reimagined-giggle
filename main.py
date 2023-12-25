664601f52c887e860b4661a1920d9f4d837e2b93
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:26 +0000

    back sore we react reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
