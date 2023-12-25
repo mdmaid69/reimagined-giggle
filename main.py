9642a562950df6cdec3b80eb0a10e6497ec2bc7c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:54:04 +0000

    block gift belief present plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
