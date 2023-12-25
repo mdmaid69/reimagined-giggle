5976ccb46caf26c7f83a88e27a7907337aa3a865
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:41:52 +0000

    hope undo fierce eager Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_perimeter_triangle(a, b, c):
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+  import os
+  def split_path(path):
+        return os.path.split(path)
