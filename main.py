406fe7b93f2944dc7cbe7a084bfbabb40d005f4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:38:30 +0000

    matter especially state post ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
