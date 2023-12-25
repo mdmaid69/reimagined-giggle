0347f8ba89af69c9f33f1db024b2f0e800cf5561
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:14:19 +0000

    crisis record trade never summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
