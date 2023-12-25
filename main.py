1ff4e970a1cfd6616c281bf6d727b99027a04876
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:02 +0000

    material Yy weak fat once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
