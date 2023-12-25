3096a239bbbb05abdc4980223094f508f6575010
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:05 +0000

    mess care share could Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def count_elements(lst):
+        return len(lst)
