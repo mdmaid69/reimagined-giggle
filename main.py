b0f18388fbcaa713abe679cc7ff722bf7951395b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:21:35 +0000

    foreign steal bite however stretch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
