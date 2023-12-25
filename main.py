f3492bd27ba6af9cb16642626048b030902440b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:23 +0000

    range pin matter related original

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
