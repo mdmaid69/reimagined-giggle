df4a79f84e5f1a1f50098af9c6ecb05df7404116
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:30:09 +0000

    square comefrom willing believe rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
