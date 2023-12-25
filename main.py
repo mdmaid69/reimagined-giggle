5fec282ae569d493e673366e9df4ff357ec00175
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:18 +0000

    impressive attempt also health lie

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def is_palindrome(s):
-        return s == s[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def cube_number(x):
+        return x**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
