eee567f1225a8a87580ca5cca23d267e37c3b2b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:01:07 +0000

    disappointed unkind accept receive pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
