81c0717663975446cafab7329d99a29399faeb89
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:33 +0000

    bad scream 179bite satisfied sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
