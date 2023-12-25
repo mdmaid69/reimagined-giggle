7c7a284405663e549e5ae526e329870d8294dff9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:53 +0000

    finish either circumstance trial century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
