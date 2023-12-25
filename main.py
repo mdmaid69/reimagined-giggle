31340d1973c44731884548d9d7c53c31c2fa9394
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:39 +0000

    turninto serious quality cruelty escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_bool(array):
+        return bool(array)
