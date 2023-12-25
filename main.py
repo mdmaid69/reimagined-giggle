2c0fd9fbbaeb6e9c857c13f27a164a52604ad70b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:44 +0000

    whenever stick band bend escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
