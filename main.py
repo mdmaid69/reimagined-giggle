92db19272ca87dedb5f40a44ff09553d1af27e49
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:47 +0000

    breakin such tie birthday die

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_force(mass, acceleration):
+        return mass * acceleration
