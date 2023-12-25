b9bae2c8b7edc359f858c53ea750e1cbbc9617a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:07 +0000

    gift special lookafter depend 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
