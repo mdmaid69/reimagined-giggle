43df382bb359e2ef5d94ef0bb4f88f0c4f74ded6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:08 +0000

    raw disturbing off dress front

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
