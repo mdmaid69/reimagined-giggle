2eb30d06036050ad71b91c795534a06e82dc6d28
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:42:04 +0000

    friend argument short absolutely behave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
