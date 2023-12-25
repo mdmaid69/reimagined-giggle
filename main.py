d0059ec50dd2fa7e9c7120380a69887f6da7bff0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:16 +0000

    subject ignore attract sleep wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
