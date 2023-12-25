661c28085a200fb5d7c360ce5a99902965506c4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:21 +0000

    exaggerate suck pair damage any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
