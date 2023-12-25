cdf960e3b4a032ec3d92f63c8eeb755a63ccfec6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:59 +0000

    programme loose person rarely except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def is_palindrome(s):
+        return s == s[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
