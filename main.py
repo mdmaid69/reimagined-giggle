52e3070491d8b635aa9e1c5b63fb9e2f6657bc4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:50 +0000

    obtain bang suffer how decision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+for i in range(10): print(i)
