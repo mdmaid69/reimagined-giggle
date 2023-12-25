72ac7467aea9e3b2752f302709041f7049f282b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:09:28 +0000

    material broken repeat stare opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
