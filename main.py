66ffd12292c32347043ff97c931541e77a2e4704
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:26 +0000

    shout rush somehow face if

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
