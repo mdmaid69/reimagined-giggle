5e6d17585bbca8e0f3e7fb41547399311004f7a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:03 +0000

    frightened recent journey impressed dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import getpass
+def get_username():
+        return getpass.getuser()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
