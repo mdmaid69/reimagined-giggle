647d1b08bb9b91ad56f2bfccd0232d67f262e82f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:09 +0000

    side contrast turn risk wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
