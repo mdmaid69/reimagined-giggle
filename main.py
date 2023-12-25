2b40c5e31c2fd27f4997cf8e9c0b34ee193110cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:39 +0000

    back once dare onto together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
