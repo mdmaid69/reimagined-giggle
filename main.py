6aab89feb25c270f8ac7bd93f3c6d8de45e4004c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:25 +0000

    Rr job through priority wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
