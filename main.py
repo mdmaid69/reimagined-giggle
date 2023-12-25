a5db0014abaaed47ddeff3cb7f2807e5baadc0d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:28:58 +0000

    estimate post little force past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
