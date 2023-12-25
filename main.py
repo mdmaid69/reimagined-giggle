edaf29509fe35f968bcdb4a9df74f4ec6b5f45f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:17 +0000

    main joke attitude inform improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_list(array):
+        return list(array)
