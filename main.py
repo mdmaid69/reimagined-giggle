ae14ac91a6317007b8a699ef5cddf56afba6c5d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:14 +0000

    complete comeover invitation win hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
