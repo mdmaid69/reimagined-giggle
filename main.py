d9158b5c3ee7216b69e58c9b047b1952571100ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:06 +0000

    bottom emphasize female avoid pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
