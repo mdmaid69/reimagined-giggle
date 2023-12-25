b08b52154fc1be1887a19963ea325ae3bbb83625
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:28 +0000

    welcome comeround young study zero

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
