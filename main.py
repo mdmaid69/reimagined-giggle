2d6ba05345ba776fe576d1c03a04237cd82e1bb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:40 +0000

    ready sound use upwards side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
