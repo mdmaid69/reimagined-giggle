abf4e8328764b118e84a50203b19251bad72d10d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:22 +0000

    that guess judge reliable figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
