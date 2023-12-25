fae075ed43dbf2e3713543ef8ed3a8bed9a0c61d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:40:26 +0000

    wish Ww by hurt strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_force(mass, acceleration):
+        return mass * acceleration
