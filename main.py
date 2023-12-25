28a1ee23edefba8cf4a3ef6e253197b7475fc024
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:40 +0000

    drop eat dull blue hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
