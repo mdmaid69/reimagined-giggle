3d2ac4503dc3a2454aeb44568928fc44fbbe2dd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:36 +0000

    together stamp thumb wonder birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
