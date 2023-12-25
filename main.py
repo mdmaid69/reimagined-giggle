dbe767eef4c8f4042dd60091e4337886b1456d8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:05 +0000

    ill steep live inform slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
