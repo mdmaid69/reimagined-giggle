580ab2cd121ff7a0d921f97a2e4362af1faf6f56
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:09:41 +0000

    present thirsty in difficulty feed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
