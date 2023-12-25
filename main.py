a2a635491a0a3b797587463555e9508a224cb245
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:50 +0000

    scene exact wealth exist part

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
