3ef236b0f11d8fd182a057bbc517490bcb6507b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:59:39 +0000

    name joke duty fear argue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
