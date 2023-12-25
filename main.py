a5292cd241c3efccf7b407dd307b1e3267e3b794
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:39 +0000

    whenever rest close rescue Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
