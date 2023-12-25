79187cc7f4f4e846573b368f4c98ffdd6efb8c69
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:53 +0000

    charge sharp way recently street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_memoryview(array):
+        return memoryview(array)
