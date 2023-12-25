d66d67f11031207613f66df0066c77556701d096
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:17 +0000

    operate responsible yes her steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
