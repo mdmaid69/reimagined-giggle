c06ccfc9e82c381885fffc4e5cf20c536b75a0a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:38 +0000

    high cope road basis judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import tensorflow as tf
+print(tf.__version__)
