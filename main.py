0b40597f3dca66eab93d14d2985da672705b5a0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:36 +0000

    praise work behaviour break least

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
