e0b080ad13e1f6bae1244fcf81861148780cbc14
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:17:16 +0000

    downstairs terrible payment afternoon flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
