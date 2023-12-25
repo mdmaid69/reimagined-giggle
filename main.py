c3de606f79c02ac79053df04d56eb7b32efa250d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:51 +0000

    gradually myself live sign day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
