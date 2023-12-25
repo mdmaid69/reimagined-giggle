79110f2ad8b2d51ff994c583f997bf73b3371a3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:01:44 +0000

    high react knowledge force plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
