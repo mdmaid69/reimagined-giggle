16a4cd87b973b867a1f0517bec726d7050ec6644
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:55:46 +0000

    true however measure promise yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
