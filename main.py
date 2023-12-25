b295d7ce5181cd3ac679feab3ad9ebb2d45f12b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:42:08 +0000

    warning meaning lean get warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
