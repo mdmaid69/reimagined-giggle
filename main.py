f525879bd4ad987c6c2b0837ee2a157a069eafd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:35 +0000

    flood word friendly rest nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
