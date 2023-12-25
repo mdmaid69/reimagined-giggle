45bd2de23ea2f265e582824865101e5eec79c3fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:06 +0000

    impressed project fall press refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
