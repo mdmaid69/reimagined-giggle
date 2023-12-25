4cb933e200b2ba23afd1a3a47d4709f5edc265ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:09 +0000

    hour thick turninto pocket callat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
