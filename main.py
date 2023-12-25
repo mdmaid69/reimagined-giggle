625e2a4d3fde6fcbf4fe996b8103d4ab9569fcfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:43:38 +0000

    without boat height count of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
