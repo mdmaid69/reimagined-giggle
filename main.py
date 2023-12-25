dd62cc6f7f04d7b7fc779888455b3cfd4531ae41
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:48 +0000

    ride transport come perfect push

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from collections import Counter
-print(Counter("hello world"))
+def convert_to_hex(n):
+        return hex(n)
+def is_odd(n):
+        return n % 2 != 0
