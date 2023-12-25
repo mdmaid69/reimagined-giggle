09c910edf5cdc7c5dfe3aadcda8d90a4c912906c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:28 +0000

    neat tradition equal meet eat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
