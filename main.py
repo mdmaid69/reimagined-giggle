53481e3f80576917bfec2990114b7c82f4630aef
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:05 +0000

    hand fetch class ocean knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
