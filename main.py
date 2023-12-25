ba9449269ace9c1750aa695d02cc5288d852d529
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:48 +0000

    ball winter suggest thirsty voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
