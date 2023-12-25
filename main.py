c87e41741f69a7aa48967b83242527d4a37a17a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:32 +0000

    exaggerate disturbing ability responsible yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
