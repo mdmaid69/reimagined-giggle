40f2b092f41462ec5616d64eb5bf30d13a1352c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:44:05 +0000

    relative water perfect over typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_int(array):
+        return int(array[0])
