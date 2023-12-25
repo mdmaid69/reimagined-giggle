34035078f3dc66832b98c93563a2603608000fa2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:32:09 +0000

    experiment difficult blame talkinto injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def multiply_numbers(x, y):
+        return x * y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
