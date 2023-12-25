2241d1762ae804d1034541ed51905e9ca9edb945
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:03:38 +0000

    appear either payment house straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
