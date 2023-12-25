15aff5171cb66fd904e3412c64efd584e32c338b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:25 +0000

    medium joke turnup unfortunate permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
