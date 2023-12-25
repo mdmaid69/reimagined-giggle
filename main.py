9deb435dd50c75acfa4ad13fe6082238c2b5a4a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:15 +0000

    secret congratulate would left crowded

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
