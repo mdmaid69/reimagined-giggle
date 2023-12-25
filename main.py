beb38809f8ca457694d81a70c09cad817578551b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:31 +0000

    journey consist doubt computer part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+print(os.getcwd())
