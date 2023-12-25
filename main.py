1acc2af7da969c4b6d14a85fcb47b12b284c344a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:28 +0000

    mean force half opposite bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
