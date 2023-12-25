7b04162ef242c90cf9decbb69e0849ba28519629
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:42 +0000

    area Ii trick teach further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
