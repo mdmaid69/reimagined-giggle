c29911078456c39644936d045b8bb6ad4e193ae0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:38:38 +0000

    offence learn loss equal worth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
