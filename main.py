f0c0523687ffce0b52f4088a6bf2f23757aebd70
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:46 +0000

    near breakthrough condition fix best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
