05437e680c3e84f72e0899b66c6c492a078bc594
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:25:41 +0000

    fork maximum blame violent activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
