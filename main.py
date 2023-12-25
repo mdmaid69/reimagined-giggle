4288e8c026a418df03d6716ce35ec43dd7d59245
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:26 +0000

    write mess name proud until

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
