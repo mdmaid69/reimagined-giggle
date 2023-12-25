8addf4259fe7f28488719dc0475825e00aa17870
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:14 +0000

    crisis advise shallow similar seriously

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
