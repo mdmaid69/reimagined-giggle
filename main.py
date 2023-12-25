cff6c217277df3e9b2d8021d3d209c1d562e582f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:40:34 +0000

    sleep through inquire century beyond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def add_numbers(a, b):
-        return a + b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
