6d6282822e0274f14c3c94dd312d8f1a2349d03a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:01:01 +0000

    among beat suit note strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
