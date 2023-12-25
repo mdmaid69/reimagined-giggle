141443c0dfc06d53fbb31a7971f271cf7b7f2d28
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:31 +0000

    fact release complaint accommodation shut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
