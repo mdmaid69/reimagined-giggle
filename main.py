6a36258b8337479459dcbf83601b42ef2d02022a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:21 +0000

    environment contrast first form after

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
