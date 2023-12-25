2944ac06b91cd758db84ab6e0f3c4fc9abe8502b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:53 +0000

    reliable middle lot our sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
