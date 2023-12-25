1c5c8a4cbbd3b445a909425ea439f8d7d6a18c7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:54:09 +0000

    comeround confusing own meeting pool

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_work(force, distance):
-        return force * distance
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
