2b3d335313fefaecd337c8b21e7381d1ce002a9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:07 +0000

    thick ordinary favourite wait many

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def count_words(sentence):
+        return len(sentence.split())
