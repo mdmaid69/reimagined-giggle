c75b4e18a93b8706c323fea1be780f2720f61f1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:19 +0000

    slight snow smooth up apparently

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+print("Hello, world!")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
