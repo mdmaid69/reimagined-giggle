74b53ae3579cc1000b69ef44db46e29ba1bd819c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:53 +0000

    target high call wing nothing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
