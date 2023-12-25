f6b8697a4a7789cb0794e769f5e03f49a524f8e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:25 +0000

    clear unfair own suppose no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def cube_number(x):
-        return x**3
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
