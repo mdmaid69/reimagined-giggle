4f503c9b90c859e5a01a4b8ee87bd24476772501
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:28 +0000

    fire passenger sort obtain development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
