ac265533385b98bdb933fc227186451042483ac9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:44:59 +0000

    treatment success judge emphasis risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_acceleration(speed, time):
+        return speed / time
