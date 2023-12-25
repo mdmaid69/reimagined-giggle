1bc0950409221f62e82c688b4f949cc296a386a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:05 +0000

    sail live common history unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_set(array):
+        return set(array)
