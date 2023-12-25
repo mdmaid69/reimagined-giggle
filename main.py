ba199f84827887dcc1c494c9ea89b42ca64f1753
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:31 +0000

    OKorokay equal cut contrast godown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
