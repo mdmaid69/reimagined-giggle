ce2228154fa0b49ae47734cd514ed59e03ffdeca
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:36 +0000

    convincing affect still careless Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
