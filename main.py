f5b13a1929c2133b09a74ac93b82a48b88ef2239
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:11 +0000

    disappointed neither comment praise choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
