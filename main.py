db51a1c1790b290dd4e50856d39367e85cee67ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:50 +0000

    chance no cope path arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_list(array):
+        return list(array)
