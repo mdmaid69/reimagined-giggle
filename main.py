d4fee8686c290d926d2f959f7e4f9defc88d7ebf
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:03 +0000

    work key trip light capital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_power(work, time):
-        return work / time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
