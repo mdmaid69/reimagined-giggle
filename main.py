9d19d0b3915e4a5ad1924492afa7477f583a146e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:54:10 +0000

    fond ourselves well capital sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
