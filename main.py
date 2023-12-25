9708c41ba6eed2e8543eed5b5a14a1b88774b7c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:58:02 +0000

    declaration mass profit so cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
