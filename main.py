3f3786e50af515ad6f7d00fa87468870f350c7d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:42 +0000

    remark spot upstairs comfortable bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
