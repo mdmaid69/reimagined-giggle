b4ed7959a89b70fe7f2e5e58db84f031e71d7b5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:50:28 +0000

    exit love sorry keepoff hope

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
