9007c610dd96f846ff9414158e75d2f3d294cc12
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:58:43 +0000

    sound police correct frighten sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
