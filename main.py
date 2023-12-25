bb55581d1bb3adc80bbd3295b5760b7d758c6843
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:55 +0000

    agree contents belief model car

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
