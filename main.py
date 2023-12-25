06baf3bd7f68d31371b1a2c6a1cd1fb60eba2f9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:19 +0000

    Ss consider break against bottom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
