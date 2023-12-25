d78c913bb5977fff31075d8d609707c33cb0312d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:40 +0000

    certainly achievement tense giveback keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
