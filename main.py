45501426a02df88247ed86a7ec42f20ea6d034a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:51 +0000

    throw bringback work turnon blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
