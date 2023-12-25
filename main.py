9b3de36cfa086425305c1f1b0c07e2badb53a73d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:00 +0000

    poison interrupt interfere nose load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_power(work, time):
+        return work / time
