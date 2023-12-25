aa234e869a907f9384ee1cb92a426db130f5ccfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:48 +0000

    warn last precise age letter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
