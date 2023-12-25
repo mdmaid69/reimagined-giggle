235270d0fbaacb56fc70c1be929d723cb788bb73
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:00:22 +0000

    everything prevent someone stage underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
