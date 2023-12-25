5e34c5782788c147d0b7511a97c2581dd904845a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:40:45 +0000

    actual blue escape ability praise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def convert_array_to_string(array):
+        return array.tostring()
