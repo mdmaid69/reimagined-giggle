0ed16bfdada0742eea3c6e0d1b442e2892e94fc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:11 +0000

    join happen relief compete alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
