99f46d2ca18f5fb8e4e1408df9607a604e989fcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:22:57 +0000

    fierce race fit offence obvious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
