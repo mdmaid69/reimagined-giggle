03a6e835a047854373104934004950443c7bad62
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:09 +0000

    probably steal very confused each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
