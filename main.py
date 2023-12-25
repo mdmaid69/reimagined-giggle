15f66a9ec531caf5bad3fcc176c78b039cd42417
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:42:39 +0000

    broken classroom team Jj need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
