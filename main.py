f6c212a3bf559cdaef03f38720279538273cd082
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:09:25 +0000

    develop Ss raw that mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
