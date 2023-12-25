a60a480e3287677c8446bfef824e640619f38f70
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:35 +0000

    ride wheel fear stay show

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
         return None
