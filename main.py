228523c628e701a0b746d09e54ee8a33705199cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:01:13 +0000

    eye dry sister convincing obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_min(numbers):
+        return min(numbers)
