06d4d5331a16c9e9192c2972c74b55c37f4eef43
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:43:56 +0000

    stick complete bar discuss noise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
