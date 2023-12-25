04a84f60407d5bb8de39b5112a9296deca609c8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:40 +0000

    pride gap health mail food

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
