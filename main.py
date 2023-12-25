d197cb5367661442a1b5d05b2e78db0047558845
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:24:54 +0000

    one trick crisis acceptable shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
