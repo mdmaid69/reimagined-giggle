86d79f8adc2e2a3b5ffa8b86c7009c2a9c0db3d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:42:24 +0000

    block concerning bet rubbish shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
