1fe32aa256d740246b7abb3d64e16eff1fa21bde
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:07 +0000

    stop funny advanced shade estimate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
