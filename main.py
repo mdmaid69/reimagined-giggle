8b1887d5d2e09fb1c814f711f7c668fb40d0822a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:58 +0000

    male direct meanwhile sail continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
