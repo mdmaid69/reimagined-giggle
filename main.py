c555b1043d9dee8abcb49271f0207d33c8dad25c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:51 +0000

    mix typical comeout Mm method

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
