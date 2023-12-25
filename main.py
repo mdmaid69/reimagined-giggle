78efa7701d7bb45b93d3b0b84041820743505641
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:21 +0000

    sleep block calmly taste little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
