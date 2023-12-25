46d1a931a0b3cff44c8ad09441fecd45935569f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:52 +0000

    race special attempt excellent reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
