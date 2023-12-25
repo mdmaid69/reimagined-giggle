415787a9b4ad7b121215296135bafc4852f0fd42
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:26:48 +0000

    hate goout anywhere aim dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
