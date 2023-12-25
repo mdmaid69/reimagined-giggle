5b59042a037ba4704d9ed59d0d8dd4bea61b91d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:46 +0000

    estimate pupil reduction there name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import time
-print(time.time())
+def get_current_time():
+        return time.time()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
