21ed9c1f748cd7f993206e2dcebaf94aadc52595
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:40 +0000

    road control where perform bar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
+def calculate_work(force, distance):
+        return force * distance
