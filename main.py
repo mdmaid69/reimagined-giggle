181ce912e45915d17bd94a126b07b06fee461b9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:42:22 +0000

    immediately frighten war production day

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
