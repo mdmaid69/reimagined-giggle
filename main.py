c012069af12e8019a6b9f645230270d75f1b9318
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:55:35 +0000

    light rob sort over hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
