475dc499e8b43de9c10c05e2ca8db13dff4b228f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:59 +0000

    of crisis material cap stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
