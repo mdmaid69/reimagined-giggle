a1ca46276d2a9ab335c71676070ac26a7a28f4d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:16 +0000

    569enjoy afterwards pretty Ww man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
