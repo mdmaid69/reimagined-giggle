27b3444ebc8b655d5beb64d6401d54d4337e937b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:33 +0000

    heat style Rr congratulate square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
