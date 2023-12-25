743bd278f304d560d358cebafd59d3a5afe5bd36
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:25 +0000

    central confuse chin floor recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def clear_array(array):
+        array *= 0
+def calculate_force(mass, acceleration):
+        return mass * acceleration
