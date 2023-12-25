acb929156cbb85c1b516b8a72e3127224146d6f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:35 +0000

    comfort holiday trousers port injury

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
