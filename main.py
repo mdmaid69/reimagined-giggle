d5311c49910849a8b7d1d9a977173a75a7517118
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:01:28 +0000

    Bb say means holdup alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
