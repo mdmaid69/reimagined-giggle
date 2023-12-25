73a9613dd8bd81db9ca4e72ff12503acaa33725e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:33:48 +0000

    far statement available chance level

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
