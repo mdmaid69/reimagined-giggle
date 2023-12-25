d4e7f9bed676b44f721317980513dba3a8e0e901
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:18:59 +0000

    feed invite offend available child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
