a1e4fffdac43f437c3d2ba68513758fe439c6674
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:22 +0000

    switch their escape picture much

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def sort_numbers(numbers):
+        return sorted(numbers)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
