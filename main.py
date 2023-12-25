cb0599a97aab532f09117a0c6dc4ffa699f0d2fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:05 +0000

    war true petrol brush round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
