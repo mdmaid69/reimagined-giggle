e02857caf1ee4fb37187737f21e0c5fc61fa24b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:28:08 +0000

    sometimes sign thirsty backwards ordinary

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
