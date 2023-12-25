a71a084cb481593b2cefe316a284dc3b96e9466d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:39 +0000

    beginning offence reserve raise convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_list(array):
+        return array.tolist()
