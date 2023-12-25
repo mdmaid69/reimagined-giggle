4b9d2a36c3eb79dac4fd719229a9e47f5f19c9f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:47:34 +0000

    answer recognize fashion worth satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
