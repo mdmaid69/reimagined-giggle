20ab7c55d3e64972e464c59e915bea931b7e5b5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:21 +0000

    ignore soil getback evening truth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
