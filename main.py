d05126d0a627b4f1867ff72f250d3bfb8683a6c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:45:05 +0000

    in spin comeup perform project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_volume(length, width, height):
+        return length * width * height
