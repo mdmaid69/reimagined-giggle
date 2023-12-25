26de6dc9005f9e70fb487ee7eec7e405356fa26e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:36 +0000

    seat opponent check exhibition but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
