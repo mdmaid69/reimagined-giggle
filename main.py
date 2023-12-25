36138b9925461bc00d6efd76a7f2392fef471ae6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:08 +0000

    maximum search capital tread compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_floor(x):
+        return math.floor(x)
