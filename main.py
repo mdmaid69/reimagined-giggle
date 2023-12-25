50953fd2daf35bd96807316cf71e0872ca87bf42
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:05 +0000

    trick imagination concerned bottom actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import http.server
 def start_http_server(port):
         http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
