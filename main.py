5817ade4605a57330d36401b31b0ed7767259666
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:42 +0000

    push several careless stage somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
