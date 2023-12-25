4146ab5ecf7aec45f9c8dd589c4229238fe95ffd
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:30 +0000

    possibility sharp manner road previous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
