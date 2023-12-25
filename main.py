06a58b53e579e08165128102a5eecefbd8aeb501
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:03 +0000

    mail original judge declare raw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_min(numbers):
+        return min(numbers)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
