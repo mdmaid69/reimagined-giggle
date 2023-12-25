07b9f18b99ce7e5b237e1126d02ff6adff31ab56
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:56 +0000

    best load ahead regional precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
