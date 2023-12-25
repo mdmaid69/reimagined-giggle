a57a8a9dbde5761b6e3dc472c8bbc1a86a00143b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:38 +0000

    Pp crowd powerful spring float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
