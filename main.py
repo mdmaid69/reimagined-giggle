a4e9e510dce736f654461b02734de7563a66e63a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:02 +0000

    many pretend rank spite complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
