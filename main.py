40230792907a83a3b0bef516518f2ad4fbc01ec8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:15 +0000

    describe interview interested fix breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
