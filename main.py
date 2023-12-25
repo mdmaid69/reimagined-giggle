3b83f33c3777da7a3640ebc53060eb9ffafa2a4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:48:00 +0000

    like importance getout immediate chat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
