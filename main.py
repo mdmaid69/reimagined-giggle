0b7f9f4eb745ca19e3f702045986cdd9e0ca11ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:35 +0000

    round Itself modern complaint swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
