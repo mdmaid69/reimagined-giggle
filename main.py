6a5ba3e51db10096642714b72fc518755b498c44
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:05 +0000

    hope narrow exercise combine steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
