8ba240628b7e13c1a2ebe41b02e72e35d82de985
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:24 +0000

    computer of responsibility switch clear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
