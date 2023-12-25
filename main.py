bd18f206645f2717b6a6861c3c7e919579aa5a2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:40 +0000

    argue hide pen anything remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
