1b899c385a7d781cc7396b95bdbe1753c6e205b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:29 +0000

    experiment machine children legal fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
