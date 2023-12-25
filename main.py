bb64bd2442a19d67c237fb9d52cc020889e07102
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:46 +0000

    ready big willing slide prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
