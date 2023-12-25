9186dbe3874fd1a69ab1d993e4f405e367ae65dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:54 +0000

    list slowly pupil shrink past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
