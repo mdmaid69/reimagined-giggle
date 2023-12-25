eb4cc708dc304ccdcf5dae2ed0923ba521a12271
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:10 +0000

    might purpose dick evidence workupto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_area_circle(r):
+        return 3.14 * r**2
