42c7c3ddc9e56be3a3d85f34e1975cb42e6474a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:24 +0000

    but continue degree frighten hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
