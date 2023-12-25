aaa3969d1d75b1d56bf2dddd5f85bcf2c949f6e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:19:16 +0000

    deep best test refuse respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
