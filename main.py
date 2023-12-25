bf0925821da14301eb633afe9bd908a1902ae991
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:04:47 +0000

    exchange friendly apart also Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
