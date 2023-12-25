9b498347def6085a5e78abd719445dabeb5934da
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:54 +0000

    shelter cruel comein middle entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
