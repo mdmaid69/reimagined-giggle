06c3c476cf17061d6ca039dbeb6147ae0f430043
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:13 +0000

    tradition together pair whistle cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import time
-print(time.time())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
