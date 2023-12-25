472d3fb3c385906ea40260f7a5be94cf7838888b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:42 +0000

    major large curious claim until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
