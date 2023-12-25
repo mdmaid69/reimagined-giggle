1adf860c2862f7d9c03af7f19bba3780afb165e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:26:02 +0000

    geton drink label store loose

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
