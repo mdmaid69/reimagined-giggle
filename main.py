3f8d0902e69bf7540f6f8ca5b231b70c5d9c0549
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:54:38 +0000

    crazy course downstairs Ww attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
