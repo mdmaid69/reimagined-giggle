7c29e67c7a931446c9198ef72b69399dad9ad41d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:45 +0000

    speak finally race interesting course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
