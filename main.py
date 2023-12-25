2ae0520f6c47c36339f76e4d08b4742fa41e2ebf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:16 +0000

    sure guess nervous absolutely culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
