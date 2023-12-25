61b8ada9306f6e5a79fa3b1b32d2a2ca5c12651d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:49:53 +0000

    lift sort ground ride calmly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
