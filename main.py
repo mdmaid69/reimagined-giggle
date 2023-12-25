73fa18d7bd3466c14b3f7fa0c2b40107022fd4c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:58 +0000

    cope arm food anybody beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
