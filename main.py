4d5686d28f613c48cc6028ce5d46937b4ee8c2d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:36 +0000

    568energy prepare father down push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
