784c26053a1be9b10742e0fd1af5971e287a049b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:02:52 +0000

    breath confused before die murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
