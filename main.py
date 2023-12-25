fc8d40e9c9db1edc0606f4aff606adc5d73a1fd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:14:07 +0000

    beneath memory impossible praise upwards

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
