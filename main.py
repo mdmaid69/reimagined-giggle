ddfd2eb6dc7541f59cae27fbb94d193c01fb0f02
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:18 +0000

    wipe level vote brief bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
