e7612eb53b3b905db064a3baabe0dc04f7716e57
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:49 +0000

    previous second beneath black right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import platform
+def get_python_version():
+        return platform.python_version()
