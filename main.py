37c1b0aae051dd248e5975269fbd7c34532ae2ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:39 +0000

    everybody black bath wide congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
