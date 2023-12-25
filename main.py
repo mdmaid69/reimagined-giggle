9aea9cfbf179e6e44ca8a87c5000c22c3df73396
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:56 +0000

    excuse worth grateful hard here

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_max(lst):
-        return max(lst)
+def calculate_square_root(x):
+        return math.sqrt(x)
