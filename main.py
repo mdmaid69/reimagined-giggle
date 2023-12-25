342679eca727bd8a88d6f9ce0393afe064982f64
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:22 +0000

    me frequent seat occasional active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import datetime
+print(datetime.datetime.now())
