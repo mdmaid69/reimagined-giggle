7987d63c8cf2019553adab68072e9ec8265e34ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:15 +0000

    protest office let exploration slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
