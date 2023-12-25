f86a88296e7e965b1df9589365c38dc2ad198064
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:46:06 +0000

    size port innocent hit flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
