43a7e400afb5c0a579a0890052615e4374281aa7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:12 +0000

    plan near breathe wonder skin

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
