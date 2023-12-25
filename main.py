36bccb41e669460552e28875e06c2b59023e307b
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:52 +0000

    cheque guess cold communicate round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_max(numbers):
-        return max(numbers)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def is_odd(n):
+        return n % 2 != 0
