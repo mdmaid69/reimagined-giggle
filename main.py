6d7af0b5d75bd889a2d91aa3c5e1d2399edd7087
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:10 +0000

    left study form situation unit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
