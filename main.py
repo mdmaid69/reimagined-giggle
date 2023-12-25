13ce242655f6b2e84e71e419f0a941733f146edb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:13 +0000

    meaning fall sound dream movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
