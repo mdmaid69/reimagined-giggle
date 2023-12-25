698544f1b69f534bc1d36aa34639f591f73956da
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:05 +0000

    crash normal call will how

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
