4d7c4957294c142c5dbff5f92828d6a5464e8d01
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:01 +0000

    opposite complaint more experiment achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
