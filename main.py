79f9d416dd6fbdc1a074c905233f77b43e038d65
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:15 +0000

    downstairs too late giveout only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
