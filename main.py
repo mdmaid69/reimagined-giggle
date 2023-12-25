8f0c3f2aa55b2f2480ce84846e14f0f88a2d985e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:10 +0000

    clearly information come break between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
