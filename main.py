de15c25824742220166d690d71f3edf857d5dc71
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:08 +0000

    bill performance whether deep discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import datetime
-print(datetime.datetime.now())
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
