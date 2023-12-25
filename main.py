e28bc69507ff410f1902aceae47f37ff9bf88156
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:36 +0000

    raise Ww accept Gg judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
