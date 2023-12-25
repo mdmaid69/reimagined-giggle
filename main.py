5b2ee4bb2c09b6520d2f82a4e2d9060943cfe377
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:06 +0000

    sell send address listen rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_array(array):
+        array.reverse()
