7f2190471cd1915916737d7d07eac64d713e3a3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:56 +0000

    style mistake deep 564encourage sing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
