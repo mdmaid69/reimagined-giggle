05614eae8090081792806b1f96ac3ce7f9237512
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:37 +0000

    snow cough violence control too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
