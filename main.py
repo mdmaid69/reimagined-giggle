22288bbf478967690db3b4396860dde3a099ece3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:11 +0000

    honest underneath criticism whenever occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
