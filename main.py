0d14cc59d95e669b7ce082783032c84f3e987cf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:32 +0000

    receive negative motion declare but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
