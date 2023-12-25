e010e71d54439105fb81fa5dc18dbdd51786fe65
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:26 +0000

    rather holdup careless trousers noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import random
   def generate_random_number(start, end):
         return random.randint(start, end)
-def convert_to_octal(n):
-        return oct(n)
