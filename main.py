9c0560f01a31d76405d5431187e1adacf2139391
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:48 +0000

    draw shrink address hard muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
