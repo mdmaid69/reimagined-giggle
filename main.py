49e5321c9dd1b887ac9ccf7f5b652d2f7f894044
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:10 +0000

    care correction workout authority anything

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
