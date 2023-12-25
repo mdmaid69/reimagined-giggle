fbe1001b8cc477dac5884f2ec0e2991dfb03a478
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:05 +0000

    fall value at light card

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_itemsize(array):
+        return array.itemsize
