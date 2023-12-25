b77f96d242fa36b2b1a581e6182eb7ef4e7481cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:24 +0000

    big so food his glad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def find_min(lst):
+        return min(lst)
