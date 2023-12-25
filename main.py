d56b5e4788b8665798c6b36a39843e7f69acc1c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:17 +0000

    on uniform although neither intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_set(array):
+        return set(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
