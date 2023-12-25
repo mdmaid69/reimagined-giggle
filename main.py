df09b6b5cfd93439a8993f39c89389b6edeab50c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:27 +0000

    answer godown OKorokay set beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
