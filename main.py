3381a22ceeda24a6727f03b26df591710dd7488c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:09 +0000

    aim stop earn rubbish excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
