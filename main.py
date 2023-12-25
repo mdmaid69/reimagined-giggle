0cfb4e191fdb29ec1f8b479df9079e3167d7ca6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:20 +0000

    hard damage measure test or

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
