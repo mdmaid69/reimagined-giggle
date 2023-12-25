9b16fd76d41153d2e04f73ab39a4a02b9dc3d4d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:01:46 +0000

    opposite claim sentence town now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
