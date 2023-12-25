6430bafbbb9d24a077b862d8ec9758ec704aca5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:26 +0000

    female size effect sensitive injury

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
