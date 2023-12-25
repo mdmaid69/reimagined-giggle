38f868b4d68f8396dda3ea67e87538fe501a3dbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:23:47 +0000

    go down call provide so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
