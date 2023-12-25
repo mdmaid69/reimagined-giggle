c59403f1d24ed93c23bbebd3b514a300586743e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:59:28 +0000

    top tool likely pair yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_current_working_directory():
+        return os.getcwd()
