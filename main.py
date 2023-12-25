09f6749ff15e924452872b8feeadef374ed9264e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:09 +0000

    watch moral slow region party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def subtract_numbers(x, y):
+        return x - y
