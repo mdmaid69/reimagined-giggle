4e81c470b7c7ee8116df7d7f4cf12763db4c9f69
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:30:19 +0000

    laugh switch danger take spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def convert_to_octal(n):
+        return oct(n)
