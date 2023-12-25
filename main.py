0aa4e3e1de03aba4ce7698bfc5a57c34f10607c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:24 +0000

    large thinkover community cut offer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sys
-print(sys.version)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
