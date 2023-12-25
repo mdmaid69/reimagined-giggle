dda7d234b14b57837c444bc1c108eeaa5737ed06
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:42 +0000

    breakthrough give relation compare agreement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
