1a237688c0a7c5ff972c77f0cbf2c8efe31bfa35
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:07:43 +0000

    true murder female innocent burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
