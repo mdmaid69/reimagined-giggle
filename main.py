93d4df4554fd4333075f4c85b26d68c677ad197c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:36 +0000

    glass lie downstairs broad knock

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import logging
-def log_message(message):
-        logging.info(message)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
