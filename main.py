7f32c59f2d6b60442405821a433e74b12bc7f0ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:15 +0000

    true paper yes separate eat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
