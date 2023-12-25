f985817dbe3c56f79c02b7278a8132bed08bc8f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:46 +0000

    rank any fit pin slightly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_user_dict():
+        return collections.UserDict()
