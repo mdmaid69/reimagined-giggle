699c5d53d68046cdfbb0413d9b09c16001cc83cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:19:26 +0000

    shock thinkover surface punishment mystery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
