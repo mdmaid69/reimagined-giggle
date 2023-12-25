fa334f3221ce356af1230586320f4771466ac625
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:11 +0000

    comment copy command strike trust

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
