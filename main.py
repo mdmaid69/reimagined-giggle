bfefa349ab1611123f3f04b86cd1c16ea3199faa
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:08 +0000

    ready recover interruption position branch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
