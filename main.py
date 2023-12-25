7f1f2086a321ec7d46622a91ac098f565ee563f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:49 +0000

    escape trust clue nurse lookup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
