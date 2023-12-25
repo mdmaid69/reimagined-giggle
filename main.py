9f4f0d1ab00cd24b700e9a251e50022c16900fcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:05 +0000

    inform face offer except upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
