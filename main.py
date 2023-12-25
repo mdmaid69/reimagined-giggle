9996f1c150a73a67eb83fefe2c0bc7b915bf833a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:25 +0000

    hers every bridge term demand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
