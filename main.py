158d4bed11275f633a879df658baba2fa2425173
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:40:31 +0000

    successful convinced level maintain defeat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
