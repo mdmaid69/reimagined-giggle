80a42e0b2c9b171a11dd57d1eeda62f4647dc8d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:53 +0000

    join aware slice rest Mm

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def divide_numbers(x, y):
+        return x / y
+def find_min(lst):
+        return min(lst)
