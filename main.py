d6cec9556623b759da2db2ff17815765d25a7393
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:13 +0000

    frighten room next apart speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
