3e80fbf28df836999f8489902965a2d8f3f479bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:42 +0000

    not quarter getoff plate guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
