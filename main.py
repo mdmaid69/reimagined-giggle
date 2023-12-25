7c78506f2e6938efb56383aa18e65cb07f46625f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:19 +0000

    nothing wealth word brother wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
