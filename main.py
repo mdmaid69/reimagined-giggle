b74d32d28b953e519e07d23cf8cb938aa5d41726
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:05 +0000

    tell adjective dark square suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
