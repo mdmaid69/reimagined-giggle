e0d47009c544a3b96eec921f09bd989604473524
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:08 +0000

    willing impressive bottom well new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_pressure(force, area):
+        return force / area
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
