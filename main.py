a6f8977417d63f504cab7f44b0735a2040c8dc57
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:14 +0000

    plenty warn would join bear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
