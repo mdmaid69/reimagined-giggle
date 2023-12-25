cdb5aadc04453406c4a5c2233045b6a73109259f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:01:43 +0000

    note occur operation pin negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
