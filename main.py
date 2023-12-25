d0ca7431a7d8cb0b6847ea3f52c1d1d96e42c768
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:10 +0000

    get account branch dinner target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
