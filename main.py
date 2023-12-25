ca621b14229e610fe00d0c1e791d8197e82c5eed
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:29:07 +0000

    deliver breakthrough possible that effort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
