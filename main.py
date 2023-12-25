1df5db82062a86cc500a8ca60d6cb5b3d9e50b50
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:00 +0000

    such path them slice Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def cube_number(x):
+        return x**3
