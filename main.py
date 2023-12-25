6276b137ce3f675bbfc1cd8ae094dee42e514646
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:35:33 +0000

    little tap visit beat party

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
