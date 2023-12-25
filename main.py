a39af4ac963c56f4e4dcf33a7355b8762e878ce7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:20 +0000

    turn touch concerned slight mixture

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+print(sum(range(10)))
