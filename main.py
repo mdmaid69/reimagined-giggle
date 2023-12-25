d30bdc4467a873dc73d847ccd6c56bf1607e25f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:02 +0000

    description rare by flood risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
