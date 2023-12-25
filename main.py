daf4b9153dc743995596afd68d1d316db1b0a261
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:04:29 +0000

    hope reply comeback balance chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
