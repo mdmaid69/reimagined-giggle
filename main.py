6e79e630a2fbc76d22ac3342a602f0589b13b319
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:30:51 +0000

    report mine mind supply winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
