37e401b529be681d39d8c54a4e1fdeb32701fad3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:59 +0000

    impossible aim cry practice definite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
