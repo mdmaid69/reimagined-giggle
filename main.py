b3eabef7408c3b20bb8da05e2b1abecebbaaa358
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:50 +0000

    education climb month hurt centre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_max(numbers):
+        return max(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
