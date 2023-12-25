ccc9b93a99b39a3cac07aeac027e45826e4b26c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:27 +0000

    search news degree film position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
