4063d0d464acc233141bfa8b23c40a215daa33b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:22:58 +0000

    mess search prove your police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_length(array):
+        return len(array)
