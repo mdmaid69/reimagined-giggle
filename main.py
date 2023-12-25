ad9ef3431c38cc86112297f3b5a4569054d70506
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:48 +0000

    ilovedicks wear oil front actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
