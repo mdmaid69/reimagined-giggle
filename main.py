523711a9f854e02eb9ff90f3247c8ed34f09ca73
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:51 +0000

    laugh pool estimate go estimate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
