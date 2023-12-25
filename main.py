6f4379bc0dae12068e75cb12b78b3c87be65e6ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:53 +0000

    interview show judge sensitive cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_square_root(x):
+        return math.sqrt(x)
