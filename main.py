ce742ac2094284648ea73218f214e363cbe022c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:10 +0000

    single smell cover equal will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
