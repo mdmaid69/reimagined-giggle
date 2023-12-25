e407eb3dca3d1efe4cbd6845b32bfb2517cb4401
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:35:07 +0000

    afraid keepfrom calm settle lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
