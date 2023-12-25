b26fd6feaa35ebaf2a5e05bf1e7f00cea5fbf037
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:30 +0000

    measure make familiar justice imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
