b9ef2bcec51d8c916662de5e01936c23a5bdad9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:16 +0000

    soak heat damage flow peaceful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
