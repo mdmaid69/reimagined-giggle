566a539b044615b5c98732882b003853dd759391
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:02 +0000

    indeed in cum computer bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
