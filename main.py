9ad02d5275f88ea1d118e31030d6f63361af9fc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:31 +0000

    cry post claim exploration skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
