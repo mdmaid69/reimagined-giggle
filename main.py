3698915dc6ddec3703a82a8dc4c491d4d1e59b44
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:18:03 +0000

    flow history trouble side probably

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def is_odd(n):
-        return n % 2 != 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
