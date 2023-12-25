b38fd38a7ddcc293781e289b2d9d1e55573290bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:49 +0000

    choose big Kk few history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
