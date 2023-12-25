710fbbd74cb43e18b384352aa6da3e25f110f052
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:26 +0000

    morning small justice carry black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def count_words(sentence):
+        return len(sentence.split())
