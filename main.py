cade9e1c73cdf490be14c8272447069c45c8eb92
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:21:50 +0000

    increase favour brief tomorrow complete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
