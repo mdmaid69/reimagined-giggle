d52680c5c90b2a5eea4c0ac7d42d406f8007f0f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:41 +0000

    lookafter meet oil hide violence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
