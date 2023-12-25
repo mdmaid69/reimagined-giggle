6889fa12a33b6a57217c3874c7e7ed34ab4cd1a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:28:55 +0000

    claim appeal makeup exam getoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
