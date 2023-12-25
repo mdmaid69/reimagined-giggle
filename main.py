1ba6184d0129cd49ac3327cbd7bf7a4fba30915f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:21 +0000

    relief comeoff basis soft appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def sort_list(lst):
+        return sorted(lst)
