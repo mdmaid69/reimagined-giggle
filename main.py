b98dca91eb74d27936177beec9ff28d6955137b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:31:33 +0000

    chat impressed able can lie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
