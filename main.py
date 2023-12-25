0e60752bcede5713803d8fc2d38ae6bf03cad413
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:36 +0000

    explain concentration hit preparation aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
