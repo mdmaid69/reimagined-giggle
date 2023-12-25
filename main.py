efc069504824c5a51c0a621a31d630553bc7558f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:26 +0000

    crowded near raise definite unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
