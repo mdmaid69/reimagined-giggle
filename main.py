cbec360a3fb0c6c3d48578803e206c02821fec87
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:37 +0000

    blame fetch century clue drink

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
