93c202bf1aa2d4d3c85ada620c85a64acd0255eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:37 +0000

    shine sign somehow rich appear

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
