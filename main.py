d802ac26159a3d38ba355f2d0a8d532501637edc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:00:56 +0000

    hand support delicate spend hers

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import random
-def roll_die():
-        return random.randint(1, 6)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
