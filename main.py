1415f1eef96e7a11934fad4f061fafaad4f1f9be
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:53 +0000

    sentence shock embarrassed spare send

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
