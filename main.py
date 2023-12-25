8e63afd1deff6c36a138d3c93e7b9da85222b794
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:46 +0000

    sight around second than long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
