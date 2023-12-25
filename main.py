d07a675584b801684d9435d9c97fab354254dc73
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:17:40 +0000

    interest thick deliberate confident insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
