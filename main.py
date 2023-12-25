0f0688f124d0a2cd7f54dbb1c969affb27430b11
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:19 +0000

    question aim steam high bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import sys
+def exit_program():
+        sys.exit()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
