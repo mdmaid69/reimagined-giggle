c2990cfb1f973e117cd445d61f1c2361ba8c6f78
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:11 +0000

    address pleased jump continue tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
