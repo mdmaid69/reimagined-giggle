310646270a14d4adb361209e94ffdedc4efd50a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:53 +0000

    Gg mine clean middle nothing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def reverse_list(lst):
+        return lst[::-1]
