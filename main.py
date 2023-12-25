e390733156dc788e195ed9f62f3e0593a8d56de5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:23 +0000

    takeoff object past mysterious fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
