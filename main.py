17542cacffe2d47f165646292e57f5420a6e2ca0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:55 +0000

    comefrom somewhere class last promise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
