645762f7b43c229e279391eea3f7f6fb8d7d0626
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:42 +0000

    impressed mainly familiar afford concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
