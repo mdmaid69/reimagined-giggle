5c14a32c50b7508d31caf0c73ba2ba1ccdfe7381
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:22 +0000

    on replace comfort shock trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
