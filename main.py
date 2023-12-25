6bbb1018f3889759eb706d1d053e15c741d2fdcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:03 +0000

    one rubbish concentrate sure swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def is_even(n):
+        return n % 2 == 0
