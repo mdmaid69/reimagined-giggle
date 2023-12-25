b99e503ff1a020364eecbdc478a62baa6e070a14
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:03 +0000

    marry left spread whatever bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
