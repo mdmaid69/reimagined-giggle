c27b1903c0918812c660ce7103665b60bf975664
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:47:38 +0000

    satisfied runover might content attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+from collections import Counter
+print(Counter("hello world"))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
