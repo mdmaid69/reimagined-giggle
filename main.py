a1e01d3772fcd213cfae619a8c159ee3b50daf32
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:21:10 +0000

    usually eager of enthusiastic image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import os
+print(os.getcwd())
