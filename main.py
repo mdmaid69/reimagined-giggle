002442fe3689a9b2850a6e901976bca3dec573ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:34 +0000

    plate big yourself gentle precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_max(lst):
+        return max(lst)
