89916593055fbe7d30e9deaad5c407c2b927f75e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:40 +0000

    skill necessary when fair sick

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 def reverse_string(s):
         return s[::-1]
