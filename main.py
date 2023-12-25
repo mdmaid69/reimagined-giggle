7d7fb46dc8047df3fbd43148cf16cc9ffcb5d870
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:32:53 +0000

    giveout famous arrangement type total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
