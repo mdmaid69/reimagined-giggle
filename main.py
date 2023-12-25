5bfd6b395b6dbb18a15b26a8ee1934cbfdbd6418
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:26:44 +0000

    edge lesson chin only weigh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
