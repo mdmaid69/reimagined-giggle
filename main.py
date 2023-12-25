b9a550a7c7dfdef614e4fb4d98e3200fb6038e26
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:23 +0000

    full set upwards difficult relation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print([x**2 for x in range(10)])
+import os
+print(os.getcwd())
