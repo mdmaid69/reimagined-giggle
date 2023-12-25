208e906c0677ddb51e6a5b9c2dc3aed24ac1af50
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:47 +0000

    history drive chin type Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
