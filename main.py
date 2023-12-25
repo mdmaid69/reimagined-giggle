e6b9ff4f957af80cc39955d7ba59b8c0e5cba624
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:01 +0000

    authority alone tie Pp marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
