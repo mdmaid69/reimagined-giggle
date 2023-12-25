7a0f27613817c040aca3d409127f214358a210e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:08:20 +0000

    properly your song Pp press

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
