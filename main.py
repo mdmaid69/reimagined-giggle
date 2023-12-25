886d8ac28eea15523f77948dd090574b61323323
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:25 +0000

    very correct least intelligent confuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def convert_to_binary(n):
-        return bin(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+text = "Hello, world!"
+print("Characters:", len(text))
