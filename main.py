83933f15d1b0b9e3daa11dfc61ef5b6469695dbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:48 +0000

    error evidence possession breakfast capital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
