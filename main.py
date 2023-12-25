15c7fb3d49a4eeb14547b82f894eccb0455b5163
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:54 +0000

    relax minimum sorry prepare walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
