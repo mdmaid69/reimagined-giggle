ecb24b2f075135b681081e4ad6761aa409408841
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:25:40 +0000

    rate coat various thinkof programme

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def add_numbers(x, y):
+        return x + y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
