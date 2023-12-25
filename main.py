577e5151b380c71bb5195f225171641cbd37e4b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:55:58 +0000

    recommend suckme formal damage brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
