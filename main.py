db307fc57527bdb1b65554cf145e6508a46e1c31
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:25 +0000

    set reply dark live demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
