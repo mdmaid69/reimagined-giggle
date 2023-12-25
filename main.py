6198041c6631b481eb851abf274c9209ceb7dce1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:25 +0000

    concentrate physical advantage case inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
