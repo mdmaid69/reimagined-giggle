75a2a9cebd3d3de560b603a859698909b7c25eb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:12:52 +0000

    formal fast actual herself good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
