837ebf7989268e93add673bd2cebb0e7d822e33e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:47 +0000

    entrance Nn nor whisper spare

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
