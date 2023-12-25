826c7f0482b1147df67d01cebbf521df7ed501ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:08 +0000

    limit plenty sure pin Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def cube_number(x):
-        return x**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
