70dbff87858bbbb7a6f1e2da9005e5993a2780c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:25 +0000

    pocket brush scrap 565end pay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
