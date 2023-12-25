99ee35601c49816adc478b2c9ad88fd31bcd560d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:48 +0000

    or petrol brush storm snow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
