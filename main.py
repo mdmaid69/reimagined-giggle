9eea32ccf1f33b2ec3a1bad9f240c2a11948274d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:43 +0000

    pick afternoon switch Itself protection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
