cba66e3e9839df7432b6f2554f2f4ff22dca8432
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:39 +0000

    uponon everyone meal alone study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
