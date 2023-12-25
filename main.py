3fb103e3d540c210b095a272e888c64945a252e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:05 +0000

    scared concerned imagine culture perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-print(sys.version)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
