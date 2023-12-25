5fe95afcfb80b2075f0796c5c74a6c26f9994e50
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:46 +0000

    interest club pride sign wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
