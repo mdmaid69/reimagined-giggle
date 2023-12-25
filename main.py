3d4328edf72eb4ec83cb4feb261ca0dcc20eb288
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:39 +0000

    arm risk man his shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
