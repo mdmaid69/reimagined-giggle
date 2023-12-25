3dd874bfa6a7311b4d2846d763003bf783ac2733
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:14 +0000

    few disagree lose shut discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
