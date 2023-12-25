f93dcc0a22fe30bea7ac0c800014ababdde06187
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:24 +0000

    idea rob snow university damage

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def count_elements(lst):
-        return len(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
