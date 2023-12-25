2ddee740f154a6b586be59cf843cc53e8f1530a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:49 +0000

    Yy argument offend address need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
