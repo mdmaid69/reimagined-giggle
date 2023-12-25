91bbbb743951ed3c861615afbdf0bacdfa89c0de
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:46:31 +0000

    beginning pretty sense love exam

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
