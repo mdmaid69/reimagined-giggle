0181f3edd4163a157828e9049900cfce25ca4e53
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:05 +0000

    famous meet government frightened clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
