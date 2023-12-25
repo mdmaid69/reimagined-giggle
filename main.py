c16698e9bc1476385a45077f522df29ea19ba3bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:42:03 +0000

    formal alive temperature authority afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
