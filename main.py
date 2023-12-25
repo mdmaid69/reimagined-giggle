f1962882728551203482b79f8c01da3e14b703a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:08 +0000

    pay compare whenever calmly importance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
