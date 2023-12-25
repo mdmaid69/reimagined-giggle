447f3d1c21b25973b1dd0d26753be1a3006e0927
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:39 +0000

    picture female matter though afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
