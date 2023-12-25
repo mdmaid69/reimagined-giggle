b236f4cc825194c4eaaa1fcb40afff4f97859ffc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:05 +0000

    fortunate explanation waste flow base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
