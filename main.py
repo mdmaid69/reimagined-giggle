a89ecb253089c919b8680a7619861e4f73b5eb26
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:18:32 +0000

    regional any sky violence introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def clear_array(array):
+        array *= 0
+def find_min(numbers):
+        return min(numbers)
