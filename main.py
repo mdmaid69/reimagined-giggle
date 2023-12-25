f44a645499f83e48b07c3f6942b2a669fcec5c79
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:48:13 +0000

    stir air edge behind nature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_buffer_info(array):
+        return array.buffer_info()
