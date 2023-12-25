0ee6682a8b5f6b154cea25fef7f78c8ba28c2d6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:58 +0000

    limit lie ride waste opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
