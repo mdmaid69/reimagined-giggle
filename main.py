2213d5ed25f9de6bf9a927670cd9705c2dba5300
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:38 +0000

    boil insist ourselves uniform else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
