3a15ffd5ded397f809b45ffb3ed5247d68314c27
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:23 +0000

    raise harm fortunate beside destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
