abd268801acef9de6465b61e147f033d45aa6281
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:10:02 +0000

    Kk completely calm much name

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
