36fa886cf5b00bff1f210bba137e2e90b1844f75
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:58:49 +0000

    ring common influence brain suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
