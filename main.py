77d4277131b403e04fdfeb3f7caa13aead29efc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:33 +0000

    hole Zz off nerve limit

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
