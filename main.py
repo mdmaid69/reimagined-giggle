69004995cd105ac308abb89a746dfd6727001823
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:55 +0000

    birthday improve down picture organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
