77e6d04364fb9ec02e06b500320fe80d13590817
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:09 +0000

    force supply hurry although slip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
