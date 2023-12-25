1d1bcee7ea9d5f6e07c7230a5cca3b1104f33d66
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:19 +0000

    split broad plenty communication match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
