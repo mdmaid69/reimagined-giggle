1956086feb38008a813840ec5375d4e94d90025e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:09:48 +0000

    mark frightened pocket worth nervous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_set(array):
+        return set(array)
