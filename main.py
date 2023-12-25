dc628783edbd9f5cbc9e975c2f93cddeee437366
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:30 +0000

    combine arrangement advice drink police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
