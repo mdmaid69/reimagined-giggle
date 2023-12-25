23241293eb0665372414cc72fe2703f448eea42b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:04 +0000

    total careful grow possession go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
