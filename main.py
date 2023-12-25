8e159842516fc000735d3ccd826dcfcaf3327e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:18 +0000

    tough much meet appearance top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_file(file_name):
+        os.remove(file_name)
