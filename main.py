1b8074cc007e685e3c0b96449c3e189465017b1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:45 +0000

    vanish estimate form fortunately heavy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
