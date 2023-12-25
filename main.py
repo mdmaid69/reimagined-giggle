4c0e22f52cbc79c4bb3cc0a5ac451981866f2004
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:43:23 +0000

    juice odd getback unkind forever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
