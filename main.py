c9dae44f2872dc4f84d50c435a1f96c49b47bf68
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:07 +0000

    criticism frighten watch through waste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def append_to_array(array, item):
+        array.append(item)
