5c2a2f181c2b88189c959e5b0ac7cda31491c7c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:08 +0000

    single surprised include condition firm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
