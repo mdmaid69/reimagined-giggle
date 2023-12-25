cfb717697ae51ee9d0d0f88350f02c16634e00ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:18:05 +0000

    attitude left event impressive wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
