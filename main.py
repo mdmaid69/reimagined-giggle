6667c2eec408f6a18ab4f5ea027bcb9922534db6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:46 +0000

    dry giveback anxious sing vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
