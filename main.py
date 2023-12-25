f23f7bc96dc27d7b57c0ebff76580ab161851f98
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:43 +0000

    fall follow variety top fair

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
