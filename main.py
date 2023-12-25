0e065c59a21cb71cc94fb8ed2e47b648bbcb16df
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:48 +0000

    brave error decide border wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
