6f1c7c1fa2c5b05182254caa866a0b03650c4e41
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:19 +0000

    material expensive born argument talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
