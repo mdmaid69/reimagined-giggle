12f03b83cfdb63b7758e468eaaf60d0a9d25b402
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:31 +0000

    hang put never accidentally standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
