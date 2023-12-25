7fa238b69c5c1a28bd0bfa11fb66a48c17c04125
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:59 +0000

    suffer that danger long swimming

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
