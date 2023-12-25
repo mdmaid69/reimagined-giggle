5fae811deaf1e6477af7ee904d10fba0c462020c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:45 +0000

    upstairs loud middle concerning then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_current_time():
-        return time.time()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
