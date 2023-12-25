d5576cde54443bfa962c780858c306a14625d956
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:46 +0000

    world occasional wind busy side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_typecode(array):
+        return array.typecode
