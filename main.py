499fcac3837a45561df439798a9dcc822d3daf64
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:08 +0000

    somebody deal convenient whenever influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
