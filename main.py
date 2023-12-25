75cc277e305bf247ff5c6c31521263990c21bf36
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:35 +0000

    tip discuss will settle single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
