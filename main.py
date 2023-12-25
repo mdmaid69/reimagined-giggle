131476b98fc4bada23f32987e7aa357a77937649
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:43:32 +0000

    crash behave float decision certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
