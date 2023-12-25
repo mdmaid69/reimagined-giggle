45c2c123978b576908f41edf2376c121c9ae5038
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:04:03 +0000

    interest occasion protection turnon welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
