b657ad084af749ec0b0e0677df5ca9bf11f4c90e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:47 +0000

    smoke alone black help accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
