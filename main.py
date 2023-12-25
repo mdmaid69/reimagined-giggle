8c2b1c40c38614bfeae8492799a3b3a2e53fc109
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:32 +0000

    phone hand getaway snow begin

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
