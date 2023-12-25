390dc9ca98e1a1cfd203c00cf04347f78a7f3155
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:14 +0000

    level excuse minimum confusing about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
