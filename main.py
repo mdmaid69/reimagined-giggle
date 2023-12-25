c8be4fdd880f58f9f8e446eb0075d386319251a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:32:58 +0000

    explanation winter lost even left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def find_max(numbers):
+        return max(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
