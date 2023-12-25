b1278b974d73d69c4b14623cd3367469ee357454
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:40 +0000

    discussion fine secret excellent find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(numbers):
-        return min(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
