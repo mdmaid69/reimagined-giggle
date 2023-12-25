678caa644da9fcab2fbfd7f37a8e682cc7015082
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:49 +0000

    within cruelty splash slowly cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
