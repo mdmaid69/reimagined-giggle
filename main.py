4b5ee539e9540acdbb3809d7f5a6e59c6dd722a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:22:33 +0000

    provide fast spoil necessary politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
