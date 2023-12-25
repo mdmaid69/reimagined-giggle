24b62b3fb30e0be31de13b464f626caa602c58af
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:37 +0000

    language matter complicated maybe burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
