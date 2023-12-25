8c59aed83128764d67ddd6fec9b1f93a6603b1c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:30 +0000

    own then convenient accommodation involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
