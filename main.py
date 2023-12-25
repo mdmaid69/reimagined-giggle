191fc027ac961baa6754c53a07d5b96ac43c4c8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:26:46 +0000

    professional usually confidence union pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
