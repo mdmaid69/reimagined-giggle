3ca0f10043cd53395ba61e780363f8327b3a427e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:33 +0000

    education surface urgent hold creature

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+for i in range(10): print(i)
