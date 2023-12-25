8a391ae0406f60916153420a07af6ad7f86a322e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:58 +0000

    reduction same scream solve skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(a, b):
-        return a + b
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
