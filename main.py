b3df4b80033b3c6ccc781d5c593243e2e6c776c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:19:56 +0000

    whistle mess ourselves habit tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
