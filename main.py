8273a29149888b71e223856cee219b289793ac7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:38 +0000

    sight drawing tool tip at

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
