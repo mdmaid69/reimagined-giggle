ec934c51561ef72bbeb9cd1ed01002ef1f89775c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:36 +0000

    course height car bound approve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
