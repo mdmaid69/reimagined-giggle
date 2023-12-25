0eeea92494a0caeceeef3c27717c275ea0e40224
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:12 +0000

    charge wing insist characteristic development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
