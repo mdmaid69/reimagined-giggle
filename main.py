62513970d7eaabf7cd3f1fa6fdad9772588d8d20
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:33 +0000

    real pipe recognize steady experience

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
