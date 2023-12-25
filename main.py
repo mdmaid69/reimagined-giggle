77095f529a146e8ff97113017b3c0b673223778a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:34:49 +0000

    outside juice over experience married

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import collections
-def create_queue():
+def create_priority_queue():
         return collections.deque()
