3df7dd25655a54c467ddaaada345949b82e3922b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:52 +0000

    happy knowledge expect spoil tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
