711a011a0164e94f0b7e070fd80d39ad9f46cd96
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:16 +0000

    sport workupto bringup thought ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
