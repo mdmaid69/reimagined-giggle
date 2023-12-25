f208dd789ad755fa80d6fd36fc578664dabb6897
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:57 +0000

    shape better buy leg 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
