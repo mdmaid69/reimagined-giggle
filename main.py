b1e6342060c4b757a36c072e06faaee999e921f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:38 +0000

    preparation scrap part shine excellent

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def add_numbers(x, y):
-        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_counter():
+        return collections.Counter()
