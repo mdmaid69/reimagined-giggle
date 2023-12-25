b0f347289ad0118e41242d94111de887ca6b293c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:21:18 +0000

    pot satisfied suggest soft control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
