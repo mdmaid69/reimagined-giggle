e49825ffb4a1eeeeb4ecd48e160ffca59c7b2c81
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:40 +0000

    runover club off could important

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
