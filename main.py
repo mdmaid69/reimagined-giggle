561405a29d01fbbdeab853498ea0fbaea7e29439
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:00:52 +0000

    pronounce that figure lookafter long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
