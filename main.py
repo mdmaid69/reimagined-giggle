db179b53c907d67d3da360504d24923dc7ef71c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:14 +0000

    trip switch geton urgent minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def reverse_string(s):
+        return s[::-1]
