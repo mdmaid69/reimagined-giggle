1491f3d8c7687efd1635eec60d50eac45e15b1c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:55:40 +0000

    even most communication round his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
