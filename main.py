9b548b38e51d6d6423bcc438721d50add623cee6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:27 +0000

    buy respect obtain mean sport

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+print("Hello, world!")
