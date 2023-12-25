24c310b4eb21f7bd0ab10149403c5b894d77bbaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:22 +0000

    remove thick pattern special alone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
