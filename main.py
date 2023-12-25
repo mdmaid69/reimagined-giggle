ffea30b1922fdabb556600b3f3c43ba7913342d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:32 +0000

    start tend after weigh Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_string(s):
+        return s[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
