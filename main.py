113ffb58506c30a85f8f475d2a0944d7daaaedcd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:53 +0000

    violence march feature meanwhile myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
