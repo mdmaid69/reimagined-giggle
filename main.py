0c296cb69190a8574175884300fccd5e6bb82e62
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:07 +0000

    print flow switch exaggerate now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
