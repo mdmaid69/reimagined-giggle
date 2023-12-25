bb1787bfdc6e48f1f740a9b490421ef36be86a8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:56:43 +0000

    both value term still avoid

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
