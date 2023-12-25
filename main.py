21bea2cfa41c11a8936ae2e5f76004fc48228a70
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:58 +0000

    himself majority petrol compare of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
