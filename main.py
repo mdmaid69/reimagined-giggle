0122a0f79c9f03dcaf84259b0f84bca5083c1902
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:54:39 +0000

    lookback(on) weak certainly convince somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
