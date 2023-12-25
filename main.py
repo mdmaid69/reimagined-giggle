1be06539dc8da5fdbdee4f1da2b9052cffffa1af
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:00:40 +0000

    573entertainment high goon record experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def reverse_array(array):
+        array.reverse()
