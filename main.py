9cbde9afffc9067718ae4f9eb6923e2c13e401a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:11 +0000

    spill tragedy whoever mystery lookaround

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
