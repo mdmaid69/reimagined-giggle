803033a61738c6d8463c579ba39dfc6af68c5ddc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:09 +0000

    appeal unpleasant fortunate talk busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
