4b5c64a3df9f0b303277c1a1c165c1ab22d00e82
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:14 +0000

    pay term generally contrast sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
