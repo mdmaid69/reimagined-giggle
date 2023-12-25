a9227e08c5bc8002bc15046633707e9dc03220f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:05 +0000

    giveup best effect backwards drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
