bb675af75bdbd9d2962efe6c58b9f3f8c3dd5ea7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:23 +0000

    slice whistle schedule excited final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
