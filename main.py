80c74db05f56bf94e486dd6e65b6b81fb6d80af3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:43:30 +0000

    admit drink report help than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
