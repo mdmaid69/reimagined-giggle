16be3d939744967f372cc3f55f5c9035b49b981d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:48 +0000

    transparent thorough survive hit comment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_to_hex(n):
+        return hex(n)
+  def convert_to_binary(n):
+        return bin(n)
