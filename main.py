b454d40c41349e674313b83a9be08b134fa8df3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:59 +0000

    comefrom church join there study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def multiply_numbers(x, y):
+        return x * y
