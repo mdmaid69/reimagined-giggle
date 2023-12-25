5ce4e352d0b868c97dc6032d97a5d36863aac683
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:50 +0000

    check forgive perform Itself cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def add_numbers(x, y):
-        return x + y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
