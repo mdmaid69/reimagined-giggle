89f82267cd61bd7cefb1df0b83a3084910e43ba2
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:14 +0000

    subject aware pinch otherwise permission

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
