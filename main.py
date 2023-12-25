881fc984b05acb87b348410baab0235f7493d78e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:09 +0000

    someone believe neither peace except

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
