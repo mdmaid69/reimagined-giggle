6dd8659bd91ebbd27403614b24066392484f98f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:35 +0000

    disappear include properly stop reduction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
