db2dd9d7510ae47ec9ada9690fea9b18886126d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:31 +0000

    disappear spoil poison play succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_acceleration(speed, time):
+        return speed / time
