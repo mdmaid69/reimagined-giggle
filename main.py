04a20a18b1c5c2f069b12e31774fb9bb20a2c152
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:25 +0000

    demand wipe grade blue survive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sys
-  def get_python_version():
-        return sys.version
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
