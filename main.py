1d5ebbd76ad6aaa2b8c74743fa4d520b4206b33b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:22 +0000

    improve make loose thin send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
