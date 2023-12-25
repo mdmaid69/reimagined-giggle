325edebf68f60288ff334f342f2423217921fa11
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:18:21 +0000

    side let opposite wish advise

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_sine(x):
+        return math.sin(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
