8c12d884ce351994bf5e407c9336b8a30a76e8d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:57:35 +0000

    custom boil suckme road block

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
