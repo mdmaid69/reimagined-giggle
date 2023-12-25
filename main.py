1170ef8fcde65b27792be2dbda6a647d0a85f2ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:22 +0000

    position service thank bread black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
