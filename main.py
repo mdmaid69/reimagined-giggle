c01f2c34ef99c1f7b96f349eea8290249e63a38f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:27 +0000

    doubt hear close manner stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
