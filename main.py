a13bb0928f2549e71c3d8a7233cadb9632538124
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:59 +0000

    foot estimate Ii nonsense combine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
