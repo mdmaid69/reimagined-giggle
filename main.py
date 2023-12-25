171fba2fcf5ae6c6db498ca384b3278554f47bc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:42:34 +0000

    lie forward lost influence ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
