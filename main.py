a982f3cd09c64c0144462ebb02b2e0af4f8b4bb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:18:52 +0000

    Cc journey medium small hit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print("Hello, world!")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
