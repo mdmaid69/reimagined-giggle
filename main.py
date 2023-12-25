1f226b736b806722b7f4e954d9c124595419f620
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:40:23 +0000

    Dd lean season unfair print

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
+def count_elements(lst):
+        return len(lst)
