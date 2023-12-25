59ada6159ac47c2392c8b0c16d451481856dec61
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:48 +0000

    operation switch deliberately law find

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
