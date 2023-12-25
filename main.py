32089904e76e610183abf4e36c485ad6e9aa34ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:09 +0000

    market fortunately pronunciation difficult record

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
