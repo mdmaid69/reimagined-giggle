eb022223aa9d63f55f3d560c816308b8632718d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:57 +0000

    some another consult correct loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_power(work, time):
+        return work / time
+import random
+def roll_die():
+        return random.randint(1, 6)
