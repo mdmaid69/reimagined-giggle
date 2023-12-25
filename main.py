df4c0a8389b1fba78626670fe2ed8abc233854b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:08 +0000

    water danger the lookup various

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
