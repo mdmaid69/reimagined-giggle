c9411fbc6238f6dbd3b4756b8a2bebac8e4dd613
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:37 +0000

    otherwise perfect neither watch round

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
