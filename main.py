b759e39493a17dfd759f4bd955a237b8278702b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:28:26 +0000

    police centre store fear turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
