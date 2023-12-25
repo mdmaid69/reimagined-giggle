f5e2d5a76bd68b35f26ef4f61557bc7135934cfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:47:30 +0000

    march situation platform tool eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
