a0bb27e9ea27228992f89ba7c3cb0bca10e556b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:36 +0000

    accidental another refuse building perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
