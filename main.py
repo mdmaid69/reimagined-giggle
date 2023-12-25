77ecb54c9a0d00fd55bbc26da6291eee58eccb34
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:43 +0000

    pull window play address height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
