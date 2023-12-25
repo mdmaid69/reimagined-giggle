e8ce1e0367f049c987131708a9d36075dd91e20e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:34 +0000

    571enough keepfrom size twist congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
