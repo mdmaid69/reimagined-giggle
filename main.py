49d83df0b84efd7038945629f1bfa4336359e095
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:47 +0000

    degree vanish comedown left gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
