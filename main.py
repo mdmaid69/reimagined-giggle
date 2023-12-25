c6aef9bff3f31ab1360a480948b618047ceeb40f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:00 +0000

    pump major notice earn single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
