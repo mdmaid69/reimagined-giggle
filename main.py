38ca9da3053b5009a94c7022e40362fc9cee0a34
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:45 +0000

    march whether pass announce speak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import tensorflow as tf
+print(tf.__version__)
