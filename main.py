8e76deb3c944b290794a493e6dcadcce2294ebc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:42:08 +0000

    fail protection popular alive medium

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_power(work, time):
-        return work / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
