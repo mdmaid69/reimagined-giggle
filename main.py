6a4ffa659d6ab1e0c7ed56abbafc0934f771c4c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:28 +0000

    scream row murder wealth respect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import tensorflow as tf
+print(tf.__version__)
+def convert_to_octal(n):
+        return oct(n)
