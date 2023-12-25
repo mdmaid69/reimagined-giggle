d8edaaa1ab4312dfb334b7385366ee1ccd6768c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:22 +0000

    contrast command contribute awake raw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
