c27d822350c16206954f1dc1a6633f098fd35fca
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:02 +0000

    throughout command sore getoff interrupt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
