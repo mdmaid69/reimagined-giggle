63d83bd9593b548c6d219c9ef36336137ef86e6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:37 +0000

    with excuse deserve container breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
