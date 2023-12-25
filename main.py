685b237bf5362ce8c6c277bb7d3a625b0adf42ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:25 +0000

    exactly bill ahead crash control

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def convert_to_hex(n):
+        return hex(n)
+import sys
+def print_python_version():
+        return sys.version
