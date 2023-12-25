32b9107de299224b6b78fa8fda6527872877e957
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:26 +0000

    worried fire pencil obvious share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
