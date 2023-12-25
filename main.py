a94a024bee02d2bc87faa16a406957b371c27b88
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:54 +0000

    left dark drive they street

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
