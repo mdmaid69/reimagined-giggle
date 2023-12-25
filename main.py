a7c7cc0d5c1707274ea42215f398e84ab4029937
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:43 +0000

    emphasize deep clothes leg spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import heapq
 def pop_from_heap(heap):
         return heapq.heappop(heap)
