bf9818ab9ba805ffb5af0d9d9fa64943eb5fd42c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:23:23 +0000

    comefrom many weigh deal advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
