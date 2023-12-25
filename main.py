7d81cd3ca073a60e15b82daf413d454577a29376
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:35 +0000

    since thick mix Zz bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Hello, world!")
+import array
+def get_array_item(array, i):
+        return array[i]
