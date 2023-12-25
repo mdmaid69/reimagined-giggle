3f76d296db5fbc8555202d6c09d504013abc525e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:44:06 +0000

    dream hurry place protect turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
