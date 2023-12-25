d64b74f6379c0196eb684671395b3dc813ab674e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:46 +0000

    suffer fact pity almost new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
