6543bf0ae1c440857a8ae3443e223c34ce4ff0a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:36 +0000

    area kind depend callup Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
