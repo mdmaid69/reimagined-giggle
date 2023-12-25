710417beb486c8f7c5ffcb2a07f3cc7123f2b381
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:35 +0000

    sense pretty evening Vv dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
