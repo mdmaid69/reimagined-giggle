4e48424ebaf4c4baf2e359cb1cae4979113343cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:46 +0000

    early telephone create appear most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
