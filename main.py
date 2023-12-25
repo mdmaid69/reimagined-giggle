80c79ce0cce0e35bbab6ee1e897aac5d904c85d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:36 +0000

    reasonable mind square nowhere offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
