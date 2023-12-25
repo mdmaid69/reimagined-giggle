16027afd1573baf5916f4793bf629fe9315e6b44
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:39:58 +0000

    blue pipe tour woman badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
