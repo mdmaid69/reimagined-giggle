7dfa8f622e739a2c6f117963a0690570c938b92f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:38 +0000

    rob path care future support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
