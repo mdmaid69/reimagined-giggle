a7f1ea2936dc67d88a104277b4839c6ab19ae46d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:46:45 +0000

    stupid picture dear worth discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
