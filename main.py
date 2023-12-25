981e61535f54e6b60c233608573511eecab57c78
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:26 +0000

    part independent debt up address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
