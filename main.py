80070607fff0289e9f27df93fb5b6265d8a6f8a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:45 +0000

    better clear centre wave freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
