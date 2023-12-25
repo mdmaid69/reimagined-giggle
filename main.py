01b1d2780de4da846c84553bb5582e3fe70d18e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:58 +0000

    centre gather film balance record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
