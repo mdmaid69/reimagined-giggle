cf057d8172147b57ae3526ff49383a5ef959423b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:01 +0000

    prepare loud head profit duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
