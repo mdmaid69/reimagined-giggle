4fc55348166fe8531fa2694c1edb08034f3643ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:30 +0000

    expect dull crash behind calloff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import random
-print(random.randint(0, 100))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
