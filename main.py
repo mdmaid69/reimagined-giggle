dc7c019264e9ee47183e2b48be1915a2f7eb02a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:39 +0000

    far performance smell later describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def sort_list(lst):
+        return sorted(lst)
