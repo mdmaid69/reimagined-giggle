238e578aae79c2c990d315f84a774542222239cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:35 +0000

    weather earn moral conversation treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_area_rectangle(l, w):
-        return l * w
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
