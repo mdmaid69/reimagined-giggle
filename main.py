6d4128fe6ab58da8957836052f4e80f3b46bad64
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:50 +0000

    nearly permission rich change lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
