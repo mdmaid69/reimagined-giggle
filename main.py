6d5310144688f013954211ac1da1e160f43f948f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:23 +0000

    smell genuine wise rest gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
