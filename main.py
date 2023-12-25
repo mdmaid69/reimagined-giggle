660a73b4204f1e5f7a271eb1aefaa2cecd9eee7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:00 +0000

    dish sky opposite couple right

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def reverse_list(lst):
+        return lst[::-1]
