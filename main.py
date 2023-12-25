2784aa449375dcc67f777113f69ce44f2bf3eb2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:44:33 +0000

    appear attempt memory recover difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
