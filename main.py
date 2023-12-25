67f4bedfeb5fd46276167da6807fc657ce6fe2be
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:55 +0000

    same narrow finally market aware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
