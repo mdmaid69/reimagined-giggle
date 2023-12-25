0ec22cc4b74b5bd025d9fa134a0785ed43c7a315
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:41 +0000

    moment stamp artificial smell fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
