7bdbe4e603cf39af18f7892d16c720cc43c03dc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:48:46 +0000

    emergency comeback similar congratulations school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
