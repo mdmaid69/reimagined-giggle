5dfea072c88a5df59f7c7663b382a32b3058c2a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:44:40 +0000

    herself hit interview cough wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
