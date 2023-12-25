c13e059ed127b99e242cb81a6becc8949294c401
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:42 +0000

    embarrassed disagree conclusion main race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_list(array):
+        return list(array)
