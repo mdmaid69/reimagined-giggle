a970c641bb6dbe0d1afa24051536aa7bf96871af
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:12 +0000

    main kind really season better

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
