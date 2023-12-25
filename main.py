70f21ce9298152da530183e4128c64215aa09dd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:45 +0000

    judge keen public warning cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
