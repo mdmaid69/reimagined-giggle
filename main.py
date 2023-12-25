2457a72810751c91c412e29787581f9223253304
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:53 +0000

    waste force fire rough nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def remove_directory(path):
+        os.rmdir(path)
