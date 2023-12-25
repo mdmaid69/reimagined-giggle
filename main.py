3679fa3d1c93851a8ccbfc5bcd7154988c687736
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:57 +0000

    face allow legal unless debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
