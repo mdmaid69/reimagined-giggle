afcecb023b60e36154d7e29199481369cddb5fa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:10 +0000

    load room ear lot aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
