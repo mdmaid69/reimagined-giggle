558715053d53fea7218982d63a12f69a0cc1eb74
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:13 +0000

    realize suit treatment clue she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
+  def is_even(n):
         return n % 2 == 0
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import getpass
+def get_username():
+        return getpass.getuser()
