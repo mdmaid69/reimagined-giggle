237a2eab30d813d5be836f6d51f43665ecadb072
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:15 +0000

    influence breakdown authority plastic pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
