eb4d8a33b4d0ef5e5f320434656869b2e75fceda
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:01 +0000

    slight approval meeting on recent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_exponential(x):
+        return math.exp(x)
