e7d1c2edc839437190ed2d0a92b4ed2f1fd6cb1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:40 +0000

    value every post sore regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-print(time.time())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def sort_list(lst):
+        return sorted(lst)
