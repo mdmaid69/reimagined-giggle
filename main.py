ef1a80f8aa3c2cc94080b7750e47913070e00695
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:31 +0000

    day argue turn load believe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
