2152f5945b72e71352a44008c861da6ccb1764ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:58 +0000

    block connection notice recommend active

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_work(force, distance):
-        return force * distance
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
