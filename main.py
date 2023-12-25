c6cbd327338c0367c2cc80f5981c87c46235912e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:38:36 +0000

    congratulations material art mess police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
