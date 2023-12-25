196441f9f7752cac4e6acf7b3e45d7aec342a8fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:29 +0000

    famous poison complaint arrange field

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
