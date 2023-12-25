ed81427965f4e99875c278b694ad969067be3f82
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:43 +0000

    thorough law 565end persuade entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
