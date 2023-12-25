3dded024c0feba01d2f183059cb9a757f712af86
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:25:18 +0000

    turnoff arrival promise result machine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
