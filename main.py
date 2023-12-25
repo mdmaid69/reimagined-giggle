3333c8fe71a7741aef30832e654e7e074d814a2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:28 +0000

    think suit pipe project Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
