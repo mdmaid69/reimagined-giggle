70ff68e82f02d69fe9d1c681c19f38b8e2a2ca56
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:20 +0000

    concerned nor drive joke pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
