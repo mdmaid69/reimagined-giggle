c0989505e6e18e1eddd2ac0734962dc3192cfdfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:00 +0000

    region sport club outside scheme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
