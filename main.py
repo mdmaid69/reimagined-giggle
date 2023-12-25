49fdcc6b34d188942c0555eb600aeff5b577fadd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:25 +0000

    taste art film trip press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
