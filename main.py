780a8711c99623ddae5e2ab18d3556583f11e75e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:20 +0000

    content past why need state

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-for i in range(10): print(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
