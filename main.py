f14a3c2f1647295277b0f5ca2085e270dd84a71e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:42 +0000

    bag less remark criticize motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_current_directory():
+        return os.getcwd()
