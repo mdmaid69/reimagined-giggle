bff626366c68d1951ef60710c10fdf7e9dea900f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:47 +0000

    accidentally wonder congratulate light shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
