9444290705a13d35c2a56517239690e1741d6f0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:16:18 +0000

    type absolutely half fat objection

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
