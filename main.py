3e6b5073727246455f94996782d64aa4541ea055
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:40:12 +0000

    stir after rent fat light

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import platform
+def get_os_info():
+        return platform.uname()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
