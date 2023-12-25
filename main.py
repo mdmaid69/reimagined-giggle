9f9395c4c9f019f18feb6d961a9d53217a007d6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:11 +0000

    sensible which basis tone corner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+print(time.time())
+def sort_list(lst):
+        return sorted(lst)
