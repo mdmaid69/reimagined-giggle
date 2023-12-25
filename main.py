de64c5e18a1029b016060bee18fe912a7a632178
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:52 +0000

    second but treat matter hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
