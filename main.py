65ec892f12bf0721a88162660e883b9860c18942
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:07:20 +0000

    fall of left break suppose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
