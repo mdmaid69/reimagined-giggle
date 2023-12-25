e42a7ff78f70254fb8efb453fe6e3455f7fd52ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:57 +0000

    ground again against maintain pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
