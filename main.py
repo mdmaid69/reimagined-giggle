aaedbda2999022bbbd814ecc70c337d108e08fe8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:27 +0000

    spread kill or confuse cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
