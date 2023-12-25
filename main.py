fe35e8d77166c77c3e2d4ac8137061ab9d2c9a4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:34 +0000

    ever lay recovery shop whom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
