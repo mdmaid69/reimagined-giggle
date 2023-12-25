f7d7cbb551e5baa8d27471fb53d89590f8fcc00c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:05 +0000

    bringdown nigga part yet bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
