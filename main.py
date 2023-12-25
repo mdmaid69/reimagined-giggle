8e710f8ad853735b04949cb6bf569427a4ba15d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:00 +0000

    new please really offer everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
