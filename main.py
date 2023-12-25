d4bb3feba476775ce1e9000f8b5bd733fa171264
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:13 +0000

    whatever outside phone final on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
