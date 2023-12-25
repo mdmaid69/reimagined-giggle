4446624c7e47cb75705a10853856f5892a217e94
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:22:13 +0000

    chase male sport mouth rate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_volume(length, width, height):
+        return length * width * height
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
