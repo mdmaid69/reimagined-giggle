abeb8a18eda1b97b2a2c733171a24db6faf39198
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:14 +0000

    goback(to) survive Ww building awake

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
