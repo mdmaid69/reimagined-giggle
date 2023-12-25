ab68f26475fdc704d08909001250574ea5437303
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:09:14 +0000

    opinion flat rubbish completely best

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
