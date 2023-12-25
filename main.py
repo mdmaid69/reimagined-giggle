4824af58d3e316f2b028bca08f5345eb12085814
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:53 +0000

    spend regard seriously Bb sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
