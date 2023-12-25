ffa884becc3906bec3c78cadef5bf6e305d1ec97
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:27 +0000

    laugh accept ordinary holiday left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
