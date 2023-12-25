bd7f2eb7424b98128f3babea562b8170d99bc0e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:24 +0000

    drink private sort tidy bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
