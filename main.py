25d99787f04882980f0bbead86a9d51d633e8485
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:24 +0000

    win trousers quit stranger block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
