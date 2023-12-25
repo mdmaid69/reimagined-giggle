f66b0aa006ed0f4f1473501aa3a11fe61a1776bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:37 +0000

    provide sport design slide switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def convert_to_binary(n):
+        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
