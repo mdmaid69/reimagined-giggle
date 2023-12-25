a80d4cff55d6fac3cab60cd9108e408ce07c4c83
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:56 +0000

    whenever movement keepfrom figure spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
