1cc40bd1feff0f95113fbda37491523af5e53f62
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:38 +0000

    same back clearly produce rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
