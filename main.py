3e7c0b5a29e42886041ac7f910a0f7bf322ef737
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:38:20 +0000

    right sad figure fear drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
