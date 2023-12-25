7db9b288c4bd95b9f0fa3e1630e9f27170b24afd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:07:46 +0000

    against decide fall extra possibly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
