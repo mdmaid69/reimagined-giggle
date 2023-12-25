222a7a643cd25a2bad7a758abe9fe71e3048718b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:25 +0000

    education Kk interested behind blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_string(array):
+        return array.tostring()
