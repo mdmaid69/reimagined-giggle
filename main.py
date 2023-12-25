4ae59d683559afd356a43e1262816e37b3569c67
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:59 +0000

    accidentally her often cross careful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
