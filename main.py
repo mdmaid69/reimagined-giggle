dfe99a5b3a4778f4c02258f8e6da0ccea09d906e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:15 +0000

    burn after makeup godown right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
