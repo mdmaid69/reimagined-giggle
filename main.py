8af9d5baf196bab58ddb63b866a69333708b5c68
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:07 +0000

    dress return listen main tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
