d55bc471655db52de0ed1ce0934d478d746702b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:51:05 +0000

    welcome rid left sink Ww

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
