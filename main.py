e6f184fb9c8b392d7be9885ae0d33e1a080bc276
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:29:05 +0000

    smell instant us temperature lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
