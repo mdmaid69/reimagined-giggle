c8799067b06ed617a0cce9383165693cb80d0403
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:22:18 +0000

    shut there black heavy peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
