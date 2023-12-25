4c2fbf627ac8b4002a3500177eee375bce0f76d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:04:27 +0000

    lean undo meeting comfort Ll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
