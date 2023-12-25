c232105f7501d8d120033d02869397e8f97c646e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:52 +0000

    comeout check deaf Qq Zz

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
