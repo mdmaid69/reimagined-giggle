1f8a8c54a188710c8e64b5d024a2c50721f2e4ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:19 +0000

    dull ago skirt until fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
