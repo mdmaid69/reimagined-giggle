2bf9ac67dd46ab4868757752161f196f9def875b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:41:31 +0000

    increase blame where cry imagine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pressure(force, area):
-        return force / area
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
