8fc3659079b55da31572e10528a4f72809a112b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:51 +0000

    impression chat receive abroad advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_unique_words(sentence):
-        return set(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
