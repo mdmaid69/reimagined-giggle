3bcb68dbc84221ddbacbcd8fc5fdd9f28f20f528
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:27 +0000

    stupid fold nearly move even

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
