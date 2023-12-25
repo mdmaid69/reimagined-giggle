1bbd2e9417c43910dbc0a7fac00e43c113d9c694
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:16 +0000

    comeover birthday ours active dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
