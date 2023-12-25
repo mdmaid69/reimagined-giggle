b983de4c7ff67cc27f7eafc1f01e05b2195bdec7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:19 +0000

    precise freeze several joke instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
