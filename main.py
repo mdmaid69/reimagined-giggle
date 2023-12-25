bf913170ac0d19771e3de65bc932a59f43891c0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:24 +0000

    surprising address disgusting quiet shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
