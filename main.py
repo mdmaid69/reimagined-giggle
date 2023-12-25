96ab2809d31ccde7124dcba93ce10a14dacc3c0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:24 +0000

    corner defend sometimes important news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def count_elements(lst):
+        return len(lst)
