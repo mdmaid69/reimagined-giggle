f4f1709c27731ebcf055a8ba6a1f20205c3d1d4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:27 +0000

    wind like simple extraordinary cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area_rectangle(l, w):
+        return l * w
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
