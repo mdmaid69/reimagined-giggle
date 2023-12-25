108d0699753b86ac2f09845542fa7b6301611646
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:57:19 +0000

    sharp bringback answer mark plate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
