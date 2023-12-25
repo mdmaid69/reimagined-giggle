eace82526b70218050517ef7e44092c4582b257b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:51 +0000

    decision matter closed while underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
