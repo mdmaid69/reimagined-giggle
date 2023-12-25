a84d25203984e75431ea9de8970fba837fad4743
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:47:18 +0000

    ride shock wherever easy value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
