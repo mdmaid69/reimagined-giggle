256e9552f136841865119f123cee24030d55ae5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:15 +0000

    shake project Jj consult holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
