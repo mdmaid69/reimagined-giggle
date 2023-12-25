f63cbad3cdcaf406eff51c19d7083f4c1744a3ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:26:06 +0000

    male upstairs air by sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
