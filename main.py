52aed5a113da01110a20d96162f609542fc19ef8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:10 +0000

    principle record turn family fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
