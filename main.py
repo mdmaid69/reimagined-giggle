67f7d44c2e8a679f43c706700fea85ce447a2364
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:46 +0000

    tense satisfactory top watch unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
