d6ba27c95d7675508fc779d308cb2ae566233346
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:33:21 +0000

    judge high neither compare run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
