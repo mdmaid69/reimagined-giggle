cb1e699442ff30f94b43051f47825d2a8f2adcc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:06:26 +0000

    front terrible date difficulty action

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
