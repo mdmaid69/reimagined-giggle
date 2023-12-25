c06c4254e160ea040a1be28d3fac251fffc8da2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:09 +0000

    score manage swim clean handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
         return lst[::-1]
-import collections
-def create_priority_queue():
-        return collections.deque()
