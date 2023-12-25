639f58b12ec17c22f44b28807affe45276890fcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:03 +0000

    attitude bad eager escape side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
