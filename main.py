697c4a9a36e77fd20bc524302fc5ca6e80df45da
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:37 +0000

    arrange rest authority unhappy angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
