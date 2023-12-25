3c73b87428bcc95514f3c35f657cc596ddffb073
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:04 +0000

    shock smile suit leg float

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_power(work, time):
+        return work / time
