0b643be0fd7bfb9bfb01462d689c82381b5dda79
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:19 +0000

    rob wrong emphasize press like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
