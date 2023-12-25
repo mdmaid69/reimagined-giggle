265aff33a2cebf8ba74814164fecc6e0b7727f69
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:40:17 +0000

    improvement entrance steer purpose set

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
