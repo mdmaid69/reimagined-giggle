d9c789240d97ebf7c8988721b15fa9d90bea465a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:49:58 +0000

    replace shape admire plan believe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
