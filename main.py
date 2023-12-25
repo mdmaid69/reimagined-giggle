e878bbba890c3327e1b6e6defcbf15a9c6da7ecf
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:51:21 +0000

    father anyone scene platform taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
