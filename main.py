8ed3655924121274d7c25adf7c5e2d5f58e756fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:19 +0000

    hope though sad exchange pleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+print(re.match("h.*o", "hello world"))
