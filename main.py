a61724831e0279e3ad524103337cfed08b784910
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:34 +0000

    wild hurt mistake principle lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
