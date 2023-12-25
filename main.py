4303fd52801c7039e71d05f71b52c9607ae749b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:50:27 +0000

    couple march actual bit contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def sort_list(lst):
+        return sorted(lst)
