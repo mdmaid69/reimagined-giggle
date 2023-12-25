7a3b359aed1682860dd5298b61021a896305d33a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:40 +0000

    rush everybody preparation usually perfect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
