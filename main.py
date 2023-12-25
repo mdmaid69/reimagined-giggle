08280becbcc5cd531b191bd7ce6e3f847a72d39d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:09 +0000

    loose shoulder soak back strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
