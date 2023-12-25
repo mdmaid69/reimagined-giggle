1e11a00d4d27f5e1eff74b25e35e48a9ca1f16bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:21 +0000

    tone skirt everyone house join

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
