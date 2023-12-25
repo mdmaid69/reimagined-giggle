cb89384b4ad66494b775031ed9102572e203aaad
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:15 +0000

    reply butter bush season quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
