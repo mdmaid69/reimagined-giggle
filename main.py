f3a7e0a6b42c19cdc9e9ccfcdc5c232465c89ada
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:59:51 +0000

    deserve different cheek Uu rely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
