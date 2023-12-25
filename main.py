d4cd79e51c56deef12540820ed39a4424445f1cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:23 +0000

    offence peaceful future every shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
