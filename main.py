b71593f99bee0716267f6c654fc4a669e9cc10fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:57:10 +0000

    turnon ability 571enough summer swim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
