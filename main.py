073ea697e5efef267345f2aa847ff9486f6c170c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:33 +0000

    late broad attitude miss trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
