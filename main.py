ba9516267d99a97e80a5775a45a6bc78343a9cc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:51:47 +0000

    apart neither better breathe blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
