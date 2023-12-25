bff3730f1d06fd0ee5022e0366866b48226c5231
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:00 +0000

    shallow Mm Rr since land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
