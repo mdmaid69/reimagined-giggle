ec329c2e606fd42c930426c20e616f30bbc74a6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:46 +0000

    job least perhaps only laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
