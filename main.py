4b5db6828f6001fa13d9ad634261a11a30198e32
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:08 +0000

    joke tradition advice Ll coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
