d5b1b3dccfb2aa46317425f627fa97e04824c8d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:17:26 +0000

    lean she every help just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
