7d0ac1966665b4c2bf6624656b44b4524781cfe6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:03 +0000

    callat silent thin against science

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
