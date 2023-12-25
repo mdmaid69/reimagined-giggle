b552dd29eae540249da4ea0200e5d74f5407f8fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:07:15 +0000

    wire system defeat knowledge rain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
