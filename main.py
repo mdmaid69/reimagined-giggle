3354dd85c56aeb52511a8e9e082093e2c477c465
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:17 +0000

    remove getoff put punishment top

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-name = "Python"
-print("Hello,", name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
