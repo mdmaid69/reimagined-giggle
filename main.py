3a8dd6b741ca0b9a43f3d352e53c5e2ef55fe009
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:03 +0000

    failure kick information passage average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
