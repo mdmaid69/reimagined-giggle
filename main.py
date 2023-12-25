642ea86f1444c67c0fe564b89070332b9b3ed50f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:12 +0000

    side complain swallow open rank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+for i in range(10): print(i)
