28c427fd579b4ff8466bb84d2a9b297084ffc524
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:49 +0000

    same bet emotion wind sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
