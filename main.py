ae82d792864680718323318250f84b38a66b79a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:59 +0000

    giveup interview this excuse belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_volume(length, width, height):
+        return length * width * height
