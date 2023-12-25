7aff81c7f03d6412f6978c90a104c7ca5c6f06e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:32:29 +0000

    brain opponent any exaggerate explain

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
