b81229c25b11a3e84e98d70fc56934a17cea6b31
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:44 +0000

    push constantly connection block measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
