1de64aecfa0112263f18579d904db296b8dcc36f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:39:06 +0000

    live admit road modern challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
