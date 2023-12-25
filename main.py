6c11666ce7bc1f6be4111bace24e69df52710bb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:47 +0000

    border pause difference goodbye aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
