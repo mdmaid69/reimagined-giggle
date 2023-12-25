86fadcb7b09f96cb21f5aa093ab7afc491d93b37
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:41:38 +0000

    chance drink sister sleep cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
