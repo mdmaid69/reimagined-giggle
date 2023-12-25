a196ef4e05f96ca448f84168d9e9f66fc32b933f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:58:14 +0000

    smile century sea by formal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
