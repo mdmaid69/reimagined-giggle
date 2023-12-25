1430a115c97b41ec4d9bfb976034ccd3368943ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:26 +0000

    lock urgent calmly dream age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
