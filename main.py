987ca9968c13ccc60dee5b94fcc2d34feaa4e29b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:56 +0000

    stop weak worth cruelty lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
