842483e4eef836ebde72d946218d1b174ee21d3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:21 +0000

    seem famous post test mine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_power(work, time):
+        return work / time
