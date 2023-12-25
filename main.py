f2e84de3108692631a5dadb58799aeb2c209eeda
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:07 +0000

    patience yesterday calloff number warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
