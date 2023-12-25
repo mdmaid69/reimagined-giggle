88fc48b3070ef1b8b32fc810d7a24261e66058db
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:14 +0000

    belong study warn let exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
