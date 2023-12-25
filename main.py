1edd5d2520460e8bfb84a7dd6f518e2f00904d80
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:04 +0000

    equipment year outside expression impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
