b64c6cb122013d91707a77de14ede86106b87baf
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:07:36 +0000

    polite explain get after talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
