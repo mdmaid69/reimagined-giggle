b8dd36c18ab04d07f4e70d3888b5f66dce470238
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:01 +0000

    fork forever thing fast delight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_stack():
+        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
