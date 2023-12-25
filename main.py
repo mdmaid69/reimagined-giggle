00d38d192a4548b3733eef72e880ff1cf7e265e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:42 +0000

    escape judge transport repair temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
