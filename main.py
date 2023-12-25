19ac51fb91fb6d4e79ad2f5cfbaec34801d949ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:10 +0000

    further read scale deliberate imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
