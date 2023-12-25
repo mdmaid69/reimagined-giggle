7f55a07ff24bc4a9c901bcf8e0205d232f196d92
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:33 +0000

    brain artificial shopping fortunately waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
