880e9a773e885bbbaa94b1d36019c6eb30902fca
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:06:45 +0000

    score cruelty claim statement exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
