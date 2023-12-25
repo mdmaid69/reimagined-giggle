cc1c887031142f0620eca584f6c39a8a9716c549
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:55 +0000

    basis factory consist record but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
