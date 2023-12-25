78926d48e50aaf4ab6c845e3fcc3073fa354647c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:36 +0000

    since communication software tooth next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
