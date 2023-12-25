dda85171f44899acf2aba718c0f49a72e4889082
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:58:19 +0000

    amount plenty picture afterwards tap

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+print("Hello, world!")
