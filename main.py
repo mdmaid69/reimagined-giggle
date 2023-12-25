3f51eba1dd143e9af41d6a297620ee5c1f7a3d3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:05 +0000

    pay awake shop comfort favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
