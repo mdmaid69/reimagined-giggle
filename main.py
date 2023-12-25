3c74b36a01fe78be5273b90d7a7fc7b64b326775
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:11 +0000

    stage shade keep aware runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def convert_to_binary(n):
+        return bin(n)
