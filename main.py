4edb63e805ec69bf27220370585260d7f75110c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:39:37 +0000

    date sell sleep trousers relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
