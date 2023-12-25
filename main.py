712a0b3f9f2fbe7583e63494f245c38cf317a87e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:05 +0000

    aim sure range mass pride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
