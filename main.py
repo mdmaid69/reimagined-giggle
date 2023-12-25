a55ad2689e19922e6b3554417f3149b66c782a8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:04 +0000

    566end juice Pp form nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
