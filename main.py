510b91a387ff468ed5190bb1677cb28245ac4e3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:28 +0000

    file already godown mystery importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
