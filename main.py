9f42a0c8af4804960170f49d068c3cb045e909d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:15 +0000

    stick passage culture new mother

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_floor(x):
+        return math.floor(x)
