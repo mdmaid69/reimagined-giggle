cbe71b1eddc7fa71f1d5d5f1d2fc1b0d63ee11a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:21 +0000

    quantity similar call hurry deserve

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
