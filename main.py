a1c39da0aa84844f28d671469c82d57a7e4b533e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:27 +0000

    need you handle cover worth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
