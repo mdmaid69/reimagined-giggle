a5f48eb862ff56bb89c8c4cd47f4cefd802cdc68
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:13:57 +0000

    stay kick half light accident

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def convert_to_binary(n):
-        return bin(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
