e61213dd8d7bd4b148a5e2dbda8c89715e56cfb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:16:59 +0000

    building happy naturally impossible fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
