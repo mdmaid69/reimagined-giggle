0dfe8de367597680d23af61ba74dc31cb97c68ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:46 +0000

    makeup second hold arrange whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
