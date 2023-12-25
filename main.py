82265b85a8d343745f8892840354cda8ef98316d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:17 +0000

    finger breakin pause use issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-x = 10
-y = 20
-print("Sum:", x + y)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
