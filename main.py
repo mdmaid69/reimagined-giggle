a2bf94e481574260ada258827dab26e77fc9857a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:08 +0000

    shall tread agreement possibly bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
