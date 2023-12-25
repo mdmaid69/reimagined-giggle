5722599bc4615a3c1f78f772060aafbe083f33e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:46 +0000

    read foreign pause science intend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def is_even(n):
+        return n % 2 == 0
