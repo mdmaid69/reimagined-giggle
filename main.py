050506fb05b372d522f6958a1d055a3d8383e7b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:16 +0000

    dress fun around stick empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def append_to_array(array, item):
+        array.append(item)
