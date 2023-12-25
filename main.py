ea2a88a39fea7532e444ef24d9e9f387e53e8fbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:50:51 +0000

    race of name condition hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
