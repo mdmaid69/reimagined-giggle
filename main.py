5f9684e8cfc0bb7a40ff04184dabe44a91f2caca
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:03:18 +0000

    comedown provide winter seat nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
