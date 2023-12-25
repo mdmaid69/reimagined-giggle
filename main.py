5ccf69fd6463578d5efa66983bdb199e9d5d803f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:55 +0000

    address sad exactly deaf pronounce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_current_working_directory():
+        return os.getcwd()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
