a89208ef38524f9e5cba53f3f55d4e1121dbeee8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:33 +0000

    object ignore slide insist pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_repr(array):
+        return repr(array)
