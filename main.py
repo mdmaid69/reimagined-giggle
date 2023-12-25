e7e916178ae3b3a1c4b81ae14bd97a9a976336b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:45 +0000

    print lean tool study powerful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
