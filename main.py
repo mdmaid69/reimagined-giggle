78eda272ca9ca50aab0853b3b84d85bd618f6245
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:27:25 +0000

    lead start grateful during ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
