416b51b59252b96e988087c5477d7034de6035d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:57 +0000

    slide reply permission proof our

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+print(os.getcwd())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
