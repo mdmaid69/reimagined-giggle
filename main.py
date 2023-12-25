7e1d3b5564ebd1734858f8e4d1c712140fc7f2ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:25 +0000

    track pronunciation related heavy tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
