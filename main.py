51728c54a48ece7dd6d6d334ec30442271ccbc71
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:07 +0000

    flight perhaps breakin communication arm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
