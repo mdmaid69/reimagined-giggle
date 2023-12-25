296fc264a9c076c5f0cd27fa66da4e17743185be
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:44:13 +0000

    ride promise into speed yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
