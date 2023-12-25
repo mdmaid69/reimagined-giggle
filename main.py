5f6d5040738613b0d3bf29f47a80d355d1487bdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:21 +0000

    takeout take train autumn family

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def sort_numbers(numbers):
+        return sorted(numbers)
