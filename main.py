fa90767e100127537300829ea5e35bd41d4420e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:36:47 +0000

    avoid sound pronounce like some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
