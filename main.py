1d4bdae05512f9e7eeac6ad23deb26d6bd498dc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:11:50 +0000

    make hello worried long bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
