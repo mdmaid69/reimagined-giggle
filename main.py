2f6e93f62124b08aad7767e4cc057442850e2590
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:06 +0000

    popular Uu picture gather cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
