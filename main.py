e0fee51519ab0287b5d8b46a2db434b3f667855d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:13 +0000

    cheque cruelty prefer port dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
