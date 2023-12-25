5973d9d7717592d549e6a015b0ec1070855ddda8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:01 +0000

    characteristic funny still ear rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
