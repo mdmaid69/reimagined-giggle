05fa7ce36f248a08673e340c62a93ba54a06e59c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:57:41 +0000

    peace slice way sticky hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_acceleration(speed, time):
+        return speed / time
