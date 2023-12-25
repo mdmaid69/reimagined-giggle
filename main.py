e289016e67186a416f11b28d9ec95278fd10fd8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:32 +0000

    Pp fit cool stone head

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def convert_to_hex(n):
+        return hex(n)
