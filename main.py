4ca10a01443ceb195f8a77b9709cf3d075355b42
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:06 +0000

    shock bringdown slowly march fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
