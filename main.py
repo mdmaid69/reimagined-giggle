613aae3847294043ee7d33a93d7342d0b739c3c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:53 +0000

    law firm successful region lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_item(array, i):
+        return array[i]
