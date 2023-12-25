1f2bb7548b27318973d6b4e97029295075817d71
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:49 +0000

    match accept live with case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
