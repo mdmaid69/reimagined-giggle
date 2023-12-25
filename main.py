7aabe28957ddaff7bd2fcbefcd6ee8a5b1981733
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:28:58 +0000

    upwards far lot takeover lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(lst):
-        return min(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
