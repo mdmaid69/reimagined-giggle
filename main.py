71337e3bd1ba3ca486fce2a49b26c5ac8a67909e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:05 +0000

    myself snow otherwise argue star

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
