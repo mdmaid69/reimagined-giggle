58743ba8bbd4db6c7f30493367b7860b3b2376e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:25 +0000

    attractive giveup breakdown apply spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
