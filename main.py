40a069248d04a7bfb05b460a9f911231e4fe006f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:57 +0000

    smile fast willing rush remember

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
