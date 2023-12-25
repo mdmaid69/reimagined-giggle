b608ad5eec46fcc293d00b7924dd8b722bf9e1a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:36 +0000

    polite hungry copy pause reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
