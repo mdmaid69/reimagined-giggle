b4e8396f41d55f38fcc39fef4919b024bf17aa1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:15 +0000

    capital pencil bath minority persuade

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_circle(r):
+        return 3.14 * r**2
