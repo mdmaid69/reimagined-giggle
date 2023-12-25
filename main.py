8b9daf0b63a8a2c4bc9c059df352b386a8a2a9d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:57 +0000

    confused no final national nerve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
