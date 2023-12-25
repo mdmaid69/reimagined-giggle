8823dc4be73fdda59e926c22899be038890a275e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:13 +0000

    smell brush how sort leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import getpass
+def get_username():
+        return getpass.getuser()
