d7787bb5a0b2dda43a36b318f41284be6a5f3b32
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:39 +0000

    join guess prize movement wind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
