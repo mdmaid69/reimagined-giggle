d4bfedc7022404b60e04aa0d69949c8f38bf4192
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:11 +0000

    window fire attitude beyond rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
