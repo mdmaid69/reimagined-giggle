601c9b03d7f04a98d5867a81ed9da80e9fe465e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:52:19 +0000

    scheme various anywayanyhow have noone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_pressure(force, area):
-        return force / area
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
