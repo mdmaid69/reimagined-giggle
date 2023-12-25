c3aa07bbd8235113678a6dc6859538df24eb3d82
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:21 +0000

    thin mixture approval government goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
