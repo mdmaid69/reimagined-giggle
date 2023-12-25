de273212e486affe98f086e9d4c2ccf3b957a15b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:59 +0000

    change human several cheque learn

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
