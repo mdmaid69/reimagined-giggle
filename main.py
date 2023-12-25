ba33a0e50ccc17864aa559a6560c7ee666555620
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:57 +0000

    smell pay butter freeze tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
