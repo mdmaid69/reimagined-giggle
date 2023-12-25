a78013b84ebebc5ea4e53b8d829b6f8445ec0fb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:44:17 +0000

    dear freedom minimum steer vanish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
