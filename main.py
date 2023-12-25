dd63c605cb95cdbed90ab7ffcda278ea55f0708a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:56 +0000

    form fashion properly trap leg

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
