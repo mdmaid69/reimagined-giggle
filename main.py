8bef5edcd6005919057321174eaf303b0da0629a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:57 +0000

    entrance minority artificial turnup especially

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 for i in range(10): print(i)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import json
+print(json.dumps({"name": "John", "age": 30}))
