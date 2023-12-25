00120dbc6a5cbca0fd7c812d41a02960a116f68b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:47 +0000

    path hair away watch teach

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
