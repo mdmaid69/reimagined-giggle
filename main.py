2a9c6e8e305b91b8a89f61618fecdf298d9e35b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:36 +0000

    concern print type order forgive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
