fd1d8771d0eb682f009d3d290d612e7d4ddcd95f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:19:08 +0000

    influence lunch dangerous phone injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
