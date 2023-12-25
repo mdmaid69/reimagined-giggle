1cab5969eee0b33484e5f6f05097229fff1c49f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:15 +0000

    such criticism temper calm mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
