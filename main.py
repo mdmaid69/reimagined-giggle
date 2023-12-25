5a08765bf9e686f3dae892020b6ba023b4e9cc4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:05:20 +0000

    turnover funny crazy opponent prepare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
