3a4ff86eaec8d391fa02cbd07cff5c24c2b9812c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:42 +0000

    excellent mixture progress anybody alone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
