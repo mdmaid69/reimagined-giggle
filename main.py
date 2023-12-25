22e43a4a6b25bed20743054edcba0e8b24b73e65
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:19:14 +0000

    collect form callup bunch crowd

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_list(array):
+        return list(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
