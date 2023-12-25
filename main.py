e79cc39b762bf5d67162fd3cbf12b7d21e58f145
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:49:27 +0000

    community sound judge before opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
