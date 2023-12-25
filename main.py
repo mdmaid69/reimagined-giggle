82ecb3a3189306e4f880dc2a0521793485527516
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:54:21 +0000

    law object ride crash hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
