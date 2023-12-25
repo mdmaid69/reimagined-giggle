bbf386f71f194279d9f5310c82c67b616cc15fdf
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:34 +0000

    workout music gate thin preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
