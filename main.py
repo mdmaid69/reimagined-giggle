971a4c7f43f54377817eb3d689c60a6cc1b662b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:18 +0000

    culture difficult visit write priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
