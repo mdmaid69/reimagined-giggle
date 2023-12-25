c893f2765e26adab2f8b637c301b7880d297d8f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:54 +0000

    community wet ilovedicks over shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
