ee054650c590d99e8f98afa720c51605448fb44e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:00 +0000

    tell shame thorough direct minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
