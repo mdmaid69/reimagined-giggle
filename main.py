d5fd5fdb09102cf66a17f1862a2699766a66df73
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:15 +0000

    exchange actually mistake like slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
