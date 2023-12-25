5d075ad2cc741c643914c4648d9fd1bf83ba591d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:50 +0000

    flat clear fail interested near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
