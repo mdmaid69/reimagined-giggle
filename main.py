2d7941f39799ab765b62e66737302727f37b9267
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:56:46 +0000

    wonder dry advantage breakin runinto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
