e14682c37a1e7d1582c9d2b23a33e9b285e728ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:12:52 +0000

    print division voice rarely dress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
