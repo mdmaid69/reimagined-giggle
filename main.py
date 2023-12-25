92f15d2f49f25287743a56de8334b2744a7c1f19
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:28 +0000

    depend flow hate part separate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+for i in range(5):
+        print(i)
+def divide_numbers(x, y):
+        return x / y
