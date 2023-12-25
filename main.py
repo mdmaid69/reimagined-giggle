72b9baf6aa988b199ecaa316de037b4511070300
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:31:32 +0000

    gentle soil careful serve city

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+for i in range(5):
+        print(i)
