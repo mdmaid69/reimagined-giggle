cf9ff6445aad531136e066494d054a4901c754a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:57 +0000

    single fat expression agreement accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
