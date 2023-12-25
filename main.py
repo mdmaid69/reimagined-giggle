4de8b136e7e75e37cada3fbd65527f75b6cba1f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:30 +0000

    throw artificial without part equipment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_power(work, time):
+        return work / time
