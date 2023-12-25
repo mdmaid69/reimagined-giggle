b0eb0891426db751075d4d671636dbe0ba06bf81
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:02:44 +0000

    butter accept might angry design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
