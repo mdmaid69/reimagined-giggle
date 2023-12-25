2994b36258852a0e07214c1a32efd1f6603f3ad7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:14:13 +0000

    order press fight ilovedicks door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def reverse_array(array):
+        array.reverse()
