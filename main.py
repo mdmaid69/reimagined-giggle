8511feb4e45166a7d95f556519dcb2e36eb2eade
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:59 +0000

    count follow thought nerve visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
