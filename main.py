2da6b9b5d004bd9cabe060dca324a5a52aa56a82
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:00 +0000

    feature benefit pile surprising transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_counter():
-        return collections.Counter()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
