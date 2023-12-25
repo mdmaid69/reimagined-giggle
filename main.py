b5cb76c1f522518f3438954f3cf129f4e96ac217
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:18 +0000

    ignore children careful their suggest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
