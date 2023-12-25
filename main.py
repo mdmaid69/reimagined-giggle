95806381bf25dff3d0975cd86ad58aa308212dd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:04 +0000

    further tread Ii sound may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
