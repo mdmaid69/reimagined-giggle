f2ff03e95a8f79ebb7fe80053cadbae64522ebd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:54 +0000

    border behind inquire somewhere cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def convert_to_hex(n):
+        return hex(n)
