880a6a3ff4ba759456bf9d3c3a4abc99212eed69
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:57 +0000

    trap fight light OKorokay quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
