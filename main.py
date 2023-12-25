20c142ab919d4abd33aac550c9883cbe74f1652e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:14 +0000

    treatment will during mark straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
