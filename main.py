41a559cc18aaa989d2627ad7073b7ced04f1ff5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:19:56 +0000

    concentrate combination geton load straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
