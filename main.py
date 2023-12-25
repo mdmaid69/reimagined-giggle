d543cef7bc119b6094b16573795ad81d04d7cc69
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:18 +0000

    hand rarely smile telephone plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
