67c6c1231d50750db8e711677ffbff5c18873095
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:04 +0000

    part mistake smooth answer front

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import numpy as np
+print(np.array([1, 2, 3]))
