3aa18e7505c54ccc0aedc8affe5eddacdcfeae07
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:20 +0000

    if brush refer giveout there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def exit_program():
+        sys.exit()
