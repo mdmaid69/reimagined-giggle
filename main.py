52e0f3e0872a2f4ab6775f17f0bf68d89ad26e9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:59 +0000

    recognize beside wait label lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
