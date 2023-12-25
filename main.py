e5b08012e487a3c6486510e22c3ab05e0537a2e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:27 +0000

    throw price insist influence wear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
