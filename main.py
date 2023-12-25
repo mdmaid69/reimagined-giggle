a8efef3575e1ed5cf36300a6c40df0f42a39acbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:33:01 +0000

    snow news handle occasion quiet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
