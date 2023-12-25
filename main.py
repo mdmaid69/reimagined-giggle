fdfcebb0faed5d9e8a790e18ddad092bda74046e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:09:59 +0000

    print past exhibition spill be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_current_working_directory():
+        return os.getcwd()
