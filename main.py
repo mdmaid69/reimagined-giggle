d5855f4b6cd8cd736fb66b766d6fc807340580a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:41 +0000

    unusual perfect new tear off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
