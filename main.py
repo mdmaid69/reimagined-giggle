174c470184eb653da394afa198fee1f5c05bc002
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:44 +0000

    criticize bunch sit little wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import logging
+def log_message(message):
+        logging.info(message)
