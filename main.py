ac5e777a9d770be00a15aa9b260e87579e7fef66
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:03 +0000

    trip complain message admire start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
