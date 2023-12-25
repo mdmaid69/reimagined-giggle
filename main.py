6c7e2cf9cc9f6d43b5de620bd615a8e7c1e086d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:39 +0000

    slice mainly advice lookup hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
