d8e70fd5fffecc1368c1db40eacaa7a93cf2855f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:52:38 +0000

    572enter wire major such promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
