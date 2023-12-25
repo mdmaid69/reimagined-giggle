671ab9a17d32f2490bad93491a58a704373863d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:11 +0000

    necessary clearly rob cross describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
