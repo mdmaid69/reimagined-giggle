cc5ff9ecb90a5616a2f16ac2804793a1a044f844
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:12 +0000

    dry bye statement view cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def square_number(x):
+        return x**2
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
