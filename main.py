5fc776c8ed2413d58a6251935824e21ee72dfdfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:26 +0000

    target lower keepout they post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
