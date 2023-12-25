d3a86b4f6edde08169730a4f74e42c0ef7e28733
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:36 +0000

    frightened exact convenient seriously hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
