2b8140ab429ac927f960ba5aa27e7c911a417b44
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:02 +0000

    confusing left tidy before against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
