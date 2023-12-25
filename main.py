050963c5ddfbaaa02e46654633b8bcdd8b3faa56
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:15 +0000

    practise travel influence standard dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+print(os.getcwd())
