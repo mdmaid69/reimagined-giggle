2fb57fa344a934e9dcdecc7cd003ef69197523bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:54 +0000

    period price force break shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_current_directory():
+        return os.getcwd()
