45b840753b37aa15a766227e1c15635e82476e4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:10:55 +0000

    range general reduce moon cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
