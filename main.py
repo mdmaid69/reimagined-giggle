64a42651b66450e104c7457708aefcdac53658e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:57 +0000

    contact lend tread drawing fight

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
