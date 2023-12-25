c8d0f58ced304f349d57f128ca9bfb04cd8af7a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:45 +0000

    after forgive light reserve professional

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_density(mass, volume):
+        return mass / volume
