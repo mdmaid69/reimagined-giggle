cc29fbf60b1da6ee95363a5f287f5797b255b2df
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:20 +0000

    workupto turnback task inform autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
