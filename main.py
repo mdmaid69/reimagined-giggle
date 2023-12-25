7543d3693da7f12f86c52768375ef9492541f4c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:10 +0000

    tired another takeout full healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
