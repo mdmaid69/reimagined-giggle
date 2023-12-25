6b9f015d8b8acb966db441617c8a409aca2c721d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:28 +0000

    telephone congratulate unfortunate reasonable demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
