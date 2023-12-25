d4aca99b3443c2e2e7c9d95aa41a4d6012cd6214
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:27 +0000

    reduce rough point file stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
