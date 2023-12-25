6d0e7c15639dca205d67d89e1bc229dff4973a40
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:52 +0000

    close soft offend geton comment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import re
+print(re.match("h.*o", "hello world"))
