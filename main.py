4a9a2343587e643e18553027748b9ea9ab231b21
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:46 +0000

    strange say wear guilty public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
