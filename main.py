49145fb6f810ce43dc693efc6d8024fc0e4799bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:49 +0000

    Gg concentration close relax band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
