ac7a6c1dd9172d425e97eacea1afbf8bc5d738b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:04 +0000

    well contribute break mistake heart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
