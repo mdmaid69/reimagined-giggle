b7b924661cfb271c49f802f5cd4cf98611caa2c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:23 +0000

    fast want leg knowledge play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
