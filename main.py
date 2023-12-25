325da0d1acaf545a58209e4592f2f424e7e6ceea
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:30 +0000

    use opposite rare sting positive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
