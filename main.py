8635f7b1d31d9b960207fb6ea782a8f1acf7d489
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:43 +0000

    send exploration wherever workon it

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
