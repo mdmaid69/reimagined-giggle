23742aab127887b53941a705c742aa5f330647a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:25 +0000

    operate join ring ride seem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
