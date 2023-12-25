7853deaa7f4d3e197e1172b1db3b5a3e89ebd6f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:15:49 +0000

    back luck weather rest word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
