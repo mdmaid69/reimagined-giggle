d0ec9556bb681f621607c5b96b33edbe877848f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:24 +0000

    identify unfair behaviour earth argument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
