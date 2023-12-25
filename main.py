ab109e5d876ac9d18d7e418de3c3a87e466efadf
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:40:24 +0000

    sweep swim follow however day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
