d7b5e4e02badb0876457be2ec1244c79b9a0df10
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:09:07 +0000

    repeat cause ever later lesson

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+from collections import Counter
+print(Counter("hello world"))
