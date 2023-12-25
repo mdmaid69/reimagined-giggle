570618aaa93533a0a949d5e029fd3ff9a26e5ea9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:06 +0000

    anything 570enjoyable peace time matter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
