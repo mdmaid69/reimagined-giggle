8c707dcd27c9232ffe7ebaae0aa3b547f7bad85b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:46 +0000

    emphasis sign circle often hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
