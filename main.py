3d9c7b94a07d632dbbbde2930701b0f796d3bcb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:30 +0000

    murder measure attempt lonely group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
