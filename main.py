807dae689e105f970038c2234dae6543dc21a95f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:09 +0000

    571enough pure basically getout sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
