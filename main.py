22b1bd4df1afa963b33871b067152bb16578702c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:29 +0000

    wrap journey hello whenever serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
