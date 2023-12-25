4b0d2660fc31f2c62366c0bb637f46f677882efb
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:48 +0000

    bright roof trick react point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
