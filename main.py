300827463882ec853c49f46d75c60e1b3165880a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:37 +0000

    sale sharp worry uncertain cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
