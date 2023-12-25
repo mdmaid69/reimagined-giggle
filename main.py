032c1c84386e7c0be78d4ab1ec051e4a89918415
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:38:21 +0000

    point just pair confident somehow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_min(lst):
-        return min(lst)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
