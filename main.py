196e42168dec706210b7881ece1358f5b5e7f773
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:44 +0000

    control block wish connect please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
