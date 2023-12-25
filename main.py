acd3f83ccb30a960d118602e16bbeb0800d41a41
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:02:10 +0000

    city goout ticket nigga agree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
