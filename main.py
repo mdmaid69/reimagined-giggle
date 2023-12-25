fa69d2b260c483b8cb97cbd7b88bfb4572e6d426
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:02 +0000

    welcome regard polite speak collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
