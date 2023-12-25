e6ef7b839e3650c75967527868c182d136b40c72
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:36 +0000

    damage that available flat edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
