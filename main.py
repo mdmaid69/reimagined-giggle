8d12969c5d432e6b0c8c0a16525e58f8f448673a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:25 +0000

    street coat letter needle business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
