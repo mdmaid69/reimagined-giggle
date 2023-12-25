dd29c323fd8b89ff61e1ba3a8cfbeaba4e0c9751
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:17 +0000

    past limit control offer central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
