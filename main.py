93b3765e362443cc2c65845e622406603c79cd4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:32:46 +0000

    commit mine move sale must

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def is_odd(n):
+        return n % 2 != 0
