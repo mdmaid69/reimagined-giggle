b155830401d85483d66e545141fe08fa908e647e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:28 +0000

    thank use movement reaction warm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
