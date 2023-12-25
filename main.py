b9275933dd77f2c6c39c1cb8a77a17c1e9c362fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:48 +0000

    their eat ask food hungry

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def square_number(x):
+        return x**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
