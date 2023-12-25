03a079ba4435fd69e25832d4e756a6dd20461ce1
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:55 +0000

    wire manage sharp accidentally brush

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
