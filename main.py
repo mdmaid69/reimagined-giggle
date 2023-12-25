954c9211f459b6224e30a62d017fc7b8ea72ce54
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:51 +0000

    twist powerful noise together sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
