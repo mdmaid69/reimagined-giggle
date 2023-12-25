80acbce9177a4ee47c6acdfb3cd47cb5b69b58cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:03 +0000

    section dead Cc direct by

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
