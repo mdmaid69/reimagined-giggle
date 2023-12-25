5a80feec82237c3fe4877a8758beffef932da841
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:22 +0000

    in immediately relationship shout confident

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
