af7c1db4f7c58b76c7138af71487dbc2a0b372cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:45 +0000

    holdon fast turninto bring half

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+for i in range(10): print(i)
