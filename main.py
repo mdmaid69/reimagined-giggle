29deec9c759b1ed69b26ef7c69e0d016ccd2b8ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:50 +0000

    up pass failure fat spread

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
