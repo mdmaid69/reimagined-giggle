421941b80f908b3c0ccd071bee3622b2bcc05b3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:26 +0000

    place art test proud behind

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def count_elements(lst):
-        return len(lst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
