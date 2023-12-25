d0e51a47c0316735a69e1831185d1948d274f523
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:13 +0000

    later this hungry everything switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
