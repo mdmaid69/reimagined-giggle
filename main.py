e69a75db472c01f5cfe17a8c1aafbec1a4e6258b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:32:14 +0000

    little valuable wave please anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def convert_to_octal(n):
+        return oct(n)
