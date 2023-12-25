a4641fc3803d3cb65f63ba11bbfcbc6f6859b8c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:49:49 +0000

    impressive apply hurry sweep provide

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print("Hello, world!")
+  def remove_duplicates(lst):
+        return list(set(lst))
