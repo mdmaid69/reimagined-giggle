f18efe46d1254c0b6747a655e889eb2edf099955
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:58 +0000

    beyond impatient cover precise suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
