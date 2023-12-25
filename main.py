8af84c010c604100047767b28df419bad4b0b557
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:36 +0000

    generally quality of clue precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_as_bool(array):
+        return bool(array)
