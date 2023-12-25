bc9a7a43e8cbbb0f70f7bf072d1be41594f91012
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:57 +0000

    empty cheek even remove female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
