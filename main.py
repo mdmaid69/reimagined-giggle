84cb9f5cb087bea5c3eab5cfae7b52f2adf2507a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:09 +0000

    important comfort blow bill correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_typecode(array):
+        return array.typecode
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
