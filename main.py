3797c0952ac0fa333aecd5f6b84553d4347e714c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:54 +0000

    expression waste stay force back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
