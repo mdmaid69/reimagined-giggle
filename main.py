ac97801c977502c8a3d7a32453b9f95a92968ab1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:53 +0000

    listen advanced thankyou chin base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
