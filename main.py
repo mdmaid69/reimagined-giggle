e07555c6270defe64310e24dfa907e6d23aa3803
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:06 +0000

    deliberate turndown plant weigh rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
