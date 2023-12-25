d4d1b24747ee9f4999377c0827e8ef559b48c089
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:13 +0000

    begin pay shut picture make

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_float(array):
+        return float(array[0])
