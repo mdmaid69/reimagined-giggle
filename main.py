74fae725ff0530710192e1d09cc2f99273fdf19e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:12 +0000

    schedule loose situation add Ww

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
