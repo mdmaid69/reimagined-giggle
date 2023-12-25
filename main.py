ca4adedf368e5a09ec59c01b2781109158ef1e80
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:57:05 +0000

    safe word represent free turndown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_directory_name(path):
+        return os.path.dirname(path)
