16675cfef734b2f6fc49c291a3c5d7521dbf7eb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:28:22 +0000

    own sign hope lookback(on) section

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
