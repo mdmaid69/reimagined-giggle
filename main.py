b8c7aed8aba9bf366cf88cd4160ad5a921a5ec00
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:50 +0000

    gate pay show 179bite us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_float(array):
+        return float(array[0])
