4b5c0d8fbfb83c33a1aab4877b470d21642a5ea5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:52:50 +0000

    loss though label whichever storm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+print(sum(range(10)))
