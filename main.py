e2a7c0a1cb9d350e56f54e3c3360dd4411c2ff4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:52 +0000

    plenty record married container record

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+print([x**2 for x in range(10)])
