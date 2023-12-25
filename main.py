7ca2285ad976757499b0bd5f3ff6fa0d6496f7b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:49 +0000

    Tt crisis blind cope she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
