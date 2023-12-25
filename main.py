0410f0e08202bcd79e5a4d7303cf012ab9cb064f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:43:00 +0000

    mistake capital suit business without

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def sort_list(lst):
-        return sorted(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
