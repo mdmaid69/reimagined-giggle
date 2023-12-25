c79e698bfc68604f52c3eca0b50787449da431b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:55 +0000

    equipment basis compare thank central

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
