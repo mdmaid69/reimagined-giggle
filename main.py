4487c58dce8ad7de2c2bfd89f42731e54135f270
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:14 +0000

    insist intelligent repeat choice speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
