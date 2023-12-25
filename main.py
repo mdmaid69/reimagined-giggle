a1e98178a235bcbb9cd1cb7d54ef71ca894befa8
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:54 +0000

    long square wise disturbing slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-print(os.getcwd())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
