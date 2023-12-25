954fdfef490984b89c0b0730ba84893fc3eb8bb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:35 +0000

    needle last trick environment branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
