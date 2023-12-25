c0492f73ca73e0765e678f86ba6c284ab8a10dfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:03 +0000

    comedown talk bet build safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
