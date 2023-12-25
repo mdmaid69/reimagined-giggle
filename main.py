14c331904c21162c8ef7a2634cbb71b7f60f8382
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:35 +0000

    ignore dare home spoil fierce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
