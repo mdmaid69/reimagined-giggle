3d024770606df355a5c771c32cd11c2057c2b621
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:58 +0000

    keen around process point copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def is_odd(n):
+        return n % 2 != 0
 import array
 def set_array_item(array, i, item):
         array[i] = item
