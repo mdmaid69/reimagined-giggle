9485b057daceeb0a22c7ff6422ee8fbda4b93a57
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:59 +0000

    muscle crime clean plan business

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
