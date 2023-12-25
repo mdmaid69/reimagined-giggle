003fbfe46864cde124d845d3186b40dfb5489d1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:40:09 +0000

    custom increase conversation least send

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def append_to_array(array, item):
+        array.append(item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
