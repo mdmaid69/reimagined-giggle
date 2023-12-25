a203c0be8cbfd0c31dcdd46aa6fd0a810b458985
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:47 +0000

    clue appearance study rest list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
