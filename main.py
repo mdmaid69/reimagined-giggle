5bde90f04c4e950e4ffbe4d5c1447ec13c33326b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:20 +0000

    insist air invitation 570enjoyable him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_string():
+        return collections.UserString()
