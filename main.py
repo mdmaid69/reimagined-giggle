502ad70dd104eb31d39106e0d538228fa512f750
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:35 +0000

    geton few information children word

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
