1f49b25e54dfd85891b1e3f1ef146cb2b812719f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:21 +0000

    comeover swim crash considering check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
