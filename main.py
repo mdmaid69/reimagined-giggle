7f4fe47816b6b545c8e5e1961a5e29fb013fb16e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:17:15 +0000

    common warn dish hope by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
