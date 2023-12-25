09315ea696f13079f44dbdebdeaaae959777687f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:10 +0000

    great be prevent fortunately combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
