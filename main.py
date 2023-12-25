cf6959524a7169575553b57f423dfd3b9d6bc670
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:20 +0000

    name measure straight bound female

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_exponential(x):
+        return math.exp(x)
