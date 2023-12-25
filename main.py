5fa5a8e783a1711db7ee4eaf34225c237ea7ea62
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:16 +0000

    serious interruption lucky centre pupil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
