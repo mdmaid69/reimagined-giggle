12f361030c4516c379456dc558ce583e185f493e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:30 +0000

    future reaction defeat soil still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
