735182d82e72a61b0f7484d17c00fad97a75d2e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:31 +0000

    replace blame safety force chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
