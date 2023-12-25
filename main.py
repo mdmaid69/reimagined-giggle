7645314f2c8b744eb8a0429e08dcfa996c63471b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:27:28 +0000

    size fasten centre hole flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
