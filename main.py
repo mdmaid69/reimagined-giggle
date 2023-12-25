3d9c93dd0c19835135e1844e155be8701860ca93
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:41 +0000

    set problem admit 566end remain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
