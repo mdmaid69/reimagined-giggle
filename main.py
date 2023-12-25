13e7b2a3825c4609101bfa63359b97a137e7254e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:28:07 +0000

    concern pipe lead recent sell

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
