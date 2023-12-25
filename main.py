f6834feed4dd0e6431f1c7d244cf9536ee588414
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:39 +0000

    quantity introduction worse ring spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
