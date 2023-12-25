46b7b1ccdfa074ce6f175edf3ad3ed54da96db36
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:21:12 +0000

    short argue recognize crazy shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
