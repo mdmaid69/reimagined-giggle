7933cffa5e24a83883e959b8717d4b1d3bbc3db7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:29:44 +0000

    respect colour bored dress coffee

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
