67ded497f3207291fef036f3d8b369a233d594c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:27:24 +0000

    Zz tragedy aware help create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_unique_words(sentence):
+        return set(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
