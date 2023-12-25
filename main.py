10e547a610d0b9f607fb25e53ca733fd1288575d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:56:17 +0000

    person to occur guess scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def print_python_version():
-        return sys.version
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
