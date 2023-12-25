fdb95a161d90244061d7c21a395ce537a1b5f73d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:54 +0000

    conclusion aim persuade comeup dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
