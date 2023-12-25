8062d15183def33b450ec76b26aca5dce7ea8289
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:07 +0000

    its nice somebody battery criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
