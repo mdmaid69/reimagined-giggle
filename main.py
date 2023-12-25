e0e21c93839e5ed0ecbfef1b0ae47d390e7f52b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:27:21 +0000

    excellent line themselves test about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
