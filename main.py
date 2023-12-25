21297b24a29d238c8e9e8ca1b466d6f55eb30a20
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:02 +0000

    send blow our equal open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import datetime
+print(datetime.datetime.now())
