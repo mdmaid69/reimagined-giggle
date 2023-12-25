60cfc20419510d8ae8e6d5245d1a596f80b6574b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:39 +0000

    afford confidence oil broad minority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
   def get_file_group(file_name):
         return os.stat(file_name).st_gid
