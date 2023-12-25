3d4e5dff3348be145591c617ee46d5e6bbf119c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:23 +0000

    tour guard stage car flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
