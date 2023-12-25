c2914688e6222e3ecf73bdf979da4c597d865351
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:15 +0000

    national 569enjoy giveout fall the

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def is_even(n):
+        return n % 2 == 0
