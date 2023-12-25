123d2d04e1ad06307e6fbd96b373daae0bb937d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:42 +0000

    check still communication whole hard

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_index(array, item):
+        return array.index(item)
