39b493f4ea0b502cfc02ac6007463f86e8c13738
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:01 +0000

    list opposite before doubt anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+print(random.randint(0, 100))
