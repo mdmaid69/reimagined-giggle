29d616291ffc5f7d4b65ebbbb440294f307cfb46
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:19:23 +0000

    great Ff extremely hole arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
