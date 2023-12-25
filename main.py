941677f17095c6c49c24eaf066bc0e87f2683575
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:59 +0000

    write inquire hot damage seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
