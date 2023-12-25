b8a3fa7e3bf7e78c1f2387434d383a88819e74bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:18 +0000

    about mysterious beside mine damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
