a261cce4bae05d22c0e407ae7cb4d1b48a3c1005
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:25 +0000

    borrow explanation every deliver bang

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
