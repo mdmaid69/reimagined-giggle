c0ab315673014f1ff6b11aa0c17493236bc49e06
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:47:22 +0000

    pause train accept ocean colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
