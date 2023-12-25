fd9a0091409b0ad219b0e3c41179a3097171158c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:28:13 +0000

    rain excited decide disgusting science

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
