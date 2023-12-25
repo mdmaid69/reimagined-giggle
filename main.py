e090ef290c762a6cdc1a3059a2dd4a476d2a0f24
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:27 +0000

    help suit disturbing round mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
