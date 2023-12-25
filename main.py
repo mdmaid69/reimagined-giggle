a426ffbf787e7051a0820690b9ee4b82ff140dc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:10:47 +0000

    young achieve lunch reply better

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import random
+def roll_die():
+        return random.randint(1, 6)
