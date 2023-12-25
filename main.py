ee4ed2ccdd29e121e6352077894a9c9a94cdb053
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:16 +0000

    dry bet course this die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
