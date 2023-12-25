c6eecd626ad8ad2da235cec48c50d54f9a4d18a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:34 +0000

    Ww exact 565end turnover load

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
