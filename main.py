f37fc2f4e1def4e2d5af5de06fd150fd155f7a9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:10:21 +0000

    whatever doctor inside welcome ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
