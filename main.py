308ebfd804f3db546d171469b0560c1bf88b1c7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:22 +0000

    fair hang weekend size minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
