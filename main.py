9b2de2362f2021fbc100a31990b500e37f74e87c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:29 +0000

    since die label thing their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def convert_to_binary(n):
+        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
