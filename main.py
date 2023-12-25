e9b2ff0087b09b630ab7cb45134b5709a26d243b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:32:00 +0000

    tough past treat badly defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
