e9660e62af7908d2a6113dcab724569b7fc2ce3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:10:07 +0000

    worth cut human below definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
