75cabd88dc3e80f587f707a37c10587d6784d048
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:09:27 +0000

    their force fit knowledge keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
