f46ef9703cee3513c59e8ad60d43c043e16c1fc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:00 +0000

    approve invitation fit breakup emphasis

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
