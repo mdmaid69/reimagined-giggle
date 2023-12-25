c78b063fbb6fa031fb10bbfb55acd15cc3238b8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:00:50 +0000

    effort keep sweet once opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
