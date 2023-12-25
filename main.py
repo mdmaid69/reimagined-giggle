bffeafc0746013c611b907221128cf1cdb17a115
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:27:06 +0000

    uponon more message afternoon argument

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_max(lst):
+        return max(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
