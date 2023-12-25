2a6c02b71fa055fbc6598851bf4d7fe4aeaf369d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:41 +0000

    rob contribute cope run base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+for i in range(5):
+        print(i)
