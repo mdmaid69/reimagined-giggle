aadfe98167d30c61300cde42e5117433b5418849
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:19 +0000

    comfortable write kind information Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
