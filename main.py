d35230974ab715da883d45d7a2c864d403e0f230
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:51 +0000

    pronunciation like law repair again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
