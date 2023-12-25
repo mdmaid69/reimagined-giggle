1e49a9ac0f5c0c6c3fa762e5e91f92cfc188b231
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:26:06 +0000

    his trousers bottom violence satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
