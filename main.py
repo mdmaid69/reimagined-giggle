3aa624e73cf9590010dc502111d958593f48e063
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:52 +0000

    holiday escape fix cold brush

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import logging
+def log_message(message):
+        logging.info(message)
+for i in range(10): print(i)
