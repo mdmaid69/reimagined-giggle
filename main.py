094a657cc82f5e01ab7e76c940115a2f3a7d0b47
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:41 +0000

    comment stone beneath minimum store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
