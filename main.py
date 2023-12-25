764e55884f57ce9a48c52b4e19aef096681f01dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:50 +0000

    order unusual arrangement experiment view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
