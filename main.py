3f6526de354187fae92e35ecd90f08f2100c3161
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:57:47 +0000

    effect above extremely big shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
