f227b09691f2d00cdf3f601b2c3452f5f38a7c92
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:44:05 +0000

    serious disgusting off comeoff opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
