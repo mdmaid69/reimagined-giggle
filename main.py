90bb5c8dee7045c51b013a2c82f54adb8441136b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:01:57 +0000

    loan absolutely nurse stick tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
