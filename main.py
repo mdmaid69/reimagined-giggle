eae546910e3b15d5daf116f747fdd14e56f4980d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:35:48 +0000

    direction holdon difficulty forgive blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
