729613988a8a3fc62b884de47920ce5328b57f45
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:08 +0000

    maximum feeling knowledge search cum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_area(radius):
+        return 3.14 * radius * radius
