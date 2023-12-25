abe802b9f9d7f8b5aa16317dacda44aa049a54f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:37 +0000

    both polite better clothes reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
