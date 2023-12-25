0cf17066f667d3eac960ebe9c97de8e2d201722b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:47 +0000

    no flavour either perhaps neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
