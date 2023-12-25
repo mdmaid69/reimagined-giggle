2c5e03bb2ba5314508005224dd3a7df20c50d018
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:35 +0000

    bringdown possibility share skin swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
