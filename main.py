cb680bf1a2c1e03e7826b36f50cf635630491682
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:41 +0000

    centimetre round urgent sore slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
