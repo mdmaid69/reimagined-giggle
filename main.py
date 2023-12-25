7ea21fbcc775220e4ae6c21365411885b3bea8e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:10:08 +0000

    debt journey special offence contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
