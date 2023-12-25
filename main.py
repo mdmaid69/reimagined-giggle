cb3c3f2f53fdb64a494fc65a491303a60cdaf713
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:56 +0000

    ignore upstairs comfortable interruption influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
