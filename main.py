c9ba9d6dc984b1588069d0f969e87dd48490f577
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:03:30 +0000

    ready unkind runout anxious delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
