6afb009d28b20a54bfdeb77512870c244b3d5134
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:53 +0000

    complicated grand glass clear pour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
