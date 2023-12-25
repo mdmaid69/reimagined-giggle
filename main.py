69db053cf86e5056b646697465866f54b4379347
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:44 +0000

    much dream force prepare worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
