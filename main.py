ba8eb4ba2e2511727cc2dc8c8e2e54d222089b31
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:08 +0000

    himself damage behind few love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
