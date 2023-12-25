3b138c2fee9f5f1bb666b551d2a3ca2ad1321235
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:16 +0000

    polite love bill unpleasant somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
