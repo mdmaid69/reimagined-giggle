178c73c666e2fe153a03a60ced4d31b5c37f79a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:36 +0000

    relief tidy drawing movement expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
