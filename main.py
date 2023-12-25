8d94e90f81b7785ae1ad506796463ede6331f394
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:38 +0000

    save damp interrupt score flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_dict():
+        return collections.UserDict()
