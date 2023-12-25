eb6c0e83f2c66e9600daeb71a77effe252c2d30d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:11 +0000

    block spare trial common instant

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
