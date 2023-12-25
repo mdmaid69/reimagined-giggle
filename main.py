067265760f43f514a8028732eeca3d164a7e8aa2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:25 +0000

    excited introduce burst strict middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+x = 10
+y = 20
+print("Sum:", x + y)
