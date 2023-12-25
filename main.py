5daeadad06fb047d23be65eb5bda0f6a88b289e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:47 +0000

    probably refer ago comedown joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
