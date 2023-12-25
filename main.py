10cde9b812cae01412e75852acc1b02e2107b50b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:26:13 +0000

    design argument further question either

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pressure(force, area):
+        return force / area
