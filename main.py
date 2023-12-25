56cadaca14461caf3be032f9424acb60e0ba8964
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:10 +0000

    whom software bank suffer child

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_queue():
+        return collections.deque()
