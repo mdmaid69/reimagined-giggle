3aa19ec9d61fc52b89e22611f8067524288a82ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:05:56 +0000

    final speed poor confuse opportunity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+print(sys.version)
