eae2714fa754bb7b15eeffc800752873fea09320
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:46 +0000

    violence clever keepoff uponon limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
