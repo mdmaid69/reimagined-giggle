20bc056eb7116296ea628b00895b2dfb5d39735c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:47 +0000

    error neither whether straight our

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
