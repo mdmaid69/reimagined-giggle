c966d0b922314d4c633ec31e58579573bfc53da2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:07 +0000

    team need central make cross

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
