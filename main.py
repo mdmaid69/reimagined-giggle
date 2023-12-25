03447cf3eeddc3549df0dc69a0cab1c632b75037
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:31 +0000

    traffic that trap scrap company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import tensorflow as tf
+print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
