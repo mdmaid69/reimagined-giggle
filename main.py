71cadbe18d10ae0736d0fb907458503e48397cb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:59 +0000

    Mm combination concerning throughout problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
