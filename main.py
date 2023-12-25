9c567b23a808ef8caa04a28b949e2fca554292c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:14:56 +0000

    fresh likely popular well reserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
