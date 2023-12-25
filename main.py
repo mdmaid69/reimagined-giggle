85f3ead04e4f862bcabb0f85b49b1e6621a9a256
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:29:44 +0000

    difference concentration laugh building ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+print(json.dumps({"name": "John", "age": 30}))
