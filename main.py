77fea7b3e4e76f0d6e27cfccdbc7c0c638f0c749
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:25 +0000

    listen behaviour takeback delicate bend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
