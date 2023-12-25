6ddedc996a9b20c1afcc4307033260f101e46284
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:30:49 +0000

    rush across complete interruption inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
