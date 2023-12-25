4ed0df4363c682dac3dda280d4e54bd930b570e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:33 +0000

    shape hers foreign bush hospital

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
