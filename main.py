bd94af2cfc7bef53fc0499b3ffe3c00a2da563ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:24 +0000

    concern joke city keepaway giveout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
