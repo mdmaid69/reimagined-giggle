146027285f95e404b9bff2fd9775b0af3172419f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:05 +0000

    stiff talkback deserve Kk of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
