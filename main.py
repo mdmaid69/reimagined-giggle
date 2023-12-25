1533c94326fc8bf8d2b18f611cc688abc5ecd019
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:49 +0000

    interruption wise comeround blow pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
