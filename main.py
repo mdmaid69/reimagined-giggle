e869d28eb873eff4a02114635f4f4fbd5a83a6c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:26:51 +0000

    amount affect train rise minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
