99d3e4e4083352aeeec6228b4177b8d9a3ef6ac5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:11 +0000

    early study find central middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_even(n):
+        return n % 2 == 0
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
