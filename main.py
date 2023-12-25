c2b57a20dcff4f53a643028f804411f7682b0118
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:36 +0000

    free protest all takeoff uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
