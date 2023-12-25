394106dafa9d23347b7eb264bcbae7413e84db15
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:18 +0000

    course steady exist be appearance

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-print([x**2 for x in range(10)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
