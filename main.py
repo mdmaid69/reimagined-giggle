286de1509cc86140f5e3643049923191b2f791b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:56:43 +0000

    man fortunate point anxious present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
