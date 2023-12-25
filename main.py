751d8c48c72587a1df15c35edb258f198101a659
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:16 +0000

    same key damage least makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
