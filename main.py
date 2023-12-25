53570a7197ddf97737373ea2f96e327d23f5a597
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:43 +0000

    across by push window connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
