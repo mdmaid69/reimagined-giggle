6be74fa3150d5f8fd7fe000209f26d5dc08eb2ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:54 +0000

    method bringforward also still strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
