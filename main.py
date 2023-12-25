93a1a8b5ec7b23673088ddd35ce96b095f9a7b1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:30 +0000

    fight place cost this man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
