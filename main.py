8b106bace18115267f43b8474ef2488ed63fa76d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:50 +0000

    cause news sleep great prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
