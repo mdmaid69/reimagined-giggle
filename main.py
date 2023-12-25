4b9379b19dc72acdff9897e3529ce521577a695c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:50:41 +0000

    keepout round do insist sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
