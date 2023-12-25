6b8b3e72149ea6915629b75e65153af42f24ba63
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:37 +0000

    job negative natural improvement tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
