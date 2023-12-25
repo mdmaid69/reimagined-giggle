26cfae61c64c358a68ea382520d9626a0cf5d957
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:13 +0000

    thumb wander mixture yours test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
