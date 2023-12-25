bdc93a8063510de3d1b4e677391d1a4b9841fcdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:33 +0000

    share taste right middle lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
