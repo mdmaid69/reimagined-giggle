03b409ec4c8831c035a95387358e1ef3ad46dce7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:24 +0000

    on wave beyond duty declare

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+print(sum(range(10)))
