7ca46ce888167256e27397540f87b2cb73dd1eeb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:02 +0000

    feed equal last sting account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
