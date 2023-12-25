51ad224778dcaf41dd8c58728119e4ebb49aef6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:48 +0000

    history myself belong keepout Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
