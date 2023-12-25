a6958f25e4a82c280adf5c132694cb7c5c33ebed
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:16:07 +0000

    relative base transport activity upstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_float(array):
+        return float(array[0])
