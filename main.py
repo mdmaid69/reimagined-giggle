78b93fe7c0202aec999625ef7ce0e9c5d4e49f3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:22:11 +0000

    punish rob before because pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
