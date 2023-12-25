8a84d210fa36d9d8a405a957fba09c6c10d2e70f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:06 +0000

    wind cope port estimate comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
