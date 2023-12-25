9f71057e46323316a444c810db36fc786278396e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:12 +0000

    hot rush container teach flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
