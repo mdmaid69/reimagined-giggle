e44d12dcd172ba4f2029d1ac75f7e45ef6290d59
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:09 +0000

    name in compare choose tax

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+from collections import Counter
+print(Counter("hello world"))
