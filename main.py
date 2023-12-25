996c22fc08913284683be1b739dbfc60660b3246
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:40 +0000

    ask let arm 570enjoyable tall

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_max(lst):
+        return max(lst)
