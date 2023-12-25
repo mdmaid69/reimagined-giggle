6223aef0b81d61f015d91823753913130b327efe
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:30 +0000

    beneath ride through extraordinary content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
