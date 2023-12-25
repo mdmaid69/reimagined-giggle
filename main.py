04bc1b9edfdaa738fce950e592a720ec100a5801
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:44:31 +0000

    reliable pattern check type relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_str(array):
+        return str(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
