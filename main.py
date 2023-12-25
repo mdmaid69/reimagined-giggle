d150114ea4f9dda2fa45042ab6efd16cac33816a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:43 +0000

    rid hers report excuse form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
