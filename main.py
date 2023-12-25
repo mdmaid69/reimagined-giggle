69d7bd4d71b0d3fe1fed673a0b8356ca3d8642a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:59:00 +0000

    score at suit up pretty

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import platform
+def get_os_info():
+        return platform.uname()
