193677b07fe28ad9bba86ef686389ebea1bbecd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:38 +0000

    wish afternoon habit mystery mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
