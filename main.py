0d07e51ad812ac3f9b98843f86831ad5089712cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:50:02 +0000

    each wire evening holdout by

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-print(sys.version)
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_ordered_dict():
+        return collections.OrderedDict()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
