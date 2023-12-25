21af7330ee9cbeb62fe2d21129798bc85b15fb91
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:32 +0000

    person slice last sleep think

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
