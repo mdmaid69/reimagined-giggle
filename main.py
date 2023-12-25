3d886b80d7dba0526af0fd16e59c2069c9d0d179
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:55:56 +0000

    appeal string weak angry danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
