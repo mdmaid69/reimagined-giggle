a71595e4800edad079004b77e9f05fda7f405723
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:43 +0000

    afraid reserve sensible trick fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
