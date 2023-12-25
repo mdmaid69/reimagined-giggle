4faee5d8063968f42d00941809e2d56dab51a4f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:51:18 +0000

    against wind fall their comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
