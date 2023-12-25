9eded827435d5ca845f65e08495e511466f31480
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:00 +0000

    collection once getup height call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def convert_list_to_array(list, typecode):
         return array.array(typecode, list)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
