f474f2cd4cf411c1be95f775ec6d6f85afd24048
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:04 +0000

    experiment thick year side party

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
