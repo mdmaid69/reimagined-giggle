7656f3947155af184659d54e8dbb4df2ed3f4cd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:08 +0000

    turninto shoot attitude community giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
+def count_words(sentence):
+        return len(sentence.split())
