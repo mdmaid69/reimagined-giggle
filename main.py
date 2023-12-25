bad48f88639e09ad42875c2fd45ae1d3012b8117
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:55 +0000

    declaration aim pass second friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
