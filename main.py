2624e38a0f6388329e16b9d7645b9f5134f549af
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:44 +0000

    speak threaten stare attract here

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_power(work, time):
-        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
