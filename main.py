aede739d41db35a38997b778fcf37900084b760a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:06 +0000

    relationship eager act health spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import re
 def split_string(pattern, string):
         return re.split(pattern, string)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
