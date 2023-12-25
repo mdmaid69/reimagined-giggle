c21a84830f060c2b051a3211e43e4fc071b6be99
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:49 +0000

    work mistake progress partner argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
