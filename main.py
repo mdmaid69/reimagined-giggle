63b85d7d6fb9e74ac5bd7728b19a385f583af86c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:16 +0000

    centimetre out heavy promise call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
