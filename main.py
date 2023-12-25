4f6225f97cba6ed6354a06b92d206a067dfc8df1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:23 +0000

    offer afterwards present hold scrap

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_density(mass, volume):
+        return mass / volume
