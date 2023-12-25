fbd3459b66652766d466605c6b5b6ea868c24961
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:36 +0000

    heat comefrom conduct rarely bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
