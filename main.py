94f2be9886ae131d8403da0fec20b15b26d6ee8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:01 +0000

    571enough average later detail ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
