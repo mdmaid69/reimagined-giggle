454cae462b4f8d96c99ce6447a1917021e24c489
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:40:18 +0000

    wander satisfactory can correction sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
