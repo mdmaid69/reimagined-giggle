88894d359d064341de177fdfda9cc19846a7dcd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:12 +0000

    dream most cope after goup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 def calculate_work(force, distance):
         return force * distance
-def calculate_power(work, time):
-        return work / time
