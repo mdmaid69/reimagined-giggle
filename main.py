7f993e9e878ae4366f10f560e9f38a5a60777704
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:05 +0000

    dream maybe know past deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
