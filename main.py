d00f6c4b977ac889d584038edf29196ef41a3680
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:36 +0000

    able shut correct purpose tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
