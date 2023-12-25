7fb740faf6a488bbbba0f3616f0abf290fccfe67
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:47 +0000

    repeat solve interview branch sort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
