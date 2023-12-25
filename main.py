618723fa831ef71e0a8c056e60ed2548dbf05b23
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:23:38 +0000

    entrance another smoke means vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
