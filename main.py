8ca8aec872d815b1a1df1e1c262c86d30b5ac59e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:29 +0000

    influence shop position church strange

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
