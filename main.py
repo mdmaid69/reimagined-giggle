cdbf61d8eb65d5f9237b1497974b78155e25ab36
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:31 +0000

    obvious twist fashion soft when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_list(lst):
+        return sorted(lst)
