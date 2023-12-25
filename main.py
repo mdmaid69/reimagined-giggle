57ccda8c46e59cf5670309abd87a0e7933785a73
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:21 +0000

    cover bite guide eat artificial

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_repr(array):
+        return repr(array)
