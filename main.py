37a0e1ead1cd25296889cffd78d247dd867d636a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:11 +0000

    them tip scrape recover major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
