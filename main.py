f116cfe332274e8cd230a2f2712c9e51a3142d34
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:58 +0000

    occasion Yy side 569enjoy evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_file_size(filename):
+        return os.path.getsize(filename)
