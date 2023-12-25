646fbde824075f481d3e270511f4837a2e71b8c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:46:56 +0000

    begin vote style powerful frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
