f16f1d6e690835501f64305e446976ae1ce199bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:55:04 +0000

    window brush shame talk worth

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import collections
+def create_priority_queue():
+        return collections.deque()
