90e9f4b3190de82ddd00c8b98a7a21a394700142
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:05:16 +0000

    fierce close concerned kind debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
