2637a044026319934da89307c34f578d5e2f388f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:13 +0000

    nigga second always both trousers

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
