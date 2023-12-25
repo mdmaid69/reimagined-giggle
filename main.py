8a5cf313bdcea896ac13559500fbcff90998c40d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:26:41 +0000

    reason this job expression free

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
