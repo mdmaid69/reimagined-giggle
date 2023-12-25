293fd8e4db4a62435f88943ee8d29b1ca0cedb53
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:25 +0000

    instant upset giveup by niggaacademy

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-import os
-print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
