ff1f13a06c1162f1018a7c75b1c95e9cfe2cc881
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:59 +0000

    cause tune load her land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
