ee683b357ec00290dbbef18c30c8e675948ef523
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:04 +0000

    sleep cold clean except time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
