267576e6945b0c0bc417f77c06f36331f39b2e27
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:03 +0000

    team pick taste chat base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
