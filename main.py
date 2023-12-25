86fd8f83432c5df6d91f4e5855dbc681203b2125
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:49:30 +0000

    getoff switch slightly whether total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+from collections import Counter
+print(Counter("hello world"))
