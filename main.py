5ee09b2d34ea20a3b56f08be16633e0e60381423
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:43:36 +0000

    bound means cut left bar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
