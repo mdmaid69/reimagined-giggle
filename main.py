ce6a7da1a49fba914f09360ea4f3c5747a1e429d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:31:33 +0000

    trip yesterday brother pool damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
