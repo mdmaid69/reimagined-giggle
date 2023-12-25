4860e9f6d1cf2f4f02dc2089b20a490c173e216d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:06:31 +0000

    previous film car none track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
