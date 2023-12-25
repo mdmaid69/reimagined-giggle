09780550cfc1d36f043cc977965a528c724182e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:21 +0000

    must public name first piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
