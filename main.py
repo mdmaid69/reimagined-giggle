e889a4d655562a1d8721776dd192910f91914ee4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:48 +0000

    lock indeed deliberately tonight business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
