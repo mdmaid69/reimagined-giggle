d638c2eae0b58f6ad994718dc7b2fb9624825233
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:55 +0000

    feeling someone dish breath exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
