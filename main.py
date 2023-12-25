3e811935fd3e7f72d42eb42461d63d6c45a970ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:05 +0000

    price film war underneath Ii

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
