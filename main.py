714ba31df13cb7b96b523ca83c4d3bfec12fd088
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:55:21 +0000

    could sing group office somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
