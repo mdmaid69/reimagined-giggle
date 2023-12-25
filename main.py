ea5d41d7c0d18aa63f945140936d0657b545fbe9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:41 +0000

    drawing Hh inform past address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
