0c20dfe4c8a58a330f7a956371d3d21f7d476a82
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:15 +0000

    phone emergency plan sort favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
