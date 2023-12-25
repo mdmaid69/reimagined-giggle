190c2e6ddc8586e6c41866499567282bf9eb954c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:49 +0000

    demand university before certain accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def convert_to_binary(n):
-        return bin(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
