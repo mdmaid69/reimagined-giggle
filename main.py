eaf53f1540c001528421dae2d1dff6e88ed514a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:26 +0000

    man difficulty crash main afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
