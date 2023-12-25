a6aa91f9786d7fd6a63fc7ed5eda9371d9d9bab5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:13:05 +0000

    set tidy zero colour entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_volume(length, width, height):
+        return length * width * height
