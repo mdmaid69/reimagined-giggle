dcb606fb614614c513f03905ec12625d4ff5ff84
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:02 +0000

    rent offence turndown together afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
