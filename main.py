716fb88f7a46773d403ad2c4d9157bf76d6efbb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:18:39 +0000

    entrance turnout over nose letter

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_power(work, time):
+        return work / time
