8aa07981545d08ab085b3c264fdba2d97c1605ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:19 +0000

    rest prove mistake block wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
