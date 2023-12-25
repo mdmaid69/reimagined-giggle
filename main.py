6e72d4ee860228cee0aca02b6f5a80a19a7e45f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:32 +0000

    dangerous world time study suckme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
