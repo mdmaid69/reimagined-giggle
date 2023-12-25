3383e125fc8db0f8d2310303d37c0ce76c19c40c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:14:45 +0000

    expensive outside disturbing present final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pressure(force, area):
+        return force / area
