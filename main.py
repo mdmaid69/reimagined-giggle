5c492b5d698a21d821c655ebbba344d39e3b1d30
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:53 +0000

    report reject watch smile weather

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
