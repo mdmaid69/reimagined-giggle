65f47bff6bbc571316e433031b2581c975d3d61e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:27 +0000

    eager church base of speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
