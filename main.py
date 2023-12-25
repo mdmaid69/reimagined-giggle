0cc2627df97c2e1d8957f16420ca88ff5296269b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:42:25 +0000

    where draw rule level natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def multiply_numbers(x, y):
+        return x * y
