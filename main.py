068ce5dd3175b3ede8ea4d59127336bbfbc19838
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:25 +0000

    exciting no fold square free

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
