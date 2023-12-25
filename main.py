047068f5c87ec7781514c4b8b95cc3ed75b60182
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:26 +0000

    welcome could escape laugh spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
