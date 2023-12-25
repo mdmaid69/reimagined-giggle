55cee1f752f84a13f1732d888f7f7fbf65eec803
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:44:39 +0000

    neither address fact cut temperature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_odd(n):
+        return n % 2 != 0
