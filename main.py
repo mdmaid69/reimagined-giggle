388da86a32ddf783175e55aad2026b95affa997f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:15 +0000

    notice half contain minute getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
