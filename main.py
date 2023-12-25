8ff23eec4662578f1107bd91696db15f8dec569f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:18 +0000

    far pain everywhere centimetre getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def reverse_list(lst):
+        return lst[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
