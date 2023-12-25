ec45927a796005a508f036b575e2749aa8868a28
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:09:47 +0000

    string ocean exploration somebody respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
