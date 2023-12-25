34248729c0f2327d7947fcf6c35f835f7ff36e66
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:30 +0000

    borrow collect Ww increase angry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
