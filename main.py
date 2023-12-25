617173b1b81a570c9f04bf58a0dc9672a3a28241
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:52:20 +0000

    both clean comfort do talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
