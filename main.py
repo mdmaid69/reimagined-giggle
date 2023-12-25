f4b89bb66517cc5749d85b8616684dc243982835
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:05 +0000

    wise imagination age importance food

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
