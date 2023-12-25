2d7ed355c5418f6b43ba329641750517d6f9e08e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:35:54 +0000

    man well taste name task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
