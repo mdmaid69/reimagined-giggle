1703498330b962b69f316b34b1b97b6780277bc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:13:59 +0000

    talkinto discovery shopping except next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
