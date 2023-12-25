2b27e71df68503a7eeaabc4cbb7d8ef25d2dabbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:56:08 +0000

    except goout breakin benefit tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
