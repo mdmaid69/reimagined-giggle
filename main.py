82df37c1a8f6869c1db8ec3586c6fdd49590a603
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:13 +0000

    art schedule file turninto in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
