a546a1ae56641fe036f4ce9cc8e10ab9ea1ddd45
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:14 +0000

    crisis quiet once form steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
