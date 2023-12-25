32c4f93cbc6836d7f1694828d1f9dc944f9dd1e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:15 +0000

    beginning general worried bad string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_acceleration(speed, time):
+        return speed / time
