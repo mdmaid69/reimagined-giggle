4b3d46692eac86589a93fceb23ca0b4943125d75
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:47 +0000

    side hard generally much top

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
