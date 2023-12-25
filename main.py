5ed106fd2e32f6f6f568ccef9b0d473ad838a37f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:56 +0000

    process balance wait row allow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
