2b421fb05a57ddfa206753224e2684cdca76668e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:57:26 +0000

    shake street change past passage

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+text = "Hello, world!"
+print("Words:", len(text.split()))
