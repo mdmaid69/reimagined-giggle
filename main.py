1311c80f2227c9fa363cf7976e750ee47919a9f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:21 +0000

    kill fair crowded plain convince

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
