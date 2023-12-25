b3950807da2236e771e6ea195762e74816107a44
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:29 +0000

    bear cope kick sensible behaviour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
