9996e82b2d8bca83576f4f267de227af4e708f3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:08 +0000

    habit happy store will by

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
