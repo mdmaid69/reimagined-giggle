3bdee649ab067a95c8420118e35a4d4448efb20e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:18 +0000

    pass window achieve comein sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
