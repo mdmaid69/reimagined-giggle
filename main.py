2408b5ee9b8d1e44412c35b8ba1596d824193adf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:07 +0000

    apply wing big life black

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
