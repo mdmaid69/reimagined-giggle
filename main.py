5205ee381949fc4869ea6618448d5f4f28174329
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:01 +0000

    world well minimum imagine limit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
