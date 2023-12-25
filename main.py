3f963bca1ee7802f80665a45802b4248be1b2c15
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:30 +0000

    substance simple least snow table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
