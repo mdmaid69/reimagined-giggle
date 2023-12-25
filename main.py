5407cb5380f473cfce42fd9786c623231bd631e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:53 +0000

    repair underneath project search to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
