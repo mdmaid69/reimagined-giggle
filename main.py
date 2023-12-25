532de2c245f9792f8ccbc64bdc9c8c8032cf6d4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:00 +0000

    collect blame fortunate market flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
