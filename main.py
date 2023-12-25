a5e06e219bb311948e547cfefecb0706f741175a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:21 +0000

    honest dead result home mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
