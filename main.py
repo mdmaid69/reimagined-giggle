18fb70dab9608c0a455c071ab5e21a9964ff29d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:30:39 +0000

    clue occasional confuse target present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
