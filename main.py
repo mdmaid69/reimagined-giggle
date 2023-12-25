93a93362154037b93ea1b9c4a574412968144328
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:14:09 +0000

    wise coffee least cut future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def cube_number(x):
+        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
