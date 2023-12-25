90e00785eda39c1ebcda3ea8be3d794256108c38
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:43:52 +0000

    road married train television weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
