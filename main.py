054aa2eb005fb238e369b0aa29f0066da549e930
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:42 +0000

    listenfor centre no switch wave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
