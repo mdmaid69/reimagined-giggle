f3f498ccfd9f5ea8be8aa445061c108ff1a4f9fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:24 +0000

    ashamed backwards pause twist moral

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
