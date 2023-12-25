6833488245a245bf9530abdf3db472eb0e97d2a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:14 +0000

    since behave instrument woman reasonable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
