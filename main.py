a228801457ce8ab33e94b047b613dce7e482d032
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:24 +0000

    thankyou song whisper translate to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
