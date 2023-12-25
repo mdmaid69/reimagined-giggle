91a8bc1bd77e743bd58de63e9e3af2f321d47ecd
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:30 +0000

    deep then ahead nature onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
