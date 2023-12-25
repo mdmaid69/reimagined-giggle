2a80197f2a000b8d50735804869a2e692fca09b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:52 +0000

    on thinkthrough phone show pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
