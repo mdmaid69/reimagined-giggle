9dc04a06a195eaaaebf0a513596d402f55fc748f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:40 +0000

    whenever hold cool against lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
