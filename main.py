84cafbbb3babc89a30fb1d94b6122b6c26319236
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:56 +0000

    responsibility soft attractive walk meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def reverse_array(array):
+        array.reverse()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
