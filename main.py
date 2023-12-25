b86ca92320596bab0903cbdaec948fa78e51274b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:48 +0000

    spin action process produce arrival

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
