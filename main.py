93a28b8e3ecf496b743718ef4acadc94b673e063
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:35:16 +0000

    turnout turnout improvement sweep successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
