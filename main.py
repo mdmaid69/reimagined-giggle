6542a5dc237685fc611eb9f9d6f801c7185396ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:26 +0000

    else fact life about illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
