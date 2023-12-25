c5702fda669cfd6b38eebd34e1bcd662c8aff064
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:33 +0000

    disagree permission trap contrast enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_item(array, i):
+        return array[i]
