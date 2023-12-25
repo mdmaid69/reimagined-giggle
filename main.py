d9fd90686cea771a732d098ee2b9e0ff0b7b864f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:35:21 +0000

    smooth blame ball aware through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
