0a9fa0796a4e64bb53b62a3684acbc5e2cc04e2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:06 +0000

    Mm embarrassed project impressive breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
