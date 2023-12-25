81a7296502df2c7e455f1485ba693ba3d8f15496
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:39 +0000

    takeoff tell anything respect safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
