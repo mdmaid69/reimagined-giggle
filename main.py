b8153a2344a0bf193fa0662e72dbfe26091362ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:24 +0000

    spring score goback(to) negative upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
