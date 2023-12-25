5eb024152e664e0df853dcdad3aa3dc9ebc6b714
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:27:12 +0000

    snow physical equal available view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import platform
+def get_os_info():
+        return platform.uname()
