19eaa48d2605891bd8ca7beaaf5fc6465ba36e24
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:38:08 +0000

    connection burn flat dear gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def convert_to_octal(n):
+        return oct(n)
