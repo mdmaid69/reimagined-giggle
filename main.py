1d0bee9b327f436d7551d56c78b6fea9057e9ae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:45 +0000

    head song offence direct address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
