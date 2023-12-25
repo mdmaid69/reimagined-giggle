9ab2164fe478b90573595b378390b80cf1a0c6fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:40:55 +0000

    report remark shout to old

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_area(radius):
+        return 3.14 * radius * radius
