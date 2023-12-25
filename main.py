f9fd164305444d5551d3767deef22882a3e1f8ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:21:50 +0000

    space exit breakout treatment unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
