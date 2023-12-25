8d7e58aa1d83aae6006b26094aa2b810d152f749
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:46:48 +0000

    bury worse impressed than world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
