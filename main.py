ea3d754acf329b75a9ae7726aa4f58df552f40d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:29:05 +0000

    long rush off manage memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
