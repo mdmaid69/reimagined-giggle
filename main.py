4751f67e06c654ba1a52418c64729d52d3629beb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:49 +0000

    recovery suffer narrow workout force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_binary(n):
+        return bin(n)
