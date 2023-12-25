b950a9fbb0bd6877120ca7942e8282e0c51d46e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:30 +0000

    will bag criticism owe low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
