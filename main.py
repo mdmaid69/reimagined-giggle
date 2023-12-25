e3540f1232a2dba52732415c1b6fddd48e95a700
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:40 +0000

    company trap well foreign change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
