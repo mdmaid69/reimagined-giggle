43b6d85d57650d991aa6783ed267376e9e052a81
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:07 +0000

    visit patience description him freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
