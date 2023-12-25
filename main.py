4a164f1024c23bc34960e17502092a2f63045d5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:31 +0000

    introduce taste getup belong sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_area_rectangle(l, w):
+        return l * w
