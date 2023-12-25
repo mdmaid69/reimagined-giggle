d8e7887888773e2af2e92531b5a87509a9128d9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:17:51 +0000

    car guard object how waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
