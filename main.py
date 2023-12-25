44b76318250396d62480c01c0291cc9fd794a23e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:57 +0000

    stage turnback loss wind file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
