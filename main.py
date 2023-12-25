7f2feec510b40041f0ed26c2372b3f5112036a00
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:52 +0000

    fortunate expression down cross urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
