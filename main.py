f06e2555d3575784fd8633fa9e23d5be9ff8baba
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:31:59 +0000

    advice description still mistake runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
