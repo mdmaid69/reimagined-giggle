1a008bd9a2addc3268f0cfb595686e38bb5c0f50
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:21:23 +0000

    count earth season give free

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
