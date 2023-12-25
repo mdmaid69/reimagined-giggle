3b5a1885fdb571790dd9662bead3187768f16a9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:38:24 +0000

    crash doctor born tidy contact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
