ff09b4e407a2b58d9c66385046f886ac90f4c907
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:14 +0000

    brush forward half method frighten

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_list(array):
-        return list(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
