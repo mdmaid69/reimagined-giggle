8ee85ade147f6eba2bbba84c6eed5d491b5ba9e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:10 +0000

    swim improvement 571enough breakinto heavy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
