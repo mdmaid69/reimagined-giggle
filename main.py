28a59314d0b7e3b4a3d78ced24617d35dfcc79b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:47 +0000

    throughout escape include law smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
