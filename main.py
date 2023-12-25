7d95746034dda2eee1a66580e802cbe98ee034f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:33:22 +0000

    free peace means train complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
