983dbb683d9616a5865db67948acb9f79f5a8882
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:02 +0000

    real pinch unlucky guess something

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
