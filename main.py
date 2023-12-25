68edb05e2ae16b61b80338b652489d2d6de23831
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:59 +0000

    boil unite rain him Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
