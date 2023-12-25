9afa871f2071379f4c23a85a4ca00087be01627e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:34 +0000

    treatment practise shock uncertain certainly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
