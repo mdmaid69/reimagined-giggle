e49aed7ee952f9a160068bb7a51c3446ea143cdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:05 +0000

    pass still most matter discovery

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
