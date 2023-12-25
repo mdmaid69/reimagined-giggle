85334f793a3e96c27785b15b34c3d40aa49592de
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:42 +0000

    be else lie past bad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
