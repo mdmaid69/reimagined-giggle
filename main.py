8046e8da2fede2752abc1c8970fa8f2262edfe67
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:54 +0000

    plate lay exactly natural approval

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
