8f05ca504847cc0238479c73dfd1a8ba44c1e9be
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:40 +0000

    tone interruption false modern section

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
