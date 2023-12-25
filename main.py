1da6be287529c5e2363d280b9345410586cbbe35
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:54 +0000

    division watch expression train wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
