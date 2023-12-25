e31b10495815158ac1067ccfdc80ae3858514f74
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:51 +0000

    lift worst false needle guide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
