d1f1c905f32504dbeb30c2dbd53e0cab85d48453
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:36:46 +0000

    cum usual remark floor needle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
