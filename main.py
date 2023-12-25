9c054ab0b5a187f84c42bc39f059d8fdd821e0a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:28 +0000

    plate workupto change switch plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
