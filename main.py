53b43f115eadcdca593a3283341ed6d3f57d588a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:40:39 +0000

    action favour first scrape free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
