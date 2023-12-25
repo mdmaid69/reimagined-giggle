50b6f3e3f3b4bc0c98e864d1c9fa9aa5a08ee3c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:38 +0000

    laugh equal under pile rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def exit_program():
-        sys.exit()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
