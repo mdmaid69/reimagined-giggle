d2e1287dfde82eea085875700080977508d60ae6
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:35:10 +0000

    picture tidy concentration replace bother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
