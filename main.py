3067bcbb9da457d8c1bdf2e3646bda15afdf67f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:04 +0000

    attempt mood wonder any debt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_power(work, time):
+        return work / time
