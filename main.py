b8ea9d921c8795de74349ce8fa3d0852ac8d78e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:04 +0000

    yes intend several eat delight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
