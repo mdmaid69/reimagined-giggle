0886b56c010e5033d6667dda46271728ff7797eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:03:38 +0000

    tap day by interview joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+import time
+print(time.time())
