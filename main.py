b5f084a649d6188914ef5f784f9d718fe35bb878
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:26:02 +0000

    anywayanyhow rate runover explain recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
