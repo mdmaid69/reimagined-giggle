38f30674ded221c0ea1271c1359d8fadf0008e23
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:08:14 +0000

    secret recover after runout form

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
