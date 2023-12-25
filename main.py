ce4acccba5dd44c8e8dc98f503d0e78002b133f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:39 +0000

    job stupid mind rub size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
