aa1b6b435c66974d8270394e6c378ce6e45bed72
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:40 +0000

    increase tonight hole watch bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
