ae5a950da2f97688ae586c6d8c0e9e270f4f8d97
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:15 +0000

    path indeed trial above confuse

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
