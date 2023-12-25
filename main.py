2e10d000d7beaf888b31cad7f864406debc7b4db
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:00:29 +0000

    office track maybe runaway point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
