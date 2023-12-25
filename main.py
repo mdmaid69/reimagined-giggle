aeb09a0dabe8b831be254d3b2561996f3cf5aa15
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:42 +0000

    muscle comfort impatient such exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
