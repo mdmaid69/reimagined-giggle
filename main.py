4dcdde8ff82bc944f3c1081575e26f74fae0d3ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:53 +0000

    them birth minute exploration tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
