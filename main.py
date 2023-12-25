b4762c429555e1fbc6edd31f12dfea772552f6f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:18:33 +0000

    where force official goout shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
