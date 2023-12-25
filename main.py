4e6baff08c9278ada78e2da6439f351a57309ff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:12 +0000

    late themselves direction official store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
