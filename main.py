259c3104d6a3e4272a63bc6d1b7947819ff27513
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:18 +0000

    niggaacademy together arrange treatment part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def multiply_numbers(x, y):
+        return x * y
