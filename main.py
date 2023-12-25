36380997f55c4edead09c63da8191f1482835e6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:23 +0000

    unpleasant face moral point cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_repr(array):
+        return repr(array)
