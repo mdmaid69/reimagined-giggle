618595bc6728543404406dbf0608100688767e30
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:11 +0000

    arrive bin switch should operation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-print("Hello, world!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_octal(n):
+        return oct(n)
