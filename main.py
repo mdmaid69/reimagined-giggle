4243e51a242e18f555de19636ee0d4d3a677cf71
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:46 +0000

    little curious land today transparent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
