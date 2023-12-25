f1d565f7f3d248d690e6fd4a744099a5b4a3102f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:51:50 +0000

    example Nn goon plan patience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
