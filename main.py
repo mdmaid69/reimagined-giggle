42a8eba09facda75a395a205fb11cbf1523a2e40
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:45 +0000

    injury fork bill eye treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
