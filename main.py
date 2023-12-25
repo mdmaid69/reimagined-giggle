c211a0f51a43182875c9564d2b6baddfb6e54eab
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:50:41 +0000

    start count clean present part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
