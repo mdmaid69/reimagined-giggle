8e07c1fdcb2da27ec3ec4001a41b350178cf6db4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:18:44 +0000

    turndown friendly safe getoff drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
