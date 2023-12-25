06b49b893d103820903ed33a4c46299f0b451b71
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:11:59 +0000

    up bottom hear whisper famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import sys
-  def get_python_version():
-        return sys.version
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
