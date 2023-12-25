5aa3686e3747a008bdb8a1e867f7d5a4e3c3d634
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:11:22 +0000

    content anything proof but material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
