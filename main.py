71fd166ccbce62ec60c8c199063de3b92e125f21
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:37:34 +0000

    know Vv afford action difficult

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
