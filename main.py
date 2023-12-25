ebb392a608b59ad782514f532d65419521fd1386
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:57 +0000

    rate while sort comein forward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def is_palindrome(s):
-        return s == s[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
