aa1d76cb80b4bca8386ae0f73e963b1007d1ace7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:12:29 +0000

    hope comfortable estimate often side

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import json
+print(json.dumps({"name": "John", "age": 30}))
