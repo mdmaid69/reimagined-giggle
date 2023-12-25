1ad96e148550105f1c7e40768943674e149c88b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:55 +0000

    while police agreement actually various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def find_unique_words(sentence):
-        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
