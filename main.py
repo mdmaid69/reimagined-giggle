3861e560d4bb74b27175cc7a0da031c47a648352
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:15:46 +0000

    teach travel ill complete past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
