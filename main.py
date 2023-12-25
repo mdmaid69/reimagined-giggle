7471f5ca0493c5f9c1c2f1afb10a6d57725e4873
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:35 +0000

    home disappear phone take after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_to_hex(n):
+        return hex(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
