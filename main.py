4aa5c9010f7b8bdd6f84c400627f37da7db443fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:14 +0000

    unit century where over wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import re
   def replace_all_occurrences(pattern, replace_with, string):
         return re.sub(pattern, replace_with, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
