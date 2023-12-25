c709cd0b20317e8f8f8c2ac8bfa7b0ed4ddf6a83
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:15 +0000

    born sticky everybody interrupt doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
