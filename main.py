73eda807375b41cf36bf0906b2b032729e535836
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:07 +0000

    consult snow accidental ring pour

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-print(math.pi)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
