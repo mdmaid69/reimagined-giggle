76fec8392a3af86df89fb93c3180fcd62402476f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:08 +0000

    town climb rub match wet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
