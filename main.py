cc06d744dd3c31bd0f57dfa687312f7bb90de583
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:44 +0000

    tall reply book rain increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def square_number(x):
+        return x**2
