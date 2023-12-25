d94a2d31abf06a24ba039e8c0b617ddb61791cc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:19 +0000

    thankyou point hardly declaration dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
