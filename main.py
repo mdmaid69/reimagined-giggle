331a2de60e098ebcd0365923c6b18c53332575a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:15 +0000

    bite always or flow land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
