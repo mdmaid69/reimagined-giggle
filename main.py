f7b720293e48d5dfa69ec4523cc86a673a69ff6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:59 +0000

    furniture alive come turnaway cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
