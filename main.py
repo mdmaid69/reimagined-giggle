2e7e243b440cce8c422549272b7bf98b02a9d779
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:26 +0000

    beside phone safe imagine rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
