b3280734023dfdd1795658e132aa06a8afbefa3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:14 +0000

    contain Nn lie hit last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
