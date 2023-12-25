59c1f641facda3e0423b4f1fd158325df0c9573b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:03 +0000

    person unite move doubt level

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def count_elements(lst):
-        return len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import numpy as np
+print(np.array([1, 2, 3]))
