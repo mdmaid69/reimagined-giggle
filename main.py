9603789d52752684f34cd3037e52a382d3e79199
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:51:35 +0000

    light pronounce style crisis giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
