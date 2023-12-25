f91b9c8dc9b25e0b402a793923c7276ce2e76c01
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:32 +0000

    vision swim beside protect seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
