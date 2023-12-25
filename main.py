4c327070bb68faa314d130016ad9e028753de501
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:50 +0000

    slow office 573entertainment stop section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def count_words(sentence):
+        return len(sentence.split())
