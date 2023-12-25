52cf424be820810d7b2275ecad63c421b64f8595
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:34 +0000

    hold everywhere during matter pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
