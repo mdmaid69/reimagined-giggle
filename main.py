93593db520f9c0e31a92da403d7d5d40b3bd68fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:29 +0000

    bend discovery call game fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
