f7abd91d578eac7f12cfaeded9a46bde496a69ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:55 +0000

    able collection authority luck cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
