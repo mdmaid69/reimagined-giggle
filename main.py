ea4ebef07714c2ccdb768d45eb681fade835e81b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:20 +0000

    team start excuse miss climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
