06681c79a4849222ec6c7fe0da90f7ebc929bb13
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:25 +0000

    suggestion explanation contribute story convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
