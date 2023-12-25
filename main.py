ce3b331087e3e30600fd1d1ac3947df668d8f001
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:24:44 +0000

    hollow Hh boring far extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
