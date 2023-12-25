bee289bc5195ec8e43185d107a7f671663a4df5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:53 +0000

    Uu stranger feed develop ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_odd(n):
-        return n % 2 != 0
+import math
+print(math.pi)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
