51be6e62b77cd93ced91d320502be6d840ba35a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:48 +0000

    patience down body car makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
