208c3cf0dfc52fb76231712328fae3954770f786
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:23 +0000

    early turninto careful than world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
