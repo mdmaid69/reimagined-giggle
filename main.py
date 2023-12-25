cc61ba701a27c41757bb4aec5e367e9de7565689
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:58:06 +0000

    break young temperature receive pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+for i in range(5):
+        print(i)
