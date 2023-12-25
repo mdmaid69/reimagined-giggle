93a176059cd61546d559063b23318f637bf7aeb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:17 +0000

    building busy science dangerous bag

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
