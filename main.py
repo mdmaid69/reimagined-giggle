cc0c5cf497b6047072b116c20d8f7a1af7c393b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:36:21 +0000

    keepup proof high string club

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
