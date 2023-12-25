222f0b7c283f22f13dbdcde6287f4884b0c3724e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:18:09 +0000

    run sure swimming hate doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+name = "Python"
+print("Hello,", name)
