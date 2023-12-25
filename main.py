76961473ef87d90ad36ea27640a681ada421ee94
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:32 +0000

    well takeback lead certainly become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
