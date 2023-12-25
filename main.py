b5b278448eedadb3b882251120c59ead54db51cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:41 +0000

    vote wherever drop there custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_odd(n):
-        return n % 2 != 0
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
