898e6e392d3eac45e36a389ef55ef11d0b4e3f15
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:11:00 +0000

    problem perform game row type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
