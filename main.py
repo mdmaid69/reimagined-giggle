75a4748c253bb3466e71a78bee98b87dfb968125
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:06 +0000

    stamp concerning trick shame equipment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
