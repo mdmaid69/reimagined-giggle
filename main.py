90b7e903c08042f0c9adc3da20d32034e2082d00
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:36 +0000

    bottom push cover together neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
