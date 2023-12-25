8d0603679dbf65a7416bd3500f99f53c949a2272
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:46:28 +0000

    565end under black or better

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
