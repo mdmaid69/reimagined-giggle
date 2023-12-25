d5c42b30893bd931c1dfd21134f5f5974711ea85
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:07 +0000

    prize reach flavour majority we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
