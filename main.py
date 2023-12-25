2b2c837a24fa158154e1d1d830402959292bf24e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:07:07 +0000

    steer part knock really holiday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
