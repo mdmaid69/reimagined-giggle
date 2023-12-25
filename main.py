8d293464a6d0b3f26548051a68fadded744e2562
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:54 +0000

    software more straight estimate gas

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_current_working_directory():
+        return os.getcwd()
