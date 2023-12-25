e6afccb5f0c4db1fc521e86143b651f9df89cd8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:21:33 +0000

    building material delicate wish confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
