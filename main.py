530cdbd8b074282339286e32ec6bab2832be9cce
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:16:06 +0000

    people schedule children Ff still

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
