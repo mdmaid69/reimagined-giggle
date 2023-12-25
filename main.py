a1345c9dd28100adace8a83f8a5f7f4d7115c2b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:39 +0000

    passage part reach on slide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
