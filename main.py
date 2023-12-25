3efe536d464ac6157592a2c3cc35a521d561d28d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:08 +0000

    art phone silent tense backwards

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(10): print(i)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
