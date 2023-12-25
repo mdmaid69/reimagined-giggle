46f41c17fc99e3385b582f19c0002ec6a86c234d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:41:18 +0000

    relationship spray keepoff decide neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def remove_duplicates(lst):
+        return list(set(lst))
+def count_words(sentence):
+        return len(sentence.split())
