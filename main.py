04b4dd7ea2a1fe8435ebb913fcadeabbc9b85de1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:14:50 +0000

    approval quick getup finish influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
