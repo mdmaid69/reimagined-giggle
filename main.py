a57876023e5116cb65ef4565eb7028691f57f40b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:12:21 +0000

    convincing experiment near fun walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
