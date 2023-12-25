3c9a6028bb175fe07fc5882c02f338f081c16def
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:05 +0000

    set that bringback cost appeal

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
