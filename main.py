fbcb4004cd126cf41bf642c348b4cf8781a55eb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:31 +0000

    change rub too their belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def cube_number(x):
+        return x**3
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
