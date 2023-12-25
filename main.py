b482f8730cfbe7218a0ff2b9798a8651542791a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:59 +0000

    row attention various old Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
