c2f1488b8fcdff8d687f9aacb1239df226b4af3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:12 +0000

    feature next age eye move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
