52392b892032fe284fd709242b7bfbff50f208ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:39 +0000

    deaf female fall definite fine

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
