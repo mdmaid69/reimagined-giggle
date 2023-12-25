3c847abf885e6eedc1150b191a05faed687617f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:52 +0000

    own leg left takeback offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
