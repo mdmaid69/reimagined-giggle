0d6b715a8df459d12554245826814b40f164fb6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:02 +0000

    although operation bottom but contrast

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
