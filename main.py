96482cb519f6a09037bc67d5bc53b6fa4d10ccb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:06:27 +0000

    unless wide worth partner promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
