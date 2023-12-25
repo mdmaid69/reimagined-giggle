178662c9d7d6b3e9116d8cc45f9c7ba293a3b6ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:55 +0000

    tap treat drink themselves together

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_float(array):
+        return float(array[0])
