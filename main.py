d06b1f83b6bfe816a9cbd0021b9a8b6c4b612438
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:12 +0000

    mother clear yourself whole whom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
