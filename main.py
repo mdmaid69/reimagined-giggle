6b40a04c9af862f1866d4cbbe82cdb8015c1b7b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:29 +0000

    wonder wait shallow bill scale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
