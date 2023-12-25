27bee20b06e19f9a184570ce6c658ba50f5bc275
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:58:37 +0000

    walk computer benefit instrument label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
