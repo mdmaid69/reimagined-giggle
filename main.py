5e8c6174ff94c0213334fc5d4a741cacaef2cd7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:22:15 +0000

    gap gooff bright joke risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
