c8666d7350ab9684712caee892b0d036b3a223c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:21 +0000

    about accidentally season performance practise

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
