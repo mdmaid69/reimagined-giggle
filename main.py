598372fc35ef0165afc331f851a47c3cf004db1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:51:52 +0000

    balance face 565end opinion understand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
