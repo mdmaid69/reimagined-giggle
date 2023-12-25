4eb10fa048b25abaadbe6d20cd8adeb8ffdd34a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:31 +0000

    whole state minor careful final

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
