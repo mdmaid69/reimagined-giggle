dc538dc438ee6be82011fd38216005f93f54f791
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:46 +0000

    different taste disturbing regret squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
