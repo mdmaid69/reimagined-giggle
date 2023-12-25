027da83dc4eace552c105b65db4f98fd5afa9d75
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:28 +0000

    combine tie determined rate press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
