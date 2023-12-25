425e2bc0fcd41af4b3eb975a4f3649e530fdd4ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:11 +0000

    sense dry offer rob mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
