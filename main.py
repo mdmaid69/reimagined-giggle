6bd30b369e327fc72b22d038003ca43fb0a22466
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:07 +0000

    match pure for fit room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
