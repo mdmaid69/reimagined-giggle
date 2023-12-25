930a2b73ecc9f19150c7ea24483f2223a411c056
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:15 +0000

    feeling arm occur paper over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
