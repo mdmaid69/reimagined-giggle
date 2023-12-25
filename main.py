22758b6ed4c038572c01b4dd53634f445b226502
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:20 +0000

    concerned failure ahead deserve town

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_slice(array, i, j):
+        return array[i:j]
