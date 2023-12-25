fe77bcec3f21631ec74e7f896bf0c17b97b17f6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:31 +0000

    moral project care getout amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
