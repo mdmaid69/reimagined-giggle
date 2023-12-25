1acbf85682f3c3cc39c2f26f8d42c69f957ebc76
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:46 +0000

    rush small task such arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
