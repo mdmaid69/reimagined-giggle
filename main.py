78420fb39a571e8a2d5cc02ee6a726dde9239c5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:15 +0000

    round still board straight proper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
