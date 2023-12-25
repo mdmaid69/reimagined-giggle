fdafae3d3db590970bcb2270e69913d0b17c6428
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:34 +0000

    idea seem practice person trust

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
