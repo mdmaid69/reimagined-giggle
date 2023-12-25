0070241b82f340def0b046948eae70d0c24ead6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:54 +0000

    nothing however nerve wire point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
