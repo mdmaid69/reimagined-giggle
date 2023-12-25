92e197b822604c1e2b2b78c1fe4b23e63c0c6a58
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:51 +0000

    wonder last especially reject glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
