fb8b4b2a869efc6d96fe9c0f0cc943882c18dabc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:46 +0000

    however car breakin letter person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def remove_duplicates(lst):
+        return list(set(lst))
