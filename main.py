ccfc2c28c9d58d94722e30956661f62ab073bf83
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:27:18 +0000

    unpleasant example giveout friendly sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_density(mass, volume):
-        return mass / volume
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
