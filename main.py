98a2c04493bb309fb4026c8ea253ec627a589fea
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:02 +0000

    string professional spring takeaway ground

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(lst):
+        return max(lst)
