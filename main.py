991b7ca1f3753e8e4e0319a8a251885053eecff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:19:25 +0000

    bear scared speed afraid learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_typecode(array):
-        return array.typecode
+def convert_array_to_list(array):
+        return array.tolist()
