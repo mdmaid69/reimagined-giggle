2c8f7588cb66849a7e7a88bd71fba58a0dcc3dab
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:46 +0000

    design visit difference sale bringback

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
