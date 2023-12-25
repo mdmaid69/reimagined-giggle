cd1f889eb7b428849aafcc2188a9caed450f4fcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:01:02 +0000

    minority clear object breakout mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
