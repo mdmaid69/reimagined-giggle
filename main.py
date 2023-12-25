ac3406a456266eb1586f9a04fee13973e16c085a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:52:27 +0000

    journey distance bus down blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
