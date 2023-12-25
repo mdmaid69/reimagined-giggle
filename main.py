c6aefd533201cc4a61c870275de304d4132a54ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:27:00 +0000

    them new tidy indeed suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_int(array):
+        return int(array[0])
