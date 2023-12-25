71b3efe3a2c1307b90e9e8c93a7b2466633c4bd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:09:27 +0000

    mass game year little tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
