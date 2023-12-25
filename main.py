932e668d2e2d331695f3dbad3644e580e439fe58
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:11:50 +0000

    reasonable deep relief impression welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
