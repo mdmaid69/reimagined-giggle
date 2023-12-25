751978c4b56b3ecf7e8669d16af95f9f49355970
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:08:11 +0000

    act moon bringdown comeup perhaps

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
