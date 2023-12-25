fc786c87e2afc8c18b71ad6d1d411369a0f432d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:24:43 +0000

    squeeze rough turn neither light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
