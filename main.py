b6685cd644af4377e65df57e9a663f307603fa5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:53 +0000

    sale heat concerning street principle

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
