c75fddbc25731a2f6a7cbe3a63c308ee1d01c0ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:41:53 +0000

    float talk noone harm shall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
