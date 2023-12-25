c4271f47a585d2d6b9caeea9258a85334c131da0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:11 +0000

    thick neither thirsty lookafter bang

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
