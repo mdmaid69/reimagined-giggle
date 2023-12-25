b9f64088f4d3794a18438e6bc49ef54987f73fb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:25:20 +0000

    tonight skill lunch why age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
