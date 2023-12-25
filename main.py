2f14d8f25c5f8b4991d946192b979273c974c698
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:55 +0000

    increase judge protect hard influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
