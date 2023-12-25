7b584934c2504e8b4429b957969920be1da48f89
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:36 +0000

    taste score responsibility relationship measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
