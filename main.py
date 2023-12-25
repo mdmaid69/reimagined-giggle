cfc0d4361e9ce0317cd720c9c3934b5eb7188d3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:48 +0000

    fault trip grow takeout Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
