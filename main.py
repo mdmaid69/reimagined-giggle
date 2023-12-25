3b9b338cf5e89c40ab8fe0eb128ed35be96cddee
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:31 +0000

    tongue gooff admit workout callat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
