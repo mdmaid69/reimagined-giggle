82391725baf402ec8f025a85c3f957f492f1249f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:28 +0000

    ring broken gooff bet combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import re
-def replace_pattern(pattern, replacement, string):
+def replace_all_occurrences(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
