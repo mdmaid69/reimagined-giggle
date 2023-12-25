98a2ffbd0224555caab708c6a08ce5ee48831b5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:27 +0000

    freedom today bunch another base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
