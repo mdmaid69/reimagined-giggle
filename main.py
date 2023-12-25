d6dc1a927e6dfbbe32e7e62bc39f2dff925755e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:40 +0000

    science go takeover perfect autumn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
