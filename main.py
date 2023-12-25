3534797518202c8ed87c63cab3a7c54537e48d4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:01 +0000

    claim schedule achieve accidentally exchange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def is_palindrome(s):
+        return s == s[::-1]
