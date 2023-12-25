19be89982e4d7f40c525e9bc3bc5e5ee54519b8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:53 +0000

    old regular shut spot violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
