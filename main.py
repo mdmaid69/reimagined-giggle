5176bb55ea8200676fbfe394a1d60fb11aa37e61
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:31 +0000

    test name scrape compare round

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
