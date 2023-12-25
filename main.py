f1960ed337ae319dfbee9f5e8ce417f66cb732e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:53 +0000

    drive read several can know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
