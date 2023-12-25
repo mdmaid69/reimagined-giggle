f98ae9b9fd582f0d08c90afd801225b4a35256e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:25 +0000

    uncomfortable lookup play control news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
