45efd464b96b05b8e643fa43c34b94747f91ad7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:11:50 +0000

    hope sweep coat doubt collect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
