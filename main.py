1d3e8833b11abbd5bc2d845bb4d543a950a11a0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:10:05 +0000

    claim squeeze just habit pretty

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
