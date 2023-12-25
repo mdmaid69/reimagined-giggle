8ea83b79a68e5155723412a0de51ea0ecce30228
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:59:41 +0000

    nature design Dd steam read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def square_number(x):
-        return x**2
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
