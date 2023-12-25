f02c3ec46f6e03f25f3cb51d7347b55b610546c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:53:11 +0000

    wipe strange contrast exhibition pump

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
