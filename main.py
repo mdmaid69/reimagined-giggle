c47f15926e110d1d3b9cb94e829db598d39c0214
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:28:02 +0000

    giveback unlikely breakout embarrassed watch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
