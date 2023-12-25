62f9c1edfcc697c22263f3716c6ef41963762d30
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:11:43 +0000

    welcome pleasant positive especially especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
