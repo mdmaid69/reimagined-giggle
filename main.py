deb6ee262e2ee6ce11f6ce31cc4555a4544b3a0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:15 +0000

    flood mystery clean everyone talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
