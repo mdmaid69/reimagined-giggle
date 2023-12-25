ff260e23868150e0e9d48749197297d419d51367
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:28:19 +0000

    ability awake push convincing lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
