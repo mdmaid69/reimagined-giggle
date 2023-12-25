eeac23dcf73044f19344d4bd36cc850482e93a0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:49 +0000

    supply single word flow sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def clear_array(array):
+        array *= 0
