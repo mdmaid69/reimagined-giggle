a4a9d6f350be0958f88b013e1f698e5707c7a194
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:05 +0000

    meaning teach wet fence funny

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
