28052d58ae9b4233ece488c33c4d0cbe240a3656
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:55 +0000

    comeover partner movement treatment respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
