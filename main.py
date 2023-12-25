7be64fec0185c159e2fbb99495ccd9281f676d10
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:15 +0000

    add geton trick pot responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
