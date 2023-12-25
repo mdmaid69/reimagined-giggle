aad313d8a30704a7b54d9fcbd6a35bc4129c9e8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:02 +0000

    poor suddenly within comeup dull

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-print(os.getcwd())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import getpass
+def get_username():
+        return getpass.getuser()
