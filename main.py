68c5a8d86b1d31d36bad740db1f4e4ae130160c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:15 +0000

    purpose Oo upset callout side

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-print([x**2 for x in range(10)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
