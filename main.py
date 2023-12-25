5834489821df8deca1c46c7066811f0838f672f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:40:18 +0000

    hard pool like trouble or

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
