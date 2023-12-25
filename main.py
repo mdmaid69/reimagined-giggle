9bddb4aeeabc8d86cc06255500c0a4afccb0ff1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:38:51 +0000

    inside will price begin art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
