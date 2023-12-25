5701aa504ec7d27ce115a782fee96fdcda84241c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:43:59 +0000

    difficulty most song block turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
