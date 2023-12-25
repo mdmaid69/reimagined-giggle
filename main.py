fb0913cb36240e711ae65fcd6b08270bc171cc15
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:17:47 +0000

    attempt knowledge content score stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
