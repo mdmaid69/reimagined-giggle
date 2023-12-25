47a133ea101616ac59abc208947ade3e10029f78
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:03:52 +0000

    ring near waste software over

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
