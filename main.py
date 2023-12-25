9ed3be0dd7b4fb561253a28d558828b23d32578a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:18 +0000

    act weigh dead attitude scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
