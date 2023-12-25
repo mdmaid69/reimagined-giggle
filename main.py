a9b3c5722ff7823195d8b799a154beb3fe9f9e7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:03 +0000

    autumn keep fact gooff little

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_min(lst):
+        return min(lst)
+import time
+print(time.time())
