9e020d1c0b483ef1f9d6dc6c34d1076caf2847c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:02 +0000

    switch tread say neat valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
