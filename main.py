3648e7e535df7039e5e32aa5b56fc836bb23025f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:42 +0000

    finger important nonsense language shadow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
