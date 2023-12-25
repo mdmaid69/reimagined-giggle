72659077e55d39185f08674ec2d26273f199886e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:50 +0000

    book practise Itself matter mother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
