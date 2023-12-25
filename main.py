6399bf5cc7ac8d28e5eb75ea12a3cf9a943822a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:39 +0000

    direction movement calloff smooth transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
