0574f6a8b7400bac2864736f7030068690ac8158
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:53 +0000

    replace typical yesterday odd blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
