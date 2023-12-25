57ed041b7c9d05a867d6bbe12ddde14dd0b00a57
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:48 +0000

    fight whatever trip journey store

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-name = "Python"
-print("Hello,", name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
