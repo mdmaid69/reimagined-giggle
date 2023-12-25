bcf08c06c22797d847d51871b0f4d07aec8e3248
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:25 +0000

    sticky education away destroy petrol

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
