5381873acb00d49e13116808318f9589ce5ea37a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:13 +0000

    minimum usually attach somehow workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def count_characters(sentence):
-        return len(sentence)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
