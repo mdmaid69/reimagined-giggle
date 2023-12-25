5bf396e716ab40276b949c1859ace9f55fca5f87
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:44 +0000

    eye bother special get name

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_priority_queue():
+        return collections.deque()
