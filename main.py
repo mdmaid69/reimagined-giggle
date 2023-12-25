d230a7e0ad60f68cb47f0505e81e19df2c2e3a77
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:04:03 +0000

    really attention feed through thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
