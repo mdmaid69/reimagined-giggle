4b16379152cd672d9ca181a63a4b81ebcd06989e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:51 +0000

    sort public building worst exciting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
