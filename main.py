5fb6660cc0768bd0f977f0612958cc0cb3f468a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:14:04 +0000

    must create getback evidence smoke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
