f3ae9bd4733b45d683bedaf539dafe20293c9e15
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:48 +0000

    great zero public damage spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_item(array, i):
+        return array[i]
