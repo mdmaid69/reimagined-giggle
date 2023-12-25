2a9acfd7e08c14763aeff15a4325c8efce6f1e13
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:15 +0000

    air press most disagree neat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
