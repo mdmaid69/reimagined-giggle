e4303408ea753bb02f35088ce65162b570437b5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:07 +0000

    sensible previous love so range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
