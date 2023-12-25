a620fd9849569071f8d16f44d22e2b207136eaae
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:43 +0000

    dull pleasant power few best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
