e6abcfa2b94114559fcf7a247662e2a17648fb0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:40 +0000

    first problem high could constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
