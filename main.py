32aea8b3949c0b89e819988f9acd474d3dbc01fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:59:08 +0000

    television Yy passenger possible think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
