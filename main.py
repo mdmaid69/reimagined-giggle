39d5fe6846b744061e75d12ed12685c1a81d9b80
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:55 +0000

    transport let because squeeze plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_sine(x):
+        return math.sin(x)
