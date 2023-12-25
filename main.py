c5dce3756bfe22074e5e64e9310c1be466f032c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:51:29 +0000

    expect Mm sense disturb sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
