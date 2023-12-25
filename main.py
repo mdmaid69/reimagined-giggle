9a9c219af45e307d5abb4d622a1b2b5adfe8534c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:28 +0000

    warm pretend water hope left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
