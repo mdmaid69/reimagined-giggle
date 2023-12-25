d253a6c55e11180a55007562bae6a3e5e3713fdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:59 +0000

    normal collect centre story confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
