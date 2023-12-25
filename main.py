a86e7ed2c0e817f22654933f4dbb91eb15df93aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:24 +0000

    spray appear dream typical rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
