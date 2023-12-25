07b490d2d38d38b87575fe5120a4682d52b7129f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:00 +0000

    change order frighten nerve mad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
