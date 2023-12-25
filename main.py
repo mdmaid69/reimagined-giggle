0c0d0713498b6d7f34b8de3f9e64acdf61b11a17
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:16 +0000

    hour miss dream blow give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
