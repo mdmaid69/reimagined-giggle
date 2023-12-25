bb4a2bf1ede3f9894fbe3cc7fe46c58253989765
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:55:57 +0000

    exchange safe neither passenger tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def convert_to_octal(n):
-        return oct(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
