5da6c3118b97974ea9ca907168f57da580f3c306
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:02 +0000

    fear thought branch Cc dry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-print([x**2 for x in range(10)])
+print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
