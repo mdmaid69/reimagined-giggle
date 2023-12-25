8b99cb94fedee61a8dfd1ba05bf96278b9f8f419
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:06:04 +0000

    arrangement on dress sell related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
