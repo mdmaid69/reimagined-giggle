b85df2699847a02a2ce1d21721683071abd6cd9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:30 +0000

    method priority agree never every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def multiply_numbers(x, y):
+        return x * y
