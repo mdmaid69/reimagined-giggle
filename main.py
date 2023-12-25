29972be59b216a1d4a62f0c71324759b115ad4d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:46 +0000

    wrong decide pollution balance stick

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
