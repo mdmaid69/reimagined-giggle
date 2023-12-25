bc17550726f3d6d3bd96ffaed3945e6940d37f10
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:32:49 +0000

    repeat rate rush increase war

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
