9974a53cbc38232ecf68a7480a32fb78d8a0f64f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:02 +0000

    just permission tone turnup payment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
