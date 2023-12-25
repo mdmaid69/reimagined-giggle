bc476b311d7d4cb1a4e61b98ea26da8f7edbb7ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:36:05 +0000

    pity minor patience entrance short

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
