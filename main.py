cae1b1f26e25fe3aaad40e7da9ece2062dfe92f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:20 +0000

    home knock bush classroom breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
