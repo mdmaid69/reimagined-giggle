892306da84811d58987abb87a14ec8c089e2d997
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:53 +0000

    distance steady lesson exercise here

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
+        return x - y
