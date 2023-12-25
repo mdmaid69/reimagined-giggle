b3ff4442adba0e5d1d139c65720b85b5d4a92825
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:20:41 +0000

    new offer above broken greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
