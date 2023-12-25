f81c81f7031e4f82ac9f2c108101952ffd3f5bbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:28 +0000

    chance breakinto same coffee during

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def add_numbers(a, b):
-        return a + b
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
