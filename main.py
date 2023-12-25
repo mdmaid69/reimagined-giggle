62db60d4d5d3b91f6fb6cbf71ff042c27c72c3b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:58 +0000

    smoke chat idea kind nurse

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
