9a4e12648fac4ff28af42fd47a7745b8f0519059
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:55:12 +0000

    goback(to) fresh club right workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
