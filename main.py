fb1b02104a24048f9aeb6f2bd8a21b6917ee9af1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:03 +0000

    awake pupil beside quit nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
