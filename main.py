4f51f1c2762ac4346facef16f8ba11baa039785a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:48 +0000

    company train recommend please single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
