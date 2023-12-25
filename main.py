3bb65f4e1fc5c4824766d9979e72594d6146f558
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:26:33 +0000

    vision criticize escape stage bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
