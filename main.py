230b9de27e37941c0e141e588fbe7b6ef11ba3fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:29 +0000

    basis exact from inform fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
