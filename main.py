ad1144f0b88b0b970185c09ba90ed1ecfc7401f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:50:05 +0000

    wonder keepup turnup back heat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
