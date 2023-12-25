4bd435d7ee65dd8fb5acb8b12ed46fb32c0f8e00
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:25 +0000

    hope blame sticky original advise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
