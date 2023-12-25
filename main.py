330ea09ab6d8e521708cab2cba9b0f60d27ed610
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:31 +0000

    sister exist natural famous figure

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-print(math.pi)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print("Hello, world!")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
