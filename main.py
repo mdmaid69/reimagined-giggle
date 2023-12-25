a189671108e8e13b4084981ccd559b9dc4cf4ca1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:21 +0000

    now him enthusiastic independent male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
