95219aea1d11fe72e22bf92eb59e2831320e1034
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:33 +0000

    little everywhere present badly interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
