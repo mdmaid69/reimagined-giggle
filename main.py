971ec51c427653b7ac252d2595c2d0117575cdf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:00:27 +0000

    attempt lie left turnback finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
