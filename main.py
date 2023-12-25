5bc57b377a7dd04b04e7e1cfba0f9c0b076a8ad3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:49 +0000

    shallow worth little safety agreement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def reverse_array(array):
-        array.reverse()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
