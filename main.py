6d22e71ce13259302a181f9855786c6b4201c47b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:34 +0000

    hurt flat pleased whom plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
