30c1cedd20a333a734a9e8fa85ba62f023502d27
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:07:15 +0000

    authority against inform behind sensitive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
