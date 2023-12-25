2c8b2e07426772d5bb792989c95476809a64867d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:43:30 +0000

    crowded exciting practise respect apart

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
