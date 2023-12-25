278edca4eb54f513f6ac9fa7c10d2a6ede9b1938
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:21:06 +0000

    road pleased doctor interview noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
