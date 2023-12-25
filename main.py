57f2632f91ace2d28a97a40fffd3b00a635c3a4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:26 +0000

    emergency lie majority step each

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
