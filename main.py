a1031c15b6268eedc72eace81b3629477b6d2e60
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:18:22 +0000

    loss on patience like pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
