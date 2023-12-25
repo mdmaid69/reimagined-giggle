84246620a2cdb0d6206bc4c9d21dd73771eab6bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:37:03 +0000

    patient black off no peaceful

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
