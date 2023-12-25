efd17038356acefe97d606de98cc5629e045611a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:45 +0000

    somebody crash concern turnover before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_work(force, distance):
+        return force * distance
