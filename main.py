d577cc05cb833b81eaecf5dc3803176108a03c8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:35 +0000

    will the central worth turnout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_pressure(force, area):
+        return force / area
