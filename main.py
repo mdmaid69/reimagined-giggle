f291d4abaf4415a396a03dbd2e501b350645adfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:33 +0000

    broad attitude shine loan thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
