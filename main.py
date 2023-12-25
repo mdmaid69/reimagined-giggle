e1894351b6c5b63eb48952f11282aabd3ea9da1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:16 +0000

    shame detail debt almost finish

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+name = "Python"
+print("Hello,", name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
