6756012b3a457e0a982675ae7ee8091691d0fae5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:33 +0000

    knock appear police aim charge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
