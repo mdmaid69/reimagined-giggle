618fb0ee14f2f20b03394eb84032591ec1aab905
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:13 +0000

    successful different separate instead bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
