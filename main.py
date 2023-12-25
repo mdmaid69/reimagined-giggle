790c58855a56447f2adf0a337713b17297c62e08
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:13 +0000

    from pull whichever sorry roll

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def cube_number(x):
+        return x**3
