55fc780a82e08ebc17390be5b2b07810531a1253
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:27 +0000

    smell shock birthday passage introduction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_string(s):
+        return s[::-1]
+  def sort_list(lst):
+        return sorted(lst)
