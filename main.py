ed40ee6e5a792b39c493b11df0dce6fdc0209984
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:25 +0000

    conversation stir frighten cough area

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
