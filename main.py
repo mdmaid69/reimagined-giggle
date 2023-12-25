a785fe4de5e6b51ab371887a504eaf59c8aa5da7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:25 +0000

    boil system luck turn Qq

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import collections
+def create_user_dict():
+        return collections.UserDict()
