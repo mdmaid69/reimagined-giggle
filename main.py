6e8ebb7d6d2c6c42972ac025298aa7082df351a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:30:26 +0000

    consult comeback exhibition hear she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
