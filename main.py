cd31d53cb0913bc0166730683bcd79c01754c9f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:41 +0000

    address callback major agree difficulty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
