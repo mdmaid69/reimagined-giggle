d23377c61a72d66df808a870aa9dc013564e2721
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:12 +0000

    never insist favourite heat face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
