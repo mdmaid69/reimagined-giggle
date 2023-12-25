e86e86e8b33924aea6e9e23aae396a6d85c813ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:55 +0000

    Vv piece size beside storm

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
