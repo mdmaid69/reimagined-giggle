84700b153c23c9039b06dd722d93dd329ccfcb05
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:36 +0000

    excuse there main soak study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
