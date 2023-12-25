3363a14afaf68bf8566f9855c4930c070287afaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:58:14 +0000

    perform strike smart ignore disappointed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+print(math.pi)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
