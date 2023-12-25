363b90aba0463843fcd497a0eacce9bf4128ce1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:37:02 +0000

    soak convince waste computer mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
