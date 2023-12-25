ee85ae784e11a80ddee77d463d492e63dc54c303
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:52 +0000

    spot pure instrument about cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+import sys
+def print_python_version():
+        print(sys.version)
+def find_max(lst):
+        return max(lst)
