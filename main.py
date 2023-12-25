61542980b62e84e6dab650f9f95f8b7382a77c1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:23 +0000

    awake rain concentration attempt handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
