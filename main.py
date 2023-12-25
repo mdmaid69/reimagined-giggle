10edf63f708840886ddf927f10943b11632fea95
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:27:16 +0000

    satisfactory forever wealth moment country

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
