1fc30affb940ccadcdb71983e06b89a0f7423ff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:40 +0000

    nose regard transport proper include

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
