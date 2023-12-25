f4f8c82bc42133ab0abab0c514447e197ef883e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:29 +0000

    line tone far heavy famous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
