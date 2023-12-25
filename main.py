03baaf5942ca76fc175a1bb1913eb366c0ad371d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:09 +0000

    walk leave away future up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
