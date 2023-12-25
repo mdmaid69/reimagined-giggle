8fe2dbef07f844b1f050c5a7a8b2f22db754b173
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:48 +0000

    afford sting shock fresh mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_stack():
-        return collections.deque()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
