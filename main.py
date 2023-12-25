200f61292289cee7d81429d568d094daa1a4e95f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:32 +0000

    error explanation correct money spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
