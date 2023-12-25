bce9db0cb9503059030fbd91345c40d909efc43b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:25 +0000

    machine attractive fact 564encourage wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
