ee0c50e6a9cfe9c2c78ccbed818fddee91af7f92
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:11:28 +0000

    its main short legal challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_ceiling(x):
+        return math.ceil(x)
