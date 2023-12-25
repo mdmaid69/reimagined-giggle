b73a28584492ebcf3e4a5a14c1e8a8ad2100a1ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:06:31 +0000

    language drop run help when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
