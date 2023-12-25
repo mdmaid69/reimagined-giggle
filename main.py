3a3670787b8a927d1e67dcba3692e3bc988d99a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:47 +0000

    kick place achieve list song

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
