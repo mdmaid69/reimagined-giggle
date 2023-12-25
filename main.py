f093433f0856ee7e0397045497710ba87b53bac5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:47:01 +0000

    church lock develop constantly brush

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+print([x**2 for x in range(10)])
+  def convert_to_binary(n):
+        return bin(n)
