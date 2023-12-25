64161ae407bbb49c96f045daf4fe91ed4b0c27bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:26 +0000

    announce inside personal run getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
