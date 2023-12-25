4aedc01e51f83cb2a3d187eec3a47000b0b739c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:22:39 +0000

    trick beginning sad stamp trouble

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
