1eb6ad3e4f6440694940dfeab5f8a0f429301ec3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:57 +0000

    comeover goaway present traffic purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
