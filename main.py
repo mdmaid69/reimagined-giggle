6ab82f2a1b9558ff1dc1bed6d88eacbc728110bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:38:53 +0000

    prize family level shrink view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
