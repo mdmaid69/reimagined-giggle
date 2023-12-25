18689916386fc1ffadd1b11bd0cdcbeb954109b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:18 +0000

    fortunately half limit really high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
