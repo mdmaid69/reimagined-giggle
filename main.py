399d4eb4062f86be3f13a45ccc708eb2bcbf311e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:43:16 +0000

    question scheme contents this sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
