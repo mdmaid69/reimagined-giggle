8959bb0df01d8b59dbb96bd8c85fedfb78a06c06
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:56 +0000

    surprise issue never cross buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
