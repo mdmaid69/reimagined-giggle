f84b817d2d9c4c584f2d9fe50dcf8928d85cf24a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:03 +0000

    cold crazy kind with design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_counter():
+        return collections.Counter()
