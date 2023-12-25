1be7c5ba41e8ab3d93dc41e08e1081bd449272cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:08 +0000

    neat sense none getout hate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_density(mass, volume):
+        return mass / volume
