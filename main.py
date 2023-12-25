77be9788ebd68e56e1c991e805396f495af40943
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:32 +0000

    little area trust finish shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
