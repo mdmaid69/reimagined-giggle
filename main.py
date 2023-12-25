0d0720657c1499d6b8d1a9e62a47d6a5e012b312
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:56:30 +0000

    shadow inside worried chance Vv

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
