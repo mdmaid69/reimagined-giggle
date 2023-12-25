0766250b816debb170080d30b3a1fe705036386d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:21 +0000

    up this 570enjoyable suit influence

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
