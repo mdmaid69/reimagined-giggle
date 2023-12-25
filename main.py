d58b151d09c3f44a20a68552d76a2fa028f35fcf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:39 +0000

    block lonely disappear praise direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def reverse_array(array):
+        array.reverse()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
