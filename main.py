aec9c9dfb89dc521aee4fbd9cf8a941e1bd8377f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:37:13 +0000

    runover convincing confident slow debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
