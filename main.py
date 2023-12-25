fc3d0d8a04392c44b016fd38f86663cb946b162b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:40 +0000

    past state sure meanwhile fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
