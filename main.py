fb5c1386fdde043216875df531fac37ba628f075
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:33:55 +0000

    quiet protest suckme many imagine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def split_path(path):
+        return os.path.split(path)
+def count_characters(sentence):
+        return len(sentence)
