079b09cb0a2471070c37097b759cac9ac5301070
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:18 +0000

    strong plan argument effort so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
