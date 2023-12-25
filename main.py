0fe00bde4a751cc7a7cc810cffbb89912bf523fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:19 +0000

    slice tip way recovery tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Words:", len(text.split()))
