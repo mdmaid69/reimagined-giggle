0d375878f799067da15c1d81478be5aa47e74f45
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:04 +0000

    criticism attitude general mind trip

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 def reverse_list(lst):
         return lst[::-1]
