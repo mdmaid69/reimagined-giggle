d4a5af1b36113ea96518312d1e9725bbdae3334c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:29 +0000

    doubt serious connection upwards 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
