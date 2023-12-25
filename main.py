f1df75894735d2c697a9e85fbb34b8e6f2908c31
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:31 +0000

    round underneath centimetre lock develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
