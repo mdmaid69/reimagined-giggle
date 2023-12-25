64a6b15a35388e1b5178e32ba4df77c0cc0da4dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:58:56 +0000

    bottom need teach public draw

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def clear_array(array):
-        array *= 0
+def count_characters(sentence):
+        return len(sentence)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
