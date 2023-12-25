bfe2db85584af2095561f083c96df4b7a78fd425
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:51:54 +0000

    change profit rise learn mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
