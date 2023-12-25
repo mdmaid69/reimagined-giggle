8371be20cc83bea0f4afaa2b2709c17d009a9759
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:42:15 +0000

    see worst dangerous address warm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
