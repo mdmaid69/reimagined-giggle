80a40717d5253c96ce14832c573df385e9e2cb98
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:58:10 +0000

    real show Ww rescue exactly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
