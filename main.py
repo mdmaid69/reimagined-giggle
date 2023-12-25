36794c2a865fc88042e5757781a6c356ba393fb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:01 +0000

    tomorrow hand question between plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
