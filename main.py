dcf529a2a9495b70fd77a1f953bb9b7e310f7472
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:11:23 +0000

    proof fill position over tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_acceleration(speed, time):
+        return speed / time
