2c39f8629cf3d6bd5b689dfe37658e0d68c7de6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:31:43 +0000

    occasional dull win kill there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
