0d5e0534a8d245fb6bcc31b87b1074bcac606b1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:42 +0000

    definite turndown in woman comein

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
