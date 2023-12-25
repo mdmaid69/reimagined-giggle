7ea1e542ffaa21cba416b5d9bfddf4424360c84d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:42 +0000

    turnup cook culture laugh shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
