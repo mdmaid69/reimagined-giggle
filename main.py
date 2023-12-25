8ab617ef14aaac6cb8aa7c91dca4b69677298faa
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:52:21 +0000

    rid town I scene imagination

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
