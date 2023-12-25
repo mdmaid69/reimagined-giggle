848547f0b23d2d88969b3c983c2bd44d5689c141
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:50:07 +0000

    apologizeapologise shoulder disturb film language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
