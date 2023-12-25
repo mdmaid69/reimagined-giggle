c78522a1ce6e9b798a8b4d8e31732464cad3a8ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:04 +0000

    thinkover crash argument rain wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
