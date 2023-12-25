f02bc8205877b87c6bf19238a0821664013df2b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:03 +0000

    fast lesson bang bite consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-print(sys.version)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
