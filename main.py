d87de737ac488459c468486bef2e736577039278
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:14:36 +0000

    purpose work themselves suck dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_str(array):
+        return str(array)
