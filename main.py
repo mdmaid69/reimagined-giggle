77b2cadb6ebf04039795ef24ed5dc05cd4155708
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:40 +0000

    message believe turndown steer detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
