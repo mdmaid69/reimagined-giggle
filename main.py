9ce2a7e2d70506933500c209c06b57d41e0390e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:25 +0000

    towards cinema tend pronounce stay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
