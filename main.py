5e92237a0d48550c78432dd19d0a26179feb2c1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:37:45 +0000

    fly wind fierce reduce doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+def log_message(message):
+        logging.info(message)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
