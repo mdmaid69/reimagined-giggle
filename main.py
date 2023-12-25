8ac957de4747ee5accc9bd379e6920a152f04303
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:45:01 +0000

    luck wipe cry defeat slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
