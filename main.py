c95aca291dd2b91d710ac5111707dc5885c65a01
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:11 +0000

    unable apart scene injury keen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
