25dcb34a45c1350ccd19edbd61f474bf4d1581f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:28 +0000

    secret software mistake extra feeling

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
