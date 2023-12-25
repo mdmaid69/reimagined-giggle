ff0f10902e65b1db1fe72e475ec9736ac5cb5b44
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:11 +0000

    hers need far borrow interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
