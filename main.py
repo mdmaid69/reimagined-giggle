7a4772dbd86b0341aac9e16e5ca432fc1c2e6c9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:17 +0000

    black wise to debt impressed

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-print(sys.version)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_min(lst):
+        return min(lst)
