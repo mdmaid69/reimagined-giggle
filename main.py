1efe7d33a5deeea060cbedc3984f67e27bc50a98
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:57:37 +0000

    wash furniture distant rid round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
