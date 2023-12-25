4064262ebd56336bcc1339c8e23d95238c1ae614
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:36 +0000

    let female represent challenge error

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
