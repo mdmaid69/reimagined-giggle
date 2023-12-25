745ab4e8c69401b2d5aecaf21414bf7aeff42cbe
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:47 +0000

    interrupt Tt they squash relative

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def split_path(path):
+        return os.path.split(path)
