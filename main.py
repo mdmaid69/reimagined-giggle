828cc74b5872a4abab2dca3bfac0ceab2ded98dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:20 +0000

    big round mark material pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
