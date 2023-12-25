98acf3410b4122f86ea78f2641dd651946a6f298
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:31 +0000

    pattern fit stop turnup thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+print(datetime.datetime.now())
