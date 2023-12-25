5ec8dcf9d2444468c7f6c90758f5819a4dc98a65
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:37 +0000

    preparation workupto confident payment union

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_max(numbers):
-        return max(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
