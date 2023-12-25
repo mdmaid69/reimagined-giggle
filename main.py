820592b80486df123badf31d259e841b90e30c8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:57:50 +0000

    store battery she life side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
