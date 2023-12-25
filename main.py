54610c1f6136111b810e57141a09b346ad8b2458
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:19 +0000

    funny test smile back anything

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def count_characters(sentence):
+        return len(sentence)
