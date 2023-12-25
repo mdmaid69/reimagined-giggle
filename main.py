4b3d2a6b472ee46c6cfda318241bfa60d1e9f2aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:10 +0000

    influence flight heat time satisfied

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
