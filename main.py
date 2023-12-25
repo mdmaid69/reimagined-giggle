7c990434727cad0f4e309e5a8fac906a551c1e31
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:40:34 +0000

    creature pick willing wish stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
 import array
 def get_array_as_list(array):
         return list(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
