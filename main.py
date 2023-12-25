01e55a28a7afa597ae2ecb769e64192a4ef28b84
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:31 +0000

    broken trip nurse repeat ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
