b7ffaad18a4c1888b77b33581107cba23e94ca3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:07:37 +0000

    stiff guess trust lost night

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
