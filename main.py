3d31fbdc0ea644619c5b5acfabceb8550d969df3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:38 +0000

    reduction tongue climb comment borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
