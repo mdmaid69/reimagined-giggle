85661ffb46871ccae5420889bb6c1d663a3ce468
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:17 +0000

    taste neat excuse fly tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
