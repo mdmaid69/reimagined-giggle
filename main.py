bf6a88af148319988e89efb0392eb6020aab5e31
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:44 +0000

    round standard choose dry persuade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
