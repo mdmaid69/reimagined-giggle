aafe8d1dadab2c210e7ea7baff16b899cc2783bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:50 +0000

    reduce swing split private dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
 import logging
 def log_message(message):
         logging.info(message)
