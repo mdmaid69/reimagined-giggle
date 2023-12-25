b80b1f8b405581303c406e12208c53e11ec99e17
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:36 +0000

    join until with calm type

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
