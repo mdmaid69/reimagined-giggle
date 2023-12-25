ba81b48eace033db200792924316dbcc6c383614
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:11 +0000

    common bear scream pay confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
