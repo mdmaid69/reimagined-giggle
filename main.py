d9d0517c3078f7c8124847be3d3e36825fc3b2bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:34 +0000

    draw hope report negative introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
