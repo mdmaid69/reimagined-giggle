efe811f5525ce2cc84e72734028c5e2267baa278
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:00 +0000

    environment programme home so goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
