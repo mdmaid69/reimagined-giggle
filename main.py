e732f8a361f159e09757085d2ea9c97bafc97c0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:50 +0000

    left use speed shock abroad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
