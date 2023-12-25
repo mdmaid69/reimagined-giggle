ad0e0f3c6ec270da03dc4c2e38e5333d084873af
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:00:33 +0000

    like light recently full remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
