9d3d1f6a9d38745b78d12174e15008b8930badcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:32 +0000

    second wing day getout wide

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
