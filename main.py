64483d7d426458220956f2fe9cbedf20a7615397
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:05:40 +0000

    issue myself hit cum grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
