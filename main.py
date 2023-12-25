88856b9b3a45ce595d753f04e269bf54d24ca662
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:56 +0000

    television impressed them swimming trap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
