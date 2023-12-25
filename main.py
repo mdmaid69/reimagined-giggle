83d0a482232754b37839ef1b07b367b75bf1371a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:29:16 +0000

    wrap like same always report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
