0de765cd579e893cebcdf033abb8c1df18b9d7c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:49 +0000

    strength account approval to through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
