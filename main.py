06e1045fe26f25a96459137986f5adbdbe82a75d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:50:01 +0000

    cost Hh big beautiful machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
