7f9fd35bebe62dbd5d84b639dc0f9bc21087ad58
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:12:42 +0000

    various treat custom equal search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
