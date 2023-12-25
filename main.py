1f4fbfd4cff52a2915a0b0429cf669842b26899f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:55 +0000

    point perform set finish long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_string():
+        return collections.UserString()
+import re
+print(re.match("h.*o", "hello world"))
