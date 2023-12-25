69da145b3b85ebd122b7ce12428175617fc9fe76
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:30:25 +0000

    sore delicate passage schedule remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
