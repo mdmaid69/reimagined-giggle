d030d7f8c99a7c8fb1154322f59660ac10cf4ba6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:01 +0000

    pour close fence tradition declare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
