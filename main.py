53a99be604b0f9391cda324bff802d086533b501
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:19 +0000

    big run space foot difference

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def square_number(x):
+        return x**2
+import sys
+def print_python_version():
+        return sys.version
