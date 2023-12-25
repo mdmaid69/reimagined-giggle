897ee7386b07eda620ac143f8d83e1b878c9caec
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:08:07 +0000

    chance discover tap hard cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
