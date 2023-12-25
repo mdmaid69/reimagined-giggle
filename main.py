b687a7bb606978e3ea3e34b0d25b79f60e13fc33
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:37:09 +0000

    float movement far pain edge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def divide_numbers(x, y):
-        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
