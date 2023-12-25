c2bf7ed78a484b319f72784380b84ca2a550547e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:49:42 +0000

    afford practical regularly tight any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
