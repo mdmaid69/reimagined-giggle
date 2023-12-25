7e765c91bbbf09534f9b23c7b50fab0d5ce58c63
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:01 +0000

    pour shadow excellent opponent fashion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
