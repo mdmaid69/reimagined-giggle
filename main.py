b2ae55306f3384df661ff91988aa378d2a0eca77
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:40 +0000

    all recent form clue slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
