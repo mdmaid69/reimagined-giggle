652485cca2c5f67f6c98aaf1d68992ebff3f8f6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:25:55 +0000

    opportunity excited evening visit various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
