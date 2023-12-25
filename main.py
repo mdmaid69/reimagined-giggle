93673cc0c6cb15350637a682b18bec832787b99e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:48:45 +0000

    hear glad proud admit laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
