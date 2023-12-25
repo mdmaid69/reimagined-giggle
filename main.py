a53492918a0caf2c47d7aee37c3d415d324ebdfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:14 +0000

    dig well warm effort about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def remove_duplicates(lst):
+        return list(set(lst))
