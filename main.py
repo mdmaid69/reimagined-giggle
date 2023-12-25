2b3ee180a9b8d4025aacd5a0403db4737d2ebc89
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:49 +0000

    deep criticism cinema object impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
