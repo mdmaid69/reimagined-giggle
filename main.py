d661326b9df28b03861cb303883287651b12f13c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:12 +0000

    judge after expression draw ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
