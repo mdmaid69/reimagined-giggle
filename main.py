a4e51f2cecb5d834ccfbb3e91364d48e302da44d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:51 +0000

    must right mark hardly recover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
