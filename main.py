9206bda1f0535f1878bc6980792276406a66770a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:20:26 +0000

    tense her century future file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
