28436cdf916f1414a38f6923dd44e23074b96085
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:29 +0000

    apart exam ability neither door

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
