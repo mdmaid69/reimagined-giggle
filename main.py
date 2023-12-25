e7e4dfdd560a7a1aa36d2ec5121f8e01842002fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:53 +0000

    instant funny space church sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
