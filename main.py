4898c190655645c52e0030295d3397bd912312db
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:38:02 +0000

    through development run address instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
