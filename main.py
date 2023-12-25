160beaab425b67d3121f8c495bb7c7d226e51c04
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:07 +0000

    rob hold band them violence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
 text = "Hello, world!"
-print("Words:", len(text.split()))
+print("Characters:", len(text))
