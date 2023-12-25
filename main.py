227e5473c5b539b2c4e8e95f9e8a5565d6a5a195
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:09 +0000

    runover trade cough exam confidence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def count_words(sentence):
-        return len(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
