5a446438377c05a48a30685a9f52883f03aab0cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:37 +0000

    couple however unlucky winter Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
