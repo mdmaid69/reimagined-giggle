e86b957ff1167ccc9544ee86d666a5dcc766eec9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:46 +0000

    modern Yy reply cost travel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def reverse_array(array):
+        array.reverse()
