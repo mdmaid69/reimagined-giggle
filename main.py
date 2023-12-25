bafd1648d93237af8be4cfaf4df8012575343f0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:41 +0000

    argue impression contain factory Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
