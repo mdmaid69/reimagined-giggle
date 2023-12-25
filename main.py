bd6b17c38ca40b8951f3e46022b86b60045258e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:24:12 +0000

    explanation dress rescue chat story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
