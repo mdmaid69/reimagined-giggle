404873b9501a6b4af58f359268ab148f73cafc4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:06:25 +0000

    partner translate report cost good

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def is_odd(n):
+        return n % 2 != 0
