f316378181773b0322b48f553984cb0c0045b83f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:38:26 +0000

    wealth move become comeover still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
