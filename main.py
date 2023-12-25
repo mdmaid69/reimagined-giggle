a3b09eaa847d77ef5c9b4a2083d23070f9894068
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:25 +0000

    nothing dish when visit recommend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
