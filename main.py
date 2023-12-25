c52fc5116462737808111e52330906140eab7836
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:03:28 +0000

    put 568energy work building community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item(array, i):
+        return array[i]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
