122d0540af9a279b509a4724c7c3508fa8d149df
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:08 +0000

    method theirs excuse both need

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
