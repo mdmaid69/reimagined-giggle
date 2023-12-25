f6f1313add4d844be85d3ae6c9cd39142840662b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:42:06 +0000

    quiet godown measure fair rate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
