4986a9620ff9b4c0a97b459302f3e3b01406017b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:06 +0000

    reserve afraid instead every story

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
