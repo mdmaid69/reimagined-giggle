07e32fa5668ee7dc545bfe8a2e7154d97fbd96f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:14 +0000

    stupid plate hear little dinner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_from_array(array, item):
+        array.remove(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
