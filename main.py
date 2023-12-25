5c756b2ce8982d3d850c580ac1e88f8212ee6a83
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:50 +0000

    below wheel small wet holdon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
