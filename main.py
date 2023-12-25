8f9ca454a6c456766b7c22166795580972681912
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:04 +0000

    behaviour meal together control relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
