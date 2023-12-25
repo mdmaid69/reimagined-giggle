07618c83b2faee468a2f7d59027289aac2ad2a13
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:14 +0000

    future shallow loss beneath rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
