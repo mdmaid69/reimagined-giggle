428f4572041c3e9c32f3fad8122e864d8aeec4b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:23:28 +0000

    apparently since joke train weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
