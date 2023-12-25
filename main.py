1f34ed88e94fb461236f9769cd501519f322244f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:51 +0000

    view card undo birth fall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
