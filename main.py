c79d921ca373f426468f2f374f7e34180512b1d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:13 +0000

    sentence Pp if Cc fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
