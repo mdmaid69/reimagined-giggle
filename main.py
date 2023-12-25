f90f7acd79fc03e416d0e7b6de5a8a7495e26764
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:45 +0000

    permission stranger shade window contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
