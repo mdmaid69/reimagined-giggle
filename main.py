7dc779e834118a94b237659818f0fa6d05009c3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:18:02 +0000

    base unhappy cry impatient land

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
