e41414d5160bd750c040754d39c58f9e26f4f416
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:03:05 +0000

    sweep slip worth refuse spray

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
