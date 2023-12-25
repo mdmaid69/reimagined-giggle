0d89c3c1e0fa00d2dddf500bb7fb2e14715f3c98
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:44:22 +0000

    steam identify wind work action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
