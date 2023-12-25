f94f6257744ecc70a8830516e40201494dc761ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:27 +0000

    science Hh right move purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
