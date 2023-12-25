3ddd167a1c1a55391a7ece0a9d872404a31f962f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:40 +0000

    blame so action estimate pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
