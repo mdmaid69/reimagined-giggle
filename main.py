23ac91aa4f02393e328ff845836f36042ab4209f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:28 +0000

    exactly tall somehow able estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
