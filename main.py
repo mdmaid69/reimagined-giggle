dd7e50184fc1272d5de37615f12f0e38f26f2b20
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:31 +0000

    mysterious unite bored interview complicated

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_set(array):
+        return set(array)
