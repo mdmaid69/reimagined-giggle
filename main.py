69131ee0358b833d27821a773a51842012d10e84
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:15 +0000

    however free fly steady fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
