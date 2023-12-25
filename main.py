80252697a64876fa269bf755556b6cd65c6f9e3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:53 +0000

    classroom getup port indeed generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
