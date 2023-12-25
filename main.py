895c74dc6efd1c5f868986c6f92dd666e9b626e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:13 +0000

    left class realize boil there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
