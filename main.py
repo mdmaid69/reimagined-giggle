d4ac4899b44633fc87b0a5d9a163b43180214336
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:41 +0000

    point forward practical available colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
