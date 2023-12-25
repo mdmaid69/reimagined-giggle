dc4f9e15a06368aafbb9fc9ba885eb320b4ec2b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:15 +0000

    every many scene matter address

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
