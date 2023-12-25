4172d055003c1fb2a85071b276b1c1d3ee1480ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:10:21 +0000

    heart night moment shake platform

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def is_odd(n):
-        return n % 2 != 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def convert_to_binary(n):
+        return bin(n)
