9f829d6227cf15b4ac28040a891f113125b0101b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:11 +0000

    new transport never unkind way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
