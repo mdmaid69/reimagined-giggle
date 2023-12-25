b071c08da334ed17253be4cd64985742280c4d2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:25 +0000

    respect present stare reach below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
+import sys
+def exit_program():
+        sys.exit()
