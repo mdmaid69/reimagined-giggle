9e342f18e14f026d2ac5b955873344f0dbfe3fc8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:05:45 +0000

    left word spin importance copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
