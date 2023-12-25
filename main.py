7ee822463b33cb5073fdae22c6419389a1dfae47
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:40 +0000

    among present quickly separate conduct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def change_working_directory(path):
+        os.chdir(path)
