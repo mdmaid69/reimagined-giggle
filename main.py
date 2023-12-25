3c75b7a4dd174ee915f3d5a767aba61b21732597
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:19 +0000

    emergency centimetre door delicate community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
