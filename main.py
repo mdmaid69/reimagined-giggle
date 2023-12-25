9a1d15f6c1da64e846afd770fcaf5074031eb84c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:16 +0000

    joke gap sad excuse achievement

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_bool(array):
+        return bool(array)
