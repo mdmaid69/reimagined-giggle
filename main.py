37ee66e3c07d06f76bd679a8de92111ed3291faa
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:16:00 +0000

    weak sink pure exactly survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
