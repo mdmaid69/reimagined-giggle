cea8977a6aa74185d75975e14480020fa2a2bb45
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:04 +0000

    health consider phone instead emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
