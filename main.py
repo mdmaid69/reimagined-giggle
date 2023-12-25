b543bc61fc89804ef10fd5d15087196575de81af
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:20 +0000

    upset dare many Vv practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
