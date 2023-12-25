a442e111564161b8becd3c6b31819f31a2f4f82b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:14 +0000

    Rr alone tomorrow post careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_power(work, time):
+        return work / time
