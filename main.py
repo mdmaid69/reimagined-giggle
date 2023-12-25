d39cd5b08539396c8d2ab8148c1386cb8948b8e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:17:50 +0000

    ground maintain region separate pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_str(array):
+        return str(array)
