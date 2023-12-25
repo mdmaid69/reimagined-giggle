0d0da2989b417d4e4e789760198381cf0873fbda
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:29 +0000

    trouble file cross effect continue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
