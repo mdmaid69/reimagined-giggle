737795d1c57a29d5d1b6a964719681e9b82a8be5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:45 +0000

    difficult ilovedicks right birth movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
