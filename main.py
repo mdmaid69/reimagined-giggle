eafd13c82c9e0edd88994caff3eb8d9b2a57e839
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:09 +0000

    round programme entrance keepaway brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
