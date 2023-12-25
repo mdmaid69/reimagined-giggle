f1b0d555871ba4bd57da30b60180f716e4ee4325
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:17 +0000

    busy soil throughout medium disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area(radius):
+        return 3.14 * radius * radius
