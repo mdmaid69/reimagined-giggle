8413f28df37e40a91d9407a154631b031a183332
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:54:45 +0000

    praise woman land address noone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def subtract_numbers(x, y):
+        return x - y
