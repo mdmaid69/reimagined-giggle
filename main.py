b1d329e1ff0e18fb83819449b47349ab4bb0ac04
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:27 +0000

    physical price card colour along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
