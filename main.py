738e596963b3ab409754c3eeac220da3cb839463
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:03 +0000

    alive attractive beside circumstance pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
