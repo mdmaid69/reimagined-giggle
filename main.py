a903eb00ce076ce21942c662013c455da698d282
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:13 +0000

    stranger city like comfortable land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
