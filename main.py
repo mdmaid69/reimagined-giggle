12bd5f1167edd293dbf7e7fffb6aabb81e19e6a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:06 +0000

    bored offer example concerning variety

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
