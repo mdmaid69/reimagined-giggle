133a96befe4dfa62d77dfea8d7b44b6fd87e0258
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:26 +0000

    chief about manner bury print

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
