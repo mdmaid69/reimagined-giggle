f2f6aefaa228d452438eb123b3ad70a3e9ab3108
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:59 +0000

    lift themselves party exploration repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
