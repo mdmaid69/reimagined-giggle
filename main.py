bf5119ea92fdf074728c51d6a2e91449f4bef055
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:28 +0000

    level morning game public hair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def convert_to_octal(n):
-        return oct(n)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
