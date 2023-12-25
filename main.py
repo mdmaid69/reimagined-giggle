2f5abceea6905da337654b24ac02a3040b6dc60c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:30 +0000

    wake may spread result pronounce

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+print("Hello, world!")
