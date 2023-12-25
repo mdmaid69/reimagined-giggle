0aa8411dcced92cec9d81282c88244cd3d5a7d71
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:50:43 +0000

    everyone desperate anxious drive earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
