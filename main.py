cc8293acd0ed4cff29b95cdd4a2b88a766bc14c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:51 +0000

    replace action nowhere hardly fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
