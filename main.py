839d559f4407be9d8d99f14a52fec317566b4ecc
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:14:04 +0000

    characteristic rubbish general narrow creature

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
