30a87dab94986bbdf4a563ff8040cd7332aa023d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:26 +0000

    sign Cc perform crime include

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+for i in range(10): print(i)
