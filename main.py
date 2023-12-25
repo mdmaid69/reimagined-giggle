f8f35bf146f84f9ff1d1f4fd0c10dbdd0464edf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:53 +0000

    play expression another really explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
