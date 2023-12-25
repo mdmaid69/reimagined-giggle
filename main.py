e59b2a33380d2bce321097c7c8bd2df58bd98060
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:30 +0000

    risk Uu advise sing powerful

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def is_odd(n):
-        return n % 2 != 0
+print("Sum:", sum(numbers))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
