f07d1d8e4cce0237e03dca380acb085b95417196
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:01:00 +0000

    thirsty content tidy minor fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
