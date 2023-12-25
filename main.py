4becf699bf1f2bf40c549734699481c9eb43227d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:13:42 +0000

    hole precise recover silent scheme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
