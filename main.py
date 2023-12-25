f75b682f0ad8efe52ff9375025144087cd93154c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:12 +0000

    wipe worth bad star within

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def is_even(n):
+        return n % 2 == 0
