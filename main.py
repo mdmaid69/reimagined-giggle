23e9a9786124d0965cf27caa217ba6495b4856fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:53:31 +0000

    term city kind unusual properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
