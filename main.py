5bd4292588d1ed4410056a0fd01f99c4dcc96c4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:07 +0000

    only boring plastic holdout remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
