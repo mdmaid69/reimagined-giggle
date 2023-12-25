25b3ccae4b53dff6d14a390bb09a52671515afc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:59 +0000

    able probably gift murder save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
