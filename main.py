b2ab45ea4247ae1e5be564567901c61df12dc60d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:31 +0000

    573entertainment between reach design passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
