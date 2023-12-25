37feac152ff61e8e961f4a11232effd986d0f98a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:20 +0000

    row opposite shout off Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
