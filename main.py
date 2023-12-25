6d2480a713335658b43e1a80a1595a4c03784ae2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:08:48 +0000

    claim cry if it tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
