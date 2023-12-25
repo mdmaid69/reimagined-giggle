615c53f60c2d28459f7a9b9a567e7f279b01970a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:19 +0000

    side again live also lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
