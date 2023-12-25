e65581523a8258ddc68949507209dc1f6efdc071
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:22 +0000

    takeaway cap convincing laugh alone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
