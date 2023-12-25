aee037f36136896989c65f8ee773104711f1978c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:39 +0000

    luck him build sign window

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import platform
+def get_python_version():
+        return platform.python_version()
