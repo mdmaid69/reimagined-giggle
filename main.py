33add4f52c393bb4732c55159ef5d5e107cbcf72
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:31:21 +0000

    company original too buy light

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
