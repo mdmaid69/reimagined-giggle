d7c868eee8d7eb0a16de5b1de4a86017c4c99eb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:54 +0000

    police few quality wind busy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
