1af70007dd175c591ccaa66b1bb5a2715be73c9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:27 +0000

    responsibility bear spring lead sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
