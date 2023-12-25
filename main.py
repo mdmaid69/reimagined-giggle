5ec49b392d6be9115a17f69d9c53a634311c741c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:53 +0000

    against glad city sting for

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
