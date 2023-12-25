2678712417cd04e76b4edccad674b3cc02971900
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:15 +0000

    tend limit tread sort pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
