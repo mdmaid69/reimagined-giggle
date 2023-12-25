10364eed5475fe223f1449febb08a603d5664621
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:31:45 +0000

    difference obvious lookafter complete talkinto

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def create_user_string():
+        return collections.UserString()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
