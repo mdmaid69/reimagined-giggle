d6619eb66057588fe298217afe9e21f6c24f54d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:09 +0000

    cost purpose spend careful fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_length(array):
+        return len(array)
