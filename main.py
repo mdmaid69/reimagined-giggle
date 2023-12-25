a6657c97162e1d60de86095b39b4b06b87f646c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:36 +0000

    warning home takeoff furniture sell

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
