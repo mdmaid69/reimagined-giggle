474f93264e4b8a8e053230ae134a8c50f46ebafe
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:40 +0000

    bored like show small settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
