e2e79d166820f3495ca444b2619a1368bdadb25d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:37 +0000

    swimming regret gate talkback ability

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def square_number(x):
+        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
