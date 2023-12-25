51c4cffa4231b9d7215e91723a9b61630007ca8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:37 +0000

    strength squash pile public possibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-print(sum(range(10)))
+def calculate_logarithm_base_e(x):
+        return math.log(x)
