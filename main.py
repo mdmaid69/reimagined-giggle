6f4bc52b67326b20418fc7c5ce4c0133542cec7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:23 +0000

    though near invitation us round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
