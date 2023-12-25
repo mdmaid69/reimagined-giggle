1d3163600594e8ffa185a25eb8c73cddde55671c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:38:03 +0000

    neck Ww either succeed kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  def square_number(x):
-        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
