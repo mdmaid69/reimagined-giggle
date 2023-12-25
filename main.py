f38e85358d83acaf0e82d626513f502d2be64811
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:27 +0000

    slight matter expect hole check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
