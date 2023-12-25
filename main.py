126bc44d0e2dae518333fea2af57c9c0b991a666
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:25 +0000

    transport defeat circle ball switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def count_words(sentence):
+        return len(sentence.split())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
