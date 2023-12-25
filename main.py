6fc871a25349b8acfb4bef15147b0a87a1b629cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:42:09 +0000

    then characteristic cost then area

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
