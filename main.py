cdec1fbdaff0fc1173a44a4a9d99733c744d74ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:26 +0000

    voice track wide convenient dick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
