0199e94518cc54aa86ca354e95904fe3b8b29610
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:31 +0000

    confidence shade substance lean extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
