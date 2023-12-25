f4738512432d3a555492aa282706e9fc10acb1ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:32 +0000

    breakthrough tune meanwhile gentle old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ino(file_name):
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
