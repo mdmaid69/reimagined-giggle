cf469f6e7683e50b954f8a899e2289a39335c6ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:31:26 +0000

    who good fence lead beyond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
