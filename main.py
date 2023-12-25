1d902bf2484696de3bc733e627179c3c2b3b0ef8
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:54:25 +0000

    thinkover normal manage brave trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
