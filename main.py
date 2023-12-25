b63b88ef18d04b68d75117fadf146726e19f0ed5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:40:23 +0000

    find petrol measure gather either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
