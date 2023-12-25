6915997aa3d3d628c026fee98232012a2f8fcf45
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:22 +0000

    complete pen pronunciation prevent class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
