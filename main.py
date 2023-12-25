8b295d062f7954a45cad7432fdbb4a69451df201
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:20 +0000

    say permission may stranger adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
