a63431615894207e4da7839fa21f4ef1c365f398
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:19 +0000

    night clue limit flight clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
