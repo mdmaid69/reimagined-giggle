00a4b1ff92996dc495722cca75a180648fd4f199
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:49 +0000

    big handle maximum comeback top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
