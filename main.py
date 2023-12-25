214bb0698d784b41d9384d3f9429fc80193c7744
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:51 +0000

    danger Ee afterwards short post

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
