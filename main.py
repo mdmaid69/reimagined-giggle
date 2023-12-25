13f5914144772a2bd2699517ecc60729e9af22fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:57:54 +0000

    mysterious lead for tooth disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
