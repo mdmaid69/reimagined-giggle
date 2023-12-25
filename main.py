d707fed4ba141943039da1940186383688009d18
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:13 +0000

    remember ring ought fierce count

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
