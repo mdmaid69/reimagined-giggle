0085fb8ae024c64b6e29614a83a656b48f397cdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:46:52 +0000

    mysterious shade comeback lend you

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
