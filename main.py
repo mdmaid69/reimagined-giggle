48b2f045cf7692913aec53f48c2ca74e8977303e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:18 +0000

    downstairs family couple extra towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
