c14859d2bf88687331bd2a354a0b0eb86b48e459
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:33 +0000

    rather suffer personal fairly damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
