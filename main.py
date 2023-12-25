e51399478044b93a3bf9c2c6f42fb4f3e5617342
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:01:46 +0000

    clear yours communication closely address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
