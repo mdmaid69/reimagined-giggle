3d9d032343c6617f9d6a5678e96e73ec6c4e2b28
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:11 +0000

    approval importance comedown raise funny

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
