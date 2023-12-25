cf9ec4c4de16b69d808ee36f9f97e47b3cbcad04
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:11 +0000

    bush threaten split sharp ordinary

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import random
-print(random.randint(0, 100))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
