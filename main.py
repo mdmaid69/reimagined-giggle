1a9389254d50f62553ed9ae6676a1d704b8c475b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:51 +0000

    start to hate less cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
