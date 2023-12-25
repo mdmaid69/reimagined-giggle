42b2282ceda9d2c54332481531da1a0f357a3a63
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:02 +0000

    them fond window so sound

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+x = 10
+y = 20
+print("Sum:", x + y)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
