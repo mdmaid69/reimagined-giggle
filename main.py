ccb002c9238a93e4ec8a7b0b1d76b235828e88a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:41 +0000

    nowhere break bottom within market

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
