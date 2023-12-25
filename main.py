207f6e6ae249b1dcbe90029ea44702add6b8b502
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:49 +0000

    cheat noone whenever complete give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
