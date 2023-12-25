bfe161da2235846f88981e64822ca849fb74ef6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:10:47 +0000

    set 567enemy raise cost physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
