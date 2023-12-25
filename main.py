b7ee87d944e14eb737244bab22ae227395282eb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:12 +0000

    indeed welcome constantly win piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
