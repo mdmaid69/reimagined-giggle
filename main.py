ede7b7f067cba6b8c1d0ff1bc0f059f9f553394f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:42 +0000

    minor shoot crazy today demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
