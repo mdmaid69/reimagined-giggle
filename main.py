beb9900c3c9b29f37c887af5e30262d8fdc28288
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:30:57 +0000

    quality shade on difficult pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
