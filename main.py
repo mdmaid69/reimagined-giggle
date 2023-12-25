23ee41f10db20ace2a602c503ed98037fcbc0036
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:40:35 +0000

    if determined objection rest spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
