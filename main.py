0e871f0fdabcd6b9dcffaa5b07d683375a303ce6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:14 +0000

    plain speed answer unable cut

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
