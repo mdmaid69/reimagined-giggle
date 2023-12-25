59dcd761125e4388c410f06820b1575dbb3aad1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:24:32 +0000

    how choose mark walk steer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
