5658d3c91b0e8bdd7732a967f5de081fe2095706
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:08 +0000

    branch thinkthrough different course include

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-for i in range(10): print(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
