ce263633feb629b503010f7ebe08b9384ca903fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:52:15 +0000

    dull bunch forward hospital bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_area_rectangle(l, w):
+        return l * w
