7270a751214579f34e70fee60d316db274209327
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:18 +0000

    blame last bad success maybe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
