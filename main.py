6aaf2c6e52784213e9a4df73a76a699bc277d33f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:04:21 +0000

    hole the provide trick suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
