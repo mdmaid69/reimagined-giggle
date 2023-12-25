54f01d179f93314edb157b117b52b312771b81e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:10 +0000

    round fortunate OKorokay runover precise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+print(re.match("h.*o", "hello world"))
