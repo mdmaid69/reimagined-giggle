11e9f422261ceec48bed8f25fbe20b89f590219a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:59 +0000

    vanish satisfactory sink under unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
