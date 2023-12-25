c0ae9821e77c178a9b6069a8952b02e93cf6a8ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:24 +0000

    within soon bright belief on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
