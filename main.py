6fcedcc8102ed6178c8ce1564cd452ce03cca549
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:46 +0000

    experience false country curious board

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def set_array_item(array, i, item):
+        array[i] = item
