57e07e4b66a9768a1d215fd41f1df115c3ebf6a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:07 +0000

    offend comeback pot open ourselves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
