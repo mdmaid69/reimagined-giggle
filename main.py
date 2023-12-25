4cec5eb321dac906b75635280db8e31cbaa56b2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:45 +0000

    tread happy gap lift artificial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
