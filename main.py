2161732786580bbdfdf789bc94bea3d53b70ff10
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:06:01 +0000

    follow state bet size contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
