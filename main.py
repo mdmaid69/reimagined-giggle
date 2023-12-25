1dd5030b7784c8feb00db4be55dce5181c8db2ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:15 +0000

    imagination apply mind group shine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
