93f192abc7295d4ed49cb422f0766a69f493cdd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:39:11 +0000

    do accordingto pride survive ask

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
