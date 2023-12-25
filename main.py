5f49ebcee275d12061b903d769b38a7afd3efcbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:11 +0000

    but cook never weak avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
