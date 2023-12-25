b6cf242f00f7594cfb04b88e556d0403dafc1181
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:55:37 +0000

    phone hang pipe however up

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
