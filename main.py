0b7463c7777ef7faa9ce58a93c9717817cffedcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:40:05 +0000

    intelligent sometimes aim send sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+print(os.getcwd())
