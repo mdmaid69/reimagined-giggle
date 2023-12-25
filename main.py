156f109265dcf973b5e2ab4cca90b2e2b0d09fd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:54 +0000

    either issue makeinto absolutely either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
