f0a6aab6c37bee8934ea7c68f3fa668527f05fd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:56 +0000

    near study beyond climb concerned

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
