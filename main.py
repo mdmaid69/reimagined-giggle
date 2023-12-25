0bc8df0fdd638ff82dc851227f7ef11b5374c754
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:33 +0000

    offence female history clothes among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def delete_file(file_name):
+        os.remove(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
