176e0b4a889e5147b0bcf7ec94cdac884d8e8de6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:31:34 +0000

    hit away after somewhere prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
