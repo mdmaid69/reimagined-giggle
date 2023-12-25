947c2996671986e0891403f8bf6139ad1b7016c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:35 +0000

    storm discuss practise put left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
