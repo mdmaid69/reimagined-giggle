4c06a1b9ff214ad9909c979e9df927e3d3e9bba7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:02 +0000

    match name brave example only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
