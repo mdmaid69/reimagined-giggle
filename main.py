a50f5d54943bf5b0af1cbc498d83dc9011d55084
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:37:02 +0000

    defend for popular only cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
