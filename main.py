189628ca7a79bca25659648bf2cabad3aefbdb98
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:40:43 +0000

    nature deserve wire agreement most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
