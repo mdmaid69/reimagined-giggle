62927eb2fa6f8d7401490f21db0456402e070c34
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:16 +0000

    pipe whoever ever Rr brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
