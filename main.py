f81678042b5a3a2a9cc7df819a47ca3e60e7935e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:49 +0000

    drawing Kk at might platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
