3d4e21167920bb1c5ce39ef64a88044145b72c8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:17:05 +0000

    tongue issue petrol new right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
