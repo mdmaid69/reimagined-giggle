da86f668795fa4d67c81406194873712ca3deaa0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:13:07 +0000

    present thumb example important before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
