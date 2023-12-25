749c49306767a49c9783648a88cb59be88708f11
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:51 +0000

    famous bringdown holdout count outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
