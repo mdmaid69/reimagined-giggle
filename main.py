90f024172518c7190336d7e1b1311726e91baa72
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:21:14 +0000

    invitation advantage cheap appeal laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
