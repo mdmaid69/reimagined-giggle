6402362be25bf83dd762b332ca15cfa034975718
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:08:24 +0000

    try block government lookup dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
