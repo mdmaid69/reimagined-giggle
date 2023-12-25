2b0f7fd8697ce691190efd6d3496db2c074cb86c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:31 +0000

    become and country short instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
