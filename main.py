e4bd3921553881bff8c26407e383b1359c87a547
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:36 +0000

    coat experience pleasure better slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def divide_numbers(x, y):
+        return x / y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
