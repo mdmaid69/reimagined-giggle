eb4d57aae9e70304fba224d0eee035dfc7a72610
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:24 +0000

    agreement violent major effect interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
