5f7841d098049ebfca1f5a36a6a8dff047fb1999
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:24:48 +0000

    sail holdout narrow fight anywhere

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
