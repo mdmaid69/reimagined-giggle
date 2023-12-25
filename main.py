7b6b243a686bac92f2c325b4a91cdabf4b1b974a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:26 +0000

    572enter male kind hide show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def convert_to_json(data):
+        return json.dumps(data)
