29a213ae8b5aad895b25e37ff4d64028baaff063
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:48 +0000

    report repeat apart warn machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_power(work, time):
+        return work / time
