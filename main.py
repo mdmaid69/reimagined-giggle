9ca82bbdf09fd57afd6902ef0e72ac9f2d3d9f92
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:43 +0000

    around correct cause keepout afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
