60fe9c48ebebd3d854c2b76257176886fa89e20a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:22 +0000

    across laugh mark treat hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
