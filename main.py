8dfdb94af1525dce9f63fd6065c0a46d27f7cae2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:18 +0000

    colour like never content stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
