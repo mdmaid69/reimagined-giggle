589baafcae24ae2cdfca05a0df981de0525f79f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:48 +0000

    weekend settle one model mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
