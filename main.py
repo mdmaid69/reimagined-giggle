3daca9a082cc3304630a003fe5a8af131bd184e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:51 +0000

    bend throughout odd store eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def get_list_from_array(array):
+        return array.tolist()
