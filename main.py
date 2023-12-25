b0d9225b4102bef3bbe4f9bcd46dd7782c726856
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:49 +0000

    branch coat circle replace stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
