cd1859f6b6f0fb9488ac6865428d887fa2ae1164
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:59 +0000

    remove giveback once steady needle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_pressure(force, area):
+        return force / area
