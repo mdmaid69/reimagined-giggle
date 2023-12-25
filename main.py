97e9813b0996ca95921e223748808d1bf3cae133
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:13:58 +0000

    debt area skirt couple what

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
