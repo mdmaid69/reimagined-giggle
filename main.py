144dad8dd46b042baf06aa2c3a1092e739762dd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:57:58 +0000

    hang question her apologizeapologise project

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
