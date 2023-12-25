0be8fab04f7ce230a07fb7bd0347a4b5aba5ab78
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:18:04 +0000

    immediately bread ahead star estimate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
