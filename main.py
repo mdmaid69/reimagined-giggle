9a999033243d3b1ac788bca4e7a0f795074d51b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:52:43 +0000

    disappear his list concentrate pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
