bbac81bee05fd3507c3a1a232b2fef05fe8b1ef3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:40 +0000

    advantage finish ear take style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def remove_directory(path):
-        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
