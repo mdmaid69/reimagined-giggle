a2bbd2cf10bf05e58b1508aa58544e6103c73414
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:56:00 +0000

    round right street mother maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
