4a590e63dca1bfec3ed7e0ed4ea87e9801a38d95
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:23 +0000

    amount pencil deep excuse familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
