f454330f16988e87a50bd9ade998520f05b53a9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:11 +0000

    slip organize sort often group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import sys
-def print_python_version():
-        print(sys.version)
+def exit_program():
+        sys.exit()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
