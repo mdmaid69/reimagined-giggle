0269d336c3ee9720c089c9058b3e8b799571ae60
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:31:46 +0000

    promise fresh silent turndown sick

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_from_file(filename, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.frombytes(bytes)
         return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
