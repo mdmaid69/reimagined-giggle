39159592f05199c124c3020aeba34701ba89aa46
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:59:58 +0000

    cook club right explanation statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
