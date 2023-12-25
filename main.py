fc3b41bd5e47337853024997c6dbeb1df1b637d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:05:18 +0000

    acceptable mind precise with moral

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from collections import Counter
+print(Counter("hello world"))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
