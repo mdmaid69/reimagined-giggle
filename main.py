aa3f486765fb42c1da9434326b3cb8de2ba29b53
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:29 +0000

    table turnover who exchange apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def count_elements(lst):
+        return len(lst)
