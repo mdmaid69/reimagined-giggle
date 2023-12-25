72a0e2ec87b63cf3204b83493939996d69cf0950
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:27 +0000

    rich means pure consist across

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
