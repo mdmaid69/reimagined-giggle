16485e65f6b51620f325a35750f9f60a217062bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:39 +0000

    object clue breakout taste trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
