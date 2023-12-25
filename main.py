50623370702688355efa77a72bd1251b9eeee45c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:48 +0000

    frighten challenge room next forward

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
