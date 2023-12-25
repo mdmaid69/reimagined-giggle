7c079385a937229c2442f1fe096ad1dc49024d41
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:24 +0000

    willing bin involve objection weather

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
