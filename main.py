70b6390f0a8f8823ab57eb788b0cf23f28ee4154
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:33:43 +0000

    since stone journey bear abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
