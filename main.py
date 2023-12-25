f3d58dc048fdba49424280b7d5b59d1d06d0aa38
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:40 +0000

    father go software skirt play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
