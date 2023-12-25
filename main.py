94a56908239d89ef43f2b4018362af818fb451db
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:29:19 +0000

    cost left absolutely share favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
