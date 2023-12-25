844c5c454d067f8652e4cea998135ab2799c0d70
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:05:31 +0000

    sort slightly extremely familiar way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
