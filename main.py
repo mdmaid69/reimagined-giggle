9eb178a6c3a73f49c90a815cd8f918636d0873b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:09 +0000

    track care age weekend threaten

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def is_odd(n):
+        return n % 2 != 0
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
