a43fe0f423c0939ea005709111c6427e855c22a2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:24 +0000

    stare thinkof ear belong search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
