d876eb57c24ee94ec8422b96ed6f95da5edb3b30
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:50:22 +0000

    memory choice rich impatient exactly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
