ed18ed9a319047ce09976f52a81427a7f84769ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:51 +0000

    get wall lock pinch event

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
