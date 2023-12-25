f5fba058d3627315e11fdcca8c6d7e93f800927e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:04 +0000

    minute offer discuss separate refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
