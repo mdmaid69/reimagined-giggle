4c4bba2ee3855939d74efc32604e3c3a6c650405
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:10 +0000

    whenever hello repeat should defeat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_acceleration(speed, time):
+        return speed / time
