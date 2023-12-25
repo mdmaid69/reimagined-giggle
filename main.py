08d0451b7d5dfc0258cfb73dc3e39f1b0b26b4aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:40 +0000

    suckme mend comment recent love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
