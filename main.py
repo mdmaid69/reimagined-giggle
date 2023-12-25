a3463b09adb71a4ba9c6e88ee1d7ff654862f5d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:57:43 +0000

    true bath face allow them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
