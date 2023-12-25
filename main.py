9a43284edcf78bc97702ba7d106f7d9250f3b9ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:40 +0000

    excuse colour warning poison behaviour

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
