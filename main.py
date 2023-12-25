5cd1e02450a7166ec9a89190783dff49f45ded9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:32 +0000

    field fresh advanced upwards borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
