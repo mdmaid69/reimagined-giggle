1b724a3f685320284a7321dc9c711bcf7a004080
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:50 +0000

    tooth giveup confused achieve proper

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-for i in range(10): print(i)
+import sys
+print(sys.version)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
