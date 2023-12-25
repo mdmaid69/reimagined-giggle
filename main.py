4da8209d67e7b8bc80a831eb6b5365bdb667ca51
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:02 +0000

    meeting national experiment gradually natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
