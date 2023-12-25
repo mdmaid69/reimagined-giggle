a634f830355d4cf0ed2f7ba6338e73edbb13adf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:49:01 +0000

    small begin stick rubbish expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
