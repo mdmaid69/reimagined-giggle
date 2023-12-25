75038a873c756d3574ab0868d97c8fa4ffddfae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:05 +0000

    cheat square science point brush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
