bed53c46fdc21bddb3a62b68503a748cacafb8d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:28:23 +0000

    confusing in plenty fat concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
