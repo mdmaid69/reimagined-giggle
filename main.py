70d2c8fe9f386d776da6c4798fe6e1e7fffbd2aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:11 +0000

    bang precise story allow refuse

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
