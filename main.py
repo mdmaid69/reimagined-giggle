84593e54f03ac5e230b3565147ac56c91e0cd1eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:19 +0000

    closed ask crash list hear

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_to_octal(n):
+        return oct(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
