26fdd62084d5885af770061255dc9554650e555c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:49 +0000

    translate past view regret profit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def convert_to_hex(n):
-        return hex(n)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
