989803840f5e43b640a70d2342d1ce420ac4ea18
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:01 +0000

    proper above praise mass something

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
