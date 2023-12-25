01c03f9f528f22613ca8659aeb335a002f49692f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:05 +0000

    laugh zero over learn runout

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def reverse_string(s):
+        return s[::-1]
