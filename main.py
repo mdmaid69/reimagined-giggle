0115f70769ba9582a5f67479bc2c8b3fce5d2de2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:54 +0000

    succeed safety mother clearly or

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
