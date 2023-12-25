45cc9d61de9b7ba06b093645754697000fea1c59
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:49 +0000

    damage platform count rub trousers

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
