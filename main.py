f060f35d5678752de7d1d729b49d3efacae309a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:29:48 +0000

    Dd division burst relationship distance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
