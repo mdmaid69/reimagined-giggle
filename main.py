6963ea925e4c3d1222ce2da8f5b81ecad312ef01
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:41:26 +0000

    ago respect worth back pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_array_to_list(array):
+        return array.tolist()
