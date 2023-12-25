d691d1d5c1ee0d2e9342aed0f63dc24fad3cb348
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:23 +0000

    makeinto meeting sister poor frighten

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
