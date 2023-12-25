916f285d3ef4974391302656e14aeb46331ad395
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:11 +0000

    trial dick influence exchange branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
