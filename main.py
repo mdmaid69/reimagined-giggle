ab63ea0ded13689ca307aad408860bbe7654ee88
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:36:45 +0000

    shake answer ground door chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
