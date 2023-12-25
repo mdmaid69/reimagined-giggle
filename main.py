530e3548636acf2a309fbdac0d74203d10c00e9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:29:46 +0000

    development area design danger limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
