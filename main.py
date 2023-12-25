f028fd46610b59ce97d5c88e5e90255a76cc2501
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:36:49 +0000

    play large give explain shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_list():
+        return collections.UserList()
