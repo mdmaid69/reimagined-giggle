af1758ad14bf7ac139008914be66b1746583a749
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:18 +0000

    file switch lower say lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
