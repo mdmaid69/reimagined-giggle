c32d1f4ca2b77aeabc5bff7f726a1dcf789b03c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:09 +0000

    afterwards as getaway male pot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
