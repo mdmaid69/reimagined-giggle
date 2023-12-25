ea7ccbe3ca8482a28e37ae6f3d8a48587634a229
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:39:38 +0000

    ought operation proper age interview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
