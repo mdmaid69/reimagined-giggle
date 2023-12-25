7d2c1ec044d94de36d6a9b4189699f956f0078fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:01:20 +0000

    label come follow last feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
