e5cabfb39f66d851a5267da37114e4eecd6038dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:39 +0000

    same combine uponon touch die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
