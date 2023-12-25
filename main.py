39ca8017764ca213d09a21f6379d8b0db71bdabd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:20 +0000

    begin man turnon slice chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
