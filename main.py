7461ccb45e84ccac492225571870b55b1c192e26
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:49:57 +0000

    stay workupto deep before curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
