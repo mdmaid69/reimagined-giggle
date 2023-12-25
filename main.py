8ebc355bc840a4c6f43bac1b2dfce96d19f165f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:42 +0000

    runaway chin clue kick worse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
