a5088e731a707875eee08380b8102cb62a4156c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:54 +0000

    mark full sure we backwards

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
