7047e09c0f98037f53af42fca9ff7be5f38aa91a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:55 +0000

    high phone dream male believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import time
+print(time.time())
