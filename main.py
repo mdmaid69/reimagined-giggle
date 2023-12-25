ebc7d520afd50a9497b15647a58a7bb25a3e84f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:33 +0000

    disturb offend regularly level call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_bytes_from_array(array):
+        return array.tobytes()
