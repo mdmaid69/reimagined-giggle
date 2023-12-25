2dffbc929dce5de153fc179dc84a454b5fc2a119
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:24:44 +0000

    satisfactory mixture opportunity crowd tape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
