31eca345c3f1cd9b514450ffa45c9abbd6b7a429
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:36 +0000

    tense cry anywhere knock discover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
