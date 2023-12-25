20c70054dc2ab2f8b6f9b8efa8235d890a8487dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:20 +0000

    help discover occur attach lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def greet(name):
+        print(f"Hello, {name}!")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
