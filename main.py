e8c21394c4b649922bac05de565b7c2e79afdade
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:03:52 +0000

    manner power spray dead almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
