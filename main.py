e6139415a06e8bd8ab602a40b0c3fea39711cadd
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:16 +0000

    basic sleep tune comeback sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
