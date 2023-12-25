5732a9049be4f26fc8292b2b23666e86d0f7569f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:56:56 +0000

    fresh impressed service breakthrough confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+name = "Python"
+print("Hello,", name)
