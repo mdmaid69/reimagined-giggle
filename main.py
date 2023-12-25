286e5db66deab2e7e125c662b451660f088a1239
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:42 +0000

    environment silent unlikely he sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_power(work, time):
-        return work / time
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
