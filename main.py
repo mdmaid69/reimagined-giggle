01cb04f704ad7060e1b27f1fb849547c7a25600e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:01:40 +0000

    bright skill part join along

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
