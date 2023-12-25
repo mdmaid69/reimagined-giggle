2622af6e6be5bfeecaf125accdeef6e6f7dc9fa7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:15 +0000

    health jump before wise may

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+print(math.pi)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
