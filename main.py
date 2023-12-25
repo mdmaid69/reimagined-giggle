4648475b9969db40b1d837b584d86d844c988bec
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:19:56 +0000

    maximum bar plan stop goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
