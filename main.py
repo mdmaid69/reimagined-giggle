8fc2ec41827966b98b406630277af5f81e3ecb4d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:08:28 +0000

    chat brother but father reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
