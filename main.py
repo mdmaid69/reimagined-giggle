8e70ec1ddcebba5920c0dc8be42f9c231e0900b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:28 +0000

    narrow exactly brush scrap worried

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
