c4d2a58df33eb4120573b4feada1bd28b2918122
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:27 +0000

    everyone commit slight time until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
