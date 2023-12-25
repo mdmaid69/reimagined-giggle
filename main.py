d2e08e626dc7a3b80e91edf28feac6bd0d70c53c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:41 +0000

    flat pin mouth urgent makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
