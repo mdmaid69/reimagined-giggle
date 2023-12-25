793b52fef7d8647ea788162063ddaceb034b80c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:00 +0000

    name coffee rich stir account

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
