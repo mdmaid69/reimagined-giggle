7e20e2061d40a90b9bac4b16d9bf0a912e6d4836
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:37 +0000

    bother cover problem dangerous compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
