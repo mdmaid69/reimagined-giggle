939f9bc597a66d9be386ca6318493607b82002ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:56 +0000

    by surprise copy choice lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
