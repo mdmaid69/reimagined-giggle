6b092607b3b2d6c018347aacc23820cba436c081
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:35:26 +0000

    need kick cap silent out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
