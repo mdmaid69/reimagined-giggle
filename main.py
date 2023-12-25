0e6a40a4ed40eb8f1e3e60cb7e36a9cf027e0840
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:24:03 +0000

    petrol out part glad science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+from collections import Counter
+print(Counter("hello world"))
+def reverse_string(s):
+        return s[::-1]
