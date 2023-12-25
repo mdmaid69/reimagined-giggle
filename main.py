a389ae09c6a9961c312780751b7d4d14a63e9776
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:06:34 +0000

    touch lost organize male Uu

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
