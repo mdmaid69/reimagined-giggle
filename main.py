7e70881cea19fb5f76f4a55309d62e90d3c863b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:54 +0000

    sleep succeed thinkof activity many

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
