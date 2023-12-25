a0a3121db267ba975530a6450ee4ad58976b27fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:30 +0000

    refer takeoff suit whom wrong

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
