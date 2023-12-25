aad73d1645635872b1f0b406c844c9ef26e1d8ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:23 +0000

    child expensive later practice region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_index(array, item):
+        return array.index(item)
