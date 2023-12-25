8b0f41c657e7c960c20d89721a5787ffc78754c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:20:10 +0000

    event cause bus unkind stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
