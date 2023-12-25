0bfd7e43ecde0b5daee729f14041336e953b01f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:51 +0000

    admit store regularly giveaway price

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
