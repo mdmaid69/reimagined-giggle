9ae44736c548ffccb99e0595d59b4ac293399c7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:40 +0000

    flavour middle nerve interesting throughout

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+print(sum(range(10)))
