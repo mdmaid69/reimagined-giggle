27cae69725f517885aad65745033cc365d06d36b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:13:26 +0000

    spin branch life guess concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
