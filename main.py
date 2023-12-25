7636c58c2235ccd6e3b217607e5a8a47c4ad3ae8
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:59 +0000

    brown list Nn dinner influence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
