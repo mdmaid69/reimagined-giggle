cdc84597a447e5f4078828a74b568b730445897e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:37:09 +0000

    run empty quit value rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
