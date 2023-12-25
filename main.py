4a005f913b947b0d6a34e55ca55935acd2072f3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:39 +0000

    considering slightly content treat special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
