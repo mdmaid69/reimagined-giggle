3f5a201db80c9008831cff884cac811242e0937e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:44 +0000

    raw suggest geton godown visit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_stack():
+        return collections.deque()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
