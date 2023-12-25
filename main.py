18b5eff63c660e1cb9f231c237fa96a5896e2b12
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:46:47 +0000

    hardly shadow comeout wise impression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
