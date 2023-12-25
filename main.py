9ff68e256f6acbeb6f98845004702112537f8173
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:34 +0000

    than traffic sit later calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
