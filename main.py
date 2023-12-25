d9233bab682b969bb2f7e5341d215f0090b678ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:51 +0000

    short trick air drop yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
