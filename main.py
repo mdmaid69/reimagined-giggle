8f70fc508d9148e61ecd679e1dd022c216e96762
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:09 +0000

    uncertain lift convenient runout most

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
