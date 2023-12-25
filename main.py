21b768bd0a9472ea34e4a32332f6c90d09941136
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:16 +0000

    where century turnon post either

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def convert_to_hex(n):
+        return hex(n)
