b43cfd987f52085d6079967a5b9074070542442f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:36 +0000

    woman piece positive throw surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
