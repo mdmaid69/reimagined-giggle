98f10aa0fad04605b4beb62551464c55969960dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:23 +0000

    lucky phone experience circle immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
