b4069d4edbd945c91f0a7ea2dd50626b037787c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:44:53 +0000

    rest bath old fair except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
