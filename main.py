e2b334b37cd4cd0f52d760a08f7611e9bd579a9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:08 +0000

    trap pencil everybody private sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
