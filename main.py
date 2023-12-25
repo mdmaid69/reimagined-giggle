12329e0c94ab9e96027f3a87b18ccfd88c246e52
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:09 +0000

    information care music hospital give

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
