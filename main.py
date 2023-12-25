ae47ff4d8e036706bc2a15a969ae605b64f2a106
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:35:12 +0000

    comeup round about almost cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
