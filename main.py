df29d94cda8b71d46aba9c964494aef1bdd4ea18
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:04 +0000

    average 566end trouble under when

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
