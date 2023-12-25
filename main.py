304ef24350d1aeb0410f039820a508ff6d0fbda6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:34 +0000

    circumstance boil create break file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
