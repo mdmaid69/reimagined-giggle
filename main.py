deada8b14c9560e61cf4bb77c65a9608938d6c61
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:27 +0000

    issue rush move copy first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
