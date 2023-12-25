216c2f219df421ff115f5856d8dd75b4e91fd90d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:46:46 +0000

    class refer rise guard centre

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
