dd6e45126c640ee2a01542c8f4b3d18b3ec539e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:19 +0000

    crash sad image unusual wire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
