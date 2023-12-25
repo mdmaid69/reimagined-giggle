3100a0f0b32f5459030fe2fcd2894b46d32b0d31
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:03:59 +0000

    everyone trousers neat rush physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
