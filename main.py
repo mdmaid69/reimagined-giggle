bee95d79124397cd14328a330e70d456bc88bf81
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:12:42 +0000

    personal let agreement reply fail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
