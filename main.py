fd87aeff10461d6acc9bea57b09ac6c24a202bae
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:50 +0000

    clean add study manage Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
