faacb25b91c79609df110ff5ab76379d1e325ca2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:12:49 +0000

    comfort principle exact name pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import time
+def get_time_since_epoch():
+        return time.time()
