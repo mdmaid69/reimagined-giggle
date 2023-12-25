8e54f721bc553f4a773c7eecc383c124e1aca496
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:24 +0000

    course through sweet year price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
