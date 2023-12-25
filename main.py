0b50e7c5d5ce83c98e7bb8fd125865d52ef9d55c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:03:10 +0000

    complete dream complicated effort young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
