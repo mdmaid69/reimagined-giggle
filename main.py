f93104bf1ae990b2e485c357a62786087967e4d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:35:03 +0000

    frighten off below government born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
