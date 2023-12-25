36552008aa3ebff31d27346755e8d1a5e7d40078
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:28 +0000

    ring thumb ready narrow call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_ceiling(x):
+        return math.ceil(x)
+import sys
+def exit_program():
+        sys.exit()
