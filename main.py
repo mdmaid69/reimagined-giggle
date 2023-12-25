ff7d14e21cc008a5393ee51f10fd8eeb084e597a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:18:32 +0000

    punish guess answer quality affect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
