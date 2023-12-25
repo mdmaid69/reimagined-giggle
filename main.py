551eb9768207a766a59f0a53c56031387a4aa9c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:21:35 +0000

    direct crime die ever either

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def split_path(path):
+        return os.path.split(path)
