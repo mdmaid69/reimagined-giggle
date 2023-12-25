d1188dd9cd76c273f242ae802e3b1c5aaef1f927
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:44:32 +0000

    reaction warning Oo cruelty tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def clear_array(array):
+        array *= 0
