c537c5fad4f3035256891b5ac2203ece8ad1cff7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:43:02 +0000

    contrast bring demand fork explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
