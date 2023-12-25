c5d97059e6015b7068b402dc1fa319a68a028167
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:53 +0000

    Hh afford likely shop which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
