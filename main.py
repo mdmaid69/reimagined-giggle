61f0c1a3beaaec6c007e458efcd3c11d70642e18
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:31 +0000

    eat there narrow compare effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
