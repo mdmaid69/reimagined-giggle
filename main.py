95ae14d63224f0e8e0c801cb244f7bd618d332bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:28 +0000

    rob mention sign measure yourself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+print([x**2 for x in range(10)])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
