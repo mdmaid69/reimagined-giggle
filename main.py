43e02ca7758aaa3bd96f82e60eecc0ab287b5d69
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:47 +0000

    name attempt bury deal love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
