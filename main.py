4cc6b102949e097897c30481b30b69927001b5e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:42 +0000

    film breath criticism bit stupid

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
