3af589da75d2e9bdf1e3b2c4e19c84bb2b16642a
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:10 +0000

    now home hard divide snow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
