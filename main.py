f4d9251a438dd33b5c6e31ee1085971194ed01b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:30 +0000

    search ask squash memory clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_stack():
+        return collections.deque()
