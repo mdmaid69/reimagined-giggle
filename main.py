5a0d27c2a7d9521750322cadfe1ab285778b0f7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:59 +0000

    lucky Tt part decide flow

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
