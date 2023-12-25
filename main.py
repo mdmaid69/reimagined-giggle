ded43a0de8ae8820335fab1b06d5cc07c98d10f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:11 +0000

    slip ever Gg rich mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
