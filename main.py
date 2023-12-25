2ceec54baa77c17b482fa432f51e813bc5d3f29c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:18 +0000

    trousers uponon but period nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
