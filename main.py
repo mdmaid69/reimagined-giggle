22a9b3a0850da71c9250cb196c27093c816f2b77
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:42:27 +0000

    supply attract describe walk ground

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
+def find_max(lst):
+        return max(lst)
