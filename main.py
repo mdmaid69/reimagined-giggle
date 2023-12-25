d2c11716f74b70ddc21ac00f441b548472eaf4fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:39:23 +0000

    rubbish address turnaway what strict

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
