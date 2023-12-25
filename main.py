d63f9ceecac6ea0901524eb375028f41b275d08d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:00:08 +0000

    most yet able course spill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
