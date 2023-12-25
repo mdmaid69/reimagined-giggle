59ac28be7c986310ee9061e97525b9e2ae16326d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:14 +0000

    search goon square ignore sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
