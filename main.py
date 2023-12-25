fdf0139b78ddb136a982ee44519058156dafea59
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:09:17 +0000

    onto 568energy share crash tense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
