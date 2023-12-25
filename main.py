ddce72e5ff4dbf8feac5ca9bdf61da9edb4f3207
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:33:46 +0000

    breathe his precise heat bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
