4526ccfa478b9008a624c8efe5c9ee15bad9cc08
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:53 +0000

    early upset shape steep wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
