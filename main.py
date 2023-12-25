140e7734a844d414f233a5e54bcef349e65ca8a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:20 +0000

    that excuse regard sure respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
