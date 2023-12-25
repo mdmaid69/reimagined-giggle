cd44cd0ab3b7cd73d1a37c4a0f9100fe77e2ba23
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:07:46 +0000

    uncomfortable include bound pick return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
