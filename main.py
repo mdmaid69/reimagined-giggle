4dd19eb5f83fb81bf8500c40f4ddacd7d7684816
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:04 +0000

    runinto long runinto confidence politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
