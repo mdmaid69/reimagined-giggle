1af6510b0a2d2baae1ec277a44af0aecde0adaea
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:29:42 +0000

    mood body silent speech true

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
