1db70c9579423e13abe64cf1ded086dd0fa0986a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:57 +0000

    wander instruction ball care stare

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
+def add_numbers(x, y):
+        return x + y
