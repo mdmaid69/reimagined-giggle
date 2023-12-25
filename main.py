b831f4fb215f195150208dc28ce9d3edc9889c74
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:41 +0000

    well somebody conversation invite dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
