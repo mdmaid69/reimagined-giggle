31a2e3699c979badb0747061cdeba4aff8712cfa
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:04 +0000

    relative cover health record talk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
