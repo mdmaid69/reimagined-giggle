acdb2c5832b25c230980a4d797facb9ce501b24e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:31 +0000

    preparation design pretty earth issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
