e6297fb56d7a4afb1e4b164d17228fa53c8a95f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:34 +0000

    needle progress Jj settle knowledge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def count_characters(sentence):
+        return len(sentence)
+def reverse_list(lst):
+        return lst[::-1]
