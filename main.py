3b6d9a0c70ef16ecb7defcf1906195d6b6ebb9eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:25:57 +0000

    air wave over convincing dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
