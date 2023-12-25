245ba858b1c92f5ed025895ae5bd399e00b570f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:22 +0000

    cum claim shut attract miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
