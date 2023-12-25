92c3574b73d9c43926396ead9ccb963b641fc6d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:21:44 +0000

    hour persuade do yours lead

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
