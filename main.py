5aab35ee8a7fce81a417924d97c7f2ee6c815b3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:34 +0000

    lend choose different getup thick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def reverse_list(lst):
-        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
