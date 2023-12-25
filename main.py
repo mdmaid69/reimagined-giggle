4a2e1fb50d0b0511a7bf42cabe4607f01ee965d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:02 +0000

    street me turnover care over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import random
+print(random.randint(0, 100))
