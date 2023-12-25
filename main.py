07b8a6208102f8cef2f5af6ecb7e91636d4169b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:42:14 +0000

    rather label lunch repair lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
