f94748a784e5486f96f7cdd6ceb3a8113d6b0a1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:58:19 +0000

    shout something smell effort sweet

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
