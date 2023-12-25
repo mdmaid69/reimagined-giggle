1ab6db1826a12cec2d68bc89455348bf2b6f232c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:50 +0000

    dance dead needle town care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
