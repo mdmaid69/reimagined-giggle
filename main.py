8340ef842f367ec27ea3941c81584be9160898d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:50 +0000

    male travel lend piece cheat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
