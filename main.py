1786160dc7938de33eea8596853527522ce4f619
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:43:35 +0000

    pool wear fear temper platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def find_max(numbers):
-        return max(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def convert_to_hex(n):
+        return hex(n)
