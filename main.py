660f110708005c55fbd6b67a5449fddf05c87f1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:43 +0000

    sort so bringback sale radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-for i in range(5):
-        print(i)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
