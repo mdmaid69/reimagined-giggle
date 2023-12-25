08bc74f1a97c9794a1ecc8ee84f9855a5e612535
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:03 +0000

    bringup apart future create perhaps

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
