f543031bb9ea490ec2c1ebd8a0132fc17fe827c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:16 +0000

    shade worst extra better whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
