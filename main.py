1c094c2955c34ca8e1827fd99791ad68f17e4c14
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:09:16 +0000

    climb description exploration eye clear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+print(os.getcwd())
