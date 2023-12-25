c2985e99c4156cdaa1e6d0797ac73afdfd5fe75c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:22:11 +0000

    Uu Hh grow goaway apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
