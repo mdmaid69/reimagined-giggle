f237d583a3c66eec8c1db7b311cb2a586da7d898
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:27 +0000

    middle yet cruelty pleased near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_power(work, time):
+        return work / time
