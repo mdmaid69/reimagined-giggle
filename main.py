c0737d23f0e94fdea16d7e300f8868eeaff1ee66
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:08:45 +0000

    coffee usually several surprised curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
