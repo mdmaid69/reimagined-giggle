9f1b79f8b84b4daa68f5948bfddbac9294f305ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:57 +0000

    total style send demand seem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
