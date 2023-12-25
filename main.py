1dfc873d151816f6b3602fb6e32ea08c62a25352
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:13 +0000

    level thanks fortunate remain herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+text = "Hello, world!"
+print("Characters:", len(text))
