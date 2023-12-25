7feb378157e8690da6ee75cf1875565a0e2ce947
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:13:31 +0000

    impression arrange giveup lend whole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
