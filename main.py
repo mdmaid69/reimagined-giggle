1f1278f65978841c8fcb9a4f7238d914be514cef
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:38:27 +0000

    programme must please on geton

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
