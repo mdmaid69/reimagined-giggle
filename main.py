a103ede0a09bd24b0df7bc70fb3fc15c7bbdee51
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:56 +0000

    learn charge mail welcome workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
