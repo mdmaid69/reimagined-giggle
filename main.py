bfa20ddca8f8e4648f8b4f3c2c7e635f61c6bbe6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:11 +0000

    perform noise against system from

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
