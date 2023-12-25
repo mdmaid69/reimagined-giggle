10bc7b4597593f5200901a2f22d11c7b7c3263b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:09:04 +0000

    forever honest there ordinary separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
