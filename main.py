51dfaa4f8134f2625d72f951c25c767854428560
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:30 +0000

    comeover muscle reasonable worth write

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def calculate_density(mass, volume):
         return mass / volume
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
