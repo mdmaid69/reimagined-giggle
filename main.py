1deb397d988e79ce03682ae50b3a48d4da0d81d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:18 +0000

    talkback raise action broken possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_octal(n):
-        return oct(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
