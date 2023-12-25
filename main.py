da7c3a94dea107b66ab8f44bfd1f8cdb6e1578db
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:37:07 +0000

    foreign fall central interview public

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
