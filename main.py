8be8c5009625149a0bb1059535d348b315d493fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:14:05 +0000

    bath left secret region unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
