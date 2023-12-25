9a2a3573f53b5441447c1fdd07720b96ec5b47c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:24 +0000

    scrap other seriously relation spin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-print(os.getcwd())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
