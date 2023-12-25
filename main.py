de7f2fe758c9953b96e88ff4f486bb45e817a7e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:00:35 +0000

    steep collection shelter early finish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
