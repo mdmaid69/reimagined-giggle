c8721aad57a9cb7bbb0d86658ef34760a33359c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:36 +0000

    speak positive sign nature store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def greet(name):
+        print(f"Hello, {name}!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
