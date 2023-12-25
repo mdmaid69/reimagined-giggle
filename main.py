57cc8ee3c14508506f471346fb40e8d0a53344f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:37 +0000

    OKorokay born bin low advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
