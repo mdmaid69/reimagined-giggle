dbfaac4265ad52a4f4cbe9fdae85169bd1594b30
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:12 +0000

    imagine sharp whistle handle key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+text = "Hello, world!"
+print("Characters:", len(text))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
