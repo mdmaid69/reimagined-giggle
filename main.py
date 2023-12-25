6df4056f7d5e1f48ac21e80828a60508fad267b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:07 +0000

    board character talkinto Dd serious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
