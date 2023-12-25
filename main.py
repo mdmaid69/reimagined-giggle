2da440b5d6ecd6f1df1e2e144a12d6567350cb28
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:16:28 +0000

    open reduction complain movement spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def add_numbers(x, y):
-        return x + y
+import random
+def roll_die():
+        return random.randint(1, 6)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
