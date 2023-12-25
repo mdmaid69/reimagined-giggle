43b4db21bd0ca6ad396e71766b30218fc318e116
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:14 +0000

    class everyone again second entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
