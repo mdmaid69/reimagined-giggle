d99abda36cf0086d6af51729eb2022051bc0ad7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:21 +0000

    winter argument meeting many roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
