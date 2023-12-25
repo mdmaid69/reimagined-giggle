de6ae299ad62ca8ad5b939d7d5e390121eac32a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:36:40 +0000

    if open concentration polite unlikely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
