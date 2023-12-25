d8fa486b42754d1d9890551cccf804aaf2426eb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:17 +0000

    pay test suit thinkover aim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
