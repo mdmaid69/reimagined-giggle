3b95b25f9ef5138fc63af60128834daffc6777c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:57 +0000

    takeback put date side trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
