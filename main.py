1dba8c427946e18cece82fb9ef8f7975ffefdca3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:46:41 +0000

    hand means feel hold pretty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def sort_list(lst):
+        return sorted(lst)
