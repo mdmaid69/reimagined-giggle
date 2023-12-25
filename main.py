041664b5848c97b62bc2edb1e00be03bae836a5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:31:12 +0000

    balance turndown cross identify ability

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
