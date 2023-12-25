7034fcfb918cab2e71c9c6f3f9813304b8a34784
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:58 +0000

    goout arrival mean exam alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
