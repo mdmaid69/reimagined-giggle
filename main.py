40d86c10352ee4ee5517bd78715b5bdcfa643995
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:24 +0000

    false walk exact play zero

--- main.py
+++ main.py
@@ -3,6 +3,4 @@ def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
