3b7edaa9f4dc5733a67370089e47dee803dadeba
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:44 +0000

    cause record remain shallow agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
