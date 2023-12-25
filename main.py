a6e54069c6150b7a23d29e4fd8b8426add8ddc74
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:39:18 +0000

    colour back choose noone improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
