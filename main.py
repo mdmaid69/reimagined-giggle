060470b384c28a3b66837ad971c916d042319a45
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:46 +0000

    feeling turnback song confusing flavour

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
