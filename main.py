540491c2421ffdda314e67afd568af8637a53b5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:47 +0000

    store prepare 570enjoyable understand fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
