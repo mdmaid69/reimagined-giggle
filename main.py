0280e1ef85caf1506f580fec3445b4c4682780a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:40 +0000

    keepaway into roll low spend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def square_number(x):
-        return x**2
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
