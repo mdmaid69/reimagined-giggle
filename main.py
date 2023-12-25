0a75affb8d76d444bef8ddd84fa4398fca4c2c7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:21 +0000

    knowledge turn ball bend attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
