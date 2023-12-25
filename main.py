3254dda34e173abcacd9f4063f6df930908b7762
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:09 +0000

    handle wave responsible back air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
