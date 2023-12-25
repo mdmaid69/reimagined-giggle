154877209f7b50f326db9cdf2c159902309616f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:23 +0000

    blame influence tax seriously apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
