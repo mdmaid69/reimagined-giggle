7cf18398b073f9b6591ee6f2699477904c5fa5bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:05 +0000

    accept skill offence find row

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
