e9781c563af030a1308ff3b4b674f8a52328446e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:38 +0000

    unpleasant only book long lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
