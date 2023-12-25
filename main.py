4ce1ea1d74c10cd62343bb400a525c4daf7406ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:59:47 +0000

    apart wrap present mine Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
