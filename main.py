9706aece55e12625c06abb0dc49f7e5dbc17435f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:58 +0000

    train smile fierce press interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_list_from_array(array):
+        return array.tolist()
