bb40c1427cd4bb4ad8856393a9addfbe31def6e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:50 +0000

    furniture maximum whether complain clue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
