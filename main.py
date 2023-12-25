1be1a99316463915e3d6ad23b9c57bca579d1c5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:10 +0000

    attract please heat later unit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import time
-def get_time_since_epoch():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
