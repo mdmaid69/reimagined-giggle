38aeea364e14c13aa2154c44ec5c48c1789d67ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:45 +0000

    fast Nn cost stamp birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
