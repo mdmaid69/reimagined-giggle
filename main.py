414bb2cb3e568a3ad81c89f42a617cf9959b453f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:50:29 +0000

    short 179bite match high turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def sort_list(lst):
-        return sorted(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
