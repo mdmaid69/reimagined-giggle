726ce9c9736f625f50e07ab0dfb50657eb5a5160
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:19:46 +0000

    ilovedicks bottom wind better prize

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
