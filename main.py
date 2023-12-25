27a4becaa4b1f4e288d88a5e53fe634b456ec37a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:06:32 +0000

    remove spend aware sell line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
