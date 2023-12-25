3444a023ab8103e1dbaecd256eb29b096297491f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:37 +0000

    knowledge 570enjoyable brush crowded meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
