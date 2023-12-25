9460912e4eda811c31b1c03c7e84266ea05f0ef9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:08:48 +0000

    unite herself gentle memory office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
