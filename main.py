d56ae4b1a521c8b997fb2f0f0d7afd81c274c9ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:42 +0000

    through waste visit handle shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
