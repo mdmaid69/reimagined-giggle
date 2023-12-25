84cac44ee4894d68a5d1e851c1db42b9d3e19461
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:12 +0000

    tape ticket neither value freeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_max(lst):
-        return max(lst)
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
