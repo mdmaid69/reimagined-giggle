357caea6d156f04a1a372a0e15cd13fb0f7df2a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:48 +0000

    possible mainly command professional slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def convert_to_octal(n):
+        return oct(n)
