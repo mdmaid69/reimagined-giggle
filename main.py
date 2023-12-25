5d51a6806b26c858bcb54eaff7a73cf858fc5248
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:26 +0000

    either shall smile busy wing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
