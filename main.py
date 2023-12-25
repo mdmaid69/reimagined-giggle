82b574c425e2f09df61e762a29e77f476228598b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:04 +0000

    conduct round as slightly further

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
