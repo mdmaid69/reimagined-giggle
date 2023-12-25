4494d976402197693cbebb24b3b3a0cd8d9fed50
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:28:14 +0000

    card achieve heavy determined extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
