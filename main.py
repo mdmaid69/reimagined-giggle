e43167f65a70f857f1d96db53b4c48c975d1f6d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:08:20 +0000

    gap hardly art jump within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
