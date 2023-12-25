4447e00d009d15a12e4e02d5012383316c1e20b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:33:43 +0000

    job where final born year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
