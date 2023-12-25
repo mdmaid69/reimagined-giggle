508194a0d14776c20d85f38168a8976285469328
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:08 +0000

    gate opposite difficulty minimum forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
 import collections
 def create_priority_queue():
         return collections.deque()
+import os
+def change_working_directory(path):
+        os.chdir(path)
