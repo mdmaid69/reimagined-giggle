7dd51b3b9db3c03240b47c355f1a3d31aab8194f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:49:33 +0000

    ticket date wheel pour picture

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
