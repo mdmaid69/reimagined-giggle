8f138cefe609325c5a7bc5882f6f9c040dc6bbc5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:12 +0000

    dare include recently peaceful equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
