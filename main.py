809852d305d62aa145156e4f57b8c8eadbc163f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:51 +0000

    underneath cost although obvious will

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
