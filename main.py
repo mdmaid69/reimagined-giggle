4891122a251f5433a06bacb8cc82005309657813
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:59 +0000

    window share human identify doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import platform
-def get_os_info():
-        return platform.uname()
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
