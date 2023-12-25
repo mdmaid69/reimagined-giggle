be18d439b9e26c3538d6787c703263c223a7b20c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:16:46 +0000

    characteristic legal side quiet similar

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
