67f3d38675f1729e2f43c722abbbd124c104904c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:38 +0000

    us goaway before angry still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
