aec884a39dcbf7ad6f78836f4c7197c99423adb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:06 +0000

    base rule slow noise take

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
