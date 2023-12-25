1f8a50647515f4ee9436920810c8829ff38fa8a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:22:44 +0000

    tall season clear beat sticky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
