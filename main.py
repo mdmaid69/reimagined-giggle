83dede947866aea6eb566c1f4b11979c769837bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:59:42 +0000

    appear present terrible affect fork

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
