184897048262b34ccf9db7503a1de411db236423
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:32:01 +0000

    bright prove respect machine fresh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_hex(n):
+        return hex(n)
