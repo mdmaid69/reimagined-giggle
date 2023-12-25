da34756b5505a90d6ce08ec41ac784953a2b36b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:00 +0000

    considering unusual appeal trade mail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_current_working_directory():
+        return os.getcwd()
