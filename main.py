5892228dc41507144447ce42a6353b6b69544063
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:35 +0000

    stranger her pull drop mess

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
