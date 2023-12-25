d5b8b16ffdcb4afd7d69f7cc33f37155e857c80f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:37 +0000

    content door our children that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
