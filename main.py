1032f347b468dad2eb0398ce421162e6ac0112e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:26 +0000

    unit play I book negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-name = "Python"
-print("Hello,", name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
