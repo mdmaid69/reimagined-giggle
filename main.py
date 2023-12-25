5f1066b1ebd895245e316225df10d1f5131247a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:10:43 +0000

    tonight ours your noisy speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
