dcf3c09b5d1a2ad27e8f49cd276f486a4ee062a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:17:41 +0000

    seriously form anything pretty right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
