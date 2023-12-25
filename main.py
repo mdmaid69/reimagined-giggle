0ea626664b96aab047e33c587fd306e0e4fccbfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:50:11 +0000

    kind belief himself schedule violence

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
