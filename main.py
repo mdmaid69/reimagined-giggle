4839733efb1b5c09aac00cfecb6ae95e06e9e244
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:58 +0000

    by wander lead dream sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
