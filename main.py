685be1d8b3d36ff936ac87ea36bffd90783d5bed
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:50 +0000

    introduction tip sudden suggest pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
