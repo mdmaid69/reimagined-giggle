082cb54ee3c707e4678c535ea83a7f11fa08a766
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:21:14 +0000

    depend ground quarter you attract

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_power(work, time):
-        return work / time
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
