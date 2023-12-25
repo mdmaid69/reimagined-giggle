0b371a47fe137918fc3eeb458924a31312a6f766
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:38 +0000

    major will row give proof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
