460e4e9b898d19023981822cc30f1548f7e1eb4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:08 +0000

    performance ashamed 572enter will answer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
