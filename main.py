61e51c68d79091ce2adf58762f6d6188c3a066eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:27 +0000

    object period chief trial benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
