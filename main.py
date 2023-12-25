5901687f260e60fd3dab476c955185b47e58dd2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:28 +0000

    company model prize entrance desperate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
