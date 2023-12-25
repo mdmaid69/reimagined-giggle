64f6016fac9dee87b0596e28024c3ba103d8dcd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:16:45 +0000

    healthy substance instrument muscle block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
