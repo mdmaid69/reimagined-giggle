fd477f374f9507b1eda30dcb1a1f096da3856ec8
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:42 +0000

    spin broad century freeze embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
