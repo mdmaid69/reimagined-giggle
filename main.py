58c56b5f8c02625fe002a248a231ede80b777218
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:35 +0000

    estimate aware consist whisper shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
