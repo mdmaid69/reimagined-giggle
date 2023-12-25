bac095d0ac44630a9f0f741d440ed3b2b48d8fd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:37 +0000

    record mother medium half lie

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
