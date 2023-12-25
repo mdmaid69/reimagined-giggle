db90f4f5064f207061bec2f6ae1b30849a62caf5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:57:12 +0000

    right close special snow needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
