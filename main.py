52fb7faeb76db25d22219a7a994c97f2be8fd2d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:08:21 +0000

    wet as flight plate wild

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
