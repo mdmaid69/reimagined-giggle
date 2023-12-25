d9ec92b6d303e56a0d52c91e52979277c9f962ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:36 +0000

    acceptable careful business sport die

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
