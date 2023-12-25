bc27f48d6b3fd11a78e9c2e79075be05a909558e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:41:37 +0000

    pure branch decision underneath appeal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
