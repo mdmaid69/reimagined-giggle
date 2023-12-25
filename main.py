f4e6d7edfc019ba8c7662a0bc386d565c52bc4f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:40 +0000

    start himself 572enter fat land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
