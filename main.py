94699709a00fce564119e2bb9ff251384be883c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:54 +0000

    little matter principle able breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
