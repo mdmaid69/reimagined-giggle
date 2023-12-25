06435b75f62683903eb459237a91f48d1192769b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:08:46 +0000

    smell off cost wide total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
