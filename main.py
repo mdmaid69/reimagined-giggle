51fee3da22f6c68843888f76ba883a1af58439ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:21 +0000

    manner walk hang love come

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
