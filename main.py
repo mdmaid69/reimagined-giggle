9113dc8b74f5ba7a696a798af1ff24ad797e38e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:46 +0000

    congratulate seem free rob runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
