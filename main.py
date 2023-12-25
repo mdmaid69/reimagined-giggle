bbd2743dfc650a9e8b54cbf5d9a112e2c700d61a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:22 +0000

    class down stretch twist trouble

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_min(numbers):
+        return min(numbers)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
