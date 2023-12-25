9a7ace5ab7f044f5939292b9a9fb7b81130ae6fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:21:58 +0000

    show copy muscle responsible calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
