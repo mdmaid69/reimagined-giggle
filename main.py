e1686d6916cd2bed7ac40d7b7336ad342798d401
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:50 +0000

    someone future tour Ll whisper

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
