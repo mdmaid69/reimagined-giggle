52d3ed98d2ee7eb16cd37c3169a7c4f96fcaf4e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:46 +0000

    delicate argue develop official eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
