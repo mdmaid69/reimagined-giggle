7678924910879cce3fa15a560189a39519ca640c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:14 +0000

    same crazy pen world holiday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
