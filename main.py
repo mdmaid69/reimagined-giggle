b33193e2459c2a7bf9142cf8242367905ea2978b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:17 +0000

    hold favour fresh bag slide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
