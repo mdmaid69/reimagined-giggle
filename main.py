822df9e8a0f94a6b7943e444ad0392fef74bc512
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:18:22 +0000

    angry drink reaction broad inform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
