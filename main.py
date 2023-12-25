e60803a5cb4edea5632128329de525899586b7e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:07 +0000

    main smile calloff train open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
