93def210efb88bbe5512861f57df2493ff88af70
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:06:22 +0000

    sudden earn doubt different awake

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
