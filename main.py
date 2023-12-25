cc4949d381adef7bd960ca70cdf7df61fc804431
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:23:23 +0000

    juice jump moral matter relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
