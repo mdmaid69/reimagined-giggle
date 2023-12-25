92b1943285b5bf2a7eb98c750f5001bb76aa8869
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:48:01 +0000

    lookup boat fast autumn once

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
