8d598df521a21ca33d7133cac497e350ee5dfd92
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:21 +0000

    prize agreement platform sea split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
