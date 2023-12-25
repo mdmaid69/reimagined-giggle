4873dac4d596cd79f97230c72c2735c14c47253c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:39 +0000

    stand getback object fine except

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def reverse_array(array):
+        array.reverse()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
