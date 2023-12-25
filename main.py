b3f6f5b7a85c39a1f1542c42d2c606e14b19c73a
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:55 +0000

    own reliable impressive estimate card

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
