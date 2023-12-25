828f8132a8102b162f1dd00c62450a40c93629ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:05 +0000

    fold pay pass set exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
