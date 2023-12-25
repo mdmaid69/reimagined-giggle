71dd1beb458cf002637aa3a096ee6a166bc86039
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:09 +0000

    account workon service weigh anxious

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
