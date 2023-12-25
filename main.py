26976acafad845c6eeb32a88ad4efe9381650111
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:21 +0000

    train turnup soil his Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
