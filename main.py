418329c1ef01a61d7765952773c77402488c7546
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:15 +0000

    meanwhile shape mind Zz brave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
