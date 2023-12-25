5baba228a90de06a0341381bef79306eb2c554e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:25 +0000

    frightened accommodation weigh company narrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
