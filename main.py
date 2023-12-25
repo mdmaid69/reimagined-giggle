60be26eb697305d643e031ed5b2d387f17af5a97
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:42:11 +0000

    soon previous lookup pure collection

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
