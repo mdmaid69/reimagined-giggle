5b533750b2eef6b95867b59bf8df3e4b8a82bce7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:08 +0000

    wake memory name absolutely pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
