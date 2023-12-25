eb32ffa59adfcb7b4d346a3716e4be42d9063129
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:22 +0000

    lean everybody breakfast frightened blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-print(time.time())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
