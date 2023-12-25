89f57957b72ab76602cba9f7eda6e520099e74a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:59 +0000

    hurt discover completely major reliable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
