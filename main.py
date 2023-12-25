81acba8805b7644e9fa847dc55d939ca3ef9bbcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:22 +0000

    I small calloff wear discussion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def cube_number(x):
+        return x**3
