306ce28bf3d8efebec4c96dd617ff00e8ae2bd70
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:28 +0000

    occasion please pencil sister world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
