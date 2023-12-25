afc706dcc81b18d86042424f5e61342f4590275b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:58 +0000

    satisfactory dry press area report

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
