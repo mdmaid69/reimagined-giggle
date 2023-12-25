85a12d438adfbbf0d9680cb865b4a6ca0fd12f04
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:09 +0000

    slice since press high service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
