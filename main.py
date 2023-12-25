212557d22741414f2206040a76d520bfffbe48ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:38:58 +0000

    wet neither possible limit tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_cosine(x):
+        return math.cos(x)
