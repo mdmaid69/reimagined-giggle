a5d9e94f29ad7944a639260e9590b46a6e98ee39
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:05 +0000

    foot congratulations block tip near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def convert_to_hex(n):
+        return hex(n)
+def subtract_numbers(x, y):
+        return x - y
