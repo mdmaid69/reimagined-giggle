0c399669389536fbdfa3d86d77e5f3a3e77d952e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:18 +0000

    degree bad happy message obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
