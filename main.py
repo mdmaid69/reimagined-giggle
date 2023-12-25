fe62f6a8635bd8194af26f8a536791004c58071e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:50 +0000

    variety workon prepare form shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
