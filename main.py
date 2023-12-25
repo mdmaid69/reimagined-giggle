e32884f7774674be9fd5555eabb62880184c4b1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:40 +0000

    arm train relax recognize born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
