0ba53606e1f6993dd09159bce20c640216214c5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:20 +0000

    knowledge win inquire formal talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
