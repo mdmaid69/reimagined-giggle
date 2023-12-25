142d721ff1a22bb9cca916afb38dae3dd7a7f394
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:19 +0000

    attempt collection beginning importance ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
