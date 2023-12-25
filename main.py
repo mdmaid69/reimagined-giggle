9ed35aff6f8f838007bce8e382ecf4aa0f06e246
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:14 +0000

    definite aim lonely point copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
