d18dc81ffd1b28ebf6dab8b1984066d4e67dbdca
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:49:33 +0000

    general important pity godown escape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
