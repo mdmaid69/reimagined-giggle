5a42d076ca78e43af394e2b8f297e3dfbb69a8c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:10:44 +0000

    exhibition answer subject thinkthrough rank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def reverse_list(lst):
+        return lst[::-1]
