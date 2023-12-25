28260d9d821c6d906a56206d71b98499b9632897
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:15 +0000

    store fasten crowd still tragedy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
