509957983771a60efe62efb086941f5143cfa3a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:22:05 +0000

    goout lucky separate excuse part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
