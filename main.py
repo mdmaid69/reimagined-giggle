1f45ad0f84666bb4aa0ab2be4fd79f352ebc59e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:24:47 +0000

    method swing related cruelty throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
