a6215c6571afff50ba94c80ae3e0c3ff35b19e82
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:37 +0000

    push occur tight damage show

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
