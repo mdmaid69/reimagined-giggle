510ae2e81d1fe510ea3501623fe457b8cdf0cd64
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:47:04 +0000

    welcome general mind achieve Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_min(numbers):
+        return min(numbers)
