df4a81e07bb24c21b762db70546428766fc4377d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:50 +0000

    criticism fork objection related judge

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
