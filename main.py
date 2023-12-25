faaed553fbdd9b77948280ff2089ed5462be9050
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:33:28 +0000

    material order bin bus busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
