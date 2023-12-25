87cf370b4b1610f0ce24c272c11275dd64837f36
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:08 +0000

    weekend difficulty custom pocket away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
