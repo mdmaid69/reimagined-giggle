db9cfa6d7760667890e86d93de7c90e36d357ab1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:55:54 +0000

    regret winter steam return considering

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
