bd50070ec4ebe7e8b87737fbc3e17dd5f1d85aa3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:15 +0000

    along save pronunciation afraid sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
