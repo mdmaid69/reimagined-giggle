86ce5bf2f572b8e1d9da219171def8cc62db085a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:10 +0000

    professional calloff watch voice waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
