2da88dbaa0f404c58a6cdf00c4fcc595bc05f7c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:32 +0000

    plenty rush account honest lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
