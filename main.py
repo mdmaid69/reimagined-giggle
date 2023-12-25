17d84a5e8ef5ff07f8d2249851940ab154e3ab4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:47:53 +0000

    string tread another manner sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def split_path(path):
+        return os.path.split(path)
