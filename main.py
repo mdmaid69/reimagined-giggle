64e398d392cd0a0f781b6aa3ff99524328a80f49
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:28 +0000

    us practical odd by remember

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_slice(array, i, j):
+        return array[i:j]
