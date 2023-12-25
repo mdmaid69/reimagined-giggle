79df2dc545750683b3687cc7ffa7d87ecde45b5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:24:38 +0000

    unpleasant term appearance necessary schedule

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def count_elements(lst):
+        return len(lst)
