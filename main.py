c393df2270d399dcc35429b34aeb33da79995015
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:32:48 +0000

    nonsense wish rubbish quiet callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def add_numbers(a, b):
+        return a + b
