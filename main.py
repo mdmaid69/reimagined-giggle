868208f5e0c01335b11a92287df2f6abcae5eac7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:59 +0000

    ahead perfect pattern only than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
