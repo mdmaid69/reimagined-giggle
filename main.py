100e6ac80c5928e45b41fe3176d0fd9b83dae79e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:47 +0000

    test roof mess coat show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
