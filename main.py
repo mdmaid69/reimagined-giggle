bb5135b8ef8ef3baf7928f791bd4e15380631f7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:21:38 +0000

    upstairs guide control stop properly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
