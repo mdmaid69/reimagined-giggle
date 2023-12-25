35db973c17c95c8949b6224553ef037aa31b6542
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:52 +0000

    twist search recommend odd shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
