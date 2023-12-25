30b9e56a1c683f158775796648b8a79eb3d60f4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:26:33 +0000

    recent goon late ask man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_triangle(b, h):
         return 0.5 * b * h
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
