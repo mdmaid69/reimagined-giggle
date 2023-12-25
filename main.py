92ae6c9ddf3f6693fe207cf90d1f6f36a253d058
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:54:28 +0000

    crisis sea hello repeat deal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
