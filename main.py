9049233ccd5a415608ea55fb423126a70728fd37
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:14 +0000

    practice artificial cross warning ahead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
