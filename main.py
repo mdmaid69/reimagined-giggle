93627e7e86f1ad950dccc9b453b20a6b6b5aa59e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:55:18 +0000

    accordingto profit whichever brave tape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
