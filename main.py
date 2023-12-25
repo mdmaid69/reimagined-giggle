77904a7fb4e4b43599f67fcafe30dd37652ffa5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:33:36 +0000

    related goaway tip machine warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
