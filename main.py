883c5f37f6b31064f8d43f0a800921c58a7b3ac9
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:21 +0000

    comeover probably cost disturbing apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
