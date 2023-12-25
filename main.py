617f4d25428232df7005bdaf9ef54f7bf2d42bdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:50 +0000

    almost turninto original wish not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def reverse_string(s):
+        return s[::-1]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
