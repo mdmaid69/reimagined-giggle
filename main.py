2ee9434ee70b6c19515c5cf66faaf57d76fa6a5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:57:52 +0000

    callat reduction mass example letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
