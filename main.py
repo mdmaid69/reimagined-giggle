43757a2a33f6f99b55ca97b67f83835361ec0d42
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:11 +0000

    breakthrough dress love cross grand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
