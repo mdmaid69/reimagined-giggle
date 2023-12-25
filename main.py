369cee96a2e272b7030144257c071106ede6acb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:00 +0000

    wander wear crazy confident Ss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
