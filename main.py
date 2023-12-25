8a8a2370a4c3d8eaa4470c3071dfed5cfa9bdf76
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:47 +0000

    occasion separate weather tap terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
