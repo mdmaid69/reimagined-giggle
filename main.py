bc4226a6b5d862eda657baaa3011610529a98df8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:34 +0000

    luck effort public hear expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
