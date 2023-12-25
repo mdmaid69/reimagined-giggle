c117ce78c278b8564d5aeb0b77791bcc5f98faaf
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:20 +0000

    music suffer dear uncertain city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
