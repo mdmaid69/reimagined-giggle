b41cb68e076702ff6dec3c4a81de94e621856138
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:12 +0000

    repeat hello blow loose cost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-print(time.time())
+def is_palindrome(s):
+        return s == s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
