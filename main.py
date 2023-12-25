703edada6040a1e657920ab23a4d8f55864a887a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:19 +0000

    rest born best minor brief

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def is_odd(n):
-        return n % 2 != 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+print("Hello, world!")
