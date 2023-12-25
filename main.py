79320449d0a050ad3217ba7b414fcaa287e35004
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:22:40 +0000

    cost crash morning strange put

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
