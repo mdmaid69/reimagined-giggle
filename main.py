610e679dd493fdb753b20eb45b15eb898675d26e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:51 +0000

    comfort glad tap satisfactory touch

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
