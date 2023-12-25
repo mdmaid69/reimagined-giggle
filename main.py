aea06e9a5bd3fd95d1d4a4cd4dc25e3e67aa7b52
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:29:37 +0000

    term everywhere talk put wake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
