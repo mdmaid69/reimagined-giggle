0278e9df2b2e18b76e3a922183f9578efcc666bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:12 +0000

    gooff right too rubbish hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
