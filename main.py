b7d002114582743d89fde3f8e60202c730cfb78f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:19:13 +0000

    far influence spring exhibition occasion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
