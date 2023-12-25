80eebb91e65a84d9588a3a360e1ecbe03f3e3a7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:29 +0000

    dead turnover true hard morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
