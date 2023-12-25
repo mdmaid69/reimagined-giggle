8c6d7851b48b72c9895baa6850b1ed49a6c1a583
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:59 +0000

    satisfied earn short study target

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
