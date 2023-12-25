5d3319257b399a1d8a32a4516271b4e22085c920
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:05 +0000

    before deliver return disgusting somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
