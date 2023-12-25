58f0c3b2951aad54652403b14fe541d5bba48d5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:03:55 +0000

    while protect comeout skirt cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
