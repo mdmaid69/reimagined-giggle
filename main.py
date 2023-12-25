55801dce62d37b1ae55225bfa9527f8d73125d3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:50:46 +0000

    picture advantage deaf never try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
