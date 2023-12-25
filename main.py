b5b40709383ddfc240d3970318a2476ea377370a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:42 +0000

    on close pin manage emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
