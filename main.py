d20db33e4ef99e5534e1f035144ec36ad9c11d9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:07 +0000

    hour clearly minimum development nearly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  def convert_to_binary(n):
+        return bin(n)
