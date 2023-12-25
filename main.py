e21a798ebc417488c0d8040bea3890d4cbf05c4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:30:40 +0000

    join first could belief action

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def add_numbers(x, y):
+        return x + y
