e4f22ce4e7115a95660274851dd6285f9c31b072
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:56:03 +0000

    image suddenly guide money criticize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
