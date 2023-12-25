5b7b7a7f0d5f8c39ca3af84f4303cbed19521fd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:26 +0000

    raw prize trade holiday connection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def convert_to_octal(n):
+        return oct(n)
