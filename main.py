05f5a03728a92af8b45ab80d5cf8544b1753b2f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:55 +0000

    deliberate contain guide meaning both

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def count_elements(lst):
-        return len(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
