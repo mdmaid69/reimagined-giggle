fcf7da94053086cd15cafe40a02284236d07748d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:42 +0000

    suppose takeoff turnon picture pin

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
