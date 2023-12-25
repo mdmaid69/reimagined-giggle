a9cb2aafc9f778ac3c8be9d0cf332f7da48f60c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:07 +0000

    reserve car godown when second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
