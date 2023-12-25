df913803b323d1d8daaf73b4a44906b0f95cde88
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:49:44 +0000

    method pronunciation contain alone track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
