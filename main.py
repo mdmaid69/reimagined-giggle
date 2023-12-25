73245950b508b37dd61d291ad805df217eb24c21
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:39 +0000

    even getaway choice card of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
