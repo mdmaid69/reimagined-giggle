2bf11d1389f17a56a27d2bfacf91bc5d78a6cd88
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:44 +0000

    real talk thing shoot evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
