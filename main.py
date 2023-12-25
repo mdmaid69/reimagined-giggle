c6e2f7f8cbabad3a90fa163c01872da5cae67b0c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:58 +0000

    black world mouth concerned reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
