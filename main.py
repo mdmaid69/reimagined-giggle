89d5ddcf5bf11961ebcd11e631d8c469d88aa1fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:27 +0000

    turnup soon phone capital my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+def is_odd(n):
+        return n % 2 != 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
