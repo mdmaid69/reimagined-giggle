3c62d377c15e8b8969ff7d76fba958f855822251
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:24 +0000

    back over cheat correct beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
