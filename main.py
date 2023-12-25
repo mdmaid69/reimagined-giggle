39759e5d899574f66bf3de2db166f686a4c02af6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:10 +0000

    remain drop perfect slightly modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def convert_to_binary(n):
+        return bin(n)
