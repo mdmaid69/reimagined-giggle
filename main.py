7fd273ce5be414784a54b710ed2fdd21db420d7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:12:51 +0000

    prove 573entertainment bottom bin bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
