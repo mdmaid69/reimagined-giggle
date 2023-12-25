b2c42c81507dc59b9803036939817f1c9fa40be9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:00:17 +0000

    another total centre close opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
