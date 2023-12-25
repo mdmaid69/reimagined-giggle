1f2d6f6436d99d82c13b7567611f1f7738bdf10f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:39 +0000

    least might blame keepaway 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
