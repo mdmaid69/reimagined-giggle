af8aceebd05e7e501e13ae75e1248d7fd97d11d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:44 +0000

    why board pupil machine sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
