3194ee162dcebfe7fa0bd5bdea30382f157477fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:08 +0000

    concern achieve hide getup something

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
