db13b3589bc66eedb112426bcda36feaffbcc510
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:18 +0000

    serve Nn especially avoid best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
