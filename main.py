eb3494e8af527cb37a7426f2309b49b6302e7276
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:42 +0000

    keepaway over improvement brush need

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import logging
+def log_message(message):
+        logging.info(message)
