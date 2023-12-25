08d3c0cfbf0c8b39bf2d364bfb435e31b7dcb674
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:42:14 +0000

    quantity better impossible sound hot

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
