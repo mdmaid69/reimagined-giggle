59409161bb0d99ec6f35a7ccee5b8e3bb2314318
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:03:03 +0000

    sharp sometimes separate point turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
