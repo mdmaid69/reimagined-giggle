f80209a308c948654924cbda503620e05a1565f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:15 +0000

    store complicated pump Nn pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import time
+def get_current_time():
+        return time.ctime()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
