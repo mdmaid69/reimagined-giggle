5d766dd9b63a54bfeb501cf79bb3e460570c2332
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:37 +0000

    less twist 569enjoy pattern pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+print(time.time())
