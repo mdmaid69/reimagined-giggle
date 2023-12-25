946128c88ef126d4ea92e718083cdfe36195cf72
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:27 +0000

    wonder up instead shine wide

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def add_numbers(x, y):
+        return x + y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
