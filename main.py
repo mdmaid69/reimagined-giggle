105cda85d388f3eeffa9d68d185fee7249721a46
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:11 +0000

    since anxious less worst sale

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
