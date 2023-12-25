f40ddd9f13d4fc6a8f5efb1cee3ada2ef83b5ec8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:04 +0000

    floor goback(to) feel act wear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
