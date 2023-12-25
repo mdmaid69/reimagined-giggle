a27449d17bc9400d0e32ed6b79fb0499e9774753
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:45:30 +0000

    Bb reason home about keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def is_palindrome(s):
+        return s == s[::-1]
