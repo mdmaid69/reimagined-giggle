95e4ecbbbd13a09c273bc9d4af2e36687d665c8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:34 +0000

    fault 573entertainment fat plant line

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
