9d1e671722df4efe44a39a7ccbccac499888a55b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:33 +0000

    waste that summer makeinto escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
