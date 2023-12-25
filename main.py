0ceb3bba5daef50ef946e7e1d70230a6c0ed194e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:43:29 +0000

    related under neck common instrument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_power(work, time):
+        return work / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
