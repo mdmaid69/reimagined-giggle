4c6148191ae1082bddb4c9039cc43e8912ceda3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:41 +0000

    spread work game under type

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_work(force, distance):
+        return force * distance
