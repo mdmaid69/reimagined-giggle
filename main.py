11dbaea47e5286026050b6111b6f7ceb4dd0c4f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:06:32 +0000

    legal emphasize concentration listen especially

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
