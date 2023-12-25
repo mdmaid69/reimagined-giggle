c67c74eab71afed212529d958263c0945c00125e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:00:31 +0000

    explanation wash creature plan consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
