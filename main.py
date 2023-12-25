e13ba0c0f9aca8a697311d6f7902b65dd00f3d5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:25:09 +0000

    scare attach depend organize report

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
