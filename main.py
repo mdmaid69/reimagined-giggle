ca90e696e19d2ff391a60f2026445ddc06f2a1ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:07 +0000

    pleasant disapprove meet rid board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
