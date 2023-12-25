4fe220dc01b46a7195d6884041294b91abd74902
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:15 +0000

    forever steer various blind any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
