923d7a44f273d287cb95ad282beecdd92e688b34
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:16 +0000

    interesting still cause still direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
