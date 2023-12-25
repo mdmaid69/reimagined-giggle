1f086bb9c3c722da40a4e7f24e1d2cbdcfe323e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:18 +0000

    body burst equipment takeback breath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
