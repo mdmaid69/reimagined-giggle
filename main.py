c07564f0282fd09c56307d7bf98e776c3d8908b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:11:07 +0000

    report hers change passenger month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
