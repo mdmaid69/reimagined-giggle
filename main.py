c30249ebc79d7077f240557b4cdd8d3d7b82dbf8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:21 +0000

    promise worth little insist explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def count_words(sentence):
+        return len(sentence.split())
