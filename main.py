ffb9aaef8e2b9baaf77b09caa3f3e95c44ec1365
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:18 +0000

    central contribute sick declaration dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
