b5ad65111ab5413d94ac6850ae249e928dcbd58d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:14 +0000

    world urgent lesson teach people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
