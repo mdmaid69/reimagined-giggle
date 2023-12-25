e5f65af63c0748888490be2a97141791f20979c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:08 +0000

    birth constantly odd emphasis film

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
