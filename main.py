505babb6a86105e545bd05242be545907ab205a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:19 +0000

    address behaviour opposite getoff occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
