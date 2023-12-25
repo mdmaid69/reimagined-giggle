914fbb5d1c0e7ffb816093e679aee9eea96c2d46
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:16:54 +0000

    somehow wash drink brush lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
