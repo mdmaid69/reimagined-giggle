e1255b78fd539395b617590ed34419f83036b02f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:03 +0000

    deaf opposite brief typical lose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-print("Hello, world!")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
