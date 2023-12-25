3f776f1efe8a6bdddd8499a534ad2335949d5c90
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:52:00 +0000

    Ii after soon piece part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def find_max(numbers):
+        return max(numbers)
