a83e3bb17d81d0705feca8271b7b04ecacd99a88
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:03:31 +0000

    petrol stamp allow post advice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
