ef2d7bf1332aade3efdcd36d9fa099a871de221a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:17 +0000

    remain steer borrow serve frame

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
