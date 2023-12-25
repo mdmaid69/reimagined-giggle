6b6f30f343eae723d64bf69daf672109585bdbda
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:17:11 +0000

    joke include independent right exist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
