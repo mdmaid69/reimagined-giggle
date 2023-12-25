d819926fa302213caee6dc5ca0f5c6ee87fea606
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:07:15 +0000

    capital identify key about uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+print(os.getcwd())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
