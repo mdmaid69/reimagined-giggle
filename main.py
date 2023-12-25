e4b3bf8e02b567d4647678bd155cbf576933bffe
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:10 +0000

    fat on born introduction bar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
