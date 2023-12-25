bbf00e445c3ac2f806d6ee1ac30484ff489b8dc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:39 +0000

    include centimetre scheme pin flow

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
