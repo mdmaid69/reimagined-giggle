ccc376f296b6a39d2dd0351b33d6dd225af056f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:33 +0000

    comeround spread convenient often compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
