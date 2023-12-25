eec51f64a5c57fa37b19989ccc80772cec928fa7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:04 +0000

    below onto forward discuss whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_user_list():
+        return collections.UserList()
