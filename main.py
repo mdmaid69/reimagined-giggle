0f7e069bad0c017ed0ca1ba1879ac9029f63aed9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:43:21 +0000

    lookup under after getback in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
