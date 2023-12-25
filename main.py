533a07fe1e864d6dea29df6e82253ec5faeb4d4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:32:54 +0000

    holdon tell feel responsibility bin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
