400bba2d5b8183dbf4cc94f0012c090200c27ee5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:06:08 +0000

    wind delicate step reject accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
