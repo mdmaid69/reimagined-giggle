2cccc9c30bcc81a5792416d6f675b90c98ad35f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:29 +0000

    secret stir plastic smile legal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_counter():
+        return collections.Counter()
