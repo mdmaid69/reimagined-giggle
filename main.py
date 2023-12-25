b6bd0d3643628ca96954318bcda215da4ef83b2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:09 +0000

    peace real contribute march best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
