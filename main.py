6ac4c6140d0af286739981ff7bfbd5c494b386e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:06 +0000

    chase identify edge control whatever

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
