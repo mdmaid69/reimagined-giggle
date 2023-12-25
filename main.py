ddc2501c2f08a954f4373dfb0f53cb6be1f65435
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:59:01 +0000

    appearance body please achieve split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_current_working_directory():
+        return os.getcwd()
