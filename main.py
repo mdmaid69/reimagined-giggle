ed54fc69f6c6d1653f0a68d5c25139bcd4817e24
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:31 +0000

    show surprising high of professional

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
