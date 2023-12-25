814f74916f88ec000df8530960273727bdf072db
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:24:52 +0000

    bag watch sail gradually sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
