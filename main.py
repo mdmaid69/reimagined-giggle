ebfba2cbd847e44a37ebc455bf68af5dfc173581
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:14 +0000

    add whistle wish 570enjoyable intelligent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
