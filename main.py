42413be9d7fd54f73aab56206daac613e2bde038
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:27 +0000

    push schedule lie content receive

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
