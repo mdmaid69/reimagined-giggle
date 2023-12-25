0ec165d14bdd0cd9d3176599a440f5670fbe0989
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:21 +0000

    convenient develop contribute deserve roll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def clear_array(array):
+        array *= 0
