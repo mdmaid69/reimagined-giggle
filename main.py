8372ab8bc0a6cc150c7da06d0c54679d8b8897f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:26 +0000

    responsible test unit entrance basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
