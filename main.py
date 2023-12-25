ef564bf4455b20e69b84c67b976dc6d9406d37d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:08:40 +0000

    fit level unpleasant fun except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
+import collections
+def create_user_string():
+        return collections.UserString()
