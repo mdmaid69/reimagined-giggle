a82fe2dab2a3f3e45f2dbb775fc69f786434ae54
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:09 +0000

    now swing scale natural position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
