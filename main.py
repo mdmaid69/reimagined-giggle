520a9c4116fe482c3d432fe288df71fd110e3850
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:02 +0000

    cheap politics narrow wing organize

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
