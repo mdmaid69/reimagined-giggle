537494e2bef1b44d6614b2fb03da372950341653
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:15:30 +0000

    direct exaggerate equipment young command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
