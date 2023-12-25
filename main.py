26ecc5c70fb89dd9553da52eedc729d0963b4a76
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:09 +0000

    tie blame notice spring lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
