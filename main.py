c8fa1c2acbe9e4dac46342275fc29300f900e195
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:23 +0000

    turnoff answer nature share disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_typecode(array):
-        return array.typecode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
