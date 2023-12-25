65960eb8489fb10eb78277f1f8307ba53dc0cdd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:56:17 +0000

    course forward exact corner across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
