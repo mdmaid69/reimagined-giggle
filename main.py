f6c665ba471674cd5e502158c9215bf9bb8d3922
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:10 +0000

    hit demonstrate exercise while sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
