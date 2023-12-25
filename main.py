6eef7e0d3d56d0f3a736699a37d12bec24c10e2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:47:54 +0000

    burst tough better press convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
