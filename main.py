65184bc6c61e57080524da4bc6476e1d73c4d741
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:15:58 +0000

    shopping direct trial catch light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
