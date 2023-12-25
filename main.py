6016d240a8a694482fea25f5a00c73b00bc671ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:45 +0000

    office breakinto ride line turndown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def delete_file(file_name):
+        os.remove(file_name)
