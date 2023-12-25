6d33f6d4d5e2cb2837373df36c19700c21df6f94
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:00 +0000

    achievement strict yours short instant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
