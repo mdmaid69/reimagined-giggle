ca6ef0ed1c801a103412474787b2ae686f37d0c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:12 +0000

    answer neck fact picture ahead

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
