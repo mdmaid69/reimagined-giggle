6a92dc501cc484465de1cf72534c67cc1ef34680
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:07:00 +0000

    half head gradually peace comeround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
