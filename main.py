988e840f664518cf1f6fa94523d2c55b3db03872
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:14 +0000

    crash takeaway tend branch surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
