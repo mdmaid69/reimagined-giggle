686e0b57d608fd2bf46366ef283f9c2402f75cf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:15 +0000

    relation base silent cry foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
