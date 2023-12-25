9c265671312eee9ddba2644295b93c5920daf3fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:15:55 +0000

    OKorokay weak prove front loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
