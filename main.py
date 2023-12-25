2107a77c64e3f250dd9b45333b0fa872956bf88b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:56 +0000

    Ww fair know excuse quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+print(math.pi)
