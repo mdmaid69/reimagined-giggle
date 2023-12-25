1e2a0f250ab2ed7d40fb062e1c9e5d9a7e0e9980
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:28 +0000

    centimetre cruelty Vv count accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
