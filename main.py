f2939461641fa8d03a4d30017d4d87afee059c02
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:44 +0000

    Pp petrol play quantity 572enter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def convert_to_binary(n):
-        return bin(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
