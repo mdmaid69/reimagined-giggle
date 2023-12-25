f587e234f7dc917765b9beeffca9042dc058e1df
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:24 +0000

    really precise beneath 565end law

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
