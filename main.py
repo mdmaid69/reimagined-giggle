4b2752930be91bf8192500b9f40859bd4db514a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:23:24 +0000

    too shut lock able division

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
