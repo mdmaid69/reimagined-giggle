8b6b396b2cf879368fc84b6d36852dc2497f82e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:23:51 +0000

    feed Ww dear thankyou crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
