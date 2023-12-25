05f96a62ad64c44e5ef96facc3c4e3a419879551
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:38 +0000

    large slice maybe ring then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def print_python_version():
+        print(sys.version)
+def square_number(x):
+        return x**2
