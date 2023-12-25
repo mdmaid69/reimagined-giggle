d1530b9a8803dfa819dcecf2ad5704f6eb8cdfd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:57 +0000

    art burn politics behaviour top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
