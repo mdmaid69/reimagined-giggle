9ea7febdadedb6363d5b96efdd603ce2d1ec5f40
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:10 +0000

    last care furniture achieve visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
