bfbb9ce1bd2ea3789c3e3b8a1b740e64898c1846
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:30:42 +0000

    pity deliver miss culture slightly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
