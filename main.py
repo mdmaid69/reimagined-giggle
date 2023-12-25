ec7f4d14665f33ccf8f27793f5b2157c831d81ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:20 +0000

    wash ignore bag arrange Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def square_number(x):
+        return x**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
