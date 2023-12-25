6dff99b032a7b1a68603c390482533df2c3288dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:32 +0000

    good hollow grateful free pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
