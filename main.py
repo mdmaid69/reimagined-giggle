17d6d3f6f65145d1465322244c1e4768ad30d7fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:25 +0000

    blame have possibly low wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
