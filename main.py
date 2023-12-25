25dcdcda57402ed1e5fed5d8cc11fa42809f8ce3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:42 +0000

    likely chase clean past shallow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
