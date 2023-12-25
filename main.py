d99789e17b8fbb9a5bef1ea1b1b1e9ca454669dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:01:35 +0000

    polite mean statement simple single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm(base, x):
+        return math.log(x, base)
