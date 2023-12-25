35633f0d5fbdad49d56e41a4487d17a91e4a12f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:32:19 +0000

    disappear undo question soak report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
