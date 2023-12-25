b4d0d93cc04581a2d8b541d17d9a819bcb19807e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:35 +0000

    Ss crazy tune tone concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
 def calculate_hyperbolic_cosine(x):
         return math.cosh(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
