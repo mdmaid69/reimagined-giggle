a21e3e39979861d425c0faeb4b6efc11b5ea8220
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:23 +0000

    heat round run concern free

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(10): print(i)
+def calculate_speed(distance, time):
+        return distance / time
