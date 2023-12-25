41aea50f385169dd408ca5808742691cec38a7ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:32 +0000

    matter whole refuse room suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
