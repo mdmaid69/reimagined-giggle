b619042fbe27a1cf38c21104720cdac2db01a9d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:20 +0000

    piece I afterwards embarrassing ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
