7864cb3f9175fe40164478d7016da08d21a41e7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:44 +0000

    share around tip straight distant

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
