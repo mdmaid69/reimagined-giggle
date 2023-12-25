b86f638cea229e37653aed698fb763871dfcdb30
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:35 +0000

    plate match suck importance next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
