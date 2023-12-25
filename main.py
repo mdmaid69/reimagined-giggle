7554d350eb47f0b3ebdbeaff4ff2a1bd3ea64059
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:55:48 +0000

    display mail pleasant unit ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+print(os.getcwd())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
