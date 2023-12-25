90732e49a7a637f71213897232d4e0c5a62ca606
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:16 +0000

    moral acceptable now basic calmly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print([x**2 for x in range(10)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
