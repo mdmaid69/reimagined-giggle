64a4ca7a209d493b83d1a2a3118cfd849dc19a40
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:23 +0000

    runinto argue not take real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
