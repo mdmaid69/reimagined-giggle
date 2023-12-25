17d9051ba96d962c4373e55524f12b5e9d636325
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:41 +0000

    load fact fierce afford trip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
