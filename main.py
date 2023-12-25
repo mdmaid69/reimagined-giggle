9a5978276881e039f4b567dd4e3936695bf86029
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:41 +0000

    protection mainly which inform embarrassing

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
