127f6f8d7a8358b061597f6e84a0a9a6d4a757ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:59 +0000

    pull commit kick murder wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
