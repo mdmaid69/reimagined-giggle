8f23558eab31769951aa83654134372176605dc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:49 +0000

    concern cost pot suck job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
