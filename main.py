35935cae5dd927d471e856563bcc108a6a62feb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:20 +0000

    right business recognize important sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
