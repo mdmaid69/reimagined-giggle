5109f3bfb90d64af9432124523f6841f23e08966
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:00 +0000

    picture fetch contact blame yet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
