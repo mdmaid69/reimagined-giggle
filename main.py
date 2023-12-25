d359bd98e83dc79d228ace6d4d74fe2615c5b537
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:11:09 +0000

    excuse anything unite one bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
