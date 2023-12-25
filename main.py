e32276ef9e28c9a1e8289fe2450e42a1e04d3c6e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:55 +0000

    proof speech treatment to fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
