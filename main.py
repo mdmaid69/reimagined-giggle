a8fdb9554e49eb4d02deddb081271d71067e5cab
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:23:05 +0000

    disappear best hers tip long

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
