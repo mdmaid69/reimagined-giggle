a92344cccf811eff2d52f9a0cb63cf527b42e07d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:54 +0000

    hope interest year dream room

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
