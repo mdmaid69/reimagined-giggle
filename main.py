9c493cc68fc7ce1320e4182ca65fd790f0e3a2d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:40 +0000

    than close nigga fear allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
