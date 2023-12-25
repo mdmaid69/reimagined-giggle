c6be944b39b8d2e7c48d1e222c0344ab4bdea31e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:52:14 +0000

    artificial opponent mend watch smooth

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
