be0463d3149e06d153c4710e6b258e0a0d03d4fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:57 +0000

    study early and rush bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_arc_sine(x):
+        return math.asin(x)
