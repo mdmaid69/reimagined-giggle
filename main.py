a8e814df3febea3bd1c8ff43726bc290e4a56921
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:48 +0000

    face return dead turnon careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
