6443a687944110940c9ad9479b9a6c27080de1b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:13 +0000

    whole soft afterwards half murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
