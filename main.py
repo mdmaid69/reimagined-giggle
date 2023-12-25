85fc9327e46f8d53118f6c675d67881287b41f83
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:22 +0000

    family hang beside rush hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
