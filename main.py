791441de1457fb3a69063ccda154c8baa2ef31d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:36:27 +0000

    Vv behaviour plenty period free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
