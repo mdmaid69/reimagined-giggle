1cdf9173c482077db6db3e7cd78529209e28798a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:49 +0000

    agreement impressed before deliver welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
