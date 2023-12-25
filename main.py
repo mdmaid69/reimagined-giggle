5b900a79baee74c9ae86324db0315fc4a8f19c7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:09:46 +0000

    back cut forward take let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import getpass
-def get_username():
-        return getpass.getuser()
+def sort_list(lst):
+        return sorted(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
