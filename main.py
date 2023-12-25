ba8997351e7973071be219e05547b763763dfa9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:12:13 +0000

    front major fasten notice rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
