7110c9f2a336bd6d462b63fd76d3babaf3dd3a74
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:33 +0000

    innocent stamp appeal back depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
