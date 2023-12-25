ddd66dcc645ad3ecefcd94e0cd9ba90f032e9133
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:07 +0000

    would cruel unfortunate bet sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
