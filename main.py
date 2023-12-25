fd1c54d51fa3b9d83399e90a48ba6c7c2efaa89d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:30:50 +0000

    cruel excuse sail piece because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
