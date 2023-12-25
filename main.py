56086a1e1566f61bee578093ac9b9e84c59fda08
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:49 +0000

    help lookup poison circle push

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
