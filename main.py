efb364fd3bf0c531629e8c15b61dee2843847ba4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:10 +0000

    Nn cause everywhere rough degree

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
