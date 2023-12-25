f63982327ccca42c9a8898b5aa92d34a67916339
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:07 +0000

    live hole guard any last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
