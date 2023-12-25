4b95c4939e4afcadfcf4dbf83f430219490ecd76
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:17 +0000

    fall negative visit equipment purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
