b700f558ee0fc15b43bea122ae71792e986bee37
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:03 +0000

    marry slowly deserve middle pride

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
