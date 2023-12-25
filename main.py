cd97989125035a23276a0ae428ce857d46506667
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:33 +0000

    fill careful want partner usually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+print(math.pi)
