071163cc822e4bc1f6e89a78f85045e9221ca258
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:23 +0000

    visit winter fasten spite sell

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
