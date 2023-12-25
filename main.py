7593bd8be073e72ca70a221e730e6eeaeeedcd37
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:35 +0000

    move replace cool scrape fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
