281c899b3019e7ae2d1983a022cd5dcb888f5638
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:23:30 +0000

    time fork onto holdup worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
