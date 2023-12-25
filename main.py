22be2750a219cde4bc501b205e462fe955f72d64
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:58:26 +0000

    plan do breathe over exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
