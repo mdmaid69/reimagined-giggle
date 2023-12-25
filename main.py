92c27fa407f021fa4d05cd071cc4c31e7f435ebf
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:18 +0000

    world place speed gap path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
