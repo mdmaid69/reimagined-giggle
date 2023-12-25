0a765c0532f75c47aea79bd73bbeb880bf0a7952
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:51:59 +0000

    gooff thin regret condition wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
