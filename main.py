654ed5ff6c1878e376506810ad6d90c10aedf73e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:50:23 +0000

    put error excellent real please

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
