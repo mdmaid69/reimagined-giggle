7028fcd71e050a27b5a3b3dfbfa0f08fe31684c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:37 +0000

    promise about escape approve task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
