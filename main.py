948bdd86c33a9255307c9e47935fe24ff6355261
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:31 +0000

    trip frighten adjective left petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
