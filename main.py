e5166adf959d8112777533444521db08fe94c46a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:22:13 +0000

    blind key pay soon none

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-from collections import Counter
-print(Counter("hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
