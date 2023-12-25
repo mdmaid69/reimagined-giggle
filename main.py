a3ae94151806cd55659a696f8233f53c54ba3988
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:22:34 +0000

    respect hurry fix press use

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_sine(x):
+        return math.sin(x)
