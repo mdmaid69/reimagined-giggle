9749d9cc75eeef80c373cca5c4f9201cde10710e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:11 +0000

    believe use ignore right board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
