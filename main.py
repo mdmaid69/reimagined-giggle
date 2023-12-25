ecdf80aa2788f012850ff939cd3022363a42f5af
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:15 +0000

    joke sit tax Itself fun

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
