e140266a40ee96a645b54a886f9dc0ef6942d030
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:40:49 +0000

    plant keep total attractive remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
