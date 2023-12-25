884e0283cc0c7f44c6d001cab89b9063c4356b54
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:37 +0000

    charge maybe rely across bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
