5716f3b3e27914355abda97e783b786b840f2c25
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:24 +0000

    hit safe only sound give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_speed(distance, time):
+        return distance / time
