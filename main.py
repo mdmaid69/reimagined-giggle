7bd596130f4ab3a4f2b0cfa991c96c9b03bdc593
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:05 +0000

    include raw production fortunately our

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
