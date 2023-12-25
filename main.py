e04701b6e7ee4b45d95df0137ab581afe38f00d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:41 +0000

    travel bet bar kill noise

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+text = "Hello, world!"
+print("Reversed:", text[::-1])
