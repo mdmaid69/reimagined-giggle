f7f23ba361ca73a94ddcd5d5cd65b7f02218e475
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:05:08 +0000

    Itself rank our edge your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
