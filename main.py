da0782f9a45b990fd41c19378d5abafdc14cccd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:48 +0000

    scrap thinkof pretend similar walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
