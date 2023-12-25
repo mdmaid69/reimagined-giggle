a4259827a104a099c98636903726451a1129518f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:36:30 +0000

    sure reach food accordingto tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
