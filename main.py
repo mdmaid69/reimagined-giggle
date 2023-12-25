6c9124eba1e08792d01e5f363ec03802bf8bdd0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:35 +0000

    lower last experiment meaning course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
