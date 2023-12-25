3cf1b7648e027e1314dd91ce083fd310c3d83db7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:48:25 +0000

    smoke especially pay twist steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
