426178d628c2bbe491701cddaf6f5669787e2bf1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:15 +0000

    turninto he load open whenever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_unique_words(sentence):
+        return set(sentence.split())
