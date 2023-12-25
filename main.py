50c34fb63d4781b0a01174e8720024b6fddce70c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:52:46 +0000

    stiff clear sharp major have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
