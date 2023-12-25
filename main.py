759f5c440e224a99d013963ae4689cc3abee7c27
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:53 +0000

    favour even again wide lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
