70fd8949fc84a8995d74a5077d65824edd66bc4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:47 +0000

    become university up region year

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
