3d080cdd057d1bf5ce0e59e6a433aca599e3b009
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:35 +0000

    correction fault ready set foreign

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
