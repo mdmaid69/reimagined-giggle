0f14856424aab425c52dd5c74461f346325b1df7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:20 +0000

    wrong wander mysterious wide coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
