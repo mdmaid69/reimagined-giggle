fff2534d72f934fe3adbc6edc2db4d9b9ecc8372
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:26 +0000

    confidence record bet flow now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
