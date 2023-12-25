e5bbf4856652cf5c6cf334d126b50996f5066117
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:27:37 +0000

    have side breakup hang here

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def multiply_numbers(x, y):
+        return x * y
