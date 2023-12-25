0f67ab5c9f122c6e21b88c3719faa307a241ee66
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:05 +0000

    negative able Hh value something

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
