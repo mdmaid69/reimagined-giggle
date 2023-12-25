8efac4d698fb4d40cb9e274678cd3b081e4901d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:14 +0000

    patient concerned jump tie bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
