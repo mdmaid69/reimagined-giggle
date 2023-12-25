438f34a751ea34ac3fc60fbf4ee8a072fe80468c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:22 +0000

    coffee message yours far lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def reverse_list(lst):
-        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
