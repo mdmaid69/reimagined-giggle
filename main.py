79bda56e97577a077bcbc4f9a4f5b2a0a22f76fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:12:54 +0000

    near plenty children stare smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
