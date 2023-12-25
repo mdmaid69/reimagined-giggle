2559761150469c21384f7db96f0773ece817cb1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:58:27 +0000

    already manage freeze I defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
