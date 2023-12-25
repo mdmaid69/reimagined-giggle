ed7dd9a0685d50c6ccd26846304f653f64e49b8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:17 +0000

    takeoff woman usually example container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
