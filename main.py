5f961a5de1196d8639f5cc4ee6f9d99191fad8e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:13 +0000

    speech aim actual by Ii

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
