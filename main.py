6f4d513ff2ec85e7cc9d9c2e337581d16c4cd487
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:08:00 +0000

    build bunch Ww hollow lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
