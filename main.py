fae9e90aafbfa51374d981598265f277b1853c11
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:30 +0000

    limit miss flood control comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
