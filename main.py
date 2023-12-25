6bf33dff6d2c5fb585bc796c35c2475fa3b12c1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:01 +0000

    pool accept 570enjoyable similar software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 def count_words(sentence):
         return len(sentence.split())
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
