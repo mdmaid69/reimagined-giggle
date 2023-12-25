128f2eb483612df167272404f81f6ed319b1f914
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:09 +0000

    total centre furniture neither tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
