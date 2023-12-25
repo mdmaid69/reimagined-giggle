022b1ee1c0db9a194a09334f13600548fc41708c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:31:35 +0000

    represent brush introduction base wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
