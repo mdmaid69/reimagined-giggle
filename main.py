f91312c35e423fe048442192d23bb91c8cf39303
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:05 +0000

    afterwards accidental imagine cope swim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
