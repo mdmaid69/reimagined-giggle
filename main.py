51de84ac8c9b83a94ff49754c08bdf6980341326
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:53:51 +0000

    contrast be near depend generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
