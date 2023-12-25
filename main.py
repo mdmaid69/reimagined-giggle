3fa0a7ba0f55665307708be453425d7d35b79ef2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:58 +0000

    giveup mail extra reduce distance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
