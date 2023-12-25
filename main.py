6aa79e8dd20de05d35b7be9b1bfa6a9c61f2fd30
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:44 +0000

    to recognize upstairs marry 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
