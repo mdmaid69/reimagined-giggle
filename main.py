c2633b05872656e473788723edf9bdf4a20b46da
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:20:09 +0000

    opportunity model fit apart Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
