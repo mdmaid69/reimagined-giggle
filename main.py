17a88fa0565f0a4703bbc3a40192ef737f3ea998
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:15:41 +0000

    but luck measure too where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
