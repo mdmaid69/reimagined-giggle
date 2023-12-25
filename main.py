e3d7f7d535962e97e028780a4374afc907902208
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:36 +0000

    disagree usual giveaway principle tired

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
