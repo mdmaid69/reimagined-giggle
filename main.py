29b40008510002a3a142c1caf3803b9ed957a94f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:10:18 +0000

    dead stone gas suppose cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
