2326db9d26373f5dcf353ac61bd336d3733f2a60
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:00 +0000

    up result fly handle lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
