9e03af9be07c04f0859665a2186c0f6f732c3106
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:42:26 +0000

    guess neither offer tooth across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
