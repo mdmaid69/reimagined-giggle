21efb00980023c3331f3ae29e7317eb52b9f8873
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:14 +0000

    onto direct freedom wear typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
