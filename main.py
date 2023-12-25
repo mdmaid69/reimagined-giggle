2df35a2971d29b2efa99255a3de6a489f33a9ae8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:50 +0000

    trouble shrink false limit hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
