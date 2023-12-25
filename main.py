da91fc0b324f5ca78760ed73edce34d6179ab676
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:31 +0000

    body transparent convincing Ll once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
