c2ffcbe449d3857a0352881109bb73bd0319efdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:41:52 +0000

    danger hold wonder innocent club

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
