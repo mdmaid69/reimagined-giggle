3c80f3d85f1cc45bb5b7be245b2bb712d199608e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:05 +0000

    fail attach intend old fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def count_elements(lst):
+        return len(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
