93e1315948776bc6a4cac23fb8a4d993e4dd335f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:15 +0000

    memory cruelty object sharp used

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+x = 10
+y = 20
+print("Sum:", x + y)
