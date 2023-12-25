8421cb2f79ddfa53e4e7199ecbdf9813b1740e28
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:19:30 +0000

    present university life trip clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
