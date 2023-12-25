0bcc92c4ff1e9e32c0b83053620faab7cc5e4297
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:05:04 +0000

    communicate control whisper pencil especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
