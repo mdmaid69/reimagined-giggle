ec07f57a5a21e8c00dc8e2589581c82762e9078a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:38 +0000

    birthday suggestion declare serious condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
