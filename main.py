c6e6d09993dc15d8583d41699cd1ed6753d58976
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:19 +0000

    shine learn answer successful away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import sys
+def exit_program():
+        sys.exit()
