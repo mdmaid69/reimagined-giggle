03495fabb081ea19f256a08fe63c2e6a644b8bb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:42 +0000

    taste live experiment drop patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
