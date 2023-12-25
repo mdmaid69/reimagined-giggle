7a3dd21eace769917d5ecb65f1ef9a6dab12fefd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:53 +0000

    open 570enjoyable recovery talk injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
