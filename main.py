e915ea3efbfa40bd63d2822f1d901e84694c3781
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:58:36 +0000

    crash well damage rush start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-print(sum(range(10)))
+import time
+print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
