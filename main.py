bfca3cf599f0f64c8115c1c36265c1e926d08940
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:01:14 +0000

    give pleasure man than start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
