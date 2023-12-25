047cb7c4f7365a2556cfe5d1cc5bc2855855d60e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:49 +0000

    book supply hurry centre wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
