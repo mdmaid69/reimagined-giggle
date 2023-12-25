8a11119d449ae623f9a4989270e4e62e362b5d9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:47 +0000

    reach embarrassing imagine throw maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
