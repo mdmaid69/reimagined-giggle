d0fd0f97ead79138fab8e35c6adfa3cb770fa7ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:31 +0000

    moon mood snow strength climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
