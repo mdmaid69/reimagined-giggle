56d0a9bcf7bc725245f477d677b6d113e0612748
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:37:27 +0000

    usually achieve snow need pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
