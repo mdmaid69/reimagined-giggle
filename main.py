ddff0216a93cf14c8011bd9859f07621eb235e48
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:55 +0000

    attention secret powerful judge identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
