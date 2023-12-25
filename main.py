e738ecb168d607d4c8641a4101992238e78a430d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:01 +0000

    before night knock powerful achievement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
