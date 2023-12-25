7ae96cd533f3377dfcb39211a311b4affa35fbe4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:33:12 +0000

    lucky goout true watch pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import random
-print(random.randint(0, 100))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
