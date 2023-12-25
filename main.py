f6b5e904dfcb0a3d19cbd62e4e05838ea805630d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:45 +0000

    violent approval square minimum unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
