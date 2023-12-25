da82b6164a64ad3e0695143f9ee3874e5bd2fba3
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:01 +0000

    until reach base slight television

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
