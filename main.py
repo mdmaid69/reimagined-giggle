6795809bbe547d97083e1687fde5c973cbab7a45
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:37 +0000

    godown complain Itself finish recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
