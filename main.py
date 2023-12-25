8ee68b6a605e45bd3adfcd7d24dbd6f7e8043634
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:45:19 +0000

    gas arrange complicated contact shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def divide_numbers(x, y):
-        return x / y
+  def cube_number(x):
+        return x**3
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
