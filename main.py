df280b7f1804a2104a9c075b532eb80011c99707
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:53 +0000

    right suffer without clothes insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
