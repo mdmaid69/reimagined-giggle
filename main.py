2b87e6823485a8c7fbe454a490baeeeddbf775dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:15 +0000

    design state contrast offence food

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
