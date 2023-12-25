eb2862bfd44e59810999294f1cd81b29273f8bb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:30 +0000

    holiday breath swing majority trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
 def get_array_as_int(array):
         return int(array[0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
