3a7cb00e2c9a20b8ea4cdba93537d56e8adbbd60
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:35:20 +0000

    sister method you original about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-print(random.randint(0, 100))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
