7d708c0c9bd35193127ff5dd42d00b06686e4d14
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:27:48 +0000

    expect minority cough comeoff cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
