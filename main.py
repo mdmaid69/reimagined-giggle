b619e79a2ae7f3c6ea0b9a9ada9e6f72c564aa75
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:08 +0000

    maintain cap tomorrow anyone ready

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
