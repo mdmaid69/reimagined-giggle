cd3fd2b62da04f23202df35b81df58420bb57092
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:33 +0000

    punish worry though regularly machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def set_array_item(array, i, item):
+        array[i] = item
