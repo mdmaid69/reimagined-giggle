a429e53ecaf94a72c143e24100367c5d72a9efc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:25 +0000

    respect affect politics takeback common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
