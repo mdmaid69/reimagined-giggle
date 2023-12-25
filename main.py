21e47447d605a10940eb0241acd34f34cc6cfff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:48:08 +0000

    good calmly include turnback left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
