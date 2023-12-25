91d90e0f0bea9b5a84219b1615a7afbecdec590d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:23:35 +0000

    ring sensible Rr score bus

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def square_number(x):
+        return x**2
+  def multiply_numbers(x, y):
+        return x * y
