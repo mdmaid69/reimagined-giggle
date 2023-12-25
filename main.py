1d9561105d1ff50e78ddae4abb38479177fb78fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:55:12 +0000

    swim tradition convincing trick better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
