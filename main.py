7b77f253ed606136ff75a070d30c4cb4218b2066
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:44 +0000

    muscle foot thorough pause callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
