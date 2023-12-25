85298dd6047e676ca0b5878b464149c0a15e9c43
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:08 +0000

    invitation contribute discuss favourite all

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print(sum(range(10)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
