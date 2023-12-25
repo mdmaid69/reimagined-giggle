7473957add46e7d1db1c082d429459e99002c508
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:54 +0000

    scrape pause consult risk burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
