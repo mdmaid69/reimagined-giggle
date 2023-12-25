5249840b457843c8919b52b6497e2e2b6deb073d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:55 +0000

    shelter punishment and sometimes complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_sine(x):
+        return math.sin(x)
