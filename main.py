074233243656f22b1c14aea9568ebe0bdd02cb7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:16 +0000

    joke comeout once pretty sorry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
