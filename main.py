562aaaaf0b457d904219ea3c36b7feaf90d0293f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:02:28 +0000

    yourself fault finger offend flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import platform
+def get_os_info():
+        return platform.uname()
