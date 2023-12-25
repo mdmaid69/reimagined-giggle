2e5ed8aaa7ed73eb7caba0766fac3499d1f92277
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:18 +0000

    sell trade march flood middle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
