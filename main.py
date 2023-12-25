60f437d8e196953b1325e585aa3b23951d8aeb96
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:32:48 +0000

    train them love would weekend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def is_odd(n):
-        return n % 2 != 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
