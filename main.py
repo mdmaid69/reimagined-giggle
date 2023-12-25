5ac01491f5ee25f8ccfa588e115baa21bbdb04a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:50 +0000

    around base insist arrive best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
