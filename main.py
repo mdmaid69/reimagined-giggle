4fa406dd6fe06a50706bb8c5a7c383d4d26a9e78
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:20 +0000

    draw switch involve whoever perform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
