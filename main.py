2dffd0bd0055f6d62a4a6e4525a593540b4111d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:42 +0000

    wind finish middle cinema frame

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-def log_message(message):
-        logging.info(message)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
