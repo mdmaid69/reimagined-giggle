78bf8a1c2a45b6714bccf18cf6369632e29e330b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:35 +0000

    defend centimetre settle juice inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
