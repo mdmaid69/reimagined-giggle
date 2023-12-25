9f82b45d7cdba25f5dc774a5a47bc176cdc02aae
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:02:42 +0000

    breakup tooth available lookback(on) back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def multiply_numbers(x, y):
-        return x * y
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
