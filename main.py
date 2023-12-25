babcc18d610a905a6394ae31b67115d4dc74eb18
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:24:21 +0000

    project right land tomorrow note

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
