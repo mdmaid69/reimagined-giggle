9128656cca1af034a8dd20409d984328c0a695f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:55 +0000

    win nigga paper limit crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
