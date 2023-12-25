977508f87d4b200e51aa856ea88e025436448f30
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:05 +0000

    holdup break comeup every most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
