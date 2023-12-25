66ff45dd52916a557d43393981e6997dbadd740a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:47:51 +0000

    suit tone yes post well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
