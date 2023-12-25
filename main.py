b9d1263a189d94ae1a795f104a4a3bb1688a0dd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:40:16 +0000

    stretch poor pupil kind ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
