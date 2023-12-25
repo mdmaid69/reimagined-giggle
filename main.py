106f3310f0596162a62b6cf995c80facecfa0af9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:32:21 +0000

    properly set suggest excuse success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
