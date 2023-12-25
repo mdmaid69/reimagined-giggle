fe0f1eda896e78a779e108e2eff2f1d7e0d87cb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:03 +0000

    getup central post comeback rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
