137de9f06830922db2c5347b67e75518c367ebd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:28:20 +0000

    whenever impressive noone get splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
