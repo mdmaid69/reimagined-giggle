8f8d9b1d9f8f3f5d607db4b04deaa007d804b2c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:12 +0000

    unusual game yourself slice city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
