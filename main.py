e073613d9163f2209a6b5cc1bbaecc514b228ec5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:03 +0000

    bear expensive regular head wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
