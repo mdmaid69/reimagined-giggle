4da120918f14aae17f60f9066988e374631bc61f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:27 +0000

    reliable smell border basically gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
