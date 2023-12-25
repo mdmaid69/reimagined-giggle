ed70e67a7cea7e7ec3df792c05150fa21e3fc07f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:22 +0000

    behaviour before year material survive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def get_time_since_epoch():
+        return time.time()
+print("Hello, world!")
