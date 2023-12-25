9929a22b0751fbe4589086e957b6ea81fe41cd58
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:01 +0000

    exchange emphasis skill dick impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
