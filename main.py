7893ca3e8f815865dabc25f2e85b9a174c71950f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:58 +0000

    workout angry talk face interest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
