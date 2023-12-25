6bac7019c8a69e351a64ff594b8d7bcc43628ef6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:09 +0000

    rise land believe Kk need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
