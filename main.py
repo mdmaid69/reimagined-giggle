0bffe894c9a8fe746c738a018e8efd9d0184036e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:44 +0000

    hospital frequent news nervous practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def clear_array(array):
+        array *= 0
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
