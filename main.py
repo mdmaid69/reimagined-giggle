ca8294deb7dfbd6798a8ec3c76d8ebdb3195f928
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:46:53 +0000

    lock disturbing tend tie and

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
