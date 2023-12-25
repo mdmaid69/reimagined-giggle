94ba5c019718974bfd344bed7a583df27ef89f4b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:56 +0000

    far present reject blind water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
