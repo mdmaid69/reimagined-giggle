1c7ca4f028bf4c5cb9d30206ab48bfa7fc65fea3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:06 +0000

    arrive exaggerate year recent bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
