54bc06a5293f63168a33d6e2c13ad58f2aabbcb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:22 +0000

    leave emergency moment content owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
