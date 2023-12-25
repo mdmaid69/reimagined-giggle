3c741da8e8c4c5430310ce8833c75a4146d1e10a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:28 +0000

    record image bread steam aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sys
-print(sys.version)
+def calculate_sign(x):
+        return math.copysign(1, x)
