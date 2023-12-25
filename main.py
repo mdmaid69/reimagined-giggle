1f7418632dbf381a5a973785c9d5b54ed259d702
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:54:47 +0000

    together kind roll shame switch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
