3331d89b276eadaa6686160576f4b83047147fd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:17:47 +0000

    win shoot clearly swim spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(10): print(i)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_odd(n):
+        return n % 2 != 0
