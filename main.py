0901cc2a71036adee20d7b9e480ca2d34feef81e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:58 +0000

    compete dick abroad dream general

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
