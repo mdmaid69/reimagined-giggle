8460aa7183a8345af86dfbd4ff1fa6018d321c33
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:50:55 +0000

    stiff ahead bottom slice excited

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
