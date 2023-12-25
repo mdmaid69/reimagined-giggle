f2b3b7115531635523680fa3f93b03021497d0f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:29:18 +0000

    division offer mean consider swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
