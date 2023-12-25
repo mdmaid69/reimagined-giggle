413024e1f39bb3568c61bf5d8ff39071af955941
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:05 +0000

    myself goodbye season moment child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
