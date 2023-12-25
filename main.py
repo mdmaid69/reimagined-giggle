ae7efbbcb209eca3baa16ea79c4b4e38e884dfb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:55 +0000

    cheek press rate deliberate issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
