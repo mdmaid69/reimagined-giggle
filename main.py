f85346511af7950c9bc79e3564f52edf64d0db3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:52 +0000

    practical handle effort cruelty press

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
