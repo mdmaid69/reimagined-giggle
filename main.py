5abb736483015eb053b0e7eff0692a67a3a3cd48
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:51 +0000

    free state yours burst reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
