65b84e09b0787ee6248e63b6bf6664c47b30159e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:08 +0000

    help save divide past discussion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_length(array):
+        return len(array)
