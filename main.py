a05b7e1b6b36d9c34f5faf418cc9be51ee2809a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:55:41 +0000

    better light skirt impressive team

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
