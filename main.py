498bb3a360a117376fa386a3ef7519522181b849
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:17 +0000

    add stop judge minor be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
