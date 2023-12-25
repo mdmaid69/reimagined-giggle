3936ba85ff7d65c39e5a3a9924303968d31da76e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:07:21 +0000

    to crash survive female final

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
+def count_elements(lst):
+        return len(lst)
