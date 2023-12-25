2226e290200d649386107049e58bc09c72950289
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:47 +0000

    fact this wonder lead fair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
