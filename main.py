f5f0bb3e264c7d321b1a3cf8b85d2f1e01b89b6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:45 +0000

    forget abroad spin butter colour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+i = 0
+while i < 5:
+        print(i)
+        i += 1
