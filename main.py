e893b7e459a4ab709a0af993bf8cccb8112c68a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:13 +0000

    within wherever somebody save lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_work(force, distance):
-        return force * distance
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
