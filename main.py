3540d5515636eb5687d0cec3789590c24f02531c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:22 +0000

    lot waste pain lay grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
