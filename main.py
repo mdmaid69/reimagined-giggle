c0fb8ff7710990fb916f70a4f98a8700b8f9ad8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:03 +0000

    measure age clean mean foreign

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
