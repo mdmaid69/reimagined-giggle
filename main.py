f78cc5f29623e91459c97568417dda68015979b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:19 +0000

    grow custom press owe hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
