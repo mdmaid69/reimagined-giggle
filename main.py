0ab131bfc0be0f95f9aad4c30573748e7026cf99
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:42 +0000

    fit book school broad claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
