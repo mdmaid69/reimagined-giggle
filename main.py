cd1a81044810ab977beb67a7c563994373cabc90
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:48:00 +0000

    soon run watch breakfast road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def count_words(sentence):
+        return len(sentence.split())
