ef78eda88038fbb5a8810797e0c615243659f3ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:09 +0000

    blame list see level whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
