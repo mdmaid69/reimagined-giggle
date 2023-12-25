c1bf446173a03a00e35879b9ddb7d68c19eea96c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:14:12 +0000

    smart fact key sleep force

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def remove_duplicates(lst):
+        return list(set(lst))
