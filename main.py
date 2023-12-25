558c136d570b31686e07b9e65ed8e9a413905b71
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:32:58 +0000

    offend convincing support commit spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def delete_file(file_name):
+        os.remove(file_name)
