9c7a062d096015b390368c33de84f03ff75f67b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:17:09 +0000

    last at put congratulate relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
