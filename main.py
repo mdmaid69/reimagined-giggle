1093d9f3ce84dd1ee090c0841284e55f330a32d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:42 +0000

    public film press side move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
