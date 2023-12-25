bb0ea150e321584884b7fca5d51e2752dfcf1af6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:29:53 +0000

    man deep sometimes centimetre show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
