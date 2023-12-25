8fea8d6933f673e82db83f947e9bc5acaf62af38
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:22 +0000

    how sick nose getout equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
