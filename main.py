189ba55bf963a8289fe886178838410ade39f67b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:55 +0000

    divide plastic time interest equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
