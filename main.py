dc7ed70e47283b8830e6286cec52988e31601e6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:45:39 +0000

    protest quarter discovery explanation certain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
