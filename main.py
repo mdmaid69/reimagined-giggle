89db7cf7ba2f6bf8fd0d4c14e864feb308f2d9d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:42:08 +0000

    learn similar feature crisis confidence

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
