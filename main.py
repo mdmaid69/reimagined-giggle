3acbf213b59b404204fa4e0c54c818b2eb82ce38
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:38:45 +0000

    second feeling everyone declaration room

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
