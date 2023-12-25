7260347a90b64b38c4e60182d6344433361f491c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:52 +0000

    estimate unlucky shock although want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
