8c40496bf60b566d2e42b455f095fcce0e5e134a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:10 +0000

    relationship pleasure large niggaacademy protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
