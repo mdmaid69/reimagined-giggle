00299575183d99b651934c0640a193846a68b16e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:59 +0000

    high should desperate famous ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
