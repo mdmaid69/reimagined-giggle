4e7a6094fad9b3ea3bc760b1eaa76192466a14ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:51 +0000

    force excited bet concentrate unless

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
