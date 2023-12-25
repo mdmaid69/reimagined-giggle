5005e2a6d1415f6c92b88efa1e535b2050cfa0cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:05 +0000

    inquire bar invite speak damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
