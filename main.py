8207b2dc12338662f37319d7ead07b7aefaa5022
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:45 +0000

    money them transparent remember cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
