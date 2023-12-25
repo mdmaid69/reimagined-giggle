5378d58479274009bbe04e11339d6ff6cd9d5735
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:53 +0000

    beat reason whatever steep occasion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+def add_numbers(a, b):
+        return a + b
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
