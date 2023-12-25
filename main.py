0b08af7ae652928ee843bc56d8902ad09745f3ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:38 +0000

    rub criticism certainly confidence base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def count_words(sentence):
-        return len(sentence.split())
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
