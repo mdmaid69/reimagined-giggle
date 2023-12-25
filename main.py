05e4e7352083edf021b3a5b43dfc500129481a54
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:03:50 +0000

    compare embarrassed type achievement under

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
