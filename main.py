5232c7322d0f389e0399a48b83c1917d9798075e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:11 +0000

    share or correct search year

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
