709fb147ddf0e04bfa5a933ce45009a68ea0443b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:44 +0000

    acceptable convenient motion glad oil

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def add_numbers(x, y):
+        return x + y
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
