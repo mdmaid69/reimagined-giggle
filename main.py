a4e0dc4eee02770b17efc1247417bb8f1fe60ee3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:12 +0000

    shelter and attempt dull upstairs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
