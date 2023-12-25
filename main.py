103024f7c795ec9b2f00151a97f61d263c402f2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:23 +0000

    blue battery effort dick whoever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
