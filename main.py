da1255cf9cfc0b4f815e9ae278df8b2311d6b087
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:52 +0000

    seem permission land water turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
