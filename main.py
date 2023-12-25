b99ca2b4a9116fd9be71aa5e58086e40f8f6a662
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:42 +0000

    crowded roll modern my offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import datetime
+def get_today_date():
+        return datetime.date.today()
