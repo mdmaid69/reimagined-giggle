214e2ee2d24ef873065ecaca337b2cf2fce0c466
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:50 +0000

    claim concerned answer lookaround upstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
