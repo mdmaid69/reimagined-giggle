3c6a65a38a2776d84da78c3d43f7e8e7ea48143a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:45:10 +0000

    wherever travel complaint first high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import sys
-def exit_program():
-        sys.exit()
-import math
-print(math.pi)
+def add_to_python_path(path):
+        sys.path.append(path)
