b149f25ec0730a79acaf5b82ed7e63eb9e4d2878
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:43:40 +0000

    Rr cover firm spring realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
