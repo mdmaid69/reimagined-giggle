c2b15c2e0f39c963b7bc17e97c4623437842b457
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:05:42 +0000

    sometimes though supply thorough rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
