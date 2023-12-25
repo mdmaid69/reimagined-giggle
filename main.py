73b38481534e5cfa7c4a3bf80ca32e1f712a41f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:41 +0000

    combine patience shop getaway neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
