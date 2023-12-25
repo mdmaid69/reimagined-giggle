b6a66960e1fa931f258a44c03e189729b97c8b07
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:52 +0000

    seem muscle past neat building

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
