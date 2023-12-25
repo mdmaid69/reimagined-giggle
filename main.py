603da5eb905666d5a7c508b80a13c79dbe7a7b6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:22 +0000

    base vision measure influence breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
