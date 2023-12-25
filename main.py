b61d9cde29cfed86238b67a7eb5107d16c76cf78
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:17:49 +0000

    accommodation dry sing operation Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def is_odd(n):
-        return n % 2 != 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
