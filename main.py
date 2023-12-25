11ebb5466b2dc8e944c05aa4ad92c081c6e6c6c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:59 +0000

    surface force breakin correct couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
