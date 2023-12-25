ee26a165372bc20585437fc9589f4ec7bb554084
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:27 +0000

    continue heart convince settle interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import time
+def get_current_time():
+        return time.time()
