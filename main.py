e70ad59f10c6e3c4833448b0695035c6a2b0fc32
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:35 +0000

    extremely satisfactory state bringback passage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
