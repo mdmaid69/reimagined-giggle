e6ca656eba28cd63c0e633704ac402deb45a3cd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:43 +0000

    ashamed how man worth miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
