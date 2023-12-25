4aac2a408c0dca7d165b5bd351a4f3b0c74c965c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:20:34 +0000

    trial sick 565end never conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
