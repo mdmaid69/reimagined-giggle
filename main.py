5fecaefe46ccf115a6c69968a8a253ea71dea820
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:13:38 +0000

    impossible male weigh strict proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-print(math.pi)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
