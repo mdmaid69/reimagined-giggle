a991cdc01422bf83e6066f29756b609d643eacca
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:34 +0000

    interrupt increase giveup roll quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
