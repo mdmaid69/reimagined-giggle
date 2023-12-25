17a4da4f3ca8cc607a9fcf8abe91722ea95845ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:14 +0000

    waste 572enter confident joke keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_length(array):
+        return len(array)
