07518d5a71a6ab1f9b39b740e43822e6e72fa221
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:16 +0000

    cry male risk reply damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+def get_current_time():
+        return time.time()
