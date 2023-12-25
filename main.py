dbb33dbce4adcc2718e5505562c4ee877d502764
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:22:09 +0000

    health process surface exchange snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_bytes(array):
+        return bytes(array)
