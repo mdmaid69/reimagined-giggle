e3cdb48589e037b3252b21631f07b7bf0006e7aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:48:46 +0000

    platform fire workon colour which

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
