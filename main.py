2cda23fac22e4d5b9cf4ac9f31ce0f67d1b471c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:42 +0000

    hungry cough swimming practical entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
