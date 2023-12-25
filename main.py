774f93c34c6a61dddacffcde21bc3ded37159038
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:17:31 +0000

    commit challenge second once modern

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
