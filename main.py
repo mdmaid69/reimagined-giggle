609a805e068ddeb375f3e9bddf6bacc3bfcac72c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:33 +0000

    score fast practice wall clear

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-print("Hello, world!")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
