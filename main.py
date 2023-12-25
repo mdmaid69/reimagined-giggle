3635e96770942191d87a6ea39be7599286d7d7d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:21:58 +0000

    favour again crisis announce chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
