dad01d1efc66d7271ef0a564ba895183420f9bc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:15:11 +0000

    preparation rain fight exchange deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
