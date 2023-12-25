9f9b7d2789bfbd631660a0a84ab44a89364a797f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:07:15 +0000

    makeup once though over feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-name = "Python"
-print("Hello,", name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
