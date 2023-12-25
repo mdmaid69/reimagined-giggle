2dcc02ef4cb098300c305ae8c52bbca934281e42
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:29:37 +0000

    raise mean affect variety eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
