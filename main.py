d3a2e147bc55f23d639ac306b7ac4a72cbb7d982
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:19:37 +0000

    ashamed union for degree disapprove

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
