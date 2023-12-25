b9767495ef4bff3aa4a8967b73e6467c8d11584e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:04:56 +0000

    Tt ago Bb at achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
