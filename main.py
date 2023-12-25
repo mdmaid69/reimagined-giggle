ec8195ff7a34dd1a6ea0864ac9a8425d20024c1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:28 +0000

    guess play plant concern next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
