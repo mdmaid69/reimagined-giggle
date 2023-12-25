57f5927c2137e53a8718e1fe33a3a9e4105f4075
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:51 +0000

    568energy pile limit practical slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
