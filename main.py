1ee2879eb096af99cd2294d7aef992214926177a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:24:30 +0000

    will pile about little aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
