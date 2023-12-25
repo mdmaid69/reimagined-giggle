8dca9615561c35f5a505c0b0bf443be642773b53
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:25:16 +0000

    punishment tread admire entry letter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
