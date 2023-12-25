e9c2c4a70af7545a93bb8ed1ae2ea1c828b061d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:19:46 +0000

    float but neither slice conclusion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-print(datetime.datetime.now())
+for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
