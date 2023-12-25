a1a94ed3cb7f520b46a31dca936a5a147798e9c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:04 +0000

    bring method spread against benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
