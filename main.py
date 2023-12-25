9b7dbfb1cd050c5d7981efe0a8054820500dd0f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:39:56 +0000

    hate confused cause shop getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_item(array, i, item):
+        array[i] = item
