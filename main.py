f7e2efb45cd8973fce4170e28cd092ba4fe05ea8
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:52:00 +0000

    increase along giveup design maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_stack():
+        return collections.deque()
