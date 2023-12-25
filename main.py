91eb9148c895428341c3ac57b714463d54f881de
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:10:08 +0000

    difference deep wave curious science

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_volume(length, width, height):
+        return length * width * height
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
