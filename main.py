567c5780a7aa519af65d1fb75febc51fb98e8961
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:18 +0000

    truth least rush unite quit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
