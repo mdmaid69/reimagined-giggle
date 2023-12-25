bd13f4887cd27257cd6bb650db4f29bcc81f831c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:38 +0000

    nothing wish sleep 567enemy pot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
