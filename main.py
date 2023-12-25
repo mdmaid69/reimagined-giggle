3656dec3cfd8a7042be385dd5c684675012b2dde
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:01:11 +0000

    realize belief another strong would

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-print(time.time())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_current_working_directory():
+        return os.getcwd()
