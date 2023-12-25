b88051cc69612630d388a124c3656318a395f9c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:39:13 +0000

    talk air affect strike circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
