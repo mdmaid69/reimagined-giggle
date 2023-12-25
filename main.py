0c18b11832d6c194d1ef113a8ffccc485ace4ccf
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:45 +0000

    pick rob plain floor place

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
