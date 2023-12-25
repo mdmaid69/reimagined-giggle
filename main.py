01d563c07ceb0ee2392f23fedde36bf11c4cf7fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:43 +0000

    object flight report round wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
