6f9c7350ad19763e8228824e89e0f860c1c54ea2
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:58 +0000

    escape sensitive will to close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
