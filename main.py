b910459fcdd40a0d5f7a27145f254536322aac31
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:07:42 +0000

    legal disturbing feel principle when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
