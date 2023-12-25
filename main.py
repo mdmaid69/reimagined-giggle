dc2be7bb270d6978c29f2eab78d739f5b4338c6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:49:42 +0000

    what remember no autumn arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import time
-print(time.time())
+import os
+print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
