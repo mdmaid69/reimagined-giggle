203e7c33dffe5d09e740d0bdaa6f56cdbc06ed87
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:50:37 +0000

    ready tip 565end maintain after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_int(array):
+        return int(array[0])
