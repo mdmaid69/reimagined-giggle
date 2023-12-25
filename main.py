343d1252e2ae80a8e7a56765cb606f879231bab4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:49:27 +0000

    subject fork holdout side become

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import json
+print(json.dumps({"name": "John", "age": 30}))
