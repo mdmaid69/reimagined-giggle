64e9babf942690514acf1c0b20dcd506f148566a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:29 +0000

    private deep sky young movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
