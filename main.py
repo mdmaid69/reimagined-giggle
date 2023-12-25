46876bb3b02a2a0a176815d0702a1eb4f7ce46c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:09 +0000

    term since earth frighten trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
