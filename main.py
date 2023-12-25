34e810feafa218e7a59c9a1ee8c1aa3ef57fe615
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:10 +0000

    force exchange somehow relief the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
