6e278eb0ee1003817d578ccbd118bb79c149687c
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:14:15 +0000

    money discovery giveout mystery load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
