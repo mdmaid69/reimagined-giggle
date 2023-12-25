8dbdb39b83b9a5ed5d624bfeb414e26036950c3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:05:16 +0000

    ability goout try surprised correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
