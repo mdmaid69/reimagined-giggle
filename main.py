8848e1fea66afdcaeeea02e723902d2491d673b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:19:02 +0000

    operation immediate estimate public heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
