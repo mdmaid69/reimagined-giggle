e9c19ba72056b00bea2f3f4434d2c5187c452588
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:29:41 +0000

    fill explanation pick vision recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_base_name(path):
+        return os.path.basename(path)
