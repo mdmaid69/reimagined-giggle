dc5ef34a9b037139abdb4f34f81e4c5bda8b0da0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:22 +0000

    total gentle bus stage comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
