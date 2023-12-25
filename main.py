cbe62fc2e93da8671198fdde560fa5b598045a4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:13 +0000

    sign several price well shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
