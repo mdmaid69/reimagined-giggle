fc88aa76b57e05e3b1b784f47b64cc4b0270f781
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:08 +0000

    normal relative regard excellent unusual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
