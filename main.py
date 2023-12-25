dd28eebf3ec3a6869b5045b6ee0b6594c72128ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:54 +0000

    know active goout mainly advice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
