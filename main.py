16ccbb3bd61c0b57a260e89b03e4d4af5a670825
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:19 +0000

    quarter frightened surprising slice lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_item(array, i):
+        return array[i]
