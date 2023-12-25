51a0801ebd76396713d690a2639458f166e294f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:01 +0000

    spend rob among unite Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
