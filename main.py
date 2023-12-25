187491900668fcb344a449f5d8a34633d201850d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:47 +0000

    female together violence couple lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
