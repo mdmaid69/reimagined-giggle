d9cf37418c33b1b53d5d1b0ec6da358c757e0688
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:31:29 +0000

    do able whole whole ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def square_number(x):
+        return x**2
