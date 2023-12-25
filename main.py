e250072152f6b04b438bbac2fef0ccb75b53ed84
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:10:50 +0000

    do phone hard me it

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def count_characters(sentence):
-        return len(sentence)
+def get_array_length(array):
+        return len(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
