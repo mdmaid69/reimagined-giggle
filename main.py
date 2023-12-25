e02fa9151b6e428a828918c81cd935c5121c7d28
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:06 +0000

    recover child somebody interfere threaten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
