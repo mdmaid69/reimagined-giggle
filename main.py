40f2f9ecc5d06d756527a1d7a4b41f571973e322
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:55:39 +0000

    pin question intend smart telephone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
