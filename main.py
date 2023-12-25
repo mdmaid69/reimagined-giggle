0505af2c4df5c7de782f0e3c6483dda89c75b162
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:39 +0000

    comeup today black by fall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
