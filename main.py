48f25caf6b9d8cf5790d516a86b42cd0bd83222a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:26 +0000

    dish play please aware pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
