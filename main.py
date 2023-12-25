3a86f9a044ccb443e6ee0b3e0b95c2193ab3194f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:52 +0000

    getout speed communication official develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
