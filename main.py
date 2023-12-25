c6d7cf6b5702c3694b0bdb2367ad10ad6bf70982
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:42 +0000

    sink strength result behaviour soon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
