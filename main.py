b7633330cb7eae21997fddb196029bb923123b5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:48:12 +0000

    car hers Pp rate pleasant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_union(list1, list2):
+        return set(list1) | set(list2)
