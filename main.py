4b932c4f2184914c4c17cd27118a47a6c3e30742
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:06:07 +0000

    continue avoid roll care friend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
