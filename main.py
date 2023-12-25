4754d17e5ac07fdb289db791a136da65a3797a51
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:45:43 +0000

    ear small rub duty about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
