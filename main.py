75fb24b187707582069ca8de10f34a8a79c67ec1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:27:59 +0000

    name during sort direct address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
