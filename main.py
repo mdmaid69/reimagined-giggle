3647cb040ab936d7f3f874379f4506c54a8ae24b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:54 +0000

    nerve with heart direct lesson

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def find_max(lst):
+        return max(lst)
