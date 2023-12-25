2e6cbaa321ef4f2abf99950583e55091e5126062
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:14:47 +0000

    bet failure Pp heart rain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def is_odd(n):
+        return n % 2 != 0
