6b6159207b6b78a7cda32e724eabaa2a0310a48a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:42 +0000

    promise satisfied thorough rescue apparently

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def remove_from_array(array, item):
+        array.remove(item)
