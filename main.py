c39d63bf6403859ad1d2509f609f8a4670a62892
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:27:53 +0000

    takeout far beside Hh food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_volume(length, width, height):
-        return length * width * height
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
