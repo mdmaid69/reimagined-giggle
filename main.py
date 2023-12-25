4ed4499aedca6397ff250a03273d1e3a42ab8d8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:52:09 +0000

    complete complete tone rare harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
