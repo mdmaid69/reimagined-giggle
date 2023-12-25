be34b468e8a62b3f8fd9bb4283b4c16b4269cb57
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:47:02 +0000

    goon such precise dear ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
