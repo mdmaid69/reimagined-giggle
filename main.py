7e41d7947d1bbdce9dd8e30b0e18b50f30e1e28f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:43 +0000

    quiet pocket Ii form action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
