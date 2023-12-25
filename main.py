69e58b359bc5e6d3b047d288c923888728bf54f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:42 +0000

    short still round border sign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def is_palindrome(s):
-        return s == s[::-1]
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
