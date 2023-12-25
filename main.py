b8f40498c66c63136f5a524cef90d499ef173af4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:28 +0000

    hard so hang heart Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cosine(x):
+        return math.cos(x)
