50f84623ab975801e3b119ea494aefca31beaac8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:19 +0000

    unit ordinary intelligent programme permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def remove_duplicates(lst):
+        return list(set(lst))
