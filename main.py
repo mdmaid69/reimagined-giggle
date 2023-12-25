39cfc0efcf5e09ee746cb86d2e5b8cf917c5c83c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:47 +0000

    kind if anywayanyhow entry fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
