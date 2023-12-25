31786e39da113a02ce8fda4ffa4f7752892cc5b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:33 +0000

    ability smell complain Ii steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
