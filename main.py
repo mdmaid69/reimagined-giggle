82a9b99aba6fb3384b0cb67c4b66b80a6df8f851
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:15:50 +0000

    rate convenient poison turnup area

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_even(n):
+        return n % 2 == 0
