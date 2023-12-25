10ac01156341760547e80c555439a033ee48c3e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:27 +0000

    replace while equal trade desperate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
