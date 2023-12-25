af5403ff974b7610362ea03e633233ad05a8407e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:32 +0000

    private apart win takeout confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
