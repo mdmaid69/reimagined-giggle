a825b7469004a59b82e19326eba39daa27332aac
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:16 +0000

    manage climb imagine charge surprised

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def convert_to_octal(n):
+        return oct(n)
