69a286572421d1af5efb3a7fdb112af6ecfbdaa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:38:39 +0000

    they grow roof exam where

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
