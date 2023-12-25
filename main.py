8402e951d0a923d98c9f51f3531bffe1fdd97ad9
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:15 +0000

    play finish may lesson nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
