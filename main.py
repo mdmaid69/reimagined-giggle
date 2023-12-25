5cda2c39945ef1d714d34edbb040923a413cff82
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:40:24 +0000

    jump harm loan reach scale

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
