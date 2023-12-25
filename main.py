4bee1eae3dd4bc45a45b0a970a34702c62891b11
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:23 +0000

    card no disturb desperate like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
