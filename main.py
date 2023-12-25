9c9bb9846df33027a2d04334c1e075fea64cdb49
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:28:05 +0000

    strength keepup contain none work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
