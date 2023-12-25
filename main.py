9084715f9de36a1dff7dc7ea2b1161c8a81d0c8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:54:33 +0000

    degree impressive song trick control

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+print(time.time())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
