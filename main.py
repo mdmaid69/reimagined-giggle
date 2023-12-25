caa717065ee13c6a78c8b49d246b6c1b5dabd82d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:36 +0000

    freeze use people pen impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
