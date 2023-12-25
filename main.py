b7237d960430e5af2c057b3388be3b7eedec3389
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:45:12 +0000

    otherwise content platform best public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
