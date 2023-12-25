15c29787df1642af900cb0841b15bef932fea84c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:40:44 +0000

    comein take create her name

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print(sum(range(10)))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
