749a29280d9afcaf610176222659696892a9a7f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:22:10 +0000

    interview again terrible light consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_exponential(x):
+        return math.exp(x)
