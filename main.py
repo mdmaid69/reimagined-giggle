081e050280dfa1fec3b21382c73fc3a5bae192b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:20 +0000

    sensible speak waste you genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
