f6fde96b0554d9e93e0f9135a6df00a6dbafd8a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:37:18 +0000

    trap recently medium disappear command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
