1b90b874151e5ce7a29c15fa835e945192745456
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:26:11 +0000

    effort present disagree bit impression

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
