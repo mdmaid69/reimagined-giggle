4243e36a5848c6683c0bc25919a640778561b02f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:43 +0000

    follow knowledge distant wide call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
