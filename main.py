31875cb4628a5f733903ed4f4498885a78665297
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:11:23 +0000

    rubbish neat summer subject shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import re
+print(re.match("h.*o", "hello world"))
