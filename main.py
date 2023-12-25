c359ee51a869c1750109770a23a3ee1802ecb04b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:06 +0000

    566end kill fail runover long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
