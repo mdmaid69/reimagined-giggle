ae88fba069d99aaa7b077a886cbee769f7fb9004
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:20 +0000

    careless scene work suggest thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
