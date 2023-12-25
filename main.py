6f97a483307dfafe4efd0c9fdafdb8ac861bd310
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:39 +0000

    drink new challenge trick task

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import sys
+print(sys.version)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
