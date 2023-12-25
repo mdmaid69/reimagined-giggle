bd1aebac3862cc1127c3172835b999df11bd81d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:37 +0000

    book commit influence train community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
