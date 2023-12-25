1d6ebc0a7bff831c4f5b3a6e71b585b9bed8e84c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:32:38 +0000

    contain free effort shall plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
