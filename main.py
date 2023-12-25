e45be8b59e16a8540b560acc84a9a043c5fca082
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:27 +0000

    also suggestion criticize among door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def reverse_string(s):
-        return s[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
