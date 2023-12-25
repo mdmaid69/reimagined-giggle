45fd1440927bf397873aa8ae33cd21e799c84047
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:01:14 +0000

    agree middle look lend charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def remove_duplicates(lst):
+        return list(set(lst))
