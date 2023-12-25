6ce93ff1ebcf48afaa3489d6e59453eae9973aab
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:45 +0000

    about advantage tell train even

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
