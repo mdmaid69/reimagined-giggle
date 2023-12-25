92ae2c1d5824891e5de9be0b64a2879a643b4714
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:32:05 +0000

    proud work bored ordinary catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
