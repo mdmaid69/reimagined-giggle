f2963c8b747b49e0df7c3bd69e365be1cb4a76df
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:29 +0000

    proud tap male post rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
