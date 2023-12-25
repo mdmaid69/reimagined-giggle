2c4dbc38a90ef685b8c53e1355ed77c7daabd8c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:10 +0000

    dance hospital show cum listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_typecode(array):
-        return array.typecode
+def is_even(n):
+        return n % 2 == 0
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
