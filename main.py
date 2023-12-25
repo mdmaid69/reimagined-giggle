f3f8f00420d07b027698d33cd077aac428f8a84e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:37 +0000

    straight respect shock ready call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
