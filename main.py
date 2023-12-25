5d72fa0e46cb97c72c8420994a89c46a46ff836e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:33:24 +0000

    short replace trap another solve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_power(work, time):
+        return work / time
