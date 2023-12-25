51e36bf47e08134df856d65dc19c22fcab39916e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:38:30 +0000

    shopping pass weather idea geton

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
