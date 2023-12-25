826dab5cad2276e5522fbaa18f064ec49d65141d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:32:08 +0000

    suck beyond computer doubt name

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
