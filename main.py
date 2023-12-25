ab7907b4618e7c98174198dbb8cceb7042a00eee
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:00 +0000

    our comfortable zero help develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def subtract_numbers(x, y):
-        return x - y
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
