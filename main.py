1e6157a1efb271e49bfe79b7512c08d2754f5a2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:13 +0000

    sound university there autumn rate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
