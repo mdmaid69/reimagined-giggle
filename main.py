d87b04b5fb70bfdb493680fdd7b69b778373ca31
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:53 +0000

    Nn key impressed myself enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
