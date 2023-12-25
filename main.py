c55c2d45f81086fed7f6a77ba3a3336c67295b43
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:30:05 +0000

    row trip gradually joke ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import time
-def get_current_time():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
