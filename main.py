186d94538e6de0b35e40ba6eeea0bdfb8930ad60
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:55:50 +0000

    back lie character people disapprove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def get_string_from_array(array):
+        return array.tobytes()
