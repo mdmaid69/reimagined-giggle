b0685e2016414c6c4e0b7a899e442f5e540b6a6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:06:04 +0000

    doubt pool positive length admire

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
