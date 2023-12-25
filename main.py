1de704c28ca0f4b88eba673ea0460d4d8899fe57
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:31 +0000

    crash foot note forward check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import sys
-  def get_python_version():
-        return sys.version
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
