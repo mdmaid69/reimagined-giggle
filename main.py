be66dd987bc593d91e87ef6c67d2cf9d9a76416c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:51 +0000

    callback drink letter lower pool

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
