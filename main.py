db191e909f45d364d17a23fa82b6ad3d8c12ed96
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:36 +0000

    downstairs slightly base snow emphasis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
