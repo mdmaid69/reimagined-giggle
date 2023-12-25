4360e520dd7f2324b8aafc76e54bfb762ef1f671
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:32 +0000

    know level deliberately furniture Tt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
