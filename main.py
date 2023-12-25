5aaac515f27069554262be71639ead39f1fdb0e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:20 +0000

    exam comefrom shoulder my satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
