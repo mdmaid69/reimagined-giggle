577090c5184639a21102e12c41b78b1287e24e0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:15:56 +0000

    break born crowded impressed life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
