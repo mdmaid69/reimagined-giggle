b911bd269b07fddad5e450cf40cf9fde46a83709
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:19:54 +0000

    blow getup name takeback convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
