986f9ae7307c94119bed0d751a47f4f78efaea65
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:13 +0000

    call centimetre low consult clothes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def cube_number(x):
-        return x**3
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def square_number(x):
+        return x**2
