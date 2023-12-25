a2cf3d5dd7ea908287750cc9015947276b96ad91
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:04 +0000

    takeout murder refuse calloff practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
