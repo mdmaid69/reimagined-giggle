2a08b0b77a4da1093f0382a6d91868adc9cf911a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:05 +0000

    love slight comeoff work poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
