6a6916db1140e5e3daa0b2936059f502c1b68b6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:16 +0000

    coffee unit worry negative than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
