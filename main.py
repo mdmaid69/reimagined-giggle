6f1e9377656cbdc66d45cc9030b2f73e292c94ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:48:44 +0000

    better doubt appear turnaway address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
