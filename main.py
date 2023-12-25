3f6f39c046ddd0e763305cad19d745ffc4e73eca
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:53:12 +0000

    coat forward question against advice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
