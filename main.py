cb363e179fd09f030082eef5bf7343e7163f938b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:34:42 +0000

    scare push uncertain otherwise must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
