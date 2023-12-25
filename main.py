269d0c377da6964373e2b2524a8d056bb39ac980
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:30 +0000

    start aim little enthusiastic anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
