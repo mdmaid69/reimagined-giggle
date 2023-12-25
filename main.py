4d68422741c8585e6edcc3522da243c3ce02c1ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:07 +0000

    regional connection Ii case minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_power(work, time):
-        return work / time
+def get_array_item_count(array, item):
+        return array.count(item)
