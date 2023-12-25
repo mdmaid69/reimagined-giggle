2afa26104e5ef929cdf779d5b14bcf5bb4039adc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:46 +0000

    aim famous dead guess sensitive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
