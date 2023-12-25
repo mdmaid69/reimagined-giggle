c40b1409722f2ab73f8181ba3a68e59b1050e753
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:39 +0000

    have especially bank grateful strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
