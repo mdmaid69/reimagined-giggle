e92cec6545d2af8ecab6c0f88f49fc74a4412dae
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:58:25 +0000

    satisfactory justice attempt wave unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
