20415d17f9ed58fefa7a9c35a211de8762711c21
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:50 +0000

    gather accommodation rain put possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
