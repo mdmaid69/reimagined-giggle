bc8fbbd32174f8b844d2804f1de79cb5303d9cde
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:18 +0000

    second afford ball backwards everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+print(math.pi)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
