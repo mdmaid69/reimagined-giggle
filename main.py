1ea2238ea34f82e9a15f2e72fadff34e9343139a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:21:46 +0000

    corner everyone squeeze point common

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def sort_list(lst):
+        return sorted(lst)
