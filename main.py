c55ceae302a1b8f7d3599ad1630e1b9235515f7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:29 +0000

    big represent game getback perfect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_min(lst):
+        return min(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
