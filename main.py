8a147f065f533e8c1284ce6b100dc851c93c4afd
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:18 +0000

    skill whichever speak appearance party

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
