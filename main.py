1b4d86e98710f99118145cc50608a4d8f3ca3088
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:12 +0000

    good contain art Ll direct

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
