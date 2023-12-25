554a1fe9847d3f3ac0cfadab7057457ca35de110
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:02:42 +0000

    switch recognize hollow for drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
