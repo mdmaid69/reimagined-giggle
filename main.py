277881bc52ca32c7030b8df2e14af333af2649ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:13:43 +0000

    behave many apologizeapologise anything drop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import os
+print(os.getcwd())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
