daf7ef2aeab7668b9c4c44606d5e37ee2ef82940
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:06:54 +0000

    organize half except progress ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
