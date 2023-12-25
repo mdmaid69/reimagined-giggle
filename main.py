d1f1500fce9f3eb6325e00876dd23ec1014b0ad5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:32 +0000

    go stay wise post shelter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
