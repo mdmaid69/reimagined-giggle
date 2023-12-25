6874df5032609e40049671b2b668245c8b1d93f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:37:53 +0000

    wall repeat rule guess bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
