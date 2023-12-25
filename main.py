96ba18e36e7c91964b2d0dae29237d1c9a7445ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:33:33 +0000

    contact embarrassed beyond take impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
