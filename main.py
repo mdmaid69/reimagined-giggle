d7c7991ba6cba85137a425823affbeb1670d74ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:56:35 +0000

    skin 565end describe quality check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
