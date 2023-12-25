acf96f7d6c7cb3ab68df0ca8e127f46bb9614541
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:56 +0000

    swing one the poison steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
