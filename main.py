8a04af61530dc60ab797a170f0aa565e95ad1856
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:11:16 +0000

    with different land history trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
