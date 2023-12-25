6fc7c5993c1e3dd63170a3392a38102a6b9367d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:18 +0000

    kind complain suppose bring treatment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def count_characters(sentence):
+        return len(sentence)
