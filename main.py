b10b3b232cfdf4c072c7339127e015cba99a04e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:22 +0000

    each train record sister future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
