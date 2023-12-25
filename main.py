babee8bb24871c2a09a4f94a38d4946cea2db7ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:13:29 +0000

    deliver violence fire reduction likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
