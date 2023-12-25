23e20bdd47b4c90e9f7728247d2031cbd7237656
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:44 +0000

    regret me solve store mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
