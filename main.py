994377f7588e53ca564caad3259f416aa2f83449
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:12 +0000

    environment neither concentration rely his

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def convert_to_binary(n):
+        return bin(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
