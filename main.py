046689cf11e375bd515917c12df74e05b9c1352e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:43 +0000

    distance point 568energy Nn ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
