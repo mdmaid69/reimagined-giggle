13e8dbda8f5423c30cfd53e7cf9f4132da2ca5c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:31:49 +0000

    sweep job conduct tragedy benefit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+for i in range(10): print(i)
