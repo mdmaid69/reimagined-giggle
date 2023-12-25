683c02c8535e8a12a0edd06e2a9a422921edd4c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:59 +0000

    second holdup conduct once except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
