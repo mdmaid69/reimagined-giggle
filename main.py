0996b701a5f4c5275bbcca62ea1057b6ce95ff1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:26:04 +0000

    do pay makeinto blow close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
