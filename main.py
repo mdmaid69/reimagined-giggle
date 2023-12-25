7011d2f8bf147668761fb2853c8e0d26c3b85856
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:20 +0000

    plastic callat size straight theirs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
