c7e76685c20d0b4ff515421150e57f6b974c6e15
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:11 +0000

    passage difference closed bringback our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
