5af89b78dc6f96d760a0ff0ac663ee0e3ffd996f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:20:28 +0000

    talkinto deserve lift comefrom phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
