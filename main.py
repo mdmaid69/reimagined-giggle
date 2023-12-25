c429e2b7abc379ddc66e57827fa0cfee731fd08f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:41 +0000

    difference reasonable swimming film right

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-text = "Hello, world!"
-print("Characters:", len(text))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_max(lst):
+        return max(lst)
