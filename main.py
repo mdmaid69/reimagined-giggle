023e3254113a39a7986aeaf62acd2661fe18cf39
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:30:42 +0000

    impatient tap force positive petrol

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
