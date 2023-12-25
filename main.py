94b35dd3ba5715c9d7b08d2f30c8e1056f42ced7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:37 +0000

    along but easy loan land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_as_int(array):
+        return int(array[0])
