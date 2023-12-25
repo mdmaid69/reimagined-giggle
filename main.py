0ecd19c4a5578095851ad23965dde6d2fe36bbca
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:37:40 +0000

    glad yes anywhere somehow rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
