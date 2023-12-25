0b64d695f104595a8279006d7bccb3d181c81de2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:21 +0000

    have satisfactory shelter earn land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
