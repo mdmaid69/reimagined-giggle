ed2f8cbc0700b482db432f1f84d330f80b5e390b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:17 +0000

    straight whoever embarrassing cold watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
