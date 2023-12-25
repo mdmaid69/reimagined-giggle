6a8b9cce6907350cdd4386967e0e36ae01e57d68
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:13:24 +0000

    bus top fold his address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+from collections import Counter
+print(Counter("hello world"))
