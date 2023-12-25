61b6f253b3963a0d63169add919a4f2b0eeb3261
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:07 +0000

    agree ignore act forget more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
