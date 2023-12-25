15e4bc3267fce85faf89220fe7ff60d1ff7893c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:17 +0000

    by part morning passenger ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
