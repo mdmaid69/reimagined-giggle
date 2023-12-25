8d0af0c27b1b7789c515669d32b39d8699743f4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:30:41 +0000

    communication shine appearance music effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
