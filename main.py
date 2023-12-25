1a0f1f01f28f8d0cf06c9b1bfb8a8c37d7b21998
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:33:39 +0000

    condition feature day share position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
