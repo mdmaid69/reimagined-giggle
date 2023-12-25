457a8edca257a4b9b2131a93d6ddd07f499164f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:28:53 +0000

    pause will sudden damage order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
