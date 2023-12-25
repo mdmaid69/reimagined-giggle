599a6d14086bc16870b92000c7c937e73f1d424b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:50 +0000

    roof settle poison bag stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
