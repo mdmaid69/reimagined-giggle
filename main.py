a195ef6d75af55605ddcb4f9adfb4e538b4c2744
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:50:54 +0000

    phone direction Yy development car

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def reverse_list(lst):
-        return lst[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
