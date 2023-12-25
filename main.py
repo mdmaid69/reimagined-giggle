5c79ae29bf57ab1162fe44f146f817b541458570
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:52 +0000

    principle born degree market century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
