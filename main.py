2353b081322f4c837f24651c7af0141da4f02c6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:03 +0000

    view tip exactly arm violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
