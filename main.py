d2f87b45df43798839b471505416653905911f7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:27:41 +0000

    instant doubt recommend company authority

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print("Hello, world!")
+def square_number(x):
+        return x**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
