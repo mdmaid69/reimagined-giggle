55d7ddd1d24014ba6ae8cd331bac86b96af9761d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:18 +0000

    rub idea behind who claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
