cf633ce996e41e08b445e38755f3834939c6cda3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:56:34 +0000

    react invite strike goback(to) dull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
