6c5730ea415a5f519534d8391ab59273c0f60908
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:14:03 +0000

    dish wander light weak compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
