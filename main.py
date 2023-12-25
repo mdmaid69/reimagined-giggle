e927435899ee27b6f97c4871d7b7f1cc30bc9f8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:00:21 +0000

    original catch violent consult about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
