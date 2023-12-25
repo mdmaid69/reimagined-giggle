cbb2fd07db7b9ab83f134f74d4fd5cd2df3d28c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:57 +0000

    violence duty probably responsible truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
