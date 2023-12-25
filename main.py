168de8e2275bd0f5d37c4ff6390616250bf87de9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:47 +0000

    only balance delicate fine tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
