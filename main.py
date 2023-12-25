dfe9186cfb0701555b830e32ea526bfd796defb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:03:57 +0000

    wonder delight easy month avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
