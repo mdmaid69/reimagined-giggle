507b4cb14920c6cf603b1f5b03d41ce35f1e5520
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:06:38 +0000

    last small face smooth wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
