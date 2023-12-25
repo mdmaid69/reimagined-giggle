1aa28dfbd47fe47086638d634d9d44d37343c6ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:36 +0000

    own pupil provide cheap travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
