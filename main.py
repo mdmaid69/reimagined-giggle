9e14e85a5724c417451168aa62e84787d5a2bece
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:49 +0000

    safety trap play birthday equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
