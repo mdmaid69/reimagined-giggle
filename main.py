05c40d885446f3183036f6067ef6826929596569
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:17:16 +0000

    mail container risk he lower

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
