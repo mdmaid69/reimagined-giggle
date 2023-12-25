3d193352f38abbb7efd6925e962f4afc3226f50a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:07:30 +0000

    open tread enthusiastic giveback freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
