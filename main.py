364885352b955b93aba7a396a91b96cb07ebbb24
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:15 +0000

    fence by common education complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
