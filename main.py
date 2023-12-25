e3deb0dd78b4f937d9f4d421d5690f22accbb7e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:06 +0000

    amount goup play movement never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
