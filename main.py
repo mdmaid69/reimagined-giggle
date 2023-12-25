7200d6c5038510893604ecbf857af1a98bb382ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:57 +0000

    deserve factory either artificial fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
