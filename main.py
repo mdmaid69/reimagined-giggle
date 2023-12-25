19a3c8f2a980f2287316d595cc95bf0da97415a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:01:29 +0000

    pour nose common gas gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def convert_to_octal(n):
+        return oct(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
