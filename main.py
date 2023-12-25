2986b39f640d1e9184d16039c1ed2122a6c30162
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:18 +0000

    tough perhaps bin shut body

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
