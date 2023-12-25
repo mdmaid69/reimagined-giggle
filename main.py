ec1d7fa023e45cda94e0aa57b5f4a9a2f0b2a96f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:22 +0000

    you statement once check callat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_power(work, time):
+        return work / time
