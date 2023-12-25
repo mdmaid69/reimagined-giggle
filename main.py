c92f51d06bd7be9dc34339417aa189ab0a2e2473
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:42 +0000

    frightened bread neat report pen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
