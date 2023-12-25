f72aa85065cb1e1fb1d434ddf771e9f656c68cfa
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:35 +0000

    can letter identify moon neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
