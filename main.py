be35a4d3849bcc89e8fbcce98103a62b3cf47c12
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:20:15 +0000

    if obvious central pressure necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
