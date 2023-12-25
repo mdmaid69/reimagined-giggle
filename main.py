c39b90502064bfe65c97a1de0649de4e1bec84eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:24:12 +0000

    increase belong shelter centimetre regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
