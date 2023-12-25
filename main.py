1c5b5b7f50691ae1faec7d9b7770ac37e0f286c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:43:21 +0000

    offer key manner ignore temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
