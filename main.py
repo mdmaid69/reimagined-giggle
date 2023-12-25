1f8e18acb8d3acdf61ff80dce542e4c42e0579a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:37:30 +0000

    exhibition year speak wind still

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
