fdb55f1b09131c19270f1500908539ffeddfabf1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:28:09 +0000

    mystery gooff Qq failure will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
