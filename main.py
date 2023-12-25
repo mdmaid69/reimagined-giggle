aa043a53abcf21b868de35ccf5a74a747032e590
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:06 +0000

    regular rank generous herself shoot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
