05ef24c7bc1989a5028f3da80d5d945941d815a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:03:18 +0000

    proper interested precise search slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
