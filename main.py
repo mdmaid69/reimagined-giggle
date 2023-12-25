4b928b14a71f464f15854b3af96ee11a6da711fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:28:54 +0000

    judge would workon content there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
