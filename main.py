3b2a298c2606d5e21ba562ef40046f3bf2a890de
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:42:30 +0000

    second gather marry class never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
