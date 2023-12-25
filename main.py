d5c885bafe39ab62a3290c7d08e09ba8e411ca67
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:47 +0000

    roof boat complete strange file

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
