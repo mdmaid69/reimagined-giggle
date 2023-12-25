054c4a171c8dda45db927b923617691e64a6f03f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:46 +0000

    substance reaction speed flow taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
