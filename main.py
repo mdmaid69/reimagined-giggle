1bba4d18439d61e1f1c793439bb6af12f5fdc4ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:31:10 +0000

    suggest deliberately book sudden avoid

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
