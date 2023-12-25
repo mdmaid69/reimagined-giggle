cc438bb35bb0d498525bb7bd9d523da0e48e8441
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:56:56 +0000

    frame left plan dress angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
