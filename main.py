43098a56d859b4f0e6cff97d5ee29f9a10e91b4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:01 +0000

    programme example turn hang exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-print(sys.version)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
