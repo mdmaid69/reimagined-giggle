9010b405c52722473ae1c113136f15800679c33b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:58:23 +0000

    promise cost this handle not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
