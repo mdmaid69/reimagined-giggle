297d5a0d451cc20dcc871f4f7d55dc9ab3becaaa
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:34 +0000

    connect badly sometimes lesson design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
