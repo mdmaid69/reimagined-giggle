30e2919a594b36d2b9ce4f296e8e74459b244935
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:20:14 +0000

    accidental 572enter twist unlikely ignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
