d4b1c2ddf33203ad1083a366b45e9115c88ed6fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:56 +0000

    admit argument branch geton stiff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+print(os.getcwd())
