789dbd0f1314a29b91b5102d9b0a5f3c827dda46
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:14:07 +0000

    mad explanation plastic they hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
