bb5345d7b87c07cf24a3df52f52a3cda482ada04
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:06 +0000

    freeze waste reach thanks collection

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
