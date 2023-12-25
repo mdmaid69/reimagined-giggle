6fc682192dce4bf51f1ca297dcd27522456f856b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:58 +0000

    practise teach from slide confident

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+print(sum(range(10)))
