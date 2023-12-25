155128520bca1c3465710f92dad1cb42eccd3c63
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:40:26 +0000

    thorough honest everything pure concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
