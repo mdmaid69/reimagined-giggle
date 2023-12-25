41e91bb298a0b7d2d77f9f837c21f356d0a6ee2c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:58 +0000

    make not distance realize convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_speed(distance, time):
+        return distance / time
