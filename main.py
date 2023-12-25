47e82a1eed6042a8738770fec9e1fe33ca5f6693
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:15 +0000

    judge Kk warning godown rely

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-print(sum(range(10)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
