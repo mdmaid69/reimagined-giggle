476ee16fb646e9f7c26192a18c6f1cd124d4a3ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:50 +0000

    case city giveback control receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
