c77eb621dc582a3c0e54f396b0e4350abc5fbe62
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:23 +0000

    takeaway weigh cut illness repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
