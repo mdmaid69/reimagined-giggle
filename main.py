17d3dcd8f812648203a458af06a9941326edd449
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:10 +0000

    concentrate capital unit so keep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
