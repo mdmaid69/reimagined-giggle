10f3e0d39f8126917dcc45ec1b78f6cf4caebe42
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:52 +0000

    variety frequent few kill life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
