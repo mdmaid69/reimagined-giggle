97a963d39350f5820ab1d9488b220b7c03420ef6
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:42 +0000

    replace shock when godown science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_work(force, distance):
+        return force * distance
