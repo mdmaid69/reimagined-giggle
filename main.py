f7b890fc57da4027f31925cbf69681f96e5ba0b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:28:57 +0000

    block bath excited until moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
