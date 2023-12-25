6bf104b1a8185012fe99d5fa2b053166e974675e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:44 +0000

    wipe speak themselves throw 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
