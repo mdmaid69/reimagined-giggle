e2f8d64b936b7b3df03105f1156a466d0541666b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:00 +0000

    share clever solve detail breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
