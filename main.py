02fa7d1a5cdedc616c869217d1b4d0e8db7abadd
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:49 +0000

    careful rare proud tune wipe

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
