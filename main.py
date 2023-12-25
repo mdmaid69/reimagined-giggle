20bdb6229eaa4289dba52326c079c5592a3fd63c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:25 +0000

    mouth create priority front bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_array_slice(array, i, j):
+        return array[i:j]
