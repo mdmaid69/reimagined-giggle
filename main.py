047f4ac081ecd4234dcd583ff45aad845d1cc959
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:23 +0000

    temper cough during string left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
