0bd4339646ded2d1d6597c7d3227ba90fe6c3bc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:34 +0000

    them adjective people mainly breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
