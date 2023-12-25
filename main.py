16302c7be566deb7752bb105da4c328eb1ca2883
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:12:09 +0000

    mess practical guess pause bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
