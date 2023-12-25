503733fda21443b5bb7e8496524e59c418875254
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:07 +0000

    reaction approval invitation unkind steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
