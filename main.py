78fa9ffc0180fc1ac294fab16acc1a3f816c41d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:09:54 +0000

    sleep soil scrap scrape the

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
