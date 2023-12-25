a921a43559771453e9403b6cbf77f01a83c19775
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:11 +0000

    left man weak dream sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
