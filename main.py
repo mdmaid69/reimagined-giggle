332e1af72b98ec1a4d3359a10e0ca049f604a6f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:25 +0000

    high stupid raise blind damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
