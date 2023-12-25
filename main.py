99e72b1b957fdc5bb3b20ae9abdec6501cd18cbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:57:41 +0000

    scared best keepaway control arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
