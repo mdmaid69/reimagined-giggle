98017b84623c82360835faafcc5fddbcc697bdb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:52 +0000

    bet blind crisis lookafter exam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
