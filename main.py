db20bcae610b56de8fa7d887b7a48b70730e7c1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:43 +0000

    check obvious damage shine sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
