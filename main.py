58007a62c05fb3057a71c4b143bf775e36048605
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:44 +0000

    sea wash file exchange change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
