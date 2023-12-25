0993bddadcc09e3c96c18920eb97f602050fc6aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:36 +0000

    goback(to) proof trousers coffee range

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
