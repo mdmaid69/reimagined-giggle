7097899f3f650e085a3a09e17f2efebf4866558b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:19:05 +0000

    poison wherever tune company weak

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
