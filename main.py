22cbf402a96215ef3305d54fdab4d438536e5139
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:30 +0000

    generally smell welcome repair embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
