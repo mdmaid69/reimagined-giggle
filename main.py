f8336b877fdde318738dd3e298e77db65307659b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:05:22 +0000

    goodbye ilovedicks goup do region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
