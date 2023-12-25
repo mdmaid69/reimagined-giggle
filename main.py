3d780ea70a1ca9b48e9d60cffa60329ef27431f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:03:48 +0000

    relation Cc face that ocean

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
