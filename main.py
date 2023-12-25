a4bcbf79953f0f0d19bdd4ae089b74499394d9c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:57:17 +0000

    custom provide human mark bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def reverse_list(lst):
-        return lst[::-1]
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
