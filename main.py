cd0b6a2830f3b870c3fdd80b850f0ff566cea408
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:01 +0000

    easy distance water tradition nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
