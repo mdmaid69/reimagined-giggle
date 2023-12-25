78f628a3a39cf197910ee6fb3dce06bb9265d4f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:26 +0000

    shut firm crash hear Ee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
