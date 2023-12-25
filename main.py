18801d2d73cd1065b3e0cfcab7030369da7ac967
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:36 +0000

    certain judge mother dance safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def is_odd(n):
+        return n % 2 != 0
