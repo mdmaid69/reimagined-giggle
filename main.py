909f774539a104c35d797a2c8ea35b0b469d78ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:43 +0000

    my main ordinary slide blue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
