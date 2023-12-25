91b0cc1cf88f08f19285b6723d591e7dda5ecb9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:46:50 +0000

    hers community post doubt tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_length(array):
+        return len(array)
