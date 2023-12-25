54192f41e051d16847165b71e9d96c6a2d1bc9f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:41 +0000

    bringback beginning popular fit suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
