9ee6acf28747ae53112caa431b39b27b7ef1c71b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:30 +0000

    search guide criticize crisis prove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_queue():
-        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
