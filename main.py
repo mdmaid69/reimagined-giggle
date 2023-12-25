c266cbda7b574822f8cf9696e9c07f39ded06c69
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:53 +0000

    bottom truth ago satisfactory listenfor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
