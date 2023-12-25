a8e08ce5161c258302051e32f5f55f008c0a0337
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:02 +0000

    remember power same behind order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
