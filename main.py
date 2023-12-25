97475541cec035c6d6e3f92784db015caa7447b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:17 +0000

    runinto soft dark getup line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
