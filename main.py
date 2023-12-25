5dec81112acd740025d3e1f4f3d898b53b36105f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:29:39 +0000

    keen talkback calm respect aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
