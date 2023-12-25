df9d5a3ead943c12ef69f74de649f56915f18abe
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:40 +0000

    content on keepoff habit whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
+import sys
+def exit_program():
+        sys.exit()
