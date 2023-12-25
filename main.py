413124f83d43df1461e5aaa4ed072b3e81dd5bf7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:25 +0000

    yourself introduction skin matter period

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def reverse_string(s):
+        return s[::-1]
