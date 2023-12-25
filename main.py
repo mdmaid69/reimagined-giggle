eb643b1e95e2f7eaf3d5325b33c9cc13fa7dec19
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:38:53 +0000

    safety serious generous report copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_power(work, time):
+        return work / time
