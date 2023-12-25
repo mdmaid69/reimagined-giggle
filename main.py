b1ef806c6dbbb149d54f20f669171fa3854cc283
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:37 +0000

    inside creature report new wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_str(array):
+        return str(array)
+  def convert_to_binary(n):
+        return bin(n)
