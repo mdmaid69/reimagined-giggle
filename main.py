61371dbf83b60d32b382a10d767d7b81f06b5665
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:19 +0000

    available punishment makeup give instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
