80341201786673b8b22b67d2a2e91eaf6bc74bf1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:07 +0000

    how smooth lost talk field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
