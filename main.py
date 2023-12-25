48dff4e960909d72de602ff12c60313da6813322
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:21 +0000

    most emphasis fault noone winter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def is_odd(n):
+        return n % 2 != 0
