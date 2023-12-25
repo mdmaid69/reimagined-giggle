4edbb5105154b21d7134dac4d9192845f12cabf6
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:22 +0000

    answer succeed lay beautiful subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
