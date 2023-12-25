56edb99ce9b5d30354340f56c04d87007061eeef
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:48:57 +0000

    funny alone myself herself time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
