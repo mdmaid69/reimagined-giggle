a8ac80ff6b1c7f1eed7655b47014de4f5c72f07e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:51:10 +0000

    collection ordinary stiff fear callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
