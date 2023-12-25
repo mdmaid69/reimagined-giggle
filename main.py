d77cd3e3e406997fbfbac5b6d3224c10047ca04f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:12 +0000

    equal handle comeup during arrive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
