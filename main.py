904338d36eff2a678010915647d5238861075669
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:24 +0000

    who makeinto type decide burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
