ff9b3176b6d72b7fb7a4a5afabe292fdd0cfdc21
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:42 +0000

    squash plenty passenger patient hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_even(n):
+        return n % 2 == 0
