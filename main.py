74cb081f5230730c0f129530dc8a32ece7a0868d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:49:27 +0000

    emphasize wrap 566end exact level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
