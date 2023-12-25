b79dc0da6b1465435ca98c1b0edc2dc42f2d63ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:11:38 +0000

    snow watch sell clear platform

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+print([x**2 for x in range(10)])
