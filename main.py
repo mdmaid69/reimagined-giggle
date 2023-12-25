01e53c45e911d07907a23ab7f570e16f88b55e3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:05 +0000

    Vv trick children grateful company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
