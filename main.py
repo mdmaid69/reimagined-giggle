3fa9e3a7b2e00457db930d8ab61b62ac6e42ecd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:04:07 +0000

    pure consider favourite protection opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
