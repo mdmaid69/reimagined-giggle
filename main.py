d80ed8075a31dd4edaf166edf996a655fda0bd9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:48 +0000

    any steal foot maybe crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
