66d6f7e36f1546da290c04b4fca1c80bbcf78979
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:18:04 +0000

    connected pipe record bridge negative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
