575b4bb5aa3038666bef8bc33e1b3b656ecaa6d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:24 +0000

    opportunity communication bringdown traffic acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
