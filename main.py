ac501b0dcf75b584866347b3adc7ecc4e84b7df0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:41 +0000

    breakup seem spite organize ours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
