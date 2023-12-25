5aa18a2cd4167bcae123173f4d00037d270ccc26
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:21:38 +0000

    criticism suffer cause used frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_as_tuple(array):
+        return tuple(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
