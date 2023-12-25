8d096ceb2053b009aab147bc8573f5f3afa3d35b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:03 +0000

    swallow our spite friendly what

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
