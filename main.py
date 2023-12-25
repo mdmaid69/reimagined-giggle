88989e7e24bffc62b7ecd9a2bf0ec8b6216789f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:42 +0000

    suckme Ww easy immediate gate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
