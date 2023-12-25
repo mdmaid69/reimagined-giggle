5795e277cf628db7ab467db1b8d0a62cf8d1ba4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:51 +0000

    classroom curious prepare attach example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
