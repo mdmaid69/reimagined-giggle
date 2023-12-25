e1459671e152b395e626eba0f7faa0e00e5e7e74
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:04 +0000

    fair position top form flavour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
