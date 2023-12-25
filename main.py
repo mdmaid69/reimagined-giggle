a34bd789db680a7d64eeb08083afe372956ec7ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:45 +0000

    avoid neck hospital swing second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
