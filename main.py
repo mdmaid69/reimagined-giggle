da31c04e541e209a31e31f95ca6fae880a077b43
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:05 +0000

    hour draw acceptable common turnup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
