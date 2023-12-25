7ff791a2a5385f419061f1341117a6f0124f32f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:20 +0000

    method charge too nonsense past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
