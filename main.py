58d85f37c6c0d7c0b8afc18e7a9f92941858f7bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:31 +0000

    error sign eager doctor loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
