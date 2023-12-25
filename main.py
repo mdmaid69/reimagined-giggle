c56c593f0e554d03b4f5c764ef8e1e99e17da67c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:56:28 +0000

    offend save risk deliberately area

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(numbers):
+        return max(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
