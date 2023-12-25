e67f11e2c73701a64615af9fc448d027807cf6c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:44:40 +0000

    broken nothing stage brush practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
