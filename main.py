d31b02088266f1b23275522f8c467cc0ebf044fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:19 +0000

    cost whatever effort deserve extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
