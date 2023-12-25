f0e94c6ec6f8b90a63138e05b0ce4a09f2562809
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:21 +0000

    forever combination pure neck tradition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
