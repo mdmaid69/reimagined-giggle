f2cd7b5006838aff8b12a57dcf7c1ed6466cb8c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:13 +0000

    plenty defeat famous need look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
