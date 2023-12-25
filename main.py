bff768b7cb7e232f9dfac3e78eb7dc151a8c4e1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:21 +0000

    swing apart Mm wash you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def divide_numbers(x, y):
+        return x / y
