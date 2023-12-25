e37b330a260ad8e7fb1a81dce2cfeea239d8a5dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:47:24 +0000

    true promise another past drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
