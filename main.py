ce6a61064e12ed9625fe27ec81fedc418a7e9896
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:01:06 +0000

    share point exist free instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
