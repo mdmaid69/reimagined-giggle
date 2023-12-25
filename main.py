cce4f57ebfca8e9826071a996c1538bbe03c2025
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:24 +0000

    makeinto block earth bit great

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
