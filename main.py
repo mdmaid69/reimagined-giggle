b945f2dc14611402a8a54e35c03719edac5c1188
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:17 +0000

    settle music count or deliberately

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import random
+def roll_die():
+        return random.randint(1, 6)
