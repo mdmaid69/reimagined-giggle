d6ea573395500edd996d28080252dca8f3110614
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:49:50 +0000

    understand lost wish generally recent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_binary(n):
+        return bin(n)
