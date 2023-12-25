4329853818846444e2a7dced11775c6beadee497
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:48 +0000

    so authority welcome proper everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
