dac22e8fb1dd1e02bb2223929d1f440342ea193b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:30 +0000

    surprise polite argue each other

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
