480a4ba3669c56fcd6aabbb6ccc14ac72822c88b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:57:47 +0000

    callup doubt friendly season around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_current_directory():
+        return os.getcwd()
