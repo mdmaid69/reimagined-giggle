3f7492083853ae02699ba25c039a1766be197e81
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:05:49 +0000

    send lock fast name another

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
