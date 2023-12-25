ae3caea627b0a71047994690edf7268e77a868ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:24 +0000

    see plain branch not beginning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_acceleration(speed, time):
+        return speed / time
