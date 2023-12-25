c5dcb0c6c872aa7d927d7b748cd2e14e38638a6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:57:39 +0000

    mind comeback rain later enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
