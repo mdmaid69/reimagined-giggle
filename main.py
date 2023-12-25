1882037e9419864777ae8051788364657a64286f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:56 +0000

    week prize calloff their see

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
