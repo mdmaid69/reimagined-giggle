84230d323c55db72406a4082ea553c95452397df
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:18 +0000

    lookaround passage like common develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
