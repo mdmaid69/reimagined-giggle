960102d9d8c717760b9a4b0a08fe5035b18feb3c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:26 +0000

    complete suffer impossible miss any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
