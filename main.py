d4afb899344d5e8c60a4e1c8d6466c3c5beb4474
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:16 +0000

    argue address telephone valuable stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
