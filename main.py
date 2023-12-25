c729780b1bc96e190c51aaecf388d1aaf61df56c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:43:54 +0000

    idea lend another suddenly shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
