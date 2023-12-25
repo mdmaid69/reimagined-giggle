3e100da6f21e2c13b079a50118fb087417db9cb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:50 +0000

    city 565end interruption race imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
