b44294c9d1f9ac7f8c78759e443df14a19ea0c43
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:45 +0000

    ashamed organize active return argue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_circle(r):
+        return 3.14 * r**2
