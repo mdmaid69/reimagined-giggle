00622c234b03509dee8cf5a2f95f3abc8dc9d47e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:50 +0000

    flood freeze exploration far previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
