790cb85ea43bd3f9dfe1d216c60d19d7eebd6812
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:08 +0000

    flight pause relationship size Rr

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_typecode(array):
-        return array.typecode
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_list_from_array(array):
+        return array.tolist()
