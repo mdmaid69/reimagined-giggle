e6a47b64ad0d0e4697f33a5ddf738695d9c02e9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:26:20 +0000

    lot callup transport expect lock

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
