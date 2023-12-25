8a57637f295997a32b4d36643ba46bb49c375e9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:38:13 +0000

    prize unfortunate like defeat while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
