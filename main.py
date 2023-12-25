5d12bf068209be9105bed19a32296b3ebb819be6
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:20:17 +0000

    attention about consist off base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
