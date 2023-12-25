b14bf55cfab38701f1004ace397fde969aa4dd76
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:24 +0000

    street term already supply perform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print(sum(range(10)))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
