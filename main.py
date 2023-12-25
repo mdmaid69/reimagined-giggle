0a936a8094b8f3222db805173a4e30eca973fb87
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:23 +0000

    instant reduce under quiet wall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
