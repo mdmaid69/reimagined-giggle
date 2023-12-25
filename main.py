7114394703b51a2c3503fbaa49fa8f45167b32d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:08:13 +0000

    success wing vote bush Uu

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_power(work, time):
-        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
