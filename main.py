49a3960f6acc749db1b4efe01476d0d9c4c817af
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:14 +0000

    basically could narrow sleep 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
