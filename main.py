225912fce80a9ff997a5bb3498722786eee5c3e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:21 +0000

    convincing pay combination plastic appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        return sys.version
