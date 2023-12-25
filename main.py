42b2fc60cd7905b5422440765be84c65514e929b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:40 +0000

    married difference scrape probably clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
