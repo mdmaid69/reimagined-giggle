a80c6dfe43a5a0ae9ba6daa65b113e528fcad9b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:17 +0000

    exhibition beside project talkback lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
