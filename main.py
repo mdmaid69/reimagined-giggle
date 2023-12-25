a6ae94e8cd388bffbe4404d7eba1be722412210e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:53 +0000

    hers snow right still it

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
