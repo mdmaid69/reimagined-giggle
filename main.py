9a11341c6d885ad48a75c0abb719e2d315f6a88b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:05 +0000

    teach your 565end live keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
