ddf3117d39b8d472b5f76a2cfc20cf9c60c87e31
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:26:56 +0000

    over fence improvement determined blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
