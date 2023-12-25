dc791f43a9265056dd9b86b8c21d65047e823d81
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:16 +0000

    stiff desperate once fly flat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_typecode(array):
+        return array.typecode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
