0c870373480d4152a61326a2f867323c10a43562
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:45 +0000

    maybe laugh want within drop

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
