d344728062017575b0d5f682f78087241f51a3bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:10 +0000

    ill punish morning borrow support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
