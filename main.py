6b5f21d1e505c66b4b9013ed2d6717612876fdb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:57 +0000

    frequent pretty file wish then

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
