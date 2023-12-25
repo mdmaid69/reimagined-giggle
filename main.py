d4bee0dec2587657e28154b123fd7a007a26d8a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:16 +0000

    include accident calloff life cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
