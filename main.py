4f84f0613b93c632e5de0230677911114803df6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:03:56 +0000

    greeting runout female destroy apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
