1d733ead831abf832af69c5c666a3e276eaad22b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:12:15 +0000

    never lesson confusing knowledge sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def is_odd(n):
+        return n % 2 != 0
+import time
+print(time.time())
