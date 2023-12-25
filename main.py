518966ef3fb99c927f47de692c0e70ab3ad160c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:49:45 +0000

    twist long board change arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
