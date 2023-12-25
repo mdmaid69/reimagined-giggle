67fe08cb991f45a6c2499c165265b2ef48d27b48
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:17:14 +0000

    arrangement habit makeinto foreign embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
