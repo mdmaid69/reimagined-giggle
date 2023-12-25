5b49f130f223fe8bedea1f6875afa46232ad7e30
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:43 +0000

    body Gg fat suffer sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
