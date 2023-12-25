e9117a993ea2162c2b35a0a72591c02ba4bd8375
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:13 +0000

    Uu minimum accept say act

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
