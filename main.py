ea17a960d4de936c4a8fbb1b17f5099dea9f11ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:23 +0000

    careless information choose runout control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
