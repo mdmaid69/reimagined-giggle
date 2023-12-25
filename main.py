6540f88e6c83e5ee9bc9f40ded715b8fe31b87b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:21 +0000

    equal stop speak behaviour comefrom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
