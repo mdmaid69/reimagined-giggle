17e5a36c3f08b4b7f6aa7ec5c627fb824fba649f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:15 +0000

    direct responsible anything himself buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def convert_to_hex(n):
+        return hex(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
