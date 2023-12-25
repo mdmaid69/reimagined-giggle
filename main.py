75101af62a8204d1431bc3d81253ac104042c508
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:11:49 +0000

    above in away thorough please

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
