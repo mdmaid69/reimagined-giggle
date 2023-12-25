9ca75152cb6f234824202fb069f8c104e1971715
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:52 +0000

    will our every minimum talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
