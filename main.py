46421253edaba0c5599d5af0979853d6c473e108
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:56 +0000

    number convinced chat godown about

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-import time
-print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
