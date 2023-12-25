94a1871c236b53d689b82f0aabb84ce95ea73ea5
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:14:22 +0000

    sign load child school statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
