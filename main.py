49a19efc85659a2bf2df02c7cc4f5569b76472b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:58:44 +0000

    join choose list luck week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
