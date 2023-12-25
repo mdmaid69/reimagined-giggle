9b1ea4199aa7430021f974f67b848a27f0378dd3
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:06:58 +0000

    drink shop development gift opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-print(sys.version)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_max(lst):
+        return max(lst)
