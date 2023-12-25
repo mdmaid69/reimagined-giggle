231cd88d41074f301ded9ba3c47c3c2b352db32a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:41 +0000

    test difficulty basically spring ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
