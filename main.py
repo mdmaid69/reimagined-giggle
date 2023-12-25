71b3b007c42fca7b0812ab1d25f185c5a20ec55c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:29:33 +0000

    together who level 573entertainment godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
