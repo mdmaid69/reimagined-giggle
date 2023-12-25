54719c7e3f176bf4e0c5a63b43b783cea79acd21
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:25:30 +0000

    disagree muscle takeoff careless return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
