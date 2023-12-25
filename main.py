62c1b45979f3b025ddb17714161378bd73117424
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:23 +0000

    wear contrast related sting rescue

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
