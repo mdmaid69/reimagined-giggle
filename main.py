ffe564f24bf87c133925cf5bccbc41b6ec3056df
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:08 +0000

    change strike forever clear before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
