70e23376baf78086635aface0ad1b338cf8ca2f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:38:52 +0000

    shame neither would herself shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
