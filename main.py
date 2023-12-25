e1fe565dba7c77ce46ce7c71f076607456ce57d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:05 +0000

    base hot popular somebody doubt

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
