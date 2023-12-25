be7ed19862ded777323e35090435ce57dab91598
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:34:28 +0000

    winter runaway breakup by capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item(array, i):
-        return array[i]
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
