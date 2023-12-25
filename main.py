030429d26c26ac87a0a4e8e477732d16a7366d29
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:40:50 +0000

    division reach length Ff damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
