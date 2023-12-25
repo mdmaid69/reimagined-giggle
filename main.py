40a792a754e43621f0d78a6d5c79c07df7a0c5b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:19 +0000

    pencil miss push measure possible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
