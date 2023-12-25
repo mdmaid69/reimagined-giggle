472f9de618cbf9a9c752086e8dfbc3456a76bd12
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:15:55 +0000

    prove tonight round improve day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
