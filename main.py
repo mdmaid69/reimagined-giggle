95eea2d3646945007257db28c20d41d8af124fc6
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:06:01 +0000

    safety original arrival test case

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
