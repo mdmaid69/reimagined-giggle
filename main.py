8780a1d8a4cf35f92fcccb160ed8003e9e9d27ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:49:00 +0000

    ticket food guess underneath type

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
