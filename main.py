14e14c433d26805cf1b8c8b670f5c13c9259a321
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:15:47 +0000

    pinch concentrate guard needle house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
