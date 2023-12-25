cee5528436460dd96cf2ca6d3afb7c273e3455a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:26:56 +0000

    score yours low number command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
