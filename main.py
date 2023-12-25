0d810eb43386f3eeeeeacdbea26663d651b47359
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:18 +0000

    disagree imagination typical ill stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
