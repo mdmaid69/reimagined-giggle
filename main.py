3bf4d4524e8b76a7fc5612053e99943eeced1a57
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:20 +0000

    band sick listenfor quit false

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def exit_program():
+        sys.exit()
