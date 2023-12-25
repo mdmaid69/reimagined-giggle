1b6773122c1048cb906086c412ff63e1ff14d578
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:36 +0000

    giveout compare 179bite cheek quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
