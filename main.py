fa1e9d792009bf3a388806d39cb0279b0c731893
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:42 +0000

    determined skirt sorry once pool

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def multiply_numbers(x, y):
+        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
