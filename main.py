c1e88766851dcf6e0a691b1fa74cbe342a16bfcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:25:18 +0000

    slip ignore force afternoon dig

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_repr(array):
+        return repr(array)
