fb9622adf8107d655c6bedeb2615b14e8a28c537
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:50 +0000

    empty live fix easy before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
