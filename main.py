2a236c03ea0496e385adb5ca30b0ef24fc1b6d32
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:48 +0000

    leg present entry variety divide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_pressure(force, area):
+        return force / area
