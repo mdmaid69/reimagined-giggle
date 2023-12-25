acf1d47be45afd1f475eae590e6dc4fcea38c59b
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:40:50 +0000

    software fear bringback principle fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
