fa3929432138e51edf16673f21e5fe73e3971bf2
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:12 +0000

    total medium right treat Oo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
