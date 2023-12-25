086f60b329827a98db42ffa1baf4931eb2b1a1e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:32 +0000

    breakout tidy ashamed money fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_binary(n):
-        return bin(n)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
