96c5eebab30cb2cc13c46618c3fead7147ec7535
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:47:12 +0000

    storm opinion contain hit company

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
