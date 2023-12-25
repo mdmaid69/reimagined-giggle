683be07ee544a4cbe4b06634c07f137a7c5688ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:47 +0000

    holdon change check extraordinary ground

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
