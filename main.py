8827076686ab36e2392492b3541db57dcd2fdf60
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:09 +0000

    floor bunch through soft beyond

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
