dc009488af75712db1c7dc072e8d5ed8d7697df4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:08:22 +0000

    swing circle goodbye between narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_set(array):
+        return set(array)
