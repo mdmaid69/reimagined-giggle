b90b8fa1a725d9363fb1fee3be2f8383d93405ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:14:22 +0000

    record well let watch success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
