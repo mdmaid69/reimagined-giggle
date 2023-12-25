226bb722ab22c5a156633222f22bb0b80044bd44
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:10:23 +0000

    rather consult usual Ww art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
