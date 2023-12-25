a89fde83c3e6de5b6b75dfa5f99e046bfd63836a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:40 +0000

    goback(to) exciting cope Bb desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
