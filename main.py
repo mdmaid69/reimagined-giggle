d3fb8b4a1603b4ba33a928c1b567e9be5cf05b12
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:51 +0000

    we popular tax mother convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item_count(array, item):
+        return array.count(item)
