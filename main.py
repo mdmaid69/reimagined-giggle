764832e2c5ed0a352717283164e6cf2612deb081
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:30 +0000

    bring Uu giveback air imagination

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import math
 def calculate_exponential(x):
         return math.exp(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
