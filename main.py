65d90adaa1371f26bbae0957031654272f2acac5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:27:42 +0000

    likely delight light simple pleasure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import sys
+def print_python_version():
+        return sys.version
