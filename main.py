7437fd891bba8787743f42fa5e1120d19245aa01
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:55:25 +0000

    song tap count direct quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_int(array):
+        return int(array[0])
