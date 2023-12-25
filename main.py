c536978c74ca07a351968115f0f3b4cd83484987
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:04 +0000

    impressive rain brief announce ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def append_to_array(array, item):
+        array.append(item)
