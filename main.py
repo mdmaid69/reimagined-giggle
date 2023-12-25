bb79043a05f3c0a3026a9b996fbfd30e5b435026
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:30 +0000

    circumstance task shelter naturally way

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
