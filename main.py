a054b2b6e6080b33ec7144b0b2b1fa9fe550e218
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:45 +0000

    command row rid convenient unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
