32fc067b7b83e4a1190e9b52a841541b625139c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:22 +0000

    quit refuse act bottom rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
