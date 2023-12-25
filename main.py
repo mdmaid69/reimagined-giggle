1b15178f7ee2ebe374aad73d352f622517456a78
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:44:12 +0000

    correct run although means note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
