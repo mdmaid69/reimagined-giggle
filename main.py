a3f0add31fb0f75f23830b30a9668b2faa022efb
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:48:04 +0000

    his terrible suppose rid cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def count_characters(sentence):
-        return len(sentence)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
