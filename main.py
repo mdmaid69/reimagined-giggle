da2c8e1b2753574ff0ab28e609f80f29140d5a90
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:32:30 +0000

    immediate view build miss block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
