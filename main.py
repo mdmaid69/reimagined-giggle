2ba4b1ea215ff4ea37fdde88b76f45faf9ce5edd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:24:40 +0000

    to talk active tradition fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
