2e5484844e061ec0713dc34bc3000caead790dc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:52 +0000

    mess forever ear reduction treat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
