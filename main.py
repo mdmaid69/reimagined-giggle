6e9c4865ca566cc2b2f9400b92f2228c9befefd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:37 +0000

    deliberate act occasional judge practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import collections
+def create_user_list():
+        return collections.UserList()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
