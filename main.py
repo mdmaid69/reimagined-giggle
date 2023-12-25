43f912c1d70f5503b8db26a898e8fc262b464e2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:36 +0000

    firm close acceptable board smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
