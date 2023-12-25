c252c76d28f717b4d7b499b366288682584e4d7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:30:46 +0000

    arrange below turn whoever money

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
