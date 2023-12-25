890748d9fd4716880da7c075e280795e1f81d7b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:21 +0000

    may matter pay geton mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-print(datetime.datetime.now())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
