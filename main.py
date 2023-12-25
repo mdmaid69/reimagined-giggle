837ce8d1153284059ca59dbc4a96b610ca294dbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:18:58 +0000

    party direction keep suit talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
