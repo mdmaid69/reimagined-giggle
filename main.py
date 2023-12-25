a59daae65015df70c6725812024c226f5d51a421
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:39:26 +0000

    cinema level nonsense sign lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
