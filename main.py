0309038abdb537d9995441fe27a58f0043d3bef2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:33:43 +0000

    entry undo cap swimming without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
