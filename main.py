04e84a8ad4b1e8612788220a20ef946efedd1024
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:18:32 +0000

    concern sport nothing show wheel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def convert_to_binary(n):
+        return bin(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
