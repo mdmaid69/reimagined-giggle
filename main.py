b548bffe0afa870010f1865ffa65b04d23728b42
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:20:02 +0000

    estimate know improvement country basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
