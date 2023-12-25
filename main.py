fb504e0d1fa7c03988799fe3891790b55319e34d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:32 +0000

    fit interrupt bet through claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def reverse_list(lst):
+        return lst[::-1]
