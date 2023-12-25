a1f4a13ffc595da965730018e09fb57c850344f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:29:00 +0000

    gas platform doubt ability mind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
