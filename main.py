3d151e0f7baf4c436c228e9f2695742381d0106d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:44 +0000

    clean little track light have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
