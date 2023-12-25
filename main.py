1244769d5e4ae8445c3ed486fd2e41963cfef78a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:28 +0000

    quickly comein position mother visit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def check_if_array_contains_item(array, item):
+        return item in array
