397489c3c23e18b2e44bb01ee84cea64a95a9af3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:18 +0000

    wish food lie start organization

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
