b2f15466767e1f12e866fa4e12475aef725adece
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:45 +0000

    complete more knock label bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
