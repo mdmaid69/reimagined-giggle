d900c5d1eb3f656c727bcc5f1de950831bbe854a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:15:48 +0000

    joke affect unkind anxious mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
