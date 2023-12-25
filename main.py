b8a84bf0c3aad546b3ec8f77fa1f01693c0b094d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:42 +0000

    pupil can hit old oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
