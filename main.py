9a0ba27df855b17d4ac12e2a8593346c462df1f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:35:56 +0000

    already once abroad cruelty thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
