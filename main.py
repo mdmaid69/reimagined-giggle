582cf919de7a1f41b03d5bc6ac242e85516176b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:29:15 +0000

    add morning holdon message comeback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
