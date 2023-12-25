57992de5440ea971e6ce9da9f65f084f20a59486
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:56:02 +0000

    club afraid real guess dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
