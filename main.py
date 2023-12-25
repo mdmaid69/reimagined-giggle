23de17c8eb60616a0f075f3bcb36ebe6da6b5ca2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:04 +0000

    occur history stamp path content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def subtract_numbers(x, y):
+        return x - y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
