cab901db398bf24bd023423870c4a47b3bc46b02
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:57:26 +0000

    no mark level fence considering

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
