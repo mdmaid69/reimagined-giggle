928255fbe7257788a773dbea7137c4d254b88dfe
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:37 +0000

    tone breath middle fasten shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def divide_numbers(x, y):
+        return x / y
