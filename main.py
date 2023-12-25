71a373aeca273d41031eaa0925cd7c467ff21d44
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:53 +0000

    please house frame noone brave

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
