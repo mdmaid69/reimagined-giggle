1e942e9e889120663863457345fcc96252013745
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:35 +0000

    vanish pin usual result any

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
