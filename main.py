28ac8e3d9f20d4ae67c8e92ad79652650c518e77
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:48 +0000

    organization middle explain body score

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
