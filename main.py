53942f1d39bce639caa4b11691be3d153983a0ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:26 +0000

    quickly length that defeat flow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
