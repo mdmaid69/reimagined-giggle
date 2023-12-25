6f36839a8f62865a0e3aad2254486239411ed4f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:40:12 +0000

    sense mystery however with short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
