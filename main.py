aa9fa58e5151d8e89c83c92f7eb487225d299a26
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:09 +0000

    drop collect return all boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
