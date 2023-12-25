c8678565f9bcd05ad6b1380e4ce28b4fd03cc771
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:57 +0000

    divide take system fall forward

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
