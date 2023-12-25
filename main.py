4d6c9e11d344f18341df84c502ba70b4624dee58
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:24 +0000

    under track case cum inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
