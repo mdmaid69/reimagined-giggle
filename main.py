f794f3ac8b59975e7ad917acd5e93ae3bae7f888
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:39 +0000

    possible influence purpose hospital march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def cube_number(x):
+        return x**3
