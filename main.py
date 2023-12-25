ab4115c0a5ab7f68197be8d30fed0bcb241570de
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:57:02 +0000

    us close record shut everyone

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
