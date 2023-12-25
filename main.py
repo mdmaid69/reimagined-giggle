39099c839525d003dbb5621bc8abd4832c40b3af
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:19 +0000

    face roof disgusting science broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
