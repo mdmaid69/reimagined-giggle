ee166dd4c8053aed4f66019c9b6054771182813d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:12:08 +0000

    slide combine bright tip copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
