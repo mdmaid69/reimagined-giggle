b4c28bec3c1ed13bf3b31b39b394975a55d9a733
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:45 +0000

    surprise stage paper sign themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
