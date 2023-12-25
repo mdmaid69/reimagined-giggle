fcd39d7ff8140a414452cfd713f07eb1dc516055
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:58:35 +0000

    ready criticize unit negative whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
