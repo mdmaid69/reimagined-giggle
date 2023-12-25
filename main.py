87332d7b0168ddfb11dbf121ce1c9c9ae76201c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:11 +0000

    convince record plan along walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
