cacddcdd7ca1017f9335cca9f1cdf9a2a8bb01eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:18:32 +0000

    science pay rule wherever upstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
