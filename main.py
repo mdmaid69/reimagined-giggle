773c7ce9772ebaa89f635bc363951de1b4d2ec62
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:55 +0000

    scene when way judge interrupt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
