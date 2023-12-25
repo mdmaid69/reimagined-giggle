2f5a53c571fb1abc21f040ba533b793f51770ec0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:43 +0000

    sell Kk dish mind chief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
