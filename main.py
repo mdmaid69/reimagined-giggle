d71b66fd4f325f352eedbd2cbc7b7aaf9385a68f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:41 +0000

    seem ready record breakout wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def divide_numbers(x, y):
+        return x / y
