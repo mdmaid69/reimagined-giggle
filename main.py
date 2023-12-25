1351e4254738593b75563f8a44fd29fa02010483
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:05:37 +0000

    interview place shake children please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
