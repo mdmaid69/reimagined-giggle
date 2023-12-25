cddc2bd98f3e106391226e165b5d887973f3652c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:43 +0000

    house calloff tread various means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
