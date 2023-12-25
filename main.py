b8d0c8ac29d04421bfe68c8bcb6ad0bf381dd73f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:13:20 +0000

    legal lot sink whoever they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
