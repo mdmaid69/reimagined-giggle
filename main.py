67e5dfce71733e059dc73f1083d4bac4612227f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:49:51 +0000

    talk either classroom suggestion still

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
