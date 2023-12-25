95db579b4c7b6102eef28008159900a3bb6b2062
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:53:03 +0000

    because temperature cost plenty face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
