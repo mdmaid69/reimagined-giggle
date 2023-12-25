e87aa42e6d4f8732f90c760bfb4be93b14995da8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:04:04 +0000

    yours action exact several consist

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_time_since_epoch():
+        return time.time()
