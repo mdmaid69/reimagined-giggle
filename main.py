01fd7534a6615c6d4bc2f82cb754da71a5d22302
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:50:09 +0000

    obvious worry recommend cross not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+name = "Python"
+print("Hello,", name)
