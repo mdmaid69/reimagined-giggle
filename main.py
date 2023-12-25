6e74e5ca5f3125ffe5b30d0399d1544518cca71a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:26:12 +0000

    skin few disturb nice on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
