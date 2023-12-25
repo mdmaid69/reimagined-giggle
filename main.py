fa25d43faeeedda09f00dca4fee5e04344940e95
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:26:44 +0000

    way window notice variety sensitive

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
