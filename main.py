887487a167bb510931e05cde7df1f7645389cf61
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:19 +0000

    tax punishment repair broken land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
