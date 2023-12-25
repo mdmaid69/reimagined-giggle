fc01eeb3257b03c4d4fe58b9750d68c739e172d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:17 +0000

    now software stretch exam walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
