05016a90374a762d943c15a002e114d1e42c9d2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:13:21 +0000

    fall divide before message urgent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
