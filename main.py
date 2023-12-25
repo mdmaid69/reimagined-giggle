1c410705d2949111b70dd1a0e29355f0c66b074a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:59 +0000

    guide see seat themselves control

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bool(array):
+        return bool(array)
