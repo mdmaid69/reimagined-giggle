f28a701d2a172171dfcc7eda70728612391c6863
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:06 +0000

    future beautiful sit factory organize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
