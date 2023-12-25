e0612ae41d1524b04906319a5c13c3843fc20f4f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:21:42 +0000

    exploration escape wise instead mix

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
