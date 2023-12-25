39a7fdbb8754321ad648d267548e8bb78d8068e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:10 +0000

    slight proper expect tidy check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
