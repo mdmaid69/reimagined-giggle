cc04dd006fc302a0ed20b7316e60a0ec4fa58e44
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:55 +0000

    defend needle door pain march

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+for i in range(10): print(i)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
