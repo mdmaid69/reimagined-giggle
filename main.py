807a4049511dac987014b19227e47fd389cd4d7c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:51:44 +0000

    gift exam set workupto shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
