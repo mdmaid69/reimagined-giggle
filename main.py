110fb067db7a322c2ce2620554886229cf53d3d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:42:49 +0000

    effort minor common desperate laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
