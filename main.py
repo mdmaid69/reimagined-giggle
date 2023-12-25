8cbe4ef6bb28fab71bdeaa2dd74c2ea67769d713
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:59 +0000

    protest black message supply black

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
