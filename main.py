e8374774b223a5cad29922de73efb2574cdc78c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:54 +0000

    deep switch reject not silent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
