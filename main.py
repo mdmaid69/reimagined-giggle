6d3c8f2ef61a4f522a58d6ac3d34f4a3be07493e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:30 +0000

    risk content concentrate rise section

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
