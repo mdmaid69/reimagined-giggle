020ea821fca4add208c13c47ec435e856c21f7c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:14:15 +0000

    mind run post means pretty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
