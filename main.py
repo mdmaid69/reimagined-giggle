5f0b055df3d9051ff22d772728b5b7f57a7ff934
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:35 +0000

    admit plan complete so sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_max(lst):
+        return max(lst)
