c6a024acd6aa8477cdbf91552fa527ce55b7e352
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:17 +0000

    various confusing loss poison accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
