f983b2d81122c8f6c6dbc13d7137a1405fbca4a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:08 +0000

    foreign completely new off stand

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
