78b7133b65b606a321d86582036a39952e5b002f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:39:24 +0000

    promise difficult because drive task

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import datetime
+print(datetime.datetime.now())
+import platform
+def get_python_version():
+        return platform.python_version()
