d4d13897c356db5d6e776560a89e3bfe273b4b96
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:12 +0000

    Ii good damage suit underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
