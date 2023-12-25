9c58b942f037a75946892f09c8def32a1a85ca85
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:37:41 +0000

    future comeround friend subject custom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
