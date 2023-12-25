c739362977fd9b64173866a819334ebf1792be96
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:14 +0000

    OKorokay steep alone capital drop

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
