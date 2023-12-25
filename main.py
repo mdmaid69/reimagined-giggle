ab4a7d6afcabe1beb8269b4aafc785cba00011d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:48 +0000

    bang lie wing continue tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
