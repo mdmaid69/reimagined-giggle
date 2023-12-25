57cb7425ccf623bad95a692d2e8fad73226af8f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:55:05 +0000

    shape beside breakdown trip turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
