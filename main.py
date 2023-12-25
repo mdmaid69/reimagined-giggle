b9f5b909bdf960c86fb06372a7453ae7b9da6462
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:21 +0000

    winter failure organize play which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
