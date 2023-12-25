ce6d0f5cb015764d3516df9e49f91e65a260394e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:02:15 +0000

    subject basic fault taste uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
