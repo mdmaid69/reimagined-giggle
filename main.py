8a6973cf6a5ce6bea76a37bd39dbd7e5d1f1c63e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:26 +0000

    regret black weigh film Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
