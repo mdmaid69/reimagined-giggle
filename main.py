3149b18454189a68e55705755b56b57fb6eaeed3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:07:51 +0000

    explanation generally attach drink plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
