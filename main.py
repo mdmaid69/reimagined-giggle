07f8db640e6297821169cd2f2089de8ea9ec8b7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:10 +0000

    patience zero concentrate surface tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
