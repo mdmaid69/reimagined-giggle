44be2582f218cd632c94d81d5b179509024c0fba
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:11 +0000

    dream hair your minimum order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
