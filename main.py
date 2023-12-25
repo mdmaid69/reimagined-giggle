1c068a6c07f7f5b0e926996b8675f1c73e833349
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:19:23 +0000

    act example ask sink Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
