5adf2159ce0a2ed83409e899e46f3574c1269889
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:35:21 +0000

    Bb star definite callat drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
