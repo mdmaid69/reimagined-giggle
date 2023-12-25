a323e888e650439000fa95ce74f72366859cf41f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:10:58 +0000

    harm available meeting feel include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
