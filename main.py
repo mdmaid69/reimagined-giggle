8b8204adfb56b6e6f7957a22feaa079ebf278349
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:20 +0000

    regret include finger obtain under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
