a0fba8b28880df7dde1fbe52bd9e103a23ae6745
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:45 +0000

    protection rush least mean pupil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def square_number(x):
+        return x**2
