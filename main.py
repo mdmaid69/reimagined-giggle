fc3f9a36cf80148a9ef736d236a60f498ee98f1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:48:02 +0000

    lie who please prepare actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
