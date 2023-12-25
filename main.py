ac54ad2f79dad02aafe2bb164ff2032f5979287c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:10 +0000

    forget sale proof power trick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
