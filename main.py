7d2a92379c7d0cc4dc18d48136e3ab9945557169
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:20 +0000

    please must tape power pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
