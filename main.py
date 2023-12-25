65b238125e302bd7698d3bb850f084941cab09b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:57 +0000

    murder recovery heart tread someone

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
