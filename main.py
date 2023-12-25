6e592f07a9e05df145e8d44e14513483c3c9769b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:33:31 +0000

    hair Ff situation comfortable loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def reverse_array(array):
+        array.reverse()
+def sort_list(lst):
+        return sorted(lst)
