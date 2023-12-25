bd6340b37df2b25186ac793821e28949c4ffdbb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:12 +0000

    happy petrol wall breakfast eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
