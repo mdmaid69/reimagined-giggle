6388359a69e7298ef21f18833c0253d3d867763c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:04 +0000

    fence print interested free world

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def is_palindrome(s):
+        return s == s[::-1]
+  def add_numbers(x, y):
+        return x + y
