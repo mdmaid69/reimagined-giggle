ff62958415b0fb8cb334bd283f969dc4580dc733
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:32:54 +0000

    heat joke pin improvement each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
