8e18ed0a8d652e560dbcfcc615de7831867cd860
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:27 +0000

    nerve play exciting the repair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(10): print(i)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
