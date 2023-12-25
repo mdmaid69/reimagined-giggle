2bfd05150444c8e1ee3319d15383ba82921d7d07
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:19 +0000

    burn callup straight who principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
