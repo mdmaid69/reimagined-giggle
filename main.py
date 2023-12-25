08013ebfefa0f0e8adccb2b30dd05b6d08616694
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:37:05 +0000

    give test meanwhile change split

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import numpy as np
-print(np.array([1, 2, 3]))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
