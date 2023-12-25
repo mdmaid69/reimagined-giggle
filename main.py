84f02c86554bfbfbf93e9e94f55a475e7f476859
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:04 +0000

    set rest thinkthrough surface loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
