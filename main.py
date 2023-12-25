f07ccf0dfecc9f3e7af90b2784524a3f604b36c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:51 +0000

    live rather mouth convincing return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
