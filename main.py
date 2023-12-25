babe7eac31accaabe535bbed6bc0f4807e1567bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:02 +0000

    copy vanish classroom software strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import platform
+def get_os_info():
+        return platform.uname()
