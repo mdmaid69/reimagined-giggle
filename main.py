ea495090abbbca9954e1cd5a9bfe967f94a8a32d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:49 +0000

    freeze building discover discuss 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+for i in range(10): print(i)
