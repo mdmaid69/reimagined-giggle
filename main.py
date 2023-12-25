51c2d40d0869f6b792ba4324fe099764a97d2262
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:49 +0000

    bottom emphasis lend speak finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
