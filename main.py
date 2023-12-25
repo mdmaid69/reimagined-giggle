1fa3db899b749f8f17bb0feccdd924638f72f1ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:54 +0000

    chase proud Bb few Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
