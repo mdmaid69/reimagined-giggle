11821352cbb2d33536abd63970defedb02db91ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:56 +0000

    Ww storm side Gg hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
