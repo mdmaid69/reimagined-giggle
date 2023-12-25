d5cf31913ef621ec689f38ec9452142d9c4928ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:06:52 +0000

    loose basic patient decision bin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
