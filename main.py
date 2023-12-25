a7837bf0f57a160dc116e0361ff8fcd41d891fd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:22:40 +0000

    large finally situation 565end place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
