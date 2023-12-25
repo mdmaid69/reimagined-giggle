bc0764e3688cb1804570019c906598edc63b02e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:58:24 +0000

    succeed pleasant used flow as

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
