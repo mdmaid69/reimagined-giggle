eec4969a2594a073eedd2f3b48cd2790b8b1009b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:51 +0000

    themselves free above famous upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
