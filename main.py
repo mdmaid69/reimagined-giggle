a401af184e22e2cb1d7993d4d7acdceb240dd4d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:44 +0000

    safe material first inside completely

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
