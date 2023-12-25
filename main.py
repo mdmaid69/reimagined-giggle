8dab1cd1c2b82ef13bb5976315a29948ae86b3e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:30:53 +0000

    flight music guard past fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
