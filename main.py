e3d22daeb8ae69c8df6f780bf8e3912c6e3a2b77
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:18:24 +0000

    polite anxious successful pleasant grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
