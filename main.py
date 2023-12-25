5aee26ce3c28608e9daa8e89ced49afc84edb864
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:49:23 +0000

    sign remark above clear forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
