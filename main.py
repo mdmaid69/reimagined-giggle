eef5715300fd9ffb04d7d1fc63b285df36d2a783
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:11:42 +0000

    undo live politics concentrate willing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import random
-print(random.randint(0, 100))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
