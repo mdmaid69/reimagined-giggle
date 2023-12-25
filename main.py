b19119a6149b317241a9b2b889d5d41075abf98c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:44:23 +0000

    naturally nervous Qq them grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
