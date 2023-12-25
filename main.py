700daaf9c574dcef88e545080adcbbc77e2818f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:32 +0000

    skill own beyond variety nervous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
