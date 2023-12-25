2e5da02d2c5023529fda5248ef6a5cd6c5d92477
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:30 +0000

    declaration breakinto peaceful petrol fight

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+for i in range(10): print(i)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
