91e28362b5f98593268df1578bf1f78bfc5c903f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:50 +0000

    convincing now cheap chat wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_floor(x):
+        return math.floor(x)
