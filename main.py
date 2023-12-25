e571fb95fa8df7b5cacedd86299a10d11bfc6341
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:23 +0000

    change nice kind control deal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
