128773a256bdc6e7ce3c4befa1bde8e9932fed0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:11:24 +0000

    pain rent fire sticky complete

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
