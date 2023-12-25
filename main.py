365d48e7780aa402a13c1c5f98c58962d677d36d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:09:11 +0000

    cover patient joke mistake his

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
