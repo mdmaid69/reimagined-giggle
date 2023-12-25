429440c000de51787bc2ecae689b3d88da8ba4c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:35:12 +0000

    Vv congratulate Ff sweet lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
