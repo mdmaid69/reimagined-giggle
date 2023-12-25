38fcbf38f43690dce1cc6be16defb87635192dec
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:12:24 +0000

    guilty hang event advise than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
