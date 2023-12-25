bf669331940400d5a10907ce02cf8c051e7b3361
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:49 +0000

    correction contrast information below please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
