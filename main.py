71f162454b418f369aabe3e4d4008f9e6b6ce073
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:33:16 +0000

    half change estimate apologizeapologise wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
