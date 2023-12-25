9f61a35fda2a0943efa00dbb2c10ca8dd3e1ad68
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:30 +0000

    respect far pocket murder bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
