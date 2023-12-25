e601cd77a404387c2f61f6917923cf869712a202
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:17 +0000

    available statement flat proper feed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-print(math.pi)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def split_path(path):
+        return os.path.split(path)
