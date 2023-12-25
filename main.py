ebae9aea540b120871272a754a1912b54b4fdb8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:28 +0000

    quarter Oo clear expression difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
