19d0ade76ae74430f605a890a1a1a12618181d3f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:55 +0000

    authority move broad find spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
