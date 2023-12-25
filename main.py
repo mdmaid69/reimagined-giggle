7d658be8cd06624c8d84c0f5c028fb7ed8c38f84
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:13:05 +0000

    lock between base to surprise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_set(array):
+        return set(array)
