e3f8dfee5f28f9ad1b53b789567b3fc74edd161a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:24:57 +0000

    tense far fall smile than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
