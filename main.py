01c8fce84d4b0265113b39aac7b1341bb35e0a60
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:53:35 +0000

    damage cheque clever letter mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
