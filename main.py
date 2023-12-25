96ba51ec8ee553baa2b493a1a49552209f49f00b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:12 +0000

    yourself result out whichever less

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
