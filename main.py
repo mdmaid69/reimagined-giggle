ef0644fada2c5fedadfee2ba848ac4fe3a3c0e6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:33 +0000

    callback determined board touch positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_odd(n):
-        return n % 2 != 0
+import numpy as np
+print(np.array([1, 2, 3]))
+import platform
+def get_os_info():
+        return platform.uname()
