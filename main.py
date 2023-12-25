a05447df253d9e7374405b7f34d245b5a3207915
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:09 +0000

    ilovedicks attitude bringup view advanced

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import time
+def get_time_since_epoch():
+        return time.time()
