b3a885ca3a777091f7ebcbe78a02794b63590b3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:02 +0000

    secret ashamed bringforward door final

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
