e3f0dd9535418312c9a0323044be739fe20bda1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:22 +0000

    course marry giveaway aware short

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
+def divide_numbers(x, y):
+        return x / y
