55eb2cf3d3b1e09b15a3b064f6cd494f3e5eccac
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:39 +0000

    complete win print towards embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
