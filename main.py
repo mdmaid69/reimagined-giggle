72c6774cca55cf5c359a65dce6b7415700cfb5d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:17 +0000

    emergency bottom either wind wind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
