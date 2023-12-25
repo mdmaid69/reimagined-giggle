3b4b27e4b61b74dac0516b1fcd61a33dd6790570
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:52 +0000

    friendly themselves memory arrangement temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
