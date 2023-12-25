71ac1ac1e1f17abfc40c3d18343863148b0022bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:01 +0000

    top refuse pleasure provide turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
