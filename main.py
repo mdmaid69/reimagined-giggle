b05987db6a2f72345cdc634bc8d40914eca803cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:04:20 +0000

    last score healthy much firm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
