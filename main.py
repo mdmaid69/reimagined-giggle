bcd07b92229984fb43c39fa53b90772e8495e297
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:18 +0000

    sticky recommend relative our themselves

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import numpy as np
-print(np.array([1, 2, 3]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
