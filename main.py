b721550a1e9d0a05968dbac3cf1c570e8c771817
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:49 +0000

    bottom soon thank nigga great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def sort_list(lst):
+        return sorted(lst)
