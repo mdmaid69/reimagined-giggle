c90cc778d58b0421aceea8b8dbb208451e482fe7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:56 +0000

    Ll pretend suck still hurt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
