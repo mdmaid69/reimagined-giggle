d50fa1401c8f82ce9fa49b985543a4d003a78922
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:54 +0000

    as wire me stop either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
