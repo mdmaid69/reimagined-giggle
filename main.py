bdfd8d24ac3ea143c875861dbf86c274b227bb97
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:49:55 +0000

    pick something mend below upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
