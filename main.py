2f2fc09304837c3fd381e5a2430415ac26b2f959
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:21:08 +0000

    guilty tax shock mean favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
