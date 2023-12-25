c824169314c7737361aabaa03d982300eb3b3d78
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:29:16 +0000

    recommend fine unkind stir advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
