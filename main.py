ca4e36341fd5f3cba66a2e35a0495132a9282805
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:38 +0000

    collect over politics flat introduce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def reverse_array(array):
+        array.reverse()
+for i in range(10): print(i)
