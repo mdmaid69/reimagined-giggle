018368ce358dd2d8c767c9120842c4d509e76f8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:40 +0000

    wherever obtain beneath count sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
