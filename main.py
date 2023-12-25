a7fde80a8ec58e996ae794dee13fe2fe700c0285
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:44:14 +0000

    cheek Ff related warm wrong

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
