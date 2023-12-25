84b895aeb42a875c0fb5b5b6e31a98a91b443dca
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:02 +0000

    narrow public seriously bar feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def reverse_list(lst):
+        return lst[::-1]
