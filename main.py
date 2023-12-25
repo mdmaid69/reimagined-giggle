db2dc4e93a37818db1ff604cc87836d07b619ac8
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:16:34 +0000

    surprising safety her image many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-x = 10
-y = 20
-print("Sum:", x + y)
+  def delete_file(file_name):
+        os.remove(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
