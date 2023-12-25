99dc553a7c259414ab35cc0617dd5446914089e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:31:52 +0000

    round proper urgent hole arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+for i in range(10): print(i)
