7e26da98e7118a6fb0809682948ae6e2a58f5b55
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:40 +0000

    Nn drawing communication otherwise busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
