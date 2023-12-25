97b8e583d2f5243719b42a11c913300efa494ac3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:20 +0000

    habit mix patience cost clever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
