15eecb22a36358acd624e66512f6e5c289b15c05
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:15:55 +0000

    exercise production thinkover reach hollow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
