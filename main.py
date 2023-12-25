9e99ff4d8e6892ef37f5210bd1ac07ea791a85b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:23 +0000

    world radio would suit sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
