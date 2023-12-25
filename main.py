16f408ff1aed2e8d07463d77eaa57dc041ba13d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:05 +0000

    pen Pp 567enemy slide considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
