ef7c44bfa5aceecfd0f1927d50f6524a7aef763d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:33:50 +0000

    him top drive flood modern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
