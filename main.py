bc863e554b0210a8451f7bcb6f391e6c23b5442e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:00 +0000

    makeup smile rush charge space

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
