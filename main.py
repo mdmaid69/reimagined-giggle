ee8fc913ccc59981b5e444638961fc30bea43837
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:18 +0000

    sweet frightened exaggerate thorough reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
