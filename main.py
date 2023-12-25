d01e29ccffbc5ce3a49fdbcad631af740f33a292
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:05 +0000

    meal shrink person style lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
