5b82a9c7d59c57ad408ee965e64740cd00563071
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:30:41 +0000

    position weigh insist national possibility

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
