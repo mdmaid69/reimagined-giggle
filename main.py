7313dd93a205c846ac8319fa36729df3fdc3ffba
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:13 +0000

    importance butter tense can bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import time
-print(time.time())
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
