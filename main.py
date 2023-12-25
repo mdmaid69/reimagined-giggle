8c3f7951092586e42186ff373b2582d1d6857f33
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:24:16 +0000

    statement bringforward check priority body

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Characters:", len(text))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
