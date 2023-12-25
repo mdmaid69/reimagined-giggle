2c50f24758e50d62f9ad81db8581776455ae9354
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:27 +0000

    lot onto could sentence correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_current_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
