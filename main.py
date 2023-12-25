021f474573596ed3c8e823d558563dc5b09c1308
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:16:57 +0000

    emergency contents Kk affect cost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
