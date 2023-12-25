2d39d2ff45fe3252281ca7f652caf9678c6ea20d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:20:37 +0000

    favour hard theirs anything judge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-for i in range(10): print(i)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
