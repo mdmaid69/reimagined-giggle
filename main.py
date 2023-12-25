f4fd19925463b35ccfce8ef953c64c23b5c5f75b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:38 +0000

    study help tread under probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
