76902b056e7ee34a973eb2e0d639ad95b8457317
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:03:09 +0000

    patient step still programme opponent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Words:", len(text.split()))
