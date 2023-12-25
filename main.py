817e368080cd3da126337229aab78d13ee380136
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:24:07 +0000

    part else lost where like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
