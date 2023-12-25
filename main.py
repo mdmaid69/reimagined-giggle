50d06a1053b9129730bc01c87acbaa8f90a872e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:16 +0000

    glad swallow eager Qq yesterday

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def is_odd(n):
-        return n % 2 != 0
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
