332521950f6e9ce6231acf67bac3886206a69428
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:53 +0000

    sink string convince cause eager

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_index(array, item):
+        return array.index(item)
