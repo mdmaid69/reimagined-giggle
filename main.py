af14d53494b0b00d239e2c725d508c09b369ecb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:28 +0000

    state desperate friendly control effect

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
