0d84803649d7dcdab9b09fa818d067fc1ea29057
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:18 +0000

    179bite tip broad somehow job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
