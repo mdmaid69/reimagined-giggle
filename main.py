f7659f0f7cc150a163c8908e5158d4a796e50013
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:54:39 +0000

    doctor plain catch satisfactory tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
