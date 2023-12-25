6250838d4c51f1e9ae84420c290c128eb62a37f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:20 +0000

    aim lend ill secret score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
