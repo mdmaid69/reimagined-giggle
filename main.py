8f0628d5d77caaf4765b03854ea1d8477ff862ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:07 +0000

    behave scared choice delicate course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+for i in range(5):
+        print(i)
+import array
+def append_to_array(array, item):
+        array.append(item)
