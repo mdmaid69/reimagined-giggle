f72254fbd11e51ff142681a3713a5e46a7797e9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:26 +0000

    pencil neither moon holdon when

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
