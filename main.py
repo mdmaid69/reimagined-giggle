889aec76c5406140fb7a6bffc5fa3be8813fd037
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:56:40 +0000

    twist separate getoff wave opportunity

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
