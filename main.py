f7f076c9df9699b33b7ac3accc282d89536596fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:24:55 +0000

    regret invitation about limit maximum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
