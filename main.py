274f9b116101d4f71e44e2e392b8c55e30d77486
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:37 +0000

    step poison female tape run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
