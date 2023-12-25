49d2c5cc31cec44aee5d3d4b24899e29dcceeb13
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:09 +0000

    belief myself charge cough treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
