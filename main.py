3570790a0a936c6c193787935531844d6c6de649
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:05 +0000

    actual operation second oil 571enough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
