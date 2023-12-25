e28237ef4341bfa36384ffe6e1b0de9ab585cda3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:24:48 +0000

    square wonder fork stupid turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
