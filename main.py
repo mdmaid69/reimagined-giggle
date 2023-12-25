bd508981d23b2baa84bf57223243f329849a3832
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:57 +0000

    drop nurse free agree generous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
