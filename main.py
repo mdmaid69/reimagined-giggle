6ccdac1e2fcb45c56c5a3cbebf6acbda5375982a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:18:48 +0000

    grade snow complete persuade condition

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import datetime
-print(datetime.datetime.now())
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
