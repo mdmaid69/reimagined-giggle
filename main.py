eb1bc2d2e546af3c4d3bbb057bffc0ac39853f0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:51:33 +0000

    company some official song permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
