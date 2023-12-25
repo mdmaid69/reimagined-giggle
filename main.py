92399cd7965488132a98f22b0cd2aa13752362a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:10:34 +0000

    attention 179bite breathe nigga base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
