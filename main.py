05960da827f3077e00fb99a22bea4178eb7f5182
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:35 +0000

    wind steer instead eat Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
