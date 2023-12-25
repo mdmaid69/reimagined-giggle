7771f40f9081327d4a57fbfa2478ecbc7f01db3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:07 +0000

    medium smoke so advantage just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
