83057e56bde722e5f139b449c98a5210464205cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:10:17 +0000

    there address lunch arm wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
