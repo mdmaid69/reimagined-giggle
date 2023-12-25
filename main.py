6285f9ae6576f2d42fa9b4c40e080ae11d61233b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:37 +0000

    brush authority active reduce many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
