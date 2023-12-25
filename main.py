1d55a9be809a76b84280cfbbe889a9e871efc8e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:46 +0000

    yes letter thought provide hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
