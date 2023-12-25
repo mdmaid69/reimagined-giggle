22936e6acd795e62946f7bfea01dd784fe6c6e6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:16 +0000

    rain leave rain swim turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
