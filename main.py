e7438baeeda4191b13557482093fe7ef98d2e082
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:43:55 +0000

    cheek factory realize height offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
