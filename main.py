4509ccd277650b3e95b26a690605a3544ff08c62
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:56 +0000

    stand throw though course pool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
