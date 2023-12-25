fcf5b49c639461b781133bcbee91cb0f7ca9b82f
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:24 +0000

    language breakfast appearance find computer

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def sort_list(lst):
+        return sorted(lst)
