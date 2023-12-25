a07429453967cdc1548d94bad060dc8ecd6705d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:18:34 +0000

    shall female badly would embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import time
+print(time.time())
