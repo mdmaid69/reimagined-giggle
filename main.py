de428297d19f6f99d43b6a23da00fc4b56802f0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:04:14 +0000

    reserve squeeze standard in apart

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
