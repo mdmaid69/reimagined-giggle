1f02e7a15a55295e94e5c61db78f18301170c49d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:52:03 +0000

    appeal doubt guess drive include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
