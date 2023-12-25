a2952174fe881e70a93f2ee5a352b50ce711c372
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:40 +0000

    attract murder argument lookafter type

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
