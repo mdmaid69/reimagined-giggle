6f7a63dc03f7ffb4dca82148785b4241d2a48746
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:05 +0000

    book understand goout increase later

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
