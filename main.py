12ec8c1d67fdf04d8d7aa6afd937aa95ad3c7fcd
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:20 +0000

    run stage record flat artificial

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+print("Hello, world!")
