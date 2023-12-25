6b7796ec1f160cc5428316ba0e272083f3b66587
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:26 +0000

    trouble maintain intelligent life generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
