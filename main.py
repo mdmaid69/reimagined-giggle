aa2dbf8c4d6bfda0be0f4896d09b395201898d9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:19 +0000

    port best vanish away squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
