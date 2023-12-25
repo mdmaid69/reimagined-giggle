1b17712885645930d2e88cb98c50a542486d6b62
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:01 +0000

    finger interruption debt fine birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
