cbd0b78265e516cc14cc81e623535f3c2117b120
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:21:06 +0000

    progress dig anywhere press ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
