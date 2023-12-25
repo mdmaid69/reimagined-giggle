448f102b4caa73015606e8cda3a63ca03199613c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:07 +0000

    correction design Ee upset escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
