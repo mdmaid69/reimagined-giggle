46415f84673d7b2d0f1f4332dca05b2cc2c0f5fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:18:36 +0000

    know disapprove swimming exact female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
