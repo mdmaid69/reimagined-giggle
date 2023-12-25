1e5dfa3f3707f52071aa0edfe8ddcdb310140131
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:14:21 +0000

    Gg feature but back new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
