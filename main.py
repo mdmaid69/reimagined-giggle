7a84bd2a6042fe71b0a0d3220689bff20633f00b
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:12 +0000

    bag like tap tense no

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
