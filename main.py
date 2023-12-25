c37c88b5070a8e6df6307ff035d1466253be55c8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:16:56 +0000

    join purpose careful passage roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
