a46b2e7bc3fc9505f67655116c7599a317defa13
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:28:54 +0000

    spill general squash stage unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
