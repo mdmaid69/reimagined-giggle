c41984321cde677472e2dcfb367eacc385ffe869
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:38 +0000

    deaf wind interview simple demand

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
