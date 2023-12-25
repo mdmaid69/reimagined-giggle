9ab03914e56a5296e377f8fba032ddf1aaa005fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:39 +0000

    description pile recently rise slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
