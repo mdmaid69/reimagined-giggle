00cf0dc1a8a2b7e77d4733fc2ca4a0d7137bbfd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:12 +0000

    refuse future threaten hurry rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
