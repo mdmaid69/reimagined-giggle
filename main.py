ffee4febd4d1016481cde5205f6662e62a84cfc2
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:48:34 +0000

    murder confidence home cost perform

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_palindrome(s):
+        return s == s[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
