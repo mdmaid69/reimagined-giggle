a33ae9ceb9e6edc191d57dbe6a46d5ba323aff3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:13 +0000

    bury steer always thinkof trouble

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
