bd501e3ed3b9da1a5ceef02e11237f7ae3a2accd
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:59 +0000

    slight heavy several avoid surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
