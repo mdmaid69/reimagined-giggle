aa3de1a10172d0b0a939d04f0f85fda051842772
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:31:43 +0000

    into confuse invitation ring already

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
