3ad19816ecbe1b53983b53b2be2676dd5703f631
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:50 +0000

    opposite makeup maintain whichever comment

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
