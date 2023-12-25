a216bf37424a8b437e6bf28cf4b71618c91e416e
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:07:13 +0000

    better away lookback(on) way hungry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
