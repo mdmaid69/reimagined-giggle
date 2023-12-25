a06c8d081d3a5478b4217c9acdb86110e0cf82d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:53 +0000

    admit takeaway crowd find possession

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
