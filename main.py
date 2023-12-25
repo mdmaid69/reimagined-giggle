4e2c3f94aba07ee0fa90f6530a3cf745e265f34d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:09 +0000

    eye transport money bet able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
