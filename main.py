cfea21b06bf2dba7cee65f55d2126ab8a3d5ae3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:19:50 +0000

    shopping because dare float 571enough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_min(lst):
-        return min(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
