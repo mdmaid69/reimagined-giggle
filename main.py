f8de6702ef55345c89df1220bb76b1f9f8a8f9e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:00:59 +0000

    be question trick pleased breath

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+from collections import Counter
+print(Counter("hello world"))
