92d76eac83d3e78469e5496ccbe08fd9e817aacd
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:19:02 +0000

    check fence tap schedule yes

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
