6da324dd3c37f0fe52dc2ead54e84e76835f0f80
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:25 +0000

    tired live praise contrast turninto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_density(mass, volume):
+        return mass / volume
