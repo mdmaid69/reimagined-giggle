2c38ab01a239837a82cceaff3d6c093978e02ecd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:38:04 +0000

    sale underneath risk comment which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_even(n):
+        return n % 2 == 0
