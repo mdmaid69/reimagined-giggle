74b53aae95f15b5ee25aaab41d60c311ac4797ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:43:35 +0000

    world straight sister soak write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
