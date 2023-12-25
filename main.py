37f819282ca6f0d6bdf9d73ea4bb768e9c655ca8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:55:57 +0000

    immediate without offend lend dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
 def calculate_factorial(n):
-        return math.factorial(n)
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
