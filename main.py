a0808e6da93da270e90932ae5704abd3c423aec5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:01:17 +0000

    mess embarrassing fortunately narrow ever

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_array_to_bytes(array):
+        return array.tobytes()
