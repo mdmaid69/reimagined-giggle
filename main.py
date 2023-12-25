2dca540efdbf421eab60594e40d88fe141289674
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:14 +0000

    some sometimes sink clever towards

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import time
-print(time.time())
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
