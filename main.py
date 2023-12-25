793b639ea4bc33ad0c2eecf64c178af5b2fa4aec
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:03 +0000

    general thin used example completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_area(radius):
+        return 3.14 * radius * radius
