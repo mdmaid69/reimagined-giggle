c285d1185de5e3d73ac4d9c0c6471983e95de632
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:51:33 +0000

    type order better little right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
