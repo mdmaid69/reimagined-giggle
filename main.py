7b9efddc299ab28b3777894da9050ef44f454484
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:46:43 +0000

    opposite considering normal possible dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_buffer_info(array):
+        return array.buffer_info()
