d30ec8e73ccbf9c8d1e1c08620df9dcdd4ec6a1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:49 +0000

    success lookafter this which so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
