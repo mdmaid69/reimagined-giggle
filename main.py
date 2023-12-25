0bc34a0059aec1c03b0d8920db2c36ba94465472
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:07:51 +0000

    dance genuine before accordingto mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
