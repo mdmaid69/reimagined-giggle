ea480f97a7bd6ed35799606302c3d7ee41292b38
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:48 +0000

    pity 567enemy contact call convinced

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
