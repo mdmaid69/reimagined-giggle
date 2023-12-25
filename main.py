52c5fdc4d6aa15ba69d06b2210a8b1357e53e4f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:48 +0000

    example press custom programme callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def square_number(x):
-        return x**2
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
