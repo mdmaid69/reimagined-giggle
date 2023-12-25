69080b27f465d6f1ac111337be1f3cf65ef97ca7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:43:48 +0000

    stretch ignore well runout poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
