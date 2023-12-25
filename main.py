e9e8ecaae9de26e429b8a9ddbcd5733a3141587b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:54 +0000

    wealth brush stare holdon feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
