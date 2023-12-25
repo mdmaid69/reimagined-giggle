12b158b2f97060060872681f78994f3eadd1727a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:36 +0000

    shine rarely bang involve behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
