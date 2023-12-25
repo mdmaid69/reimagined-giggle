9824429078b41d50b045dfb3b64a67317f50df88
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:19 +0000

    cheap fly it hair second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+print(os.getcwd())
