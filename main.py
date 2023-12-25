c85a870d3a1a56a6e0be98ffa9997d1f9eb57551
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:05 +0000

    practical wait somebody mean would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
