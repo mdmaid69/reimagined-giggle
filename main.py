71baa995fc3b6d7b3cc2ccba19483a7b6bc36eb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:13 +0000

    choice spring steam direct up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
