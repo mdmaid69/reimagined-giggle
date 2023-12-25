d21ded794791eec02502da57ce717bc7e65aa1de
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:15:39 +0000

    float sometimes help as disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
