e9fee96ba653277ee2cd75b9864c071330d73776
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:53 +0000

    variety hard ear belong turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def add_numbers(a, b):
+        return a + b
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
