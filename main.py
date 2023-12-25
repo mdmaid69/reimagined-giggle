1c8129b47c31374bee65be7ffa69f3b0494f0be8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:54:31 +0000

    live room woman theirs consider

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_user_dict():
+        return collections.UserDict()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
