aa1b7566f44de804b0110a3060c18697f3240608
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:27 +0000

    tall carry attempt among special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
