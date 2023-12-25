5fee8e12eda6b53f9748ed401ed0a95cbb814591
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:45 +0000

    Jj ticket hand them takeover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
