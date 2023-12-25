f500775c1f38d54417d78fe72a6be38d99ed98f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:09 +0000

    gooff black not gather class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def square_number(x):
-        return x**2
+def calculate_area_rectangle(l, w):
+        return l * w
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
