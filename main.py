da6555977bcda0266d855f090c4954997a9fad47
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:09 +0000

    field drawing because lean woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
