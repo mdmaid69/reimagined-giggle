9b72fab00f6e77e6abbb4a106bfc00660e47d41e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:39 +0000

    temper onto field live onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
