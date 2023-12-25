3a77aa162514d49f3666706e8bfed06a0af70ffe
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:15:43 +0000

    dance comeoff nice when promise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_item(array, i):
+        return array[i]
