86cc6f7a37f377a7f01ea4c268abd681d2adc2fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:19:03 +0000

    season vision pause rob still

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
