305c0bc84f9fc76192b381a33e729d0f56468d8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:05:03 +0000

    well prove present emergency draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
