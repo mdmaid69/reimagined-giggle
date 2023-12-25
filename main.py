01264bbd2cdda599145098c9d21a4c9e1bc80981
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:31:13 +0000

    train offer poison bored theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
