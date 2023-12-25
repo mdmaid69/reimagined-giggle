9a090080715a41501cdcf22db3ba658981afb3e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:10 +0000

    live sharp hear summer suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
