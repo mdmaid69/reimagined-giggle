3945168ae0c75d0031ae8b0171ac1b6aebfe81dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:45:07 +0000

    their blind stare custom load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
