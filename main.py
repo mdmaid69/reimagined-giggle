b81d77f3a00e4fc44e5b153a6e0db40a75dd77d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:26 +0000

    mystery satisfactory left flood objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
