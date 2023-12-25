25a80bc0d9f4c86a38b6c1c9f549de83d3c52fcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:08:12 +0000

    plenty label warning explain turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
