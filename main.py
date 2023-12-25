c60953a5eb02924bce0e96e98255bee094bf55d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:59 +0000

    pain anxious disgusting necessary blue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def is_even(n):
+        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
