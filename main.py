384c1bd7110f75cd2ac24c15db2a38f5f98e043b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:06 +0000

    actual wall careful inside sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
