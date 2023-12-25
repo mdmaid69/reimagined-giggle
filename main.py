7071f06c55f5fc32c0be67f6eb12d0330704997d
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:13 +0000

    platform miss spin remain goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
