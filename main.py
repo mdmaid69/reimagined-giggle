1a66b7a30358f2392178d5c23bad4310273e6a5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:14 +0000

    ride may break fairly piece

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_item(array, i):
+        return array[i]
