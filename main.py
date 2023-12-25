1729de84114fdc41e93e04ff53916a8ce8d4ffab
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:59 +0000

    order post objection come place

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
