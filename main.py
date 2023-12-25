70483d9619f8d41bbdd2b84b177829e7e0acfc63
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:07 +0000

    continue confident often year opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
