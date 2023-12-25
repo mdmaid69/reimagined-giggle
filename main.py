364f27c26c340895822c33809394a5ce1e4ed57e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:16:47 +0000

    cook company him disappointed breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
