73e15dafb8a9c893ce39e3e6d23b62127c6e8ef4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:55 +0000

    beneath clothes geton correct none

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_user_dict():
+        return collections.UserDict()
