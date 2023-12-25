466f130011b773887f72783fb589dd752a1a4ff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:44 +0000

    makeup possible quick thankyou turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def find_max(lst):
+        return max(lst)
