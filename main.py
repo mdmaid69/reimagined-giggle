e1a60100e15a7c7bc796f4af5a1ccfe4f75e57f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:34:56 +0000

    soft develop swing gradually turnout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-def sort_list(lst):
-        return sorted(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
