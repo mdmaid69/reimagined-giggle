d7d7db4258de849dad5d71a6742ffecf2fe33fe9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:19 +0000

    necessary bear worse place belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
