207113ee1916edb203664852326f9f7c2f0aed73
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:50 +0000

    inquire will become maximum nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
