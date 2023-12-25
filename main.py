1418172fefe20ad3cd76e4ef9b70a616560cd48f
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:40:08 +0000

    hers place keepaway thing shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
