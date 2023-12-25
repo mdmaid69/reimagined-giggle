49c3048f15ac870bbaf26e5d35581716d80ab55b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:27:24 +0000

    probably turn tone raise your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
