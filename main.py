bf6c7b60ed0c100ba0a4a661c51b12c9e1afb84b
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:48:57 +0000

    than wrap shopping myself relative

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
