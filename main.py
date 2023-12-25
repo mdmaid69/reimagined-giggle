66ac156b1873e9209e60abb19ee102641da30bb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:30:56 +0000

    once grade priority bill attention

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
