fe5708504f16506cd14fd0cd563b03b36598297c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:00:46 +0000

    advantage shrink interview want whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
