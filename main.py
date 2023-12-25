9bd17cc3e4f611e8d851faa253ae88b5981b0a2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:59:47 +0000

    calm free star point tie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
