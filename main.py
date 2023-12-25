d65828e48a17b8f036914d00532c1c927fd26f4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:51 +0000

    play attempt angry could uncomfortable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
