13dcba1fd2d2c2d57bf73d65f868717383960700
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:02 +0000

    sometimes from lean cruel fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
