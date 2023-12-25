14d6898f81f1d27f3f987170b01b26855345b298
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:46 +0000

    mind means certainly smile open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
