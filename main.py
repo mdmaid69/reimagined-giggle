2803394ed0ac3242ebce8669aef6061420243bf3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:28 +0000

    leave among quick downstairs sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import getpass
+def get_username():
+        return getpass.getuser()
