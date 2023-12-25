775cfca353ef56dff0ae2afa5b038ba59c9baff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:50 +0000

    necessary that limit imagination confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
