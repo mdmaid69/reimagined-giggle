91ef45c6c004341e0c08ae3a29c09c97390ad84f
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:56 +0000

    music afterwards human decision mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_priority_queue():
+        return collections.deque()
