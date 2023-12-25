2a3770b0fbfd57c254d6a22d5dac7b3565c30738
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:38 +0000

    substance whistle doctor dry cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
