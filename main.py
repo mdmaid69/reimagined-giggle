10472ec3dae422942f2c706940a62a045a0833e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:52:58 +0000

    address scale claim protest getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def square_number(x):
+        return x**2
