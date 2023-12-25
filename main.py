3611b60957265a4bd3a83e9ec9da571790feb061
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:41 +0000

    law tongue getaway nigga by

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
