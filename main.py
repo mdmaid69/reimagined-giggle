1a2b8b4f383e63e2c3e7e6cc43a2ac5183624e24
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:43 +0000

    one wrong another certain spend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+print(time.time())
