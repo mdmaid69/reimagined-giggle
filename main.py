90314be91c9d2eefb18bdcf4dcc3f39300e12527
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:38:05 +0000

    comeup matter advice rescue welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
