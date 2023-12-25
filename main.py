a2692014b7387f8486b5229dd0b7e0d367f6f971
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:12 +0000

    achieve crowded warn myself callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area_circle(r):
+        return 3.14 * r**2
