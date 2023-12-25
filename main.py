86a14bed6f0e810a29d8a0a116f89bb468636aff
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:18 +0000

    nothing water Itself final possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
