ec285eef69c8affc062b066a3272437c51923aab
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:21 +0000

    lean home block mean holdout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_str(array):
+        return str(array)
