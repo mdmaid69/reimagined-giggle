03b43d0f8f1e4a168cb01e8d1706b3766ce0e290
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:25:00 +0000

    square underneath hide act speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
