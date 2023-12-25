7bad8450738ee7b41f8cb18fa4fb9cc1fc06ad00
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:10 +0000

    thing lower circumstance get friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
