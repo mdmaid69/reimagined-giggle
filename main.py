d638953b877d399cf693a48d7d09a9c08d279a3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:21:46 +0000

    black light whom right pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
