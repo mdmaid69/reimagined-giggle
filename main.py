218d2de3d0707ba4f99c39bbd07215139d4f3422
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:36 +0000

    steep official crime live warm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
