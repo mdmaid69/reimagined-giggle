220726d3ff7a2d4d6a6acadb21d48e3bb3cca2f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:55:20 +0000

    thinkover instrument contents invitation do

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def cube_number(x):
+        return x**3
