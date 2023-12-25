7350bb4429e27700b43205222cfc4610b3112b9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:37 +0000

    reduction against meeting suggest convenient

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
