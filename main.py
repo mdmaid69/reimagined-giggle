54244c6c1f93fdb695fe2c9d47ded69ef2d24311
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:51:45 +0000

    nervous steer woman tax complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
