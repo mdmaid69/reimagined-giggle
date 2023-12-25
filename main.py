b6d9ff07d468f670dafad40d1371ea7adce86765
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:32 +0000

    ride patient cum level exercise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
