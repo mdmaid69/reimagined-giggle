cb16aca6dfe70d41b4096b728cbc34fe5e2a0927
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:09 +0000

    section heart doctor behind difficult

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def split_path(path):
+        return os.path.split(path)
