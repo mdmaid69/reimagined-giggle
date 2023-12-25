02f1856aa73cc8e9b4f45b523bf85a7c4e74c4e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:39 +0000

    smile sky experience position there

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
