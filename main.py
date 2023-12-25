d3fac07809de75242c17dc35a307d76106bcbee3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:27 +0000

    whether thirsty taste safety smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
