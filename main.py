9e8e835199a2b4785400a0f6efb7cc9658f2a39d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:57:15 +0000

    price go pleased communicate home

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
