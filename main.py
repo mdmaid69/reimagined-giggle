82fc3e39d47b142ab579364fdd7a904a00381f50
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:02:31 +0000

    judge question jump 568energy remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
