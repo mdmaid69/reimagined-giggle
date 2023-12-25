1b2759adb585231e44af56780ee58b74d89b1316
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:48 +0000

    trust target post imagination couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
