785c9610449ea5d668bfa70b399f6ad4316844e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:49:03 +0000

    lock ocean scene OKorokay invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
