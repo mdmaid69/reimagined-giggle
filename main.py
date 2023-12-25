4ea39db8bdcc136ed34fcaa1036d296257810fb0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:27 +0000

    lean information low pride use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_list_from_array(array):
+        return array.tolist()
