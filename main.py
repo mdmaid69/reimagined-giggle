e02b28e3038cbc5a83723c0ae100bc9c172c1baa
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:54 +0000

    inquire fast cry other owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def count_elements(lst):
+        return len(lst)
