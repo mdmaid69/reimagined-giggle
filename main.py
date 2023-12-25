ced6cefef7823b4554fe2a3d7fdccb390adb06dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:42 +0000

    hang strength grade under favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
