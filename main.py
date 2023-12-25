dc43813741d19aeac1abd33690897c665bebbde4
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:20 +0000

    earn all company acceptable round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
