b5abf5e23761ee12fd09d21991f46864f67fddd5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:17 +0000

    bite determined air agreement idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
