0ef8b88329c49e1e0bf5db1538412f1680a127bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:51:56 +0000

    spend shine also cheap wild

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
