8c345a2f8f6b74087a3ba783a0d655b897d96ff2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:19:53 +0000

    flood concerned greeting depend agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
