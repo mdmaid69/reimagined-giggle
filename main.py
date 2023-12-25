074d1210c26570d294b99c8510a35d34031650c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:30:35 +0000

    avoid holdon spray long reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
