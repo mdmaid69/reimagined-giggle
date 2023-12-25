a8942270fe0e27c3ae5d38dc0f2f4b32a8704229
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:52 +0000

    appearance receive study so slow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
