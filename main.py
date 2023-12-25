cdb3786311821d20e355117beaac932b9d37a8c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:43:05 +0000

    approve live rub freeze opportunity

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
