0695be869c8e3578eb60c74e3cbad8718cf9b9c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:54 +0000

    apologizeapologise smell production smell profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
