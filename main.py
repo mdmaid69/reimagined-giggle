64cf45ca0bf066db59543dc2ab252b204f750945
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:40 +0000

    announce affect gather error welcome

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(lst):
-        return max(lst)
-def square_number(x):
-        return x**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
