45f0ffb07a14ac34280942882802b3af898e0889
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:10:35 +0000

    attitude profit lonely material motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
