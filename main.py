208a8b16f34edaca861e31e3206be56a7afc4899
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:31:59 +0000

    trade dress instant shine remember

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
