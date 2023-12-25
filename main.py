4122737c4c5f9ae39934e86ba7a000037a6e582c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:08 +0000

    study turndown throw government wash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import time
+def get_current_time():
+        return time.time()
