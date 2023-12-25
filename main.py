2f42223df2d923b5067770860b46688c5b0204a3
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:15:54 +0000

    brush country Itself cover pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
