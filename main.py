8f77e50370e458672d45f17f3b98afbe26d482a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:37 +0000

    Ww shoot object level type

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
