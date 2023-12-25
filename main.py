e35c1e237262c5b3b123c3b81be74f9f4432f080
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:34:37 +0000

    climb clean aim price destroy

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
