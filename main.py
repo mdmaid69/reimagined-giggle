99db680b478323a61b85168435dc10ffb976005e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:55 +0000

    offence worth reserve own expensive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
