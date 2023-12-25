252b57d40599f35085d9b24e7bfb4cc6541459c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:26 +0000

    some impression hurt care Yy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_area_rectangle(l, w):
+        return l * w
