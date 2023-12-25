42ce3f0542787d497a2e0ba4d1700810c9054cf9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:40 +0000

    anxious radio believe expensive probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-name = "Python"
-print("Hello,", name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
