5822ec53e9f5881459159a930edb88a835be80d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:46 +0000

    considering force wish expression disappear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_pressure(force, area):
+        return force / area
