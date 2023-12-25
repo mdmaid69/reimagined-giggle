7f1879172ed549d6565a50c9d526dfd54974bcca
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:05 +0000

    comeout visit butter factory while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
