39a5ebfaab0b88d8808f03df267b0007eeb10ef3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:41 +0000

    challenge reasonable giveback refer noise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+print(os.getcwd())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
