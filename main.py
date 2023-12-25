0827b438be09b16aba26bb101317e1c29c94984b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:11 +0000

    expensive consult burn wall year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
