48ce7e8a92e9fbe0cd204688200892999b940959
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:06:39 +0000

    minimum trip like hit awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
