074158fed81b4cf73ae1d126b646d2f153bdcf09
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:14 +0000

    authority strike join ought aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_str(array):
+        return str(array)
