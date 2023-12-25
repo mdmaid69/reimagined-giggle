182f47a41739ad59a9c8a1a2608a01bb2a065693
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:10 +0000

    herself not street use fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
