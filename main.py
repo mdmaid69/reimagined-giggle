ef5c370c68dc5f34e686189b60a236608f8d6774
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:38 +0000

    justice look fine exam see

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
