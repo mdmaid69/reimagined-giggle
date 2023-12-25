e04370ea619e54299d99a4e1356a3a72d3ff0eab
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:02 +0000

    confuse real sting pleased ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-print(sys.version)
+def reverse_string(s):
+        return s[::-1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
