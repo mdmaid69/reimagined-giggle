1ce801723386c17a6fe61bc38b1ec9303cc86447
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:02:34 +0000

    during pull proper lock original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
