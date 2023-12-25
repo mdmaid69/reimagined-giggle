bff5aa31214d28095378b4c6c464607b548a973f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:55:51 +0000

    organization little finger calm mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
