7fd3e36f729a0ebbd0f139544853d0fbb04d5d89
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:51 +0000

    another scream sure weather present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
