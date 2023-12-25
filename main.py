6dc58e51c0553c726ebde52eecfaa2fd8a3fc8f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:19:09 +0000

    weigh congratulate than each draw

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
