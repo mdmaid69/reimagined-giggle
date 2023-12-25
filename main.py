cb6d7e66a9f7a1c59407c76389b500ea73992570
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:24 +0000

    extremely create trip preparation rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def append_to_array(array, item):
+        array.append(item)
