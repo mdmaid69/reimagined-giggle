fdf3ecee9ed47dea7cb96bb6e55968dfaa533226
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:06 +0000

    plenty sound 572enter exchange round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
