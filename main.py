0a602185bfcc7de993619fa1bec07b7690c4d403
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:56 +0000

    recently fashion big effect need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
