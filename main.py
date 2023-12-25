916840795931aff9c629c2f63666108e71da6a46
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:45:57 +0000

    Gg much believe task boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
+from collections import Counter
+print(Counter("hello world"))
