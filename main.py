2bce665aae1ea073ed313b0b545ad77080bb0ce2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:46 +0000

    thought complain regularly modern human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_length(array):
-        return len(array)
+def reverse_array(array):
+        array.reverse()
+import numpy as np
+print(np.array([1, 2, 3]))
