37886d4806c6672f1522b4ad08da1340d2b1b32e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:34:25 +0000

    actual some size prevent possession

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_odd(n):
-        return n % 2 != 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def multiply_numbers(x, y):
+        return x * y
