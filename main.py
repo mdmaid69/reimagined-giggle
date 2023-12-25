99a9b94501cd81a5ed7846fd878138d938b69420
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:59:49 +0000

    alive translate floor base him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
