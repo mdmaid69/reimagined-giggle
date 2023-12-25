423f0fcbc5fbcf83a4b214905e8dfb6aedf7bcd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:16:18 +0000

    name note bush switch low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
