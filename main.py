26efc55e0d19eb536bdc7dee184d64cfb52855c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:41 +0000

    joke question round keepoff acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
