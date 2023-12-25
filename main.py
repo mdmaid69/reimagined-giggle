469428758215552b3851ff8a7ab5fe4e5f63e38c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:01:04 +0000

    ought shout taste natural lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
