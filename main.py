2ead9fc7f0b2a298a904d705ef2e0fa7052f8cfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:10:51 +0000

    soft name talk float shake

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
