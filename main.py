ac2d50f2a9fc45ed77ad18cdb03fed33387128b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:39:46 +0000

    tune practice without pollution opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
