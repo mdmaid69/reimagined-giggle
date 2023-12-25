3b15eefb764ee669fa1643e119e9624817e0e671
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:21 +0000

    expect able transparent produce constantly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def clear_array(array):
+        array *= 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
