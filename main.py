c9e61d3070608b55c5f6d9df9f0023ad4d734a27
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:01 +0000

    music never mind fly hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_priority_queue():
+        return collections.deque()
