7efaeb9f6c70a0600048e3b6261f3775f0ebb998
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:41:03 +0000

    physical achieve correct wide use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
