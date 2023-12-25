53b2a42eb82492dfccaa33124f239d1fea54a8b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:34 +0000

    table major breakdown block angry

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
