a2f6f9702b06ae74422553d13d2346def18be170
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:46:03 +0000

    pronounce flood mother lonely lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
