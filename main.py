3e8b25430077b6a9c16df8496dedcdf74eca3213
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:33:01 +0000

    minority copy nothing keep board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
