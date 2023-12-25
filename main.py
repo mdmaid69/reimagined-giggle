eb081842a622f56d868b862325f473ca129341dd
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:47:00 +0000

    hard strict visit short brief

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
