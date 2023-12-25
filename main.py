c9cbe3235913626976daaabf3853f2f3d5e59e69
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:36 +0000

    lift calmly content party react

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def reverse_string(s):
-        return s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
