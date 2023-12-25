aadfe88a87743d177bde21ef5792379855264f86
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:44:36 +0000

    unlikely please excellent guide fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
