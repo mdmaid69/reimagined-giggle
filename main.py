33fe767c4140b3f30b0c2dbc39c175dafc73b39f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:35:53 +0000

    stare benefit except flat mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_power(work, time):
-        return work / time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
