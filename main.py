355d21c3e1f30a6848fd17b4ed806f6a2bd87697
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:11:30 +0000

    then rich likely splash when

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
