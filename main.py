dbc0251a129acc56a29d7f25c05b38704b0d4b7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:15 +0000

    collect authority level night sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
