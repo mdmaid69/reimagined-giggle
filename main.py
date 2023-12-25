ef96524e4392cc35ad963a99b94c6dfc8ca7072c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:00 +0000

    only finger side confuse giveout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
