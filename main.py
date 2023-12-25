767232532dac7bc8d88b29ae0cf2ed89be8a6720
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:02:02 +0000

    upset young noone him fear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
