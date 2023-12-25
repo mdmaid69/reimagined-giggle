091fd06d0f46d1ce43fbda27f4f380080d20b2bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:44 +0000

    back whenever young clear keen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
