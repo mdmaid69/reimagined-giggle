5b9637386a06e89e468430d5161297db68ea9f68
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:20 +0000

    edge realize train price level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def sort_list(lst):
+        return sorted(lst)
