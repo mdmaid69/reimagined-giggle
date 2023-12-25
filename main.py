c60515940a817578a0e08054007b34e0af5263cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:14:09 +0000

    power left by weather mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
