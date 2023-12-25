597a5d33b75c533c6038122c9cfc64e952239252
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:15 +0000

    wind criticism invitation without reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
