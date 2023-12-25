2ba936e5fe1aa0500e347306df6343cf0d356946
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:12 +0000

    fence frame personal top threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
