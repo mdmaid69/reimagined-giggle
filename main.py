73093ea3b45769a527e8fede52e4a8363a827eb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:27 +0000

    joke church shoulder list roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
