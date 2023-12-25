fb68dfff43a7536b546347ed74bc80e6d87ee9e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:52 +0000

    else fork goback(to) tour fond

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
