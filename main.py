ebadcd4afdcbb47c8359f2105bec16c0e6e0e936
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:34 +0000

    small bush air drop eager

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
