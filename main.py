791c42a9c12d5d3389b53f58e15277339aa0803b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:00 +0000

    open length see impossible door

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
