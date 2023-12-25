640b104500a715846940918531e368917c712804
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:41:47 +0000

    command influence actual nowhere far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
