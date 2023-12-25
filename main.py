d2fee1faea38e2c484249c0c6b98a910108310af
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:02 +0000

    will yours 570enjoyable forget taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
