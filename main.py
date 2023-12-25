31de383e0114f313c7d4779d0ca2287b63e20d26
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:58 +0000

    become meeting on feeling neat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
