215ce6973c4d27a52858e9f53a731f5742a17fce
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:48 +0000

    better talkoutof escape damp regional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_even(n):
+        return n % 2 == 0
