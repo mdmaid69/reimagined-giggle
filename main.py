67ae02fde32a15af0cbb837585e1b9c8a097193c
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:38:24 +0000

    warm pocket suppose tape seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
