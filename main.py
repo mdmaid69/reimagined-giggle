58758b250bf62b6a373bacf0944205219dda00cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:44 +0000

    future tidy dig turnback blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def square_number(x):
+        return x**2
