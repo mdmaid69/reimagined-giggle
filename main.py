6f10d75a33cdcb5c4b96b95b790e5d3e4bf31aab
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:33:11 +0000

    Cc count forget lower far

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-from collections import Counter
-print(Counter("hello world"))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_repr(array):
+        return repr(array)
