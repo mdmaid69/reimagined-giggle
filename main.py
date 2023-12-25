abd388a7366c4f35df71a8de56d2236115d03a5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:43:34 +0000

    failure turnout name turninto telephone

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print(sum(range(10)))
