5aa6d1e4ea9fd9373a9e7d42c7468e9e42b6dafc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:51 +0000

    violence bend report quality life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
