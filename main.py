8371009df07ded74967f1efba156c13de4953eac
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:41 +0000

    business pronunciation fence word hurt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def find_min(numbers):
-        return min(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
