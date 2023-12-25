1a900b381e90086c21185d44791b4a39451f57b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:32 +0000

    recently family young pin forever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
