3164453ec07febf8dac8192e4b9bedd9f5d68812
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:21:23 +0000

    wrong shock tall together state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
