7f367763f67c160e1f40fa35a4dc311d4557d978
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:30:40 +0000

    foot split debt admire claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
