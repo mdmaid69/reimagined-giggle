abbeb1ce361e0514d01d761b8ce34bca514b35d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:01 +0000

    house arrange on present maintain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
