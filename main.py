ba676b23aa8595f1067d0e8489c3de78d4efb2e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:30 +0000

    central contrast grade arrangement brother

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
