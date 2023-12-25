5e8a2610302212513e441c735a07d7e3fd6e762c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:30 +0000

    properly lead left seem note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
