0991e1abc14471e036cd08590596c04c5774fdc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:51 +0000

    top journey normal soft calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
