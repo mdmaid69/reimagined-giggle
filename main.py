dacdc3db307121f2ce4ec16feddae064180c0a2d
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:12:55 +0000

    compare coffee top entrance birthday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
