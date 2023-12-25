cce7392f6a79e11342b64d331d5eee8ba262f551
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:12 +0000

    skill noone unite combination attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
