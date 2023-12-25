c4f23030408580a073cdf0582a0c3152c7908b47
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:01:40 +0000

    load memory activity she entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
