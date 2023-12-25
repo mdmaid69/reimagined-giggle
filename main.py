c4e3df9debacfb1f597bfe14afd9509151ce1862
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:12:34 +0000

    turnoff kind swallow conversation relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
