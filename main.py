2a9a9236dba745db9f80d6b13efe75c1fabd4365
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:03:09 +0000

    artificial choose scrap Mm lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
