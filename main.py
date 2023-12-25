780af58614e83d7ceb518ad6dd64e89bd29f6df1
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:40:53 +0000

    needle benefit bad guide stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
 def create_array(typecode, initializer):
         return array.array(typecode, initializer)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
