e9bb8a812c80990fc30ea4a738166d4a1ce7fd9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:36:55 +0000

    under expect no mine inside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
