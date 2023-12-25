2fcebd48628c422cbfe4600064b8446e0fd03a59
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:45:59 +0000

    something summer country announce manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
