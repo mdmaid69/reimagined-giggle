30eecdfcfd6d030387ed17b080d674509d030a5c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:44 +0000

    serve Ee sometimes her play

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
