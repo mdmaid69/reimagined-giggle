7af3fa59e61b9b504bc5020a8a3b33c8b14d7593
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:59 +0000

    check trick result remove your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
