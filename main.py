b9b51062dce075fd26d9f94aec2deecae1da0e39
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:01:04 +0000

    by cruel mine pen under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
