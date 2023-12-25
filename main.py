7d960222e169fab3fe051e88a011b18b39120567
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:33:19 +0000

    whole lookback(on) cheap stiff object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
