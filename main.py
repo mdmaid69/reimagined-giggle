1541c12f03af08182633141cc743afd2d558431b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:02 +0000

    tidy mistake dig answer comeup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_length(array):
+        return len(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
