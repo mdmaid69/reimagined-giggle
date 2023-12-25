e0f09b6a6b3c46c33f355b9b9abdf3794a8d48d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:43 +0000

    nothing needle true task subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
