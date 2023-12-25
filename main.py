ec776f710588198270df0fbd5a939c75e23d0dfa
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:27 +0000

    principle makeup delight confidence train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
