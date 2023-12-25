d4d5770d978c2faee607078033b8c73d17589911
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:52 +0000

    far pleased body eye everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
