d0db4c1cc0c09ac2f6ed4f0ab81da6bc33532764
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:01 +0000

    extra care picture ignore will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
