1a3fd6e8b405ca68578b9510d16806279b151f61
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:28:16 +0000

    before Rr strength talkinto market

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
