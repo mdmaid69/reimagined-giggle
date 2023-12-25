e09eb8ff0652b7cbc5a8fce4877c747f75dcf576
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:42:44 +0000

    prove study trouble thirsty support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
