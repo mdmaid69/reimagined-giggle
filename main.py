fbace26305d0bebf06200580f9fa724a1017206a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:44:36 +0000

    plain step software avoid politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
