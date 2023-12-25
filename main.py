537e0677136499ca86727ae576e31e4f242d33b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:28:48 +0000

    system rescue suck walk brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_volume(length, width, height):
+        return length * width * height
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
