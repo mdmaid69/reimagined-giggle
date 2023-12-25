8e92b428c8e969076181b39f858acfea564ee466
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:53 +0000

    public pain touch get spoil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
