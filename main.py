f969ec09ddebca175face1be0b2d0802658230c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:21:59 +0000

    offence set connect report fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
