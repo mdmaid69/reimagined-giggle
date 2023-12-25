66107b9a5ac0d2af72175485a141fef2a4217063
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:17:20 +0000

    expression make dry ourselves quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
