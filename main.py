2535301fc5bb9598da4a23ab6a981bbcef230eb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:10:40 +0000

    excited arrive number area test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
