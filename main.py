829b6296d688948c6de40f9e23f12ed6fe568b30
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:38 +0000

    zero patient fork exam mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
