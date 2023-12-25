42177df0ea800e5b72913d483af11bd17cbbc7da
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:18:06 +0000

    mention cry sleep meaning trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
