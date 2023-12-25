66a1684b7f610cb474bad02f8867d168bd2c0fa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:50 +0000

    face sweet wish throw downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def cube_number(x):
+        return x**3
