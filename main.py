7cc47c05778f21973c7045c79e2ce6e6bed4a821
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:37:54 +0000

    spot lookback(on) surprised comeover by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
