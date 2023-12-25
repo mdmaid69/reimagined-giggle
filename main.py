4050b90eec99ab1f11871c5362d4f5ee7c695601
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:02 +0000

    shut warm justice 569enjoy shut

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_power(work, time):
-        return work / time
+for i in range(10): print(i)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
