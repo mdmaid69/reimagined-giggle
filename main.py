ced4e155027d884f2d9c1c2a1bee7677d7c99646
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:36:23 +0000

    between and march remove fasten

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
