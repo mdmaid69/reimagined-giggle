ed156e3bec1d67c866353bd1d2676c1f4f06a1fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:09 +0000

    moon swimming protection exchange scheme

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+name = "Python"
+print("Hello,", name)
+def square_number(x):
+        return x**2
