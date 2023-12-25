6fcda1951f42df87e244b3b25352bd28d8616cbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:19:35 +0000

    fire cut flat injury everyone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
