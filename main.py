4faf0322cc040f75a44f7aff1806c5c7352caae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:26:17 +0000

    apart family ring strict brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_complex(array):
+        return complex(array[0])
