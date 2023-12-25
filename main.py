421d3fea40e6907ae29f7a65bdea924e8d516906
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:24 +0000

    service finger education for evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
