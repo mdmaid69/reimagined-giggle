a71bf7d4c6de5897bfc2d37118677ee516069e18
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:14 +0000

    put poison bottom say force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
