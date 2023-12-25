671ea30c3a77a1f86be6d1092dbcb66c9595da02
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:32 +0000

    flat shallow neat letter level

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
