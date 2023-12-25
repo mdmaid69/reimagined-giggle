ea494083dad72092b6754e4521f7fa1416538098
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:52:27 +0000

    Zz freeze whom chin way

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-print("Hello, world!")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def reverse_list(lst):
+        return lst[::-1]
