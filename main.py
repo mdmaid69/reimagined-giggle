181cbd8cba8ee96b9c83cc637a8e3d456da8b953
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:09:59 +0000

    name model easy sit mess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
