e5445f6080b8b99c2cd442b3552a1e46d6148dce
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:10:21 +0000

    base cold schedule attach fast

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def convert_to_hex(n):
+        return hex(n)
