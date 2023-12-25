560b73a6705f054383d70c01794fc12341d30f8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:29 +0000

    brush bite large pause man

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
