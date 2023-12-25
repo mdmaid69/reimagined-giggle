6e7d09c5b99d916e7bf876eeae9fd96d94a6d0e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:36:07 +0000

    plenty live buy air round

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def multiply_numbers(x, y):
+        return x * y
