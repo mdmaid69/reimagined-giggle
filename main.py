52eda199727594b2a433e5be79cbf74cbf4a1b0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:30:32 +0000

    plenty wave rich hope water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
