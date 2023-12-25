9014d91a50c49b009a928bf2dc2bdf6b75e5560c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:48:07 +0000

    trick lookaround ours indeed yet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
