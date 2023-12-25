03d23874f9ca6736d45b7c57b5e8c086c82b2cbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:10:36 +0000

    legal come powerful vanish while

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import sys
-def print_python_version():
-        return sys.version
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+print(sys.version)
