5a3bfa4f726ce22f8dc262a0b86b6c4ba32811aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:35 +0000

    pleased loud thanks objection nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_binary(n):
+        return bin(n)
