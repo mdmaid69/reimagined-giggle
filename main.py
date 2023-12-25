0a0e637a04e776ca46a1f708dfe5c0a3fd149088
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:26:27 +0000

    respect prepare afternoon address doctor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
