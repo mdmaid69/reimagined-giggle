c7bb0ff4301672550bd001376267d0c5b195d26c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:54:07 +0000

    change as live interruption callback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
