63ef2bfbeea11c68edfefcbc7828253aa4365e0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:58 +0000

    comeover know afterwards formal door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
