f5c3c2fd8ec012d60f0baf6656a2c19676a46bec
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:04:49 +0000

    communication come build normal absolutely

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
