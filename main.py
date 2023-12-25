abb21fe88635e4a6130080b4caf0332b58679ecc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:28 +0000

    arrive sleep friendly fight government

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
