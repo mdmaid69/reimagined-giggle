0634a630ea9dee19b66122eb5e278f9fd34d3935
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:16 +0000

    sure present doubt do start

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  def multiply_numbers(x, y):
-        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
