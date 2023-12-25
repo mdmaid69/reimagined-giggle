c2e135ab1ee5d11cbf07f8f7322bdaeb13a721ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:31:26 +0000

    married less bottom succeed middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
