023037661bd56d7225270bdc8fe062b28380798c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:29:53 +0000

    estimate rescue voice mistake fetch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
