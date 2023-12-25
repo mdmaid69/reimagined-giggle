e0dd4138b7823837367fb445d81b92827a92a354
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:38:50 +0000

    paper step successful slip in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
