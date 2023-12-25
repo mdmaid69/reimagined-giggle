b2a345368dba9d7f8ef2f5fb53a236c4900b38ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:37:52 +0000

    she catch surprised flow branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
