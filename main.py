4fe42404732040b6e9f596fd84b19a26e047c1a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:57:34 +0000

    shallow dinner weigh marry dream

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
