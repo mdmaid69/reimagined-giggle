4ad4f405a63a7ecdf973ff1afb104fdbbe868a7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:52:54 +0000

    sound write usually snow play

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
