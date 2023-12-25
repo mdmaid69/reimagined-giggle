be450e8d79fb7f759f66a8582eb43f7ba12b7a4c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:14 +0000

    usually please result once build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
