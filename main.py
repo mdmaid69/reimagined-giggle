f36fde7b21cbf85c0b22f9b2cb377e24faaedab6
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:31 +0000

    under flat thankyou terrible programme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
