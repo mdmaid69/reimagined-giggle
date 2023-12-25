c516b3489626297ff69192e99ece6222d79fcb9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:50 +0000

    do positive dish pretty problem

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
