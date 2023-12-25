94e96aa7fa3b128818599c75902250d59ed7180b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:33:19 +0000

    before city tradition real television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
