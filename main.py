42092ecb27933529d5e2d9e25fa7d9b654e304eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:01 +0000

    best attach mystery earn load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
