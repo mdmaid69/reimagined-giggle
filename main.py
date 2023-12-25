4a75af1036ef61e6e8adc66ecee2299822172999
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:02 +0000

    best blind back down risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def greet(name):
+        print(f"Hello, {name}!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
