733d147b54238367ad670cd86085250d6b877ad8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:48:12 +0000

    shallow relief rubbish equipment pollution

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
