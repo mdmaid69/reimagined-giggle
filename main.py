507d44e981f0a4b37dd87f0673c395ad1ef53acb
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:25:41 +0000

    thinkover dance nice honest clue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+print(os.getcwd())
