f5c4fa053a7f3ea7c33a6fc0cf0815909b968f56
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:33:44 +0000

    properly just exaggerate taste left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
