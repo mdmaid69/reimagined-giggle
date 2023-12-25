45f0d43a5a530272ef290a391c23b676b73cd372
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:29:23 +0000

    lesson delicate arm warm getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
