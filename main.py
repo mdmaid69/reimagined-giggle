abba90c29c57b190467fcdb4926627cad5e53a5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:27 +0000

    combine walk pressure meaning attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
