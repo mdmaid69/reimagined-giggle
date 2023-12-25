d69b4b90da0e3c7c66a6fff19f6db258f2e83d1b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:02:03 +0000

    emergency interview sort mean pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def square_number(x):
-        return x**2
+  def get_directory_name(path):
+        return os.path.dirname(path)
