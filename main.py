f8cf1a2e002e30e6e14d6582779bdd9dcc723e5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:58 +0000

    lay Jj tonight collection point

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
