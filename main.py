a0b9e674fb9772b63289daa89861c8d988256b5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:06:00 +0000

    basically ours colour grade justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
