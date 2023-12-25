f8628595a93d650837b439c560cf89ce27249444
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:44:42 +0000

    basic boat debt money land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+def count_characters(sentence):
+        return len(sentence)
