5d80699119f9f4412deed408fcbde1679ad0829e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:01 +0000

    remark answer note read direction

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import random
+print(random.randint(0, 100))
