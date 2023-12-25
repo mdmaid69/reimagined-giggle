6ae4c8fce16758268d50aaf35754af266387fecf
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:40:33 +0000

    best stone once summer used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
