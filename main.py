ffad70fc169b3366dd9f01331dd662cd7b6e13e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:39:57 +0000

    concerning smart snow letter independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
