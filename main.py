35aa7c3e7c4786d33b9f16446a72f827db0f1075
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:22 +0000

    lose still contrast brother backwards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-print(os.getcwd())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
