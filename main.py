00c6a106a312378b7f5bd768ce20f7855020063c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:04 +0000

    makeinto suggest start earth ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
