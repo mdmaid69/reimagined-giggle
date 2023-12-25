8b0aca91eb76ba984654601c541293450076e549
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:13:44 +0000

    risk challenge badly laugh handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
