8a2ac9db57576d647076401908928359e38fe524
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:06 +0000

    organization whenever with mind respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
