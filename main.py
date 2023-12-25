bb4c8fe1cfd028cc95d8fed3e1267652f8bfb258
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:33 +0000

    sign sing fetch actual yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
