202324870ea0a4e4c7fa177614b1f313f2fa335d
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:24:59 +0000

    although loose describe phone snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
