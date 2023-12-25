94c40bb385efde061bd5267f59e2e7eb75003845
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:17 +0000

    bet situation must score more

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def subtract_numbers(x, y):
-        return x - y
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
