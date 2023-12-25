0bc3c2d9f17311ad3a90f4ca2526f0f851137085
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:59 +0000

    length still side refuse suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
