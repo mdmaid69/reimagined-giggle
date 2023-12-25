4f08873b3a8ff7e769d3a20453fbdcba08cd0abb
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:15:24 +0000

    record store ear proof possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
