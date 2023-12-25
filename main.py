a39efeca3f7bc448096b0cae5403805855e9cd75
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:14:28 +0000

    ill innocent ill name famous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
