43d32b9b3162f531bfc34703c15016988db13137
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:55:42 +0000

    mind refuse majority unkind constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def add_numbers(x, y):
+        return x + y
