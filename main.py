cbb4ff07a7349c87d48437cf035cb1edf78933a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:15:08 +0000

    Mm attention carry politics taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
