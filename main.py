fcf7396be9c32c3fdffe96ecdacc62589132c4be
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:05 +0000

    top either hit least burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_volume(length, width, height):
+        return length * width * height
