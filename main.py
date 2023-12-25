fcc102916cd826d490c1b56d1488b5df89fac298
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:29 +0000

    succeed heavy precise agree consist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
