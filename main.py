89140fc7254825c2af8ccaa75821898b6f1b875f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:14:46 +0000

    speech nurse estimate almost so

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import os
-print(os.getcwd())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
