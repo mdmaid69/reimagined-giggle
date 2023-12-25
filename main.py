8a1e3d8684a30a297692fc11cbaa19d23f97a62f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:15 +0000

    work single risk impatient imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
