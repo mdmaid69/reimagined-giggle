97f4cbb810dfa388fdef7356b8983b3659889b0a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:28:29 +0000

    but marry apply but 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
