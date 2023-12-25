aaf20ccd2f3a5093ceccde84b54a70f5d49ce93b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:17:22 +0000

    tidy handle sail top position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-for i in range(5):
-        print(i)
+def get_array_as_int(array):
+        return int(array[0])
