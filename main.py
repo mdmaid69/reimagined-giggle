0eca2ce97a863867907d23e327763746533d0f5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:52:46 +0000

    car special rush immediately goon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
