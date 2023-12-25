8ad4884a3ad487ea9f04369981e051b8ef481328
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:28:57 +0000

    emphasize what number can occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
