c322584eaebb0af5bdfbca7cf8a1dd148b317c6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:15 +0000

    consider lookaround accident Uu mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_work(force, distance):
-        return force * distance
+def get_array_as_set(array):
+        return set(array)
