1914a9ffe35a104eae303158a0a20cbb1247a86b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:25 +0000

    Uu example instant really surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
