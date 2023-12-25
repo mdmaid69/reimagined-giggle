163112f509b103a1ffa2eb7b070cd4ea30eb7d11
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:56 +0000

    block idea clothes almost visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
