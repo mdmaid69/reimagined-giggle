eb436f50457706aa1c5ceace5ccd07c2738f687d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:07:51 +0000

    dress may convincing whichever themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_characters(sentence):
+        return len(sentence)
