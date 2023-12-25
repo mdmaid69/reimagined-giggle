72fc7efe87e492a10ac2294c90d4061eccb02732
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:52 +0000

    up black shock bringup Ff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
