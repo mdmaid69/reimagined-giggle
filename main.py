aab8ff3dff99d6a38b6aa4314f21dcdfd244d30e
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:20:20 +0000

    within breakout century tomorrow near

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def multiply_numbers(x, y):
-        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def reverse_list(lst):
+        return lst[::-1]
