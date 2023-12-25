dc6352fc18723b789658fa0516f16218e58c611c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:18 +0000

    turndown phone loan comeout could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
