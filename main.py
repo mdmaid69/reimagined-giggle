a7567259e0c065816ab7dff296a59b8dadf52a14
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:08:22 +0000

    similar street who house ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
