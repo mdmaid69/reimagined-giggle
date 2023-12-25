6594a195327f4efdc3f85c40ec99302404d5f7a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:23 +0000

    band preparation communicate possibility ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
