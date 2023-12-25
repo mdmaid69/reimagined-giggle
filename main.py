e87abebb18bb3d42dfe76a072c557ecf0af900c4
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:39:13 +0000

    sorry cause splash weigh share

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_octal(n):
-        return oct(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
