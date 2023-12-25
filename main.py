ec0fa123728572827db4e1c9cf32b9d24adf2823
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:23:32 +0000

    low turnover dream obtain instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.time()
