603c29c11330074597da9ca47f894a1da95092f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:48:40 +0000

    pause evening off heat front

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
