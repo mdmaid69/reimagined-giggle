a3944fad6d2df85bbeb205e4c2f89ba041d789e2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:37:03 +0000

    instant drop entrance prefer kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
