38ba0eadfff2dc73419d011ad6e63daa79d2a024
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:19:26 +0000

    guard only dare busy runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
