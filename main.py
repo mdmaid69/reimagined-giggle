4062650edc203f4cd7e55bc23b543206f17d37c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:35:45 +0000

    thinkthrough Itself hide tough maximum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import datetime
+print(datetime.datetime.now())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
