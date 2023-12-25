819584d76779c8e8f2a7a7ebaabfd44dd4e2bdd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:55:31 +0000

    estimate side themselves his summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
