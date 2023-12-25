f69d28d10c648c9c2e9838b3758623bd64579f92
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:50:03 +0000

    top afternoon throw remove gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
