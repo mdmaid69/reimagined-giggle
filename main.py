c5b8d24086d7a05b2ccbc23064de1a8bf983b72f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:06:12 +0000

    hurt bound healthy trap unhappy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import datetime
-print(datetime.datetime.now())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_circle(r):
+        return 3.14 * r**2
