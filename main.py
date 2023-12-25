e2b583617f4be616921f3ac0b5006b785576c3a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:36 +0000

    laugh coat tired sleep too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
