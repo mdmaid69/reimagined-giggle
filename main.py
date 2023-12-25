7ea7b81160bbf607b561fff862fc811cd4faa5e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:36:55 +0000

    shopping available prefer agree trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
