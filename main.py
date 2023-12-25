2ddccd2c2a57df994767853f82f15d1e6dfc75ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:38:20 +0000

    otherwise rise importance tomorrow concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def square_number(x):
+        return x**2
