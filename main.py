0a879ae46ee1d91f9a3b7eac87f249e9d22e5a08
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:42:40 +0000

    sound bottom find against then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
