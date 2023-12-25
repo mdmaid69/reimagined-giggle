d9e964f67f5ec069200adf2630c628f89a457bad
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:16:41 +0000

    light whistle thirsty wild clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
