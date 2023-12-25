d3ef7248172672d50540e3f69f6355803f614abc
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:30:34 +0000

    dick roof this whether performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
