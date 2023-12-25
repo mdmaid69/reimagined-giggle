0ffce986c306eca88e22c94b61aa0ccfa074a15f
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:58:59 +0000

    save listenfor list room equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
   def get_parent_directory(dir_name):
         return os.path.dirname(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
