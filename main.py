970d46f862c2d996579e1a1dbf02abf2a91479c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:19:52 +0000

    pen sale similar hand really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+print(math.pi)
