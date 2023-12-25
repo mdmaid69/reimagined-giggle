686810dd226af7d8eac7929dd65673e3714b7f13
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:36 +0000

    maybe rest task hard tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
