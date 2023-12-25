33b46eda3befa3990f0219a52fdeb3342dc58448
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:32:36 +0000

    temperature reduce wide concerning sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
