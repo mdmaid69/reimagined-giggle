4e3e7a6421fffb1db2027e3187fd35967c84d3eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:31:07 +0000

    naturally report age cross manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
