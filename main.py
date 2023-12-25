a0cfec90db3c82c3a21a390b8a3a3d3f6eb6bf2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:48 +0000

    ignore movement country command on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
