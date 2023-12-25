56b6d543365cbff16309ce052d84e42325690713
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:13 +0000

    where really soon pause form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_acceleration(speed, time):
-        return speed / time
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
