ff676327dc96494dca775a75b13f7ad872fecc56
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:29:46 +0000

    increase getoff order accident separate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
