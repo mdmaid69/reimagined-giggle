e883732edb38e1b8df2d5e92df73897fab4e147f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:54 +0000

    tape pass idea eye communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
