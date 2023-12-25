4c57ab9998192e4290569017aec71db95da461ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:07 +0000

    kind art married unlikely Yy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
