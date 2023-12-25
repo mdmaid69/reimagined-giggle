f4b0511f12b9f323b8b37372b80ed30c8749b8b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:30:25 +0000

    confuse compete Ff sure voice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
