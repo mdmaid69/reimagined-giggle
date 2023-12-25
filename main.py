aeb4d5363d5053edf86eabff2a7457446bf368b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:07:33 +0000

    proof perform mass connected guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
