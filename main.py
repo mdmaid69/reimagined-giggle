a25a54b8490ed0bfc91847f504a494578cbbabb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:02 +0000

    post square for sit level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
