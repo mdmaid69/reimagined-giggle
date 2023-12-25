52c4d6dd9eacea75901694a9cff0239b3e697347
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:57 +0000

    crazy us impressive sleep innocent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
