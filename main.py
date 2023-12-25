8450fc180a529ad25cd9c6953962bc08f938935d
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:08 +0000

    forever seem either morning answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+print("Common elements:", set(list1) & set(list2))
