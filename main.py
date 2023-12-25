3ab7f2a9c7089acc4e638a1fc19b22fa8a30094f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:05 +0000

    beside method juice scrape remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_float(array):
+        return float(array[0])
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def check_if_array_contains_item(array, item):
+        return item in array
