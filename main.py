c7ce1e9777ce4fef93985767b62fc41751b98c8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:56 +0000

    each support gift Nn thing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
