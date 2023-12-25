ad5bf02abee616f18bb1adf79b21c8caf1d342b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:38:40 +0000

    card there point date state

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
