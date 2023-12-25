b4a534a4c35bd989b05497ef5350064f1358d145
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:53:01 +0000

    convince handle swimming shut city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
