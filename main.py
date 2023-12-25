4bd2a882c1c2cffee47a006d212b23cd16d9580b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:29 +0000

    indeed rub part shine come

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
