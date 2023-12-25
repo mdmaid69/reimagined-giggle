00a407edbe81c641335e18f44826e20838a90800
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:49 +0000

    trousers bridge separate rather entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
