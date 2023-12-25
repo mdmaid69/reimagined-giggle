ad8aa54b06fa94200fcba4107b0f7ea731c8946d
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:13:38 +0000

    getaway accidental passenger cheat artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
