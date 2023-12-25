f3700dc7141ab3b55a558c555fbf7b8198f87496
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:31:01 +0000

    result disapprove calm pass swallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
