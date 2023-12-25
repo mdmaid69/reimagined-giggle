f51d0a0aa13c3e3c1c435047a798f1024eacdf78
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:20:51 +0000

    correction floor whistle matter goup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
