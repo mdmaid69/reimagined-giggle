7212e9bf79bc88d7b91ea5eb42565253719a2f59
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:43:36 +0000

    class gate dry lose coat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
