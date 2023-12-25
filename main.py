b9822529960a81b530f46ade28bba5b743616df9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:02 +0000

    whistle creature curious giveup give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
