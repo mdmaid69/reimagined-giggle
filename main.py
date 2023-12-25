971d128a5a6a7b3a628dba99e4495660d15b6044
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:07:15 +0000

    star discuss quit fail closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
