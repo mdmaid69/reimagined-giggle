31bf0cb52518d1a5b4f4296b2371546c33fa6db4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:33 +0000

    from meanwhile giveout why willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
