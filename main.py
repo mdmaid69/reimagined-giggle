cfd407985f4ddcb63fae104698080f752c7ec51c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:55 +0000

    tomorrow peaceful plain point hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
