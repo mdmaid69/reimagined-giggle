f5bc869c9a84535885412e4cabed77230e801258
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:16 +0000

    relax definite maximum however wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
