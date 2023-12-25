e4acdc49e88eec17b6333e5f946794e30e317df1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:06 +0000

    pool appeal constantly clear division

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
