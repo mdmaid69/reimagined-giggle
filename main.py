0cfe9c50cac161fd6892498478cfab489e7c286a
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:28:54 +0000

    class well criticize country professional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def is_even(n):
-        return n % 2 == 0
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
