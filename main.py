12adaf4d369e91f51c8e6099acf03483dec676ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:52 +0000

    dear turnon compete runaway underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
