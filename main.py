73b287f13c773d60d415fe4e8aafefdd7613fddc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:10:02 +0000

    cope sting claim measure cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
