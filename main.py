742abc7f8afce23877f334456ffe79fd8aefe562
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:15 +0000

    quantity live perfect connect trouble

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
