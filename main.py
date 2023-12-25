8a1e4fd13ec97f1cbf1ffaf3d2f6016f6aa46238
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:06:18 +0000

    finger thinkthrough plan else freeze

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
