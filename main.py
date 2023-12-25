9d67f2ec4eb409c6899455f662adcfcce835bb8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:37 +0000

    keepout whatever pay likely plenty

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import numpy as np
+print(np.array([1, 2, 3]))
