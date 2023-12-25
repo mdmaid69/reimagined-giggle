e47bc8be516b3870a2c439bacc631177036fc92e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:05 +0000

    scrap busy challenge copy bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
