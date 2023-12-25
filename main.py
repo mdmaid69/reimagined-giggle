b71e56562cc6a2fecf1aff4fe6611af29f95f494
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:44 +0000

    snow switch close couple father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
