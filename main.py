854206b5b564356596e9e5f2937bc05575672ab1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:08 +0000

    telephone trick throughout illness comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
