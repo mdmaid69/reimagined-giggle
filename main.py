d6fabeeeb7f4c7b5534f1c00ef4a5e6406d7c39b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:52 +0000

    clear copy Ss means wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
