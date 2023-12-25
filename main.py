5e43012446d50e3628501fa54951e0e9b79cc024
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:47 +0000

    distance cook niggaacademy through division

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
