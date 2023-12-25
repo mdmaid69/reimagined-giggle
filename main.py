681fc64cca4b10fd9c9257693d155251adec2ee8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:08:10 +0000

    plenty back moment break rule

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
