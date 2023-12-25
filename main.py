fb1c8ec835e84e019b2a30607eab7f555a42f0bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:10 +0000

    passage sit street 567enemy possession

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
