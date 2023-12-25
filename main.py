9c5ca82612e20350980b8a2a01b2a6dd4b8b733c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:46:18 +0000

    by 567enemy giveout comeover common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
