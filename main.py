1dcebb07e8b0e2a0fae8017d5c9ef93a1f143953
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:03:23 +0000

    recovery medium beautiful stamp address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
