263e9cb53fafe6bcd87bb8325096842f3260b171
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:55 +0000

    turnback include regularly he brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
