54f5dd45f07f2dd504f5bc7a2cd31251173762f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:46:20 +0000

    mad then information you mother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
