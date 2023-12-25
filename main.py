31ca919b526428f85529fc2b5c514de38ae318a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:48 +0000

    clothes strict loose contact fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
