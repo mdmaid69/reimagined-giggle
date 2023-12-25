f03526a2d6c4fb20092b82d71a265a08b1c7256a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:22:18 +0000

    scare rule goaway so sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
