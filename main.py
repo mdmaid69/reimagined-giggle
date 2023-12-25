b34b0d30f0aa66e684ff2bf10e3fb856165dc682
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:12:43 +0000

    leave roof problem smart amount

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
