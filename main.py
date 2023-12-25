00ac5fe272cb1561c05ccad83bf30909ddac0cac
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:51 +0000

    use task fine long motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+print([x**2 for x in range(10)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
