3a17ed743776ac22694b69c969aa5ab4ce5ee9a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:41:34 +0000

    neat appeal convince I small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
