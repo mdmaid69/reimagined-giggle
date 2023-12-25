63780781c77db029bca1461ea422872d4a9e453c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:40:01 +0000

    ready education protection measure pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
