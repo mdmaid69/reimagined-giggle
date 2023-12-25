316bd2803ee0a246009e860f7c3f6a8ec2e93c4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:35:49 +0000

    supply be get brief past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
