7b12011f76017c24112067dbc6780e54a27bd421
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:06:42 +0000

    return quarter challenge trial fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
