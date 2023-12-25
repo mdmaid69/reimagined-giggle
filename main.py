5c428b882ae173e0c6698f5d7c055e422cf378c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:25 +0000

    leave tone touch discuss unkind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
