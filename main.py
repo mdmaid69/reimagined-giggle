44f4788ff3edafb3c08da093a237fb196b17f80e
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:55 +0000

    chin custom whole hand yours

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(10): print(i)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
