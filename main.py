a83fe8bf5ecd9ed2814e19a8c454ae18869c2401
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:09:49 +0000

    accidental beginning sit freeze desperate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
