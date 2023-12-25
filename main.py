beb6854806f11446ea830fc742c8e8b1a1a029d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:26:42 +0000

    scared major belief arrange inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
