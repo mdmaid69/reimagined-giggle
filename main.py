1d0caa175cac07d8232ca637890fff957477f8dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:50 +0000

    strange force cruel really set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def reverse_list(lst):
+        return lst[::-1]
+import re
+print(re.match("h.*o", "hello world"))
