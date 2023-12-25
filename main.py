e796d2ebf03e33d7b1ab7f8a333a70bde1e24882
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:34 +0000

    now when tool better unit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_slice(array, i, j):
+        return array[i:j]
