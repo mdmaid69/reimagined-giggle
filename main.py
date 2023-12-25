8d7947fb5481c705ddde9c62779c2e8f37b2d941
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:07 +0000

    moon used welcome and could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
