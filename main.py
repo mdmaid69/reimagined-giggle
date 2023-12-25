2e4e866fb98abcad49ab7d3de6a649500bea35fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:08:57 +0000

    shadow late think wide than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
