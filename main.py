37b0febd6b86b38a533af164230361b165924391
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:56:32 +0000

    doubt influence sense shake visit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
