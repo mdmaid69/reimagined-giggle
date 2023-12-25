52b4bbf2dd251d69b17f3a025e7481a05f96789e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:39 +0000

    fall 571enough declare worry direction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
