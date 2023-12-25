022b9a4f215beefb41d10d2cd90014af5fddd2c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:51:15 +0000

    bye guard roll smile ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_item(array, i):
+        return array[i]
+  def count_elements(lst):
+        return len(lst)
