fcced100515e679980907d0e5f33cde7bccab7b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:47:50 +0000

    bite image ours smile piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
