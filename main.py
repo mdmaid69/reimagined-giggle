bf411d14b7db0f14f3547bf2cafb8fd04572b898
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:39:51 +0000

    save serious side refer constantly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
