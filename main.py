b1427295107495b949b6c304fe440ba13b87b403
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:08:57 +0000

    forward create learn advantage will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
