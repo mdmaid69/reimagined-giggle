9b4670b545644359dedb6e13235d1d23c1982f89
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:06:51 +0000

    meeting brown university mixture education

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_speed(distance, time):
+        return distance / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
