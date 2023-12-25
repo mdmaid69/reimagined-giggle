25847e9d093ec51fe04828cb9055fe7ad7d897b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:13:16 +0000

    pleasant language low art work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
