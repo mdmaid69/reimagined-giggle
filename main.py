cb2f44eac78b2f049fe3b23cb5702d357ecee6cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:47 +0000

    joke live mine switch perhaps

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
