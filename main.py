4e88cf6891d351c33860f9c1b520bdf12acad39b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:25:28 +0000

    stay trouble bad description crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
