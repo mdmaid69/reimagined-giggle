cc25018741915b4e5045f3c324461fcccb37ddd9
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:08:42 +0000

    rarely container punishment use left

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+x = 10
+y = 20
+print("Sum:", x + y)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
