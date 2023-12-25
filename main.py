fae5f21ab857cfa2b5ebacde280cb61951a8d394
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:59:47 +0000

    bunch office hate result floor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
