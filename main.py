bcf2184593de89b95081bd1a5ce3fd8b7b320bb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:42 +0000

    comeup dance reserve door shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
