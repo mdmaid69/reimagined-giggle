113981fafd0929931a1d4cd4168e13758e0f4018
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:11:16 +0000

    she immediately sudden makeup calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import platform
+def get_os_info():
+        return platform.uname()
