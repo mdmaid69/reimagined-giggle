916b42e94141a3504ca7e5355573c95b42214be9
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:51:17 +0000

    operation ready side 572enter without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
