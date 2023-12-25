508f398abb999dc333523f8b16d63e494e23ca61
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:28:22 +0000

    pretty cover uniform create especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def reverse_array(array):
-        array.reverse()
+def find_max(lst):
+        return max(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
