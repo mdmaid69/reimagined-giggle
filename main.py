4ffb24d3043013fc144241e455bebde6fb980d80
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:35:27 +0000

    work actually close mainly march

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
