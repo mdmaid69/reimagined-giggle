f8666f62776b32c0d3a3bc3178bbae647cf0c006
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:45:00 +0000

    play main lookback(on) fall confidence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
