0730b3e9b04819b241bec0ceb8a227b5fdea4c5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:39:08 +0000

    along corner think whether interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
