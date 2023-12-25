1486f58db6b14bc781f614001a73fd57db02ac8e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:46 +0000

    over demand evening violent fortunately

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
