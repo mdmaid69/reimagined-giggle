f217329f03a2674efa73a81f83a7939839a338f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:23:56 +0000

    none consist bad intend convincing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_item(array, i):
+        return array[i]
