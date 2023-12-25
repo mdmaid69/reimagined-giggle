7688943b740471f1d70725f6a1d99bebf9c8dc69
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:21:42 +0000

    keepup remain most cruel guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
