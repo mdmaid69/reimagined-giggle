59dd70aa16285c8b7302871cb7b553635f6d551d
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:35 +0000

    mystery type subject race information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_bool(array):
+        return bool(array)
