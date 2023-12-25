9a1c0d6086de1f8eeb4ad5b7d3b3e84eaa5e54da
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:03 +0000

    bite out check lead Bb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+print(os.getcwd())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
