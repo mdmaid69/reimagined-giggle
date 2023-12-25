093577677df8b49fea8ac3d48aa4ed1a62e93be3
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:10 +0000

    crime close tradition foot anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
