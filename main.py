6f27abce3e73b76119a332ecf0a138b28772a9b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:56:04 +0000

    report maybe experiment 566end classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
