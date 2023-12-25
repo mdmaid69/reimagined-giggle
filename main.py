31a0bebea07f6deaf162502a401211c2061984ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:08 +0000

    create play contact blow run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
