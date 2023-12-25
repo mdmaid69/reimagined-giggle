7cb47ef92238bed6b24c27e82443dacf46812fb7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:47:56 +0000

    slip but fond receive open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import sys
-print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
