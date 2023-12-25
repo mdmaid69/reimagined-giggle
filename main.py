cb9f87b65eef0c80d6f0c6c2a3e131b379d1ec7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:25:38 +0000

    never one exploration contrast young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_array_as_set(array):
-        return set(array)
+def set_array_item(array, i, item):
+        array[i] = item
