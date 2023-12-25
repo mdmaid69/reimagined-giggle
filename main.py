e54588d6fcdedc25409cc5910dd2cbc431a3f4ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:36 +0000

    entrance container square swim neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
+def calculate_power(work, time):
+        return work / time
