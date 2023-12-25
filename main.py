3c2bcc7219d651cd0de36d0435bf9f455fe1d145
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:54 +0000

    common risk effort estimate home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
