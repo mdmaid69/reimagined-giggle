6413458851ca12cb9255b8e7330558f38dfcca70
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:34 +0000

    wealth along worried confused shine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
