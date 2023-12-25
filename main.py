aa5216ae9466f45295ca89e6d5abcd9278899168
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:53:09 +0000

    interruption exactly light hope race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
