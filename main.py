c84ca15ec50558260b1227546345df7c732951a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:31:15 +0000

    ever opposite wild operate peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_index(array, item):
+        return array.index(item)
