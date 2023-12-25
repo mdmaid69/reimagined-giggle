4d76371a4e77e844f8968298945bbfb13eaddc5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:40:02 +0000

    connected interview miss claim drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
