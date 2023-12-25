b98415a3b3a719ec43e0c7d1b861de310ebec469
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:03:04 +0000

    factory getback occur nose look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
