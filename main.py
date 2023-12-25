0c3097bd99f49e45b56eb3a273cebc687172c12c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:10:25 +0000

    fold thirsty forgive criticism check

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
