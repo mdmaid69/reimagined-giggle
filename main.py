7155dea6218f0fcd4e338998dcf05e7c1bbe8e90
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:42 +0000

    protect school silent afternoon correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
