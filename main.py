a47188859e5b65225a62d3d438655a330568065c
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:44 +0000

    flight cheap official kind post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
