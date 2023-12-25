df060310b696631260f58c04ba793911702afb89
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:43 +0000

    dull surface relief quickly habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
