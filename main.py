277de90ccc6f3050eb9de0302f0ffa600587b882
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:41:06 +0000

    open fit tune one contents

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def split_path(path):
+        return os.path.split(path)
