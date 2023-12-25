43931e06c9e6d67f15a390115b2537862729c199
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:23 +0000

    build suppose form figure wall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def count_elements(lst):
+        return len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
