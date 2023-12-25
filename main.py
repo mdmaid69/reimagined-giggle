1a3a6dfa14b24794f1592003b04bd10181c859a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:51:48 +0000

    squeeze born further prefer familiar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
