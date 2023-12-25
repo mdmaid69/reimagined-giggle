ed358e421834fd72c282376f87a4a804e507ccae
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:25 +0000

    workout mess except flow earth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
