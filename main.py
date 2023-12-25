a434da59b71a7bffdd12e5bb087c05708a8af047
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:52 +0000

    ahead entry tip load information

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
