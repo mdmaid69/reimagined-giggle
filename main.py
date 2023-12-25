eb588838164c73ca8e532155c1124f386b42f099
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:32 +0000

    hold benefit world back knowledge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
