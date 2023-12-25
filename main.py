dd660944a88d77a5440bfc91241fd2425eb4eedd
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:32:11 +0000

    neither bad description gap path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
