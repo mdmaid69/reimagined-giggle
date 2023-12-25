6892bec4ee9a64043de9153d6f30da45493415e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:00:13 +0000

    right excuse around well conduct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
