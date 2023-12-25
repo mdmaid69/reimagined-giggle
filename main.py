40d71574a5b38182bf61c3551b3f47a8f20ced91
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:48:16 +0000

    flow help Ll kick on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
