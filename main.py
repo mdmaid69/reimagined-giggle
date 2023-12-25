8dd41fe538c27b1d6117d04510d78a114abff074
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:40 +0000

    properly union rich who theirs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def subtract_numbers(x, y):
-        return x - y
+for i in range(10): print(i)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
