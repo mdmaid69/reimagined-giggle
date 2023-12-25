d9d507fedf3a1126464c0b86d9acf325863cd756
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:17 +0000

    rub impatient port ahead load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def sort_numbers(numbers):
-        return sorted(numbers)
+for i in range(5):
+        print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
