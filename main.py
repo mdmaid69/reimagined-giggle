37538c261eeb7410b2939ec951026996b5e3cc98
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:39:57 +0000

    fire skin trousers begin thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
