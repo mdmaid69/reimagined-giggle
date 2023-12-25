1c9ddf870105f25c2ef44543aaabbf9cefa70717
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:02 +0000

    desperate chance exciting pressure comeover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
