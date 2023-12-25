7701a63e0b88d28752de520f373a8b70e1ef6231
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:41:01 +0000

    bear Qq tragedy crash shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_tangent(x):
+        return math.tan(x)
