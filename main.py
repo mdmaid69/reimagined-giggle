a761ffe31bcca0aeea0adc21c3abe3476f477ae3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:58:41 +0000

    process street foot trap car

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
