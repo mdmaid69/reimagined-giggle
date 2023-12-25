45cd31359c4ed40cf6cb814d52296fc4f3c71f16
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:31 +0000

    runaway impatient criticize divide extra

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
