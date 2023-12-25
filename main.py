1b6dce19cce129c318c4086a849cc10f7240b5b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:34:32 +0000

    catch what wild live news

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+name = "Python"
+print("Hello,", name)
