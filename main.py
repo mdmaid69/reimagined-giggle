cfe4867e6f3cb387e3b02e806b4a9aa8bf43daa2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:01:19 +0000

    child maybe impression close nearly

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_power(work, time):
+        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
