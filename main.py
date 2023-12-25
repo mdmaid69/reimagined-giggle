ea2abfd70304a97bb5e6734498af7583b53b8e9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:59:36 +0000

    wonder book march thought shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
