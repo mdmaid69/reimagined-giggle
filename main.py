7934b13eeb667e3cbcdcbc652d93ae583cc362aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:22:14 +0000

    thin accommodation tread cost discuss

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
