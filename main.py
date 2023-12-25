8d61f28ac1055d68c60ed6d0d954e4b13ffb11d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:01:21 +0000

    secret fasten major impressed then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
