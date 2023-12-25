aa6cc8b2e2c20dc58adaa5b114e720c4f1302d95
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:42:21 +0000

    wave pretend sit weak question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
