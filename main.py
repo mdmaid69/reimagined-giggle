f4032283995e5598d3002963c33a60451ef87a0e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:34:14 +0000

    excuse light wish better finally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def split_path(path):
+        return os.path.split(path)
