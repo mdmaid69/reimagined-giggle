86caf506e6c66b727403baf62d68df040a01c6d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:44:43 +0000

    week story course odd brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
