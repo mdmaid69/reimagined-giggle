eb20f2e36aecde41dd7e112348f66cfaec13d77f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:41 +0000

    frightened century rest advanced actual

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
