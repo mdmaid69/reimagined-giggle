b6d7428c3adec046869eab4a2a894a387f3e0662
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:22 +0000

    comfort tour rule accidental second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
