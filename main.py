50f46f931e3d3a28472ec1e7594a39a4b387bfc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:53:37 +0000

    relative instant study needle number

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
