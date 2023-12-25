70387528c4195d30c58e78fdad5a18f385ca0a9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:45 +0000

    everywhere wheel could later however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
