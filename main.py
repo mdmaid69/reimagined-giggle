8f4b05934dfaf4088bec4382c71a6adcddc0a0fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:11:11 +0000

    weak successful somebody convince regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
