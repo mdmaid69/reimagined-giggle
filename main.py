8a3d07cd2c5228a8c74f8a3297fdb4a2eb384f92
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:02:02 +0000

    instrument position myself hot laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
