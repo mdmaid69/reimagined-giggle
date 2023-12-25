c980a2e6f20834f385f1b0596dc55c849259b129
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:38:41 +0000

    head entrance turnout Gg confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+print(sys.version)
