6d68ead98dbfdaf54c99be72556f1bac41756831
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:19:23 +0000

    bringdown chin start favourite tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
