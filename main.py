6added4abec487a82cd8b1fa3848e40c304c414a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:13:09 +0000

    mark past address earn event

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
