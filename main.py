8451e078038cb84b6fd7397c603a19efbb3767d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:55:20 +0000

    all giveback ticket original anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
