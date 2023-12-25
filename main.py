543b96839d901e240592dc0c95fc4f62a4c7881e
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:52:00 +0000

    fail lock disappear possession evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
