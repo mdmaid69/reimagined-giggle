f4072663ceca740cb5a83143f079ba04d1a9cedc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:00:28 +0000

    charge design replace squeeze 565end

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+print([x**2 for x in range(10)])
