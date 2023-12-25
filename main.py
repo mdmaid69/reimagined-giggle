cabc4fe0c22f08211f6c3365a4520b0edce8e83b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:59 +0000

    study present emphasis aware dig

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
