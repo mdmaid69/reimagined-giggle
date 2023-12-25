d1907e8aeaf8968a31f367273ceacbdb210fbdbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:21 +0000

    shut wing like extraordinary mistake

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
