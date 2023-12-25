d6bf03e4fc89b81d30af92166ecb058cdfb198a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:11 +0000

    people butter introduce centimetre vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+print("Hello, world!")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
