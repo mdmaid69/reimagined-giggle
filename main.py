506817ecdb9e5276ccc4569974652e4fb63f1247
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:52:07 +0000

    explain dance front count shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
