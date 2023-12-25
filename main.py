29c08e9a450835cba1a4d0d80ea8477906c98913
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:58 +0000

    section feed point in Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
