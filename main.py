f3038b7a8f8df7013b3aca6be305fb530efc780b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:32:24 +0000

    see lock learn avoid step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_error_function(x):
+        return math.erf(x)
