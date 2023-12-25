82726a1e79dd0b0daf00570306cd573c775cc495
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:44:18 +0000

    break wash could bush road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
