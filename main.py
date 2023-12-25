4d45b52c5a7e3b783196c1d65a37810f5d598198
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:23:45 +0000

    patient similar for break name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_work(force, distance):
+        return force * distance
