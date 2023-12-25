b73fae20ee31c1ac576d9eaf2fdc99cbbd58d4c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:41:12 +0000

    cool accommodation study left smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
