41c19799e5b77ea8af1fde2727706ea7bb0b1e9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:47 +0000

    bank create plan children house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
