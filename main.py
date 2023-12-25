90d57ce8df47c730dd672d803d7956129b01d632
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:13:55 +0000

    ocean promise shake justice drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import functools
 def memoize(func):
         cache = {}
