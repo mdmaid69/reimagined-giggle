902a20f100b198f3c594a41129a0ebcd53f4cb66
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:10:11 +0000

    least cry trousers result instant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
