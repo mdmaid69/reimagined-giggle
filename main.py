6f6e7f05eb63f029da6570c172a537504f35e1b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:26 +0000

    afterwards wipe since warn sign

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
