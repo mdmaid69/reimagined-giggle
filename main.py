ebac6d6c719fdb0f37b0ce4b792313c5efe2d421
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:08:46 +0000

    except considering successful list passenger

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-print(sys.version)
+def calculate_density(mass, volume):
+        return mass / volume
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
