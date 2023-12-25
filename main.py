de27762cc83f17f897d2452d4efb97b5fd135c3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:01 +0000

    565end bunch artificial proud theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
