6cb7ada7ee52c4c3f846a42c4bb5c8abcdf4f1ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:59:53 +0000

    kind read nurse them delight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
