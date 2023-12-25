92fe319ef1967a38b812c4ef50ff0eb8527c780f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:28:44 +0000

    message side laugh snow happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
