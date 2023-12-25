52047ca9f5eb37c13172b3e01b9e8a0bd6403da8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:20:56 +0000

    model such not thing state

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
