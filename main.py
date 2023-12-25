1deae50f9ff3ce57c5465acfe1678967a56895df
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:18:13 +0000

    place computer little before block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
