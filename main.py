bfddb27cbfa1dc6ddcbbefa4600bd8209db19e1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:39:12 +0000

    opportunity trouble sign nearly alone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
