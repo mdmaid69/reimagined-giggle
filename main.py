4ab75014d0d808137a542eddbcfa42e4cee89843
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:08:50 +0000

    down scene interview repair keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_stack():
+        return collections.deque()
