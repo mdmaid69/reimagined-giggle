16b1505c8cb0c86e8d149915c1610f2a44cb0408
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:26 +0000

    by lookup steam tired workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
