575b49115fdb848d39d44fd3dae88f29867f47e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:30 +0000

    habit pair phone classroom course

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
