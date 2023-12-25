94ee4e856c48f20cb66528c982ba643701b86016
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:24 +0000

    hour bite impatient cough below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_float(array):
+        return float(array[0])
