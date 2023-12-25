845873b484af45797d08f89410d89a1959f5abed
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:53 +0000

    closed expensive theirs beneath exactly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_volume(length, width, height):
+        return length * width * height
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
