04479adcc14ab8eef84f09a7037fe79ab248b1fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:29:35 +0000

    march tie tooth out dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
