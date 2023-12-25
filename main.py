dc4f1173eb2603bbaa59b73b59dacce5d55761c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:10:04 +0000

    meet run hello movement capital

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-def convert_to_octal(n):
-        return oct(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
