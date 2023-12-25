f2a8eb5fd66537e19a43583dd9a071300d5a42f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:01:25 +0000

    evidence performance slowly runaway large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
