c5712ce4927193d2880458af5bf870bf27394fb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:20:56 +0000

    getback manner handle raw wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def count_elements(lst):
-        return len(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
