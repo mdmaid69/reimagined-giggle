729bebf761226f2a7780a8d5ecab830cf0ea2190
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:56:22 +0000

    towards sail copy stay concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
