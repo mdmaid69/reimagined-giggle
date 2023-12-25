01381b2cbf1574c675d688cfa836ae895cd2f68d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:30:36 +0000

    bring burn sense get mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def create_user_string():
+        return collections.UserString()
