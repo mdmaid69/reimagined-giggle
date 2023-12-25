f78cd12db3db94af04fcfdbb6ac5afaeea3031c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:46 +0000

    die guess process to sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
