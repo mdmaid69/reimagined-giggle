2c3c4a70867ae200bfbaea57672dd4e8f85f690f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:56:30 +0000

    needle artificial spring board straight

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-def convert_to_octal(n):
-        return oct(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
