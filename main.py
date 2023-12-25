e6686bb4d61373f16ae3f13ce246c2470c806ecf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:42:23 +0000

    back dance transparent formal advanced

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
