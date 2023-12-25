f764a49c09ebf11ce2aa42529f9492e3ec6d1c3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:09:09 +0000

    repair confidence experiment return bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
