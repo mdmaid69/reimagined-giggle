d65ff3089f3b8635ad76a5586726da030a9a9e86
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:16:10 +0000

    zero shallow among somehow fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+name = "Python"
+print("Hello,", name)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
