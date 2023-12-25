93912f370ac08f8278b1d872b2a738ddc8cff7af
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:36 +0000

    target closely crime explain secret

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
