c38b78f0faa2db734c4eaa3d8e9ff7e03a0a1fca
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:43:34 +0000

    join bath circle trousers dream

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
