4309b1813f065127f998c86de986fe2bb2476732
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:55:29 +0000

    determined criticism well practical shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def reverse_array(array):
+        array.reverse()
