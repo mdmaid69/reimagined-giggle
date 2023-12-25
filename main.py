1e50c9d12525f3814f3e44cc83ef1dba97b3cd41
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:55:00 +0000

    hang best field disagree guide

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
