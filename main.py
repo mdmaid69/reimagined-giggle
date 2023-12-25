1ef6a6529de439846885ba4ca168e38e0974cbbd
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:19:32 +0000

    suppose rely inside him direct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
