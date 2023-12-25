599450cfb9d287ea07ae6cac5d009bdd2fb2066e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:04:25 +0000

    proper entry point ordinary entrance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
