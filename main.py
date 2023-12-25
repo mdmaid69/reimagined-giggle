0c759818c1504eef60cdd72c656df99ffbadee8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:22:07 +0000

    slip copy sell inside confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
