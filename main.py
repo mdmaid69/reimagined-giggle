a4fbb9b646d99367d5709342cf2b66d21a5175f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:19:13 +0000

    anywhere problem during away Ww

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
