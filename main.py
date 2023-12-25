0d3746178ce49538f53402c094fddae46956246e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:49:11 +0000

    central instant aware reason accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
