dc0655c77d37e272e1390cae32c67ccc35781a2f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:25:16 +0000

    wear exact congratulate movement late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-print(sys.version)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
