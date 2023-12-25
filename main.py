28f102ac7f71bb37551e29353b721f674ee23fe2
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:30 +0000

    rank hate burst dark thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import platform
+def get_python_version():
+        return platform.python_version()
