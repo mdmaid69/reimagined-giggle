1b664ea8008fff242d0d862b25a81dbb796ccce5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:54:25 +0000

    address no spot frightened across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tensorflow as tf
+print(tf.__version__)
