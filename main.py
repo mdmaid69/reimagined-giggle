fcdbd49445cca8e2906146d9bcb9819ee3160fa9
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:46:23 +0000

    speak settle confidence last mad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
