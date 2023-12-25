2ab0262f9556e3f1c501ec0feb7b3830edfccd97
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:23 +0000

    lock read rain hope false

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def clear_array(array):
-        array *= 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def create_priority_queue():
+        return collections.deque()
