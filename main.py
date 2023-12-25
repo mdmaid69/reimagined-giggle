85b03680b218ce6df6938c4481567a0d889f3453
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:57:47 +0000

    wish popular pause declare myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
