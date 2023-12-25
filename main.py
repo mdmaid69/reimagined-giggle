ce1159c386c06a7674359aa2bbc583786f6be33b
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:53:15 +0000

    reply moon warning several oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
