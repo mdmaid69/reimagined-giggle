92d3c50008447f32387b13f43492b8be3fe81904
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:04:17 +0000

    Kk rent team impatient exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
