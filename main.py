2a85f14d2e8ace4a9fa03b376e85acef3bbd75cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:11 +0000

    nor apply software proud use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
