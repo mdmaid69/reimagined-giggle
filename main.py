43632462109db7aa78ccf330b1714d0cb2cc31d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:34:30 +0000

    little base closely ever battery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
