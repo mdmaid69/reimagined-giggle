a3e415ec1eba2ba3594241a5e609214666ffa976
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:20 +0000

    wing point to change once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
