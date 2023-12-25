f8ffba14d100820957975b7956729ecbf95781b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:42 +0000

    fast measure afterwards among calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
