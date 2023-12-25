1f778e367327d2b7850cdbd86e8197159dfd81ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:37 +0000

    repair centimetre mysterious bus middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
