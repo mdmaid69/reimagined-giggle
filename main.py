b6ac3c8febd95265126179e76ebafaa65e4d66ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:46:26 +0000

    grow delicate near cook threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
