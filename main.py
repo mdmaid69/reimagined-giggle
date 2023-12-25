96eea07b9aa9ecdbdd175945e235eaa9f068d722
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:58 +0000

    share image acceptable soak bag

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
