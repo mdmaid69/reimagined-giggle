74876a37cca54fe944f7018a8f56c4d004a3b10c
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:45 +0000

    best callback main survive explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
