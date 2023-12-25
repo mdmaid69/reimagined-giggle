6d184444ecd2bb40a9f9321854143a8e1abbe76a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:14 +0000

    child less cause book intend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
