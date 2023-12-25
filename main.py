cabfef7b6e0d66fdf221a4efca84beed316ca682
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:23 +0000

    country force joke ride performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
