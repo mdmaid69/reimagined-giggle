af474257eb4d40050798a38059a02a15f924f224
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:59:31 +0000

    unkind problem fortunately heat keen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
