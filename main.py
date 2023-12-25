64e592ceb585266e9e3668ad30371059118d8a45
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:01 +0000

    set disturbing like party congratulate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
