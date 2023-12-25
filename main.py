46e092ac1fa4a76fe4558762684c78ae82511354
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:23 +0000

    train because protest night coffee

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
