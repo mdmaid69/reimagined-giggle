b0fb791870490540875a5fcf89fda72c7d8b738f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:06 +0000

    sale slowly confused start could

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
