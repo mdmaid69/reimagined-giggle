f038f776e91b74f725cc56f8c8752b2ca5eb968b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:20:41 +0000

    object tidy goon wide step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
