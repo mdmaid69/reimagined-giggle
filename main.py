d2c649cf2c6e30ba9848040bf037462ccd1398ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:49:44 +0000

    stiff broad finger OKorokay disturb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
