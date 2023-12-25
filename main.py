fb3f06edc399acfa5ba0c7c9422bc2d1cf59c817
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:15:32 +0000

    plastic hospital estimate admit unite

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def multiply_numbers(x, y):
-        return x * y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
