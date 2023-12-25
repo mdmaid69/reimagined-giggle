21dd73d6859534c13c00e9137cbe72d25f2ac764
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:35 +0000

    573entertainment production Uu guess serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
