742f3f6c6c01273703ec451ae66d7d91d6d2f80f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:19 +0000

    collect behaviour afternoon or contrast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
