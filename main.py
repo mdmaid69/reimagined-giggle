757278dc8fe4cad2067e189e097c8a0a06b73a69
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:41:19 +0000

    callup calmly wait 571enough aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
