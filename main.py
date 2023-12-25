9529da208c6380eb12347157c5939de9e3a9e96c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:59 +0000

    open moral slice look shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def reverse_string(s):
-        return s[::-1]
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
