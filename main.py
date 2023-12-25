2a0bba8a4ad5c44b321ca061510a3787bd5a7cad
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:46 +0000

    rarely protest language smile bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
