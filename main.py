d2f2f68765e7f8af084a04234757db540726c8cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:51:43 +0000

    might quality punishment prove prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
