bc89f4ae5d26ebf207fed7533b00e3c92ace951d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:31 +0000

    play at design visit here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
