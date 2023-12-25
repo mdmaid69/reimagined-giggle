32a8f19cd16922d80b0641933784a958e5a901c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:52 +0000

    turn there sound her unit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
