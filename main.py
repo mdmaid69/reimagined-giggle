3caf50e906a27edb77153cc5e68a51f92a545d6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:13 +0000

    window scene rush holdup effort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
