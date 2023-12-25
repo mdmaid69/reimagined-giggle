86626b1676890ffb55405925e4d0037a04040dba
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:47 +0000

    plenty force clean spend thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
