bd72e9f0d240706921b192df2f87e073806bee49
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:57 +0000

    pull law basis arrive change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
