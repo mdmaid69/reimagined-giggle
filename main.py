eeb257f983a1ad8adb5fee5d82cda503e25ed7b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:04 +0000

    address receive accident introduction bath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import collections
+def create_queue():
+        return collections.deque()
