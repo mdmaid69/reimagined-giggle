5bddc92f4dc509d840d29b4fbe4e497e71625688
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:58 +0000

    seat pronounce idea preparation equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
