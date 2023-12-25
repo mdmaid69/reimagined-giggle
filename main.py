03b090e0184756ffd36f800d2e8250ea97b9d6c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:39 +0000

    appeal write test while moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
