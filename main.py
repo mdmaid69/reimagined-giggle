528b1efe79814388050d03fc4b0f7b2ad22682c9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:59:30 +0000

    lock city close key yet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_float(array):
+        return float(array[0])
