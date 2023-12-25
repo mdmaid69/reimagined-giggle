78f115ea57862683681177218b1d8c31d322ca8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:41 +0000

    insist doubt tune nowhere draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
