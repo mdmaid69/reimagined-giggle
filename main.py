03b5997ad9b13afb5d4e980304c03ec257fe6376
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:09:28 +0000

    behind shake congratulations OKorokay test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
