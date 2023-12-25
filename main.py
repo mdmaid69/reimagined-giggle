251f8a1029040c480f009f08cfe0ed00505ce5f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:30 +0000

    interrupt last can information head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
