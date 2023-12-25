3fcb36e4b25e58c1fdc11807259931696334f2b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:44 +0000

    command memory mixture store 179bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
