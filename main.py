119819d9390488fa646cfde94bda83de312b13e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:29 +0000

    shout beat flavour callout bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
