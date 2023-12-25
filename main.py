6159743a40eb837099409fadbda73d92661c3f8b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:42:28 +0000

    phone mix circumstance object month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_pressure(force, area):
+        return force / area
