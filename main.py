577392048f8dfd4d74a3951a518d0d06525cf514
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:31 +0000

    as sudden spend poor round

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
