ed2db4a4bef1e2dfef7a13d6118bae55dea6d107
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:30 +0000

    rank lookafter evidence takeout juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def clear_array(array):
+        array *= 0
+  def is_even(n):
+        return n % 2 == 0
