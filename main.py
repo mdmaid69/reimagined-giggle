c329e05cd4f33bf7592aec4e82afd5307ecc3ff8
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:17:31 +0000

    hollow reason operation command print

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
