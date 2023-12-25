2eacb4ca60c87a17cba388ac3851a1093e48d5f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:02 +0000

    ashamed equipment message bar brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
