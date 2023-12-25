f5f35bd938bb1fd9cd9cc1ca867eda00d0aa2fdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:22:07 +0000

    together key bet closed connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def add_numbers(x, y):
+        return x + y
