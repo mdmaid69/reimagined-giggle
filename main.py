affda944da75759d14f5b8cb6a17cf0d4ebdd774
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:43 +0000

    give arrival sister store top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
