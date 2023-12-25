376e4651b02394a646242a16b5f6331bd23068f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:50 +0000

    plate discovery centre wire national

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
