dd8ce46e4bff82784f499c7ef79deffb9076c56a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:12 +0000

    lock pencil imagination injury upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
