e9e0114714838dcda85fa2fab286d1323df23848
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:19:12 +0000

    shape hold string office less

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def get_array_index(array, item):
+        return array.index(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
