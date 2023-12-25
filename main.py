c68eacb396eb22f94a18bcc8bdd52a8a9361060e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:01:26 +0000

    holiday shut target bringup normal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_queue():
+        return collections.deque()
