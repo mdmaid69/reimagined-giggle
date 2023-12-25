0592094161ee962633f660ad9d195d6936f9ea70
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:26:36 +0000

    lower reply woman comfort way

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
