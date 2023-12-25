21cea0441d46b9cd655fd365174df4c4ee37db30
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:23:26 +0000

    compare giveup young Nn try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
