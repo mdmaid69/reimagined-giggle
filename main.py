029eb9a9deae861a489b12341e4c5c599cf5fd82
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:41 +0000

    sink appeal scheme avoid necessary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
