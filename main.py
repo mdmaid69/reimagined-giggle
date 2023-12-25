ba4f42e4536a59099ab5672503978b6ec9f9156c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:29:42 +0000

    crowd stick realize worst aware

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
