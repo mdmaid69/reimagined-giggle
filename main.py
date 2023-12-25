f9083556f2eb1b512e663d2899d80223150bf435
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:55:30 +0000

    delight might unhappy someone interfere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
