b15fd91be54a568aea35910f8304f47a7227842e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:36:16 +0000

    holiday question risk original stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
