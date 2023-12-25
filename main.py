79a390fadc856f113598ff914792b1fbab757aa3
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:06 +0000

    typical emphasis over tip Vv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_elements(lst):
+        return len(lst)
