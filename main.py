3c888c139ee63fe60788200c8e9202e5b7acec59
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:04:42 +0000

    die you just ashamed especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(a, b):
-        return a + b
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
