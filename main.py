f69127278ad10a2530d21a70ce86e4d64b922095
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:22 +0000

    land content giveback fast wing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_int(array):
+        return int(array[0])
