7e6fe9c565c0f7b8d923edb49e53777e3599152d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:47:55 +0000

    geton press further substance freeze

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
