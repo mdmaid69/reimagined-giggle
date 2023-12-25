3724f70c606883bd74429eeb0adab027ab807e37
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:06:01 +0000

    switch worth advise tense excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_as_repr(array):
+        return repr(array)
+x = 10
+y = 20
+print("Sum:", x + y)
