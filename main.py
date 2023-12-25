a8dfe61e2ffa1ab57df958a15ce23667ac8f50c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:17 +0000

    voice become arrival cause today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
