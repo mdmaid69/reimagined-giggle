2b648bbf715af45b6c29c9305cac5be018d8d277
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:07:06 +0000

    pleasant education minimum message height

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
