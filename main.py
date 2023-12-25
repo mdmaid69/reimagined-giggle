4bf6d5ee8610b280a459aee242203af6ff458f59
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:29 +0000

    very persuade play that row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
