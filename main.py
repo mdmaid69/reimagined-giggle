f94bbbbcf685fd18df76d1b14671fb46a6c7e49e
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:09:18 +0000

    unlucky left pupil help tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def append_to_array(array, item):
+        array.append(item)
