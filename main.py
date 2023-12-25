2673f71db9c4c5afdbfac68aae6b61b36365c0c7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:57 +0000

    than treat constantly lose scrap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
