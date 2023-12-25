45881bcc380330ffdc03524527d69a831f2d926b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:55:38 +0000

    family air have snow demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
