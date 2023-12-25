4d4f33cd8107003db6f09b02963ab07173cc483d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:00:25 +0000

    slow keepfrom smooth stone unusual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def is_odd(n):
+        return n % 2 != 0
