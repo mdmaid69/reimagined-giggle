b0c229adb4b22e18d3fccca43f16943f4eff5bf3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:30:52 +0000

    special commit actual odd at

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
