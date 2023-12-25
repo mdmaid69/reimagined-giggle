7cdd41337d07908dbe1d76c72401a868983c8c10
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:32:20 +0000

    past awake city high appearance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-name = "Python"
-print("Hello,", name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
