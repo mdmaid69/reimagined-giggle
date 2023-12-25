5a6ad43f3427d5b34968729726a2c4cf6675006f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:38:53 +0000

    everyone choice scream ago move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
