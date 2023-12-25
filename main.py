e2e0d8f462b783d609383fe29058449e68524333
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:02 +0000

    track believe past grade approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
