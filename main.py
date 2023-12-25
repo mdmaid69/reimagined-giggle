6bd43cdc4a5993d95189f768a95a1de2aad087c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:37:55 +0000

    steep key acceptable help grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
