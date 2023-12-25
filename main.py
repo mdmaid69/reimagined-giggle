fc81f0dc6be7bcd4f0dcca0b4f3f1772f6345603
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:45:40 +0000

    blind ready coffee goback(to) free

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-name = "Python"
-print("Hello,", name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
