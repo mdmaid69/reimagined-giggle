a294bf6b653fc3106c25555f6a5526881fb9a8b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:11 +0000

    giveup major heavy alone succeed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
