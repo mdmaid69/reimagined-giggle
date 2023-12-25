56462fcc8ecb4cdd6c7b0a438a6132ebc6cd2f1f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:12 +0000

    search hair ilovedicks dig dull

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
