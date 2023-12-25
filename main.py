ccf15d5fa39f2e31488f202db4dbcc33de3eafe1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:43 +0000

    burn loose appearance minimum operation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def count_elements(lst):
+        return len(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
