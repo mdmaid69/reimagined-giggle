303ee83e42ffe81fe0ce58464fd28914153c852a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:50:44 +0000

    trap cook team shape protect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
