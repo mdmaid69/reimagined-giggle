df018efa67290892c2db2e5611a6e8e4bda2b83c
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:47:25 +0000

    except cover blame bread simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_set(array):
+        return set(array)
