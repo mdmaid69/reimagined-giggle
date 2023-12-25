b654809cd1ce2b1c785a0820cfd276c0fc4cefe6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:19 +0000

    not show whistle total dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
