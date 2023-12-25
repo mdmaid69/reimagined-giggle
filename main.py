a59acaba6d482352ac37c4e0fe985fc110f79598
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:19:26 +0000

    various him bother dinner keepup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
