dd885a61b95c272791e8ad0f9f97583318af7be8
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:20 +0000

    comeup train sore tread fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
