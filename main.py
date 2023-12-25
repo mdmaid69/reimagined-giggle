cfc7af1c32e28b861153f63f056e5708dbf05190
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:04:43 +0000

    fresh tidy callat attract result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
