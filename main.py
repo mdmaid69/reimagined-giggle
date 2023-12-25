06c7525b0a52c55954c9ad8bf511c94c321a1523
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:43:21 +0000

    possibility freeze side write zero

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_work(force, distance):
-        return force * distance
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
