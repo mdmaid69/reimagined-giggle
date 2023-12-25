8270145009687927e86035ecaa2ad7b0682cb78a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:22:30 +0000

    snow fault aware tap dangerous

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import random
-print(random.randint(0, 100))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
