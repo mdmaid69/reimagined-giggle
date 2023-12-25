5e0d59163887f5536d651c0f972a8e15f28e0940
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:22:58 +0000

    advanced trap possibly spill Ll

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
