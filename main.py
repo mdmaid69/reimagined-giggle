ecbf42c48fa47be2e806e98262c15b7e7a04b29a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:59:46 +0000

    office behind polite coffee evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
