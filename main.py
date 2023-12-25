ee46ef13bd6c2d6526a1e636e5fc17598014e439
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:05 +0000

    angry weigh quality side detail

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
