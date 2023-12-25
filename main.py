2a681eae6eb5a9fe4a78b41b8ffc0719b2337ec0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:09:54 +0000

    even left wrong terrible wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
