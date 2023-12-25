bebbbf4ee80aada8e290bc0b608902fa559a5018
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:16:29 +0000

    interruption still variety lend strength

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def reverse_list(lst):
+        return lst[::-1]
