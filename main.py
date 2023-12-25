c544d264fd3ddf9548edf6e269055d1ce59dc8b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:20 +0000

    into goout quantity warning classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
