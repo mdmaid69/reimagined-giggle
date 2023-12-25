bea8b30763f4cc910a5df3153c962eefd9d71f06
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:39 +0000

    pay as view disturb rank

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import math
 def calculate_cylinder_volume(radius, height):
         return math.pi * radius**2 * height
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
