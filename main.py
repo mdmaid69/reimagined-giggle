e57b7c857d3fa87959e3ea66fffd24da15bae40a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:32:00 +0000

    mark influence fortunate excellent ear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def sort_list(lst):
+        return sorted(lst)
