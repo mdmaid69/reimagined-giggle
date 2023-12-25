159ccb6e4a1c0caf6d01a91b8ba81f6d78e512f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:04 +0000

    separate might dance comeoff book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
