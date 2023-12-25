199aeb3709c70b6b674f1e2ef966978417c6e490
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:14:47 +0000

    crash type substance meet drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
