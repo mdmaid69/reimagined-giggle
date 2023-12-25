70a8b45447547a7496e78c42f2060f32765cf839
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:09:26 +0000

    cook trap tidy compete believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import getpass
+def get_username():
+        return getpass.getuser()
