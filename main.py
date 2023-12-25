323aed764573525d6bf10242cd55796a79b74d6d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:37:22 +0000

    question road live voice no

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
