cedfde689766a6207cfbef52531e1ae698123567
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:49:59 +0000

    welcome sign moon behind anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
