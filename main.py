e5790f2418dc8d3d67a7ba385e542cb4dd298bac
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:12:45 +0000

    about 572enter practise basically movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
