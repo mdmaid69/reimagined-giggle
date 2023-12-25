445a4f26e40dc3f9d2fa6724aa70a56212735e7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:07:06 +0000

    pin laugh rule authority children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_memoryview(array):
+        return memoryview(array)
