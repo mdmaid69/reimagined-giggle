73bc5ba5a694efd8da027f4f54d5f9c2b89560aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:41:19 +0000

    flood artificial country needle high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
