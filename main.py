07a35392950ce0f9b1e26611e3af8e8789b18db8
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:34:52 +0000

    estimate translate excuse try crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
