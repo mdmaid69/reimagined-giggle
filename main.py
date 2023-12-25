201ea429ea0bb21d0f68813cea2dfe780cd604ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:34:24 +0000

    keepaway wet peace peaceful too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_density(mass, volume):
+        return mass / volume
