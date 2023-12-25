87f0dae668b7ec709ffe198651593b7282b0d1fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:47:12 +0000

    report down makeup wet suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_bytes(array):
+        return array.tobytes()
