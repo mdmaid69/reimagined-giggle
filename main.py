9ac719765eb7235d175a32c6ff5ce13eabb75ee0
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:16:16 +0000

    store offend attempt person bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def add_numbers(a, b):
-        return a + b
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
