ae72d659d9708cffb5af773e261a4b8865c80795
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:39 +0000

    unlikely terrible ring variety rare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def square_number(x):
+        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
