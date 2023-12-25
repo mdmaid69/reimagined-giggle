0b58dd4f6a468528f2949f7b606d9825fccfaffe
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:11:00 +0000

    pattern any likely drink lookaround

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
