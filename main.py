b5465251785dde7572cbed3a8ab776a67ccf140f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:07 +0000

    into artificial anywayanyhow left fact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_power(work, time):
+        return work / time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
