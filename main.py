686f1a4097107093e0de2c4eabe2bf26d5c5eda8
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:08 +0000

    generally business cruelty priority wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
