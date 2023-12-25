5f052bd18ae79e061729abbdd43989f68e6221b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:58 +0000

    pretty now some positive blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+for i in range(5):
+        print(i)
