de7198275ad8378d0d6bff0d1a4a13d502a6044c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:00 +0000

    relative Qq another rain with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_base_name(path):
+        return os.path.basename(path)
