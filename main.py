5318698083cfa04fe1362f27736f698f900774e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:10 +0000

    copy switch middle practice trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_bytes_from_array(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
