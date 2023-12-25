d3a1fe5f16bef571f20d5235b37d8f475f5fab27
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:24 +0000

    subject far clean car height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
