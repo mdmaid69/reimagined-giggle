45bf6f13b019233284a990ba5a45837cdae56670
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:46 +0000

    mind to everything see count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
