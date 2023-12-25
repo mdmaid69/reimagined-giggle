58b5f90fd0fd2b567c098f350a594aca9c08f6fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:28:42 +0000

    feature separate moral tune certain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
