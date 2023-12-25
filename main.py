c76a281e8130151e1b532b318a542ab7cf48d603
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:29 +0000

    phone excellent nothing save plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
