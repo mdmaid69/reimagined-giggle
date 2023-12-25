46e6165023d97289877255e7121b0e87fc80146c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:12:05 +0000

    word illness talk crash fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
