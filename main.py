fb44c9c13e94474c4e19e9b677ab95cd6c77bd50
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:12 +0000

    throw wear substance circumstance bye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_palindrome(s):
+        return s == s[::-1]
