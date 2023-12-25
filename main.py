530fbd6bf65eeca931dbf8098a3787cd6137d7b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:37:16 +0000

    out copy tip dick or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+def exit_program():
+        sys.exit()
