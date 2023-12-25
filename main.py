cdcfeeb913c289d5ad3bf6a32cbb79125fe32a12
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:26:35 +0000

    education remark sister vanish sometimes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
