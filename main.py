6776a96c9141b269dd13cc1b2a1a853a8f50af48
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:18:23 +0000

    guilty off plain comeout cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
