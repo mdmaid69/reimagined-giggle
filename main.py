24ea3e466231ba8cd2d8d1ad35a2ca147755a61b
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:37:44 +0000

    can freeze offence far expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
