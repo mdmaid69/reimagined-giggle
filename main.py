d58c94a5f492c0835ce5de2f66add2ba8ae54961
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:27:11 +0000

    you suckme still offend wall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def count_elements(lst):
-        return len(lst)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
