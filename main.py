b405d01ca0aec1e4ed6fd48e70dbd4f570677818
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:41:42 +0000

    schedule sting stretch immediate anyone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
