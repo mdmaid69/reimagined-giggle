92a39727f6107ea746eefbfc0c46689bf795db6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:25 +0000

    although value separate point flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_itemsize(array):
+        return array.itemsize
