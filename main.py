a1c3702d445a906c3ecda492180b4eae11a8f2cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:07:48 +0000

    callback factory tap protect completely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def subtract_numbers(x, y):
+        return x - y
+def reverse_string(s):
+        return s[::-1]
