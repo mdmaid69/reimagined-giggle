b6bc220352ad394ab2ebee3531280e0a8dbecccf
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:40:51 +0000

    plenty suffer that treatment have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
