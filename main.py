ca596e607d67cc21f1ebe910c175f7fc670fa86f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:13 +0000

    emphasize impatient legal everybody comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
