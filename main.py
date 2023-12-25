1b82662ce488492af574fd54798b14861c15c854
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:50 +0000

    fierce catch develop recommend above

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def is_palindrome(s):
-        return s == s[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
