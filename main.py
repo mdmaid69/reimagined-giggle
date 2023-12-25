ffba8a3eeba1ded75873c2fa19a8dc54a10e69e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:53:27 +0000

    live tool term her continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
