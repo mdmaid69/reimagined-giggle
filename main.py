ef3a88266a54f22937ccd3510ff27ccc1d4b42dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:55 +0000

    keen whistle pass unfortunate hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
