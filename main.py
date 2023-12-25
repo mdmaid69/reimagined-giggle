6196ec69ee2cfecb065c7f5cd7cd9783772c41f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:18 +0000

    stir difficulty cinema hope bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
