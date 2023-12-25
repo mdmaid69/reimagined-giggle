8358e01f361e90a840c48a79ef06ef99a43cb393
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:57 +0000

    job tough snow objection upset

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(10): print(i)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
