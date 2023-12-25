632910bc43ef04b7233a5dd1be048660f7ed3e38
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:20:50 +0000

    just like extra sink drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
