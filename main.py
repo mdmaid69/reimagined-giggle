28fd25e3df2f86aad2e2cbc6aa8f288035b85c94
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:40 +0000

    noisy spill scared think would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
