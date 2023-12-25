f38332ff8bf9ec92e8f7b9a7c50dc93564a0781c
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:43:53 +0000

    now meanwhile spare press its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
