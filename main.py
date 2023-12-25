2756189029155212a37b83ee37270fd75db9a465
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:15:54 +0000

    food people kind operate shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_unique_words(sentence):
+        return set(sentence.split())
