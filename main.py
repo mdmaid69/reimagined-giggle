073cf9ccb4209d6a257090caf5180c109fb23674
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:39:19 +0000

    cook environment spot commit doubt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
