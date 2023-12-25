700c921faafbb3f6cecb059b95245dce0edc8db6
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:48 +0000

    afternoon good trick grow satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
