257c53664810795820a32aae58c6a437f1f0c58c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:13 +0000

    reach since health pause too

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+for i in range(10): print(i)
