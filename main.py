2180e00052f8ba0266f47ec9841c01a70eb2c7f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:56:38 +0000

    consist score guard total period

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def add_numbers(x, y):
+        return x + y
