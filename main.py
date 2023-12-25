e7d9c31d6065d7f8eda66f7006978b146a2c2a27
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:44:46 +0000

    lead whoever unkind short somehow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
