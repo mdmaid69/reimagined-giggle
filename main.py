f6c6babc10c2610cab7cf19bf09aae0a730e1ada
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:55:50 +0000

    part raw hers show wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+name = "Python"
+print("Hello,", name)
