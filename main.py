ca625346d9f098358b2cf21a1f174d223d51bce6
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:20:31 +0000

    hour aim gentle us need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
