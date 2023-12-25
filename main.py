c8abee77f63acbc2c52fb3a6f58a7cdf962cad81
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:35:56 +0000

    cruel fix smile try disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
