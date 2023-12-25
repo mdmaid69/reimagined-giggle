1fe9fa1a2b5f2bc1878e1f8623bb72e977785041
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:20 +0000

    border congratulate attempt coat explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
