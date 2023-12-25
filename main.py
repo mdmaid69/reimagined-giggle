400327fb54078dfcc4a40240029870be6110a4f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:51 +0000

    do contact risk way change

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
