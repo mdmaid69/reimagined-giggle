fea7b2a4bfdb780944ca8fddcfa2d798e779a8a5
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:11 +0000

    window strange view in soon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
