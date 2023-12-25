ad1b22e9fdd8122786179ed0b746787d80b51f3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:20 +0000

    scheme seriously theirs cut false

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
