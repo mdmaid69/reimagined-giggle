3788bc35440c2f83173889b33126d39247991bc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:26:40 +0000

    wave inside typical tie play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
