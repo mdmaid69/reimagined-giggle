36e6d705765aaad908018063fa70a12b3a47a029
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:46 +0000

    yet across suckme pattern collect

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
