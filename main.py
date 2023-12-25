6100c35cb78207bf16b320d941cb01ed18c295bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:54:09 +0000

    worth common straight forget cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
