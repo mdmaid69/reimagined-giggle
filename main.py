ee22c7ad448c6d3e92eb07ca524e0e32f300082d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:37:25 +0000

    pretty someone no read calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
