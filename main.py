70c0ca43f65f718c4cd2037e1b86ae43ab5366f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:53 +0000

    bye result name second rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+print(sum(range(10)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
