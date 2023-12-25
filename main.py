5e81ad034e4f59268378ca6cd0073b83b8a52c67
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:49 +0000

    sorry but crowd heavy bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
