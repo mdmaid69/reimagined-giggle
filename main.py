5a06a111d2a8ecae62fb3d00f3fc913c789779fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:41:49 +0000

    enthusiastic excellent violent water telephone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
