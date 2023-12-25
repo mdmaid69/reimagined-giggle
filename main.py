f0d284fc4201d71e5362f67734912ce0bd65ee08
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:53 +0000

    thick dead exchange settle tough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
