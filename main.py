e375fa4b75ff015484a469529ea5604a814ddabc
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:02:13 +0000

    reasonable bring comefrom foreign trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
