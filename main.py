ab4b90ac0f71750edecce04ec47531dfd60686a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:29 +0000

    with mind forward effect likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
