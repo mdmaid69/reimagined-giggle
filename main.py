40ea0eab3ccb6ba476c2c013ac1fb25178e8ee6b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:23:17 +0000

    find far Oo use neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
