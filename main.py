15868d3c91ccd25da3027ac3f47a8b02ef9861d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:10:40 +0000

    later ourselves little hurry nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
