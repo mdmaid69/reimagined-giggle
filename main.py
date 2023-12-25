c765090254fb46698bb4626d5a0509eeca5cfdb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:20:06 +0000

    anything surface so exist record

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def convert_to_octal(n):
-        return oct(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
