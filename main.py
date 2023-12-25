4765e39920e14e1572c0f7d9d2ddaf1b0c8f5332
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:56:59 +0000

    extremely frightened rush quick silent

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def convert_to_octal(n):
+        return oct(n)
