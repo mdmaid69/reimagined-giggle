5197ca86896447a3d62853219f3eef9dee5a7352
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:51 +0000

    strike scream love description maintain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
