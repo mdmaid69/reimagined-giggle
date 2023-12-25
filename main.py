235e791eff56ae6f66296d482cb91b6bbcbd11b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:09:58 +0000

    dangerous family deep fat front

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def add_numbers(a, b):
+        return a + b
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
