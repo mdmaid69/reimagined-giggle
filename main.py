c910b16bdf85757f438078e0c7be17b6e9e2c977
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:34:23 +0000

    angry giveup occasional possibly gift

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
