bbebf41a8ae90ed676552f0f73c78cf89d4f28e8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:45:48 +0000

    notice getoff address joke cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
