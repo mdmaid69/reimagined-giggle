1ad467962edda733140d50679542e108efddecb9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:09:46 +0000

    before negative touch announce yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
