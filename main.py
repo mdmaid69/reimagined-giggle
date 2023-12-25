d89b0c3766aaf78a04b2923cdf626fe49f00421b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:44 +0000

    order mess only satisfactory match

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
