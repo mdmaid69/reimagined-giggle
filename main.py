c4e9f9eb02cf6585c7c2888dc163988d97ca6b17
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:09 +0000

    564encourage for round tradition believe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
