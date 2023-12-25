50630180e00560dd8ba77f96f684ab38a19ff4cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:36 +0000

    dare expression short protect unfair

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
