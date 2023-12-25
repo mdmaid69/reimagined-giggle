0eaf4a93fd8aef217ca89965516c3c7af62b0278
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:29 +0000

    care problem point if country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
