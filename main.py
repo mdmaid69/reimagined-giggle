47699c8ddc886de0d34cae15e843bacf38e7bbd4
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:58 +0000

    business medium discussion surprise refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
