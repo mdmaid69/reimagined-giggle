c7fa97838b3ff4acea02c79a0f007ae705e9dac4
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:18:00 +0000

    accommodation 567enemy truth nerve complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
