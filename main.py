343cc60e0f7703771ae96e73d9895c8becdf616f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:00:05 +0000

    bored theirs copy talkoutof female

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+print(os.getcwd())
