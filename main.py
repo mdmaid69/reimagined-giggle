126c63e7db55efe5ac80e7ee7dcba1b7810111d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:47:42 +0000

    worth appeal regularly step phone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
