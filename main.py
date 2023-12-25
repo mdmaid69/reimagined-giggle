2b5f590f6ea8f521fb03fefff8b17ad5a4dceb28
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:21:06 +0000

    period but sensible keepfrom anxious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
