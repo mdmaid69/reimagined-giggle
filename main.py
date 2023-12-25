a292a08e9bccbe5db185e95a365b0e46dd8738df
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:56:56 +0000

    keep workupto available twist yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def is_odd(n):
+        return n % 2 != 0
