1c6c543e8de5ace388926f49196dee80fbc81509
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:19 +0000

    me tall forward late question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
