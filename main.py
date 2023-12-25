aae27a695f384419ed87a2abed67ba668f77c726
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:48 +0000

    top bang stand its do

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
