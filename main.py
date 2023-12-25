e1a7c4cabf032f303784b571a0f8710659b9e184
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:32 +0000

    record fear principle feeling perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.ctime()
