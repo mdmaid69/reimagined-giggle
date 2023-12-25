649561935f227ef220b5e2ea07717fa636921e70
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:26 +0000

    tall own we follow active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
