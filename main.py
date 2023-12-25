523d27d6b3ff33b5f95cf87ae29dc6026657f5f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:09 +0000

    convenient low spend choose sensible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
