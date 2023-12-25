8bc205f534787fbbf1fe089f8b53c58d05eb2ad3
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:53:47 +0000

    colour dear birthday blame advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
