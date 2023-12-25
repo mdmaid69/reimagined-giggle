3b621eb627a754ab0135482f2d2ab21a60c35a76
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:24 +0000

    spite aim always recognize ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
