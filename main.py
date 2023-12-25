ec37389b4655b470481fdc3ea7fb1bb39571f79a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:15:06 +0000

    fear tax talk emotion evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-for i in range(10): print(i)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
