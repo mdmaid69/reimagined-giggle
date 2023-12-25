7c478b06d265daff5a9cc2197a5acca25a04afc1
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:54:37 +0000

    still happen label lead breakout

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
