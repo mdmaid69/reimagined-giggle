252da5275afbd6204b3296e6215158e53b56331f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:45:45 +0000

    beneath brother circumstance use record

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def greet(name):
+        print(f"Hello, {name}!")
