22fd161790174bfccc0047ec56ff9ce02eacec48
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:20 +0000

    return question neat calmly handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import platform
+def get_os_info():
+        return platform.uname()
