e69e2b44076967b28520ed4babe3f108bda6ce9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:33:42 +0000

    do I life suck progress

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
