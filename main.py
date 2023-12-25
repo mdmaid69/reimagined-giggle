b2b165be9ce58f844ea9f5cfc65aa315bd80d8ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:15 +0000

    sweep wash noone anywayanyhow immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
