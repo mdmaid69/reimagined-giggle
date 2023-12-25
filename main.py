11f76a3087ed394722c91dcad8bbd3c62c624674
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:07 +0000

    journey meal immediate share affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
