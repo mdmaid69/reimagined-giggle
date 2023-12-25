b894fba44f452115c04f699f538712ea89dad4b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:30:34 +0000

    worth mix negative back increase

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def add_numbers(x, y):
-        return x + y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
