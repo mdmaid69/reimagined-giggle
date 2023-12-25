9d4a1f874c87e22b308801d9eeb21f51acfbeb17
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:23 +0000

    police rob organization whatever excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
