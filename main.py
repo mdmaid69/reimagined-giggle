0873b0dd9f8ff383fd13a5de0061c021963c7dd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:23:44 +0000

    give knowledge clue face tragedy

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
