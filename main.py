204e45c58a2111823403a752b08a31e658afd0ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:21:48 +0000

    instrument trade left rather even

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
