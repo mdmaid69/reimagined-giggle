dd212ace4f310b42c5ce7b6f178e14f9593dc322
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:50:17 +0000

    boring busy phone turnup cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
