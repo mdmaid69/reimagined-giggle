e17f0242081148b44ba7955a0361af9d683b0540
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:48:38 +0000

    lookaround fear answer sound burst

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
