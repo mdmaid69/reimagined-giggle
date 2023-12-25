47225c8c918c8dd6446d8c330e388093b29eeee9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:02 +0000

    butter service turn phone file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
