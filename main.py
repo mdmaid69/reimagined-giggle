44b353a65ead18430ba08f86ebd034d3f1d41181
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:01:17 +0000

    Ee mark pretty how start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
