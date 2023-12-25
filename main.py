b3a463fe325bcc62209b53ea85de7387c6c29664
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:54:43 +0000

    tune relationship thinkof describe unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
