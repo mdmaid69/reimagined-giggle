87d7fa843adbe0fb0fa39cc313ff2c00476fb489
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:49 +0000

    bush lookaround arrival can steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
