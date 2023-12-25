d82063c5af35c4759a18bdad1432a3549e408131
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:20:26 +0000

    hold valuable cost mother expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
