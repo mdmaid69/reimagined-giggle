acc18b5ab5a4f04c8ffe99e71a5bbae0ad13b9a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:32 +0000

    less thirsty last wait goon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+import sys
+print(sys.version)
