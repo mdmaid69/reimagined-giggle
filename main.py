cf2ddfe9b860b8f53df79f4733c0f7308143b4e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:51:17 +0000

    project favourite enthusiastic condition together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
