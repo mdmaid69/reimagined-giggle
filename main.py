5f646bffc3619d250139f3f295c237e677002d5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:39 +0000

    accident organize although keepoff contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def greet(name):
+        print(f"Hello, {name}!")
