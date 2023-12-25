6168f6e224c54d1d4639c9b16c7d6cf9e169e0fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:45 +0000

    quick slightly compare hand against

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+name = "Python"
+print("Hello,", name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
