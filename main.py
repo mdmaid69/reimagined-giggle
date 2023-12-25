9ae76db7c1575891e3780070d863d60c057ae3c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:27 +0000

    coffee period your lookafter once

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
