8bdcef8ef55b0914b8035742008fce54e5a96559
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:20:10 +0000

    takeback extraordinary state succeed region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import time
+def get_time_since_epoch():
+        return time.time()
