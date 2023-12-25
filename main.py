1cb7a5e97b69e9e977ba7ae322f0fa0c34a16688
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:16 +0000

    release meaning low wide manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
