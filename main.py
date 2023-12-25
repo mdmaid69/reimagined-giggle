866f79df8d820f396e6db85cfe50019669c0d095
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:34:30 +0000

    crime opposite pin owe fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
