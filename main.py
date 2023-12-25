09905fb567fbd706ac7d75a927b042ab91162f5b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:55:54 +0000

    upwards towards noisy interview contain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
