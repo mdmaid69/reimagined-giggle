712578a9b45ed1d49978698fe0c9ade40cd3b02f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:48 +0000

    them maybe ever creature chase

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
