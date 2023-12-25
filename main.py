1a09615b86b6b7b01442c0501e1a49bda7d93ffc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:12:38 +0000

    school earth recognize thankyou pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
 def calculate_circle_area(radius):
         return math.pi * radius**2
