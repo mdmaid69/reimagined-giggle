45ff339a1e06a67f1a9c47e9e60e010871a33030
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:24:24 +0000

    practise address consider war slightly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+for i in range(10): print(i)
