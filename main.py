a581e4148ff9285939e1da70236deb637a095b7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:58:01 +0000

    talkinto speak clean work proud

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
