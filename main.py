f7d3f9392c9e180ed60dee1bba94acda1d620e2b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:02 +0000

    discussion pin success kind substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
+def is_even(n):
+        return n % 2 == 0
