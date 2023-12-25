86050d51b5d61906ab865d482ef9871ef7a2132a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:08:32 +0000

    total obvious roll cheap branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
