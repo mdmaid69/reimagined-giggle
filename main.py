9a9265697d08bdbcbf5061ed6fee7d43dcca2f5f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:11:40 +0000

    bye lend from moon worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def create_user_string():
+        return collections.UserString()
