b6cd18ae7cd50c7b1f21b3891c648a05e98080d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:53 +0000

    mysterious pattern deserve impression shadow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def is_palindrome(s):
+        return s == s[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
