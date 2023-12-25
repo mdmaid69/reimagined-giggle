be24112a6e4500ff31cc11058179e91775b87eab
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:50 +0000

    us expression rich downstairs wake

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def count_characters(sentence):
+        return len(sentence)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
