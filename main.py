397ca0ba0577890281fb87e3563e59c4f7197033
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:11:42 +0000

    wear then wherever so give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
