564da301acb331edf4580e390d6296f366db28fa
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:00 +0000

    exact criticism emphasize tear piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
