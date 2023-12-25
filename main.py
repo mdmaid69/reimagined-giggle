adc0227baf697bc2261af8f2d514322c9f6a6f9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:20 +0000

    condition brief instant guess measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
