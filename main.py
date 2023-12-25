cdcfee47db71f845b1d67752366283c6d2dd114c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:05 +0000

    familiar favourite programme standard alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
