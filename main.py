bdfe65514973e894ef58c9b627f32ebd68acacff
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:03:44 +0000

    callback left otherwise closed hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
