4ff834b530f2641dca64aee8460ad1dc3c6335ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:02:09 +0000

    less leave single defend away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
