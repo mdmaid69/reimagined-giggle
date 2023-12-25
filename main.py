dc4789ca12db32954acd87d3ece9b2da660bda63
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:15 +0000

    rare us while zero Ii

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
