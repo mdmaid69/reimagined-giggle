811e23d1b1a631c85c1447aa26738210e8aa92c1
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:18 +0000

    flood follow become 179bite room

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
