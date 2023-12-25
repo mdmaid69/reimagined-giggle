8bc54d73d61a54c5700ac83e3e3aa2e0baa994ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:53:13 +0000

    travel foot goout safe dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
