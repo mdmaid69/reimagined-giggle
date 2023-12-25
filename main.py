fd2ca020e81222a062cce778288e1d09998b5e34
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:41 +0000

    skirt emphasize limit meaning private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
