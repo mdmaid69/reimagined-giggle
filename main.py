f5ea1a62e7b7434614ee3bbfb3961bcfb2799637
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:36:11 +0000

    official embarrassed shoulder among quit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
