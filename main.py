e5f2c6b847bfdab62dcb0ad72175f03251703f97
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:59:54 +0000

    unkind skill scrape challenge view

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
