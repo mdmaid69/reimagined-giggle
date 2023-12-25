a98f060cca441cb17cbfb331147bb09e48760716
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:38 +0000

    comeback fill ride equal trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print(sum(range(10)))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def iterate_over_array(array):
+        for item in array:
+        print(item)
