1039f5ab58d95e6d634e18a880c65d799fdd832d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:53:56 +0000

    equal its pretend song then

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
