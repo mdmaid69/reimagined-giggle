8d772a0e6b1b2a711c3fd429dd9cc99eb1ffb442
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:29:08 +0000

    before smart skill plenty heavy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+print(sys.version)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
