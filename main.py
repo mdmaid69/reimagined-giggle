3bcd0f5bbcd990ea9dcca978b9627136fdabc53a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:49:39 +0000

    spot your time noone accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
