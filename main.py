0b4c1afa1c4293c519e6ba846231100f41648b14
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:58:00 +0000

    total concerned needle narrow display

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_typecode(array):
+        return array.typecode
