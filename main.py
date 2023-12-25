6569e93736dc2d138a96cbbb5f9a4c609b226a1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:19:13 +0000

    instant show mess opposite laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
