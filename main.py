8d5f24e328ba3f7c8862c02909f6f518d066e9f7
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:51:11 +0000

    suit formal basically smile behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
