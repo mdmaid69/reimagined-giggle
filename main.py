2bdaac1307d47665e444c461508cc9c37c669dc3
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:02:57 +0000

    male hit help wall wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
