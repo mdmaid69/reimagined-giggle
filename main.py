2b496cc9c4624a997bbccf787515faffe3f9b1f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:51:59 +0000

    fresh wonder behave throughout door

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
