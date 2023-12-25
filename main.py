fb8142e25ebeb294d4e0b11748234352a4215451
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:56:34 +0000

    laugh regret interesting whenever unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
