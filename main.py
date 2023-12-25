b8a73df032fda8e13570e9e1cd1d9b7983673242
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:09:44 +0000

    freedom prize usually whistle university

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
