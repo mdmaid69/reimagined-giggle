0f4d8f90fa518318b5bbf044187787f9522ba715
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:40 +0000

    cost war murder surface complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
