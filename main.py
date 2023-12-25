753704858435af05839712f8f0439e3741400304
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:06:19 +0000

    since equal turnon keep but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
