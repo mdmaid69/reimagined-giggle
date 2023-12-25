bce76a85f7823463be0e329d355a39501b49aa6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:11:21 +0000

    disturb that keepup poison command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
