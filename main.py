57cc70ba3f6e6632fd1887f173e1814e75770220
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:22:49 +0000

    usually swim progress serve how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
