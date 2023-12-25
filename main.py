471db4e38059e08944e9e9c9a1a3327234b97aa2
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:44:14 +0000

    feeling house public gooff also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
