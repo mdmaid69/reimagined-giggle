fb1a9dffa4887a25bea96dc789e2f871d45486ed
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:15:38 +0000

    shape limit comeoff ear conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
