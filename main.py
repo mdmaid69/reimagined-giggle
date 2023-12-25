207a698e330ee7d473f5692070d568550844aaa2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:31 +0000

    mess advise rely account abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
