d28257e96d3fe08e0a0105bc175bbcda9583911b
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:59 +0000

    joke cope drop wake pin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_int(array):
+        return int(array[0])
