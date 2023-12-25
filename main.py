f1f3f691d29cdab8f8726ca5b72e4ad8e0f1eb09
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:02:50 +0000

    ashamed goaway view concentrate difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
