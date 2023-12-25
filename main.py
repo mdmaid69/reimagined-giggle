8c34b1b2a4a8a5868544efd5117dcc8984ba0afc
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:57:51 +0000

    problem mend arrive idea shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
