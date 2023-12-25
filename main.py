ab7838e36c0b68d582e8e02b0402819ea8e292f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:32 +0000

    reduction win close only church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
