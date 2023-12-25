bb974f2993dcdbf344f0322760a047301c6810f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:01:27 +0000

    cross combine skirt attractive callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
