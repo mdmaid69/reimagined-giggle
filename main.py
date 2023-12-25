d61b0f88f45d30327236a13bb3fbb1efbff162b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:36 +0000

    worth acceptable bet silent kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def reverse_list(lst):
+        return lst[::-1]
