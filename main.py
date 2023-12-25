2f20f06fc996fb831a6c3052030943ab9bb6eec8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:39 +0000

    whichever even related takeback further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
