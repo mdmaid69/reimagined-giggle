1f8f5578c45fdaf9087277166d0418b4f83c1b35
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:35 +0000

    combine content earth rid concentrate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
