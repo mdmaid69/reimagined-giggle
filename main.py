6738dc7ca1491ed6000a6fb57f5bed00a4cffaca
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:19:48 +0000

    inside free willing safety colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
