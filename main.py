e16b6d7a1ad2244e6c417afb239679a686740f76
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:55 +0000

    thinkthrough pause embarrassed beyond raw

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
