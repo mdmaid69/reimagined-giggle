fb35f57f8e34f1514307eb52c34480d596700c90
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:17:08 +0000

    exactly result hide certain example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
