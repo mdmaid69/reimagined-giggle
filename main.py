80ed11c2e77d1214f6769719259f7fba61b9c8b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:11 +0000

    one soil tongue comeback pull

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
