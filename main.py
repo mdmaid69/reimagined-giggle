ee6f949d14f55668dca400dfbf06fb70fa01c0ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:53 +0000

    should command tip young boat

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
