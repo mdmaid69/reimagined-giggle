dcc4e4e1ad21b12952e0ac0e025daa5e22354a9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:20:42 +0000

    storm period imagine left congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
