6b050429195646ae72a04311dae7afaf3628840a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:44:41 +0000

    deep explanation dig kind somebody

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+print(re.match("h.*o", "hello world"))
