2996244bbdd14f455848b27b9c2e787e7ea432c5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:52:26 +0000

    stretch soil profit alone progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
