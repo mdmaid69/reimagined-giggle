19ac734caea962c8439a6cf0dd8783e4c2b1a316
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:50 +0000

    determined limit distance afford through

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
