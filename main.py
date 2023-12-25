7c53e0140ec117a6c92470503119c7afdc436af4
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:16:26 +0000

    dear comeoff sometimes dead emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
