b46ad49df4c73dd5708862daaa717dbc60ea3f28
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:15:26 +0000

    mess whole demonstrate behaviour fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
