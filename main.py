d22eb67b89e54d9f1bce4c876e5d4ce10cdba6d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:48:30 +0000

    aim reliable refer ability giveup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
