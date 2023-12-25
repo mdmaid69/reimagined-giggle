70c7b1923be62699c90fafa439bbe740aee9157d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:51 +0000

    join between street key satisfactory

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def add_numbers(a, b):
+        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
