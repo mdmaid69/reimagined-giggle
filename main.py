4e7bb55ec638b5da023be644d5705ff950f8cafb
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:45:22 +0000

    tall swallow stop appear read

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
