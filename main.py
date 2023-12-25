f6376408acb110db43843bfc863cf66398ae6a78
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:06:11 +0000

    roof figure train trousers declare

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
