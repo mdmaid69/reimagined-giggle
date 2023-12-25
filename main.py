6b3576d1c0de0344bf3b4798c6ad9b63d5f2197c
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:33:17 +0000

    land room fond outside fact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
+def reverse_string(s):
+        return s[::-1]
