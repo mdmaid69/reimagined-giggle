ea83d49f3f91465ceaeb240affc0fb82ef06c89f
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:15:14 +0000

    vote disgusting experience throughout turnaway

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
