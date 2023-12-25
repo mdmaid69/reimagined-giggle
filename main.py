7abfee9bbd92ea4aabca27e7ce0cb5d755530d58
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:52:02 +0000

    anything beat under post vote

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def clear_array(array):
-        array *= 0
+  def cube_number(x):
+        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
