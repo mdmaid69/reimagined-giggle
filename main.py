71b1e6d4f8ddea01b1cbbd81a21e1bc9294ff810
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:26:49 +0000

    than hospital rare many could

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_palindrome(s):
-        return s == s[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
