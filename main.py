e0ab4941116a5e751b8789a0bf1dbb2ebcc160b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:05 +0000

    feeling top push and back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
