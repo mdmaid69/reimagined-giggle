8a5d16d5d04b2b5b87f39ad22db4af13e7d3d1fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:01 +0000

    contact anywayanyhow sometimes board there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
