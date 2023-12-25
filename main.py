47c76dd6a0075fbbe3bfea1c8034ad73fa15e7d7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:17:27 +0000

    emergency mine off fly criticize

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
