0597a3f55ae54caab6a06c4d0137c99eb4b9064a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:35:18 +0000

    subject nigga anywayanyhow office word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
