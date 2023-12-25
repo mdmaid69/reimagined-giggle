c072b60507e929ec13862400552bee408ad0d9ab
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:52:28 +0000

    me round ahead content pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
