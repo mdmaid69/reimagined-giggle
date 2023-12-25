75c3722647cefe67031023b1fdd4e021fbfadb73
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:50:17 +0000

    opportunity office fire authority tongue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
