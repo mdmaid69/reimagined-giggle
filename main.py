cc47b14942730cd415795796d06ff2d6da168c88
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:33 +0000

    worth replace cope excellent off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
