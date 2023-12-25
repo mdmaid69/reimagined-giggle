40d3409720316197d9c7ae9a8f0b505b1033f49c
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:08:21 +0000

    open possible once worried favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def set_array_item(array, i, item):
+        array[i] = item
