9814eae3b44ca1e580ed31e2f3d5766fb0f445f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:05 +0000

    awake shoot arm cry direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
