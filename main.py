dd08aca4aabe97d84eb249b35e7fe85b203eab42
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:24 +0000

    neck appeal relief production quantity

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
