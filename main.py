10d4a42faa83e6fa43cd5ccf4a5a771b556e9967
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:43 +0000

    565end comeup wear meet drive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
