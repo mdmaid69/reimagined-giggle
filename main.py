908a8e0f23b632634e7c73100710c241a4f9a9bd
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:02 +0000

    lesson mass limit bear go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
