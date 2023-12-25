43b676e06a58bf38d89948efb1a6347e04b7f7c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:53:35 +0000

    flight suit notice weak gift

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
