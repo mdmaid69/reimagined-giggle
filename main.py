1b29d48ba45077b70822be490794ba7d279fa640
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:22:07 +0000

    wise container mind vote tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
