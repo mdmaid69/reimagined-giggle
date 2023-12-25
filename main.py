e4c23aea455987678a7bdd2b10e6010521614004
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:06:09 +0000

    worry anyone build new account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
