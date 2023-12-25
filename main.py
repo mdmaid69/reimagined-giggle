5d3df313cf70ec9e6939d2cbc343a8bbce45814f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:23:09 +0000

    time change embarrassing contrast 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def convert_to_binary(n):
+        return bin(n)
