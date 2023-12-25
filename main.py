451a424dc97d9e13a008171c6bfb4ddddf970cb2
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:52:15 +0000

    danger bank book plain motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
