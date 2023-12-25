efe33809c5d32fda6d0f9e614b2a5d05ed74b5bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:26:07 +0000

    lock slide equal scene sleep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
