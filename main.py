0d5beb491180bfd7181628a69a73f02ed0660fa1
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:20:32 +0000

    rise pour suppose part increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
