271170dce38c2adf69294e1a3d88770a7ad17c36
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:01 +0000

    defend board young nerve foot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
