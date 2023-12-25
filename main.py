526bb351acae1134691691d74e31b441faff7abf
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:33:58 +0000

    able main exercise whether convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
