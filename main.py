9833572a83c9aaae1ea6fce035fe147107cdbdc0
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:21 +0000

    pressure demonstrate indeed unfortunate open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
