38a8a513a4bd67b4857c40820f0e1500c6654622
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:38:28 +0000

    steer long valuable suggest respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def convert_to_octal(n):
+        return oct(n)
