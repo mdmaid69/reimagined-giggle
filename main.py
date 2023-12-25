0ea25727a1c632f61fb9ac620263ff1946e71942
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:42:10 +0000

    platform suggestion total lean confidence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def count_elements(lst):
-        return len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+name = "Python"
+print("Hello,", name)
