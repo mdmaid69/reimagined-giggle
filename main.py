b5211a4faf0fd720cdfee8c8cca91de932bf41ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:29 +0000

    side brave free continue somehow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def exit_program():
+        sys.exit()
