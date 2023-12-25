709b54b4403a365bb1fd543b756bd448e0922485
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:04 +0000

    feeling along respect suppose alive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
