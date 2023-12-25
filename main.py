0911ef1fc8bfbbd84800d23279162f3b29cc4b1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:40:56 +0000

    fasten various rest imagine greeting

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
