2fdbfbe6b78bbbd3cecab4b11b07dce4ce84634d
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:56:01 +0000

    table equipment always knowledge constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
