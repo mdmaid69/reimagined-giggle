87d6f406bc908d04628ab3da20a641153119da88
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:06 +0000

    about result gift waste explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
