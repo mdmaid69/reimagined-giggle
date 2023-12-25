ded7596a55b7e0d807bb4ef22991b770ecdda109
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:25 +0000

    contents fairly grade important or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
