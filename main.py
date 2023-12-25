bd8406ed0b43f9aefbe6d858a13876ba74a8fc18
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:08 +0000

    unless tax suffer thinkover track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
