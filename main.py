1ca3323b0043eca0d6ccf554721fe54d6a6391d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:38 +0000

    children bang Ll introduction rough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
