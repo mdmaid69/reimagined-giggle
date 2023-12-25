15fa3ae37c117c46292dfe78b911159e1c110070
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:34 +0000

    shopping do tour list innocent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
