d9386e3db0858f2a6128d3eee7cb9beb51c0a640
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:34:57 +0000

    runover cause rain promise argue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
