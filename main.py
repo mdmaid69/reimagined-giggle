e921a8d4d52c3245004d4aad4f47f53c726103ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:55:52 +0000

    effort lose slip mainly become

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
