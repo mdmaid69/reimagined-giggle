ac3b19faa6a8067c8e5652b99c24ded81fa9f358
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:00:37 +0000

    win size justice even very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
