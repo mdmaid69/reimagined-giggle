736213f2cfe477f0454188c740f8b35e490800a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:04:15 +0000

    congratulations straight total either away

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
