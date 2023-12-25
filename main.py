2877efe6e8376323c93c7386ddf476cc0f3f5dff
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:24:47 +0000

    butter draw charge praise smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
