f4caccd07ed924a1bcf73b960193dc34c46c65bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:58 +0000

    trade warning 570enjoyable unlikely increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
