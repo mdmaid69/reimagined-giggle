abf0ae5c0307f0bf99ef5be418300356fa3f34ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:13:26 +0000

    count lay careless charge spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
