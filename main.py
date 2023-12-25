fb890bb8e336e8e8b6dbe114e241377b22c5c969
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:04:30 +0000

    to winter now rough afraid

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
