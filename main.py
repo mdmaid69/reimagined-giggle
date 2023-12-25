0806a05adadc38b405c22be1a90fbbe9d3f31362
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:44:44 +0000

    brush slight public smell ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
