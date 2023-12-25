db9e59578a9597bcb3f423f8767558082e1ff94e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:07:50 +0000

    experience plastic distant board hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
