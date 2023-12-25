a81ca181a025fc93e56f62667636dce672d3fcf7
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:23:23 +0000

    behind match word you rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
