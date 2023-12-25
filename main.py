395866adec76b21903faeeb3a1786091ec7e5f34
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:26 +0000

    minor want mess job Cc

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
