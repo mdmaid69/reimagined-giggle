0fd5d937c88c8bc528a626e5aee446efc30bdc8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:25:22 +0000

    influence callup bad comedown fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
