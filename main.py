81f4d44f6d2ad094936426dff0b3a09e983887f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:08 +0000

    proper information trick break soon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
