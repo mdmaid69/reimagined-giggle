0d639c25da924d29806fd66545597988eae04dd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:23:30 +0000

    freedom remember closely settle whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
