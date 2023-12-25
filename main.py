e02bee74bbf5cc142b4920b6af4497200b00ec38
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:17 +0000

    allow content debt travel murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
