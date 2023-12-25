58a159bcdc8cf356ea1c9d24b35b9e7703390dfc
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:08:47 +0000

    narrow step talkback children order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
