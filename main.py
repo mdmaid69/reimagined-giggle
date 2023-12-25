231927541141e1bc688a16f5047d5ac0fb8574ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:12 +0000

    familiar notice well account slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
