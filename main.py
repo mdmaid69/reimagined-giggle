6ee6fbbcd8d75d8b9d5afcde8e08ed48da4a29ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:33:34 +0000

    break variety explanation like direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
