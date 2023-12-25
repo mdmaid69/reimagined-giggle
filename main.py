df252f7ba4e3911f60f2aea498304516fa97fb2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:16:10 +0000

    any disgusting can balance slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
