a508fee152989fe60896743db4e486d49ff74b91
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:26:18 +0000

    sick find mistake radio tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
