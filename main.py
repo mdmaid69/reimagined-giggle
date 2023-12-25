7c46aa1a6595be817b7787b2b977dd7584217564
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:53 +0000

    negative quality even plenty equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
