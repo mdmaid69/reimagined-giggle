c06e594cda5c10e0901bdd4e47cd72bc92d73ad3
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:10 +0000

    adjective hope refuse regional snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
