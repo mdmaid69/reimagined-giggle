c36dfe4effb8fef0a7853d42509baedda561c1f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:25 +0000

    alive keen duty perfect scrap

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
