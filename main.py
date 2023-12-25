3a10c4d9d60e0725fa50c0962881147c8895950b
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:44:56 +0000

    bring space exchange argue reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
