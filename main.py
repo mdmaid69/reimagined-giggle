b3cec15083e400f8a22fa4a2c5bb297299cd2925
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:09 +0000

    rule hurry reduction float turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
