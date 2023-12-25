1d5e595a6dd7cc7d292fc04c512223c8b82d1520
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:22:58 +0000

    butter used harm right present

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
