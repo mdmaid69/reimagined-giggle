aafd849a7a7ee92956739cf47f7c61ce22a6efcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:38:26 +0000

    state crime near similar comeout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import tensorflow as tf
-print(tf.__version__)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
