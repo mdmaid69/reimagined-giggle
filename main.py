28eef93a494228ef7b07b60cb1a763d424c2fb34
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:08:27 +0000

    crowd sail mysterious stick breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
