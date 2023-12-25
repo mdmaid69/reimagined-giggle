ac0bf7712cfecdad6bb3526787739f84a65d94e9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:50:09 +0000

    plenty objection length runinto complicated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_rectangle(l, w):
+        return l * w
