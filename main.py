e93d56147efaed5122982aeb4127eae093cf7dcb
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:40 +0000

    hole note strike mad luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
