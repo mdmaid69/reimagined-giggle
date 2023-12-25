5ab75f883716892942c620e851f52310143d6eda
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:43 +0000

    business common little cost correct

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
