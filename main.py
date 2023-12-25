cd195fec0ec15e1d31e91a0b8637fe1b893070cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:01:43 +0000

    big tap sound original include

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
