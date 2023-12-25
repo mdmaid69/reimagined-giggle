017987a2259d4735bc84e60f24cf7180fc4bace0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:12:27 +0000

    own Gg prove unless follow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-from collections import Counter
-print(Counter("hello world"))
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
