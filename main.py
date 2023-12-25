b66e340cb3f05e020445f1b104d0dce96eea1c75
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:35:12 +0000

    border they cross transparent several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
