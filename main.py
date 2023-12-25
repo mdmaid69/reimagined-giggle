2f2b71e56c21fb676fe3690415ba7f69d2286726
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:31:15 +0000

    mouth Cc book meaning large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
