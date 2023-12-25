bd2a50db3a07c5ccd817fba5810fc0eb152ae850
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:32:35 +0000

    correction hair rubbish possibility principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
