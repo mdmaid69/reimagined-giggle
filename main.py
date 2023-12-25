b41a94f157624d0bda48b481e1858ee863f514cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:17:59 +0000

    towards play gift thumb error

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-print(math.pi)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
