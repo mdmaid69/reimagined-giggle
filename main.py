846b36bac6cb6a5d7abbdd93862a4b2a1563ed7f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:31:20 +0000

    some blind round lucky gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
