d1c879cc71b75447482f9030c76a47391b3871ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:16:53 +0000

    fire fly thanks mean shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
