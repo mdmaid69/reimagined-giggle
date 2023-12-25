4e89b94a26d61c8f537ebb4f4b6fcfbdbdd0ef27
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:15 +0000

    shine snow conclusion callout strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
