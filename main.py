28dc34a3ca752b1d607caa0079fd4160a4757e95
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:18:00 +0000

    wave everywhere draw trap tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
