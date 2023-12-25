f1ceddefa24bfa7092f6b977f2cb7f411dda84a6
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:21 +0000

    reasonable attract steal especially aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
