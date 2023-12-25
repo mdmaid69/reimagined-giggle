74ecc31cacbb847cc4bc886c7190bd9c7447eb1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:31:01 +0000

    slip century except fence dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
