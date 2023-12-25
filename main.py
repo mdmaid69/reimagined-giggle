818c96d5648a00fb98da134e676d4f68c7a5a3be
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:05:08 +0000

    dangerous woman runout tear winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
