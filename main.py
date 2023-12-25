d03a69eb6e537b2a07ee3230973db6681a1fa0ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:10:59 +0000

    straight base improvement ball spring

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
