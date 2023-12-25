6a6e34fd16f5b0d0f3b711c3792620a411c9a232
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:54:46 +0000

    piece gate music lookup comfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def convert_to_octal(n):
+        return oct(n)
