3e2a4d1b888db02fc6d4d161b0fa375c45e2c53b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:34:22 +0000

    conduct fold congratulate direct blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
