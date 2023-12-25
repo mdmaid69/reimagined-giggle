b7d6760f278cb6d7b693613e0fcd8062dff9d34f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:35:57 +0000

    discover polite pump 565end term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
