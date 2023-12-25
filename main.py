c1080af9876e54d93db4b73a4b62700500f58121
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:48:17 +0000

    thing form country trip payment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def print_python_version():
+        print(sys.version)
