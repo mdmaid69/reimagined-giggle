44f9712a9c1bda18710753deff7689ca23f4a526
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:09:47 +0000

    fall town possibility hide drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
