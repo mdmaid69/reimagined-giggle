00ec68709bfcc21d058bf70f71b581bfe5028351
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:56 +0000

    slide but tap no OKorokay

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Words:", len(text.split()))
