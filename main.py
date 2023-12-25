6bdbfefdd61e14ada76c31192e4a8233a4bd16eb
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:48:59 +0000

    you ago value problem show

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
