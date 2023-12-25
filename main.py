85995ebdf02a92064dd16c9710c0a08ae3ae5732
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:00 +0000

    one bringdown wide dear impatient

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
