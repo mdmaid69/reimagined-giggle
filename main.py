f0d4adb3872779b5cbec868085f2a13e1a82e701
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:17:46 +0000

    seat move so that at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_directory_name(path):
+        return os.path.dirname(path)
