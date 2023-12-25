2a983db5df1f44aceb6e87a64f28402c9f8f8875
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:10 +0000

    nigga time consult ask write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
