55d2ef1420498c828d79a89fc825833ea39fb648
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:42:50 +0000

    pot naturally table single help

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
