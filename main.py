bccb6636d93f04d0e77222fb838bf6997f7aabdd
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:14:16 +0000

    judge discussion fun swing consider

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
