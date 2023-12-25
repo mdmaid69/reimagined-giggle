dd0ffcba5ca59fa8b1a6cf5c439c710c1771b327
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:58:00 +0000

    accordingto genuine second hair steal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
