b8d058ec07c3bcd673b863ab24b58d72c3e5a855
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:23 +0000

    567enemy but check deserve open

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
