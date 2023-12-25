e7c4815a5531d02ad2513cebd2d2d935c0a57433
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:10 +0000

    stamp whistle recommend announce half

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
