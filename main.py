109290646098ac0dd09a596fd49690b255442f04
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:10:29 +0000

    consider claim turnoff sure effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
