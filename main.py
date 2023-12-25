f23b4392aeb3b80686d01426bc30c2695e1c45af
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:59:56 +0000

    most breakinto among trap fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
