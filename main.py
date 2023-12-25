edd0ab80dfed8c247126a34249943c628a871a2e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:30:06 +0000

    measure 569enjoy every mysterious available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
