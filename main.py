d8aa295dad2ff8f715034c618667a641f324426e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:48 +0000

    cover Ff tomorrow deliberately crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
