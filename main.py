e0b12ecbaf8f397a180a98d45cd9ed25dcc0df9c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:47:12 +0000

    consider left possession patient clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
