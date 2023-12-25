0398e2bff02f0ffab76dd32527f61d937f4e10e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:45 +0000

    pencil hit stay relative pocket

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(5):
-        print(i)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
