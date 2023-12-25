dace26789ecbdb51c3fd2c3b42cadce67baf7a67
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:40:54 +0000

    square freedom makeinto begin scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_max(numbers):
-        return max(numbers)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
