ff82d0740c7a99c28cd71f18fa76377feb19ada7
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:14:30 +0000

    mean firm mouth boring front

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
