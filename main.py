b135f41ed1ada5681814911dd006aa436fdb27d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:18:15 +0000

    regional mysterious realize far reply

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def square_number(x):
+        return x**2
+print([x**2 for x in range(10)])
