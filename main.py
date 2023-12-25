9750ee0ff955d4f08705e6abca19f080b2e8a194
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:08:06 +0000

    lock few few offence square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
