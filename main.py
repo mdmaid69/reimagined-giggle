443571d7acd0734194490dd2c9f9afc772137fdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:54:00 +0000

    share less office paper emergency

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
