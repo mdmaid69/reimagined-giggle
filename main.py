e23da92408345aaaddfd986d91d9873d372c5c78
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:05:10 +0000

    back later however fold official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
