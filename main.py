58be1879d2da42c915536b3b98f5b7c030f2d189
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:46:15 +0000

    brown neither gap neither sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
