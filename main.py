a1ea83756b61962bcc174627efa42f940cb27004
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:54:50 +0000

    kill aim fire knowledge bringup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
