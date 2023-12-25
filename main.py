55521623463dd435d48fd4fc6f9a9542841d2c06
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:17:38 +0000

    only pot hurry cry delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_acceleration(speed, time):
+        return speed / time
