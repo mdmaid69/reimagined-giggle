912894534288635d1a2bf85834066948b76012f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:58:03 +0000

    serve object sell ground money

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
