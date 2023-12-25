94b1d3faf864c4e674cbe55eed7dad03c2a04fca
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:43:46 +0000

    whole hope child play glad

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
