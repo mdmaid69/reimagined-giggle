2297d873d21ef510b6673e11772ce7bb01bdf735
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:25:56 +0000

    year shoulder regard small they

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def reverse_array(array):
+        array.reverse()
