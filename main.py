edfb283fc6545849ee4aec92ca4b583cf17da6ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:52:36 +0000

    shall congratulations track attach shape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
