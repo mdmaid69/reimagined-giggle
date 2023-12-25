d11e0cfdff583986d146de5ba277400fd4acf668
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:41:22 +0000

    official again consider before thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
