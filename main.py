e28f89dab301c3452c741cb18dac592455a0a0d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:06 +0000

    plant prevent add available habit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+print(time.time())
+import time
+def get_current_time():
+        return time.ctime()
