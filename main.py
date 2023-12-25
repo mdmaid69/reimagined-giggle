b4bfa04937495e330f537d7fdd83ce5557b69f37
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:27 +0000

    responsibility yes look fair object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
