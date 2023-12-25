0b1bb999c7215ad329d206d8963d19e7578e8218
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:24:40 +0000

    long birth healthy father chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
