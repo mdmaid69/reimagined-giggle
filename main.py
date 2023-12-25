934bc4ceede03c10f733044710c8253ae2923e28
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:19:35 +0000

    breakfast match several unhappy act

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
