349bdba15a4df2cdb463425640318863287f2ceb
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:23 +0000

    disappear this unfortunate thumb the

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+print(random.randint(0, 100))
