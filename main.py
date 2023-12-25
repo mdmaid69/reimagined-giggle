c14ad58679fccef25a9df40d0b28fb387e2add67
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:32:47 +0000

    evening maintain on connect win

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import sys
+def exit_program():
+        sys.exit()
