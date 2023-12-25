b0cf8c0d2704fcc53919bcbf956e332ac4eb6393
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:40 +0000

    people flow star equipment progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
