98f8a8c91945106fe688a365e89f499b9184cb84
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:39 +0000

    control attach forever rush share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
