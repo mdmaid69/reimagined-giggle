3580e5aa9d9a3fefda6b3f5132eb12f7a6d53e91
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:04 +0000

    high makeinto wealth many solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
