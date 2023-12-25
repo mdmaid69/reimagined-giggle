5a42f5ef5a445b2f314f14660b788c79699b5646
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:42:44 +0000

    building climb plan hole limit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
