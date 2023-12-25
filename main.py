02848921fa615e4539bbd6cb5c00a562278e4abc
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:00:27 +0000

    name winter obvious except spite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
