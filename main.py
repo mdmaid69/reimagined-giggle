774e25c0ac7712248313fbd0c1e15f7dc900379a
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:11 +0000

    gift degree satisfied powerful continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
