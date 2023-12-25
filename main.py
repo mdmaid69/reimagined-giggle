fc126f5476cc9b3e9ced24a40a8745b788393e7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:15:55 +0000

    bend lucky previous him persuade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
