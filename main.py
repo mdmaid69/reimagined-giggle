3ea1868b9fb65c643169c367da6a8341ba3ef332
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:30:23 +0000

    mood disturb freeze deaf boat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
