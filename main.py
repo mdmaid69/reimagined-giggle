009f7cf037b1d1210f9a535e600b294ff27d423f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:45 +0000

    569enjoy fold treat afford put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
