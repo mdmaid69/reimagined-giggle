36c7c44131638d9549476c7e6535bcc40684cb67
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:29:38 +0000

    centimetre note centre into coffee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import random
+print(random.randint(0, 100))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
