0ce1f5b32ed02942abfab1f557b8a03c959b85ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:07:05 +0000

    admit fashion fail male lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
