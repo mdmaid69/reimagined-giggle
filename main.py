2ed32b3fb1c65d6a959c7979a64fdf7df81d25f8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:19:31 +0000

    female strike demonstrate well branch

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
