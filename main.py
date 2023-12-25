ed6ebc073d5a039b30ec4983e04e1ca13fe5ace6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:44:47 +0000

    guess copy still explanation set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
