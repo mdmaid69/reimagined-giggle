5900d19f2234eb9dcb732f1832957630b14e222a
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:28:25 +0000

    hope object wind bunch put

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+print(math.pi)
