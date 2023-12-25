fc3a0e8acd2461f1f5fbe0b5b68fe14d0287fea8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:14:17 +0000

    especially impression if copy rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
