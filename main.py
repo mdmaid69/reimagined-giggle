651d2bce6a1cdc1e80b6d95b4dcb7a6bf57e58fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:18 +0000

    play balance message abroad he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import platform
+def get_os_info():
+        return platform.uname()
