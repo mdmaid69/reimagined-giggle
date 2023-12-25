18fc03f7dd7589c611b84aa61994147de8922da5
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:24:59 +0000

    like book sight surface instruction

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
