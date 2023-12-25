0204923554ff1a699e150ad74671da96170701ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:20:35 +0000

    apologizeapologise consult free damage rare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+print(os.getcwd())
