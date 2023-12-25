13cff0f1da1813fa3b697bc22f9d088f611387cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:47:31 +0000

    responsibility delight runinto mention fetch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def count_elements(lst):
-        return len(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
