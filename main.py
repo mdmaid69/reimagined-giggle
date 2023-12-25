e0afc9e4cba2873badd9872d26c13e0dc80a9c33
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:32:21 +0000

    cope protest schedule Kk odd

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import re
-print(re.match("h.*o", "hello world"))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
