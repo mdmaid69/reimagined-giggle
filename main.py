9ad4910b2a1a7b3cb0eec3d922b6f2bd064af423
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:03:25 +0000

    low deliver line bit couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
