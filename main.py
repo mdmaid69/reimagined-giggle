a7a608691e40d9f722769b2b737503efb5630282
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:15:46 +0000

    embarrassed difficult concerning apply sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
