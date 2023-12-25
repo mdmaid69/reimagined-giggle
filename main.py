e4455daa0416568b568af5567304e05074e8985b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:47 +0000

    bet country introduce keep offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
