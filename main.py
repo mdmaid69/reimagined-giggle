cb2e2fbbdb110cc03dfa7b3dc05f759906e9e303
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:13 +0000

    around correction brief kind we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
