69ab7929d9abd9dc7e9f0eb45f19eb76c098b4b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:12:55 +0000

    below confident characteristic lower distant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_current_time():
+        return time.time()
