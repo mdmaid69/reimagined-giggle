3098f038aa2cc4bc94811e2fee742a20096bba9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:32 +0000

    guard shopping operation since change

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
