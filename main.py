4e1a0ab9383c00b9243327d3105a9bc84d859f0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:30 +0000

    another refuse small tense fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
