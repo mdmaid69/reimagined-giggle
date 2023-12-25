424d3954efe0c7d2f4843a05438c68e36205b6cd
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:29:34 +0000

    damage remove car remember take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
