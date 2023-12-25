020f045f72e43613a9191c35a4b5e916d9c38f9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:35 +0000

    stage flat previous preparation across

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
