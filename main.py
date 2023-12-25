3b50340714d779c2805d5c41d0102d49e03021b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:04 +0000

    cut pin mind wet project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_complex(array):
+        return complex(array[0])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
