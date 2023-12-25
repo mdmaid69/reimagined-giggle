fd69795b6b67ad6950283dc140a5c993a562ab83
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:11:54 +0000

    forgive crowded splash patient shopping

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_acceleration(speed, time):
+        return speed / time
