6c48665f13d2a89bd13e11a258530b846ed7f0b5
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:15:30 +0000

    car shame level accordingto may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+print(os.getcwd())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
