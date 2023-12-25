db6c19af2d3d5809b12c7b74b8e8ef6b7b52d970
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:01:56 +0000

    shoulder whether her train juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def greet(name):
+        print(f"Hello, {name}!")
