886f5eb35d5587f7fd737be544ca5c13abdca048
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:11 +0000

    pull apologizeapologise shall wing also

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-def add_numbers(a, b):
-        return a + b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
