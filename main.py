f87712a3736ffeea3a96f1ee85abb6f0e8863658
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:19 +0000

    ilovedicks turnaway bottom call tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_length(array):
+        return len(array)
