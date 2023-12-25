de5758d97f2a49124f3cf64b3335801ce36862b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:01:42 +0000

    friend crowd pin model important

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_elements(lst):
-        return len(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
