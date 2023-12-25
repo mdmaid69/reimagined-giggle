bb4a590f6ff36e6d3c97601c68d91f220fec40ef
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:10 +0000

    impossible capital arrival frighten niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
