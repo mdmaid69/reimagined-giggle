3d8c9a635ba5a5d43ae41ad282d2ae25363977d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:32 +0000

    near produce shake grade exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def pop_from_array(array, i=-1):
+        return array.pop(i)
