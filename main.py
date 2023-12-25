f5fdd874d8ed97cb6aece6349e20817295ae217c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:34:33 +0000

    moment surface open deliver properly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
