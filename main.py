aef3f6629a345e83a85a17e0620e656c7b74e507
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:03:19 +0000

    mess among mind matter sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_power(work, time):
+        return work / time
