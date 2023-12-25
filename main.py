c2a2c49e44dc0821a87b10c967dbb3d55f4c8a6a
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:39:00 +0000

    hot scrap twist reason frighten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
