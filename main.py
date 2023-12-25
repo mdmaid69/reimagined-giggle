53415d59046b1b2d2876d0beb76751ca287daad0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:46:11 +0000

    broken face joke rain instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def count_elements(lst):
+        return len(lst)
