e4805e10d90c1c0c135c4538177062e4ca76f804
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:13 +0000

    least its man threaten chance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
