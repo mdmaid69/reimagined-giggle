5bfccfa4f31be163b3df8a1ddf815deba532fd57
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:25 +0000

    holdout condition affect fair reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
