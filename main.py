b16edc40dce7818412d0781fda7540f978d9ef9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:20:57 +0000

    promise want lock back lookup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_even(n):
+        return n % 2 == 0
