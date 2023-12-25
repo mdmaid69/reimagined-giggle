8643b9b259820290af8547b10a7b24cee0c19fa4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:11 +0000

    fast full smoke subject tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
