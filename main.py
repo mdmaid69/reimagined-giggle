afb6b6a5ce6b0a3ef0744a4034aaac8a852082b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:34 +0000

    lookup last schedule move division

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import numpy as np
-print(np.array([1, 2, 3]))
+import re
+print(re.match("h.*o", "hello world"))
+x = 10
+y = 20
+print("Sum:", x + y)
