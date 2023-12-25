32f9c5d19dce9729339a21983bb5036db22a7e9b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:34:49 +0000

    unless tap minute clear direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
