90c97ee8026658e7475ca4812351c5821d97d79f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:25:27 +0000

    wire chance careful since closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
