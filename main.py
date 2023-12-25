d3cece496d0acccec365a028b1d6fb181bf0d3f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:31:10 +0000

    ring repeat match border single

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
