a55ad8e56a2bd3e77158a13897ff355eeb090193
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:50:36 +0000

    friend serious present delight smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
