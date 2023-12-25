793bf0819f75be95135d779323cb5107ff70158f
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:56:32 +0000

    surprised substance mind speak delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
