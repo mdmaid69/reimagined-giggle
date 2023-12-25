d2e389c2d02776fa26663eb4412276ad428ffafa
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:58:45 +0000

    unpleasant especially work defend mistake

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
