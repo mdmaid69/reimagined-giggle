b0d1654e7f3b361cbf7fd17c9feba5c086f5fe64
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:35 +0000

    Rr meet waste reason condition

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
