37412be5ac1b1c3713cd98870da37edf66e11fcc
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:03:06 +0000

    term discussion dig room country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
