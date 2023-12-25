51ee8af1d9114fd46311fd6f75ece73f3dedf7dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:44:59 +0000

    guard circumstance mass computer repair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
