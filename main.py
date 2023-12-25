7fc1033801592b969f731ce33667b86457f40f96
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:40 +0000

    fly guard criticism note comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
