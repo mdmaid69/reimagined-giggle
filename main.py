c57f1f4ca043795af925b1a73c9d68fc0e58f6db
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:29:03 +0000

    under Hh pin alive plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
