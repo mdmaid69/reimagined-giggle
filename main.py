0fa0aca883d5f8ca9607bd748bfad35fca7e357b
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:39:33 +0000

    place increase prevent cruel design

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
