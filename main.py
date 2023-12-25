740f090422ecea5c6fd1c1fd2a190d2ca96f22f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:37:33 +0000

    tragedy case keep prevent cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
