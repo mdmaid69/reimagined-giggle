283e586c9f2f310c00b781eb8f9737a0e457e195
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:49:06 +0000

    generally long theirs admit crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
