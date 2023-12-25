fa54a3fc0e635742c211e92d693ed3a660f7dcb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:05:49 +0000

    believe waste outside part join

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
