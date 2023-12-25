fd56f11824310d21aa1be266fddf3d2252ecc078
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:09:39 +0000

    famous buy along relationship basic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_power(work, time):
+        return work / time
