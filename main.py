f4876a8394a8f62b11a8c73c91b2cb9f9e4f7a3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:02:46 +0000

    comeback destroy as front occasional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_area_rectangle(l, w):
+        return l * w
