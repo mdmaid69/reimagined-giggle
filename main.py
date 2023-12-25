cee670cb72289bd7aeafa40a8e3c1a9d229b40f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:02:05 +0000

    report stare excited keen no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
