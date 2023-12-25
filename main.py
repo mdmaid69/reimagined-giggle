e6133873c8168d6eec6fee9fee04f6bfd3522c21
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:21:38 +0000

    decide breakinto wrong offer neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
