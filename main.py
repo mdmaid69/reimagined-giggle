6c31af7ae9a9b22f3f3b4648a24f2cae0eb9086f
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:35 +0000

    tool quality now yet tip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def subtract_numbers(x, y):
+        return x - y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
