9f48053b37b1355c23e8977f7339170fd57841fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:35:51 +0000

    cheat likely fair makeinto century

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_odd(n):
-        return n % 2 != 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
