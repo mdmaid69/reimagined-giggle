0cb1321e5ea76cbc3b8b2694529f8622816310d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:12:15 +0000

    violent stir dream unkind breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
