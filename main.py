b396f440bf34088a79585dadfddbb5a6807e6a08
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:54:39 +0000

    race private before decide all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
