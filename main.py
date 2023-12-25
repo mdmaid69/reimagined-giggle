555d44b8b492ae6793203ec7d791b309ca783d8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:51:58 +0000

    old whatever further bringdown recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def reverse_list(lst):
+        return lst[::-1]
