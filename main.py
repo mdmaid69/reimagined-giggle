1e4db34e7ae54439cbd302ab296b3250d2a4ec83
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:56:00 +0000

    careless foreign tonight control sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_queue():
+        return collections.deque()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
