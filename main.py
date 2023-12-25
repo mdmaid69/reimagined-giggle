8e9a36be19d7b3b6315c908807d5bada774c810d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:04 +0000

    tell board move appearance talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
