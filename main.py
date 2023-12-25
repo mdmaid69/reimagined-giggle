cb24f9b8f5a7aef1f5289ecd1fb2cf5bbaeb968b
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:22:13 +0000

    mainly lost through concern wild

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-print(sum(range(10)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
