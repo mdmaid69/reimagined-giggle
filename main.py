315309de474e2f9f34f591e93e7da8e8e2ac893d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:56:47 +0000

    rubbish cum crime lost join

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
