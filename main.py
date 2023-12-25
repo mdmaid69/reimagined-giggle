aec4108c3539168afb6ab681047430c0209cd9b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:10:48 +0000

    no surprise general add fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_characters(sentence):
+        return len(sentence)
