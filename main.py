f609c282530f4984587aa08550aa5c26e66ba348
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:32:56 +0000

    extremely uponon notice possibly comein

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
