f2e05c8d2b3f36bb79352bb8cef64493809c5e01
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:29:42 +0000

    Pp visit fetch full demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def cube_number(x):
+        return x**3
