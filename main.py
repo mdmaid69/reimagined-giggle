dab81d4149d3477b05f8ae08ec2b9aa747a10734
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:33:22 +0000

    disagree ready post apologizeapologise worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_length(array):
+        return len(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
