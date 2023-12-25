8454172110a1582834d403135aeebb5f85cfe59b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:05:28 +0000

    paper family basically level production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
