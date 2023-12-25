06a95af805cafeb54e2b597400d5e03ff05bb867
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:24:23 +0000

    smell breathe polite false our

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
-def divide_numbers(x, y):
-        return x / y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
