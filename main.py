e70eb2d957b5a250a8c9fe7440ccf16dad6f7fd6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:30:24 +0000

    along broad quickly hold listenfor

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
+import sys
+def print_python_version():
+        print(sys.version)
