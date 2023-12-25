772a7681a55313a45e2b4ba36f22ef2af9ee86d8
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:22:42 +0000

    please wait suppose shelter holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
+def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
