29a7e18975322cc5fb457e9464353f76f14d56f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:41 +0000

    pleased public like breathe unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+name = "Python"
+print("Hello,", name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
