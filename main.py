7dbb81d98c4a3f492da1c022f21e2b2fea0c9fcd
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:30:26 +0000

    offence crash word drink stranger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
