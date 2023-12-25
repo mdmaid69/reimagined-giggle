902fa5cb529af1d92efbabef36ee37347fade320
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:43:41 +0000

    character OKorokay meanwhile distance female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def reverse_array(array):
-        array.reverse()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
