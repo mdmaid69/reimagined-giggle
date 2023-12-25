2aba6e12c204f66d9fa7373b6e22ccb4d5abd229
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:16:44 +0000

    into table cruelty run display

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(5):
-        print(i)
-import datetime
-print(datetime.datetime.now())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
