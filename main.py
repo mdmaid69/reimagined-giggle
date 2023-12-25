868e656bee8199fe2f6a8e2312585e1b18c81c08
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:28 +0000

    nothing same which callback transport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
