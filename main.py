fed826a9b9c4d173116e80299bdfea257e45b36e
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:28 +0000

    disapprove yourself pause challenge one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
