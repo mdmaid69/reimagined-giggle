5924d972ecf8044e16c3d003b1a22a9a6206ac5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:53:57 +0000

    punishment tidy popular age set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
