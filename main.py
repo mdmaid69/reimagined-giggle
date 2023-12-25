9c85814bdb165bc9448de53694b3b83c45d1b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:56:08 +0000

    whistle giveout goaway patience stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
