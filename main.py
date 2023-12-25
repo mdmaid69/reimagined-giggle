22550dbbb4e0f8142468eb9ebbfcba5b8efcfbd0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:02:12 +0000

    task chief consist bringback wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
