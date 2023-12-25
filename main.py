f8d19e9350b71a61b238da496db51e8a97b3a94b
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:14:10 +0000

    finally protect owe brain of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
