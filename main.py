a1faa94fb8ccd32eec2cdeae266c793ea386abfa
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:28:39 +0000

    phone keepout underneath instant exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
