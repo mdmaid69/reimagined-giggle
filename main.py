28a2638d16b02a5d66541cc9ad1b5e89e4de27a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:02:01 +0000

    anything combine national market actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
