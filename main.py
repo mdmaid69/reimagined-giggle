5e3bc81c6f47bc32d54bceba7dcfb5308acd3c9f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:39 +0000

    organize ask mine 179bite square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
