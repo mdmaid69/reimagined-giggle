4699527a3a1c2e2a5b57886af3773e447bc375b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:39 +0000

    dead pump pretend mystery sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
