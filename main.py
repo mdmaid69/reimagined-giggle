217afa15c42e268fd2604052f548b761097b4d3e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:45:27 +0000

    talkinto else circumstance introduction somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
