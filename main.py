08939814af6bb15f1e811a46cff9a0a39bc3c391
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:54:31 +0000

    by turninto afterwards mind constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
