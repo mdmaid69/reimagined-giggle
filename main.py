9d4e77a7ac48116d395af26c713f72d9e392ed95
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:12:25 +0000

    separate further polite fix guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def sort_numbers(numbers):
+        return sorted(numbers)
