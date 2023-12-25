84f92d97d4fec54a3e181ef95300d1a6d3494520
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:47:14 +0000

    space full because crash whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def get_array_item(array, i):
+        return array[i]
