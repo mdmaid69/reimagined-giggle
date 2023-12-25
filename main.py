6e61bedc4328ea89e6a5c7104e3399670b20c15d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:04:42 +0000

    drink demand clearly small prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_length(array):
+        return len(array)
