e5c524cda7f171154ada621d9e9ae9a957b2e0b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:25:21 +0000

    profit especially bringback improvement past

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
