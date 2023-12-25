bb6185e8b38a929e4e3b9dd070ecf7e6204b3228
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:23 +0000

    to low project evidence lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def divide_numbers(x, y):
-        return x / y
+def is_palindrome(s):
+        return s == s[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
