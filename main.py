a766bf5c2de09629f603663f630c48143d87789f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:59:33 +0000

    most tight top pronunciation address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
