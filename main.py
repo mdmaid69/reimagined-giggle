99bb60eec823f97e8da9d6e52381b7ae39534fad
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:14:07 +0000

    surprising tear rub bend mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
