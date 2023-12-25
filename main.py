ece943dee72fe2e4932e146b8962bfe015914858
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:25:15 +0000

    thick will continue season means

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
