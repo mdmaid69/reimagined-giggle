bb0f4035db9accbcb1f9f93a5530c6b6fc7383e1
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:01:23 +0000

    crash smell mix convenient before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
