25053a46fa5959c0902c91459e54a338d6017b21
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:47:09 +0000

    criticize difficult authority plenty regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_list(array):
+        return list(array)
