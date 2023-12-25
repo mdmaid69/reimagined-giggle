3978ddd32076eeac8cffecdf5d2142c243d70b82
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:54:06 +0000

    moral fasten trip freedom attractive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
