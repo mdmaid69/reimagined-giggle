3fc04f8ba9bbb11a6a1cab6ea94151598139b6bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:13:02 +0000

    once software block organize child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
