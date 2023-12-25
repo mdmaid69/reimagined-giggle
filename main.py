1174b5c78896a53d88d030acf8cb597956a9a2ba
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:58:50 +0000

    bad across mad fit day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
