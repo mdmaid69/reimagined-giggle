69db1e1b562b82ef0479c370c63df9c92b646dfb
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:59 +0000

    upset polite night oil your

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
