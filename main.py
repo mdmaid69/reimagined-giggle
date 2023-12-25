184811e83ae65303b507af940843db9f5966e1d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:11:01 +0000

    stamp furniture reaction sorry kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
