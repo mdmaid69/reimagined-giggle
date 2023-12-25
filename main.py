65f17df8b94838e4daaefded00973cc67d007a03
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:49 +0000

    duty music receive long shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
