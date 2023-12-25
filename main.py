52a0a10624b0b19cf2947bcc0f03ee753239fd8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:52 +0000

    study achievement within scrap prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def convert_to_binary(n):
+        return bin(n)
