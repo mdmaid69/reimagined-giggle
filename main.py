d025f5d4bd548507c287adb5968bd25034dff94a
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:00 +0000

    quick sound crisis combination empty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
