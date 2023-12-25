68a7d7935f1f7380e2daa16bb0448333f4b87fc7
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:35:11 +0000

    shout little strict few organize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
