96cdc20fd88f622e3e48b19fab14f27e7a48f992
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:30 +0000

    trade uponon foot behave organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
