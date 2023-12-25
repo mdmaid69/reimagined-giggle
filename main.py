1de34397b4182aff3af88175c296f1358c94360e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:28:24 +0000

    situation run lower tend loan

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+print(os.getcwd())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
