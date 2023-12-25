f114c6e99437c5974dd4398d4d117d6c6874e8a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:57 +0000

    pot call activity spin sensible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
