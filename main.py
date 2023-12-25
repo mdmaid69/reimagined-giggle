05aba53011d1d367bcacc49bd7425f6f02ad92ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:54:19 +0000

    ability professional whether study buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def get_current_time():
+        return time.time()
