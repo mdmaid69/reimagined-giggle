0b383d0093c7a973b0384085b38366f2f4723918
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:16 +0000

    letter single involve suggestion dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
