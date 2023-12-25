90af9e4c989a6ea0f46065e08c1202e28086877c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:03 +0000

    habit fresh never city sleep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(5):
+        print(i)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
