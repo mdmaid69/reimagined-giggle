bc8236cfc5a3f859605bbd2f9288a1698e42b0b1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:05 +0000

    knock several show under mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
