7c3ae9eb5409f4068f939cfa4c061151e33952b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:55:13 +0000

    sensitive consider formal getoff border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
