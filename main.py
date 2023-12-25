4e972b8d66a71e55be4cc033a4d5d9e2fef931f5
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:58:53 +0000

    touch hand he demonstrate giveaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-def calculate_speed(distance, time):
-        return distance / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
