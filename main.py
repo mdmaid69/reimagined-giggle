57f6a70935edfac4b6d2c3e84b7957dd0a361fb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:14:33 +0000

    illness matter turnover trust follow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
