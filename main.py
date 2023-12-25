00938d862ba21233762d3b03ef4473f81295d4aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:11 +0000

    detail giveout instrument past class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
