8c4a321701cd8f3f1a07f6721e72bf9b41fc38ec
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:54 +0000

    this still exercise mouth positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
