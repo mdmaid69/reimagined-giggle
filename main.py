86e90244a557bea76679ac95d172edc7fb332b7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:08:58 +0000

    manage maintain difficulty again account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_mean(arr):
+        return np.mean(arr)
