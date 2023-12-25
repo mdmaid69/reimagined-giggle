cfc15d3e35ca65cd42ded0f38fbb10a8d8263650
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:47:26 +0000

    few possible flat Oo convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
