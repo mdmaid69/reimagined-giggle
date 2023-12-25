4d99391f5dfa0715ce3fc05784c3e8527ccac1b8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:23:01 +0000

    ago look honest autumn acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
