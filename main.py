754165384e6acd973cceefea6550fd26ddd3dd74
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:07:06 +0000

    unlikely break light border event

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_max(lst):
+        return max(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
