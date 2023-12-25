95d42345d6d6fac3d5b1e975096a77e1b6f9ad1d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:50:14 +0000

    memory fast lookafter regret doubt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
