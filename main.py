b6782798627cd1cce5c1da0a17a9863f3ce1c684
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:55:47 +0000

    also accident enthusiastic exact present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
