3d7feda6a255cf3b7a22209c4036f434fa6f6f7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:22:16 +0000

    rubbish all ride culture reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
