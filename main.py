697cec0923caec79b8cc1cec1301112047944df6
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:51 +0000

    independent better care regular beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
