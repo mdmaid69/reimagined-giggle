7749183611419eaecb70fc1cf7259a45f6e00556
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:38:21 +0000

    wear notice condition feature action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import re
+print(re.match("h.*o", "hello world"))
