0e106421f186795c13344e493f385e39677db7e5
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:42:34 +0000

    pour final wind yourself increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import getpass
+def get_username():
+        return getpass.getuser()
