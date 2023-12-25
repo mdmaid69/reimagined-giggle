d27ed5cf2bf1a2a6cd20a4feeadba0c4d07fd40a
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:28:15 +0000

    comeround winter clean start lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_work(force, distance):
-        return force * distance
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
