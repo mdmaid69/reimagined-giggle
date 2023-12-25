75fd597c1729bcb27f004a7ce3be16e9724e563b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:49 +0000

    skin 571enough part objection urgent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
