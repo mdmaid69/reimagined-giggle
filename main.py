355f8da9486a48f4fa97bb834c6364e36c661772
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:52:06 +0000

    job remark round advice possible

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import random
+print(random.randint(0, 100))
