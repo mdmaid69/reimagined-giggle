9d3dfc23e274da7feddb0d6b179a6dd82f639bb4
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:52:21 +0000

    slide after basis much principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_min(numbers):
+        return min(numbers)
