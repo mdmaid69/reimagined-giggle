941ef9e169cf79997dfc6993bbf6244a82ab54fc
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:16:36 +0000

    what instead thin deliberate language

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
