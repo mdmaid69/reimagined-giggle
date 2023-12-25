56552bdb815b8e974231847334a0a8497dbda94c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:21 +0000

    wish attractive image win leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
