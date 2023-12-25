5f76419a668cf6ef2bd1d0e63b0188a928e76bad
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:13:30 +0000

    fix table takeback lunch nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-def print_python_version():
-        return sys.version
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
