0eac46542d4dcb8062066c6491a5eb8687a1ca08
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:02:47 +0000

    schedule upset lay cruelty turnoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def reverse_string(s):
+        return s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
