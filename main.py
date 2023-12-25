60fdf51e57d0d45dd0ffa71d93cc5c1ceeff3962
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:15:34 +0000

    standard thorough congratulate turnoff live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
