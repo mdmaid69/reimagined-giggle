317e36a9e797f6e625fd52395dc21c1fa363b648
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:07 +0000

    comeoff ourselves help aim mood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
