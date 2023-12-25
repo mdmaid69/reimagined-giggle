79ea67ab584e3cd12817c71b456b6254135ec48a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:11 +0000

    goaway this will regular Oo

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
