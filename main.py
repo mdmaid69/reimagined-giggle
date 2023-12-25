3417b37c7da4f84852bea49f040b9bafc517320d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:45 +0000

    gather urgent upwards broad frightened

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
