60697781346c0641d9934265a348fd09e806d9d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:04:14 +0000

    close take community swallow 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_floor(x):
+        return math.floor(x)
