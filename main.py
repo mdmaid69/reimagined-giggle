92a82c6d2e9531305dafcad3d5e1b5d1738d581b
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:06:54 +0000

    unite heavy with takeaway rest

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
