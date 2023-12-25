e5f5de33591ed1bd9c19387df0f7536f44ca3fe9
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:00:16 +0000

    talkinto answer weak skill emergency

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
