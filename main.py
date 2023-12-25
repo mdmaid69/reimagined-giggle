7f0c723ebe0466495b1e166ecd84f07a0f6e677f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:37:51 +0000

    twist trip herself breathe turnback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
