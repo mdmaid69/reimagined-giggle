bed05e5b6306051c2904393aaade5909339fbdd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:02 +0000

    workon exchange mass responsibility supply

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
