0a87660330136f51418f4ddbcd46861210f92674
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:16:21 +0000

    takeaway display issue shelter allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
