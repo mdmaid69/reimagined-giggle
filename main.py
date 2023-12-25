36ce19c49211e61b6599fc67c8c64dbb152b4542
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:45:02 +0000

    turnout unfortunate remove along callout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def convert_to_octal(n):
+        return oct(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
