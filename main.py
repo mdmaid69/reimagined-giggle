0ea1ad627ad901f49f2974788c448b585fdfadb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:06 +0000

    payment challenge Yy relax damage

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
