a5bc27c5d1d9023e462f637df7a2067879609d8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:19:00 +0000

    chin affect responsibility unhappy lay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
