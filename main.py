060008cec5ca8741c8102f112e9b02875171e6b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:08:53 +0000

    true extremely nowhere insist power

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
