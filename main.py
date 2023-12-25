794a4e613d453fb920c9e1cab056c576f0e1df99
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:30:06 +0000

    certain identify war traffic base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
