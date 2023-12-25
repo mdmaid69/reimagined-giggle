9708d3cc5a322edd38e2beafde258b8661cf8737
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:12:32 +0000

    pocket lead 569enjoy square communicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
