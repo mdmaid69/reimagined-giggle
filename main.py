9264e00368b705678a08a7ea2621636e0fe4c09c
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:57 +0000

    side let lookup become manage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
