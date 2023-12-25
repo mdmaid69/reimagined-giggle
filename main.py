165aed650afd08f87e86fa76d9258679daa6feb6
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:48 +0000

    notice limit 564encourage private never

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
