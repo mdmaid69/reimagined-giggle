0e823497d02837b116d7fa3cc4df5e635a24dbdc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:25:17 +0000

    weather fortunate closely needle never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
