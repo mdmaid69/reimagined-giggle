0468041274ba2f8e2c5b09f11627839c5da716e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:00:05 +0000

    line reason share introduce raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
