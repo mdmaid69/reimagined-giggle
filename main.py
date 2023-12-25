fedc876d5fad62a84b1dfa4d8aadef40bbc2db0b
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:06 +0000

    completely back become aim wash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_counter():
+        return collections.Counter()
+def square_number(x):
+        return x**2
