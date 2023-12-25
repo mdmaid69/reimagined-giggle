b1ea90d9d56fb2dd556252705a1d1b2ed60b35d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:36:45 +0000

    callat slice owe check present

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
