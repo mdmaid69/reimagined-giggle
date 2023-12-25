4363d2517bbc6d9e1fbb5b97aa56345f5c2274c6
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:42:44 +0000

    quick easy tight completely business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
