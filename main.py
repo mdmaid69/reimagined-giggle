27eeb2a3cb43c3db16152d21ea385556d7373142
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:53:27 +0000

    surprised the study that giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
