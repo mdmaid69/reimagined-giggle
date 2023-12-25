872e8adea93d4765c5fa10bbec55b11c4edf6ef5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:50:46 +0000

    pleasure comfortable wrong pot attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
