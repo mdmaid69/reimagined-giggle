a419028a2c7d8a143eec8b7cf7496eb284ac4207
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:56:23 +0000

    target unusual swim rest market

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(10): print(i)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
