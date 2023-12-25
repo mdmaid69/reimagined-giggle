d99ef91172d24a1ce4af86380cabb927e5f50d3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:39:51 +0000

    Qq nowhere react fetch shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
