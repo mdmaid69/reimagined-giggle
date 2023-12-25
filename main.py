ff8a477ad5d3b8d5038f2c2df612cde406796c96
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:33:21 +0000

    use sort note exciting popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
