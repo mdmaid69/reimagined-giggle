3fd5e896cbbc9f2db02f422e1e5748b340a572a0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:13:44 +0000

    switch admire advice doubt money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
