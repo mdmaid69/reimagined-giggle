09469e3291605b74011cfbe174fd6fcfcab5744e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:08:10 +0000

    discussion protection live chat callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
