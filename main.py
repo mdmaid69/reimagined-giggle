39c697b1dd3c20d85d7c2a0a01d34c631c56d084
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:36:08 +0000

    fear brave commit experience operate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
