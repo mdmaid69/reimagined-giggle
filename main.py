8d98f3564a21e5231189a80f90d18b7f86215440
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:35:58 +0000

    snow shock run influence check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
