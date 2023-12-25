6f7d8d5f949a6105c9f458b396f1db465d061a85
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:58 +0000

    risk seriously advise line unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
