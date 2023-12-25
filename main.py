d53ae419e5e0955c9c6ae0124b6cedd2e243190b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:15:11 +0000

    previous Ss contents way close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
