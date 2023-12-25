2f7258e10cdc3cedae0498aa74fe44e4e87a6ab9
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:30 +0000

    drawing Ff tight whichever introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import platform
+def get_os_info():
+        return platform.uname()
