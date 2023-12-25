b443eb9fb551b15859a88e362ea234d9562eee12
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:49:23 +0000

    live space pick win error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-print(time.time())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
