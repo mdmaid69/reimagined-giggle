794eb26b3b3cadcb3f935c37b40a60546d85c5c2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:38:25 +0000

    lend suckme improve seat room

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_median(arr):
+        return np.median(arr)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
