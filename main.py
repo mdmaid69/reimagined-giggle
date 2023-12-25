aa6ca3f929e1c7ab0608184c41b6c9a0eebe1068
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:44:57 +0000

    whatever us develop away thin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
