b80f635aa6e95dbd64eb9fbe4f426665a9ff9254
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:03 +0000

    effect alive he reduce mood

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
