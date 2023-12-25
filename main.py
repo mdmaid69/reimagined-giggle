6955bac4b05bdc3dc7216bd5951ffbaf80010ba0
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:47:11 +0000

    early figure escape himself goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
