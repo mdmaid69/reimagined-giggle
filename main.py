f184d26d0d8cd62febd98a9b880ee4bd969ffd5d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:51:56 +0000

    begin separate fit suffer stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_queue():
+        return collections.deque()
