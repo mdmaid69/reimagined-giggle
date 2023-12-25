1d665a25432829bd90dfce6173af7e74dd43e6d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:01:19 +0000

    excuse keep turnback any thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
