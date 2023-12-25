79a4f101aec7502eb679d4c8e5c3c134b5b7c73c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:03:04 +0000

    should sore state obvious sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
