45bca49734b53427dfd3e45c12057b0660f3264b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:47:00 +0000

    vanish deaf control lie basis

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
