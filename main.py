e191892e62ed3078c69cdeb55485bca9b3b078f4
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:14:30 +0000

    imagine thankyou angry politics thirsty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
