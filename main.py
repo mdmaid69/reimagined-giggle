e978aa53b3691bece4394e3f10e2e12c9f57c735
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:38:09 +0000

    threaten smoke dream rid damage

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
