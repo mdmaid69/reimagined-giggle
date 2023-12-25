a7dfaa0812064c78ab092a4ff5d1957c29484d4e
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:56:56 +0000

    excellent special confuse room meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
