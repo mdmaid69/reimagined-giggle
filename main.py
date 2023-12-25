567702c315598ae54e6b54c4ca685dd3be5872f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:04 +0000

    honest wet pupil direct lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
