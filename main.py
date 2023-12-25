7b5484edd5f56ee6b88e1133ff2757644225807e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:21:30 +0000

    flow guess land mouth little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
