0eec96f7db15673751261c9b39a3cea44aa0e65b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:30:45 +0000

    loss account pain on scared

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
