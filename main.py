3917dd3bf4d147be7961c8f942c77956d2040ad4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:57:46 +0000

    close allow life name stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
