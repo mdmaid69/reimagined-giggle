1b5ba41597b65814086f9f1cd4ee079272217128
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:25:27 +0000

    insist wind play punish land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
