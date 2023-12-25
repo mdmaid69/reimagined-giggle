a9c8904e424348c6f5189599ade9fb8c59e4aad6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:47:37 +0000

    pronunciation right excellent lend inquire

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_current_time():
-        return time.ctime()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
