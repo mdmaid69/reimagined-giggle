69ffb1cb589e62dd7c73474f0d249a8977d74e65
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:52 +0000

    steep confuse fault appeal connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
