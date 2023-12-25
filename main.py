aef4ef8a251c04108cfd8e662ea6c44e0ad028a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:55:04 +0000

    bear tap family study protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
