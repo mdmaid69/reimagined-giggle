202c05bff881ab4884ce387eab30245addaf6a14
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:22:03 +0000

    give guard move pride yourself

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
