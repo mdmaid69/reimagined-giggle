47753bcef8f38a2d871de3bd4db2e29fa7a2f00d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:32:26 +0000

    up fasten exchange length rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def square_number(x):
+        return x**2
