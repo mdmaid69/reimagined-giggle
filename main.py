dba881f0a58caf8374d54f4b7cdcca983fae89a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:41:16 +0000

    pour constantly bar doubt fine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
