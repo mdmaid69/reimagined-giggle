ade90e6ca6173ca745b55cba7e207dc7f51ee29d
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:46:59 +0000

    right into really risk forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
