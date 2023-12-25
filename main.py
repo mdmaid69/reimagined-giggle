82756fe0d5517a1cd575f7343bb0bbf6b5ab8b54
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:35:29 +0000

    only secret separate offend mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+for i in range(5):
+        print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
