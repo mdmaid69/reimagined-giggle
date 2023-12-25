2b2562012093ff10b75f5dbb2f1b27c0e9a2f51e
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:47:18 +0000

    danger ashamed worst easy slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
