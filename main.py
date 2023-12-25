901377bb99d6bb0a215124b77892e540a56ea089
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:18 +0000

    direct next rub great low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
