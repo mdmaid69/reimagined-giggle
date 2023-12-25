eca51520e8f0aa95faa40e5d8a633f706e1ab507
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:05:35 +0000

    visit turnaway tomorrow skin succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
