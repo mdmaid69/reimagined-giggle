4e432563dd90f52371ea852b550c46849a66feca
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:59 +0000

    present minor say their again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
