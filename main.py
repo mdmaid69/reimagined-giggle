6e56936297faca523fa9812019041f91a4aebee1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:39:12 +0000

    business need pick air proper

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
