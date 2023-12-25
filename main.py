782b3712a1cdb3461069fc5fc255091d887d6790
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:01:11 +0000

    interrupt disappear this total better

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import re
+print(re.match("h.*o", "hello world"))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
