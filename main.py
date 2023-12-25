9850ec052c291d35ab8cc5f3a49a1aedf3398485
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:05:17 +0000

    government ring space broad improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
