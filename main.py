b1eb4cd137665c2cb35113fc05b0314b9eca8d30
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:58:53 +0000

    people dish feel how ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
