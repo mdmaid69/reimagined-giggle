6208f35f4874d5fd6a8e083976eff2334b82b020
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:19:09 +0000

    179bite breakthrough opponent to me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(lst):
-        return min(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
