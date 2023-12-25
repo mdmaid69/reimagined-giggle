96f7d48366529c7585fd7b7e8d803652021f0c97
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:45:10 +0000

    risk same space and market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_os_info():
-        return platform.uname()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
