3b657143d7e7a3828162e323966d3c892450e9b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:09:08 +0000

    brain 570enjoyable feed fetch grand

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_list(array):
+        return list(array)
