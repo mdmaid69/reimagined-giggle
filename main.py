68c3d25b3c9436d02e875e11414d55add7d37409
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:05:00 +0000

    again doctor their television punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
