11ab1ae9f6631ab198a84d9629247454623c808f
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:26:33 +0000

    promise slowly fly spring hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
