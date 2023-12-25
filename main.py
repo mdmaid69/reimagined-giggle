e2d500d75898809d73baa346c51348a57d4de146
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:55:07 +0000

    Rr anxious advice listenfor turnon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
