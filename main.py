9cf465734ec9a261761562f09653ec609d93507d
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:43 +0000

    tax storm honest platform waste

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
