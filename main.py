c8cf01ee66cb068b1cfd9bd79dcc1d8856ff79ac
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:10:03 +0000

    century programme bottom delicate 571enough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
