d9882ecdfbd78d1ed02626ec265d40350b2b832a
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:07:43 +0000

    clear legal new who achieve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
