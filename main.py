4bbdd9f8b6b6521486bc6c97b30b4feeeaed73af
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:37 +0000

    behind accommodation type total start

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
