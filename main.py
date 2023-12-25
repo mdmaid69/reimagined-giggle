90bc359e6ee1ffb90ac1da82022286e4a905e490
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:04:12 +0000

    punish sale 573entertainment equal payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
