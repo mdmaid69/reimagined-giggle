5f9ccc4647a87096dd8664d8b8cb611a446c9748
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:40:06 +0000

    defend clever standard skirt price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
