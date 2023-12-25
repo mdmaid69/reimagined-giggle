11c7156919e6515982398a6d2bf18f9409ab2739
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:35:42 +0000

    steer dig expensive sign birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
