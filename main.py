920e5969ef12c15c10d1a53f6d4a84750a9b663f
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:53 +0000

    tip suggestion fresh story punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
