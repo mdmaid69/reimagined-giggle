a84a9fe7539fd2c1018626003fe5967422838a42
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:17:08 +0000

    usually limit woman success rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def delete_file(file_name):
+        os.remove(file_name)
