146d94bc0b13f59a863814ea11fc1cb957fc1ab2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:19:31 +0000

    doubt left once false Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import random
+print(random.randint(0, 100))
