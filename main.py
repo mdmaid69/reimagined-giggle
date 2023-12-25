3548f9726b01ab9d158f5f5ba9d31a2f186269ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:53:30 +0000

    slide rough bound tape same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
