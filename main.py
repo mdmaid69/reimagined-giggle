3bce31d7046ed797a13a6184dd32746c9626475e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:17:47 +0000

    explain model control just free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+for i in range(10): print(i)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
