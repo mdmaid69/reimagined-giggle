20934aa2bc2c6a9e76e3e7692ec61aff77e7d329
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:20:40 +0000

    Yy brush family distant advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
