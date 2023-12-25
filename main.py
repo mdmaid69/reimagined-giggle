6ccbeed25c47ba081e6b6eb7667366a3efadcd16
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:56 +0000

    Jj formal among classroom art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
