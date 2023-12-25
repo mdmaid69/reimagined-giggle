65f55e814a79b8d31f180c632e8afd18d3931c25
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:03:26 +0000

    lead flat refuse occasion under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
