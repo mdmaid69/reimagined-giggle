5298bd41e1c91730776f99ad5e9d01fb91151457
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:14:06 +0000

    usually explain defend lookaround change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
