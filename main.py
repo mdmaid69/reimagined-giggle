5d71c2f7f37f6732966b7f61af8da5275d779205
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:21:36 +0000

    to surprise talkoutof cool maintain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
