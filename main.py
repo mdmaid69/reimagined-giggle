2a8cc80b36ae4c4a3df720cd521e63b08e7d71a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:38:27 +0000

    free once plain factory sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_cosine(x):
+        return math.cos(x)
