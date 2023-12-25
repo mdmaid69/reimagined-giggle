16737f796521105f490bbe2708f94a90bd85cf84
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:50:59 +0000

    chin willing bank cut claim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
