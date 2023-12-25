447ffc73378ef6539858c9d5967464779b53dd3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:17:08 +0000

    from regional spot spill file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
