fc50cf1ea7e779a08cf2671f2980e3dd8462e612
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:36:43 +0000

    service suddenly entrance receive empty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
