22aca46848f20451cc1749596f703628b2261672
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:39:53 +0000

    aim contrast arrive born dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
