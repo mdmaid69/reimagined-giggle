b89851fa7fe91d90ddecad237ccb3c36f20d7e9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:53:19 +0000

    waste 573entertainment wave ticket rescue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def sort_list(lst):
+        return sorted(lst)
