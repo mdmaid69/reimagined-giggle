7b783cd879fdd91b362bf78a873f5e325cb5c996
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:17:43 +0000

    complicated comedown ought suddenly relative

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
