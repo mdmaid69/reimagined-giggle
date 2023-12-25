0f92d35668c69903c621cc955f968b701698fec0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:55:05 +0000

    surface switch murder person Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import time
-print(time.time())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
