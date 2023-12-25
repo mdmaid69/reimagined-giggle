d709a6e06c631ed622be292f44c3cc58fae6f942
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:43:41 +0000

    dull cook angry complaint rain

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
