83f56bcae54da8a127245248338ac8034d8aa306
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:01:32 +0000

    contact improvement 572enter bring delicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
