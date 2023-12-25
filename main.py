36c368f0c3b17c7558eb951ae9a861b1c2517f5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:59:14 +0000

    satisfied mind offer loan shock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
