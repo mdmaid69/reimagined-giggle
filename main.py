fa6685c8126947a94037db65b86d9c2ad707b034
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:38:44 +0000

    drink fail feed natural advise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
