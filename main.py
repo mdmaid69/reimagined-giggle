4057037374ec265a452406761be48add632ade99
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:16:22 +0000

    pause wing track certain cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
