cb2c837524150707ca75bb1601805314c4593286
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:52:39 +0000

    pretty quit wall communicate past

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
