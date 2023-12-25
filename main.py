f62038452a2dd7d47776553053657bf2031d852b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:38:10 +0000

    suggestion journey 572enter apply medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
