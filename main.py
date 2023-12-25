606d2952f66d23735b2121b3f676553027ba317c
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:46:47 +0000

    her play true contact front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
