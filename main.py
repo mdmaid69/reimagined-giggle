677a4aea14bd93598d156484447b3232bf635188
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:50:41 +0000

    suck provide hello wall bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
