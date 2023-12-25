198cab0df53b6f8126b764de77d1dd49847e012e
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:56 +0000

    meaning state curious vote beneath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
