77eb4bddaa157c0a077fb67c764ab4ea5cadf72c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:26:07 +0000

    road Kk claim ours figure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
