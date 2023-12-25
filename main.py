70308ce137a51bb4b0536f4ca19574b5146b866d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:28 +0000

    somehow lay draw there comfortable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
