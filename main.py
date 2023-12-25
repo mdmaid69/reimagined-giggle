34d32757dde7a0bc70ed846acdaf1924225407ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:51:27 +0000

    careless final convinced when theirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
