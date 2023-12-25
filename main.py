856b2e60c1eb9a5969d644928990faa3d0f811dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:15:48 +0000

    bang vote less 569enjoy divide

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
