f4932222cc785a1fd6622aabd838d532c56cab8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:36:15 +0000

    keepout keepaway blind whoever accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def convert_to_binary(n):
+        return bin(n)
