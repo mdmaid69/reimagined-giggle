ebffafb2d9316110387107f73333c57b1efaba1e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:27:12 +0000

    wash thinkover giveback formal common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
