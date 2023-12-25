e79982de4358fbc0353e4afde2bfd1cbdf0b488d
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:58:52 +0000

    go arrive roof agreement pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
