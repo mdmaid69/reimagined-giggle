3f37507b17f8a27427e799563934345b66bb058c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:11:29 +0000

    confidence Uu love serve image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-for i in range(5):
-        print(i)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
