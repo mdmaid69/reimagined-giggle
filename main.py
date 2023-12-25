1de82872d9d219cc7f84f48628fa845ff64de652
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:03:04 +0000

    person building tall bang gather

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
