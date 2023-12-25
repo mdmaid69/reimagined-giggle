084723f2292ecb8426ad2134c557b8daff63aa7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:11:57 +0000

    gift left rob act operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_max(numbers):
+        return max(numbers)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
