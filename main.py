75dee5557dde43ca8798a9a2c081ca851b46fd13
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:09:15 +0000

    hard continue callback high mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_tangent(x):
+        return math.tan(x)
