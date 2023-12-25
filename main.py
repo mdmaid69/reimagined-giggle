5fdbb0aec928b8d6d31631560de1fc09a56e8e2a
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:34:45 +0000

    week week trade battery ticket

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
