235293fb86d79fd8623b451be001f33007aabfc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:12:41 +0000

    deep fight callat computer world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_item_count(array, item):
+        return array.count(item)
+  def reverse_list(lst):
+        return lst[::-1]
