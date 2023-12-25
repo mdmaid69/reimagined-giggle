6253fa4ac78f375691fbed67d6516a8ac5e22452
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:26:48 +0000

    taste exchange quickly 573entertainment communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
