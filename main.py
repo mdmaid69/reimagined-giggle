62633da1fd50fc0c27ac01b00d7dd37fcaa67c41
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:53:17 +0000

    fire supply father comedown keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
