0de517c5cef5838549ca8886f99ddfe6e4b23cdf
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:08:30 +0000

    less field mass sign exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
