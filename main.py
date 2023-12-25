4917a3ffd2c9930431f69d193c2e06e52184be44
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:41:11 +0000

    off dark art hello hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
