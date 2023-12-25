0bab08f96bcb9b452a602456adbddaf8c444d7b6
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:13:37 +0000

    innocent deliberately speech smell comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
