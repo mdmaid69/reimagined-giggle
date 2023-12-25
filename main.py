93412ab3bc286266cce5c363f1b42e8a13e8cfc4
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:22:33 +0000

    shop sink Hh sell pain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
