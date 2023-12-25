07d2c1ec5f95a7813cf38c0cd036439d7740e6dc
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:08:11 +0000

    settle no visit container needle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_power(work, time):
+        return work / time
