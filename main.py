34a4afceefe73e069761f2b39b88de2d367a0561
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:10:53 +0000

    moral welcome accept Mm mean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
