f9c31987a7794d5bcb409c63b39dfc8bce166990
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:13 +0000

    first keepout afterwards somehow set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Hello, world!")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
