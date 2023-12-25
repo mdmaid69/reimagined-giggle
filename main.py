dad24c7bd1630742eb2dde6305773aec01f26414
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:13:21 +0000

    mind kind period advanced case

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def remove_duplicates(lst):
+        return list(set(lst))
