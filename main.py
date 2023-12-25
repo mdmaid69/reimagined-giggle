c1bf0e510ddc8f8bb8503e3e738fdc3277ea0d6f
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:15:17 +0000

    hurt stretch right stiff chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
