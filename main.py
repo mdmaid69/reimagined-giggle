108d3fef59c4c1fedc23cf91b4fcb1f1a9d74cab
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:56:02 +0000

    machine official programme grow within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
