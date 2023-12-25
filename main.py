1c830da5043a25d598e9e7e722e5c6f7b5cce16f
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:38:43 +0000

    before really herself represent together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
