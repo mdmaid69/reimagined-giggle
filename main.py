b27cde773422b80177b9ac9c436c333f69bfa5ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:53:14 +0000

    work possible peaceful meanwhile since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
