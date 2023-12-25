84080519c59e918cf88e4a02448383fd78632e18
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:53:56 +0000

    snow needle welcome time add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import sys
-def exit_program():
-        sys.exit()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
