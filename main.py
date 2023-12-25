5856a5fb56fdb02f3a415b915d8ac69b670048ca
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:18:50 +0000

    finally copy contrast hollow formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
