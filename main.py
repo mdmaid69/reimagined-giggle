090a285d32fc18c2de62452b23f93145249de34a
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:39:07 +0000

    suffer backwards vision belief often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
