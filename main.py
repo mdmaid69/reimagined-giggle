b0320c5db51f1daa3f66090b037f89495109aa4a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:08:30 +0000

    lock vision improvement scene lot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_binary(n):
+        return bin(n)
