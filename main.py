7bc7fd5d8e6f7527992b833dccb58b0e2f3c2ec8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:33:08 +0000

    noone repeat face plan lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
