beadb115845f5f69f31814533bccf75fb950dc5e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:38:35 +0000

    clever catch kill week shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
