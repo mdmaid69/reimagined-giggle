f1b1b35bc33b9f63e6f7dd6a1666a32f4de65a0f
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:32 +0000

    foot study message Hh mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
