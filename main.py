b844b2fcb0a908190a159c11977ff94752ec7839
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:41:47 +0000

    best break involve breakin why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
