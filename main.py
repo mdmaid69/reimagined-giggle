c53575b49a2ea332bd86d55e5f843efb45b43fe0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:45:15 +0000

    takeback hospital brush many factory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
