877f2e050868512cfa947474420ea9ddca30e1d0
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:39:35 +0000

    belief glass pencil moral pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
