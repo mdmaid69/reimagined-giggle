4f785b90f40694098f088872c17b36e0850d1bbb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:18:56 +0000

    size apart clue across seriously

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
