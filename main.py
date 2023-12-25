0d598ecf18495e9e179d18b41af808d1b8349abe
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:50:40 +0000

    ground pay connected Itself clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
