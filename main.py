ae188a750fccc7bb2f87ca498a32147ceda17eac
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:26:06 +0000

    improvement direct hollow argue holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
