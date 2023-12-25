25c2927808a745fb5c06dd00a1bfab62170540e3
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:18:54 +0000

    turndown hide region set call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_itemsize(array):
+        return array.itemsize
