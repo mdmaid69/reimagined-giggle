afa23e4ba1e7134b8917a186cfc15478223bf695
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:23:14 +0000

    brush extraordinary there human praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
