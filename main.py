1c2eb683b0822f82e0d7fcd044c70a3e49e68e75
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:31:23 +0000

    hide complaint comein though skill

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
