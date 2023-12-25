a10bdcff6be3750317e037db39c43f19586d6716
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:45 +0000

    morning breakin opposite area interview

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
