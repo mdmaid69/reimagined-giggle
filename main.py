1e50746dc376cb2dc64a3ef37f71dcbdea652d0d
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:00:29 +0000

    size scream convince mistake branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def split_path(path):
+        return os.path.split(path)
