75ac0f9c6ec139fbaf6067e0e436373409ee41f3
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:24:54 +0000

    understand from closed month should

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
