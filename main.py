adf1a96cf1f93a2f253652c72f4de9414d697a27
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:59:37 +0000

    shut payment proud fire emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
