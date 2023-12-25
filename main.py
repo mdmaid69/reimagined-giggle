c3b6e88c6a891438d2cb28f083b8ca15c772527c
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:36:27 +0000

    scrape surprised process attempt behave

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import sys
+print(sys.version)
+import array
+def get_array_as_float(array):
+        return float(array[0])
