003b8c3445658dfb7caaf049981e80d0b9ee4fc9
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:07 +0000

    steer likely injury form along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
