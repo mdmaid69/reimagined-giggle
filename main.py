b81315dffa72feaf648c8a1ec067813900fd4775
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:01 +0000

    crash unlikely night cope straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
