b47287cd970684128c4c0235c4904207ed3d0da0
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:43:42 +0000

    suffer giveup safe watch geton

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_sine(x):
+        return math.sin(x)
