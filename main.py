f07c13eaf0b225fa5a9916577be1d8c682394119
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:30:33 +0000

    bye operate lean operation radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
