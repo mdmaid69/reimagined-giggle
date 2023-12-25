fea6751496cfdf271a57a925889318037f69e493
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:39:45 +0000

    half ground danger lookback(on) main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
