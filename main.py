b9e281cc3559139b6bee5e48600b0f8e83a91701
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:09:08 +0000

    win imagination since feeling interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
