2745cf321e705c98a1089a3defd04bccb5adf489
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:34:25 +0000

    lead workupto approval petrol shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
