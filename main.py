507379629986b25829c00b40db0a8ea3611283ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:33:59 +0000

    patient opposite easy judge believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
