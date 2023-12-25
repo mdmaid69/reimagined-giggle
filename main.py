2e412c4c046424c24edcecf31f428b0e1c6ce34d
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:01:25 +0000

    birth rather immediate finish satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
