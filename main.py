0d13797070268a255bc2b5570c0a64f7bb88c088
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:24:08 +0000

    design funny beneath skill succeed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
