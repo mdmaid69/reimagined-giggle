288cc3dcef2e9f198debf5f42cfbe42dbb4e6509
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:47:23 +0000

    section grateful grade claim raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
