bdd7849744eac99b30b1a2b7b0743d8e86525a26
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:27:04 +0000

    benefit stir honest declare breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_min(numbers):
+        return min(numbers)
