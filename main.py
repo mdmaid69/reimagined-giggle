beefa67020125dc04ed4c17522aef8943c65c727
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:21:49 +0000

    already chin popular wave dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
