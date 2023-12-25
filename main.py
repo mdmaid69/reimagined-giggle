9ad7774e4473637f3b3ab1588f7c446f1e39556c
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:05:11 +0000

    over leg shout vision settle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
