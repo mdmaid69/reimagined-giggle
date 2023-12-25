6a8b8f0c1bd2d79a7c4ee2cb625b75eba4168a65
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:25 +0000

    final test fill hole explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
