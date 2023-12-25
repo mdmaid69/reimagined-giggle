e23d9b457cbfb0ddc4c1c3b723a5809e3ba53bb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:41 +0000

    organization impatient cut swallow fat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import re
+print(re.match("h.*o", "hello world"))
