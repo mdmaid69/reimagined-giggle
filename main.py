6201d04bb6e0bebcb9f59fd5a49891fbb75143b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:41 +0000

    all thirsty suffer action woman

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
