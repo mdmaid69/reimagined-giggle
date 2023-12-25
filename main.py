4f97610558d7fd11fe3c3ef6ddaa9f39ad8949f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:33:35 +0000

    priority otherwise frightened stop ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
