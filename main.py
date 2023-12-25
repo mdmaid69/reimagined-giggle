61fd3f360304a014b37ae27bcf6e41b6dac9b446
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:37:14 +0000

    behave occasional support lose frightened

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-print("Hello, world!")
+print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
