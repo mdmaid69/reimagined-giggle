6a24cce63ae75c81ee0010c50b58f97058d40169
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:39:29 +0000

    exaggerate expensive movement experiment skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
