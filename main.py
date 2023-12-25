eadec7811f5a3fca6a387cb24e06cf37bb5a5fff
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:11:44 +0000

    address opinion neither arrival knock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_user_string():
-        return collections.UserString()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
