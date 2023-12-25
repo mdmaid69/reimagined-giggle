f0461f11c75231ede758f66d35d29e7b2b26fbb5
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:13:22 +0000

    fairly taste dream glad advantage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
