7c7dd8a1c1af2fc1b42f37814bdde6d2b664cd60
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:04:49 +0000

    naturally makeinto dangerous date tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
