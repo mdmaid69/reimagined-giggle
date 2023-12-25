aa962fa1a09da74e78040d0d3ab15587a7cff86a
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:32:02 +0000

    stretch argument once edge back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def reverse_list(lst):
+        return lst[::-1]
+from collections import Counter
+print(Counter("hello world"))
