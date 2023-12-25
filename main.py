8f5e4333fca355cd2972039c3de20a5453bca115
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:57 +0000

    equal swim miss truth match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
