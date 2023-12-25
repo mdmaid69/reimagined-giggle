b703c4b9b0482dc1b1e26d2a0939a922e30d7dd2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:54:29 +0000

    exit concentration lead aware protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_palindrome(s):
+        return s == s[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
