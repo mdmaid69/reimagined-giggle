bb2cbae254e7f22c55ca8be6b679cf6119ef4b8a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:50:25 +0000

    police port control cheque factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
