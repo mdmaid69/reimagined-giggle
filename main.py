2d493ef9668a4429a20d3d8729a8cd270984b8cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:13 +0000

    remove past yours confident grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
