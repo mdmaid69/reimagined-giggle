eb6676b2f4717f3d3c0f288880abc9e287416d8c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:37:49 +0000

    second whatever bad deliberately for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
