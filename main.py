77c98406726213990e83eece665812d5ae68889b
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:10:38 +0000

    my go block tell smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
