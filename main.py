c27416e7cec4dae1f7409e665f637053fb245cee
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:44:49 +0000

    method wind action special repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
