12c0b5cb1af190920b471ba78202b691d9ed9488
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:08:25 +0000

    mean course will reduce deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
