10d4f19873daa6cbffa181c4e7c1975febf0ebab
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:04:31 +0000

    promise anxious lift out she

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
