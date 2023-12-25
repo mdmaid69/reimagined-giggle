ce740fbf461b1c6ed382402a0192789245fcbd3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:16:33 +0000

    talkoutof shoot peaceful comment become

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import datetime
+print(datetime.datetime.now())
