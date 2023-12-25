07adbff6ceaecbb0096d783682b42d386c82e277
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:32:04 +0000

    post tomorrow keep congratulations foreign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
