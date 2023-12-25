95cc566219ee3ea6ddfcd2b23a7f4840d8310a17
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:58 +0000

    while juice petrol disturb book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
