3e15a964ee7b6fda4d12265fc7d5b6e9b05239b3
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:42 +0000

    comeoff related past free sit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
