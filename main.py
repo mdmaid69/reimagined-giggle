ad167c707f25e18aa58cde41ad4a4f8db7d28930
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:41:53 +0000

    wall goback(to) shop indeed maybe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
