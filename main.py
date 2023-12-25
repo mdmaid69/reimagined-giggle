7141504e51cbc77e954ce242a6907c4c8ecb30ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:20:44 +0000

    smile point shallow support ourselves

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
