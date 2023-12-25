695e9342e84b1d097c1c01e902cab9a77fe202aa
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:44:00 +0000

    record disapprove what appear correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
