89ceea6ca7cd43a5d4ea5bd8b9eb5d90bef679ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:27 +0000

    payment bear turn car steady

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
