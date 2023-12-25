230aac9d9e7ef596897e980ade0e8cef426a23f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:45:17 +0000

    spring confident matter miss clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
