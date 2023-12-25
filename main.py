3d6159d7a947d6441e176bc922fea0a23c7ac11c
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:57 +0000

    mass holiday legal hard appeal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_str(array):
+        return str(array)
