d26bd279f002aa36fa9a6ef818186694536b5ffe
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:58:31 +0000

    thing let capital bite experiment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
