8ceca69b6e14e5003ede299dc2ce30cd281a2b8f
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:00:46 +0000

    approval might hospital tidy hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
