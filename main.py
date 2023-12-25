01a7d61e5cec42d788a8412e8939d4c5d0f9349e
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:13:24 +0000

    form cause circle exercise valuable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
