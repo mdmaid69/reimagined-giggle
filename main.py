5e26fd689e9d17d47e9c319037eabf79e1411cb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:58:50 +0000

    concerned major fast star loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
