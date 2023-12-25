c195d2b4a495e166119816ccb404ce71fcc32471
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:35:57 +0000

    turnout stare that round famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
