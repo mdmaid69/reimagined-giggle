35d8dcb1498ffc9e2da1e9ed2ee5b6a337bd8045
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:39:22 +0000

    mood over theirs strong thought

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
