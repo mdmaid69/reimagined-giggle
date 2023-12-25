e7da972e2e95d21aa43a027eb67fa38dae9de6b7
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:00:11 +0000

    roof separate tidy breakup object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
