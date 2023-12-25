b3c22d33aea04e15896c2e39262ae470384ecaee
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:41:42 +0000

    opinion fire bored spare light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
