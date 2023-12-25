ce14a7af382072e76ba8179b9bd2df51e2fb48b0
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:10:05 +0000

    defend travel you land convenient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_force(mass, acceleration):
+        return mass * acceleration
