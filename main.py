12d5b2b011119346e6e986588250173d6f79dd3d
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:59:55 +0000

    gas would drop man feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
