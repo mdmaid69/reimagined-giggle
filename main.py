d59d9f85174948185f3836702c96b7be607c5236
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:17:49 +0000

    shop glad succeed film accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_list(array):
+        return list(array)
