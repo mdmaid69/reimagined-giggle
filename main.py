75af9589df45c2d1238d773a13e8a36249c7bc52
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:27:10 +0000

    with keen dry below fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
