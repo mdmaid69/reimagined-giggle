3f2d22b32429d1c9de0fdaa79fc460c6b30ca873
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:37:05 +0000

    familiar Itself holiday profit hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
