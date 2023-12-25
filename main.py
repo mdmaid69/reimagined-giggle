ce75c4bdf047203d92992787a5c283336ed52d50
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:28:10 +0000

    lose ability escape possibly heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
