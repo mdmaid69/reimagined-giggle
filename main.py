1179e7fadb98837397c50532f947e20016290c01
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:32:43 +0000

    valuable threaten sentence bet below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
