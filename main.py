4a1a40293e3f35ac29f195ad5c4b6388ec71e29d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:32:59 +0000

    boring show hurt chat reduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
