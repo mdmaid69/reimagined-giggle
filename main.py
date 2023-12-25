b78317acc40dabdb5341b7b7ea81212e026fe6f6
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:52:59 +0000

    soft grand smile convince under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
