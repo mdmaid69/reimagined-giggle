c5102083627220a108f138ed04481d4274f3ce1a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:21:31 +0000

    suggestion push prevent object murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
