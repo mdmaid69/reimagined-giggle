f0e4f46eef9e98ea7005e7795b7e8c54d9c670db
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:18:15 +0000

    behave else thin nerve hospital

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
