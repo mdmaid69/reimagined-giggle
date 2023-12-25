40ff1bf1a0faa38aa9109087183958f9e23940a1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:43:55 +0000

    small crazy store talk description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
