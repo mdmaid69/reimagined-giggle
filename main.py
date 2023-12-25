33fbedc3963c733030b71d294d4b0d3414b7e4de
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:11:17 +0000

    between hole exploration how impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
