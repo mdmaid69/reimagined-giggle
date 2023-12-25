c9b75cbbe5a41d147a42d2224cae4c232317ede1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:25:27 +0000

    gap wire dick little me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
