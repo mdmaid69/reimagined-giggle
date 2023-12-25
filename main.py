d75566b9b6e98969f065b89d9c7b88ac5d9f1d57
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:15:48 +0000

    sign open never frightened soft

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_array_typecode(array):
+        return array.typecode
