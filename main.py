778261fae9a31875d115d6aff5983d8654899d81
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:52:35 +0000

    dare ring bother back murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
