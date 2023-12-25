ae4fa32a1515e7bafa1ff2e0770f432c2fbc98df
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:42:00 +0000

    strict secret square offend sister

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
