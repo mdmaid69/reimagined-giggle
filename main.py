5eb244f6c1fd9a6c8a8f0704cd571175d01789c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:48:52 +0000

    determined year union afterwards mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def is_palindrome(s):
+        return s == s[::-1]
