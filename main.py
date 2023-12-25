f311dec5d1c17de5a63a2798ad2b22e3ede7e6b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:22:12 +0000

    pure force back workon direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
