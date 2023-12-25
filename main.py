a7177a325913aa29a4750a29a143900dd7c6ac65
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:14:31 +0000

    everybody variety fix deliberate answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
