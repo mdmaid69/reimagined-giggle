518153f3b0f5ebd9a7263a6c787d5a08b79eaea7
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:17:14 +0000

    earn official behind wild criticism

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-name = "Python"
-print("Hello,", name)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
