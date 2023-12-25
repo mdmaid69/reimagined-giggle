47260d28754359e49373f51a85460c85d526807e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:12:26 +0000

    burn raw play workon sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
