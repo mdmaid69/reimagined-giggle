8291f21e7446b92e579cb831e5874b3cea9be52b
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:27:06 +0000

    form dull slightly creature reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
