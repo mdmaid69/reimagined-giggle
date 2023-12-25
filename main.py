4b345c0d43b95e4fa479977c90fbe727c8b4bc5a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:32:12 +0000

    difficult that load season certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def find_max(numbers):
+        return max(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
