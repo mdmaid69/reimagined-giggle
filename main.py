5bc8fb0e643e3d924fe8df3bc8a3df700321ee3a
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:07:14 +0000

    speak big defend goon shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
