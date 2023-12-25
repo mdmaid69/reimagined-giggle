f85d9edf2964cc3de26677ab2ea7d6961b6b6f55
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:56:50 +0000

    worth declare hang kind attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
