c45fa6f37b6dd2651761ee9c7d3ccbbda61a7b99
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:35:49 +0000

    congratulate onto cheat sweep needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
