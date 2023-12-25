eeefb71edb0b88565a9fee751a36ec5bb15e0d79
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:56:41 +0000

    by steady tear expect takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+def is_palindrome(s):
+        return s == s[::-1]
