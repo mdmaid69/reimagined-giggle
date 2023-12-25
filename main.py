38305cea104de6dddbe5d04714d2a1b94572acf0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:03 +0000

    everyone mail up fill report

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import random
+def roll_die():
+        return random.randint(1, 6)
