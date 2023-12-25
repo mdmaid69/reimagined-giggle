db6dd4412e89521b7313cb59e25cdefe9b5b96a9
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:39:09 +0000

    final here expensive complete tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
