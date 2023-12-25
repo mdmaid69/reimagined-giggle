a0f154865e3aca0f24d7f03513afc89cf1a837de
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:10:00 +0000

    pair tough learn float father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
