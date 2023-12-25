1b7091b01a688e70225c0e35e6544c673eb09f58
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:25:29 +0000

    responsibility 566end careful month comeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def add_numbers(x, y):
+        return x + y
