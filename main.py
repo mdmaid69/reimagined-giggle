3c85642974a31a0945aa1e07ae6de3e1f1fc931b
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:11:46 +0000

    sensible takeback piece pollution travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
