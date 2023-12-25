881d8c0f798312d954657c003da4053421daf3d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:20:28 +0000

    emphasize meet relief confidence mind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
