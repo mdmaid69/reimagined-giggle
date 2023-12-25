75d115b4b2952896267e6299345abe1acb651177
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:05:22 +0000

    turn Jj anything takeover great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_frozenset(array):
+        return frozenset(array)
