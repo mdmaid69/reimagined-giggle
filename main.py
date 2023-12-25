7bba1514e88d568797063849af424fabadcf358e
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:25:53 +0000

    dream breakout her scare report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_list(array):
+        return list(array)
