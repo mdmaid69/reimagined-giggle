55603f304f56a56a3615afc302308e86ffcce37f
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:31:29 +0000

    clue wing lucky scream on

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
