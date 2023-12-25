59648c2482518275cf9e3e951fa228fc4a93055c
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:39:03 +0000

    deaf earth 567enemy fix unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
+name = "Python"
+print("Hello,", name)
