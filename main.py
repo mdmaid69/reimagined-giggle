d8e55d5115a7ccc5e916e04ffed6bf5f9f725a1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:47:12 +0000

    respect war use free uponon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
