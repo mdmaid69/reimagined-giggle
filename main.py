e8c752a614f6fb0d532ba6017ae98f59d3ff2394
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:32:55 +0000

    stranger anywayanyhow confidence rent way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
