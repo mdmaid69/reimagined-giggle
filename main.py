b1e28e20857e96615e2dc102584615c71205a5b9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:34:10 +0000

    embarrassed future negative success this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
