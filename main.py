afaecce0113f9062da1a7e4403d51f4894505db0
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:40:59 +0000

    careful broad means concentration fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
