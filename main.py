8f767bf0582713a95414529bcff9d18b139ade6c
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:00 +0000

    it none classroom ago how

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
