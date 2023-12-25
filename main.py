3d81f014c2253edabcb777c6a6dcfd0e5859f699
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:42:44 +0000

    geton lay might impatient exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
