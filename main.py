b5fca889aab62ffd76170cfb47fc16384df7c0fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:51 +0000

    treat successful gather shine sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def multiply_numbers(x, y):
+        return x * y
