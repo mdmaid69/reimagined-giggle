1cb9c6b377024e8c36eeb6cd3e95933106c3c6d4
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:04:00 +0000

    risk reduce female track variety

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
