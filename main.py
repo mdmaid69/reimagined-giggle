13b9261db4d7081a86863d3daf23749307a2bee7
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:24:10 +0000

    open lean myself begin defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def create_user_dict():
+        return collections.UserDict()
