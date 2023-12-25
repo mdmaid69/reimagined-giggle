1960cb3a644e7d133e5c254b99230244755f18c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:55:43 +0000

    set unpleasant hour often fat

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
