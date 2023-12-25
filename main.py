a77a54ebf744aadb88d295bb888d57aaeb9583d1
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:53:24 +0000

    spill spend hand of behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
