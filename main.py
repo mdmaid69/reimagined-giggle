2fa3006acf1b4a4e1313b0e060c8a93db114d666
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:51:37 +0000

    bread upwards naturally autumn nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
