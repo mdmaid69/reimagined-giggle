b63190edd5832551449721f6008ce3b0efdae348
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:00:57 +0000

    loan possession determined shelter muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
