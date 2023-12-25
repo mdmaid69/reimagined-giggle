7354cacdc80ac35370783abff6e838230293906f
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:21:28 +0000

    doubt authority someone noise chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
