5273893c53fb8425e80b9fd804df569471ad8fb3
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:27:53 +0000

    concerned early pressure stop culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def convert_array_to_list(array):
+        return array.tolist()
