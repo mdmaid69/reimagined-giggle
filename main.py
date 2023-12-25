4f90ba9fd09759772c6c240a45245b5bb43cf96d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:05:17 +0000

    bottom union almost protest cool

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
