0e6af7b28f82356c25a6e38dc7a6c440fe7795a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:51:52 +0000

    find way method trust any

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
