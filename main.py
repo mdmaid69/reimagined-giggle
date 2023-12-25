81ffe803df57b0b99d8021ed0b58fc3a76b1fd36
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:14 +0000

    dull condition workout turndown reasonable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 def calculate_pressure(force, area):
         return force / area
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
