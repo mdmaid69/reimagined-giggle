82fb23e1d109ae25493e67d75664f8de7a7ee8a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:34:09 +0000

    able fast schedule couple seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
