43fd17a454055f14c7250ee6b925f3a7b583d7d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:14 +0000

    lose among picture sound buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
