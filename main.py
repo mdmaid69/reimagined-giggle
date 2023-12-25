85b389870b881d443d8b860fa875fef6bd67c49f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:51:44 +0000

    total actual occasional snow base

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_str(array):
+        return str(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
