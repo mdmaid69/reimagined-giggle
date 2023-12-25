2052226921b51099fde0e7dbca43b904f26498bc
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:19:14 +0000

    attach claim none lookafter neither

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
