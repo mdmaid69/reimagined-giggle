bc335c62160413b2e5cebae4ecc3a959a934c0ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:43:31 +0000

    length workout able rescue shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
