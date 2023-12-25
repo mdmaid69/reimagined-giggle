3b4923006b285dbf4d06c4c9aa3e3bdbf8c10702
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:24:08 +0000

    I play include print know

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
