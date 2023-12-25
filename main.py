b17489ebc8a6d7f081cae37264967c0342a5445c
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:14:53 +0000

    fault over moment turn film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
