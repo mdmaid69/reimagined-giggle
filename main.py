78e52583c2f4925e485f14bf4473552f7012fa3b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:32:09 +0000

    comeround desperate minute moral sound

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_min(numbers):
+        return min(numbers)
+print(sum(range(10)))
