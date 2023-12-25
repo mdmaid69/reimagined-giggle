5701248b199784b887d9893185491c8fbdd635d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:10:18 +0000

    for turnup mysterious brush simple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
