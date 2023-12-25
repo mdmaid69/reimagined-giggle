c698c8d0e485af1821b25ae0474548b80da6bb7e
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:25 +0000

    shoulder die pull band common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
