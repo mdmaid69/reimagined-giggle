c62e6a709102fa7be98daa4ec2c6d7334dee97fe
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:48:50 +0000

    along admire smile day extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
