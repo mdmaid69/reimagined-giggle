fe82f60db1b89b4425cff1bae76e6b789e39a72c
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:37:36 +0000

    getback guide buy sudden it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
