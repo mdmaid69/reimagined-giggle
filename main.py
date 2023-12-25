92efc8d519bf4edb724489fa1d8aa72db7875454
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:36:24 +0000

    nurse invitation fire squash interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
