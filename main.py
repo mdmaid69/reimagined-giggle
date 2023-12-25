8a3595672c29209a343b4a8e0f2c26e62d65c46a
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:51:54 +0000

    hollow add will normal crash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
