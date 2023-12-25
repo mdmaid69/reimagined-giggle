fe9d09dca31653a530def340783b8e7b609bf085
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:15:29 +0000

    amount under wind demonstrate vision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def convert_to_hex(n):
+        return hex(n)
