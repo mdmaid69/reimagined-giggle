c1bf985a3212d509332cefae98bd39859b0d8658
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:50:13 +0000

    first habit grade price talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
