4d01df7b0aff0bf14bc7efd8e394d7a442f6b629
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:05:19 +0000

    future runout friendly big basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
 import array
-def get_array_length(array):
-        return len(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
