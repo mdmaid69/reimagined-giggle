06d141ef7f3f147f0aa2249dd12897682a9c79cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:21:59 +0000

    class full accommodation borrow unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
