0e585d70638ea46a708d5c1f4ee3e4a6d3504776
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:23:03 +0000

    guard dinner coffee patience bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
