6e3adf997e8c2e7b356995fcdc6bea2c71075add
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:34:04 +0000

    whisper often 572enter getback drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
