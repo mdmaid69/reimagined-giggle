f65c71a4699aa61744a86a37f3e510408b0f683c
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:37:11 +0000

    cause feeling complicated tough wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def get_array_item(array, i):
+        return array[i]
