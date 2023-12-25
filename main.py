1452b8bc5768132ddef689ebc8b26099f3d8b2f9
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:52:30 +0000

    regularly unhappy keepaway period hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
