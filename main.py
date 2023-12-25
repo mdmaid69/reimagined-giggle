9a55cd5e57a7ad1a8aeec73294d59f439c0d7239
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:47:37 +0000

    equipment hear quantity minor report

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
