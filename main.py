5913f4a54c812ff48e3918ba857db6575f7da877
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:00:15 +0000

    save habit crisis within takeaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def count_characters(sentence):
-        return len(sentence)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
