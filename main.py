938ba7daf545a48cb94e1ccf7f336720214a457f
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:31 +0000

    rather mention talk keepup offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
