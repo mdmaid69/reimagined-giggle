12dada7f7fc0b969b94b9ca7f8d793a2f60c3a99
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:51:19 +0000

    secret basically unable fit minimum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+for i in range(10): print(i)
