2dc4e3777b7bedba1aa806fdf34637aebdbd523d
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:33 +0000

    bread runaway design smooth bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
