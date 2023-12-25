ea6687aa739de11387dbc310dbfa02ae5be369a7
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:36:11 +0000

    congratulate central argue swallow confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_str(array):
-        return str(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
