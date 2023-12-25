519fa763234d6345e5635a0061220209cc3fd396
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:44:05 +0000

    suggestion weak steady away convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
