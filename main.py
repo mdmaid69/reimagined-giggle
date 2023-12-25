2561448e3e3a9241e886e04cc2a2dea2b3fdfb7a
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:00:58 +0000

    definite form will late wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
