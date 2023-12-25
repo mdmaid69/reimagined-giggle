8df2831fb3ab9c5b18d3259d97ee240cc4b21568
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:18:50 +0000

    switch honest proud record politics

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def subtract_numbers(x, y):
+        return x - y
