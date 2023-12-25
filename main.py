a3cd3c20515b3887d0e5e299272651aee3fbcfee
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:33:06 +0000

    tidy serious scream cruelty spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_power(work, time):
+        return work / time
