7cd96017d1b7675c9c3cf4f8d1be8c46c1bb7192
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:25 +0000

    announce within shopping concentrate fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
