24dc3523b47762d2cb8ce3f915f5032079b3bc7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:48:40 +0000

    handle marry business top passage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
