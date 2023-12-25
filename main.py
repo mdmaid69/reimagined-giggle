22990277283819aeddc98f550629bad2baf909b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:47:37 +0000

    question dream murder reply after

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_power(work, time):
+        return work / time
