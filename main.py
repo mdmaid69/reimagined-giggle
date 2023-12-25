ff12dac72ae785b5563c7cb37d2b867304484879
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:18:20 +0000

    obtain game target something answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
