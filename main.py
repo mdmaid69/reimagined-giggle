6920e9378a2f4cd5bcd7aeec9685abcf0918053d
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:26:06 +0000

    Pp gap sting Rr positive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
