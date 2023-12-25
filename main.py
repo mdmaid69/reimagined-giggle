08982096ab527ca631f150e9de5a56a137815453
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:05:52 +0000

    some tap line confusing holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
