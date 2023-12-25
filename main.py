2d7577597315b07589bee6e110a4fa2c55057741
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:48:30 +0000

    hour simple towards myself claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def multiply_numbers(x, y):
-        return x * y
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
