5f0222ac153aa5bab31af9e2099680d87782cc50
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:03 +0000

    stay smell bringdown material pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
