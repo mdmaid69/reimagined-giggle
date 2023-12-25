3aede91a832de9daf5cd49ec8224a23b8d0dee40
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:45:54 +0000

    regard remember room level course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
