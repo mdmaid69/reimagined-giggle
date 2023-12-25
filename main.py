9cedb05467087fd10a3abb89178c867c796bb1d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:53:27 +0000

    trust suck drive talkinto mention

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def reverse_array(array):
+        array.reverse()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
