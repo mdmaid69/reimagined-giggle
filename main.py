d419b019b7a8071b75134e2b9afc82b96c7fd4b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:54:37 +0000

    heavy fill influence temperature male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def remove_directory(path):
+        os.rmdir(path)
