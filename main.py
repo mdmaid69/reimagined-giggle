491fc858fb69d85c85230eef7aca76529836bb1c
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:46:40 +0000

    survive degree rubbish deep love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
