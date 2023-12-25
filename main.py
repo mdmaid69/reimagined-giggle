bedaec6ed502e955a2b597b865f5cf61e6c40e12
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:17 +0000

    attach again drive business absolutely

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
