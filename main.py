94de47079f15dbf51299f8fe1e25b53bf27700cb
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:13:45 +0000

    tone instead still embarrassed sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
