f819c06dbe00197df2741d857e9d0952772535e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:27:48 +0000

    least nor spot trouble long

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_max(lst):
-        return max(lst)
+for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
