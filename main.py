1eaf8c4abba95a082725b12fd161cfab3480b601
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:03:32 +0000

    wild influence division contact seem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_item(array, i):
+        return array[i]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
