13b1c2cb68a644399730f4bba97118281728c979
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:21:18 +0000

    well back main subject motion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
