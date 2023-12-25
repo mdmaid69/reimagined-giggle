5a1e9b184560a0268ffb20d9887d10caa0d91baa
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:56:39 +0000

    our blame deep brain ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_area_rectangle(l, w):
+        return l * w
