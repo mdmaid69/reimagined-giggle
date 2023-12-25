236ebe8260af5036e4e050de5a374cf61cea50d5
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:43:08 +0000

    if risk drink wrap black

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
