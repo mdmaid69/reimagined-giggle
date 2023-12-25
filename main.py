ab76b2a03056dfeacacab1f43ab1332a1a8d0ebe
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:59:14 +0000

    glass past sleep tone debt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
