1c9f0690647cc4d7502fd090e0e6479a321799b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:07:08 +0000

    gather blame rest at priority

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def count_elements(lst):
+        return len(lst)
