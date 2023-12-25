767c8cb7cdab190d68281e4292d6f399409bc228
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:41:19 +0000

    her small fashion characteristic Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
