e9a19afa28fe65397a9281a3df96be7e8a879ad8
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:43:39 +0000

    impressive warning tip benefit anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_density(mass, volume):
-        return mass / volume
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
