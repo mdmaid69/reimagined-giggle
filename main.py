c8691ba933d1e63bf3d2fd001eaf95336aa3f378
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:26:38 +0000

    wish slowly regional make connected

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
