e207ecf01f989b5c1d10a788ff51f35e222a309b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:26:14 +0000

    about tool bet simple announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import time
+def get_time_since_epoch():
+        return time.time()
