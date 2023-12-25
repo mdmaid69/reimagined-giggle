e194e6a87444fb73a5d4a430d627f338a087639f
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:35:00 +0000

    advise careful complete likely guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
