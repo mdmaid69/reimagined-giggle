d3c9390f00098b9d85388e3cc1dc1ffd1b711047
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:36:35 +0000

    rise actually university report both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_min(numbers):
-        return min(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
