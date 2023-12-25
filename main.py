5a399599ef968a84e53ba91604c8050939ed34cc
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:57:50 +0000

    uncertain previous break positive cause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
