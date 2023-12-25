1df67740b63fb572d3f0b7094b70a12c90ff1b00
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:27:09 +0000

    public interest none exit apparently

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
