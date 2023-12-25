43172c8ae671cf8ce795b8abf95d11864e0d6a8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:45:01 +0000

    trial threaten Rr charge runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
