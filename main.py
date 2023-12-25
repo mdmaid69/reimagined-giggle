dc9bf9ed156ee4f1fc55af14aa5499b0b20879bf
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:29:58 +0000

    its rub chin sea modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
+import array
+def get_array_as_list(array):
+        return list(array)
