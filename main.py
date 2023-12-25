cb46a9e2b2bf71c1dfab9cf19817c64e281224ae
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:23:34 +0000

    approval praise uncertain embarrassed grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
