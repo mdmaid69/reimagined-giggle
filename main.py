5999d4db4e709f12b3fa140c60bf83bc2e42c43e
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:34:45 +0000

    mind our later black would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_work(force, distance):
-        return force * distance
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
