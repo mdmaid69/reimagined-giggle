7c2746be6f93c234cdc34ba8a09cf94cb6c2421f
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:49:33 +0000

    break surprising breath and custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
