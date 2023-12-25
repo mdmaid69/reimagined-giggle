b23ffc60e3d369e22ed5044882aec80ed8b399df
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:59:48 +0000

    announce forever back sleep storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sine(x):
+        return math.sin(x)
