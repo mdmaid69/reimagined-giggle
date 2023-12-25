7705c4ec7f6f1dffcae88fee1c3c7f36d9da8856
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:28:22 +0000

    hit unpleasant office difficulty slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
