451c7d5c9bf4371eb35f4849914b923f8d8fdd43
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:51:27 +0000

    flat rush upwards improve estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
