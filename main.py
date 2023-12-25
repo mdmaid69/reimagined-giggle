4230ac0a41b737d85a00a2d811a49d6d18ca56fd
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:47:56 +0000

    shade opportunity town its expect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
