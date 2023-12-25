bebe95c80b22f6d8e19ca9ec4460c76102c400c0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:55:06 +0000

    transport book instruction soil jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
