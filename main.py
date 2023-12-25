20567b010a693a4a082733c676e83338cb8d8c9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:38:32 +0000

    pronounce sometimes holdout place recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
