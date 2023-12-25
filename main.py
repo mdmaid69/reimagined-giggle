d43ddb5e62d1bb70e67fa92794d61045315dc350
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:25:59 +0000

    about sleep by fun ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
