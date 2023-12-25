292f6b0e221e485912f88176827bb546de4bf374
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:25:50 +0000

    material its bin lean breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
