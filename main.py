83b86306405c16d59bd1a75740522ce5076189f1
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:11:07 +0000

    tap cum ear circumstance boil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
