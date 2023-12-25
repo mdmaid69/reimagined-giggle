8c9db5a2993b974031b2436fa130fabaf9fb4a51
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:37:15 +0000

    plan nose yesterday when muscle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
