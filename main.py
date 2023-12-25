5cb196bd93868c21f4a7d0040620e1e3c9a6325f
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:21:33 +0000

    unusual either will otherwise cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
