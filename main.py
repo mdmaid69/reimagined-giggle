cf56dc292e8a06c8184668ec7ad1365d6f238260
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:12:34 +0000

    confused tough negative certainly wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import collections
+def create_stack():
+        return collections.deque()
