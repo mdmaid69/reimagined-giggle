03340a272d72c9c995054ef86fc05330e8e918b4
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:57:41 +0000

    take never yesterday hour pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
