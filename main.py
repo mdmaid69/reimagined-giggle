601fe3914fa9dcaaa9005208cd9fcd05b8089cbf
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:55:58 +0000

    attach stick gift various trousers

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_stack():
+        return collections.deque()
