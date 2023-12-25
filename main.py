c09686e2f85b5d6d269e04fba6937950836dce87
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:27:23 +0000

    principle fun process formal feed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
