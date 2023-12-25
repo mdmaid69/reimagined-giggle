6343c389f128737ceba90729e4eef46148fc1913
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:01:21 +0000

    react 565end raise introduction discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_tangent(x):
+        return math.tan(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
