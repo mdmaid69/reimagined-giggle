c63e223eae56fb7b1b671f223b4743175632505b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:41:33 +0000

    explanation sing defeat definite float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
