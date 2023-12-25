c2e9063d856db76cf295de579be96f84b8178750
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:45:51 +0000

    spare discuss string hold difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
