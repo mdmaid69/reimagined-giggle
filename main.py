24ab3d7eed534a15a8ba070a97993c1a87f1cdd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:29:51 +0000

    herself alive least lock ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def find_unique_words(sentence):
+        return set(sentence.split())
