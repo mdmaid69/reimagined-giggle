9b1ef76c67492c90d5c397178758afdfc803f4c3
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:34:27 +0000

    be estimate chin this tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
