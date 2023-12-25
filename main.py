ef562478b06b6565edd9f9a86c1fd449068946e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:40:04 +0000

    object protest slow unkind wipe

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
