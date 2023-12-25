8e1e703365cf24e7fa71e08a5030f80c14ed260f
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:20:08 +0000

    night match out callout physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
