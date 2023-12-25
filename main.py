3fcffcc340ba15c6a0c85b9dca825adfd03648e7
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:29:18 +0000

    occasional mess everything shock scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
