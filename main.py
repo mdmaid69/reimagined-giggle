7af2ee68bd15ac8ec4049c946c1482272c81954e
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:23:32 +0000

    shock disgusting day frightened design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
