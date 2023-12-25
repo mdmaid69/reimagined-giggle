ab3ec724f3b9ddcccc7f2eb16139020ea42edeb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:47:55 +0000

    Yy study practical maybe should

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
