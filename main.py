e497ec4e16b96b7c23ee1322e45c079181187689
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:45:16 +0000

    Dd frighten should fortunately suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def sort_list(lst):
+        return sorted(lst)
+for i in range(5):
+        print(i)
