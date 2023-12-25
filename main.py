eb70f9700482ac6ab6e248eb9fdecc38a1c1e589
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:37:22 +0000

    medium perhaps defeat wear dry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
