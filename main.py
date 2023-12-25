026f6ba6aadc7d9a149e158dcf84c5e1fc3a821b
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:01:27 +0000

    compare comeback twist attempt future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
