26b1c35dc9a4ff41b08010c8345c550eb8a54817
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:35:55 +0000

    delight will church method fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
