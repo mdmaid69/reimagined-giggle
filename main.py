9200d8ae399d0d760209fe0f614f09dcd330d354
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:58:16 +0000

    basically Vv intelligent frightened lunch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(10): print(i)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
