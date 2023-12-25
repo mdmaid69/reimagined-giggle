7ea45a40aa4d19857f2092e0ae4f90dc1462634e
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:15:38 +0000

    conclusion thanks possession closed together

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
