dfba4935a242d1f1052ef47f0c49d9000c5e340e
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:57:53 +0000

    advantage national connect up ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
