ace47c5df62c819cb261e9e2dbddfcc654db7fca
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:49:24 +0000

    waste under right influence close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def is_odd(n):
+        return n % 2 != 0
