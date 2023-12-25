15d010e47702ccd450f94f4db7091b1ccbaa2608
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:22:53 +0000

    key home serious separate cruelty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
