9375fd85ebbe95b9e2cda8d0e935bf1808e1126e
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:04 +0000

    quit sweet reason complete home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
