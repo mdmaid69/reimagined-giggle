6ef6f7a5f56a2a5a5d42784c614e47d1c5cafa8d
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:42:00 +0000

    zero it train then theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def count_words(sentence):
+        return len(sentence.split())
