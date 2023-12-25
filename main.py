15155af7189feff24946691c8d554909854f2ad1
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:23:13 +0000

    comedown upstairs suddenly accidental pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
