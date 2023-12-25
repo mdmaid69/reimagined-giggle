ea2e023bf9f7ae58053cf206a12b76fee2291a7b
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:11:58 +0000

    challenge without cover quit too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
