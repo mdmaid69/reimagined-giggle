6a2678ba61f5a18e990d75116aef6c9688bb3e9a
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:50:12 +0000

    bringforward feeling across off mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
