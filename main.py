d8c3b83017f25bdab8e6bc3781f6c2b1be580954
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:49:23 +0000

    gather hardly laugh night test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
