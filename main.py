cba2e30906480165979fc6cea9731f4e5a62ab33
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:25:51 +0000

    begin join bend fold else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
