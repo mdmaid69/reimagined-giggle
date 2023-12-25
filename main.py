9ecdcd1a9077f56d636f4d554b8a70c9a21d4fbc
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:18:40 +0000

    blame trust straight whisper straight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
