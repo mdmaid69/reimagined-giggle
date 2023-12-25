08c119275e97b157ccfd32776b0dfb236e548cd1
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:37:15 +0000

    from dry trust soak temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
