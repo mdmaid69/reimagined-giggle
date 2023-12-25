9f4ddbc3485ea841335f99b63e9cf8b7c0a0d59a
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:26:25 +0000

    convincing tough road base miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
