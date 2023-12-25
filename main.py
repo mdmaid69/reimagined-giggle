f2ad3213310382a48e0ee647b4de0b28bbd2a5ea
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:48:39 +0000

    lock upstairs hello rate sale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
