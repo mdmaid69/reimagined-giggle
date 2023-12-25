456d5673cd24120b16995a9723f09367a07e10d3
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:49:31 +0000

    manage talkinto attach before problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
