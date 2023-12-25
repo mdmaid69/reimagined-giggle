509250b6d2367ffce5ad3506715b16563e64dde7
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:00:33 +0000

    contain well workupto keen goodbye

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
