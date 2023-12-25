cf9a4b39e69d8aa536081a66797ed8495cf29739
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:43:57 +0000

    build once call punishment talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_max(lst):
+        return max(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
