1aeb83f6791c75d30c7c1b0fd5db46ba16f86836
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:53:28 +0000

    return plate stamp situation recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
