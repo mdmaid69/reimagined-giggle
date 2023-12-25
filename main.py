d8cf8d26f38e8c9a405defe32392df3593019d14
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:58:09 +0000

    runinto forgive judge begin certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
