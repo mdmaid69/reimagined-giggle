6fd94e97967e82030607050f8587947846c04524
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:25 +0000

    search argue fire accept promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
