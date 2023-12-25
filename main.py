06502883fb7ddd0ed90adf0b4cd9d38c042abae2
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:41:33 +0000

    story Pp talk cheque talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
