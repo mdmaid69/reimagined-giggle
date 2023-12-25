e4773f8c609ef8de20be3565b862a6a59db85e7d
mdmaid69 <robot@alexsyw.me>
2023-12-25 15:39:27 +0000

    meanwhile throw imagination spare borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
