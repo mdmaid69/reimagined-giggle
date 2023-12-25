1ce110b65438920ec563f9edfa458de55339f588
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:22:26 +0000

    impression nothing before learn start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def sort_numbers(numbers):
-        return sorted(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
