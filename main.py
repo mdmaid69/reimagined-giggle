b82036b33adc091905db8f39c6fa673b736e8db4
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:12:36 +0000

    out television swallow cost any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
