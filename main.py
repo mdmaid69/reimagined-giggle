d9c660052376f81d9a73b63d4db714142829cc44
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:03:40 +0000

    win mean dare solve appear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
