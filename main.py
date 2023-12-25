e9a78ba1184b692459b33350d994834979063d42
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:17:39 +0000

    exercise case before reasonable float

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_float(array):
+        return float(array[0])
