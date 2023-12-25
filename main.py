0230fc95c0ee28d58d002888bb9241c236ebecde
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:02:11 +0000

    familiar guess communicate tight moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def square_number(x):
+        return x**2
