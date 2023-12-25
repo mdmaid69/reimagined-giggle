19c01fd2626d3a663234851511706dc61c5fabea
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:34:24 +0000

    doctor mainly Zz patience squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
