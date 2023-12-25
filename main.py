ad30257c4611d34b02c82b0c3f8a0ff988d799bb
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:07:45 +0000

    poison crazy until out spite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
