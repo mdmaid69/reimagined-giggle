5312915bec26f8332abe1597620602af126470af
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:15:52 +0000

    damage limit calloff description night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
