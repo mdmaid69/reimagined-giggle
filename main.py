6d273779bb07433bfdb7d3137883e5266527957f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:44:21 +0000

    without last invite avoid someone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_hex(n):
+        return hex(n)
