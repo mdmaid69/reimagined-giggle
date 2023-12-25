dcc1e82a0abd0fb9761fb9c63e04929824a0fd39
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:40:10 +0000

    change female careful trap bag

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
