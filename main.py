937280f346070b0b16d86ad4afb565946ddd9183
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:13:53 +0000

    preparation after wander fit few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_current_working_directory():
+        return os.getcwd()
