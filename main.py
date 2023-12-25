4b78b7458bc2322b195298bc8634976bfd491476
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:02:33 +0000

    invitation report relax appeal place

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
