e54dbe4d9f1c6ce23b64ce66a1d39d4f69b2cb47
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:17:31 +0000

    scream star dish month need

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def add_numbers(x, y):
-        return x + y
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
