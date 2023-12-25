b1f9dfae626a081fc47b45fb01bb0285dbaa5ffa
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:06:09 +0000

    pocket abroad stay height bag

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
