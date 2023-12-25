8fd74f94df7350abc124ed45ebe4b3699350508a
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:40:07 +0000

    include influence represent gap memory

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
