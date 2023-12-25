1b588dcbed421ef1c54df1decb0eb3b9fff80d01
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:23:55 +0000

    turnout Jj sit bridge measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
