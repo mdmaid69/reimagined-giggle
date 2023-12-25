62f18ee4e618b2a05a9d73c89af59b3994eb1ec4
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:39:16 +0000

    right clear pin me live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def reverse_string(s):
-        return s[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
