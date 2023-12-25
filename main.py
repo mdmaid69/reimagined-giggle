5e9c1ddc4c50f0836bece1636dec5aba6d65f87e
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:48:36 +0000

    Kk pollution various loose deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_words(sentence):
+        return len(sentence.split())
