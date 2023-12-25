70c998eed61a761a8e0528cd72f10cd7dd19f161
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:22:45 +0000

    people successful weigh turn once

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
