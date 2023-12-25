2613be23f00a6c4d3f2ec7eb43f2837fcfaca6ce
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:20:31 +0000

    forward grade murder workout treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import time
+def get_time_since_epoch():
+        return time.time()
