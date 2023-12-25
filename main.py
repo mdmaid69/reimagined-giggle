59c4ffb49171526ff2ac84126e8cdf21b6f502fb
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:33:48 +0000

    list brown slight declare point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
