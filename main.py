6471701f6bd8f457ebc8edd42a5da4898891f50b
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:00:34 +0000

    surprised also rest later ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
