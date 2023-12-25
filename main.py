3e3f9e3b2c17465890bf0582161a346f69d633a8
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:20:25 +0000

    join callup shadow save approval

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def remove_duplicates(lst):
+        return list(set(lst))
