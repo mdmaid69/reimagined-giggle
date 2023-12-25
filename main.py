de8ccb31da9f2f7184ac0c017169ed1af8d60a32
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:29:47 +0000

    thought runinto force breakdown skill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_even(n):
+        return n % 2 == 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
