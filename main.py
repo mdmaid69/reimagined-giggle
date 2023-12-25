20ef6367fbedb5b148275588ec726ff940d26dba
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:47:22 +0000

    side autumn something handle party

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
