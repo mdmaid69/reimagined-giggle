6d1098ca9ef19f095100bb643f98a79583054282
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:04:02 +0000

    little declaration service bury where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
