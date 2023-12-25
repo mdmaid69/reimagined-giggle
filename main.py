25a10cb794bfb05756fb15b54d15596a49aec533
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:54:42 +0000

    noisy solve work related unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
