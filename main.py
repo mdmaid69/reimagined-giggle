e8bc51b61c999497811b2f25abb591907a6f7818
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:44:49 +0000

    physical production interest wipe light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
