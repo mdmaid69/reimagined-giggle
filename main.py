fab35f0fdf758afc035c35283e6635f31aa09cb8
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:48:12 +0000

    boil clear grand offend lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
