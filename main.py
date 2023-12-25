b3cdfbed63eb7c241f4faf1a3727b67e878d976c
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:20:01 +0000

    language limit cool fierce swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
