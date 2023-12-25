71f2ea02b72fa047ee86af97cd2662af8f7d8da8
mdmaid69 <robot@alexsyw.me>
2023-12-25 22:36:46 +0000

    market design round ours attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_list(lst):
+        return lst[::-1]
