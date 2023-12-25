353ee71b65c065a0079ee1a64639566d0f83cd9e
mdmaid69 <robot@alexsyw.me>
2023-12-25 21:07:29 +0000

    fairly direct swallow beyond hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import getpass
+def get_username():
+        return getpass.getuser()
