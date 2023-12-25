5ac0363abe0526e951547dd5796ceb4f5271688a
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:25:56 +0000

    fair thankyou agreement over noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
