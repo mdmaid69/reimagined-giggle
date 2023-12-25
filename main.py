02c9884a5f8b642d812cb329cb6e3f4aac61e7e6
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:08:25 +0000

    welcome shape age similar then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
