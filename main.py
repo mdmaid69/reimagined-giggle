d9a6d8b2ac37236255c552c7c95ae925d860a5d9
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:13:28 +0000

    mine region pride lot trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
