1095d76ed6010b24f74403ba1cdda77f92c5c227
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:55:10 +0000

    danger ever discussion meeting tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
