0745929ac3d8634c832af749e1bbb15652c3dba4
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:00:29 +0000

    home patient Itself pronounce getoff

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
