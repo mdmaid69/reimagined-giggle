f8376eeb5a61e6054da3820911fdad75c9f1eebd
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:03:45 +0000

    purpose skirt man I themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
