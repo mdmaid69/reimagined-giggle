7c333d8507b40ba2970d765e271b9c711ec3728b
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:36:39 +0000

    forgive OKorokay eye record sign

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-for i in range(5):
-        print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
