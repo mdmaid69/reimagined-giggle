8b796762dd3e1485eaabd218db0020d45f45e5f2
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:31:07 +0000

    behind right thinkthrough push possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
