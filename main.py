af9c00a6674790d8c48dd2c9a8a82d4bbcff3dd8
mdmaid69 <robot@alexsyw.me>
2023-12-25 09:44:41 +0000

    middle until guilty whether safety

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def cube_number(x):
+        return x**3
