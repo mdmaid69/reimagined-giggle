02087603d3b20a5df4a07ba81a0d600086c92a17
mdmaid69 <robot@alexsyw.me>
2023-12-25 14:18:42 +0000

    in leg experience convincing lead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
