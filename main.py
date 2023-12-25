fe2ab1a330cd5c8cdbadea3a753b97079d1c75ad
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:33:21 +0000

    moon spring compare supply television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def clear_array(array):
-        array *= 0
+def get_string_from_array(array):
+        return array.tobytes()
