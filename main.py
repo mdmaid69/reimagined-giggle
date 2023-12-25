86f9c6f0285766492870b1290f739ae257001ff5
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:18:42 +0000

    know cum learn desperate uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
