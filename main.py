db5a4be0c82e36c7301b19afc35dfcb655c211ff
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:54:42 +0000

    lead pot wrong just related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
