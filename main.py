505030105ee720dc351e1dd8a4e9590ce5b9e493
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:46:50 +0000

    catch expect then consult turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
