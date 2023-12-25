edec46250ef030941b06d56666094032dcfe3414
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:01:26 +0000

    eat check exchange patience work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
