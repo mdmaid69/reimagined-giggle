35d693c8cabdb748f8a1182eb70c8c9c0f907e81
mdmaid69 <robot@alexsyw.me>
2023-12-25 18:29:51 +0000

    fortunately difficulty bright case task

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
