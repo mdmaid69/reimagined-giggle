24c0946b7b9555c807af42ba894349703b5ff072
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:03:55 +0000

    comfortable name opinion any tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
