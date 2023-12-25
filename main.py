ecb92e42e3d7af2fc29b5ab5b081ecb8e66b066a
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:18:15 +0000

    weekend Ww spend down travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
