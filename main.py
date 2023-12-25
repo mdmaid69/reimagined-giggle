fb6992fb91e0d6f450723d4243cead1c70419f72
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:34:14 +0000

    broad practise visit tell fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
