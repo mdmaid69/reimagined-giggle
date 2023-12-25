bf80ccaea43f99539c00e8af2a67bb538e28a94b
mdmaid69 <robot@alexsyw.me>
2023-12-25 06:09:48 +0000

    style reduce language community unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
