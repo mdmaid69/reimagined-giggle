b59fe8da8a59ed874abddc02c25d996206523d14
mdmaid69 <robot@alexsyw.me>
2023-12-25 19:11:38 +0000

    development play yours dead gate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
