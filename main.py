21490647f111bbc89c1c90e7c433b58e392296e4
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:23:24 +0000

    otherwise image please insist man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
