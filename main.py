bd9f272eddfc9f733ca5203b6e17b91c353e6118
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:27:49 +0000

    question couple trap light sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
