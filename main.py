017f30bc036877c57a629d8d45c40355984d7e90
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:51:12 +0000

    kill swimming talk national smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
