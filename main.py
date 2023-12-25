b721e0d87a5277381db96cddd7d096d63a74f94f
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:00:20 +0000

    phone hole workout thing violence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
