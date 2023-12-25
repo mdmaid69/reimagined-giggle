965f99316c30f35274608444f59b55d91ab5d844
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:57:41 +0000

    skill attach declare extra sky

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_speed(distance, time):
-        return distance / time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
