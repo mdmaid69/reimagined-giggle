2c02bc5a4ec7100ec106f2265a34fe7d31e57224
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:52:54 +0000

    wide rest turnon tonight public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
