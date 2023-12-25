ed5522086f95fa270948984fdc4adef41b1768ee
mdmaid69 <robot@alexsyw.me>
2023-12-25 03:23:51 +0000

    circle message apply bank platform

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
