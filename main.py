a0adb8046b0404d76ee2951a15a95931fe4ae44f
mdmaid69 <robot@alexsyw.me>
2023-12-25 16:21:12 +0000

    yesterday roof chat giveout hardly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
