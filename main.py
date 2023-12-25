d4365499ba5439814a5e6d4372cb35025f237464
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:20:42 +0000

    tend realize hide control problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
