d5db89a6b26fd99971a3b41a0e710392b9b8d7e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:13:49 +0000

    wish gift school long callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_max(lst):
-        return max(lst)
   import matplotlib.pyplot as plt
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
