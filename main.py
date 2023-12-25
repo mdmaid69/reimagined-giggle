ba9b55327ebf91d36b64a495b0e07ec67df3e5a4
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:24:13 +0000

    makeinto wise force 570enjoyable no

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
+import numpy as np
+print(np.array([1, 2, 3]))
