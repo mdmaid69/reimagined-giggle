fbabc1d14553461bbbdfbadddf2b50bdf754a2b2
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:16:06 +0000

    future supply stranger rest either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
