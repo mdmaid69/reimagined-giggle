0563df862de5a11faa80bc54612b405bb26a9182
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:52:32 +0000

    busy trap plan unable Kk

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
