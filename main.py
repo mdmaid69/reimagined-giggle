99a2d53da9ed292fb99b5ee4bb7422d93a2ce0e0
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:32:24 +0000

    fast period rid famous rough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
