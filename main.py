ef70802b604f307f4b1248ca819bcf6d22798ea8
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:06:10 +0000

    although speed feature his win

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
