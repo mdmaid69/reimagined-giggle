89d073a03eca60cd7ebc44a014c42f1fb4231bb1
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:39:09 +0000

    news Ii clear frequent by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
