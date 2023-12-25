1b677e31fc7a9ef21f73b2cf931321054434d3cf
mdmaid69 <robot@alexsyw.me>
2023-12-25 01:36:29 +0000

    care cold you interest how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
