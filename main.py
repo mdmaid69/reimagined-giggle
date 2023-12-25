3fb40603dbe89ddb527eba903507fe4cc1df6b9d
mdmaid69 <robot@alexsyw.me>
2023-12-25 20:12:21 +0000

    bunch lead many explanation quickly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
+import random
+print(random.randint(0, 100))
