02f1f8c8a7e9083d89f49c28ec507713cb2704d6
mdmaid69 <robot@alexsyw.me>
2023-12-25 10:36:56 +0000

    knock talkoutof excuse cause dish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_work(force, distance):
+        return force * distance
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
