1ac52a96c2eadaea7570f90d135bf345489db59e
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:53:25 +0000

    expression lookback(on) lot do range

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+print(time.time())
