490ba1a2cfdb6735588983fc40332a0c84f2f889
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:30:07 +0000

    happy sense road regional bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
