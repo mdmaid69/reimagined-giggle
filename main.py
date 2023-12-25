3b35943c4505e2d1cfb30a065434a8573dfda4db
mdmaid69 <robot@alexsyw.me>
2023-12-25 13:17:34 +0000

    breakout part law kind immediate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
