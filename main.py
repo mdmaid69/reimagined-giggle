5d903e841f0476e269bfd493142596e2782be228
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:10:41 +0000

    ball Bb beat breakin responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
