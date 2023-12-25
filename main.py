339d10edb0f2b31ba4d2faa5a521afe554a4c07b
mdmaid69 <robot@alexsyw.me>
2023-12-25 11:48:15 +0000

    memory later rely word hand

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+print(sum(range(10)))
+def calculate_power(work, time):
+        return work / time
