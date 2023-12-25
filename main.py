9e35dfbe66da48959cca1c84b96f56be70b22524
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:27:19 +0000

    beginning there father want smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
