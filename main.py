24e351e1e6bfdd88e442efeb72cb4690afa12018
mdmaid69 <robot@alexsyw.me>
2023-12-25 08:18:16 +0000

    chat town upstairs suit agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-print(time.time())
+for i in range(5):
+        print(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
