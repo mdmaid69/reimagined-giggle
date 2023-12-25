c3b53f5f77a285d471ddfdbf2450d78d76c8e7d2
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:04:24 +0000

    worried brush strike comment afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
