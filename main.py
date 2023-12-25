6bcff08fa9b58da1b38c4a739df59b505af0df43
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:57:55 +0000

    fact store introduction lot weigh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
