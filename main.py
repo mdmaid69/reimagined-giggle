483b4794093cfb29bfa21e9df3199dbd6a83d611
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:28:16 +0000

    burst fat below fear communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
 import array
-def clear_array(array):
-        array *= 0
+def reverse_array(array):
+        array.reverse()
