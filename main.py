b749f38d267c264b9b6fa1ee289854d6406c876e
mdmaid69 <robot@alexsyw.me>
2023-12-25 02:38:51 +0000

    concerned sea disturb work Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
