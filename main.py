a60ff39cf6608c052cbcece527c1a57d23216fa5
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:49:24 +0000

    keepout happen similar crisis around

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
