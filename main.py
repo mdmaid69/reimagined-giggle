e4fc0943b6b8166b68e9c6d1e27a63dc040fd9f0
mdmaid69 <robot@alexsyw.me>
2023-12-25 07:52:28 +0000

    door door brief normal many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+print("Common elements:", set(list1) & set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
