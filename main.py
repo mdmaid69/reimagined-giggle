5ca333924631ec78e1657f05c56692ae888e9ba5
mdmaid69 <robot@alexsyw.me>
2023-12-25 12:54:40 +0000

    declaration harm top impression shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
