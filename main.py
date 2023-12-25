07dff29e64216dafadc54c5fffced0f17f84767c
mdmaid69 <robot@alexsyw.me>
2023-12-25 04:17:37 +0000

    correct plan snow dry better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
