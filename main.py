6f30eb8def855063ccb669865a9fad3ee3e13fd7
mdmaid69 <robot@alexsyw.me>
2023-12-25 00:18:14 +0000

    guess steep date tidy clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
