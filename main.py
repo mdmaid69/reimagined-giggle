0202bcb0c6cb7588a4a80dfe8ea4c8f02c38480c
mdmaid69 <robot@alexsyw.me>
2023-12-25 05:46:27 +0000

    strength soak keepfrom disturb laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def change_working_directory(path):
+        os.chdir(path)
