fd2be7e0819e3ee2ff798efff91c4cfabc8c4df4
mdmaid69 <robot@alexsyw.me>
2023-12-25 17:09:34 +0000

    fortunately pleasure his fetch greeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
