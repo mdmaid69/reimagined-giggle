f5308f7c1a9ac2d31c696b3c6b6b91663a78bc08
mdmaid69 <robot@alexsyw.me>
2023-12-25 23:09:25 +0000

    holiday wave course snow ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
