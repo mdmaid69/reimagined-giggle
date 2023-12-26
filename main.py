811e649117bd1e2eb21a2e494a06a44dfc5a8ca3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:17 +0000

    family breath clear exit entrance

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
