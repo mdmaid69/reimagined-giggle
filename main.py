4954c67339a53b5da8a67eba1c54304d4720f94b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:14 +0000

    exam acceptable experiment receive regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
