16fe80d3ff1550309cc94ebff18d0e8826bff93d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:07 +0000

    even before angry character argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
