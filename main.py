3ec518a27b64a64fc17c9769bb60ba5694b593bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:23 +0000

    steal heavy sale benefit rich

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
