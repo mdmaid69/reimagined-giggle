63bece794656967bcf37b93b70b6c3c9f2b27c19
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:00 +0000

    may light design embarrassing getup

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
