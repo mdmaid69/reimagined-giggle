c2d8505b5506ba9de493fb2157d7fcdd46b8eb02
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:39 +0000

    plant year wrong bridge gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
