e91e6d9fd830effdadb5fbb45b51e3fc3fac22f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:03 +0000

    boring talkinto naturally forgive honest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
