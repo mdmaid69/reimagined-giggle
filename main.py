e8aeb92eaac01e7c66e50a97edbc810a6c423e59
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:12 +0000

    straight hurt flavour comfortable skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
