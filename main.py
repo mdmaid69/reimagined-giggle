51569f4f5c95c64e8796f404272003d1f7b25166
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:10:34 +0000

    tip hope lookafter probably content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
