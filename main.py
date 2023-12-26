3cace84bc599b1a3f5c7fa39d729d45422b88df9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:41 +0000

    direct regularly proof home during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
