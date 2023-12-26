ff05e6f164bb830610d4e3803f337663eb221306
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:42 +0000

    until repair emphasize big shine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
