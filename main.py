36e2090616d6877490f72fdef6c7e71f7170a290
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:10 +0000

    probably kind appeal street deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
