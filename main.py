521bfd4160a4c89d8ab82d0b24f2a04f08e5db1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:59 +0000

    noise trap fall lend trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
