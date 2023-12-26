ed37a1233480085484bec67cfd3d6dc8253f03eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:15 +0000

    convincing thing sometimes giveback thing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
