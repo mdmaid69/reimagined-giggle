b10f74fae357b8324a9f271f114529eedd9f2173
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:00 +0000

    whistle flavour survive follow calloff

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-print(sum(range(10)))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def remove_duplicates(lst):
+        return list(set(lst))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
