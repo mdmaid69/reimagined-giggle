c5c5396562d9d47837143638b048ae1adc43de1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:02 +0000

    secret flood breakout fork occasion

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
