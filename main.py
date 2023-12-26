49191e2857cf92fb5457641a7386c2e1a19f994e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:12 +0000

    cruel squash piece special spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
