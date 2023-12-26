79eb2aa5d7f31315afa2306ef1335824f0377703
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:34 +0000

    lucky prove comfortable bye on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
