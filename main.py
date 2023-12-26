02fe072a9eb074bc9c2a18aaf120d8eaae74c295
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:14 +0000

    regard tone bunch to makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
