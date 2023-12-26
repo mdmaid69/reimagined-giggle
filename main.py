231bfa638b210fc01a8dde72ad023eb92d910959
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:20 +0000

    respect congratulate further impression familiar

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def square_number(x):
+        return x**2
