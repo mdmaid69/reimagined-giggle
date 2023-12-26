040864d8d57c6856c1fe898aae04cd2a81e598d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:15 +0000

    actually city Ss middle information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
