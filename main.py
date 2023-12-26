459e43a0169a73e4cb799c35fa31216238628fb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:54 +0000

    freedom cough off moral bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
