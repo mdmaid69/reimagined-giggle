a233da46968dc591c780101ac44bcb7dbba986f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:01 +0000

    giveaway goout none previous put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 def calculate_area_triangle(b, h):
         return 0.5 * b * h
