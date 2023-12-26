2edd84361dca6319fef67d733e31c7b00bf52aae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:20:58 +0000

    depend circle just Nn treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
