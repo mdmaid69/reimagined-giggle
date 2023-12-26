09c5db5affd71f7c44804bb722dbb7a4471e21fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:17 +0000

    pronounce needle perfect congratulate rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
