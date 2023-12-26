3aa43eb3857a159d406d36c21c4b86052225de35
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:44 +0000

    hold nonsense speech on correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
