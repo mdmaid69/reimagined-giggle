62873f6cf2c828a9c35b8df95ae0e203791b785a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:42 +0000

    church by touch 564encourage OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
