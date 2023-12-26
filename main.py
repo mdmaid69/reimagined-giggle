e80ad50f7bd30b70938e9a00cc2014fd5d95b555
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:06 +0000

    eat accident goout figure relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
