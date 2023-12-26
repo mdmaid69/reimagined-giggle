dc443ce9c3172bb15289b671d71e4ad2802ebb7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:00 +0000

    slide must evening best tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
