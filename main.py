14ec19758075aa399b6562bfded0707e960d9a89
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:27 +0000

    across solve switch suppose collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
