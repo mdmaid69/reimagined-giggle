cf3745ed5aa9edbc34a458db4b821227a53d7a4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:38:38 +0000

    his mass pupil breakinto giveback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
