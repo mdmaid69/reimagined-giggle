949ee9423df55c6de030b8bfec094b8f2668848e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:30 +0000

    injury major climb smile quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
