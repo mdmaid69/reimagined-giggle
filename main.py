72361bc450abcf2f46c96bead1cdf6488b11caa9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:45 +0000

    why long job measure quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import collections
+def create_stack():
+        return collections.deque()
