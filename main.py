1c7849b60d49eb0bc43606c1a2be9169ff22509f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:25 +0000

    we this active breakinto sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
