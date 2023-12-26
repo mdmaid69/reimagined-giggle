6e6baa18b13e791960d1e5fdd26afbb0c0f8ae92
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:00:49 +0000

    effort have bury design shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def greet(name):
+        print(f"Hello, {name}!")
