35567eb2a1db252fe1fae5c0653524005ebdebee
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:40 +0000

    tall personal interesting hate brown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
