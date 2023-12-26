713edf7327b8d778796a5849ff7277f6ae9c807e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:44 +0000

    quickly light storm start service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
