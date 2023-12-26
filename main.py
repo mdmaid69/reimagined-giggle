71f32cd48166f819f68c55d91494c067ec05aa80
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:02 +0000

    string appearance software home intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def square_number(x):
-        return x**2
+def get_array_as_set(array):
+        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
