16f9f402ac3a3106c370b5192d08ba33b2afab10
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:39 +0000

    argument substance any trade building

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
