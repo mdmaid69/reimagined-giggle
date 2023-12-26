935b6b37ec2f72135e43c471f2900604cfa2dd86
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:12 +0000

    swing report later wind healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def delete_file(file_name):
+        os.remove(file_name)
