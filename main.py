0caa954aad1a72199d3ac2debe52247256b5a33d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:32 +0000

    but takeback explanation offend especially

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
