150f6c12bf1f57056a42f6f57878c6ef9096a444
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:04:40 +0000

    keepfrom pair meaning spread swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
