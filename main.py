76ae1bf187f964e61ee5d8158eea6531c6227b43
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:29 +0000

    call arrange taste funny little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
