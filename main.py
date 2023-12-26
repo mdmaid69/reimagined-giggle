dbf4d228e804806c717eb0dda796dfc9dbaccb77
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:36 +0000

    foot search suggestion tip card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
