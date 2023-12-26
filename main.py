4e31d313fe2dcfa0e656d20e1aa8700d9a4ff153
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:43:56 +0000

    plain rub breakfast be content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
