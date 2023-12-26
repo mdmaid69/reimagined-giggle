da11faa74930328c3f8da6e6737b15a8cd922b97
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:01 +0000

    above along day tap tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
