d88a402c340c82664d275cf4e7de13e7bdf4ec94
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:58 +0000

    cry attractive arrangement unless aware

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
