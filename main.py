ffa29ac6bb98a4ab3ac58dd903a99ae8a4bcdc59
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:44 +0000

    kind accidentally introduce old increase

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
