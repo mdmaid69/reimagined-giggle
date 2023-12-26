4733cb95935aa1fd238d2e5ed5d3a9fb6fa00e03
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:12 +0000

    admire waste situation habit exploration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
