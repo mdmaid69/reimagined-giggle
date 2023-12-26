ad63812a725b7c61d02509d42612edfd7ef47014
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:23 +0000

    comeback cause factory true accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_elements(lst):
-        return len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
