4cd83ddfb880b90a8e60dda5579c3b3e91e3a0a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:04:49 +0000

    getout pattern still everywhere affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def clear_array(array):
+        array *= 0
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
