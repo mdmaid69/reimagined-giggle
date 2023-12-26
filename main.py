78eece545709f1c0527daa4190411635d8adfff1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:19 +0000

    false throw lay score television

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_rectangle(l, w):
+        return l * w
