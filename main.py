57c805ae260dcbbe06a8542936de917c2ede3471
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:48 +0000

    content shallow project young leg

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
