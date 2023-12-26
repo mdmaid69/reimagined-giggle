ad5dc06ce3261e57e08b3f7e8e2318bba9e271dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:48 +0000

    meet weak indeed sing strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
