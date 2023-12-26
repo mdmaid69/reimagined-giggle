6795ccbcd3ef3640a5dd79f5cc52781806066808
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:54:45 +0000

    pencil makeinto view every spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
