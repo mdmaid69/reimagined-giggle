2605a2ee50a45d35a80dad6244340e56cc4742b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:15:31 +0000

    completely art about interview Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
