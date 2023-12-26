ab771bc9d1dca3a8435a89b03474e09356e2c069
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:46:12 +0000

    steep gradually wide run backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
