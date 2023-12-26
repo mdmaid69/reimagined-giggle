1d0ee835fe7dd37798994bb6a1803f444cb1841f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:42:31 +0000

    entrance takeback keen bet breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
