23f1c54d25ca422a4e3d69174dd602345a95dae5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:31 +0000

    party throughout consist hurry slowly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
