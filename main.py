e441dc2b2aca238ec8a002d825a686a9204570fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:17 +0000

    test Bb thinkof foot talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_force(mass, acceleration):
+        return mass * acceleration
