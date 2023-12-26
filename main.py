ca4bdda82d874fb6f5ae7daed1f5defadc2e6b0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:43:02 +0000

    somewhere side always getup name

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def remove_from_array(array, item):
+        array.remove(item)
