be35a77fa8696058a1ebe3913cef960a52e85010
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:44 +0000

    way murder feature official skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def is_even(n):
+        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
