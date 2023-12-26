11d9db00d6fe7060f90c5e38a9908add6b14333d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:47 +0000

    winter 572enter thought actual action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
