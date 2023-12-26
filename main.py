69bacb0c6af6483846090d161a270039bcd81c97
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:06 +0000

    amount unpleasant cover maintain crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+print(time.time())
