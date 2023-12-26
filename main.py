26f65b8f8e60f971534f61583777e0142e7c39c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:37 +0000

    square care search silent show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
