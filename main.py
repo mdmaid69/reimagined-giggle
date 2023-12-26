798ea75be3af6bfbede4fff9ee97b684af11654f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:31 +0000

    difference keepfrom lay bus discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
