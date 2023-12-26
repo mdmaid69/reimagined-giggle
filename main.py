d0a40f15918336e081a901073d5f0d9b05ee1918
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:09 +0000

    again estimate loose order worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
