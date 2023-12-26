7597ccae33da80bd0981b691e84bbe5dbdb762de
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:26 +0000

    song borrow on check space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
