5ad4e6dc807a62bf19283e97cc229284cbacf140
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:32 +0000

    blow fork loud issue obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
+import sys
+def print_python_version():
+        print(sys.version)
