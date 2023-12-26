b7b656782a9633d4808ef578061fa80610a4dc2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:04 +0000

    big protect cruelty minor variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
