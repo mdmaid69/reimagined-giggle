296bf51c3c5b3080a0c3a903a4123577a3a31dbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:07 +0000

    language law hit passenger pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
