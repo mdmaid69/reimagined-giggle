5ba911ea3751fe513facfbad2ed2402b3e2b1894
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:58 +0000

    copy turn beat spring job

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
