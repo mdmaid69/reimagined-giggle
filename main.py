7cae6e6cd360598d304485e564b4f49a77e8ee65
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:43:57 +0000

    surprised then turnup comeover keen

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def multiply_numbers(x, y):
+        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
