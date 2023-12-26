d3561e30401e20fb1e9ae83557868eb558b0e04d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:51 +0000

    custom Hh lookaround takeback convincing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
