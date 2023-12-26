df9d759921b515d097a37bd32d1178fc35715649
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:30 +0000

    represent attitude nor your meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_tuple(array):
+        return tuple(array)
