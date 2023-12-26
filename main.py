f80f3ee3e72d30e29b0d6e38c820a956256f69c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:57:22 +0000

    each apologizeapologise report experience choice

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
