a99a50524ba2ee21e11c5375bad3bc151ae76bf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:57:31 +0000

    eat freeze who challenge belong

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-print(os.getcwd())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+def get_current_time():
+        return time.time()
