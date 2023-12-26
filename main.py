4bf4647a484c4e658261d06ea927b54490db7d99
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:33 +0000

    somewhere notice not action stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def print_python_version():
+        print(sys.version)
