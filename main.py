508b1c186e960fab1cc5c69fcac7ccf5fa1886d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:23:49 +0000

    square see occur office chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
