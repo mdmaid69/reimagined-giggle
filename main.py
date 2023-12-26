518db82f389b8e6dd244eb6b839ab0289e1c74b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:27 +0000

    clear mess who satisfied rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
