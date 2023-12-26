4413cabcdcdebadae34bb362d802722920b250a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:07 +0000

    run cook measure 572enter frighten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
