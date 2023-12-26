d2e822bc5fc0b4c2411ef55713858341e8075100
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:52 +0000

    wrong city moon basically mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
