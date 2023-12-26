4ad5929487e7eaaf068dbd231f492900b482751b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:41 +0000

    separate later light runout town

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
