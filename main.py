a73ad986e89a7ebef9a40325dc52e211a89f9e1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:49:09 +0000

    comfortable boat little street innocent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
