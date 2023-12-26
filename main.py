6ea473ed8f18f2dbbeb45e02fbd7e920ee405a1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:14 +0000

    decide go play swallow on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import time
-print(time.time())
+def get_current_time():
+        return time.ctime()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
