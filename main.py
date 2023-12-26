f18ba9ab538695bd1644f22151534d58a0ec448e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:46 +0000

    number available do towards blind

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
