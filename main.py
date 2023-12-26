282a90d99214e1b1d3f763362ef4c92e35cd51cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:24 +0000

    can correct carry you snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def sort_list(lst):
+        return sorted(lst)
