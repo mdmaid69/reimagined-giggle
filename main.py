275a0b1f89dd21819c2619c01f57f5c65ee9e6b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:05 +0000

    debt live escape exhibition character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Squared:", [n**2 for n in numbers])
