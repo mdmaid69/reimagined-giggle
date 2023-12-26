2d9f3d1e95771a60734148a685b99a28c9d69313
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:59 +0000

    uncomfortable seriously active broad label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
