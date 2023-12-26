24c6eada89c0c1aa10701870531583b1cf624e7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:00 +0000

    doctor software impossible wind available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
