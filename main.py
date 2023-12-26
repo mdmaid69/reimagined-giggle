c9222cf293100a0c59640998ddd2088c92cfdf6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:03 +0000

    deep agree gather I wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_current_working_directory():
+        return os.getcwd()
