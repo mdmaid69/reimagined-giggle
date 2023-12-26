63f3b0ac36c04f3361ce7ad0200f0b09dd548b27
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:57 +0000

    put report light sink anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
