22764c50e47e8b0c37d6cd2f347c4c8bc363dd94
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:02 +0000

    later instead communicate fight ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
