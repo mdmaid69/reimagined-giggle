0b41283a59333d5e4a12d5aa31426373013a26fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:46 +0000

    round past post pride possibly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def reverse_string(s):
+        return s[::-1]
+import platform
+def get_python_version():
+        return platform.python_version()
