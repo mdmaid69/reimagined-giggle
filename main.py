1f0872e1dbeba40ae27e9b662a0ecc990c156476
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:47 +0000

    skin feel funny holiday lean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
