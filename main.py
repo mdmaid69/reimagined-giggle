ad4c6833a8a8e283cdfeec027e1700ba7f50995e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:30 +0000

    for gap in value clearly

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def is_even(n):
+        return n % 2 == 0
