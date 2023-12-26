2e714fb6b8a154355d02fa607f672171b1d583a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:10 +0000

    name music apparently company definite

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print(sum(range(10)))
-import math
-print(math.pi)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
