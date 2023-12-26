05c6d31c3f21d1c7a51082549fc52828abf0f600
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:36 +0000

    guess steer 565end comefrom want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def exit_program():
+        sys.exit()
