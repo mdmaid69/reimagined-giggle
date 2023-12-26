ab999a7676eb34da764e92ec368c3fa2b055fde1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:57 +0000

    argument hardly interesting example bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
