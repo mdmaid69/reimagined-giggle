0f2ca3c2e188616fcd0e65b6a80da121bbd10f8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:32 +0000

    scared thought clean bringback myself

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import platform
-def get_os_info():
-        return platform.uname()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def remove_duplicates(lst):
+        return list(set(lst))
