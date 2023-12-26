a6b4dcbf97a20f92f598ffa1ec41e1ad5e556193
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:43 +0000

    include fine statement replace wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_power(work, time):
+        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
