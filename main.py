c100814103dcb167f999f6763f0b4190342e1bfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:56 +0000

    unite aim like fall force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def reverse_list(lst):
+        return lst[::-1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
