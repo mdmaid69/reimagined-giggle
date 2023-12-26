c5ecf73b93ffadc3015badb8156148dc6b6ab5af
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:49 +0000

    him rely 569enjoy last trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
