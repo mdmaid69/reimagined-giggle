a560aafbfe602917bed54e64d3ae603044b3142e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:20 +0000

    forgive hold insist clue culture

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
