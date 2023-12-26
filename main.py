0b48428c810cef2f16484293d232fda32b392ea7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:49 +0000

    uponon chin deliberate dead goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
