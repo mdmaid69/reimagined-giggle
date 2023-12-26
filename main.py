179390f6060660ab1b9348721ff311007c6aae9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:42 +0000

    future forget thanks lean instead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
