e55431de0edfb0433f85fa5ea2bb0eb4f193036f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:01 +0000

    wire scrap power loud language

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
