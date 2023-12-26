281e9c74a3db570660d4dfbb3b01d3de894ef43e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:53 +0000

    find bringback which giveaway juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
