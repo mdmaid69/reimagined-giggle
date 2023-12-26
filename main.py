2c2cd8faac6691929a6a9a34df3319c806d199f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:36:45 +0000

    cry example through mean certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
