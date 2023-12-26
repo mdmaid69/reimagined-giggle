e0afb62050bccc386f23736f2efac1e4ac49b9cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:35 +0000

    play instrument imagine scare person

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
