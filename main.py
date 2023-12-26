7d81f5bbd1bad2a69f5b7133aa49745050fab304
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:23 +0000

    dick real sweet hold smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_str(array):
+        return str(array)
+import array
+def clear_array(array):
+        array *= 0
