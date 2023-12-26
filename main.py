bcdadfd8034091185995dce71134389f5ad6ecfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:22 +0000

    advise fit satisfactory maximum reach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_palindrome(s):
+        return s == s[::-1]
