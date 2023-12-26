fa8d65d834f2e28bcd0ad3ab5a40d1027d96623e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:54 +0000

    friend keepout serious vision experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
