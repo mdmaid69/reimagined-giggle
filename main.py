b183da43d4d5509cc4cdaa84fee18ea83a0f3216
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:03 +0000

    seem flood complain trial pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
