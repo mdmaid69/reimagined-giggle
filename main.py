e7387b6756269c296db97c5063dbfed180c3e26d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:33 +0000

    watch mind mass steam herself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
