151dcb74d5f99fa45e73eb06cc41049089d7df18
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:55 +0000

    plenty place principle success concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
