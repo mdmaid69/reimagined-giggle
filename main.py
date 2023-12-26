0380b1874a58d75603d2531d20728dec276280a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:33 +0000

    pause point whistle market strike

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
