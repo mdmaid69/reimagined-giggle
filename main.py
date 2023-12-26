ed7fde24ff5f15379fb31c3883bc7a99e0bd4c4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:26 +0000

    positive call fat always dream

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-for i in range(10): print(i)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
