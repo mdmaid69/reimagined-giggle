f99eff4b579e8a4e7a4afb8b6bf4d9e23cedaafc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:21:34 +0000

    correct fall punish save kill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def is_odd(n):
+        return n % 2 != 0
