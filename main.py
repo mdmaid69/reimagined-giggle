8357f1178db7243067f51f112f598276859c2fb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:34:29 +0000

    slip stop normal freedom border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
