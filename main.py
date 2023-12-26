2cfde4e3d58872a5de6bf6101f9c2815461e54a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:17:29 +0000

    unlucky skirt shallow slip place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
