43dded7fa8d5cc7b304a92dc20c488b9f94b22a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:21 +0000

    gift town thirsty think total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_even(n):
+        return n % 2 == 0
