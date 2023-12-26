66bd267df81a0ee00e4639c519891c82b5b18b5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:47 +0000

    human clue maybe road change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
