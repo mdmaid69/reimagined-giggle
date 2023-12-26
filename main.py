235d781b2170f173eeea1e906b11820d58cd4973
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:00 +0000

    autumn rule risk guess greeting

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
