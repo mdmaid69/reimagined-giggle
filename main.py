0c108d30a2bd54766a1545993d8248ee34435921
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:22 +0000

    spread new exit thick decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
