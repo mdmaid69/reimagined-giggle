95a14761a4ba01e6654e651e73e4045d32927e83
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:21 +0000

    patient unlikely situation be mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
