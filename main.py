6803f31be5bafa80cc08447cae635f025d55332a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:40:53 +0000

    believe fire general clear afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
