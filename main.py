dd9a8f3ee8176ed159a8f3e14db3b6292e8352d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:04 +0000

    live turnout strength foreign dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def cube_number(x):
-        return x**3
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
