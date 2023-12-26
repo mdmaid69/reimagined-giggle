7731706fdf0296c754475e0cda420214f2f2544f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:57 +0000

    declare wipe equal size no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
