e68ce561890bea492077eb91bdc7596dec0b830f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:04 +0000

    safety order write cut load

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import datetime
+def get_today_date():
+        return datetime.date.today()
