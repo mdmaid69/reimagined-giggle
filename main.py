33dbaecef5576d8104e37bfb06c068b700067cdb
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:53 +0000

    split precise sentence press divide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
