bf0c69f3c6e080f7c1f8dae1ce4fbc630e3876e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:09 +0000

    their card deal more comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def find_min(numbers):
+        return min(numbers)
