78a05ac03f9fbdb162969c93ff0d4d2e45f645c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:10 +0000

    last certain dance Nn present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
