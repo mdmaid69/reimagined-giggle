a7440dfd52d6cc81d1ec400cc9ebf85fffd09692
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:16 +0000

    suppose loud nature of cinema

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
