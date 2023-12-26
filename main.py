b1f3cdc7a189cb6ed5c560a3353f9d7b0d2e9181
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:14 +0000

    attempt bath fierce ilovedicks realize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def count_characters(sentence):
+        return len(sentence)
+def count_elements(lst):
+        return len(lst)
