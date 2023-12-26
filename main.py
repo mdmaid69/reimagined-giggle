00a798994d9cecc5bfe67f0cdfd155fa547db756
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:39:35 +0000

    vision stiff circumstance offend surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
