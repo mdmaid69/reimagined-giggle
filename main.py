aa02775275a571a99c48cf675d7d88391e1f863b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:44 +0000

    far certain maintain birth house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
