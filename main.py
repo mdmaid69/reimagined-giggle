c7ae4f1fc851796505143dc967e2b48fcaf269cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:41 +0000

    official towards claim pause smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
