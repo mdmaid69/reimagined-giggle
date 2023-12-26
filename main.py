17c1fab714638bc990d36ff51967e0a320291a12
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:30 +0000

    straight lay declare bus miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
