a26aeef438b07d756632ed390b0ae64415209d72
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:42 +0000

    practical show curious government next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
