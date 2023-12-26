d9411607d8430ca410f69ebcdb79cc1c11d8b6b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:50:49 +0000

    no intelligent tonight fire ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
