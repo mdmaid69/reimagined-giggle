adce56870271fa59dfb89116dff5cd23688f45b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:08 +0000

    government how build forgive pleased

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import logging
-def log_message(message):
-        logging.info(message)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+def remove_directory(path):
+        os.rmdir(path)
