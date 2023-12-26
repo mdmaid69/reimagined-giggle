555c4035413c0e6efa3dd785054e954998cbd1ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:57 +0000

    stick usually dream software bread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import random
+print(random.randint(0, 100))
+def calculate_power(work, time):
+        return work / time
