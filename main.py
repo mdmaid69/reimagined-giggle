b869d4f234960d1e30880e97790fb85a74321838
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:28 +0000

    crash correction to declare skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
