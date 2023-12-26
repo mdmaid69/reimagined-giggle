0898a97835da25ceb78098fe0e054dbca5d31976
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:13 +0000

    hurt chance pronunciation gooff wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
