9d86022c00b34c7946c0e867eb13ebeb1b8bd4c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:21 +0000

    closed Ii suggestion rich object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
