f9409a87e044e5f8ecd5b119974a8449262145b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:09 +0000

    569enjoy truth prepare go who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import tensorflow as tf
+print(tf.__version__)
