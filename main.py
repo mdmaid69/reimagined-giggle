a0fd62e3e2756b178ecbe98a39bc41316db75391
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:45 +0000

    squeeze vanish onto building today

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
