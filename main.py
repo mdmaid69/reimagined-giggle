c9475d62f8e99e6b2a40a28495ecc57fa22372c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:31 +0000

    shake bringback mail offer speed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
