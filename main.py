4b8d32fb5908a3fbe86dfef7bb8fde9528257ee7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:12 +0000

    turn goup available exaggerate centimetre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
