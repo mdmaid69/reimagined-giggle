431210239ffc58d7687929ab4df5ceb69eb93441
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:12 +0000

    joke education convinced experience add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import re
+print(re.match("h.*o", "hello world"))
