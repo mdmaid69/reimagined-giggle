370db00549f41b8ddcbeb08d6b69349ca80e99c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:40 +0000

    age getoff organize delight ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
