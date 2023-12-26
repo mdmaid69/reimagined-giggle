f778dad492e35187a89fe1a504fca6f8214b5728
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:48:12 +0000

    border this cover satisfied translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
