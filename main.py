028b8599830ce11b3031d2d88d9bc670cd449a8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:18:53 +0000

    borrow callout exact false willing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
