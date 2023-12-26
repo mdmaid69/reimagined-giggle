a39a7bae1973496bc3a038e46623b035292cae31
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:59 +0000

    calloff responsibility everywhere pin organize

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_str(array):
+        return str(array)
