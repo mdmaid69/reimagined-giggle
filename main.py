e18d98d8a4049c46eeb707bb7c8222e664c15112
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:46 +0000

    precise holiday design country work

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
