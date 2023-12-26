42ce2a49ed328ec2b71e5da1ca1c47c381da1cde
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:38 +0000

    course basis pair difference ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_set(array):
+        return set(array)
