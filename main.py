6859bb9615216c8e0e54a4cca0b64c998a0b26ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:27 +0000

    of finish simple government contents

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
