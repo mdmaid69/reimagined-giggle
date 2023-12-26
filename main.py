064c8b90749fb03abb0fc6845e95a5f73ff4bd3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:46 +0000

    school record interesting onto quality

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
