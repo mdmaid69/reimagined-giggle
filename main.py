1c85536c50684fb0165cc5571e2634ae4bb0b969
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:13 +0000

    reliable year tooth convenient joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
