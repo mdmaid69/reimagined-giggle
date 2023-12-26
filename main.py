db9ea53f8dd80c7d4fae0fe69e6a493a4ad4b778
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:44:40 +0000

    search transparent sit borrow nervous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
