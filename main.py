4bb84b7b456b653037b885451f89810f37777cb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:15 +0000

    whistle summer chase communicate gradually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
