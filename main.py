f66c9ac5a2c10c38c8e3b99f5b22b737e97fe413
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:04 +0000

    offence limit wash feeling course

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
