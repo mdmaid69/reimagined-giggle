7db8d65f6df8bbfc4e699554d91955d95bfb650d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:36 +0000

    lesson watch goaway course public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-for i in range(5):
-        print(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
