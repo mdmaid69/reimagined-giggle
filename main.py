15e337151bf5f8341eb4b706dd8189d8249e7543
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:12 +0000

    here dry safety concerning sink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
