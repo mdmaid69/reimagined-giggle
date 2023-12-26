b8b069caa2159173a24cb0363594af00be9ddba6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:09 +0000

    my ago raise inquire match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
