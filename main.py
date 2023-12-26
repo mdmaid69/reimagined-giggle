ec6d6f40a52617064ae9ac02fde73b11f36b1241
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:23 +0000

    some exam finger pronounce cut

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_pressure(force, area):
-        return force / area
+print(sum(range(10)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
