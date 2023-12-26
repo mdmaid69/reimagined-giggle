0e523af812c9772d522fd9fb806d3283310fd871
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:00 +0000

    relationship ahead hers ring popular

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_acceleration(speed, time):
+        return speed / time
