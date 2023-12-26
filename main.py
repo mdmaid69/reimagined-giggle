ebdf47f252426e4040be17fdb9e6a9c1bb70690a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:00 +0000

    warning shut quiet approval excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
