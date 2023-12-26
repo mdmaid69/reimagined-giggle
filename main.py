4264aa2eadd3137648810adf21bbfff0f8cfe0f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:41 +0000

    progress dare warm term object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def subtract_numbers(x, y):
+        return x - y
