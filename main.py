7114c6894dd3e3ebdb5b78a3905811b2516ebb9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:07:49 +0000

    scene date increase wait coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_work(force, distance):
+        return force * distance
