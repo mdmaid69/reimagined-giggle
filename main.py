c191fecf0daab417ed77fbfa080c85199c79b924
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:59 +0000

    characteristic glass precise near curious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
