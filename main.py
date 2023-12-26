dbfcfdbd8b47cf04e3dfa15ed9d26f8819f2d19e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:08 +0000

    enthusiastic rely choose thick female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
