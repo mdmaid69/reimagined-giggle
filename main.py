c103f59e506438802ca10f9349511d72cd72d860
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:58 +0000

    nurse tap 572enter breakout window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
