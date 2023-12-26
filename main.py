f2d6cfe88a15ba6e1cf14d80d0ced0b76a747c39
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:20:01 +0000

    anywayanyhow black wash hand plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_volume(length, width, height):
+        return length * width * height
