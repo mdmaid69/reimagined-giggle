3be8ca4d02e4e874a6f1a048fce8568cd2ec2a10
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:20 +0000

    sudden past owe fetch spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bool(array):
+        return bool(array)
