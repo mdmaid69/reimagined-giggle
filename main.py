9cef84de425eb99581866a469f0b4085af5e5569
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:17 +0000

    whether gentle earn comment best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
