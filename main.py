70fc4c972246dda66d9fc3353171fd624b4bd043
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:30:34 +0000

    throughout warning rob deliver collect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
