f7821871755c49c3f0dffe042d47c078e27da5a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:07:38 +0000

    everywhere godown certain sometimes until

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
