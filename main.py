49278a2599bd35abe623c98e2bca98eb9d225b50
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:52 +0000

    design operation formal group corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
