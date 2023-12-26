8f23b67c8239c831ab9182e01a11b8616a321663
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:13 +0000

    skin squeeze immediate stand mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
