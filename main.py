38d132b363500463bcb3c1bca229bc4dbb5f5965
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:33 +0000

    admit transport exist estimate guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
