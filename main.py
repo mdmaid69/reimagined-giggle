4e28dcca83b5d42b5c56a6c41050282128bef8de
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:28 +0000

    mood unite walk case congratulate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
