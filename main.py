6da6711073d126bfb2dbe9c8970eef895f0461fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:19 +0000

    object precise snow wheel relationship

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def add_numbers(a, b):
+        return a + b
