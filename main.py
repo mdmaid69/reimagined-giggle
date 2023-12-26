4de74eff60010da25bd3a98e16d80a02beb741fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:40 +0000

    smell expression joke record still

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
