63a4efcf69af98897d5b7dc353f65b57e3b92cb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:28:28 +0000

    below curious actually study over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
