189af3d8af3e368f946acc8bb363ec766a264abc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:31 +0000

    cheek this run crisis around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def convert_to_octal(n):
+        return oct(n)
