6d53928e5e1dc4aca046646087269b8dd6270b64
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:56:27 +0000

    always improve decide hurry pretty

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
