3edf5a789d48ac8d4f1f3ba004b7b91b23ece73b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:57 +0000

    can may flow nice murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
