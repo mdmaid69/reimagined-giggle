21613ff472c14092e67ffe805c0c4e08c5ebb452
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:39 +0000

    him relief familiar truth pretty

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
