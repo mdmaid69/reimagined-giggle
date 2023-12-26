51c3b6f3c665d674dd1a8d5c9d3d119b191fd839
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:55 +0000

    anywhere spite sea 568energy wipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
