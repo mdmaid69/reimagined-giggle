16e14a46288fb5805ec495a369bb2727ed37db38
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:56:48 +0000

    possibly hello shrink part goup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def clear_array(array):
-        array *= 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
