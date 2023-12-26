aaa39e9d88429783747e818e508013dbaddbc777
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:15 +0000

    port balance operate tip morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
