b085c5144128e2f54b698d01e051079d4354f4b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:18:04 +0000

    grade jump there journey care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_min(lst):
+        return min(lst)
