b2de873c2f623fee57289f07f45b7fa52532013b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:06 +0000

    various path history deal food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
