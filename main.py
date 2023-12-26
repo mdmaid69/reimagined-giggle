bfaad695aabc2c83ce63f2f34f880e28427bf794
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:34:17 +0000

    protection tooth act even matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
