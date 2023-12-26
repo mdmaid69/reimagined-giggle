4cba4a3b5206c98109d37dabb42999b4eb755f33
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:10:56 +0000

    experience object smile confusing strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
