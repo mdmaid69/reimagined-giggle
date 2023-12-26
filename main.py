28ade95ea8e2377cb257760cf506ea4d6681690b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:24 +0000

    mark injury firm note murder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_max(lst):
+        return max(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
