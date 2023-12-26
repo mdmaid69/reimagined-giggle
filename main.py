1b76fc4f4d5b1eb2791fb94b624084fab44a223b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:03 +0000

    progress war punishment either there

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
