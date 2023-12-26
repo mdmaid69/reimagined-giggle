620f70fa3900c39865cb3c2a8fc9cd4cfab15c51
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:21 +0000

    pin chief negative before really

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_current_working_directory():
+        return os.getcwd()
