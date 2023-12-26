993a8e4bc4a7379de2a63a671ea40f4a22265491
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:56 +0000

    turninto wise interview class small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
