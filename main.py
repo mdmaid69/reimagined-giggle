f9a2716487cc86f8836eb04e3511e871b663a53b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:06 +0000

    honest pay neat step production

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
