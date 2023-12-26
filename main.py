f6d39368091f8992ce44d78c3f9c1c3b9d4f7edc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:29 +0000

    poison shall while once some

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
