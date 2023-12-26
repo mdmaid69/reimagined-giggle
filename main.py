1065043fb40ef4ffc86bb5570378e05064d8bb0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:01 +0000

    drink unusual best inquire I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def print_python_version():
+        print(sys.version)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
