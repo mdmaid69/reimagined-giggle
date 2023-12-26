d44fb60046ce6138a6a580f1df189506a7783f60
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:17 +0000

    as look finally shoulder neck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
