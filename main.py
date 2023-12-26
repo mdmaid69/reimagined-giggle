48028c3803ca22368e8477fc6701979b51a12f09
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:42:36 +0000

    habit at disappointed objection this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
