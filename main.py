a5e993fb3e99a8e7849b8c5d1d8825385a9ccf6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:00 +0000

    trap compare invite pleased surprise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_density(mass, volume):
-        return mass / volume
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
