a2216333ff2bdae875916a7d3c57a24fd12e8b76
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:35:12 +0000

    father wind movement long interrupt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
