e4e76f4d24a89946cc4f38f878020599277d83dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:03 +0000

    takeback cool offend surprised wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
