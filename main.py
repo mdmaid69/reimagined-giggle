2989085c5f61d763347fbbbe186a62b162b54364
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:30 +0000

    anybody maximum 573entertainment turnup arrival

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
