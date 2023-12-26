60a169625418242016510493ac3cfdeb5f0f01d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:28 +0000

    water holdup material along breakdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
