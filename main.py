5a851a4bcc7ed158b8693477cd5b37155599d552
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:45:39 +0000

    comedown content introduce father stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
