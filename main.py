08fda71f65a6a854e103769d264ccfbab9d88d4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:34 +0000

    twist division negative discover kick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
