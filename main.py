6ec7061b60e5e445432276412c4728e06ec227ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:39 +0000

    imagine cover study machine live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_speed(distance, time):
+        return distance / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
