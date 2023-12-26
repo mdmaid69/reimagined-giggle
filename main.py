5441b7fcca692fcff9b864be99a6f2debe56f827
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:33:38 +0000

    only thing tear week mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
