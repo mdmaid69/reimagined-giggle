01a2b65fb9fe66c608b05d35ac0d092925982f79
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:32 +0000

    take aware married lot clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
