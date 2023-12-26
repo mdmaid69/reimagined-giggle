fc10f4df5d21a60cf06ede2eb746659cdf49094a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:17:56 +0000

    roll law murder laugh off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-print(time.time())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
