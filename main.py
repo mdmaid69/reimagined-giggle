5b80efa586c22c5f7f39ba06e5b0ceebe9c27be0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:22 +0000

    guess cover classroom begin suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_item_count(array, item):
+        return array.count(item)
+  def cube_number(x):
+        return x**3
