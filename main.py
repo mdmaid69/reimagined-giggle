1cf7a1c64c1d8fe181135250048dc68a41ca3123
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:21 +0000

    count relief stamp turnon state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
