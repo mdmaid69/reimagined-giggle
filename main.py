7f16f86804809e4116ea110a7a36f8f7b3f2f575
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:21 +0000

    sail write flight sorry urgent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
