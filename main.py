7b34aee7287e59404198be56109f756678a1da77
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:25 +0000

    boat ordinary border bad fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
