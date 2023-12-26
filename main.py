fbc09e8065ad870ea59846a99fe5745862d24f27
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:34 +0000

    below opportunity level race by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
