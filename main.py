3983ae9bbea5742ddfe396dcae4d58ae83f85882
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:13 +0000

    turninto sail prefer payment turnon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-print(os.getcwd())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
