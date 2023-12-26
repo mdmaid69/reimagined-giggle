1674ee37c702930892752d7e7fd8d5e707857fbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:59 +0000

    zero damp small everyone tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
