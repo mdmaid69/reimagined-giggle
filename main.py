7ea749a040dce01458ce9547b7f6a1887fa54117
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:27:29 +0000

    cinema breakinto final issue reduce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
