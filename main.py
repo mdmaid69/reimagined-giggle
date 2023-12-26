5db3dcb0e3fb181e1617d2fafb66584f10a73c1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:54 +0000

    workon average responsible heat open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
