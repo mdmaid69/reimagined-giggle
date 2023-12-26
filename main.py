bc882fdc3939391b26d2b1822b4917f9af7d234b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:09 +0000

    fight for amount standard get

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
