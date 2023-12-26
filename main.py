db5f8f1b615eec03a91c572ace383961ffb5130c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:25 +0000

    justice suddenly back police ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+print(re.match("h.*o", "hello world"))
