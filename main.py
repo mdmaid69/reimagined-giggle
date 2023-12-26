d8f7d26b876390a44b15340f2c39a38fbca4419a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:23 +0000

    minor geton fly collection taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
