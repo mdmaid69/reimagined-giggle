ab6741b2bd8cf18a7acf7e9d0ee54f86780ffe91
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:19 +0000

    correction coat takeover round great

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
