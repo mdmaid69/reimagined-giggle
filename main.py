3264c4b18eaa590bdd6ae31208fd667509e11111
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:46 +0000

    shout nervous common emphasize takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
