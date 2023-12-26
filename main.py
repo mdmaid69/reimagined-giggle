fd52e9dc73c8afa2a406a4ef21a14b07381b4337
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:58:48 +0000

    guess life cover interesting smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
