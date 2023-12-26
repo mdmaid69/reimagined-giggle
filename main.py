3ad8512b47982d0a83f4eae9408fce4d4f957f1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:25 +0000

    hello sound sticky else day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
