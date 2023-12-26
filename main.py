22487222d1f5e3f14bc56e2900a47edab2840ceb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:03 +0000

    pretend couple tired holdup swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
