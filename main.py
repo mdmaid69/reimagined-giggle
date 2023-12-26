ccff3750678ea187c42b6d9f3032d10b07d39c6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:32 +0000

    understand print meet breakdown anxious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
