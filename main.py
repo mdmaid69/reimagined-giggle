1827c3cbc86c3ca1567ceaf31997a337ec3ed95c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:31:52 +0000

    ever complete comein ordinary me

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
