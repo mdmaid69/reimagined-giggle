c64590bef9846878489bed1a0ba893a0ca4329b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:41:43 +0000

    this whole rush laugh comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
