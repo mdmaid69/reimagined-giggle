1977730eec30ae8ad3bf56c7fc4fd8d8b8f209a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:02 +0000

    turnon interested risk pretty lonely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
