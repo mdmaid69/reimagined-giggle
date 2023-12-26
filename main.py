95b3f3e8dce04b2bc8f9855b7750f9ee0cb89e8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:56 +0000

    ourselves sleep entrance sore prepare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
