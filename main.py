5e8bbcb361a9b25cf82419afe1b62029eea75a16
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:02 +0000

    have difference threaten quantity operate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_min(lst):
-        return min(lst)
-print("Hello, world!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
