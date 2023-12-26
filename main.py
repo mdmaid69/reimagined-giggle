84cbc483be6f6396b63af5aadab2f6c2a30031af
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:10:52 +0000

    afraid major joke piece we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_set(array):
-        return set(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def remove_duplicates(lst):
+        return list(set(lst))
