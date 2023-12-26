c6aee346ca8c50ee46f2e2b19739d35c9ce64cfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:02 +0000

    Zz your hide tidy whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
