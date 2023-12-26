396eaf7e23d7d0c46e69e04cbb3f743743ce0f7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:08:42 +0000

    takeout street trip history centre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
