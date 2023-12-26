31d49b077f9a3388b05dca3b982d4bc68a5156b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:49 +0000

    show possibility of model generous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
