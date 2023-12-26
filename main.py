15597ed728e84d8f3822d67914885174f6c94650
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:38 +0000

    downstairs object fetch shape squash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
