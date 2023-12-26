0a9bd3b7e7b61933de354a627ae358047d6ed97e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:49:56 +0000

    beneath sink before wear prepare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
