58f22dd4649e9deeacf19114adaa041702577b58
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:28 +0000

    worth basic advantage Itself hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
