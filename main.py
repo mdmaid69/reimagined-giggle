aeb00be4c4ae2456e6ec2f3073892d18fe00025b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:02 +0000

    bar law satisfied believe pause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+for i in range(10): print(i)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
