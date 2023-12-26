f00acf8be765deef390a8d2239b0075e4db0d23f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:39 +0000

    that however earth plenty theirs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
