9836cdf06b2f5b2b8a5819f0edf3c96625e0eedb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:05 +0000

    division bill world each scare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
