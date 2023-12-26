aedde9f27a1d9897115bc07ebdba19dbafb1f808
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:48 +0000

    willing statement excuse fix afterwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
