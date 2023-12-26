f64634f7bb6de3bf13870247f773f6550edff50e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:59 +0000

    question generally history talk react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def get_list_from_array(array):
+        return array.tolist()
