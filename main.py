40ec494350d297031223d09d50a85e75c8ace053
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:31 +0000

    shock evidence mine preparation cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
