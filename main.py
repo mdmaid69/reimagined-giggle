dd6d3d6d5042d09b749e5c8cccaf4ca44a41e77d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:51 +0000

    present journey know suit guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def convert_to_octal(n):
+        return oct(n)
