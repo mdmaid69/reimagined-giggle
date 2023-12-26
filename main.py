37aa701ff2166fc9ef0dddb872564560fb49db46
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:02 +0000

    talk wave beside trap meeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
