0132936dd4b7ef82827ec57a6a601c595efc68aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:16 +0000

    tragedy cross interfere powerful perform

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
