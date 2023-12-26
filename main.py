605e5f13a30bfc3dbc7c57b47697ac45634489c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:10:15 +0000

    afterwards tear into near mad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
