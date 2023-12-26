d3a5163cda5e015850c4434a11880616e7533451
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:32 +0000

    chase keen even set line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
