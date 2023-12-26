981afea5c6d754b036621c93fc9c40d4ec145b74
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:03 +0000

    Vv heat pocket put thumb

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
