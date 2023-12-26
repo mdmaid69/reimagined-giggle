e5369239a8ae8220f052a98e53b0268dc621b37b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:22 +0000

    hurry interview education male reasonable

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
