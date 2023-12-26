6442d1e617cd7d40d72e12e32defb4450dd0a29a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:30 +0000

    hole repair success direction knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
