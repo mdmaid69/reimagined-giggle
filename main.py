5112991b9d65a1ebcccf7b612ab456834f6b4a14
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:59:07 +0000

    us over concerning suddenly environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def is_palindrome(s):
+        return s == s[::-1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
