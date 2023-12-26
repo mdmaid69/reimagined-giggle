0bbc4c0c788870b01794b1f535cdef4d46e4d525
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:04 +0000

    round hope back feeling quit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
