418826c77b22272b80e41a32c180e90e725849c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:29 +0000

    contribute perform seriously interview couple

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
