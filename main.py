1f0d52b0f0ea56c6fc4a4e1f2076fc8edb4d246e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:19 +0000

    past goodbye emphasize dream comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
