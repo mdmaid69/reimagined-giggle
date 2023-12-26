9b6ac87c4af25c68e16644452699d1cbe5eb3fd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:30 +0000

    spoil behaviour Kk trial road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
