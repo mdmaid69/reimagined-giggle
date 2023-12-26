1e9c49de7c030af4ed68ab5e6aea10a3b08cb544
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:09 +0000

    cinema late grateful getup creature

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
