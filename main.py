46604e23abc9bfdf23ede927c69a014675919a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:05 +0000

    accidental centre remark month ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
