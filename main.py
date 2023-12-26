47420d315c3c3b43646906f58db64020dc4eb600
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:53 +0000

    hot answer never shut even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
