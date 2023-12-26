6d920b2e4af4889cac9cb2e14cfe9cffb8f917a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:36 +0000

    bottom shall face private bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
