cc2c659e1ef2aba0084c9d023ca676848d4a8797
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:20:52 +0000

    recently sing but drink beautiful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
