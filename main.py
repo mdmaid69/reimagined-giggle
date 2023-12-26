1881409e7e600805e9ad57574e216367eeff2551
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:39 +0000

    increase work combine language people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def count_characters(sentence):
+        return len(sentence)
