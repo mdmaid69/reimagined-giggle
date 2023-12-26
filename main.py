085ff4831415c08bb8cc5c8e0ac80c5677097527
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:54:41 +0000

    shrink equal middle final shoulder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
