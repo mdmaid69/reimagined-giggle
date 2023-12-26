2650fccba16f579848b4e857aad2d6d9d63df08f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:25 +0000

    tour impressive switch cinema flat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
