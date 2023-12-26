bfee5b1b3f005623bf6be4c44c6f1ddc1aca8bbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:19 +0000

    goout method memory aim clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
