c51ce99b003d7ecc65b3e3740e09851d22c8cdfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:00 +0000

    instead through minority side watch

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
