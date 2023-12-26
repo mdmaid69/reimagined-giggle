a4ccf21c2cd577eb086e4744c5a8c50b3100d6c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:21 +0000

    impressive getup invite container disappointed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
