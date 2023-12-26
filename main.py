25e3577af5ed167d3a8169146a147a36df771cbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:28 +0000

    care dead appeal confused moral

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_palindrome(s):
+        return s == s[::-1]
