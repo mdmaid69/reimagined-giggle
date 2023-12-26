4acd551213d6504ebfa6b4c039bc325c7507a2ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:22 +0000

    limit estimate second create maximum

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
