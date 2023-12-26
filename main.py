4abba83d6bec83e575206e3e72683fdda31edbf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:23:10 +0000

    arrive straight miss tidy cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def sort_list(lst):
+        return sorted(lst)
