cb049018e47e72cca771e6852449ec5b4eef0ff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:12 +0000

    print realize thought there proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def is_even(n):
+        return n % 2 == 0
