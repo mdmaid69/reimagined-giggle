b3d0d885d9eea8f097a3570df03c9f174151a671
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:07 +0000

    treatment course consist turnon quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def cube_number(x):
-        return x**3
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
