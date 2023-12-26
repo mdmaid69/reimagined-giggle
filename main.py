307c5b5dfa6f9345ac0eca516d87608255ff0295
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:57 +0000

    corner within mark right meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
