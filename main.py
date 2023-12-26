63e4a063c88a34487e5ca5b25a30876cd79c5375
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:51 +0000

    thick behaviour authority their patient

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
