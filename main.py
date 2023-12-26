af32bdb02d0e2464d7539b24b49bfb8f3d737a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:17 +0000

    excellent accordingto jump care autumn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import getpass
+def get_username():
+        return getpass.getuser()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
