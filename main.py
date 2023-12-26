25f802fe1e967b1c0b2f7c73d163f8c25455fec1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:09 +0000

    why rise alone familiar wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
