a50adf1f6443a52dc8c6fac6f7865a670b529b2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:48:19 +0000

    sort young lead confidence comfort

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
