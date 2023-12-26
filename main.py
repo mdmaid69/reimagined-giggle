13bd7e7b9b5ac456c2e24a525adaaad9068035bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:47 +0000

    thank keepout original centimetre approval

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
