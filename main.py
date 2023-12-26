dbe5ee7f044e75defc46dce009cae18678944b63
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:51 +0000

    brief condition allow in tell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
