b4d284de6a8dc06c0f986b3ea9dbb02edd1be06b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:19 +0000

    getup Qq bother damage confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
