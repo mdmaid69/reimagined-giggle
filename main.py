76104bfa18a02401a0a094c3c4135da9127edd0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:04 +0000

    message slow 569enjoy relax communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
