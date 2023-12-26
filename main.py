2bde3df456a3cddc72e589213da282e74cfe49fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:40 +0000

    sore pin post few several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
