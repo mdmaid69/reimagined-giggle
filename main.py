8c496c2f346adeef9c6e59911b3b143d6244e00a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:20 +0000

    mood finally reserve climb burst

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
