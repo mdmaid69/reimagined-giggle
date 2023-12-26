efd818cc532535187cc1b39e3b1ba97b72c82e49
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:34 +0000

    better unit patient lookaround side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def is_odd(n):
-        return n % 2 != 0
+def sort_list(lst):
+        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
