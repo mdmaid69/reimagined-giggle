51f8a6e4569178f1a385661397eaa95a87439b53
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:56 +0000

    completely art safe wave glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_bytes_from_array(array):
+        return array.tobytes()
