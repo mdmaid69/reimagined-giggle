4e87d0b2cf31a9545ab91bd95eadcb8b8b1c5083
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:07 +0000

    climb collect deep free team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
