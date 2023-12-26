f6b7094af3a77a6e3e7b8c6b6fc4c689c3cea0ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:34 +0000

    mind rest blue desperate recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
