06991c203c2b753ede287070e4883833f9bfa808
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:43 +0000

    luck lost ours match imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
