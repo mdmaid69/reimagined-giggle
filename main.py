26d31ad753cc57ff2c45b2e8849330bd06051d7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:41 +0000

    lend each fire best statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
