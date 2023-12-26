cbce62cb6867b248386d1a587734d824e9d59a09
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:00 +0000

    right truth trouble cook correct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
