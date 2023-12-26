dd27daff76faf26b1ee65a8b34eb0273de3ff204
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:22 +0000

    goaway book love rough miss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-print(time.time())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_os_info():
+        return platform.uname()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
