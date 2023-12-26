07a7866cc70a06db0d77d2bcf9bbb1e8273efb0d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:30 +0000

    degree ground mistake better cover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print([x**2 for x in range(10)])
