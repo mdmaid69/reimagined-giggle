a82907e0cfe781733e4a0ed9ae110f7bd5469fbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:43:15 +0000

    base collection anxious hold colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
