d07ce8f732cd5c638d0243ae44612b09d6666266
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:27 +0000

    police little official influence question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def sort_list(lst):
+        return sorted(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
