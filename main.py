90c13ce8967046411a188f5aa6c6330449e0736f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:54:45 +0000

    cross method rain Ff yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
