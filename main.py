1b36641d1e0a6f0de2d046bbe5ee1520523ae905
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:24 +0000

    jump arrangement fear test form

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
