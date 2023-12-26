c47aa86392658aad97de3bdd11ffa949d31a177e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:01 +0000

    prefer keepup pain bet fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
