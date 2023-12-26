cf8eaf4ac32ad126ca5d59b74575a7063bf78fbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:10 +0000

    fall rain cost aim talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
