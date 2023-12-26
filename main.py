5093b76846a52dc38be655557aed1c56a65b203d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:55 +0000

    borrow top bringforward hand crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
