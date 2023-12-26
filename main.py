ca0cd935338439aa612b3e97c854f41743d0c9cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:49 +0000

    please pressure excuse view issue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
