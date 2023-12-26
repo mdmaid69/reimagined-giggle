d7d65ae09a60c6842ef6e5e7fa9cdd35f5a32a23
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:22 +0000

    takeoff cut impression failure than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
