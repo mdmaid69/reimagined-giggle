ace198aca3d9633614eed2c602f81633107e3ab1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:59 +0000

    warning reject guess consult they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
