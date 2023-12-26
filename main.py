8900e820e9641933610cc84224389fc27f5c66ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:23 +0000

    only settle hello clue workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
