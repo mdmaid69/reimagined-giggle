8405d1b486f7db241cff48d4ca393dc24b395e71
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:50 +0000

    less sing order guard computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
