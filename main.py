99394392a7c623206fc395f66a9c2ab747879d2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:37 +0000

    cry journey however generous difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
