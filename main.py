f278cfa810640dd14ccffcfb97b79829fb972e10
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:38 +0000

    exam serve carry money question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
