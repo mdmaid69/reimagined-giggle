1ae7b73753af22b3f3ea477bd682b49511c7f547
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:33 +0000

    interview listen lookup punish yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_length(array):
+        return len(array)
