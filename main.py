faa85163c11bb0403079ac550f6d3c824b62f9ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:33 +0000

    themselves guess pay most family

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
