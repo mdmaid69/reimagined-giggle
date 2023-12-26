e6a866930003855b0a810cc4ed769be52b72f31b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:21 +0000

    Pp embarrassing juice comedown among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
