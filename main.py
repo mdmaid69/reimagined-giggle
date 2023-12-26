4db19b2840e8c874b89a7f7314adb8e3acb0a1e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:55 +0000

    preparation narrow over nice section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
