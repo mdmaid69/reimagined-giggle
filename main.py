5fe97a7956bf86c2f979ac3c4df23d4a36890fef
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:38:59 +0000

    business skirt such law difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
