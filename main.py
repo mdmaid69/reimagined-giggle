c07ff3924f0dd7396e4444ef535e264d86102c11
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:43 +0000

    type myself sink sting attempt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
