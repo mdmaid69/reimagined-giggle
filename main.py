3380290034418fb90cbd0f1dac84237f58d1debe
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:20 +0000

    help tidy face year could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
