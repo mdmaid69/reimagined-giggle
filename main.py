67bf9188345b23db93dfaebe07663078dd12236a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:08 +0000

    passage expression manage run surprise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
