5dbbeca0d48cc448c3ad34bac26ad7faa55033a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:39 +0000

    calm illness travel conversation shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
