8c6795097279221f9b680d317c0ba2933604f74e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:52 +0000

    build work motion period callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
