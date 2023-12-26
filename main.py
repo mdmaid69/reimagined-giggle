8201e6ccf770ab9f039b651c001e9e89b8a2199a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:21 +0000

    spin situation less interest around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
