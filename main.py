bfd6e5e8cc91dd9578fd82f7605bcf6d94b4bf93
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:02 +0000

    accommodation she breakinto clothes limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
