13c766a6bc3959956bbbaf42aa14e315f5863dbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:08:58 +0000

    interrupt private operate bored autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
