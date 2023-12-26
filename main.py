f96a56402411d345f6bb64e0ee4c1fc05c571fea
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:07 +0000

    zero moral plate finger ever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
