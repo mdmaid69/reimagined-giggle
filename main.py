ae2af7514228a1ee13cc6c999ec6733d0bae0a27
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:29 +0000

    poor chief hold lucky available

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
