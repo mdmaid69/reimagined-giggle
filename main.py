7431a241f6b7fca5ddebe440c33175f3f9248930
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:32 +0000

    trial stage related rescue flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_as_list(array):
+        return list(array)
