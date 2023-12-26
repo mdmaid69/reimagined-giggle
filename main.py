b8c5bf33cd0173150b6af8f2d9a7dc7d56dd6c1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:38:15 +0000

    ago connected its model see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
