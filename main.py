1eb75dddec862255cddee221ab1d1a33b5da89df
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:36:03 +0000

    murder tape skirt middle stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
