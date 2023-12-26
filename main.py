f08ea2429f0bfa5ed6407a226f3ea92a33fd7376
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:53 +0000

    embarrassed finally present base stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
