53d49b2f1b79916c035982ea0b0a0b251d032e85
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:33:26 +0000

    snow although explain complicated hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
