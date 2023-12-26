7401b1c34485ef597854f6cde7db01b878494d5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:53 +0000

    meeting feed sale mix advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
