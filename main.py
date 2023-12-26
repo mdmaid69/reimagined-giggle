630d101bb1d564ed03a70aca1f22fad81036184a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:37 +0000

    communication lookup her finger pupil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
