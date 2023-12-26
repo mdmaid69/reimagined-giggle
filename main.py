58a07e9aaa4bb5b5a76f16a0180760e1118232c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:32 +0000

    deserve remember discover underneath variety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
