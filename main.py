56bf5640387f5c19c99855b170a5449397364824
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:31 +0000

    somewhere roof worry cry watch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
