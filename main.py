f71f0792a1e73c50a39ae84f367e8669fede9123
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:27:27 +0000

    progress opposite course nerve welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
