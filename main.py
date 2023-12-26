feede1e4b5a39425cfc470f07c7f1e137a5bc497
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:40 +0000

    direction closed our shall scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
