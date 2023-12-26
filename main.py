ed287bc559d2b47c13eca483b919bc97aded4948
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:41 +0000

    hard school voice myself strong

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
