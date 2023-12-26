9fe11c883377c82e5ba925ec9e8d6b5ea4a858f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:22 +0000

    track degree autumn science settle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
