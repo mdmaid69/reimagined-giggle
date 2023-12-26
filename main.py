5cbd73533a391082e427a406fee9b49794792c28
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:54 +0000

    embarrassed maximum way capital spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
