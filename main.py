aee21578a17efc946d5c9cf2f00716073341ce04
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:15 +0000

    young steady through cry closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+x = 10
+y = 20
+print("Sum:", x + y)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
