1291e15454af74540cdc366e5bdf0f68805f13aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:07 +0000

    same past begin recently software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+def get_current_working_directory():
+        return os.getcwd()
