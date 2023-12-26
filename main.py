cbad5e55e453a102fa00f423001ace11fb8a2eb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:41 +0000

    straight only needle future uponon

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
