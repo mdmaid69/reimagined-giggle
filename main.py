86ff6724752d0abbfe30e24fc54ed79f33f4d623
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:52 +0000

    soil both almost trouble lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
