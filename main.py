4815f732e4741adef81d3ce7d31ff07bedfe1d60
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:08 +0000

    second murder feed keepaway makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
