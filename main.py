8c28fb5ffa987758b9df38beb8236485493bf137
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:20 +0000

    suppose form like takeaway dinner

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
