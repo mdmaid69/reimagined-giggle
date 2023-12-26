6777a46d7bd4d3118751e1d248a568d19ecd9dfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:16 +0000

    excuse crazy complaint hard bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def reverse_string(s):
-        return s[::-1]
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
