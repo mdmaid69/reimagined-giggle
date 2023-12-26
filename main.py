9299047670f05eb028a50ae6d76ce8b44744f3ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:40 +0000

    real coffee interview getback sentence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def square_number(x):
-        return x**2
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_max(numbers):
+        return max(numbers)
