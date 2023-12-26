3e9a8a1ad299966916372def54d76817f2e87824
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:38 +0000

    ability tall mass idea play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
