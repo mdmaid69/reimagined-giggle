b2a370ce7aece77678736ee8446bce1bf57545e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:31 +0000

    cause treatment warning after part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
