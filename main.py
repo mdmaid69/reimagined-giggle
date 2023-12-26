173dbd39af2f3bdf395b0e1955e7f9cd7789a4cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:11:27 +0000

    might deep mysterious principle unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
