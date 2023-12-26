16ef226c5d7bbb992cd520b03dad9f79a81a458d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:30 +0000

    chief discussion left claim likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
