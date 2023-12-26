f4117ea61169ee3e53efe097641de20de426cda2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:56 +0000

    dear back connection weak design

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+x = 10
+y = 20
+print("Sum:", x + y)
 import array
-def clear_array(array):
-        array *= 0
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
