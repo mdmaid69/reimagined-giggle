a64ed15b08f7729ef12f3b79b04baaf1de226d74
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:09 +0000

    city wave cheque imagine now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
