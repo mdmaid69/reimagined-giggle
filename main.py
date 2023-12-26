bdef30ba1a2b19361d95e9642f6c61f3b88acc62
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:13 +0000

    step community funny save whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
