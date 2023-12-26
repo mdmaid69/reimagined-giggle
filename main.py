75dfeeab7a459b3d084cfd55d1e4e925d3206fb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:12 +0000

    blue set mind worry someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
