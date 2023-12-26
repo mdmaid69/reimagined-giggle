73715ee7ca4c509c4867a2fa0ef622adac767d71
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:37 +0000

    different account patient main high

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
