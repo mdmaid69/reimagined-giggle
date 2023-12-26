cd7c955cc70b0aceaedf433ea1c48a567e1fff81
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:33 +0000

    suggest rid autumn factory take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def find_min(lst):
-        return min(lst)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  def convert_to_octal(n):
+        return oct(n)
