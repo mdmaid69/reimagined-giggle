677cb3897944d2d2c6165fcc6b842cf61fc327e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:31:54 +0000

    join rid doubt closely with

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_density(mass, volume):
+        return mass / volume
+text = "Hello, world!"
+print("Characters:", len(text))
