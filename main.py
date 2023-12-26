7c6a5202f6eefce3a21504d65adf94018edaa040
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:42:53 +0000

    than lie guilty tell split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
