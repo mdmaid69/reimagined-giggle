4149aad7946db0f4d752421f610812ff1437f8cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:28 +0000

    remove tonight noone watch party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
