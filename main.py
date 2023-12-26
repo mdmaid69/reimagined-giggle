242aba5fa8111ed9b402ff5caa107f3df20de2d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:56 +0000

    shut another bringforward declare home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
