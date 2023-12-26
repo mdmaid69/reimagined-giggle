17b0a2d3a190ab0b7f7746e62297df503e2c6c5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:58 +0000

    pollution please die calloff chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
