5902c7659a3c933710be4b5c9974a34d5869cf54
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:02 +0000

    shall air though duty comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
