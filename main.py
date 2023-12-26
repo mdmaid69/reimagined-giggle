11dba350ac1956a6539fbebe9c36b4aa892345c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:01:31 +0000

    attractive rank scheme same public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
