57bcea157c39d0c128ccba50b51885a621410dbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:03 +0000

    spray always soil closed emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
