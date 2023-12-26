522775e3be663598fd01b2dd6599e1b80237c182
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:23 +0000

    board work travel inside threaten

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
