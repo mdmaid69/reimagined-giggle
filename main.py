8b2f6411c060cbb4325a65eef487a461052397e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:02 +0000

    future happen serious record after

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
