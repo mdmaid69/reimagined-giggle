86346d84cfe8c93e443eb4e2028aeaaf57c02120
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:32 +0000

    criticism habit cost trip floor

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
