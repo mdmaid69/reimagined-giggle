d1a0b461cde0c1130a4d73ef77a39c4de112954f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:15:39 +0000

    holdon once base holdup woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
