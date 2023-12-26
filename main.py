2ec47f75c60ebcf251000072bb89ef2f5dfa9c5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:51 +0000

    border give fat old blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import time
+print(time.time())
