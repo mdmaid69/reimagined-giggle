46a7f54622d61d8bb6580ab871d98ac515c51504
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:16 +0000

    exactly combine shade bound trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
