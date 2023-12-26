5b0ca75fe1e1ec226857812215b17cde319a20cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:57 +0000

    keen relative Ff however nowhere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
