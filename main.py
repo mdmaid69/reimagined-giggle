3ec3a167ddf78f62cb27d2111f8b823a5a44429f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:36 +0000

    Cc present cool say nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
