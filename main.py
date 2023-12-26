7dd12484fb997c633bd3c4209ec3d011a79e1816
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:08 +0000

    interesting confusing second feed call

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_max(numbers):
+        return max(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
