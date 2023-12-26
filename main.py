438dfdd1ee080adb634437f27ca254182a2074d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:53 +0000

    closely gift criticize hot information

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
