f2ad8cc7d7fd8376f039b5949ff35ccdff82c6d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:47 +0000

    bet prefer rest sometimes hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
