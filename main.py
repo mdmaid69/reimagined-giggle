42df099c4eaa21b65036fef2a196140d134f6c16
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:13 +0000

    future criticism fit keepout interruption

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-for i in range(10): print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
