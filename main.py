698d394735e3c6a84007a2592f672b900f3efacf
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:32:59 +0000

    pretty target expression neck Bb

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
