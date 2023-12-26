0bc47f8e81bd202f1af51fbb7d17f118570d8274
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:23 +0000

    rob immediately badly indeed carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
