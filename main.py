014293a642a34974fe746b8ad14f748155e9a619
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:12 +0000

    discuss medium goout strange roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
