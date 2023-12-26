d4fc0c84a38190940149b5842c25aad76cdedf74
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:18 +0000

    wash recover week Tt decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def reverse_list(lst):
+        return lst[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
