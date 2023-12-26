ff9c5d161a96d921010b4370c69e6011024a2ae3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:31 +0000

    foot awake world burn 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
