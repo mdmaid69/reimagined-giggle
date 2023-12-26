012edc581bec9fbd53f82954736c7ce3baf596f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:15 +0000

    throw somewhere kind good pull

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
