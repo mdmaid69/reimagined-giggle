65c9ebf2e1a6bc43d516afae86651308901f906a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:40 +0000

    cold contents aim demonstrate pretty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def count_elements(lst):
+        return len(lst)
