be96936b5acfdbc276bcebadfd4c249cbe4e08e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:19 +0000

    whistle off fun worried sense

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
