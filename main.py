09ce4d5f4e2a951d453a592fe184d90434986019
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:18 +0000

    exercise who whether pen spin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
