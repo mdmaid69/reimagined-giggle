b5b7ff736c9b1fa66a64c78fc76cdf036a7984ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:19 +0000

    strange settle suck score whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
