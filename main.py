183747a8607300981a97e11ee6889761e21d246f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:25 +0000

    sky clothes tape place rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
