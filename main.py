7cceb1c61f56dd98531d1b80d888ef219fbfa055
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:51:05 +0000

    even or confuse business dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
