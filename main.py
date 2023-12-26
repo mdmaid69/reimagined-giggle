c6f1c13fbe2cb203cdad2bfaf8714fd43a40a4dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:56:05 +0000

    shop snow straight drop her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_current_working_directory():
         return os.getcwd()
