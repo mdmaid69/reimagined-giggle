460acfe79184151e4d007e72ca028573dfa8c704
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:51 +0000

    part wave fortunate whichever extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
