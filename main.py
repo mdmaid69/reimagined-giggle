2042f5b9652a2f263ad6223822b0f980b57afbfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:57 +0000

    letter priority centre Oo up

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import sys
+  def get_python_version():
+        return sys.version
