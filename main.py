3ec2ef05aa82c058fdfa96f429b6a12f5e2b2b93
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:20 +0000

    cool young talkoutof split buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
