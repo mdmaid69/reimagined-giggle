f7db86807b315fa0d8675c482c7cef3b99f55949
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:27 +0000

    walk goaway section lonely score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_binary(n):
+        return bin(n)
