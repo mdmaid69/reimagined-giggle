3bb64b7f64b1d7c570bdbbc58b589021d12321fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:16 +0000

    regional somewhere invitation variety suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
