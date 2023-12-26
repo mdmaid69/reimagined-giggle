ac5dc35fae99f6a095e0d34f2f63881a11f33ebf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:15 +0000

    speed round hard idea shut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
