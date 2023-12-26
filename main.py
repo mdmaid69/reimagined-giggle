2126d0c780d019aafe0113f9edb438cd16e54ebc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:38 +0000

    back ball turnoff occur total

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import sys
+print(sys.version)
