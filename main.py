3f1d652107c084cf98f3e1b909a7fe5bdacb576c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:58 +0000

    freeze succeed shout partner wash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-print(sys.version)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
