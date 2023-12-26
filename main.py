fe1977d72ac3337e9055586be620ac83d665130f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:24 +0000

    everybody shoot fierce getup union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_power(work, time):
+        return work / time
