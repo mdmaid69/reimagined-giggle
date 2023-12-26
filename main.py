a113b88d38413ee628c6f1013045996adb1769a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:09 +0000

    anyone basic care comeoff view

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import tensorflow as tf
+print(tf.__version__)
