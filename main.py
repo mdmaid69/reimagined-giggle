af7b8fab33905d2fe12b30dad0c099f01dd33c15
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:32 +0000

    quarter rub yours bag unless

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
