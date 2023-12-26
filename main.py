7aecbee2e79b4416bb8615304635f3db6eadcf0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:55 +0000

    where respect steady upset break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
