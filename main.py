03f0494f7d79a3fd9db72df6e2ee05ab02738588
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:38:14 +0000

    usual birth dress interesting warning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
