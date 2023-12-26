8851013b4bd154e54c6731d5dd9b8211947d5c6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:02 +0000

    more concentrate recent union comein

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
