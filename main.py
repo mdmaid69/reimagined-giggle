ac3c008bb26ffef063c892540fb893f281a76544
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:11 +0000

    beat my treat ticket study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import tensorflow as tf
-print(tf.__version__)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
