043802482a05f5932287aec33f03e8e18d5f2711
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:19 +0000

    usually respect cheap used arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
