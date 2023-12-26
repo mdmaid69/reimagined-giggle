ccdfe819fad95d48000dc5f737f1eb5cb6b14481
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:38 +0000

    admit violent exit stamp over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
