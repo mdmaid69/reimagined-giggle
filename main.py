a395b7ff21697142e43174d459f58ad4dbe5adfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:52:04 +0000

    file opposite 564encourage tragedy accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
