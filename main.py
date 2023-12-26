aba192a41fbd0d0f5bf667822e1efa65ef3197c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:47 +0000

    over introduce pass guard whistle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
