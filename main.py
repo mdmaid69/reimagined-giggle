5dafb15769b64d8c52035bcb24954020e1df1b5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:45 +0000

    this funny once entrance problem

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
