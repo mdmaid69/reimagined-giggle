deea6eaec712a4592df0e6e359add4a18aa27096
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:05 +0000

    grade inquire slice forward tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
