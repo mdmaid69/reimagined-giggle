dd70fc3b8fe16c19ba2c4c8b5e4263595b93e015
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:00 +0000

    owe tie swim tall gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
