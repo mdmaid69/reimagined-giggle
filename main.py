92602d80edfe10d35c4b361505ec09c28a7364b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:18 +0000

    trust independent flow Ff she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
