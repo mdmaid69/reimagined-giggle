b8b786a30890f1ef7aaf370fbe34eb6735ce4381
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:17 +0000

    official wire square return talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
