7d3a9eefcea23f96d4b8ba19a7e3f2aa56447eaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:30 +0000

    sense record brother summer yes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
