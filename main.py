13a7c086459caa8bd2c806c27895028f2d7054f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:49 +0000

    advice defeat high visit there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
