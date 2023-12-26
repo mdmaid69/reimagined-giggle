64bb781b50833571b0db867f85d67e39233071d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:55 +0000

    disagree down round proud reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def convert_array_to_list(array):
+        return array.tolist()
