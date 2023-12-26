0601186315de90a5fcdba31db1923fe68d6c4767
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:55 +0000

    change the me human takeaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
