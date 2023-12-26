6074d548d8dc5d7f9594f475c99dc78a8b10a73f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:51 +0000

    need keepfrom Ff almost community

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
