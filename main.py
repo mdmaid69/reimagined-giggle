4dc615df5dd939707a5ce2f83d05df683f819be8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:17 +0000

    female shame organize single contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_even(n):
+        return n % 2 == 0
