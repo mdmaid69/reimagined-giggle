0831672840de13df65baa8cb34e7bcb59575123a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:56:34 +0000

    whichever hungry aim might minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
