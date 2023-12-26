de4885b2f8f62289fa666d3515215947243e8fd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:30:48 +0000

    but cheat country question holdon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
