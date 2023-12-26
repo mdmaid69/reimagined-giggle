277c0f7faa934c843c93240312ba6cee40db60a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:31 +0000

    Mm taste close service there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
