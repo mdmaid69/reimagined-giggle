81f4b1ecf3857bf2d40760ab0c1c5b6cb743fd12
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:12 +0000

    sort store last will favourite

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def remove_duplicates(lst):
+        return list(set(lst))
+print(sum(range(10)))
