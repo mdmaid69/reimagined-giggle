54f8da66e7e4a2a38544184cd903677568fb5e01
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:51 +0000

    rescue contribute want guess Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
