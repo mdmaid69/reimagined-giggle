68c2b4e3b644e5a42dcb143d45e9d8b07183a30a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:15 +0000

    familiar above complete improvement miss

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
