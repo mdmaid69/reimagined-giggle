b0b012c8266f34b417452f2c31bd46af0b2a89dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:30 +0000

    leave joke sorry 571enough upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
