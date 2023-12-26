6f6220ae2b0ea5ec6677db782458ff94411368f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:30 +0000

    lot hear activity wide doctor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
