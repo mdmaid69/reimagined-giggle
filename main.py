7faad7c3a2d0987632c87240df0531a358aa8bdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:20 +0000

    breakfast find advantage style keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
