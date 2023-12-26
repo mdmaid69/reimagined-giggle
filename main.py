75eadc4ec883b6cf1328104f6dcfd5bb55edadb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:23 +0000

    own intelligent sense nearly complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
