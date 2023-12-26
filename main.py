23c44377449084aed9c8063070b6c5667ec2e227
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:30:39 +0000

    underneath responsibility autumn 564encourage wish

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+print("Hello, world!")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
