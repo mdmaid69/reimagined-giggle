817bf3dd2287de8abafaad3f755e7c0430412536
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:21 +0000

    swing quiet use stop wake

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
