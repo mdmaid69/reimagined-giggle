1b829a69bb9e86938b167e8f10be328251ad389c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:34 +0000

    basis meanwhile much crisis turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_date():
+        return datetime.date.today()
