9685a5ec9c6387c3198622d763b35d244d19ed9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:47 +0000

    breakfast time extra burst scare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def reverse_list(lst):
+        return lst[::-1]
