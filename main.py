75160adb3c50339f4ad56349dbd8b5b79b4b7e95
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:32 +0000

    skill bringdown back do upwards

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import collections
-def create_stack():
-        return collections.deque()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
