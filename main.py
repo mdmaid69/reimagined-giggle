a4343b1fd61a9ab252ad23727073cc3803569e4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:44 +0000

    group post careless insist visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
