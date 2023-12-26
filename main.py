6d1ed585d31454938bce8fd5c9ffbdbff9e34dd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:27 +0000

    shine debt waste disgusting die

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_user_string():
-        return collections.UserString()
+def reverse_string(s):
+        return s[::-1]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
