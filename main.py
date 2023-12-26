06948a64cec6d23b73c1e38ee40c2e206be37725
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:35 +0000

    collection worse spoil Tt supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print(sum(range(10)))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
