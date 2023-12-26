f3bdeb996ec64a70b7bfec85bc342bf42e909c4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:14:15 +0000

    closed platform suggest large phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
