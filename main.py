7f8c25dcddc0ebc21707605ecc93b82224c10b63
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:50 +0000

    method balance neat warning ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
