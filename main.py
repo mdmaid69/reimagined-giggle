ac82a311a68ccbeb199fa6a8a01b19a3b21851af
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:12:15 +0000

    sure drop getaway none declare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
