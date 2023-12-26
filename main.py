adf435349797e3230e28fb44a1d5b06eefb0f680
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:31 +0000

    bringdown welcome shine mean curious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_min(numbers):
-        return min(numbers)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
