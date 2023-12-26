a3709f73539015c87185dcb81cebe56532b6cedb
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:34 +0000

    apart reliable treatment theirs independent

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
