e2e3d17d65302b37baf6ea59da4082a6e601006d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:14 +0000

    pressure community noise for talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_array_as_set(array):
+        return set(array)
