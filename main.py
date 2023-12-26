0a68ce32652928876767f16fb7b7e900d5f860b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:27 +0000

    touch right journey holdout bottom

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
