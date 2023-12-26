d827bd71d0c71f977642bf9f9ed203d4b94b5d13
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:15 +0000

    definite grade accommodation survive range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
