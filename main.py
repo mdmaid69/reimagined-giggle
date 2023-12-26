2f768cb65ad666781a46b8c1e7f49a2fba9d9c8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:12 +0000

    collect either lock eye wealth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
