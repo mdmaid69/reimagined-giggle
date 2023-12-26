a9450839b5751bf407f16eec24af5c3d551200e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:45 +0000

    sure flight upstairs forget hospital

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_circle(r):
+        return 3.14 * r**2
