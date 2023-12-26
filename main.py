2f58456b2ae98c0ce322eb4aeccb1f8d239775c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:47 +0000

    push Ww century unfair strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
