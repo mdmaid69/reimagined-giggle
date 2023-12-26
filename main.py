8eba51c8a623f7abf6e8f7a3d5166e320a7a4f23
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:45 +0000

    capital every minority born once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
