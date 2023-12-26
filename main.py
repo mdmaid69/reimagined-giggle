5f0e10d35dd2adbbd01c7e9e15b8fc747dcc4e71
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:06 +0000

    start build risk push without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
