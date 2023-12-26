07a02902225e0d2ffe79625ffb6442b353559af4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:27 +0000

    content square improvement let die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_queue():
-        return collections.deque()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
