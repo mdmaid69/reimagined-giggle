c6acdc3b77209382223971b1e87075eaba8aa410
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:54 +0000

    possibly noisy present single survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
