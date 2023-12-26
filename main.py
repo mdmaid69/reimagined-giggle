6bc5c09c3077b9c72aa5f23a253ddb24b484922f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:57 +0000

    goout intend safety stupid target

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
