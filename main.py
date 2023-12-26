c6eca215f32049d142cf3abc46d4cc3294fe0742
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:44 +0000

    soon bringdown high bag cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_area(radius):
+        return 3.14 * radius * radius
