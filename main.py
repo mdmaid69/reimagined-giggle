3881fead8b60740bed64bf6309cc671571937de2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:33 +0000

    sensible young creature stare affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def convert_to_hex(n):
+        return hex(n)
