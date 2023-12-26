ad24b85e2e750589e225aa52040eb16db99f37bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:27 +0000

    surprised lay various lose affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
