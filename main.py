afbad3e4e531f121a3c2ca3cf71416e9653b7b46
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:15 +0000

    act letter familiar wash secret

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
