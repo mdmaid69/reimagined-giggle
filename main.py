b5aece0c598d49fc0421af1fd52a9fe15d789f6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:05 +0000

    passenger test soon polite press

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-print("Hello, world!")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
