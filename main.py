7930560de2130dbfaa5a1a444323128ee1087b2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:52 +0000

    runover play easy goback(to) until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
