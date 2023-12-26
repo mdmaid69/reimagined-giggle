d0e64ac232c313ffbb461d41fa2ad76ebbeaac7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:33 +0000

    mess charge reason strike worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
