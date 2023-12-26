bdd286fc5592fb31f4d17cfd73f78051c73ae4c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:44 +0000

    after brush cheat serve shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
