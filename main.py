f398292418e1e36e19fc46662aa238a9e173d431
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:47 +0000

    complete rob especially scream basis

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
