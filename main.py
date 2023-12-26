14660461f439c6dfd02b29f9e2a75f9425835922
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:29 +0000

    silent hot card opinion flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_today_date():
+        return datetime.date.today()
