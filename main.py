4b52c20c5feb521573602379a5c50f8a645bb2f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:53 +0000

    wrong young bill Gg bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
