7c3d1f365f10397551d5b6ca22f125a6cdb1d26e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:37 +0000

    we breakin conduct people building

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+print("Hello, world!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
