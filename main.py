8916fc17fc75741d3cab75f766e4fcce8ae6f738
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:11 +0000

    politics whichever interested discovery communicate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
