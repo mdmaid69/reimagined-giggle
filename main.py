055baf47011cf48a9aede5e4d202d81b00f0580f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:52 +0000

    gooff large they trade mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
