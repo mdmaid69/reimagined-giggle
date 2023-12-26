3b8741431ffdaae9b3fff5992ec6a0e0744b6a2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:27 +0000

    develop match degree accordingto price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_pressure(force, area):
+        return force / area
