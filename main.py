83d9b72c326e92cf98ed3b6b312aec8decbb64f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:26 +0000

    his area greeting shut shall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
