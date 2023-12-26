fc7b2c461b4bc7262361b85976732e6b175fe596
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:38:13 +0000

    eat bet forget introduction use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
