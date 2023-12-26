4c8cb66c6c84a177ddc7ba5b8d4f693217382f02
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:59:00 +0000

    else laugh that breakdown wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_speed(distance, time):
+        return distance / time
