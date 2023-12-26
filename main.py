2c8cb01d4f192cc38fbbaba24a522768d01c4469
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:25 +0000

    push only decision flavour tip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
