1f0eb91f35a17eb0dc1668e02995ef9b8428e497
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:04:38 +0000

    little like eye head only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
