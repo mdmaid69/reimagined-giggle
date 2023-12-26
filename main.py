cb4e70ae1ef0904eaee7ad63e1e3046b2347e090
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:38 +0000

    famous law recover walk announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
