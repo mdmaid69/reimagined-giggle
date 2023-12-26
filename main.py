8cd2605c27fc51198d662ccd4b00a6c6dd200ccd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:56 +0000

    message everybody else beyond cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
