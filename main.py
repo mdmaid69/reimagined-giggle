71325388a63273fc144fb24aa57242c863ad038c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:57 +0000

    capital work shelter advice block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
