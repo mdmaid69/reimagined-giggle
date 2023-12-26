4accfd23c70af91a4803a8d4df308a0625938f93
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:21 +0000

    stop interrupt suffer prevent influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
