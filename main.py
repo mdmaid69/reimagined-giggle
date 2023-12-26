9e64704652c34aa41fe63bfed81b85521c8afa99
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:40 +0000

    thanks describe fast crash prepare

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
