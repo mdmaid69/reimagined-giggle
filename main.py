d4d65fb05868ddc8b76f7b5095d4b96cba9e7039
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:27 +0000

    theirs through escape nigga fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
