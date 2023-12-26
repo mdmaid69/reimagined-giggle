0132c01753dd1ce6fa764e4065e19b042a45a0d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:04 +0000

    wing slip pay middle only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
