8528a3cbe457eeaf5526f2eace8dd90633b5c5f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:42:53 +0000

    abroad shop skill bring rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_exponential(x):
+        return math.exp(x)
+  def sort_list(lst):
+        return sorted(lst)
