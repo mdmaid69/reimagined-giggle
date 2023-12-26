cd5c6da82f05a2f867e60c944fd2d7e807dd739c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:35 +0000

    shame pronounce noisy favourite pull

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
