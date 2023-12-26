444bd93f64e9216b3e12159761802e39b4f5849a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:07 +0000

    giveout badly lucky them mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
