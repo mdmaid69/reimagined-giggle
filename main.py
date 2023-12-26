f924269dc22ce99791e878c98fdf04fe38bce88c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:39 +0000

    translate scrap glass final cheque

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
