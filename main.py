959c145af736f1673d94d6f4bd74f96d6c43e3a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:08 +0000

    concentration age style display love

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
