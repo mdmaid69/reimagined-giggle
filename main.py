cf17e69a8f874abe0b29f9ad8e9464304c626dc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:32 +0000

    beyond attach one after accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
