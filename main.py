f93d1d95bbbc094a6159bf09cdc63b6d01afa5a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:56 +0000

    about section supply course to

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_user_list():
+        return collections.UserList()
