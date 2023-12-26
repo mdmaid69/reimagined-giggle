b67e420b8d6a4b7feb51810cf80c1ddcf8b49f8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:03 +0000

    police reject affect jump suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
