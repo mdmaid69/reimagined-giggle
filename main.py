30084d35ce24be47deb63c180983f42dc43d5067
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:24:08 +0000

    organize cheque could blue place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Max:", max(numbers))
