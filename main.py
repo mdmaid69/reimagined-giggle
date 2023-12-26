ad68f06dccd121a288162f12e9fb8426cfbe3a50
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:45 +0000

    tour rest course usually properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
