9eeeb1857adce7b66441e20b1aec944964eb36dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:21:06 +0000

    influence bush start what busy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
