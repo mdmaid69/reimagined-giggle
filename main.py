fc89da58f9e91a84c77e6e163e4fb68c8ed6b1ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:10:18 +0000

    common squash break power mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
