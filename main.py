dff36021e710f1a343f93c8a671ab228f896d690
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:07 +0000

    quality poor round school different

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
