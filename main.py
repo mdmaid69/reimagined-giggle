f22bf4dc4e2252a17605b7f2f2c797d6d56ca0f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:59:10 +0000

    claim before shopping set translate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
