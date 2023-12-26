a71abd5742012c2f5c6dbdcafd47c0c887a3b688
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:15 +0000

    middle blue still arrange sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
