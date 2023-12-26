edb6dce1d67b0612cf3c1eaad35f0017005d3741
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:09 +0000

    unhappy over water getout keepup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
