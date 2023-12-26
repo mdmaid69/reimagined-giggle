f83a9d58022f5ca468527c2f89d7c4e05da5f856
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:06 +0000

    combination crash rain used believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def get_current_time():
-        return time.ctime()
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
