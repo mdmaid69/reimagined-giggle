bc69706ee435b873c59a0f9ced1ef0607de7f8b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:53 +0000

    rid only fond bye turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
