72796678e3d34db5b32cb86f28449d4c6fc1d904
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:06 +0000

    object interest sometimes friend mean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
