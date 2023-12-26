75bc7e4ccfdd22e0182c38ac00659c86e0490274
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:37 +0000

    estimate special pair giveup imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
