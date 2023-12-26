7e05ccdd56edb850f177246730c3bbd5ec7a3e9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:08 +0000

    welcome disturb make low positive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_set(array):
-        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
