5fc99fff703d9cbae01f140d20042a63d1c710d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:35 +0000

    trap workout like taste comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
