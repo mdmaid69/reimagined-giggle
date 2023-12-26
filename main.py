9c6cdc61ba6f04dcb4993c4e0fe72c757e98f117
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:13 +0000

    everywhere impressive solve pot post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
