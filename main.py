5023dfa20a83af1cfae71c75b090972054563f99
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:01 +0000

    path protest confident thin interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
