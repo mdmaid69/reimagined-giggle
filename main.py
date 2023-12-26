cf630e590815e0f77a801354b9c18da272a88b36
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:34 +0000

    nonsense warm floor firm juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import platform
+def get_python_version():
+        return platform.python_version()
