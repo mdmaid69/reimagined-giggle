397d3a166b6a2686cfccf7114fcd87dd4148c712
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:24 +0000

    market may tidy badly price

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_speed(distance, time):
+        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
