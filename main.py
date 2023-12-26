c850b5a9049955c11c939e6d973a3aaecc079f0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:12 +0000

    last cool makeinto lesson love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
