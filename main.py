0589c34999ef9fd91dec02813e557dc430621444
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:36 +0000

    last recognize rise quality welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
