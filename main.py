daa3a3c379ab9f9b9d37d5a309630c6235b3bd15
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:08 +0000

    protest weak process if will

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
