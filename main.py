e6467049217bfb6bd2fff20729d428661ad68bbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:24:35 +0000

    stiff dish kill Cc put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_bytes(array):
+        return bytes(array)
