e8e727aa100ca155446a11beab8956f8954d03e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:08 +0000

    young absolutely floor cum respect

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def add_numbers(x, y):
+        return x + y
