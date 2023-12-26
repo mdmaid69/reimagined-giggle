99ff3521ed1bbdfea69ade2872fc1a45683fe39c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:30 +0000

    schedule exact odd talkback 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
