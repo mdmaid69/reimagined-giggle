54fa0c6a949726681e78f96810b97a95ee78b0ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:51 +0000

    bunch rush Rr objection section

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
