85708704530799855511306fc3b9bba75a32a137
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:44 +0000

    chase yours consider speak drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
