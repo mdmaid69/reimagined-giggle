d984f0a9b4eb96b6ae399a51166c1ddad8420dc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:26 +0000

    difficulty accident listen conduct exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_current_directory():
+        return os.getcwd()
