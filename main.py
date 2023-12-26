00ee986c1e71c292ff7ca6d11ba2445e99e93f90
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:49 +0000

    impossible crash exercise slow but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
