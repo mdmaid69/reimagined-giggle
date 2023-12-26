eb9950a7a3002d2d419b08a427774929bd278add
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:39:37 +0000

    formal someone strong vote event

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
