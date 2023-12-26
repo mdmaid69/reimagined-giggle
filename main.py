6f5440c3d9dbcbf806fc97fd7d08febd31329a61
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:47 +0000

    cruelty punishment holiday comein state

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
