468fb333994c0e580fa50a3bcf78dc5323e9256b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:42 +0000

    zero you luck discovery water

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def sort_numbers(numbers):
-        return sorted(numbers)
+print("Hello, world!")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
