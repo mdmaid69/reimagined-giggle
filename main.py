3d98a3f1713714b8e75dda44f80d15a8e734b796
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:20 +0000

    squash plastic born relation post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
