a397abea5c8cba517fac7ec10d07332e390f8286
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:28 +0000

    below sort interested land worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
