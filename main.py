5816a6f7412a22a7293175cd89a1e8c5dbf6231a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:21 +0000

    sticky write Pp meeting brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
