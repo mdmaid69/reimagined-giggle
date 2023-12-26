5e474080892c085334a6feea17c99e7b83bf5f94
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:24:58 +0000

    throughout female best become distance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
