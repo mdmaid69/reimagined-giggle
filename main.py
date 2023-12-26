9071f4300a1ba1970814d7aee1bc828ff7f5ae43
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:03 +0000

    desperate flight compete tight commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
