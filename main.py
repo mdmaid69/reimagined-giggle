7827bac68c6be5c41201606dc30d914fb6e697d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:02 +0000

    my embarrassed together gradually curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
