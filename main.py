efaf850d415425a2f0fb476f8bec32829f5bf333
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:46 +0000

    hide direction mix comein easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
