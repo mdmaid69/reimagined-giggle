112000b7938385c521b0b23ca63ff9ad2ee5571e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:59 +0000

    damage include convenient throw how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
