c67ac2cfba5b9ae319d71cad322b0e522f8e4580
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:49 +0000

    consist juice level period declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
