dcd542bd83f4c0b6999b65a73f9739ae06c2802d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:04:47 +0000

    bringback your luck good dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
