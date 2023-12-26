a31d5ab6ad10c1a3f5fa705ae7b76b405584dd98
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:42 +0000

    book centimetre comeoff discussion hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
