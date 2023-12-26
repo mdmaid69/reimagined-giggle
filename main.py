6bae3115f52420bf761c3704650b71d9b8f917cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:42 +0000

    bottom pain fortunate my dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
