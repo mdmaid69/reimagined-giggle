6d9c190f72f6f17c780b3b738239bbb3a211a073
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:02 +0000

    shadow whether plenty public also

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def exit_program():
+        sys.exit()
