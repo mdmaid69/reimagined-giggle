d9ad9a00ac950ea3b98e4f7b806a04d3bf4d8241
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:13:33 +0000

    heat tax report ill drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+for i in range(5):
+        print(i)
+import array
+def get_array_as_set(array):
+        return set(array)
