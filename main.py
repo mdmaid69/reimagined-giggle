25676f4616a05e54b51a9d84496b3de40adaccc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:35 +0000

    fault slice start book get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
