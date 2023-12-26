153a0fae6bd26e948ef0565d8bddd621e43f5b6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:02 +0000

    roof man anywayanyhow behind modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
