a73d41e88dd9ba0612d78962b7498aa3d27a8171
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:13 +0000

    talkback crowded minimum complete her

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
