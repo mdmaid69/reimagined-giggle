1b202a9676588b4e2d1a8310791f7eab378dc716
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:36 +0000

    snow separate safety near available

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_min(numbers):
+        return min(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
