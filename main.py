dac510ba96b96980cc73070a7c17d1f07ec1febb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:03 +0000

    crash hello miss willing week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
