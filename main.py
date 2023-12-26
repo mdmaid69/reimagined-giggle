3e94b44e0a3fc9c11f363892ea1be72cd3eb84a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:32 +0000

    speech claim crowd wild ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_counter():
+        return collections.Counter()
