28d15dbb3e8350d839079c830b93ab03acfc3db4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:04:38 +0000

    nerve include count sport square

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def convert_to_hex(n):
+        return hex(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
