3f7da40ea7080725f24daf78573cf4dbf8235420
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:54:23 +0000

    contribute rent care weather funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
