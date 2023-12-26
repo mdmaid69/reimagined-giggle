320cccc421bb7ab6bd902683627af65a2f4ae311
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:51 +0000

    factory pleased rich learn relief

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
