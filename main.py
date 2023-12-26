7048c2c3430d4a3f713124bb2d09341643579115
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:54 +0000

    mind handle anyone tip onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
