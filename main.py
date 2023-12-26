71d27d4891740f1f3f195e19ab28a1e63f34c782
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:23:34 +0000

    simple period under blue loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
