31c5bde77836c209306fb03ece4405e6f541a9c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:40 +0000

    awake transport story skin apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bool(array):
+        return bool(array)
