6ef05978ab441a5d0ed61200866d2bd144f8346e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:44:57 +0000

    pass cross know past effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
