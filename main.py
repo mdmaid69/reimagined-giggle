14bdf67288f2a12214f3eafed03a2c9311c04f63
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:01 +0000

    normal prefer unfair dream picture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
