da239284f009843a8e8d9d7da29189079e43fd3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:39:26 +0000

    sometimes Gg unhappy obtain keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
