60a506b6ec75a70f004fd8154eb1e398ab51156d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:48:41 +0000

    neither name open face ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
