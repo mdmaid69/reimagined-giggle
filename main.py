f6e97272fb849b6d0f51cdda32ce2a1d20835afc
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:26 +0000

    point badly offer how forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import datetime
+def get_today_date():
+        return datetime.date.today()
