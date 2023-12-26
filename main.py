90c7ff76d1f4da32338484eb7a2dc26e360deb13
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:48 +0000

    confusing hand absolutely season real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
