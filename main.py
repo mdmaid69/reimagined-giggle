6b2788d76db41a811195974beffcce213fcc095c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:54 +0000

    reduction regularly bank Oo first

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
