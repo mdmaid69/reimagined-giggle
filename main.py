13bb1061f888f34bfc131e24e78e20da799a3785
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:31:20 +0000

    behave pause anxious bear flat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
