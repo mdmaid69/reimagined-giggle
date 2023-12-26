5eeb21c8fa9227a97758fc67c90336e0a8fd1f55
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:36 +0000

    crisis date neck old almost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
