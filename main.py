3f320dd5bbf98d4d5bd2e750f1047bed4bf88b40
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:56 +0000

    wind suggest image unpleasant firm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
