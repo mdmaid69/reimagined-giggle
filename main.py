843937a721d3d953a1931990824abfcd43e29d2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:42:18 +0000

    pile familiar turnon cross sight

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-for i in range(5):
-        print(i)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def greet(name):
+        print(f"Hello, {name}!")
