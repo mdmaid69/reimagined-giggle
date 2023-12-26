b8ac88fcf9cd0d689f62dbbbb021df84e56f1fc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:21:15 +0000

    part recovery appear fault middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
