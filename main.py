0d0ab49a57de6cba16645559948758d4344a6fc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:42 +0000

    any priority talkinto somebody entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
