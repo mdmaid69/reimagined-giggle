66c6b559a44b64fbb345e15e052467471de88599
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:55 +0000

    hurry system Vv comeback combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def get_array_as_set(array):
+        return set(array)
+def find_min(numbers):
+        return min(numbers)
