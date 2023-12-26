698874a46c69fb688b6ff04c7a7ac5d80fb1714d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:48 +0000

    OKorokay thorough into full land

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
