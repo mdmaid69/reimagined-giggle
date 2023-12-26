8bbe0c1014352ddc8d1c5a38ed6e96d7f435f1ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:40 +0000

    boring slip conclusion unite luck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def subtract_numbers(x, y):
+        return x - y
