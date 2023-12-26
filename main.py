8967720de94faed49fb486fb9e1ad0ba0c4656ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:44 +0000

    train claim seat break skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
