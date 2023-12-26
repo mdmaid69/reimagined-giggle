b62bf67c5b49df74fd7cb7baec5f18faf66b1401
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:21 +0000

    thinkof question improve false slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import collections
+def create_user_list():
+        return collections.UserList()
