0d9b52b10920a680727400522178767c88c0e2b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:40 +0000

    give tall modern scale godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
