6aa8dd9091e486be30c4c6085eaf72ecb2b0bda5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:57 +0000

    definite towards think cool question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
