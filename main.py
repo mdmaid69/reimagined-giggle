7552c4e53934c20b2dcb6d1037d3780df1150e13
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:25 +0000

    responsibility takeback pure bath guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
