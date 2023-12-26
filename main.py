13d4517ccea8866f3ac43bf355dc98f626a3c971
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:16 +0000

    completely hand stage statement estimate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_min(numbers):
+        return min(numbers)
