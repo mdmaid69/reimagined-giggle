eb3e0192a2b08b467f36073f93d4187ba499f05c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:50:07 +0000

    machine track and petrol manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def add_numbers(x, y):
+        return x + y
