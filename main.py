6d607eb89f7506bb7563bfe7d4f83086718380e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:12:32 +0000

    nowhere quick afternoon complete safety

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-print(sum(range(10)))
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
