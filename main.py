659e8b9c11892c6419c1c10a949fa2c784e3e1e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:32 +0000

    Tt love distance extra broken

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
