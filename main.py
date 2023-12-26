ae685c5d54b71c6273d651837a6a274dd5ae8bd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:52:12 +0000

    basic tread takeover greeting bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def reverse_array(array):
+        array.reverse()
