5c64f7d10fe97b7e2aeab33379cb6c6d8ad5e17b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:15 +0000

    town emotion gooff live squeeze

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
