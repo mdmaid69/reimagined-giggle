463023c93e3c3b0d7f08e6c55fbd6e20c61da533
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:39 +0000

    up rain lot top reach

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print(sum(range(10)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
