de42630a7dbac3322c2620c2a442c022a12ecf1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:37 +0000

    offer turn job measure payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
