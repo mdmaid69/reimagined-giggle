b852486eb38d6d999072dad98723e15b17decf4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:29:25 +0000

    follow run fork set talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
