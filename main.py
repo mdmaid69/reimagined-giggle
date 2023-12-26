979a34dcdbf5dc5f68bbfb90ef19bd488941d5c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:45 +0000

    quarter score satisfied calm silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def remove_duplicates(lst):
+        return list(set(lst))
