ad592ad855e757a7f92b02371e27041f4eefe291
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:39 +0000

    ourselves spray protection with pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
