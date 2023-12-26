056073c6d59be9bf9c7a23942959c25c242367d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:36 +0000

    low shut right tonight his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
