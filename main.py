fa9e90d38b3d4a84ff4fdfa76d182e0a24a8c204
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:27:06 +0000

    ground thing bringup children arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def delete_file(file_name):
+        os.remove(file_name)
