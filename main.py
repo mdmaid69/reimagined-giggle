abaff2a729b9ccf980bf105e18061b7b3091cac4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:34 +0000

    breakup cheque honest basic middle

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
