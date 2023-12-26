d4686e77bd67a434407cab412de76ba72973d316
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:38 +0000

    fall improve wipe instead exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
