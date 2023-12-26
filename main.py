2dd87c398a4afce0536af59f10bebd7642ad111f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:52:10 +0000

    method destroy equal I make

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def sort_numbers(numbers):
+        return sorted(numbers)
