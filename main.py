4312e4dc4341badc6399da20e8016006186f3391
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:26 +0000

    pain greeting trip doubt admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
