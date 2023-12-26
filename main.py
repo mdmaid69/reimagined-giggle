de19718eafdd372c2bdf43bd870972f03c970561
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:07 +0000

    choose Pp train instead talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
