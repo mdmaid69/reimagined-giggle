6027de32cad6922f8bc0ae71ae59e62a3c52e1f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:55 +0000

    crime bet address aware finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
