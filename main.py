859183e6d3d78161f5e2f06b32bb47996242e32e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:38 +0000

    declare tired joke emphasize badly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
