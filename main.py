f181eb7b8cfae6babf61b17b8f7eee848e39704f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:04 +0000

    accidental mine nose chief full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
