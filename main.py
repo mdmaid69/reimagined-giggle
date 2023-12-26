09a777b8dd3b6da8406d5233c9315d53d6c0d5f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:57:03 +0000

    pupil nor tape remove aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
