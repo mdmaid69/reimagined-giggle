d165406f191d70446652c82b8591dffd4fd098f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:09 +0000

    cross pipe control sudden mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
