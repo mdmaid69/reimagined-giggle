9177ec5179b237728b9927fd43a8c8bf428cc503
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:01 +0000

    pay after oil lunch vanish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
