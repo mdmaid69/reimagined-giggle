6136a11123826a6786326d22be0974766f95886e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:53:17 +0000

    previous worth pause float everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
