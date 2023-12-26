0899b7226be0b90793b9566006e3f8de3ff91c8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:37 +0000

    recommend body different save blue

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-import sys
-print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
