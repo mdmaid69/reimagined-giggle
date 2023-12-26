b5704483ede6a30f0eeb4b41943613ccd7b3629c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:09 +0000

    average demand count past lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
