589fd494efd07123e8c0d3348c89df17706e2fa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:27 +0000

    tooth delight prepare programme rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
