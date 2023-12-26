06d5c2cec9225d8ab4e8cd21abaff59b5a0edeb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:30 +0000

    size draw plate bin minority

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
