bf766a2c8ae24fff6d16b39b7df2d16e5f82e483
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:44 +0000

    like intend than sort bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
