64ad9486a367d52e13075dcc3378c4bb6cbbf6b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:24 +0000

    strike lookaround turnout improve wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def sort_list(lst):
-        return sorted(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
