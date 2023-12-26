859ef85848e3c098cef6c62deeb93f4c871a2308
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:03 +0000

    Hh coat Yy lookafter share

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
