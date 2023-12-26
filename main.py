fe2ea52d9fd6ae376e6c6cb519385bba951703b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:48 +0000

    rise woman soon alive available

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
