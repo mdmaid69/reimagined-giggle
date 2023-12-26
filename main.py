0b9bba59de1105ca174788bf4d49f7cc37df1d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:29 +0000

    clothes rich software Bb forever

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
