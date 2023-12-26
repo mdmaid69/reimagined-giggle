fbbfd8aacab8e56d6c22d6656190be15c0a84a7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:48 +0000

    try loud wide noise ordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
