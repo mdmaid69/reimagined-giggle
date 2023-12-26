d24a4a7b6b776aeb33975d2008009f6ac7b1da73
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:57 +0000

    waste polite trick positive oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
