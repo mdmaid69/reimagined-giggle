228659f6e296aea53e7147525d8f47ce9afcb949
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:43 +0000

    fat impressive guess anybody collection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
