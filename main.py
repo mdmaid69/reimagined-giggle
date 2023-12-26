1b7b8a41e392e6b810b6ebc4dad40d18a021c210
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:03 +0000

    impressed embarrassed safety rescue condition

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def cube_number(x):
-        return x**3
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def convert_to_binary(n):
+        return bin(n)
