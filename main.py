47c166d47fa0e866ef11e71efd3cb38c8e40a7f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:35 +0000

    quickly arrange hit move reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
