0a13cbc2fb0e5a96268c172a3b6186eea7f445b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:22:20 +0000

    side talkoutof hurt convenient rule

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
