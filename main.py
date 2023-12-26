09deeeba9d4364682a3441523580f07e25e3184d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:29 +0000

    break substance rush empty accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
