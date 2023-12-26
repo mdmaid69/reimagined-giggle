905344f6cc579cd0cb6de39f520c923c3ca9f160
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:43 +0000

    swing tradition blow instant injury

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
