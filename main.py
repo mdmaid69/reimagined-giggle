43a3aef7ca837d89f87588d2829b26ff9cf7a51d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:31 +0000

    stiff plan only experience sensitive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
