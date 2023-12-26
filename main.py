a9e8edc6eb82ee34bbc14874cf731192cba7d8d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:21 +0000

    entrance comfortable completely pretend increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
