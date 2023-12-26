594b1fc3e5629d1d1fd83c3e0050b9f05316afe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:41 +0000

    accidental movement leg road workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
