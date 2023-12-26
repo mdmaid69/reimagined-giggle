fb3b050253b38db968f02cb76994c7028103b3c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:04 +0000

    nearly person skill dress nose

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_list(array):
+        return list(array)
