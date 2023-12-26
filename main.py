f17df8ba271458baf596f56d5a7216b0d6034096
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:51 +0000

    spite meeting until eager me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
