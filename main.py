c0f0fbfcee9074acdbeb257dee17c7b4924e767d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:38 +0000

    corner cap family rent region

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
