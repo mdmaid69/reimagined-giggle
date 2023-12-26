4dabd9f2a4ba67225629ecd50e641ea8c6a65042
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:43 +0000

    fall recover break chat top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
