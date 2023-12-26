4e759fc76c2b08a522c77ba92ef0f5492d2530e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:31 +0000

    live fix talkback no our

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
