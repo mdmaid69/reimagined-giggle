e1db1417074c47de26e878eff1fc66085a7937ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:10 +0000

    makeinto home aim Dd allow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
