5107b19f2e916d2760cf8cfddc8886b5e2dc0464
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:18 +0000

    rule divide regional woman well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
