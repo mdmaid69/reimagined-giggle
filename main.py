c8e8df08aa753f259cf0291b6b7eaf53a4729971
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:07 +0000

    admire target represent any ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
