97809c29ed7784b7dcc23b0e62bd12d3ee6abb97
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:15:48 +0000

    once admire unfortunate mind activity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
