0b55e7e59e8d76764512aa4d8af9ce7e1ba0eded
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:38:28 +0000

    cool straight nose bet concern

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
