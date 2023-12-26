ad1c7174c59d39c67ef056845b5136241090d479
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:35 +0000

    party together fat should frighten

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
