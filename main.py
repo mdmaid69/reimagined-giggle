b7bb75c83acce8583939b43a580466adbe4f5af4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:56:59 +0000

    conclusion ordinary nothing base professional

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def square_number(x):
+        return x**2
+def find_max(numbers):
+        return max(numbers)
