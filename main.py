b9cb2ead8123a7808ef97c1cc697a79d33b31b0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:02 +0000

    public talkoutof quality steam attitude

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
