4286eede388d128fb2dcee0e37d55032c932faa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:29 +0000

    in type pleasure lie dig

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
