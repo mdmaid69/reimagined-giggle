383bd31da4dfe768b7eb074b699102626abf7a2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:29 +0000

    work style obtain frequent movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
