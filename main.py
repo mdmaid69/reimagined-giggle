be05e0e887ec14126e4da78df755519b3d5c5e3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:52 +0000

    keen bar then genuine cross

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
