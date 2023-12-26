51678d52fa1ab7f177f44c7f49b2c7aa8b2352e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:37 +0000

    vanish flavour minority sit emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
