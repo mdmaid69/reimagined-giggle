e89998b34d38f2fa9214b334d5f02c38fe54ccec
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:44 +0000

    children couple limit Nn sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
