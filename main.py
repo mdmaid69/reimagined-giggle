f9ac1e8cab212f961b94108b5dd0227eae67581b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:39 +0000

    comeoff to increase history tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
