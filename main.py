bda435b44c24cc46af8f7420c41e122548dd1062
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:12 +0000

    mine refuse rich immediate much

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
