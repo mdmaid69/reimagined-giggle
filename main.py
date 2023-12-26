719aa023f5a6faca6896c14ac2bc5735823953c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:49 +0000

    sad circumstance straight female tongue

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
