f0f8ad54b2b023c90288dba5b71fec629128caa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:04 +0000

    space commit keepup in speech

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
