33bee492d5bfc920d5eca6704378c438a0e5069b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:46:41 +0000

    travel listenfor birth error professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def count_elements(lst):
-        return len(lst)
+  def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
