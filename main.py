95d0ce9f7c74c9751e9e4c395409e4655dabe974
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:27 +0000

    thumb gate attempt separate give

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
