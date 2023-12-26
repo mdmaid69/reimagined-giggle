b3a3d845d57728926f3a1209ef92bd3adba48a7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:23 +0000

    567enemy convenient ago minimum force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
