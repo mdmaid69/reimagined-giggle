609b473426e416823c310b970a8ee96981ad86db
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:25 +0000

    yours fresh government apologizeapologise wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
