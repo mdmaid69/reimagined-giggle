f14beac87c96916031d4dacf33d9e4a9528ab432
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:35 +0000

    genuine replace anywhere his point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
