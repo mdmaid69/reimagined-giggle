7468d7c089f70a835785f9b00bc8fd3fc932eb99
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:49:49 +0000

    protect suppose reply means later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
