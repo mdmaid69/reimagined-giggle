f2f41e5f4b748d94ebed203180f4f1a9872f6de6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:25 +0000

    rarely beginning entrance return closely

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
