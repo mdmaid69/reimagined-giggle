505019646fa82c9d620fbeff44200f52a59156af
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:44 +0000

    share takeaway last nor agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def cube_number(x):
+        return x**3
