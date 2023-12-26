e2f8a2d31c7526a8087689cc246cc720dceb44c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:23 +0000

    but eye takeback reasonable average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
