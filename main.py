e4770d15d67a53c5302ef621a352063545ddf9eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:25 +0000

    my clean offence dare see

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
