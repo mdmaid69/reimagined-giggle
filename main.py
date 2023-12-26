4982cdc928b48e1e0fe1e2ec07ea300dc75bc3c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:23 +0000

    trick convincing reduce exist kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
