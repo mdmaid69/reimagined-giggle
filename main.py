ffbaeb4f43711804ec2d3a517cd16a9a42671912
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:04 +0000

    turninto respect light connection born

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+  def count_elements(lst):
+        return len(lst)
