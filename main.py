c6ab618c2fcacb7217a923bd584053769a39a809
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:14 +0000

    low close cry rid basically

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def convert_to_hex(n):
-        return hex(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
