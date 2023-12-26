0d567f07b6242e9090bfbf07acc1f2a486e66099
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:07 +0000

    quantity summer count bend whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
