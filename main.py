638945f8af1291a9686bcdfa949e9ca846291d0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:12 +0000

    even clear defeat immediately blow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def delete_file(file_name):
+        os.remove(file_name)
