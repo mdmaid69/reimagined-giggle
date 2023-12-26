1192e05cfd5d41d858b30d3d921bee5e29718fef
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:12:53 +0000

    goback(to) fun we summer 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
