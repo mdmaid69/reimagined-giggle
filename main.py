47fbd98ab9fc224f608e6baba7ccd31ebd297d2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:16 +0000

    hold progress production remark into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
