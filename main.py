aa1ae638aac90a21c980bb49fd013ffb59ba983d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:02 +0000

    thumb herself Ff few yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
