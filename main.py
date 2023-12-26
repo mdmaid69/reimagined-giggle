8c34c1cf3d3020261e70b03f273150cf2c73554f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:37 +0000

    interview unkind cry result wish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_rectangle(l, w):
+        return l * w
