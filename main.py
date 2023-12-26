f5b228c0154320c124f7bd9fccb5a976eebe9ce2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:49 +0000

    year father arrival hit raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import platform
+def get_python_version():
+        return platform.python_version()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
