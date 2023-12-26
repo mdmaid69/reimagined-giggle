a2cadfb3177254e3df32b0bf205f6b850b82334a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:35 +0000

    force visit born declare consult

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+print(json.dumps({"name": "John", "age": 30}))
