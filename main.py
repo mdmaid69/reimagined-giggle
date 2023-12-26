fcafe6dd202d5827540c188563fd92db67924902
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:54 +0000

    bend shrink consider workout already

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
