045e3913dc5eebdd6457d580642333d04a034ff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:12 +0000

    think afford shine border have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import platform
+def get_python_version():
+        return platform.python_version()
