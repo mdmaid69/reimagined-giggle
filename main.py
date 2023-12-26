ea9249b3aa931d176995b1a3441e36074124fa1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:09 +0000

    holiday final turnback change break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
