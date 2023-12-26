3c7fff50225b26d983f11bf65ab2e42ee8258fd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:07 +0000

    friendly bet yet cope holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
