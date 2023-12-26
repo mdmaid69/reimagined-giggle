3d8b80fd24e8d5a41d6b7cf7d541b8610bb92d38
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:51:05 +0000

    compete choice happen satisfactory once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
