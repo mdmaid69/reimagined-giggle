deed5d6d3622f7d824046c6d7c4755ce9b5b349c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:22:43 +0000

    slight close circle everywhere scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
