3d6b1613789356cf43e2f5fed5dc7353d7161c39
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:05 +0000

    cheat nerve pleasant performance long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
