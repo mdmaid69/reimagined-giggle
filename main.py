33fbfd9eb400bbb796e79f5efd5b33e5fe5fef50
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:04 +0000

    agree journey typical invitation makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
