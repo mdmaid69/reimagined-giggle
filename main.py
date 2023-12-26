67a8f44771ac88ceee0943be3b68fd20ab169b82
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:40 +0000

    last passenger afford label debt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def convert_to_hex(n):
+        return hex(n)
