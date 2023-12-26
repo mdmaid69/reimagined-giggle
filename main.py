bbb89762ddd34a930ed6281b97b3f3a67cdad862
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:55 +0000

    shock pot birthday near rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
