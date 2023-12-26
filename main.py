04a9a4566e6e7cb9fdff2e76f2c8fb3f9512fedb
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:17:04 +0000

    today call prepare unkind argument

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
