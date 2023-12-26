3c8c1006bc6d073aca46c618b5a19dd87ed3d272
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:21 +0000

    wonder frequent each excited miss

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
