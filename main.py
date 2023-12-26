fda2ec3ba843e9660e4454aa0f7af5903fe8644f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:04 +0000

    apply careful offer chat innocent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_stack():
-        return collections.deque()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import re
+print(re.match("h.*o", "hello world"))
