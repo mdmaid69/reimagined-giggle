024786c739ba0899b876f1be82a78f68d0c69fd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:08 +0000

    pleasant pay wrong skirt chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
