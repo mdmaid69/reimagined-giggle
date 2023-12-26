00657349f129dd744917d3245b41c9f351009376
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:45 +0000

    rain summer stage embarrassed stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
