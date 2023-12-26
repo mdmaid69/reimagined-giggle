e23b1f20f15b8b18bc6668056a15a7fe06ebb40f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:15 +0000

    pollution may ready appear autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
