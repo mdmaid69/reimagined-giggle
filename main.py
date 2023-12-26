d6879f47a465c96c70e20185f02fc53e0f200527
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:44 +0000

    sit trust hide shallow everybody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def multiply_numbers(x, y):
+        return x * y
