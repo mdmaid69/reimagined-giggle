d9fba7039f380f6088f4e5e3111cd6f9b055c982
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:40 +0000

    unless impressed drop different rest

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
