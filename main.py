cd70725c407efbe06ddcddac3f783d702780a2c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:54 +0000

    considering who date before otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_typecode(array):
-        return array.typecode
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def set_array_item(array, i, item):
+        array[i] = item
