fb2824ffd89d966b9282920761baac586fc349bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:06:11 +0000

    standard behaviour arrive them relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
