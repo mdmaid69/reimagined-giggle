1c03df3c5b2b6aa1487c5d2f54730c33d61ba3fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:42:41 +0000

    correct his hello harm gradually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
