cdabb226e6c4e942b3d2bbb3f9fadd364561159f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:49:30 +0000

    friendly free reaction tired nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
