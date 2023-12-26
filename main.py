eef105b699a69f0a73501e3cad75aec6763fcbf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:25 +0000

    consist needle air excuse exciting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
