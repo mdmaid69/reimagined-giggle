280e42523dcda883613c98c3e4a99e33b44ee142
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:48:11 +0000

    everyone mistake various recently heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
