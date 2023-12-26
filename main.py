1c4b9edde375167eb0d786789662d30db63cbeef
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:10 +0000

    takeback thanks concerning whole foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
