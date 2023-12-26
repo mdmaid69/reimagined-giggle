43e4a09f38dd9e00dc7640ba5c88fdc975415d73
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:02 +0000

    chief classroom opposite wide scene

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
