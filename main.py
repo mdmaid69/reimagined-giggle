bf3e44f310abc0a0047efdaf3963efb6caff2f92
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:44 +0000

    afford threaten getout serious find

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
