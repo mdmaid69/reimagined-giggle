e9f5dbaa16726c87ba0ddfd3e5432d7388b2b724
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:41 +0000

    even today night prize memory

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_stack():
+        return collections.deque()
