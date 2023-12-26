13031069071f9c52c1cec5cfb387be747607ce6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:14 +0000

    happy sleep myself team trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_user_list():
+        return collections.UserList()
