0db7e3fc8d095a44d42116ca5ce1cdb70ce6b538
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:40 +0000

    set only least again decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
