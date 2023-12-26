4fb11e1ab4d6340c4a44487587b106c0c1ab61f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:47 +0000

    until fasten because opportunity rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
