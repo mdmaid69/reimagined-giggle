c3cc06806d6c1cc94b967b339e3cbf16fe069de8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:29 +0000

    distant worse door brown confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
