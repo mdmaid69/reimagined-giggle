afcfaa62a71751beca2c2acfb11eb8900600b7b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:05 +0000

    raw learn takeoff usually like

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
