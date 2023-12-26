e1d30bfbd66c44ab65aac2b77461e62132a52c9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:48 +0000

    act cost ago better 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
