f54427a6562a2372535d9ef3d1a72caa5668d92e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:45 +0000

    takeaway meaning stone idea lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
