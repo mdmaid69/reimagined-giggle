445433e84b0576a2284e2f4092cf66661dc5513b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:22 +0000

    slow absolutely apply course season

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
