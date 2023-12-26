ab3c27a264244f0221eae5be94a9b89d6f8df399
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:53 +0000

    rest pin smart plastic complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
