caad2ddc8603fe0aa416397f8c89ce8b7457bdf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:34 +0000

    empty tonight borrow character film

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
