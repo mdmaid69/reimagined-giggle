561a67a3a0b6c2cd2a37ee30d447efddd75af208
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:58 +0000

    clean comeup radio strike equal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
