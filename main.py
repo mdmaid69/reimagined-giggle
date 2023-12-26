36c1f78e095af9fc8250848958081f95c6ba9fac
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:57:22 +0000

    runinto at money press speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
