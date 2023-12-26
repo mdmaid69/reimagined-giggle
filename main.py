03231eb03dc8055da875f4705301f5cef743aa3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:31 +0000

    under without each distant track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
