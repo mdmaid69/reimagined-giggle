9fdd98eb46e160103ea0d1e5f85a6d3d51842433
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:39 +0000

    perfect list over slip question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
