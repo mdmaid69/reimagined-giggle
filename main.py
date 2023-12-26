8e2eb1011010ca70a1357e8747203a2899a0cfa4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:06 +0000

    also patient deliberate record nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
