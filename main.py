59054d1ccb5ffd73649b8960e7c8040e4bbf74e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:08 +0000

    minor feature my need plenty

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_item(array, i):
+        return array[i]
+def is_even(n):
+        return n % 2 == 0
