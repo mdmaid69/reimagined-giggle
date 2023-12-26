2ae5e69eaea443aeb2e14bcf16039684b984aac3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:38 +0000

    skin forever introduction jump second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
