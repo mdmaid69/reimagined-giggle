04bf22641171baa1cb37b6521e4d10a37e8897a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:13 +0000

    hate shrink block generally bad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
