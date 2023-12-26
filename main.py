18112b9b0591c148982832be5e513f69e353f2fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:50 +0000

    voice cough during communicate guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
