14568e58c88fc7c73c37960eb58825ede33f5456
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:38 +0000

    by space dry below unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
