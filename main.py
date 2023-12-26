08122f912220540885dbda04976f6171d70b16c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:29 +0000

    heart attention together occasion congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
