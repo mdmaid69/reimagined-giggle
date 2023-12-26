283da4fa1f6ac55cca9c7221610e62106d85c7f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:11 +0000

    corner listen gap arrangement bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
