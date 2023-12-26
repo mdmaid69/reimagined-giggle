a1cf3fa03bc90809e4c09ed361d48e72f08bb7b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:46 +0000

    shop hurt team cinema ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
