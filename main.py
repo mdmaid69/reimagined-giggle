760b14ae9d335ca18d4b5e1f4f73384f6cef2041
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:55:14 +0000

    work tired pronunciation any lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
