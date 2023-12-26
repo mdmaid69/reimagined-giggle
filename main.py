abdb23164e44a6ffcdfa7290224edd5ed6d3a209
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:33:29 +0000

    Ff straight exercise along little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def cube_number(x):
+        return x**3
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
