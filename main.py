967708a6ef5a836d37813878a711bddedc5b8caa
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:06 +0000

    sister label discovery switch behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_area_circle(r):
+        return 3.14 * r**2
