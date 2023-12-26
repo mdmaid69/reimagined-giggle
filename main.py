f98c2db96e07ed8760fd627934b376132797bc9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:13 +0000

    prepare vanish dress before care

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
