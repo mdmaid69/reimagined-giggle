06f91a5178ee03784ffeb72d622758d4bc1f2c9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:32:21 +0000

    bread style influence live 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
