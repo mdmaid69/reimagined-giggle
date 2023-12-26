3375f6ecb0489404c0eed918ae1b9b0651854753
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:30 +0000

    forget positive cum scrap aware

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area_rectangle(l, w):
+        return l * w
