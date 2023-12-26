0260389bb0862997460f401d33352c9ee013cbb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:22 +0000

    thorough bet shock straight speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
