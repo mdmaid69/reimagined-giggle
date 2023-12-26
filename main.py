8966d8e12fbb8b90c518e0c43275dfcd8280d863
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:21 +0000

    second level match right whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
