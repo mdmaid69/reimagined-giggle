920a56889a28643905586fb3da1f6b6577ffefe5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:31 +0000

    violence certainly sweet confuse into

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
