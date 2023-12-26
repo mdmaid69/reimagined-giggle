82e25dc1155fc73ce6d44b392886ca2ebd2fd803
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:03 +0000

    callup drink there shall sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_str(array):
-        return str(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
