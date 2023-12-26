085752b61cfea8ecfb2ed73014c8a53edc8ffea4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:46 +0000

    there fine part bringdown Ff

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
