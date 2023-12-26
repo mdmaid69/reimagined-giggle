27053687d40e9b592218efb1081beb3db1846e39
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:54 +0000

    talkinto treat experiment from less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
