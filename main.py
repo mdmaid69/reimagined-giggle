d5b1c91c4513b944e46104041dc0a6a52cce8c6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:07 +0000

    relation sound shake precise plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
