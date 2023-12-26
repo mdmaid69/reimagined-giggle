ae06b47e070981ed23362e5677290281ff7cbbb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:47 +0000

    so nice famous time his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
