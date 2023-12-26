ee5cf96afd7279e2a2020cfea30f23c059532832
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:51:32 +0000

    muscle among rare collect crash

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-for i in range(10): print(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
