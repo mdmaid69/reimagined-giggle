c74f56d95013fbaa5e42ba588da0ffa3a0654b4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:42:41 +0000

    price hospital port pay equal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
