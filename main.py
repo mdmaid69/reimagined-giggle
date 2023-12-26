2d6a15d8f9c1a235e74244f085579eb13a75a0f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:13 +0000

    little detail shut education able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_set(array):
+        return set(array)
+  def convert_to_octal(n):
+        return oct(n)
