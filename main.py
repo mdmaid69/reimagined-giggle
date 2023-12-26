707c6c083b749394dcf144229814aaa16d029c53
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:36 +0000

    alone reply further plain sight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_odd(n):
+        return n % 2 != 0
