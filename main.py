9ae8a82a60985bc09cef3f3b66c1dd0a29b53812
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:09 +0000

    juice Bb Mm none frightened

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
