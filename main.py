5ca3ac383abf50a0b678f9b1e4788dae19e48e3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:54 +0000

    head float fetch die tell

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
