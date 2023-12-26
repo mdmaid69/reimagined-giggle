a643d79941dca9bac42a237d03b544b62a63b921
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:10 +0000

    once you combine poor price

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
