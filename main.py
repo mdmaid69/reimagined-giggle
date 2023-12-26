1da79927df5a9b53052256aa0d0721eb0cba92db
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:10:36 +0000

    even paper search old powerful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
