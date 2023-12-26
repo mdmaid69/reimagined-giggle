b3732278b9610968618bc13a972a43b9429ae9dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:26:40 +0000

    swallow trousers lend charge question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
