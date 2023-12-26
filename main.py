cf38574f84218509733fbdf753409b7919431f71
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:11 +0000

    together lunch disturbing standard doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_speed(distance, time):
+        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
