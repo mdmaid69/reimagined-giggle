2c9f8a774bafb820556b35be24c9cb5fdc806a57
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:51 +0000

    confuse after smile song already

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_float(array):
+        return float(array[0])
