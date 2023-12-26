812a0b9dc7bda85018cc65cb99009a7d301e0fb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:14 +0000

    pump moral distant fresh steady

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_even(n):
-        return n % 2 == 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
