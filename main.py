db51f87509d9351e34c6134f100d4674c0f6ff4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:08 +0000

    address window wet medium sign

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import getpass
+def get_username():
+        return getpass.getuser()
