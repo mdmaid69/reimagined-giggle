5b12ed23d75e814c18df43223299fbc35d42f26a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:22 +0000

    scrape course as drop official

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
