4cc194771f6b77214ace0b892031e3a430ad2f55
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:20:03 +0000

    morning weigh education season disturbing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
