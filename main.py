c813c855a491f77da0d576238ce3f589cb556cf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:14:07 +0000

    write introduction worry tend yes

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
