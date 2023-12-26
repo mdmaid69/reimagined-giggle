e8f626069bfdc3ea320e928cb243a53817fac8a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:06:55 +0000

    subject even treatment dry poison

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
