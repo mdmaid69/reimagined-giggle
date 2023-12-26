b21997c3bab50c2a5cbbe2368e9dcf90cde755fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:54 +0000

    certain room move sea experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
