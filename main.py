a172bd121d60ff45a7cbde198958da2fae6662ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:21 +0000

    before comeup muscle treat occasional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
