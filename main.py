0cca0b88226b9b83ef87ed9ade798fb9193e1f20
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:48:27 +0000

    intend criticize round instant react

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_power(work, time):
+        return work / time
