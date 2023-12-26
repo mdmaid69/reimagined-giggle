01be33b4d2131e235221fdf63c3499b90461085c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:21 +0000

    trouble street injury reserve workupto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_max(lst):
+        return max(lst)
