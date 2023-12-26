548145ce87cf88027ef7e7a91bd6a5f47731f5b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:34 +0000

    chin sweep far exchange second

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
