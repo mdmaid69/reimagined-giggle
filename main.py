21e8899b0d3020e0009b69e9d9629a1d3f4537ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:52 +0000

    upset comment confuse food substance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
