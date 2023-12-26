1ffaacc7d8cf9765208ec93ac90eaac6b52d98fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:29 +0000

    shop spare worse minimum full

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
