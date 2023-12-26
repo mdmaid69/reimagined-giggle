a43cc528a853af91a4fc7f78ee651f8f0b94ab4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:03:10 +0000

    more violent quality ball spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
