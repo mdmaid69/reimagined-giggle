eb4e5e42ba2c8b8398aa1aaf896431a15dd0bdfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:14 +0000

    fly underneath tonight hold mark

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
