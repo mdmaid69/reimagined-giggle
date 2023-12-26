ee6c711102757a4f61e0d0a32266106db53c53fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:58 +0000

    last ready right rubbish me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
