3ae7afc92badda60abb6ff4c633e788b7d4b4e54
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:42 +0000

    regional measure gentle fix declare

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
