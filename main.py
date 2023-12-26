45715e83db482504799853149edaac683a207b09
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:06:11 +0000

    finish face glass artificial protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
