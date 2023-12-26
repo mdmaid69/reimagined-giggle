47ff0f2133f577eeb6eaf8dc2496c6d10d4c036c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:59 +0000

    thumb concentration blame yes interview

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
