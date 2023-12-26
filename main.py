611fbebd8b97226cdd49ed38f44c88ee6b4d2803
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:01 +0000

    task exercise fond second doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_list(lst):
-        return sorted(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
