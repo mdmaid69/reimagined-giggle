04e1c88bc82d77acc1a7df90c1813e403a280834
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:13 +0000

    during wall stop row train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
