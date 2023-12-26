97d3f860c851ededeaadb9f41b6ca45cd8ff66e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:24 +0000

    force prevent pupil instrument claim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
