235a30f0bad143f5a1bc472333e62b309a021650
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:00 +0000

    really throughout relation material gather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
