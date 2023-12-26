ef72b1dad9035476c362eb3dbb6689c45ed1f84c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:04 +0000

    supply strict boil name discovery

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
