174be529d1b701ca0ba1bd7674add46ccf1b6460
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:43:23 +0000

    negative concerned inside repair strike

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
