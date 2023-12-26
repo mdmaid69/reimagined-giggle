bcd59d9582594c844808e6cd22c9238f1355901a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:25 +0000

    disagree laugh shallow spend tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
