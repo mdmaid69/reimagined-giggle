00f7e449afad822c99a6c8530cfc2cc02e71a5b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:30 +0000

    buy match feeling tap concerning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
