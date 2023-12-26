15f9d9f81fce449ba07913ca0618ec53099a39f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:57 +0000

    generally judge murder practise funny

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
