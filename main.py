62c47bb08b60e1aa70cdd19ffd5e795f3635e141
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:41 +0000

    band tall OKorokay dance react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
