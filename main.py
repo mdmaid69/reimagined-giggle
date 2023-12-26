953cb4056cfb9c74817eb06f71bf7c66758d9c25
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:51 +0000

    handle time best lead anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
