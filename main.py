a6d34400dc360fc0befb808b97a955ec37430e02
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:22 +0000

    waste narrow course demand light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_counter():
+        return collections.Counter()
