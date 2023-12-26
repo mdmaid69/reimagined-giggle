0f1a05322549de4fa583f9f6593565a7c603333a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:48 +0000

    fashion smell division our myself

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  def subtract_numbers(x, y):
-        return x - y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
