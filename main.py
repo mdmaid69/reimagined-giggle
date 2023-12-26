68e68dc823928a6314521ab6365ee39878b7597e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:44 +0000

    apart safety imagine file fierce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
