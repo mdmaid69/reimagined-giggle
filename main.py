c70bb02825b37af95ab95bda282aa29139868c66
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:20 +0000

    turn life lay match mend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
