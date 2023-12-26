dddedab5d1e1849d74a465c77174163cde71a42a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:35 +0000

    blow tool talk port side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
