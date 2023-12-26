acd81108dacd3834b277ed872c045c7f2e6e0771
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:27 +0000

    dream interest circle behave substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
