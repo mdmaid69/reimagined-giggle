d975763d79a60cd035f1865edda09724c58de8ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:14 +0000

    used reject later holdup piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def remove_duplicates(lst):
+        return list(set(lst))
