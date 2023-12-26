77fe9613271df5573bbb945c31663b8710bf8ce5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:23 +0000

    one offer however different somewhere

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
