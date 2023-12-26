4ef3ea59e4c7546ee7c1cb58d525d28322b95b58
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:24 +0000

    runout criticize appeal keepaway another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
