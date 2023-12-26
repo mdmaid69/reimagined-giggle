d8dbfd26d309555fed27f9b0351ef048f7126732
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:33:55 +0000

    try excited difference pattern plain

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
