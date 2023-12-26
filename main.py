8d5c8b4e25904ebc99f43ba9abecf36e2ed9695b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:48 +0000

    never breakthrough tend culture part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def reverse_list(lst):
+        return lst[::-1]
