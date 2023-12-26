c329899d6fc9881a27cab437c5573ffc216f63f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:17 +0000

    operate relation about sleep conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
