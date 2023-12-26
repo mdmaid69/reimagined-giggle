f3af31f4b81b4a3f26e8306e612edc6e2acb447f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:55 +0000

    control project rare less apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
