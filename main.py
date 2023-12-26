293cae8f76a781592acf85b820a1f4330080ac60
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:24 +0000

    worst command wherever thinkthrough like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
