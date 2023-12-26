5684c50537f42598fdd928fdc4107a3ecae9c9b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:52 +0000

    near stay bear wall petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
