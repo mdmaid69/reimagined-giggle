c09d2fb39141bd1d8e90da07cf23b7d9f0697fc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:42 +0000

    woman length cruelty nurse lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
