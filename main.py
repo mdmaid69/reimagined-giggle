c436ecd52cea65ed46fa666b09613bdd845aa9c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:42 +0000

    speech order soft path trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
