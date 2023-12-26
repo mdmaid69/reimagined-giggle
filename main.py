3cd76a9d0c88243220d7977612df3dfb821a8e60
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:22 +0000

    lookback(on) comedown attractive yourself instrument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
