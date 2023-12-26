b6e493a0c97baaa4499d2b61e73c370fa8d4feed
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:50 +0000

    wild interest convenient large table

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def convert_to_hex(n):
+        return hex(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
