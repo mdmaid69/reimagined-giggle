fc157d17597e6f00124cad4d5acdb53f5f17d17b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:21 +0000

    apply snow health imagination effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
