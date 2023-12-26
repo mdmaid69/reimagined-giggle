4131bb4b0613bddf8b66cd274fc2835f029860b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:43:22 +0000

    score even represent dark boring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-print(sum(range(10)))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
