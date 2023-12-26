d6c7968b3a723b6389e3a95bdb660e72fae1554a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:24 +0000

    thing hers many contact game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
