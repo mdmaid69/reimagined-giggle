25f07c105f511357e8944212fd2f116609d03065
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:28 +0000

    Ss intelligent waste comeback complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
