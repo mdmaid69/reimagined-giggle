1c076d8ffe6391f15a3e976253356369a86cb3a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:17 +0000

    whistle refer emphasis sell class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
