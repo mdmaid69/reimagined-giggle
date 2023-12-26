23d7f7fae566e27854e6c61753b88690fb04bb66
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:10 +0000

    quiet emotion punish pause possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
