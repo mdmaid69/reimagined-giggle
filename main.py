a7b720c1d93795122a4c0bfef9ad00ed1dbb1567
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:50 +0000

    valuable hit move shall raise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_pressure(force, area):
+        return force / area
