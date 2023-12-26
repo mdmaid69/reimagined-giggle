61bb100456f78f46ecf580df95da7f150ff05383
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:11 +0000

    plate wherever experiment prevent part

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def find_max(numbers):
-        return max(numbers)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
