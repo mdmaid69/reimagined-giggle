ce48d776b60d931780ec473ab863b8d27031172a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:10:51 +0000

    nowhere free problem finish charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
