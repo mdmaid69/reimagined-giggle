1ca5877edb60ac3ca1b464608b68302dd211b17a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:38:53 +0000

    close ready top drive clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
