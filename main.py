3fdaa62f1ee84b1cc80a1d1e857a9e39d511d949
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:26:23 +0000

    steep along exist material single

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
