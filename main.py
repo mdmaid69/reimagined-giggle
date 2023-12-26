a443e00eed48be9bf7800a7698fedf2348834525
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:20:50 +0000

    lonely female feed organization gas

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+print([x**2 for x in range(10)])
