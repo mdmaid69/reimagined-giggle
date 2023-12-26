55ddb78f5ce5cffa34e71733b4f1a1c9586be8e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:21 +0000

    board school right attention discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
