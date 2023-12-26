4a9e1be83f8367098589e3f74ebc8082265e2815
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:45 +0000

    union Uu matter record worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
