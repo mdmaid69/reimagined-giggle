59c04267652aee226757ef6e9e07eb5eaeaffb03
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:51:04 +0000

    them often calmly nearly lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import numpy as np
+print(np.array([1, 2, 3]))
