936a4b941944f8aa156b6c9c4f48cadad888aae5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:45 +0000

    turn rain official separate finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
