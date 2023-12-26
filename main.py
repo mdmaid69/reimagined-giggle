484ef202f1e6fcfaa52b3da9ecda472aaa8ecc62
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:16 +0000

    fact comeoff hard reject some

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def add_numbers(a, b):
+        return a + b
