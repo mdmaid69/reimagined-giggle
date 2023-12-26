29967862a3458e487f0f470b4e03bf1124f8ee23
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:09 +0000

    tool extra opportunity visit bringback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
