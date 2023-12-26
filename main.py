e2747b637032782cc5fdb0c94510d28385b6e84d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:26:33 +0000

    programme embarrassing than pleased cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
