ac55d2bf7647d4c38d2b31a33634010f91efe26c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:35 +0000

    pattern possession accidentally because somehow

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
