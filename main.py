f28a39d7409546bf678bf9d435462beb48a34a88
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:37 +0000

    prepare him sign one nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
