ae11f60a9b7563e71efd8db12a11c2ddd7556c1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:01 +0000

    remember full switch successful be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_repr(array):
+        return repr(array)
