559a08a712f12157178a0062d57419bed3f833b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:35 +0000

    money turnup listen sight centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
