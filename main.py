effaf985763b9a4516fbcbc6029cdfc9dbab18c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:38 +0000

    occasional wave alive train cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
