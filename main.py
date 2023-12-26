5faf275f73bd05f44ad6eca7cf5be831ff74640c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:16 +0000

    majority under clean store Ee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_length(array):
+        return len(array)
