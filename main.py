846316909fe48bf71f33623f7a391f556bddfe56
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:16 +0000

    real else long react who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
