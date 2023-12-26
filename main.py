b400d6bb6217714bd2192b09b8f2091d8918b9f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:40 +0000

    badly person physical emphasis strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
