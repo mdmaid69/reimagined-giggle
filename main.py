c7fc5f894c11aa9a9bf730d52adbbca2bf437d8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:25 +0000

    immediately opposite convenient embarrassing voice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_max(numbers):
+        return max(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
