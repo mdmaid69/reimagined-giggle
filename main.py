49925ea0d1b86505b46a6f10ca2c68ea60c2e06a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:21:05 +0000

    moral habit over best pattern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
