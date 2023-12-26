af577b4d7865466c2bbb16d5af68a48d3a15fdd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:38 +0000

    base he anywhere themselves new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
