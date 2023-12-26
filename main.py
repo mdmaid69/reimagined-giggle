409564d6a96ab971e944063e45d246f551049fdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:35 +0000

    condition through fence journey regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_set(array):
-        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def convert_to_binary(n):
+        return bin(n)
