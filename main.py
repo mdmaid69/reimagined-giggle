4933a26ef60d83d207cf644243e4389c9b68cd49
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:22 +0000

    neck ask post pleasant anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
