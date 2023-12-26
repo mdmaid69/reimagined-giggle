fea4b084e34db5acae7c2db0d2372146a1fdf4ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:05 +0000

    lookup class blame considering alive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
