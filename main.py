f9a2d59f500d86756a9bf525928c61b190c8bfe5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:42:00 +0000

    dress drink react feeling but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
