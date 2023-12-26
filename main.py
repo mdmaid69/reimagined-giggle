9a3167f0db1c1d3a08d365438dd7aee137e716e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:28 +0000

    neither naturally trust worth love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
