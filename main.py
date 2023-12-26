8d231d055959dbedf26db4b247ca6f24857ae0a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:54 +0000

    house tap run week search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
