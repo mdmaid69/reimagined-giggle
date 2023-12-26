cb846708fc79d42b30cccd0471049cc87d46b878
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:09 +0000

    command clean look knowledge replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
