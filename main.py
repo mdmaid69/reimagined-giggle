1519928c8ee2c8573ad00938b02be9f6320c2bd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:43 +0000

    patient yours survive government dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import logging
+def log_message(message):
+        logging.info(message)
