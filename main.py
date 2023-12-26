165fdd6a661195a2f721ad4795ccaee8bc4a9aa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:41 +0000

    check amount float suggestion objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def remove_from_array(array, item):
+        array.remove(item)
