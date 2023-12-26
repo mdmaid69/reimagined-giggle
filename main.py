64e60669b3ecfc4dffe16d0b8cd691f5f14dccb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:33 +0000

    off priority belief division rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_min(numbers):
+        return min(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
