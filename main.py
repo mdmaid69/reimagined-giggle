fc2d9c98d989c7e61694a2306ddfd40b0195330c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:24 +0000

    usual crisis disagree recognize land

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
