c54bd96c072bb850a21d451ee31e9a4890f99103
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:45 +0000

    bound ask suggestion storm dinner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
