f20816d7ef65303abf0d6a535a9a05f6745f3fa5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:47 +0000

    adjective breakup constantly appear battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
