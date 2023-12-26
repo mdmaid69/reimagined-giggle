d14e5db0ab25b583d0d8ff13c5c9435f797c5e56
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:04 +0000

    scared store not hollow shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
