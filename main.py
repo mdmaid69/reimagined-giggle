60ae53fa95729d5860a1cc8b2b89c860ee1f562e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:42 +0000

    convenient copy wander promise influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
