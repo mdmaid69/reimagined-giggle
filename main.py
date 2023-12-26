696741f1900e1c5ebfecd4076a5eb797443fa634
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:22 +0000

    now straight might conduct exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
