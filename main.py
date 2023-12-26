ade2d9a1792a9027dd400e62d4ac1ea00b1e1b6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:32 +0000

    cost mixture lesson better proud

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
