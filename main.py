16244b28727fba38510f77ea764ab0d6e3b31bdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:12 +0000

    flavour spin same human your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
