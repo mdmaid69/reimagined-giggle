3c496eafed8f0f9f6575ba20ee7a64c9324b2637
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:36 +0000

    nervous attract connection question desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-for i in range(5):
-        print(i)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
