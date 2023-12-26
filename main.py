25cd815e21fdc333908836dcfbd0efa8309e51d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:49 +0000

    market check squeeze thinkof declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
