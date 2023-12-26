184cb07ca52237bf27fa5a27de7e7a97240da56b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:31:33 +0000

    quickly listenfor wrap emphasis left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
