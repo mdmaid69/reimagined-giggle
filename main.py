59ee6e9d65c1ab55c69fed672116f45ff90a982e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:38 +0000

    previous difficult no thought perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
