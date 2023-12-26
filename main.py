c49aefd6e81cbc83a6b549e9bab0c67d8171e50b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:26:17 +0000

    release all spend Gg hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
