1a58cc5c32c894d988feaa2cb7fb0a0704ea2870
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:08 +0000

    style class apparently plant question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_float(array):
+        return float(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
