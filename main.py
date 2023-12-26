e405e9784575eb1fbb2c370fbca85fd49fd9603a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:24 +0000

    scale strict beat fetch shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
