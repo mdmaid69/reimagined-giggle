e60c9179e58725ad28bd18f9faabbda41bf96e54
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:29:33 +0000

    schedule second 568energy strange of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_work(force, distance):
+        return force * distance
+import os
+print(os.getcwd())
