0aa2009029a83dbdc134b8e306cd394b8ca5a9e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:12 +0000

    whichever ring ground pleasant temperature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_list():
+        return collections.UserList()
