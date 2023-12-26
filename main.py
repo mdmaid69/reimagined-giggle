30e69c1085e973ccbc3741c13160652554ec13e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:27 +0000

    value worth charge pause problem

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
