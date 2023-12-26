da1f424b7db48a2c61d3259569e510e1589a08ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:36 +0000

    onto opinion notice mind able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
