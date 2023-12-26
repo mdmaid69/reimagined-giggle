44a461a79be8309a0b011b62558f7cb54c80a67e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:56:24 +0000

    trouble tongue boil ability nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
