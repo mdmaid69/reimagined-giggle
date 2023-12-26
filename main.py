a4f2995fd4e35e231ac4f06af83ccff46d47aa49
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:33 +0000

    we runinto variety basic hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
