a0ffdf3c7ce36b7d34ff9d3c313f4f4d5a773a64
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:23 +0000

    drop cut everyone seat sure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
