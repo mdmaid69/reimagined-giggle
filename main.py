a536b88b6e74cb8500925b113b949b23379cb837
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:47:02 +0000

    nature waste Cc slight sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
