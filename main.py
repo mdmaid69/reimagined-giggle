9ee8c620fc7095f662cf6027741251fe74900bf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:58 +0000

    send software themselves exercise brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
