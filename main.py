1d2435774b3946d1a8741323f40119bf96cd1ea7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:10 +0000

    suddenly soon write charge degree

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
