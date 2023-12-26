473bf9e4a6e3cee3e4b332685cb1294000dbdd76
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:04 +0000

    future hurt absolutely call except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
