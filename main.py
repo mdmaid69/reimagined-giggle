435267b23202543d2d087b9713fd5cfb107fce0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:08 +0000

    sail punishment task load likely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
