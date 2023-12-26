3693ac0df3e3752a0487a5f749f3a07d1a7161a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:08 +0000

    road prize prefer deep means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
