12e59d7626851e386e0609ec437a4bb63aa84d50
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:12 +0000

    guess charge begin ordinary combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
