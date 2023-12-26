af53f631797632ba7c7d7261a5e88ec097591057
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:38 +0000

    failure cold dare space illness

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
