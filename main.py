6aa05b465e4474993490e328c54802b997c25428
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:32 +0000

    desperate bang course share embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import tensorflow as tf
+print(tf.__version__)
