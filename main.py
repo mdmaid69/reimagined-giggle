d3aa17e60b8acd5ced34c7041c903fb0fde97523
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:18 +0000

    attractive bar rest answer leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
