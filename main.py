56719326229eefc33a4ace2fe5c764350f472898
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:36:37 +0000

    name himself workupto office thank

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(5):
+        print(i)
