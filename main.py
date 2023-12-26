d626e8e63d8438808ed3fb95c8b7ec3e0f7f2751
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:39:39 +0000

    remember formal strange disgusting neither

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
