a1b6f57ddf90978d21455ddc2cd5a7af1c7f5d5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:27 +0000

    shoot set other guilty stamp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_time_since_epoch():
-        return time.time()
+import sys
+print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
