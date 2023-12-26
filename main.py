f928fd574af0d32cdd8ea9a40194341f44c317fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:09 +0000

    throughout woman correct frighten get

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
