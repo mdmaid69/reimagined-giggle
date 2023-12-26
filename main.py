e8bb05838fbc714c7199ead0bf3a4690657a613f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:29:42 +0000

    exercise minimum lookafter narrow classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
