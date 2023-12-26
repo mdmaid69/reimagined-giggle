c880e23428f202f9f2e602d190a5940be49177d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:15 +0000

    handle whenever waste second smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
