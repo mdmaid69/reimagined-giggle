09645a04cade4b9dc753461ff0f5bc2e37e9419f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:04:10 +0000

    turnover increase Yy mess theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
