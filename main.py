5d1294862c0ae89a294814dbfc41fcecb20b82e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:24 +0000

    proof sign suddenly fill season

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
