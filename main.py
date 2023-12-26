c6e92b3f0ab67bd00a8145aa0144dd406f3078c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:34 +0000

    Ss board passenger thinkof us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
