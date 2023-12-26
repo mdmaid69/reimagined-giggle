63ee2f02e72caef7bda365c9defac5a046ab7557
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:10 +0000

    card doctor secret risk frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
