39e5106c7f19274de7afaceee72b8dd5dc1ac499
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:43 +0000

    negative recent shut charge medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import re
+print(re.match("h.*o", "hello world"))
