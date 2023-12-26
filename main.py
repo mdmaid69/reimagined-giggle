20ad508fa5a200b7ea003a965612725337247a2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:27 +0000

    nerve plenty break rain point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import time
-def get_current_time():
-        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
