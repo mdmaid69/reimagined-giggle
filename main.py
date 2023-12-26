3ac4e54685daa86beb42a96c01d0bac1d9f98758
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:48 +0000

    card car congratulate breakin breakinto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-name = "Python"
-print("Hello,", name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def print_python_version():
+        print(sys.version)
