bac1f3f58a469397c64d1e0ba6c07369771900b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:53 +0000

    keepup female keepout pretend often

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_max(numbers):
+        return max(numbers)
