2e148a9cd4a2bc74457d3c0fa4eb236f3498797f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:54 +0000

    tradition away recovery length below

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
