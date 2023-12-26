23723f28df3fa97f71d4496f45fcaa74bb6d32a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:27 +0000

    chat check train several desperate

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-print(sum(range(10)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
