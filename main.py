0b7b8e08d3aefb73e00c69b47bf395c4a1bd7c39
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:09 +0000

    wrong kind thanks hope temperature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
