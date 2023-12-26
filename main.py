a91f4883fdc6d558365daaa47af2e447b826e60f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:36:52 +0000

    ticket pin loose team sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
