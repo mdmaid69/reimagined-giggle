dd9b1bb88c6867c26f24a0f2aaff61babbfcd97b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:17 +0000

    announce respect trouble future dare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
