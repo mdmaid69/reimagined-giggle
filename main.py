8c826d0a13ef7e291de43c5023aeface7489255a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:58:16 +0000

    workon belief big load land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
