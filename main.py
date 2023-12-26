5b2c81152b147ce5bb692d5787ff8538996b2b36
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:39:10 +0000

    lookback(on) air treat question process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
