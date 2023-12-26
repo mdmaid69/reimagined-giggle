0dd125bf0fa88c82b23bb166f439ed1914430713
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:16 +0000

    better used community contact boring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
