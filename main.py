ca573c74c053e9d136207cc925235b15e5fa9d39
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:27 +0000

    weekend culture keepfrom future issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def convert_to_hex(n):
+        return hex(n)
