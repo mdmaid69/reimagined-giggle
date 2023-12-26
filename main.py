4527fedb1df9851fd260de9fd402a1b8a541ffbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:46 +0000

    tough Itself persuade main fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
