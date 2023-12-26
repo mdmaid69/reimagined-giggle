f76ed15d4e378ac81a50bc1054d2b06c5da7102b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:24:42 +0000

    Ss since long pair runinto

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-def divide_numbers(x, y):
-        return x / y
+def is_odd(n):
+        return n % 2 != 0
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
