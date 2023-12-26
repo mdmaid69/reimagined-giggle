fb3eedafbbb6a8792abe1100ce85922106ad7e8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:01 +0000

    burn card spin influence shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
