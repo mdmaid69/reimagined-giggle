c9dfb443a394c4bfff117a01f678f2e0d39b376e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:32:15 +0000

    hour anyone considering hollow giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def subtract_numbers(x, y):
+        return x - y
