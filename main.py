3e367d250c6fb02bc095b7139b3f642400cd36a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:33 +0000

    as sick treatment discovery uncomfortable

--- main.py
+++ main.py
@@ -2,5 +2,6 @@
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
