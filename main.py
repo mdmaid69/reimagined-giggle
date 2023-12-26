f6d493fab8a1c94059340e2b4ff478a43fd16ffc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:51 +0000

    variety dry length unpleasant whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
