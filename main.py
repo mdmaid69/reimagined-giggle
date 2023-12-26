801845101cc4ccee7ae2a14d603d87812e84602d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:32 +0000

    praise manner report care dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
