03c9473c0831736edcb9d330cb7d7dc459c36338
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:49:12 +0000

    light trust comeout now finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
