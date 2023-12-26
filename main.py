cad4593719442c7e279cbcdd686edf6d83cb6fb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:20:10 +0000

    compete less land know squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
