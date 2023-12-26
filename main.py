be62698d6b8bb17c07f1f84e96768ca75b5e8975
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:40 +0000

    mine early teach sticky dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_min(numbers):
+        return min(numbers)
