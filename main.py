a7de1ccb6116f6d9b971fcb56bc189926a5dbcf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:21 +0000

    book bang bright glass breath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def find_min(lst):
+        return min(lst)
