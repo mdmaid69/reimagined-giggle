085da426d6e5d9e34a427c1ed85bcdb119a7711c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:00 +0000

    past danger decide minority anxious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
