16ee9420c48a1bc90b4cf2863c5e8b9fc3471727
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:55:07 +0000

    lift anxious chat none real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
