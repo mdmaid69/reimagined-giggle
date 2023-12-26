79075d23b4f5fbabdb6ed2f241a2a4e90d332f95
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:54 +0000

    song hard immediate paper beautiful

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_octal(n):
+        return oct(n)
+for i in range(5):
+        print(i)
