9e7c18c098be5742821958cdf3ddd82e4e2c00bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:34 +0000

    common regular grade none offence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
