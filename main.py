2e80d613686adf67372760b489a9d2711086c28c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:43:40 +0000

    past lean fresh miss achieve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
