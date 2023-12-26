996e7b17af15e61eb76e1d46de8fe208a3e37637
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:19 +0000

    noone describe coat shame maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
