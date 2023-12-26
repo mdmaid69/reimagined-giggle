8d3dc2de0a6dc93912cf487b6ab65a6611a866f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:30 +0000

    breakup friend finish worry turndown

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
