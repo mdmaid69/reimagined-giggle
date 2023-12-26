162fe54289eeef0aab87a9f2bfefa5fe492314b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:17:13 +0000

    copy his now thorough base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
