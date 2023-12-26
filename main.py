6490c3c3fb5eb1148edb7e3a35e7e72b94aea639
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:02 +0000

    accidentally concentrate find swimming plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
