39691896df3b5d0c236420ab7cb589584343a2be
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:32:57 +0000

    short confusing fairly while image

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
