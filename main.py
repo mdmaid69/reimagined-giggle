61d13af2fe454b13a8d7922b6a17a384114626c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:50:45 +0000

    turn press recovery front rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
