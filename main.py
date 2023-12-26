5021be1777afbf0f12f44f30b0714f6b1e2e7a67
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:17 +0000

    fond relation exam steam voice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
