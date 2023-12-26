487d1358b52a0e6356633a363fc4dcbfd964a128
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:42:56 +0000

    petrol car radio chat spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
