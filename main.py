9bd5a19f552be1e0e094829d50004cfcce830a53
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:22 +0000

    sink mistake heavy movement mass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-print("Hello, world!")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
