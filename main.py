708a32c59c5f54043e2301fb45a2752a054133c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:14:04 +0000

    machine Rr his attitude front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
