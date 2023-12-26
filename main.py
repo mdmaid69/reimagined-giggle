c5c4349d55e30d39fb3d03b4508880b708b3a2a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:59 +0000

    arrive organization pinch fasten bar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import time
+def get_current_time():
+        return time.time()
