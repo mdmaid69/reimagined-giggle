168e6d62cfa6c226238360202bbda6feb80db235
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:37 +0000

    desperate suppose exam chat born

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_min(numbers):
-        return min(numbers)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
