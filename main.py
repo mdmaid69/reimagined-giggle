267174d0b060c52bdd9b5c422c7fb3822206f60b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:32 +0000

    share respect right careful speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
