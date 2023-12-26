481a0ea431d033a430186422a07115cb2a4d26c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:24 +0000

    sharp left declare power sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
