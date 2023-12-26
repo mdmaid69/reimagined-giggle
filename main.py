ec89ca1f64f7d97985f3b666c2a9a764c53e4bf4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:19 +0000

    shame judge should splash responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
