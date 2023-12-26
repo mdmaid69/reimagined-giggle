3a2705327320148fd68a191c156956906077f91a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:29 +0000

    runover hold answer journey difference

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
