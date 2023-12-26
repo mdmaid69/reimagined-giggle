991dcbfc45e11a601259f7ffae4ac95b6fbd8051
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:18 +0000

    announce towards clever pause expensive

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
