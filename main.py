c3afefc112082fef80a0461b4066caf63b447074
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:06:33 +0000

    hang worse while stop drop

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_numbers(numbers):
+        return sorted(numbers)
