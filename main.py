73c2b2ad8f8069facd366f8768de0cd7787a3e63
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:05 +0000

    chin tool murder shout successful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
