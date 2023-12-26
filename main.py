a4af44387d12cf549daac57e1364db4aff767310
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:33 +0000

    dick she accept shallow protest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
