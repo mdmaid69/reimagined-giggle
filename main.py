008cca8d775d3093e71b6911a3f06275fa648f17
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:56 +0000

    cry operate force fond goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import time
+print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
