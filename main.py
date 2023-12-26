c3d429e838512f46dbf328d846aa0335026573ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:42 +0000

    bush satisfactory food flavour or

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
