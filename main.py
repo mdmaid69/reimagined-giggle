c6cad238cf70e3fc21d7a2e625cdf91b526e4dd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:35 +0000

    rain report walk sleep report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
