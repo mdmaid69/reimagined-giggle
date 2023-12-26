95dba8e13066eb8f0d87e190e1f945f7c1d0bc07
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:19 +0000

    temperature plastic show exploration concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
