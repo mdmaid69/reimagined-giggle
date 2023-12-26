c79c8a9497a9c787d82a16edeebe812c2df94bcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:13 +0000

    possibility car related scare bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
