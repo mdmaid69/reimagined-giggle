e65d0ea2a2399ec457b2504b08631ed3f0db2932
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:33:35 +0000

    themselves achieve bang mention government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
