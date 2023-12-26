9e112d6ed3239ea3f34c2859e2ec04a2a41cf0a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:59:47 +0000

    mind stiff without callback makeinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
