7ee6c6a418249458fa8c56d95b1333a8089ea6e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:55 +0000

    mistake laugh company handle bad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
