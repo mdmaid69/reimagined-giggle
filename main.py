80423a24f1655cbba7b9d93bff76ec43f2392751
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:28 +0000

    character cover trade needle turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
