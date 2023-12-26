ecf7a75b903ac725d9f6206e15cca724b6c7e4d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:36 +0000

    foreign fact callout Ll attractive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def clear_array(array):
-        array *= 0
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
