caba5ff82c67dcfc07171b9d81e8b654ca49ece7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:15 +0000

    about error miss poor fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
