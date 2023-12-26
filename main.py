49c37d7e7b06ab718c2614770ee966243ad85c9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:46 +0000

    window close careless kind scream

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def reverse_string(s):
+        return s[::-1]
