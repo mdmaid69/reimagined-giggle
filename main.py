00b63efde3fe59e39d74a97966de3f38d1b9c5e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:48 +0000

    trust conclusion test score definite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
