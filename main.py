bd43b8006440e218d181e7046ff3ab020682c54e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:21:14 +0000

    behind run impression size scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def count_elements(lst):
+        return len(lst)
