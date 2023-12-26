a01ac1676eef6580dfd46d7664e18acf35d2c346
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:28:13 +0000

    rain my try print cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_error_function(x):
+        return math.erf(x)
