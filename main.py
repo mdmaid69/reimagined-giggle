984b066ff6e99373229f4cbbb4d0157eb01c3551
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:14 +0000

    angry earth mess past slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
