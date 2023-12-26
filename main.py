fcadeaf5078f07c3ccffc1384f1a57b13c6b54ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:13 +0000

    reduction little region getback up

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_pressure(force, area):
+        return force / area
