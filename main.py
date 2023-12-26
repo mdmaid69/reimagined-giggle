cb481ee103117c2ef9eaf9e1e8591cb12dc8aa45
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:54 +0000

    record stage none dangerous stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
