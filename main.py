4bfc600d07769e22c1e861451c9b0fdea17a0844
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:29 +0000

    thank pause spill sometimes bite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
