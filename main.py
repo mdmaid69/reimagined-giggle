a1984db37953450482304b1f1746197e35c539f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:44 +0000

    offer combination he love cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
