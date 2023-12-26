98f939529efe8c9d023c57bb253bcd4c3f59279d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:33 +0000

    train empty quarter explanation speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
