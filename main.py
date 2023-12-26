d1a864a41f01f2aaef656834fe59a09b0471b939
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:02 +0000

    understand star charge shake stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_array_as_memoryview(array):
+        return memoryview(array)
