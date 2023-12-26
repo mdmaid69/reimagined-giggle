6c6d8988eecf3ca75039e37335ad4b982408f733
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:38 +0000

    Kk shut touch mysterious error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
