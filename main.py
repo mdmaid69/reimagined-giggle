a06c2570d1ecb3e5e6378cff1f37f554205b4cc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:27 +0000

    bar before priority heavy afraid

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def count_words(sentence):
+        return len(sentence.split())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
