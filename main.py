fce013af0691bdf3da30975c09577ea08fcf53b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:27 +0000

    brave breakinto obtain however figure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
