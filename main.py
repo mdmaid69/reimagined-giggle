c3f8c6976f60bfbfc60dd61f9e87990a39247120
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:25 +0000

    safe include bottom hot fun

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def count_elements(lst):
-        return len(lst)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
