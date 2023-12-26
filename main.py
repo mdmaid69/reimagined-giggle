6cc6dae859823d82ef0a75646f390170f4e87f95
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:20 +0000

    get stretch flow upset now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
