3a8e0ec2183d55cf65f7ce8d3e44c854715fa223
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:26 +0000

    father watch dress fork before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
