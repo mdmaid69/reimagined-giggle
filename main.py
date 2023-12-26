ee0ac5f5b65b78c72b50cdcffd8838e4009674c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:13 +0000

    568energy crowd broken pen Ff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
