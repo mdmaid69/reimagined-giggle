6ff1e77f4ed4c9dee9ffc1e6b6558f32cd46a10c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:49 +0000

    carry lie sight book flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
