1a5dd8461692ff3958a9a9cd9dc73502d0935184
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:14 +0000

    reject coat scream crowd somehow

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_cosine(x):
+        return math.cos(x)
