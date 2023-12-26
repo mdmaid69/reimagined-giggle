588b86ac859425489a6957b2b7ed1b0f3fd043cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:20:50 +0000

    sink cool distance error impatient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
