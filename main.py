1a4ab34280a00b3038b54b644dddbaa488d02d08
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:15 +0000

    sweet flat prevent precise getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
