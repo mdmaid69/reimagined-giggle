1be5519b14ca6a52ecebe571deb7b39a9d3ee7a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:50 +0000

    he amount around move unhappy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
