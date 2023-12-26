12e36a50c41e73c43d79959ae238a1191a508012
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:41 +0000

    quit patience blow trip street

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_acceleration(speed, time):
+        return speed / time
