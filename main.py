aa971511a2bd27ab6a83cfa55eba87c88ab31390
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:53 +0000

    fact flight busy Uu spare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
