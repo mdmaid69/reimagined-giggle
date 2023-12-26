1fb203687b8f8f6d0e21e3901f6e93e22ae0f8ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:44 +0000

    bet different mind shake steer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-print([x**2 for x in range(10)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
