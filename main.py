f27ab019d62e6925176fc4df4a2bd69b797fbf05
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:26 +0000

    emphasis persuade hurt under pleasure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_work(force, distance):
+        return force * distance
