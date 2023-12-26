4ce6575abd457572ca2e5114cd33e5003a7bb1d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:16:21 +0000

    yet imagination life shout school

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
