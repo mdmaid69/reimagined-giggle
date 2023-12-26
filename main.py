75789018477140be9855b939321398ece789c873
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:03:56 +0000

    strike success face whole question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
