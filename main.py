c05fab2d17a5b2f2f05f9166cd0d1606489deb73
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:06:17 +0000

    clean advice fix offer then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def exit_program():
+        sys.exit()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
