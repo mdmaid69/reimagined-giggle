45d16ddc93bca307e24cac70b1a911ee200d6fdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:22 +0000

    commit main first winter tone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
