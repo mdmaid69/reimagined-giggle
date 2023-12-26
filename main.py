5119152863b9623d2beecbfb693d75504ecf78c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:56 +0000

    couple hurt round obvious plan

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-for i in range(5):
-        print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
