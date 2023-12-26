b0ea2b75ae34a439b4fc72da9ceb14cb3a58cfd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:17 +0000

    stare clear physical eat violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
