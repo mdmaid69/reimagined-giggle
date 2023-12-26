42eff29c1273968d025c7f6625d5f071a8798811
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:02 +0000

    project hardly foreign laugh goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
