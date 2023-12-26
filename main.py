71b188c73bcae53d1d1b3d773ee5e468da4a1594
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:26 +0000

    defeat speed gooff practise score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
