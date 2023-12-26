f3d1e6a40a659ff83217439c27cf03f74ae55c1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:33 +0000

    sky wish classroom pick over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   def remove_duplicates(lst):
         return list(set(lst))
-import array
-def get_array_as_int(array):
-        return int(array[0])
