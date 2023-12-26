e6b4a7482d8588a6cdee10a5466899f51111cbba
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:01:24 +0000

    convincing embarrassed short game short

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
