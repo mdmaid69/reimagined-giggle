c240057563fceb9bf53c336f32265aa9ed00eb8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:06 +0000

    edge beginning sign neither mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
