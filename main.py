95518aff0c353720fc6c7e885029c6da2a185039
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:23 +0000

    bright makeup regard terrible under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
