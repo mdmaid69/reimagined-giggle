eb9a53ae91320c9bc135c5a76c5400e2de07f457
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:28:49 +0000

    satisfied pronunciation doubt vote guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
