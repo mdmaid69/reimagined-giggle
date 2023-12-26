c6d87d972dc59fd313e0a55e52b4fc727b99b1a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:44 +0000

    emphasis tap slice fit lose

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
