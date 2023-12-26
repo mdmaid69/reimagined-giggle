61affd62076e1a95c404feb22df040f4ab54ce73
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:30 +0000

    draw turndown test influence immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_octal(n):
+        return oct(n)
