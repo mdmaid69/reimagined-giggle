a5e67cf7cdc9d167486025074a9e5ac26746c0d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:37 +0000

    there subject narrow suppose last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def convert_to_hex(n):
+        return hex(n)
