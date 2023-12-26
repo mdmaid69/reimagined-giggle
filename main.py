7710f318fc28622ae9a8bd452267e072d0350eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:41 +0000

    speed organization goup beautiful dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
