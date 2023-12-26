bcc0ac9e97996a47a801f5b2e1fa835ee020d4ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:17 +0000

    force method one comment fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
