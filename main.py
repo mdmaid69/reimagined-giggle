0ad2ef2310f5b1836081e9625451dda454e3fcf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:10:34 +0000

    trouble breakinto degree chat by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
