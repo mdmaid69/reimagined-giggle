a2bd2b8eaeb4746d18b579b00bf90cd5b1cee8fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:58 +0000

    Rr responsibility division contrast minimum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def add_numbers(x, y):
+        return x + y
