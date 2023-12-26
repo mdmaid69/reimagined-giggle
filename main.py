c78ced9e6563810d678d33613d7b049b589ec99d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:04 +0000

    contain kill secret willing means

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
