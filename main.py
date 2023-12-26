1beb1b62736c5ada1356b3198bf4227cb32139be
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:22 +0000

    rub pool equal equipment dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
