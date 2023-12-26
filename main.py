58248df09847efd391ad38355143f5c23974c6c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:18:29 +0000

    official under workupto nonsense cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
