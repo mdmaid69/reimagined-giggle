575fbb458bb5e217bab404c67b2fc84adfe8f154
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:55 +0000

    temper criticism team street level

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_typecode(array):
+        return array.typecode
