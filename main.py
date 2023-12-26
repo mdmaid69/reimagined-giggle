832940ef92fecf057f69a7bf5fd043d07fa4833f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:30 +0000

    waste violent perfect outside pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
