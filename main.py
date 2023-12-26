130d40dfdba0aca1a17b7d492a05fd867d420288
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:37 +0000

    keep practical majority friendly view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
