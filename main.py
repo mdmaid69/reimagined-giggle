c8d75d6400b9690663a62dbe6c7fcbb45c09e1e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:38:17 +0000

    though smart comment play plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
