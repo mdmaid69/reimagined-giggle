89940b087aa172bf6aefec0250db7024f1f0747d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:30 +0000

    class brave somebody see broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
