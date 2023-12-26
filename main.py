26b47061a811e43034b967548f3a6e96e1fe9ca7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:26 +0000

    project never instead simple escape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
