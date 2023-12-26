8ad561c9628a3a7ce404fc441bc05baa1f8442d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:31:05 +0000

    may list opportunity plan television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
