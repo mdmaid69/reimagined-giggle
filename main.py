45cd972f7b3b4974a481412372fad7db2b23c46d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:23 +0000

    569enjoy than either decide bottom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
