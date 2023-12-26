ea3ae2afdcda7aa5b4677a94851910ff9db5b1b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:52:02 +0000

    evening principle congratulate accept tidy

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print("Hello, world!")
+def find_max(lst):
+        return max(lst)
