303140fd61e057928da18760bfc1514c601af920
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:06 +0000

    feel getaway bin trick wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sys
+  def get_python_version():
+        return sys.version
