04ddbafe99efff20859dc83c94e1cd27e4aef6da
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:29 +0000

    calmly not lookup butter connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
