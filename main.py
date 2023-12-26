547760c019da28b3be08dd0706c0c811d4977929
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:36 +0000

    brain obvious stop depend condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_pressure(force, area):
+        return force / area
