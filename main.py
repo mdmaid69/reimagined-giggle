328d43886999e883c53a8d761abd4a63f1e889b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:03 +0000

    offer marry innocent vanish giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
