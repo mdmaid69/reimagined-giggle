ebca076b66e748219007bcb895c980b4312ae1ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:34 +0000

    emotion issue total suck talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
