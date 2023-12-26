595e0f2b3518fc110412b06cc1197ad835482b16
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:59 +0000

    broken invite 564encourage uncomfortable Nn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def generate_random_sample(population, k):
+        return random.sample(population, k)
