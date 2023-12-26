06add21906f7f217d1799f729716446f74e4b312
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:37:13 +0000

    complicated accordingto star high plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
