e0dbcebdd502dbfc9ce506efdab1840e8aa4ab8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:49 +0000

    cinema strength communication deliberately system

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
