54e656e4e6d3c72c3ed77322c4470ae7c545919d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:16 +0000

    whichever few mention sore swim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
