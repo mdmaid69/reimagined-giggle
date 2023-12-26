e9ada40a16256660515a6ec936c3fbcc1fea61a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:58 +0000

    wrong noise interest Zz side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
