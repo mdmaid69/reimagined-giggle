d0e8532632ab0f0c0280ff4181d4e964b1a6be1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:37 +0000

    boring set describe sign maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
