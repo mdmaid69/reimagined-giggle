d4673d42833b6eb3dae1c7d891cbcea3c56ba1d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:20 +0000

    understand 568energy reason niggaacademy round

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
