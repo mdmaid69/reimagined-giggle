e3a92c5d5039716ffde63fd80c885f5018e57827
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:46 +0000

    stone because extremely tidy petrol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
