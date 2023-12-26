cb83648098695506218268a2f1188792e8300781
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:29 +0000

    bath fact earth hello space

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
