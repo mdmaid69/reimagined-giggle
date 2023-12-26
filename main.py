dbbd311bb55ba4e9796bda98fff16ccf4588e3ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:35 +0000

    turnback amount embarrassed stop prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_speed(distance, time):
+        return distance / time
