3e39330bfc497c6b55b5759a1c836b3d617d1232
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:21 +0000

    bank laugh gradually phone pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
