02c6391353d317b738da0ecd08872e5ee3ef91bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:12 +0000

    creature say point trust trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
