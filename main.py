e6951040d868d9bdc8abd0da923819db7b457f8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:20 +0000

    loose seriously fly line later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
