079efdf3a5ade29b2518fc51416194b1c3762b9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:25 +0000

    door promise balance lonely sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+name = "Python"
+print("Hello,", name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
