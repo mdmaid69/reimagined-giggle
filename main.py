704756c0ad3403e006941fe7fa2136b8b35ad053
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:08:54 +0000

    advice centimetre study by into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
