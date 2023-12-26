14250c88c64e9ec089607c117f634ddc7c174ad2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:15 +0000

    impossible attractive paper defend complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
