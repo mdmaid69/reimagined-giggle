6139ecc1c2bc2d76996e36346ca7ed86123beca9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:19 +0000

    turnback start calloff afterwards stretch

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
