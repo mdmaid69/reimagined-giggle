066f8399341447663501315715c48aacc3487f59
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:22 +0000

    suit deal square tax listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
