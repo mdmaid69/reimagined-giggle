97e0b8ed7d3b09b52910b99dd25674bf6b91a2f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:17 +0000

    knowledge shock object eat drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
