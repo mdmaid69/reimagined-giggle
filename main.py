a398ad76b179c1dd080f75cbeb56e7a56b13efaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:42 +0000

    fly week practice separate speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
