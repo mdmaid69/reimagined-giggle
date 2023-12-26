de0e8662bcfe929ba39f0306d01c30e2b4602515
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:41 +0000

    introduce bridge dull school poison

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
