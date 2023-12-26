0ec7e04d3fc489457833cafc0483ceb2f781dbe6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:16:23 +0000

    disagree feature interest hole film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
