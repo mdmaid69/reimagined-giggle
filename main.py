21eb3fd199e8600d690f2f38e0d743d9bd197426
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:40 +0000

    problem busy strong thankyou considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def cube_number(x):
+        return x**3
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
