3d3e75f7a71d907f65d03ef7bbe92e33cd2bf3db
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:50 +0000

    give past embarrassed clever lock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def square_number(x):
+        return x**2
