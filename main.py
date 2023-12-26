82e62c4be21cd8bd064fd0148dfc42797672cae4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:29 +0000

    bill Ll influence upset Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
