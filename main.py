c5169bcbea56c8ac38f5fa1b28a5fde883a30c98
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:22 +0000

    total count commit giveaway opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
