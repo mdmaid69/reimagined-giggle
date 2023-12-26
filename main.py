09cc6a3ead237458298a690a80f8da9d4e7280d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:20 +0000

    build down slow downstairs Oo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import datetime
+def get_today_date():
+        return datetime.date.today()
