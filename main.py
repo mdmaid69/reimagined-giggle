e3693a0fd5edb34da2eee3a2f39dd83f4a5bbc22
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:18 +0000

    combination television practice truth summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
