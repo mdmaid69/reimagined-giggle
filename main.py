df91a2b626ca486a6424252dd1e8b6f3ba130adf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:58 +0000

    square prepare scream point partner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
+import re
+print(re.match("h.*o", "hello world"))
