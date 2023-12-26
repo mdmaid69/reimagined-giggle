c167d69490b03b12bac6c0a727894fcaca8b806d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:25 +0000

    full detail loose takeback interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_power(work, time):
+        return work / time
