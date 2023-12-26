86b11746f623dea423a34e4a9ef7dc55d1d4cd90
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:15:32 +0000

    disturb attempt injury score spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
