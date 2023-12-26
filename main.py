92bf26eec62ca15fe29f38e198001b8e7ed7740c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:41 +0000

    fierce string sea memory unfair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
