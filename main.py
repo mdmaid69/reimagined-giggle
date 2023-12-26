4ef5df2c5605cfbf53eb43a77bcd5fb4ce9404ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:22 +0000

    opposite row cruel harm proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
