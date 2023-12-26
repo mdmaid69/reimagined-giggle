1c54dc27bb7abfc81d306e006eac2f5eab5c8033
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:49 +0000

    treat sail centre under check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
