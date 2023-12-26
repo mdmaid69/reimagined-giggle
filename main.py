c258a6c2d01d2f2632352c591edb62131ffe5715
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:52 +0000

    run television quality score memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
