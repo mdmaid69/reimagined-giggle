81bb4d59f83b64bcba41602d5e83ad0fe0f334af
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:32 +0000

    arrangement purpose quiet history grateful

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
