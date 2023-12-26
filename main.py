4a57c423699a2ab0083367f7cf66cf8711c53eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:42 +0000

    fail unfair proud against mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
