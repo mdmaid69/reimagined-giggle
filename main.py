bf9fe91bb59b183b5bfaf29ba9035043b2d72ed1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:13 +0000

    habit 566end successful plate duty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
