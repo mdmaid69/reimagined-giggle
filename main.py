434f45c10392e5dcd062cb1830e38ce068fe456c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:18 +0000

    foot snow confusing extra load

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
