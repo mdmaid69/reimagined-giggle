77bfb75f96e7756ed0ddbd73e5aac88e4acbb4f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:55:34 +0000

    closely conversation comment school bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def reverse_array(array):
+        array.reverse()
