1ecc725792aaf8dfc70910cd7560b6c02c02f824
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:35 +0000

    detail excuse star Ll burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
