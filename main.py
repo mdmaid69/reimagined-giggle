0aa6ab13e25b3c0d9eef05c22bc4a4830dfa1ebc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:52 +0000

    uniform cover unlucky style very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
