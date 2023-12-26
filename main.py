73fa1f399e008d770b84e25ae103e51635cd6c12
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:43 +0000

    uncertain much blow middle exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
