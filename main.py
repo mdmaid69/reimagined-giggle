59db89871b7caaba8529265f69fc65c7181ac1b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:47 +0000

    angry street sensitive consult several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
