b582be8488d952b74e69be81ae455c4cfc43f9f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:36 +0000

    rank system experience later space

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
