a20402f07dff1d390edc523503d6d0f0300898c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:24 +0000

    later underneath afraid bother cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
