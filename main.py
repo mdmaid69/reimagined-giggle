b611d27fd155aad7d59a71505559c21c5ca4e448
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:34:02 +0000

    translate fast there hear medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def multiply_numbers(x, y):
+        return x * y
