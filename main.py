4d34f7ad46e721367323486539ddc16a5baa5f44
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:49 +0000

    trap balance quit comefrom manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_power(work, time):
+        return work / time
