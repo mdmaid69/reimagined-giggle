317c05fd630362fbdb8011150b20dc325825972e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:28:59 +0000

    follow float complete neither drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
