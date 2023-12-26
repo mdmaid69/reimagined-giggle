46acac34de77662d3fd0c4942a6ef7e9061d9c08
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:33 +0000

    rent lookback(on) wire point tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def extend_array(array, iterable):
+        array.extend(iterable)
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
