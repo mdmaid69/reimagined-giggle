ba45a8c1a0914777077ab3b97a373651d0b3d38c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:24 +0000

    deal apologizeapologise male creature remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
