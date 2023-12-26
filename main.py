8839f9649ebe60e9821cd242613d5cc85b1a1f40
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:40 +0000

    drop word enthusiastic communication happy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
