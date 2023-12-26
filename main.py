393753f716a3f92fc935822a52ef476778150942
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:05 +0000

    sharp fortunately bottom confidence direct

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import time
-def get_current_time():
-        return time.ctime()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
