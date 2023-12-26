a355090bf89ec3af797eb1769b7d8f6bb45b0bfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:40 +0000

    generous unusual dream cold beyond

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
