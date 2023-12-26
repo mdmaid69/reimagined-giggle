127dda3d7d7ba8838f73f805126921dcabeefda5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:33:08 +0000

    trade work proof cinema pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
