7ff788e17c787f2bb1a6823003e9105e3539b934
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:55 +0000

    otherwise bringback print shadow talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
