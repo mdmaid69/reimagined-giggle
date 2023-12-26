fd3889620334488980d39069ed8feddbc4f230b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:11 +0000

    alone fit maintain unlikely arrange

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def append_to_array(array, item):
-        array.append(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
