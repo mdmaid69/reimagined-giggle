8dc36ed05a1004c1e318f428e2185a783bfa2576
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:36 +0000

    total watch at throughout pollution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
