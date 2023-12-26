d3a14a9ac1d0d46897370d9aeb2e2f8a39c9b420
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:17 +0000

    workon substance trade friend city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
