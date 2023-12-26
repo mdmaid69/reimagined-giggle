3abf7cbaa302401fd7b645b94f6ebf096e4d8887
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:04 +0000

    amount roll turnback as method

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
