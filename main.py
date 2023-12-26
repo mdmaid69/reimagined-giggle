7104c36240a7df85580658b7848d3ab16851319e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:36:16 +0000

    powerful after fetch clear 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
