36e0ab265da02b12c5788583283733c018edef61
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:13:50 +0000

    block mend shop wipe pronounce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
