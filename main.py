6e1ac61c76acfc600eb013202c67406fb9d5790e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:15 +0000

    mistake laugh shame fear course

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
