d923c503c31d9956893e1c8661cc53eb414e0015
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:51 +0000

    store name curious joke count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
