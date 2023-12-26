5c9dc23950911600bbdafe517be5ef6a28275ec9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:12 +0000

    scream with rough speech skin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item(array, i):
+        return array[i]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
