f4a858319764f8dafd35a1c7cb107a1e5258ad5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:57 +0000

    public stare accommodation during bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
