3a6dadd56ff7f5dd2a6110b712f25ae6c207af99
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:24 +0000

    charge move whistle plain again

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
