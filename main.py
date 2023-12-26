6bb10465188e3b2205b26ebd59eea7d622e25848
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:18:40 +0000

    quantity row somehow wall beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
