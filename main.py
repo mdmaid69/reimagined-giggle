46082e665a30ea5ef6902813dbb51ef963fb3aa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:39 +0000

    lesson wild smooth threaten address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_set(array):
+        return set(array)
