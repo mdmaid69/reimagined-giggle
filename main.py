2b33585cc47951dc3d50e78d00c8329a9aee4f7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:29:03 +0000

    wing watch apart fresh mention

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def add_numbers(x, y):
+        return x + y
