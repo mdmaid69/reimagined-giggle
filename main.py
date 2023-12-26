d04467cc3daccaf4893e9fbe150b9fd986a60eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:16 +0000

    burn pass that enthusiastic after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
