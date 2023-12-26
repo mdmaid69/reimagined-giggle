a3bb1df4a325e9ef63cbeea9c3bab06086dfda8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:13 +0000

    time curious land rare smoke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
