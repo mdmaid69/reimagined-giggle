5647682b2aea1925c9a0dc97a671ed0a7f4850f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:44 +0000

    respect set snow task turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
