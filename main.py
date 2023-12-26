edf63bca8075461148ebc903b28557e97727fd12
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:30 +0000

    hers giveout telephone put mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
