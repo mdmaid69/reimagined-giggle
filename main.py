d91b4f1b3ebe19675755457fd3303cf612ae77e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:59 +0000

    tip kick ground stiff nature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
