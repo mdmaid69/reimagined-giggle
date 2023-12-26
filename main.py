4734cf180996222a34df43caf6cdf4bf82f3b94d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:32 +0000

    fence final ability blue phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
