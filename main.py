c13ee60dd801648f47b79783c95d323c4734ec56
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:07 +0000

    cut Dd smile drink accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
