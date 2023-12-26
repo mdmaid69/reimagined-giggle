5c17818e154e9cdc4ed338f7e95d52c52442040b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:30 +0000

    print ordinary answer maybe relation

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import time
-def get_current_time():
-        return time.ctime()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
