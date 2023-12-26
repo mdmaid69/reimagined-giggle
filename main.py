660e67789a6ef9a8ebdf7b810fb34772f73cfc48
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:06 +0000

    holiday vision across violence breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def add_numbers(a, b):
+        return a + b
