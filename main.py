0e0c5a20bbc3b88afb4bbb655a08ac096be7f759
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:57:16 +0000

    before after Bb miss terrible

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def add_numbers(a, b):
+        return a + b
