ca72e8e5d7c685e37d87626e21c49ac9cb2b1268
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:24 +0000

    shopping plan clear person breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
