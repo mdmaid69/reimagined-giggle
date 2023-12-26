d9da037150d2a58f195188b8bb8052ec315756c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:46 +0000

    possible kill pass grow cause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_to_binary(n):
+        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
