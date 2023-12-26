9ca4a28400247847af8f9ca071c264b99660bf57
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:19 +0000

    loan male turnout exam sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
