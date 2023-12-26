57c420eec686ffba0d0566b62f4be938598b1657
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:55 +0000

    by drop value upwards your

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_acceleration(speed, time):
-        return speed / time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
