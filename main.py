54b82efd6c41b002a241e423f0a8a8077ac8d609
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:03 +0000

    medium forward square friendly talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
