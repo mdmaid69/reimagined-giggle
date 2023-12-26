bc8320d7668653d8a5bb92259c136c61b8378130
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:12 +0000

    murder ordinary drop sweep rare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
