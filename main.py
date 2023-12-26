4a6c7ea3a433dd974e9b6e5a920a23243c484842
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:22 +0000

    indeed with Ll try muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
