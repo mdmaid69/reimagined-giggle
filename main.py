693ba9f5c17c27451fc9ff325454a079143d922e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:44 +0000

    whichever rub piece exactly still

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
