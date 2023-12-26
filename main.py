ec8def2bc6dbb4d8f8c4de3bfb57669e745226b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:44 +0000

    turnon tired Bb behind fix

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
