001c779deb86916527fa410e7636f9df427e67e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:03 +0000

    that absolutely Tt somewhere luck

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_words(sentence):
+        return len(sentence.split())
+def convert_to_hex(n):
+        return hex(n)
