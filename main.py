8779170fcc580557ba8e862f87906d5fa76dd523
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:47 +0000

    apply offend low possession beat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_octal(n):
+        return oct(n)
