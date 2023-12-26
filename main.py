672477ca09c3100ce5e678dc2b9be78b56279948
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:04:04 +0000

    considering foot pain about centre

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
