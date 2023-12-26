af7eb696f4fd57ad01d30d0312bf2c46657f6bf4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:37 +0000

    side wake include street eye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
