bcbbf628a68cb1196a5ccbc2f9a189ac3949fc92
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:24 +0000

    injury practical spot big fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
