15afabe14420a4993cda99e613f5e865f3edd1ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:34:40 +0000

    after opposite turnaway slice law

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
