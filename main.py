5e12a971af8e688ef2b44c7396ea082114928dac
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:54 +0000

    workupto place list field cap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
