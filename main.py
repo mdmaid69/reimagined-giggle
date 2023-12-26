8b9f9a29d34b7850a66eeab8bd097e16b9dde804
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:07 +0000

    spare unusual geton stand steer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
