261a72986e9d76bfd03bbc1039048fe8a12781f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:37 +0000

    equal about increase do talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def is_palindrome(s):
+        return s == s[::-1]
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_square_root(x):
+        return math.sqrt(x)
