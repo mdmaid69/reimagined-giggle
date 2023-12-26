7e89e3188d6624608fc1a783fd38992b580813e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:14 +0000

    by unpleasant expensive brave right

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_elements(lst):
+        return len(lst)
+print("Hello, world!")
