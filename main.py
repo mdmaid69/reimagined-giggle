3f44705d8f50c170be25a19d30dab51a023b5d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:18 +0000

    takeover sleep view confidence tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
