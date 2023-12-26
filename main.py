ac17a703d96d43c32de2f3f9e98c0dbd1c21e57f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:45 +0000

    passenger prefer harm empty common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
