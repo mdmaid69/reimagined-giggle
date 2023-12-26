ce8427d4f20ae103aff20b60c714cc85eb367dfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:18 +0000

    bill several problem steal married

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
