c1929c925ca53ac0726c21d3728df99ad4395a28
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:22:08 +0000

    back black class than bag

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_length(array):
-        return len(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
