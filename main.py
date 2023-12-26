d25ce4bbaa6523415f2d3761ffaa7b9f9c4abe3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:00 +0000

    welcome sail eat proof shock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import time
-print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
