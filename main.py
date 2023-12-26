61596dce59308c27bf5d6483ad1ecc9c44805a83
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:35 +0000

    hardly computer work top good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def print_python_version():
+        return sys.version
