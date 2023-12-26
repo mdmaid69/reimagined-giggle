ca901eba21aaf7373cf6277dfc8ff5c0a2cbdcf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:27 +0000

    phone fear not war between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
