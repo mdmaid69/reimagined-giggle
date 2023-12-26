1c857fb5f075e736bc92f279c0301b9141ca4ebc
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:29 +0000

    second separate before out to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_acceleration(speed, time):
+        return speed / time
