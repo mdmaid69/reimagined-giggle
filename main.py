876df86dcd0f7aaedc14a53ed0491cffc13a0ff7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:14:52 +0000

    pretty print through cry business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
