46234d117bd96c1bb676d7362837af864419c6c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:00 +0000

    ignore stupid fact course tune

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def count_elements(lst):
+        return len(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
