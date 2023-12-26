a7483d7efe4e22973b288dcff779a0a6386c6686
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:26 +0000

    morning finish wire ours experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
