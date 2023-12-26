18dad74a7f2b77ccaca16233f3a6ed0a73cf1ad1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:13 +0000

    neat through speech comeback frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
