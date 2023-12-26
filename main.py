dd3b3ca555b3f36389ee3f1568af3f30ad5299b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:17 +0000

    fly violent branch passenger exciting

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(10): print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
