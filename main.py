50dd372f78a13fe8e884bc231d6b7cbce15693a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:00 +0000

    cinema usual various leg here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
