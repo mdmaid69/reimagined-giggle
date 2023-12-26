3aa7d345ad12c90d843b5fe1ae5f12318e6a0da6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:47 +0000

    night plenty practice careful aim

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_queue():
+        return collections.deque()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
