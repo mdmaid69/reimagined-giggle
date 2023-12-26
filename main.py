8f3bba93e2911efbb9952c404a6d30ee39e0c6b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:14 +0000

    object success add progress behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
