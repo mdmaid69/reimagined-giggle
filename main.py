26badd33ebd7f04edca14c1ccedf55476086d077
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:04 +0000

    dream pattern year closed thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
