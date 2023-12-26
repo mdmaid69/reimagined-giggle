ce7e0d515965fd192afecb9dd94babefcf1df286
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:16 +0000

    report score print transport father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
