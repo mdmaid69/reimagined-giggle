4a8d8fb4f0c1b4668cd8195b2825317faa4f29b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:42 +0000

    unable towards apparently feed loud

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
