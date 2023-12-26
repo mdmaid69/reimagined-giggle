ca2888664a45edd4b2e44e8690ed91265455affd
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:37 +0000

    battery uponon laugh square play

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
