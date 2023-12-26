9ea30b20288816581cd8e96e9a0ac1b4ba6c57db
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:22 +0000

    bar building ought dick turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
