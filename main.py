5f09ad15566d4eed9c14a07fa93b5d27924f3d5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:00:26 +0000

    air rid frightened married justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
