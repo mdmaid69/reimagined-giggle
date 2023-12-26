3780816e7a70e82227df448553df178f31edb4a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:17 +0000

    belong boring well loss sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
