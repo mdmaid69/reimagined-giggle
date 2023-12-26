71eeefb6d311f5faeaac932ed1709d7ed901064a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:28 +0000

    confusing change good breakthrough interrupt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
