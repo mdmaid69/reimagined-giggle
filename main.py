00476c28d92aa1a96b454bce67b1add9f0a16fbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:02 +0000

    blame movement identify true forgive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
