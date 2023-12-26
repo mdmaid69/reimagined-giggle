c2eff8b4d5362484d957fce80bce7c5fd827c72c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:30 +0000

    fair urgent strange breakinto group

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
