b0e38cafcdb475e19c75209f64e11152fa320cd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:50 +0000

    right shine hate inquire speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def multiply_numbers(x, y):
+        return x * y
