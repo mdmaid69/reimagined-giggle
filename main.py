e6afcee8b95aabfad5758c541da870a5d6a45bdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:40 +0000

    finally flat or Ff wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+def square_number(x):
+        return x**2
+import array
+def clear_array(array):
+        array *= 0
