9f9fadaf03dc289e6382aef1412b1f18117ceeb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:16 +0000

    expression up cool undo thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
