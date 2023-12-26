a53f7ab20e54c9f0916c043163e62722a3766b77
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:02 +0000

    cough spray uponon sorry character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
