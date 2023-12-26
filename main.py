8d3663cf53351043aa01f3af5dd325631133828a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:56 +0000

    general lie yet long tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
