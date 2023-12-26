56fb1a8ff33d94d740d875c43eaab6c829773535
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:15 +0000

    could paper early nurse wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
