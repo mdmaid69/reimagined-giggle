6c859b3c5f8329b39db44cd280462cabb0c6383c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:53 +0000

    arrival emergency division temper urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
