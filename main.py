1a3853fb97a1b01b850b3a1d07ee16980613970b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:25 +0000

    protect crisis talk geton speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
