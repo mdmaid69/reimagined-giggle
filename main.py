f3bfdc56bc93ac70bafdabcadb11a029081a3f0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:03:57 +0000

    answer breakinto situation expression turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
