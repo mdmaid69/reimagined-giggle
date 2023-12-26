1a30070f99314cea68f4884ff8e247ae59bd9ae6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:52:38 +0000

    lead measure cause into dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
