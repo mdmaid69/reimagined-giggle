518bf46b7fe0fd43be64bb8dcef2c54e7c32956b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:38:15 +0000

    centre press fun partner whichever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
