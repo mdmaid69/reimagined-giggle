7a6dc3ad641fc160f6cce178be0459eaca2f14b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:43 +0000

    simple callback shame physical limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
