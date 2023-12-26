77814342a01d6c413f3f607f84f384924e917eb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:29:47 +0000

    final female pay total wish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
+with open("some.csv", "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
