0363f8c9e90d35499a3d3a6dd4b444caa42030e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:46 +0000

    scale look married sleep complain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
