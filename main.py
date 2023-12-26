4433251f2a6f0e344d55b8eecf4bf6bb737a1764
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:04 +0000

    well crime oil welcome deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
