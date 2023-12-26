5a244c5e11b009a6e65e78d3d5779b681f78fbc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:21:49 +0000

    heavy talk very attractive conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
