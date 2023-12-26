8bab59c4808248073c72ec26ddb71f6d2028d9f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:38 +0000

    guide influence front several empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
