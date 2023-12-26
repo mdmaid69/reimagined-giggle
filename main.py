50d4cae11a46c401a440793fdb91b2fbe3da8571
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:28 +0000

    stop greeting development flood suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
