2dc231cf94be02d4e4602f718e42cc4bac4d6d2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:13:41 +0000

    welcome slide downstairs trousers break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
