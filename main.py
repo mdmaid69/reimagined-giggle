2f7a5772446b1ada8ff5ff2cb8944883a96c5759
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:20 +0000

    snow giveaway sport meet thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_speed(distance, time):
-        return distance / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
