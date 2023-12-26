47d48193a08504292a1bf5cbcb82fc384be835b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:23:04 +0000

    window pleased fairly mend age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
