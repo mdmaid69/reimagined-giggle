c0d9ab0b77914ff311be1768c8ed4be174b8d63b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:52 +0000

    lift memory could ticket below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
