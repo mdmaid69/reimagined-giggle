262b0a4e3788c79e1939b609a61ed20390275726
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:11 +0000

    actually perhaps within future curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
