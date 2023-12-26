9f35a616f690fb09b7632d5e51369902d7a633a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:14 +0000

    phone runout closed shut zero

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
