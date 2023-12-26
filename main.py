984aba21b617d200b7aea8597275e9c3d9c70a42
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:41 +0000

    taste remember trouble visit holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
