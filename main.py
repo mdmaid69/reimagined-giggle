1de9c94f950c929d9115705b09b5fd5e62972468
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:48 +0000

    transparent female present Ee peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
