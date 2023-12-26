c5dc736ee0a42e9ea224a4ad54a375f8e6862b3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:19 +0000

    disturb be smooth walk unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
