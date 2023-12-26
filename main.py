92a9233078fd4c23ddbd64520a7a44b27b220b1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:48 +0000

    holdup because that full upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
