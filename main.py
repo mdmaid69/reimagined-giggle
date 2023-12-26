3cc523207532adefb990d3ee155dfb2208f1e7d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:09 +0000

    probably murder offence file soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
