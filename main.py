61b19d87ccb0b95508d9dce237bce2b0c78416ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:42 +0000

    claim Ss fit shine nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
