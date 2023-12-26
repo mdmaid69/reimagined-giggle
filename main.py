beee5d19843811427ea3e5bace4ff42fa0d07a8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:57 +0000

    excited ride possible prefer contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
