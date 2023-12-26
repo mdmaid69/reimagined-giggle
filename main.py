f8863858b873911c04a3e992e8fede3bf799591a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:27 +0000

    secret recommend equal test favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
