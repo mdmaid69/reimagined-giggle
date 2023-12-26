c1458631ae0ae84818cb83d194a182573238d251
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:39 +0000

    health relative deliberate spare short

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
