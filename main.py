ebf3bd8f29056bcea38757bd8a1e2bc8abfaae8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:01 +0000

    land ear indeed leave curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
