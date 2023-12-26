aa52718ce64bb534f6ecd6ee85a81b6a3337d357
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:28 +0000

    damp design by school evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
