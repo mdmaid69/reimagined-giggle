32c6a2d1decd29801f19b31cb92dc3172f1ff1fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:12 +0000

    worse interrupt achieve interruption stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
