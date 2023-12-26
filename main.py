16479d8f81762a96366665ebbee1bb2102173699
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:59 +0000

    improve generous floor minority announce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
