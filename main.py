ecb6fd6703fefe16e16069638d356b6206e28db5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:54 +0000

    cheek record appeal suddenly temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
