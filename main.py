dcede7b78d924657ecd6db6b2e78bb1a3ae5be70
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:54 +0000

    impression steer interesting rely talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
