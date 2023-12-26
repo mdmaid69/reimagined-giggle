9b55899614aeb9c65b41697ba94eba2daa750e25
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:28 +0000

    whom immediately available bad anything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_odd(n):
+        return n % 2 != 0
