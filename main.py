c12663c21a44375f0d263359c56aa6fcdbac4cd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:46 +0000

    its university approval before keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def split_path(path):
+        return os.path.split(path)
