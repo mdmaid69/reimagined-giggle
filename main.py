bc3ac436f6f51ce72c5ddc0fce69206c14ed303c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:12:08 +0000

    strong late world wherever Yy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
