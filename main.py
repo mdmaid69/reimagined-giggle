a3678a1a16ceba99ae629da24d35cc7b3ac198da
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:48:36 +0000

    badly experiment Zz find mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
