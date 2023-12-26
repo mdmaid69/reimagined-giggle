2b070ec7c598d4a9a0dd673c68d017871eda674f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:42 +0000

    inquire share march stone cough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
