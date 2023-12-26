cfcc2acc289433db4c09225215045b7d5177d9a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:24 +0000

    experiment design fortunate solve mainly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
