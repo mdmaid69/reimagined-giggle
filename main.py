5372e5f8e6a0ac245a4090eacd4f2c7c1d1d4a1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:23:46 +0000

    turn moral concern us normal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
