f9fc907ce8e4369c5c52e3c22542f65db16a9e1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:42 +0000

    recognize yours top back talkinto

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
