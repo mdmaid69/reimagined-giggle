baa64084fff3f7894074f8784e898a259e670fb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:24 +0000

    fast so worried getup support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
