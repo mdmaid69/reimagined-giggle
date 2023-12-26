779a67c4451dca771fa6b36a51438300179ae7c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:15 +0000

    marry pupil affect hardly touch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
