60971db878b2d9e565a5b510d83d0104ae49aa2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:57 +0000

    out rest maximum realize exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
