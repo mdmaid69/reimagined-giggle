f664d01b0c5518b9e9191a0464daf177864b84b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:00 +0000

    point fresh trap squeeze new

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
