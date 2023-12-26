34e55c6bd3c52030dc6445a4fa9003740aad4905
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:34:24 +0000

    sport brave rain apply hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import logging
-def log_message(message):
-        logging.info(message)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
