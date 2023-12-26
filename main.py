1200664d16ff7c75e3768ed4fd4d24bb44208acb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:22 +0000

    story wave than sad deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
