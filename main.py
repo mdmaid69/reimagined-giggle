5c933da6d6d97923736c4577b8ddf00e445ad7af
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:41 +0000

    disgusting world do design price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
