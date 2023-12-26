efb3c3172872f7c40dff929f18d58934260e0af8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:00 +0000

    steer open definite arrival repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
