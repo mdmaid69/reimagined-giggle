c979bbe6b924c5a03279f7f599aedde1bf3c404e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:02 +0000

    tread cross tone reply whom

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
