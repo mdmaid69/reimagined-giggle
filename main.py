28893698d14e59c0ce076f5e38b4a0da6eb56d55
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:38:57 +0000

    can foot never than urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import tensorflow as tf
+print(tf.__version__)
