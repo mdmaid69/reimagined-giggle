b5073335c89fd838e43cc1e18ee204addd7beede
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:49:24 +0000

    flow lower prepare radio height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
