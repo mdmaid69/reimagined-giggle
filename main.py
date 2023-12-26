47ac1dfe5a4d334d620cdc9b2c91963c8c7b7030
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:47 +0000

    your Dd join he breakin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
