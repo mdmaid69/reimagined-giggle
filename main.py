2638c5b696c7960e670018c9cb457553c9b8a359
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:48:07 +0000

    vision character rob stamp dark

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def greet(name):
-        print(f"Hello, {name}!")
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
