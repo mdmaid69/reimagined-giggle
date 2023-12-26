9912a0bc076de7b41d947e2b0e96c1ef46068c31
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:18:43 +0000

    hot find stretch pleasant floor

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def convert_to_binary(n):
-        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
