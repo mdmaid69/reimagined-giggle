7db1e21c918442b8eac6b22c1fae1507bc470d2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:43 +0000

    share excited autumn port creature

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import sys
+print(sys.version)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
