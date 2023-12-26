9b1bf7bcce775ae458eadc44c84eacfdf6404107
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:47 +0000

    mail suckme hurry might succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_array_as_bool(array):
+        return bool(array)
