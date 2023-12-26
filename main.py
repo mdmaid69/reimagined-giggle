4ce4a6d2300eb6d662db3a37244c934e5b1d923d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:05 +0000

    upwards personal finish what key

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
