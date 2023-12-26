5b30e7fc396efc86c965be2851cc302481d32608
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:05 +0000

    hole have rise earth winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
