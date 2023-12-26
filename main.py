8b8c90c942a36b383532f6a9e190444006fb3617
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:05 +0000

    569enjoy glass term you each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
