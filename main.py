f3a9331f8199489ffcd12fd0d9c48e7c535882c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:55:46 +0000

    convincing bored embarrassing capital bush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
