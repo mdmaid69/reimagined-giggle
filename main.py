23e8d242a58e2079b65b0c735e0cde30f3b5d548
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:38 +0000

    threaten sleep skirt easy total

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
