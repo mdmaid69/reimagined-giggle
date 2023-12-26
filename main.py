81048b7bbad43f309d1f2a722b68fcae35a59017
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:31 +0000

    goaway fire sweep means sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
