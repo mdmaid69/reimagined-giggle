2cf45b0a7dd5d7ccb6901227ac069c7b066c1093
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:03 +0000

    matter sweep drawing snow before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
