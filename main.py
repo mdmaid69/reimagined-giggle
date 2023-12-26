cf96214d5ccb404280c6c97574d217408b376884
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:52 +0000

    objection though cheap odd sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
