410694a28c5e166d0372dc5ea20e62ddb457c028
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:08:18 +0000

    fence blue catch ilovedicks receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def convert_to_binary(n):
+        return bin(n)
