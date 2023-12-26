cb63f87ca4ec572c71ee019fe63dcec556cc3d06
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:24 +0000

    mean bunch upwards phone fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def generate_random_choice(choices):
+        return random.choice(choices)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
