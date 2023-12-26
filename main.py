40256dc1451500ac239d8a5488803d7f9da54337
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:28 +0000

    fun among confusing cover tradition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(lst):
+        return min(lst)
+def sort_list(lst):
+        return sorted(lst)
