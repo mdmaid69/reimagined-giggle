d8730e6deb4f847c146d1d2c358eee260ecad560
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:28 +0000

    mean fear scare on party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
