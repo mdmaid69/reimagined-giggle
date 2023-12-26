d54472cd0e0c64ed72d4257be1a805c581c53542
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:22:21 +0000

    free right pause comefrom ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
