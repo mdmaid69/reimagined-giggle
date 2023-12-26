7a6338b47bb78e0352074f8538d48a4718aa539e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:56 +0000

    window few similar Ff trap

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
