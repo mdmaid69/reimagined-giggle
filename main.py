958b4f408960c4922d549ae797c051003434806f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:28 +0000

    easy copy consult comment form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
