75f3c94855fa6d35036758f44596b235ae2131a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:49 +0000

    set Tt under declaration wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
