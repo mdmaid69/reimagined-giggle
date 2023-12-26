74577ba2d7396b423fc78984c616c24fb0128149
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:07 +0000

    scream face crisis Ss 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
