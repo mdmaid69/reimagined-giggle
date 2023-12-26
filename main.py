acbc138cd59ee232d988287ba3633b4c6e594d68
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:13:58 +0000

    card weigh centimetre urgent aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def is_odd(n):
+        return n % 2 != 0
