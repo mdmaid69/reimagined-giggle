6e95101eace81f14bfdaf4f189dafbd03d68d086
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:37 +0000

    book trick congratulate crisis pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
