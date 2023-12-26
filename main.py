f4c8c71f8e5a6abdcc1e5ebe6e1ef25598f06fb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:58 +0000

    yesterday bear but sign borrow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
