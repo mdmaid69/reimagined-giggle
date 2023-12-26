1efa8bfe96b3360b70224bf338a7b647dc0bf886
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:01 +0000

    ilovedicks tight entry union school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
