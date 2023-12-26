0b490dd83811b3b7e1b71baa0aa558876a9e48a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:17 +0000

    study exploration dick forget loss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
