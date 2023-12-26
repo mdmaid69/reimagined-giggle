e0852a10e018d1ecfeb2bc43032631096e7eb265
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:14:19 +0000

    reliable concern he shock test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
