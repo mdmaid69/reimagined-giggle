dd51ab2cc0ea91409cd335bcb2f346718b6a57f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:51 +0000

    unfair include prepare sky accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
