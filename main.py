2ababf39d01ad848363f7337412eebdd34ed3845
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:36:56 +0000

    contain tense yours crisis although

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
