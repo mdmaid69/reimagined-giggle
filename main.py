91736da5c6ddb1acdf8b9e58e5cb48ad86a0a6b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:13 +0000

    repeat likely put radio murder

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
