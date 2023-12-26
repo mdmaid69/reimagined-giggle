d97979922721fd8f24e3b0e65bcc52f5507840ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:31:40 +0000

    platform load communicate male reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
