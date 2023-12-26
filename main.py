fd97bd928e18347812cac8ed1583ad1d00ce2f7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:44 +0000

    very simple acceptable agreement chase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
