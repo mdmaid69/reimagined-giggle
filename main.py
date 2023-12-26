6e4d4292c0cea8286fcb40c682a58f4e520124fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:40 +0000

    buy impressed stupid emergency feel

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
