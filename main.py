8f2f861dac3483c0fa4eb3ed2905ec30eece4a8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:27:42 +0000

    godown keepfrom lean anybody impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
