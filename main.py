e5d69d9608a182dc8ea1ce24dd0a867409b674ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:53 +0000

    else friend return sky praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def add_to_python_path(path):
+        sys.path.append(path)
