cd6dec6f1b8a480420115ad7345b0ef779c5a13b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:47 +0000

    blind every gradually solve brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
