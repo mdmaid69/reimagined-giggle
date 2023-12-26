29be02c392be1b2bbb0e64d37103ae67ff1eaf4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:42:16 +0000

    appeal rich present second manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
