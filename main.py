a2c7f69f3038fcbffad2eea1920aea5b0445ee59
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:18:23 +0000

    system notice during sorry tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
