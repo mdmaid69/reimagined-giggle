709e972fe61524a1901a8139894d4ada50bc174f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:19 +0000

    reserve pattern sit pretend mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
