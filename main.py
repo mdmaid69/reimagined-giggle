255f5ccfb11f106e38c4c51cbdbb078ba6052c1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:15 +0000

    kind fold base conduct to

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+print(random.randint(0, 100))
