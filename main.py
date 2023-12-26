833eda6dad0448b71bae6bc4bbeed4c2fb54baab
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:08:11 +0000

    mouth whichever law reasonable operation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
