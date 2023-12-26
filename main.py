c169dabcd270a4d13d7dd3cbf03364bede5eb1cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:39 +0000

    unite surprised soft it callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def cube_number(x):
-        return x**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
