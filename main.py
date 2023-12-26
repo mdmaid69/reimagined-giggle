14a523f10cc7b30efa20545f46b9cd48a641d0cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:49 +0000

    flow back offer oil politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
