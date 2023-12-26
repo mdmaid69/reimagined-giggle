4a7db3c144bbe8e56d00f80d60833eda17bdf1ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:49:01 +0000

    play shop typical exhibition increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
