fc0cdb1a3526d9e2495624aea2fbdb62ef5429f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:26 +0000

    object Bb peace feeling fit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def convert_to_octal(n):
-        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
