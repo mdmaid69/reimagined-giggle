b58de99c001c87244168972ecec7deca5e66d092
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:49 +0000

    smart thankyou grand nurse sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
