5cade7c2c143705ae62e5e239a217817d0cedf28
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:13:40 +0000

    necessary fat except kind memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
