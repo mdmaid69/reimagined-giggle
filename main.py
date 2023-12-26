5c295de8b576a9ad44199f69b09dbeb8bae700c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:39 +0000

    phone lend cost mark through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
