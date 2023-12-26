a4f073481085b4cd76b419fb1daa75a660b6bf70
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:41 +0000

    them art swallow beat runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
