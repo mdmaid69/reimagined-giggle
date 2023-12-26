2f2ba17ee8bfac4efa8d88ac2d3d3cec10efb1ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:31 +0000

    possession dull choose rarely type

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
