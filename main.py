b81b0c999771c6da81778d7d26dd29b06ef35f94
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:18:47 +0000

    sad represent Zz passage weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_array_to_list(array):
+        return array.tolist()
