2a517f038c8c3ccaf4e0267a09ae673153df4d36
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:31 +0000

    bunch sky row year make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
