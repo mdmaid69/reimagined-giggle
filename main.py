4afbcbc5022c34d539f4b61197cdb8c2b6b983be
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:58 +0000

    regret claim impatient weekend prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def get_array_as_set(array):
+        return set(array)
