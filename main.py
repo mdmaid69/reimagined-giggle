86b74549c1a0cf0e3a313dfc0ab8f96f0c545f23
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:10 +0000

    custom type stamp reasonable dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
