07473df411131d740ba9439f738334cbe69d7eac
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:58 +0000

    job blue precise wave pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_index(array, item):
+        return array.index(item)
