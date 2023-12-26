f6926f16a84ca1c8f0c3a46acadf5b5c8b9544ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:50 +0000

    flood conclusion father content piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
