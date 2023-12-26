6cdd57d2277ccff430c70ed00187ccd825364b31
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:25 +0000

    several narrow this that well

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
