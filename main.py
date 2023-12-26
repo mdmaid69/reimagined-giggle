2c1d9c8ebec904d736bc4ba926c94cb573d720fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:32 +0000

    ourselves deliberately quarter upstairs Ff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
