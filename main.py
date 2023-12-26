739d0a6a4290f1b0552f3b516a8acf6df7943118
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:08 +0000

    apart bad keepout time strong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
