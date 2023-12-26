6808ad4f3f01802d69446d028a431341e3b34179
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:51 +0000

    reach gradually sky wrap rely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_length(array):
+        return len(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
