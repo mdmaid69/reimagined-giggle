33bad3e91c9b40ca8d9bd0a9b668d9ff96bb0053
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:26:59 +0000

    effect drop organization measure smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
