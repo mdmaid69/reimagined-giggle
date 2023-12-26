ef6e451666f368f7fe0ecdd6cf82c9b43bf594ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:17 +0000

    everywhere identify Ll play off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
