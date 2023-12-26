e3ed9068ec05e52a5f92ee889f5ce5374a76de6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:48 +0000

    attitude free switch beat message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
