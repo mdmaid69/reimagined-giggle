9109c3647c28d93f2b53b037170beea73a7b7d6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:02 +0000

    workon replace air taste grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_max(lst):
+        return max(lst)
