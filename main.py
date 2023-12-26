b8f81bbdd5587b47b69602bc11a45e12b0ad6573
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:36 +0000

    with pinch keepoff by offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
