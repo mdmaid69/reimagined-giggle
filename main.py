ad25cfd079447693b9d89c65bea4976a53615324
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:33 +0000

    anxious careful search father picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def clear_array(array):
-        array *= 0
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
