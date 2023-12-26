2d28090e1c885bb13f8e7f44c03628f95ec58b78
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:24:01 +0000

    bound tongue brain fight butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
