1e206d71604e61f80ddfa2f7c8aa9ca0d724c33e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:04 +0000

    one woman pleasure plan funny

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
