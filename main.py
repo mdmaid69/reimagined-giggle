95f54db2d0a21ff4415f8226be45ce5386604f94
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:23 +0000

    attach breakin impossible through them

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
