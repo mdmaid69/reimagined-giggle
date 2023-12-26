2c4d12a08846c5675204b2bfa0d0280943ecae75
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:38:04 +0000

    repair next getaway steep repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_counter():
+        return collections.Counter()
