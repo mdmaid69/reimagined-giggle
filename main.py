512ecf3926045b628fa6f7e258acbcadec19eb63
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:34 +0000

    violent bored marry fond such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
