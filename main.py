81b91d004608e474cc24ff3c75e4f22312301f92
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:18 +0000

    frighten pinch have there sing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
