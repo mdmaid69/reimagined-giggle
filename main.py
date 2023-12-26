f16a5fd4614efa0d682feb58f3cfdd0a5ec95891
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:58 +0000

    arrival refer Vv impressed carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
