18fe7ed67c0d80279b85c13e53e6fad6255b273b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:08 +0000

    we angry match pass edge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
