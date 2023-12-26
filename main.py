7e1f2e5ae16e8703682c69a0234704f5cdf56514
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:51 +0000

    fierce very wrong next second

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import re
+print(re.match("h.*o", "hello world"))
