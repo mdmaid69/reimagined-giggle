bd2596ff2c128bf3576e62bc72929096ee774e1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:24 +0000

    cut physical powerful similar bring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
