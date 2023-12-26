c6c17c98d67128ed81c948ec4861ff97055ef0dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:12 +0000

    children disappointed outside fond whenever

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
