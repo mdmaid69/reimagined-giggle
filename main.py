2bb7bcff5dc3ac0fb860e7a7bb1be9b2a05a2372
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:44 +0000

    dear street sensible fault calm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
