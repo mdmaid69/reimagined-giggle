76b4ac8d28555a0d6a66e5ad37866afee891e252
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:32 +0000

    book awake culture goaway afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
