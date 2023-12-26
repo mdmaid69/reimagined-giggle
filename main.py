11d3efc74a307de3eb22cbb59f3f63cf28e72961
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:23:31 +0000

    goodbye her familiar the cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
