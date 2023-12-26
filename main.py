7ec44aede857d9a588c150bc12b91976ea7a61c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:19:23 +0000

    remark wave alone interview load

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
