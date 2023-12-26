1e9b0f5784c2c85cd4435833f2ab0d67f60bef15
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:32 +0000

    workout evidence belief pump compete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
