9079b4887a6464ca5a3ae89f87c8aa0f8e551c6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:58 +0000

    solve bottom spite beat reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
