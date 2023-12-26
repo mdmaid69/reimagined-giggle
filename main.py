abc95924f0825737903a0b0895e16229873c0baa
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:15 +0000

    receive tone rush giveback strict

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
