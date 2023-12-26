6decb9f8f005d9b27a4b7e0b5857e62404f466d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:23 +0000

    every stay before hope breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
