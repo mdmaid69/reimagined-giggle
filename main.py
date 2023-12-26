20d4f710aa13a6f217d237a66d77c63a7ef101b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:43 +0000

    present performance also bunch slice

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_work(force, distance):
+        return force * distance
