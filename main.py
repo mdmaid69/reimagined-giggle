691e48c5150e91a0345619c98f28851e3908efc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:20 +0000

    scare yet switch shop possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
