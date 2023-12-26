846e84f1c1180ce5a504209916a768c8d3c65315
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:02 +0000

    formal from wave keepout artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
