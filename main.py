2f38d62775d4e10699a375bf1746c5b2b9e1e3de
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:11 +0000

    speed clean tape lookaround affect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def cube_number(x):
+        return x**3
+def find_max(numbers):
+        return max(numbers)
