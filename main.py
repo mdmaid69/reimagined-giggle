895cd482f7447671cba5714d91b339572a4f7c8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:39 +0000

    571enough fond accept himself welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.ctime()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
