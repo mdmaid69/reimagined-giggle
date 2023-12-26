27384b9cba5fb7fae1e9889430b05783c4bbda08
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:21 +0000

    word blame proof home appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
