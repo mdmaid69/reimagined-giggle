6cf6adf1d6121cf241bb1cddf1da3a46d40f4bc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:41 +0000

    area clue result nor properly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def set_array_item(array, i, item):
+        array[i] = item
