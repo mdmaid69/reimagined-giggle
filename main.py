ee71073ed1b572653e5b7038e8d6c2ed5b550e9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:48 +0000

    city independent crowd ring people

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import time
-print(time.time())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
