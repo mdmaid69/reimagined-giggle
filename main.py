ec5b62b75f06ba167393c01adfbf475383a145ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:12 +0000

    571enough reduction start pupil summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
