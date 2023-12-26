b93f43217f4745d1f3d672ac53ea65280d3729b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:22 +0000

    ready need rare letter coffee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
