c43bc708f1bf35b385cbc1330b0b9824d707968d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:02 +0000

    example godown terrible suit care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_max(lst):
-        return max(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
