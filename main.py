fa1a92d65329112952afd9e19ef79dddc8e09cda
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:31 +0000

    rub gas roll angry room

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
