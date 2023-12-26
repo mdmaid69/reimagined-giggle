c8b8bd6fb2578b94fb20bdaf28d6b442976a70b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:19 +0000

    statement party earth create would

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
