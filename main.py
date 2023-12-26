5bc5fbacad666befeba5791d7615ee2717769d15
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:58 +0000

    wide block clothes Uu car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_length(array):
+        return len(array)
