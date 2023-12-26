58b235c791d76d5c3bcb6502e81aad47c787c01b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:34 +0000

    fortunately flow sell exercise gap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(lst):
+        return min(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
