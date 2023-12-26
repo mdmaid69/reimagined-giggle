dfd20075d230e1d75dc2177ccd988f368ab81fd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:26 +0000

    prevent trick doctor regional thirsty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
