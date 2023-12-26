3c98d8d56d010b889ecdd3410edf490179165fd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:24 +0000

    crime proud apparently print debt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_str(array):
+        return str(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
