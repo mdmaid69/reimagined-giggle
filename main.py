b645b34bc3a27ea2a0768d552c6e3e9ca67990c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:20 +0000

    figure command contact crazy walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
