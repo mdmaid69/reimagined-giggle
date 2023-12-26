6b9a175373067ce4fabfd748d22f3e38355c233e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:35 +0000

    benefit nonsense lonely less shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def get_array_as_float(array):
+        return float(array[0])
