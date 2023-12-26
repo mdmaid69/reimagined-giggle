7f0c26bdb11a17ecaeb6ef4c6831756af2a800be
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:06 +0000

    careful until slow board fact

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
