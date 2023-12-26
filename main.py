efa7b0d3bda4ca368e9d89368ed1463ef47d0f47
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:55 +0000

    branch operation wear feed pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
