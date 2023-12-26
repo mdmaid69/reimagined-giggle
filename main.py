3a6c48188562a0038fc4e4fea04a77f5673d80e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:26 +0000

    hurry warn hurt solve when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
