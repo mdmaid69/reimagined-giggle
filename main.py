6d17f54d54a0d9e16c108e57bb16758142878d67
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:10:20 +0000

    bus birthday opposite couple eye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def remove_directory(path):
+        os.rmdir(path)
