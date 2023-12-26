4f3d239b7b80af57aef7bdccb20275c0d11e17cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:21 +0000

    chat vision teach I birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
