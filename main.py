58b75b964ff2be2b060fcfa4e9fd7563eee36172
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:17 +0000

    dance noisy loud moral wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
