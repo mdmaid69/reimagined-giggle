6fa37286080d3620d71f5a28d5a3188ea05bb693
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:33 +0000

    season message pinch nearly brother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
