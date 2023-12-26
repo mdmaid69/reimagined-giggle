7f943569a5e44c9ca60c94a815cea8c0765ee921
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:33 +0000

    mess notice brown fold average

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
