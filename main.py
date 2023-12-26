02c63cef8c51b87fa526627885b76f1f60cd7646
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:46 +0000

    listenfor criticism rarely impression now

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import re
+print(re.match("h.*o", "hello world"))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
