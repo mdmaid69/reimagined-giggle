cb4933196e011ddad9986ae0ef5fc8f1d877be09
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:27:42 +0000

    wind something operate top fine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
