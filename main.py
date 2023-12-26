15be086fe18d41140d559a1cf79fdfaf063cb3ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:20 +0000

    buy passage sensible picture where

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
