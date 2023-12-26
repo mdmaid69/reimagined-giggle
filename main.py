e5f25ac218efa98c594fc3e44b7121f50967ef93
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:55 +0000

    shrink speech runover turnover family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_str(array):
+        return str(array)
