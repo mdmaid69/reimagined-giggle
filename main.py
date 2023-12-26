7aa6cd9fc1a4039723df0a1c69ca1268f3a77e7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:36 +0000

    level price bang wave information

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
