be274b73772df763c205790431628acbed08aab7
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:21 +0000

    appear whichever admit temper floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
