bfd114e95bbf25776874ef458e5d67f2a5b64516
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:44 +0000

    blind during somewhere forward stamp

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
