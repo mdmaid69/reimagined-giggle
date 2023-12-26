f5ce84a0d8a66c0caaa3db2e57a617277f8b86bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:57 +0000

    now sing rise demand connection

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
