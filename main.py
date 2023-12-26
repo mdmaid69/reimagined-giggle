f3c98ff3d6d90b6090900943eb327ce5df825b11
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:32 +0000

    soil Kk workon bringdown give

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
