fdfd8279971de3da5b9d66f1e33de7be87c74fea
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:21 +0000

    soon point right fork dangerous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
