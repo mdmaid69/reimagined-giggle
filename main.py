c6bf1b440ee7c779cb381c9de0ef74088b96603a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:08 +0000

    switch perfect add shelter ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
