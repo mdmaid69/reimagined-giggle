95c38db8068dcd22f2d502fe77d77462d05cf02f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:16:54 +0000

    quantity bet set dream acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
+def add_numbers(x, y):
+        return x + y
