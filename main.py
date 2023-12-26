33081bc9f410729a45c3dcbf644963f9c1ab7011
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:15 +0000

    if tired ignore as swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
