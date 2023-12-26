d383b67c6a66c3b5824a054f65aa167639a25ed7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:33 +0000

    sale vote neither repair improvement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+print([x**2 for x in range(10)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
