4a44c6fdb23f2bfcc45e9dd2804629caac69142d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:56 +0000

    announce short sticky past sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
