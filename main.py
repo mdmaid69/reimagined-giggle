0ff34c2cbb1281c953368d66a827026e4cc85050
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:16:04 +0000

    factory wear along doctor popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
