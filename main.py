5130908a45a81b8d75c2039528aed4652e709f51
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:56:49 +0000

    dry tidy getout date meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
