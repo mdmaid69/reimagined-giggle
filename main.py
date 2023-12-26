e8c803ebaed159d1c8c1d3f559c112fb3cf5febd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:59 +0000

    command on number listenfor otherwise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
