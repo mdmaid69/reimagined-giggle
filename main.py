bbafe0cb5fb510152a45287cb27a11f8c6c207cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:47 +0000

    popular sometimes trick shallow sensitive

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
