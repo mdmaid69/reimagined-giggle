e7721f9a227ef341f3e1af702d14c9133e3c8b4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:29 +0000

    temper public worst translate wake

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
