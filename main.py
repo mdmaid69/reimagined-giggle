45f265877e2bcbbefb5945a4d9021b403811a129
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:50 +0000

    spray refer mind grade deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
