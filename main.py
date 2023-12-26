4d6c13ba5a039de18066e2bce10cc632daf69b99
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:02 +0000

    rob embarrassing paper estimate feature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
