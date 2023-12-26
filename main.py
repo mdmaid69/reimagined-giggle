28aa6cadce0f86ee70daf855d4bebecbac5af1d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:58 +0000

    port nice show correct only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
