daa705353eccecceb9a0488f1ddaa7a9f50b20d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:35 +0000

    gooff defeat list steam declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
