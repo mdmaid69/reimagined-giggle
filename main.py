95e83a4c339da36c22060952de1a543eee2e2bad
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:28 +0000

    doubt closely turn this talk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
