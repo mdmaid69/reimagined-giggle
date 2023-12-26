41a5cf8dc9dbdf567872f70ce988621fc55a4782
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:24 +0000

    distance proud pin holdout confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
