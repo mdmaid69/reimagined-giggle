2b89a1e40da571509611f8efa5b0cd7b9e34db3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:36 +0000

    shock collection expression include modern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
