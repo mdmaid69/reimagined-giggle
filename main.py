55e432c36150543102d062a83bb47125b8d791aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:26:19 +0000

    tape easy healthy unfortunate measure

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
