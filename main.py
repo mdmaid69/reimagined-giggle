e5b8ec1c4937cb0d1124911a8a2ea6fc6903a52d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:44:23 +0000

    happy forever file expensive forward

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_pressure(force, area):
+        return force / area
