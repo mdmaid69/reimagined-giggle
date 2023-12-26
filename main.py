ee285073b74f7f5fbd73501fd900652bbf9e6c41
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:37 +0000

    around indeed occasion spray cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def reverse_list(lst):
+def reverse_list(lst):
         return lst[::-1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
