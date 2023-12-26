894729bf4a919d9b86ba102ea56e9fb8f08d3211
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:25:12 +0000

    walk flight advantage Oo slide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_directory_name(path):
+        return os.path.dirname(path)
