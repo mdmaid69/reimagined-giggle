3cda725398425cd5389c49f64a2dcbd867fe92f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:31 +0000

    noisy succeed cum plenty doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def count_characters(sentence):
+        return len(sentence)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
