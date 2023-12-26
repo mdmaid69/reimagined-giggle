1b1db2cc9aec870be2eaca2ae91f530bdac72f9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:47 +0000

    moral off cost judge fetch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
