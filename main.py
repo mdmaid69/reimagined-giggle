24ac9ed658cc2763b652f3610227db95958ec14b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:25:42 +0000

    regularly still comeup ride gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
