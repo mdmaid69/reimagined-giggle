ec48fd4880e0926f7a0f36fbfe5a2535832cba49
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:39 +0000

    equal bunch round difficult wheel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
