c7057a3798b9b6b825273064140b4d25310538f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:42 +0000

    band force insist grateful luck

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_min(numbers):
+        return min(numbers)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
