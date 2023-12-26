481ad09c44daf31e309be60e54aac08cd24045cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:45 +0000

    switch squeeze drop false loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
