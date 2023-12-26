517bce791695d3a13422463d369002a96e5b759a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:34 +0000

    prefer dance rush hate more

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
