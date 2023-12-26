905af12d837bd2c20a2a93942e60ed798d0dc888
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:20 +0000

    clean Ii pencil like train

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-def exit_program():
-        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
