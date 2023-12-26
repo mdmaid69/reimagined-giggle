80b7c0c2055bd133062de60ec835f3af0ed5aac4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:36 +0000

    public post considering ilovedicks this

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
