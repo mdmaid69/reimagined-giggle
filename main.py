8cca0252bce0d51e261c309d2babb66826b01179
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:52 +0000

    music case around circle tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
