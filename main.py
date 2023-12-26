f01a2af0e6c3f1a0ee94cde75ec2fd2af8d295d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:41 +0000

    contact getout interesting family bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
