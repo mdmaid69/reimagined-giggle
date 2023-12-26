bdb7ba6e459deb13ee4a826da5300307d3454863
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:51 +0000

    achieve rent protect thank world

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
