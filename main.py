1358a000c25afbff4fee0fe4422a250ce596446d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:03 +0000

    genuine cope great tax trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
