8b5be43e76feb93c10d6ae98797914cff9d8b5da
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:14 +0000

    pure finish season interested common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
