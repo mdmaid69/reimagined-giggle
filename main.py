a28112e37ab5ab9bf560cbe3a26d72a685154d06
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:11:29 +0000

    interfere emphasize please deaf car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_acceleration(speed, time):
+        return speed / time
