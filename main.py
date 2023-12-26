3b61512ab34753ca2a9b214732598c575c1f3747
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:02:02 +0000

    consist list which tend help

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
