b6d5d5a1ca28733eeeb3116d4ee69915b9825c6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:46 +0000

    quality connect underneath city only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-print(math.pi)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
