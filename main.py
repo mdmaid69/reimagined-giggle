b8058dde1c457ca611bf2d5614b78cbb9576c68f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:27:01 +0000

    fill theirs turn geton blind

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print(sum(range(10)))
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
