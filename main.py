ba5c7c7e72edf184c7f7b9d12e1eeb25f093c2ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:46:47 +0000

    bag pretend copy whisper all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
