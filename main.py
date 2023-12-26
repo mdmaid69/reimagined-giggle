14bca5ca3b20044b9a93c2d7bc46c8c328fea2da
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:40 +0000

    increase process pin tend inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
