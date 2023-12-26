f8ba7118af261adb9e800334e1d4024fe872d3c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:46 +0000

    thank Jj suddenly shade insist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
