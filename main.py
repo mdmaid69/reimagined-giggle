124d968de039cd8714a76b511302baf182c87174
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:46:24 +0000

    love pleased unite soon impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_directory_name(path):
+        return os.path.dirname(path)
