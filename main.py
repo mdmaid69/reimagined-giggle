5cf8b60efe110c498591e24cd0ca8e38afd8eb83
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:39 +0000

    rain police many life few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
