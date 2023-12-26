6268f15533494a499c9dfb8b077df341d16b0395
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:28 +0000

    spring however flavour who in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
