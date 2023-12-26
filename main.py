54b71328bf5e0c79080995f0c616867017633d7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:35 +0000

    contrast stir best commit back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
