48b5be4e26017eac2f0c775a401981f82e5877fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:18 +0000

    exchange interfere precise blue pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-name = "Python"
-print("Hello,", name)
+def is_palindrome(s):
+        return s == s[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
