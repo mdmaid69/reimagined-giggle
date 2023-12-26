719c60aa4b4f522465b42560a3b3a7312bd63cc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:09 +0000

    identify move couple never spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
