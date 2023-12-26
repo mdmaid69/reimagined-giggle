12e5ed5f818ccbd683dbbc9aff72c237af4f5a5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:59 +0000

    hand memory heart lower club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
