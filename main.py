4849734bad47d7279929ce05a1da687bbb7a3cc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:39 +0000

    description stick limit doctor still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_density(mass, volume):
+        return mass / volume
