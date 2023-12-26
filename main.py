4628d8c6f53bf4d6decc5ed5e7b6281f0c878215
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:43 +0000

    praise godown consist once unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
