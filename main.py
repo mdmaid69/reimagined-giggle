630473ef24f22897bc8e79744527b64585c4cb56
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:59 +0000

    prove birth and thinkof satisfied

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
