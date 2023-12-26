3354acda2a401ca5532e5b4fd1c5568faafecb8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:06 +0000

    record date spring worry fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
