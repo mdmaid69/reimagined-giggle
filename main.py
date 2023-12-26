a7e196df2b00d69b1ad0bb401692d14c140fc77f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:55 +0000

    offence matter unhappy damp tired

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def sort_list(lst):
-        return sorted(lst)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
