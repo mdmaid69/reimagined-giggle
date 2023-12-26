63f334c8d136ad7f75e91cdab6b69a868e98dc4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:59 +0000

    angry high copy spill event

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
