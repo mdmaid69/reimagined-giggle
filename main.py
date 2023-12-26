82336a9a16f8372f4f8667f47a5f6d8d0714e38b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:25 +0000

    film like love fond mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
