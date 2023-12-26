4dfd1b8f878d5e54302c4308856c34badbda841c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:01 +0000

    love fix thinkof abroad jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
