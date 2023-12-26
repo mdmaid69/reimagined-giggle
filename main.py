8254f121b457ee50ec87caeebf2974b4db860cf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:57 +0000

    Ww goback(to) confident emphasis point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
