8c90526a2688d90d2ba960273d6b890a18e59894
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:05 +0000

    willing whether big swim shout

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
