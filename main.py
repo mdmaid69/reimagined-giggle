973eed361d54e0e7fc80dbf26c86df59904e588f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:00 +0000

    centre pride excited politics goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def clear_array(array):
+        array *= 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
