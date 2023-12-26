1e7876c05cded62f00d6aa065fbba10b718198e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:11 +0000

    children immediate fall size school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
