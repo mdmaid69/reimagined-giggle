7029f87539af54a4aae8ff774e8c9ea47316ffb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:30 +0000

    all talk please yourself power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
