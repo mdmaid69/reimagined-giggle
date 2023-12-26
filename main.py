54c32c9c7596d26a24af534e9fb29c39d5c626eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:18 +0000

    union lower grateful among right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sys
-  def get_python_version():
-        return sys.version
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
