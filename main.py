0eadf27e9bc5450bd7fc1e49828ee02bdde9bd15
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:43 +0000

    store interview close pause crash

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_priority_queue():
-        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
