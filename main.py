32a55b5d8fb0fe3e436e33dc9a98f1d35b726fc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:36:46 +0000

    odd suit prevent about please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
