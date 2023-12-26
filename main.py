26d4379195dda69b167149df795477ea50309ab6
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:09 +0000

    translate switch wind forgive place

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
