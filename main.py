acf6739cc3a9327dfe419fd0e09b308be68a5d92
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:53:58 +0000

    pupil shallow wealth Uu alive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
