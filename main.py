3588939c8d711438e12f058b19edf1bc6673fe04
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:01 +0000

    flow object breakdown oil wipe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import tensorflow as tf
+print(tf.__version__)
