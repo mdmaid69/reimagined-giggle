f750a8fe2993dd30ae0deca8d455e8dc75c158e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:45 +0000

    brush off finish dear runover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(lst):
-        return max(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
