c8e3d9781536bfbf5e13b1f157cb18a2411adc4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:38 +0000

    available test turn terrible noisy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
