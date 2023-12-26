8d31ac959025f4231b0dd2c981f67e7256565443
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:08 +0000

    anywhere hit thinkthrough division generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
