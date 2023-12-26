5edf12d546adab4cbdc66e3b94e25211d3499b47
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:36 +0000

    play my second direct deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
