b306342963e65b9a50029d85aecad7f241b4e257
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:57 +0000

    unusual lead door floor gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def extend_array(array, iterable):
+        array.extend(iterable)
