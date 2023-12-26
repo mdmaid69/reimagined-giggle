ee46cbfc07acd9e1dbe8667537b177fe9736a567
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:29 +0000

    once care eye rush 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
