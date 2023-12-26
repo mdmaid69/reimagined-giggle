f011366337548d79d0ee6b5d040de698201c9be9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:03:47 +0000

    dream everything most shape colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
