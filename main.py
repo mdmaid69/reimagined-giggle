a1f3d1dc49b3b8f4f04a2945e321879a880df40a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:44 +0000

    to visit active because this

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
