05257b4ff9ef0a53a2fe8f6e2ce6730f8ddce53c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:24 +0000

    shadow patience frame urgent meaning

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+for i in range(5):
+        print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
