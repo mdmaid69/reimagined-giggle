98dea582ef4e6dcb6a9d0c762dbd413f1ff76f1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:26 +0000

    giveback type post true truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_current_time():
-        return time.ctime()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
