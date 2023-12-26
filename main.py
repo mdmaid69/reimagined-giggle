5cf17aa6f30f4d2945abf0664fddfa424595877e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:08 +0000

    often say rather support exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_user_dict():
+        return collections.UserDict()
