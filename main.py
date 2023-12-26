5e15a0f0f8798e1d276e189d3488fce615a205dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:12 +0000

    suit fine basic my post

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
