655d49f359db8836141d731c637e7492667d4824
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:10 +0000

    home thanks distance keepfrom load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
