291408fe9497e63df3cb2b6748361c778d16bc67
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:13:07 +0000

    underneath attempt press odd giveout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
