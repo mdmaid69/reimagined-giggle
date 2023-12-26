24fb9299149684993dda6af07e94085d341da2cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:42 +0000

    tough city sensible priority recover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_area(radius):
+        return 3.14 * radius * radius
