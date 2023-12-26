617b29490d07be7a67bed4317d7bc7b7848cd1a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:10:18 +0000

    better tomorrow luck turnback regular

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
