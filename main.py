9f0813948cc0facb3b16a5b756747eaf22313ede
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:44 +0000

    apply average giveup education bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
