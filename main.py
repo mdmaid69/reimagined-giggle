9f5c59235a5153ead80a539b6ed189c2a1353da0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:39 +0000

    discussion embarrassing exciting design proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
