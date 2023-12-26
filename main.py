c46f22a84d945ac589c0fb05a365bc916c46340c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:29 +0000

    joke form several obvious party

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_max(lst):
-        return max(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
