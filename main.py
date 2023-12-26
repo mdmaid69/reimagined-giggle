01469ed670f90ca6f0b61d547b533dc1ba291141
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:05:22 +0000

    fear sure inquire suddenly talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def is_palindrome(s):
+        return s == s[::-1]
