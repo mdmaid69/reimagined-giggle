4d7a050ffd17cd205e3d958c03c35936fc675d1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:15 +0000

    dress accident murder danger dress

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_odd(n):
+        return n % 2 != 0
