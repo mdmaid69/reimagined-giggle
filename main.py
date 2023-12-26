9adfc6f3f625f01b7cb5b9185e43796ee190a67d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:28 +0000

    behind trick once suck rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
