0a89b77d6c3d3f41cc420b0384c081341ad0f313
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:56 +0000

    mad hold instruction bill dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
