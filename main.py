c546118ac9f2e5afa3137d3f434f8a0bd456257f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:54 +0000

    born decide repeat authority impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
