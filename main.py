0edc928088dd95c11b27fd92db90e66954d6d4dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:44 +0000

    spend cool goback(to) 572enter tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def add_numbers(a, b):
+        return a + b
