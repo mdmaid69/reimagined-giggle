0c13e660e140cc48b28954f56940288453e621b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:31 +0000

    sound number order breathe commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
