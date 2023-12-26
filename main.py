c36488facfb2139fffba144f7ed3d548a1d1b928
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:06 +0000

    explanation patient quantity breakout expect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def multiply_numbers(x, y):
+        return x * y
