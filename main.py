c44aaa159b69d8c679dccc55fb9ac0ef69b2a0f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:52 +0000

    process pull wet emphasis funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
