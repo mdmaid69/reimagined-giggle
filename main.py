902d8a296e7e86e46ae3f2182867f3b73129dd69
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:55 +0000

    away average Rr exchange plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
