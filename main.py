6f974708cfd4d3f89f1904914d4596769bf92c2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:00 +0000

    science lead 568energy put accordingto

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
