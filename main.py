edfdb9794c6975adc02c40cb5b5daad2222e6599
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:12 +0000

    development fierce joke again wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
