736c1eb7b150ec56a35593bc736611a91d6e2e16
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:09:13 +0000

    memory upstairs keepout arrive getoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
