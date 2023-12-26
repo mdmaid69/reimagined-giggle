3661ee33acb86ce66b5a2a002f1e3bd66e859b32
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:19 +0000

    worried ago want loud remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def square_number(x):
-        return x**2
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
