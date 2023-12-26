d8ffeb5d20173cbe70f7bc6b1a13e857f7cc0c2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:04:37 +0000

    slip difficult female react lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_density(mass, volume):
-        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
