2bfbf21e30813ce74b5d88a25a62f693adef76c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:54 +0000

    deliver rub sense meeting pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_min(lst):
+        return min(lst)
