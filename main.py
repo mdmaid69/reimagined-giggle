31a13a4f7bac011f1ac65612bbf3ba12dd37301b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:50 +0000

    exam sticky identify fortunate patience

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
