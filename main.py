d7bb2f0938e3e755b19e37c045f1d8ae5639b53e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:11 +0000

    concerned pinch commit story concerned

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
