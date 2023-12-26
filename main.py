a55ff292dc65403408ac6854a6946399ff86b7c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:45 +0000

    collect rely stop choice refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
