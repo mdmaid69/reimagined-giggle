127f17e344025fb11f753863eaebb94d72bf833c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:25 +0000

    loose yet ago design anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
