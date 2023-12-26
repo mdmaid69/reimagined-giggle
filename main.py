72700dfdfb3d11fd041513e0c0e603c6157d796b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:12:47 +0000

    letter thin ability task frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
