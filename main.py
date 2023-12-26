63cc61d2e9121ce66866ce7e4ae84eb3ea7a7f75
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:52 +0000

    cold put birthday means cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_str(array):
+        return str(array)
