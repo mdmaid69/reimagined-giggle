b1d0ceb0935b1b61bf8d5e0501f9ae4b350639bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:57 +0000

    treat eager scheme recover forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
