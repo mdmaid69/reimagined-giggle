d3e0027ab9b69249e38d1b42c40ac9b6ad67ad70
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:28 +0000

    between man gradually active pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
