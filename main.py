58339d94fced0a7b4216d1be5481b6c53acc4957
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:55 +0000

    disapprove science bang angry skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def reverse_array(array):
+        array.reverse()
