a64d3aee703a7b4aaabd2e67d41cb63d3e09942a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:13 +0000

    judge downstairs mind join music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
