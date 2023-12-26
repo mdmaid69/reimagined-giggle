5b85e953e733c061d5a4c4f2cbb29d587bc9dfae
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:57 +0000

    last cook shout stamp ours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def is_odd(n):
-        return n % 2 != 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
