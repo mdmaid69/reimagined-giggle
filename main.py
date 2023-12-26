2f23903faf3a0d7af83b459d3b3bb8b945677a0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:33 +0000

    bottom difficulty keepout declare keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
