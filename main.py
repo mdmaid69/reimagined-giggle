34919a5d98a22752d6f0555189b4587cb9d0dddd
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:20:09 +0000

    into week suit press unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
