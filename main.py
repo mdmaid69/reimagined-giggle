29be1ec4f1226fa29aeed00e221df7df61e449e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:43 +0000

    eat aim almost over about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
