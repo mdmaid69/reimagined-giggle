f45b638a7ef2244d7c83412426fe732701552d4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:48 +0000

    speech height proof deliver change

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
