73558ffa4353219f7130548e37e0d3ec00820bfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:04 +0000

    idea manage lookback(on) excited aware

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
