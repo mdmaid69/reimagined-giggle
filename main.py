5555cbfcef450c54dba45315d864910f9b9537c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:35 +0000

    one cruelty luck loss trial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
