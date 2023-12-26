82f5a75f675469c37047937db7c9f1a7be195bcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:08 +0000

    rely might introduce petrol relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
