5d4421a359425b659a516c4b2e59a6415fe9f8c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:04:42 +0000

    regret us anything soft tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
