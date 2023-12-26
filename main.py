115e9dbeea0b56f072e3df6df11ef74faf2240de
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:16 +0000

    attempt regret difficulty unless suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def add_numbers(x, y):
+        return x + y
