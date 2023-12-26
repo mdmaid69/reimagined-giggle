081d298b48435a8b91d409a9f09262c496591f4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:39 +0000

    wave worry fetch rate pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_odd(n):
+        return n % 2 != 0
