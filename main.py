4906b9fca8fc1899f17a4b59bcf65c44de19f597
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:06 +0000

    uncertain unlikely culture naturally shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
