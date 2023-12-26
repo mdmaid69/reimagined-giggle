722e4c472d70bcb9a2a73e7fff25380b2250e9f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:15:54 +0000

    very little fire smoke beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
