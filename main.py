0ab6ef1e1a826947a51388802d9c7f2fedec902f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:02 +0000

    bang explanation test well capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
