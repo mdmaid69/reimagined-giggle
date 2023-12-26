7873adb9b5dfa15046926d27dc0a9d60995ac947
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:34 +0000

    seem careless path health laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
