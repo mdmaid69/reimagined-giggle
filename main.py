8010972407fb2f0eb0f3c5d5968a66db2b2def27
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:16 +0000

    bread similar concerned miss Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
