311f8292fd558c6197fa09ccac94c3bb6eaba5a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:55 +0000

    reduction favourite cross sound friend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
