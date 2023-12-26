1f6939c556aac8c85b85716df2090fc585dba4af
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:34:15 +0000

    pressure unhappy sound heart radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
