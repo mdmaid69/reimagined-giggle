2d47aabb914c1a618a8e5bb6d795525aa2736e0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:19 +0000

    afterwards soft might mess ill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
