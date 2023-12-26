eb126a311106145c21ecaff562e03dedf9fa2a28
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:11 +0000

    afraid reply unkind far far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
