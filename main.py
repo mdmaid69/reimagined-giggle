c67faf0f36f5457a71237387339fbcf84a73aee8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:10 +0000

    popular or broken expression makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
