8b0d0d6f694883b6d3093223fda61dcb6ea2fbfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:35 +0000

    once plain begin tidy pronounce

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
