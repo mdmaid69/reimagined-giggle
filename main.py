6a81745cb91200d6eb3b6904d0b3b406fa9d8e4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:31:09 +0000

    crowded match concerned skin shrink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
