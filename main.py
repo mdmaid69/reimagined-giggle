bfa9cda1a87bc2dc927e00244931ae4628420536
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:48 +0000

    light regard chat edge exam

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
