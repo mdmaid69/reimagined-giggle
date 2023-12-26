ffcfd0483e3fcffef5bed0e51dd87070fd8c2017
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:00 +0000

    ring wave which score exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_volume(length, width, height):
+        return length * width * height
