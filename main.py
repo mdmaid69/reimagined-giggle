3da19941a822e66987ec94b51448d613b7ce27b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:22 +0000

    turnback me discover conversation safe

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+  def add_numbers(x, y):
+        return x + y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
