667f643774a314865bc59ed5a69e7ab9e96b9aed
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:42:52 +0000

    answer wonder mark else already

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
