605210ae91e7e7513a67cfdab04af07616096ac6
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:06 +0000

    hear confusing relax conversation display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
