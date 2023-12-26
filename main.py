5d1d35870f1aaec6861196691d64394b75fdeb92
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:10:01 +0000

    hurry coffee opposite through unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+print(sum(range(10)))
