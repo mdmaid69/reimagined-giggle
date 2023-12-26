a684827c7ddaaa731ff85dbbd856cd1b90b6872a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:01 +0000

    later mystery right step thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
