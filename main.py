67843491313a07bba6c64f537414d2c7ad9caee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:54 +0000

    suggestion downstairs during funny laugh

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+for i in range(10): print(i)
