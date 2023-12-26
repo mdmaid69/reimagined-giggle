f41f8230fbb0a44d0ee77f51917414b3753e736a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:39:27 +0000

    wrap proper poison bill behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
