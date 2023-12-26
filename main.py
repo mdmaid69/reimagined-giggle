d178961903ba88822ccd7b66048e96f14e28f546
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:13 +0000

    sign represent light goout sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_user_list():
+        return collections.UserList()
