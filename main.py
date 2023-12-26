4a0819877cfa8abb9dc7516f3780e260043bf8ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:54 +0000

    arrive he pool cause machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
+import sys
+print(sys.version)
