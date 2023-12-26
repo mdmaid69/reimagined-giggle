e8dca249d1bd5f7c3d6495a76fdf31a5ed4481aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:44 +0000

    we wear comfortable suffer healthy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
