04a8f31d21bcd312960141eac95520574d435ffd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:39 +0000

    confuse lookback(on) meaning person innocent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def add_numbers(a, b):
-        return a + b
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
