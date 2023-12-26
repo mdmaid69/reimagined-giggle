c71033e6db847d610afdba3ab6d5f3c718619490
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:59 +0000

    everybody town shade drop oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-print(sys.version)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
