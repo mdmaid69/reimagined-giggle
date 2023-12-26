f36d38c1c55f65b421d65bff1fa180ebc9977b95
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:11:16 +0000

    soon well beyond claim suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
