892dcedf158601bc8ecc4e59d638a0a43aa1bfbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:15 +0000

    material Cc tidy otherwise thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_list(lst):
+        return sorted(lst)
+import collections
+def create_queue():
+        return collections.deque()
