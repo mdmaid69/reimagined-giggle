bc21820c8966637c79a7aa1a51f36bf0af173b94
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:25 +0000

    equal square something better drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
