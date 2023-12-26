1ab11e05b3b9dcccc13352853a51c5f5f5b78f6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:34 +0000

    kind late burn road tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
