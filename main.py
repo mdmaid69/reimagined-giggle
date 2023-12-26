33c51e76f97a9324df20997d32824ea5bd299461
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:24 +0000

    dull nose explain content recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
