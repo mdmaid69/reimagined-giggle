034c7d9b31984d4c616b79cb6be12155441c252e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:53 +0000

    head probably country lead scale

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def reverse_array(array):
+        array.reverse()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
