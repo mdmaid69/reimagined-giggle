b461445db555bb01844d234eb63e11bb09747a20
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:45 +0000

    thing limit duty embarrassing refuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
