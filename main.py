1d30205bab0643953c5ad92776221a089f8cc0fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:55 +0000

    gradually reply meet sight come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import sys
+  def get_python_version():
+        return sys.version
