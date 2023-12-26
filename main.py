d6af1f38907bf8999633d28a4f70b66ccbe3bd5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:10 +0000

    message answer relief understand bound

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
