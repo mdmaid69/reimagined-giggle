3cb3c571ac4537acbe8554b0434944264ac4c65a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:43 +0000

    shelter tired ignore fight copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
+  def add_numbers(x, y):
+        return x + y
