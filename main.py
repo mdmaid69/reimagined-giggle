7646ea329ca9bebe592d478343fa297908e4405e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:05 +0000

    building arrival agree he hers

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import random
 def shuffle_list(my_list):
         random.shuffle(my_list)
         return my_list
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
