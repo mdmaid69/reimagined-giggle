16742a64d85fa41bf75789da257a9ce485fd6165
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:56 +0000

    free turndown ashamed holiday human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
