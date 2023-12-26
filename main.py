e155d82a7d6d1affc07fb83a32fac7a1ec973ced
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:03 +0000

    no opponent home let wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
