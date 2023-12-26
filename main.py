2bd6aae2acd0953ccfaf9f339538b8e42dcf1250
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:44 +0000

    tragedy myself material some cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def count_words(sentence):
+        return len(sentence.split())
