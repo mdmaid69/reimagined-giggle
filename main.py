155d04f2098bad782b6dc2e7c5d52ea81fa84bbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:53 +0000

    Pp knock fail uncomfortable lunch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
