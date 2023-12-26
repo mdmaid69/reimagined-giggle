dcfb7142dacc069ff3cb1132f41645a92db5f55c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:40:17 +0000

    realize standard do pity comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
