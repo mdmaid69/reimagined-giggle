6709bd87aca2bd0c69e01997155c4628b6c18e04
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:23 +0000

    proof border bus boring ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_set(array):
-        return set(array)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
