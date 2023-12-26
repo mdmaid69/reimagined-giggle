d6cd26cadc0fd4346293976cbf5dc845a598d70b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:52 +0000

    direct ability bringdown job juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
