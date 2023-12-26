e0a4a64caf2a0e921d2506adde92ca2311adecff
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:28 +0000

    idea snow force aim reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
