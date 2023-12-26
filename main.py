bb2585a36c8ce3f1a8e4df68375fe5cc7afe5b5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:24:07 +0000

    split out our smoke muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
