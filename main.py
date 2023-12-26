1785e16606236512ed24017e1c37b4d4595447f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:48:06 +0000

    nose left any thin big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
