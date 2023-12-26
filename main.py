e9e6242f72ed8ca9bd9bd62b6480af7c9a292449
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:22:29 +0000

    stick Pp regular notice maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
