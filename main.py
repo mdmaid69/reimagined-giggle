6b7124610407c5c38c6baa5040c8fc856abcd64a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:12 +0000

    frighten introduction unhappy workupto scale

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
