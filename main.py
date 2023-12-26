889d788534d8f35c66f2c6c84ea8afc4fa86ff40
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:29 +0000

    spin brain outside recovery nonsense

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
