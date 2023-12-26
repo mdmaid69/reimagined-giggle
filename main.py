5947bdb08d35bd9e3180e6ce56b3df4017e04ec2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:11 +0000

    agree recognize almost drawing forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
