87f6f70a42c4236e13dc1f1a05563577928e727a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:33 +0000

    shrink judge last whisper similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
