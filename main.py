3cfd79399c620a5f84bb0f703165a358257139d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:30 +0000

    healthy height firm underneath beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
