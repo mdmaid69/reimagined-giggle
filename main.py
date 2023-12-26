ddbd21c2d92ed596ba1f890b80f091fa34193445
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:44 +0000

    confused chase Yy 569enjoy generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
