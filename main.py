514a574b077bc92c8191bcc9136e50ec63d371c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:16 +0000

    quickly discovery touch thirsty bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
 import os
-print(os.getcwd())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_environment_variable(var):
+        return os.getenv(var)
