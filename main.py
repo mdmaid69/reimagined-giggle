9e7ba6f99462cf7b56cb2ccd60e216a802bf1fd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:27:05 +0000

    reject female respect Tt tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+print(math.pi)
