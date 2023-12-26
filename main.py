406f16fc2f4426afb6a386d050fb59a87191b70d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:01 +0000

    level successful move more ocean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
