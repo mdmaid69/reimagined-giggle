decbd78768c38fa5cf71eafee7db6d2705bf0f37
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:30 +0000

    sing introduce introduce every black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
