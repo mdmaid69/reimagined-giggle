5f284281b1b964e266b91e02eea0941ed210f0c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:14 +0000

    relax understand smooth process offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
