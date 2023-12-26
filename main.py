b822cb79e43240ea3f805d79a657ce56c29b6c6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:06 +0000

    fact way takeback underneath meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
