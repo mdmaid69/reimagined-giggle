88214b46abe83f9d52769bc773028fcf4b08ee6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:25 +0000

    maximum worth attention regard of

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
