66e3b6473f08cea77c0cbe4e3126fa9b82637c80
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:49:42 +0000

    someone getup favourite seriously contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
