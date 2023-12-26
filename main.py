ee8c7fb50934690631b88d0ded9f28f3a73a3310
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:18:40 +0000

    part school into how play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_bytes_from_array(array):
+        return array.tobytes()
