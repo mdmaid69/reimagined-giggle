afba40eba96212d23ef3c5fb10fcb0dfc2451995
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:28 +0000

    effect mark takeoff break that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
