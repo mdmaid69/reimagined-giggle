a200d02c5f5ee6e8f9807cecd12520e4f948a942
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:29 +0000

    body speed cap aim afternoon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+print(os.getcwd())
