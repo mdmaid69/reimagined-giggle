3a994c1a4e179aac6c917c000a8ed6266fb02f69
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:52 +0000

    concentrate active lonely trap possibly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
