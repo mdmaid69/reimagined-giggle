14a583087cedeecf967163cebc92ba19fbd8d60d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:40 +0000

    port seat Ss act reaction

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
