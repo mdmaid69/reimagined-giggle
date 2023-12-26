bc3a127606d6c09b958aea025cdf8cd4d30fb90b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:47 +0000

    dare pronunciation block keepout kill

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
