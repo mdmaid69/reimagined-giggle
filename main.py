baaec063e85246d88e440c6a4d45e1d50b27c7a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:50 +0000

    past accommodation cruel shopping spread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
