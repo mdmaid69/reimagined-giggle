5aaaf52fa3dd0f3ef13fbf5d8beebfec1df8036c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:24 +0000

    suffer eat ocean central nurse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_working_directory():
+        return os.getcwd()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
