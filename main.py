803476953dd9fabf9da150bb6813528aafba2d57
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:24 +0000

    suit declaration middle sore hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
