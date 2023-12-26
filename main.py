b73f1f1bc46a775679c90646c90f40aa2f964c65
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:10:48 +0000

    effect put close capital sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
