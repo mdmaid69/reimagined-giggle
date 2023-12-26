8fda152df8f561b5d7b67582834ee9028bcf1309
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:56:03 +0000

    tune dead proof lean collect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
