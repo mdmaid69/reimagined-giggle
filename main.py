ade9dd5c6b8683ed646da4a57dab70e2aff1cfbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:54 +0000

    cruel brown disgusting join deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
