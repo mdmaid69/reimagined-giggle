1c7afd3e52a32ddf84016bf774b662ab449f4896
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:11 +0000

    score concern well guess need

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-print(os.getcwd())
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
