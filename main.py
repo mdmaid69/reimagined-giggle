1f5ffa1cb0dfed7b7d6901d6867970aa4ef1673a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:03 +0000

    date spill strong exercise second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import tensorflow as tf
+print(tf.__version__)
