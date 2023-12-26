9182fc2ff383c64b1bb1b2e83b731d4a8d7a1f7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:54 +0000

    keepoff memory somehow age Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
