cb97990209671b75502bd7dbea975dad9f527eac
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:38 +0000

    skin replace idea worried almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
