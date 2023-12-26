6fcb83776ad5e90436bae47f38226ca7064a7bcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:01 +0000

    skirt easy sport nothing ignore

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
