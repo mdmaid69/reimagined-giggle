a7bdd7194395e553f8bac9d54abdb62e812fdef0
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:51:36 +0000

    against news bang 571enough ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_item(array, i):
+        return array[i]
