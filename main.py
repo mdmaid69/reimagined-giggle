3e54132db210e8fe8453da5f9da574a34700f778
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:40 +0000

    drawing giveaway apologizeapologise avoid petrol

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import random
+print(random.randint(0, 100))
+def is_odd(n):
+        return n % 2 != 0
