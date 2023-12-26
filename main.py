01abed8f97b8d690dcc73654ba9a4d218f5a99fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:47 +0000

    my calloff principle skin glass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
