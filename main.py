cd970302dedfcadd18d270fc7813cdbdbffb9609
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:30 +0000

    Gg right speech phone disapprove

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
