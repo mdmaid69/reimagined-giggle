3a798f9cd28768a69dde7993553e37bcef3f3f68
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:16:35 +0000

    mouth shallow motion bottom promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
