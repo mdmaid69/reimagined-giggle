e9653c59c78c397da24436e855707153b6c90e3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:23 +0000

    dish scare produce love lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
