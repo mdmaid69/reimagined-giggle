afb22acfae9fd096ca4cc485ef8b7c187a50bcf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:35 +0000

    cut sink eager official leg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
