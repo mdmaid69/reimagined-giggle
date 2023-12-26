7e9a4bed1d6be391be278b199e25a3f83c2ef5c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:00 +0000

    pronunciation spring sink mixture shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
