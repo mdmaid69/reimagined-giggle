ce53f5da8b94351339bf66a369c7ba8a0e99ccb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:21 +0000

    fierce detail grade laugh tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
