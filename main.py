a5ab1c3f0c90da92d6d4c0a0b48693f93eaf5388
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:44 +0000

    on personal pile naturally window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
