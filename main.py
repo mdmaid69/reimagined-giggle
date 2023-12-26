23f8a4191c60e2143d47564026316fee76c28ee5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:44 +0000

    position bank bright squeeze imagination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_min(lst):
+        return min(lst)
