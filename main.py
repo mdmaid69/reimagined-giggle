bc1335440d6825aeefd6667414b9d275fe10e777
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:00:06 +0000

    correction bit breakin politics happen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pressure(force, area):
+        return force / area
