be0d9cd820133dd7eda5114e70b6b1848b956178
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:15 +0000

    past hang explanation word warning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
