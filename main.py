2bacee87527d8517f48212cb07810bb57aa6710e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:59:14 +0000

    listen other transport party empty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_item(array, i):
+        return array[i]
