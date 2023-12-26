5756999362aed9213824ec6623945f14fe533519
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:13 +0000

    neither consist this front talk

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
