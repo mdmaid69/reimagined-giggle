4d3ef3ddef7d77491a615d3edeb9cb00676d5426
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:33 +0000

    sleep still concentrate wander announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
