460e0a4b1bd2a2216a23103cb69b0108a70ffa46
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:29 +0000

    jump ask edge match already

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
