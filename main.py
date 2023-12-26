9b726694f5e14a0150180293e6562b656c97c3e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:11:18 +0000

    sore substance lookback(on) fortunate bother

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+import re
+print(re.match("h.*o", "hello world"))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
