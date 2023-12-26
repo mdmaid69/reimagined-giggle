ab5604873ec4b48ac1b4ddc2790910bce5157c83
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:02 +0000

    interested company breakup mystery benefit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
