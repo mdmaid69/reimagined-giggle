02c6188b2e38bf3605fd321e1d3c01f21a002d00
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:02 +0000

    for instrument takeaway discover physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
