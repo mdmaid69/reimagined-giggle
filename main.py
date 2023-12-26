e9e3de89bf51e60ddc6129f841f300f0e8921693
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:38 +0000

    level suppose plan me lock

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
