bbd5026afa41b158cda1c52e76cb9536b56806e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:05 +0000

    responsibility sport impossible immediately only

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def reverse_list(lst):
+        return lst[::-1]
+import numpy as np
+print(np.array([1, 2, 3]))
