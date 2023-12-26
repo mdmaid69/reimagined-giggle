d20d87176b84f8a28fb0bc05a46f3bfc796c8cdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:55 +0000

    sound space hurt circle justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
