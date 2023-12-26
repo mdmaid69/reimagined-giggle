c1b88d8e7ec204d221806cd696af81f6ab45f476
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:18 +0000

    whistle expression benefit sea consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
