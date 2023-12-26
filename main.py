aeb096158afddff5a43c7b501456675d672aae08
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:44:34 +0000

    choose sentence bin sing apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
