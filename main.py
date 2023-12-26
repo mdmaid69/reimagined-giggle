b989a54d2a5a1da4000f72eb762c2c08501d3a7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:26 +0000

    telephone prevent voice large Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
