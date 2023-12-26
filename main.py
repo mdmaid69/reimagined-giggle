be6d227bd8b42d71d760cc57caff6725d77e0f1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:29 +0000

    progress mouth future spot hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
