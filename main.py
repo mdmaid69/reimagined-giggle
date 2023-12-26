430939ee8420d73d6345e8f7be426cdb613f8590
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:32 +0000

    my under lesson vote wall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
