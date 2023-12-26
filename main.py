2fbaae821fc176452c35ad0c5d5e5ba388ee560a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:26 +0000

    permission walk which drawing breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
