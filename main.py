c642758451ec225631055058450b298ed208d1ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:37 +0000

    late exploration point female brown

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_pressure(force, area):
+        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
