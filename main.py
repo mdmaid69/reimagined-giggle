cf7a4530f4e4e66d62e4ff7be49941eddc4efc74
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:39:12 +0000

    punish just today turninto food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
