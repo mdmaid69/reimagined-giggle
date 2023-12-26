b1cac95566a888a44d7186a42da86004c86d0fd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:24 +0000

    typical bury region ask cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
