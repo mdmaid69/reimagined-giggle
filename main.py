53e369e6437f442795c8fe2d871928cd4fa1b4bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:01 +0000

    slip level doubt receive rent

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
