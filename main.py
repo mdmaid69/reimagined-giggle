d5a567646975d4fe9e2a3dc2ff303491e57f87e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:23 +0000

    class lot schedule hand should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
