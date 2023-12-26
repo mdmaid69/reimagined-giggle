b750e32bcfe14365ba06c5cabf379771950ec67e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:09:54 +0000

    watch brother band smell tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-print(os.getcwd())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
