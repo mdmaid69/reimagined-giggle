e8cdb77e76268fb94825958ce9d320220efea5a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:06 +0000

    really afford write roof admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def split_string(pattern, string):
+        return re.split(pattern, string)
