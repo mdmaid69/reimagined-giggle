907ab316ea86f770a7dde7354c7049788e26cbd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:11 +0000

    nearly regional minimum complaint hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
