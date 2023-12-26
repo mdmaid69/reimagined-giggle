62c2074330260d42541b921866dc28fcf75e0b47
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:51 +0000

    side smile right apart approval

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
