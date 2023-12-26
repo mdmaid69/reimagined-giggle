7e867e660754ef30ace44bab0a38244ca2cc8462
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:28 +0000

    fly actually judge middle climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
