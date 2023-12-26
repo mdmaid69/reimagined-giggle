0641d61a857780f1b358baf9ff273935ad3dcfb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:22 +0000

    sensible tour belief rather herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
