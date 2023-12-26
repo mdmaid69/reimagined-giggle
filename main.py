2c1e72c34c2b73b3b93ce3dc3e8c7c5cac613e71
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:58 +0000

    reaction face point so ought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
