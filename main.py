08b970940a13a20f6d9c343a52f5df8199b51522
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:48:11 +0000

    downstairs guess just plan keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
