455b9c8928ca3145872a0e368094f37d1f822f15
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:40 +0000

    fork top raw commit respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
