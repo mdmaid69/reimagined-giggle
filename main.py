473dda25452dc93b11b674d8165aa92fd95c008f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:47 +0000

    flight rest he false crime

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
