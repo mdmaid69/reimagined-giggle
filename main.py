752b2dd9ae878873cf3a1a6f67355d41e6ad6b39
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:00:02 +0000

    trip word art grade quick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
