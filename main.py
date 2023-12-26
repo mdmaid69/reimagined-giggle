e8ed767fc50d15577e1a54810d7bc2f230fd9ab0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:30 +0000

    telephone shadow until punishment course

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   def cube_number(x):
         return x**3
