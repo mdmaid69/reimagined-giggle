bea91e7eb334e006901069ca22207689ea4e2297
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:34 +0000

    from similar seem succeed communicate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
