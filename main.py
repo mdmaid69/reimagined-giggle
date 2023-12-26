a45585bf7bb4403941b28d10d9dc024c957f8301
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:33 +0000

    unless luck yes squash game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
