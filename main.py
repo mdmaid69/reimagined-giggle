e50a91ffa69cab1529b80d7bdf1473c99bc68a5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:24 +0000

    comfort brown factory fairly workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
