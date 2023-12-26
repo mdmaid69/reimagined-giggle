63c76c07070321e92fb30fbe010c0ee7ff873115
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:40 +0000

    breathe why twist Dd exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
