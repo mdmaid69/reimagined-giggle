35435c8dace82b30950ba52fb2132428f31ca9b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:52:42 +0000

    now hurry swimming zero bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
