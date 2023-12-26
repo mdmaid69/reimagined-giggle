aba61fe7516721c1831f94149f65332a5d9a3f17
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:41 +0000

    getup dish none giveout cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_rectangle(l, w):
+        return l * w
