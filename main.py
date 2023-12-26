8bf952b5f9a9e5cb35aff482006afc89f6d21e51
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:53 +0000

    abroad luck risk so cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
