785a175c260823bd7d4cd1a6c583cb301ed057d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:43 +0000

    juice fork pretend brain our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
