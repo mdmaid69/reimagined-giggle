334bb1fe38ecce09eb8e66a04acdc88ac2ce9abf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:34 +0000

    apologizeapologise fall deliberately turnback wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
