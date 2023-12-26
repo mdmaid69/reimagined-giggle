4ea46e6a257b1c95c51e1620f8a6b684c07a5897
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:26 +0000

    comfortable size plan comeout sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
