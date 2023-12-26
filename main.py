0d4c558279cf437d42e70f7f105dd1cc325574af
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:08:12 +0000

    crash either back introduction takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
