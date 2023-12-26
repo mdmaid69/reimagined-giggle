503c0f8e6e8f43053c9e91cb342f1f2a60bc4025
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:19 +0000

    push second shallow bury true

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def square_number(x):
-        return x**2
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
