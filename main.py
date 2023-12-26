360268cfc6e70d6cdab9bb2a59be8d943b05b383
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:02 +0000

    regular rule myself soft weekend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
