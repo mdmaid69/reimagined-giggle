f33b0ecf1ea09a1cd1f4697472938ad195c5440b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:03 +0000

    shadow polite ill bright divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import datetime
+def get_today_date():
+        return datetime.date.today()
