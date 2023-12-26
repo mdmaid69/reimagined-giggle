52263ffe0bbbfe644c36e7bd776861d0d16b599d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:59 +0000

    fault delight ground wide smile

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
