58748e3e64d9098cca98e9bae7dba2c32f510b54
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:03 +0000

    top solve emphasis among round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
