5eedd46ca0d2b4974d5844fb50ca44415223253d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:29 +0000

    one convince wrong practise fortunately

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_floor(x):
+        return math.floor(x)
