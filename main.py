3c2ebb84b2a8f2e71cfeb2746900e61a454f7381
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:44 +0000

    house direct duty matter lookaround

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
