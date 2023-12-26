58782438fbc2d293a8c946758acd2702757c1625
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:54 +0000

    crisis throughout cost approve holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_array_as_int(array):
+        return int(array[0])
