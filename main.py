03b2c93ea845381c293f5d40b2507666b4d9807e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:48:33 +0000

    back cover true wall evidence

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+for i in range(10): print(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
