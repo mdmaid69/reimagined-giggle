7a54b36801a395065417a1c61b02646276c6032c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:06 +0000

    hang steer fit influence cook

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
