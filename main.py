398480e8145cfa3e59eeb0a43acf583dcaa720ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:18 +0000

    term lock kind swallow sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
