5add5bef8b6469742491acbe89ae9cc17b8758e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:16 +0000

    pollution right suit suggest dance

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
