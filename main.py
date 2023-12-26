22d618a79bf1b87477a5ad27b2829a54ef1316f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:11:06 +0000

    valuable lead supply treat public

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import sys
-def print_python_version():
-        return sys.version
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
