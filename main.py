6f2410856bfd4e3473dcf7ec132011fa2c616815
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:24:03 +0000

    correction own otherwise thick last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
