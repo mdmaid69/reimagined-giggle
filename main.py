3f65aeb3035a845ecc01f145bce8117346ae75e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:48:03 +0000

    so phone breakfast shall concern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def sort_list(lst):
-        return sorted(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
