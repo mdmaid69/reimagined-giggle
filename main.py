849ec2e75a74d6f53dfed4b51b0981e92e809a0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:05 +0000

    other boil matter fasten large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
