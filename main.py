37e28456643c9c5182c8f16b541b4e7f668b892e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:13 +0000

    money 569enjoy sudden sort innocent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
