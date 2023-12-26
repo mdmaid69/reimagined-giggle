329e152404e0b4ea647d11e639f66585943f5ebe
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:21:43 +0000

    not star meet affect pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+for i in range(5):
+        print(i)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
