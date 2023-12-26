c3ffa204d8924f753f63dcd91fab7cce1e7c8a88
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:40 +0000

    kill joke answer unfortunate she

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
