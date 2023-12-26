8e1b3f381f37608fcbc29ecb24c0795a641d091a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:56 +0000

    bag concentration future offer search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
