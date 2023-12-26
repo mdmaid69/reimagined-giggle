ceea8c5ba4ce25be0700cb30e45702fb3f0d0f1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:08 +0000

    crisis Bb goout snow awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
