3c367c8cfd0bae641768f70e319ca7ecbe350a04
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:56:26 +0000

    square able table funny no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
