75d5cb0e438dc900f08c37ef7a801abd79ec5aa0
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:17 +0000

    may speed language lay forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_list_from_array(array):
+        return array.tolist()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
