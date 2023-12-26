e10d9e77763c9bb461f754c8ff3e176773e25684
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:59 +0000

    front defeat sport hair responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
