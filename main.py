35329db8b7c2fe4445b2c53964730be0e0c27673
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:46 +0000

    leave lend date too later

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
