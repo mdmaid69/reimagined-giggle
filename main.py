72766e4e508915237e037172ffbc06495adfe32b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:56 +0000

    medium face reject clear effort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-x = 10
-y = 20
-print("Sum:", x + y)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
