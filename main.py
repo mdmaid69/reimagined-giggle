46b17fe52cbbfac30277210bf6845b6c844ccf0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:35 +0000

    large whichever shop read back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
