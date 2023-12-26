07976e60cfcc8c97ce29546dd9a2e612cb6e7ec7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:05 +0000

    small join dig direct trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
