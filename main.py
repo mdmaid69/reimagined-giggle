6bd52965e5d878032a85b04e1cdbffaa3a653f38
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:24 +0000

    but tonight work traffic tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
