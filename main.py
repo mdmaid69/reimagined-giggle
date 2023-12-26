2160143694c455bcf3003961c6e53ce1903d14a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:48 +0000

    violent rank active basically cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
