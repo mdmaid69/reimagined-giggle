8a6365475b767d416b1fad1740d749229ef9a6e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:16 +0000

    mainly later away breathe for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
