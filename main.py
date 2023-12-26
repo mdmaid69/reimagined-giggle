42db7977a447089df2fcd7563af0087f10411964
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:05 +0000

    but turninto cruelty interrupt blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import time
+def get_current_time():
+        return time.time()
+import os
+def get_current_working_directory():
+        return os.getcwd()
