dd688518b547f774b3a9be3386aac536f0cb593c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:37:46 +0000

    land 565end common top region

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
