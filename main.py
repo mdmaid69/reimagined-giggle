45121e9cc3d16f350ae2299b81ffac75dee41a29
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:13 +0000

    care proud to recognize Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
