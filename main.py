4dfc0ff385437df02e626b2fa4845c7cc4b3414a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:05 +0000

    quiet limit pinch task deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
