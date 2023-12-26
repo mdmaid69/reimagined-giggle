70f98c0167fd7a8f39cb011a6bbd7ac4f69354f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:26 +0000

    secret stamp big small knowledge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
