a0949a9a6398127674d2f0231b0488e494659d13
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:37 +0000

    prefer interruption turninto central character

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
