0f58e541070eaa03a70422611e8cda0fa6a05d66
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:38 +0000

    muscle clear behind free back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def convert_to_hex(n):
-        return hex(n)
+  def multiply_numbers(x, y):
+        return x * y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
