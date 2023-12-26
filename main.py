c80b5524cdf5f6f2dc161a8cd43f07e4172b8bab
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:45 +0000

    breakdown set earth smoke marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
