3526d322c974d4aad84b16d6e3d298bc34c1deec
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:09:20 +0000

    kind play wild anything argue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import platform
+def get_os_info():
+        return platform.uname()
