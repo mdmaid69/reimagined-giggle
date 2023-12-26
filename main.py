247528ca89ec1629882678f3ee58712efd279f22
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:44 +0000

    accidentally main pile complicated ring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
