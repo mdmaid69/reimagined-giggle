f5d5d906cbbae6a07ba6aedfa5a062fe0390116b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:13 +0000

    behind phone lose dangerous all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
