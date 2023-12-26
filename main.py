843782b3e7a925e6d2722c3eb9e0acd571cfe184
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:15:07 +0000

    confusing thinkthrough forgive sticky join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_list_from_array(array):
+        return array.tolist()
