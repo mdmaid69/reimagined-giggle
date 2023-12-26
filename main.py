24b7b4085150e0db53875e1196035c49f40c89f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:19 +0000

    downstairs press event but string

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
