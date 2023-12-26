19089d174f8e082d5905c96c0277223a4dc779e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:34 +0000

    rarely half takeoff swim unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
