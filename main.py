d841e5ea636b432bbe768ace5216625904802cc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:41 +0000

    suffer recently sight maximum family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
