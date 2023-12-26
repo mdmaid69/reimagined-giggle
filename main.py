918531d1ae203fcb8e3c901d03f942121de2ed66
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:48 +0000

    confidence welcome recognize emotion 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
