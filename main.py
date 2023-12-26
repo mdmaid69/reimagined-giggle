f500bc52315a98daca0ac85774e9b958f312e5a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:07 +0000

    store mysterious criticize lunch now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
