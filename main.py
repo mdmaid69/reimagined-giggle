42fb52e40ebb7f6e40d34edf0e2fa7309db00369
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:39 +0000

    many date invitation strength rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
