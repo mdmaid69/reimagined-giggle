7b968dee8cd3011293b6b6323bc9472cf34f02c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:28 +0000

    exhibition impatient word judge medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
