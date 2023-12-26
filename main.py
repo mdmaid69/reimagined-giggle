67a064253c6025afcdae95328445fcecd64307b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:24 +0000

    distance science border original charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Reversed:", text[::-1])
