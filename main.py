d2bcfe2913223b96dde80b6807f7f17d5f8e7e0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:56:24 +0000

    many swallow development miss possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_item(array, i):
+        return array[i]
