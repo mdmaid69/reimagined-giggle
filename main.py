a25daefc0c298e0744e0f4de0b832733b222bca2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:30 +0000

    behind over pronunciation peaceful skill

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def is_even(n):
+        return n % 2 == 0
