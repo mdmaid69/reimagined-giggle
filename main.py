158415664759af68bd2f7571e4c8f81347c6f094
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:33 +0000

    fold handle Zz culture clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
