62cef72b6071b1185729a0d34fe27f0e3a53648b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:33 +0000

    plain visit pipe instead machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
