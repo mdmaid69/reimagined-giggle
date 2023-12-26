4f1a9e2df0281a43ce620ca73d3eafade07337b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:42 +0000

    manage early talkinto separate completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
