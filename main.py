1d2f4919ec7a8b0fa14530c40fb1fe639e7bff7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:25 +0000

    petrol law scrap typical excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
