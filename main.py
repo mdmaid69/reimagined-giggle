73fd6be77133cb57c99d70d5daedeed39376f0fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:22 +0000

    thing blue minority duty amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
