5f30d9738916184f83b3831657ffbd159fbe5a6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:36 +0000

    complicated sound bite moon full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
