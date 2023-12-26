07d4390f1e20d9fa9d2b0f4605c766f3705cef79
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:55 +0000

    label service dick mother feeling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-for i in range(10): print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
