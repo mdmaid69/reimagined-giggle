36d77919d502be930a56ab7add4e4f54854db2e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:58 +0000

    actual tool public phone ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def convert_to_binary(n):
+        return bin(n)
