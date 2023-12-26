978a72512bed1d0503e7ecf1e4fb25217e65f953
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:56 +0000

    where talk boat frequent swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-for i in range(10): print(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
