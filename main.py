b1bf4fccc1a97a3e7ddaf11c68db53fc2be5c87d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:24 +0000

    tend goon callat action realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
