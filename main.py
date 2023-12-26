0dfc59219d20a94c18aacc0c407f0543e809f6bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:39 +0000

    majority satisfied first effect damp

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Hello, world!")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
