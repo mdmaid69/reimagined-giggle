ca3f9e16b9eae336c0eb381981bafa2036d364a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:39 +0000

    communication yourself innocent blame still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
