c85b4fcddbf2e530139fa23a0cb2a1c748ed0223
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:57 +0000

    give shallow runaway effort match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
