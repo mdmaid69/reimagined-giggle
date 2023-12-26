6db4214e0200e56c95e61ec6e1361fd59172bbce
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:40 +0000

    support payment crash continue board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
