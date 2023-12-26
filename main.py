dba54c90ad3f6517278c7201102c9e56f8a3bec7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:50 +0000

    Jj not tend please sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_repr(array):
+        return repr(array)
+name = "Python"
+print("Hello,", name)
