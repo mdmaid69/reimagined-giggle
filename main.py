6a488e41fe10090319634e087acc8decd50c980f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:11 +0000

    underneath difference underneath whistle another

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
