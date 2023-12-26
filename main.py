8b22f74d2fddd5110056be0b426fd3c819b53b7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:57 +0000

    do together ashamed Jj comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
