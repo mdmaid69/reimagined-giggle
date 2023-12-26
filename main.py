508bde92deabcf89d1739a1373abeb37d17c6e29
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:24 +0000

    heart intend practise hold phone

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
