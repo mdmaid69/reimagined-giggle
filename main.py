17af5f9ca06bfc88665a161a81a303742ba3afb1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:58 +0000

    group holiday tear fire lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
