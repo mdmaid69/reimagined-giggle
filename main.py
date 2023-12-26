ad2720963096c604fe6f4d248656751574d0ade5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:47 +0000

    watch delight blame within die

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
