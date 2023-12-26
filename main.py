1ba534c71ce0585f2a9fe2829bec89c39b918c83
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:49 +0000

    cheap female straight lesson stone

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
