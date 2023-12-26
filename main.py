1a0288d3fca6a3d91dedb894f7edebba23e94462
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:08:31 +0000

    field reduction this emphasize later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
