dfc72e34ce1eb22336e908a129848bdff0c8dcae
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:23 +0000

    occur now satisfactory day discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
