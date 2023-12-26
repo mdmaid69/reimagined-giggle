6a48535f11048ea1d638786429701fce1fc5b876
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:27 +0000

    tooth basis defend school mean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
