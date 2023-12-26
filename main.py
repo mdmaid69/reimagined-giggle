04685ac87475a2bc3986f64642da94ffa2c7538d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:53 +0000

    well basis word relief comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
