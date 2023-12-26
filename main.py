6a613143a75745c6661b3d1e798fa16c1d61ec18
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:28 +0000

    different illness sail accident mood

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
