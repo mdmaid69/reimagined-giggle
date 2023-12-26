0a67d09f720a17319ed1b5f693db928fad4f9fa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:29:59 +0000

    someone leave goodbye turninto bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_speed(distance, time):
+        return distance / time
