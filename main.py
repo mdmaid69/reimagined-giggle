5bf48c73fc5195ee945f8f8cbee29d7e82354f71
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:58 +0000

    careless will bar key book

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+from collections import Counter
+print(Counter("hello world"))
