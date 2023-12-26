cfa382faab8fd59fbb8034d6cfe579b2dd163392
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:46 +0000

    safety last work mail objection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
