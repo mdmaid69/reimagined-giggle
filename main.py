d062eba6fa3ee0729f5b92ca2231a8e029f6966a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:29 +0000

    agree along life effect blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
