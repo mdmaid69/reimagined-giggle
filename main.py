19f3745a10b8649c790edf5a7088fa3c63c1ad4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:56 +0000

    right sick importance relation flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_perimeter_rectangle(l, w):
+  def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
