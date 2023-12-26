7460cd14c7c936d23484e27f3539e18d0923b15f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:14 +0000

    arm thinkthrough ordinary no 568energy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def count_elements(lst):
+        return len(lst)
