b131b597a26dba2faaaf2dc0115447bf0f716784
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:59 +0000

    record opinion excellent shop ashamed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
