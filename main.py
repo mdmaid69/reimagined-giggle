2bf40c42d7de41510caadd06e1803a57e9b1a5f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:59 +0000

    tax excuse system against smile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
