cd24aa596554575b91d016b0eba067c9b1791717
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:14:48 +0000

    talk gradually boil concerned possession

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(x, y):
+        return x + y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
