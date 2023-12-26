cc3b9d9a4d1943f9acb470f815674b1224dd1349
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:50 +0000

    onto pattern season lead pour

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def clear_array(array):
-        array *= 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
