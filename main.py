5b9b6de8688b1c684719b17bca5e7388b2c6aa70
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:10:14 +0000

    once fold takeback friend impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
