be911962152b006385967141ac3d88313602b3af
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:59 +0000

    tear priority bridge affect coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
