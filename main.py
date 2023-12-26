28c14d72392057f5d5315b19543fab3d78995177
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:42 +0000

    account contact risk left seem

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
