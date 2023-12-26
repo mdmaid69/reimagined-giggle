d05e642069d3c51cc51e4d7b44992304aa809268
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:25 +0000

    hurt minimum save dance contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def count_characters(sentence):
-        return len(sentence)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
