beedd4b68005c6655be1b3eed1ff54a85455198f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:58 +0000

    up kind illness runinto popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
