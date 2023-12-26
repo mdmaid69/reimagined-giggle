fb0fd2dc04c9e30ef268ec64aaa38599059da1af
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:45 +0000

    hide which roll small though

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-print(math.pi)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
