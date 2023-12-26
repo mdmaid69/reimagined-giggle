f5385e1d13d85fe1e0f58f81b10d6c1fcfed903f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:22 +0000

    suggestion giveaway excuse passenger behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+for i in range(10): print(i)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
