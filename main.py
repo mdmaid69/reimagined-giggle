4bcd88da31d5754464e75a77d733167506f0d356
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:12 +0000

    bunch embarrassed watch tragedy matter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
