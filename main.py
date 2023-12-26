a89d5166bee4bc02c247ed1d06accd891098918a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:16 +0000

    finish forward nerve great settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
