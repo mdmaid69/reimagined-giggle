86e3f66ed396f2701af3fc3f50f892c6cd4060d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:33 +0000

    gather pride Mm recover damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def split_path(path):
+        return os.path.split(path)
