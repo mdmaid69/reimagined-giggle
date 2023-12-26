949a143619bcd35d8c1367c25293f6807419efd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:45 +0000

    until bringup shame recovery pile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
