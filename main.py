372a8f2d150dcf35473f427114090f3e92145988
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:49 +0000

    convince taste interruption sign shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
+        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
