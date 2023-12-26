7805cc984a2497219991b89dec1761959385a0e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:48:12 +0000

    whom fold difficult average centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
