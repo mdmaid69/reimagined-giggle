a6671735088291110f42cc8e19bb65d28538b271
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:31:34 +0000

    thin fashion plant even remain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
