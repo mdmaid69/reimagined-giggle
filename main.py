fcbf8c72401f904a7a6f416432052250d47e7a44
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:49 +0000

    judge uniform discussion length turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
