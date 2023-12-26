063d71f7a7d8ac3f4e536a0274d9829bba73a52b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:20 +0000

    clean actual breakthrough less alone

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
