42ace0443c9c840b23d3cbb75d900f1d3429bfe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:16 +0000

    inside turn inside unless most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
