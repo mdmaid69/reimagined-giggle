3529bf95da9a7116920a251e7bd20e89b467fc3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:09 +0000

    tax short soak bread sore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
