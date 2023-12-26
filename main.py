ad3c915fefa88bc64f82d6424cfae002eb9f6501
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:43 +0000

    object would between OKorokay trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
