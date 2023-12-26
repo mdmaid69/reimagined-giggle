d1a95ae6255dbdf583e08123f81290fb294b4117
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:15:19 +0000

    impatient tap natural mad afford

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
