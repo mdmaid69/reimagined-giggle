e77e8f6d3d7ca3914ab382f02277f68fc9351a52
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:31 +0000

    stand squeeze floor search among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
