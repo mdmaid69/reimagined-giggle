5755805e12ec482aed706102e4368d2344729e43
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:28 +0000

    clean name tape breakthrough belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def is_odd(n):
+        return n % 2 != 0
