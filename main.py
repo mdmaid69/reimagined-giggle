5f0b9bcbcc282cb06b826f74fcc0b16d37441b71
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:17:06 +0000

    speed story professional file acceptable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def convert_to_hex(n):
-        return hex(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
