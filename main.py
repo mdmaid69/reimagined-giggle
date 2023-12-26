d1a904cfc11c49444fa82babf1f41a4fd71e7ed8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:36 +0000

    proper big slight because tidy

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
