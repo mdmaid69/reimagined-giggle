2b69e617240bde9b82a014b3d273894b424c2700
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:13 +0000

    load strike tall spot state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
