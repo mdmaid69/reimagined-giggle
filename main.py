8339c825cab227fcfc2721faf53fc3f9cc334e78
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:57:37 +0000

    outside somehow breathe up owe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import re
+print(re.match("h.*o", "hello world"))
