abcdf82674d61b17f5ed70a70eca76ccf373e46b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:50 +0000

    family teach point attractive battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
