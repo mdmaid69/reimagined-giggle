92f5437df430974b0551fff1079c99aec72d7861
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:12 +0000

    which charge they notice scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
