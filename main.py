5a1dae60bc0d2d6703314ade15517e0391893411
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:57 +0000

    poor underneath bunch image nerve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
