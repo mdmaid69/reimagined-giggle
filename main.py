23fe21da3b8976c3d7b2d958292cc8030387a5a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:57 +0000

    effort tell weak balance recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
