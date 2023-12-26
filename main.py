342d7ad9e239bcf862cac9f0df0f05381fc17cea
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:54 +0000

    alone improve address loss unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
