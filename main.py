48a96e5ffa2ff0ce2b8b4ea8379e9ba0216b2d49
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:12 +0000

    thinkof wind any sign once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
