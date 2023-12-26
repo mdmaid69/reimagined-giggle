a2474b0f418e5d6b8a92d1d6478dd3f5db198869
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:54 +0000

    birth cap region short keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
