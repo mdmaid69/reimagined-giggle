89c35306da9f10813476180ee4abe34b7e246c42
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:22:37 +0000

    might physical rush track quality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-print(math.pi)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
