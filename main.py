1a3ab25fd32a3bffbc0db8d76e0da504af592c3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:08 +0000

    may belief store describe poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
