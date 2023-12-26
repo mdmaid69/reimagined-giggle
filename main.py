68a6f8b3f2800a3c121ada204ea4d908a59cfdac
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:01 +0000

    Bb keepup plate eager mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
