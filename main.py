752fd2316578a353240bb64ce058040152172d1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:12 +0000

    tread squash collect object sort

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import datetime
+print(datetime.datetime.now())
