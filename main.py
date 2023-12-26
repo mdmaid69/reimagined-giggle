333b6cd708e07d728bedda0b8df132385d44a0e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:02 +0000

    same let though answer once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_length(array):
+        return len(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
