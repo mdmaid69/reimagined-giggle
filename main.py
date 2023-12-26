6053ea59d1bf7dc081ac927ad1d94306eadcfa06
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:20:07 +0000

    guide space alone wait bear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
