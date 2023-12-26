4560e66791ebb3d7f694185001691a246f4a7ba0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:34 +0000

    illness unite shame children real

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-  def subtract_numbers(x, y):
-        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
