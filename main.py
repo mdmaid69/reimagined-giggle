ac2121bd3f08f0f8086310863d20712dc65e386e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:27 +0000

    music violence impressive push plastic

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
