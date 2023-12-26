32490edc662a718ffaaf215d77dbe8e0b4aa49e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:06 +0000

    comeround pretty admit 571enough lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
