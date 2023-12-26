fcea5964e3d3a71fa6d7eb5812d8bfb99baa4191
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:11:39 +0000

    badly major sell pause scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
