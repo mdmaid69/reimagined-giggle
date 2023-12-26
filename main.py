9802fd009eaec014f866cafcb7e0d14d68f6e4fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:01:59 +0000

    ignore plenty play cause stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
