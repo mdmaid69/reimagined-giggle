9b4c0c531e82352eb07c59aefd7b39cd6b9b53b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:34 +0000

    stage unlucky cold watch loan

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import numpy as np
+print(np.array([1, 2, 3]))
