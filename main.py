a217454eb99321577649e1c8d40ca6f1d2601474
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:04 +0000

    noone look follow brain complicated

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
