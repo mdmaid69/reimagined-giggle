ea32404e9f4775a11e90f2c693190dcd18dfc70d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:50 +0000

    woman arrive oil half load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
