719de471dbabd4636ed72782254b41f592a96d62
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:09 +0000

    lookaround shake dress bear interfere

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def cube_number(x):
-        return x**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
