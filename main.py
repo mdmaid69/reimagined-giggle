aa5f408d765fde0c056985eb280c7d481b55fc4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:35 +0000

    taste talk finger listenfor nice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
