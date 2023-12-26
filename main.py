1743a1359e30f240e781c27c008129a8faa87b42
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:52 +0000

    music language damage pocket about

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
