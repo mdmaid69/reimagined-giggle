d8d0fdd652792b3379159c8bcce00b6becdd2466
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:04:57 +0000

    what drive head star characteristic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_str(array):
+        return str(array)
