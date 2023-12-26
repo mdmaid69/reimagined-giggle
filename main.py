e13176623008b3cc1933979a6e4b900e2bc54183
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:36 +0000

    line arrival human attract bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
