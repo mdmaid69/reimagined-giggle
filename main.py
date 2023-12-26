f21c1132475cd21c1dd768ea5a7c37754fda127b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:27 +0000

    mend support account collection awake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
