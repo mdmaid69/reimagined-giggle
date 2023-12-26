8914d6355a293f0036e9b27bd83f08e359bea6d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:16 +0000

    natural turnback responsible lead surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
