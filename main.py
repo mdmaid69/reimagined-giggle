bbb18097a525b86343ab39f5ee7182cd01d65fbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:39 +0000

    themselves obvious attitude believe type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
