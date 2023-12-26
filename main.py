116423fead8fa673a23e051cc724b6d35495f811
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:37:34 +0000

    fast tone its afford official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
