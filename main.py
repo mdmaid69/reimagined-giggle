ac378589be128acd2c14b86997ed39beedb65a28
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:55:27 +0000

    save spite tight trouble solve

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
