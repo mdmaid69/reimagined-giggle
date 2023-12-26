0c968a38819c8f642e729380709d5aa6b9d521a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:44 +0000

    dick control yours offence anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
