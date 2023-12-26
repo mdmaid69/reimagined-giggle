5d96226e00596c172b95a098ed2f1f325d076081
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:45 +0000

    pile guess ours muscle still

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
