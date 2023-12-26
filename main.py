8602a3eb658cb3609758bb83019c1f92fdbca504
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:40 +0000

    plenty wire tragedy mean key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
