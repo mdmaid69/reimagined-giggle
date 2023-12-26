3b295f05fe23f9c18501daf13e22af49b4e28f62
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:39 +0000

    again thick pay address one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
