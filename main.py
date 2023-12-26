5bb001bb814ee658715912227bbda523f75cafa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:43:00 +0000

    medium eager cold listen equipment

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_area(radius):
+        return 3.14 * radius * radius
