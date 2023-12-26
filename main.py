254e0dfdc7c671719ef2f6ec2a2ca8899fa80494
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:23:02 +0000

    dance minute damage coat Ff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
