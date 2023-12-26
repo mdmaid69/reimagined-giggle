06a9d8db1f7d0b37c738b4e4bad46d42274d198d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:44 +0000

    pin since other comeoff develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
