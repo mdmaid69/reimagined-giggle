c9df5dfd483fa464466672cb497f48a8dea97d0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:49:39 +0000

    circumstance direct accommodation board boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
