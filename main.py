3c5525f5d2f8033838bea5b6eeb85d22a5017ab2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:45 +0000

    story bend failure visit interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_index(array, item):
+        return array.index(item)
