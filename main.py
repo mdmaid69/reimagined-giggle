b820ffb28f5ff15e6b3f8eacb0bf2af12e221824
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:02 +0000

    sense remember common drive genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
