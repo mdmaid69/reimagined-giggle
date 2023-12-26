daaa738da534221936037ce60ae809c7f85ecf51
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:28 +0000

    worst shout region protection child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
