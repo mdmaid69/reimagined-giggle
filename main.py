05410dd9f6ad51b2750170c48f598e28e6c8f693
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:18 +0000

    habit excited takeout now related

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
