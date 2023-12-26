f59e27c7d4152484d02753d9443576172c5b2e80
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:14 +0000

    begin influence keepaway it appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
