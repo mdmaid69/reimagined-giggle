b1a9be54f5cf6be07fedf4f650052756bed68e17
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:24 +0000

    slightly breakout key passage forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
