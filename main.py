dec50e14beb748cdfa3267bec840c895778b1839
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:44 +0000

    exercise feel pause instruction pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
