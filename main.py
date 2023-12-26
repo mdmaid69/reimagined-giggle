1f5584d049e070abc5c0631451c7fd4849042bf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:06 +0000

    hit away far serious knowledge

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_even(n):
+        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
