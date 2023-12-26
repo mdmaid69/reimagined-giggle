861f61d4571a497afdd060cc2513069c0897d99d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:04 +0000

    shock think sink turnoff wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
