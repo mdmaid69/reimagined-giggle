c598eb0bce2a3e408064d23bcd7969f7f5889615
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:55 +0000

    company roof exactly clever square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def reverse_string(s):
+        return s[::-1]
