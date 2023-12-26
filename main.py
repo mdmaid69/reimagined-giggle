59594bce7bf7626df859c99d0438e3a2c5733285
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:48:57 +0000

    chief cry conclusion but catch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
