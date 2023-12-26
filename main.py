f4fcbdfc40fea9b86a5b6a475fa6309367b90a07
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:55:29 +0000

    small geton giveup sink brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import getpass
+def get_username():
+        return getpass.getuser()
+from collections import Counter
+print(Counter("hello world"))
