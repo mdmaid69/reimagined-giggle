83561679fa740aa144841058bafea859bffa3150
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:22:02 +0000

    pair warm arrive port hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
