a41361298787dc525a4ecdbdf0067a4522c4eab1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:07 +0000

    improvement frame method negative part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
