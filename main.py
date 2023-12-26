aba8e75222cdfa89e5c1249da5aca5c4fd58468e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:21:55 +0000

    disturbing thumb sticky university where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
