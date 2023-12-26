23ab5a4127fb3e390efc95c977472dcd41c7d834
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:54:09 +0000

    rush learn convinced bit complaint

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_array_slice(array, i, j):
+        return array[i:j]
