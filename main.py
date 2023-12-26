e9049721f314bce432166aa74c9906ece88b871e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:39:34 +0000

    course thank hate today hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
