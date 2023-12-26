62740b1dc2bed427be24d5fdc24f5283dd505683
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:58 +0000

    emphasize file conclusion before interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
