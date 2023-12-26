6219dab43e447400d089b7ebc115bc92b12a1c7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:41 +0000

    draw course hope rate grade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
