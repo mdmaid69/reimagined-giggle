1d6abbe4b9f7e055b2210ea840313d43e21e0d99
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:32 +0000

    sweep hole distance confidence set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
