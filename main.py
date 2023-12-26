7cff7d57474a353c1e01814365cdf827aad03f8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:03:28 +0000

    floor natural mystery hard of

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def convert_to_octal(n):
+        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
