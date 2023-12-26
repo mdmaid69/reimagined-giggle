e2c226418ea17dcbdfb107e567950d3e366712d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:51:16 +0000

    gap enthusiastic achieve name likely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_min(numbers):
+        return min(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
