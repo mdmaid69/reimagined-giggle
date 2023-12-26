a1d8b9fc0215cb788663851e6c85c17d6e5c90eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:26 +0000

    operate yet evening branch everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
