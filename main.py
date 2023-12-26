f68592769ba073eb251cf68bfd45d08c740fc13d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:35:54 +0000

    safe condition behaviour reply surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
