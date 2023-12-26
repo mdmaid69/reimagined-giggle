b19d7f5fe35e5922f61447eb3cce36e5353aac68
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:01 +0000

    drop shrink hospital argument exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
