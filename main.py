7e1575b61a6329b70931cebad5d9ab1d99b61e82
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:49 +0000

    forever problem content choice wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
