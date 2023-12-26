74ed58a1b50051437dbf7bececfab385688f37a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:10 +0000

    face argument excited break easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(lst):
-        return max(lst)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
