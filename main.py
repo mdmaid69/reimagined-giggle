65499a1cadc2c3bf25abf3359aafbe8e722eec9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:53 +0000

    far unkind thick finger wind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Characters:", len(text))
