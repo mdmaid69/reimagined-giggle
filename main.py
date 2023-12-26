22aabbea5e3d5fcacc1211f5c5a31f064b636710
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:52 +0000

    check throughout drive half behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
