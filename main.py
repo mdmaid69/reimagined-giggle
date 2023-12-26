85e9584558ea8654500843dbaa2ffe8693c32fb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:40 +0000

    570enjoyable will wrap keepout influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_array_item_count(array, item):
+        return array.count(item)
