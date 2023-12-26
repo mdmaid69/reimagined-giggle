62430dfe10c54c077e48afb673c64f025b0d9325
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:25 +0000

    bush write advantage break ill

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def sort_numbers(numbers):
+        return sorted(numbers)
