31ee1fa8c752e5aa6b77ee82a289fb0d1abae12a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:01 +0000

    vanish letter natural chase crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def is_odd(n):
+        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
