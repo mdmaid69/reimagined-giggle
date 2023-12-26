4d648ae6406a94c578b038f9ea32abf9c716c67c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:10 +0000

    neck deaf our sky back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
