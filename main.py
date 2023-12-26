7fae11583fc70898e2f9084884d5bcd726f4d7c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:56 +0000

    ilovedicks tap worst tour spin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
