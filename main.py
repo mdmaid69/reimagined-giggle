e7bbcbfd3c706e28703aedc682da21c246865da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:58 +0000

    fly limit contact Nn often

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
