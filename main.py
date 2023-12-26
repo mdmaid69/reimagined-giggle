e71961b4b1135870fa0a9de59941c86f75b0cb10
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:41 +0000

    abroad beneath offend fight food

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
