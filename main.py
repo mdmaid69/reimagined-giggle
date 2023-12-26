f5a88e7a4a94185f49a753f361cbdf7b700cc645
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:36 +0000

    anybody suckme degree pick loan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
