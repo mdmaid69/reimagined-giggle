a65441aac01a7e78e9760e235b020f9468a016a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:02 +0000

    long inside plant feature future

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
