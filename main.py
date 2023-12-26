174b94b9c96915efe13ab265b5be449605dfbae6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:11 +0000

    life importance Zz runinto play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
