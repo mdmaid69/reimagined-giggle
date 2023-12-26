e6e13cfb1be079825f4521b9088d73515c01c5cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:35 +0000

    quarter base play work left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
