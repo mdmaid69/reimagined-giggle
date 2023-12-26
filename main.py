8a698ab1079bfd40ed74a6fabc6d77691d4f6a7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:10 +0000

    afternoon to inside process danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def convert_to_octal(n):
+        return oct(n)
