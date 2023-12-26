5fbff4738180e6848376a6448db2085ab64517ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:58 +0000

    black hold part gradually subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
