ef81a65884a71e74b7549505f7e7823b285923fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:04 +0000

    pile everyone lie kick scared

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
