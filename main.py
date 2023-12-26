82275b1e620bcd99b1cbde2a0d2447f3afc7a37d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:00 +0000

    rich child seat special among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
