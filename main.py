b16fa22ff9b886953b932ebcd911a5c00fb2e45d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:00 +0000

    everywhere cut morning attract typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+import sys
+print(sys.version)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
