22fd3fb36ba56b58f43eac0fa49cf71b62f24488
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:35:03 +0000

    goout guide different tongue friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
