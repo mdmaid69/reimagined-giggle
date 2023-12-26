3c7c76839b86f719e470137a84187532b2df1266
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:58 +0000

    where workupto whatever pour sweep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
