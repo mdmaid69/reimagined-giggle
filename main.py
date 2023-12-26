04b180aadb21492a87806d1505a53b26601ee5c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:05 +0000

    meal argue measure bottom roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
