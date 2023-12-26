4ffd7cbc38eeeeda7d94011280ad5e74fd9f03d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:13 +0000

    thankyou angry tip limit talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
