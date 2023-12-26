2a03e7dafcb7f5226e57f171c7a7fecd08b2f0c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:10 +0000

    nowhere division ill worse fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def square_number(x):
+        return x**2
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
