35d77528059f8558ea85a4b6186c831842f9aaa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:37 +0000

    form inquire shame unable Kk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
