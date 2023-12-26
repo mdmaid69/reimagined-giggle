495fbbab6d69c39692cf7b60b84e540fd4f7ee31
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:53 +0000

    attach target relative square hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
