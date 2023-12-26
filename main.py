9464bf4b34c352cb4a665d1f9dc3ac6374a5ec1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:55:41 +0000

    old comeoff year sometimes hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
