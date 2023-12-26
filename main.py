f3190052497250ff21d14d3362d5858045427937
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:20 +0000

    wait sting turn cross silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_error_function(x):
+        return math.erf(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
