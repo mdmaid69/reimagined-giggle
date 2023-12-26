f473dd5e4c764e64068f42e175c0a855b6e8b787
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:17 +0000

    period direct star development pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
