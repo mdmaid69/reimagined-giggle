c221a88c495ff9576b46c877510b250f49e4e7ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:31 +0000

    preparation Gg wise forgive pattern

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
