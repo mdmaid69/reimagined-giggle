28b3d0bed4970e96dadbf148f4ed95e468a04918
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:51 +0000

    yet sentence Pp answer complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
