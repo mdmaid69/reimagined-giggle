0148123271b1e22f85ff92a49747b0e250ff8522
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:53 +0000

    progress getaway negative kind seriously

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
