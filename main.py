de88fa8eaf3ea77de097e441c8c2dc47c95095bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:32 +0000

    exist either much somebody left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
