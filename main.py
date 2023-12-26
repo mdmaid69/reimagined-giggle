c943dcf0e058a0b2e4ede5df00f294eaebdaf043
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:15 +0000

    inform better piece reaction figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
