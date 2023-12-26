e6e83357d107c2e4e0f654e8eb944eec3a377222
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:51 +0000

    tell precise concentrate weigh its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
