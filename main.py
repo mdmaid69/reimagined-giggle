e3b79ebb205216ad3f4a9013bd26eedbb0ff2753
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:45 +0000

    unfair disgusting possibility accidentally smart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
