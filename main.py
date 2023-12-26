e579bf2d0e5bc61d453a7dcc6863edc1917c3ef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:56 +0000

    air either accidentally summer deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
