906d354f7921312c7276a44d4841883454a6bd96
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:15:44 +0000

    match road disagree behind bye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
