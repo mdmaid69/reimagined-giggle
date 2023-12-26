3f9f3f0a71031133168ba19ed509b32b6adbf1b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:27 +0000

    occasional surprised now combine image

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
