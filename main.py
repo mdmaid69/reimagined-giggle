42a39fd8b0feb2abff02121813ae19b0e64f67bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:14 +0000

    luck perhaps unusual control hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
