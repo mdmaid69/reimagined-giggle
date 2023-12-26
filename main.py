d044b5eafe40b53a9fd6121aa715f37b131f9594
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:41 +0000

    narrow supply vote comment market

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
