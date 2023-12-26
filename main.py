fb87ebbfd247eeb076cc55888d2d0a3b16fe21f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:04 +0000

    act sensitive fix sign respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
