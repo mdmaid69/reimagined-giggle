b2e40184a48292f90ca3f674b1356f5f72bcbb71
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:46 +0000

    gift shopping stare big bag

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
