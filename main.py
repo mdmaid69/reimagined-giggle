84945bf2d6bf9b8ce91e3729d7526059be7de1bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:48 +0000

    community shoot unit by top

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
