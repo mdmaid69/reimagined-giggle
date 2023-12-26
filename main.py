0cf2766156a6902a08f71f0e0ed1087900ca285e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:37 +0000

    platform set contact second offend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
