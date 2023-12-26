960bf0a2ad55edf6a324ec7697e27c9810a0508f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:22 +0000

    either answer giveout weekend before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
