d0463e0fd276c294934f9c1cf8b5c838a0064a4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:28 +0000

    block wonder unlucky question reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
