13ad18db4d774c73c1b16590959ac01f87497c85
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:17 +0000

    both curious vanish mention tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+x = 10
+y = 20
+print("Sum:", x + y)
