1dbf3a63ce8189d57c6d62405da0078eb9189cc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:50 +0000

    together explain tooth sure exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def cube_number(x):
-        return x**3
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def convert_to_hex(n):
+        return hex(n)
