6db3cf0e5c5fb516449f75f3e7016e665946384c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:45:27 +0000

    rather spring priority part part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
