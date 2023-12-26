0f8d79190e2987182fe8bacb30b6d0a174cd9c59
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:14 +0000

    spare float date wave yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
