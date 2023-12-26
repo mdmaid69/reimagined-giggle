02de8a117ee1f0b1562681f892fd8c8ae3771c52
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:11 +0000

    fix exactly acceptable inside offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
