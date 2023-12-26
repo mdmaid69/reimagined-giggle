1577a421fdc88863b97ce8b1d14cb3d451f676c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:35:52 +0000

    car length result ilovedicks regular

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_list_from_array(array):
+        return array.tolist()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
