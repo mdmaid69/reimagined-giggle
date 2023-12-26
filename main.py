3620f25e6eadab195a170849acb56f7068c25bf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:35 +0000

    reserve region left area wherever

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
