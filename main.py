b7d8ce4cd9d833d2e8a8c7cc0ff47d1fff588973
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:53 +0000

    vanish progress punish ever body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
