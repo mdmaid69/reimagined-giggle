27c1554a9b5a6b68e809588cc46a811b37aa8af1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:37 +0000

    surface false beautiful clever mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
