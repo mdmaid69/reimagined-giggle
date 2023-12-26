f3207e674f73046987143ca9664ad9e7d0563308
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:12:38 +0000

    affect pretty thought immediate increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import itertools
 def get_combinations(iterable, r):
         return list(itertools.combinations(iterable, r))
