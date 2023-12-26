3a145deaaf3fcbf7ef9c564fef771ca8ae7f4d50
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:48 +0000

    rubbish match bet roll calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
