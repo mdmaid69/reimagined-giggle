a87737e524eb5de685128bdcfcdece48f98fcb6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:10 +0000

    himself appearance club they comeout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
