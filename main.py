b41728863e6892d6689c8d8e8d66122469e5bf40
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:40 +0000

    refuse second bar earn summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
