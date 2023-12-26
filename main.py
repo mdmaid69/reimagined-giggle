6cb6722fdbe4aef620fd2c5772552fc6f1398e26
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:04 +0000

    she unfortunate notice kill punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
