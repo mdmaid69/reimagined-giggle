d41104b8e0b6dca556eaa02e96540171c92e13dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:21 +0000

    exhibition platform score complete form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
