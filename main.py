a5b2b2edb850cdd3247178b61406bbf456cd8640
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:50 +0000

    storm report fold female immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_float(array):
+        return float(array[0])
