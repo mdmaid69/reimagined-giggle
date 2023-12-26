0adf3b66ad7b1b9c8c61141eed843c3c49dfdbc9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:46 +0000

    fit would leg head survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
