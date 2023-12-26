39c215b9c9150893e81d6c05a3f49120d5145c6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:38 +0000

    glass like match confusing goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
