206a2396609ac465654b51aca892b163539bfb5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:31 +0000

    only rare worth thorough car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
