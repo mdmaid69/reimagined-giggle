efbdf4352192281487c32fe9f4d7e7b8de023c2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:25 +0000

    179bite excited quit rob treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
