d2e87fe000521fb221af6ce960b5e77656f0c240
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:15 +0000

    care language furniture body dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+for i in range(5):
+        print(i)
