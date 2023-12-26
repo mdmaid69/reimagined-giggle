8adfbe44c0c35a6e0e1d9774f2ba4f40b2a6a96d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:33 +0000

    mine forgive somewhere surface order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_area_circle(r):
+        return 3.14 * r**2
