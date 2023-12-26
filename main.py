0dec141611612421c98a108636eae1bbc7a2e73e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:02 +0000

    bin modern war only talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_tangent(x):
+        return math.tan(x)
