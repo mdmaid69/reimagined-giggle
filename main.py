bca3af82d4700c809f841dba62ee22b82c930434
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:39 +0000

    tense for creature lay row

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
