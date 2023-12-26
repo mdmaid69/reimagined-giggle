81032d649bf62cdb10c7f924366c26b0e305d1f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:40 +0000

    window name safety fairly secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
