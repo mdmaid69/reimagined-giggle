802e964fb4b743ef46e01af198b953e88f38567b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:41:03 +0000

    road figure frequent real justice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
