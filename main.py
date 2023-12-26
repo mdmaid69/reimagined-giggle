4285464a87b9c23fbf6841ba2c2ab1f279e8d745
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:32 +0000

    married material lay gap call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
