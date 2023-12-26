0bf287e122e5da500279e85a64ae62fcd7767c7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:16:55 +0000

    allow he suit conclusion age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
