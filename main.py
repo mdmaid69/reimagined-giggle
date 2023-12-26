87c5f50ad02ac6455c7f6a6758c199ef1614ef89
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:21 +0000

    entrance suit pollution patience ability

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def square_number(x):
+        return x**2
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
