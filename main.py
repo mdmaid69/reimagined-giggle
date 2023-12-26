fc8dbcd7e9d38d152811cb7ac44729fdc09ca245
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:40 +0000

    immediate swim average brush down

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
