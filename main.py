5536db15f88059b4e7dd1a40b0961d603356b7d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:59 +0000

    move where return suck find

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
