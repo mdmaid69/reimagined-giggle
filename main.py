db4ad42221637ec53c431805287ad2773a2a0f1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:56:37 +0000

    occasional suggestion brown minority forever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
