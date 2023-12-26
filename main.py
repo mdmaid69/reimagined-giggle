59e354810cd8725a03e3fd583b78a81e71645313
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:22 +0000

    thanks tax complete sensible tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
