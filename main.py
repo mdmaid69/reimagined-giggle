3e9e40bf1011db0858b009100dd951df999442c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:19 +0000

    length roll affect constantly market

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_repr(array):
+        return repr(array)
