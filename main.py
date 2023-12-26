81762ec3c2b96fb20639d7fd70b32d904b71019a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:27 +0000

    minimum drive fat evidence laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
