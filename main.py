70f7f0c6d71b1bbbeba0dcbfba15506f6e7fa2e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:10 +0000

    pen permission birth characteristic responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
