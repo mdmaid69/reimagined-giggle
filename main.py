18982f1c17d4e8742e4f88f50e0e60ae108a8eea
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:06 +0000

    collection I custom birthday sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
