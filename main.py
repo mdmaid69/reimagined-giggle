fe855e2b925289dfa7ca52574f22b14d47aa13df
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:34 +0000

    dry national children walk 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
