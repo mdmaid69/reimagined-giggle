2421adec966a9bfe56b00f49b79f5d49fc84c1d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:00 +0000

    turnoff cross add record comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
