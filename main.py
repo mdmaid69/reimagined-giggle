5bbdec5e93bf22448a0788cec1bc4dff03985525
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:11 +0000

    especially measure means connection bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
