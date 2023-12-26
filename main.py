2d70d5267772624ba39d676b4eb983807ae4fdcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:00:29 +0000

    sound broad send bang abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
