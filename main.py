a8b020e3b36b7e9a15b438fba316af317977ade9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:52 +0000

    wire wind coffee snow vanish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
