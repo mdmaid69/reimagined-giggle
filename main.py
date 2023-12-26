778a9abf229bff87fbc4a2f4ee53e000ef4b038c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:47 +0000

    reason reduce tradition by cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
