df49d7c57b9b311812d1f1934195c4480cb151fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:59 +0000

    shop stage shout although Oo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import datetime
-print(datetime.datetime.now())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
