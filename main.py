4813ce5f27fc87f6fac4d72f6d8e0230a7071a42
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:09 +0000

    squash treat boat silent confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
