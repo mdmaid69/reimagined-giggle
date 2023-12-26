aab5607e4334adf1693f26370da427666b99409a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:14 +0000

    spend money contact computer possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
