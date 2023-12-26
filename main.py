3ca5729a6419ea95d15aca737fbcb0b67f96e840
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:49:37 +0000

    rubbish pencil rough as by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
