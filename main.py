a98ef77b8ca6db01ff1f7fbafb37eab88071df66
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:27 +0000

    partner belief copy may fall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
