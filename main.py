dc18e996a3c07402fb650aa7924672fa4f24b82c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:40 +0000

    door shopping tap temperature bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
