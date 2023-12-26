4da32ff21e00b2718544ae280922445c759d1e0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:07:15 +0000

    point stir Zz arm grand

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_max(numbers):
+        return max(numbers)
