4d6e6c41298e3d6d2d44215ccb385e950c8f3f4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:07 +0000

    mother yours behave characteristic develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Characters:", len(text))
