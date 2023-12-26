b576151ce6f56f8729f9890c0d73ff9cf61ef80b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:08 +0000

    chat empty tense positive brain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
