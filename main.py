855f9aa360b958097dde848c4f789a03dd80bf57
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:26 +0000

    factory pool regional impossible related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
