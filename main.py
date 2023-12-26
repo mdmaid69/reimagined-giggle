acafab732cd6f34108c3efede5d753b3ea5f8d41
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:44 +0000

    tear release gradually kill sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
