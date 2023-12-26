8283da30d3fcd587c4c6c7d1388ccd7e785d2962
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:46:57 +0000

    back gentle translate kind underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
