7c948e94b8ebc2f332293820f2225c60a491d2cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:04:00 +0000

    probably computer continue lock treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
