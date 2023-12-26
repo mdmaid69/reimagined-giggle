a5d63f2ecc3f2c5a1838bf20b83e3238b4839d0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:26 +0000

    could tragedy force figure water

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
