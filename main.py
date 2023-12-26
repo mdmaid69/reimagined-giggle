eddf94c28b7292f3640ac374ddb2d6ab1df6097d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:13 +0000

    simple compare black stick Cc

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
