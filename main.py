f3b0a66a684d048226d820a610fa3d4d9deb660a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:39 +0000

    personal anybody moon order bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_pressure(force, area):
+        return force / area
+import getpass
+def get_username():
+        return getpass.getuser()
