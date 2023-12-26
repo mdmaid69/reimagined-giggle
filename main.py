604ad4921b64c2d24b50dd68f81abbc1fde4382e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:47 +0000

    interesting responsible regularly happen risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
