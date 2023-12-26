c1161803385dc61bb4a3608e9d557f448c6cbd4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:53 +0000

    on ear cheap beside whatever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
