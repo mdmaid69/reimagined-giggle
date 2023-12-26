51b33a4830923d5dbc6653d820d0a9532c129189
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:43 +0000

    wherever completely please male centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
