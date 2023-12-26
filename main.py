8d31e01fd1b5b3042a64d782dd0bca4d1e5a06ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:57 +0000

    suppose nonsense rule matter favour

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
