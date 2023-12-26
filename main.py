a92ca9d556cf9b11f39d45d7d5281ce625726cf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:56 +0000

    get clothes fault body rid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+print(datetime.datetime.now())
