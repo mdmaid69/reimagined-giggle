a323cfd9c5f10162da0f195be0a99ef22980cda5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:35 +0000

    Mm remember cover feature wrong

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
