f693f4d40b9071eaa2679456b8a5a7f5495ee82f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:02 +0000

    Pp crowd perform careful much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
