7c0de0454e5f266a795d6445840df43528fd1fe7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:24 +0000

    best steady still punishment peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
