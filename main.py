f9fd74846fd8181de86b98880baa78cc6dd6f3b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:09 +0000

    nose object callat doctor change

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-print(time.time())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
