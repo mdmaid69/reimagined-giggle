4e6430c8893573cf40988a9fd690ce9625c522c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:32 +0000

    possession turnon full file throw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def subtract_numbers(x, y):
+        return x - y
+def is_odd(n):
+        return n % 2 != 0
