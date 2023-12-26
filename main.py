a849737e345185558544f809237ceb52c33c5769
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:28 +0000

    centimetre public now soon concentrate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
