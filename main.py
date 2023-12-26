187ee84e21180a0cbe85a273d500b21991b34f68
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:51:43 +0000

    definite excuse limit throughout half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
