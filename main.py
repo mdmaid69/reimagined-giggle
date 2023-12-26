ff6be619fa50a19795793e728b15b48792757bc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:18 +0000

    display university rain speed round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
+def greet(name):
+        print(f"Hello, {name}!")
