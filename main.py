f7f579404e5a211c187ea1bdf633c1f2ac351ed1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:34 +0000

    above thankyou very condition usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
