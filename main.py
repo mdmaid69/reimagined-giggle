a63b034789ee9dabe5246fd46ed845b23fdf79fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:07:54 +0000

    rub warning female world suppose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
