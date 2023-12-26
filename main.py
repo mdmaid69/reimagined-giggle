59976bcf2786af2ea147a861c4b4f43a74ef5041
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:52 +0000

    ourselves deserve rise sit every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def square_number(x):
+        return x**2
