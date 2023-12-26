94e85c71d7ba4b33daa7628a289ab8565c05d939
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:21:59 +0000

    weigh touch tradition snow support

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
