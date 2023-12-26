da09bfd7d7cff8c39ef783a9775646b3e010a394
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:21 +0000

    wake average moment means gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
