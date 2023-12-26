d8aa8a6487b03ebe0e4f7884a90d0a9976995e1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:44 +0000

    remark murder murder basis especially

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
