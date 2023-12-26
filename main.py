b60a822a2b89e972aa3ed13a7bc1758e4e956e06
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:19 +0000

    guilty weigh actual noisy attach

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def remove_duplicates(lst):
+        return list(set(lst))
