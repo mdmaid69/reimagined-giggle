0891a085816e04aee65d2e16bfea254efcd1925f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:07 +0000

    knowledge reply heavy still type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
