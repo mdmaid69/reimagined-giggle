b43218a581834825fb2a2dfa1692ff804e516d27
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:16 +0000

    that succeed slice lock false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
