3023771f216041900e905cd82f6e91c7ce339610
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:41 +0000

    everything takeaway operation comein perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
-def convert_to_binary(n):
-        return bin(n)
