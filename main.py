74e0a1885f9fc5594d2cf05996f33a3bcc4fac82
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:20:48 +0000

    blame available thumb land shallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
