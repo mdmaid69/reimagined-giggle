a3611f2a6cbdf3dfa0b24c792581f43818d85ae0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:56 +0000

    first consist clue goout dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
