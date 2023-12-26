4622c3e2e0bc7e919367b239da2371fb1075ebb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:53:23 +0000

    stop deal answer behind last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_min(numbers):
-        return min(numbers)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
