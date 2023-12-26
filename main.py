a81d3753e9b36718f2e4180088bb1abad140aaaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:57 +0000

    behind outside treat waste calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
