6d7cd80d7ec11453e747978acb7e02f93c00e51a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:48:06 +0000

    sorry size notice extremely male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def convert_to_binary(n):
+        return bin(n)
