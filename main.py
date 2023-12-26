06081909f0d09b0a6b25c1f5c9636ee3701d85d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:22 +0000

    hospital none tradition lend present

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
