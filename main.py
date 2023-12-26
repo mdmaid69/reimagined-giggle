d48a36c3d26ff90fe990e1e6162bef10fbf6fa66
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:27:00 +0000

    funny little shop plant attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
