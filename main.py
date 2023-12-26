ea9791bd07d0296fafb40a7a47517094268df917
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:23 +0000

    class 568energy Uu cool past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
