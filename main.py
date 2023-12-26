25ca8c44bb429b4e43c51b6623da57d8c174605d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:43 +0000

    least rush statement allow follow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def square_number(x):
-        return x**2
+def count_characters(sentence):
+        return len(sentence)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
