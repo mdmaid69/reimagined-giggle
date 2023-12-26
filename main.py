5bce120181bdbcaff924bcd5e27fd20195518354
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:18 +0000

    wonder her drop this degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
