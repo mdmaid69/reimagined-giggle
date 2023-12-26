0ff0006b8340bce14bf43ee2641134c9170ce036
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:44 +0000

    present process disappointed alive bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+for i in range(5):
+        print(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
