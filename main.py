39d2181597b9cf30c86a5845d5ee8c5a17941f7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:49 +0000

    cause behind direct spot problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
