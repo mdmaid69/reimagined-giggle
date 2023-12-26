8dcc4d77fc4b842a656a20e390a47d2d64e2521e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:48 +0000

    goaway office remember lift pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
