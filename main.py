281b04c53311506adb4c80cafdb56cb344d936f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:53 +0000

    sound fair uniform sink trust

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+print(math.pi)
