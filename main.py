a2233be160e18a009aaefc149c6206c6815c7039
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:16 +0000

    goodbye nose below 573entertainment job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
