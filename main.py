9d5ef8356e08db5aff9a6f9c95ea3ce7b8705c3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:51 +0000

    scheme hot complete for steam

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 def divide_numbers(x, y):
         return x / y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
