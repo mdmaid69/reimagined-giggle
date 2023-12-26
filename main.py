e96b144b6edd6ce8ba1568cec1be0f7a53532d56
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:40 +0000

    lookafter anywhere crazy skirt rest

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
