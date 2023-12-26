606eb08cd1fc7b117560795923d0f738e692068a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:46 +0000

    pencil characteristic coffee ourselves wide

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_acceleration(speed, time):
-        return speed / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
