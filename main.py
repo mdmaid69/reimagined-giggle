dc20e6ba0ff5db3fbda25fdc5d83e96bcd5ea426
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:10 +0000

    relation die central what war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_min(numbers):
-        return min(numbers)
+  def delete_file(file_name):
+        os.remove(file_name)
