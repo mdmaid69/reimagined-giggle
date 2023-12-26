bc10045a9251413915e4cdd2961df2fae2052e90
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:29 +0000

    under wall dress contact build

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
