17f39839c067c1c11e090e6b5a033821188b86c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:54 +0000

    bush terrible after maximum computer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
