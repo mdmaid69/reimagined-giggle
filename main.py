116e0e47f20afcdf154254068d36212f143c0e1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:51:24 +0000

    ought suppose plenty wheel chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
