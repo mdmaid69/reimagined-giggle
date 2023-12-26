76ba3eb8186f434d5f8610205c00e77936ae183c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:22 +0000

    can false tight that custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_unique_words(sentence):
+        return set(sentence.split())
