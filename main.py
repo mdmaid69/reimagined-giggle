7dc3cd9e611b7d7d66619b72fb949c93f670c2f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:28:37 +0000

    when ashamed concerned history soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
