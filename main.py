46c38a54475a86af4f25b381990950368e1c1f3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:23:30 +0000

    use trade machine without record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
