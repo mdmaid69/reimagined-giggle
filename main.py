c0d50f27dcf5e0979e00f0dd4d13ff7de81d02e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:44 +0000

    region closed song close who

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
