1ff1455d8ec1e8c0e188f56e8eb8290f2d98f49a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:32 +0000

    getout under trust sting brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_priority_queue():
+        return collections.deque()
