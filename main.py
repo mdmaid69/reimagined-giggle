430b49e8f8964427bb3ba41fb2e83ee50e2f9fe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:36 +0000

    ball noisy company all cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
