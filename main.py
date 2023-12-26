44597cd0fc8663535f6bf52c39c9f4b215129f90
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:59 +0000

    dark flood beat thin good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
