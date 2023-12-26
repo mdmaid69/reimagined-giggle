f4ce84a2e1774a08a2fc476c3ce73a5e93a93406
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:59 +0000

    embarrassing 573entertainment total climb cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
