1173d45051af013a1eec246aa94c07decd17eddb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:53 +0000

    butter shallow environment marry characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
