c6fac125ec969850cc80937ea7bef7e9b44d6ade
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:54 +0000

    male trade understand determined for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
