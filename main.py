90f937278e3f857bce90690df7778a70273e7948
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:21 +0000

    although achieve statement stiff thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
