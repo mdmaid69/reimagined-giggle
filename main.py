ad5351541fd4318781dcbfb075a449e71ddffd00
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:19 +0000

    marry nowhere ring this discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
