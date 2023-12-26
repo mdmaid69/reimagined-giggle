ff3565cd02ee9951f74ea44364ac03ebaa0154de
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:31 +0000

    rest opposite summer autumn never

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import numpy as np
-print(np.array([1, 2, 3]))
+print([x**2 for x in range(10)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
