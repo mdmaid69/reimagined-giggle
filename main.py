d9c1b6a79297a5d4f47c90dce2d993940dbec623
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:50 +0000

    average convince straight and comeup

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
