001658c19531ab6bc4d0a6420bce5599fb4c6779
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:28 +0000

    spoil somebody join police base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
