bf7f0fe5f84c1bbb6b1f77cce1f910b6358a11e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:25 +0000

    pause rule Ff interruption telephone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
