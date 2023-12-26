15bb6d2e07ce3f5bf013054b3bf62a91051b9716
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:43 +0000

    journey poor ready persuade transparent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
