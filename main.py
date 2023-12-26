cac859784d8bd60cf63ef803c85c625220350545
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:15 +0000

    naturally heart nonsense talkinto immediate

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
