8ed01288b511bc2771b41ecdb4d71c1b1ac4fda8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:25:34 +0000

    song address transparent word push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
