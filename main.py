e182c31ec1491b54a91dc664fa23cded17aa595d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:42 +0000

    slide scrap fire go when

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
