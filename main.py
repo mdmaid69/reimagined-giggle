e0a4cbf6968da8f00ad73e7f430b1582f48432a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:43:10 +0000

    rid load disappear coffee none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
