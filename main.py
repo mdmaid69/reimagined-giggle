7f8fba65e92da570ec1c033145f1b715fb698310
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:11 +0000

    about entry delight demonstrate represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+print(datetime.datetime.now())
