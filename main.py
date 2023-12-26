987dbe8cd3ad7e9d68910c7415e03313518250f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:03 +0000

    stage shelter centre bottom entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
