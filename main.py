6d1ff568b95fc0708625081e8bb5a4d00e4146b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:00 +0000

    if leg natural period borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
