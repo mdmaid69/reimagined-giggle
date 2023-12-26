7458360a19296ad5ecac0b3563076d663fbea6e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:55 +0000

    identify case opportunity send disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def convert_to_binary(n):
-        return bin(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
