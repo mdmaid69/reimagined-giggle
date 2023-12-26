f9b0fd01b94571083c3132f83e630b8b12f975d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:16:32 +0000

    several behaviour goup advise mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
