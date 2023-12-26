1cb5c389d071000b9b82b9ca7a9d947374534526
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:15 +0000

    compare above sad chin recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
