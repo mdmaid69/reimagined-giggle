7f10f7eb1ac2e20b02bda4968b58ddf44cefd6d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:31 +0000

    sure fire ability seat escape

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
