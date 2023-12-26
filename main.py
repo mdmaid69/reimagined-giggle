c94e09589d63b9f915cfaed87d9c991e298f04c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:12 +0000

    lean vote giveup someone thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
