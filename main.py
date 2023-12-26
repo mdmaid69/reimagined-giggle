674564925cf93baecba03c30f1f1cfc509a1038f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:59:55 +0000

    male share hardly mean suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def check_if_array_contains_item(array, item):
+        return item in array
