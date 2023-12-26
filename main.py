f752af4f8d577b75cc62abc8aac594719620d593
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:29:56 +0000

    smile heavy solve plain platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
