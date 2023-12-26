5c433156c6c662c63ea03f48a16e3d0eadca617e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:57 +0000

    basically quiet deal defeat grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
