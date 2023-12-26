8c5f5410db4aeab21ea91a7f98739299eb98388a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:55 +0000

    enthusiastic Ee worth hospital left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
