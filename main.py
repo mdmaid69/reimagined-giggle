ac9e3589567a95c4d8c0732c51d5539d4e63848c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:13 +0000

    recent complete continue set favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
