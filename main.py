a59a5f7190bb2782cff460cc136fcee59d2da27c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:03 +0000

    original declaration boring wild rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
