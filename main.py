5c4f20ee095642eb387e9c0365af02b543f2645c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:01 +0000

    fall thirsty promise getoff pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def rename_file(old_name, new_name):
         os.rename(old_name, new_name)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
