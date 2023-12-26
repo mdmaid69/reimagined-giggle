2a7680828dc9ab9dc3964aab90f814865c13cf18
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:22:21 +0000

    nowhere be straight thumb equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_pressure(force, area):
+        return force / area
