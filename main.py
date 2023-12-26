b95e4c1481953672177fb7764d65bf11913f3ee7
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:12:13 +0000

    sorry treatment cinema tidy trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
