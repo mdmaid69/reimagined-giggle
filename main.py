3c3d6111d420c7c179d1850af07e3645691154ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:09 +0000

    clean everyone feature he just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
