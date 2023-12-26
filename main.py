a4b025945d037732902ec09245b85c25658b70e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:22 +0000

    police failure trousers attention again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
