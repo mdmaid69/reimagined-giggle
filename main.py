af661bdfaf391d17f4322ea8ff38c97a3a437a27
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:07 +0000

    together relative equal lift shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
