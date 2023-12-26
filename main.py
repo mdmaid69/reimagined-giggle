09d57314bfc1502e40e09afdd47fa7d9db8cf125
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:49 +0000

    famous clothes relief in meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
