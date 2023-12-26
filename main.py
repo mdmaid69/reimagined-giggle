8c139232d6e526f173fedeeef596f6793cbfa908
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:22:30 +0000

    reply consider pause rubbish knowledge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def divide_numbers(x, y):
-        return x / y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
