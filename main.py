dd8e381130daf0e23c7cd3dbd32d9d4cd7fdc737
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:17 +0000

    read healthy 567enemy 573entertainment illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
