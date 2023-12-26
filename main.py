32bb8a660501a3ced4f72975d217402640b3c7d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:05 +0000

    ground rub warning high trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
