8ed645e9645f0485202f3df86030d83cf2724853
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:01 +0000

    at comeback complete excuse treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
