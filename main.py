5fcf64783b01178b6896267939b9460a7102a2e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:28 +0000

    street great true shut gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
