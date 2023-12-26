36e7be47be978ccb708f09938b4444fcb9d56e00
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:00:24 +0000

    cut journey stir offer true

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
