e30ed9277fe0b418d9e488428b79081670144267
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:04 +0000

    winter OKorokay brush contact then

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
