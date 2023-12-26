2d0ec29ad448bc4748512419b76f4041b225e8c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:51 +0000

    project closely maximum later bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_list(array):
-        return list(array)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
