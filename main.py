1873da7ef205070c28e72fb7e5417946b3b726a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:16 +0000

    shock takeout treatment squash conduct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.ctime()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
