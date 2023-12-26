8821dd2f916b60847c3d42554f11a93575d71108
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:26 +0000

    real tidy single minimum injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
