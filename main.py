ecd198af539c5e7b76dfc073db20d37addaee56d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:11 +0000

    shape good ill healthy means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
