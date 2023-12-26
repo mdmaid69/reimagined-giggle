b8a0ed5c036a31cf5953bc161f886ad18478ade5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:49 +0000

    better result beside fill environment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
