c7f6290adff767161a258dedabd63a199c27b02f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:18 +0000

    disagree description time contact fault

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
