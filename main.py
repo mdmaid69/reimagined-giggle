d23121a730ea00fbe7cd5fd0811fde8673106101
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:49 +0000

    fault fast angry after bang

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print(sum(range(10)))
