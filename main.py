2504414adba4dbdd6625b0f2ea42b8a5aa60ad20
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:27 +0000

    reasonable crash catch time listen

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
