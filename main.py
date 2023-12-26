58aeec4f518ee577b2b4025f1f69a8a4d183fcd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:46 +0000

    important minimum belief die pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
