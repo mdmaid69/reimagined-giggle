88de6493ecc08adeef20b871b2534dba04773482
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:11 +0000

    light no plate light shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_work(force, distance):
+        return force * distance
