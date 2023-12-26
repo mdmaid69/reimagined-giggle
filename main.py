39f496de256d838e18c4d20a07e05226075870ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:43 +0000

    world gate sticky fault mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
