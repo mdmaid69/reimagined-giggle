536bff5f3fb407bddfc6e48970e2ea3e38e0e60c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:43 +0000

    school everyone practical to wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
