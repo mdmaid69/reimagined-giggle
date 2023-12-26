a34fbb82dcb073893ca9acc59343ce25d5c24e1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:46 +0000

    goout smile interesting kind burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
