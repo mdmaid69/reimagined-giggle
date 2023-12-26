42d705f800578fd3a98dd8fa9c3d077c1337181a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:47 +0000

    getout goon up world Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
