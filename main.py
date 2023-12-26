f2a5b37a949a98766e86c2433d05c495defeee0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:09 +0000

    or backwards there size discuss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
