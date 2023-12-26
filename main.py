5c65c9132118bd25070779cc75f0e4c11da9f92f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:42 +0000

    kind contrast late pure circle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_base_name(path):
+        return os.path.basename(path)
+def reverse_string(s):
+        return s[::-1]
