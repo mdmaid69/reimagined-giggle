3e2ba2ae3486926fcc154825b55c469ac91ec027
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:58:37 +0000

    wrong scheme certain contents weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print("Hello, world!")
