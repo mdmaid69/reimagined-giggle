cc4d6fba37e7703b26dbac81b5ac0c8890f17306
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:07 +0000

    message strength soil shape platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def convert_to_binary(n):
+        return bin(n)
