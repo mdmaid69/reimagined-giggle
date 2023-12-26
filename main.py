505edfe127d5c8c73cd007f14ab3927ae71749b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:15 +0000

    idea flat talkinto difficult previous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-def calculate_speed(distance, time):
-        return distance / time
+text = "Hello, world!"
+print("Characters:", len(text))
+  def convert_to_binary(n):
+        return bin(n)
