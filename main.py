b1c07fd781031c485f9a9c6603f209ef71043f91
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:15 +0000

    deserve strike person popular extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
