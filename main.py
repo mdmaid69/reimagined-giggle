a7282c0d3d98f44fd5bde935a2418eb34cb768ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:21 +0000

    sign disturbing charge gap trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
