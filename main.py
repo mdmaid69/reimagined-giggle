cbe17a39cac1544c05c9d354aa7e52431b2d1f10
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:05 +0000

    either brush lucky creature tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
