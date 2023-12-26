c708e7f9e377e39d74c581fa77d882f22c77afdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:57 +0000

    Rr present operation lookup best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
