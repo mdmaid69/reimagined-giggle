16f6999ece7c1ceb7a5f6dd6cadce625206ea35c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:40 +0000

    at play pair constantly powerful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
