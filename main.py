f2ddc25f1269461e7bcc2dacf38ace2afd84f69b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:15 +0000

    like content set whatever listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
