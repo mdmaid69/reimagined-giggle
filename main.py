1c165f45881b46187c0b887c2cd0f54722c4edd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:39 +0000

    authority part transport several joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
