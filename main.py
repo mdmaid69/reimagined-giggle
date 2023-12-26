1d131cfaea761d57f2b9ee95535483ee409d0788
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:34 +0000

    politics rough invite strength welcome

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
