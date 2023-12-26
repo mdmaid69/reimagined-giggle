af64ff5817752aece5a536e8613cedce5b52ad40
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:14:20 +0000

    custom remove needle present enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
