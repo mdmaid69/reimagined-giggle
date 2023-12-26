5af43c5a4e716aae1292f7761fc2dfb5bdd48914
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:42 +0000

    pen afford section if patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
