69afa3a5099506c77f9747191d27e15fb56eef91
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:43 +0000

    one complain worth area first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
