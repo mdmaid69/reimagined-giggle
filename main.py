eb8a06d8bc4ddd6d7251da3090d35b0b582608c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:14 +0000

    petrol unlikely direct new put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_stack():
-        return collections.deque()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
