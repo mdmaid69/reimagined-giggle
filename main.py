dbb1877d4a33efccb7bd6456443442201346bb8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:32 +0000

    praise if total should door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-def get_current_time():
-        return time.time()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
