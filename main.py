c3cb7c15c5965e5e1f990311940849388a8fa29f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:14 +0000

    fork degree proof strange Ss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def iterate_over_array(array):
+        for item in array:
+        print(item)
