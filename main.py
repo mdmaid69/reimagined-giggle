7a4922aa95d813ba2647c6dc7920a3924a8f7fc9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:32 +0000

    try professional close miss avoid

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import getpass
+def get_username():
+        return getpass.getuser()
