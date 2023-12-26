81011119731d0a4b1f7c51bf37986d077d07ac26
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:13 +0000

    survive second so set dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def add_numbers(x, y):
+        return x + y
