eee01b0223597e1e64f5a92daf4aac50adcc5c67
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:06 +0000

    suggestion situation nose fit die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import numpy as np
+print(np.array([1, 2, 3]))
