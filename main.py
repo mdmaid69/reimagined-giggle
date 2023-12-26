4882041b645981831f00ed3384fe8e97a1529fbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:15 +0000

    beneath 565end separate plastic his

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
