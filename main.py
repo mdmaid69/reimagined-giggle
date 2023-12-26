ed4de83674ce651d6e65b0a6f7c0888981d82bac
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:50 +0000

    raw manage judge swim cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
