d9ce376d196902fb0dafefa006a05ee548cde376
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:52:40 +0000

    lean sit organize tip by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
