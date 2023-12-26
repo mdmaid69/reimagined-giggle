ba2a043b3bd9617ab3ba13dcebff41009fc4a53e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:43 +0000

    steep weak wake vote point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def get_current_working_directory():
+        return os.getcwd()
