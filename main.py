7d19f9991192259513e1ffd5703d5ad6024357e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:27 +0000

    fold underneath remember float fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
