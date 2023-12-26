f2098248a278ef87c19d0fb26cda671958f22b9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:19 +0000

    middle several its pencil where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
