fe6c199f240ea00482397a672d28af6c2e90d180
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:05:55 +0000

    bringforward again grateful satisfied popular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def is_even(n):
-        return n % 2 == 0
+import os
+print(os.getcwd())
+import tensorflow as tf
+print(tf.__version__)
