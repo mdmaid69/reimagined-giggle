cc82585a8db94b6983f4fd2e13bc88477c8df1c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:40 +0000

    desperate major freeze constantly record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
