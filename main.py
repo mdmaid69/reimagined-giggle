41babced7d2e1b308160777538b1c75797ffd91a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:51 +0000

    damage damp challenge right border

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
