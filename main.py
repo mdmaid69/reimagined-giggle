09d5f9aeba4ef31875ba720e0263891d57ce2687
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:47 +0000

    port sky comeoff forever dish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def convert_to_binary(n):
+        return bin(n)
