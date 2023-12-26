b47a84fe920151041da7f8453cd1e58b6a995b54
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:32 +0000

    wipe impossible table year post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
