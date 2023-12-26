010915865013e0129c751247fa4bda88958dd03a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:32 +0000

    use prepare defeat advice sudden

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import socket
 
-{body}")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_speed(distance, time):
+        return distance / time
