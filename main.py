f506db1ae7f64d7846596262b618341bccda79fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:01 +0000

    agree transparent thought anybody fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
