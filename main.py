7a30b3739dea9f7cb37bbf93c5b4955439c41e62
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:30 +0000

    meanwhile geton steam apply refuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
