80708ea11998f8e89bdc63921a55198071c20fd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:38 +0000

    underneath unfortunate real whichever completely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(a, b):
+        return a + b
