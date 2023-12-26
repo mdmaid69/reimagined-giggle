fac6661987ba50d4be7b31e56f22b3a910a79b86
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:04 +0000

    fast freeze easy firm patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import os
-print(os.getcwd())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
