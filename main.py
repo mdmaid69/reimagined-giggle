8f010748aaa56e9bd71f47648d2f339b176549a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:49 +0000

    develop door Ww buy seat

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
