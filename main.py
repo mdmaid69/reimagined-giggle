9dc7ea0cb4010e5332cce2b84ebbe3c8b5ea961f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:01 +0000

    petrol entry repeat common circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
