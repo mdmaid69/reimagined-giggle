bc4a9d564dd40e8cb19da8d9034a91db648df14e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:56 +0000

    sting official 568energy several twist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
