9c364fcb1c888728b79eedf4ac412a21d378d45f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:49:57 +0000

    plant quiet demand thinkof authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def convert_to_binary(n):
+        return bin(n)
