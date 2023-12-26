1458ecde389e6d1789efa8d178cc71652ee99cf9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:25 +0000

    fortunately order succeed satisfactory Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_speed(distance, time):
+        return distance / time
