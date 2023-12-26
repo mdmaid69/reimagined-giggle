0069eb49bddd1be5cce890c63db379fdfe90795e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:58:28 +0000

    achieve sea easy contain hurt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
