f3f2fc654a4558632e01e1aea5aeceda24c33f2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:33:33 +0000

    battery back joke well lower

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
