e6add965d3dc280c4ea68843362c2af5f5358ea8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:34 +0000

    turnback narrow left release ahead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(x, y):
+        return x + y
