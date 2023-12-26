e52d7993377e531d68ce8e8cfdbcc5632c9ffa55
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:24 +0000

    hold whether eat lift suit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_typecode(array):
+        return array.typecode
