7cf074d779f4704abc5306adef52a68df4114626
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:29 +0000

    dream contact angry expect border

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
