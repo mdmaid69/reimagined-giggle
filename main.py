5abdc1b02c62f32030978723bb7a3fc873f6bf6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:41 +0000

    ours used add lead join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
