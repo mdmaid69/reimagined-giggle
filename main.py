045ca790ab5bc03c36ac3ab5bc6033e0e63f4535
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:38 +0000

    long zero ordinary giveback work

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
