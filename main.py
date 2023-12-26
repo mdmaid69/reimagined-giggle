33eef35736ecbfb601ef86f3b10fb093b79d82f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:49 +0000

    strict say study course recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+for i in range(10): print(i)
