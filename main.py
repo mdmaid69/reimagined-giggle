8d8978ee1651e25fbc0050246efd38926dca3b15
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:24:33 +0000

    anyone trip region spot not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def delete_file(file_name):
+        os.remove(file_name)
