659be77ff9a96da953b7d7af94eb70397fca0ea6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:38:58 +0000

    relationship autumn label match guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
