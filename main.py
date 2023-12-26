003b2d6d25330da92bffa86cf41a2846e788258c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:42 +0000

    translate list run holiday speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
