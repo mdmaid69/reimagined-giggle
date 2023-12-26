ec2c73b55eaac0e6104e65917889084c4f4ea09d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:36 +0000

    angry brush getaway country raw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
