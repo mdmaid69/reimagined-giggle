d7184fbf5b361830c2777b79f302e652a075f34c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:53 +0000

    569enjoy all drive exact style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
