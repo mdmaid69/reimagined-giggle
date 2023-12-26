51b3653ffb2b7df8703a08e65409d22ccb73962a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:31 +0000

    minute everybody pure hit more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
