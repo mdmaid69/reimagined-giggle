8aaeee574eecf1b051e5bb98419a6ac8bf15727c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:57 +0000

    majority any interesting scream defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
