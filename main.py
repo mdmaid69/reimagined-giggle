58d415f26bfc4f15addb42ddd6fe3090280019d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:44 +0000

    pride schedule crash sometimes again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
