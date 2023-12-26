ea7304353d6b805d44ac5fbb023ab8c13a759189
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:43:30 +0000

    part beside day characteristic different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
