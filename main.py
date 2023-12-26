c0c9e0933652bf09ca69162c05071169c1727b5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:55 +0000

    mark dear care talk belief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
