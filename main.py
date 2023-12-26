e69e437ec45c69eb1642ac2d401e9819f003f65a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:22:07 +0000

    off trap major left 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
