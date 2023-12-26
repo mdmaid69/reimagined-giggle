32d53490fd354f2ed4401ad489e6746b7d051cfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:35 +0000

    simple accordingto important holdon down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
