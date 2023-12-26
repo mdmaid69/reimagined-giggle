e0dfd975e3cdc4b83f05cae0da839e39c43fdb9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:13 +0000

    related lean through first pause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+  def subtract_numbers(x, y):
+        return x - y
