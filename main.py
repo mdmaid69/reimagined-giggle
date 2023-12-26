be8385c82ba8366c8d527bce0a9bf654d41df6fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:36 +0000

    thankyou Jj target in correction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_power(work, time):
-        return work / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
