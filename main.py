d8643d274674967c3760a9ac58bc22c95ce48037
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:23 +0000

    path show gentle anywhere interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
