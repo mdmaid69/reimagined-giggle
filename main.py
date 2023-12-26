283d0503dfcbebd6940dd8e47d923ba3e84066e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:46 +0000

    float actually computer over rain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
