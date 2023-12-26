c9c0ee1a7d9247c6a42d87a2008764db5ce83197
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:50 +0000

    schedule cool damp speed goon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
