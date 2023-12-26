7f82954601e581b57921a91b0df122bf4daacad1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:45:18 +0000

    curious whom definite fine desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_gamma_function(x):
+        return math.gamma(x)
