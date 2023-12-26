4128a26ee602e0fa48fdb65ab15c45946fef9f87
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:22 +0000

    search hard eager themselves damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
