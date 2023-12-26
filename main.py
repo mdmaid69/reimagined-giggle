6be72e50e0cbf6210f4471384c699c3a3abb18df
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:11 +0000

    mass steal general shop job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
