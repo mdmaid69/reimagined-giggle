b985c2858123cd3c0179e1edf13e2e4006690e20
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:42 +0000

    unfortunate as cum dull say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
