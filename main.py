917db18d3ebf6bc157285bcb56d518e08b77c433
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:32 +0000

    home message firm plain ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_floor(x):
+        return math.floor(x)
