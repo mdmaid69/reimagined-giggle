188734ed5d7a1e9169425ea391111ee56c4af60d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:40 +0000

    Zz trouble disappear advantage reduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
