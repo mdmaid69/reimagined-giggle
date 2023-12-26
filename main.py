85ed219e79200ab46f36b4135a9a9c68d1b5f19a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:26 +0000

    offend unless tongue turnaway contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
