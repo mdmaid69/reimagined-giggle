3fd2ccc74f887a1216a268dd1a11267c69a09cb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:11 +0000

    prize Ff decide comment murder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def count_elements(lst):
-        return len(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
