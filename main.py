20525f1dc7dd453081dd77ed59536178b3e0c14c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:16 +0000

    modern debt blow somewhere my

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
