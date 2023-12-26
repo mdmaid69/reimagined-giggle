7ee6ba00a3d87695444a75b18394c5ccf97d7402
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:09 +0000

    agreement copy glad mood system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
