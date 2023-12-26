1c5e2d7449d47f8fa40f0238c2c2944483e5ff16
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:53 +0000

    admit bridge bad entrance sport

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-for i in range(10): print(i)
+import random
+print(random.randint(0, 100))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
