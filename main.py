233aeaa5d787397d9be5d8f8e2c9ce98eb34edae
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:42 +0000

    Pp whom else fast belief

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_even(n):
-        return n % 2 == 0
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def exit_program():
+        sys.exit()
