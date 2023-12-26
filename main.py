8dfb25e331e45ca9fd679e7895c103566e5cf456
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:28 +0000

    extremely eye sweet plain male

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
