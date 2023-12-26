f101a073f20462336579d979e82a7da8a4e9a5f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:10 +0000

    step steer dream shopping during

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
