2e5be4a7fdc409efc6d33e17426a5b3b884689cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:21 +0000

    object try advantage then none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
