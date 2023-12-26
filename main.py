0f5be7f37e3f2927dc1e0f69d352c326bacf171a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:28 +0000

    practical charge warn provide serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
