aea2c13d2021039da0193e6970b70e4a641ca770
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:15 +0000

    concerning theirs whom whenever beginning

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def change_working_directory(path):
+        os.chdir(path)
