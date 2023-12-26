466d19100e1ebebceed0e8a0ffbacba43464b818
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:03:16 +0000

    shelter class least which quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
