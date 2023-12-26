be9751e32a8572502a7eb9732dfc5130deb94a1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:50 +0000

    positive term unpleasant related turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def cube_number(x):
-        return x**3
+def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
