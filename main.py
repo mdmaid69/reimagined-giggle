4489389e15484403200ae05e566631b47ef393d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:08 +0000

    use bit comeup book smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_min(lst):
-        return min(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
