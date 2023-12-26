7e082b58902b7851f90c94de7dc358a04d8d58d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:28:55 +0000

    operate put sink later sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
