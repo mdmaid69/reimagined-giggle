fee8c9f164c3f651632b8065199d8513799b14cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:41 +0000

    Ee stiff advantage wash consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
