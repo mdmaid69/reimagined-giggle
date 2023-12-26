632bdd25f826218c7b9329051eace2b6be73305b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:08 +0000

    dance 572enter everywhere song fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_as_str(array):
+        return str(array)
