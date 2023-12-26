670acdcfc651283563e6228b9741221cf2b7efcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:00 +0000

    software careless arrange politics sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
