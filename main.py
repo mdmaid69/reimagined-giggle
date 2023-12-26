8e059d97560b905f02982d645ce694d91f77aa70
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:06 +0000

    influence holiday sail negative march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def clear_array(array):
+        array *= 0
