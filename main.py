32ccadb76e6af05c7e7dad7d2cd52653829b661b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:27 +0000

    trade danger law hospital circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
