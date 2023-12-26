29fbbed8efb860bb491d7be4b244e8fa89c04e18
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:42 +0000

    scrape shine spoil love butter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
