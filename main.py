a2467c649dac8012d0d6ddcffb2e3a4867868f69
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:35 +0000

    tend sight intend lock adjective

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import random
+print(random.randint(0, 100))
+  def remove_duplicates(lst):
+        return list(set(lst))
