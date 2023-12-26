4a8a9c7e4588d6e24a46f671dec48d46aaa261b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:29 +0000

    rarely habit plenty exhibition figure

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-print(random.randint(0, 100))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
