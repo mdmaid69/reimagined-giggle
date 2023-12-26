3edc6d5cb9873a0a9370d6d51949fcc620def85b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:09 +0000

    aim introduction breakin what can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
