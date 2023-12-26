1405f3f3442b3cf0080c32e5a4b7289575c3f70f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:08 +0000

    draw decide excuse valuable forever

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_pressure(force, area):
+        return force / area
+def count_words(sentence):
+        return len(sentence.split())
