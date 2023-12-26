4653f6bae90ccfb14dadd259f3e85ac60d563b00
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:52 +0000

    trousers concerned guess strike fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_index(array, item):
+        return array.index(item)
