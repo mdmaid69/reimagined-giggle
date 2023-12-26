42321cd72657023055a4c4ab6ffe92e357959a30
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:45 +0000

    really general apply statement arm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
