aef63c5c3ca4a483e4291fed0846016ea221378b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:46 +0000

    previous guard year pretty picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_power(work, time):
-        return work / time
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
