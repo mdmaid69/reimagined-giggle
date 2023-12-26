fdf376e1499d62356e6ca27a5ecd991c2c6e7bbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:20 +0000

    deserve between occasional belong minority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def is_odd(n):
+        return n % 2 != 0
+  def convert_to_hex(n):
+        return hex(n)
