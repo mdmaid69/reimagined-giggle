35202052cb5aaf0d4df8a1654a28c6fa2c57ddd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:55:47 +0000

    event smell change prize interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
