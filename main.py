fb596af81a8bf377c11bd386ce008e66996c00b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:42:23 +0000

    wait post related survive behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
