45ebcb0c87b556b762a60857030f79bb14f59874
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:56 +0000

    unfortunate sing sister usually better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
