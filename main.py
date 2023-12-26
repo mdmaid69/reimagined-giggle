452df07e3d9308ea67a701cdf30b7b7602645ec0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:56 +0000

    job turnout public path art

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_base_name(path):
+        return os.path.basename(path)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
