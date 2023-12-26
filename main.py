248e3a7cdefe740b83ca1865340f1488979c2993
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:35 +0000

    dream she division right cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
