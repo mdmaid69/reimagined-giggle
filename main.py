9c063c9abe32c6b9325109c446bd89fec77c4a12
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:57 +0000

    protest nose confidence otherwise information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
