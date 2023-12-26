2bb2562cd7d1c6bd598c741994657caf0962651d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:31 +0000

    spare smell land manage flood

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
