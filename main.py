8d1da40dad963928d53446a14ec2dea9746da647
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:35 +0000

    necessary terrible sensible action occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_typecode(array):
+        return array.typecode
