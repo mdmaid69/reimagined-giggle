6a80ac4b4afc78796cf07fa9c10bf752b952e5e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:30:21 +0000

    tidy justice exciting solve bar

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
