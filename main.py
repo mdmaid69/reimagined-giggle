c1f4dd6cac58b5a75b1fe2f741e0c5ef0fa3fb7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:05 +0000

    workupto winter split supply abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
