aada9e1561267a09ffb9978249677824343d519a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:55:15 +0000

    pay bye range confusing run

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_density(mass, volume):
+        return mass / volume
