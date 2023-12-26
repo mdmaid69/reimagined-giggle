36e38068225a99de73c68bae6f3da7c35bd22f5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:02 +0000

    unpleasant vanish intend stage fasten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
