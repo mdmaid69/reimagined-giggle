2cdc84668ce1a1e07dc96afb0e4713496de94528
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:55 +0000

    occur second rub far strong

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
