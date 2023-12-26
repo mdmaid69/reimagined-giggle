bf7e4f572b72c99927ec19306a9bfe29e9701954
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:09 +0000

    collect except open education release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
+name = "Python"
+print("Hello,", name)
