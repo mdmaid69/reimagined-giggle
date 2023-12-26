c53098767257cd9e9cf8f2e1fcd325e311d7e108
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:56:18 +0000

    learn meal politics turn related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
