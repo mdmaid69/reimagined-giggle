e371ca8d2d45775545749eedab0fb72d971deb9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:04 +0000

    radio clear shoulder broad butter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
