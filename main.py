326e8a493af730ead21317b82a1f69a7c932a935
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:38 +0000

    sit heart put bread dull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
