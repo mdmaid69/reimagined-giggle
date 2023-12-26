ed2ac34d3c5a100bdbc9c0bf0f4223d242799457
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:28 +0000

    everything wet whenever behind adjective

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
