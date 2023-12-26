df92916758300dc01b1bac1f331c7ccaee6305ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:50:55 +0000

    interview minor dark turn instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
