6db811dd91cda296dcf6812c9aa68c60e9479dfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:19 +0000

    change them protest definite wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
