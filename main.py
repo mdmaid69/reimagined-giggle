50473d685d5e3347c556facd321aad3d35440a35
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:04 +0000

    bottom half study turn height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def append_to_array(array, item):
+        array.append(item)
