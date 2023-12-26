083f2a52d036346f291c638399b66bf69c32d8f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:42 +0000

    hello abroad opportunity life team

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def cube_number(x):
-        return x**3
-  def subtract_numbers(x, y):
-        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
