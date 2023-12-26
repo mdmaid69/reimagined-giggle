0952b41a6378bac9c767f1fcda79722cbf1c6d25
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:36 +0000

    market favourite takeout hear earn

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_even(n):
+        return n % 2 == 0
