5b2c62c3e090bc8cfaf9a356e1f6646c47789024
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:18:00 +0000

    sport press pain workon beautiful

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
