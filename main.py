1d9b3b84fe4d358c6f846f65b5f93966d4504c4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:53 +0000

    572enter shout action influence 566end

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_min(numbers):
+        return min(numbers)
