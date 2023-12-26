432d5d051132e67c4731f3c77882fd5bb1fa2565
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:56 +0000

    Gg prevent concerned goodbye opponent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
