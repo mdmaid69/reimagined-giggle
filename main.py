3fbfcdada644d649958da7c398bf07fcb7425a56
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:02 +0000

    bringback law properly read coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
