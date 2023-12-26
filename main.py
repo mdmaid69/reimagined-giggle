8ae29a7c86ab9b8901ff26817c894c9316383bac
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:05 +0000

    comeround among fall with control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
