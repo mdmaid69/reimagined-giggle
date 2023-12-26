22fc2cc9ebfd8ea3ca1862e0158fd3fcad3d8db4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:23:02 +0000

    slice confusing there welcome circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
