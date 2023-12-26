fb2b0b63adeb7fb7a5e413a2b54b295fd8805235
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:04:22 +0000

    question dick comment convenient event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print("Reversed:", text[::-1])
