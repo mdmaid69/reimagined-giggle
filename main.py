d95fdeec7f2aafcf2dafb587554c29106852a122
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:25 +0000

    squash will knowledge best gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
