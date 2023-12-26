41597cdfd5872c19e9490e368bb52b8763934b78
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:16 +0000

    not prevent comment save gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-print(time.time())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import sys
+print(sys.version)
