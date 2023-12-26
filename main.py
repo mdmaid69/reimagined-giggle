6a9332aa3c2d534bcecd65e06e6c82529bb6d849
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:36 +0000

    fortunately course meaning change cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
