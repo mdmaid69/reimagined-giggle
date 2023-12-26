463d0ce9a738b9c50965da1357215418daa6e1d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:51 +0000

    exhibition pleasure ball defend pocket

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
