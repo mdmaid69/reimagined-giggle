8bd6f89e69e0eb1720b14a5c73bf4504c7b6f4fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:37 +0000

    accept clever beginning we hit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
