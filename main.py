36a47f80d93bba46c78ad8f0529e906e7e0abba8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:23 +0000

    impossible wheel unlikely feel occasional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
