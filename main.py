8156e310469b7227a4372f0a75399eb8789a5ce4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:48 +0000

    talk justice approve discuss complicated

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
