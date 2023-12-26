2bef94271bba4ac5ae4b9b0ceda66ff125381647
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:25 +0000

    smile earth giveout normal square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
