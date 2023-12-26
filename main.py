52d456328bd8f0cdf2a57ffca34d9ce836574c12
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:37:39 +0000

    radio wake clue either bread

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
 import os
-def remove_directory(path):
-        os.rmdir(path)
+print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
