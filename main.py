04b271d4e1d0498be6028d7b752a9b7f2f2ac0da
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:00 +0000

    shock piece mark sick dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sys
-def print_python_version():
-        print(sys.version)
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
