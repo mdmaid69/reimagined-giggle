d15c5913cb40b300f5535d97b5de420274d76056
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:27 +0000

    exploration television transport odd deep

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def is_even(n):
+        return n % 2 == 0
