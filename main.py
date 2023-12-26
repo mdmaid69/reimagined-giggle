bd5af2d4a2bbf6944a7d996036dba3d3bad9e422
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:58 +0000

    skill broad fast copy phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
