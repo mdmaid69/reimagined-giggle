e6775f9f2a1d0c3366187dbe55e447b3a3a9f5f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:08 +0000

    smell popular since straight intelligent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
