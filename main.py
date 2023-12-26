f9b40904b6c17df09269640d85624d854e3440e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:15 +0000

    ahead jump slice bag interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-print(math.pi)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
