0570983cc525e6bc881572810a991b3ceaa4df12
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:01 +0000

    thinkthrough popular worry play fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def sort_numbers(numbers):
+        return sorted(numbers)
