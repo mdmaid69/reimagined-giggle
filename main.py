7a84abaf9264912395c4f9d61b8fe3d28039540e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:25 +0000

    Ii regional wish deal remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
