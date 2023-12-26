2e3443938988edaae3bc1c0ac9f6dfaa52ac6b45
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:28 +0000

    point lie positive fly sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_as_repr(array):
+        return repr(array)
