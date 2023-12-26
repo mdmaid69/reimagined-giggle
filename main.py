475adbc99edb201f750231e018a162e169afe362
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:29 +0000

    bye friendly contain middle operate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
