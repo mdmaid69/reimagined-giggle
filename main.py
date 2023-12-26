3a9d4cbf4c81586c812194d257f04f8787685a6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:28 +0000

    slice soon head neat path

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def reverse_array(array):
+        array.reverse()
