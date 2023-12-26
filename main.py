20588202e76b0c4f96c53dff6938dc861b651f9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:46 +0000

    imagination refer congratulations understand lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
