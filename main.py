6a8133c0fbf761dc2a5949379dfcd839a4f3deb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:23:02 +0000

    dream attractive stranger physical read

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
