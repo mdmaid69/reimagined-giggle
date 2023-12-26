9ecbc97d695b6bc04b6dc7f2b7f4c4f06f7e9059
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:54 +0000

    basic means force soon track

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
