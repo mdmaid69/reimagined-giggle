17e58944ea5370172658947365a5b38cc3110b53
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:08 +0000

    follow up disturb yes mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def calculate_variance(arr):
+        return np.var(arr)
+import time
+def get_time_since_epoch():
+        return time.time()
