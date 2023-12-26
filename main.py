62d733aed802a8a9633f693a3e57d71675151eea
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:52 +0000

    schedule read building keepup bottom

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-print(time.time())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
