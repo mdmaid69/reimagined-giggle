22124e095cc38fc2c18eadedadd9a4f4dcf67768
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:32 +0000

    kind mixture dark goaway boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
