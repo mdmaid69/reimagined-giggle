51315735a771bfd7e57dfaa59f33de01ff9a8213
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:20:18 +0000

    runinto combine seem excuse married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
