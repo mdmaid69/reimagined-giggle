7239d22b7f06d600b9fb5a1d74e92cfae438ffa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:36 +0000

    tired afternoon small Cc cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def reverse_string(s):
+        return s[::-1]
