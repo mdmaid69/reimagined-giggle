fdd878355837a396af711154c40a62ce90491648
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:47 +0000

    confuse manner freedom break use

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import time
+print(time.time())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
