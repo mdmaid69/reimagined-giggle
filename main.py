c762bf4e5ab842b71e428c2c71f3916ff472ec63
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:25 +0000

    both software kill afternoon pull

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
