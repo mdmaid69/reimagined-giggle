7f05b80a6812d0ea66d5eef2f6712496f8712f83
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:20 +0000

    keepoff cheek group cheque worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
