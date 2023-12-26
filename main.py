fa403e4196200e3ef1c47aeda30030423bd5b2e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:09 +0000

    568energy flat scrape pass our

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
