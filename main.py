70ae7dcddc910d378022f00c1d0bec4f998b11fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:00 +0000

    flat cry bunch disgusting whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
