af9ce33f42f766e1fd8ee118b6ca12dd0365a051
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:27 +0000

    this there work emphasize Ee

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
