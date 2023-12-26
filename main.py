4198124c2b7c4c35c03c0205e460f7457122b911
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:09 +0000

    squash disturb sorry start once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
