4660f5b4684d56a87d32b50c81a549694272bec4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:43 +0000

    reliable laugh point equipment match

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
