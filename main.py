90a75bcb51273b3012d7975fbdfda6d04835e353
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:15 +0000

    wall public beyond band lonely

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def multiply_numbers(x, y):
+        return x * y
