f9ea4e11f2579351bf6677ab7b93fbc3f55f8330
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:30 +0000

    talkback standard letter our black

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def reverse_list(lst):
+        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
