6010a49a773b398718900f42cea3fdfefd8298e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:13 +0000

    taste born once range well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_volume(length, width, height):
+        return length * width * height
