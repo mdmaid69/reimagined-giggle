763e07e2cd84017dec63c68e2befcb045bbf2e1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:18 +0000

    aim year load rub language

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
