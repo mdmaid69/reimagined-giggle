417ed4b034a21382018b564ae1ff3afb1debc426
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:28 +0000

    bath cry raw month settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
