377e845fae6748e4eb785aed955e4913969353d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:08:49 +0000

    ought special direct pass until

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
