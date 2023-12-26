62a0b1920aaa06f21fd94a82196c270d62fa42a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:24 +0000

    remove respect discover grow damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
