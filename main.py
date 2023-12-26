274a6f0e82b97adaafc35e9cf0ae5b4aa3620a3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:07 +0000

    keepout relative combine debt news

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
