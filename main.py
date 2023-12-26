78398dca4cad1f0a7cd1488a591a1ea6dbb106c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:00 +0000

    find song sound group survive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
