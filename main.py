9f0a64021fbed78ff179c50b3e1aa21e4cf8d477
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:03 +0000

    blue pot cough main survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
