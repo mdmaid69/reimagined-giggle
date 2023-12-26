ded74354f7034fe5685a949fb314217a80a129b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:56 +0000

    care deep appeal brush anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
