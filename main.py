4d7bdd01531079dd36a840e428ce023898e2eee9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:03 +0000

    table smell talk brush large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
