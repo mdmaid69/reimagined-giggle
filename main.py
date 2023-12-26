d3d365f4ee18a76564dbb6dee634279404a2ac73
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:58 +0000

    fear everywhere wide water himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
