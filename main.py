7e21e627d4ee747a5e5baf88b4775be141664448
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:01 +0000

    carry speak disappear use during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-def print_python_version():
-        print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
