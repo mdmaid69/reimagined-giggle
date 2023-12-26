1615be362515d1d069151770105f41e652be6c47
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:12 +0000

    force cut violent complete do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
