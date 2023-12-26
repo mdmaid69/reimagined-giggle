aa68094bc1099365e42ae34a239394e4cf61d717
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:11 +0000

    edge loan thinkthrough towards declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
