b10a5713698f4ce98dbbf398331c1d2dd02a3494
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:53 +0000

    education normal could finish gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+import collections
+def create_user_list():
+        return collections.UserList()
