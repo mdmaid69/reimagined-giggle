e743d51d15fa15bafaa876dbf3a5c9007bba07ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:22 +0000

    love cry safety genuine entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
