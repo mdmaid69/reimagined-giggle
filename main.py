9d58c48eae5bacac59c2edf1b530705015eefc76
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:18 +0000

    moment amount prefer phone shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
