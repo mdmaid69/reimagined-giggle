b478c2abbf45014a71122ee103b275f5c8e948c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:18 +0000

    same strict land frame cheap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
