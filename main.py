50c4eb673b5e950a8bd29665f2897f6cff17ec64
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:51 +0000

    excuse row fresh forever container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def convert_to_binary(n):
+        return bin(n)
