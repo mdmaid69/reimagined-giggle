4761234407bc7894188adf29ca936a3d1cec44ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:03 +0000

    although plate stranger instead holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
