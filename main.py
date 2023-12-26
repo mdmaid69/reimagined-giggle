980c604f4c57fbaa3ac15c8e7714deb5373215aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:04 +0000

    tidy spill neither roof sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
