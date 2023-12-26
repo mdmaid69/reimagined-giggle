6c2f218355c8bd05dce650f662465cf47599cd82
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:43 +0000

    church minor Pp left office

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
