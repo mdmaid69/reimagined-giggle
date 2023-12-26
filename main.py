9d7ce81e1898952637c6b0c6843a209052740371
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:57 +0000

    surprise and walk spill please

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
