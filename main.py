c34dda1f063deaeaaf34808bfb6791f9b4e10abd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:55:45 +0000

    mood takeout goon wheel realize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
