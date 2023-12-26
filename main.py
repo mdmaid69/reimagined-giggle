6fcf1d4f4592c802aaa2d297fed6d72519bc86c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:54 +0000

    responsibility legal prize lunch advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
