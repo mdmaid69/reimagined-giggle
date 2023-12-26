46316615bdf2fcb31e2072a7a3196cca89efad97
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:56 +0000

    bang carry operate perhaps worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
