19a26c8227701443321c4e80e336173aec4ad444
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:00 +0000

    another callback hardly workon deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
