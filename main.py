72ef7f1f9686642b66534780c851094ff6b1effc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:32 +0000

    for plant giveup brain mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
