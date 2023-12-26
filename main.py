63d8311351ccdfd83be68c0a1394d52e6bd38ba5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:23 +0000

    operate contrast error early lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+def change_working_directory(path):
+        os.chdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
