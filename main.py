7aa8c6c134cdfe10d1e81a060bb133d748e58a4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:48 +0000

    depend chief delight impressive hurt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
