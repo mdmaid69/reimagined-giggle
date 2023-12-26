3914a8b6b430eeff4b2af7a32ccb4b1d98989717
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:38 +0000

    recently store comeround challenge telephone

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
