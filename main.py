ec2e2f5d4afcad80a0b9f6d223b0b3ae398de2ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:50 +0000

    tread high lunch judge great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
