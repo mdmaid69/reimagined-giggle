e34184f51270fb6de9cd560533d70a28fa88dd4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:58 +0000

    concentration off yourself charge movement

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
