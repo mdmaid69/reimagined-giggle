038c320d0c0ebb79b8ae899b047c8aeb18385ed6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:06 +0000

    probably us tape centimetre afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_max(lst):
-        return max(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
