2ad8c5c4d3f3116bd3735ee3a63ac98adf46693a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:40 +0000

    reply town complicated wake callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
