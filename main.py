7c93befa6ef290270f297fc49c5172d2ec3af5ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:56 +0000

    sort waste sail speed female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
