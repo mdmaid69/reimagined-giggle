8a1b075982af4f0331eaef7181769fac3e2d9653
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:09 +0000

    check support bar yours should

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
