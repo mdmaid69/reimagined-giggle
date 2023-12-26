843b8d5ff16fbb01dc6b236334df8303fb7c8ee2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:22 +0000

    contain goout past mean advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
