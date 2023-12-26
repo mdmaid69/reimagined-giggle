9636a236feea6a15ee731a896960eacf56dfbcad
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:24 +0000

    stop anywayanyhow gather failure crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
