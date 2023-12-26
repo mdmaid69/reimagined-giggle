770edcc42a52f55b6d0e0e12f1fde3e27c0ceca1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:24 +0000

    majority breakfast smell shout the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
