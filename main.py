33cd93b703d33e7dac9e435b3336281b56bffc58
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:33 +0000

    term getout send think thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import os
-print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
