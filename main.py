bad2a7aabcb7a88e1beb233f98e184f3d9573a4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:34 +0000

    study card shut difference high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
