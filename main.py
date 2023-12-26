a37a0c25a3ee93f6051994fe969afa445caea79a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:20 +0000

    relative fortunate sense comfortable sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
