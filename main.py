6f68616a8ce7a2e3c2ab7cd5c0a868dae0933c31
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:06 +0000

    go account she heavy black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
