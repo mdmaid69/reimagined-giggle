bce525e6fa92322b58406c4898266925dd4ec294
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:15 +0000

    one round about solve steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
