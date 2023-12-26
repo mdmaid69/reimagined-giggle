80eccd8c871787934d01c0e7908cd30ec4bda305
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:48 +0000

    section recommend invite hurry sure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
