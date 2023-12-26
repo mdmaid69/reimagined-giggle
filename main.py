e011e1c34849dd9ae486ec2c1ba9beaa837f6716
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:44:15 +0000

    play ball explain spare fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
