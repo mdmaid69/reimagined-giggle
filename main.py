0136ee4ba7a32ef71fbe2283bdec251dcca7d4fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:49 +0000

    skirt effort operation now bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
