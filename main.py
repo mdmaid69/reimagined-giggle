0c5eb547a467c4189d36c992aa0a280e078d8f4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:39:41 +0000

    accidentally below cap chase combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
