80b2da499212596661e77b4fba0f466fa8b2ab3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:04:48 +0000

    curious quiet hand meal maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
