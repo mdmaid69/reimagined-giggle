bffac1d5f311fe9db67507ea2d8dab83fcdf2a5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:26 +0000

    build embarrassing 564encourage sad lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
