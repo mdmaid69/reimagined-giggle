0b22b027bd40ee06095e883ba876826417da8cfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:25:37 +0000

    lead after pretty Uu friendly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
