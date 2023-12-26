a8a78c3a1bf3450bb22259675708603324093532
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:33 +0000

    bin visit school noisy he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
