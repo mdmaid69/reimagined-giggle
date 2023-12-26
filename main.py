2bf29ee6d584a4ee112ffe655fd0e28f5cce6dca
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:10 +0000

    tragedy ticket attitude seriously discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
