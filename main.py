e74465bc670408977a352425e3a5588c4efde760
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:46 +0000

    mess light cough command field

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import time
+print(time.time())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
