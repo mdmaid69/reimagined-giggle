9cff1969707a3dd5fed43ce3837107054cddc352
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:02 +0000

    left change uniform this carry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_area(radius):
+        return 3.14 * radius * radius
