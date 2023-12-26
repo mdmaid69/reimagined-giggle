6d8e7dbd6212c9811cbd47e2eca50d7018b36f8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:08 +0000

    minute force complete far comment

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def append_to_array(array, item):
+        array.append(item)
