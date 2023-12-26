06318218ecdc062bb4c63fcc6b63bc34e3e4a625
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:40 +0000

    free first actually cross war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
