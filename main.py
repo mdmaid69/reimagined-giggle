c6a5b9f38267b98d2a9ec141c7bba1485250c963
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:56 +0000

    breath hold convinced promise hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
