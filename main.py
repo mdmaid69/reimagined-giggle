3363dfdd0a3b2a974e745a0dc3f814d83ad4b2b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:41:49 +0000

    willing worse fat squeeze out

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
