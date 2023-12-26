f153bb3c1bea39cb1d3584ee196fd787e084e624
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:11 +0000

    rescue separate story second future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
