a3b22b47f2c60094f9a0a388a0a107f8a9046302
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:21 +0000

    straight extraordinary tend miss regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
