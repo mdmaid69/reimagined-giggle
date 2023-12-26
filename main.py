ec5e4e7c8f39e76e190cdfe9f5ab246bbe3d3faa
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:13:30 +0000

    jump bend into central shame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
