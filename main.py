1d09fc588897c317a11b52d14e6f8c88ed4b0f8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:06:07 +0000

    time least flood beautiful ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
