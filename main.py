d0837d9208345bece21eedad712bb11a67f728c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:10 +0000

    drive cost bored name delight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
