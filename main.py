0545bd128caea7f2d071a352c9079361cdd43cc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:59 +0000

    increase although weigh just opponent

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
