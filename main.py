fb42366487e4e1b7c9c040d2dacc6bedd4e127c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:57 +0000

    pattern Ss health lonely design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
