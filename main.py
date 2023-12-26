17ae500dc1d39f6ef72eeec1e815ea7b631713ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:03 +0000

    lost respect bang system all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
