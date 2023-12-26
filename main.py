06ebec4a4e28b5a4a88bc0ae2dc036dabcf5cb2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:48:57 +0000

    secret like side workon store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import collections
+def create_user_string():
+        return collections.UserString()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
