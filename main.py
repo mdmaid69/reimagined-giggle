279448e7eff6f805c2ceb685fe02dd722a3dd975
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:25 +0000

    deaf trap word activity process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def remove_duplicates(lst):
-        return list(set(lst))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
