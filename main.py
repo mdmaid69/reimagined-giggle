f1766abbfd25963637686769527f2b8c55c5775f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:55 +0000

    schedule foot progress wipe mixture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
