3550fed1a0f914bc91b7533da791b8ffdb830509
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:22 +0000

    comeround doctor office demand interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
