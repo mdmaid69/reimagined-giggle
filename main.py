087d825def3c854a9335b2bdb75543b1e528e8ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:52 +0000

    wrap heat spring coffee pocket

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
