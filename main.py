af5d080159864831c7c15a649b526012217f504c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:36 +0000

    silent get violent watch too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
