889eb05a590c9f55046c31cd1fac18786d92dc5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:54 +0000

    comeback issue patience pass control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_elements(lst):
+        return len(lst)
