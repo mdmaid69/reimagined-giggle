ec69184e2560c4cb4383c12415f4ca0f37209e22
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:17 +0000

    set view 572enter free callout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
