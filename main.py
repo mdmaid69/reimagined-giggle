6e2cdfeed50e593456a64102543a9f90c97bfa00
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:22:25 +0000

    intend start hour snow avoid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
