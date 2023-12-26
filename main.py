1e34cdb1b7d9b53bd1b5683579c99b63dfcec8a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:16 +0000

    same hot and healthy if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
 def create_user_dict():
         return collections.UserDict()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
