634641697abab06339ceca3efa7af8254b8f4253
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:34 +0000

    decision water connection apologizeapologise curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
