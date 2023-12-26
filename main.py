30b4f3382cf7b3abb0e64b705e65484af6151196
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:38 +0000

    godown scared include goout yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
 def calculate_degrees_to_radians(degrees):
         return math.radians(degrees)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
