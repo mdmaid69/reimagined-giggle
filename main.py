757828e83478971bf01570b4c72755aee953d592
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:57 +0000

    blame base duty flood bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
