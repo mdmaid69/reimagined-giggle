913938b6325805d7004b8cc62675604b16ec878a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:25 +0000

    mess pronounce outside slowly exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def append_to_array(array, item):
-        array.append(item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
