3744954e35e67821837514c667ab3b9406eb950b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:04 +0000

    change mass could Qq fun

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
