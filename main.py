d0490dcc3f1caf73e9c9f2be2a6483d5ea29c1b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:17 +0000

    offer phone face maybe spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+for i in range(5):
+        print(i)
