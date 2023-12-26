2b268879be61f1b5f05fe55c931975fa236c5fc6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:35 +0000

    step actually runaway walk during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
