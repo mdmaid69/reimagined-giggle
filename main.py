11ac372ef6a09533a2ef5e8c20fc2d21c9328e77
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:43 +0000

    apply receive under before popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
