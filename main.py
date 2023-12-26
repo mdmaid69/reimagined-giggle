9ae4bb69a52919946d66f682f1aa078219a2b9a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:14 +0000

    reason and callat of where

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_string():
+        return collections.UserString()
