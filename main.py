dd77ae4f23fc6b65313e56d343ea88b8fc089ce4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:00:58 +0000

    become fill beat catch turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_speed(distance, time):
+        return distance / time
+  def sort_list(lst):
+        return sorted(lst)
