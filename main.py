aebfe55367f434f2e0f73c3accff248458ddbdb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:55 +0000

    thirsty Zz fire call interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
