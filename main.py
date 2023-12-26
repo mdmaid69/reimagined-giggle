91e1caf93a54b38c0e9716edfafd294e1e0b07d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:45 +0000

    his pair realize stamp connected

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
