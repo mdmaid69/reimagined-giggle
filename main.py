cb051c6226e970e0a617e62e91a8e1b5f4430aca
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:44 +0000

    Kk safety realize public house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
