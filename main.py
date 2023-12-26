ae11f3ef4312fd1ec17283ba79ab42ee1e18177b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:23 +0000

    blow amount advice detail object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
