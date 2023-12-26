4fdd6df260941f57f910464d3a2fc4d3f16673c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:29 +0000

    willing achieve polite comeoff influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
