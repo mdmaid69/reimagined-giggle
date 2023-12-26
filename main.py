684c63f63fcd97003719740307c92871cf24126b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:21 +0000

    floor race help have question

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(lst):
-        return min(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
