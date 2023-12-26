8cca097945eda527877bf49cb26a49589db9d1f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:31 +0000

    might afford leave machine complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
