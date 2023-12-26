06d35ac18a421d4b01ab6789e82782fec4960fac
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:38 +0000

    brush issue where no copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
