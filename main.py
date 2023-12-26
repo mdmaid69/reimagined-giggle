3365fd1f9f0b07136030b34e5a6ae2445e16bd9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:55 +0000

    pay tend square 569enjoy behave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
