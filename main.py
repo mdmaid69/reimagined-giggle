d2b13865b17e7cb7825597d37c1a441f3f8c0a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:30 +0000

    567enemy report emphasize direct apply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
