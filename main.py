3cf5fdd4d7a4ad0a0b1e63e8f2fdb39a8cbf5b08
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:18 +0000

    set date bar path gradually

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+def log_message(message):
+        logging.info(message)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
