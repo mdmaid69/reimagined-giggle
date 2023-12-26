dad629334872e66b8063031749ce2366240225d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:34 +0000

    nor tax criticism half every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
