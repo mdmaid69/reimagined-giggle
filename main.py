e74416659b04eaa6745188974b7afa63b232a67d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:05 +0000

    gooff gate emotion carry search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
