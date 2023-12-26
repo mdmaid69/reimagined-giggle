277cba00473482c7759b623dba0ae9a9274a92dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:58:23 +0000

    arrive behave greeting run whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
