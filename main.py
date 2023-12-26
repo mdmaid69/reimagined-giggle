67f833ec315ccf025bb83fa192726349db612385
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:03 +0000

    sensitive neither activity slightly left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def append_to_array(array, item):
-        array.append(item)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def get_array_as_repr(array):
+        return repr(array)
