58e56a11ca3bba79bc88713821cca3a4c3b827f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:40 +0000

    furniture gooff kind probably meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
