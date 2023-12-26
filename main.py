fc5357efe46cdef41111cab70085f2cd4f0c1cc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:15:58 +0000

    gentle today clean upwards sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
