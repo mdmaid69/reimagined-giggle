0666416693ac1e364c82d0b632569e5d32d8c47d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:30 +0000

    shut inquire cold cost performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
 def pop_from_array(array, i=-1):
         return array.pop(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
