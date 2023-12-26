83fc26c39aba9509758392510be16846d9aa1fe5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:35 +0000

    necessary draw run also education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
