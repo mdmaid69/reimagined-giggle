c93a6b3c70fadd536732a012017c686df46b19e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:56 +0000

    design fat disappear careful measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
