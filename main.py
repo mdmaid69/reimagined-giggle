7b58861bae607aacd315772e289f0b85b336a000
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:36 +0000

    laugh accidental everyone contact male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
