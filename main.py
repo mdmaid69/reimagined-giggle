335bbaea428ab48e813f51f6510c2e4a403e1e4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:38 +0000

    survive medium calm share goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import datetime
-print(datetime.datetime.now())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
