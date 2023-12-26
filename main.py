4b8ddecd945c4965ced4e65d4db89eb0afeea123
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:34 +0000

    frame band mother bridge about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+for i in range(5):
+        print(i)
