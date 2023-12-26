54ef466cb4545fe6779d38d0d3f3524b94231cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:27 +0000

    table mixture getoff squeeze to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
