a40c0c8b5f488163217223ff81ff8ceb3274f8ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:20 +0000

    depend 568energy so Oo earth

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
