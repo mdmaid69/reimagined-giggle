1e243f3f1aa7c41c43108c845d87dd1fa90733d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:31 +0000

    cheek stay development crime impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
