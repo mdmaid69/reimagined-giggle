8472b40409c882aa43e69ff0b40a61f3214d71ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:10 +0000

    every confused completely mouth separate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
