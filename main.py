0b20c0e4879b5389610b545151cbd1f36d14cbb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:25 +0000

    best freeze actual smell through

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
