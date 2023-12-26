35d71e1246a1fece23a79b928701fc5ff41300c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:59:53 +0000

    concern fat turnout swim country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
