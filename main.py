2c0f63093b110f53ae95d54bbb27dad911e63f80
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:32:09 +0000

    cold since expect profit turn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
