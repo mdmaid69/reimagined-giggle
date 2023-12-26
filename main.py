8cad9d60c449d8742e54211c4eeaca84bc5c8257
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:15 +0000

    gentle excellent strike course interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
