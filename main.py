806a858d397e52f6b4007a54725ead1b8e1b808b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:17 +0000

    interesting level production accommodation radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
