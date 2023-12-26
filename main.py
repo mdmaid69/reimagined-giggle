350e66828c5f7cd8874630af3c63ed4f4ed1bebb
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:34 +0000

    uniform shout corner contrast previous

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import platform
-def get_python_version():
-        return platform.python_version()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
