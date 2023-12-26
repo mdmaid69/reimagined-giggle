69638b533e858714e4d46ea6b6490a253abb6529
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:00:19 +0000

    convinced turn sail pipe left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def count_elements(lst):
+        return len(lst)
