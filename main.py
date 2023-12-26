e65dfc6ea121e84e3faa03b3008fa13bc28b3657
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:53 +0000

    even command confidence ticket bend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
