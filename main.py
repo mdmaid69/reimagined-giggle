2e2f214d51b9ed2febbacb25ccd130c08e1ebc09
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:09:36 +0000

    large congratulate mine cross straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
