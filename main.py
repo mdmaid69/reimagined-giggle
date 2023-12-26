8baabeb4e206297d08dadfefac2d70797c7edf22
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:12:34 +0000

    calmly uncertain flow she weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
