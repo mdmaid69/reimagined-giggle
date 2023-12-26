d87eb5ca18272e74403c1a6b977b0aa569cb3f5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:03:04 +0000

    completely material matter chief stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
