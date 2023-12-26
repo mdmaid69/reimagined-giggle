ef2272685a370a6d8a1b012bd3a5cd48bf6d46a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:22:03 +0000

    bury centre home turnup cool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
