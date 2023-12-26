042e576b5a63f4d0972d3d3407c9757940ea399c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:12 +0000

    continue sign turnoff bother although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
