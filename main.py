8169292ca30a9dafbd8964a21a69d7fff7198bbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:21:04 +0000

    print push ahead instruction ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def sort_list(lst):
+        return sorted(lst)
