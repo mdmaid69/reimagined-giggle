611798b801986c3294f8ebb946f00408a0623201
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:40:06 +0000

    much beside below priority they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
