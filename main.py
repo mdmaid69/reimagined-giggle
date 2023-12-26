7483135ca0132f8dba28b275bf349f6a7da2ca0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:53 +0000

    room official charge rely bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_area_rectangle(l, w):
+        return l * w
