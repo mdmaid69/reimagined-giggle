bbe25326fb8a910d22d96c6e4072e49a92b0b0f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:49 +0000

    hard marry present apart height

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
