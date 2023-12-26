693e74193b315d693fde9cf2f494db58c4a646d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:45 +0000

    influence hold understand quarter sound

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
