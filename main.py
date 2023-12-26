47cbcf3c05b851d8e32ca1f9297c29abc411bc6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:00:04 +0000

    interfere quarter visit account recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
