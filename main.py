d9c042dc115a2af5f34d4842851a0fb63b74c6c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:17 +0000

    hit stop community right angry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
