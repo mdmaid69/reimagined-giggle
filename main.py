f9494453f3e025b2c3dc94ec75fe9e3aa3e6fa3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:50 +0000

    drawing plate mistake anywhere move

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
