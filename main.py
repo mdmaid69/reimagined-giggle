18164c3adef284610a8e5b14dba914fcfd7dd7cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:17 +0000

    smile Gg confusing land land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
