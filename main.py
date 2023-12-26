50489cf1e69e7106d3083b0ee2644911a5322246
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:55 +0000

    pair brave perfect combine opposite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def get_current_working_directory():
+        return os.getcwd()
