b2950cc768975d541aa2490247d7b77a2687b10b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:16 +0000

    visit frightened also attempt truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
