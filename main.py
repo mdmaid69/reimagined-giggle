feac44fdccb4b4cd6d748daa3008a07fa7bc7b59
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:49:12 +0000

    warn meeting breakout positive silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
