e6f4b665aea75bad55143e7053a619aa27b91842
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:47 +0000

    artificial boat skill poor behaviour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
