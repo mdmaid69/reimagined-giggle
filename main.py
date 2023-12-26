fedd84b3edd39c1b53655c15e20335a723e2acf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:22 +0000

    right permission building ocean far

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_itemsize(array):
+        return array.itemsize
