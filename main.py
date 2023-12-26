a87a02d414b9c0efafeff930530157e11c580353
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:45:58 +0000

    match before getaway call war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
