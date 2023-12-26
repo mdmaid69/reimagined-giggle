6f91b6ed3436ccaa28bbc047be1981c3ebeec8fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:39 +0000

    pause blame laugh long it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
