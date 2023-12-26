38b5f47ccbbf60925b72dfab3b7fb82b765b36e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:57:54 +0000

    fact object rough file cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
