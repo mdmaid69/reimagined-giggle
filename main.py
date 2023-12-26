666b08993fc310175b71d69b917ab0e5e2418cc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:48:21 +0000

    together juice greeting somehow coat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
