ca19e175f74c9ad8bb4abef833f227916c4556b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:47 +0000

    phone rule quarter gradually Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def convert_to_binary(n):
+        return bin(n)
