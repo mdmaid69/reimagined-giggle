b870e83019e07c5767142806238ee41dc7e15462
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:04 +0000

    brave broken distant relative criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
