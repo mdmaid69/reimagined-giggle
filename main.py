425e9b3b02b182ce3b880f17fb2420ddec36cc05
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:05 +0000

    no crash late pretty moral

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
