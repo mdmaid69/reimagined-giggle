29a185670c897fe7df1877be7b19384bc86c6c05
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:05:11 +0000

    its guilty smart wall lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_pressure(force, area):
-        return force / area
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
