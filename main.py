d9192b6b764640cd7854c34cb95b207fa7838a8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:45 +0000

    turnover ourselves concern right skill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
