56efcafa958fc084114f38bcf236d48fdb1c3cdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:38 +0000

    rush aim face patience path

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def cube_number(x):
+        return x**3
