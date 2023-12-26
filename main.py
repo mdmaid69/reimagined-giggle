55cb50b1b3288a1d63eb5961fe6d05441df6881e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:07 +0000

    protest equal stare suggestion emergency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
