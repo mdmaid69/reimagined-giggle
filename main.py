368abf9a706da87d7d7490550baf1cee7e5eb017
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:18 +0000

    against available load on male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_max(lst):
+        return max(lst)
