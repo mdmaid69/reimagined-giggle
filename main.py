5b9dde770005ae92a60d7fe69e9e8c04e04efbac
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:31 +0000

    stamp wrong behaviour offence last

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
