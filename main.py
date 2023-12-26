22e91371d50895529f141c337c2e865e040eac50
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:36:42 +0000

    more Ee smell Kk around

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
