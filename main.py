0bbcc43b2e5da82a600fc758f715a944eb248c3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:25:34 +0000

    board round exist charge official

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import time
+print(time.time())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
