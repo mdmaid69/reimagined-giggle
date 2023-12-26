2923c3c27cb6517a3438b4f5e9c6430ad12546b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:03 +0000

    happy loss strange could ground

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
