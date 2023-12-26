5d7af47b222698d72afa9fe808aafe5d1e7e1c5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:28 +0000

    fence possible joke history expensive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_acceleration(speed, time):
-        return speed / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
