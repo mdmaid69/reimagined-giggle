a8fa6b6e3a21de215328dd25c5a5c21ca41fec89
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:47 +0000

    relative now shine appeal fight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
