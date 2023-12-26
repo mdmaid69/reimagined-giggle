efa03c2760e112868c2e69c024f48bc48c16159c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:28 +0000

    179bite bye patience circle blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
