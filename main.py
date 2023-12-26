b32d449205879212c313944f598fd76277714d44
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:07 +0000

    figure whistle wealth suddenly Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
