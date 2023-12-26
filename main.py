9ac8b2489eb0c133aa9680145a6b37f99a127414
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:45 +0000

    although grand able guard nurse

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
