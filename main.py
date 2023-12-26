08676b35b705cae47bf92f40c659ac0f3819b4ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:04:25 +0000

    earth convincing instrument able could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+for i in range(5):
+        print(i)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
