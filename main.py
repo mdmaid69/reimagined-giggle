25fd6b6fb6ceb928f133e43fb4cb87923334263b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:15 +0000

    pay present house list whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
