c3788ae716cd02edd513590a9fcc10bf5257b78e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:43 +0000

    deep fall sea look touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def is_palindrome(s):
-        return s == s[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
