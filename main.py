2a549abad27fb9c247c94f078ce87e7642af2198
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:13 +0000

    thanks Zz child story woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
