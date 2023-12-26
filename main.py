7fec0d208191dec9633a409f1d0d71c74e87696a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:25 +0000

    meeting uncomfortable recommend suit him

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
