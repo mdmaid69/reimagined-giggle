c63bcb9938972d3531964dc8bab14a1db0b68755
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:10:53 +0000

    train between call later legal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
