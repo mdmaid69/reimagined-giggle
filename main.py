4b01f06916e7e8a76145fed33f3648898ccb59d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:52 +0000

    bunch Ll pin right dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
