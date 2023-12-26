8d64e290aa0d53653bc9d048b9fd935bac2d43c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:41 +0000

    actual comeback abroad peace stay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def greet(name):
+        print(f"Hello, {name}!")
