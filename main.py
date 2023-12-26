5794f9b6a00dba74c74cbe5c74f976cb2478ea19
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:25 +0000

    often apparently face afternoon godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
