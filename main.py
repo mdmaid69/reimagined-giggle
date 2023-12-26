890b492f853139fc454188d39a2f7593e4b675a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:01 +0000

    any load concerned shelter sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
