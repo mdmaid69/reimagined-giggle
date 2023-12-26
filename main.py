a4a463a41fac1574a6f48ea39eea90832a399860
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:57 +0000

    hold exist stand slightly while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
