f5e010aae6289d42eafea980a2fbb747fb70c5e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:51 +0000

    steer unable cheque appear risk

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
