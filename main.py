c71d8b923156cc3d2a64b028dce04cba4613b170
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:02 +0000

    level unlucky sister street direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Uppercase:", text.upper())
