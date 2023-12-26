57704b5b7fb3e84de3bab5790004293f733fc233
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:09 +0000

    can pipe accidentally Ff criticize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
