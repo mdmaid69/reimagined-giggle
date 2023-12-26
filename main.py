f2ad232e3ae10f4cf04f90385bad86a0fc6429b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:10 +0000

    way apologizeapologise takeout report plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def reverse_array(array):
+        array.reverse()
