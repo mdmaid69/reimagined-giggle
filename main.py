6ef1e886f21063f5414bcd964cb3875ffd86a2dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:41 +0000

    scared dress match substance goout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
