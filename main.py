6d5ff105581c38c434681e310857f8e767f7b3ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:20 +0000

    question burn all back return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
