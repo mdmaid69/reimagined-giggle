97e72a4e8f7af3ceee02145ac7a7908e247daf88
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:23 +0000

    just black exciting attitude grand

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
