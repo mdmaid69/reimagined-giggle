00e930e9197ff0709d56a1e59c5f60553b3455d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:39 +0000

    outside emphasize head weak fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
