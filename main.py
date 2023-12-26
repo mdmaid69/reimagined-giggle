dd24eda574f0dead12017c0e3d479941cc8e03fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:55 +0000

    runover murder gap fear rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
