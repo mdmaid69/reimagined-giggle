6b959b5c7c5e1311a52f898c36683fd4bb76a9b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:01:14 +0000

    fear respect bridge point bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
