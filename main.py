86ca5a3f46b4781afdf647b31e3830f978fbfbfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:42 +0000

    probably often that either congratulate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
