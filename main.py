eeedff9a1c88579a0f33712a4aabe6923bc26ad7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:44 +0000

    promise downstairs along sort conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
