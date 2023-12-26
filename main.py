b6323e6f729fe03cd5ae4db1124ed4e28b09f75b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:19 +0000

    obvious comeround glass spin new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_float(array):
+        return float(array[0])
+import sys
+print(sys.version)
