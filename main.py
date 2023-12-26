ea54d368d310ca1580bb16c366580804e40b0a0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:20 +0000

    wake foreign love reduce before

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        return sys.version
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
