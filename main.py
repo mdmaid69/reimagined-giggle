1dddc1f54f1dc522ce60456d7fd052b358cae883
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:20 +0000

    old test trial town listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
