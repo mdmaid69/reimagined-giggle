5cef3e89ed4f8803e5c0d62f7330f1f2f7ffa44d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:40 +0000

    think pure print bend name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
