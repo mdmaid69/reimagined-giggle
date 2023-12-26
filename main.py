54ca4786ed061bdead4715a52285b39f6f7da406
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:30 +0000

    example challenge nice what trip

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
