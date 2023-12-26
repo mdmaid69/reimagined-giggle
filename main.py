396aabd080166d0630697c568e01b46e74f224c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:40:19 +0000

    for trick second piece unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
