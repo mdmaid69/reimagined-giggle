1de19521b426e94acffbfefe9f3c681f4ffce69d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:22:29 +0000

    stiff bored explain tongue shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
