5bd0752d2e3e130e8653b0f1e8aa4591a162c827
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:42 +0000

    getup cold comedown onto once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_time_since_epoch():
+        return time.time()
