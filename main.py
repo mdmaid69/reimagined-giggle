7f6e9a4b3222ddde4d73b170221c9849921abfb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:15 +0000

    raise apply contribute absolutely among

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def reverse_list(lst):
+        return lst[::-1]
