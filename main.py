aeece0c43dd896016ac62a0ebe490392da52fe24
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:23 +0000

    cover brave zero 569enjoy need

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def count_elements(lst):
+        return len(lst)
