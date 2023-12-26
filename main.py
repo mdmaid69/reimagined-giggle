48020fe3fc8257a8b1bcb3a825f53880ad97726e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:18:53 +0000

    available use speak OKorokay bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
