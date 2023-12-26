9191b9e8ae18ba0cc42f9c5d477b585e2019c5cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:26 +0000

    year work correct which dull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def multiply_numbers(x, y):
+        return x * y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
