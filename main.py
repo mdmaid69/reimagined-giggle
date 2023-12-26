f405babeb3c7f1c37381d176daef8e758bf64a2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:15 +0000

    tread high collect chase who

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-print(math.pi)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def subtract_numbers(x, y):
+        return x - y
