6a793925f44c3a8226b60bc09bfad5deca989286
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:34:44 +0000

    familiar film total scared skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Uppercase:", text.upper())
