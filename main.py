2a43688314885a1c050f1eb637ba20f81c0907e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:45 +0000

    present hit when begin nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
