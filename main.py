d72c8365ceaf922f6a45daac44f5ca40763ffb23
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:56:47 +0000

    snow bring bringup calm collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
