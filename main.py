9d203482823a2e077fa3b388e417c6d9eae6d15a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:51 +0000

    unfair tight promise his safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
