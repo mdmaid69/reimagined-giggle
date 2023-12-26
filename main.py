c2f2aea2b97be31be4e427b67d67807eb9049e31
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:02 +0000

    thinkthrough position punishment already custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
