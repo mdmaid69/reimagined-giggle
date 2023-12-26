10b1a7c0ad098f015ee06f22e917f80ce2696792
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:43 +0000

    shrink pleasure turn alone shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
