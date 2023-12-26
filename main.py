4d0b46c18ba5a90c705d27a300ce0fdfe345e2ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:53:13 +0000

    collect run share Ee rescue

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
