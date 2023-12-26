fd9b88703a5199180739776ef1ba31f4c549c875
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:24 +0000

    little pile cool born opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
