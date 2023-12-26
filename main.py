38ee299c7d6d22dc93a38b83dd06e1c16f59ac15
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:05 +0000

    holdout increase direct contribute it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
