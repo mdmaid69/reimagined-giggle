a1143ddc354db0e0af4b821aa9032075aec7634b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:46 +0000

    separate flow plenty because innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
