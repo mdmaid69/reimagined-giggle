a90dac8eccc881539f1aa4e34181407f69d1c745
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:37 +0000

    help leave flat offence congratulations

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
