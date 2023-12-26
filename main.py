cde203eb59112cecc25feaa4dc171d3b1b23047d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:30 +0000

    attempt scream claim necessary account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
