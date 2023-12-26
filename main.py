8e528c4c94d309b803ae79e2ca2dce778fb68f04
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:15 +0000

    along Kk introduce plenty round

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
