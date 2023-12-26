2b340b428d5e0949ffcc279f3ea8df882e15763e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:58:50 +0000

    along mistake rub whoever would

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
