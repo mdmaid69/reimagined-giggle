b847125d0156aa1f841eddb31b2be71ff637e8ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:17 +0000

    sure happen lookafter sometimes spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def cube_number(x):
+        return x**3
