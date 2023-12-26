020c3eb80276fafdf95f03a6b6306ce2c0022378
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:04 +0000

    set home refer wide connected

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
