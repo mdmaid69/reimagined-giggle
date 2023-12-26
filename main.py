60529686a13deb4ed5bfdb46eb54513898ce23cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:55 +0000

    constantly century respect sweep shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_set(array):
+        return set(array)
