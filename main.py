c52718b6613a0e48274d202143239cd019ad7e2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:27 +0000

    hurt block keepoff wheel deserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_unique_words(sentence):
+        return set(sentence.split())
