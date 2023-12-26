b445ab2bab8e052a07f5562adff393f0d8ed520f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:45 +0000

    practise shout sleep picture runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
