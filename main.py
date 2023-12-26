b30d68df6ef22c9d7fab08a23d2f36bd441c5f71
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:03 +0000

    harm represent shut immediate Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
