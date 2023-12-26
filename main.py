d45a3f18cdff3f27e9aa7b312c81715091579e73
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:37:55 +0000

    brother fact trousers shoot pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
