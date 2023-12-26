2d979c15d6b8e3c99fe82c7e8212f2dedbf98ce4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:26 +0000

    yet plenty comein stare use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
