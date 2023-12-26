540eefc25338a5ee5161a5c29cb451ce675d7b62
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:56 +0000

    mine within attitude spite tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
