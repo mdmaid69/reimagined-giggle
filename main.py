37a841d7ffcc4966685223624fc8d0e94896bb1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:07 +0000

    introduction regularly smoke opposite event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
