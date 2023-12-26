22760ab4f26012f3f48ac3f8a279c1cd90a68d71
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:17 +0000

    form paper of concerned tall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(lst):
+        return min(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
