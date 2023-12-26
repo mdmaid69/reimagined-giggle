f45483e88e17f657579b9279c01ebba887df921d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:53:40 +0000

    port few different dish least

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def convert_to_octal(n):
-        return oct(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
