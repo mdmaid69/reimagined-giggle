0e361880ce1f9d0614f740180f325ca54f1149ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:57 +0000

    talkback support cut bang deaf

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
