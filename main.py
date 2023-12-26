5ebbd7d69fe77d80c983697ba053322f2d6eccf9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:40 +0000

    trick quality noone little chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
