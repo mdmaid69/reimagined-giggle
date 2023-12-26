cc959a993d24e11a71c34f23d2b426260126d29a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:28 +0000

    forgive weigh shape contain respect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
