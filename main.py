d6d154e41f96871ac31e2ee5f779ac064c91bdb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:10 +0000

    wake patience warning goout legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import collections
+def create_user_list():
+        return collections.UserList()
