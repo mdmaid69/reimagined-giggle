d13654865fe781efc88d1ea345e0be88b886ddf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:26 +0000

    brief funny suppose arrangement dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
