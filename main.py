0514316481a85537ede1b659564f7e42e7fbf4bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:00 +0000

    check swallow image maintain fault

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
