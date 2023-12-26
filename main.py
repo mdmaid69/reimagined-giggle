030dc7be1a9d73d71388668a65175b4cd4aa271f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:26 +0000

    belong Ll discovery bury plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
