c73a91dee2061595e3bf3309f6fecd5087578342
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:32 +0000

    government under luck whether area

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
