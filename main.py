e320a9a35cb599e6ba237f5f2735d07cf306a3ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:43 +0000

    rob arrive plant remember impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
