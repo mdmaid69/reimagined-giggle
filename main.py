7ee5ba3da8a3f8cb9099ffb246b0700ce437f2b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:35 +0000

    deserve punishment favourite sight excited

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
