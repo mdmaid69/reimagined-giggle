735f6e166ca6d43dc46f698b97d4414457db988a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:10 +0000

    shrink suckme side union right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
