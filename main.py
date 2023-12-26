b087e76a25938813c54c6b7a036445cb9bb3e39b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:17:04 +0000

    their stone 564encourage angry wait

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import sys
+print(sys.version)
+  def convert_to_hex(n):
+        return hex(n)
