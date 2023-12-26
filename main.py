431d85c419530422ceadbee6f9538df38a818f21
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:56 +0000

    steam lost band remark opponent

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
