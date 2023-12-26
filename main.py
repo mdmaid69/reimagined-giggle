a224c125ffd643990a08f0b879112a205c5a5fc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:47 +0000

    holdup improvement hope file live

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
