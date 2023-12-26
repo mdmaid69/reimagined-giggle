f7b3cb0e05a28844e09b83e2bde820babc4f92a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:30:08 +0000

    bank perfect through earn turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
