7d72b485786af12eeccbeea87cd1c66230385d25
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:56 +0000

    wide admit safe settle cheat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
