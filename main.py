868d4c08312313e2ca93d347ee09267e6aff33a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:07 +0000

    injury plain tense drawing write

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_speed(distance, time):
+        return distance / time
+def square_number(x):
+        return x**2
