0860a0f8f7d14cfa0ff574d23a1aa502e4b20267
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:21 +0000

    straight climb protection especially manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_queue():
-        return collections.deque()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_int(array):
+        return int(array[0])
