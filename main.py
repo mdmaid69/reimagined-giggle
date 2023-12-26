30162bf6150eef55293814e37d8f2accd3eebec9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:40 +0000

    friend fetch strange even kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
