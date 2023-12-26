fa56e82244ec5f582e9b0991cc38b0ba7e550977
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:21:55 +0000

    temperature steam shall flight gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
