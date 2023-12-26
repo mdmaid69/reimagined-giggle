c5a3d626be0a9a928a39a1c91d168d93932129a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:17:09 +0000

    splash relation set try safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
