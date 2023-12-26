be458feb62ba0fe8c9deb25fd1e6e1fb3139b697
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:57 +0000

    minority lead run sting tool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+print("Hello, world!")
