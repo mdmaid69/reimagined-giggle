78e6bb4869e7f37f08e395d5d96ba7a1f8072534
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:49 +0000

    unusual type scheme so tip

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_power(work, time):
-        return work / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
