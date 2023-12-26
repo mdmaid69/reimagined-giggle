bd9b9589b14ca3988ede601487c4073160d4fbe6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:30:50 +0000

    needle accept tight miss until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
