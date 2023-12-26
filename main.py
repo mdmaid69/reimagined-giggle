76bef8b2d361519a761e86a34051492911782ac4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:03 +0000

    accommodation short you prize Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
