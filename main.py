f6cf90cd94acbbed92d3d7ff8592e84b53e2dc64
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:54 +0000

    Gg mark attach instrument unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
