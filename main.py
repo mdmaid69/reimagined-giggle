49f06fe85ff638b5f3419735b9b90a52fed561ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:20:38 +0000

    middle until rain nothing dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
