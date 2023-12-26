e9ab8721c83d00067d5aaeac79b14793e427d12e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:31:30 +0000

    actually completely Yy trouble ball

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
