addb666508f4c1bf05b39ce69397420cc8d54fcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:01 +0000

    shame take secret into wing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
