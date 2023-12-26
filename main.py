5561ea852b8e9796de1c2baeda86687ca12d274d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:30 +0000

    disappointed dark pause what full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
