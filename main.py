7094049c1863548e8183c7b7fcf5cbcace85e237
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:18 +0000

    comment knock hope equal care

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
