f5cfdd7b8db00510d7913588554bfd560a0670be
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:21 +0000

    why afraid point many modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
