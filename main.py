59da3ee1bb3c3bbceb3332a99bf136af7c49edfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:58:41 +0000

    patience affect community reasonable purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
