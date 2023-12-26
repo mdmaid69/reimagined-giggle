fee2e89e6309c3d18a82cc46ee705f93250ae296
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:55 +0000

    herself realize issue main nature

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
