6835e6defe8ee53ff52162ca93f31bf86304b976
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:08:58 +0000

    exist contact country act night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
