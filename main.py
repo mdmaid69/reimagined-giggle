4b93c202221d1357cff1922c772358d10ccb14f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:47 +0000

    division flow pronunciation who thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
