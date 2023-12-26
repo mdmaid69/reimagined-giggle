cf3ef52b1cfa34977f842cd3b907c4660d16b540
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:23 +0000

    reason regularly regular crash spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
