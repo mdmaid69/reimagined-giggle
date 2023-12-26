ddd85647376085303220ade31d36f45e7b26fa29
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:50 +0000

    spill could for Bb how

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
