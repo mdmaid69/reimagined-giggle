8fdeab57f9386db948c558aed96905414124d1c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:52 +0000

    excuse comment start that clever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_binary(n):
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def convert_to_binary(n):
         return bin(n)
