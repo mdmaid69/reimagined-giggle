d4331bd83ed4486c75aef1cc9ae0047acda72057
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:29 +0000

    summer flat fetch subject pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
