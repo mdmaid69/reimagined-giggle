ac09ce8d4b765d95d45c2817d5c62cc0c4ac2d41
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:13 +0000

    him slow thin book silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
