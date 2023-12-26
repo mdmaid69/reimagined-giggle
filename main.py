308822f1e72c446405a133d7f4193e96bca57834
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:24 +0000

    contents warm lookup dish full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
