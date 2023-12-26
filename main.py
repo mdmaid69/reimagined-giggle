eb555481ead54b2e3f2d7e800d8914fe56e87224
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:15 +0000

    valuable affect exciting occur flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
