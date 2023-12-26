2ed0efcc667f773cc128214acab07f174653a0b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:26:11 +0000

    eager Oo piece visit whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def find_min(numbers):
+        return min(numbers)
