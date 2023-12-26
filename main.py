2fe4f2b273a62420480a8ec8e6ca03bc7390a701
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:22 +0000

    introduce move together steer influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
