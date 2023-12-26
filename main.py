50c5554c461c623d727cb95b812e17434cb3eeb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:14 +0000

    alone them distant introduce pleased

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
