86829aea14a6a402995e4fda594f3195a885f002
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:05 +0000

    sting pollution as stage turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_acceleration(speed, time):
+        return speed / time
