115ae32cddc1191462e1ea5735d72702febe4a04
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:07:59 +0000

    announce fond disagree they opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
