347864d3ec36fa2cdfbf93ff76e0b877bfdc02ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:59 +0000

    save please worse mention spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
