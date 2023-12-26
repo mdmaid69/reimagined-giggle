e08d4e19ebe460422a5ef4717774321c2bcb5475
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:25 +0000

    market discuss force crisis wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
