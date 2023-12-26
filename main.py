5f03430e308aec11bf4614338b428d5bf9d98001
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:49 +0000

    sign thought apart kind Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
