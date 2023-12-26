9605319a99c68f43508beb820e2b1831a3bb3454
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:26 +0000

    mouth afternoon respect hurt later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_max(lst):
+        return max(lst)
