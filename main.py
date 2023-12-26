3c5d54c67c611e05bb7fdf86fdf47eae872321ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:14 +0000

    pride uncertain shoulder Uu music

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
