cc8d78248e8857a847b08a591a0613d48624304d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:26 +0000

    block similar now path behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
