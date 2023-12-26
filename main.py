3fc1d6609af52115f68e1fb5cd000aff8cf497ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:39 +0000

    recover swim report brain talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
