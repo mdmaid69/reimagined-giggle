36480d0af52864bb49cd60a44de3ba32defa300d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:20 +0000

    quiet pile terrible similar difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
