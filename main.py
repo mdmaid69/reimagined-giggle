2836b67b1516c2a3e07000a33cda980d698d639e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:01 +0000

    guess stupid pump complicated straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
