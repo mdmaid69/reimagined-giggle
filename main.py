72f6b7069dd12e020efd3ce9a3280537d2482ee0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:50 +0000

    niggaacademy bother hit afterwards scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
 def find_max(lst):
         return max(lst)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
