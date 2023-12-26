4f14825e523390840ab68fbe8809b266b3c558e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:44 +0000

    crisis health probably break OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
