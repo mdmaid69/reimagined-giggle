616e67b1565ee166c0299d0c95f3af6eebb79707
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:10 +0000

    means name closely order tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
