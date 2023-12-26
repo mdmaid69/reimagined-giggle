baf819fa6e077f8836446ad0ef21c52af490bd40
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:49 +0000

    destroy bank worth neat immediate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
