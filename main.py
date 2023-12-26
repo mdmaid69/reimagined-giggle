817f667a4683d5d0f02d07921f79756b6b5a0b73
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:41 +0000

    how challenge week go material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
