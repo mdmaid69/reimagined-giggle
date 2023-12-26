82fcb063b6ae2dad7d43d0e9106a6e4e9c9ce561
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:51 +0000

    cheap tired wash mix but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
