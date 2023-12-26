7aa0cb66f8d325d77f06d95176d2aa78c5bc556e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:54 +0000

    pipe sign tradition definite unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
