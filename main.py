19367330662181ee671c33a86b7907fd339de75e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:30 +0000

    dare joke maybe foot outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
