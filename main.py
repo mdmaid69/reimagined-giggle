5cead75e058b1b571a14175eef5c7a9b82d119ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:43 +0000

    table either fast rest sail

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def remove_directory(path):
+        os.rmdir(path)
