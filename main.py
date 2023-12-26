3e9ff0893097477276983a36e2207fd8a8aefd46
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:31 +0000

    track play cold tool fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
