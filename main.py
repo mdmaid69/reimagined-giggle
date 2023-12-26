3a04c77fe2bb524e2db20b29dbed5f5374bbefd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:47 +0000

    dry communication weekend fortunate sensitive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
