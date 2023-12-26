2a420c2412b6e93169dbfc9468408c07abaeaaeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:00 +0000

    learn anywhere feed soon achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_sine(x):
+        return math.sin(x)
