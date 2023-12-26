8db039217e8586deac343cf0c3bc0ab127f35c07
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:35 +0000

    comeout back successful tip most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
