6578205ee2c06b8eb0e8009a895f2061728bbb03
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:32:52 +0000

    scream worried thing somewhere confusing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
