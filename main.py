8f7ec5f9536e6e7fa5353040f02313a9b8992c64
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:21 +0000

    ahead city act contain easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
