106b3e3523b7f56282a747a4b7c87f85f91a2fca
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:36 +0000

    return used excuse anywhere help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
