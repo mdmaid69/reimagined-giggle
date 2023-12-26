6c9a5bc05bcd41a2bbcc8913a2d22eb0fb4214b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:51 +0000

    survive science aim size best

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
