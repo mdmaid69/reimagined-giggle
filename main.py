3b8e6ee6ea8b072d5d28127f50a722ee83cf6b84
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:42 +0000

    intend bath star positive shrink

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_density(mass, volume):
+        return mass / volume
