01a2bdf0243dc911a5c17ef3a295ce00771799cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:35 +0000

    lookback(on) firm regard image general

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import time
-print(time.time())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
