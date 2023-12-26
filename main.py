b045425127c3b32df84c643f37673b6d80cfd304
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:07 +0000

    sudden heat shadow subject turnout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_max(lst):
+        return max(lst)
