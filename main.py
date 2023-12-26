29b73a7df09c2ee22a73423fa5d044d7b751daf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:00 +0000

    theirs pick food yes sit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
