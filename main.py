5657573d6919de3b4db5b8b0e35d16b0f39dd6aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:49 +0000

    emphasize thing disagree subject offend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
