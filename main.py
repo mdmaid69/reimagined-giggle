4893129dfab13694ca2f64e862b5532575708bd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:03 +0000

    produce worth past style sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
