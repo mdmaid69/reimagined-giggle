9575747700a7f85d4b6f5a76993ba5f864a5e3bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:36 +0000

    runinto sign connection disappointed Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
