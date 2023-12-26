14706b8fd3083b193726910482ecb2322c0b89e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:07 +0000

    suffer damp away mail runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
