a7957f3ca05db0b0f288e7d77390c12251512136
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:23 +0000

    tell modern connect ilovedicks hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_bytes(array):
+        return bytes(array)
