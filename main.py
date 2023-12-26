ee9548f618ef278d6b90da12d1ab88f178ee58ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:12 +0000

    thinkof storm maybe be physical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
