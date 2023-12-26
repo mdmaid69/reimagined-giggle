ac86c7fc7dcc0be985dec8a403f9d1998a890982
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:21 +0000

    weigh autumn wrong represent become

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
