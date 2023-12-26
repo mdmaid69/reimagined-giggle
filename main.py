c41659edf88270f275d3c6b036636294e67feb85
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:38 +0000

    whoever complaint calmly wish culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
