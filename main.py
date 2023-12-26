f359cf11df9160a045c3e128b06263ed9c12d895
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:31 +0000

    broken fine raise nervous sound

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
