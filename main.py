bfc8e9b9cecda2dd23d31bea58e6e0bf62e9ea42
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:29 +0000

    knock somewhere mistake gap quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
