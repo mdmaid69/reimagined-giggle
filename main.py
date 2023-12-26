63cfc659a35f01d4537b4e8a2f52d595a4890550
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:07 +0000

    performance steam bound earth strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_set(array):
+        return set(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
