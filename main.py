8c299cc0d69854841040ccd71e2e0e20af2001a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:12 +0000

    roof strong boring glad coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_area_circle(r):
+        return 3.14 * r**2
