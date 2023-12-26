0568b825156a92167d3ebfcb1e20d154e9b8186e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:07:39 +0000

    bus test slide complaint Dd

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
