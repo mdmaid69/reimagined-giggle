7083434b23b9c4fba465791382017bb7ec39e192
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:04 +0000

    beyond respect big past you

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import tensorflow as tf
+print(tf.__version__)
