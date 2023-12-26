704ed7e8aa4ea1393a92467ff0660f982dfc27c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:16:45 +0000

    clear winter them turnon subject

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-from collections import Counter
-print(Counter("hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
