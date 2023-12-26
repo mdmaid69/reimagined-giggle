24e98d9279c964b38219d98e9cceb047e1fb9cef
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:36:41 +0000

    sort dead Ff comment again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_volume(length, width, height):
+        return length * width * height
