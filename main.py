ae47e6b9ffdd066ace4e646074a1d6b4d1cd5f62
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:18 +0000

    too refer flat inform balance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
+  def convert_to_hex(n):
+        return hex(n)
