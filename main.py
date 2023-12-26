6e5a316f57f311fc3361c7c982ab8f2fe563025c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:57 +0000

    mess wealth blame complicated aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
