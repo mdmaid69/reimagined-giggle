a895738632901f3e0d1f4497dae62940367e8a9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:43 +0000

    stage normal prove cap ground

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_acceleration(speed, time):
-        return speed / time
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
