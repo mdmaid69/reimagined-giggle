fbc32c15ec85b1d551ec6f1f2b52f8a074099b02
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:19 +0000

    race terrible contact row lay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
