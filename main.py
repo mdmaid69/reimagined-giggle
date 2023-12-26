6240428b6322bc2e4ab42c268791dbae6bc743b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:45 +0000

    another crowded trick moment platform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
