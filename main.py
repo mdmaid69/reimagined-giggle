77dc238a622106c44ea4ea35f4231ae48f0ce630
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:02 +0000

    pipe murder dance risk would

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
