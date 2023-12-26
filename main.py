6c4b2063ce6cf43de8338fe11b0a65c9d7be0599
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:53 +0000

    flavour common match skill passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
