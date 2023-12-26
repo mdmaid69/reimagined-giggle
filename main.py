6561b946f9af21fc7fa34e505e083fb121c7adeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:30 +0000

    strike dance top ring buy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
