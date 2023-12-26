12eb66ac8a78812ccc4bd1fa77718ce30c790814
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:01:28 +0000

    heat foot uncertain or fasten

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def count_elements(lst):
-        return len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
