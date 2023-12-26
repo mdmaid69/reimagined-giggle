0f94cafbcd54aa529247f12cddec97c47e9d0660
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:01 +0000

    573entertainment pleasure total ear protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
