73f528ea4bd7c3adcbf159257f30c93becac1856
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:43 +0000

    basic ability when dig afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
