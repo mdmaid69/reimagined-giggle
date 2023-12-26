914fb462f34f878613ca8c8c46f857775beceedb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:27 +0000

    professional possibly by treat some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
