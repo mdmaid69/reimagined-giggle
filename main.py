17ca27f30db1dbdb91a65d951f4f90d50166756b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:40 +0000

    cheap pleased splash bright how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
