ddee8d10c60cdddfcd8d3fb7304e3d9852e80107
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:44 +0000

    smile temperature catch small either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
