7d41801e22faa8dee87bcfab2507e119c5045da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:32 +0000

    back wall once aim town

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
