c5156a015ef524ae47d35e6995361e89cd48b8ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:52 +0000

    build risk none mess string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def count_elements(lst):
-        return len(lst)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_as_float(array):
+        return float(array[0])
