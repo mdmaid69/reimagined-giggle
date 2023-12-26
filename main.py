1e8079c380a292a3b6c71a3abfc1f21408594e20
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:05 +0000

    share affect holiday thin content

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def square_number(x):
+        return x**2
