83dd5baada4ca07b6dd85bce6ed74ef365896ed3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:19:43 +0000

    ready regret butter spend satisfied

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-for i in range(10): print(i)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
