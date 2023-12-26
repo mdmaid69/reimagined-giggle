e5b1a5fd7c3766595d2520e08d2fa4cbbf186fa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:14 +0000

    waste march special refuse neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
