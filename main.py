2e947e9d252f91aea7daf979a42b45d3d33c4d69
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:43:25 +0000

    march talk pipe correct wire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+print([x**2 for x in range(10)])
