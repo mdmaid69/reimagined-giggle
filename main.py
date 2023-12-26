277d99b85e3b45bdc7ae530d5620e2bb5299e30b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:18 +0000

    definite Gg takeout buy lay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
