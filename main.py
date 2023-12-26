899a4b0c80d6e0a071ef4846bfda107bd2b826e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:49 +0000

    as develop few wear runaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
