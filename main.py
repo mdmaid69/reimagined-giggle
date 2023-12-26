a5712a602d6a32dbec168ece16aecf60c877f042
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:19 +0000

    pile many welcome soon phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
