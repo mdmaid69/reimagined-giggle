61323f7e3705fafc9c9002c15d6f28c861177556
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:43 +0000

    gradually familiar window gentle fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
