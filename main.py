7725e010517bcbca7970edad855976468728d222
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:18 +0000

    excuse emphasize guilty freedom kind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-def add_numbers(a, b):
-        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
