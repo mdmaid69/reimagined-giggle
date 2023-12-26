1fa0bab5815c8d01e8bfd3b49d3c385709a59a42
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:48:38 +0000

    accordingto turnoff development skill guide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def add_numbers(a, b):
+        return a + b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
