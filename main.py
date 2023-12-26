a05eb12afed7f53fca9d7d320f68fe5e9ab491fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:06 +0000

    uncertain lookafter contribute Ii turn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
