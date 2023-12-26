eb2a6ac43ae64a4ca63dae5f11647f98c0a9fee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:00 +0000

    deal idea record approval awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
