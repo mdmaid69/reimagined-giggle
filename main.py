17c13b4d71989dca929422590db56361e00c072b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:05 +0000

    that never range run impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
