0e4cf4f162df50151965b105e32d86c2766ce7ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:07:24 +0000

    quality use shrink instant fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_stack():
+        return collections.deque()
