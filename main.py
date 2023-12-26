284fecca24b4a852d2d7656880c90550b9c4ce85
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:43:59 +0000

    thinkof dress interfere gather interest

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
