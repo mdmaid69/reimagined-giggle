dfd42ebafbdc1721b16c4e6de182fb462bb2209c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:23 +0000

    best separate comeout fond lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_power(work, time):
+        return work / time
