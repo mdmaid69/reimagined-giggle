1775c60a2075816d61f9c96e7735b12ac3083da8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:56 +0000

    little callback refuse lucky far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def log_message(message):
+        logging.info(message)
