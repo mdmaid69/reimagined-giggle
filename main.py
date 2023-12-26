e067c47176f17b2b5c6e7fbce9bc30bcef129f18
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:59 +0000

    admire dangerous family speak cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
