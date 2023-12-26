ae5f87e5cd02a00b793a9c2eb37730fdb3e7f4a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:07 +0000

    except for perfect decide able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sine(x):
+        return math.sin(x)
