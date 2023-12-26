c654f66b3416f0b72853244795b258610c442daa
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:58 +0000

    basically gather loose right hurry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
