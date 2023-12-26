aac5ab3153e8448b4199636be42e9f888d9e90ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:49 +0000

    wealth breakin direct calmly change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
