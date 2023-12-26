89ef5c8650e27e893077088a2ee19c2815e1bd04
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:34 +0000

    frequent politics company passage drawing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
