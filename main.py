1b53b6acc834b5eb76965e6a5599b48abe7c9761
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:41 +0000

    convenient lie thanks instruction importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
