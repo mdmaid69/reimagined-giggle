d1803b0ac0d947b64f034c5852f584652dc63cbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:48 +0000

    act mixture smile opponent skin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
