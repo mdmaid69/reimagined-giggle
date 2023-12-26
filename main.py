8cfd7ed28cb08c1302270aa514d3c644ca5e990c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:04 +0000

    simple holdon pronounce cook challenge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
