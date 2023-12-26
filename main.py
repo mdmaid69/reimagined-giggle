ffbd195e94bf573ca3ecf28dd9a0aac4a145574e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:49 +0000

    pretty minimum gooff pass dull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def convert_to_hex(n):
-        return hex(n)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
