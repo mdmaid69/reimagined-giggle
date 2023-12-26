e96ec500b2437a190e8ed02ffd8d972ebc2a7c62
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:56 +0000

    modern lead reserve health scale

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
+import platform
+def get_os_info():
+        return platform.uname()
