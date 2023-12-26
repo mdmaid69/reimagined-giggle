4f97b8a1046da082c23cb1505f3a681ad9e25545
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:49 +0000

    travel mend late during dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
