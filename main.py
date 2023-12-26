229ffcfa4839906898a0610fa70a938a59b090fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:50 +0000

    underneath letter hungry us capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
