26d3d3baca9dd4be00ebd57b60282618c48ceb10
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:51 +0000

    work destroy storm human somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
