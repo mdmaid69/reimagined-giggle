8ed7e339b6d6b5494ea0346bed4e956eebccb492
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:07 +0000

    contain say ask plate mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
