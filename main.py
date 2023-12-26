b7649350d969059c9b0759ac6a8cbd26a16baeda
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:17 +0000

    either closed clever over workout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-for i in range(5):
-        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
