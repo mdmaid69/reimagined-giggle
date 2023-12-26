21b939769f8e0c2cf49384fbeb995eee83925533
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:06:18 +0000

    on see swallow height wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
