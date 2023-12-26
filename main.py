e58431a687d0c23c00fa3005cafca42e85d7e326
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:31:26 +0000

    wherever fear birthday tune trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def is_odd(n):
+        return n % 2 != 0
+x = 10
+y = 20
+print("Sum:", x + y)
