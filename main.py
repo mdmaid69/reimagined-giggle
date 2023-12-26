e73f79150423c0713443737e455e59a249a2ff5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:38 +0000

    defend takeover pain by dream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_volume(length, width, height):
-        return length * width * height
+import time
+print(time.time())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
