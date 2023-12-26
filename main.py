4d7373e73754c1a1cc7b12103520d9667013ef56
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:53:46 +0000

    turndown round cheap shallow object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
