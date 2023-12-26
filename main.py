cb146f253f80714e901e14cf2c7e4404a4f39ef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:49:35 +0000

    cut sting occasion hungry complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
