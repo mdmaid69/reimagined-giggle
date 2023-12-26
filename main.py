486f978d9e4e1669da6949a8d72c8362fe0ba0c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:26 +0000

    mark important please exploration nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
