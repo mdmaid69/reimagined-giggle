aecfdb949eb224d8cc528fc997446c151c354c38
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:52 +0000

    public various control size true

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
