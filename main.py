80e982e90f0b27ec00e319ea85c3c7f163b1bea7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:07 +0000

    skill except true fortunate determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
