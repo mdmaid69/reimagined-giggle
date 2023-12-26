671b1d4c519e4249fb481f1cebb904c5fe06feb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:39:03 +0000

    fault pure building ball cheque

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
