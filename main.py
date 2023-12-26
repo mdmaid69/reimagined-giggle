6fbfdb3a00968ce068408d1b548351a840e63c76
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:53 +0000

    move lead experiment aim position

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
