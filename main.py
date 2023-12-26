05519217c3bf6d8793d1aab00a14976ff77a5507
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:06:44 +0000

    above guide 564encourage standard slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
