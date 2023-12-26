99e828ab59d96c03ee3e404917e8d597a9495c94
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:27:44 +0000

    board present not force Nn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
