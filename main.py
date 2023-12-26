a41d18b31b08db58a80d179d854a3576d8b1c01d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:09 +0000

    destroy regret Ff necessary foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
