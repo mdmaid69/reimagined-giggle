1405144c19db33481a2fa3c97cae5b23a8569cb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:47 +0000

    pleasant hold rich explanation theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
