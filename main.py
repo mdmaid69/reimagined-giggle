06e1e8e8d09d4eb1421235460cc5b750a0bb885f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:50:10 +0000

    wonder push know squash fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
