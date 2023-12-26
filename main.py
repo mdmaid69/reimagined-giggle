92da19c93acb6b87c2c9d5293ea077e916eddef3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:31:50 +0000

    point influence report guess badly

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-for i in range(5):
-        print(i)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
