38a5a23b1a5a58b460c9d81f5b281eacb45e4785
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:30 +0000

    count class turnup partner beyond

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
