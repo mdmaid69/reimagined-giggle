8d12287bc75fbe2f299751377ad1696ea8dc6217
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:44:49 +0000

    even ours noone vanish closed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
