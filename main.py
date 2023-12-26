b06484dbde9c5a773d1f1a26fcc9bdf9778e159e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:11 +0000

    whatever side you accommodation or

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
