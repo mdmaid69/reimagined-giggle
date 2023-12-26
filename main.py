e9b952e9e6c2613d2c6851d9d560f14f503dec04
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:44 +0000

    contribute valuable shame belong afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
