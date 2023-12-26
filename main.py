9b1e6a1cfa82f8908747eb111978947e9ad1b32b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:03 +0000

    closed worth official dig wave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import datetime
+def get_current_date():
+        return datetime.date.today()
