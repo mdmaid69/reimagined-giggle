91ff6011a5295bec0b2c9b798472c724c436d3e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:33 +0000

    female though aim opportunity rich

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import time
+print(time.time())
