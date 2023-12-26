18effe58d73e2962b7e5f61f442873b6da8c17f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:32 +0000

    start show partner deserve exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
