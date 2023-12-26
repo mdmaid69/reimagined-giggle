32626bf832b5371b9f7c4cd3e47524969a3da79d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:08:47 +0000

    perfect period wrong luck stage

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def reverse_list(lst):
-        return lst[::-1]
+for i in range(10): print(i)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
