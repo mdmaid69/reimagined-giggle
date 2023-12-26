2cfd55b4fd6ab52981fce9cf0c2330f81e3b34fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:22 +0000

    cruelty unusual I private might

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
