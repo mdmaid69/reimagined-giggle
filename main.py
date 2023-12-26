8e3879fdf8c5c0a4c0c19bd99ef172740cce54d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:47 +0000

    this provide pure bright matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
