9e34ddd42b74ac1bf0bbacb58cfcec21ba82f37a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:53 +0000

    buy bend conversation worst mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def square_number(x):
+        return x**2
