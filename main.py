9acb72917dc0a802b82933b39bfe9373a968ffed
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:25 +0000

    convince last address finish minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
