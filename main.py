0337a6bdcdd9af064c13f386c76bff5ac32a55f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:01 +0000

    load sound nigga Rr expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
