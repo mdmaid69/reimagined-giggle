cd140f295e497a60bb7928cd302dc3ef031b9f5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:12 +0000

    whistle plant account weigh favour

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
