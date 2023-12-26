941d243558d4f4ea5ba95232d7427537f39880fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:47:21 +0000

    silent fall emphasize truth science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+print(os.getcwd())
