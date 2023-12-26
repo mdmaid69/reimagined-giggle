75733c96a4a1962645f9550b39373bb0cf6ced8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:16 +0000

    turnon measure bringup crisis moral

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
