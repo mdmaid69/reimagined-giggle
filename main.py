36f722ec6e798f1ece7986a829ccd36271972ff4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:57 +0000

    system programme single tomorrow embarrassing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
