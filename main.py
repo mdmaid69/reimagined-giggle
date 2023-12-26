545624b7338632fa8280351d3dcba03c59a48c9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:35 +0000

    worst priority tell target shut

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
