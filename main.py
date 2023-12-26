f6150bee78b207a48c293ea3c7acc2079a5637f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:36 +0000

    person runover unless all precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
