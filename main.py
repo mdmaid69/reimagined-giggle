69ce76aa3688d86681eb8dbcae77f88a4e876c2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:08 +0000

    unpleasant light danger dull difficulty

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
