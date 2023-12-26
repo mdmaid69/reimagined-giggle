bec4c9ae93d9f3d45db81ddfeb883968aeec1877
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:17 +0000

    need alive careless priority worried

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
