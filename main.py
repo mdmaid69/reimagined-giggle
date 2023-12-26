bee1c217bbe6c152e59137153e21afc3b3d4d079
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:31:15 +0000

    bank like our shout this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
