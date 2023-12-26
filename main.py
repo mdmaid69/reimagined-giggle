2818990bd2c5d021e87f4abf88075ec8076c4b3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:44 +0000

    fail punishment Ff finger Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
