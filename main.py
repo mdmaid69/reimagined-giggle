66d6291634a3ac6435781c8943e2a0b3137cb922
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:41 +0000

    hear settle thanks often run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
