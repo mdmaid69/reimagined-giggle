7d8bdd31c15d67fc29fe5a3736f793df077b0061
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:15 +0000

    disturb letter couple breakup wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
