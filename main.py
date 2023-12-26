cf5d8278ed04bdf6635ffa614f41c337cff4ec77
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:19 +0000

    shut cheap hurt pay Qq

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
