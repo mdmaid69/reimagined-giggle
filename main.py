1be142aa4b08c491706063999da4c7a31797dd82
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:13 +0000

    anywayanyhow bus related central unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
