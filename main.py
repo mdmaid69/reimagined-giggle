03585d1b38845ea3703041ee7c08ee0c226f01b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:25:32 +0000

    hurt fortunate change below organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
