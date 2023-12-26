83d947cd100b91283d7df5b623e85c9e45cad53e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:10 +0000

    stone car needle city whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
