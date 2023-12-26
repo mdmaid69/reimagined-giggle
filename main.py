550ba1eab0c50f3d8643bb3922969f5180313006
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:48 +0000

    better spread hollow attitude wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
