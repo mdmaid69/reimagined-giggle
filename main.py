2364272542c0c986003f7d85f77f80ccd332bba9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:42 +0000

    soil genuine sick defend turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
