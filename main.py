21d4805d7beb3c430dd9a009df3e7dd29aad4373
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:00 +0000

    whether interest unkind dream luck

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def sort_list(lst):
+        return sorted(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
