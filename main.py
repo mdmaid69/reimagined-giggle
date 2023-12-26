5e029f77ca797cd518009ba36ffffed6b78ae5be
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:56:56 +0000

    let square strength confused pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
