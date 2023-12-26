f4106ff1a17fdf81496ce6ece54cc4eeae77f379
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:49 +0000

    discuss buy lower ought wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
