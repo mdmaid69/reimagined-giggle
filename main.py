8a8f25130694bf87b8a37afd84cae4a9c6b54fa8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:46:22 +0000

    runaway honest holdon comefrom weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def convert_to_hex(n):
+        return hex(n)
