10f1d9616bd5358eaaa0db47743f5ecb1352866a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:24 +0000

    harm welcome your taste old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_index(array, item):
+        return array.index(item)
