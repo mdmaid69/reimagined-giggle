359d5386ca8688dd2deda4986a95cae2346cdc42
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:21:47 +0000

    Uu turnaway regularly how complaint

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
