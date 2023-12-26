56d49d019e7a69bbc0e7bdd8de099fa3bf69ce51
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:18 +0000

    type explanation rush invitation recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
