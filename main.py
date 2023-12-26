c21c4e02e296fd0bf33fa5a1cec0091627e247e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:18 +0000

    move deep bite card callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
