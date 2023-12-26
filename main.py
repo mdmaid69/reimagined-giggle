23567767feada3c63ec231bcd9f9629a3e4fbde4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:53 +0000

    lean anybody breakup mean discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
