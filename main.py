d70e5c993074e96f78321642ef3024f2ee2ffd15
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:26 +0000

    hope much afford keepfrom 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
