2aed6fdabcf2aca7db0d22cf6f2552bb4bb6990e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:45 +0000

    month government welcome promise comment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
