c3ab1abd98155ca67ab28dec2ca08a42334db71c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:48 +0000

    dare finger handle noisy lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
