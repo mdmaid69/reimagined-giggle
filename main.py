09a459473d77c6d2469eb5c9ad984ebb783626b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:56 +0000

    goaway bury young know which

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_pressure(force, area):
+        return force / area
