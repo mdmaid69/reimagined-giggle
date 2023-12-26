b166c6cf9e67cbf18d4122cd980a5a7f4c9a4e58
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:05 +0000

    whistle sweep trial serve this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
