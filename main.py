e3d7261141163fbb5fdf86f16efcb38b0dac4e2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:14 +0000

    comedown where tap hurt race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_length(array):
+        return len(array)
+def find_min(lst):
+        return min(lst)
