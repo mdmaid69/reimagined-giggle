6aad93295add450eef3ed8573abe03f035561782
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:59 +0000

    flood design slice union recover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
