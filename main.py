9e59895465f14dfe9d5525cf82aa43051151f629
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:12 +0000

    hello tone sentence ticket programme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
