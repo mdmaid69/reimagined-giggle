8eb38247af6ea164912adac75a600a8db730d26c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:08 +0000

    funny talkback strike precise storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
