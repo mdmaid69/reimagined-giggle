a83a47e2e5d9a74e379827b1a1897fc115868258
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:00 +0000

    spare window eager run may

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
