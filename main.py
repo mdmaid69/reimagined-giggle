5c3b098d71df2700e8a9cc8bbcdc4c01062886c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:55:37 +0000

    transport be relief general slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
