17335d0bf3cace88fdd00796b8c5f823fdc5e803
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:18:32 +0000

    long tip mine answer strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
