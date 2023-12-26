c5084f805a36f95f658d3d1638d68bfd24969cad
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:50 +0000

    tip brain black safe sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
