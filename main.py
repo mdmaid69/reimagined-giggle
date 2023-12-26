e5b23c2cd602525e485b1483bb3a61b0f3411623
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:36 +0000

    answer trust giveout order temperature

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
-        return x**3
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
