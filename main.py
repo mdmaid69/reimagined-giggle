13ba765405a4ada9aa998144280bc21f6cad17cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:12:30 +0000

    comefrom present this language house

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def reverse_array(array):
+        array.reverse()
+print(sum(range(10)))
