48159536bc6f26469ab9ff4c8d2b3e677f037734
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:24 +0000

    boring rule stretch file fine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_area_rectangle(l, w):
+        return l * w
