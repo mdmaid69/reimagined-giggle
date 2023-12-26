56564062ef95eb86b3e4d061affdaec62f3dbee3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:43 +0000

    container estimate arm belong dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+print(math.pi)
