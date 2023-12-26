369654c5cfc9525de236e89e870252b0c52644c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:57 +0000

    stop sweet forget Ee maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
