5fef57ed56fce8b584f38fe486655d2386f331c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:52:00 +0000

    offend thought belief tip better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_tangent(x):
+        return math.tan(x)
