0b078fb6e013aef5617a410648e74fcd65ab5a18
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:04 +0000

    manner drawing plate breakin entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
