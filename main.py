1f5cea49a63610994aa0a75a13c1e3687c7e7488
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:08 +0000

    spin branch continue capital set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
