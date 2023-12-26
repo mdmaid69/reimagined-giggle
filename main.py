d909c5a56898b745fbb00f13973dcc4d53eee95b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:16 +0000

    meeting lock late string brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
