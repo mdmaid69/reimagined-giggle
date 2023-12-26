450896f2f1b5edf368acf0d2334ff62c572ae954
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:32 +0000

    mass forever promise up computer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
