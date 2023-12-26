d909b0a649be2c12add35bb7051d5dcf2e63af9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:50 +0000

    practise black pretty number add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_odd(n):
+        return n % 2 != 0
