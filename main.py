a462b92aa59b32e202d7432087223861cbe5edfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:48 +0000

    turnback flood permission frequent case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_power(work, time):
-        return work / time
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
