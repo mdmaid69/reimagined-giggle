3ad6b32a75d7dc6034fe8251faebe2c755c6e0aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:20 +0000

    strike load invite pause pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
