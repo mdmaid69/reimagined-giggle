2b1beca8a0cb8195b771b55e35e3868e5d516b9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:52 +0000

    memory interview progress downstairs search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
