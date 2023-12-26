ce659170d59cbb9c1b9dfa9146d078cec4738312
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:55:23 +0000

    genuine degree far impressed wise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_current_time():
+        return time.time()
