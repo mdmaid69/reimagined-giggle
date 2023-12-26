500c16c2d5547ea962658ff876d3ac8b1dfa8c47
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:54 +0000

    freeze wet sweep acceptable famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_list(array):
+        return list(array)
