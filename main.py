595415366fd791f6827b5d8441b783a9f5914338
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:53:35 +0000

    tip peaceful tired about line

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
