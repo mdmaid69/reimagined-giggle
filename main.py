2974639fb7dfc49e581ad67969c5d3586c5adbbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:37 +0000

    honest completely free earth steal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_min(lst):
-        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
