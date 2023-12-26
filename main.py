3c33986e666af59e81f6bfd8b5c807ddb89c706d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:17 +0000

    quality complaint quantity train accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
