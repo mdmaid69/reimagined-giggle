a7aefa7ba63f2e270a43ff5ddeb27318f4bfeffc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:07 +0000

    chin cut sign will section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
