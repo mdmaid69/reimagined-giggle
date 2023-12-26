bca34abe8537fa3cdebcae50e2ff69cd8541227c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:05 +0000

    run walk air callup aim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
