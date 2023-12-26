66a34d6c524bc89d9e28da40ebce2b5b7da158d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:33 +0000

    range describe poison floor expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
