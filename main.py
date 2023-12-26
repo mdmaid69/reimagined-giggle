baccaccca32dfe297fd93e833ddd870bf15a605d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:06 +0000

    patience hard jump angry size

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
