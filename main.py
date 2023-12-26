cf4d9ac7caa730ea7d88cee5af9697ec4c50bf92
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:05:43 +0000

    kind favourite hair bringup memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
