8e8af0c35f98d4c4d6581692954e5492a347bf34
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:25 +0000

    instant consult demonstrate like car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
