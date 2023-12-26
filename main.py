052b209015b7280da54b53bf549c91e524a8845c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:07:22 +0000

    swing right foreign immediate workout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
