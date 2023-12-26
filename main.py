eb3ceabbc3d53d12e88a005caad4d2bcb4715644
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:39 +0000

    container rubbish along sell look

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
