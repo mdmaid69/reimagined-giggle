9d1783edb475c7b5f35e94ccfde7e70fd0dca9b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:48 +0000

    become smell nervous school occasional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
