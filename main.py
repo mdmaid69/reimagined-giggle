ad3a9a2ffcc84feaadfcfa6252b046f80743342b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:21 +0000

    inquire feeling exit while silent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-print([x**2 for x in range(10)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_area_circle(r):
+        return 3.14 * r**2
