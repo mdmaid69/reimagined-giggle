2cfdaccb324466636aee3a022f516af4e827da44
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:30:23 +0000

    disturb during lose feel workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
