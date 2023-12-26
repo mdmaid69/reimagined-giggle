da66c85aeffd286b7f2986af63cc39abca7c93fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:02:16 +0000

    operation much they paper practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
