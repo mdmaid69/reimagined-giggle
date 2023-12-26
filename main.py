8c96ededf2ceeec94def7c773456118376ec0c1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:30 +0000

    undo breath sail area doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
