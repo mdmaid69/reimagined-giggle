85393f10262334aa4f72227285e7a4eeeba9ebed
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:05 +0000

    comeup wish foot mess deserve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
