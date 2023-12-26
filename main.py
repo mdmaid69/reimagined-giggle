843a825e8f907ade9bfe6a8cb971122a040f7855
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:33 +0000

    later earth last by dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
