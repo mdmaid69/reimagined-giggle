a3f1bd207bcfb891262d971606dd7721f489d6e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:57 +0000

    check patience beyond contain miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
