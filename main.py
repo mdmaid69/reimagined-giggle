fb9883482523b13af59caa4fbd28e41a061c570f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:42:52 +0000

    advanced exhibition worse away turnout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
