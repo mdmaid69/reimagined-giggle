0edf2321bfb0312a7023fdc000a4efee32d76bd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:23:00 +0000

    approve design we road secret

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
