6b7fb127409981332c9115c4cb20b3aff20ce344
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:41 +0000

    advise need thorough necessary thanks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
