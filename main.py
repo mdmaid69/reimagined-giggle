3fe667e0447de7aa2801967079a5963aad75dd8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:33 +0000

    transport unable estimate bye quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
