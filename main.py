6b43fcb78386e11c0e2ab15117ea84b759b2561e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:58 +0000

    include connect nigga window city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
