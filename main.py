5928c43ec44b2aa2633594d638525c77555bf35a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:17:18 +0000

    careful breathe such area walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
