14fdaedffc91d7bec08290a79fbac476f569a506
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:10:59 +0000

    temperature handle upwards hit business

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
