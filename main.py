73c98885d5cfee27d1c762adf826174972c116f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:27 +0000

    sweep copy use blame 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
