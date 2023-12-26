227193b1613045b814d327489a3f37918c70e8eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:14 +0000

    base feature like as majority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
