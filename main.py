9f3294a2122121b6e1cc7c349449eca071947ecf
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:37 +0000

    post bad key cause tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
