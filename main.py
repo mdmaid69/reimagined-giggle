9afe087a5abd785b12313a9c2dfeb9f22cd9d51d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:31 +0000

    able protect waste left ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
