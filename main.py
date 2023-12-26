886fb5dd6286cae0cc27c609e72f747482d6cca6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:37 +0000

    pronunciation worse proper display smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
