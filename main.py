fa0339c6e4663eb3733066e88b29511051040bc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:14 +0000

    apparently shape contact Rr safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
