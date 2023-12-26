93d1b4e8538acd58f17e9e659f80357d7daee51b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:39 +0000

    float never glass division spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
