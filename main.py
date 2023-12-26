2cd47db053d39b48cd7c261484d5bec62bcd69b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:17 +0000

    yesterday characteristic dress practise appearance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def find_max(lst):
+        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
