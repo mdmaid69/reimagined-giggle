ba09452fded6fcc660d1eed252573387effbfb2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:48:20 +0000

    available show experience angry contact

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def square_number(x):
+        return x**2
