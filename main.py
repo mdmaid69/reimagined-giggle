31f580317c671b5ce28ab1c1b7145ed5fcc88b87
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:08 +0000

    zero without opinion world getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
