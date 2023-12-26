4c0e67c9c3abbdfe4f8190bbc46cd6bdc6cba52c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:46 +0000

    through treat sort false always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
