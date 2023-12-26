c61884697d6550cbe6ce414afd8fa24d6bca7055
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:42:24 +0000

    around slice key system business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
