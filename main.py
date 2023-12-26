826641cd18cda72f6e155d0c52042e6163c30dc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:15 +0000

    ear matter giveup listen level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
