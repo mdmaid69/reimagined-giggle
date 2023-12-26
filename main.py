4611e6e6faaf46506b3f0c9bdf6a92246cb95b02
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:46 +0000

    healthy stand stiff mixture uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
