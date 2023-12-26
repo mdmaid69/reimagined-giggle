dfdb16cdc04375ece20a3eeb1a230a19f1579edc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:52 +0000

    place case full present perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
