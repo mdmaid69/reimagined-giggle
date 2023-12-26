6ebcfca160d0b66ec42836a593de7f618b305ede
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:44 +0000

    section flat everywhere lift glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_float(array):
+        return float(array[0])
