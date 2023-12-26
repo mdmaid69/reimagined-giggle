a34c80c70a70410870b7044c1c6711698a558d13
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:09 +0000

    mean breakfast scrape anything separate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
