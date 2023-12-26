9a792b579a0b7e44444d537eea7c57e8b5601deb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:05:34 +0000

    sharp private back treat strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+for i in range(5):
+        print(i)
