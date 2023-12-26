8a851c75f0405f6271b5f288218e40d93fa2d82b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:17 +0000

    less condition definite Yy ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
