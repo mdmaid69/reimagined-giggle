122f01486e226b0d638aa0a78a86e694b1cdf9e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:15:59 +0000

    awake when post load take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
