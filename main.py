696fe6dd77c0ea4d0da3e5c935c984cdd0736cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:43 +0000

    pattern story hers interview train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
