4f473c61632d9553147e1b4d339995470ad9d5f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:42 +0000

    workon occasional mend once father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
