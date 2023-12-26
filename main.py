ef09f1ebba3ed1520580064b65288a20d6542bec
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:08 +0000

    deep punishment accident lay lookup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def square_number(x):
+        return x**2
+  def convert_to_hex(n):
+        return hex(n)
