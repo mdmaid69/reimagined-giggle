a6901bfe75e6a1f13ac33e9c03eb93d8148c2ba7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:25 +0000

    situation finger separate hardly cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
