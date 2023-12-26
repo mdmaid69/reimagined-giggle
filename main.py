414b4e5b234de69f567938e6ec3e122d21ee3f0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:32 +0000

    lookback(on) plain fond backwards include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
