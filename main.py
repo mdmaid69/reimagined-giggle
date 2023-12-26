f09f58bcd4e12abc7aff063949a6a70fda4fc4eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:59 +0000

    possession thing contents limit free

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def is_palindrome(s):
-        return s == s[::-1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
