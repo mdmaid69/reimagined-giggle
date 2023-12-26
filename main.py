e70a6a6ee473fcce47332c71e8423e6435bddb37
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:45 +0000

    needle splash threaten knock activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_binary(n):
+        return bin(n)
