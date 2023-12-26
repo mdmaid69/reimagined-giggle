c125dd46de4e39892e4edee23993cf4314e50a31
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:21 +0000

    ready complete single unlikely pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
