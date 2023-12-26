142bfcf08bb5e470e7cb479b418f6f9e03466e95
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:55:48 +0000

    enthusiastic know alone result best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
