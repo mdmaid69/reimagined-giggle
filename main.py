d12d1fdf57b5449c667c12d4c6c67fcc43b73f58
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:23 +0000

    condition remember holdon dig introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
