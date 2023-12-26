6ad94d7d377ce4b92cdc2326921af4fe16321558
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:25 +0000

    consider charge unpleasant extraordinary criticism

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
