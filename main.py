522132da8141a865ea0196c5c596dc272e995c07
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:25 +0000

    gap pause continue model count

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
