750c963a991e5cbd8b5bf5a6ee33520432bcd6d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:44:06 +0000

    foreign shallow next hole fresh

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
