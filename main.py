fea1b7a903c36d9e75e3d00812739ad3870ceef9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:41 +0000

    talkoutof clue pure burst 566end

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
