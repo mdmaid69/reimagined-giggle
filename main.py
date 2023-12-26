0f30a772d2dac6a5de57151f14427539d186b5c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:56 +0000

    turnout path runout giveout along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-print(math.pi)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
