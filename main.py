9eea85c98589e266467076da39c82708c4785523
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:42 +0000

    sound deaf in damage coat

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
