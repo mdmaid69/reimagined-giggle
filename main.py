09a7c4014136de0f60e7981898ae5429d79e4bf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:16 +0000

    pass none telephone convenient patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
