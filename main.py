f7b7cbeff44a7d05a3291e72345cd37b7a687642
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:14 +0000

    target find month generous mass

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
