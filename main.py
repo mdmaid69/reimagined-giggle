f66bf23112b21d34cd1018aeca58502f939443db
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:07 +0000

    final breathe operate rare Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
