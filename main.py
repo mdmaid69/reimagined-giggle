85562a77bbbdaa2e54bb1c7d82793657a8289c90
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:23 +0000

    sale regret afraid modern black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
