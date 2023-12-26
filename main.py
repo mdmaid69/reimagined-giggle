5505df0aee51b139b87119243e18cb86539afcc9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:45 +0000

    complete wherever unable turnover outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
