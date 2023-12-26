f8eb6fedd9e7291275fd108d3b02ad85f716f3d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:21 +0000

    between now appearance turn even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
