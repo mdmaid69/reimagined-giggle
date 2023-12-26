27ec7aa3ad6f5dd509096ac2884db66de1865c27
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:15 +0000

    think downstairs aim affect act

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
