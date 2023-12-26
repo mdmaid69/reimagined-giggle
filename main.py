ab1e2e70291ae0d03de2964c0fa2b4cb1390b20d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:08:48 +0000

    last principle 570enjoyable class lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
