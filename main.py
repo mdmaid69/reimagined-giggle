867037c7c9f35d88e9bc082d10ff22c8f19f4524
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:42 +0000

    petrol afraid loud choice foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
