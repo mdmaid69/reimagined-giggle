5d8eb658f3564820eb19c2036317063670ae6a80
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:27 +0000

    buy finger open share either

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
