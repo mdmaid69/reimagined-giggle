6aa6009928aeb87611f236674dc2595c9342d214
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:21 +0000

    Gg pencil church reject giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_as_repr(array):
-        return repr(array)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
