4fabe3b2a339ecbb5a862a421554bc4315ad4ef5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:04 +0000

    straight whisper occasional careful blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
