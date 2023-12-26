ba795ad562b636463ecea91f2c94e1f0e15406d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:11 +0000

    they season action study my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_current_directory():
+        return os.getcwd()
