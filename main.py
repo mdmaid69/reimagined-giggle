5abde2f39d8eb3514d3fb52322ede0f72ad24825
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:14 +0000

    capital appear manner injury blame

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
