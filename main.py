36dd5f97cc5abae8449846c2334abb586e95c003
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:55 +0000

    suit principle value physical mention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_acceleration(speed, time):
+        return speed / time
