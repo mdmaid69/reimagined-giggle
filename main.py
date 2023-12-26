6e56a4f571f4fc45357172f1f6666abbf18972d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:50 +0000

    niggaacademy centimetre absolutely old minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
