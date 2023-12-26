1679cc6f7637941c5b688214e44c10971e00d703
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:57 +0000

    friendly feel fortunately base convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
