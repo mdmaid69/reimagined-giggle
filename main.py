74f1e2a5cd0a785c4e40f9a0f72d0f7e6028affb
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:48 +0000

    reply recognize open half large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
