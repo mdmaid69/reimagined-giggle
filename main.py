9c057cf725ff6987c1502665377081c0f01777d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:24 +0000

    tread the the positive behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
