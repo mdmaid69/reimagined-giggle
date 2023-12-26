de55306e1bd5c348cb883eb2d9e6f7f219322b60
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:19 +0000

    false appearance final attach hers

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
