00ccb12980623402f629d3914768f68a03fda8d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:06 +0000

    show dare visit boring Qq

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
