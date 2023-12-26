76360f3b749b0faa209ba2ad6770662c6c256d5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:45 +0000

    command about ground value exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
