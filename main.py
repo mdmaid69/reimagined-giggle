a786fb87955bb3e604633729fb1179cbe3e7faf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:25 +0000

    similar impossible tidy poor him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
