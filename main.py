0176a5b2d38c0787045cf1a1e880d9d81dfbb447
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:59:24 +0000

    569enjoy level building brown owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
