72cb7d60b04ce18347bc1e5b8f8f9cb424026249
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:24 +0000

    single belief lunch crime ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
