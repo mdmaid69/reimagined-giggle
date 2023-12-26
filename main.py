3651e19642cf364a178f9cdc5644ea6bdef7acfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:30 +0000

    whistle truth hang him gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_force(mass, acceleration):
+        return mass * acceleration
