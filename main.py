0609ecbed7385a58e9ab45cfa1ca75ef3da88269
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:20 +0000

    port talkback thankyou mine favourite

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def add_numbers(a, b):
-        return a + b
-def reverse_string(s):
-        return s[::-1]
+def convert_to_binary(n):
+        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
