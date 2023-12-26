af3013a4d87b2a51d7faa38d10dd06a30476f11f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:22 +0000

    mark look further once club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
