62dabc61307e44ac4b82a724833b5b8f53144e37
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:35 +0000

    extraordinary somewhere hardly smart sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
