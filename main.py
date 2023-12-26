0e22651ef26dd6a6dd24c474e238c1b34134cbb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:52:28 +0000

    same software shrink improve clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
