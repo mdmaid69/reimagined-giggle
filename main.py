1cc5e16d3cda208df598ceef41324f64ddc980d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:25 +0000

    whether over surface cut dark

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+def calculate_speed(distance, time):
+        return distance / time
