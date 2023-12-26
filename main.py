f95a7ec39781ab66fcb055486be9b03814acab79
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:20:29 +0000

    law charge we goout go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
