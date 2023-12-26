8cb699545fa58de1638f0b72bfc5330be4cb9187
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:08 +0000

    talkoutof bad life switch heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
