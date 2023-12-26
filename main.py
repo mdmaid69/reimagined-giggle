c6cba6bb44e98fc79c4179bc8dc5529d062e5be6
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:18 +0000

    children central fire 179bite imagination

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def square_number(x):
-        return x**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
