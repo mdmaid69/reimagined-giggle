8e7cad630de05233af05260adc2866e12d622ff5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:23 +0000

    damage corner plan relief bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
