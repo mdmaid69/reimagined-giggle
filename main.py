d80d8d5e9c382dd9b2028bfca1215c5fbe54bcee
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:12 +0000

    deserve change future roll crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def sort_list(lst):
+        return sorted(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
