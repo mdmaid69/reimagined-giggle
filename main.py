6b9049b4e71154f9caa0757e6ba40dc6e890c066
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:07 +0000

    platform appearance later trip workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
