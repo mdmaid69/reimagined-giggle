8ec00213e9f127c25d272d40b9324597b3837e04
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:42 +0000

    weekend report butter distant passage

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
