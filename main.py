65fefa0c4badd4b10e02f41dad7318d7e0424045
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:28 +0000

    disturbing claim evidence anywayanyhow possession

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
