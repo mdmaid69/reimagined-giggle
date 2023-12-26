cb8ec5fc23cbd9705dabac171b7e7473297608bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:35 +0000

    memory marry back beat top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
