5ac08847d6b175b2227535ef3c55f7eb6a2b1e4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:05 +0000

    thumb break pleasure generally majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
