93bcd686c8643a51af89547656e5c3dd4e17ff3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:57 +0000

    rate somebody convenient related refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
