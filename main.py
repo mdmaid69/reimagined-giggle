454d94139f2ec44a1f6f02a78cb1dc880cbcf526
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:15 +0000

    recommend suggest comment shop pot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
