df00243c1fa7a85f9bec0cf41377546cc1001f42
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:11 +0000

    clean possible vanish also complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def set_array_item(array, i, item):
+        array[i] = item
+  def subtract_numbers(x, y):
+        return x - y
