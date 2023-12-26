1f8ce1098b56025d795a80ed97c250623c00ff8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:49 +0000

    law suddenly past joke scream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
