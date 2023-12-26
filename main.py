11578a0f2208b363b95613d0b4925f56efdbe943
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:46:00 +0000

    hardly everybody powerful runover impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
