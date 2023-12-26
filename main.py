f776fa37bea6c56c7861e5dfe1eaa64f1a7786eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:19:40 +0000

    profit suffer flight alive unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
