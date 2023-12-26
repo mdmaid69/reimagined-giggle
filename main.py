496db6f45ece90b2b31928e7a95cafd16cf50cd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:31 +0000

    act frightened offend finally why

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
