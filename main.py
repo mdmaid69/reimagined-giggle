1881684b7f7d0109521617a3131fdccee88b1052
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:20 +0000

    air not sure general exactly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
