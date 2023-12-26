286bff56ef60947e981d7daa77be9f9540e7a23e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:20 +0000

    everyone smart friend generous load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
