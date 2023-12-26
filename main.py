18d95c1d6392952222051249c296fe4881589053
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:44:40 +0000

    perhaps plan extremely hand field

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
