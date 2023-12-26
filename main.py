6c42c9ad1a4a154fcd4cca2fbc68a55739fc4e48
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:31 +0000

    meeting land spare wish storm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def is_even(n):
-        return n % 2 == 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
