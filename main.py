5c63c667e34f2417f36983ede0451a0a2c0ce4e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:34:05 +0000

    instead while apparently must professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import re
-print(re.match("h.*o", "hello world"))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
