1ec41f491ebaea8241f1d1a330acda7ca41e555f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:37 +0000

    bet performance lift tonight clear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_base_name(path):
+        return os.path.basename(path)
