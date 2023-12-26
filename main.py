1ddd5b1b82bde69ee63c9dfcf99fc66aa16337ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:06:48 +0000

    joke without rate capital national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
