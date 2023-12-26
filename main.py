5ad98b59bfde1f51100caa83247f5da7266857b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:14:22 +0000

    goaway in temper solve hot

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def remove_duplicates(lst):
+        return list(set(lst))
+def convert_to_hex(n):
+        return hex(n)
