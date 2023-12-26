e5b4e5bb8d85816d7dd57277d61dee0d2776f70f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:54 +0000

    start Cc there careless success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
