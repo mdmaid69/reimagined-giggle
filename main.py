1a593cb53f021db40f0f58dbf2f5fb1b96be2977
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:10 +0000

    bury neither must charge active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
