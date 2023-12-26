f8c32331aaf2c57f16e8ee846f1497660c56465e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:34 +0000

    dress pleasure thinkof immediate breath

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def count_characters(sentence):
+        return len(sentence)
