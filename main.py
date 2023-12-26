55d519b1a61302db3b1950e86b4cc2c51d331cab
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:19 +0000

    major where tip bury contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
