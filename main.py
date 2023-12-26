21c49934a90833efc8d1cc69d0eae20c644d5eec
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:47 +0000

    track organize than around activity

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
