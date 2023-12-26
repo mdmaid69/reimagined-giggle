721ff7c228ae23eefe4d5010ba446bffcac1a313
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:07 +0000

    nature connect final nervous order

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
