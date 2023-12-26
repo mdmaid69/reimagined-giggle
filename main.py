96cd114794ac4876f7e8e68063889a3c3a0be1f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:52 +0000

    comeout subject total separate imagine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
