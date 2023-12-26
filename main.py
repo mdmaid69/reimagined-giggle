dd9675655dc162d964cfbdc32837d73e463c3a8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:25:28 +0000

    attention nothing rest middle cry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
