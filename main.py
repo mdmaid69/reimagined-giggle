c7df437f82868d5a5544ea9b740e65e42222e70d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:32 +0000

    duty question emergency attach cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+print(datetime.datetime.now())
