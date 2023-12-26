48288cd0661c2f31a62fb551dd37679fe307045b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:01 +0000

    community actually knock opportunity space

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_area(radius):
+        return 3.14 * radius * radius
