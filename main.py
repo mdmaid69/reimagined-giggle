c846d9d97d0bcaa5cb8381c39af02407c4bfd02f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:33 +0000

    gas take between beat male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
