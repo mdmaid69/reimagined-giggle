768ccf088288103e65b66170674bd6f6c2a04674
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:47 +0000

    lunch payment continue nice responsible

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
