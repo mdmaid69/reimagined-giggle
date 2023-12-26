3feba857b970ae6504edc4281ba1d0daa3991f23
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:04 +0000

    thanks nearly warm keen proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
