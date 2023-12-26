d7dd9d23f9ef6294d6065657e50720da4a0c3db8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:27 +0000

    frequent stick wire tongue size

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
