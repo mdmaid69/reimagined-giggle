c6a928615cf95bfa0fec3c74a020636a4b112c19
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:34:30 +0000

    comment letter earn let confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def append_to_array(array, item):
+        array.append(item)
