da81edf557dd8723937d245149a57004bf3ba2da
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:08 +0000

    twist takeout opinion noone at

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
