4a36bbd103d206285c5ddee14d09a602c7eb30b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:00 +0000

    approve hour history hang corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def convert_to_binary(n):
+        return bin(n)
