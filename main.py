8b6338f4a0538f5787632f107b746bdb89d1a1bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:22 +0000

    formal butter speak risk reasonable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-from collections import Counter
-print(Counter("hello world"))
+import sys
+def exit_program():
+        sys.exit()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
