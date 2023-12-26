0db5f45547506193bd8806c82fb4eca4df8a49af
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:46 +0000

    basic sort maintain goout rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import datetime
+def get_today_date():
+        return datetime.date.today()
