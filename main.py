605b59b46bd8d00cd7d02f9c1033587cde3f4e14
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:31 +0000

    platform cut anywhere month mistake

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
