005151c73a1c054047842c739834644a1b62d18f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:35 +0000

    praise pick matter interesting public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
