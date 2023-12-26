76fe28e72f48183ac7e4ad5ac69cc03015c42dfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:54 +0000

    protest underneath hers attempt store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
