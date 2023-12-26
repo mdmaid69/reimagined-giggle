d911bf80780e23f165a0d766748284af510de1c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:21 +0000

    tongue model age no arrival

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
