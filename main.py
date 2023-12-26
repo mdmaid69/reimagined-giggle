77fe2d18d2663c25d585a1d273f680a334ff88c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:04 +0000

    contact may bright talkback question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
