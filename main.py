5b9a5673b502c625253cde5bef1be82a2c2fb1ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:47 +0000

    wish close trade organization hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
