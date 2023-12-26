0627e6c1f4956fd3bee94820d38db9612b267e9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:39 +0000

    across exaggerate transparent skin rest

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+print("Hello, world!")
