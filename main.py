591d5d4752123b78e315c57bc61e9f1d472d07dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:45 +0000

    accident note somewhere away week

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_density(mass, volume):
-        return mass / volume
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+print(sum(range(10)))
