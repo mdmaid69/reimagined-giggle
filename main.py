4f7a7b7a4f63b02da468ce77d1ac2b767603f65a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:39 +0000

    shout black list paper goback(to)

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
