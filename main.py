a022bd7f55ebbd35bcae7fc664bead1e173d1a4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:34 +0000

    speed land music splash move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def convert_array_to_bytes(array):
+def get_string_from_array(array):
         return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
