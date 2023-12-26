a7a6c301bc1d289a1ed3d874ef04b0c9400dece6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:25 +0000

    suggestion right normal service who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
