3da5d071ed914ce23eb5117f24bb2a062f4f033f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:19 +0000

    term close keepoff protest within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
