e3caa3d610cd3981eec7ff63a6d38c2e43a8d6e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:51 +0000

    pleasant part spoil belief gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
