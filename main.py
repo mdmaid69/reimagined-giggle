aa5a9a2d3758fa0fb36c186c1cfe5d9923c5b14e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:23 +0000

    sing regard uniform money jump

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
