34d8c90e59cf6313dd264abb9b13054169fdc26a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:01 +0000

    spring here recognize walk discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
