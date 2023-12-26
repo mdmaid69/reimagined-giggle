ceab54e6c8183ba2c3502d24f48eb9ae3481f3c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:32 +0000

    typical modern soft stupid Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
