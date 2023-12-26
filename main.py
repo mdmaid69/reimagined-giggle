ebdc312f05effd24a9f30121dc5fbc14ac887462
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:39:27 +0000

    smooth doctor official big promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
