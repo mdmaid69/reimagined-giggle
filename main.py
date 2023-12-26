93f07a162c2e743e7e84235c90f0340bd57ea02b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:57 +0000

    shallow introduce doctor thick hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+print(sys.version)
+def cube_number(x):
+        return x**3
