42571fc18f709e441a11900e914e81b42a8cd569
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:48 +0000

    wise keepoff certainly environment bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_max(lst):
+        return max(lst)
