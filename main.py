6f69a00b7a5e9f1816e1cd605f85321a2dba7915
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:36 +0000

    further say breakin give minority

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
