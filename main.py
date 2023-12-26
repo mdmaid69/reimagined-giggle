08040836851c188e022b97b6801d25c807f336db
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:15 +0000

    repeat challenge I hear nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
