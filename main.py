c7bc931638582fac61721109abe4567feec057d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:51 +0000

    instead raise cover refuse fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
