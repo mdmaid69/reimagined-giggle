375ea10e487af9cc102a91ac7ae1cd5cd309cb70
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:07:12 +0000

    ability corner earn love brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
