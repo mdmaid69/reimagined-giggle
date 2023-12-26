7277f265d7b7b3773ee06cbf9651409d3b3683ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:46 +0000

    ours handle know label long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_min(lst):
+        return min(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
