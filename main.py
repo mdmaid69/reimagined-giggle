eb42822f383f15f1fe8c16b8ff3e6547daa2bcc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:09:29 +0000

    beat floor plastic protest dinner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_counter():
+        return collections.Counter()
