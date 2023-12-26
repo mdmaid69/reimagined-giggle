d233cc1dc94c6cdbf8339c4af2d1f441fe176712
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:05 +0000

    letter this design bottom feeling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
