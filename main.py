fbadc55b0be6052c943d3ac88c7b26b64745f318
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:42:58 +0000

    exit wet confident should replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
