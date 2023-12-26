bbfe64fd3b1703d69564708802c2510c9a07bc01
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:01 +0000

    practical suit increase steady popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
