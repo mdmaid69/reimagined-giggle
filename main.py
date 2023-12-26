70bdb30aa3e4128546d397dd82e5fe16646e5299
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:30 +0000

    along exam mean speak mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
