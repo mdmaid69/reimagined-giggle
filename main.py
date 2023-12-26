cde86aa68dbc429d167051d1654a1ba5dffd3363
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:05 +0000

    short combination transport before solve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
