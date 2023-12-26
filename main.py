bd1ad167a0130d3303c6ef1f2084ac175cde481f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:33:09 +0000

    shop result sting familiar rise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_current_date():
+        return datetime.date.today()
