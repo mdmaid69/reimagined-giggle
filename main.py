f0befcc097f6b2f2b4a740c1559acd2dd0a14f99
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:25 +0000

    description increase may lift cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
