d09020f90610f7f7a5fd7d9a48a0eb01b462d6c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:06 +0000

    nerve convinced before consider wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
