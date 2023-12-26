4f81cc551266e7febdacf6ea462486989ebf0bc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:33 +0000

    me problem minimum break description

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
