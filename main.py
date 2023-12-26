78231d80581f8bcbbcd64a615845983290dcd5d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:34 +0000

    chance interruption believe constantly eager

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_even(n):
+        return n % 2 == 0
+def calculate_power(work, time):
+        return work / time
