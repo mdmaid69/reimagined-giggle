4bf67f6528c0b5bbec29aa8adc823444f6302646
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:16:22 +0000

    move expect involve pipe error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_item(array, i):
+        return array[i]
