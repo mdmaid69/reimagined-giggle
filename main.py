6b8cf761ba0d8e5af22e7dee8797e16f7020fb81
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:35 +0000

    likely general explain month call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
