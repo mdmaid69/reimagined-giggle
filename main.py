e6b240bcdfd50598113f265fb790b943993f1acd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:22:58 +0000

    university scheme prepare want quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def convert_array_to_list(array):
+        return array.tolist()
