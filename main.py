2b2b29f02a8e1624e168910554bdb0e7cd7c72be
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:37 +0000

    may climb damp understand correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
