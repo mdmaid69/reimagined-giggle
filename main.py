be1cf5810a6314250dee430e3673c4737a9a49af
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:03 +0000

    pull possible track deliver house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def insert_into_array(array, i, item):
+        array.insert(i, item)
