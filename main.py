25b7a157ffca66a6e3d0c02479e144b36a8ed7a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:29:10 +0000

    goaway possibly partner best callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def convert_to_binary(n):
+        return bin(n)
