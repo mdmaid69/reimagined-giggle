640ba557c77ba1861e7753f4409dc25fb204b06b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:05 +0000

    reliable party lookaround bad of

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+for i in range(10): print(i)
