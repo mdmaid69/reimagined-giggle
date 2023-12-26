1f56679589a8d750e1ba1bfac748a9d2ec427384
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:37:04 +0000

    to trouble plan row before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
