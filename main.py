14606f6f485641135d5c8ef5b4552539ff2757d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:48 +0000

    move noisy callat rough deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
