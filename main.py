193a4bc7f8f523498b6ad93c5d98ecd5fd268e7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:25 +0000

    unable account argument nigga awake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
