acb20e3889dd839626f0ea558458548676f91f45
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:22 +0000

    under favourite please rather heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def add_numbers(x, y):
+        return x + y
