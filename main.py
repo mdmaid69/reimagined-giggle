cbad864d3383ed7d0014aebb46af6ba0b05141eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:08:10 +0000

    turnback combine will minority pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
