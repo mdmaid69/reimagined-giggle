ff314e9c16398bdb46c49060e43d2ec366f34380
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:39 +0000

    length over comment bread sorry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def square_number(x):
+        return x**2
