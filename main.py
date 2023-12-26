c9fc41483f8cf83b7c85e6c99cccf34c67dec2b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:02 +0000

    tired exercise open aim rate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
