e00c4bae7a2726aafbfc0a56935e7a8066fb3fe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:40 +0000

    polite doubt rain pinch thinkover

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print("Hello, world!")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
