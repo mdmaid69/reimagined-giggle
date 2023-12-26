cadc7688e8042ac11a6e6617add60761bafb43ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:36:57 +0000

    smooth affect satisfied attract clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
