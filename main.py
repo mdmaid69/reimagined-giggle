5472120bb0226f728974843864f056b3ebdcca67
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:06:45 +0000

    unlikely empty deliberate damage book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
