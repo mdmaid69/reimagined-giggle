3b1d7a0c4654b91c023d562549e41c7c8859c9bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:31 +0000

    principle foreign natural discover boil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
