60cc3d12444f053ad177748e313e29ab631cd1df
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:20 +0000

    activity female rise shape ring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
