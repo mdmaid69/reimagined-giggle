0fba482855c267b10bdaef547360900b9c33201a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:37 +0000

    regularly exciting experience perfect sort

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
