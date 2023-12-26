1c58c2341fe99a45919ac9638216b97012de66e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:22 +0000

    they flood miss custom paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
