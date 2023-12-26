6c6ab13e6019041fba28d9f74f8d35357b8fe35b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:24 +0000

    possibly goaway this cinema shock

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
