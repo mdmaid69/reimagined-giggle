d130d2a68450e36d691667a22e5b5e2a19393d3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:25:08 +0000

    minimum form to complete may

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+for i in range(10): print(i)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
