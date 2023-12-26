3ba6c01e490bbc8a44abbe9454272a2570ac565c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:37 +0000

    score medium healthy she shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
