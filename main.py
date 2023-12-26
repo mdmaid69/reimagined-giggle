af4b15b4370b1159e1b6d172428df602cdbad9ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:41 +0000

    either makeup evening down smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
