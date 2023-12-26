abadf62e22e0c1ea6f199262b5f05c6077e95497
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:28 +0000

    well comefrom goup let answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
