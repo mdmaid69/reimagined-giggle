32f16480a9568def716050746a181f8d88be2ebf
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:19 +0000

    cheat giveout needle along offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
