a67261d19eec35308d463f96cc2b0489151bb41b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:48 +0000

    callback somewhere about gradually nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
