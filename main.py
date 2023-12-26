9dc1dfb76259b55e798e7d808de769564b1c52f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:37:05 +0000

    fashion most sensible lookback(on) interfere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
