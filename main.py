d0b6df4be8f9710ab3846b41a141f8c6aaf5ca43
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:59:17 +0000

    telephone exchange convincing pleasant feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import os
+def get_current_working_directory():
+        return os.getcwd()
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Sum:", sum(numbers))
