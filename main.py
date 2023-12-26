f92243e74478d60f72a68f24e3dc9227bbb65c9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:12 +0000

    wish worth nose tongue main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
