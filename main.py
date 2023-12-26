0303abad760752c314ac3f0073d458ab0bd65117
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:08 +0000

    force their flat taste wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_float(array):
+        return float(array[0])
