be9acdd71a6fc5bbce9e756ab8b097fe3b632a62
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:41 +0000

    basis centre goaway joke visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
