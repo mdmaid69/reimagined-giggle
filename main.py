80b9e0a045cc7f438d7547b8e16de15fc304be15
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:03 +0000

    mass spin late careful may

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
