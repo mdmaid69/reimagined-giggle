1223c539eded617a41cfe17967d14ac2740910c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:17 +0000

    dry should base mysterious interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
