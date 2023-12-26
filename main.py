a9aeec1e9594d26bf708c8f1ed318c826a1aa524
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:19 +0000

    unable clever lay acceptable curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
