51e17102b683fe064f26cc6e8161c17e9116d589
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:56 +0000

    blame wake arrive abroad swallow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import datetime
-print(datetime.datetime.now())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_item(array, i):
+        return array[i]
