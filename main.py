59512c55d185072216816a8ef3bb8fdaf7b9e212
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:18 +0000

    pile roll radio city shoulder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_speed(distance, time):
+        return distance / time
