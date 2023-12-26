50f68133ac46e969162d7330ba4e2738da1fa1d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:20 +0000

    advise sink depend something wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
