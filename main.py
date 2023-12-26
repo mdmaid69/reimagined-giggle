bbb76245208834ab24d71ea83328864fbb5a5fb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:54 +0000

    joke artificial of exchange pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def count_elements(lst):
+        return len(lst)
