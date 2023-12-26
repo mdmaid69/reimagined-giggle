426f1282d9bebbce3d7665c50713ba871fa39683
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:53 +0000

    practical Pp voice change getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
