30c64b53fe1b93df2c8e13e0f15230dd8fe6b063
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:33 +0000

    gap laugh improvement takeover pretend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_current_directory():
+        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
