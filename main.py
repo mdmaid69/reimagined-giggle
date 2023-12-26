5b8fb7451c8359a2090e84b8f8034aebeaa12eca
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:17 +0000

    hurt complain alive 566end join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
