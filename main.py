34584125c642e77f747bef7f4e4fcc806d4c4410
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:28:54 +0000

    sick satisfactory name walk feed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
