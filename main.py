e3af56d3e1e5b5ede7bfe41bedb54ce0b68cce8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:40 +0000

    provide boring various factory science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
