1d1a701e1b56f3453a6610f1d1aeff6f4fa9e9dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:58 +0000

    confidence middle word anywhere laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_list(array):
-        return list(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
