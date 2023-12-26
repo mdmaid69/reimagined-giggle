207a0c79a757bd7182666f60ed87e6635cd82e51
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:10 +0000

    practical take roll brother practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
