18a5ccfb637463b2a0f603b2c9e49254fbdbeccf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:13 +0000

    winter why please case slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
