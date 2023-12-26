8e37952204cfed900ba9c221654fedc5873d6215
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:28 +0000

    tape win object workout dear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
