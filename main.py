1c5ef8c3f520d4911d74d123fc3c7ab6357810c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:55 +0000

    information kill contrast organization date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
