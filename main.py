22b7f37d0dad936dd27beb2710199bfa1b7be2f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:17 +0000

    behind tread now lock down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
