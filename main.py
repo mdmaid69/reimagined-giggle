623b386fe74e4886da9a7423702e4fa1e2b459c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:20 +0000

    hand search complicated clean afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
