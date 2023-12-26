748c62191bf034689d1c9f38f5e11c9614dc2be3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:01 +0000

    cruel service exactly confident team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
