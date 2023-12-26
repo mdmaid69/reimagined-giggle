958bd190d7fb640fd8253b18625655d973f0b93c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:01 +0000

    brown draw carry Qq consult

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def subtract_numbers(x, y):
-        return x - y
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
