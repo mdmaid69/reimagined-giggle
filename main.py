0926f2d76efda0b725a4304a1478e464bf3e5bcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:42 +0000

    regularly bury desperate upwards process

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
