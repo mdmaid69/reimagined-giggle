28fca7eaa7ad39e62671279c239ba659f571599d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:15 +0000

    union wash there feature believe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_bytes(array):
+        return array.tobytes()
