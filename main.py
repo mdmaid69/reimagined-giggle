9919dfb1e2c904e03d1297a387b73824e9059997
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:14 +0000

    confident pleased later naturally suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-for i in range(10): print(i)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
