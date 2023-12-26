4c40e8446d7b95ce6a053431f392f42bd15bb66d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:18 +0000

    rank room circle foot want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_min(numbers):
+        return min(numbers)
