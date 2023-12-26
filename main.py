1c7125da3ae8aee7cd1c0ea8be17c972e4824f8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:04 +0000

    muscle minor tip necessary centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sys
-def print_python_version():
-        print(sys.version)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
