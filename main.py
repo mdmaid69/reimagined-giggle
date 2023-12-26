8d3b8fccbc0f26968bddfd46c8f7bd505719e04c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:05 +0000

    environment blame beneath cruel top

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_counter():
+        return collections.Counter()
