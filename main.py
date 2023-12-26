40949af9dc9de67afdf386a8163f2fd4b4e67f90
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:09:59 +0000

    music address somebody tie 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
