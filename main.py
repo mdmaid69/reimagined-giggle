92476ee99115bb15c1cc2e837feef6238ffde69c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:07 +0000

    turnout take in family before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
