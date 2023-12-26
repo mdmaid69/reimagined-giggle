81161d6fb32428fdbe339f38c8ed566b68edd096
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:32 +0000

    safe spend hit runover doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_bytes(array):
+        return bytes(array)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
