762ce091f48ffbcb029b979968e72dc66459a6e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:07:28 +0000

    ask exercise lucky walk correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
