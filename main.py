41df4bea2c5d142dbbe33a23b9ba41946de7aa77
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:57 +0000

    demonstrate proof I risk meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
