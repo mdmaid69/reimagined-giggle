f29fb32e6b3b4e60fde2b27486215a8bf6eeb80a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:22 +0000

    put frightened more beside thankyou

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_max(lst):
+        return max(lst)
