1d1c5ef5792e541d43c91c9960cd851704faa9d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:03 +0000

    disapprove equal year spring explain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
