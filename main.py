6b3d72e9b2b4b465c85d5caf0f209fbf27bfd1ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:42 +0000

    brown please wave unable shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
