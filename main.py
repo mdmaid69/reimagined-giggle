e170f40aa1541ddb2a60304689701c6aa62b1ae5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:21 +0000

    approve earn precise use comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
