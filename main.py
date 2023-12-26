1748416f649de28a9184949afe954d15d36c9f3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:34 +0000

    brush report ring appear degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_typecode(array):
-        return array.typecode
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_list_from_array(array):
+        return array.tolist()
