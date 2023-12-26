850a50d3855943942d3b765bb32ea4e9c635cb22
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:54:42 +0000

    supply fit dry language order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
