3ac6a47c339f4d85ba004a305422dfda41fe7abd
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:11 +0000

    measure plan runout flat keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
