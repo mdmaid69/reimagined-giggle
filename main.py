f95c542a8a5455338607d351f789b9bfadf559c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:27 +0000

    favour loan image shine thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
