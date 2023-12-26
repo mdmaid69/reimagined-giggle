7f307a9714497745d3848aa6acce917b4b74e19a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:06 +0000

    air damage sister about should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
