ae5ec96b27b4e7be625b1eb6b154e0bf41dc370a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:53 +0000

    rush straight takeover explain heavy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
