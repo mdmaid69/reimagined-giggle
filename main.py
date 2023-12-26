ad0299ebb969247348fd68a6bf292104c065c03b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:12:33 +0000

    fashion recover pump quantity win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
