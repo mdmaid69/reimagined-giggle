135f9293e32e77e75e95fcdca85d8d1529c7fc89
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:09 +0000

    this giveup strange explanation close

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def count_characters(sentence):
+        return len(sentence)
