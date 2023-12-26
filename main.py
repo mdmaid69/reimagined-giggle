1b7bd3240853a70deb1bfa083aa167a13996a024
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:09 +0000

    greeting noise miss poison split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
