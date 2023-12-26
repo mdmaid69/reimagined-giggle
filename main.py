4690241e32526febf97bb2aaee7ed371f4edc591
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:02 +0000

    means pause step go fortunately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+text = "Hello, world!"
+print("Reversed:", text[::-1])
