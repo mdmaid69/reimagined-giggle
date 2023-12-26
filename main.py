29907dd3299f8751438e79129336347c070d2ff8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:15 +0000

    scene tough personal sink willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_pressure(force, area):
-        return force / area
+def get_array_slice(array, i, j):
+        return array[i:j]
