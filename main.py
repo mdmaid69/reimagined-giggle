6ce28aee5112f8b40cdef4738056f4b888610223
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:14 +0000

    lesson advise slip lookafter gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
