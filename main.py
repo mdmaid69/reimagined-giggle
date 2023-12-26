cfceb5af0c1ba958f0559939c297f1ecfbdbb845
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:40:26 +0000

    nor find contents surprised design

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
