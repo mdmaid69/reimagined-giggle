ae233d1aded8401a864bb1c497f2c7d0a6e912d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:12 +0000

    board temperature connection finish quiet

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
