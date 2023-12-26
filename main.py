59a9b812caf012fc1c6f4c684914c3c5b9a90f5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:14:36 +0000

    period wild worth vote listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
