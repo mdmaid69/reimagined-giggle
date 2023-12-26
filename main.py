cb29ab00c39b7216b69a168abe6b546c24e46bd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:44 +0000

    load anxious slowly clean defeat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
