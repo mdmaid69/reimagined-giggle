4e0391eca8af5397348e258c59146430e9d4307e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:37:33 +0000

    water study survive loose word

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-import re
-print(re.match("h.*o", "hello world"))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
