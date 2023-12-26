674fab98bf0a87c7542ecb5ddea60a2d1c8ef613
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:25 +0000

    remember major land callback few

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
