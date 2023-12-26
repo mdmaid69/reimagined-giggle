5ab66d584f98588de19ca45e1f6ca926f97031ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:35 +0000

    flight shall owe wheel level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_density(mass, volume):
+        return mass / volume
