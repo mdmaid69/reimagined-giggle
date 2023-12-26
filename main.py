9375084dab09055ddea0b66df54696cee6e4f8b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:47 +0000

    borrow woman copy argue honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
