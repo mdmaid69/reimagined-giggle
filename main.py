cc67ff03b34c1d3194abd3d4a9ce544193c42b61
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:57 +0000

    father influence muscle speak off

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
