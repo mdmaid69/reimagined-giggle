46a22ba04872d81211784c77480f077b6a5ec01b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:35 +0000

    Mm practise scream agreement sport

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
