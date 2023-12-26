d1aef7bf2153cb1aa8014eac8f34b5286c33c301
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:35:06 +0000

    combination pile offend stone left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
