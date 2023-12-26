7b57f8a3ca207abbb7beedf330512707ffb05abe
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:44 +0000

    only cover chin use private

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import time
-def get_current_time():
-        return time.time()
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
