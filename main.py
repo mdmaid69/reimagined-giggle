028025f9bdeb875f042ac8cf126d1950c1bab24a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:28:29 +0000

    police terrible period desperate cover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_set(array):
+        return set(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
