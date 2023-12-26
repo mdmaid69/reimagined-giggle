95d6b73df048395c4222b3d63cd9191f92443379
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:56 +0000

    eye year special practice stick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
