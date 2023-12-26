4ebbe1a9b78321fcd7817e25cb8ec8ef7992ce84
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:46 +0000

    Vv strange official turnout mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
