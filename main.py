f44281f05137c9e4efabc7f4404e8c957566edeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:06:30 +0000

    official black average post towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
