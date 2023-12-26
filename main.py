35d93fb9c159528d532dfb8e182b26d0943e1045
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:03 +0000

    variety if object themselves today

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
