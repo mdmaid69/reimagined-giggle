4f0884c7d7381b948a70f7d52d13a3495553d590
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:24 +0000

    frighten weekend fence police smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def get_current_time():
-        return time.ctime()
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
