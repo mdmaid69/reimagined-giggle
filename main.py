b47ec3cc07164d7327468bcd0e6f8c654869dd83
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:46:31 +0000

    gas list with meeting public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
