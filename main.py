2be79aae1192d5dc9ce403005152e004207fba35
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:09:01 +0000

    event previous claim window like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
