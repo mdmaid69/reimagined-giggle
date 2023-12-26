07bb1ba221c7956dcae81f2d44980040250a399d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:32:58 +0000

    downstairs uponon front present think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
