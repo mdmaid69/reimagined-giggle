7d82f74df3caad2fc06455bdca9a6230a66c570d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:22 +0000

    lay along hurry gentle switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
