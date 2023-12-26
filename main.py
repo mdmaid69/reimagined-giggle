d8b979c1e3243e24ab32f2253e074dca57ef9298
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:04 +0000

    polite judge understand include patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
