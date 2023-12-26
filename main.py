6781c0dea4cc22f7e8f7f5e4aa71028acd12fdeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:52 +0000

    choose criticism nurse coat 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
