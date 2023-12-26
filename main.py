427e99a6b3bd2d95ee298d223a0ad8d733b29b61
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:26 +0000

    cut plastic breath law excited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
