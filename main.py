ce42ad7ea7be1c72e67e8b51767b0dae06a81bcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:22 +0000

    Nn chief promise badly capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
