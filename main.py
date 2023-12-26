8dc231db486317101a90e2b8a41e0e0292bcda7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:46 +0000

    agree division arrangement bush determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
