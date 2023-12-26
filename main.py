ec02adfe56728f579977daaf3857d7c46c94c860
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:50:50 +0000

    treat pin hope Vv just

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
