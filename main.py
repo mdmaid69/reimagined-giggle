0c50ccce798ec8e98cb67e41d724a515001821d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:55 +0000

    when tie design public all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
