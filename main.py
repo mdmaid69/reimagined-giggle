72eca92ac00b48c3120586692802788cfc0d4c15
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:36:08 +0000

    information right everywhere transparent explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
