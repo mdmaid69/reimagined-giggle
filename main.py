16243395707f27cce0dccefd64c4f87f9d49c36a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:27 +0000

    discovery difference put ilovedicks party

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
