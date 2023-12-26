bbc070211add007acebc761a19629646cc23dc9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:49:05 +0000

    window bush autumn uniform mind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
