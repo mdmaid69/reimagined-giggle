d746b742b7ccf7a349bae93d3140fe9266ed999f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:48 +0000

    formal deep male keepaway distant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def cube_number(x):
+        return x**3
