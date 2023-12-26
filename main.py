a775e1d51a6103854306995eafb69dac56a49bbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:14 +0000

    arrange pupil bridge shake who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_min(lst):
-        return min(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
