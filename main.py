7a6c3f288e0adf5765e7f6eca903d251a1eccab3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:02 +0000

    rough wind distance promise Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
