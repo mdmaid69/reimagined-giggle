dfa38c70438631caf966359e352101d652a8bffa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:24 +0000

    whenever inquire block result design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
