380bba2d9b2b0d2be7cfc1730a030932a8b525bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:27 +0000

    match string noisy deep duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
