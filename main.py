454306d055e6668a89e1b560dbb71313804ee6e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:04 +0000

    think age not out deliver

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_floor(x):
+        return math.floor(x)
