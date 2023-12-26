9446ec8e09ac3c43748e1fb9ed020fd8c17e0933
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:40 +0000

    human afterwards water once unfair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def sort_numbers(numbers):
+        return sorted(numbers)
