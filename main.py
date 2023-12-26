29514e49f5d830d63d3ca59c474a71c769eed19b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:07:54 +0000

    and bye save good always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
