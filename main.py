e39a0b358b5a824eea08facf9f2a30f17f0b0350
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:35 +0000

    choice squeeze breathe experiment summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_index(array, item):
+        return array.index(item)
