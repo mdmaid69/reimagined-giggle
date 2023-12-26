95106b65ebd41872897a9bc827a16201afa53642
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:56 +0000

    dare pencil right wherever organization

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import re
-print(re.match("h.*o", "hello world"))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print([x**2 for x in range(10)])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
