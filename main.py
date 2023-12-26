656e25741fcc65f7b40df486a0c997af110c55e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:57 +0000

    tone command offer test vanish

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
