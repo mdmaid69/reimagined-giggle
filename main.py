c4809e6d3d5c9e8b9e58d0ece8e95f30a73d8366
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:00 +0000

    declare swing yesterday prevent government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+  def get_current_working_directory():
+        return os.getcwd()
