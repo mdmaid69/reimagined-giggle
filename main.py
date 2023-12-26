eeb0a8e416517538887be0942d2b7ed732b0a72e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:01 +0000

    broad class accept dream unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def square_number(x):
-        return x**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
