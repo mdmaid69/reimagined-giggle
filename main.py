5f2e40ecea14294a36629e42eced037331d594ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:08 +0000

    rare thank prevent sense comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
