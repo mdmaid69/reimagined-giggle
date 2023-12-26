2c3c07b87317111f3bc15a34d71f6ac342565348
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:09 +0000

    few left somehow belong special

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
