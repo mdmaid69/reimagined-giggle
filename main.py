90fc013c6a4391cf84bf6de8fa6984cc7da046d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:37 +0000

    measure warm young bad broad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
