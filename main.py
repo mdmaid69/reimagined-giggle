75c082554a4e992443888f20061621b7c6c9d335
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:31 +0000

    risk load secret front hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def reverse_string(s):
-        return s[::-1]
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
