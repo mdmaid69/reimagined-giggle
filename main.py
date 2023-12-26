1bb1edc43265ae5f734896b746599a6230b9a459
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:00 +0000

    light satisfactory survive there unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
