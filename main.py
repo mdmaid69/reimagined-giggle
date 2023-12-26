050c705a4ee2e5be906f741cfb01f060830deb56
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:26 +0000

    height geton protest forever copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
