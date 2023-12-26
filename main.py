34aa230cb994463cd18e34fcea759c8cdac60b30
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:07 +0000

    invite tend fit split better

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
