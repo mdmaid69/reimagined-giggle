c6db7823ca1f96308bc3c84307f4c7d5b3acbb50
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:58:51 +0000

    late never choice strict lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_palindrome(s):
-        return s == s[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
