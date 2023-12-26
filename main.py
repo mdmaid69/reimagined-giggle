7d16ceea0247442e665b242de51e3fd572506e7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:08 +0000

    skin rank because fierce service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
