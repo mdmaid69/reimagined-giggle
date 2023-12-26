86b6765c148d3e60ee0c06c2a32b4fa7e855e3bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:25 +0000

    Dd rule mine out cum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def exit_program():
+        sys.exit()
