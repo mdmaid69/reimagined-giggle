3eb369f565602ebcec8d433fd04adffebff4a7ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:11 +0000

    exhibition getout face car contain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
