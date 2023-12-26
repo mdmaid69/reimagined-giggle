0fcf5b0bb542648cb8f9a284315117f340f4308f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:38 +0000

    nervous shock majority strong come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print([x**2 for x in range(10)])
