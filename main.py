7bbb6e6544e95d9240b9c239a4a6025f49871ccd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:58 +0000

    name listenfor shelter during include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
