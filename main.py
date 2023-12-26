085dd1306cd8eeeb73bd35c82f89c73910c019e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:27 +0000

    alive mad anyone interview difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
