ee400e6529221f498006712ffa434d0af76ce555
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:04 +0000

    noisy kind real tax private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
