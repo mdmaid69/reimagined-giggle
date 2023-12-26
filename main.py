be009994187eb9405c739231d22542355f5cf553
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:34 +0000

    translate protest slip exactly myself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
+  def count_elements(lst):
+        return len(lst)
