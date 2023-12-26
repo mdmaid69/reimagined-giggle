aad2fe5c5be4c3d7949ccc96ede979936621d1e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:42:54 +0000

    pump upwards into intend cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
