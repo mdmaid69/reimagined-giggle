b935cc17c99892c8b98437e9daf3ab0c71c06380
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:39 +0000

    strict slice comefrom replace uncertain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
