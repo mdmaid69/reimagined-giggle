d70e7f38ebe570f2f97d446e7911d44849d2733a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:35 +0000

    opposite capital direct human convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
