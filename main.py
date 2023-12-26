fd0932de09964197a4171f17192f0f841118a655
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:42 +0000

    it bring attitude supply colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
