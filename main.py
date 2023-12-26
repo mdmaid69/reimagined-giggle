59404cb5c9e3cf3210a770a580e8dd8065adeb11
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:20 +0000

    performance step worst hour achievement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_work(force, distance):
+        return force * distance
