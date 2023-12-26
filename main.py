fe2eda6b128d56c7e754bb8d595daa1ee9172108
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:42:56 +0000

    dull eager hit excellent ashamed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
