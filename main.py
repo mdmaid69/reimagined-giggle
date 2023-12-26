36d5d7460196824c0403ff210cc5ef2bf26d3685
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:18 +0000

    total burn strict off bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
