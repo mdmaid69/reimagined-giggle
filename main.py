ed3e29cedc0e463e4e0943587d454c8e6104c76b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:38:18 +0000

    comeout out record straight need

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
