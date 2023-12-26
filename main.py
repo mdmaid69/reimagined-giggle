b47d7e38c83f56bbd4fecfc6ae214971531fd3b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:31 +0000

    amount at some mix degree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
