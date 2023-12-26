9ae3b746419d4649dfa5803aac13da6f580f0104
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:31 +0000

    empty backwards brother lookafter exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
