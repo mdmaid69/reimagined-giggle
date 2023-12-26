84982e73c5708492245b4519cfe012d2c5d24ef5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:51:43 +0000

    strength character aim needle city

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
