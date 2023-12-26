ca36b865f2dbe58afcf60c40a12e9d7220da3d6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:34 +0000

    join sink tax lose friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
