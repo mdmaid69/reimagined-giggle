d7636b5c40139f1c29b6656005bee85e4ce6f000
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:04 +0000

    perhaps drop temper decision become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
