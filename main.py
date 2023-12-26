b7bc8bd54954e0bb2269bd94bf4b0e105e7808a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:38 +0000

    sometimes waste sweep nice past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
