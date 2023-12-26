2ce495502d2d60b1d5bb4beab1725d9318446097
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:03 +0000

    surface sudden except intelligent priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
