22fa71adefcce89fa38580307c52c96e6b1d4363
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:15 +0000

    even shrink concern other exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def reverse_array(array):
+        array.reverse()
