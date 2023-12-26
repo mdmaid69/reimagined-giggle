7f7513e043abf42eeab95513842cabf0fbf0ee54
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:00 +0000

    herself wrong rain press under

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
