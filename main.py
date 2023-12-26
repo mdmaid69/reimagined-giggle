328ccb064f261dda7a1e78eccbd750a7404491a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:25:59 +0000

    weather city face Vv prevent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-print([x**2 for x in range(10)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
