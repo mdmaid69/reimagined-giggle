c2248050e97c31ce4b034ed90f0f3d6508319098
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:10 +0000

    improve well criticize container check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+print(random.randint(0, 100))
