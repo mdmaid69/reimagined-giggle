5707a5c22f358428a44268e6a00cd7a1dedd116b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:19 +0000

    bend rise straight board usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
