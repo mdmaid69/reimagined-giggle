237e74dc91e3b7f61b4cb7c75902bf6a7997e711
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:51 +0000

    thinkthrough almost drawing average actually

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_acceleration(speed, time):
+        return speed / time
+print(sum(range(10)))
