44641f401271e1788ec0d2212ec845ce229ae148
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:04 +0000

    travel wherever everywhere deliberate both

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import os
+print(os.getcwd())
