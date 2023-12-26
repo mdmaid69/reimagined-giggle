86c0ef1a33e8330b75590a6423f3ba215687d103
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:47 +0000

    return cool because fast include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
