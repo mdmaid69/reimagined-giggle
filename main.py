705df26a622eacf8d5f218ae48899ba1233bea9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:29 +0000

    secret where as hour write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_base_name(path):
+        return os.path.basename(path)
