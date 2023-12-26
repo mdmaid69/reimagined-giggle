a8bf29b0454eb40e8890ec162d32281a932e2592
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:12 +0000

    you road so wrap now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
