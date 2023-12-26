fc7e030a839768321969726a0242a238d71f9c72
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:49 +0000

    eager talk public petrol memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
