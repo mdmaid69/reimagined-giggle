a7cb87015f58c49eb12ac7e64e5801ed38f207b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:35:27 +0000

    rest noone point imagine stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_length(array):
+        return len(array)
