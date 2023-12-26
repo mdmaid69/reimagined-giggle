4aa298943ed317749f64469bbc7879f94682d09d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:11 +0000

    peaceful equal collection experience Nn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def reverse_string(s):
+        return s[::-1]
+def calculate_acceleration(speed, time):
+        return speed / time
