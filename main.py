0c0ae4283ab9f62fca098cffcfd98b4cbd132e74
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:17 +0000

    advice future appearance special miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
