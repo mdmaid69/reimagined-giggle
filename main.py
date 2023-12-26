0623806ceee38cd6c0669db7d06a17ec446097ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:33:23 +0000

    somewhere injury dinner last 572enter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_array_length(array):
+        return len(array)
