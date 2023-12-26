f028b5f1d62356ded3ac859cbdcee8f239857c0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:19:51 +0000

    proud our once both table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
