2d34b9071075a4ac28a68eafbbfec35377c413d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:27 +0000

    show soil test downstairs left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
