028b04d6ea3a823d511d6a37faf6e6927ff834bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:21:11 +0000

    punish behave consider mail trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_user_string():
+        return collections.UserString()
