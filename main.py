eb468657cf4bc206fdbb98a2b0228eb8f36b7c13
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:18 +0000

    post anybody bit honest fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
