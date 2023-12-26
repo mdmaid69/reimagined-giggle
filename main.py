1971566c1dfe2df99b0cdfabda8115121c026c75
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:33:26 +0000

    loan tongue block either shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
