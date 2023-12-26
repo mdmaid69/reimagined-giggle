501cc9f1db5eb7cdd0f986d7346e194511fa7a8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:11 +0000

    address effort sing mind even

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
