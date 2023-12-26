f6c4b2a047bef84497877119c7f786f1bd68bdf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:41:07 +0000

    benefit common trouble other his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
