bf7fec3274c8367ab40c0f9f72d57e5fe8f8ab4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:53 +0000

    pretty gift bush smile still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
