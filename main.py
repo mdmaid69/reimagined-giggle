86cf449104b2ea8e8a66a46b0a39a2debbf62e8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:07 +0000

    empty classroom custom goback(to) straight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
