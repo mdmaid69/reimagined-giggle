1a72b0123fb313747918a5d16c8a00d03bfc2b18
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:15 +0000

    bringdown full then exact likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
