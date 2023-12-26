7cf84cd1f38c7247db9fc6aa06de7627cfaae6ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:41 +0000

    exam suggestion trap rank sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
