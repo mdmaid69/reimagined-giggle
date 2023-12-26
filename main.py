cd8b83d190a48635f367109ee40487c6af6967ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:25 +0000

    during shallow snow holiday choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
