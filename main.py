3af834783e94b0b4fef8062227bbc5a83cd8892a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:23 +0000

    off then praise frequent nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
