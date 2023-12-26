3569ae4e959fcdcadc7335d83ac7d86b7cda4d60
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:53 +0000

    experiment it until confused complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
