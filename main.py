ab9d8e49b4204bdc8d2062dc9bc0bd6a077b5599
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:30 +0000

    equal taste hers embarrassing bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
