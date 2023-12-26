b2e564f3caa0005b9e71231b72cbd418029a306c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:45 +0000

    cheap afterwards other guess legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
