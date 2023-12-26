1a5548d36d9141faf5533a80ce7ef882aaaa251f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:20 +0000

    may steal consult window pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_string():
+        return collections.UserString()
