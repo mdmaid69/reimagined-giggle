8033d2bb65c80707006d42941f647e72ea6a47a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:22 +0000

    affect improve careful comment market

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
