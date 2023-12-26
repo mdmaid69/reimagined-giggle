4419e133f08d14a7e321cdb27654ed129af0dad5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:58 +0000

    normal love stay dinner female

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
