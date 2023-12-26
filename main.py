cf2954d6cf06e09e72e90a850d537a9831bde06d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:15 +0000

    fresh use burn new get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
