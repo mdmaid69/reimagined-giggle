79f8caeededccd1b0229016594d14e098e57ab2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:05:43 +0000

    distant search perfect glad closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
