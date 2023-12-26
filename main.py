246ad74002d9f96bad277aa656f7a4af00f9bef0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:34 +0000

    note Jj broad depend autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
