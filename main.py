280d9daafa6623a42329bd9a13389d2dcab4073a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:00:05 +0000

    who uponon however preparation post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def convert_to_json(data):
+        return json.dumps(data)
