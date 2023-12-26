31b976b1c83b9f2e8c6d66b08bd3bfc99f348022
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:09 +0000

    sleep phone any anywhere cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
