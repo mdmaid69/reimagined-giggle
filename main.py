1d5894cefecbeedb2aabf3d97bf5e88ae6db490a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:30 +0000

    so feeling blame sing weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def get_array_as_frozenset(array):
+        return frozenset(array)
