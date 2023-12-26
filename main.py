abe55b019611d00810e00ae8c57af3858d3cd470
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:17 +0000

    sudden operation proper light equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_area_rectangle(l, w):
+        return l * w
