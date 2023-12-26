91a562d3ebc1e6aa591f280d2ab9eeb3b994e169
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:05 +0000

    add very image pronunciation special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
