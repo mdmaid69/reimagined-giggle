83884d7e06fe90f97590e73c1126ef20b0cc8516
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:28 +0000

    point spare other suggest talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
