2e4123dc013cb5a03b855ea9075106068d81f969
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:29 +0000

    turnout handle taste experiment breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
