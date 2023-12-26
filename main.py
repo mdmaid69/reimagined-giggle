3f0c04c404927a64cc79ef15281c0ef47fd69b27
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:09:59 +0000

    I border matter similar ear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import time
+def get_time_since_epoch():
+        return time.time()
