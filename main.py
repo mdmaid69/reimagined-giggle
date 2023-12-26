3f5713df68951cdf4b958325a5c2ecac85227720
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:52:40 +0000

    bottom so left important ask

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_length(array):
-        return len(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
