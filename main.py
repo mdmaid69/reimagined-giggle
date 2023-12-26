872e1d027692b98e95160fe23fc6f538b280ca9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:14 +0000

    control extremely friend splash ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
