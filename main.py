594565301a35a719620bd7b7fb3ad94e9f36c56c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:06:48 +0000

    mind rescue bury minimum makeinto

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
