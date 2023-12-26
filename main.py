95c54b954b78dff6bfcd5b14d6e8d66d0ff42bb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:19 +0000

    possibility land load say accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
