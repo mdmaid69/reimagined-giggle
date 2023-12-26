0dbcbc90e1baefa2003762643d301f66e55d3f9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:12 +0000

    music runover communicate interest unhappy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
