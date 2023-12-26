b46a132a1e779806ec72ad3363f7e68c917d0bcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:54 +0000

    almost error large memory change

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
