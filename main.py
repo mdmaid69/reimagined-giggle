0a8187c023dd6ba0bfe713263daaca25b6756066
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:21:11 +0000

    sharp free distance name instant

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(10): print(i)
