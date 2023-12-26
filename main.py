8e5a8f55acdf5480293292bd73fb952a75d03055
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:29 +0000

    tool unkind quantity rush match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
