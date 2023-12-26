918bc8eb6e88cf5249d36972e2eb69b9510e21ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:30 +0000

    read better estimate woman they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
