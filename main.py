02712b5078998169d76dd77ac176a522ea4ecf0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:18 +0000

    quit majority peaceful equal black

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
