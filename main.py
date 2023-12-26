69fc01ec32d3e65af8a023d00e778d425edd146f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:02 +0000

    lay talkoutof public story beat

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
