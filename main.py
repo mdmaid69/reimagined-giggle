b880028d269ae4514d6fb72c7b140039f238ceba
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:38:19 +0000

    record artificial slip twist design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
