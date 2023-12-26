961c862f84091b9878c7ea3cd5255eb6f931b9f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:16 +0000

    listenfor ill unless collect up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
