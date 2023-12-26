93ade23d3f03c7552f1800bb291434bed6c90e1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:42 +0000

    hurt trousers roof warn count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
