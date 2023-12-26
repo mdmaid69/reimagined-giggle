3b2c70b1450f6eb633411ac1913c966cfaf4c931
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:34 +0000

    relation address flow down peaceful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
