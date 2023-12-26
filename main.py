7a966db2ab392d14dd0252732b66399bd1a1a9a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:19 +0000

    forgive mail clean guide mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
