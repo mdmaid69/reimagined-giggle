b5dfd2c890ba1a133a68485a02e430bc27bd90fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:50:02 +0000

    several before game neat flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
