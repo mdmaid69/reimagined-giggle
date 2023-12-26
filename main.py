0e1b173f95115e7658c16c5ce378aa6a5a26186b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:51 +0000

    compete occur pen yes help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
