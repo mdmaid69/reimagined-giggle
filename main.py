1f1b7b0a071199b0abd04392b92f06d065053847
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:33 +0000

    lend suit sea exciting talkinto

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
