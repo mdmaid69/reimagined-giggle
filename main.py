c07c577130e60b5a44fb77417c87d6cef951c178
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:00 +0000

    Ww ordinary less holdout remain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def find_max(numbers):
+        return max(numbers)
+import os
+print(os.getcwd())
