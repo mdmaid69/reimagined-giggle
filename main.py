619849d9a23220dbce649dad35b9cccc56ca370c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:44 +0000

    thanks swim answer clothes file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
