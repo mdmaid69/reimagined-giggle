e5a8d68c1ad5183f031acb239763e2c69ab84838
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:38:46 +0000

    late minimum leave report mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
