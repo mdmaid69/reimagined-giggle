4d7e407e12200ab7adca98ecf15fba0946cff9e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:43 +0000

    bye round duty stop yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
