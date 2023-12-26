6ab94733f8a6c7279b6124455b9e134b6047bd27
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:42 +0000

    turninto for yesterday free lower

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
