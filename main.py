da4cab4ff30e271d0eea6d946d4fcf779f28655a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:46 +0000

    marry state arm realize authority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_volume(length, width, height):
+        return length * width * height
