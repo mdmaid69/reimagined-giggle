77e615a7c0954a5ca310f01d98434690413d4e20
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:47 +0000

    male appeal suggestion pile how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
