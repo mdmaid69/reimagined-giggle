0ab8e2e877d1ad04f1a17fd3a71eb6bd9fcba724
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:13 +0000

    insist father trade play brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
