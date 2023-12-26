695c1d8609cb2d18123b14f1c8a9ffdbe83a1e3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:14 +0000

    scheme record design comeup straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
