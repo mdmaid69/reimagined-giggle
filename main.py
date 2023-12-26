9a8a98586d6f2ba54e3f25b706f8ad31688578a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:32 +0000

    hold pause dish although mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
