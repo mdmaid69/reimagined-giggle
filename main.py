137eb876d58098fd66381b76707afa06c50fa165
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:23 +0000

    ride use difficulty shopping impressed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-name = "Python"
-print("Hello,", name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
