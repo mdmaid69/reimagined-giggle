4fdf1171ac6c379a6617e0c31297760400cca096
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:31 +0000

    cut impressed else burst damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def count_elements(lst):
-        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
