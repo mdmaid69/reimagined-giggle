0462d99f9fd17d8b9af60a07764d309567f3999c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:03 +0000

    grade hello juice war responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
