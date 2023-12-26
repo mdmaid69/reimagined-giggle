5a78f2e130f7d0899ffba351889141f0fe174fab
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:36:31 +0000

    trap decide impression education quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
