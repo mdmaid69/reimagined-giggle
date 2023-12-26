e04a5c7d4c113d4d1dde7c1460ef35bc1757ac76
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:45 +0000

    mother considering cross coat family

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
