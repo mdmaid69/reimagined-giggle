cbe9952e8a53a407dfaa78b5dc320e4c662035ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:56 +0000

    clothes eye shut bunch practise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
