f7c5c13e565ecb1fac4b2a9e40a6df4e84842755
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:52:51 +0000

    lead various rain convinced exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
