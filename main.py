e67fa4dc39ef90415ee827b78f40dc814b955d6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:34 +0000

    in distant read completely suppose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
