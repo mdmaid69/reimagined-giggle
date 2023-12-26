0baa92307f1bc1cc73e05c862f6c9b39d526440e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:59:10 +0000

    whatever dress catch back situation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
