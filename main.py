2051a7418222922c9efa6bedf516eabec0e7f2cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:34 +0000

    force getup legal disagree 564encourage

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
