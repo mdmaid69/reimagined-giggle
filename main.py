c8d255fa1ed3fb819a240f3527ebaac0e14c5e9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:16 +0000

    weak draw because wing if

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_work(force, distance):
+        return force * distance
+def cube_number(x):
+        return x**3
