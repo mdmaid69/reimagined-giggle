d555e71e6ddbcc9e0f762f6f950b03b7a25591d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:01 +0000

    rubbish culture beat certain sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
+def sort_list(lst):
+        return sorted(lst)
