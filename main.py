e59402f5fd6918713ef040806ec8f91bdb95efe2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:09:12 +0000

    straight question sleep division sensible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+print("Hello, world!")
