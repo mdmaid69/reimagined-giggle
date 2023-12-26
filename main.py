521d98dec74e552f7a293dc71cf20bad7c3ef35d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:25 +0000

    pain pen turndown transparent belief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
