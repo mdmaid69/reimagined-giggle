820c410d65e011d6d522f28e0abc4295c451368e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:58 +0000

    thinkthrough Ss various expensive his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
