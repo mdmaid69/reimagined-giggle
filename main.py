75f68ff3a475275985149a9a0d2ab2653ddd4fed
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:14 +0000

    sense bottom straight comeup interfere

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
