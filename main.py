c3e1b209abd4168d3d3a09d3b06220332c7b843b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:55 +0000

    none innocent dream defend luck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_unique_words(sentence):
+        return set(sentence.split())
