bf5bbc3cbb81612dac51ec658195eb0fe49de91c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:27 +0000

    pure unusual proper tight beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_min(lst):
+        return min(lst)
