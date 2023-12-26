9113499e1a98135416522810b2aa2eb0c6be4db3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:52 +0000

    task lead basic please pin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+name = "Python"
+print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
