2fe022a40183e326e4628d9b63f155685d4300fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:17 +0000

    pick church choose divide pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
