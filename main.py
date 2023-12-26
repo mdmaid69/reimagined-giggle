bbcfd6747dcb4bea987bfebc64e121dba6dc84f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:14 +0000

    information lonely whistle substance downstairs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
