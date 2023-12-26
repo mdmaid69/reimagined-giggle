60bbcce9f222e551b966b8cfbdf27c98c8066332
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:20 +0000

    cap favourite suppose bag runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
