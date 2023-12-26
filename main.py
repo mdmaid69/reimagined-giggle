26eb40a64c84426603c70bbc049b84682232f98f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:36:47 +0000

    worth Tt pot possible cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_set(array):
+        return set(array)
