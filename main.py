7ffcd232bb4e9c4ec68ddfb70128e1c3158e2161
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:00 +0000

    identify surprise lift healthy inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
