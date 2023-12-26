b5def71a683d918fde3b6fa4404f471f99bb8ef6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:02 +0000

    admire out measure passage turn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_speed(distance, time):
+        return distance / time
+  def subtract_numbers(x, y):
+        return x - y
