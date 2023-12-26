c73c2ee41268f939100b275575e599e0fad2ae4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:44 +0000

    set moment nigga flat lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
