6fedb088774d6710c386ce3886f6165247320c33
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:44 +0000

    its performance fault bringback shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
