fcf9ba6e698dbe078137a000a313d9ebfa2dafce
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:55:52 +0000

    takeout male impressive middle reply

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
