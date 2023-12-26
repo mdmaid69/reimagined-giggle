8913890349e1d542fbaf48761655d5b28f75ba50
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:34 +0000

    destroy note scale remove protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
