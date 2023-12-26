dfcb2bbad8b7a3afe51285c96c27e5ec9ed6995d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:16 +0000

    father about dream tall opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
