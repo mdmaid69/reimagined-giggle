84f9af319da4506bab1a5c7536eb5d9e6c4b3c16
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:26 +0000

    note add free gap swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def square_number(x):
+        return x**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
