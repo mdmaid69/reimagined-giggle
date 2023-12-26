cb397a8d75d338b62aa7f1af4fc253bb4e475d7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:13 +0000

    crowded purpose piece smart that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
