838347f34d7812f8bfa3317046c160b12961d37c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:06 +0000

    sensible there emphasize male rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
