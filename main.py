98a696c74af230adaaf588d010d77789ae69c2e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:50 +0000

    safe against neither love you

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bool(array):
+        return bool(array)
