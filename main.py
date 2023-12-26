06434b1edfe732f9fba730013eb2b1f66ec5c0eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:42 +0000

    than mix Ss describe view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
