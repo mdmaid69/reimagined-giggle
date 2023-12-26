8028bc0ef129e7a198d1d0c53d064c85bfce83cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:14 +0000

    basically follow matter within act

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
