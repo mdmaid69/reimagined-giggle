aef0a25f81e8e9aca5ef8473f1e9214904eb4e97
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:30 +0000

    present profit cool soon music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
