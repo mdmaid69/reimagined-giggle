f371f6a266860ec916bc6d3babdb2288dbe5004d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:57 +0000

    correction turn frighten action getaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
