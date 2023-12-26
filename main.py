83537cee6e32700b5839322beee9190732540a17
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:55 +0000

    wonder wrong nice warn any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
