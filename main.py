4a063beef410659cf3159a2482e791f177557e64
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:09 +0000

    glass vote display idea unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
