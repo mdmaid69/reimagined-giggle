a841a8fa428a8d5916fe8d4009abfcad0ea4c973
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:50 +0000

    admit keepup alone goout there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_binary(n):
+        return bin(n)
