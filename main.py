9195abbce4046b014f2ee3c7ed549d5e4854d159
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:33:53 +0000

    add intend comfortable within play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def convert_to_octal(n):
+        return oct(n)
