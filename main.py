62bd3ff8a5dfd4a1f5e833bcc831f71647c9ed58
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:23 +0000

    want direct tradition pride warning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
