8c2838e2e529d74bdd402f529e98ca911f899e0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:58 +0000

    impression left especially ilovedicks cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(lst):
+        return max(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
