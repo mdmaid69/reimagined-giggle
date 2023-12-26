6120127e8c9c534b72add0a9eda2db769e070aa0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:02 +0000

    top typical mail Ll pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
