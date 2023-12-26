1ac60f003b8a20e3050427bfa98e7dc4212026a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:28 +0000

    yourself race waste keepout detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
