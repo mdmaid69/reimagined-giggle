8d26e717b4c456db1c5d0cd23b15e8d19d8dd742
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:03 +0000

    band occasional shade fall risk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
