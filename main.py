d64a27696b4ec01f118039badfaf13cbb45b18fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:17 +0000

    blame organize emergency holdon lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
