afcbc063eb5c19081612505f0384e14bddd37e93
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:44 +0000

    naturally worry complaint warm capital

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
