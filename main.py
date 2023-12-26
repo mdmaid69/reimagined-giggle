f658700b60a6c46478e0972032120bb1dd52436c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:22 +0000

    police brother row next female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
