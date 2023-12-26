26073ccbfbc6a3d816db87df12a951e5fd439036
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:26 +0000

    embarrassing cum gradually wish careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
