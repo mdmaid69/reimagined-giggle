5187b05b17175bb2f2097e31075caae01046654e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:17:36 +0000

    lonely Zz against dear acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
