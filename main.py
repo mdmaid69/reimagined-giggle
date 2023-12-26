2ca71d3badef205282fbdc1b1a55f51db69b41a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:29 +0000

    area lunch anything congratulate country

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
