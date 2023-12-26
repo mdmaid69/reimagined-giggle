b171ae40ddb0c5b89ff8c6d475e79b08cc153572
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:37 +0000

    up morning or kind birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
