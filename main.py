b5a8660cb0bdecc0d3a283a62b6bfc5f31aa5ea1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:14 +0000

    mass regard 567enemy advice exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
