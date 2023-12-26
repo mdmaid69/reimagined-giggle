d057439d46a38dd6fee5a617f32347b95e4f1788
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:17 +0000

    fast breakinto fire factory proud

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-for i in range(5):
-        print(i)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
