15ff7f4b456f8b31ef7f84f07b1563c2ebf0e30c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:51:24 +0000

    order lot bear estimate raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_acceleration(speed, time):
-        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def multiply_numbers(x, y):
+        return x * y
