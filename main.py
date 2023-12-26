8a1eb7aed6840562cd2aca5b3fb14f9c917e6006
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:13 +0000

    joke underneath stay relation book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
