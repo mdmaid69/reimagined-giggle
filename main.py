4fe04e3cb0fc1ca02bfbd208094395e6d2994781
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:43 +0000

    crash weekend raise study separate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_counter():
-        return collections.Counter()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
