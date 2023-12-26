ccd6b8ddced2d0453bdae061fab78ae94ee622aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:34 +0000

    declare taste passage 179bite afraid

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
