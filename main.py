9cb6fa5689377252ab9789a1ce0b8ecfd559134b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:48 +0000

    excuse back correction rich help

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
