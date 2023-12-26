5b52eba8c7a315f80e797b129d5f23bec542558d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:58 +0000

    under level hold waste motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
