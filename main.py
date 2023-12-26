8caa695d7a069d79aca624352b7f94c1817e7693
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:43 +0000

    repair brother call right risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
