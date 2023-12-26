d950b618e9ec9c145d6c0f99a6a4f0ca1832b653
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:57 +0000

    save hold violence throughout lock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
