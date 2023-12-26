9143c0e2eb97ff293cfff690d6913dabbe2ac6f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:03 +0000

    fortunate him determined shelter best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
