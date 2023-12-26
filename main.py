28fb1834e0e02f25110af6e04bfe2fb62f39c4ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:55 +0000

    agree production secret throw present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
