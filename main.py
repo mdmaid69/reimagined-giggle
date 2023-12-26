8347d573a0943cb0242b437a957838b01a700eac
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:24 +0000

    previous nor thorough choose disturbing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_rectangle(l, w):
+        return l * w
+def square_number(x):
+        return x**2
