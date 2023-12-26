923d48af3f2be097b1b59f2c61ff2b4d8996402a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:57:09 +0000

    have generous accidentally whether gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
