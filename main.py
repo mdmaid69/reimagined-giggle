e4982c11934eef7f641d9347e62b939d2202c5e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:28 +0000

    circumstance true stand steep emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
