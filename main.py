8f4f665c2bf779d4237188f8a96335c63cb0b3f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:58 +0000

    believe please frighten demand production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-name = "Python"
-print("Hello,", name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
