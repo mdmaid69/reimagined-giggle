a569a8b9fb3a46b48096402d5bf321387a462662
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:07 +0000

    could point create mess cross

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def convert_to_octal(n):
+        return oct(n)
