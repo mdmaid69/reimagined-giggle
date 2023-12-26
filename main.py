f512f3ee5fe4c72f5c025dd054c52807d342de2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:43 +0000

    unlucky worth blame alive whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def square_number(x):
+        return x**2
