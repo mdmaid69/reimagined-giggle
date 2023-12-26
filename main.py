0efd40698d5b5a3c7db8d777de553832cfe40acd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:59 +0000

    swimming sign near with attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
