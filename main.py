35a9ebb287f10c50af272854447f7b3eb4956324
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:09 +0000

    closed through yet between sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
