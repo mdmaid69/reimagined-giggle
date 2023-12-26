1d6241f3e2497242752633f36be0e829a0be62c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:15 +0000

    on jump base rush glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def count_elements(lst):
+        return len(lst)
