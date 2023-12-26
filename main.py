b9dd48d4bfb02e9b334899cc070455b3ac04de87
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:12 +0000

    low my turnout upstairs bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
