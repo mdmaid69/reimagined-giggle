5c9a25d4fa05e78a4f7b103f344ad90e843cd5e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:15:37 +0000

    figure inside crime close downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
