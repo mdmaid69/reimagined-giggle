0c731824f85919bdc45bec41d2d19decb0872a07
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:10:10 +0000

    seriously save here perhaps responsible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
