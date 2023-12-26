6b6cff1de4d9b0b1e12e6290723fa9873f6b3e26
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:35 +0000

    concerned here until agreement lower

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
