295afa138102d78c859c862a473b99ff40146c5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:27 +0000

    mysterious mind foreign lucky culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
