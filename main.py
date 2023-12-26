e4e29cc42fbeba23dc54d9dcbf620c29ca722c07
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:36:47 +0000

    talkinto fear quit callup clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def subtract_numbers(x, y):
+        return x - y
