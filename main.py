74bea1ae637284a58e997877e61fe995ffe38633
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:58:34 +0000

    become grateful listenfor wander really

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
