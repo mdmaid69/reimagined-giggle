91d350f3c9c3a7d3981856001f8be702bbbc71b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:47 +0000

    medium dear success sure harm

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
