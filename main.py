db6cb0985702f75c4766d1296b565c7d1eae8f78
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:40:24 +0000

    part track image appearance improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
