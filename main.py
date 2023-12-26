3558b8858e4f769060db600236d7525f994dcb91
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:09:54 +0000

    also play chin practical advise

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
