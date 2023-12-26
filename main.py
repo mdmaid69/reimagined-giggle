170f6608e5871d5a5a9592f90d1ff3aec6b36e4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:12 +0000

    them free yourself lookback(on) belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def subtract_numbers(x, y):
+        return x - y
