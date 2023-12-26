ac077777539710d80d5dba17e01e514a96583872
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:53 +0000

    lose Uu knowledge tense near

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
