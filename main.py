c3ddc48029ac57b8a7fef26e753740626e634c04
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:30 +0000

    already declare heat pretty possibly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
