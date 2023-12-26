44719a8309a48f4e0361fa6ccb20b5fcc8a2f71e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:15 +0000

    system night exchange callat holdout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
