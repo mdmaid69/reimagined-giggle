6f2860e365f199e5f48c4fc91acb5acabd68b8a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:00 +0000

    wash step pretty begin thin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
