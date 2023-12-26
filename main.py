01d88ebf1ccd34023c9590d4b29843f7a8e974ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:15:47 +0000

    accept see plenty pretty shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
