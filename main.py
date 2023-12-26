6b560eb9c18d511e86cae1efdbc0d0e00a49ca53
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:37 +0000

    mad near translate pay appeal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
