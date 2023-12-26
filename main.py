b4716c8a0c8cd79b162bdf0d0adef60f4b9fcf57
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:52 +0000

    many tip father common introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
