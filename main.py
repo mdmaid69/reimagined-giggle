08d94be0786696860f7bfc69e9ef4af20536b7c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:59 +0000

    return interview eat this waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
