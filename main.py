bd0ebf892e8a5c57da7ddd8c1e5268377098cbf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:30:47 +0000

    people geton bang accident consist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
