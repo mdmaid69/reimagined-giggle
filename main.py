9505bc85e7b9dcfb91aad9dbca65b45dffb53fa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:11 +0000

    start dress Kk before name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
