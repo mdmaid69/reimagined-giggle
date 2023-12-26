d990afe9eb27c9a34489668c09e741939d9f83ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:51 +0000

    disapprove prevent unusual giveup choose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
