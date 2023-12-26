fdbec3c4cc3a9b1def80f6dc6ce89cf7fe5f0853
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:30:27 +0000

    brush offer tonight film repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
