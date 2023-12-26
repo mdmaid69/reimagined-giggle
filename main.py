fc886d40d2e9f9909b04b51bd83e049a44e42466
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:28 +0000

    city scrap congratulate freedom nonsense

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
