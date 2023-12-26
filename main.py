e33139a2047e22cad335da1881eb1f2ecb26b9b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:18 +0000

    always comeover store cum Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def subtract_numbers(x, y):
+        return x - y
