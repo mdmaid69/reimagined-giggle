e05f9d7f6ae0527cfcb61a30c091745cdea0d161
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:31 +0000

    better down regularly rank holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+print(os.getcwd())
