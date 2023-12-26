3d9ca1a43ca9a2dd7ade00820489b1e5237d0140
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:15 +0000

    mine consist distant sea total

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
