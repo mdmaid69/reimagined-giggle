2d5a8949f6e48469fe135ee341a9bbdfa9b0f8dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:56 +0000

    beside pronounce show neither laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
