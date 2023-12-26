d894b18a212609b40fa9ae63d8bbba8dd1e50af7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:43 +0000

    herself arm fast your freeze

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_area_circle(r):
+        return 3.14 * r**2
