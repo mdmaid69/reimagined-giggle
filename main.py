e97dcd1e7121020640fe54a498348679d6ae0d71
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:51 +0000

    bunch demonstrate neither part add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
