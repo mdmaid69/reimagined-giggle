3e360473d089d6d0631e71e04169aeb1ab2c83f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:12 +0000

    and Rr reduce calloff right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_queue():
+        return collections.deque()
