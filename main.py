ba8d020aa156958640981253f049915dc0e8d843
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:55 +0000

    or ring contrast success next

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def reverse_list(lst):
+        return lst[::-1]
