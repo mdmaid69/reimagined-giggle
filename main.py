b64f6659319da9edbc766a70af622bbb014d90b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:50:46 +0000

    schedule fit next lose awake

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_work(force, distance):
+        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
