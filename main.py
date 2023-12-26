83ba1c0c6c2759c6685e53f88f87316618af3ad1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:10 +0000

    type colour let issue record

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def is_even(n):
-        return n % 2 == 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
