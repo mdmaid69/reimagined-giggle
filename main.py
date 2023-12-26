bf0f6a9dd14347512ec1ebb561f4cae159971ff9
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:38 +0000

    under ground arrive why creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
