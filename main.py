145da0ed28b6fcfcb900b2a5d49b5af02c0c1518
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:30 +0000

    wise breakinto improve seat quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
