4d4b7ae8b5ad0fa376808667eceac8c11c7e2e48
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:21 +0000

    earn material trick suck later

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
