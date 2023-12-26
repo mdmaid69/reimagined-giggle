09b93b5b336231949bb60023b491e46c68699d96
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:14 +0000

    plan warning sad prove turninto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
