50730b5604faa4d06de74a350d7358fd0fc8e31a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:31 +0000

    child appear corner opponent natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+print(os.getcwd())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
