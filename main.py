abc33bf1600f28de367b38d668d9dd4143c6058e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:07 +0000

    film since emphasis mind science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
