6c2eed63e3d9604ae7dffe79e4b111bb85b92352
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:33 +0000

    Zz smell ordinary future intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
