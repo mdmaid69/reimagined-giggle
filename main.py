3f2275ce4ed031a1275832af9ff64d1c6dc874f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:46 +0000

    any pocket maintain act after

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
