936b6b9737af52141e03da76c1ced9193e0f440a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:17 +0000

    need consist section lock off

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+print(sum(range(10)))
+name = "Python"
+print("Hello,", name)
