76f41199bd02ed2cbc1b550fe0ddd1ccb49b2050
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:19 +0000

    reply difficult present machine store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def reverse_array(array):
-        array.reverse()
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
