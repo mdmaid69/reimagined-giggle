96ac90fa6f58b9e71f600bddcc1b8be9ab9c999f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:54 +0000

    attitude uponon free conclusion artificial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
