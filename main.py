47f0b7159af7001cf5aa369a81bc03d174b677ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:36 +0000

    roll admire art month bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
