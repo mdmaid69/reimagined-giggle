a9134fca9b9dbaac1058bea69f72d8052519b321
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:42:47 +0000

    cover unlucky aware build string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
