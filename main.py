7358823af577800fc19076c2687717339e51a346
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:56:56 +0000

    afraid expensive home truth inside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
