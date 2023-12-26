1ffbbef745c8eb0f37b14dc67fc247dacce144aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:07 +0000

    car dark runinto collection lead

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_length(array):
+        return len(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
