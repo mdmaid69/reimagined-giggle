662d58266eaee0721d88bb6490769a97037e64b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:51 +0000

    hurt cruel over worry angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
