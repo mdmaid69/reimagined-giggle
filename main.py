dec06091b91595199eadeaf5cccb9636730a03d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:18 +0000

    private runinto travel bar touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+print(os.getcwd())
+def convert_to_binary(n):
+        return bin(n)
