3995f7f3c6dfc3db32aa8e6d0e413c85ada9a868
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:15 +0000

    habit past giveaway limit spare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def reverse_string(s):
+        return s[::-1]
