8bd715b58410aaa79aba9ce354ef7fa35ba3ea7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:08 +0000

    full any loose ago future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
