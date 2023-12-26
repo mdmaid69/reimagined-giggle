d78497353f4f55091fc7c55f63d605e4091adaa5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:11 +0000

    tense catch record explain sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
