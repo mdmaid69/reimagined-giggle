bc05a8a550eb42e74dca11fc85cf4f99f54b5f21
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:37 +0000

    hardly crazy national borrow ear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
