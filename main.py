c574869c84a92d39fe74404ddd46303fc4b5f7f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:31 +0000

    confidence hear soft tie direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
