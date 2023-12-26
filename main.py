ae7d0209f8e8f0cee3c93ea6121d6562424870b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:25:56 +0000

    too slowly battery book train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
