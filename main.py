70579c079ba5b389dbc295032f203df52c70286a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:35 +0000

    fierce they murder record accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
