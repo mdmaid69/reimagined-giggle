8161966a2744ef98e1466e1cdcea5bd8cc912f04
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:36 +0000

    total raw pretty height average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
