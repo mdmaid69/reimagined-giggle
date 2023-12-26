bb4affa14899c6f0b136a0c30160a1a22d725c09
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:39 +0000

    stick switch would along pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import json
+print(json.dumps({"name": "John", "age": 30}))
