1f0fa9992da5977d00f8463d6cb032f226419edc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:16 +0000

    pool imagination fall but drop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
