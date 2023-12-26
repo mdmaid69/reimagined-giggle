08c69f54d9ca10596ba5ab3289689959aaf191a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:29 +0000

    stand list maximum soak seem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
