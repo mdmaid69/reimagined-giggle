e4b105eff3acc6fd8cf29d341ce4ade542fcdaf9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:19 +0000

    doctor language bend pass keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
