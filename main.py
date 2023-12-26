466eefdf9000fe2899adad41ec95d59e28f15016
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:40:36 +0000

    again chase disappointed less free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
