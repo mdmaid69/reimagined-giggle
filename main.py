03f4f5e87a321b435914950c14b8d6dd9448e862
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:36 +0000

    reply loud bunch unlucky hospital

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
         return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
