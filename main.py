5e2340089a7b7d96f3909178fdae08bd8d7ca7d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:40 +0000

    dare course advanced square one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def subtract_numbers(x, y):
-        return x - y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
