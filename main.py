f96b303ed42b845d64258354fd9ba76ae194304b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:50 +0000

    with ours destroy otherwise event

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
