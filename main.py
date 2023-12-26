650b4507cff73f103c025b3e697f44a03fa6bd02
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:32 +0000

    answer today crazy me wait

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  def calculate_area_rectangle(l, w):
+        return l * w
