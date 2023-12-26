53569863fb131a9c71fb814f6410e447d8a320b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:18:50 +0000

    once woman sweet comedown drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
