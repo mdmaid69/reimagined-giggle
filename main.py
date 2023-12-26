33d1ee75aacfe83b4a7e5be28303f52bd5fbdcc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:37 +0000

    light necessary sensitive drive aim

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
