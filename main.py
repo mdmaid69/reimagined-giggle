edf6f708cfa5340d25768cd4a934b2e0ba73bf38
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:38 +0000

    see mark scrape impatient force

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
