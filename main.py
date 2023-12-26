6b6235412be0932d56f749cc98751ce23d2151e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:22:27 +0000

    sure attract confidence flight autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pressure(force, area):
-        return force / area
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
