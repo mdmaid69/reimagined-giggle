0ff579be1f7b0291cc4c620a84cd793c6931ef77
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:29 +0000

    refuse suddenly stare maintain middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
