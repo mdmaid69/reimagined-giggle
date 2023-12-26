448129edcc4a954f455d01f2346c9ee4d305bdae
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:53:21 +0000

    shape long quick suit bringup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
