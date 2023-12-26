645e579ad60345d1116b5d655656bd75302dc0f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:38 +0000

    pull confused storm quickly heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_array_as_tuple(array):
+        return tuple(array)
