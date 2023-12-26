d74a41e4c549622e992249ee86329d449c4c64f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:57 +0000

    low part contrast imagine nothing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+import collections
+def create_queue():
+        return collections.deque()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
