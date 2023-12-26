5568e35157d5ba57797d881bfc02fa5d85cd27a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:50 +0000

    bet for survive correct funny

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
