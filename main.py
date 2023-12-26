aad571c69c32ef39abc6b4f9f9f34db0e14c1ab5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:55 +0000

    its fit live answer strike

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
