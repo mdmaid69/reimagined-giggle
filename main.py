5709d97a7715b8df0afdd9d14bf250307eceae55
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:33 +0000

    concerned contain breakdown port store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
