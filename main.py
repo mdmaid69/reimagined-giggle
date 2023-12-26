8b0ea29636320603b0b81118aaac3421bcd0d4c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:48 +0000

    issue constantly let constantly base

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import sys
-def print_python_version():
-        print(sys.version)
+def add_to_python_path(path):
+        sys.path.append(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
