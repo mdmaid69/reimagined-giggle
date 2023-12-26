6e5855dd6aab17b1874d8e8942e238fea209c92d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:27 +0000

    foreign scared abroad difficult culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
