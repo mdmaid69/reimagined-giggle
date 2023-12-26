90d081e14f7bb7ef2f2f137641592a85acd50a0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:25 +0000

    illness attitude hard occasion swallow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def cube_number(x):
-        return x**3
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def convert_to_octal(n):
+        return oct(n)
