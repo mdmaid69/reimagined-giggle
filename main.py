eb7878cbd033fb8a7632ed5038088e5e6bfe099a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:23:32 +0000

    preparation less expensive goaway project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
