72c74d2f49ed31293a478a62e42243483caa5e76
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:39 +0000

    zero beside quality dress fire

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import datetime
-print(datetime.datetime.now())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
