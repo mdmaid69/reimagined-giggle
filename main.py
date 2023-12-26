07602bd35c2afd27f2e92a41c7ce0ff6710a09cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:55 +0000

    vision will sorry level fun

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
