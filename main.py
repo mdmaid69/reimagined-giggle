97b21b3f09a79654457e4d050172b747c5ec53b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:42 +0000

    high coat turnaway guide doctor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
