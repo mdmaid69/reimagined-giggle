37d5cc05265518c213faa4567b2f40b51a2ba3bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:26 +0000

    copy wherever Kk wide it

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def clear_array(array):
+        array *= 0
