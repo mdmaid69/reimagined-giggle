0fd93515b4ee9b4b8a6e9cf5cf53a6cf875a8333
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:24 +0000

    any ought warm whether generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
