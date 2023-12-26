d9f2f06ba5c47c9f325aa46e1884f1ec5aa71465
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:57 +0000

    storm taste yourself fun path

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
