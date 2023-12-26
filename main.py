703e9ca0620d91ebcca62e5b0fb20f4a9120b65a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:55 +0000

    popular callout disappointed opportunity callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def is_odd(n):
+        return n % 2 != 0
