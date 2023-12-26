b6120ad7f16f3de59f64524f4cc449579e8cc322
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:18 +0000

    Gg noone agreement exaggerate off

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
