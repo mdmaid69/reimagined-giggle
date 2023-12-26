2c562d19d7c2b5a1a7191298e30e3da260f5cc2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:40 +0000

    try cost medium throughout print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
