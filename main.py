39e59e90e9dbc91c2ce579534580e612cfc02759
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:18 +0000

    whichever coffee step warning club

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import getpass
+def get_username():
+        return getpass.getuser()
