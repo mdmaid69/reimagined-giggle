12cd37b5d10af29fd88536dc8c8f0d102124a80c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:53:40 +0000

    under emphasize concern trousers suck

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+print(sum(range(10)))
