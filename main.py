f8cfc4c076c5568ba4d02227f657afc513b7cb88
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:10 +0000

    attract brother freeze community trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
