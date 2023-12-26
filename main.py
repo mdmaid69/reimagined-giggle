518ea925bdf0e6eb1e722cb5d310dab7c9e08531
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:07:13 +0000

    butter demand message snow cap

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
