b392f812445ecdb70c81ea5c51e30be1204bb27f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:23:50 +0000

    turnover language workupto comfort across

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
