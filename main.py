5a050212b0a89b40f12687fefe94f54833f20fb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:03:19 +0000

    forget club cruelty Rr getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
