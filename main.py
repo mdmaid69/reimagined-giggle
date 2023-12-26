0890c051c4bb388a78314710beb54f6eed787516
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:57 +0000

    over advice welcome common report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
