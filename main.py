3676993a972257c8a08ab6126f6314f12eb18e2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:16 +0000

    sea careful refuse ocean confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
