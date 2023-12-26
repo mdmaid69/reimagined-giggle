33b3b1798d1489ac7f21bd3590640bdc4e9b7906
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:43 +0000

    shadow pronounce present usual degree

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
