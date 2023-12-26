1351d00d9a78334d258ae5332fbd7bbeb28539c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:14 +0000

    bear another corner major brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
