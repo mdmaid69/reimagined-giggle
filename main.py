94a4bbc6d91a891bd0be6e4db8637203d2e4c8ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:07 +0000

    lead neither ourselves suddenly impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_index(array, item):
+        return array.index(item)
