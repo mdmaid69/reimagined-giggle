25e6a909b72f4762113ef78b43692cb7b2d21d03
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:13:08 +0000

    weekend rubbish perform hold public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
