23501600fa7d9cc68d6cd334ceeb8765d439a523
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:40 +0000

    least likely want either you

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
