fb7167aa9afda8ca524b31a9001b262be5ccca43
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:39 +0000

    border organize tap mention funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
