302c6a6cb70f2da99abfa34e31549ade0b8a4a8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:04 +0000

    sound regard talkoutof hang film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
