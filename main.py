4bdda486321ca73c72278a07e4231cfa8c2697a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:10 +0000

    head watch flow image fortunate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import getpass
-def get_username():
-        return getpass.getuser()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
