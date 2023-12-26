7ae655a857320d529d4d431511ca6c6f7263cf60
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:24:38 +0000

    unpleasant through 567enemy achievement influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def is_odd(n):
+        return n % 2 != 0
