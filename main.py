e4bb7c5c0f8f46b8c6ca5dcafdaa41bb9426387c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:03:08 +0000

    close frequent expression drawing lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
