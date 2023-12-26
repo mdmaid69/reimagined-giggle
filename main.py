859f8d198f091dbdefc11008aed0fb7159528e77
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:16 +0000

    apparently pupil arrival very regular

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
