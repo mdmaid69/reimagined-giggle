7848492481c706891e08ff6463c2d4bfae569bcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:17:12 +0000

    spare print quantity excited 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
