de5cbeb47cc6e86f25e9b6f5cc814d20f7c4d044
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:56 +0000

    marry soil top fairly everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
