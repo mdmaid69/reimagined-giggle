62921e5f56e3220b40b8cb19b3f9b4daa26684a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:03 +0000

    lay proper success firm truth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
