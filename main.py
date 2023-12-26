6cfa2f9b3fc3d44b5465c5525852b00cf421c053
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:53 +0000

    attract someone run noisy unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
