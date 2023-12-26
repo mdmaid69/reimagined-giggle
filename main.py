c6c213886f513eeb594e7a0517035bd3a0e40f5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:26 +0000

    entrance stay sport callup family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
