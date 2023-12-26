05ce8b9cc277fd670aa0e122831ad90f3cd56f5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:55 +0000

    process workout tense record university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
