5654ec7a3cf9f42745955ed3dc37870ab618b5dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:21 +0000

    arrangement quick snow ability centre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
