7365ff0beabbff3c95a93c2b8285df12b1a9124e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:38 +0000

    argument class nowhere translate aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def subtract_numbers(x, y):
+        return x - y
