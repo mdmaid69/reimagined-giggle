a3d1675516b319a7fbc0be73ed25a0ed52213de1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:22 +0000

    bill board second allow picture

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
