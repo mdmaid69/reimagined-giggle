1ac4a8e6fd1c71315b6731ac8d6bf17b90d919e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:08 +0000

    them avoid goaway flat something

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
