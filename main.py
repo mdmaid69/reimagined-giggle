5a0c80c82e6aebe95671b6e05b42c8e9ee2d69ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:31 +0000

    offer sleep satisfactory feeling centre

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
