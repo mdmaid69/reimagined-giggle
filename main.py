31a0397a62bbdaaa9f9c04e716d77fb1603f1b6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:55 +0000

    violent fair lose division switch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
