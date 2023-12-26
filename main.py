93367c8a9a23959ba090152b3e8587f401d0707a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:56 +0000

    his piece wealth aim soft

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
