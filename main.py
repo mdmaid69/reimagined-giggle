2d9d6e22e0cbbb5b3344b1bd21d44d313f7d55a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:07 +0000

    tonight unit sticky breakinto badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
