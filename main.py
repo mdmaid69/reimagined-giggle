c120a5cd0283b14eaebe160dc1a93d9867319824
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:38 +0000

    less plenty process attitude simple

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
