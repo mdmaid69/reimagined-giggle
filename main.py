11d6d80905c0c377c23a9abf41432c2a02aaef99
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:29:04 +0000

    generous square respect cruel ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_user_list():
-        return collections.UserList()
+import platform
+def get_python_version():
+        return platform.python_version()
+  def reverse_list(lst):
+        return lst[::-1]
