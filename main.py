7d5918d0556232bbf92c6a9c19af5c8772f33d77
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:47:44 +0000

    relax sit spring sensitive law

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
