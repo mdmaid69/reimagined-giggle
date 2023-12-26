dfa83e685f78a70b0864a3146995e6346d4b5169
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:43 +0000

    window feed play rest sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
