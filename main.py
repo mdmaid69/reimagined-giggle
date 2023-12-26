ff40496847dfa97a43681d3662f25cd39c46124b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:48 +0000

    hurt share write probably yes

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
