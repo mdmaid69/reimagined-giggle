aa62106c3e4857bf0784f3ad3a30daac37e5e834
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:39 +0000

    her rather otherwise odd idea

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
