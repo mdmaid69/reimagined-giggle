57b562622199e86cbcdd1b2b04d3ca94cceb0102
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:31 +0000

    to intelligent yourself confidence fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
