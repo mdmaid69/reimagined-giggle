c3c0ba0149ca616c0005058fddcd4d26e76db1d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:35 +0000

    journey clean mystery woman last

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
