8b65493b0a0ffa0b75afb6197622504c47c7e082
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:45:59 +0000

    cruelty undo there score door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def divide_numbers(x, y):
+        return x / y
