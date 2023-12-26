491b1e983e08ad51ffd4914e8857d062224dbb3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:55 +0000

    even object agree post course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_str(array):
+        return str(array)
