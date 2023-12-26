bad9d3cdad27bffa3939a8838daec3c8bf10eb35
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:59 +0000

    closely 571enough now another connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
