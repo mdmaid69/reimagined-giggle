7e72663a1bccc55f1a64fd7a50eef5984bace50f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:35 +0000

    refer treatment whoever together instruction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_work(force, distance):
+        return force * distance
