0ea9687e98df299e2d61c7fc0e5f99696109b863
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:58:01 +0000

    study well schedule each wide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
