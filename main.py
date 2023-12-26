1f0c17f8bb980ec94504437ecbdc30944f40671a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:04 +0000

    Gg wish basis rest train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
