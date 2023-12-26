d5e4072c9f75eb316219cc099ea83b1cc30a5c77
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:54 +0000

    morning into nigga collection basic

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
