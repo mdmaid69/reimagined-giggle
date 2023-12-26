5ceb95a1223cce70660593ba64d3d583edcbf6d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:36 +0000

    middle up save ashamed precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
