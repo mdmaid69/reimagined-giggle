57161de8e99f711776b11800155a975850e616ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:18 +0000

    cross worth uncertain thumb often

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def reverse_list(lst):
+        return lst[::-1]
+import json
+print(json.dumps({"name": "John", "age": 30}))
