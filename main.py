b8f1295df5edc6c07fdccc911a4b4d524ac85c41
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:44:40 +0000

    decision load other equal honest

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
