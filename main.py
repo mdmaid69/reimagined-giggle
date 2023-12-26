568cfbf44f000f810c2ad548e68c46f5376e9ee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:44 +0000

    anywhere stop fix type factory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def append_to_array(array, item):
+        array.append(item)
