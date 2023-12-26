35f411610332e0b5b292ecb0f029c56ec6977f67
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:59:03 +0000

    thankyou beside cover project everywhere

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
