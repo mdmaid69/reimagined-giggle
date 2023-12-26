b01887bee1775213c8ba082a6583ba558e29d160
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:21 +0000

    rarely pen return now herself

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
