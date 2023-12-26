685361176d68edbb736b7a0cf359dd50a7b7f08c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:10:03 +0000

    disturbing describe anyone clean centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
