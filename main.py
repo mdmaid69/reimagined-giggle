1edce139ddd90e4f21fe391b08a2b77e17e51277
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:56 +0000

    attach variety sad least print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_absolute_value(x):
+        return math.fabs(x)
