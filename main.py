a7de789fa4b608c4b8ad3a5c1540ea452863e666
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:04 +0000

    slow welcome lesson solve problem

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+print(math.pi)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
