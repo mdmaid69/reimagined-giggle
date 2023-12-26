acf5b2f1562f71f4ba2633400e2c03653699a9d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:23 +0000

    throughout tool level estimate giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
