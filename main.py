852a74f33b00035e79792a6b5e7b0eff8500703b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:41 +0000

    welcome Ee yet law capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
