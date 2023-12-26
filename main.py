963179816f20bfe4a85966184ea60e49d72c7a07
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:23:57 +0000

    Mm of simple 565end give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_max(lst):
+        return max(lst)
