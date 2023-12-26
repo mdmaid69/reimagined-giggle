4f9a255efb74512c22c9bca0f154d82c6d1abb36
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:51 +0000

    hard different enthusiastic allow reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
