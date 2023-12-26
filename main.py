408dbe1d95803be7dcfb057ac511f71e2e1aa151
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:38 +0000

    add bet down business summer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
