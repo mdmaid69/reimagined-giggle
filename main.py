c767587bc4fa099b7f333974c64e823169daeed6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:21:39 +0000

    hurt thank accordingto winter back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
