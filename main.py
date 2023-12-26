74707e193191a2c9819e75ac942ae157e65e892c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:18 +0000

    position promise giveaway steam area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
