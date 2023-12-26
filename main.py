f4f9301f84e510ef7f4163ab806914f6e64363c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:44 +0000

    standard club play team lower

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sys
-print(sys.version)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
