ebf9243db5d59d0062cc312a0637ffff7384898f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:06 +0000

    profit succeed burn aim address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_floor(x):
+        return math.floor(x)
