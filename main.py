9e237c549110535d2f4653cc6dbc90487cafb3a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:49 +0000

    exaggerate contribute within design public

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
