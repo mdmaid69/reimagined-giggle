366ef1877ccc99b2991fd0647aa56156751788f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:55 +0000

    begin somehow whisper leg trick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def square_number(x):
+        return x**2
+import array
+def append_to_array(array, item):
+        array.append(item)
