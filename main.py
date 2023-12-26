91aa9040b920cfd46706d4b871c2e903c9d6e341
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:21 +0000

    better appeal maintain mine almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_force(mass, acceleration):
+        return mass * acceleration
