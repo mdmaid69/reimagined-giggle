860a1fd37ff7dc82bef682a5f6670c7357ebbdd3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:03 +0000

    solve warn factory just pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
