ca4f873136dbfb5c995441cac46deb5d9896c768
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:02 +0000

    wind hurt lower go work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_queue():
-        return collections.deque()
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
