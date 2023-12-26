9752f5b28cb6753890c6ee6f6231b006a9c90d99
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:22 +0000

    soak worth feel quantity room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
