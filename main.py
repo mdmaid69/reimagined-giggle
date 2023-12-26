a26f051fc9c96cecb23fb073269828ecd4b364bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:06 +0000

    slight holdon upstairs union Pp

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import os
-print(os.getcwd())
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
