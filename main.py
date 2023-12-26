6b3fcfd957df53eb993b937c80e5394cab55dddf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:15 +0000

    address believe inform wide century

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
