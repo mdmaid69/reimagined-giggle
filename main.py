6ae6eb8aa431becdfe0669991192ed48623206ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:37:17 +0000

    convenient class present trade sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
