568d0fc5a008f3888c6ec7f84de6d67fba948b18
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:20 +0000

    weak sign sometimes experiment attempt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
