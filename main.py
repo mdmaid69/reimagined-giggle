84e5b01daf51a2a9be5a2034f2ab8906639a3778
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:48 +0000

    last awake rob measure search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
