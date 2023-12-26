44133c8ce9ae5f3af279c96a130d4848454e2341
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:00:15 +0000

    club next turnout distance creature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
