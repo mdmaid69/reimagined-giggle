7043a956633eaf0038047cfce30feb090a1a24d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:44 +0000

    black bite direction accidental guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
