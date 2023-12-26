72dcc2ee27ebf313e5a7c039ea906be324160a63
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:34 +0000

    maximum friendly pretty slowly thirsty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+for i in range(5):
+        print(i)
