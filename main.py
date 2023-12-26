1ff5ddcd87ad4c7fecab17ea1f8898f33f1b26a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:46 +0000

    check increase consult letter exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
