376e86f4a308a4fa2f5ef461e14dbda3a6903254
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:15 +0000

    takeout wise future concerned call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import datetime
-print(datetime.datetime.now())
+def get_array_as_float(array):
+        return float(array[0])
