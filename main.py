9d6cd9d10e5c62b134f74a1c7adb07f8b261f8a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:08 +0000

    shut discuss hot nor young

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
