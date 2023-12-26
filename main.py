2621b24f6765346ac61f838d1543f150fbc2770a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:00 +0000

    willing ability section forever convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
