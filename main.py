fc6ff22565a771bb9c5b5bbbc0f7e0ddfb37e984
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:34:08 +0000

    turnout surprised over high throughout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def convert_to_hex(n):
-        return hex(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
