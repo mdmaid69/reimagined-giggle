f9c521b50d26ba90370e206eaed65fb86785e489
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:50 +0000

    greeting increase protest knowledge control

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
