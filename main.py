545604034a26a1702b7d038dc67c957ae3912b0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:26 +0000

    needle shine repeat respect disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
