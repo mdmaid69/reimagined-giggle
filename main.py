e6394dfdbe98ca6dc23adef040adb30f90766c02
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:14 +0000

    tour mark full danger onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
