37c5d4e1e044155aacee0d896125c2253172de80
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:52 +0000

    holdout exit keepfrom cope 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def count_words(sentence):
+        return len(sentence.split())
