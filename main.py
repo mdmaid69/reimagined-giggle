01aec301e34d5d300246b11508df100c29a219b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:46 +0000

    themselves onto nowhere involve hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
