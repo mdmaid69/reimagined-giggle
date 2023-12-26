9463db13842e18760d7ec618406fee19c1889469
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:39:59 +0000

    effort attractive workout so lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
