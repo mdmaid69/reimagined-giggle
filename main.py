fcc42b78d45257187266f80b6551c917b483b4ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:09:15 +0000

    thick unusual fail whistle fire

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-print("Hello, world!")
+print([x**2 for x in range(10)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
