113475e2d0da6f9a1a0fbcea7faf77bb5b72dd68
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:26 +0000

    refuse school floor no separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def add_numbers(a, b):
+        return a + b
