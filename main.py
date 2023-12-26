845e34b0ebcf6d764244c9e0c5e4d92906448585
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:14:13 +0000

    low live tough figure spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
