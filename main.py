4bcb863363475555d4caa5d53d97f999da6eb462
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:58 +0000

    bet illness none weak successful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
