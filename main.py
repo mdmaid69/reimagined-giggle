37d1b7ea791681bd046e89d8f6e659cceaa8fe0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:51 +0000

    read keep plastic common towards

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def square_number(x):
-        return x**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
