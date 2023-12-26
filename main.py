08f229ad4008de42cbadf34f7fdbde7ca81e25cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:52 +0000

    split of little brown official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
