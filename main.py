7b0c78d8f07ad6a55139be010fff6de9984d50ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:04 +0000

    record keepup imagine terrible race

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
