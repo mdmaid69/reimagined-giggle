62ac2df478aae106b7e41f38b6a89108e35a63ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:30 +0000

    detail view importance cover central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
