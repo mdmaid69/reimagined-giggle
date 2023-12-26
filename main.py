7d360d2d71f8f5dd61e135844d0c656eb2552a13
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:27 +0000

    right regular free eager miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
