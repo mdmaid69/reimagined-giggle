2acbfd46c8fa68647225441e94154041cc0cc488
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:21 +0000

    university second next thinkover around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print([x**2 for x in range(10)])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
