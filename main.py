2f87c7af9237f3bd071b5a967bc6e5f0642c4ded
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:00 +0000

    cover gradually goup community makeinto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
