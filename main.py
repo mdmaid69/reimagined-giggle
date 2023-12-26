b4a238d56b27486a46a488882e7c288e86100934
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:33 +0000

    wash rob fair secret worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
