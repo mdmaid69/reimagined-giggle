d11cb47932f3cfa1f32a560ce511bf5c60fe4dee
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:15 +0000

    suit moon clean land muscle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_work(force, distance):
+        return force * distance
