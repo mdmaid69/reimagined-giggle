8d0ac462a6f2d71647044ba6743c12a1ac4eb114
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:25 +0000

    Ll black move along waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
