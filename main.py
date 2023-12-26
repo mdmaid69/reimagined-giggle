ba8df6d3e5509622c9647557cef0dff106cdd66c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:33 +0000

    regard vanish 566end lower bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_python_version():
+        return platform.python_version()
