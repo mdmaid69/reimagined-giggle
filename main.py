c096a544198779159b1a48336202a7b70e23fb34
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:08 +0000

    classroom 569enjoy extraordinary cap stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
