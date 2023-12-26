412b0847fb41944460fd36d8b71bd882dcabec51
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:20 +0000

    block cold year glad basically

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
