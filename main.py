76b695aa3b6877691953df775a5ef946d2741577
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:46 +0000

    responsible above different practise Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_pressure(force, area):
+        return force / area
