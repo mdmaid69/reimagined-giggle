d046510ddeb53fba839607d80bedc4bb616231f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:23 +0000

    serve sort claim road smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
