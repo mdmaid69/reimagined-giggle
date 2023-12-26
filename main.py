92fbd129639e6f51eae8d58da7e703d5269f0c0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:21:22 +0000

    ability float pleasant private trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def split_by_pattern(pattern, string):
+def split_string(pattern, string):
         return re.split(pattern, string)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
