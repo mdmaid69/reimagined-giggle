4e447a60b6daf2822ab86abff7d1c73966516862
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:46 +0000

    runaway turndown thanks contain top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
