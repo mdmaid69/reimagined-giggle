2211ef465efdcdc55eae0663de2642d8019f091d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:59:15 +0000

    old knock minor well goon

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def close_database_connection(connection):
+        connection.close()
