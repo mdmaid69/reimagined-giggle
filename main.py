e79979b454dce3b11c772764c50f1498758685f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:54 +0000

    dinner information trip message direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
