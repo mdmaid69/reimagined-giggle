b4531ccf746465a526493509b8481b55c1b656a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:32:01 +0000

    brush any excellent clear breathe

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
