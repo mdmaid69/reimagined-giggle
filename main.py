31afc36f82e4a00df39d3d33b8d46280a235ccbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:36 +0000

    unit picture name question sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
