4250cbcaa386cc2b7561fa2210e28f4304a36fb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:30 +0000

    reply feature delight exercise home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
