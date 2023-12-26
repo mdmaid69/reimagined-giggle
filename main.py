ff18fceba92a3f4846d64a7f2f28cba885bc99e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:01:38 +0000

    considering spite solve definite face

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
