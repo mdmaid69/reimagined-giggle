86922b46f724d112101110ef677b09766852dbfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:56 +0000

    chin surprising male settle tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
