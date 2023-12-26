9e892ab8fb766000cf917306f631d4bad626f490
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:51:43 +0000

    file practise shelter plenty answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
