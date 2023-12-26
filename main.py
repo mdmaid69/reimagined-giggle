d64aa59fb362d8a22d2828f9eaadd496a17006ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:21 +0000

    substance organization value wherever prepare

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
