e9c59d1049e5b75ac14edb95be3cb18138bbcbc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:37 +0000

    responsibility easy joke grand pour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
