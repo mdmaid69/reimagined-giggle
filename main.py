19ae6927e405814d0fbb57d2eb5a13012ba9facc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:23:19 +0000

    preparation pile really themselves buy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
