ded2154efbbb6ec2e07d74c17a8f4be2307fc0fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:34 +0000

    measure official by start reach

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-print(os.getcwd())
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
