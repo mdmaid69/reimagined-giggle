c2ea8f68ca27b206efda9803296651b3446dcd59
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:08 +0000

    issue direction cope branch dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
