d7a2c67b65af405c6fc033195778bb8137dc73c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:57 +0000

    keepfrom risk square body could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def add_numbers(x, y):
-        return x + y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
