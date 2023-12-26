80a93fe513d1ad6e56e59e532dc2a7d914f163b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:32:54 +0000

    pity circle translate season squash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
