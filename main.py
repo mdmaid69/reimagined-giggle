7eba849fc0ddc4a5ad1284476fdc9c675c087f05
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:59:53 +0000

    trade space negative talk reasonable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_power(work, time):
+        return work / time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
