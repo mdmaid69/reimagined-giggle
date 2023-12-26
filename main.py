4ae90b037b242f179d8546d48be2a2580ab75994
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:14 +0000

    want conclusion full degree apart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
