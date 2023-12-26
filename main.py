a09d6a615fb3c7f6601ee56d568f041eab986138
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:52 +0000

    do succeed seat every mixture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def convert_to_hex(n):
+        return hex(n)
