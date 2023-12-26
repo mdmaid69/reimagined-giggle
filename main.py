6f7239bf6ef925e800f0fdcccd80979c87a7834f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:54 +0000

    joke good have approve choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
