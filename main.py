e2664ef247c6d56a01ea63e39e10c6906de77fb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:55:34 +0000

    teach responsible ocean experience rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
