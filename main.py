5a5e323569f72763800851a379c9b1b9c82b0eda
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:24 +0000

    strong bridge feel bend her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
