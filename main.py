3cf7291c9614e69bf788200348022c9abd7b9702
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:41 +0000

    upset sale blue will issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
