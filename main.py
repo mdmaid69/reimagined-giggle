4622240a6806c830764342549f8dec01041a47d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:14 +0000

    single year regret short to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import numpy as np
+print(np.array([1, 2, 3]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
