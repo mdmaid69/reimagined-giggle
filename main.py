2ecad03f808570d8df857306b3f65615a000c033
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:36 +0000

    fall quick condition change furniture

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_queue():
+        return collections.deque()
