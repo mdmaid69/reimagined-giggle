109510efeb072fa547125f62adb0e620bfd3a29f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:54 +0000

    fierce block battery separate firm

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
