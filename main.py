3d59cccc97e6f31f17bd2f50bedbd6619070b66c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:28:58 +0000

    feature maximum fresh brain loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
