550375cfb99c74803df0015a8479e14480d2fe63
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:48 +0000

    now legal rich suggest convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
