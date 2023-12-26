bcb866804fb814fd10d6989071f269ff2be435dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:23 +0000

    with broken further collect sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
