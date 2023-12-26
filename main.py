21a5a3837c879c6dfe0e0d7354407fd52cb76825
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:04 +0000

    cut ready acceptable mainly equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
