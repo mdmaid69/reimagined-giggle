dc193dc79da819e30e107d3e1bae52a3403df405
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:23 +0000

    stamp now illness dress smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
