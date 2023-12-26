0e569d3b271336fe8bfc51dfb17a154cf54280e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:34 +0000

    as positive explanation middle belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
