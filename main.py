18b1899af7aa19b8cfd3a81e0245e10d5c81e84f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:05 +0000

    design successful awake hard charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
