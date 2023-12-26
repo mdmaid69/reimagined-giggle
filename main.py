b8e965f11be18319f3261cbbb9231068ee13b475
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:42 +0000

    dream seriously please yesterday complain

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(sum(range(10)))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
