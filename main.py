506dba6a3549260f3c8fdae588ba682e71a18833
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:43 +0000

    refuse by share perfect track

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import datetime
-print(datetime.datetime.now())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
