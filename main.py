bbbdba294e7fa459ad6c6c71016180147e9b90ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:09 +0000

    better degree edge invite war

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_as_set(array):
+        return set(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
