1817f9a916f23e04fa74f6c678909178331a8d04
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:20 +0000

    quickly chat perhaps guess makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
