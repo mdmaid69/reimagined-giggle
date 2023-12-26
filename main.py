6313e1732a2c95c7a1da85e0051500c7bf0a7f62
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:12 +0000

    form rob intend connect market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
