4aef67348522bc73dc2cd8b609e396b2a62bf647
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:42 +0000

    age in pressure easy case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def convert_to_hex(n):
+        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
