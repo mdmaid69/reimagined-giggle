699d8af68632b58c97b602f23f6ebd0406ef1f5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:58 +0000

    downstairs become still anxious computer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_power(work, time):
+        return work / time
