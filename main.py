0ae25b514f4219e521264a20e525acc82a3fa606
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:07 +0000

    concerned small place agreement party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
