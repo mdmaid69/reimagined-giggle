577837d1a13b052c0062f27beb499c555e3d3ed8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:34 +0000

    machine subject division group tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
