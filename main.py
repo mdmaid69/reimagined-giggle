ccf6250d6f135ad1847fd7091c7e2212469b1649
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:45 +0000

    office lookback(on) smell turndown load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
