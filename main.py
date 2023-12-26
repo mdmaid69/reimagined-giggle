fd669d6ac95702ef0a9c7975b6f7973903565bca
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:54 +0000

    get will immediate future furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
