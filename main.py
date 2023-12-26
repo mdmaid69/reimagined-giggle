5fdbb600e85517f6a1df936100c11f2e6e2cef7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:17 +0000

    whether characteristic unpleasant fat talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
