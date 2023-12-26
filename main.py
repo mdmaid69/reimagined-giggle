6e889515831d5ef09dbe60c119e3da39b38b84e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:06 +0000

    appearance measure however office drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
