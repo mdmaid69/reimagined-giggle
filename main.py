97b32578e0138e7ee79db5f5265d73081a3de416
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:51 +0000

    ask stay breath wave still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Characters:", len(text))
