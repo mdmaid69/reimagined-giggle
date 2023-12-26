2b2e074f9dcac926c71a82688c88641f48b2c295
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:36 +0000

    murder either protest university depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
