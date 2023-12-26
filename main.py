0dc65ac574f20a0af9fc44ed0cfb22e7cc865893
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:54 +0000

    summer ignore shut television sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
