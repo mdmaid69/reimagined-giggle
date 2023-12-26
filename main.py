1758fc0e695bc58051181be91f208e1f70ef66c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:37 +0000

    middle greeting correct switch busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
