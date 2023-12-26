997b9fce60a6d118719b0da2be39c2fb23959d14
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:16 +0000

    still use murder accident form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
