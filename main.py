a44485e5681acd447566094ef8b7bdb2ea21db4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:08 +0000

    wrong corner by or colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import platform
+def get_python_version():
+        return platform.python_version()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
