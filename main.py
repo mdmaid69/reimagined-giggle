f0b3f6ef38ea2ebf2aeb9c30593b4443e74ec70b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:57 +0000

    priority measure loud set pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
