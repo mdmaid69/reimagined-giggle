82aac9f6a1fb4a0e5e9895556b66bbb1989e0366
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:51 +0000

    impressive find ability finish one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def find_max(numbers):
-        return max(numbers)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
