263ae41fb5dfe4a41d45f7103ca956a186c721a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:50 +0000

    top agree surprise war represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
