9ec0f9bbd1f501dd46a7ceb95a91c29fb0eedbd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:08 +0000

    pot undo advice callback Pp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
