43340fdb737b422ea4e8cc82d8d95dda99c58235
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:31 +0000

    cold breakup cost earth rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
