13aff3cd6d3e372987d9f8c4f2e644341bb846e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:47 +0000

    view shoot centimetre meanwhile afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+print(os.getcwd())
