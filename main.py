3f88be8b96b758448b877290bc6e45cd0d3d4252
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:33 +0000

    appear go big wander smell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+print([x**2 for x in range(10)])
