e8c0a22e7b0f7cb33a0684cc55f71bd31d5fdcc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:49 +0000

    side 179bite drop noone sink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
