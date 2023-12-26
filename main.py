163052ad3bcfb14ac47399e9d219d3bd0e9944f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:54 +0000

    precise lost praise use hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
