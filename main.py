4cd38db47d89b85479d914afb4775f5fdae8573a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:40 +0000

    post than cope except print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
