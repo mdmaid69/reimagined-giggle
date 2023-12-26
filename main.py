7743cf1eacb00abe4407c61971b871a334cfecac
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:13 +0000

    some quality dare violent live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
