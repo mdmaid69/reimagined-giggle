d271618229e12ed50869d19f0e87d55dc176e443
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:08:29 +0000

    refer prepare rush at tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
