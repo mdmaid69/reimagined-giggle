219f29c42ed0cf128e2679d43f1a4c499031ffba
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:23 +0000

    boat eat meaning borrow note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import sys
+print(sys.version)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
