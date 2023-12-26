f518172367a10b8e6e888a5c572c6f4fd349466f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:05 +0000

    keepaway left front free noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
