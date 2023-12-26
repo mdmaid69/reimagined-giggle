940bfd2bac2e365fb9b7011a89d73e5c87a0a6f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:33 +0000

    hand arrange marry angry port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
