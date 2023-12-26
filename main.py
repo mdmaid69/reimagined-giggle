37ec890e5d9b6f5a79f8f86a08015a0325fef5e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:27 +0000

    admire company raw nonsense underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
