0dc99365a01df920e577511badb66c88991c2b6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:48 +0000

    frightened politics line convenient race

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
