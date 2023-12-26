a970c91cb8259ba4f843fe6bc9650d259488f0f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:14:33 +0000

    wall check range unpleasant university

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
