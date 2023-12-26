29bd7ee3ae655ecda2a9094c3064339819e8c0ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:57 +0000

    number by train children break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def calculate_area_rectangle(l, w):
+        return l * w
