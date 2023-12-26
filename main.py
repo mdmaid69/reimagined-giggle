24ebde6d0be4f6995632d24645b2d13733ef680a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:09 +0000

    kind fall turnback deep introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
