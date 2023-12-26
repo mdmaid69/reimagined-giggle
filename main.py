2ac3d351ef83e852511abf546d0d65f0e20615c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:39:30 +0000

    pen turnoff wipe minor live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
