9ef81c7b0a35f657bbd0a7699089a3410f3cb92c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:07 +0000

    tour consider cope each before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
