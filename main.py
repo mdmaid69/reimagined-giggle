8ec2db9cde560cffce3c0b201054bed89d4bf294
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:26 +0000

    later succeed equipment warm more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_memoryview(array):
+        return memoryview(array)
