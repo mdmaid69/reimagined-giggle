919b088243c162d466c1ce202276b43222c35707
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:56 +0000

    deliver lock waste weather pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
