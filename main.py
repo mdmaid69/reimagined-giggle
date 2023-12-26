53424d4b27ab17e2ae7686c489896bd6114ecdf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:03 +0000

    goback(to) get bread convincing regular

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
