515a46bc758250bb68f854173a40d2f3e00d45ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:49 +0000

    how deserve dish plenty every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
