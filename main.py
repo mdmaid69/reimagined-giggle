2155355ccd961dce1a4792ec0a5fa40d595c5534
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:02 +0000

    train your disturb deserve temper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def count_characters(sentence):
-        return len(sentence)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_acceleration(speed, time):
+        return speed / time
