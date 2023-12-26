cadb53d413f2826a9d472a588d3019b3f249b784
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:24 +0000

    objection view play telephone Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
