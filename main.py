dad18987bb961b30e6c522d39b0cd768e86ac56f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:23 +0000

    experience brave whenever weigh achievement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+for i in range(10): print(i)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
