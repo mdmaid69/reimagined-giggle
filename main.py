910902e5c7933e9792e977585c167448ad0ba9f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:24 +0000

    less proof giveout benefit side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
