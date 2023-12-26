13e24033f2fb24dc6eafaa5a5633a8a40687b499
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:43 +0000

    stamp mass steer sensible arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
