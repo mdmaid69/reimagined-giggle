6c8044b02cfa3a01af361023912515e671c834ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:10 +0000

    yet already available lock pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
