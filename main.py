91a2ec25061db5237ed97ecff7e4837c9e4d19f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:15:45 +0000

    disturb section charge will unit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_min(lst):
+        return min(lst)
+import collections
+def create_priority_queue():
+        return collections.deque()
