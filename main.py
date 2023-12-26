42f10d1097f1f4fea3a3a267911dc37bd2affa25
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:02 +0000

    attach pencil let laugh win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
