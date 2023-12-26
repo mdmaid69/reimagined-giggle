aafd39e8d6fb636b3f88b2a3859df3a9837f3a06
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:58 +0000

    count shop responsible catch shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
