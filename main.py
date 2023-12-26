3cccb5b81d76e179c458c8ad0431eab3c24b1cc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:03 +0000

    join respect field congratulations repeat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
