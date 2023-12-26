ffb7b8ece15b471999fbd6699c774aeb2affdf5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:28 +0000

    regard anyone phone eye believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
