68ccfa2772fa2d27ac0d4eb7382476434cee0174
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:29:46 +0000

    other active city anybody born

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def subtract_numbers(x, y):
+        return x - y
