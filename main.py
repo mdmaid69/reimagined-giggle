b923dfa023fb1505484e4735f4b11e6b4720e1d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:56 +0000

    punish environment discussion dress emphasis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+print(math.pi)
+import platform
+def get_os_info():
+        return platform.uname()
