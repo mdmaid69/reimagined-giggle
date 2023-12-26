93f4d3c24544a3af19bebca5bf83f0e18226195c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:17 +0000

    protect ring agreement type next

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
