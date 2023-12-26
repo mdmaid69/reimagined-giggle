883ab69a513888344b47523f82d8fdf256637946
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:44 +0000

    our than emphasis language contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import time
+def get_current_time():
+        return time.ctime()
