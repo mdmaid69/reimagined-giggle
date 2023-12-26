21c9a90bb0473fb1f7cd66fc467f1bbec4169df2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:20 +0000

    wherever round minute anybody settle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
+def square_number(x):
+        return x**2
