c331ae5bddde23bf23e66e4203b653d0ad5ac437
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:03 +0000

    float disappear information distance give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
