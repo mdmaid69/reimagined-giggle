9bcb5b74753fc58511db58c787cfdd1e91fe4ba4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:03 +0000

    miss case everybody anybody silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
