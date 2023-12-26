93959d98789dc126a7622394c794f5bb90e89bbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:38 +0000

    identify weak minor cost instruction

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+from collections import Counter
+print(Counter("hello world"))
