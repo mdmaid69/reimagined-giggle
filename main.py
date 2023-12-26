dbb7f3b5430097055b5eea80187504c6a614fee5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:21 +0000

    seem special far pull turn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import collections
+def create_user_list():
+        return collections.UserList()
