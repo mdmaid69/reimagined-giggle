e816dec67d1e171c5bccc6c9550f59cf0710feb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:08 +0000

    container situation wire high exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
