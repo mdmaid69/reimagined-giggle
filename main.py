999500b712b987f225e1349f2ff894ec59a8d16f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:45:30 +0000

    claim often bother combine description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
