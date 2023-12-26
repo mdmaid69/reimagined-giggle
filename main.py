6f7bdec720e8a1a629cb73aa55da44652f994b65
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:37:27 +0000

    unpleasant improvement workon boat surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
