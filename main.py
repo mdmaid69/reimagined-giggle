abc73f9aed5a04f41acbe53c076df7bdd0c6defb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:10 +0000

    arrange low outside total hurt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
