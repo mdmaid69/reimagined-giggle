6e8befcc444c3ab72f7aea1e2158799e3ba226c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:05 +0000

    lend responsibility angry wish row

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def divide_numbers(x, y):
+        return x / y
