07b3d25be4bf835c2cb4dc21a6ffd3051021529d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:25 +0000

    learn offend touch fit once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
