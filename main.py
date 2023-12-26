bbd310b1048e354f91293cabad20c04d09a9dc9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:57 +0000

    cheek scrap class home total

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
