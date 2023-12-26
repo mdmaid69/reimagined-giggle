69227c22212b6dde84fa7e19d3369e3ec0c919aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:21:37 +0000

    process physical wire sell shout

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
