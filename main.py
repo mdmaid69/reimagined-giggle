378a45c1bb8a172d8a8968f3edb1563dad828c3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:01 +0000

    proud fire rush proper achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
