d84b2a62bc304bd8437ec4dc7ccf0e795ed4b5d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:52 +0000

    remark future stamp mean exchange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def find_max(lst):
+        return max(lst)
