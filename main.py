24a21af89625d6a2a21f618fc54c2eed874b144e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:26 +0000

    prove wherever means defeat kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
