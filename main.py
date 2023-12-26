13223799f97a4e5bd342217ad97b383a7bf9bba4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:04 +0000

    lock supply keep involve keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
