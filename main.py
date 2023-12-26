7a1a945748b09f194bc4d55346da0211e72431ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:39 +0000

    fairly communication crazy high branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
