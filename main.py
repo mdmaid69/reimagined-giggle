9783db8072c8f077c5a4fa70ea83ad180ab54c97
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:04 +0000

    transport clear twist comedown charge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import random
+def roll_die():
+        return random.randint(1, 6)
