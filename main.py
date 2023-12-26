d1524e7bab3642ca48d1e628418ef00fbb111a11
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:57:26 +0000

    demand himself shelter bread jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
