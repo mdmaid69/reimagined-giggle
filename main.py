397508c4614d16a2ceaf17dc3ba6bae1a8ec3ff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:48 +0000

    news means work hardly hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
