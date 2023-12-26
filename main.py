f6d43d6e206436664d0c9d58a76628500277a31a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:07:47 +0000

    total talkoutof during single soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
