15e556e5d7c7f43207693ba5ab77f72a2dbe543e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:46 +0000

    thin frighten standard praise ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 import math
 def calculate_factorial(n):
         return math.factorial(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
