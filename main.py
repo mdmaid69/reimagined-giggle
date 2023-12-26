68d04226cf09c66bc393365c5a5701240c942ec4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:09 +0000

    during spot callup his pay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
