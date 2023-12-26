c6cf89059fdf8c02e51d4626dbe5ac19223d5afc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:29 +0000

    nice other maybe water hardly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
