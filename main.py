43706cfd224a29bb4b9bfd77b919fc5298796c7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:56 +0000

    gas classroom beat whether Uu

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
