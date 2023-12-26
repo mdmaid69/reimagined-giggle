12e92c4d865a69bc7a009ce1c1acabcc810de09b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:25 +0000

    exciting hello successful so me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
