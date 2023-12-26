eeb301d50cfcdcef73593bb156143d7f76fc5427
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:03 +0000

    into operate warning suggestion determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
