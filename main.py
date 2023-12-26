2dbb32b2590e56d361c303bd1aac1b854d63f0b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:01 +0000

    pronunciation face change prepare Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
