d0b9f956195df933bd7caa735a0f1cf8adc2f3e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:02 +0000

    practise history reply cause Cc

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def square_number(x):
-        return x**2
+from collections import Counter
+print(Counter("hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
