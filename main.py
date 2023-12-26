0bb3f54c0d97e9e2d74f689a743c238c6eef627b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:03:42 +0000

    secret dance uniform watch exam

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
