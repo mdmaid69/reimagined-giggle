205a4d9a038f109bc162c0ac1858267de36fd22a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:50 +0000

    organize she takeoff steam Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
