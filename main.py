4957efcb6b13faffa8fa85a5d013686a9d9b7e59
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:04:56 +0000

    reduce practice bet climb dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
