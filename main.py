64ade81b99d87a4a777f5e532626f09c27b27ed6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:02 +0000

    rather pause recently holdon appear

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import time
+print(time.time())
+def is_odd(n):
+        return n % 2 != 0
