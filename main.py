3b8082677a5397e2ebaa984f32607e03fedf5779
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:34 +0000

    keepoff breakup mixture anywayanyhow otherwise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def count_characters(sentence):
+        return len(sentence)
