44fdd44819521cccec269a9e5eb7abb0c8fcf941
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:14 +0000

    after track still necessary broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def convert_to_hex(n):
+        return hex(n)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
