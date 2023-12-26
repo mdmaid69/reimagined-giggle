abcd8e4872793b5a9e5bb36f9042104a4d1d1e81
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:30 +0000

    occasional quit quit cook mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
