1d95ea8297c46d4d83bd4f2d0682d8da5f5435c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:21 +0000

    become ashamed date gentle tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
