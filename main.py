7de0e1f6b4e4b55f012d0ba67bb74035616ace5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:48 +0000

    involve takeoff pupil join people

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+name = "Python"
+print("Hello,", name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
