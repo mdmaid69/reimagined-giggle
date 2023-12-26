716f28dff73fb09051541eafbf98614985b1a62a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:50 +0000

    country kind relationship expression dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
