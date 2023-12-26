d5be4f8f9d7e0cd756dff08852cf8bb54ba7fcfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:31 +0000

    wall little comein loose low

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
