dc6b43a57c0e85026035ecf89d83df79a573c8ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:03:18 +0000

    hard achievement shopping fall to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
