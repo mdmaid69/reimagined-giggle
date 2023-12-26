bf05a413afafc74aa722a723d1aa3e77b4f6c9c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:52 +0000

    weigh model roll afterwards fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_min(lst):
+        return min(lst)
