7f3c5e11d77142f8c09326741cdcda7f67c7059d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:48 +0000

    coat male high contribute sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
