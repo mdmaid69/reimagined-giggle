15e5f4bf1fd87bb32601ece99ed521a16c8e7f2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:33 +0000

    equipment forget building country hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
