04261dbaba2e71383267ecdbe9a2eb081dd7bb33
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:30 +0000

    trip once support few polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
