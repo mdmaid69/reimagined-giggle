d86fa79857f4b7f9ff9e0395677bf44971878f78
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:03 +0000

    circle sink loss lend snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def square_number(x):
-        return x**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
