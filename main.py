e63057344d73e7d1e43a235d5c6c7e25beb94991
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:33 +0000

    height material pity material record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
