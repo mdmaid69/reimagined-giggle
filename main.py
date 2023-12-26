b8dba730ad477abeaa50f89c5b11cf01af419fe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:58 +0000

    only crime never glass near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
