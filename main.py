7f3e073736afeb1bc025ad1c9c56277842298497
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:14 +0000

    lesson Kk tragedy shall step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
