156d0b175e98df395ad8b677d2ae43f6b372b1ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:28 +0000

    importance owe treat material getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
