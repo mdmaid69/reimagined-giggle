f87cf01d0f3bbca3b4c98570c5922024a0576fd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:41 +0000

    murder Tt block decision regret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
