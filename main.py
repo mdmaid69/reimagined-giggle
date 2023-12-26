01e072fecd6baf340a216653debfb7b07365b350
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:58 +0000

    guess broken large walk off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
