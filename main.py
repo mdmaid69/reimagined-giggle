b8cdc61ab05ebc1fdc5125cf63cce80b82a6426d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:05 +0000

    comeround spend happen authority tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
