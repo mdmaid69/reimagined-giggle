d27947551c3dc9023e44fdc8b3cf6750a9f630ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:28 +0000

    warning about usual mood burst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
