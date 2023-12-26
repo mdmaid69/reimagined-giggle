5402fbe99e268813ac21a24422acef96061570ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:04 +0000

    maintain everything past deaf fault

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
