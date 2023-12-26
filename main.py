51757a13f2626267de8d38d72838f4932cb2a4d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:28 +0000

    task attention hardly goout well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
