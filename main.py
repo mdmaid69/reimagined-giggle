7365bc6b42d390148737b0db3c5fb69d4c7445e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:47:19 +0000

    loud vision system too walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
