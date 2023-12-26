55dd2f081a575e7b662812dc1ca7ba65e8a2f592
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:39:22 +0000

    clean broad slowly standard talkinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
