783b6c52fdac2ae2611b926885ee53f1f400fcc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:06 +0000

    telephone press treatment sudden herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
