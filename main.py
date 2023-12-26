790d84354ab83ffc89d82bb2994d69c43b89e159
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:52:40 +0000

    partner face let ever thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
