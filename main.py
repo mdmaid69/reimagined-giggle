9167315b9ed82ffceda295e308d389a56ee6fbbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:07 +0000

    never government who corner century

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
