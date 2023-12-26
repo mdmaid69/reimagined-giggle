a6a77c54dcd550a8f91943b76f511a59cb4a0321
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:28 +0000

    suckme afternoon table mess tooth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
