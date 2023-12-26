1692b17b16d7ff6c3468a05b97014ad74802fc4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:58 +0000

    touch afternoon simple follow service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
