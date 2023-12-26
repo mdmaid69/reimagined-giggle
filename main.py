38562db1e66c6fd698a92f87b8aeaddc104f147f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:12 +0000

    murder woman honest trap stranger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
