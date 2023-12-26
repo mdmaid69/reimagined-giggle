e8c5e9682ac350f25d9faf42d9ffebe7bced6788
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:38 +0000

    runinto message the better slowly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
