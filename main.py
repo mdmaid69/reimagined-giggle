1925bf3616485ca92c5047da54ca3fa828b9bb08
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:35:16 +0000

    theirs appear really circle try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
 def calculate_cylinder_volume(radius, height):
         return math.pi * radius**2 * height
