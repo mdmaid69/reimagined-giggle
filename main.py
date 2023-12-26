2d9fc380a1c78cb947d21b6e91be626b2580e912
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:38:19 +0000

    because street case interrupt reason

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
