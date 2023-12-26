d51e8eada1609fdaaef69b607c268c6712d12257
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:03 +0000

    by high workon congratulate try

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
