bf4dd372a031d4fc9382fede391f6023b7a582b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:04 +0000

    reply fork professional skin cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
