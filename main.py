f14013383ccd98434f9450ee36f51148bfc38a2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:28:06 +0000

    break build sometimes lookback(on) involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
