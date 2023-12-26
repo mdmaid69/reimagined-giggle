0bae6f69817bc086e64fde1ffb5bf89e824fe418
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:25:10 +0000

    blame possible fill meet remove

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
