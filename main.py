d9eb6ceb8ea4a5d72a931918633a4892f7b5d037
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:32 +0000

    season deliver whether mark against

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import time
+print(time.time())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
