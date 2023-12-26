d9dc75be9e7a07cc9b9cfb8eff711f2337465bfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:02 +0000

    door represent wire demonstrate bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
