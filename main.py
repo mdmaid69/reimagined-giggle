b08fb6110f304a641c86266c6ed2ac369256eee9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:11 +0000

    set coffee sad like repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def get_array_item(array, i):
+        return array[i]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
