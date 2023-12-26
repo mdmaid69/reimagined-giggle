56b50756c1a23bd7119407850e2a28e12283f2de
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:56:33 +0000

    demonstrate aware rare notice their

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
   import requests
   def get_web_page(url):
         response = requests.get(url)
         return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
