cf47debd80932095369cb32848fdca2f6866c75e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:10:14 +0000

    race pity somewhere yours maintain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
