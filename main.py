a57ee1942fdef6c28e6d1059bd649176ea9ff1b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:51:47 +0000

    judge opposite declare little custom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
