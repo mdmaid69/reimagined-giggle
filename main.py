e58e48f76fee728285e1d0efcc2b828dde2097db
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:56 +0000

    suggest kill print later practice

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
