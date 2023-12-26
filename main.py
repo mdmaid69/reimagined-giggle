2c61fdbbab6433a23bd27dc5251af5e0da9788e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:35 +0000

    public quit far within be

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_work(force, distance):
-        return force * distance
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
