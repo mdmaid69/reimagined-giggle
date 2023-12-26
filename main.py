b0ee9b81bf8b9c592d929af71a8ed0b16b9e31e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:38 +0000

    runinto door manage fast possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
