869338c25bf30022a55d02844504896433398bc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:08 +0000

    smart negative team finish spite

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_str(array):
+        return str(array)
