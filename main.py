c7f262895db9c3579225727aea5c83e69c950988
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:09:54 +0000

    on bank want form cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
