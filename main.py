c27f7f8dc74b532b6a412aeb04d213a78a6912ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:23 +0000

    soil excuse on impression winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
