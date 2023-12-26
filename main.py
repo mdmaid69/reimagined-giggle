d1f49b805ffb537497d4656f535a53cbb5b1af91
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:45 +0000

    accidental rent partner instruction touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+print(random.randint(0, 100))
