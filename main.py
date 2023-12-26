05dac1dc27a2a798d87a9b4934b02be48b4803cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:35 +0000

    book ready odd cap drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
