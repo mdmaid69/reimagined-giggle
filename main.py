0feb7342eb69cf3ab21d041b0430a62812ffec4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:03 +0000

    sure spend muscle makeinto above

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
