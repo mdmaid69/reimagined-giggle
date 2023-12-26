d1de6eb454a27ad55f60767bd4c4c8dbcf5fbd74
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:45 +0000

    waste meaning hurry top attempt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
