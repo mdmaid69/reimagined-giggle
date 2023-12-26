838b852fab1839a2a48a179ebb01e4d33b005c2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:52 +0000

    minute talkinto since scene cry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
