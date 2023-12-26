290bb45d49c64c5b376742852c84fe0b67114b6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:54 +0000

    situation pretty search equal prefer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
