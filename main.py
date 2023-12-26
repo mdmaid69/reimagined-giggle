88cda8d59a947b4e22f83f8a393a9cbb68a2ed73
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:28:20 +0000

    between crash knowledge practical organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
