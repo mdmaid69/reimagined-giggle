baba4d731ccc2abcf42d205c7ad18d68e4068a36
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:09 +0000

    block breakthrough one art public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
