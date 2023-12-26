8ae2206dd056e6a371ce53a2fedc8ed08108212f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:13:36 +0000

    thinkthrough process bag pair her

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
