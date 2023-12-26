78cb8121dfd8832ea41944e0a0d2654382e520cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:17 +0000

    workout flow hole or naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
