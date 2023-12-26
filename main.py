22ed3fe2f80290ab34b5c848639b5f1d00527eed
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:09 +0000

    cum stone early space explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
