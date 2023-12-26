a4faeb3b059735f054770c47e29e6ad1b5b2e42e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:12 +0000

    trick shop talk drop decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
