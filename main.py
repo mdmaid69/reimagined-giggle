6c02f79b3d2534324b9f33be9d63e8679f0e3579
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:37 +0000

    uponon complicated to few guard

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
