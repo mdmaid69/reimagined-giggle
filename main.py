646ab59e3b387293b0667ce1ca2cc819fb710f9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:54 +0000

    sad bang personal relation bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
