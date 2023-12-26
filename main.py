3ae4a1a2fbd5ba6257a98b836c61c7d166188ad6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:10 +0000

    shallow door to laugh uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
