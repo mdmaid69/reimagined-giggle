d2eef5b28a3e3a19e27550a6cafe1d9f803a3f50
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:01 +0000

    no pause about shout culture

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
