70481303ed1972a700c172b4841990359945b7f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:38 +0000

    interested drop pleased cause save

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
