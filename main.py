d819698fd9fe763f354b481fdcb28764a818383d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:40 +0000

    comeoff drink dream worst means

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
