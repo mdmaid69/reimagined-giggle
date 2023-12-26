d09257a441db6614d76e12ca1490af852d39a72c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:56 +0000

    use really march fast shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
