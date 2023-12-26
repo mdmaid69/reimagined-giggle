6c082f09c1fe62fa2ef18b992b8180dbc009c28d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:32:49 +0000

    coat will criticism bang fit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def add_numbers(x, y):
-        return x + y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
