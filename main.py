33b0d9e4c21528f58107236593edbf3715a33e91
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:59 +0000

    anyone polite confusing one top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
