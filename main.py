82bcca75d562f85a38588d2580d44698d489b8d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:15:54 +0000

    better how shake politics love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
