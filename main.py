db9663db3d6003279bd90f6c78950325c5a3c120
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:17:30 +0000

    clear crash ignore quantity bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
