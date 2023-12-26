f7925e792894fc081536ed8a751300039f0539d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:15 +0000

    571enough grow aim climb laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
