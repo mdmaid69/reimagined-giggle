b5944c76e2c2b638515b9c29fa6b7399a87a6ef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:59:58 +0000

    himself believe danger sight relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
