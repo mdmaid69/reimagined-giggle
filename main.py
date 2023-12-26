04d28f2243a90d73ba22b52722b5c2753ad633bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:15:42 +0000

    young to turnback apply workupto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
