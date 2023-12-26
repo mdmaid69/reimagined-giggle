7fb04790465ff710a4309d87a5ac943279a3db96
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:39 +0000

    test steady gift plenty remain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def print_python_version():
-        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
