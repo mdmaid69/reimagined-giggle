f4c2b2cec95c7875bd53c5ea77126e79d0bf0ead
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:06 +0000

    independent book heart upwards run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
