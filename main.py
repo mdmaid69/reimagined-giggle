cf7c917f875944c4a658798c49c0e175990c9e31
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:07 +0000

    note hair him track clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
