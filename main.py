1acc4129dcb9cf35855e9363d529c7df66776357
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:47 +0000

    workupto discovery maybe declare Yy

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_acceleration(speed, time):
-        return speed / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
