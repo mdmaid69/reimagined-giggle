b02a6aad7adb690ce37dd5165e9e3e841ce94958
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:26:38 +0000

    seriously basis most ahead first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
