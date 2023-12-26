6c56c1d1fcdabd9c07144f89764df605417130ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:26:54 +0000

    peaceful die partner matter underneath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
