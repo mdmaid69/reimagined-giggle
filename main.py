565832a5da91451e30b59427acec9cd9f40ed942
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:21 +0000

    agreement die admire laugh slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
