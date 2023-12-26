717265ea1a511ac3c60d3d8a999c317805fb9a50
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:16 +0000

    interview desperate shut match vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import time
-def get_current_time():
-        return time.time()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
