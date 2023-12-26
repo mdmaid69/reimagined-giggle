926ae59548691e74b60ae23c798652f09f589ed7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:13 +0000

    least slow office difference independent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
