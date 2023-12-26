b0a0f750adecabbfe7bf42815f660bbc04780e00
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:39 +0000

    explain rare beginning really tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
