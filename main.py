23b9ea77251d2174df30d8eaf6a1de3c66b64af1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:12 +0000

    wire why stone contents indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_average(lst):
+        return sum(lst) / len(lst)
