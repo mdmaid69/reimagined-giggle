e57e61a899bd77049084fb3a0d201e1d38d5fbac
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:01 +0000

    tone Pp hand mention book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
