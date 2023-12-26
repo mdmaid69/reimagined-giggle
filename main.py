85a49d01ec610248ee70c3ef9bc591a30e954f69
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:23:09 +0000

    claim schedule Cc upstairs instrument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
