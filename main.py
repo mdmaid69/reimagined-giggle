53d757e85a6e27054da32187f871c1cb890008fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:45 +0000

    spring strike class straight used

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
