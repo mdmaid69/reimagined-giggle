33c5f4abf33dbc753221940a0148f6bfba1b38af
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:20 +0000

    person crazy imagine generous fetch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
