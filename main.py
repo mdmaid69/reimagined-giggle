1fb6d5d0a035aacb1da3e82de098a42636c528cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:40 +0000

    payment gather quiet secret pollution

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
