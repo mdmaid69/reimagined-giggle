7eb48e3e1bfc7afa3d15f90262302a89702c46ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:50 +0000

    holdon remove basis switch dear

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
