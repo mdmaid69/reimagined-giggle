0ef0f3611dd79738c6a12d8e751ab3879b093c18
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:55 +0000

    issue interested check makeinto claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
