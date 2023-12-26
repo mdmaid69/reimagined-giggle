a5d07a0bdbd1d963b5a4ce8aa39c5619acc58839
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:05 +0000

    willing achieve alive busy impatient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def reverse_string(s):
+        return s[::-1]
