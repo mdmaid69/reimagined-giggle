9fcb229c93f76883e761fa494f9a40cffc77fc1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:58 +0000

    book promise breathe practical force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_power(work, time):
+        return work / time
