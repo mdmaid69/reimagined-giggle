f2241182288a5fa0f4ef1c9d32a160b3807e0232
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:14 +0000

    expression list sweep pleasure hide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
