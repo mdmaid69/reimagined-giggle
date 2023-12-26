7d519a54cca4a9aa9b1f26891b65053a014c2ca3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:11 +0000

    use respect payment attempt base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
