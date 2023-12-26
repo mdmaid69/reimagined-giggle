0ecd880cb464c581b304b9408da1652efa1fc187
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:58 +0000

    breakin backwards ear sometimes borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def clear_array(array):
+        array *= 0
