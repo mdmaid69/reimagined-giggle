822838eb32d75f3f3a1b81904991b67b47571c7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:30 +0000

    towards Yy accidentally sense I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
