0931340fd6b0ebe3772a50b4357bbf6647ff1266
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:41 +0000

    copy along fast male lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
