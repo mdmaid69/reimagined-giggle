78cf5c4a160113e37c3de94d755bdc73011ad1d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:07 +0000

    valuable add tie rather Itself

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_typecode(array):
+        return array.typecode
