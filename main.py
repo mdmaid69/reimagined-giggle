07c5e90a79191301bfd03cdbd38b79285b60d855
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:14 +0000

    final material sound sweet wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
