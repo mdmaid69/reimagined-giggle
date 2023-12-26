38184524f47a0c0ceaf2bbdef3d9a46a947776c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:41 +0000

    be Bb full autumn instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
