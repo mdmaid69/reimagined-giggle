c3ae7dbd91dc4cc3ef88e14bc01be603bb319b43
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:19 +0000

    year mainly art safe correction

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_str(array):
-        return str(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
