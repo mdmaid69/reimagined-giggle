84ff91cb6f0d826baeb1757d5b9130246c3d9573
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:27 +0000

    operation at advanced criticism tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
