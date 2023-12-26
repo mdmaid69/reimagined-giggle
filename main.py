935e6d24168e5087df2ca813609361129a01740c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:29 +0000

    fault hide amount happen could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
