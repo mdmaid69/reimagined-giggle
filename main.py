fef62707e75e94689c3ee0a6cf4a2052326545be
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:00 +0000

    still reduction excellent advanced theirs

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_min(lst):
+        return min(lst)
+print("Hello, world!")
