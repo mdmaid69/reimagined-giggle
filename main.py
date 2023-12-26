a7819ea9173b0552ccc377b09ef0298db7022a84
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:08 +0000

    door quit hair degree split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
