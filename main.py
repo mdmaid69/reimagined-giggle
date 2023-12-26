5a67c74599516d60e24d6016204f784b745c2bf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:27 +0000

    plenty move land miss occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
