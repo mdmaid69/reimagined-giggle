aec1a8a14ef3ead1eff94760d44c92c8c1117aad
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:05 +0000

    suit waste write exact world

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-for i in range(10): print(i)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
