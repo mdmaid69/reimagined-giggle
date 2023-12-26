ad6e8ade3c56982cd68a6e1404c911f9b7b11690
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:41:29 +0000

    from side breakout obtain entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
