9f5cc14c79ffd99d08594e93c429d9849cd9e745
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:24 +0000

    mark choice aim wet copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
