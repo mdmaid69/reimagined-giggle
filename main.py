3fedaf6af4e7d24a4e963a5f51f67d164b2e62f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:10 +0000

    peace something service makeup backwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
