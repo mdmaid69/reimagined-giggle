ee3323b4b2435138311a15bc40e8f9dde4903629
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:49 +0000

    dare image creature purpose port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
