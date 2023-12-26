a16d3026a986e5d6d82e89a90302da71766523ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:16 +0000

    mean top comment speed perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-from collections import Counter
-print(Counter("hello world"))
+def count_characters(sentence):
+        return len(sentence)
+import time
+def get_time_since_epoch():
+        return time.time()
