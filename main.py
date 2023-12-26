17c2cb11c641353cd12ff009a0db861c283e6f33
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:17:08 +0000

    silent compete reduction start each

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
