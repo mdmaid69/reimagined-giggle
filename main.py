4f53ebc14d38fe87fca818d97362b8e41d38f749
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:33:14 +0000

    satisfied now emphasis pretty total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_list(array):
-        return list(array)
+import random
+print(random.randint(0, 100))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
