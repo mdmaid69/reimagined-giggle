3f24f7c76a0155f701cdcbd8ad33231cbbff102f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:04 +0000

    offer able us ability see

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-for i in range(5):
-        print(i)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
