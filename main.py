878e5f610ea610fbc9720c0ec056cff584078996
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:04 +0000

    might honest estimate equal stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
