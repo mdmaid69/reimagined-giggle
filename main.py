f7a71ec761acc1251b9f806fcaebd3359a29b860
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:44 +0000

    doubt giveback rule fit day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
