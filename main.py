183273b69e7ef2c4fc32c40578589861ff72bf23
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:45 +0000

    someone after fail good hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import collections
+def create_stack():
+        return collections.deque()
