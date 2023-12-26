edfb201d27ced30f4d1349c73b21cb4b6b33a982
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:33 +0000

    opportunity lesson imagination refer runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_list_from_array(array):
+        return array.tolist()
