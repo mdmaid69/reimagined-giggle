522d42d54fa34b113a493fd751d480706b155844
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:57 +0000

    like tie better second for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
