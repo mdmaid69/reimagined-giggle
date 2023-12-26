018fe336835cedc905b4495d008eb34b41122efd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:25 +0000

    married certain passage capital glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_power(work, time):
-        return work / time
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
