1b2a4dfb9e1b514bcd89e25da29a9b585b3bd15d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:08 +0000

    bye basic arm comeover anyone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_bytes_from_array(array):
+        return array.tobytes()
