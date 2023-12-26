5272f3beb2e381a73773c9997d01246e04345075
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:46 +0000

    woman relax undo decide whom

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
