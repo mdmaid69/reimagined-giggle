9ab181101a7260b57a6403bfc12a1943e2fe654e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:05:22 +0000

    pain wide protection formal left

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
