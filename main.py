c8bd57d1a4bea66c0d5fefa956d88321ae607ab8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:57 +0000

    feed march thanks keepaway furniture

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bytes(array):
+        return bytes(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
