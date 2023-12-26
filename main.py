e81b4132c64cd0524f20dd8e05b60071eb5a5364
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:38 +0000

    wire flow amount still getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import sys
+def print_python_version():
+        print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
