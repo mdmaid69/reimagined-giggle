e47bf16d0cc04be0af2c49e6d3a5527faab976f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:22 +0000

    within reason claim file sight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
