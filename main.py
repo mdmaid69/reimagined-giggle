dae7ca2ebe6a9b25eb44200df4dd4a202361e334
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:29:41 +0000

    string odd angry mix ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
