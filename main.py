1022ce5ddb27e3edf4cc7fb1e184123772fc0650
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:36 +0000

    admire shop press market mix

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
