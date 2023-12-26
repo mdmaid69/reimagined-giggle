d7b08efcee634f198a0978d8d851de9f38d9ee95
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:33:27 +0000

    discover actual become takeback guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
