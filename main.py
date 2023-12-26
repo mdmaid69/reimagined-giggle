fd743d8b111ac91207c0483f78d216eb54219cad
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:16 +0000

    near fence lookback(on) dare scare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
