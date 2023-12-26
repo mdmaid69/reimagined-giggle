bb62a9de9acb5a1cd03361607115f13709873179
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:17:00 +0000

    appeal look brain furniture prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
