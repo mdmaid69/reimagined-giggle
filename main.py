1344a1a95a8aac947e3b07a8a590a753dca2eb19
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:37 +0000

    study past unfortunate hit mystery

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
