c505bd4418e666c5949a6f4d1ea015d3050cee6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:24 +0000

    mean software stretch home contribute

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+print("Hello, world!")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
