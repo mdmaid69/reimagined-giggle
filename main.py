2530bdf6c66536fec841604fa52c51cbe22e9023
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:30 +0000

    quickly under freeze release proud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
