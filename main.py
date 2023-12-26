4a7230d3029a4bec49a440d2e6c5285561d82b4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:02 +0000

    grow child perfect apart calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
