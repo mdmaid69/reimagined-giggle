aaa97b5a14632088e5945c021a434bcf3f516405
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:22 +0000

    violent slice feeling ahead hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
