24214e3ffad7a2544984f0d9667a9d32c65299e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:08 +0000

    dangerous fail punishment more march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
