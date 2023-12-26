5208e64dfe125ef5956c0d45b4358ba21ebfa4e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:47 +0000

    add region secret board worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_item(array, i):
+        return array[i]
