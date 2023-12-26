7882fc5dea164d1a0f4770e9be40ae5873cb64d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:03 +0000

    smooth cause sign which pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
