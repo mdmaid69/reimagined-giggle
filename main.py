e79260758a40a0121e0bc5d7aeb54cb915986186
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:26 +0000

    trade little traffic evidence free

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_list(array):
+        return list(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
