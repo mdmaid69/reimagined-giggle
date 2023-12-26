b7c3c4d941af405462597911c084c018bd13263e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:42:55 +0000

    force deep band long keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
