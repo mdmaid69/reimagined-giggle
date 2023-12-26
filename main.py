4862fb0a6574c76e7193fc656cfbd83082abf6f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:42 +0000

    stamp hollow bringback convinced home

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
