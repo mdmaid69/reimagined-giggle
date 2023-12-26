ec7fdcd8e86ef7702c8df4eb7312b0cb3b99ea92
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:57:41 +0000

    mad pronounce accidental side right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
