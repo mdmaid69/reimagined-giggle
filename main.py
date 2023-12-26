c8b00d12805a8c60d0e891e127509834d86b26bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:18 +0000

    offend bury mistake rest ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
