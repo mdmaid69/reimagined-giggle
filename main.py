f77d1c3495420fb7d7c377bc785c3deb79df3773
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:16 +0000

    slight wire wish black then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
