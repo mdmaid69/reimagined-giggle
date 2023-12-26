f54db690d2fffad6854bf6e6dbaf492ba972ec74
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:18 +0000

    dick small chance gap feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
