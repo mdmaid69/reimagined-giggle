68e725d9face38a6da5ed702cfdf6076185ed07f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:08 +0000

    again Dd quantity plain goup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
