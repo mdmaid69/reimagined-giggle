0787fb5a4a897f6658a006a0297911f2564713b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:09:12 +0000

    lookup furniture legal place sorry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-print(datetime.datetime.now())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
