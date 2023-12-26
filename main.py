c104ddb45cc2d322c45a8fd5e346a8549f206adb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:04 +0000

    makeup admit safety high it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
