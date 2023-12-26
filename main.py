140f0c18bf076755227571b81029f5be5d8b3402
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:14 +0000

    match base roof apologizeapologise belief

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
