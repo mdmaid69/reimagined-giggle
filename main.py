220eebed42a3d6802341e37f954a2b7e492287cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:24 +0000

    either left car lead picture

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
