2cceb2dd4743c283eb1cf1084516e08070fe2ced
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:15 +0000

    part view while means injury

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
