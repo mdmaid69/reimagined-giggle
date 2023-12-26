8c96f81d0da0d02ab556711aabb90b2f6f951d20
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:14 +0000

    rich noisy ordinary public appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
