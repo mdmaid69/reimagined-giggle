f4f87b26507a0d3dad318162dcf03534be935387
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:59 +0000

    179bite study there basis pin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
