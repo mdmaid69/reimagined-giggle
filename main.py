fc4bf008ab753786257e55c8c9f1f3d0ec849591
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:43 +0000

    equal risk false time party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_current_directory():
+        return os.getcwd()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
