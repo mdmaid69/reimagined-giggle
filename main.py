84a4629bdda2e2661d343750e02796ea310c5b83
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:07 +0000

    present black break under bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
