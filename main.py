32a1c0a0ead96d75989dbde6f8e07f4eb4a9569e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:17 +0000

    mix hit feed old compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
