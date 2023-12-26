2bf2f908cf3ff72ca0fe9d1f2a4f78673ba7777f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:19 +0000

    shop relative chief glass somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
