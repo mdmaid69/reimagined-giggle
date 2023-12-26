5e9a683b57b90e3e43f08756d436e1c3db70b0b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:17 +0000

    exercise check dry lock send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-print(re.match("h.*o", "hello world"))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_area_rectangle(l, w):
+        return l * w
