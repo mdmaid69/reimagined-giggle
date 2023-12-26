6141c9d44104601143fa1d27dfbbf147a3004f46
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:15:58 +0000

    hungry come life or block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
