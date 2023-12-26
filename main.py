5c7bb0720b40cc4c8b1aa9ba39882e0c7409155a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:23:50 +0000

    hers action surprising Ff character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def append_to_array(array, item):
+        array.append(item)
