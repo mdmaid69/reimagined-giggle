4f63b27f6f8446f85dd5aee7e869df10d015b015
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:29 +0000

    godown effect write thumb major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
