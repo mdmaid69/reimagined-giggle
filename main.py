58dc4a46274d636d98d593a6af335808afbc6050
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:33 +0000

    eye try settle fact use

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
