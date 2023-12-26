5ef12768f5bd3e913cdb590a8a9d5a657a431415
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:04 +0000

    weigh opinion flow wish heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
