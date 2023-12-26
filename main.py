16a9bc78c45c1de7fd648276d19064d110e5c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:41 +0000

    front question OKorokay yours yet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
