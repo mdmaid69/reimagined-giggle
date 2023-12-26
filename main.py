a06c979c7a0b09f381e844440b77645b34df4ddb
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:31 +0000

    rid onto cut any want

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
