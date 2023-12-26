db4bd0e8b1dd6fb004ad335d4930c0c98ec88724
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:40 +0000

    fortunately excited talkoutof respect pinch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-print([x**2 for x in range(10)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
