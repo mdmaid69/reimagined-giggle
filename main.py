5ca4431f2169d2eef8b3864b078b96271abc311e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:10 +0000

    lay container minority immediate consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
