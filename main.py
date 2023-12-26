dca6d5ec2ccefdd5680d1ec8602b7b4789f3c6a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:23 +0000

    new share affect 571enough land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
