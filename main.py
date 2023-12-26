8c750b4ad8bd10ea1d909e85097861eadf5acc58
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:14:42 +0000

    shall seem fix row breakfast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import getpass
+def get_username():
+        return getpass.getuser()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
