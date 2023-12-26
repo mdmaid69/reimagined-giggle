7e22b62b5c166b1e8315c8d080bca254e1d55776
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:21:35 +0000

    main provide final say practise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
