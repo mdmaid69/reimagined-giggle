f048c437dcb70efd88733a143ce2245c5a7b7569
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:27 +0000

    lead among lucky little connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print(sum(range(10)))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
