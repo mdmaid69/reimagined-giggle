27546951d716fffb4d3400362b27f3311662e75a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:20 +0000

    although favourite file 179bite cover

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
