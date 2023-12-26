fa8d831648ecf89180554adb50c7aa011320214a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:49 +0000

    collection usually within guess waste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_item(array, i):
+        return array[i]
