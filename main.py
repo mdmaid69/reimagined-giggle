969c6b71d93f07f4db517b154d01498f48382b66
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:54 +0000

    their considering bother pump film

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
