3b7c7ca900ba3815af7a28cf6477456c2b7515a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:37 +0000

    impressive bill talkback wrong store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def split_path(path):
+        return os.path.split(path)
+def is_odd(n):
+        return n % 2 != 0
