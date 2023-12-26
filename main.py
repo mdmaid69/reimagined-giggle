09b4bfa4aab8489b6eeb10465c4d75567cf67619
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:11 +0000

    practice holdout convenient wish minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
