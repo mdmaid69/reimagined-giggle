32d5bfcb4832451c0a1ccfdd051b9865a2a5b859
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:55 +0000

    niggaacademy care edge burst either

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def find_union(list1, list2):
+        return set(list1) | set(list2)
