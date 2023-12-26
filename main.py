16a6e91621a159997ed88779d81cb0ebf38ce5d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:44:07 +0000

    play rarely free cap turnover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
