de66eb9c8f26c1cb27c02404e68b2901ab3b415f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:37 +0000

    lesson attract company payment professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import random
+print(random.randint(0, 100))
