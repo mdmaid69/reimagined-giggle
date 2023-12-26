31caa44de3064541aba694d9bdc16f114d5ecd5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:20 +0000

    raise my equal skin world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
