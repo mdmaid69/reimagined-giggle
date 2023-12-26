978c0bf6ae236f7ddcb2413d3677eb6115348cd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:41 +0000

    Oo affect programme Ll bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
