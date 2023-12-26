4ae4e988986ab67dd9e1985525d706bef7c0a8d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:56:58 +0000

    shoot decide imagine discussion measure

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
