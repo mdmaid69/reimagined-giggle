09c81ad541adad201d280b5603856cd3fba611e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:49 +0000

    total he address begin on

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
