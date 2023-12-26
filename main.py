59b77abb4e9b0d8347d30a9c62068d0f9a7d6b9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:47 +0000

    history exam all relationship affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
