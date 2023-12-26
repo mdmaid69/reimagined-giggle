de0afda75a813ee37a4b74613164ccf4a6cd20c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:00 +0000

    satisfactory lock trick constantly block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
