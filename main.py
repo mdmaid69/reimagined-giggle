a96962705dcf485bdc18d606b0421f27b91a0bb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:27 +0000

    excited swallow fetch necessary suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_counter():
+        return collections.Counter()
