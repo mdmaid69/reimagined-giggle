4bba85bfe0cf3aef1af6c2436ca00f715cf7d8f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:20 +0000

    turn show trip wave dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
