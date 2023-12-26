45cf09e421564b327961fea1014feadda8312b8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:25 +0000

    store care quick listenfor spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
