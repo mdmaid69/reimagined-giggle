50eff6b2e239d34547080de4eb819a78b91e35b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:48 +0000

    hole delicate 570enjoyable breakthrough freeze

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(a, b):
-        return a + b
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
