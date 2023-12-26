c57954ddabd23451a7aca1daee96325ea064d25e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:36 +0000

    me properly turnup pot extra

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
