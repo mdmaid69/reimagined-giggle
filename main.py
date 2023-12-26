5b88e42d772e611045221c0154036abe6a167035
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:55 +0000

    special artificial woman common reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
