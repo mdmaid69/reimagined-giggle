d1110b7e4c8ed6005d03efbcb119e81d44078e58
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:05 +0000

    message round breakin chat morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
