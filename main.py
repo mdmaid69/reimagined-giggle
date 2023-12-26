4c4c16b6357bed236d8d5630843d68f119ab6608
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:22:06 +0000

    ignore combine town giveaway cough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def reverse_array(array):
+        array.reverse()
