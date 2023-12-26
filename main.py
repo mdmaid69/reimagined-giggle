8e2e9418449d1c649b7d9e7cbaeb17bf5c24292a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:02 +0000

    suggest best want contribute successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
