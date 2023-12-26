2b70b1d142dafb443619b02dc5936ac25cdf05a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:59 +0000

    open claim splash correct wash

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
