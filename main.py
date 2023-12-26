6557283a69d6d545204a6f7d4c2a60266804603d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:38 +0000

    exactly pay realize certain anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import os
+def change_working_directory(path):
+        os.chdir(path)
