1dd68d34ddacfe2ca53669306e86c0cbaee2a0bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:00:45 +0000

    moral determined exact good shallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def find_min(lst):
-        return min(lst)
+def calculate_factorial(n):
+        return math.factorial(n)
