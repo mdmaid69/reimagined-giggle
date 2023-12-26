620cefe33f42be7a93b8cd31b0fb42d5b1be4c64
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:38 +0000

    before even rubbish promise pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
