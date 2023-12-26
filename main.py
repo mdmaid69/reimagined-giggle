675793778a7baa7ce97053297410c1e502d098d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:55 +0000

    break school rob tool wash

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_area(radius):
+        return 3.14 * radius * radius
