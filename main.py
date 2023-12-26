1f7a486fa5a4e69f927da34f76f46f57a2fca55d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:03 +0000

    Ll field underneath each message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
