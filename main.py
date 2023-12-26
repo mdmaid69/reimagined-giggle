cadc9d6eac2eaa2e112ea882bde06dbe1ab655a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:34 +0000

    appeal that science description so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
