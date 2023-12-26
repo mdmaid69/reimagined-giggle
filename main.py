34514ae6d339098e79ee920a464823b48562fe22
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:16 +0000

    normal Kk swing turnup slide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def square_number(x):
-        return x**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
