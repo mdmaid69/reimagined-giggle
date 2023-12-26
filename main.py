719b1ee2b090d524b5b192a05349c159bd9791a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:17 +0000

    type awake closed brush earn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
