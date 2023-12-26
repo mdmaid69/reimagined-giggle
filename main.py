ac87a595614b3ce86a725f71549b79dfa334d968
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:45 +0000

    city bit fat separate shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def clear_array(array):
+        array *= 0
+import time
+def get_current_time():
+        return time.time()
