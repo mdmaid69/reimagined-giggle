e7b7080b8ea026c67c43c9418dcdb7e54e8e6936
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:56 +0000

    spring secret slight children meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+print([x**2 for x in range(10)])
