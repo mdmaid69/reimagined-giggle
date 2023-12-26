3771ede22c8ac9779c1f194b04c2f8fa35824f18
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:37:13 +0000

    road realize how mysterious open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
