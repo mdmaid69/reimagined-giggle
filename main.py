ff5b64b7188f7c0899c4443252bd2e2365ee610a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:23 +0000

    introduce study accept Jj change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def convert_to_hex(n):
-        return hex(n)
+print(sum(range(10)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
