e0ef29378301815003f52f168a7aace0ab0167c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:53 +0000

    rely pool affect to drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
