8a791d93e1a2aa2efd0db7ec43565f230292b225
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:25 +0000

    feeling traffic frame through office

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
