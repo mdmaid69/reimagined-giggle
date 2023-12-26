4e11f014b908e1defc19338bde62e563b06495bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:09 +0000

    dig tragedy takeback break confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
