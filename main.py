078a9df8166bf7c16f763a2bb55e1cd18c3202cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:01:19 +0000

    smart drop break land group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
