e7adbc4157af8943659747888e3002a81f638ff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:46 +0000

    middle share mean fear close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
