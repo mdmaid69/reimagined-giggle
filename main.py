c0ab65dcd5233e2e34483888818128634e85936f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:10:22 +0000

    outside disapprove talk both past

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
