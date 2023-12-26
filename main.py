8e2fd7ce4f49188e0fe07e336c8a8f783d1288e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:32 +0000

    night bit centimetre priority glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
