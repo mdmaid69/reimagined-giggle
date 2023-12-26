d01f73eef4a7e0c4aea4f18d6a4c6906c4956373
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:48 +0000

    Ii point 566end situation sick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
