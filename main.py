6e52b610a5be30ea0afc2023315578a6fd2732b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:29:33 +0000

    like history Dd smile left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def get_today_date():
+        return datetime.date.today()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
