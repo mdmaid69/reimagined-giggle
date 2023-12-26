178fec8c1ce1668c4aee51df47b9accc7adbea32
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:21 +0000

    advantage pass makeup I birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
