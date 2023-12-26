626b8148521faae41bcdc356ab1850e6a0def507
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:04 +0000

    ever service right top comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def sort_list(lst):
+        return sorted(lst)
