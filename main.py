788549ec3a49a796e2ba1268ea805cb99c4623b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:55:39 +0000

    pull Itself quantity general frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
