00fdddf14090a16419ce6149bca44e8fc3761de5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:04:36 +0000

    snow about question language pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
