c0325ac93a99ce0ea33e29848ea4e2f840983982
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:25 +0000

    cover beat straight pen underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def add_numbers(a, b):
-        return a + b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
