c6caf225180b7a7dcf6933728c671951e98b2a8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:55 +0000

    curious church take gas worse

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
