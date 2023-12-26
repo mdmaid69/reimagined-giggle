ca057dec5500f4cdd9f4cb0d6a9d25ceff42c7cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:54:41 +0000

    history Ss runover estimate drive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
