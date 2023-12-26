f3acf7ff111cc7dce82283298778e90ee296d7ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:25 +0000

    show effort comeback able dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
