68633bd9348dfeaaa91666accc3d4cb26bb9facb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:05 +0000

    limit interesting pull sad original

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_work(force, distance):
+        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
