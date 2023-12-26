f9af1846638b67ac425ff45fa810032d922c5ae7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:43 +0000

    pretty body chance terrible kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
