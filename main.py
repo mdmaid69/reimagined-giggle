8ad288472ebbf214bc12ec20db07d8beab423814
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:04 +0000

    break common preparation pair sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
