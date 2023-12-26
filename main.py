622bb12c57eb7205272629be16923d28856b8f7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:38 +0000

    callat cut flight wind except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
