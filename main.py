b5c6a6a7ac0feb7d1c132b62aeae1afb560e4600
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:21:23 +0000

    stare satisfied where care previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
