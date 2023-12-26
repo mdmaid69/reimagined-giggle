cfd11ef026555bb60f5e0f69ccd1873d38ed8118
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:22:14 +0000

    mistake hold upstairs protest runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
