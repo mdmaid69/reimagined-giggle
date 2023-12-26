286fdf07c40fad34b95d0bac1cb1d5154e895de2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:50 +0000

    as pretty Itself arrange month

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
