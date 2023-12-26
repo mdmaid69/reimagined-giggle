954eb68a069b72d31923e8e85093f8a2065acd5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:19 +0000

    deep introduction my regret new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
