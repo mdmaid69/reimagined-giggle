3448bfbb5a023dd934c011169115cfcb50d2837b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:00 +0000

    ring vote most guess description

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
