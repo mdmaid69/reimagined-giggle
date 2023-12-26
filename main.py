c7ae4759968262fcbc42e86db66fae1e2476affd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:04 +0000

    regional holdup reject target journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
