51419773ab79aea50bd87782c67e8dd0d29fa7db
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:21 +0000

    alive neck couple satisfactory trade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
