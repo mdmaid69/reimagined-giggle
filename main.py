770c67bce905a20aca269cba0c185f770e23005e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:26 +0000

    include month instrument exercise face

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
