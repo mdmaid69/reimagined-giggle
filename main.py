ae76ddc48d7545dc9c3b0552ab700b0fb4274c7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:20 +0000

    listen his visit attempt description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-print(sys.version)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
