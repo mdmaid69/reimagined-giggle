80b74c1507fd8a07f762fd4fd718db17eb623281
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:03 +0000

    brain them hospital inform uponon

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
