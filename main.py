e75d21875b9e4808d03797cfad3154deedb3baee
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:19 +0000

    adjective getout may pool main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
