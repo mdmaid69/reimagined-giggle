963bd9f60f26019a69dc89c5ed46e2c4c47a1e15
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:33 +0000

    young school abroad smile wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
 def calculate_polar_to_cartesian_coordinates(r, theta):
         return math.polar((r, theta))
+  import os
+  def get_current_directory():
+        return os.getcwd()
