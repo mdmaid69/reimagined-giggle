34dc3979fb584c913c2b7fc62c6615a1fa0c3cdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:41 +0000

    than runaway guard wind damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import getpass
+def get_username():
+        return getpass.getuser()
