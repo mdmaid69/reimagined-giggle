e3315b6dfb145118b7cc400362c156b83c8df6b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:43 +0000

    part man make fat concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_queue():
+        return collections.deque()
