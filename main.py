4e9630fb12d8e815a5fa6ccd4ca7bb80456fece1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:29 +0000

    tooth spray walk moral prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_volume(length, width, height):
+        return length * width * height
