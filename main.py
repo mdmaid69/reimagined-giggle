24b4e850b98bea0e9516c489f318d31124b388d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:20 +0000

    callout disturbing chin increase relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
