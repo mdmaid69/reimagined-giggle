75981309334ba0fa68fc18d56e572dfd03831afa
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:43 +0000

    central wish crazy result total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+from collections import Counter
+print(Counter("hello world"))
