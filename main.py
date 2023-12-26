ca65ad0fce01f06e4a3633407b984801e4779f85
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:16:47 +0000

    seat shade beginning wave 569enjoy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perpetuity(payment, rate):
+        return payment / rate
