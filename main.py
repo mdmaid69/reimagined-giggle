15a736a01c74342f53cda8309fa9501bfe39bee9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:57 +0000

    fault march organize belief ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
