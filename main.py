ce5a311731fead2ceb701703b19bfc0e77714c0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:33 +0000

    container turnout talkoutof back open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
