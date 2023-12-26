601e14770efd2759151481d1516c98b3a39b24e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:03 +0000

    569enjoy capital lookup throw form

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
