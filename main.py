d83af8a915b782135bf3a47ef521772d547edd64
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:15 +0000

    welcome impressive punish present acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
