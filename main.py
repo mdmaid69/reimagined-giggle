e58a4b24f8cc63c45625cd277be6224ad1f67ed9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:42 +0000

    wrong walk argument hold second

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
