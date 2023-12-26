ac1fb63edb801fed7d55779b5d39ad11ab1649a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:55 +0000

    sound pause fold skirt other

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
