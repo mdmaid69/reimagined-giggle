a6bc4a774e536e71f8f1593f958b18f700f4a1d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:28 +0000

    far trouble breakin talkinto public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
