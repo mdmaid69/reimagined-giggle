977a8eff06a7de40c0d4617b2a9ff19a7d56c2b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:52 +0000

    floor variety estimate cruelty tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
