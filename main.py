5769d7a2ccbe657a1b212ef08b0e82bedca22b09
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:55 +0000

    long ground common Mm burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
