4be83b7eb4e554bc0d532b8fc689e5cf20b3567a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:57 +0000

    realize hard then last hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
