ca21fd80a81423a7462866a2216df1ade236ce93
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:44 +0000

    most always call content start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
