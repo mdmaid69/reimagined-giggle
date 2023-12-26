f693413d1c7a6caf80052938aae80a7ec927d969
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:50 +0000

    complaint getout language godown fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_length(array):
+        return len(array)
