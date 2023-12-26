f20c9d201afe734f74a7eb55775c1af8c0ccb7ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:02 +0000

    poor ball bath bother formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
