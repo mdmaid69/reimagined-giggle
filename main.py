f3e9becc63f85cdd5f875cf4bf03888aa8762d81
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:19 +0000

    hair offer comfort tongue value

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
