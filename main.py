d14a7b5daeb4492fb4228305e7d14ab028d101b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:28 +0000

    careless fresh afternoon touch pretend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import numpy as np
+print(np.array([1, 2, 3]))
