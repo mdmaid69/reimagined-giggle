ccb42f7b1da9d7eefd575f8806d3b743f5298d89
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:40 +0000

    look talkinto tomorrow bar pile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
+def is_odd(n):
+        return n % 2 != 0
