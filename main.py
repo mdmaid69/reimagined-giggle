5453a5da26810088cada5100864140953bdb7118
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:49 +0000

    movement container danger age thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
