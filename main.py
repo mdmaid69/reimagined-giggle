aa005ac47362c291acf2348bac71a689726846a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:18 +0000

    collect sharp record identify further

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
