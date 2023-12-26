d842afe5df19f6a6202f2b9c94c0462a5170c358
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:16 +0000

    future boat classroom deal machine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
