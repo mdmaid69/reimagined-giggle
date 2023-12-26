b999a8e400f91a0c1de50d9b7f86a57fa196abae
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:44 +0000

    mouth wonder laugh circle almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
