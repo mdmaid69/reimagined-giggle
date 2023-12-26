e0f68bfc1c3c5fcd0206af85c09ebb6f7a43c551
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:02 +0000

    free sail recognize limit peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
