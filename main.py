464aed9587e3f43518492a9fa858c5649fe94850
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:49 +0000

    feel hang certain onto find

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_pressure(force, area):
+        return force / area
+print("Hello, world!")
