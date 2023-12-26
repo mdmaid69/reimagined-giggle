2473645a017d38ec71049434b5ceaa6e0a4fb673
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:23 +0000

    interview future rely too degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import collections
-def create_queue():
-        return collections.deque()
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
