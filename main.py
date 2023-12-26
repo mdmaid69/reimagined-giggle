0647a2c2dde6e657dd6d425da136b279454c236a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:58 +0000

    interview tear science pull bang

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
