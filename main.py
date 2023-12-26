606f8ab4e2dc1aecf51ec04b78bd7e034c18b1af
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:06 +0000

    keepout brief urgent honest tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
