cab77fd2eeb0a2fca8bc856b5356fd0643f23a25
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:13:30 +0000

    borrow recently slowly public improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
