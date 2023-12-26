2bf40ca8914967a88ce3bb01778c93e077c06f4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:23 +0000

    scream trouble little boring them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_base_name(path):
+        return os.path.basename(path)
