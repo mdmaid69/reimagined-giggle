3aa682cb7e39a35adeff6dbfdd71e6af582e4b50
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:56 +0000

    callout aim performance lead failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
