a00b078819e1ee79bc6e682111b69e032a6b88ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:48 +0000

    question especially hate quantity geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
