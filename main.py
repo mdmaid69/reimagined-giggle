3900e5cd85d408f11516431932cad5b2da95e809
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:26 +0000

    noisy bury advanced repair lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
