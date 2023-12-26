b9541a80cbd0fb73fe6db04282793c0798c51cc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:45 +0000

    tooth learn unlucky explain sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_pressure(force, area):
+        return force / area
