24c700bb5ecd8597e3ed6f5797d361381c48872d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:23 +0000

    music fire make towards big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
