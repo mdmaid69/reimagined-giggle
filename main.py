c09aa11ed6e6cc2af0cd5717b1d55476165f98c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:29:15 +0000

    getoff this relationship weak lookup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_max(numbers):
+        return max(numbers)
+import sys
+def print_python_version():
+        print(sys.version)
