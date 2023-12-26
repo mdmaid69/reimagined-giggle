a10710903a5f8b4b5a80005deb8a2e90f57c31dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:16:17 +0000

    push event total repeat Pp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
