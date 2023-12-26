74225ad098bc70030a99f94e0e893fc539dd632f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:19 +0000

    push this foot completely false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
