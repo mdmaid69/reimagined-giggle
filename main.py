ef6fa17b3cabce9e67aa7b604af6c2e2b547b097
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:53 +0000

    correct suppose blame remark all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
