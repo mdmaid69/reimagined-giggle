bb8ddca5f822814ad01222be6e35dd1f5fcce5ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:09:58 +0000

    matter discovery mine instrument young

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
