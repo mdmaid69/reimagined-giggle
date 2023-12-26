82f1406d17e4a3e20ab18002f8aa3ba2f3361e7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:04:08 +0000

    brave extra spite small turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
