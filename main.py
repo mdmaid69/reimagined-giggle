0aa1d0beda64816d50f3b55d08280e2f19440e39
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:36 +0000

    middle include low interview breakout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_max(lst):
+        return max(lst)
