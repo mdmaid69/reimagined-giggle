fb68b1fae74f6c0ea051e0ed05d7baa6a9b7275b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:02:52 +0000

    nigga collect talkinto 564encourage whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
