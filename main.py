d4a0d5e220e2d9e6297604ac16894da8918e72ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:11 +0000

    between stiff offence need translate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
