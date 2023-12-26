dc34a60b3f0b944a2f06f2c0c1bf0141f9aeb975
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:10:26 +0000

    prevent imagination mouth summer emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
