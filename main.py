02ae5d269337c9e8257f8e2d62d3946e2735c257
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:40 +0000

    fortunately many inside dead float

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def subtract_numbers(x, y):
-        return x - y
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_set(array):
+        return set(array)
