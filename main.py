267c082f4a7c17063b7cbbd9f6ca0c634c6bb7e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:13 +0000

    connection special herself secret preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
