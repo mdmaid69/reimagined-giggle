6d6f757d76ff11f44086d670152def3ecbcab231
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:51 +0000

    callout cut rule still holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
