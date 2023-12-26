18ea971ec0f3625d3d2456c0de9fdb60f162c6f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:04 +0000

    age perfect whisper sticky teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
