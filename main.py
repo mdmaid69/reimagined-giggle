a22d2c8814ae55e733b92f40572b0fc74a2ce43b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:55 +0000

    platform run capital available 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
