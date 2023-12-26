73e4621e0f9a3ac0b05fabd59c101a5f3c78e9e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:41 +0000

    declaration impressive awake brain sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
