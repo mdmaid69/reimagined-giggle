07f29be88347f47d6ee4ced8599db34d9ff06766
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:35 +0000

    sight violence turnback judge but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
