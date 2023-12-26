75611282b8edb1eb292402d7b1aa90d3d09b8f94
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:24 +0000

    else limit threaten comefrom tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
