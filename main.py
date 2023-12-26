186231c93e0d836759d460658eb7ef9744d9c3f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:30 +0000

    style true beside imagination moment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
