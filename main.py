dfec5e047560157e031b3710c902b90656bad9e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:01:55 +0000

    hope hurt left both passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
