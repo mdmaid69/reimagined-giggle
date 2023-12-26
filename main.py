b7f13e1002f9829526bc74e1ffa56f44cde1439e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:46 +0000

    brave welcome repair gentle cap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
