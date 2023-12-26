861daac8dc55940eaf02434e94bb3d008574df1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:27 +0000

    partner identify tend discussion snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
