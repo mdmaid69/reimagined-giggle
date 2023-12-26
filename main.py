f423ad26aa58407bd3aba9451146e0b375d05b6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:13 +0000

    bear after sky clothes true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+name = "Python"
+print("Hello,", name)
