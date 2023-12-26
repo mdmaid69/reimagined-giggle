7625b44ced1eb4d127111749d887a3806d8b01f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:43 +0000

    fortunately base type paper develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
