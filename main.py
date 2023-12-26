1b93c12ffb757b51329d1704db9c3bf38cd68102
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:48 +0000

    nonsense feeling by reaction practice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
