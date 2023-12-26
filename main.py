6118d44c636762b70ec72ba984c965b9dc5e6269
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:15:55 +0000

    desperate fold limit for silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+def print_python_version():
+        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
