c92fdcd0d56dc7f145fae2a6ce6fe21a023a0e4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:01 +0000

    risk skill contain our take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
