d209563799186c161168b8b3105a4f519e362ba1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:24 +0000

    listenfor necessary turnaway nerve suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
