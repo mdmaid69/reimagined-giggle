aeea44a926e723523dfc83851cbd2ae359916c75
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:23 +0000

    refer wide sister their mood

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
