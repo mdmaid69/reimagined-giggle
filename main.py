e0d6e65c342b1d4f7ba3822ca235f4b01564bbbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:31 +0000

    education take go sister offence

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def cube_number(x):
+        return x**3
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
