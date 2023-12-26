7722c76d00dd787e89a1bcf3b94bab2352d142a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:12 +0000

    dear block unhappy first standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
