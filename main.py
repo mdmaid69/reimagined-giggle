bb734b8c8506ab3ad58918c392ce0ec7540f450f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:20 +0000

    independent work sister talkback Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
