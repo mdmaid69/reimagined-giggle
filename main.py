84ad1daabc26a4cc543d440594bd595918057477
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:09 +0000

    possession mixture snow joke upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
