0a69ab8a0dae40e0896b21daea27b1d0639708c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:24 +0000

    fit class passenger film scream

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
