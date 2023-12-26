797bea1316ff8222a016bd2689452d9e868f4332
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:39 +0000

    heart operate trust confident basic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
