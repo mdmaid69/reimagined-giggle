99ee3d447b36e81e8480ba29c05fc69cf3c92824
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:23:42 +0000

    whether safety charge genuine fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
