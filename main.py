10762dd389f95b9ac40b0e47c1d215ff53586cb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:54:18 +0000

    against criticize Yy pretty comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
