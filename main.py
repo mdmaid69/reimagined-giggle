747489285279e1d9da94b20deea8625b0d3da533
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:46 +0000

    kind interruption ready price bad

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
