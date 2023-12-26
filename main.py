3b86b4b378162aa843c21144941f49927d2efed4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:35 +0000

    to hand sticky suggestion however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
