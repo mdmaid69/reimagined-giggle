efb7fcbeea4433083814cd2126c2d6a380e21f26
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:08 +0000

    path strange choice Rr slide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 def convert_to_octal(n):
         return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
