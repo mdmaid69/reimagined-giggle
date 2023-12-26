662aad53a433c9f64fd786e3b592bd3162f280ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:18 +0000

    likely movement row thing track

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def convert_to_binary(n):
+        return bin(n)
