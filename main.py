173a553965a3c442d1b859caae11ccadd3eee058
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:30 +0000

    furniture act runinto threaten pair

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
