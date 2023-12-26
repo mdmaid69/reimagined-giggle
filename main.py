290e53d2d16cb4b2eb7dc801576bbbbdf8a9db64
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:18:13 +0000

    funny so sell speak cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
