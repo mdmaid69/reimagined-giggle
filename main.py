aac0ea8bea5f24ec3e036a3b3e5656df2a3f6f91
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:10 +0000

    getup warning praise show capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
