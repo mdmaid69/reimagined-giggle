4db5f97ec314fcff0192403979efe9e6cd041b01
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:27 +0000

    anybody fierce wander owe near

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
