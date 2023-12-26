c6c8fc7e96b7a053ef67f026e332937949689bfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:36 +0000

    party build trade mixture too

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
