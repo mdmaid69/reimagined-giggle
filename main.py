64615e30bf766ac51ae1ca95872a63475088c507
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:50 +0000

    doubt hungry information progress business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
