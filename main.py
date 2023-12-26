e4ff949605b3a70f563a437529387bb7eed9ee28
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:44 +0000

    next correct consist tread inside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
