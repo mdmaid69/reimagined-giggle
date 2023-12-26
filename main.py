0ae7480215cd9f1fa50da6f3c0b4569cf4804ec1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:56 +0000

    Ii consist collection tone handle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
