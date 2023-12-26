72eb8e13c3e19d725e2ec1737c7f8540b790908a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:07 +0000

    blame copy again disappointed too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
