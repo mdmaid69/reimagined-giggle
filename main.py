80d211c503097639003efe0ee78e76031dcc6705
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:32 +0000

    string term pure demonstrate formal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
