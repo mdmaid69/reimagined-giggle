b8d6ca99afdce7d70cf503ebed8a673cca579da7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:15 +0000

    talkback free battery band evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
