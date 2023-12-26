ac12492bfd1d2b10b6f58f13e2c0b79d758f9057
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:22 +0000

    lesson division piece interesting record

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        return sys.version
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
