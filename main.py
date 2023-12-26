c14121800ef2be3c78a8ff48c088806dd20aa4c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:15 +0000

    able trial ride food question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
