6fd9c44b06ff055e1b4ba59f8c8a9fec8e87a009
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:58 +0000

    consist interfere comeround avoid everybody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
