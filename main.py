00a697ae51e3da17b77148b2a4456b1b71b1480f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:24 +0000

    favour ourselves repeat worst trial

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def greet(name):
+        print(f"Hello, {name}!")
