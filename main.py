6cb07245d5d2e5aa5510b3c7123c97d6db778a11
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:46 +0000

    trap cry table hope backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_power(work, time):
+        return work / time
