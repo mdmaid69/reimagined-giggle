2b437d3b74833b204c50c38e964fc3b79b758d53
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:36 +0000

    neither detail inform ordinary art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
