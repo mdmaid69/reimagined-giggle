d0846c52e13916ad72eff6c07060c3ec5ed64bb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:23 +0000

    star there pronunciation accident mean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_sign(x):
+        return math.copysign(1, x)
