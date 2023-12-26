b7c64c42951bc933fc81722faabf8f3687e39e3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:09 +0000

    juice under search recommend terrible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
