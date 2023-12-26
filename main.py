738b9bc5854d55e67f6b411430f38c3ef82bf301
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:35 +0000

    remember about treatment bringforward dear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import datetime
+print(datetime.datetime.now())
+import sys
+print(sys.version)
