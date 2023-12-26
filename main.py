22ad91c81080187f53e78fe74c31ed5a8dfa31be
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:43 +0000

    nonsense cough open send commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
