98175d9b72b73ef1f180883f0da8cf18b72c91ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:08 +0000

    once software ill correct dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
