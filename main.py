fc87730cad09263b85b4e424008e8efe7d558076
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:41 +0000

    common vanish fill mainly pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
