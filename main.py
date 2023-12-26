659299f8d201d6dbfd081d2e14d7bf025c8af145
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:25 +0000

    attitude workupto experience term cost

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-print(time.time())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_area_circle(r):
+        return 3.14 * r**2
