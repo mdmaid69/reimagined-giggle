6d57ff0c34e5fb88cfc2eec4121d165372f6aa12
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:29 +0000

    weather name theirs tread murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
