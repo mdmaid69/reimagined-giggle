0442a44f8f1a55ec373bfa0f9b2d2edb6c68b697
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:41 +0000

    on congratulate sharp develop connected

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
