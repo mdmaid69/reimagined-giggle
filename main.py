f0b582b1369d1c657da6c054133716e7c66bb538
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:55 +0000

    565end business lonely appear get

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
