bde4b5fded4da0d6c6f8fb48abeb93f4df010128
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:59:14 +0000

    responsible tune far unusual fresh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-name = "Python"
-print("Hello,", name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
