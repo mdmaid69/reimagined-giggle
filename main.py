bcae9802cc3ce9ba8bce1170460941793318755a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:57 +0000

    same notice centimetre soak solve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+print([x**2 for x in range(10)])
