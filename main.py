99baff7d531c3672df18e6c8466147fc2856cbbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:57 +0000

    wash objection Yy exit past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_set(array):
+        return set(array)
