0d89da205c3c307d426724f47e03637ebf392f45
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:04 +0000

    shout harm deep adjective meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_min(numbers):
+        return min(numbers)
