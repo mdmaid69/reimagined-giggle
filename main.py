8aaf1953051ae768fe58a8a8a4ff607cd902e4dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:50 +0000

    impatient positive yet brain support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import tempfile
 def create_temp_file():
         return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
