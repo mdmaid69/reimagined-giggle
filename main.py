bb6280f38cab563a598d4aaeb559c162b8aacd4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:48:40 +0000

    alone our front sing practical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.time()
