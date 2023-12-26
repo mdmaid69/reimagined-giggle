b26291c0f4173ea273740fd7d6cd04bc8c05f96b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:28 +0000

    569enjoy underneath way corner rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
