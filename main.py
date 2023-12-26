5040222fb968149793b261967f6d933ff8eb66c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:05 +0000

    thinkthrough description pay so someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
