2d0e852529e0dcf4be73243f49c927af07fb2541
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:53 +0000

    excellent everyone remember bridge warn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
