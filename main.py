4f50f966e1c31660b32eb6ca9f53b24338d02674
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:59 +0000

    complete must agreement inside false

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def is_odd(n):
+        return n % 2 != 0
