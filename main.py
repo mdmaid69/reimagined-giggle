00ec4e25d7c3465be24f762d031341ec0ff988e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:13 +0000

    able lonely soil now summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
