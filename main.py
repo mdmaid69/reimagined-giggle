25f9196b5767dd2d44bb7500ed4ea42a5b9370a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:53 +0000

    otherwise return exist hour build

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
