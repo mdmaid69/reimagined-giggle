93fc8166f52422984a821f11d8322f95e8ba22b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:30 +0000

    later delicate pretty appearance public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
