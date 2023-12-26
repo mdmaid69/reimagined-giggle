ff22744b2eec4f58831f17c951752e5ec1ecb90e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:33 +0000

    doubt think shout release shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
