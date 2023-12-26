f12de2c178b05eefd942c46222010da3a4b93599
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:59 +0000

    responsibility state advantage welcome related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
