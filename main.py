5d22066a758f793bd689eb8018f5283cdd86013d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:02 +0000

    picture lookaround average constantly base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
