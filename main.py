54e5b79c6dd1dec8fc78bb23ff1f0f4bd1a32914
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:41 +0000

    cook passage game justice getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
