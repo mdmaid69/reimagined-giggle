6b3cc2174f72944f8e2218150bac9bd3b2aaa6c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:17 +0000

    fresh party computer bringforward confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
