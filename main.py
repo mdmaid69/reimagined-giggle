6719a71809b58bba747b97f4fa21c88f956d3111
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:19 +0000

    purpose fork plant belong must

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
