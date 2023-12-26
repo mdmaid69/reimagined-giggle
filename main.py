e45efe69a4553d49b72e214d45a821643abbda99
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:20:06 +0000

    rid soak greeting he alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
