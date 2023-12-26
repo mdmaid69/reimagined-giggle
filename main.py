c8de321281e374d6accdc460cee9a47bbb52509a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:57 +0000

    connection hope level climb part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
