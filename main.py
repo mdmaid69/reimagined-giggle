a60b0e4da356cb68d903b6a494e399e801c3252b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:55 +0000

    large shrink unlikely part runinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
