adf30d1826325971e5593ae06d1ada5be589e106
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:00 +0000

    already check bang apologizeapologise runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
