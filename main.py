22c1fcb039bf840318bbecffa335da5f44d4dc5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:35 +0000

    myself hope disagree purpose tape

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_index(array, item):
+        return array.index(item)
