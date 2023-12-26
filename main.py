e24b5ceaa65c06116db26993e07257711351a38e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:55:04 +0000

    impossible danger sell set plenty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
