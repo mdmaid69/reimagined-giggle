59e86fc2a3d519c1721b5c9dd8f1182775e32b45
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:22:29 +0000

    impressive speech pool mind top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
