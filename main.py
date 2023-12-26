2559e733de1abfed45af9bcd09a071040ecf0570
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:20 +0000

    underneath sea light still through

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
