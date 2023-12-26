0e0c7ae191812ed19ac9821965826526e3cae28a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:01 +0000

    nature stare crazy post when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
