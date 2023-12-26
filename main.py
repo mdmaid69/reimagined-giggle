6886968a24b75071763e34a93d74746b14f1cf1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:06 +0000

    question vote keepfrom world successful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
