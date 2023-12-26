aac3f280ef6db19f73d9b526cb1fbc8b41e2b39e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:12 +0000

    argument correct plenty through during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
