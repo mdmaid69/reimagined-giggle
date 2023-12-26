d50836e0da60bd55102813c559c5ffd543ec33ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:53 +0000

    improve callat determined tool chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
