0d89cc23dc9218d02b15a9030bc8e9fb228ab306
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:51 +0000

    world now thinkover who friendly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
