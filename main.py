ea1a7d2edbeee3d00444ec40f6b0ebd0ba583e4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:02 +0000

    hour breakinto below along also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
