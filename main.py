45f3f65ee4eb4a9937b96d22ac3531b362facbdb
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:44 +0000

    original shelter left want belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
