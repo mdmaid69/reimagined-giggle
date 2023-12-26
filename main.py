a1846fad21a310939f798eb45ab5296157aa2486
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:08 +0000

    wake runout disappointed passage destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
