c976900f5b793e2e854a453a1e75c7eec6a04c99
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:15 +0000

    surprising some runinto married better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
