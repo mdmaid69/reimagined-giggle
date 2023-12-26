78b0c56c9a118c4bb3f47808ec6486c652177b56
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:50:16 +0000

    waste want definite position forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
