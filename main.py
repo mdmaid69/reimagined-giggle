2dd1dde56ad6249e98e6c93ff5dab4577baef7fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:58 +0000

    rubbish pain disagree gradually mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def append_to_array(array, item):
+        array.append(item)
