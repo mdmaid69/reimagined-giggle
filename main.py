898d432e69a5616b6f5c326a585adbecc0d243af
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:46 +0000

    justice community beautiful secret abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
