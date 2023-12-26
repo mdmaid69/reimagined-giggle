c5088ce253cd5c190e073a8b507c0374b29321c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:47 +0000

    transparent serious cum smile cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
