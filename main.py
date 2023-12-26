980feffd0ee1b6150876755940973a6a1dbd85ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:52 +0000

    sport our head neat except

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
