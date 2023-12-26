133f28a0ea7b233ed4367146e73ffb3ab94b0419
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:02:45 +0000

    luck now fasten fetch maximum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
