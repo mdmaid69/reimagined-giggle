4924534687f0e96e4d8822ed10890d0512722604
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:45 +0000

    since impressive bus common downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+from collections import Counter
+print(Counter("hello world"))
