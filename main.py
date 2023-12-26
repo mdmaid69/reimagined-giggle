8566b722ec9d193749e9fb0cf618d75bf052a671
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:51 +0000

    once responsible trouble forever half

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
