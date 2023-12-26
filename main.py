c0ae8177693c93c01b90e667493a7b9308188306
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:49 +0000

    used Qq hour row definite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
