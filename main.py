3171c3d02dbc69ea6a2023dd116a2355948150e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:43 +0000

    wheel rest answer through swim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
