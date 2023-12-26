fab9703f53826f470ac273facb8978fcdc5aa50d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:33 +0000

    woman somebody favour turnaway copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_set(array):
+        return set(array)
