a258ec4cf40db7fb53d4950233f2a294e49283af
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:39 +0000

    already fine uniform phone action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
