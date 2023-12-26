c56e367a36fbeafe8cd7448dc0d3248be4fd8369
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:08:33 +0000

    team drive me herself for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
