51ed0083abb9806d145a291c09fe849b5b68e968
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:01 +0000

    bag less glad judge instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
