bee53f1fefc0951cede85dc1debc5658d1dc491e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:06 +0000

    judge which talk favour frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
