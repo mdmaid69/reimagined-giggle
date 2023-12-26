5175e0c123bd6c44d4e3f1f6e6b09f419ca6d92d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:15 +0000

    damp talkoutof at definite real

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
