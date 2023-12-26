a5b1211cfd1f07b71491b09faff02699686d0147
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:39 +0000

    doubt free transport slow rate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_index(array, item):
+        return array.index(item)
