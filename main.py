45a5da9f13e499cfb82c92f31923958850952c5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:26:22 +0000

    workupto probably somehow better produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
