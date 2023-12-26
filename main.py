71b60349423bb5bee247967cc81efafa6bdc4e88
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:58 +0000

    anywhere peace nose escape greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
