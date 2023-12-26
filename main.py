d73983855b3369cf0305594d5ecc8565131c4c2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:11 +0000

    depend stop pool history appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
