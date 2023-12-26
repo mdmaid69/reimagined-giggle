22b3c492c130e25ad95e269bd55f4cbc934779a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:39 +0000

    recover main scene correct manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
