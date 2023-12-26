83641d381cce81ae4004cc0a76e36d4e088f8a30
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:53 +0000

    only moment number office variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_power(work, time):
-        return work / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
