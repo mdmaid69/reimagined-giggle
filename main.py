5ea108847ee49142c9ab8abbcb7652f1dd4649d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:01 +0000

    consist estimate likely mixture brain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
