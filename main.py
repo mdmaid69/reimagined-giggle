17516b0ff6483f2bd201f605d0710bf344a81119
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:29 +0000

    left refuse beneath detail marry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+print(os.getcwd())
+def add_numbers(x, y):
+        return x + y
