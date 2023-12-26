e8e91aa1497b1aa45e0fafccf82bbf5c5ef1808a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:54 +0000

    bringforward bury later slow onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
