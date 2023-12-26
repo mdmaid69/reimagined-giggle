9eb78fc93cef928734740ae747fde5f99f2eb1c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:13 +0000

    street dance name owe mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
