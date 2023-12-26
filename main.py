822ac0be8291659c6eca9d29db7e82ae08aecebd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:03:22 +0000

    equipment turn matter most finally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
