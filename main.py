0bd6d3b574e3be4f68418c94b0c4d14c9f00d035
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:32 +0000

    land possibly production we national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
