12acc1b6a87c719e6595cf414c22e0ff2897af13
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:50 +0000

    release up truth pain high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
