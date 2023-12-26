8dc6e5e2d5c9df5f47ddc493546491dc5e538476
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:59 +0000

    involve reject no stranger early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
