6e5c1b7d2565e8bd18de1dfb049522db8e92db6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:38 +0000

    nor terrible on year represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
