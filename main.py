f6a3b3a9f48867d4bfffd213085e0e7098a68063
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:52 +0000

    profit comment downstairs right Gg

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_pressure(force, area):
-        return force / area
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
