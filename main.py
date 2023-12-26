cb78ff31c6f26277b3b946b2c0a201c43eac2794
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:56:50 +0000

    rare perform shake dry will

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_octal(n):
+        return oct(n)
