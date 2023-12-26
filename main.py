19f2443e54537bbd4a39759a65d5a7139fbf6e3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:28:51 +0000

    progress substance board reserve list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
