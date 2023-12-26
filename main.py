c014cd9d796410a071a9bc4094009f21fba25221
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:25 +0000

    fire trust cinema section Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
