8b5c7d81f51f90534639203e377798f343742c62
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:03 +0000

    bring list estimate doubt confused

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
