9fca542ec049aed1c12645505abb26333d40afde
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:49 +0000

    excuse regret well like together

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
