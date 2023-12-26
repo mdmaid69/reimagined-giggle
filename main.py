1bc6aa51c80cc2a72c04d285bf42b3b33a8d03be
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:54 +0000

    match year undo meet sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def clear_array(array):
-        array *= 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_density(mass, volume):
+        return mass / volume
