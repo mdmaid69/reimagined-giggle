f87ee6d58af89f42fb14c60c7e1354b9f7c75aec
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:57 +0000

    provide under comefrom side grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
