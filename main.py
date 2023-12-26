53c727ab84b5be2d9a457b3d1e742f68517dcb37
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:41 +0000

    escape fast regret under interruption

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_item(array, i):
+        return array[i]
