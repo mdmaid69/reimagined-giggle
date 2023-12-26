32e7375bacbd188a4a74961704404ea835a76f64
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:59 +0000

    under television morning in language

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
