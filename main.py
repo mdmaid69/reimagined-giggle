64389184859bf7255ce9cc68739b65a94278edd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:12 +0000

    muscle turnoff cut pupil peace

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+i = 0
+while i < 5:
+        print(i)
+        i += 1
