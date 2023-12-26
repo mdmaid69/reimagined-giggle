cbca78b9dcffdafaaa7af5ec2c69597f6bd0d232
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:18 +0000

    correction public equipment your share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import sys
+def print_python_version():
+        print(sys.version)
