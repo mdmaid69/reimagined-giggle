2284441dc752263bcbc6f26fe7fc74538a3d6d88
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:17 +0000

    admire development along case because

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
