fcf0d8f39f5c467c9324e2d22932d50dc4eb7117
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:56 +0000

    happen unusual fall yourself crazy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
