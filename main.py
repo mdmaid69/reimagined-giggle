d85c584c3700f18396f9b7ae7ca7fb0cae5f9e45
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:48 +0000

    plenty spray comeover city later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
