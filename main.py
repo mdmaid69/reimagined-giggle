a3214515347a44f708d4ad07761505063fa2a861
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:21 +0000

    promise forgive come hair thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
