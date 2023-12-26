91288615241c4133528d356e66ca68cac90496be
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:46 +0000

    zero surprising warning case laugh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
