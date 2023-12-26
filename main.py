90fe221b1a08ed3712c12a5cf95d97720b6ac5f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:59:12 +0000

    getout related tough forgive statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
