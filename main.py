58c44ba5e67ad0e5b2709cff3391c47a9804653e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:05:49 +0000

    clear feeling relax alone well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
