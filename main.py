344437b6cc790ef272372b035bf33c7935de9ae4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:57:10 +0000

    miss cause still record attach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
