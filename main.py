76045ee894af7b18cddbc6e2417ca0d465fb83ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:14:21 +0000

    survive female there among hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
