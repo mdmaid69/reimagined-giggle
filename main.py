d11443316499c80768e0d7b9e5d5b591cc4a9aea
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:45 +0000

    history other peace character angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
