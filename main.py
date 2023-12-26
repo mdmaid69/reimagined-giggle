39cb88ea32c7e8910bc16732b47feadce68e88ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:54 +0000

    backwards light dish issue content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
