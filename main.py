86f56a242248a6f61c18c4f78485801b875ff81d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:33 +0000

    advise later mainly usually platform

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
