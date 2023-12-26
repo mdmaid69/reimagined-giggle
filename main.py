d226f031d79cada817af773274baa60353c94561
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:44 +0000

    brief getup pick detail concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
