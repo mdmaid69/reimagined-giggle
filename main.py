d27a31618e1eba45c5c7f87f5b7870b48ea08f7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:06 +0000

    loose could translate attempt worse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
