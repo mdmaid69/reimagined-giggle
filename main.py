8d6b93b3ef2b683396135081f50d92cf8ee5eea8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:12 +0000

    past 566end high nor surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
