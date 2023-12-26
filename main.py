24da0b0361b3c21ae11bbab5a2235bf40a49ae1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:08 +0000

    565end communicate wonder plan interruption

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import platform
+def get_python_version():
+        return platform.python_version()
