c003cbf0ce37e1cc0c53e4880a7c01eb1126b353
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:45 +0000

    bringup push throw remove off

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_list(array):
-        return list(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def get_string_from_array(array):
+        return array.tobytes()
