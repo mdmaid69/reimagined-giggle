cd668fdac31e3170ebdc854070e62bf508bc03be
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:43 +0000

    wander moon than worst control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_odd(n):
+        return n % 2 != 0
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
