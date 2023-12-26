84d4acd9bab4d4fc631a6db30961629dd5076159
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:21 +0000

    suckme organization correct freedom nervous

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def reverse_list(lst):
+        return lst[::-1]
