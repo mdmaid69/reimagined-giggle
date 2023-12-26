89ccce3f6d4a3ad38b32b6274c2bc9afc7c118e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:07 +0000

    plenty taste rank wear project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
