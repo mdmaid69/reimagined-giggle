a2108c43d54804945ed2bb363de6ebd102be2d02
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:51 +0000

    last answer concern increase control

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
