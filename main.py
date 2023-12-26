23f449a98554a1c23f1cb8ee2d66d6341ffb35a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:58 +0000

    thankyou that shall exhibition keep

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def sort_numbers(numbers):
+        return sorted(numbers)
