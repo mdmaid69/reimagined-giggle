9ec50cabe159addf3db2dbb4ca97c9e457cb0c1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:46 +0000

    dark once load size fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
