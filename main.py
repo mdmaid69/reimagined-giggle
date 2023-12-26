3b24da1a5ba1e1e21dde9fd8fca246f516dfc6f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:55:37 +0000

    place suit increase health work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
