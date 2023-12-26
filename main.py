f869fcd410d5f827a213d8f70f4449a0e3778c18
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:58 +0000

    side near support undo save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
