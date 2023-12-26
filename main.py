8dda46c68a9670aadb31a112c445da793fc7c324
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:10 +0000

    perform press party gooff failure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
