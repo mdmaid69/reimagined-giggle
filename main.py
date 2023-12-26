2d7bcbb60ec9ee848a01686100369ff633c1edad
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:30 +0000

    fine wear pause violent label

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
