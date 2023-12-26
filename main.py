c742317239775a9f11ddcd6c870f0a3f59021786
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:45 +0000

    narrow rough breakinto card plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
