2bde8d1edf1352755ea66207fd23e1ba4205b2f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:09:46 +0000

    simple step main keep sudden

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
