31024d0fc4ffd58108d18124265a739bd37279cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:27 +0000

    track extra failure play back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
