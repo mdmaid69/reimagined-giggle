14340f89a0a8267970d9159e566724fe23e92d54
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:21:25 +0000

    tune by fence quickly increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
