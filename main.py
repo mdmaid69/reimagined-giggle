67dd96cc5f4ffbe222672d260b3e01861c72ad84
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:31 +0000

    extremely mad shine part city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
