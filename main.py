8a290b5a285abb03719c593ad90f5d462322f985
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:11 +0000

    freeze partner mother risk live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
