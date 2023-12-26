32bedc02c6880db5c5af8930fafa77dd5d4e9861
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:50 +0000

    Gg find quickly church vote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
