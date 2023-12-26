2a0940188c8a9063b39a84beedb18e80193a46c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:02 +0000

    wide feed takeback shadow support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_min(lst):
+        return min(lst)
