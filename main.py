eb3598f28a8bf008dddc9d15d8c0a2f55b573fb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:19 +0000

    discuss brain skirt turnover turnback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def cube_number(x):
+        return x**3
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
