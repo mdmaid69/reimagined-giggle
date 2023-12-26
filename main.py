cba93c7c7ece1f573766d4946c44226a84b68af2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:40 +0000

    detail bath culture wealth broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
