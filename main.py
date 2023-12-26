e4a262c11bd11872a7a4ac70243a78b7a5afe1d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:29 +0000

    stranger follow tense before warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
