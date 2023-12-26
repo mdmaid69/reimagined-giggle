64510993e54940a35de311105c693fc4ed09933a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:32 +0000

    children trick dream pause change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
