0602cb85975421febbd7da9a6222fc781c055e7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:50 +0000

    bear arrangement nervous physical right

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_min(lst):
+        return min(lst)
