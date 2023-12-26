cccc7d56d25785c01a163b1fa8d637ac416dcf66
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:14:02 +0000

    wish original contact shout we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
