c993a412098ecc078727522aa95f621dd7633fcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:07 +0000

    visit position band claim deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_length(array):
+        return len(array)
