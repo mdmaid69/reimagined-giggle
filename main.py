499f9bc5e6e7fd1250bb4d1aee2321bb7b577c14
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:57 +0000

    look lock under worse cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def count_characters(sentence):
+        return len(sentence)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
