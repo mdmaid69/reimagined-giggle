14342fac637b5826f66b3a3c441ccdbabde6a00f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:31 +0000

    badly respect uniform correct opponent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+print(math.pi)
