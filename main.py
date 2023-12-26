7bf915448c0801074b24ec4fd13d7fe84c065c51
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:50 +0000

    outside responsibility active future matter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print([x**2 for x in range(10)])
