bb007e261dc620a7d69d21013510cc6b4bb3c7f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:50 +0000

    related emphasis walk transport edge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_str(array):
+        return str(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
