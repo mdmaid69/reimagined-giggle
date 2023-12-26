677ff9d92edf9a0d9d478ccaf59ef4d25655803e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:48 +0000

    getback bus object mess Ii

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
