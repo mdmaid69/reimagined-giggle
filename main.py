db2d1ce83da992687e35a6c687ddd82fb8f8fd59
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:17 +0000

    grade arm judge mark take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
