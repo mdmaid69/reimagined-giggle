d207c181fd5846da81ebd60702e6cc03609fa1b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:31 +0000

    national distant mysterious afternoon shoulder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
