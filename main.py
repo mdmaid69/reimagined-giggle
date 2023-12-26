93bb165c23b93154c57d354ae72e218e294f1788
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:42 +0000

    watch bus lock leave squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
