fd20d733bb88951a5bfd7e18f4319302dc206d5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:59 +0000

    wire usual exaggerate day tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def cube_number(x):
-        return x**3
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
