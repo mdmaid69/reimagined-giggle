e3af2a6fa2e26e478d4dc15ddf8c2b66eaf64b05
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:44 +0000

    regularly explain journey calloff goodbye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
