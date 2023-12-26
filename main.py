1918dd2b19d86911b792e59fcecdcd400bf80ab5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:24 +0000

    game press instruction form unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_array_to_string(array):
+        return array.tostring()
