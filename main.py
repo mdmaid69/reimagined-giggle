f6ef1b4a0e50bb4abed9ce1139a04bee32c9ec67
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:20:23 +0000

    lead comment purpose away food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
