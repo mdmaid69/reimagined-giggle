6bd87f7bc2c951f9c32c4d63ce71f9241e103409
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:01 +0000

    perform Zz want wash somewhere

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
