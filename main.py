f52cf76d11789cfba99b9da2ce873ab1e726d485
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:15 +0000

    price fall boil impressed certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def remove_duplicates(lst):
+        return list(set(lst))
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
