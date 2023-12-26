54664d5e64d0015c1906925a2089471e09c4c871
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:23 +0000

    rely pleasant common sleep both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
