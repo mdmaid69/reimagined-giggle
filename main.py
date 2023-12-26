e492eb21f20839738161245b5a2205255a3d8cdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:47 +0000

    under succeed swim cry though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
