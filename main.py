8d8d8cb8062e5d64766072ffda6b7e9401a80372
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:39:50 +0000

    throw arm corner trade lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
