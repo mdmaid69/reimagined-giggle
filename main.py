601e519eb7cf55c9914b3c64fb906c245a3638a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:44 +0000

    factory Itself climb callat spill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import sys
-def print_python_version():
-        return sys.version
+  def square_number(x):
+        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
