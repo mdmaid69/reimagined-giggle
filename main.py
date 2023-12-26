87a01ccba7f164070ce14d7a7c7402205b053043
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:04 +0000

    nothing shout food sorry date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import getpass
+def get_username():
+        return getpass.getuser()
