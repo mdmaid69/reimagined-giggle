c8047ca0966c5a8496b0aedd5d87fa8539f1c109
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:56 +0000

    present confuse method command detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
