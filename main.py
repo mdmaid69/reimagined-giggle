2a951e2bb4507a835da83b0b21c6e040e1dd1f22
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:28 +0000

    conduct copy period wire as

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
