ed4e3fc62021e0a078d46163c0e3a6fec84189b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:29 +0000

    insist me home short attention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
