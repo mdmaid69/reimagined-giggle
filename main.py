ac42a5e68eb731b39319f3c6d728bc198492bf81
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:58 +0000

    pipe hair steer clean past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
