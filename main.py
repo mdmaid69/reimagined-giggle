fed153a9f97898d0e62af65d9af52115eb2387a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:09 +0000

    owe carry match effort goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def append_to_array(array, item):
+        array.append(item)
