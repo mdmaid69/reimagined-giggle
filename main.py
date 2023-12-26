5d9939cd1ca65f8e0dc7b69b5aa089d3d92b9d9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:47 +0000

    callup organization convenient left usually

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
