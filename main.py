5b93cee65d29afba6bbee072aede3a48e2ebe9c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:13 +0000

    summer offence key custom wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_list(array):
+        return list(array)
