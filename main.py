c2ca2354b248f3aed22598f5e95c25030f8693ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:35:16 +0000

    target me practical relief Vv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
