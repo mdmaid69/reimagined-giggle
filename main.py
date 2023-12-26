0c258c70b2420b0e6899ca76ca1139c1f5b502e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:10 +0000

    spend play team unit might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
