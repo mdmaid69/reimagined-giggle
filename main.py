098ed386e122d9a3f68e79090a691ae794017aab
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:38:26 +0000

    smell anywhere medium type judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sys
+print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
