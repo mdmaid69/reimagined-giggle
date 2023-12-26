be1b89fa373cc3fc9fbec584e48a6f5c89f6cf8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:23 +0000

    condition style note direct manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
