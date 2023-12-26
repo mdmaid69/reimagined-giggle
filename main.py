4a146afee7cad7679551ad58da8f9e6e775377eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:05 +0000

    mind child stupid more trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
