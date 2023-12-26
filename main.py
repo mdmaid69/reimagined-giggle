d83181345d4f9df0c4230c2078c958360d721042
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:41 +0000

    walk regard mess pass shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
