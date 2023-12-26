970fb79e6955a37dc2a383d2b6fe637843301a73
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:35 +0000

    talkinto comein keep proud height

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def exit_program():
+        sys.exit()
