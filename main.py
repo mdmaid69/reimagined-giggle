97eb9bc8aa8908ce3fda29e7f959dfc645b22af5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:19 +0000

    cry hide journey weigh feeling

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
