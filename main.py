ecd3a3d10b6147bf117b78aa9460842cf50e20e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:17 +0000

    thinkover earn phone equipment lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+print(math.pi)
