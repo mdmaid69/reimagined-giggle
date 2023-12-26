be4af30d367fb5d2a0519d008664df39eaac39a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:36 +0000

    direction party goodbye shall concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
