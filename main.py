3dfb2828765704a153cff1e0826eb321a7f79d54
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:09 +0000

    bound Kk influence turn understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import time
-print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
