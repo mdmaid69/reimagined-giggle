8b2418a77312f1cf17c2f77f90eecfbd1bd5f08f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:55 +0000

    inform question low regard may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
