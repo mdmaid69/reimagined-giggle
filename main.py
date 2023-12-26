9fda79d2d25b9d53224beb1c9e7301bb5cabaad0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:16 +0000

    duty whole argument win account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
