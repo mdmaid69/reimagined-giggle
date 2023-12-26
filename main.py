7001f22687f703bc96cec1610aee46174b56a1f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:00:37 +0000

    system front clean she final

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
