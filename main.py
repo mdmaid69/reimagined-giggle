d3d0099937468ffd07bb73bcbe25d95df2d0a9e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:33 +0000

    concerned dull owe direct down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
