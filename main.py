1b45d0910c1a48d7b12531fb82ea6d79947f0db5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:16 +0000

    contents violent rate few upwards

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
