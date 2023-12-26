59a48f810bf049ef9d29ed89515cef270d8ff787
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:01:32 +0000

    sick bound play later sick

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import getpass
+def get_username():
+        return getpass.getuser()
