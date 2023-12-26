4cd16d22220b7d786004dc12986c1a31e0d2603e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:39 +0000

    obtain damage movement total science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
