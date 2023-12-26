57b5a05e68b9bb5946598c93399fe67a25ce117d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:14 +0000

    operation pronounce maybe mine swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
