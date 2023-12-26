143daa0cf20b56e1c338203df7d432be467bde26
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:40:13 +0000

    rob point central free memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
