3efc58c487be110f6b2ccb59548238220aab76b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:31 +0000

    keepup wave yours print room

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
