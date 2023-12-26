03bdf8b36f96586dc9ea1e871143fb88626e990d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:30 +0000

    holdout piece say mind bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
