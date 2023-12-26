8b9dbead6b2fe26de32453a19eb6a2de4384a159
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:29 +0000

    lonely theirs ball bear file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
