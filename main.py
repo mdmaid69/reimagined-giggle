b37b5f36818aa2fba2b2508336fceea5a4379a02
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:57 +0000

    unless question obvious punish zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
