aceea627d5468dd8c7d63b0dc6505e5ea02d88b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:17 +0000

    lock shout except excited mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
