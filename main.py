4aac17d1c41405af8bc1826b6c4e12e7199561fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:01 +0000

    situation thick past suit attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
