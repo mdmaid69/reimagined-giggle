b37de1e9bc029e0da2c4d4910f6fb654bdd6508e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:00 +0000

    depend fold bank fence face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
