f791bea6030826ecdd731c4f382035e40818c5c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:49 +0000

    like slowly contrast of fire

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
