1be6842173ad667df1144683262e3395a901c4aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:21 +0000

    middle never announce cheat reply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
