4324d01525e12b44455f348eface6b3382bc4a33
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:17 +0000

    tool murder surface wake slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
