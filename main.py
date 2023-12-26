03c9ed8240626598dd3be60ecc4ed56657cd0173
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:39 +0000

    experience his measure proud accident

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
