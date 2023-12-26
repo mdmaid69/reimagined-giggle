1d022b034682ac4011f4d8bc751cc026b90dfdc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:49 +0000

    after slightly come sing usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
