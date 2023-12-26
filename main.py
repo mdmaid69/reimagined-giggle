f73c27d3aa4f97974b05b533d87ca7f2beabca63
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:21 +0000

    club also lead cut land

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
