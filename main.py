3fce0beb14241d95d68ec0c436b12f15fbf727e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:24 +0000

    finish patient score hurry improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
