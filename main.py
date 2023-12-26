299290125b11d6906c13d4afa7019660805203f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:42 +0000

    lower hate stir married excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
