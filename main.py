b1633152350e4165de76fe204fb648423eac890a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:07:50 +0000

    name joke dead keepoff suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+print([x**2 for x in range(10)])
