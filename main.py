12fac2ee2d122d190eba914e08bec2d26fa96684
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:05 +0000

    568energy closed laugh shadow treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
