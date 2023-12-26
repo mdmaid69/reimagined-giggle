83b253814e38ba0a2d6de8abd53a62c2084c7a53
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:26 +0000

    type tense compete flat along

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
