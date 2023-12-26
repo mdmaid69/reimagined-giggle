715901170b246a13cfd4fafa47204c98807b19e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:37 +0000

    example pleasure many generally cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
