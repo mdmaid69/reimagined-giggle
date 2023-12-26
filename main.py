4656ebaea49b61091099b9c19f32b735f54e2d82
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:49 +0000

    closely cover fault or burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
