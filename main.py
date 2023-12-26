0493d2a218e3584a205bdb1a0528d76ad2244598
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:57 +0000

    far opportunity listen how include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
