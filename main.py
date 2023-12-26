2df36fb1328a63d972c3f7629dfdc755b99b46ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:32 +0000

    funny top load emphasis contrast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
