eafc0686e933fa3daf5be15925a416ae777fc67f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:32 +0000

    awake advanced sure authority single

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
