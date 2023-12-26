d10f8740fd1a527eada71dcbe7ec7fc94939b68f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:43 +0000

    cry surprised thought on visit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
