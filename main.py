924be7b07c193df9365f081e3902dc2efc552fad
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:01:53 +0000

    interruption need close polite reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
