126f507cecb8a0f1a6803d3026fb3084b2cdd371
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:10:20 +0000

    cook story turndown report water

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
