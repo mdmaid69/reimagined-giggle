b9808f155222d55215b7ec2c93ceadd0399c86ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:06:05 +0000

    grand makeinto judge step alive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def is_even(n):
+        return n % 2 == 0
