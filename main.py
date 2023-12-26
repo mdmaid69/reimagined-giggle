ce169a20fc2e1a1be2fc0cd6a060aceadfc7b306
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:15 +0000

    giveback hole land content action

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
