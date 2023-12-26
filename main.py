4efe93866e973a2771ad3505fe3c9fa83f7d76ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:34 +0000

    direct defeat offend hit person

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
