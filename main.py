a42cd465bb6f18c90fe981f38a5348d2e3b9c8b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:28 +0000

    frequent apologizeapologise more somebody cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
