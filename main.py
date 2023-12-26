9e019fc55f92bd51bafa27fcee800735acfb4e75
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:32 +0000

    hers appeal burn sleep nonsense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
