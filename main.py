aa5f8fcb391a0330027ea5695efa862f5bbdc95f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:19:13 +0000

    phone emergency bang length empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
