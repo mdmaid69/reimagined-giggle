d11c32c0238aa8a8dd3734f3877bc273ee04a520
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:59 +0000

    write pay poor vanish question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
