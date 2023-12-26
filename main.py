a7226f099cf25e4b2463ad26e9ffd63b20c5bd2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:34 +0000

    heat travel decision space find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
