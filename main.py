68c499f33b8df7055d60d4fd938b22f43f6a14d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:21 +0000

    Jj issue play fair responsibility

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-print(sys.version)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
