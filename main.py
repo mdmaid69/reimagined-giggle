9f3ffeb63c746c5547fca1316ad724e21a309fb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:15:10 +0000

    evidence impatient responsible mend regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
