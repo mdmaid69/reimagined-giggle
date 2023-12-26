5246a8186e8d4dde518654276e0cb8f0ee3883bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:04 +0000

    provide kill show lookaround funny

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def sort_list(lst):
+        return sorted(lst)
