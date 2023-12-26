582d694a28d95b4a18461cd2e2ace69fc09942f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:29 +0000

    just deal unlikely behave suck

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
