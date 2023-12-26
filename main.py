21f5284a3114e55e2ca6d0c782ce87dad4dae0d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:18 +0000

    dry vision correct coat live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
