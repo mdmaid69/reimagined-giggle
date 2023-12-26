111ce3f10fda7d972653ad38ec3a17394db8f621
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:41:49 +0000

    use it scare slightly himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
