e36d03a9038f5ee819b5243099c96b40fbd09122
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:02:23 +0000

    long stupid trick issue try

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
