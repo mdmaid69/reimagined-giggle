ec293b73c521f849a7596b066a61a17bab0e3203
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:40 +0000

    future figure callout tap wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
