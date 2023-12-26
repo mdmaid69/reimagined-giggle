ac55a555959c008504896f47d4ce6aa91030eaf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:46 +0000

    slowly much progress similar brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
