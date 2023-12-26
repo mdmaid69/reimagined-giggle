61e72d11982f02488deed92bed23a07f55e402f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:05 +0000

    past partner level story once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def convert_to_hex(n):
-        return hex(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
