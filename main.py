5771f1381415669c6f25424338d1f780e789050d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:30 +0000

    impossible pretty may tell nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
