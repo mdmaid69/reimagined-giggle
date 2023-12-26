d5002301703bb98ca1a2dd5fac444aa98e65d003
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:57 +0000

    emphasize sign turnup fit programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
