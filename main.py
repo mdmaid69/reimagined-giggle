746b870b80a4396583900b84601e60429c281b27
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:26 +0000

    stop air steer steep whole

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
