e7c9cd248c610baa78f9906b7790be20c6a2a681
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:39 +0000

    people could firm first position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
