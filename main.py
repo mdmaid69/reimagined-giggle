37bca09a4cd3235933be9feb6ca15785e62e69cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:10 +0000

    because lesson really real quarter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
