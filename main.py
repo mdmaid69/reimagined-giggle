3958d926b6b4914ba83e93e0baf3763dec7e1380
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:03 +0000

    march impressed comfortable permission appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
