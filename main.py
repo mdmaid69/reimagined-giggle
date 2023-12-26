0d9185480494ad6fa9c0d4712234fcd46aec07bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:05:56 +0000

    which how mad half plain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_typecode(array):
+        return array.typecode
+def divide_numbers(x, y):
+        return x / y
