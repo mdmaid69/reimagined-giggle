55651c8c584ca09b8e50bb8505f8b751a7fd5c6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:29 +0000

    bringup when style inside story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def cube_number(x):
+        return x**3
