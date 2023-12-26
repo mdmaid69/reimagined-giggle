8c883e8681689c2fc3b499f62130b70e3ee64617
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:59 +0000

    centre wave prize wherever heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-name = "Python"
-print("Hello,", name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
