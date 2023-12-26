6fa96cdd3c800d2cefd41412294196532ab07866
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:39 +0000

    attitude miss fun really joke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
