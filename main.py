1166bc26c9416d7ca1f4c3e670f6d40ac9b93f47
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:28:12 +0000

    make inside nearly correct phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
