f7cc7fdebc041f7dc9ce346a44bc4bd00fe345fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:42 +0000

    ashamed violent mistake open near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
