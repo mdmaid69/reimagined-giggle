1bda6ef55f96757e841d7a0a99ca579c633be59c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:27 +0000

    square live keepup influence getoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_float(array):
+        return float(array[0])
