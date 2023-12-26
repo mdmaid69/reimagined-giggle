a57908ca83cb549b0bd801160cc1ec7db3bcd19d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:01 +0000

    translate duty shallow national especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
