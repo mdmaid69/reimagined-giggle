fe7d09474221eea4c188f80c1e069cb6803497a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:28 +0000

    sweet shallow cheat comedown declaration

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
