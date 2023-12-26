842a1ba3d11e418e971aaa1f25ab78ac1fc29959
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:19 +0000

    name appeal drop tap comfortable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
