e45a3aa3b59033544126033b955d040170160c0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:41 +0000

    godown 565end whistle let tape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
