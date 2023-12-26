e203f27f8ddce909565044a66de2f55753e97d71
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:39 +0000

    shoulder bringdown talkoutof size generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
