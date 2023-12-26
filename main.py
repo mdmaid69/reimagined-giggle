3f64d25bebb26da0bf94dca64cd7f9c411212e8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:20:06 +0000

    first remark surprising Dd choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
