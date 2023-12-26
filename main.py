855e117e318904e4c8322b3fad13139cda3ed571
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:05 +0000

    object claim another their common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
