892a27295abc0b52e9098ee69d325c7ae451098f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:37 +0000

    especially ear fortunate sleep their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
