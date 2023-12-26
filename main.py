7e6fa548d865b9c7c96b51bd9f9a03d707012775
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:21 +0000

    word scream our round comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
