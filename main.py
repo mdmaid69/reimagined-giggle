4ba252d835b9900c6e894641bff616a4ac59082e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:22:19 +0000

    hate mail degree thirsty estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import time
-print(time.time())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
