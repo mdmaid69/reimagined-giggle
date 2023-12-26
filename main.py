3f14d8a28865a46252abb51d29827ae7ba04bb5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:36 +0000

    habit afford Rr both shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_str(array):
+        return str(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
