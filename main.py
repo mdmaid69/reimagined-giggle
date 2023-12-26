2c953b5456de477070af78d5a4bac0237593e264
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:27:54 +0000

    conduct interview least time bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
