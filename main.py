aa29b2b5a8003ec2853ee0bd26b6544550fd8fbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:46:13 +0000

    today if Gg look suck

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
