24fc258077331a4fd11851cdb1ce490b56a2d6fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:57 +0000

    about Zz rent reach decide

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
