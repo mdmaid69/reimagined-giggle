60dfe9d64b72fb8d729899afafaeb08b8396621f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:40 +0000

    slide home feel since far

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
