5c756d3af437225c7b2e79420df9c886393b1858
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:29 +0000

    memory rule draw computer turnback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def cube_number(x):
-        return x**3
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_str(array):
+        return str(array)
