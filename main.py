4df5a86ff3fbfc6b5dcb2534adeb63baaa903589
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:10 +0000

    window entrance appeal light makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
