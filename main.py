891037f4a4a1003ba5933802e5700ea1d85afa20
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:14 +0000

    either question possible window influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
