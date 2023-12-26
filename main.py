e19db90aae9ab31157aa96a1380d7f5668ee5e45
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:36 +0000

    holdup contribute thorough mine me

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def square_number(x):
+        return x**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
