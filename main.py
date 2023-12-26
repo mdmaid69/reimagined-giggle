7183f93d9ef0dfe5a76684e26c3159569e4cf272
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:00 +0000

    limit upset glad answer price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
