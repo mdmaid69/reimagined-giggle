77f8f6b9bde334b7384ee1238de83d81aa18f2f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:24 +0000

    lock only benefit water careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
