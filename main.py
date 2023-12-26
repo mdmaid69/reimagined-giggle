3dd49119576d2712ddee1ffb994991c2b89e9e74
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:17:38 +0000

    no write catch object goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_perpetuity(payment, rate):
+        return payment / rate
