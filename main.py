b243fedce18fa6dcac9d4532e48964b4d7684418
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:15 +0000

    there sign pause by capital

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import sys
+  def get_python_version():
+        return sys.version
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
