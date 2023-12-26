8f5151e8545febef52588667507213629d129e73
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:14:33 +0000

    dark cover market town process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
