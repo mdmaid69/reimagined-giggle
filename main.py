beae6d1a42a84d9d84f7fac277b67c307c1f324e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:00 +0000

    somewhere there old complete stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_power(work, time):
-        return work / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
