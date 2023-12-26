188ebf348b764851a78cd3becd75138527ac62e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:37:08 +0000

    convenient might 570enjoyable generous insist

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
