429a280cce85764f68012344fb05d8e5cd6f2d32
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:40 +0000

    Oo rest practice section command

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def cube_number(x):
+        return x**3
