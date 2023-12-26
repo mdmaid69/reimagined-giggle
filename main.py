b5375ae8044755ad1eccf2f80c2b1908b6c3a16f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:14 +0000

    week pleasure design ground programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
