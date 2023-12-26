365eb82a3de131496c03839b523c668d08f10a27
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:08:41 +0000

    breakthrough into start talk some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
