054150791cdb80a8819ec6d48de4ac24b570e43f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:24 +0000

    spare swim cost operation history

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def add_numbers(x, y):
-        return x + y
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
