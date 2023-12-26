f0af126f0db973cdb731516f42b2f7f172cd9110
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:38 +0000

    plain thing curious favourite intelligent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
