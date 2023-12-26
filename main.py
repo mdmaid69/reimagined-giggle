145431d44393e3528916767b2116b52c3c8dd5fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:07 +0000

    plan punish godown below rank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
