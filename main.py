3b65302b4cff1893d54719b67c1cce4b9f13832b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:08:20 +0000

    564encourage medium Ss win before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
