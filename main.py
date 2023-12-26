4ff0446f2c44dd50de7d731d169bcf4819dd1837
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:59:44 +0000

    joke brave rely model level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
