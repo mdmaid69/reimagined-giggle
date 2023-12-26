ad28e70bd10f6d13b9efd10dba2a145d7ae77140
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:09 +0000

    like yes spin combine hers

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
