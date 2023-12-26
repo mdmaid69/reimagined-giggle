53ffe545bc370ed020a047f2895fcce52a684d76
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:49 +0000

    OKorokay beautiful do match ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
