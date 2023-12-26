0dc693417e2725519f4fdf30622cb8d4a5c0dc84
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:23 +0000

    object public throughout similar over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
