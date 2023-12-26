0214e1b590cb2c5ec225d985c1eb93231b4e0d5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:07 +0000

    school low anything ordinary instrument

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import math
+def calculate_floor(x):
+        return math.floor(x)
