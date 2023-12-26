5b3203c25b078395a7a2d6b9e0d1ba608b5ed3d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:27:00 +0000

    179bite Hh without knowledge gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
