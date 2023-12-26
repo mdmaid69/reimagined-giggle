b31a2664d5c67aa97c94e24e619c0e661785762a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:22 +0000

    temper tidy complete identify quiet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
