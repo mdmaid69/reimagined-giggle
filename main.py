927557a81ba41bedad0773c409c116316120b1af
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:14 +0000

    below left consider bread cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
