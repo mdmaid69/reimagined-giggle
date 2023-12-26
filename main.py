abdc996e093cbaf7022231415264868d26f17be9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:59 +0000

    dear murder person welcome last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
