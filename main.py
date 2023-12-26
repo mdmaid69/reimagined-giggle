8c0c97c03078e18b2668aade7e89fa3bf36b286f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:39 +0000

    switch keepaway poor home join

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
