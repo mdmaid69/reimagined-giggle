bb58a75aa896d06565814fe90ee2b7a137f7fd2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:10 +0000

    separate board art all mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
