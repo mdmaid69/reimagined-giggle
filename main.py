fd168a4679dd8a84cdc8f93fd97c13ffe04dde97
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:41 +0000

    emotion drop various appear ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
