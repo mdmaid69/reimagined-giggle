6d16bfdb4f1a94147fec5529d99c00168ee2e0aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:26 +0000

    idea raise upwards eye wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
