3fb80222b565c9c9b8c48212c99a59072b8ad1a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:22 +0000

    cruel style while Qq runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
