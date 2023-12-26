48f9fbabe7acf48ab173a80e1df3e4014d4a162c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:52:14 +0000

    knowledge regional stop dear leg

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
