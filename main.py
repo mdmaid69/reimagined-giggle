cb31d21cf162b8d8abbb41e1ce5c2684a2492dd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:04 +0000

    say black clever bored off

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
