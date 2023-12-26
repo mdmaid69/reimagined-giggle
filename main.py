5534942927f214136f8edb5cca2249e7c302857c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:20 +0000

    mind one reach tonight do

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def count_elements(lst):
+        return len(lst)
