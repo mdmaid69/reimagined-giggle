c636040e5b6877b61aa63edf2792d5987f238ece
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:48 +0000

    recognize desperate allow rush experiment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
