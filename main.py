6a020732970a86528f175e4ae6c4cc24e0138fc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:47 +0000

    become art interest some fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
