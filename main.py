7cb4c55246d6d8ca4c841c7aed3f25370ad1bf9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:06:42 +0000

    minority break concerning aim lead

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(5):
-        print(i)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
