03ef57e62f2f57466af6b22109b1ee036bb020b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:23:42 +0000

    grateful effect send breakup suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
