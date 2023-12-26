3517bd99881f6ca58babdc128b0d4298122006c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:46 +0000

    sleep mark sensitive ocean tense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(lst):
+        return max(lst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
