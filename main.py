cac31c23cf623d85daacaae40b23afa6d1e9c64e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:21 +0000

    since everyone power weak quarter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
