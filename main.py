0435580ca995e56c8e75a975fc4b7d4cdc16899c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:12 +0000

    comeover kill modern tie tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import os
+print(os.getcwd())
