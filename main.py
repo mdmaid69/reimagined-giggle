e73890910ef900622c3a5169f0d8556c2ba091eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:18 +0000

    branch loss creature turninto tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
