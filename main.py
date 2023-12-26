4ef91cedd60d20c74b4cf58308f2b218918c2725
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:17 +0000

    it turnout cough home pressure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import sys
-print(sys.version)
-def calculate_work(force, distance):
-        return force * distance
+def exit_program():
+        sys.exit()
