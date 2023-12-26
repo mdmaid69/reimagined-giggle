904ef5838e9a223b26a4d4d0d67512adff9fe112
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:26 +0000

    slightly promise apologizeapologise quality piece

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
