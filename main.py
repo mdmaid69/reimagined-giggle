4565f408149685f8ed6189f50a3bd60856f738e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:06 +0000

    target study rob seem speech

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def multiply_numbers(x, y):
+        return x * y
