0acbc4683b89e4fe8d3a0477ed383c87ebf6290b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:11 +0000

    limit Ff woman Uu play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
