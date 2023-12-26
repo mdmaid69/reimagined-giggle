2fc77bf784925a561a73d383042e0b132f5a868a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:54 +0000

    sticky imagination sit guard decision

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
