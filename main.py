d863a04b668558a93f12578b7a7e001421173502
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:31 +0000

    under spin again Pp nowhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_work(force, distance):
+        return force * distance
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
