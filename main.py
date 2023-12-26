c368e0ef37119b5f4fa0665dada930ed4114e82e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:51 +0000

    minor inside clue hole common

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
