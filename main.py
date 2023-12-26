031db4e64f61af6b0ec5a49a37d045745d107128
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:57 +0000

    permission space office trust small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_length(array):
+        return len(array)
