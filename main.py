1a3165e9eb678bbf4b1ca75528e4112f46900fb5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:13 +0000

    fit might recently enthusiastic needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
