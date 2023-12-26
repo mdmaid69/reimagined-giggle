85c7ebef9363d2b992f382428c85eb6e796619b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:33:58 +0000

    mend responsibility neither string disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
