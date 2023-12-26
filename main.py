1acb92d11a68af10b3a560591d901c8566210d8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:39 +0000

    smart listen office talk hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
