8912ca7ea92a5cc5806e6a5a9b322de3407f977a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:53:09 +0000

    disagree warning perhaps 573entertainment wander

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def remove_duplicates(lst):
+        return list(set(lst))
