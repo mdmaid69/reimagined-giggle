a14fc8ffc37337a0be5178518a667c0606f1e9da
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:05:19 +0000

    position sudden sometimes pure behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
