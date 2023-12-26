d2b2948c400ff3b2828f10281d9a9de0998744b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:39:35 +0000

    report workout capital few Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
