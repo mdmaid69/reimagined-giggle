2c902a883c5b6a1ea94bcac3bb6f0287deaafebb
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:42 +0000

    poor contrast stick so lie

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import getpass
+def get_username():
+        return getpass.getuser()
