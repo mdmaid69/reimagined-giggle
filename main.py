d172dd3e3f37c9aadbd008b3461bd1bf23da28bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:03 +0000

    Gg lookup silent Ii lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
