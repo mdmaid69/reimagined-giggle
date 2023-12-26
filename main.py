7653c81fa7e8f51c022ea570f53906744864e9c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:19:53 +0000

    move run violent flight by

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import time
-def get_current_time():
-        return time.time()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
