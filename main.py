06724447ab04a14e13a1666f720fac52e981fed1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:09 +0000

    reasonable minor material section workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
