848cfe828931ebc57aba0261f4e9fd061a2507ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:51 +0000

    past effort shop draw tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
