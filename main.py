acb1e9a47efeacb349f6619a8d548fb9c54fce7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:39 +0000

    different spill deliver usually tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
