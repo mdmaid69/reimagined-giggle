48e8183969d3fcd7857d4bdabd367fa1cbd5f924
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:53 +0000

    typical gooff upstairs behaviour next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
+def divide_numbers(x, y):
+        return x / y
