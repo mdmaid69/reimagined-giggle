f66d6178ab6039b5baffe3e8e43d5fe27f86a03b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:38 +0000

    label front develop season except

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def count_characters(sentence):
+        return len(sentence)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
