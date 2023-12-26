7b95d83e93a57277b3db85cd2cc0d9f7de4acc8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:08 +0000

    impressed university very light aware

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
+def is_even(n):
+        return n % 2 == 0
