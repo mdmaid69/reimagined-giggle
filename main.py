2631667e37ecf2533a9a2538e905782f3e55afe1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:28:49 +0000

    thorough limit either breakfast practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
