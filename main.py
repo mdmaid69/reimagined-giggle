5ca470b88bd4992c5cbc4c48343f745694d59145
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:49 +0000

    himself which receive under succeed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def convert_to_hex(n):
-        return hex(n)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def count_characters(sentence):
+        return len(sentence)
