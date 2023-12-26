b28e0adafb626a8bb49f010f09fbbd4c3b9dc673
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:34 +0000

    recognize clear bet deep present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
