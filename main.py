fbbc0795e50ebfca510e972e6b7dd9866b1abbfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:21 +0000

    accidental famous emphasis attempt ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
