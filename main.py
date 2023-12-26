46601c056d76619c9b8f8f5be390d05732d9008e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:16:57 +0000

    fear child direct thumb stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
