ccd92ff266b2f3ba5f5731da2e56ebb173fe0442
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:43 +0000

    important cap holdout brush intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
