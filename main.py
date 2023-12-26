299fce1851b8f104b6c0656470f78adfe81635ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:13 +0000

    effort throw familiar reliable exciting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
