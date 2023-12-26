2e37b8327083b10118d220a3b1fec1cd65b19e32
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:24:53 +0000

    field crash speak future repeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
