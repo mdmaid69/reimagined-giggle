850e555b09d2cbf079cf47104ea31061f040fd65
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:18 +0000

    impatient unpleasant inside Qq wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
