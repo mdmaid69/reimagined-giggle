16811512dab86c334d934af2f72d35f4d2a097e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:47:22 +0000

    movement achievement any bill exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
