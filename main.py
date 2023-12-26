65a027e104407c83949dae0440c57d750f462ba0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:55 +0000

    now considering illness phone winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
