d4ad55b2777b8911e1c3fb4238fa2369ea34cdfa
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:40 +0000

    tone across occasional born see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
