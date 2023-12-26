8f486536717ef138a1981079a20c6f39f647fe22
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:44 +0000

    design head battery away turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_odd(n):
+        return n % 2 != 0
