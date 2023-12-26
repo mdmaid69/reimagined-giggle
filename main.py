847eb11709020a6254bacddfb6a79d5fc9d40f4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:27 +0000

    morning message refuse guilty attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_numbers(numbers):
+        return sorted(numbers)
