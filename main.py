39c752456f01e21720c7adc7484ddbbc0ffb1615
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:13 +0000

    Zz them fasten tour tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def clear_array(array):
+        array *= 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
