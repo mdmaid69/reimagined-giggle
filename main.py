507ba16849416fbec15ec8d739689834b9992988
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:49 +0000

    lost Jj across seat home

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
