0353e09541ce52b60ecb2578b46d9625e805a10b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:06 +0000

    night government power yet important

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
