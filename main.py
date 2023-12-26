4899776bad11ffa5c2633c268470953e9ffd3955
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:48:37 +0000

    butter average turnaway before thank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_current_working_directory():
+        return os.getcwd()
