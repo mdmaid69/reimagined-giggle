f9dd19bf232fa58e7a206358887e5d7b50d3e9a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:57 +0000

    pain notice all account ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_string_from_array(array):
+        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
