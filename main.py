d1bfe6f4d2c72dbf33427f6f379b04d82f70de61
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:19 +0000

    Uu apart boat claim burst

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
