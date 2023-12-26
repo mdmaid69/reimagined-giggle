4df51b630884e04bdcc627bd827aba0de1d6aa4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:08 +0000

    his total active debt compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def convert_to_binary(n):
+        return bin(n)
