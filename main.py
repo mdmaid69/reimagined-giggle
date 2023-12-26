43b3e691430727017c9bb0326a134d4cb1efdc86
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:13 +0000

    dry practice discuss impressive mess

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def subtract_numbers(x, y):
-        return x - y
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
