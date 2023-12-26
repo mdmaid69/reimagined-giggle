ebe5bc9b82d607b5b46b4458c81620831b3f9176
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:52:08 +0000

    blame brave yet callback middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
