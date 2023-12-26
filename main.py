7bc78d6bacd154563f639599d37111af43336df4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:56 +0000

    clean who each speed negative

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
