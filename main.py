dd52d1dea7d18b7fe6bf1a8eaf19acd53e48d140
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:27:50 +0000

    fact quarter guilty finish 568energy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
