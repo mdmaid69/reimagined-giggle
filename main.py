1eadeb469670e5d784016227a06647d5e9faf2e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:05 +0000

    after wind shape fly behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
