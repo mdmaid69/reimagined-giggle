eb79048f0daab6c1fa7926eeb8903573958d5cf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:20 +0000

    letter autumn straight major frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_bool(array):
+        return bool(array)
