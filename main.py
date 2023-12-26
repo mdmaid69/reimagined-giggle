850a4fb82ad36d4c5dee80362e1d5f89049e051c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:05 +0000

    neat gate leave suppose lonely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
