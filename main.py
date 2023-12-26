7c5cea737c5b4a23d5ebbcbe43e7358c70b62af4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:35 +0000

    laugh not organization change fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
