955665cabde693abe3e0cdb7f88ff268794f7a67
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:19 +0000

    tired strike black mysterious constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
