8165b86afcf7467caf0f4173609269955964fba2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:43 +0000

    slide truth noise male them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
