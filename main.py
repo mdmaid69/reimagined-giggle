ac1146d2da2d575c7b7c6657cb85ffadfcd10ddc
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:34:49 +0000

    insist mistake uncertain embarrassing sore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
