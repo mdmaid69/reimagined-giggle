20fb611f1ac23481a7bde0775e5072796fa99e01
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:17:47 +0000

    cry sharp please tear lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+for i in range(5):
+        print(i)
