c9d138934406c2890518de658ec8ba69fd708acc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:01 +0000

    culture effort read about television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
