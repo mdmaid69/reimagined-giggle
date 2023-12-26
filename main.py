b0523bf7243505039e7cb5da5408c8c967ba9906
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:19 +0000

    reject speed borrow display treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+def reverse_string(s):
+        return s[::-1]
