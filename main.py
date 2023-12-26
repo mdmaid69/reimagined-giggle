28b96ff2f781f78a43835ed4e5b89615c12863ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:18 +0000

    tonight level century right either

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
