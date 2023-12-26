b67fde157d15a6a8b0d8b428df42f39fc636b7f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:40 +0000

    thing season help object handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_max(numbers):
-        return max(numbers)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
