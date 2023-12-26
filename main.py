d49699d8972ad9cfc16589b0439ed2fda8c1af3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:14:58 +0000

    round Gg sweet advantage crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-def exit_program():
-        sys.exit()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
