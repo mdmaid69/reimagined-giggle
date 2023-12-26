1524932b761785f39d55f3a950ec1051c1f3e0af
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:55 +0000

    scream fly careless field bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
