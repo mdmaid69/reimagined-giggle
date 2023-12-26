c3b037e8039c2e8358f52253498ab254f0cef95c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:52 +0000

    ourselves disturb thankyou spite star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
