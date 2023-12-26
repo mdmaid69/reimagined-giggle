3dae2740faccd9f06a09c73c8cb4cab9b57d4b4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:41:39 +0000

    ask will voice loose official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
