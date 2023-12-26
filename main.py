62b8eddc885a49b3c3a1fd10921987ea80c5208e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:49:28 +0000

    near criticize say fence attitude

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
