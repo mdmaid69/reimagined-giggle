fe0c7ac0e7f86017a5d54301d5b83a937446475a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:34:55 +0000

    story generous unhappy will train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
