88079f8152318db7c1eff219e7b4beb32ca910e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:37 +0000

    laugh match clothes runout around

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
