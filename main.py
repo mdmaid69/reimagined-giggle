cb4e8e4e387037b21baf3a96639b392bda1f9a1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:35 +0000

    call drawing underneath nice after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
