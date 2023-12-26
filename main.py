0bbca0d51fbb36a6a52fcde115c91b0512faf700
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:46 +0000

    567enemy approve but neither callout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
