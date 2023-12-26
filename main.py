303975ffef6df3fbc95ab01939722b3e7a7029a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:49 +0000

    car actually content bad release

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
