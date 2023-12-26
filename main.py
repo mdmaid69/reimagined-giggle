48438f25e8ca538af503b8c984d082db85c1c2e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:16:17 +0000

    seat trap shine bored prepare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
