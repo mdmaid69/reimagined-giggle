927513a82136a02372affb308ac83aac2b880b5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:54 +0000

    that hungry fit check pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
