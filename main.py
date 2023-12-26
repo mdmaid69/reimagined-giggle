69f1a792f85be653a87b4179499c0ad52c8dc12b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:29 +0000

    occur add before suggestion over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
