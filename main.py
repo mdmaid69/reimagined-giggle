ddd9ed55e923f500b89309962b9b5a271ceba801
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:44:10 +0000

    branch description pass duty cook

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
