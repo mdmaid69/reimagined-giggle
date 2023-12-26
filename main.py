b6273fb333c2923e107238c4a20aa2086c93392c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:50:50 +0000

    workon there information ocean pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
