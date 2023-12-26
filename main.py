d8baa347345c66cd643e0c75ace7409c60e38740
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:47 +0000

    570enjoyable sleep guard hair experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
