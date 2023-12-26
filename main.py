d3118f069749722cd1d8fd81c535e7aed8ea1cc9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:15 +0000

    clear true crash 564encourage estimate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_pressure(force, area):
         return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
