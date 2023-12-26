e9800f3b18539fe715d965bbc25d4711bd0b1ad5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:57 +0000

    arm Qq in character claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
