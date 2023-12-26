cf3ca1bcfadcb6d3c5a237296425b416e19d3445
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:07 +0000

    actually reach mark cost break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
