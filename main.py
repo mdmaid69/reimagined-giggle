f4dd9a1e192fa13d12ef06fdb321688d5c612080
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:55 +0000

    fault active tonight dull job

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
