c98c28528be8afb095a64e18e42ab50427a05e61
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:52:27 +0000

    scheme idea official loud like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
