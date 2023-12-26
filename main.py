1f9164c1655add8d48a59ed76613534eed7754f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:03 +0000

    Ii mouth clue dear neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
