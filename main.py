1af13ab2d14c9bdccd95a1731cfbffcf320b23a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:48 +0000

    high interested whom carry trial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
