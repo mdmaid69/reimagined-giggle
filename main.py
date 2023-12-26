287096b3dfa68be14466f95faeddf3ff5270a719
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:39 +0000

    hers bunch pipe stupid flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
