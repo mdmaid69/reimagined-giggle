748e83e6368cc6357dee2d56b7dfba0d3e897f75
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:29 +0000

    female except wave anybody dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
