dbe6f9f996c0cd6883321866a5f01de8c70cc1c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:18 +0000

    private regret block runinto slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import sys
+def exit_program():
+        sys.exit()
