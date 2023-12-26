1bd32b2045282d13c2f2f9e0cd771673367d4bc6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:35 +0000

    afford keepaway country best wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
