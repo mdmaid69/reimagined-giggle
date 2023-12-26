e8873c52ee233b054c5210a26b520e452183d88f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:18 +0000

    yet machine field perfect thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
