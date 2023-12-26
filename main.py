d9606f029c6312b3eedf9875088842d9baa0f510
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:07 +0000

    night date 572enter rather song

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def count_characters(sentence):
-        return len(sentence)
+for i in range(5):
+        print(i)
+import collections
+def create_priority_queue():
+        return collections.deque()
