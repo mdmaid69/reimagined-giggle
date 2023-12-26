1adf007a9e44aebbfdabf5706ad15148527c009f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:26 +0000

    nose mass lucky passage science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(x, y):
-        return x + y
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
