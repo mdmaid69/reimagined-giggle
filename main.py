cf02619da1f160fbc945cafdfd20ac01b06e9a34
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:14 +0000

    everybody here switch admire boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
