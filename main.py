0fb6434102c6ce162c07a1c8e943df95e36ccc8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:31 +0000

    afternoon stick thank many careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
