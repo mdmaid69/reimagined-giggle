fb9ab9e59575eabd105a3393b3500dc04987cfa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:43 +0000

    left 565end congratulate wet risk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
