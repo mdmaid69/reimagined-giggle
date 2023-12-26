9250aecc1f694c3a8b5d5fb35ce6c5b48144bc18
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:35 +0000

    somebody strike custom risk organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
