2f542c87364f071b2c991fb5b3bd6f11330bad1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:13 +0000

    connected art mine attach split

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
