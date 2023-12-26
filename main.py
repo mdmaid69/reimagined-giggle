f10ed6d94695fa0688300f33cad7267d94e3f18c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:28:28 +0000

    lead different condition goback(to) her

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
