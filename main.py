e7561daf39699659ba1c7bc93859b5adb672fde2
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:39:11 +0000

    after become father hurt extremely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
