ae0afb12edc6d1f5346ee4166b8af941dea3efb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:03 +0000

    left dress set afford unit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
