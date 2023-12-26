57c50d6e1f4ed49385f3e5e3b86959cd8b2fa6c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:36 +0000

    unkind watch friendly present board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
