9da5e9414344f780c8ab8a68f8786bd58871acd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:03:58 +0000

    honest convenient art rush town

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
