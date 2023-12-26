d4afb411f3f33cca0f72d21a71e006e7f39ae23f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:44 +0000

    old cold lookup prefer at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
