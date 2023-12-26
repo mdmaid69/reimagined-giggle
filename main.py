4f3151ef7f8d7e1cb7113809cdd0eb9507adb0b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:20 +0000

    allow many guide undo close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def subtract_numbers(x, y):
-        return x - y
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
