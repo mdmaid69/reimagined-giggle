35eae8d8fcd96f80ecc529580d10f10049c7ee90
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:41 +0000

    thorough impossible workout plant across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
