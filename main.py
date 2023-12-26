ddd5d2e949a0ca01893b95b8e6acc9ca8b4fa72c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:48 +0000

    claim plant tap suddenly collect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
