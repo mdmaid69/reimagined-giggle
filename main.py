aebe2f569052cefaf6b41475572519dba79dc146
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:49 +0000

    depend estimate minimum hole possibly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def reverse_list(lst):
-        return lst[::-1]
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
