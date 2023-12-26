0117a24f0858ea239e293ec090c6efdc22a010b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:55 +0000

    rise careless bad niggaacademy release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
