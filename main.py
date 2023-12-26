4f3730d1b36748cd2fc26a2241b233325700f09d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:13 +0000

    cruelty thank window guide inquire

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
