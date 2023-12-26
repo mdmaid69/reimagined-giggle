e6ea714281d4e387c6ca7c150a9396d96aa6b281
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:40 +0000

    smell 571enough getaway squash 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
