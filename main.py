5151b17b550b492f4eb0418927f1d88fa99d4bbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:50 +0000

    capital division determined production loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
