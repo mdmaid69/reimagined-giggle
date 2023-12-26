d268d4d9af95d105a8729a29ca6b0aabeffc39b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:06 +0000

    side raw store know keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def subtract_numbers(x, y):
+        return x - y
