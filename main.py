3fb0a4282e13ea735b7f0f7bd96d873d1d9d7c9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:12 +0000

    art tie border Mm Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
