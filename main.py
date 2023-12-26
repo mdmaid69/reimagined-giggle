5a94963ad59b0ad297fd63018a5114c8348ef2eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:31 +0000

    basis best fire press hand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def subtract_numbers(x, y):
+        return x - y
