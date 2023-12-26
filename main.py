3052209ec1e486eb50992ee5d2082bbf5b04ab6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:39 +0000

    polite family impatient history pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
