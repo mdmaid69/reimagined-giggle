35b23e8b3ee65193e2c419f37608d745adc2c0c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:16 +0000

    able land odd male sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
