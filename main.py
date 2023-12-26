3eb94a8a1d812268132d92800668f1c343c43fd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:16 +0000

    improvement high bus stage cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
