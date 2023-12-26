ffbacdbdefb6ac6cd3f9add004ad4ae4109ed743
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:34 +0000

    degree maximum pretend sea pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
