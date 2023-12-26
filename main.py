2c5be378925d5c99cdd25e97db57593c11c3b280
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:59:48 +0000

    habit ignore knowledge case square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
