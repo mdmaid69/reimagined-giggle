4c8a3ffb6b8a2d110a5dbdff7bf35ad51c349c3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:02 +0000

    drink event blame relative likely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
