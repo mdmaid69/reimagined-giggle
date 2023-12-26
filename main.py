c8aa72301f7a3e953cc30a05d46a05d9c8ddcbf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:06 +0000

    sound mend this handle task

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
