545e8ba0bf477378a94f8fe39e32d9f5e60686b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:09 +0000

    equal card reject goon pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
