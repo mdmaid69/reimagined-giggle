4d2ebbf4d26aef119b6081e810a527dd103f3eb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:09 +0000

    quick sorry bin 571enough except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
