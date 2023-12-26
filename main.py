0495fc2a079e252d7d2aec68f1c996a55be50760
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:11 +0000

    tough welcome worried private lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
