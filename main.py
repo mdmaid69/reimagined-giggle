f47faacb6a2c2fd04b6fe4a9b23203346a9e7f1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:30 +0000

    certain bear anything head arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
