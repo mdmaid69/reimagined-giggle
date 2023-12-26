9201e2aa57edc879021b3a6f18d768dbba7ed70e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:36 +0000

    perfect regard result height suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
