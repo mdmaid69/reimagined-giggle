eb4de84a4bc4b46004ba2bb3e60bbbb6f4370226
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:29 +0000

    possibility grand top why pass

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
