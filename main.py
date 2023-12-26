eeb7a86ca4b33f8ce08d055195d5f0738b02b591
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:59:11 +0000

    should issue preparation race prevent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
