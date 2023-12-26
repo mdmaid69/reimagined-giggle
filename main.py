16e0b6276e3236f315b53c4c2bca8df6281b4e89
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:11 +0000

    sale moment wheel ring paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
