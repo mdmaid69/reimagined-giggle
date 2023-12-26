bf874ca29066abb242809a11dc6cc9efcaa976d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:53 +0000

    turnback ought along butter base

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+from collections import Counter
+print(Counter("hello world"))
