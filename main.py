e77ca1307d612486c6f6c8a6c0b274f91febf5fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:17 +0000

    statement 570enjoyable spend rush attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def remove_duplicates(lst):
-        return list(set(lst))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def convert_to_octal(n):
+        return oct(n)
