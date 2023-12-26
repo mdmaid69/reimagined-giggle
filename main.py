dfd3982ebbc8a7937790981057dd260c3442eab2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:12 +0000

    would taste tool confused somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def add_numbers(x, y):
+        return x + y
