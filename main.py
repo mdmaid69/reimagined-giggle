9e052d09c225002f3f5e35a0b31a70af08ba1c1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:11:29 +0000

    drive rain life shade cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
