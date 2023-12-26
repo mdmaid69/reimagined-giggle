0a1fdbd7d9967310060e748f7c2c765e809454cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:20 +0000

    false suggestion Pp why free

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
