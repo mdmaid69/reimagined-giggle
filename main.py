f2b36c078430d7792a773271f7800ce671d91c96
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:43 +0000

    game impatient yet danger taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
