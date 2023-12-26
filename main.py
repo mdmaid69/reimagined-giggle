165acb175038d0a59621aa30ba0d20a05a6f79fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:45 +0000

    protection brain stick attach woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
