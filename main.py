6dbe73a0be7dd44018739e58921663986ebb1b5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:50 +0000

    blind general improve shallow sight

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
