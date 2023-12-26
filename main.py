0146e3b08aa0c1b289ec5e1a5bec1398e17c85b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:47 +0000

    shade breakthrough thanks defeat basically

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_speed(distance, time):
+        return distance / time
