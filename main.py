a8f73d54d004c09db5a00b47024c1b6e76e3f972
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:04:02 +0000

    convince unhappy long burn basically

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_power(work, time):
+        return work / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
