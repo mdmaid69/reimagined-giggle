50ce1e506e8c068a10dda27480723f733581bcaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:01 +0000

    hello relation expensive mark form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
