ffff1138c7f6500c8907e00815ce72a0028ddadf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:16 +0000

    collect wonder this middle telephone

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
