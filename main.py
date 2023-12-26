9d62a7a7124d3780a5a2bddd87f609912538ec75
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:04 +0000

    less calmly narrow complete trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
