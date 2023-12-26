851df804f9f31a3e809ce35e6ffb2f86fa592e4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:30 +0000

    damage blame broken sound correct

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
