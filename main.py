580bd43087833e38e0b3cf2de7c2530307d47cd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:16 +0000

    better coffee survive example just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
