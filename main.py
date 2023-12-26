85df337f1b403997daa2dc9e30fa2d9acc38e105
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:57 +0000

    real over attempt path battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
