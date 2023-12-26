d1410e3c27002de6565c10fba2250bea0a15c7e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:32 +0000

    turninto likely arrive real affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
