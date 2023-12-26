86bf404bba6eb022db88c98a298367d67967b520
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:33 +0000

    interest by walk trust summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
