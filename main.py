0da25ab6d679108405c7db522a249fc44bdf1610
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:21 +0000

    sweet my estimate land creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
