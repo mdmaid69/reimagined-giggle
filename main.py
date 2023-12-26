0c27efc6e50b4b31a132d0694e836f861df7499d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:03:47 +0000

    dress emphasize shopping beneath watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
