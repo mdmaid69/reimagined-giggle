5dabd1eeab8993b13f12f871639406b3c2f315e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:06 +0000

    worry neck term come callup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
