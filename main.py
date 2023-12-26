85b183bcfbec502dc1a03eee857086b255103a1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:34 +0000

    quit disturb wise care apply

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
