79296375c06eb19732b7f0071436a277cfcd4cb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:19:48 +0000

    slowly Ww shame whole just

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def is_palindrome(s):
-        return s == s[::-1]
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
