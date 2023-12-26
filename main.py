018762a81adf74efe0ae5dc55a375b20046000fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:36 +0000

    undo get weekend talk guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
