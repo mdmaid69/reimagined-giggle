710b6d2cb6f8392f4ba07e3f78cc43a8c95f0334
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:32 +0000

    excellent absolutely wake kind sight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
