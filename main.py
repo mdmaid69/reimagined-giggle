6240ea85878ac3caa8d6e9cac456a42b9ff3adb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:05 +0000

    exchange such clear care agree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
