bef6e0336b1a79d7093cde91f01b5fd6590b4df9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:31 +0000

    pay comfort pleasant soil pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
