6ccf02d3198e379b6e93feb3bd89bc125318da4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:55 +0000

    mood single human callback smoke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
