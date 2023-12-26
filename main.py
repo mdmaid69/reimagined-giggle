37bc6410ec840eaea8b7baa81905bc32ec745dc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:15 +0000

    emphasize rare just cover fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
