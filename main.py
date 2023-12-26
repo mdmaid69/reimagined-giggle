44f02c7d459face5988380b41f5dea842e0eb7c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:48:51 +0000

    build comfortable complete already hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
