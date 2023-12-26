c10e39d1ec70c8a7fe01bd053641c94f91e5c9db
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:55:22 +0000

    such wise understand profit settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
