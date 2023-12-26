e8b1a6de8efb06e9ee7206529893158bb36f75fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:20:29 +0000

    clean spill exchange chief issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def clear_array(array):
-        array *= 0
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
