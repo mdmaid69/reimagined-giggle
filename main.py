1f3349c99f70995c8380647b658bda4ec78e645b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:44 +0000

    star as bringback vanish age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+print(math.pi)
