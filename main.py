33277aa727189f25b9bb97e29a86c059ca89a590
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:17 +0000

    to copy enthusiastic as under

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def convert_to_octal(n):
+        return oct(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
