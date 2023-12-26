f192a03c4e38f97b81b4096687aeb9f0cfc55dc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:35 +0000

    today contents moon tend turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_speed(distance, time):
-        return distance / time
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_length(array):
+        return len(array)
