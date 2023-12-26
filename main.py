9d219fc874c1aabf79e57f7d75ab3b58d51597a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:24 +0000

    team raise translate stage sensitive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 def calculate_npv(rate, cash_flows):
         return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
