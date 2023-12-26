98d0602f8f447ac70c9bf7f19d22e6c1257e46e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:14 +0000

    arrangement affect pure runout order

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
