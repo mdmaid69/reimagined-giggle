78754dbc66abf6b6b8852910200a59ba60d5ef17
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:01 +0000

    combination hard genuine hope deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
