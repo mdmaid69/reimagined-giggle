399f477e4dfc3a1244908d204ff5584ab23a444a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:27 +0000

    something cum count achievement entry

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
