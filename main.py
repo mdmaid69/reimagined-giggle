f73c6a0d82a978b7db582711cfdf5bb9ce9a7b58
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:30 +0000

    deliberate wind suggestion language knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
