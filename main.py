fdf59a0925a70b41ae30c99e855a0c4fc3ded5fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:49 +0000

    minimum well sleep authority flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
