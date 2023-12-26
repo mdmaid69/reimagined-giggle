24e0abff7d4ed215071c9d65547b4f9bd872107b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:40 +0000

    nor ordinary season little tense

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
