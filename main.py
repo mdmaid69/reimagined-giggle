0e86820497a7083156f6a86f3f9e09909f6a9412
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:49 +0000

    regard fortunately type police main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
