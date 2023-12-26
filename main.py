ba09371c03b0afdf5498bd8376a6f32aedfdb2cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:36 +0000

    wind dare offence fasten throughout

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
