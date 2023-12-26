5a1569b393bad557d6a36b45b37ca9db0bf4fdae
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:42:35 +0000

    worst terrible judge 566end tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
