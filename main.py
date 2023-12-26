4b435ef6f30f979c5465dc61807d20f2121a0f63
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:46 +0000

    open possession guess future silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
