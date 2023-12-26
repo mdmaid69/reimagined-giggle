81d9e96032300b2a6a50742df312f94a9baaee1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:40 +0000

    brain let workupto pleased also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
