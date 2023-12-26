9bbd5ebf2a15938087ada89a9c80ca9c799cd34c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:30 +0000

    benefit pleased reply science then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
