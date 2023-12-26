89ca0c412a78149fe69df3310910ca2c753f9b3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:09 +0000

    turnon taste snow preparation wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
