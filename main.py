1d9f090b2ef497157ac878d3d5c2fac23fe2c613
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:57 +0000

    because little disgusting themselves search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
