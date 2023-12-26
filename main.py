76c5a4dce15345a5a3193d3153dd263c41614aa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:33 +0000

    duty human precise available heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
