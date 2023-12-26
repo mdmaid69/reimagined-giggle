7cc52febe0ed11aebacd04c59cd1e780c9c29b0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:02 +0000

    afternoon against so keen negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
