9ca953ceb06b6a8f9b3473c446edb335665f79e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:08 +0000

    please necessary black embarrassed too

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def convert_to_binary(n):
-        return bin(n)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
