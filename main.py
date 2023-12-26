964a4a5f9c870583109517353cfa2d0f5ef626b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:27 +0000

    live exact standard greeting cinema

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
