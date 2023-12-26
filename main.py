15ddc99d76586bd54e3001e0bf53a63ff8f36c34
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:11 +0000

    clear study cost cross improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
