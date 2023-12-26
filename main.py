989cbae3c4e23b6431d78ea311adbe20d959f40d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:04 +0000

    complain case under production takeout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_work(force, distance):
+        return force * distance
