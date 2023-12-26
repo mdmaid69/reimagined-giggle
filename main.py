2ad933abd7320d5c8b4556879012b67b737afc8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:31 +0000

    hit holdout turnback train group

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
