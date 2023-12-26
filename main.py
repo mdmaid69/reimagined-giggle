dc55b701e0e4c3985e2c276d9819ea14af3abd77
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:49 +0000

    advice lookback(on) feel notice length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
