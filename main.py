8068b4ce473f9d73bd3fa8d55dde9e1552b80c3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:34 +0000

    stare Bb sleep rub unkind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
