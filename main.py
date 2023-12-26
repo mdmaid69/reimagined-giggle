20c1063f6f47786ca79d970ef94a99dc264e7a1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:30:40 +0000

    quantity borrow basic keepup dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def remove_duplicates(lst):
+        return list(set(lst))
