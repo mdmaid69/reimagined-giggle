4d47c65a98f6fa61fde869c8d569e84a8cc90fd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:01 +0000

    make later room rub easy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tensorflow as tf
+print(tf.__version__)
