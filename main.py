000610903a115cb21ac0f53b8707a6372538f0e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:01:41 +0000

    offence clearly display date his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
