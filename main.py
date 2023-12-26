a716d02769dc496d800a45cef2da9e4b01d24ccc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:00 +0000

    swimming burn label seat pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
