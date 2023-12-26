99c50057df0a1d5a488acd2a8951bf738bb471d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:44 +0000

    profit pleasant giveup surface shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-text = "Hello, world!"
-print("Characters:", len(text))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
