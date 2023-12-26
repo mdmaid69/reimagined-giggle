b642bfeba23f149b07c9897f00bf39640db4b85e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:07:40 +0000

    contents print somehow choose forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
