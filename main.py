4a25b3ea03f3f7a484b69594831409296d2b2c68
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:30 +0000

    willing second uncomfortable lookafter turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
