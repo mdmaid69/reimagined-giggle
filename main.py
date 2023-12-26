68fc9258559ee30ef7d97dc881375a9aca4b40ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:22 +0000

    interesting far furniture exciting accident

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
