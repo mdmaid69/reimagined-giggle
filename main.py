185ec1805b3f1184e984a8b2eb455b03ed298b9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:34 +0000

    hang wipe equal circumstance twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
