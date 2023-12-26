87f09edb598231a721c56af3d7f2facbe29f193e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:36:54 +0000

    share report system heavy during

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-print("Hello, world!")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
