cbef23f036144fc325d923212631507da6e8bcd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:19 +0000

    mean bite authority talkoutof bored

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
