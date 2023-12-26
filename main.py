e4442bb42e82494e3c292ca0e58623d54837d9b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:47 +0000

    what spend turnoff doctor general

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
