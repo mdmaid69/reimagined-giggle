7b7999ed25dc8a1561011be65ab118e69169cf74
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:14 +0000

    touch authority furniture near decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
