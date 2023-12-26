28815f2c56b6762cf4848b5dba91b8f08c8d45f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:01 +0000

    distant favour art guilty moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
