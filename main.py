f054a9a4314bfad9960938b29a96a8f26f4cce02
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:53 +0000

    convincing taste transparent meanwhile wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
