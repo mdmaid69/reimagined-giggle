436bcf046f67aa73401350c62d6d847aa3c2b566
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:14 +0000

    hospital badly around murder necessary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
