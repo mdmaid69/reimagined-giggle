143ffd5a1784fcacf7c9df54a6852d11a6f44173
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:35 +0000

    demonstrate foot dear lock complain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_circle(r):
+        return 3.14 * r**2
