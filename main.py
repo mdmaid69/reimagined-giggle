a522d77e101cdfb14596b51f2f1b8074983b5ce8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:57 +0000

    giveback proof guess the effect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
