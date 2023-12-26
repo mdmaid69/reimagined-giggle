f2b429d713e8d11bb79a26b6b434305637a53fd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:13 +0000

    both excuse regret need later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
