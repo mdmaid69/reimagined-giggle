5ab80494a13b350c40b0fdacc3b2ac61bcfcbd9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:43:04 +0000

    group gas special properly scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
