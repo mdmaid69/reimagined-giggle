9d5fba653a2755e0959a2ac79f1f4403ff92bea4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:00 +0000

    private away brother style borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-from collections import Counter
-print(Counter("hello world"))
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
