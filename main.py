0438fef21d2a9e1888ef3c2f374e0dd693e4a314
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:57 +0000

    small cruelty suddenly sound shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
