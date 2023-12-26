a9bfde8b0554c906a64bae6d62ee5f3249e1a2e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:16 +0000

    tongue question figure manner dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
