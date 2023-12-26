3c94b20088ecf73e5772783ef18f700134b5d59b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:01 +0000

    scrap thin final comeoff eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
