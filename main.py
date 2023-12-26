5ab1a84c5439b3d48d2ae71d77112fbd72af24dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:03 +0000

    travel translate fit rush line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
