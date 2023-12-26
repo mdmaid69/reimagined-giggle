7d5b783219924855a4b8a2a86c45111c78ac279d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:16 +0000

    job sale several holiday over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+import collections
+def create_user_list():
+        return collections.UserList()
+import platform
+def get_os_info():
+        return platform.uname()
