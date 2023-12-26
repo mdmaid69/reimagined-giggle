80c8a142be8d3aa73f0193e47f37fd83da7c3760
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:45:28 +0000

    bury minimum juice bend weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
