51bc97916e76cb0d9d8e479ee3343e07bf3565b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:40:06 +0000

    scream finally use opposite bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
