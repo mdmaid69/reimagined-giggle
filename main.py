9ea70669625a69aafdd85d342d7935fa7d421d19
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:58 +0000

    continue birthday combine which more

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
