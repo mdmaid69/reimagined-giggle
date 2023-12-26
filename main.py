55e46094e40f03ba72a3a2440d89aac542593083
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:34 +0000

    feeling wait cheque constantly raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
