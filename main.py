9f927cabce84a1a55e805264371660608de05834
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:17 +0000

    power related jump scene own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
