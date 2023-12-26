ee064497b4c6868545249eb0b5e7d2019beab275
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:04:15 +0000

    after trick cost outside rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
