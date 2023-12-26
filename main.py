125475e38185584e722c42fcb43a6bc5232f937c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:08 +0000

    stranger classroom Cc holdup record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
