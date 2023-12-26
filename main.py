1e6859ba7501919751dedd4f3101d16a39fa9150
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:41 +0000

    situation divide ticket definite watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
