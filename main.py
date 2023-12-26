e6cbe6bf653354b1790786de72b1ec9bfad96fc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:36 +0000

    nerve imagination intend majority turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_unique_words(sentence):
+        return set(sentence.split())
