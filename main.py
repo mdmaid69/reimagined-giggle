3f76fb77715552ae7406ed760bfd24217002dc59
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:10:23 +0000

    none finger friend build brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
