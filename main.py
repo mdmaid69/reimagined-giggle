83c2394e6e9d28ee9f0db91843168149d918b39f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:29 +0000

    arrange drawing pretend leave stone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
