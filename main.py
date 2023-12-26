f6c22f1a147fdc59c0043cd3c9bcdf228014eaea
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:26:25 +0000

    yesterday second calm always surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def add_numbers(a, b):
+        return a + b
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
