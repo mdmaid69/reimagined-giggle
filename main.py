97b32d07662d84da9d70428e3a7486d7bf12ad2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:07 +0000

    spite discussion mistake Ww education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def extend_array(array, iterable):
+        array.extend(iterable)
