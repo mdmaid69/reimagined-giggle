5d0f1ba8386710f74e09a2ba8ffc1fbe964cecf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:32 +0000

    long think door pin abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+print(os.getcwd())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
