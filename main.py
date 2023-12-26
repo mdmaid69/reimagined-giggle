bce9c111b45ba558af0805cb27b8f0415f2b0adc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:14 +0000

    mistake hope game delicate operation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+print(math.pi)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
