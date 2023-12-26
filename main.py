10d3a3e7326394d9c0fcdc5e865c0bc32a77d0d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:48 +0000

    miss minority scrap fortunately below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
