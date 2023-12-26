994d95552edbb56332b5a248bad66b6e42f3cee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:40 +0000

    avoid friendly news guess typical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
