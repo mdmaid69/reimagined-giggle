cae490bf4eddb147df7d5a4588070ea28db4d4ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:52 +0000

    run subject wake arrive strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
