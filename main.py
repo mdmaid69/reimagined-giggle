861fbdad091365895ebae532134d6e95b3af0475
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:53 +0000

    properly considering plastic thinkover allow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
