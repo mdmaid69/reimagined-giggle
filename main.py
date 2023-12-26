ec83aa5e910416e40dbe4adaec779df21e1baa3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:21 +0000

    preparation squash free force free

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
