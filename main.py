8b0b148ab612469562e5941d8c760a20daeb2cce
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:09 +0000

    forget considering step swimming church

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print([x**2 for x in range(10)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
