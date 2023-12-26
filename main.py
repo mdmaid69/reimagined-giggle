21c18aa8cafd80b833c4f1bc9e6dad00919221ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:19 +0000

    buy style repeat grateful weekend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def is_even(n):
+        return n % 2 == 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
