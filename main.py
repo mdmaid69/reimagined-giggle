bcc95365a03bafbc39f76e207df403672a382358
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:06 +0000

    most breakthrough lookup couple turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
