86d2e962016099b3654c00ccdee966985042ca0d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:40 +0000

    female partner send left fill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_int(array):
+        return int(array[0])
