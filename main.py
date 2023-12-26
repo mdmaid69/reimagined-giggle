3dcfa3802a16b420411d6c4ebb2dd4148ceea555
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:24:47 +0000

    name cross join wealth teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_item(array, i):
+        return array[i]
