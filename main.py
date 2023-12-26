c47326db9f57232094ba695d8fb46b319d11829f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:29 +0000

    lesson lead thirsty target factory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import logging
+def log_message(message):
+        logging.info(message)
