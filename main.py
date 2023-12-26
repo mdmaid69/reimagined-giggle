2ecbf92442a13062f01bf7cba7464b0a99e17cd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:27 +0000

    hers depend unite probably meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_even(n):
-        return n % 2 == 0
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
