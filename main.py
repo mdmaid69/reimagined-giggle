38882f24c041804c020d5b25d3c99248e9f39d58
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:32 +0000

    creature responsible practical name relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
