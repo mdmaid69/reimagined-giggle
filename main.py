4ffbaf6ec69a327c5be924a159316183ee27a298
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:58 +0000

    undo position gentle reasonable anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
