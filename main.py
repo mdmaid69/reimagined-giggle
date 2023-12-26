5abf8b0a10fce617d42e350029c652030c72fcc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:32 +0000

    trap practical valuable Qq previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def square_number(x):
-        return x**2
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
