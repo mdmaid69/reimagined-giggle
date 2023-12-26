3c1d41398c698e1a228a3cb33f1458672970ceba
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:32:18 +0000

    lookup someone remove ring weak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def subtract_numbers(x, y):
+        return x - y
