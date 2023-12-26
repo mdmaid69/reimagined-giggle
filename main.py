da8053bb153fc625862e4a225a88c393cf8f03e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:29 +0000

    feeling form performance circle legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
