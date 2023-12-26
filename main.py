fb67304a2de058f6577c51dc403f6107e31b3c9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:49 +0000

    suit suppose male advanced spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
