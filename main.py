0cba3807606aaaedcda63d7946c2f5d8555e1060
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:31 +0000

    upwards badly naturally hand careless

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
