1b88d4ff973edeafee4cb9c2127c003a6d077e08
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:29 +0000

    problem command complete fine mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
