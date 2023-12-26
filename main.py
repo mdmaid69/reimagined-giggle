b709d96eb78e4c9ef96767ea441038f02739a0af
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:52 +0000

    impossible plastic now somewhere flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
