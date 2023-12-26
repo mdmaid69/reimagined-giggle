b80d4b8e2ff72f79ece7d7416c2d81495500fd64
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:29 +0000

    control watch feed event work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
