f28ad94cc008c3bdfabff6d33fbd1379357f6c93
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:37:29 +0000

    bottom very move object trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
