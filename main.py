fc006c8cd4b0834281afc7adc8196036d41bec1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:26 +0000

    condition jump pause example active

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
