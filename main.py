c024451847b006366c9a7cc8df7527c0a0dd9eaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:29 +0000

    workon right gentle calmly comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
