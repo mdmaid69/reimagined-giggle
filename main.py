e47da3a38ec90b96d7eac5c144d4a310b7d9c666
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:34 +0000

    appearance turndown active go preparation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
