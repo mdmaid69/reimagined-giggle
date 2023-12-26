688184ab9a20a87c3a01055d2761a540ca9f85ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:22 +0000

    dish terrible no special maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datetime
-def get_today_date():
+def get_current_date():
         return datetime.date.today()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import re
+print(re.match("h.*o", "hello world"))
