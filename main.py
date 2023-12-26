348b62720bd18c88791809b14c161482f58a5084
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:58 +0000

    hate gift smooth provide prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
