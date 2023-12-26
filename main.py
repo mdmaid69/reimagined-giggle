9a0dca9d0c7562265458dd4bcf255d1bc8603b07
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:55:14 +0000

    walk rain task wealth fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
