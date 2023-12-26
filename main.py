6ede16768cd5b7447c4096c0a0670edb4c248f3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:18 +0000

    thank reply 179bite week certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
