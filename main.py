45e2ae6eed1f99c73fd03fafb89081122ae4072b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:51 +0000

    secret shock doubt heat generally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
