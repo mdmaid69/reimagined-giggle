fec13b57b3e5eb1909fd74a43a7a752e96e595a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:45 +0000

    play confident comment equipment custom

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
