9b54cba7d0087223ee4ed6461999cfd007fd83bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:03 +0000

    callup deserve joke stop they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
