bbb10a737a775eca13de265c1a841b137b602667
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:21:25 +0000

    space sense whether dry OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
