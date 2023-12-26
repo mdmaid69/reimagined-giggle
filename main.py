26dd3ea88d9bb0693a48b1f3ff4803dc37134011
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:59:48 +0000

    meaning border report flood book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
