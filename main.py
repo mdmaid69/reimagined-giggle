35741fb11f155fdabb2901ba814dd5a62a3dd34e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:45 +0000

    fear band common drop belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
