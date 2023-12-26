0d3f4d1bfdb7f998628c25779d23a310886275b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:56 +0000

    memory dream face or reject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
