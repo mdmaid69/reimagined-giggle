15ab791cb6f3fdf0791c16190cd8ef18d9d69759
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:18 +0000

    573entertainment maintain interrupt cheat building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
