15005d1856dea3393d1008f81dd671b69d6de64c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:04 +0000

    come right maximum breakthrough comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
