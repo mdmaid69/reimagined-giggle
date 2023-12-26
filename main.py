c1a362168c84e7a91a01f7e1836d7ce03e7f3ebd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:36 +0000

    taste bang basic free getup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
