e383f2c1e3b0ab159600e2d1ea50989c96f5cd58
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:29 +0000

    survive foreign comment sense blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
