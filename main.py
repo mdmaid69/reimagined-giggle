1602b0a57c7aa46be49e02dbe71ca6a490ba36e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:52 +0000

    broken finger until shape invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
