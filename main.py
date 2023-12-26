be524fa2437c83c359ab999ee5f7eccc09f39f0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:59 +0000

    this noone matter protest new

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
