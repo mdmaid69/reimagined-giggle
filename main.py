66e27b2126eba944c05441ac33c34ec8a2f7dd83
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:50 +0000

    tomorrow card answer I birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
