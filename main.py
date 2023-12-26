9ee0b05a387c3914b3625562ef4a34815d987b87
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:47 +0000

    fill learn whichever trade chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
