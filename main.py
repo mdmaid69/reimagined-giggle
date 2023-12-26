b8e7cb921d17d404c2bb8777855b865f18f981ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:06:30 +0000

    tired quick Gg judge me

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def divide_numbers(x, y):
+        return x / y
+def calculate_area_circle(r):
+        return 3.14 * r**2
