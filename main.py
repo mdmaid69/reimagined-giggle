13cb79b65976b2dd3ad358789d81f2edccc8af28
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:32 +0000

    children public far tomorrow age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
