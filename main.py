16bd4c9e2be8c508316aeb64383eb734b3a9bbcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:58 +0000

    hurt file protect moment male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
