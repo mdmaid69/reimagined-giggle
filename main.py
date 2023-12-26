e0dd68223d942241445cfe039d3d798ada267eea
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:17 +0000

    middle professional friend idea warning

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
