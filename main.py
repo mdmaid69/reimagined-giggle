a00bf253ac9ecce4d8bfc4e9a7a94ff3b628167e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:43 +0000

    birth frequent object callback aim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
