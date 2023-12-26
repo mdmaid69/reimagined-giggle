78b253ec9a97644cd5f8e6e620b71327992fcab6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:03 +0000

    bath total relation half only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
