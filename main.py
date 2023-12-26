1f21a65d658b6a49690c726d38dee6c76889f5e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:03:44 +0000

    factory mad create thinkover wide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
