c092fc58f0b7e541096dcf459e7b757c716da677
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:20 +0000

    train lock brief her distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import json
+print(json.dumps({"name": "John", "age": 30}))
