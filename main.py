9202c26fe5392d422959b6aa28ba0cd97c45b050
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:53:34 +0000

    special group soon down mistake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import sys
 def print_python_version():
         return sys.version
