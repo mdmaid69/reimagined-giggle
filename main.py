9dfc1387febad57a4ad5108f587e5cbf0f99f95d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:48 +0000

    convincing tour same talkinto yourself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
