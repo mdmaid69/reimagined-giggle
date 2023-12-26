5a8231c5d12e9434d832bf9fd1b7aed61b88a001
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:26 +0000

    makeinto shall win height file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
