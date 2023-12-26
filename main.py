c16077e6a806d7d7264b1d0ffa5f422346821371
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:12 +0000

    main Qq behind during whistle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
