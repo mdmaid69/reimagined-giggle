fbe484b5005bd05b6d9d1e5ab818ce8368e6c1f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:46 +0000

    past club correct defend urgent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
