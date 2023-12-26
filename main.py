150197ef78d369c30f02ad6cb93c2456c94a32a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:07 +0000

    wrong constantly through real taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
