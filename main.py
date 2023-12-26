5ee38753e2f5819705bd398afffe6878502a55b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:43 +0000

    say Dd every project combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
