076ac79b6d44c0eb2971cc8a80cb9d39b8d89116
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:23 +0000

    blame nowhere accept rent table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
