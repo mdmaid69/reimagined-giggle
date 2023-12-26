25ff0e82e4a6add73c9bf993d7306054571d030f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:33 +0000

    concentration hang sometimes shelter steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
