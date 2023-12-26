ba1e97a89d8fac3053cb4965df517669614d0e69
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:19:18 +0000

    swimming uponon exercise immediate bush

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import tensorflow as tf
-print(tf.__version__)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
