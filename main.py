9cce01192bc89a6e186057cb64e6e5c158b0a7f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:10 +0000

    lean message workupto Kk permission

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def square_number(x):
+        return x**2
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
