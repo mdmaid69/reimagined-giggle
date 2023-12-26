2b37907001e1cf791054962c98af15747ef82198
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:06 +0000

    treatment personal deep famous threaten

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
