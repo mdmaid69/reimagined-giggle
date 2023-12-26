4fa06eab0a06dc6259eb7b99a71646de5a1f3289
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:38 +0000

    public transport union well scrape

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
