57f4a465ace76f3b83f855e4cb337eb57ce34167
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:47 +0000

    badly compare release weekend section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_current_time():
+        return time.time()
