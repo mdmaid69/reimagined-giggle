326a8550022e2851af81e00ae883375372df4c6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:56 +0000

    mystery guard black furniture could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
