e06413a476014abc2bfa7eaba0683f6b28430ff8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:23 +0000

    experiment leg surface frighten finally

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
