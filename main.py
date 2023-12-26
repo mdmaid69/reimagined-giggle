b98151776ed1406b22c0ce939b9b506b1962f90e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:57:25 +0000

    about how confuse work store

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
