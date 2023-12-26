f7f210e4c1b6662c229b88ba53233cbdb4c14cf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:52 +0000

    back tall intend road brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def is_palindrome(s):
+        return s == s[::-1]
