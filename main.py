3f9f61550b277cb3f77980d96c4380235d83aebd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:49 +0000

    yesterday respect worry spray crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
