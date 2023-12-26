102660224379eefdb5521072bd4ff7f69a795712
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:21 +0000

    another goout close for develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
