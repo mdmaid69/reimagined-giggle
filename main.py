76b86aec9b42e3e57f666ca3e570351ffb9b1ee1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:01 +0000

    permission behind close giveout Tt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
