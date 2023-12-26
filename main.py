4b8ed83994e14fd49778e2dd6831c4892815a7b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:06 +0000

    effect nothing tell accommodation include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
