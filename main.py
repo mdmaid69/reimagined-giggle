f5da6caa22962267af9504243e8ac1ce9fca77d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:51 +0000

    serious sort glad interview high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
