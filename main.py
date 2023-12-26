e698815b4f703d375c8772926e4b6258a55818af
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:19 +0000

    interview quick open rest return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
