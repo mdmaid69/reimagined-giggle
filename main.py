8e1291f59a5ff259aa4ffa4c9e8517c9e5c2a11d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:28 +0000

    tidy stiff fact about everyone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
