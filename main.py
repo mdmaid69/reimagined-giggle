5f25cec2cce931d075e08d08f1a92f910125c258
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:07 +0000

    winter dare thick possibility write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import collections
+def create_stack():
+        return collections.deque()
