20a0e02d9c0631d408e7b83008f3f80963ca0ab6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:26 +0000

    phone part woman during moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
