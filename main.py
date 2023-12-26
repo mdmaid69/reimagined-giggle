0bd510cba38921090b6a1dbc88aac66d7dc4e551
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:51 +0000

    path imagination fire pattern scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
