63b50b929af7f56148c2e51546416331ed2d970f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:54:12 +0000

    main apologizeapologise key fail pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_base_name(path):
+        return os.path.basename(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
