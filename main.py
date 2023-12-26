1e2fddcd41bb594ccecbb515411bc1a55439f0e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:38 +0000

    bringback bag figure emergency before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
