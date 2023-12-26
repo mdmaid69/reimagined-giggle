55906971fb3ca6bd33bab87010a43d49e3418744
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:02:19 +0000

    them exact thanks reject wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
