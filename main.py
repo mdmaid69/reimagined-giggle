eaafc4164e391de603f88c1ba53806a05a45f94c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:28 +0000

    line crime art hello market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
