1dda20cc1e46038663baba647a7e149a9d8984cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:56 +0000

    miss name arrival drive tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
