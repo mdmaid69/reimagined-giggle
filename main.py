32ae3184595ece8fadf8bf0c8e5267002552d4fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:34 +0000

    morning reason geton apart birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def add_numbers(x, y):
-        return x + y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
