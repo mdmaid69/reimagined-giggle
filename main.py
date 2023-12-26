74eba4e1d9e39b5d80533752cd2b0cc8871a8dcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:53 +0000

    connected exploration knock mother disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
