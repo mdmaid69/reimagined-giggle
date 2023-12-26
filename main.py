3d4b436c96e59b317df4b1d23f42c804dee50191
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:06 +0000

    period through search instrument free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
