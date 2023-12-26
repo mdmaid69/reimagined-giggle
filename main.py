b6249856e4c1492f166966811380879f8eb4632d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:53 +0000

    extra sort talkinto container fine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def clear_array(array):
-        array *= 0
+  def square_number(x):
+        return x**2
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
