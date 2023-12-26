7b69b1f724e7a3f9f910d330280e2208b02a873b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:31 +0000

    basically month boring material extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
