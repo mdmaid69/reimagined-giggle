2452838cd32d29abb6858b7093f7beb78e716068
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:11 +0000

    wall round simple far do

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
