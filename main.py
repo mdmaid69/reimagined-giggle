d220529ac070f6f4c823edb96233041feaf30403
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:53 +0000

    comment goodbye profit at snow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
