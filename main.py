608cf6f81f0012432be481b0fa07212ea9b94feb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:50 +0000

    matter suggest brush birthday connected

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
