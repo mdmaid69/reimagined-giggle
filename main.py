1d7cbb7efe2ffeafdd5b1f47202ea67c465b8d1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:20 +0000

    expect pool subject nurse past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
