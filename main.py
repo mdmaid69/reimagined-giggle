683a95b94d00c33cd8f5920addc4d9f8f1fcecd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:45 +0000

    weekend slice against wake interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
