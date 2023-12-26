3f6269e2b20d6b3e9f7c5a6f32e778b8c3da04af
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:22 +0000

    concern declaration press disapprove rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
