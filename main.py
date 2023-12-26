6758e70dde48ef329d7ffc8acd27df0c42fb6c35
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:33 +0000

    Ff somebody each damage somehow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
