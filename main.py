5deb5fb922ea2391305f4ec5e5d7b0b58cfa459b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:26 +0000

    that flight belief discover hold

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
