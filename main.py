df7615efb6530daa07c53a5dfbddf0313786c607
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:04 +0000

    pronunciation rent substance who quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
