a12064b08a175d7b0b36b9f8e400cec369e73ad0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:10 +0000

    address whistle holdon left concerning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
