22ec964f0d26e5160bb6e8b5b0e3a9345a7de3ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:49 +0000

    field quit there pass female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
