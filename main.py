5963584be93fa4058e31721388ed2107740d9206
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:55 +0000

    important temperature strange 573entertainment whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
