799ab8549691219eb3edb364aa5e1249568cf88f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:56 +0000

    otherwise punish past already office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
