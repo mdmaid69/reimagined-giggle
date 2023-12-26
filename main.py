a57492acd0fbbbfc083907ce961d9ff0ba2f3095
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:31 +0000

    meal tall clean talkback will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
