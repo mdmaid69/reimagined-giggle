047e187c5a2a470dc4dceb489e36878ca2a49d9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:22 +0000

    play judge truth difference book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
