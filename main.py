5e3a662acb4266228e8e2e51c79340461814393d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:36 +0000

    paper clever glass unable duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_density(mass, volume):
+        return mass / volume
