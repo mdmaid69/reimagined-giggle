de88c7a016ccd31d805fedce535d288687dd2f0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:17 +0000

    lose speed always country as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
