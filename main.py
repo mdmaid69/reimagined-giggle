e887f3ec0bcdc55260d9c85706b946b081be2a93
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:47 +0000

    shock contents return there full

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sys
-print(sys.version)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
