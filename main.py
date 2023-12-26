d5ac8f9410df4c7966d82895aeb3ebf001f11668
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:22 +0000

    off offence sail Oo often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
