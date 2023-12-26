bbb1972a204ffba3d5be78d83a385c2d5d204bb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:00:22 +0000

    habit each praise disappear ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
