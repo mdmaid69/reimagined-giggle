55efa7a0e57b9c15b38bc0258b36be763c13c1e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:51:00 +0000

    ring comment Cc could Gg

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
