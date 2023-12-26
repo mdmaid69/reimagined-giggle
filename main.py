148d395526211b03084d5bda3166219aed0c026e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:57 +0000

    suppose division best copy tone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
