6cd56d96258359ff58a7f1c5268245c5361773d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:27 +0000

    I finally afraid nothing Uu

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
