e69fdcd8b215ae3d488f592a5ceb278d96fb2848
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:13 +0000

    eat explanation hot the land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
