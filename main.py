41f0fcd27d5013d1f0fb4162588980c9b1e75f99
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:47 +0000

    petrol possession sink difficult control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
