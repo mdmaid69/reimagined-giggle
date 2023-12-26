e78a618ccd05f89ae91e390b75fd19aa79a148f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:46 +0000

    to level unit gift today

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def multiply_numbers(x, y):
-        return x * y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
