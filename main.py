c2df2310f0e76855d357db17b4b047d529ad3336
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:57 +0000

    run makeinto spin perform that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
