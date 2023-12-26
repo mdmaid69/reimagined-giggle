f7277c3e3ddc29cca8d235fbd9b2612335af5499
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:52 +0000

    choose band succeed under sign

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
