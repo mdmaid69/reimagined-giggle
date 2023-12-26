3a5e84e6e28b395635c64a302e0afd58095d29a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:00 +0000

    with keepoff maybe look convenient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
