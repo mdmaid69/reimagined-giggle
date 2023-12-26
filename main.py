846af7efada29dd0c2d0ebb2279ea69af0e2186a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:06 +0000

    how any ball post education

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_array_as_repr(array):
+        return repr(array)
