48a03fe0b75fb10f9018d4d881a6086944b0a6a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:56 +0000

    wrong view to command patient

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def convert_to_hex(n):
+        return hex(n)
+import time
+print(time.time())
