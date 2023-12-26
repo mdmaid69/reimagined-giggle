756f17af5e43b463164b47cd49c4e09ef0034f47
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:32 +0000

    niggaacademy very mark store runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
