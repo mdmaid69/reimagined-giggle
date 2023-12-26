aa128c79389914dee330edcc3f09ba35c3c50be6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:38 +0000

    organize always degree medium goon

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_max(lst):
+        return max(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
