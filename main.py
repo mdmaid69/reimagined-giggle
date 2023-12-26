81856c3fd7297dd713140a9e6eb6e779b8291629
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:01:06 +0000

    treatment track borrow official argument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
