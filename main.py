148d23de885bb5123a130970eb864c8e8576c179
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:24:13 +0000

    although suggest only coffee offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_rectangle(l, w):
+        return l * w
