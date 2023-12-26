59e1041528257ccf835c174a6f6c37f7e2dd05bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:34 +0000

    callback culture meal deliberate point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
