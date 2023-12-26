fe002d11c40f03b3822ad58504bcd9cf68c0780e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:00 +0000

    contact urgent believe never drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
