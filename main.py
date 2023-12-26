5a357a44a03d8f9cd2409200e7201f7f76fb1009
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:56 +0000

    anywayanyhow while between name wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
