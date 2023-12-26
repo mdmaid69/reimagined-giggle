83146eca6f7f28d4f45efb504753c900b0115908
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:31 +0000

    immediate operate judge disturb trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
