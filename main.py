a093c912322d26a56be609cc69d099568005080d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:22 +0000

    music how succeed price principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
