644cdfbd22a0c4ddee5de67ea080f2e5f565b6da
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:44:38 +0000

    instrument instruction word it rent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
