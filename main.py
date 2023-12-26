1b1f48240c99365c670611f0f8bfe331af4e3ec6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:06 +0000

    question regularly guess major rubbish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
