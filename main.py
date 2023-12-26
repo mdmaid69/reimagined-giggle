aac394c5bf5fc16b1a0114cf2fe02e77cec5ddc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:05 +0000

    lost rent valuable combination gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
