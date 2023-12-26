21bff80a46d74943f7022323f7928e565dd41567
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:40:40 +0000

    Kk permission thirsty breath mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
