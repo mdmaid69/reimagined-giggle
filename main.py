9cb9cd6fb57c4b82ed93d3a95279af4048189b62
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:01 +0000

    jump welcome memory occur stir

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
