fa98c6c2e5b2e7778a1ad97de466b21111f76846
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:48 +0000

    physical wish believe building voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
