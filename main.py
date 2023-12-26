aba20f5e682b0e22b328193fb7dce24230aeaf25
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:09:24 +0000

    cheek congratulate crash feature bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
