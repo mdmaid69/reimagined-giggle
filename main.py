14529050e86a184b3083299d2bf5dfea60fc2957
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:53:43 +0000

    comeout convenient limit solve see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
