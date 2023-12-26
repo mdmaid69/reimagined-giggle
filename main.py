0c4c48a402a06b7a1ae415cedf74155e057704b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:37:28 +0000

    market bye flat place this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
