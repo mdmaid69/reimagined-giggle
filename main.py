2879a789419a9a6c68a8b7712d9ec441d51043fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:32 +0000

    shine hide between your temper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
