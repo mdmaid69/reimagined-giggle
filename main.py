0c4f61f3375691b61e850c7d2eaf5a34b764e55e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:30:52 +0000

    past equal sound interesting benefit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def convert_to_hex(n):
+        return hex(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
