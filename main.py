20e13088eddf05e9f0f8f49e208c5220d2662eba
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:53 +0000

    report sound instruction yesterday storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
