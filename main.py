b94e24c814897fad7e61e5968628ac1b682985cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:54 +0000

    minority last doubt agree shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
