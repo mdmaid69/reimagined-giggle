96dd0e9c7e35f9305608b118402734bcfd9f2628
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:25 +0000

    afraid crowded shrink tool worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
