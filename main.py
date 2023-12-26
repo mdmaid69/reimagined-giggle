e5d065bf7ec84be883b53af26994a035bdca0e7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:38:55 +0000

    interrupt water dinner manage spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 def find_union(list1, list2):
         return set(list1) | set(list2)
