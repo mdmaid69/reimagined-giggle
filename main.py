be8423f3ed313c604f1c57480a5b90212c9845f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:35 +0000

    past shock temperature valuable along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def convert_to_hex(n):
+        return hex(n)
