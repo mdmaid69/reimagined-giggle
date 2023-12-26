dbaac6050a15b92a9def392c82eb38a22eeb6731
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:39 +0000

    band separate pool absolutely once

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
