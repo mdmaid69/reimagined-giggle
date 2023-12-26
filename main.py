7bee1e9026fb709f7a5fdb86becbb2da6d63fad5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:10:09 +0000

    suppose silent challenge improvement why

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
