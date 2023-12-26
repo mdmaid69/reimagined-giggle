30102c3fe392023b51898e28b883eb633aeefbeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:20 +0000

    authority reaction someone wave poor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
