da35c2ad11446fed069eed4a277d10e7cf25b721
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:22 +0000

    less breath subject explanation join

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import re
-print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
