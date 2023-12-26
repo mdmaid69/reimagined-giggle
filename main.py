253f389f759082541444e08afc2ba1ceb8683a1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:08 +0000

    offence spot declare failure fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
