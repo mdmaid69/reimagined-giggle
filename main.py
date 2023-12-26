78f7999bf5cd738eac11a5a9e842ad0c202a761f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:59 +0000

    rush answer brave accordingto cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
