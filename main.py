916d41a3fd8899392438ec02caa058349f9e44aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:59:38 +0000

    spread mine correct under noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
