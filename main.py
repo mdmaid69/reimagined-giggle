4122c16449a1b865bd69584412d4923d35026672
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:09 +0000

    Itself knowledge rarely long criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
