86a1c4187b9612afc399c2866bec8ab92c4c5866
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:38 +0000

    break odd shrink care godown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import os
+def change_working_directory(path):
+        os.chdir(path)
