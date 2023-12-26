692845dade980ee24d7424380087582f5f55cc7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:40:33 +0000

    amount report balance motion trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
