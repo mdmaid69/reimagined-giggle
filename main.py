780aac31c70c2087db472e3a01528d5da5703b7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:52:15 +0000

    thumb public add correction I

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
