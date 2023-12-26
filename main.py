ed627357838d75ee47d462d36940ddb8d7f9dcec
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:20 +0000

    deal word giveup leave commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def calculate_area_rectangle(l, w):
+        return l * w
