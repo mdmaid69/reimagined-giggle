25fee56995abfdc983fd3dae74baae3024bbabe1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:46 +0000

    pinch somehow over telephone worry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
