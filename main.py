1017ada9e8c706aac8b46b42a6ff087987f5de70
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:00 +0000

    thinkof they relief recovery decide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
 import array
-def clear_array(array):
-        array *= 0
+def get_array_typecode(array):
+        return array.typecode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
