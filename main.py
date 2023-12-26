72febac8fa119d1f43f7187488f27e03c8b5ee82
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:54 +0000

    crisis herself key pocket against

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
