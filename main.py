dd98871260cf0df08d5bbaca3f951a683fb026df
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:40 +0000

    talk how slide pot send

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import re
+print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
