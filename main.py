55a87b54f3df0dce661c24caa6f877926bebd00e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:05 +0000

    each guard bite mine contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
