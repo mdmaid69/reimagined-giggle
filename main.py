4302ff3b84c92dfe72463e1c77dda3c424821929
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:51 +0000

    road turnon your whistle comein

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
