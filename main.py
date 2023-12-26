7117b0d22f35225992022f0c098b302c64cb31f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:53 +0000

    tonight bet hurry rescue or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
