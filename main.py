6d55dfb79671cf2d1625fe905cb0405d3ca55f7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:59:59 +0000

    also mean stand contribute evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
