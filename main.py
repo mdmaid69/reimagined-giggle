785b5d9e4322aad2d1ff979304f4d1c789ac63a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:45:20 +0000

    point whom family cruelty any

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
