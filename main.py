521fb62ee67d14bc842bf0eb664a54553108ec56
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:53 +0000

    test ask stare present pile

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
