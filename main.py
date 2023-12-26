61e78f739d36695a26eee724886864ec6f145cb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:48 +0000

    radio him unable pretty smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
