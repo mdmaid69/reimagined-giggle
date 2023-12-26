81da1c8d91e0701f9eeeb015ea6444aec613326b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:00 +0000

    us finish watch weekend embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
