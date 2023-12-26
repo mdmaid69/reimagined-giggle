5666d3741fe0571fde85104f4ee98a88514f09fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:18 +0000

    miss back reduction wash talk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_even(n):
+        return n % 2 == 0
