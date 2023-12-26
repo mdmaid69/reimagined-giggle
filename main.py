1ef9713cfa67cb0104ed7527a26749fc66ed6d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:10 +0000

    group drive true invitation near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
