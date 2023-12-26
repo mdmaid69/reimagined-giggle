2f312ee45a9e9d34e4a854918369a244ae6cd52c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:26 +0000

    drink record rise treat drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
