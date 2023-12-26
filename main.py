bea5bb75af3f1aa6cf0db61df854795e9433f4d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:22 +0000

    safe sort action bury common

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-name = "Python"
-print("Hello,", name)
-def calculate_acceleration(speed, time):
-        return speed / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
