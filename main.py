1d16a5f033dc7752cb09c24729f7c2d23a870ffd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:28:45 +0000

    able out rarely wind praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
