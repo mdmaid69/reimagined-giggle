d78b27268e4fac5ffa191a0c92401324463aaa80
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:58 +0000

    such uncertain certainly answer furniture

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
