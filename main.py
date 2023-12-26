e671f4f56f123f163b44e8446d9f4b2268fb8f7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:38 +0000

    control learn above shape sport

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
