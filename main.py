0a0527c711bb37ab39e266a9d4c90bd9ac467777
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:49 +0000

    wall same loud favour determined

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
