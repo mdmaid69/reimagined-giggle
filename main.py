e2698b1696093c0e5bc95476ad51eded5d9ed946
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:59 +0000

    somewhere number beat vision plant

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
