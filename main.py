82f1201fa211aee345478f20939c3ac120cb213d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:33 +0000

    word round up empty conversation

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
