7ada7b97f847c5f868af08d38f098aa4002a63c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:29:39 +0000

    bath wave key house considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
