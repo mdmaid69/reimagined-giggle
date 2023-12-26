287420dca3239d824b5c2f4dba2e31099d1f3436
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:09:17 +0000

    rid allow money force trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
