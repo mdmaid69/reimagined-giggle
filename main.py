099c16d4c02147a4a083abf16f8bba1a9b680c41
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:08 +0000

    notice taste rain quit needle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import collections
+def create_queue():
+        return collections.deque()
