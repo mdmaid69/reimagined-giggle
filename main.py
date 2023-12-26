7427769f1df3ab3539eff81ca84fc4ba97a0d64e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:25 +0000

    unfair frightened note common roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
