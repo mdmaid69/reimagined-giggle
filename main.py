b2c0ef3326d8d2760b229f0a832490377267577d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:52 +0000

    second active quality character everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
