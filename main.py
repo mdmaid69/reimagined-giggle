9f58bf9724abd4f285437ce1dd74e284d0533bdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:35 +0000

    treat sorry statement prevent mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-print(sys.version)
+def get_bytes_from_array(array):
+        return array.tobytes()
