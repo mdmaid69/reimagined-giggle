4a6fec6f3f09f187a6119c9981fe701ca25dfa5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:02 +0000

    improvement view pick soil offence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+print(time.time())
