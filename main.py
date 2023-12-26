4cbd344683b8c91bc6caedd75ac22d3334a16047
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:08 +0000

    answer rare wander invitation ourselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
