1f3b9d2f0da43ad130b8f03b4cd59b42417d56fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:18 +0000

    much goodbye lead make practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def reverse_list(lst):
+        return lst[::-1]
