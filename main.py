b73af149d609c0f5ef1c3ce7e191204069fea9f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:48 +0000

    finger switch survive either workout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
