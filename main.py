e127a5d246db126c19704538332a42b4e9675246
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:07 +0000

    permission machine course satisfactory change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
