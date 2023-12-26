c49bc8fc14a17d0d39eb0695e043ad8147f693b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:50 +0000

    surprising getaway destroy careless fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
