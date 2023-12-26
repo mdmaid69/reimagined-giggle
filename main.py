2203d576d39fe62c10b38c00b8982d5b2e334576
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:22 +0000

    substance until such majority fine

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
