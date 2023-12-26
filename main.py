443bb57ae0216abaaaaff12b780d198c674aa45b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:27 +0000

    behaviour ground scheme our black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
