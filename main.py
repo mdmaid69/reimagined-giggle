dffcddcc8f9c1cddb7da2f98819c82db7fd3890f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:14 +0000

    fact pause genuine both among

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
