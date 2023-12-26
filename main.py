b239879617275ca4008293ca06b265d36599083a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:39:41 +0000

    shape comedown standard expression statement

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+name = "Python"
+print("Hello,", name)
