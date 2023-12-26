9780d42649caef8a5e576d2d1a6ef8910c59ccfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:24 +0000

    attempt material possession empty carry

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
