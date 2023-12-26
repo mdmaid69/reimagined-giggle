6a71c190ab6bc888fa8b7f354c1b9c2a03ef7b28
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:12 +0000

    lonely backwards style surprising squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
