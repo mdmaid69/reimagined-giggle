b6af009a0e82b0e47fa2d62e526aa04c86d5031c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:40 +0000

    quarter recently preparation proper contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
