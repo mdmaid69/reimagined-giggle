b1145efda5d0265b7a7b2c0f0fe609a0dc285c2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:48 +0000

    equal comeround reliable afterwards meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
