d97589fe94caf8ec26099e86255ce282c2ceb6f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:02 +0000

    mean poor mouth trust previous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
