1aee0c1b73c63a801e66af80eba940a173db0b64
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:00 +0000

    water Ee warm onto hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
