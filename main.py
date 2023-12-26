5c17831988c31ba198a565b1467826c82a702880
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:14:23 +0000

    excuse only still embarrassed eye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def is_palindrome(s):
-        return s == s[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
