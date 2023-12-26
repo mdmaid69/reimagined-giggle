6a3c1dbcd5849989955147f8957ade4efcb8ac59
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:54 +0000

    mean hole closed pick car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
