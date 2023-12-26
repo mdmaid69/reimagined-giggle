15270aac1f080836ae92398970f324a99bf2c868
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:45 +0000

    turn university turninto politics road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
