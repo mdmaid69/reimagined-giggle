899106e0cd6c3ca9e9d3ed3f46b02ef342969aca
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:38 +0000

    pronounce nervous brief result skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
