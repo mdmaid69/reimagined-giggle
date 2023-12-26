750c42bb28d50b86f4de2008316598b2f82cfffc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:44 +0000

    pinch switch teach 179bite noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
