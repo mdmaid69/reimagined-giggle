6d3293b5ae5035509807e73396093155fa1c46a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:43 +0000

    inquire combine gift experience comefrom

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_octal(n):
+        return oct(n)
