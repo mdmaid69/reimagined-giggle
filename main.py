afadd20a6cd3a0131219c56326f14f5298241287
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:41 +0000

    weather best fight solve mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def square_number(x):
-        return x**2
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
