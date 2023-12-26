8717cc84001e07350447d0403309898c14d79dbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:46 +0000

    immediate comeover warning yourself side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
