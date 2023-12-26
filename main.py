7bdc0dd42d80f5c1709f1d812b9cccea33984242
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:54:49 +0000

    lookafter table rid study value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
