6e9e8e68f3397ed406eb84bcf4429be26b5e1b8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:40 +0000

    tomorrow conclusion tragedy say wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
