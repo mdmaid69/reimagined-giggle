9e2edf3e9fca6fdc584b47b85407fb7010dc63c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:04 +0000

    share loan level tear test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
