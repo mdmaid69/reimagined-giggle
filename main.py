39c63170f5915ffe3e160510d8cdacf457da99da
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:25 +0000

    joke mean cinema little clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
