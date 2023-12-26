99dc83af4b0180af0052c439b413dae0ce50872b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:21 +0000

    war main trousers stare lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
