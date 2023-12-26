6ac532620fbc3215c4460060994bf4c6cf88f1d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:17 +0000

    lock bill Itself different draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
