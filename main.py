5f3e4dea016f1ef3b45be2f8723af66d86d6197b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:41 +0000

    sorry prepare everything operation equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
