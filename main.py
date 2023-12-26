1e85061a4bdc7d3ef212cbc10be3506870001e74
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:07:41 +0000

    club shop may draw boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(a, b):
+        return a + b
