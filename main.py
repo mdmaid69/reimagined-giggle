65998e8febe7ae2e252bae56097c174532c47896
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:48 +0000

    soft original range blind succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
