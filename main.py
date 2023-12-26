66579d7354a7968859effd385e07a3e949adc7b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:28 +0000

    practice centre mad build sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
