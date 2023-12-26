e15c825b9881db9d94b916f3d8c52a2afb542081
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:33 +0000

    sleep reply rob basis collect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+  def remove_duplicates(lst):
+        return list(set(lst))
