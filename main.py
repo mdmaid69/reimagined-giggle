9385e01e46ec7e246f60f158a8182edf5a0688e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:19 +0000

    thin respect wish takeover sport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
