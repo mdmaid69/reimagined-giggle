148895fc44f018eef79edb809d3a7d2aacbb8f0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:55:31 +0000

    rest giveup cost piece takeaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
