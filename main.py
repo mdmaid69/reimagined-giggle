a965446d138a9f3bfab29fb3a0d024cd95958df3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:23 +0000

    sting scale except leave figure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
