326efc662ba22bf0d925a7da8d18aa50f7982b2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:10:37 +0000

    experience drop crash takeoff opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import os
+def change_working_directory(path):
+        os.chdir(path)
