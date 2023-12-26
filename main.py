9f201ed13d17fcaa33fd8bad72b579ae4921ede3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:47 +0000

    shoot uponon time soon fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
