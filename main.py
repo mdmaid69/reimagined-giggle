2efa8d1c191f5f8a31c9d6a11283850865e441f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:22 +0000

    Gg more ball again soil

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
