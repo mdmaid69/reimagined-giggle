a800bf9985f9598f2866c44ff3daa6f2e4bec6f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:04:12 +0000

    second organize cut bus climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
