6e846986270d2ad735fee3ba89f6ae2f530fc4e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:37 +0000

    vision name settle team active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
