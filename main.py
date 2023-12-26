d67fc1b531c15956ffeeed0fcd22e4524d1fa485
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:03 +0000

    anywhere tone business flat swim

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_speed(distance, time):
+        return distance / time
