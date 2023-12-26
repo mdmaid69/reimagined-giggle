abc8cb244d62363c370456cee4a85746a17a4d53
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:46 +0000

    happy system before relation traffic

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
