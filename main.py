982bb2e04306b37223f3ec8101cb6dae39be436f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:24:31 +0000

    poison schedule copy pause left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
