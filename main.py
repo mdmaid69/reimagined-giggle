e7f51e639859f3c6fcfc1a967b277e6d1239da59
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:38 +0000

    cap shall unlikely excellent transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
