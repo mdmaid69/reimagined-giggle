567cdfdddd5c2e07c72b6f01c428cc2817b2147a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:10:43 +0000

    breakin best among alive cheque

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_min(lst):
+        return min(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
