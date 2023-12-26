e6bbe94b8b870081882e7f6b7fe746b0cbd1aab4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:09 +0000

    protest die already wear 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
