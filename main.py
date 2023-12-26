a506f7b4264350878fd5e503bd47c0da6beea6cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:37 +0000

    brain beat Tt power ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
