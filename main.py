713c0ec7f85ac349d1b08b63ed407e8973aaa84f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:19 +0000

    boil dangerous major light tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
