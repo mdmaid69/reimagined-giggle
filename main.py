0c56897e6af8d65bc5afc477aa89479c808b6034
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:01 +0000

    deep recently their inform town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
