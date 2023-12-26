2aeda6177f2450d60394d85cedef790e5573b3e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:14 +0000

    die they exact press square

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def count_elements(lst):
+        return len(lst)
