2ce4dcd9a09f4a98263b6e58eaf7492c96a28e2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:29 +0000

    meanwhile building carry search bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
