ce4c62d1a96129bd117894032cbc3bbf219bddf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:56 +0000

    have collection flight personal exactly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
