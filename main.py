174b93a0d913e522c1b98b349633e2da093713d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:54 +0000

    accordingto people among nice comfort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
