f623c265f8874dbd19183ce902e11d12abe0ce5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:34 +0000

    worth about practice world various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
+import datetime
+def get_current_date():
+        return datetime.date.today()
