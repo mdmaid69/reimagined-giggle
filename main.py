5ae39a34acf13c2e4589834138092f041fa5edc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:48 +0000

    display dance yet satisfied light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        return sys.version
