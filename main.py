bd2153738e25bedb6768c6b3a333e0b2c4974366
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:15 +0000

    age cool talkback translate wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
