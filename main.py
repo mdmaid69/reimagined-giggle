08bf2dfcf5f5f918f5cf217f104bd73ec62a2502
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:27 +0000

    swimming poison react callup guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
