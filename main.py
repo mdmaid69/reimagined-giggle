bc464afe616d46e492907a265e1b463cb2672eaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:31 +0000

    joke fight majority skin couple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
