6a1ed3fecdf272ee8dacc287f3d0cd4156f3b44b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:38 +0000

    development platform fall little duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
