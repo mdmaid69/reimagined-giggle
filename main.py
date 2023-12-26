49184ad804cacb88706a9a0cd61a625ea201dbe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:53 +0000

    punishment another corner ago gas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
