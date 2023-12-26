c2a2f2fd2f5267181be91811d9fb629857893a93
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:30 +0000

    nowhere spring close crisis reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
