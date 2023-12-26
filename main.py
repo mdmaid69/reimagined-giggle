68b308d27e7061dbbc7caf369451aaa2bb05209a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:24 +0000

    exciting lookafter wall news like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.ctime()
