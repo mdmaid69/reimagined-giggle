2a7fb08b3b078dd7458db7bb49bdd908e371d3b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:10 +0000

    experience argument search introduce female

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
