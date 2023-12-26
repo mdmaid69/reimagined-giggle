10d3644cfd526aee45dfc2c928352ba000c45e04
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:19 +0000

    bit some professional while innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
