0d455086364ca7466abe26d38c8ae5ea64f74eee
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:34 +0000

    justice permission base hold deliberate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
