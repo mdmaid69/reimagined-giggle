f59754ceadabd87778851ffc4c2bcda8c2d3c827
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:21 +0000

    remove beside do surface stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+name = "Python"
+print("Hello,", name)
