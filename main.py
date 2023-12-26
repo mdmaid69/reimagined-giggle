564a6faaf3a0ae836c276ae24fc461349ba8f903
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:18 +0000

    suit consist skirt sticky service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def cube_number(x):
+        return x**3
