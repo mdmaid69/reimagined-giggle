2ce7af3ab63af207cb61976c1b97db3d28215341
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:07 +0000

    Ff upset treat rub high

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
