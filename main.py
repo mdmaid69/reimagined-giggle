323e0e14bf540833b6b8956adf667859a4639e6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:51 +0000

    nervous so argument depend deserve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
