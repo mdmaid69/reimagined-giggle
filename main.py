72bf237a2d0c6ed5e4e31bf8a1a9f5e26ce860ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:28 +0000

    argue ground compete shopping they

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
