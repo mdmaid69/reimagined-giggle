f95f47c7319a675dcfef79da99fb224ca0d8e45a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:06:46 +0000

    apologizeapologise strange ocean should obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
