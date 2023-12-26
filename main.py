3dbc05d18cb5557dbbb20085e11e2adb2bedb37d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:01 +0000

    generally whoever march impossible church

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print("Hello, world!")
