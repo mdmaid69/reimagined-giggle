33cbbe6c33ab6fac8cf970e1c7b2c700e6c584c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:41 +0000

    calloff clean level properly otherwise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
