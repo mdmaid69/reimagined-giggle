3a31d9d33a9bbd76ab67b823baf86c084386beaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:32:53 +0000

    divide break bringforward goodbye seat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sys
-print(sys.version)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
