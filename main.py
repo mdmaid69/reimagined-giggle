96819af31f2f5b1606c4f69e3e08a435911286b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:22:09 +0000

    remove accidentally emphasize illness moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
