fc7ecfd281e3b7381ff2c2f397e29e78b37cc2bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:11 +0000

    warm anywhere scrap prepare practise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
