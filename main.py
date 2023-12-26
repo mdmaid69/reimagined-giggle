603a64910e5525e946a5f63ac7bd67a5825cb1b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:19 +0000

    come cope difficulty pretty building

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
