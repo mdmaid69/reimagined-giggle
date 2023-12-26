343237d50de44654b196b0b9b36164158c9bbff8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:55 +0000

    side 573entertainment duty cap television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
