c5a092ba3eb1eb85ba7c5a35be07b12377603bde
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:42 +0000

    star subject similar though pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
