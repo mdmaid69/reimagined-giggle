cab783a2229ed1f7ffd373854cd9c423f4ae40db
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:19:33 +0000

    couple chance brother lose along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
