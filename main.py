97899c65a6d5801725aee7e08ba5c0dbebc9da23
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:56 +0000

    complicated detail neither band only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
