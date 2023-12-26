595d77def54cece84d5de82adef5e7ec27d3571a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:04:44 +0000

    rather what beyond brief nurse

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
