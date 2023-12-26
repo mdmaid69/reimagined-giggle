993536e525997e97a2e9befd34aafce7fcc8be97
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:19 +0000

    present nurse freeze unfortunate lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
