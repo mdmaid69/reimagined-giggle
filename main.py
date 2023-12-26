f62f23f74d89f3c260acaf8c2bdc4a1ea37fe0ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:07 +0000

    amount weak trick generally none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
