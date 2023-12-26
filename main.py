cbb29c3d89a5526e4d2b764f45fe579e991c7b3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:32 +0000

    regularly thumb confuse scheme belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
