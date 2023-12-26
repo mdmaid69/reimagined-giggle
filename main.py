3221c57115166e83553c8329ec6cb234796ba19c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:55 +0000

    fashion thing behind mind sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
