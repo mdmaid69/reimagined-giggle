59756e222ad281f9d3b6311ce514ee189330f6fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:28 +0000

    waste calm season doubt board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def add_numbers(a, b):
-        return a + b
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
