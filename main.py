390b9f0c8a9c839657a929ce13bb030f1761cbcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:05:44 +0000

    term zero motion progress immediate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
