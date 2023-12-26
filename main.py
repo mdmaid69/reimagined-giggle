e24be499cdc8dfba82504fbcc0167863acdf2b4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:52:35 +0000

    age scrap properly street dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_min(lst):
+        return min(lst)
