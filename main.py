a0deb3aac6b1c7cd2ca48ced1e33fe59bf8ca361
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:34:09 +0000

    could agree nice tread watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-print(sys.version)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
