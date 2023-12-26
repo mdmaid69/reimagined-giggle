9a497bd34a449f61c2d28484a2f416444c91dc93
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:04 +0000

    characteristic pity sure should model

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def cube_number(x):
+        return x**3
