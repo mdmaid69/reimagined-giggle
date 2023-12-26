7dfef8dab4bb985403cf555eb3525e521cd4e44a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:04 +0000

    side desperate watch amount official

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
