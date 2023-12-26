769ac597078df1a9c4cd5e2ebc07017277aaa9b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:47 +0000

    fill summer comein produce rest

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import array
-def get_array_as_list(array):
-        return list(array)
+def clear_array(array):
+        array *= 0
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
