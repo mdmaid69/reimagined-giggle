bcf7ce2118c40413cd5bc4e8bffb25f0835ba565
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:24 +0000

    board fix unfortunate history idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def get_array_as_repr(array):
+        return repr(array)
