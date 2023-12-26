3acf5be80695c2e7274373ee5a7f811e88d3284e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:23 +0000

    point time mine example sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
