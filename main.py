fb62516fcf008fe0f2d5257d7ddc4b9889e4e749
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:42 +0000

    shut shopping spread book to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
