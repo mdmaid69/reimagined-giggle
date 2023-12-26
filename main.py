14860aabd8a578ba45ea4f2ae778389f989cb97d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:29 +0000

    move share thankyou hard family

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+print(math.pi)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
