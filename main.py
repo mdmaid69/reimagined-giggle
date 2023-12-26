98e87c0becb0bb1bf3c6d7f3e0a2dc9c6cf5caca
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:55 +0000

    and noone pronunciation basis swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
