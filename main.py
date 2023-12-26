ea78f777f87ebd99d7f2f5c5a0379f17f08caac5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:40 +0000

    concentration leave pick suit breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import numpy as np
+print(np.array([1, 2, 3]))
