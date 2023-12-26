2db270d2e78315bbc50526f025f393ae38396e54
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:11 +0000

    drop no claim yourself keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
