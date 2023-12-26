75557666e85f882f8bbcf6a7f4b7ab5a0ebeb32a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:21 +0000

    post year enthusiastic side ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
