51f5fb110c733e9ab0844e98a00122a1dd0ee384
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:58 +0000

    hate afford press middle separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
