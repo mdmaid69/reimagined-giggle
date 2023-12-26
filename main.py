a0b4d1af08132450bde1be8a222141afb8778062
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:25:20 +0000

    accommodation Yy born now far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
