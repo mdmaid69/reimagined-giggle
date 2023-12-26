dd8af2d477794c30299f6c0f08a7a8c6b196a931
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:43 +0000

    mix nonsense hold closely firm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
