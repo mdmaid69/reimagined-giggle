9e727a209984c6f1c01af92dc230df051a92ae91
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:40 +0000

    must high outside welcome Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_counter():
+        return collections.Counter()
