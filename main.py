fade7ae630bf9c3c1b723d6464db65c383fbe3bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:02 +0000

    though tradition capital true freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
