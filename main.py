3b3c65bc330f8c4a5903ead4d21fb8709dd23a2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:27:10 +0000

    complicated get collection muscle weak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
