a59cbf2d3da6867ab33c00d634b19244027c88d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:44 +0000

    time ball will family shut

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
