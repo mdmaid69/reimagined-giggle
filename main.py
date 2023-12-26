4522218e9ea106d377b42ef4b3292594d3fb241f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:00 +0000

    turnaway comfortable raw production talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_str(array):
+        return str(array)
