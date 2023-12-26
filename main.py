da2b598144dc51c77368b09f938a09078556e1bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:54:04 +0000

    communication pool runover whenever building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
