59ab8b7c7fd75a19b88b7880f25c5e3e56f1bd80
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:24 +0000

    heart include lonely move laugh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
