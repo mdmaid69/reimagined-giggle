d483c63d84e85409e5c9c63487a50aaf78f40ebf
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:02 +0000

    smile summer treat tap memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
