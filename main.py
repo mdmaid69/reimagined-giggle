162d8e0fbb6dfe4d5d08ef5ec708a5779b3ec51d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:31 +0000

    566end conduct dare emotion maximum

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
