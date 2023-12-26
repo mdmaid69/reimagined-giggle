8bb5b8ba33cdaf0fa12bbecc53488ff4eba08316
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:57 +0000

    very contain record meaning bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
