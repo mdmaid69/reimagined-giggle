05a1c1204fc2fc7167a80387ac8444c3db2fac6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:37 +0000

    uncomfortable shake area moral use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
