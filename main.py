8f0354e6adccd8265ae3466e28e4ecdadc638ec7
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:47 +0000

    completely persuade afford activity my

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
