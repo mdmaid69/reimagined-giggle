2ae85cafa7755114ebca8f69ac21c3efd88f8c70
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:27 +0000

    news use hold ask physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
