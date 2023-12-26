ac7e3826b84137495d90f1749878f6f9bd0d7e34
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:40:31 +0000

    friend tidy pretty border wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
