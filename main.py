cb9be531e4fee01d424064d4ee509f53f1b2b684
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:59 +0000

    best plan fast Kk tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
