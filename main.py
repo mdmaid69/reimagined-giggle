ae89ab4cd8a936738b48c6d9eb1a2e61031761d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:14 +0000

    record colour lookaround bag die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
