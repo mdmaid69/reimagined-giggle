86f223eb568d0e142c1df4de998a8e206650d734
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:04 +0000

    lay free circle off get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import time
+print(time.time())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
