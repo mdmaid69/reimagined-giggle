1b53c2a610d6e5c2efa5754f6b17327507b8b252
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:21 +0000

    565end trip once react area

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
