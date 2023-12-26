3d7af3cdf6b39fdac6bd03c17121bff1d247c67f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:13 +0000

    meal keepoff tread in declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def subtract_numbers(x, y):
-        return x - y
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
