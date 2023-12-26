abe0c7343aabe1e69ad9f98e72c58779c084947e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:22 +0000

    declare allow manage drop quiet

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
