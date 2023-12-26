8b98a1e16655308c1335b5b3331c0a9b3c19adac
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:45 +0000

    turnon guess quarter over shop

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
