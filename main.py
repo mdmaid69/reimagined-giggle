6047a07d419ab24e0f72ad35bc280ef53657fb2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:16:34 +0000

    tap fierce workupto field try

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
