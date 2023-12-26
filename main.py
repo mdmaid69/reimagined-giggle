7af6c5a63b5ab96abff13b9e86d95534b9ef7e4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:20 +0000

    Ww direct mouth colour mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
