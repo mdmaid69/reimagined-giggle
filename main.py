82e03978f5f889f4a445e82695fe7574ce59d10f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:59 +0000

    join cut base unable waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
