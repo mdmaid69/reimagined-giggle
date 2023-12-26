10ae8d99f6120b5e5d3492ffd684e3a7c02f2a8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:52 +0000

    bored laugh prevent bury level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
