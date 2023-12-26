e217e2ead792dfa85f2d283691c2df740ad67df7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:29 +0000

    sore fun fast name emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def append_to_array(array, item):
+        array.append(item)
