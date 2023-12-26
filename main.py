775a535c71437666443532cab30fbf810ad0c747
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:14 +0000

    everywhere draw so sticky hardly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
