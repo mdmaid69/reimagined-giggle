653313fa6f7e3333315903eae928d49b1833e591
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:01 +0000

    head flavour push bet congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
