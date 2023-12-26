b4e9a6847e8520dc257e3688b8f43ca5a0ecd54f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:49:07 +0000

    attitude at lookup up perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
