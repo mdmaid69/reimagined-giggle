36bd47fad56a9880ea9ec473311df9b93317959a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:45 +0000

    aim beat comeoff add include

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-print(sum(range(10)))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
