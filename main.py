2076a69301a4e64902cac5f30a60478830eab4c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:27 +0000

    occasional reduce swimming area takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_array_index(array, item):
+        return array.index(item)
