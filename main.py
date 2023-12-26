fc3b7bfe3f48014c15e87ced5c7e01099c82216f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:32 +0000

    moon benefit unable 572enter straight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  import sys
+  def get_python_version():
+        return sys.version
+for i in range(5):
+        print(i)
