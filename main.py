3ba4980dbcfcb7ebfa6b2fa158a31b1d85799e51
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:08 +0000

    forget but high please between

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import os
+print(os.getcwd())
