f5bd6de87605de7f6c39543c967dd0d4e611b647
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:33:22 +0000

    trap sky smell skin love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def add_numbers(x, y):
-        return x + y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
