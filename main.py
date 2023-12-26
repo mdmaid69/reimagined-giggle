494de7849ccba4f620591ef6d488809c591f125b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:16:36 +0000

    floor unite real definite straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import platform
-def get_python_version():
-        return platform.python_version()
+def get_array_as_complex(array):
+        return complex(array[0])
