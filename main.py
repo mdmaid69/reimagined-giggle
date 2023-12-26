c8f0a10e023a65860ea285e6cca9e3789fe6dae6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:35 +0000

    judge keepoff quit partner must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
