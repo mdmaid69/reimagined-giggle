97f7409f7a1c7f9ce50a9ac55b80a9eaa24e8690
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:25 +0000

    learn pay cheat sign with

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
