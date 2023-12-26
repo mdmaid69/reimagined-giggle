1dee60fa727fce4ff87cab4fb5dbd0f9f65afdc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:22 +0000

    attempt promise excuse narrow stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
