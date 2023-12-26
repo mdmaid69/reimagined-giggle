1de240fb9cbc4b8d5ee1350c3cb2504862e14b8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:19 +0000

    slow share sign preparation recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
