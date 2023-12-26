27721dee11df80d5df5cbf108540448706598bc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:53 +0000

    swing difference design arrival check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
