1e4b94a9cc0b97c488f275a412e843520fe93ba2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:39 +0000

    stop cinema bother speak let

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
