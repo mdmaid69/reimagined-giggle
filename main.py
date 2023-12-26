b1139b81855fdbd057061ee7dc5a91562bb5951b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:52 +0000

    hospital fair sit shock imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
