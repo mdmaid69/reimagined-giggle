8e0e0ddb68764aa0946c63f39750e2ce6c71e3b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:48 +0000

    subject disappear book set method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def convert_to_octal(n):
+        return oct(n)
