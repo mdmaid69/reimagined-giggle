d78f0a8d96afb94941c94cfbff182fed023d9d9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:19:08 +0000

    fear treat female neither pretty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
