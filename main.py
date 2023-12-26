4b9c5978fd91ca4e97db1991b9e1e69d187aa7d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:43:50 +0000

    personal otherwise put promise stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import sys
+  def get_python_version():
+        return sys.version
