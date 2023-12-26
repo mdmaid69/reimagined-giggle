7163604ca990d1c4f672adce15a9b1889b9138c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:17:14 +0000

    fat wide boring couple weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
