5c86750ce89a0b7288190f1d30a65a0467ebf6f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:27 +0000

    pipe country sick hard reject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
