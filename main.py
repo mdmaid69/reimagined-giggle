47d624daf79ab459090cbfd89e56a3b772b7c8e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:36 +0000

    pain building within nonsense dream

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
