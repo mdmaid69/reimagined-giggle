0c792753895ff9a2c78fbf465401d5a09e80d2c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:57:48 +0000

    as mine neither pity closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
