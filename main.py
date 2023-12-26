1d8c0c1927a6e62710a6b09138288b53e6f3d0af
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:52 +0000

    noone form lower pass limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
