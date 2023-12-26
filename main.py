15bbd7303b930d19218c14ddfc91be8e93e9177c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:15:18 +0000

    appearance criticize achievement steep coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
