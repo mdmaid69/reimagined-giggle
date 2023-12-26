09ab6a15126504dcac4454f811f5f45d2251c082
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:33 +0000

    knowledge contact protest immediate wealth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
