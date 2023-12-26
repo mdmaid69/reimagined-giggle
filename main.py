1167eae601c8fddf1d2037445a4fcc94ec499818
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:33:23 +0000

    bill treatment church combination cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
