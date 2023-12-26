1aaff83b5b3d2c253e2b15ce4b294c86b28a17a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:08 +0000

    characteristic whole excellent passenger call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
