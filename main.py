ae0babbac738ee516357a2d7cbb8d7a024835cef
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:55:22 +0000

    limit limit buy pretty warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
