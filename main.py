ccb4f2d107507f2cf4b0c9849e0b3c0efef3c493
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:43 +0000

    which especially acceptable stare insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
