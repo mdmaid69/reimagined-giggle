c98c6f4302cb37f3ff0fe861e87a57f305fc54c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:18 +0000

    themselves style pure calmly loud

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
