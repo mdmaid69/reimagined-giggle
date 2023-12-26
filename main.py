94388a89955496f750eb74829bbf9aa490e88eaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:46 +0000

    trust hand rule glad wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
