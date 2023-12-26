592b8c1c93532f99b94a6fa5764c7a8e8f7117f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:18 +0000

    hurt cheap everywhere Nn classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_current_working_directory():
+        return os.getcwd()
