5d8c8a159b3ec5d3af28212ca1c75dca545800f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:43 +0000

    seat pupil generous passenger sound

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
