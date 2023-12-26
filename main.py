d43121b1cb5f4f19ffa6b1c0b9f168976278df6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:08:33 +0000

    world flavour ball path myself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
