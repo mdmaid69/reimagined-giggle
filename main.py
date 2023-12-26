3f05d2d870e77a889b6589c3f40620f51be8fd53
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:25 +0000

    independent occur gift confidence plenty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
