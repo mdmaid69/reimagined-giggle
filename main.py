4e78c23e667009ab7ba61c8383afe15480f9f85b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:42:20 +0000

    write reply hospital modern live

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
