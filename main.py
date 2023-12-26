cfedda89017fcc67b66481b130236530d9011831
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:52 +0000

    home television touch how sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
