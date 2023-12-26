48278687d0b8cd293660f29d1551f41432b548c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:30 +0000

    platform quit control shop persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def multiply_numbers(x, y):
+        return x * y
