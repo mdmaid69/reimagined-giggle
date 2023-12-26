6d34a12ee1ff439a9686508a91db0e27e7fbd86f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:06 +0000

    close any height realize dig

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
