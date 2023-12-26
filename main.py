51beceb4326e286aaa03f096b442d18b63493e5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:20 +0000

    greeting admire contact experience during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
