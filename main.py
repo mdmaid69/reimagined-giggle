6306aaf6793c1d9d4833ef83e6ab0832814fea3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:04:19 +0000

    hurt swallow sink hold medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
