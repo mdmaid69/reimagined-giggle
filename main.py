8a39b2d2c53ba00a5eb8e5966bd42380d230a369
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:25:38 +0000

    more fear sure top sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
