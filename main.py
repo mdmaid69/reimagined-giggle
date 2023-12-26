6c1c9c17265295b42cdf09f34496b842096a2735
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:54 +0000

    follow operate proof 573entertainment conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
