a7e6dfd49c29f05df74c3e73a7c90c193319817a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:56 +0000

    understand butter plan woman ready

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
