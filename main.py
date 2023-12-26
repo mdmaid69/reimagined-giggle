3beaab0360869d32b65135ce654b0f0d40ff4dc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:12 +0000

    ahead Tt ordinary aware centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
