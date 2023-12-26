3e24c8eac36779dbbf92682cd758b3210f18d757
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:40 +0000

    geton lower suppose tap shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
