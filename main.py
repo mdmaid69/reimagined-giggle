ece3ce448c0b71543fcbe541adb28c5bc129857f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:40:42 +0000

    legal lunch flat manage murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
