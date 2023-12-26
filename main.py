a2b018f7062c803f406dea17e969126fc4740a7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:28 +0000

    friendly evidence future treat dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
