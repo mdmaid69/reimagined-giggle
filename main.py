58742958562bef91e364225687107d740e106b29
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:03 +0000

    nigga concentration put necessary affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
