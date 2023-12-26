22423316ec040918bd604fb2a77a2e1e65040571
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:23:50 +0000

    before knock secret fail base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
