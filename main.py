f6b47ea9cc23462e85ea6764f927f7cbbd47f6f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:16 +0000

    fold stop achievement bye battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
