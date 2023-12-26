4f709be41b51d235aaebe3de6e27253189ecfc9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:45:44 +0000

    pleased second cut another traffic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
