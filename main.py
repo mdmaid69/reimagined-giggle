bc96f27bdeb1fbe7fbb87a3bea1cd1bd55cda271
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:22 +0000

    concentrate acceptable equipment be surprise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+print(re.match("h.*o", "hello world"))
