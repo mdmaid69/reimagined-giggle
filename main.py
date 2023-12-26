a158612d1b42566302e0bc803e21ee33fb5c804c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:18 +0000

    confidence address to enthusiastic duty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
