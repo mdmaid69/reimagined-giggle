99303c6b46c96bb2cb34dd1f7318a9a1c8e55f94
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:47 +0000

    listen regret bringdown border Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
