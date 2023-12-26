9f5eb260c2118a9fd15a2b98a74f899804c4797b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:44:44 +0000

    swimming coffee gas corner fight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
