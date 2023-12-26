fdc7606bcae1c294941fc8801151c7a6015f8823
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:30 +0000

    may charge consist split talk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
