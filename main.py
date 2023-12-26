529a103864a4220b47c2781f5fe40b36b2f0889c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:52 +0000

    personal level last Gg dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def clear_array(array):
+        array *= 0
