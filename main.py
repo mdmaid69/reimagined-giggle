53c7805b7b03902d414e72bc77a079855b2b31e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:38 +0000

    keep wave workon pause order

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
