148f1fb157fecc91bddf931cf530e30d30e76a5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:54 +0000

    behind back sink worst 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_power(work, time):
+        return work / time
