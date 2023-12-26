6fdc5188ebae0b91c9df6b1096e7b0f677006532
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:14:08 +0000

    definite nose height create act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
