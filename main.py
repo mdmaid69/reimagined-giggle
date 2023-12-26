ddaeed3cfa14c5605017b3448f28bbc1acc37f01
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:24 +0000

    combine anyone fair difficulty achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import time
+print(time.time())
