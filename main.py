8b76cfa5834773f59b0400f05d81e02a853fb2e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:53 +0000

    earth truth too trade sorry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
