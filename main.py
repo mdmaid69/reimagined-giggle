a0f21698ced8c56f40a4851425ad3e2fb142e0ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:38:32 +0000

    shock spite steep lookaround ring

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
