435812dd377bcaaca466578014094ce9ef57fc27
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:43 +0000

    politics ought platform copy work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
