9c4bebfc50d1a3363bd646b85383fb09acd95c71
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:15:47 +0000

    many concern equal 179bite still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def convert_to_octal(n):
+        return oct(n)
+  def count_elements(lst):
+        return len(lst)
