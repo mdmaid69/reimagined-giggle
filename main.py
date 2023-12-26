a1b1f90be214f9f4db3354008c226d31e92dfe71
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:42 +0000

    drive search slide weigh place

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
