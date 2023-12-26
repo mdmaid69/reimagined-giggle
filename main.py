3b05cf45307198abb8ba35377203a2db0ed78ef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:24 +0000

    Ss now road wave suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
