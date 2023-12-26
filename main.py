3b9b9faae47beadc755a5181ec087af7dddbb150
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:06:50 +0000

    represent under beginning complete unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
