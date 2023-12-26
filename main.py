bdfa32a24e876a32b05896824890b8437ba29285
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:13:24 +0000

    mistake interest slice start 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
