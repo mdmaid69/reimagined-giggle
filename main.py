789b8f8d13b4720166a57e9b914dfffe760f5f92
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:33 +0000

    in survive wish group amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
