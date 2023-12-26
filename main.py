5b11c22e63fd894260bf429cf15893536cd9b510
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:59 +0000

    loose necessary breakdown string suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_octal(n):
-        return oct(n)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
