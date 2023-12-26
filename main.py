fc39b56f4021ec4c24f6c8d88642f5ccde21f138
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:14 +0000

    by 572enter imagine report stamp

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
