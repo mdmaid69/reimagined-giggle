f52c2d75b3baee0b2a1892c883de2b0ddc373096
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:22 +0000

    learn such bin main cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
