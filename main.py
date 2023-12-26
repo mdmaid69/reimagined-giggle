ee165b4ef4b468e8cd393ba7beb6a42b1774b96a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:34 +0000

    charge chief forgive company correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
