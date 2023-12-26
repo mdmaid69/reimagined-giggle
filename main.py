b9817f6f3cf2a7d755dcb7f16c9eb4f3608567ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:44:44 +0000

    tap reach begin near thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
