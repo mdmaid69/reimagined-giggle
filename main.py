e3537acca1e42cb6d9dc89a3a7790b425996ee8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:32:21 +0000

    speak closely relax considering thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
