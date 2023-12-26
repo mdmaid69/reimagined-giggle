ef150709e4200fae76fa38148e7749ec9134dd11
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:38 +0000

    excuse unlucky rain human form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
