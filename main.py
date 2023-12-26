081cd9c90ab30d09fff414a8a25d6fe0264ecb22
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:01 +0000

    holdout born brush light spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
