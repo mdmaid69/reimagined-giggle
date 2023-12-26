2cb56fae4e635da4f77a85948909deb4a5901283
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:50 +0000

    embarrassed Ww left expression programme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
