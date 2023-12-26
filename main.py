d150f4f7cd2275ebe37d8fdf9b156139956c1b9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:51 +0000

    oil pull sick argument rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
 def create_user_dict():
         return collections.UserDict()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
