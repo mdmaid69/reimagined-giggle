772898e26230001a4443703473df24a8ba215785
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:07 +0000

    estimate really square artificial schedule

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
