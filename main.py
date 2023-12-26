020f7445c0214080df0d8b53139985ba0b8c9c12
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:07 +0000

    shop me book wave attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
