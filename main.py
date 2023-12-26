dfb4c9a1060fddd0f5003e35a4f001988baed634
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:58 +0000

    time law strict best unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import time
 def get_current_time():
-        return time.ctime()
+        return time.time()
