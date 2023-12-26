981a4cad8802619e9f23339bc8a61ec01328468d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:16 +0000

    war attract repeat she shallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_slice(array, i, j):
+        return array[i:j]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
