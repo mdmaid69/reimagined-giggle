13f7e1ca806ea1d9b171bfede1d068ce54a4e957
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:57 +0000

    protection memory uncertain influence holiday

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
