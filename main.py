7fd519013fd9bc4dc6b3bbcefa411e30a29cbe53
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:33 +0000

    discover appeal minute building almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
