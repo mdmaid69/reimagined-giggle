208e75362ec0d171547f9ec0e8e7282a93deae59
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:39:33 +0000

    always weigh introduce communication press

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-print("Hello, world!")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import tensorflow as tf
+print(tf.__version__)
