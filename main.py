a0cd41d2a789417277fa3a6ed234a50126ede3a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:22 +0000

    polite different means strike male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def convert_to_hex(n):
-        return hex(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
