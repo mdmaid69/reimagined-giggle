ce236822fab08439b216e74d973de5b6af3787ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:44:21 +0000

    build address among close person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
