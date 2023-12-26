08a983f97e917799c4309434151de83c26ccb64d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:18 +0000

    roll fat kind admit rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_speed(distance, time):
+        return distance / time
