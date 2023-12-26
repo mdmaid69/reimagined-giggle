0618f0da5811096b4929d7f352561d75b6592510
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:54:33 +0000

    sting available fence destroy used

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
