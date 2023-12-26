96482f1e7f1bdf722f2d2916a0be8a3d0205f31c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:39:39 +0000

    sight goup wipe amount cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
