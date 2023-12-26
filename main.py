bf779fe67fc430bf48afc67e30641850546156e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:20 +0000

    further swim only Qq closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
