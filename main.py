51c2a3071e49d8d8bb4276818a2869145d132f13
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:44 +0000

    likely community prefer 570enjoyable fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
