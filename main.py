6b22b7606007b9a5989ab0bf51314cc8e1d64299
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:52 +0000

    trap speak recently understand only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_str(array):
+        return str(array)
