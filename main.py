7fb446a533d9c0a0bd0fd63f6e252a575ac97f39
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:18:55 +0000

    develop letter flight crisis goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-print(sys.version)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
