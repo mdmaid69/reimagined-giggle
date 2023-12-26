a7abe62755a641f84c6da5b0e72fff3080a7bf28
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:00 +0000

    hardly mess unlucky left anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_acceleration(speed, time):
-        return speed / time
+x = 10
+y = 20
+print("Sum:", x + y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
