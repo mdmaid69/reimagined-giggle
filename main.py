ac230a5fa55d5c30875b2188f316311d73f5009e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:58 +0000

    grand hour press mixture deep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+print([x**2 for x in range(10)])
