47920e85c9eaa43874eee7be757b15882b3e854b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:41 +0000

    storm deep centre really throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
