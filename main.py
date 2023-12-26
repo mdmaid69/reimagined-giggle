55772a6afdaae49b8e4859e6c3e64921fab69ac9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:08 +0000

    personal save within feature climb

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
