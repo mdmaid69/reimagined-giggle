de7f0401813794ab851c30ab596f124e1bebc1b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:44 +0000

    evening basic Ss bother apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_max(lst):
-        return max(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
