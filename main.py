9bb145dd657127e497f37681628be7a6ff3aa0fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:55 +0000

    keep old wrap forward feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
