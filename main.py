1b619e817767d776c8a4a7e8138133babe7e4d88
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:09:55 +0000

    earth your snow custom release

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
