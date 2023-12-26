4f137d1dd26b038d035ebd8ff38a59ebe16758f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:12 +0000

    light include mess quit wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
