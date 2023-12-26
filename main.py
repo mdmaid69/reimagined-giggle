ddf1c5f2173cb7b24a46265216c19379009ff060
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:57:53 +0000

    embarrassed start waste mainly achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
