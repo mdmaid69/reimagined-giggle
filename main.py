e161f87245818b17c1d6ca978115e08802600a41
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:08 +0000

    bright call plant hit date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
