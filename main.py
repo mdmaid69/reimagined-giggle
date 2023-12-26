7a6afefd1adedcb601ac24bcaaa7fd0bcb525595
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:29 +0000

    sit prize more discussion progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
