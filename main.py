46caeeff67771b935d2178ca72c0c92fd299df59
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:02 +0000

    decision exercise force cry last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
