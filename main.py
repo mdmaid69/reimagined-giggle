f22e445cc359a4e64e764854d97930fac0335480
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:38 +0000

    sentence race violence bet Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
