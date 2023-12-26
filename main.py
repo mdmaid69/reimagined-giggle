f40bad51c39996fbfa2178beacfad2237b434428
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:31:09 +0000

    Gg attractive matter feeling ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
