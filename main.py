059ae7143e98ab58b404a97d227edb09d8804800
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:01 +0000

    adjective delicate meaning party display

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+for i in range(10): print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
