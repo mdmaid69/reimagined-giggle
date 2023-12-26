8df22b526473c17b2af0924e212169e5a8c14aa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:50:05 +0000

    print prefer approval about provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
