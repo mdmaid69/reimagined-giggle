88100b9c431f37c15b20fde03712405803c9bf68
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:32 +0000

    bus flight mend giveaway makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
