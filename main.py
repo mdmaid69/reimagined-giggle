a0ded987c53afe8351f88ebb89244d1efcc1b2f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:00 +0000

    near powerful winter fast ball

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
