980ae2276d15af98108bb51ba462234239e7fc46
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:11 +0000

    exchange minimum ocean form acceptable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
