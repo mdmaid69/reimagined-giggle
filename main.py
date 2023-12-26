dcbd9f0785adf5d17abe6aec032b63b5f01201ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:07:59 +0000

    them recover pause consist experience

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
