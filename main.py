2cb786bf96feeca8015bb9d45e6e26e27ae6fccf
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:17 +0000

    minute miss nice some stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
