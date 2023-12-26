8afe40650865ba747666545825c1a3e1bbeceba3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:55:22 +0000

    eager worth far section bored

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
