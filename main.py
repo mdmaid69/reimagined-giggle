4c272b555ad427775eb99212d474cecc77fa8b4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:32 +0000

    promise comeout creature within serious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
