0a4520ba59f995289cffe82e45760b2bc5b92b43
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:31 +0000

    tap scrap relief bunch scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
