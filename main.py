96ab2f914a48a47b5ff1d6c174756a717e3afe00
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:39 +0000

    meaning workon remember communicate from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
