39a4b1dd57d2e373612978390c895bf12c85ff6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:42:29 +0000

    speed sensitive reduce win show

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
