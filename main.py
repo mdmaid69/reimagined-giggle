13ba8cbdeececc28077a48ac1ca745d70b082d71
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:37:15 +0000

    occasion cross load below long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def is_even(n):
+        return n % 2 == 0
