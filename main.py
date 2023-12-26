f667be06c2a68c152a45b42aacc305b7f4f8341d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:38 +0000

    advise high shoulder divide too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_speed(distance, time):
+        return distance / time
