2d6370db14ccbf041fda837ccefe29c85939d926
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:22 +0000

    people properly bringforward loose mouth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
