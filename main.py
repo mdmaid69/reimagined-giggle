01935049c52c563692fd78a1f340e36d44edd016
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:20 +0000

    long relative earn unhappy fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
