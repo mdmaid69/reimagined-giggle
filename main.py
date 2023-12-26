7dd23228e150ad7c0a3c39f47311d7aaf0468e69
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:14 +0000

    hide come shadow love bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
