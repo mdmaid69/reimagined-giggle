b0730b65b4e5a7b15d30ebe52a8611426f364279
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:14 +0000

    discovery joke lunch precise marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
