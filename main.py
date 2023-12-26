3d7cd73e5bb662030c1820e32394d2ba955c59ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:21 +0000

    hollow nice bury anywhere square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
