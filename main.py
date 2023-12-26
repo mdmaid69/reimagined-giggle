eaf3a67fd9d3b12420cd11a704f6a08ca249401a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:44 +0000

    rid freedom kick say food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
