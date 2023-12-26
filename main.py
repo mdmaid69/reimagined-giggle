4b1d10e4c776a80f0bb9b85f901929c34c0572eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:50 +0000

    special war market mouth interested

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
