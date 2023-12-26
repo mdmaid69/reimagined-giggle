79a0f4b74cfb4d6c09226259d07e7a7062e6c868
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:51:10 +0000

    Dd journey see object plate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def subtract_numbers(x, y):
-        return x - y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
