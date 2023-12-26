d87cce76c72fd5f57ab5116878c528ba678a013a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:37:15 +0000

    mistake reduce slow free personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
