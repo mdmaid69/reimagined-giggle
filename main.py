b18d4edb34ad94022baad39e053dc550c2be5ac8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:41 +0000

    mark sudden amount left figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
