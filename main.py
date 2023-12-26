8fc97dc66a5c0b40c07ff7e267c1906dd6b7002a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:09 +0000

    confused equipment wonder take touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_dict():
-        return collections.UserDict()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
