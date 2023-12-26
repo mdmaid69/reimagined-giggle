aba8a3b01c226ea6bfd58ddc6d2e4b0d8f8ff65c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:30 +0000

    dark transport touch bored journey

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
