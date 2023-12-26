ee8202f85671084a0d752ddf40a58f609943e32e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:55 +0000

    disappear getout breakin where kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
