c19e2425515c8221bab9845d62c5565387a384e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:56:32 +0000

    production provide best dance holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
