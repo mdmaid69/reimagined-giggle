eee78fd4de2d2b09b00bf4491fc1c9848df90dea
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:14 +0000

    vote its glad combine cross

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
