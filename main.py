eb99dc034bfa15c54e93db345d5d290a3f27112a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:24:30 +0000

    big attitude treat invitation pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
