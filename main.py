4ad02938d90d3cd480b5be392a02c3459d3f9115
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:49 +0000

    every fit trust forgive proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
