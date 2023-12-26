7124fdb2130e39bbe8c18f1b5fe55831431f44ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:16 +0000

    preparation authority just back hear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def sort_list(lst):
-        return sorted(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
