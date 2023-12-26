2eeb654356c54bf539dc52dfa8f70f5329ea638c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:08 +0000

    Nn float opinion immediately stranger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
