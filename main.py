913624325ee83f9a54638cff6aa646e4b39b1576
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:37 +0000

    wild dear bear build material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
