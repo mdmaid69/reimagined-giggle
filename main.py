057edb26f426f597bcc659fe9e585a0012aa763f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:08 +0000

    closed hospital anxious rank kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
