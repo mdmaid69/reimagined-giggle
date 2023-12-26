481419db22cb78672f4e4a54155afafdc9bc0952
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:19 +0000

    check at ready correct meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
