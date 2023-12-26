f35b4cf39063802160671b989d3a50d9efe11f00
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:53:25 +0000

    approval neither total comeoff wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
