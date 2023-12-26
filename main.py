af1ee6fc9c5a9798c7e45eda61bd5b34f8a3e014
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:06 +0000

    bring flat always conduct example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
