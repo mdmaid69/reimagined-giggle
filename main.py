667cccca4c259faee545a5b2f30eaeefbb769bf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:21 +0000

    announce remove drink defeat operate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
