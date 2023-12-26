d6f4f3f81f0f44b52833f1de4518ff70ecd6edb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:02 +0000

    thing immediate sore your care

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
