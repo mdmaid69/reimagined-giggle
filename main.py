80686b9476ff2c2b9cc502f3f718e90f248281f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:38:48 +0000

    stop escape appeal pinch matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
