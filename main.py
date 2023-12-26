1cef2e31d699612c1da6c21bc5dd6a1d4fcd7440
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:14:17 +0000

    hour carry favourite memory feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
