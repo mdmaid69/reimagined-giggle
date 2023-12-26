0b5ca0cf3fb68f5f4a1c7638d74259fa198032d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:54:33 +0000

    total impressed she describe dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
