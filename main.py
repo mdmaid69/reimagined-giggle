6c0a54f6443085802af129e51655d54e16e22039
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:30:50 +0000

    steady partner would tear goup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
