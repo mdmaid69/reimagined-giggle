8a70fa3a812d81c2842d6a691548228f93177c8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:20 +0000

    effect long value quarter waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
