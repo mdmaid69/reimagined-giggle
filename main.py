06620a9e1fce44b2c367aaedcd91cf98c8d56297
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:55 +0000

    now show spot care like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
