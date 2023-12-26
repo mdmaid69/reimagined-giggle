46dfc07ec582cf0283f853b3d2dd358af194243f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:23:08 +0000

    demonstrate disturbing throw sea ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+print(os.getcwd())
 import array
-def get_array_as_list(array):
-        return list(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
