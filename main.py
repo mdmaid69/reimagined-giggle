a2f95babb54e7eb72f5d1f6f3dca8656e39da188
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:02 +0000

    pleasure result embarrassed cruel switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
