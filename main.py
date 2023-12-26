b7917866a7884aaa1a191edb9b9f3bc38f40843f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:56 +0000

    smell performance to Zz hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
