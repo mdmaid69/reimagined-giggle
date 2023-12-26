4beeba1fce841e7fc28ca19969dd6723fa83e698
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:42:48 +0000

    object match description land which

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
