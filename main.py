297d010a632c970367ad393677198aa71f360840
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:25:45 +0000

    nor nervous woman advise moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print([x**2 for x in range(10)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
