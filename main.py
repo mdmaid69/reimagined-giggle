8ef3f81a52d376fffba7613faec5e0b97e326bad
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:07 +0000

    vote certain complain ground male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
