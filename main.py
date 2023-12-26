7258e0b9e13b96770e4caf52689a6c31e6c3ce62
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:45 +0000

    related disapprove recent next far

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
