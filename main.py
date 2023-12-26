e88415906c878835ec0aad17ef2f5529b1e697c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:51 +0000

    pronunciation light bury operate goodbye

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
