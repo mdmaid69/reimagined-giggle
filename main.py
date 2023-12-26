3dd7eb1f7974ec327638ed474951d567475814ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:19:55 +0000

    argument market gap programme edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
