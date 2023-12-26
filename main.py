732168f57b9b3a69cf6d7e48fe72fc5ef3b6c5bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:44 +0000

    hold Cc around control operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
