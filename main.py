ec92c315830b5153601be462e1b0ca998f3a7cda
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:54 +0000

    immediately power path guard plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
