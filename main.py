8a072d4b769a6f0d514e11e92d5cdfc3602dafac
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:40 +0000

    circle confused mean direction takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
