605ca7e16b34552e8227ad746c04934a52eef992
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:17:18 +0000

    charge set workout comment door

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+for i in range(10): print(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
