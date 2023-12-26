7cdb28c78fc38f2bb0a73e97e4234eb5994ecae5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:02 +0000

    true bringback still transparent them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+print(math.pi)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
