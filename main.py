a82e02cb9f60048732ecf24c74ca09ec42bda98a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:19 +0000

    actual dear mystery rise dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
 print(math.pi)
