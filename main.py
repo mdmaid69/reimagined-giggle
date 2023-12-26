0bc7af5ec3c1850b866277caa6823b98292d239e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:51 +0000

    start both challenge best remark

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
