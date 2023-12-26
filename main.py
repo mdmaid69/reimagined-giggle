050fe04f511aecb9fab8c718456a97eae563168b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:24:34 +0000

    also something eager mess strike

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
