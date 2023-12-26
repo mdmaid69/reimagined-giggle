6f528ec030ff9805121d29da24eaae94e8d4dd85
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:08:11 +0000

    emphasize swimming otherwise education lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
