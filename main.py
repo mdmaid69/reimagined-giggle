6f03247a3ef31b0b562c433e595f21a3c3380f91
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:31:11 +0000

    mood lesson gate weather sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_index(array, item):
+        return array.index(item)
