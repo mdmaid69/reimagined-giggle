76b2d7792a31a56d094a30bed14091b594439d34
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:08:24 +0000

    forgive bite something certainly second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
