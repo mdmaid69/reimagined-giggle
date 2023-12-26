6f3f7fc4926b872e51765e1e2398d50e24ce4d64
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:26 +0000

    criticism straight between thorough slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
