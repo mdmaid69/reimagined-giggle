da6f65d53fba647dc4fe15e8e51736f9dab0950c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:38 +0000

    sea speed road over he

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
