77af3d36a8e9a5f2eef8e55f633aadbc3f736dcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:55 +0000

    responsible improvement aim organize section

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
