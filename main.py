984c3d245790664de9eea69cbdecf8485da67c03
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:48 +0000

    house still sink art swimming

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def check_if_array_contains_item(array, item):
+        return item in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
