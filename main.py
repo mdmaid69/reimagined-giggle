09a4c0fbe455d74e5c0e99cc6bffeca91bf2d8c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:14 +0000

    choose declaration little seriously concerning

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import collections
-def create_user_list():
-        return collections.UserList()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_item(array, i):
+        return array[i]
