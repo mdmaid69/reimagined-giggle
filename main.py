fc9d31fb1d69a54199487d762a2eac862a0e8927
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:39 +0000

    expression cause increase anybody before

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
