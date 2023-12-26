3a48d750706899e81b36e632bb52a652d4a6eed2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:50 +0000

    reliable invite present university fairly

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def remove_duplicates(lst):
+        return list(set(lst))
