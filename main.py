c0bdfc70233101cf63ae81948e311cb4169f5889
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:20 +0000

    comeround pinch silent well out

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
