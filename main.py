228c0d09811e1f0422636798720aa0195242a64b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:15 +0000

    refer ride fair kick terrible

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def sort_list(lst):
+        return sorted(lst)
+import random
+def roll_die():
+        return random.randint(1, 6)
