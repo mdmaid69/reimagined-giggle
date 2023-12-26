c7b2d781fe56fa42030bf94c6d2f305b3919aafb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:59 +0000

    hurt whichever rate suit makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
