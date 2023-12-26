b2ceeca195b85546aa7cd7fc7fe796d86c1bfcb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:34 +0000

    constantly embarrassing of nurse clearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
