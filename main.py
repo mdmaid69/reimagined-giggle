02c47b68076d02bf8d154d22ce9d490532dde3e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:46 +0000

    passage case post act breakin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
