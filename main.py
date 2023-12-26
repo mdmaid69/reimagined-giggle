514e0d9e4840f7cf05ab152fc43ad7c7cfb84fba
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:12 +0000

    gap wish runout tomorrow mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
