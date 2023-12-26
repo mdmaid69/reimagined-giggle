e989087220d2827de43ce74e4ceb4987ecb95b88
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:26 +0000

    charge loose branch turnout cough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
