5932484dd6e73fc8670f89272fa8ad12bd230db6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:30:30 +0000

    over design hear loud spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_speed(distance, time):
+        return distance / time
