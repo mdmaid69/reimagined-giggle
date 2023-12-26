eb198ed5f776e813f4c2e7821d26664726547e48
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:38 +0000

    shade wish prize basis wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
