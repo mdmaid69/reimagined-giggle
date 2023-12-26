c19fbd85852c8d4e83cf67239dc7a691dc162a33
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:49 +0000

    hello control knowledge smile respect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
