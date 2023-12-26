217adff751c0653dbfcc2fe7e8aecced9854019d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:35 +0000

    main surprising show woman bunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_power(work, time):
-        return work / time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_array_as_bool(array):
+        return bool(array)
