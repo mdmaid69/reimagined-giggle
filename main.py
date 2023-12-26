f6ee07bee2e4be9cfde34410c7ff37c250ee636a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:33 +0000

    soil advantage her Kk popular

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
