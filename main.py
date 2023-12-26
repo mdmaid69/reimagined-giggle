ba481861af8fcee36a1ff9d50685d94f2bb23c20
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:50:34 +0000

    bite hair correct Ss middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
