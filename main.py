3d479fd704b5a1b7eb874a28a663ae28ba7009c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:16 +0000

    beside part soil read react

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
