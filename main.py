9f62c328a13801760f6a67d7c8b7c9a6ff06ab0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:47 +0000

    against makeup square live another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def subtract_numbers(x, y):
-        return x - y
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
