341283801fb45e6fca84de4d9888ea7078899c03
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:14 +0000

    reliable reply store reply hair

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
