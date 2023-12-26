7cb17cfa102bef04d67d761b638a3a50c98c7b36
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:25 +0000

    depend less convincing comeup instead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def find_unique_words(sentence):
+        return set(sentence.split())
