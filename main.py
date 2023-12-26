e96a3f36e21dba2eb73bef2bfa0ac262892e8bd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:19 +0000

    scale slide gas equal belief

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
