ea110010b22382e8bb4418ebb95ade40e1ccd963
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:01 +0000

    first determined error getback comeback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
