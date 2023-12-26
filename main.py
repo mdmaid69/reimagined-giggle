97d865a899e734c5c8700283cea106088e07d69a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:34 +0000

    bath new call last event

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
