47a5a9c70a21d710a89ec8762b0d6ecab03451df
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:06 +0000

    experience influence sweep little in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
