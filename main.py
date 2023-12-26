82850b4645bdd3d95dcb730e85252c1e4efdb434
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:22 +0000

    side top child operate entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
