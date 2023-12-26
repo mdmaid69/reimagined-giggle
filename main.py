1e951cece04b60b4ab5a4f030d5b8d29be5be2d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:00 +0000

    list advise relax day equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
