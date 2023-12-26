bad9bc8f7860362f2ada4200bc4c66a5edd13103
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:09 +0000

    phone record mind section declaration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
