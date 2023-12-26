4640692ed2ab0bf182d16ff41fb6cc39db8b651d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:34:49 +0000

    sort extremely murder noone workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
