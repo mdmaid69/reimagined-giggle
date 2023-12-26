8f50b8b382f3f5cb1dbb69d47acce403e0e349c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:13:36 +0000

    genuine area curious winter delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_index(array, item):
+        return array.index(item)
