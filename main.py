8573e4e1b1d3df36bbc89a890b7664a8bc05d583
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:53 +0000

    remark beside hungry train snow

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
