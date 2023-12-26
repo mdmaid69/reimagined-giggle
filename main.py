ad0310d0f497c6baf7041e022ec8774e8a713576
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:02 +0000

    give dance culture peace discuss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
