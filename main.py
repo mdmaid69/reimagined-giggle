e2dfe02a1552fce54be4ccdff11744d05842a60e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:45 +0000

    light communicate develop furniture mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_list(array):
+        return array.tolist()
