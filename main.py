5b65532a1909ac233dbdee853c7c409155732341
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:52 +0000

    compete goodbye tomorrow figure importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def sort_numbers(numbers):
+        return sorted(numbers)
