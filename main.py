559134283da8079e2693cb8dbf5acc1be6c52574
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:40 +0000

    shame turnoff old nothing quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
