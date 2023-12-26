66f9261f662b1abdab8a5822535b8edded8e03e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:51 +0000

    nothing play cheque once compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
