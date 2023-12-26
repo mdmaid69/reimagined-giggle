4e344646c19bc9d4aa32bf7b84dbc060cd239f8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:18 +0000

    moon stay guide Oo brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
