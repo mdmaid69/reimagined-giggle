89e3f9ea09646bec63ad999c2f3a120182b06196
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:31 +0000

    hate straight defend angry cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
