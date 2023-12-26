0a0d5752351b42420f84d5bb0aa4a3f7074df4a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:42:49 +0000

    port where partner dull state

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
