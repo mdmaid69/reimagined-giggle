83254e48666c3a27c6278a5f0b0871f224126b25
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:44 +0000

    regret original normal score lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
