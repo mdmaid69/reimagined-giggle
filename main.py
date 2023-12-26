d3a3c0278974642c71b7eb4bd9cc085093d8a1bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:14 +0000

    emergency comein read marry callback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
