d0d1e4fb7a2695bc6f979f934f3d246ce6a5a8fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:05 +0000

    below closely embarrassing plate official

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area(radius):
+        return 3.14 * radius * radius
