f1eb5f1f9f71dffeb2b206cad5d3ae1de9bba399
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:16 +0000

    Jj high city object used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
