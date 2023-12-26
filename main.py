8c9dbb3b3d177623cd58fd6a2eee189590abe125
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:43 +0000

    memory give passenger pair actual

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
