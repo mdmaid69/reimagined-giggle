9050fcc26c3b0e6bbfa5f0209971a3ea06d369db
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:41:15 +0000

    heavy warning complain history cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
