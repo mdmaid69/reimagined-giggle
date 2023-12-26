810badb3918db1523b2c4ee15ee6a7c80e305a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:42:22 +0000

    environment accidental attractive lock bound

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
