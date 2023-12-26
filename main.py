cb7510526041811c3e81d7ee66d311d83cac5e69
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:17:18 +0000

    ask law collection affect disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
