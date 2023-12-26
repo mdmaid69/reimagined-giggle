7ef33e16ffb5baaa82b065288efb34127baa164c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:20 +0000

    relax to stare party sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
