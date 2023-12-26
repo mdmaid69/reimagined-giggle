60e68d4a9ffb93a273d621b38bfc8c7ec15da27f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:09 +0000

    as touch see possible mixture

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-def count_elements(lst):
-        return len(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
