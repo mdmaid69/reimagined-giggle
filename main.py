341ee2ccd533f7ed75c7c70a50ee7651409466cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:28:03 +0000

    goon scared decide attract wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
