c4a2a6b82ad1c6024e9a229af0f10f70507c516f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:13 +0000

    order aim perform brush born

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
