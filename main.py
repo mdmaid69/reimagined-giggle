abd8b275aaeadd10dc429639a1a8fded04e85ad9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:02 +0000

    characteristic phone floor discussion check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
