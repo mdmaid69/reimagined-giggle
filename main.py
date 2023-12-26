1647fe02b449825b60b765b252830748ed0074ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:44 +0000

    clothes attempt onto degree earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
