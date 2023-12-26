983513cc33866b62e51193189f1fb951541fe2cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:59:25 +0000

    unfair would previous call such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
