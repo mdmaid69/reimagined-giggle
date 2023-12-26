87becf6d49929ec3bd81b76b64a3469d5e33fb2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:28 +0000

    complicated many else low drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def check_if_array_contains_item(array, item):
+        return item in array
