f18ca0dab00094a6352c5ba0c1055fb19179bba7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:37 +0000

    turnup may pull pour birthday

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def clear_array(array):
+        array *= 0
+def find_min(numbers):
+        return min(numbers)
