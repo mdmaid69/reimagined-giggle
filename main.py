5fdc58808e50ca3a400301e20a66e3ed01912918
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:33 +0000

    deliberately mess face that unit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
