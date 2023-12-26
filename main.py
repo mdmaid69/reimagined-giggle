24ac3b260cf9b961652ae8b43422ebca4435a7fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:19:16 +0000

    content see its proof check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def clear_array(array):
+        array *= 0
