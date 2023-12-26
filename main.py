2f2fb854b014d6f1ce125a9dda268a35609dac6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:36:42 +0000

    entry geton seem convenient shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
+import time
+def get_current_time():
+        return time.time()
