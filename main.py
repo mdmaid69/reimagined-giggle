d98446497990508fe55eb6170b524b2ea6bd0565
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:03 +0000

    idea trap turn pretty along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
