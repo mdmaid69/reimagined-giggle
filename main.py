a266ef39a99329bb8efbeb0d765bcc8ee2266cd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:21 +0000

    game confidence difficult beside height

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
