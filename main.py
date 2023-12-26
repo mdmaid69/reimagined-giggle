81d1e739b6d01cebc31e5fe98f782576e4a2ecbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:06 +0000

    down flood secret it arrival

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
