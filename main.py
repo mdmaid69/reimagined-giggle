4d6c403b929d06ae94f86f9023e76d16137b7a9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:36 +0000

    life mark proper exaggerate fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
