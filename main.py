4404365980401b79c52d6b3ca852e513e21dbbb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:31 +0000

    feed demonstrate principle begin lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+name = "Python"
+print("Hello,", name)
