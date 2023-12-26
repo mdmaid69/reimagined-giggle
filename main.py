0a3dbf82a84f47ebcfd947a656907f7f754533c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:22:58 +0000

    find fortunate available kick angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
