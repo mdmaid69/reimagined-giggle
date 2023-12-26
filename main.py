c01e7bc727017c2aafbaf8262aad19be83ed5442
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:08 +0000

    apply imagination fork careful shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
