4dd10098e3eac6b217d3c6419f8ee5b2461f096f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:45:38 +0000

    contribute near lunch once would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
