21a97f10ada8c949f3a53f80c951effeb457dd01
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:20 +0000

    possibly yesterday live permission onto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-print([x**2 for x in range(10)])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
