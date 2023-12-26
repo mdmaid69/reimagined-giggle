94046b2c06861d286e8b9b35cd7dc64ed38aec52
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:50 +0000

    uncomfortable unkind wave dangerous convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
