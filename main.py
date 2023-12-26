188f28395236bc0c28c0f2f3f7b7da531232c684
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:15 +0000

    wish middle identify price Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
