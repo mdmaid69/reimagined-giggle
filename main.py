267361d41a3c78cbae09ea3f70bea916ba569e71
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:34:59 +0000

    cost murder praise suppose off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
+name = "Python"
+print("Hello,", name)
