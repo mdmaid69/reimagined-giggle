d1a9fe164c249e30212d832b56c5d2d63bb7f9c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:21:57 +0000

    breakdown lunch change flood side

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_max(lst):
-        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
