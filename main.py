b806cf68f9dd4c510c29a5f4462245e6772c31a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:41:23 +0000

    congratulate relief ear interesting family

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
