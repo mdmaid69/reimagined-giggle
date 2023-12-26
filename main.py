8178df87b8df43c1b53f0b8fbe76818e20cc7d00
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:26 +0000

    tragedy length front situation promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
