c1d149894889c8cb3a15399ea9bd366c3937087e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:38 +0000

    object knock popular trick cough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def sort_list(lst):
+        return sorted(lst)
