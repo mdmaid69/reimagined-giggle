25381a9ad53a88c143904101a6019dcd0f4f0e97
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:07 +0000

    band continue mess door firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
