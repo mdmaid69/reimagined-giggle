ff6da260a75f9fcd0e0a8e862e39bc5b9f3859be
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:58 +0000

    holdup best city height brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
