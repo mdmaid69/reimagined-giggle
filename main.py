2bc7641831c6f4516a77aa977ab848b48a039d85
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:23:21 +0000

    coffee strange trick check error

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
