e9b7220e5612305cd294bc717fe88080d6fd273c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:12 +0000

    copy breakout through fact understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
