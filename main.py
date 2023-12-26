c98fe5ab2a008f5eb2a07f4e19d2dec69c01040a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:14:00 +0000

    freeze he across instead settle

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
