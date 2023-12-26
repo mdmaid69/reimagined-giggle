3ee45fddc8771e858d5d22bfd2233ebf3057c699
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:08 +0000

    cap congratulations mother refer pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
