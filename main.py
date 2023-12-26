0ccd10e11a02aa4e242af9184535ff446ddf9d4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:28 +0000

    roll habit remember subject share

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def sort_list(lst):
+        return sorted(lst)
