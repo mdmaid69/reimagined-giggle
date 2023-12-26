f7e5510c4b6b171c19b28ce42d9fbcbea467c295
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:16 +0000

    bear except build business impossible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
