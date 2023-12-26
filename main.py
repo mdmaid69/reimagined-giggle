e1fa3c7134ab3ab94de9798171567e2395448ee9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:40 +0000

    shopping play swing rather cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
