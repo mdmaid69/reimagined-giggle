0a3383eccd553b363075b67ac69f3a5ce60dec23
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:06 +0000

    Hh repair Ii gap comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_floor(x):
+        return math.floor(x)
