6a238928542d39cf3d45cfb6dcf4a564264d98db
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:11 +0000

    dig completely wake false Ww

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(numbers):
-        return max(numbers)
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
