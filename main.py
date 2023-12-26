89de37559893dba55f3a2dcbcb11c6d81d2eec75
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:52 +0000

    creature flavour talkback anywhere workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
