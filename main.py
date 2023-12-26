01c83238e9eaadb605a8f3869fa55b17ef5dd094
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:58 +0000

    giveup give never later connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
