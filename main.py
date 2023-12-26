7e9c702f352625677d10c634aa61ded972333a66
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:37 +0000

    behave dull though reach sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
