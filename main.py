0ed6cc68c5343e910a3a0edd5d5cf9c5c0e189a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:59 +0000

    that profit content punishment somewhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
