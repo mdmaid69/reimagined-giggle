e56146646e5a2aff9bcef6727c1f6b1eaa9016b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:21 +0000

    should match love safety love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def append_to_array(array, item):
+        array.append(item)
