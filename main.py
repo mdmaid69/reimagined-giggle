dbe910afd113fbbdba4e104606eed2be3b2f3014
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:14:41 +0000

    entrance high right anybody meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def remove_duplicates(lst):
+        return list(set(lst))
