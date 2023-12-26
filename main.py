6f97d83091fb7b707165367dc63725e84d0cf552
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:05 +0000

    birthday worry spoil attention talkinto

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
