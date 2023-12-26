62e0092d370a0617c666881852404f02cf871378
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:06:58 +0000

    base light answer rare right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
