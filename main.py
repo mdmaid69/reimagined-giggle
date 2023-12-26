d98728f367f49a1d09ad264e6b33f0485bbd6f7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:25 +0000

    lift stay list concerning on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
