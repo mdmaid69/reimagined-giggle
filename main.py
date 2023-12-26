1873273039484ae1733c16f5e11f9a21fcd53a16
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:24 +0000

    bright knowledge idea down rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
