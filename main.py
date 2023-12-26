1d22d7c1a0217411c93bf55f120343f965c18b8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:31 +0000

    look 179bite sound grade custom

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def remove_duplicates(lst):
+        return list(set(lst))
+print("Hello, world!")
