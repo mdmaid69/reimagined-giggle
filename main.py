24ab3bb1405232ca8af0b352c2c519c752f06fcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:14 +0000

    behave trouble admit near minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
