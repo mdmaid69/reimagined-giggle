b41843117b9ab36c3901004632ce378e2a7c8115
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:00 +0000

    size connect proper so except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
