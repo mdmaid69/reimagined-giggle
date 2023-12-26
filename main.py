820cc97528d490273053ca49e17e2eb0c5d6e6ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:11 +0000

    exploration shop seriously whole off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
