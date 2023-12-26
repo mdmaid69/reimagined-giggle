7f2a9801d74319b4377f9ea1bbaaf42899865dc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:35 +0000

    settle convince affect no plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def reverse_array(array):
+        array.reverse()
+def convert_to_binary(n):
+        return bin(n)
