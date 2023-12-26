c3589277f4e72cb00def23de93b7439b627c4a3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:37 +0000

    ready swallow less power gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
