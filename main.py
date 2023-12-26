4e7e18d643efce83039db8114fe26d3f7278f302
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:26 +0000

    illness into responsible empty nigga

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
