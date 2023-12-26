094f1b371c10bd036dbd9c1ca3172b1942f7253b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:55 +0000

    crash complete kick flat taste

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
