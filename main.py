0d71c0a97a083e32f8ce318af265df0e0e76e101
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:52 +0000

    classroom satisfactory naturally exact suppose

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
