9cfd836444ce258cc27c7eca3152d12163718492
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:25:57 +0000

    team themselves snow getoff proof

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_work(force, distance):
+        return force * distance
+print([x**2 for x in range(10)])
