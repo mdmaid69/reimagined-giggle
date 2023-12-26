4cf7bb93579eb6d298d978bed53a9f3c468f824b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:44 +0000

    suggest breakdown scheme age freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
