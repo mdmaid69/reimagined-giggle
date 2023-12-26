46c8a2c17df9d9340fc02b1c7bfbd15c213664a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:27 +0000

    shake turn home play unit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_speed(distance, time):
+        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
