a9bb9702bfa1265a00152a43dd59db301c47f9a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:54:55 +0000

    post correct interview increase new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        return sys.version
