4d9d72c2236b438035067371b4a51c43e1a419e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:54:21 +0000

    badly agreement comment ready doubt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
