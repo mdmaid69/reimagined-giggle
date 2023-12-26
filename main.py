b5fd501e37ba73cfa0b46b5a71da561274c6b688
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:52 +0000

    protect crowd free scream expensive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
