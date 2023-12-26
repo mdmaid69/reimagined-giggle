7eafa0b18fa46266ffd676df8bf6eb7edb2dc190
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:43 +0000

    behind emergency whatever sentence among

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_density(mass, volume):
+        return mass / volume
