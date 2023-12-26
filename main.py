99f18213ebc3fcc761d2b30c8365f81e76397f5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:21 +0000

    niggaacademy giveaway description laugh blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
