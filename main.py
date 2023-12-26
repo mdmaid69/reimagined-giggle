f0d73ec0d0813f84ccfc90a640384f3d4d40b67c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:56 +0000

    miss foot quantity to pipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
