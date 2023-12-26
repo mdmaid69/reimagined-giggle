c59882f4893f78e0c45bcb6a6b893f69fb2d0907
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:28:09 +0000

    charge certain life flood talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
