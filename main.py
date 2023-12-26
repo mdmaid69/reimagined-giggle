77484016eda8266b9afa19d942e7e032fed43eae
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:18 +0000

    check pleasant once scare tough

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
