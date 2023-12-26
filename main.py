1a20b6db666d5c875f25ad0aafef954fabd830f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:44 +0000

    careful say edge settle alone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
