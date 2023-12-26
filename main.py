e35308507faf55e05c18fcd5ff8cd2b55ffcd2a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:05 +0000

    affect unlucky continue experiment split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
