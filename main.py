c193ff3f090ce25497af6efc98876379f7453c57
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:44:25 +0000

    previous month round still exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import collections
+def create_stack():
+        return collections.deque()
