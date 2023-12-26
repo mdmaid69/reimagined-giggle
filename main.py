18009d2a80ab045a49e5c4d2b62ace7e1a8a56e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:13:22 +0000

    trap direct forgive environment record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
