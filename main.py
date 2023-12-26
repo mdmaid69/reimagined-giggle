5ad5636bef4e3357ac17bf741b3f7fac46c0fa8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:53 +0000

    uniform describe permission which calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
