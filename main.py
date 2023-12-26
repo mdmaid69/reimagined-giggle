9008620cc4af993987320fff899c922a2e156db8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:19 +0000

    closely personal realize gift term

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
