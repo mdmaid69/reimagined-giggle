5c6eadb13f786f84badfe1c35051cf8e095940eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:47:48 +0000

    Ii hard container cruel pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
