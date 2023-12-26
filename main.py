e093540013f01b789b7930c43994d49d42a784da
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:44 +0000

    simple target nothing niggaacademy bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def pop_from_array(array, i=-1):
+        return array.pop(i)
