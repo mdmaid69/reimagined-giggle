6a7852164ffadd2af01385b9e729f96972e7af46
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:26:28 +0000

    card invitation true convince offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_typecode(array):
-        return array.typecode
+def find_max(lst):
+        return max(lst)
+x = 10
+y = 20
+print("Sum:", x + y)
