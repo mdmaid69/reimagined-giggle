84514aa281d6327f04fbbd0150ffcaf6078dee9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:18 +0000

    play expression various past string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
