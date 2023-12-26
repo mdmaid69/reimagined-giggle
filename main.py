bee713566bd8dca8cea269de5b5b583145e432a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:34 +0000

    wake breakout hear set during

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+print(math.pi)
