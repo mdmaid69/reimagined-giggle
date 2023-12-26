ffe34aa8314f4ef7589a13dd9e7c1626d2d3a416
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:50 +0000

    keepaway listenfor effort comment owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
