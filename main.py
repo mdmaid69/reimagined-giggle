c2195cd0d482db30680342e7e3dbd8c23a0ce770
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:32:47 +0000

    steady far tongue report comefrom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
