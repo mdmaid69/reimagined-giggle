31e89d2d8d0dc3b9c408c066dc30a70d5cdd99f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:58 +0000

    path collect 565end otherwise able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
