40325a27f2dfb30133c7f7297b48b735ae1ddbc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:46 +0000

    expect generally seat advantage turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
