368182d5a4a065afc04d7b9dc699a7039e98fb69
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:29 +0000

    Kk hold material table substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
