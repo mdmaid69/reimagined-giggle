ba7cbd64c4ed4643740a0f8db4e40a3b20051cf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:59 +0000

    contribute thinkof repair bright congratulate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
