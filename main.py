9650ef56ef08b24b101b105600d8202378b5abd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:23 +0000

    burn bang carry comein control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
