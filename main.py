55c18ebc0dc16882477205cc18d18487dfe755dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:51 +0000

    instruction behind careful live autumn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
