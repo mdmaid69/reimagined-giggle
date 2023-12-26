e881002d2036e47aa5fd8e251ec2f15c75b63697
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:19 +0000

    future perfect underneath word steady

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
