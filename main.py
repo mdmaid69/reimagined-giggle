790383edbfff71ba1f123af04bdb8813256c71ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:25 +0000

    fairly slow use file talkback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def count_elements(lst):
         return len(lst)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
