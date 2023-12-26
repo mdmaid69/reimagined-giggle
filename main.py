2dc89d138a57e53f08793310f93535041dfebf95
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:41:36 +0000

    trade towards Ee dance mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
+import os
+print(os.getcwd())
