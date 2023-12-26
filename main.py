22e44db320e280516fca8559cc77b583056b9e76
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:00 +0000

    worse contrast Hh importance copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_length(array):
-        return len(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
