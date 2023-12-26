99e4a2d7c0e7693cd551384dcd93df91ce4fc808
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:03 +0000

    rescue certainly rest special there

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-from collections import Counter
-print(Counter("hello world"))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
