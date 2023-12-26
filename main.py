d1dc0bdc3d61dd2f1ef18f4fc04dbffd322a6ef0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:28 +0000

    agree possession 571enough crisis march

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
