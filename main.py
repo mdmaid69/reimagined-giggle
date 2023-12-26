e0a7fcb2ff540f3f0fc3eac4750cddf897dd5d52
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:27 +0000

    may ring left skill workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
