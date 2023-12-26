b8e0cface8adecc366f5b6ee94cdb393cdc7b6ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:43 +0000

    slice combine smell miss win

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
