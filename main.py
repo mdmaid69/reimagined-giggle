973662ba48e17d64a824af7152ae11f6c811e492
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:07:51 +0000

    order stone wrap far whoever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
