18e26b757eafb23a557f0dcf31215498841d724d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:57 +0000

    trick sensitive force school great

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
