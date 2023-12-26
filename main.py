ecbea96af537d4a3eec3548123a8ddc4ad1d5f8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:50 +0000

    pattern advise shine bet ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
