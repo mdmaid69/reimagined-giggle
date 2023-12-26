f703ea21afc0e77c5fa00e88d6753717f1ddd7c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:22 +0000

    trust public square block house

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
