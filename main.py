b8972c9e2cc06f6875012ff765725b0752f4fae0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:25 +0000

    bread fix reasonable invitation cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
