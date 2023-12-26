66aed2dae9416d42d0f6d5ae7a09781f447a9a1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:49 +0000

    practise school record arrive your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
