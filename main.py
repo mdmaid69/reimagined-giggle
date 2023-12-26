0a1d6f92e10e5510fc6dd089f999f700c80ba8c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:10:39 +0000

    show proud friend hand thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
