e3124e5b80ada50f5c5ec0af7f4527866e8b06ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:42:53 +0000

    excited sky get town slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
