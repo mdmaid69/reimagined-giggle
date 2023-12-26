665c63c27dcf8e3fe70cd8db845b6a1b96bbb48b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:24 +0000

    label prevent ocean stick formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import getpass
+def get_username():
+        return getpass.getuser()
