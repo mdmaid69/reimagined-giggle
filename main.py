c3e4019f18b607f4dd1b52b125b78c16b27a89d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:26 +0000

    beside private smile naturally summer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
