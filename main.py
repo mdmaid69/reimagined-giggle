314ec26203b6dbcefec52714e8c2da7467cb6b5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:54:18 +0000

    this introduction near along scream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
