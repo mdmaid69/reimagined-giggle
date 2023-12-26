ab4f93db215c1277cf841eb1be514b5af78c5bf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:36 +0000

    blame quickly truth exact thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
