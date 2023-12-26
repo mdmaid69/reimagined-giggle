caf18f893b6825701a344e8772cfefbdcbc3b047
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:05 +0000

    children design tall belief later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
