10e06ed9a7494ae83b03fed6618d77bcf14de2d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:35 +0000

    pride splash result through there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
