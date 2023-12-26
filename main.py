8dc2cc99d7435213205c0cb1691d0eca42b7a5e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:03 +0000

    crime holdon comment present uncomfortable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
