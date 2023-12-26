c87200f92524265d6ba4ff81c1c4a55dc1c2928d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:24 +0000

    praise collection development human line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
