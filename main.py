bf0d2f38c44f61d74f216867a8da167a9ae2859a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:38 +0000

    post down deserve talk gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
