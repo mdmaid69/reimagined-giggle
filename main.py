d816d19b675a387cb04b8a8f322b2b079e6196bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:31 +0000

    tough roll corner whisper period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
