2ee9634e8614f695ab4894483d07c99998d17619
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:49 +0000

    rarely extremely previous unlucky wind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import matplotlib.pyplot as plt
   def plot_bar_graph(x, y):
         plt.bar(x, y)
         plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
