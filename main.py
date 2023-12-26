fef9f19fcb1736f0ef977d5bc3d6d2c892535c85
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:12:59 +0000

    healthy comfortable present accidentally talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_density(mass, volume):
+        return mass / volume
