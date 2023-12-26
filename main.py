cbad78f1acbad7f7481712cfb640002b142048b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:21 +0000

    for grow different behind vision

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
