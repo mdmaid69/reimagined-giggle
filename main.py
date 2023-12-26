e654fc83b404d64dc22271b0769d28ca8fcf5314
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:29 +0000

    attach hot ordinary niggaacademy she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
