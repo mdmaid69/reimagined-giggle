68f00c621d91a62108323a1888f6ce35775a07fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:14 +0000

    well comeoff date private wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
