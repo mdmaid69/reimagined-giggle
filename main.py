67a64b92382886057fa54424c90075b7e093a43e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:00 +0000

    tear listenfor expect once hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
