5a1d7b55d13cceb8d4a2154db3abd5725a3056c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:49 +0000

    love ordinary suggest yes now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
