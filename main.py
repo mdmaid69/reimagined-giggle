8eb8c3d18da3665ff50d9167b9b13c4ca800a278
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:19 +0000

    neither tool properly bringdown through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
