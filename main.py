a4591dafcf44e17fce26706a76229cc9ad10aab3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:48 +0000

    furniture moon justice better well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
