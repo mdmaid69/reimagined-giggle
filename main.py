3606d7be45782af5f3ff3924923d9a492eaa0625
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:50 +0000

    lucky born exact method purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def get_current_working_directory():
+        return os.getcwd()
