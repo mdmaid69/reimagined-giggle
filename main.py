853ea91b9b5e18e468d2fa80e200318bcb919460
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:05 +0000

    certainly pretty ignore equipment opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_odd(n):
+        return n % 2 != 0
