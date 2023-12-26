d239d41723ad21d05eeaab7f221e4bb11c691a5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:44 +0000

    collect snow neither solve science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
