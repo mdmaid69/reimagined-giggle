ab2b4b1530569546b19e412b2642627df6606b84
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:32 +0000

    deliberate difficulty comfortable minimum quality

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
