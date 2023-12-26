79536ff654a5ef40405a2adb9932dc5508a9a0fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:20 +0000

    none tragedy smooth brave church

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
