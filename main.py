1b6aaf31414773b8985c2dab07bf11a54e7226d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:09:06 +0000

    avoid near unusual distance table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
