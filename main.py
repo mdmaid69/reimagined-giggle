aa80cf14a43b3570ce936c44f9f3d098dbb3f141
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:15 +0000

    open before away proud escape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
