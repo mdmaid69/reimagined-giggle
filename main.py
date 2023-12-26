951faaa73e28e96c95d69e9df1c7eeb7109bccf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:37 +0000

    display strength clear office bottom

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
