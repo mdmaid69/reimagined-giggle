bb30b9f1046b987d8a531e15059a5dea8d99207b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:43 +0000

    storm gate butter mysterious I

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def cube_number(x):
+        return x**3
+  def reverse_list(lst):
+        return lst[::-1]
