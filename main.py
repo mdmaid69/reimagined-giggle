33e62917a26cbb89d08e8acd4db60302bdebbad3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:21 +0000

    feeling operation left become cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
