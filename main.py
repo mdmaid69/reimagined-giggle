bf3af9424e42660fdb387f63f6889ee977a75334
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:31 +0000

    post occur occasion none reasonable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
