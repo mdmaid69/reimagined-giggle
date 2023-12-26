c9d3b290fdd0801dca1732e4779eea70a9e90fe2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:12 +0000

    legal nonsense confusing ball earth

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
