e342cec87ebc8832b4089718800692586a8a0fa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:09 +0000

    learn first maybe miss offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
