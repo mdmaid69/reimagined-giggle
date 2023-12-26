7172743079e9f2604ba001511ee2c940add10cfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:46 +0000

    near taste plant immediately bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
