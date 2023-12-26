58e9c8583b97fa872a16ec58e8e53b3b310a1e3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:19:32 +0000

    bar standard adjective whenever cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import getpass
-def get_username():
-        return getpass.getuser()
+print("Hello, world!")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
