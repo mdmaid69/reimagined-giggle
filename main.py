d5814b4309e2e2ef062002be54b7adda75bd798d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:05:14 +0000

    place square mine avoid bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
