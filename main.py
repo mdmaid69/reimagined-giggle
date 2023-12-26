5aae7dfd514577668b1d8640382b59471cb127fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:57 +0000

    switch exaggerate getaway list increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
