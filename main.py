e735d6c2c445a92af63cf91e18e71ddf154d4d24
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:01 +0000

    frighten mind lose book push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
