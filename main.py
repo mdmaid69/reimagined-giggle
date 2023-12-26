c4607eb443477ca5552a0d682b8305f6fb3ad034
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:09 +0000

    somehow poison free off keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
