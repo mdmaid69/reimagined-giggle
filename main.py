999ab0ff8c7f009978058c179638ab5547735f59
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:10 +0000

    compete spot wait 571enough cinema

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def multiply_numbers(x, y):
+        return x * y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
