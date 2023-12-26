665706b19f156a2043052aff4ef6214661b4d2e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:30:08 +0000

    inside advantage soak now custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
