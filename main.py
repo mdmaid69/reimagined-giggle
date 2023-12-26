781fcbaf9f9db8b3f9596a3d2706c4fb096a9076
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:52 +0000

    deliver refer sink throw count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
