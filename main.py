646ba59003e6a7f97e3b81fef5efefa53c6ad59c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:52 +0000

    ball risk tell trade advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
