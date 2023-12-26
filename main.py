c8cc3acd088475bb2fd4aa4356a754181350c3f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:19 +0000

    only fence music muscle along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
