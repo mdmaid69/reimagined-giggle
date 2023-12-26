64c8d65bb88552c2e4fbe52ab5a0cfb0d3b391eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:55 +0000

    floor each bridge direction goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
