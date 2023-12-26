3d2bd48debdd178b16b71938e53e25dd7c999d1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:03 +0000

    innocent here surprised run rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
