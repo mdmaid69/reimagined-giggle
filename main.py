68782d04e850cce65fa08962811cad452f2c11d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:33 +0000

    black unhappy duty happen delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
