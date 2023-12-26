fca38383b42b5320be15f25e6d2295d89e0e5f9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:29 +0000

    plant tend below prevent comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
