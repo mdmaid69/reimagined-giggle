91213ee12a84e50f3faa75ccd394458f64ffc165
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:13:51 +0000

    quick yet last ago arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
