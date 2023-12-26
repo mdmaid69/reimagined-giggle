f64855bf1f5d45d5932fe6c6eea611c3c04365bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:35 +0000

    emphasis relative indeed brain getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def set_array_item(array, i, item):
+        array[i] = item
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
