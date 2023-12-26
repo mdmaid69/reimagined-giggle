dcb9a41e113aa3ff9b107ec76c83776ea5c1432f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:34 +0000

    567enemy challenge train train term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
