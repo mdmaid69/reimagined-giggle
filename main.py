1f2714a548f2871c78e22b5e56cee96010fc12ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:50 +0000

    emphasize bother special conduct broad

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
