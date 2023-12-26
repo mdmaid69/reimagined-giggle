3daabb626878b5b28207358cc19a1e0580baadea
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:25 +0000

    rough fit question callup study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
