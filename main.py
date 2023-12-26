79868f1ae494eb53757d44ce8d7ca769f59a092e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:40 +0000

    relative sentence immediately foreign law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
