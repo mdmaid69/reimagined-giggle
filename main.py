299f0bd10a56434adf00353e74815b74eff69ccf
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:55 +0000

    shop female bit surface shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
