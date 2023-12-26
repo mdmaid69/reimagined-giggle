672158a14cdb58f36fe8f7d1753ac36c2640089a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:26:56 +0000

    frequent intelligent division comfortable window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
