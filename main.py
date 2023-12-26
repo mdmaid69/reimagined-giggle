76ffa2112d2ea7c99a3c49a3cf081a933c6df919
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:41 +0000

    different deliver shopping relax regularly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
