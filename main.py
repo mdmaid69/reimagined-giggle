25527033ad7391fefd79d370bf0eb9a30ebcefc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:39 +0000

    accidentally notice shape either father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+x = 10
+y = 20
+print("Sum:", x + y)
+import time
+def get_current_time():
+        return time.time()
