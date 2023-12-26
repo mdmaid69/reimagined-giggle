dcd2d341f68a208cc71320ab722608d51240d312
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:34 +0000

    off phone ring already world

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def roll_die():
+        return random.randint(1, 6)
