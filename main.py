0f9226b877bea0ef6109a5fa80b1ec845b69a139
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:51 +0000

    skill plan negative record have

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
