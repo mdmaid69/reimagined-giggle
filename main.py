6ba0e9e9781cbda272d0ea559904101e1cd5f544
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:11 +0000

    pleasure port group in arrangement

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-def is_palindrome(s):
-        return s == s[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
