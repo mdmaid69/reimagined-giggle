3fd3f3ca723a39bcfb16308bd36f919d9adda805
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:03 +0000

    telephone keepaway shout level easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
