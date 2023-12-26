fbdbf74b96e760a778626d3cf7c718a629bb0825
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:05:16 +0000

    now ball speak one takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
