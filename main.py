b9e4705a18891eec5d31a786a406ae80b66b2826
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:23 +0000

    swim form address fortunately finger

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_length(array):
-        return len(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import sys
+def exit_program():
+        sys.exit()
