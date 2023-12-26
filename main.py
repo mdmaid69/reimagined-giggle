1206e5129f76381350f06b964ffaa285f519859e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:13 +0000

    exactly let charge beyond public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
