ce0e550c2381423db9732541c1d01d6d7a4e9590
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:03:15 +0000

    file hollow head season factory

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print("Hello, world!")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
