ab32657beb95303ecd057b2d59e4186ae982efe6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:44:17 +0000

    short low way bush drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
