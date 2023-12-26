1e7d0ea9e53dc09b9df3eda3420f7809d5001273
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:49 +0000

    sound worse pass occur happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
