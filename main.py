1d7474b45a0144ef9aaf2814bc2e93fbd1e813d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:48 +0000

    sound thirsty usual freeze head

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
