953aed7e3122c506d1cd5e806cdcd23b62273f44
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:46 +0000

    memory prize equal there there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
