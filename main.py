3a96028838b9076c5712dec7529091dd330a5cda
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:31:22 +0000

    common fashion anybody contact exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
