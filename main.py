a99faf5f57ff820c76a705750eb01c7552a45e4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:04:35 +0000

    pile begin nervous sometimes difficult

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
