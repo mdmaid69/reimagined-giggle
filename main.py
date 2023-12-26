3ef5ab274597930c4204f222ee781e5635cc327d
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:56 +0000

    pinch nor bit agree use

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+def calculate_power(work, time):
+        return work / time
