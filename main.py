36fb92182ef9217a05ca01a27f30a2bbef598f07
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:08 +0000

    underneath bag weigh not check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import getpass
+def get_username():
+        return getpass.getuser()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
