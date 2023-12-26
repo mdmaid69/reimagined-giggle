6b82d91c4163b58f9f411eebb711ecd503141e6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:07 +0000

    Oo insist discover Oo simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
