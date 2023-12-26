fbc529cb85eaf8693d437c3fdba80386c7a94a11
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:19 +0000

    fortunate condition callout out beautiful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
