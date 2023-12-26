87eb5bfeda8e9ea2794b92414885378993248362
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:27 +0000

    apply nor relax strength glad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
