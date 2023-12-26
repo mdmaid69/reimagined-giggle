366aebaec1f2304062b08aca42cae459d6de4a7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:45:31 +0000

    below anything prefer content sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
