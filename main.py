7b3941c994b7eb4305a7cc71cfa3f7d721261b67
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:12:56 +0000

    573entertainment talk top ill start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
