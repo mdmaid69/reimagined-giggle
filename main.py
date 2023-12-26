422840b09b854da235b10efcc67969097d78a786
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:53:13 +0000

    he influence spot downstairs mistake

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
