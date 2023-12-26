2f7aae0914f6f6563132f38863fcb53ceaa46876
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:00:05 +0000

    shade pattern jump brain write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
