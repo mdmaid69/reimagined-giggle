246a394241ba768ec8f8f91879055f41f6cf8780
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:23 +0000

    level success transport reaction comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def is_odd(n):
+        return n % 2 != 0
