acb83f5e35324de63e36e47528f707de8764c529
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:52 +0000

    bear until worst rarely further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
