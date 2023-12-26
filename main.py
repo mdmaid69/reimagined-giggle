afc6ff8e465e18355cc0704f15eeca1a52014ff4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:13 +0000

    heavy lead spend attractive job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
