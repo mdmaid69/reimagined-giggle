ea29a4fa754bba7eb643ca517f57ed899d51e6de
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:58 +0000

    move breathe care cut information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
