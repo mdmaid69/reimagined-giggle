de09feff3c5769bdd100f49dcccb909afda5fd14
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:00 +0000

    anywayanyhow project thirsty deliberate minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
