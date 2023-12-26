68c570d4ef113d04cbe2587cea452c5e552d3e85
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:30 +0000

    winter wheel talk serve breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
