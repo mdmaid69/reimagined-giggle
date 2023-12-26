311de69dd5c9bd0e807f2db3aa0743368bc86e17
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:37 +0000

    stranger smile pocket minimum stop

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
