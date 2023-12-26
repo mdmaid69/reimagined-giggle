f62643a3ca5196b1b5984fed6a72c00eb1893a5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:57 +0000

    style steam neither mine moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
