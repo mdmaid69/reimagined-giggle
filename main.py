78b307364a2ed97d651bc87f7e7a0459c0756d7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:18:38 +0000

    me around date breakinto free

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
