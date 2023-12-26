746d9f2a6a9a5530957786547959cb24667e0339
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:54:51 +0000

    uponon raise cross message square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
