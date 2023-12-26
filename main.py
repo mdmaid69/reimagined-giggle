673474834d2b784135354d6c088552a71a41efcf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:28 +0000

    glad such tax promise pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
