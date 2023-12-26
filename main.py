5bdd3f2eac9b102d7030c61aef6bdc7f3d94f0a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:20 +0000

    container ilovedicks child reduce add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
