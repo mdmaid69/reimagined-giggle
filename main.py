b9fcec7a6221b8adaf58d6eb6294d30d083c1525
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:46 +0000

    crowded loud imagination total release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
