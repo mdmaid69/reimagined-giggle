4133ecda5aaa40b2db5708a500e838d13a85448c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:09:34 +0000

    young justice charge fat prefer

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
