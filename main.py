a01368d63e077eab984a09e5c6c423e8a9798d7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:12 +0000

    sell left burst remark Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
