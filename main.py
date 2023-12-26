b73d6f4e16794c567870ec844e1618ac6d8fc667
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:07 +0000

    suffer to run thinkthrough meaning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
