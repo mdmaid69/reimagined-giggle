2ea4c44b0a49d9aa6d5aaa4901e86fd13fa136a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:37 +0000

    dangerous pretty bath afternoon risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def exit_program():
+        sys.exit()
