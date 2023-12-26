9c7e5a60fe53e0a306d84814bf641283cbabfe7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:01 +0000

    head though react free content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
