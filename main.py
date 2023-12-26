8f4001b0b106c71b2e7fdaa0318945cd55e3d552
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:42 +0000

    definite lie main try cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
