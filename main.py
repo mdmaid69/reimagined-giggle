d5f4973e277c029231268abc198e0d137792107d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:24 +0000

    sticky 567enemy tone agree row

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
