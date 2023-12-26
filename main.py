e2b3d5e80f0a8a898547c49ffcacb0fee680753c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:19 +0000

    water possibly twist sort common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
