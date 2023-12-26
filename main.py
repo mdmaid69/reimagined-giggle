4a66de87ed6eb2a4ecc59fab0a3a6e4c1a099908
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:33 +0000

    curious work organization score duty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
