5156053ca288147d01a460c7644c2777d99980b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:29 +0000

    consult error reduce actual cough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
