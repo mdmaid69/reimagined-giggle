99c4b67271ae0f2a21754e6c2fc370ee3243a425
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:56 +0000

    approve between morning crowded stay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
