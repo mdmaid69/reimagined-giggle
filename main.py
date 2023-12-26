a16e27214ddb8fda84ed4d8a9ee5c335b15842e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:59:53 +0000

    satisfactory check centimetre office comefrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
