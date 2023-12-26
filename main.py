51ddb836984c608d7a456353038bc83d3a910784
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:23 +0000

    taste bus worth container lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
