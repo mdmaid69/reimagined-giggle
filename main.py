0ac57e2b846155f44578ec8afb123aba5dbf5676
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:24 +0000

    breakout handle thanks land wake

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
