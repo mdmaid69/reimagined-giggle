df61f9d57174853bb934bb7cecf60526a168edf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:47 +0000

    waste temper tune collection needle

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
