6a9a2c55fa6120e263fd1114b9495833842da8e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:03 +0000

    payment cost smart police side

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def remove_duplicates(lst):
+        return list(set(lst))
