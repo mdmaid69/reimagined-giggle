c8ede21606cbddcf5a84be836e0ec2490e449a9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:47 +0000

    lend breakout work nonsense physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
