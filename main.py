6dead45f3170017a9ab39809285e94ebf4f37736
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:19 +0000

    disturbing big paper maximum information

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
