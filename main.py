c989daada8ab157854e5dec0c7ec783c0b784184
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:03:31 +0000

    border nowhere her bunch rather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_min(numbers):
+        return min(numbers)
