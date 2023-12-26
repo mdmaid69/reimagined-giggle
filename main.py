743d603807c49d991a4d2d83f6c42a7209936bf7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:39:45 +0000

    far sad rain practice find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
