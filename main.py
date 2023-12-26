e7a472ff8fea234d96a00cf3bafa247361335eea
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:27:15 +0000

    smile callat door sure dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
