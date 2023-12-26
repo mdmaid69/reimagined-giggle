69a512b16f6e86cb45bbe4b5998729f55a9bab90
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:21:06 +0000

    positive learn press common follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_base_name(path):
+        return os.path.basename(path)
