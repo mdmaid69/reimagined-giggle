2096b6c0468af536755878539d306b665ffbd705
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:53 +0000

    key workon dead aim busy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
