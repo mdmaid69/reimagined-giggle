16a95d8956d2a143dfce878fbfdb9a63b9779857
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:37 +0000

    swallow means declare dear comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
