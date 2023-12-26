6a324e1bbac3e29bd9e592e95ffa5dfeabca6bce
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:46:08 +0000

    everyone minute prevent many plain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
