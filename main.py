3acb94e0237bb0e9f0f799837b38b876fe41929a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:51 +0000

    woman cross obtain convenient avoid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def is_palindrome(s):
+        return s == s[::-1]
