292dcedfd1d9926e9028bb8944a5adcdd3da2798
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:14 +0000

    suppose lend considering to share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
