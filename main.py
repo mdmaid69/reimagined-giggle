67643834b5cd6b29c57379843380206b704d4f39
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:43 +0000

    middle clearly pin no no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
