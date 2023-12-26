27784c4ee62439d9ef3e8477703d93828d6708e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:39 +0000

    cover makeinto warm speed complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
