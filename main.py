0d871dbbdb14a6d14560a66d86b953ee00407c46
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:36 +0000

    period takeback female stone owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
