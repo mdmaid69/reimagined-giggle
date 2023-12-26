267c1132776f3217cd2cace702abf01e67c268ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:20 +0000

    thirsty needle slice guess shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
