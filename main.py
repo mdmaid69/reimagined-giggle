f7d175eda3fe3767085690cb756de5cced56aec1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:03:45 +0000

    wait permission measure shade wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
