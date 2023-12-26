3cdc6d984a1f9e199b86c419313850628340b0a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:13 +0000

    importance announce share goback(to) distance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
