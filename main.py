96206835fb694a0b669419cb0fd4b917a5db144c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:40 +0000

    exact project thin certain simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_user_list():
+        return collections.UserList()
