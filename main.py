49c8046bbf1a3ea52c81c32b8875787d25e3fc1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:12 +0000

    release great left run on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_int(array):
+        return int(array[0])
+def add_numbers(a, b):
+        return a + b
