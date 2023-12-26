dd9934a511905b657fc55512014fa95b40e36c78
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:48:47 +0000

    Ee sudden drive peace great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
