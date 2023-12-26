400cda53bb19315de4407c3c8171258327963143
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:48 +0000

    difficulty Oo anybody image evidence

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-print("Hello, world!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
