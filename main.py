3a0279365185e52c822be44de6526ce772a1e2b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:05 +0000

    surprised probably fire recently after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
