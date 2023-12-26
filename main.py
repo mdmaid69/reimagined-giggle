e681208979783940050ad084d4fa308bc5ce0f93
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:21 +0000

    ball main first Zz mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
