24de76acfb9e483c8c2fc136eef037b8ba871826
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:22:25 +0000

    glad surprised pupil slightly listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
