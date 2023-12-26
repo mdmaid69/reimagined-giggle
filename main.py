acbf49094839862531d36cad58351f71acb2720f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:13 +0000

    event adjective clean off image

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
