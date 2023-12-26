1af4efd91564bd92df8ca69c587b64b61ba221a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:49:57 +0000

    friendly tone check wing have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def square_number(x):
+        return x**2
