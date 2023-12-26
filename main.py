5a5dcb203c698989308e59a05d4274d1406810df
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:35 +0000

    through opportunity negative cough smile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
