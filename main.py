9ef205ac3421681901435b0f2a6e79b2829433db
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:50 +0000

    lookback(on) convince quarter sign like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def count_elements(lst):
+        return len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
