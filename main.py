b5cdf4811fe50ed94bd99eb19152b570fa9004d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:00 +0000

    prepare scale neither try holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
