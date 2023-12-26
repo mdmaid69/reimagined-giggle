9433f274bae64bbd94228c9f17617a6df607a5b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:41 +0000

    phone characteristic reason secret basically

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def convert_to_hex(n):
-        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
