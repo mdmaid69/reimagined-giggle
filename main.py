6ccd732f577fb4f44387e1509d07e2e4c9ddd4e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:25:30 +0000

    light pride plan slow keepup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
