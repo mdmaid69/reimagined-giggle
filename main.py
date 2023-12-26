dbb42ad6ae096491574c81a2dd1d66835a8ebc05
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:42 +0000

    usual well out fun love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
