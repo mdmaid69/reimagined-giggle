91b5859a70d742caedd3236e502a783165bc4d63
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:25 +0000

    extremely understand characteristic embarrassing regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
