39cc1e2d067acdde0271ed45e3de5f5b94688c1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:27 +0000

    already look separate muscle fresh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
