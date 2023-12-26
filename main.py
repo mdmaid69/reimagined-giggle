a865c16526d0488af8d5f2ab729d8e72c9c9b44a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:14 +0000

    slowly direction tough tap summer

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
