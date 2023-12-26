18b225be877b39617bdd43fde5495b85bcee203b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:08 +0000

    expect water further trip personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
