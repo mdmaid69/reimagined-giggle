6faf6fa754cb08d20b5e3810153865db2b61d8d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:48:19 +0000

    poison mark intelligent entry breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
