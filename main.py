63268e999b3426ea1995298c2ba85b533e782953
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:52 +0000

    through ground negative splash react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
