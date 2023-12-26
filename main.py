00ea5c45ee755f640a5f32434eb9968639a15575
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:07:57 +0000

    disgusting either smooth protest claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
