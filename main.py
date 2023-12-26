156bb01856d6ea9489278b524e708dba074a9224
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:12 +0000

    nice workout float mixture dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
