d32c1bf1bd553ff5b4ff42e94c55eaf96b5a6f01
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:21:18 +0000

    none alive possession attempt news

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
