fc07b059a8ebf9996ef7000218d85d343006046e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:23 +0000

    curious pretty big ask sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import platform
-def get_os_info():
-        return platform.uname()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
