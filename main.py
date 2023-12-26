18d6cfd55513c704c7dcdee32610ff86eafe6676
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:29 +0000

    ago comein how back trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
