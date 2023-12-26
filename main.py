d718f6a81aa13dba249d75228fb3ba7447707aab
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:12 +0000

    without especially immediately lose fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def reverse_string(s):
+        return s[::-1]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
