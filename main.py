a8c2bf72db4c06a3d9076f4634f99e237687f856
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:41 +0000

    so appearance step cry order

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
