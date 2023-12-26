d2f8b74e52c236a1f13a18fbe04ad8d8a033e749
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:04:13 +0000

    government awake plenty argue better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
