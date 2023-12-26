6d2bcbaae5bf373d5172316c4d458389facd75ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:05 +0000

    interview discuss repeat gift consult

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_work(force, distance):
+        return force * distance
