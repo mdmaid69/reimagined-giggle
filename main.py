834a12bea528e397169b8ecf7a70946c34628aca
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:08 +0000

    pump picture community far pass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_item_count(array, item):
+        return array.count(item)
