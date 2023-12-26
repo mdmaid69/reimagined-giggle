14c03dc3490480a855391d03873a79744fb3d1b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:56 +0000

    attention Hh lie closed clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
