95619bdbe0ca45e19e7546fa744ba9d23f0dea2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:22 +0000

    switch fold yourself neither hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
