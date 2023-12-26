bc96267cdb56b6f3f6c2cdb8df26f065d5706d57
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:30 +0000

    workon edge uncomfortable obtain church

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+def convert_to_binary(n):
+        return bin(n)
