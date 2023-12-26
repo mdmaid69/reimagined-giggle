d7eb3300287a8653263b43b9faec60d68473f2ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:42 +0000

    large lookback(on) always spray free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
