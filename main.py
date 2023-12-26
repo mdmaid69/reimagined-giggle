f5e7ba9ef4523dd4e84e0aa3a68bbee9ac4ac02e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:02 +0000

    about claim mean country to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
