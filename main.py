eac49cc33e30edebc2c45ef8fcaea39d53ec5637
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:52:39 +0000

    effort cause come charge tooth

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
