380da7a974cbee948de5ec91335c808161766a79
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:22 +0000

    only 567enemy frightened loss country

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
