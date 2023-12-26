9c7d1aceb7839d69c439b89b8a89bef3104e92fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:00:24 +0000

    feature work buy since information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
