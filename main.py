5c393d473c357cc42096d160a419710d34ab71db
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:10 +0000

    night like throughout bringforward zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
