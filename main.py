ea58f507105123acb00a5f7f6ff63dc97fe46bb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:51:32 +0000

    police trap print oil cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
