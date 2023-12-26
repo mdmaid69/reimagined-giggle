03ada0c7efc8b4acfd596c6999e1805605a8ac0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:23 +0000

    comment whom could excuse alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
