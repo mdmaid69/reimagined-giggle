8ceda0ebab6f4c25442e157debea4fe48c73d59a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:38 +0000

    forward violent chance clear ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import time
+def get_current_time():
+        return time.time()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
