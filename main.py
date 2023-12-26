8a367b2539ae42272508b76b1e120754d7c02b90
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:57:06 +0000

    this fall water bringback plate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
