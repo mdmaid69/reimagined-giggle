7ae9d380d80261c2b47267d0ccdd2dd738b2d885
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:37 +0000

    little underneath business afford late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_tangent(x):
+        return math.tan(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
