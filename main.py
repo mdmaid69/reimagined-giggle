55d2942091b86f3a008085020d10844926eaa5e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:09 +0000

    hard degree shut science before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
