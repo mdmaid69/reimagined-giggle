d44a6f127dff22b5e2be393035cc68c53511fd60
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:01 +0000

    threaten innocent excuse colour foot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
