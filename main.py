1a4f6b0ca8386cd0b09add2a8389b559729584e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:14 +0000

    reject nowhere coat offence something

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
