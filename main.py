24980688eac2ba67a88e59c8ebf3e2defb8d690d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:40 +0000

    agree partner explanation 566end star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
