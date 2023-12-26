eb381909906633e4696804e4c99ff9149db63dab
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:22:19 +0000

    criticize building sister good during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
