d996d36b819fdb8c0287ca500d7087a218c8ed8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:07 +0000

    571enough shine band mistake learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def get_current_datetime():
+        return datetime.datetime.now()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
