6ea5b91dd8a292acb5864f94cf08a1c990201101
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:30 +0000

    pretend behaviour joke blame then

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
