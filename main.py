b11ad79b7061bbc599273c723280aaa82096fba4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:34 +0000

    obtain develop moon level himself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
