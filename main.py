d47593e70b6636d8f14ad43ef9072573803a25ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:37:12 +0000

    within back callout but like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
