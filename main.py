258e3ff20fa4b245c74f09e0cb64cf7245598870
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:18 +0000

    place regularly real sleep unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def find_min(lst):
-        return min(lst)
+def list_files_in_directory(path):
+        return os.listdir(path)
