1e4a8932ddc52f0460cde83917a9e191a9651e57
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:23 +0000

    neat stretch catch always anywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
