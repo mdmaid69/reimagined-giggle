3430d20f01b3f7d572deb1c55ac7ac3ac5372a6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:58 +0000

    face clean fly debt ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
