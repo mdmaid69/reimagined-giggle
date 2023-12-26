7a324c08925c0701ee3f715a74f696e1334888dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:07:53 +0000

    hear say rarely advanced press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
