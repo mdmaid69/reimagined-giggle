ac7854cb79d16b07a33a18a5d853620a1cdd9a41
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:43 +0000

    behind skill tend them name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
