4d51078c4a3e2054e7fc8976f95d8fce103ca153
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:33 +0000

    long dangerous careful tall tough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
