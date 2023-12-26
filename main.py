01af42b88381eaec632b305dc5179c1d02b15409
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:23 +0000

    bang kind active date trap

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  def reverse_list(lst):
+        return lst[::-1]
 def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
+        if n % i == 0:
                 return False
         return True
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
