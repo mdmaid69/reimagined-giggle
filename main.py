724362f3df338fe70be7b65359c6368362418863
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:11 +0000

    familiar official check same either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_str(array):
-        return str(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
