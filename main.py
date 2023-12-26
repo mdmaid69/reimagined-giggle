4473a77f8dc3657602768ecf33c1a900ee8e51c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:22 +0000

    breath test seem goon minority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def count_elements(lst):
+        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
