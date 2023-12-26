4f0e1ab8e65231bbaf9acad6c0172324645b39e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:08:22 +0000

    cause meeting excuse inside tax

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
