f520ea3adb6a874c6a94ed7b05ad326f38ae33c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:20 +0000

    rare for bend shade certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
