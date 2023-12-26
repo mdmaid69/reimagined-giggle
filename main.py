e52a595f26b3d7470ae61bdf243acff711be3b40
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:14 +0000

    favour obtain wrap development swim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def count_elements(lst):
+        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
