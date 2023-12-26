9fb4128a2260a91b604934e75b09d9c8a46c1360
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:42:01 +0000

    ready influence always petrol juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
