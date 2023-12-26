b86d7a048ffc23ab5b85209cfcdfaeb73ad59e98
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:14 +0000

    command price happen also should

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
