000d087b94c142084c867b91158c2358796a941d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:37 +0000

    sure powerful hurt base pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
