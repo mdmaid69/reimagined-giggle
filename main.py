ff530f9bfb30dda1bed10d61866b3992f4edae54
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:48:54 +0000

    furniture arrange emotion help crash

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
