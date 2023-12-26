c4dda2a232a6d84c76628965e7c9c400efd3fa17
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:48 +0000

    opposite straight seem makeinto through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
