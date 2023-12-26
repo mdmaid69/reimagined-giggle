5e8a447bd4421e53f9519cff90c051f1811481b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:45:05 +0000

    insist file middle apply lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
