cde62a14fc19c98325e23d2728ff57c79461e2e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:42 +0000

    estimate grand voice use rest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
