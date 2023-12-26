f5f6557e9b2ba5063eee63a811c8588af560e2f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:11 +0000

    store refuse declaration base keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
