8dbc24a535196ac4a8848fc6d69a596a365847d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:05 +0000

    prepare set goout guide her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_ceiling(x):
+        return math.ceil(x)
