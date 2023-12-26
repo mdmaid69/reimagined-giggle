7c87cfcdb18087aefbb54408a4f5c5bc1b7eeeff
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:27 +0000

    black alive spring record evening

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
