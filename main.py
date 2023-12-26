b2e882fbaf916e8708eb3625ea226730dab70678
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:10 +0000

    piece anxious middle soil question

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(numbers):
+        return max(numbers)
