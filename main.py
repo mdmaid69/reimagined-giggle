ae3b7dd6ecbdc0226b14bc4c17efbf4bca97b468
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:04 +0000

    house home careful complain impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
