d30aeda1f3fabf09dd7c9a56e1c60b21f2fe9bcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:59 +0000

    sad giveback waste much mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
