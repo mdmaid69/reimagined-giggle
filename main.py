68c36eef08ce71cd586d845961df7cc97f847df4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:09 +0000

    top comedown search again giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
