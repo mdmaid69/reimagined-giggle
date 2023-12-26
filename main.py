111ca8eba9fb32333c494b09dbe7f79ce56330b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:50 +0000

    home car destroy force window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
