d0a96047b05d2a0b6756bd22393d5cde185789eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:50 +0000

    relation suggestion along admire lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
