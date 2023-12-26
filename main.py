663e7832f4e5502517eb02642585462ae8556a28
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:34 +0000

    label welcome trap nothing Kk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
