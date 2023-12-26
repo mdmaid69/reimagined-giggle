d4c68359cade107d74f6904439a169ad37e97dec
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:37 +0000

    friendly lonely reliable comeover confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
