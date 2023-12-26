a62786ee0bbf9a68164ed3ae2332af16ba68fb53
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:57 +0000

    prove giveout threaten Jj base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
