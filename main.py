e21dc956e3c865452691b78980534e9db823dd70
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:02 +0000

    where free mean level cheek

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
