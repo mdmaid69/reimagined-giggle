fbe22a3736191dfdabec723dff7deeaf6ff90f8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:17 +0000

    strong character sleep everybody invite

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_sine(x):
+        return math.sin(x)
