e5c43375b8cee6aa30e52a96bd52da06b7e3b870
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:54 +0000

    getout loan empty profit pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
