4483643f2f82f61ddd7565e6930856ff2799b146
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:38 +0000

    certain complete centimetre guide true

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
