d18d2e74095370610f1788b7d967108bc511b780
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:11:43 +0000

    surprise term comment hungry later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
