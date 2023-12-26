cb559b0cff10b44bbbaff909fb511938ce15f264
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:10:05 +0000

    passenger but right noise prepare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
