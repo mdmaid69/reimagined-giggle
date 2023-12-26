8801b7108d5604060e3f482a0151e85f2257b61c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:33 +0000

    influence Ee easy secret murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def remove_duplicates(lst):
-        return list(set(lst))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
