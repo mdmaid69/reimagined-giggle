d28d34f7243fe4373083098a12eb699909b3e793
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:40 +0000

    will danger blame exist murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
