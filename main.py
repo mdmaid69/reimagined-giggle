4d9ed70b5ebda3bbcca8e86904d1c3dc9ea5d948
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:08 +0000

    reliable mistake sentence comedown peace

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_floor(x):
+        return math.floor(x)
