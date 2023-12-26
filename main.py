c062b39dda1587ac4ea01e150b7a5e29e1ec3bbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:54 +0000

    us big keepout union uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
