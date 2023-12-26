5a2308fd5cda794a5f92222927b0799314375886
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:12 +0000

    Uu dead someone someone authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
