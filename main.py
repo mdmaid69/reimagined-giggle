c8a2f2c3113a6373a8a160a2f15728e992f65ee6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:05 +0000

    breath afternoon long suckme unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
