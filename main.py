b56f3f47b5baa796d4b7ade019dfb297e19da22b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:05 +0000

    nothing love fork dry ourselves

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
