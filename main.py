f4c60ca8050ba5c7e5ee120ee901751814271d01
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:48 +0000

    quit manage tidy classroom skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
