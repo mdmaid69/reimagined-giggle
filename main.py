9116d94fe3fd418b08ffee1f3fabbabb750114eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:34:54 +0000

    each deal exercise concerning without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
