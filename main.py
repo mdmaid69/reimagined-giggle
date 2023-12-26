336a9442b041685538d08352c149c1a10672c8c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:30:45 +0000

    join lunch scrap spill comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
