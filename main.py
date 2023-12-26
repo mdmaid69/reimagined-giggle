9454e702b77ec71389b1312e3d10cc095c4028df
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:00:31 +0000

    high turnback child relative unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
