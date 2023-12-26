a9cbd85dbb8e1751d887669778c6890bbaa3f7c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:07 +0000

    worst meanwhile already question race

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
