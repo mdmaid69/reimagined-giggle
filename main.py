874b478cae653af4b1d59d8bb14c86beca830cfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:22:38 +0000

    contrast small bag warning getback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from collections import Counter
+print(Counter("hello world"))
