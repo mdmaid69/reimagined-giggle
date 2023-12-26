917b95fa988c535b4016ba7acf8dc6b87a199004
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:14 +0000

    same butter everywhere pupil unable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
