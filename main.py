96247d639d034a8ffb81f5b16442e425e41aa051
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:17 +0000

    female breakdown whether confident interview

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
