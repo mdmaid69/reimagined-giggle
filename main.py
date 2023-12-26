8332782f9e95363fcb8b9ec881b8be2d98ba6404
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:42 +0000

    dig middle declare report beyond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
