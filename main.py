22af354d9c9d75d8146e91096d4ae50171f06234
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:55 +0000

    either with practical happen love

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
