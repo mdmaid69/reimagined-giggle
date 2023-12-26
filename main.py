b1fe68c7a4b547802d1711db72fb8b6a839d75b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:22 +0000

    education performance war there along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
