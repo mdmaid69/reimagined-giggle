118592b57524b08a7f16834787bfb7777c220dd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:30 +0000

    hers match takeback keepoff memory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
