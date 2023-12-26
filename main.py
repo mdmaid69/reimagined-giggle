84fe0c1b7070ddf6e05226bbc0934ac8c897b0b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:24:20 +0000

    right exaggerate makeup equal them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def cube_number(x):
-        return x**3
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
