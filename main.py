ac5837404f1bf698e6d28e0ee1fdd9afd1788584
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:40:49 +0000

    plate crash continue declare objection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_binary(n):
+        return bin(n)
