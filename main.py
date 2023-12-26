3183313ef3354daa3f695b82db1ab8e23e53bb3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:22 +0000

    spite ability guard tap taste

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_str(array):
+        return str(array)
