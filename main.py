8a447f06f0e8e412fdafd36a669a59495b2d3f2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:24 +0000

    round unfortunate hers big period

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_current_working_directory():
+        return os.getcwd()
