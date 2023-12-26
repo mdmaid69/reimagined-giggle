a11720f1518b18f00e682713a11a0ccd425d6620
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:23 +0000

    public explain attempt workout never

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_force(mass, acceleration):
+        return mass * acceleration
