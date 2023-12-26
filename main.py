bdbbee74f8ae91619f80cd16d966af59829f92a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:04 +0000

    importance still criticism disturb join

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
