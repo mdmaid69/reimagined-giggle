95d54d7b5b58f7626eb5d5b24ecb13748160eeb1
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:06:06 +0000

    squash attention measure bottom thin

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
