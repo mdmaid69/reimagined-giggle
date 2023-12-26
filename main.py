9966b5401d59647c45234936d42cd318e1115bf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:30 +0000

    exchange cut while listen immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_repr(array):
+        return repr(array)
