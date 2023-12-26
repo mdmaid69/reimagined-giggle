349a7b46c99a36b434348e52b8bd93ef90225fd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:43 +0000

    heavy goback(to) window set fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
