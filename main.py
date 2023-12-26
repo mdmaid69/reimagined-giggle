25b1f2fcf13a8639bf06cd85080ed31c20bfdffd
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:00 +0000

    concentrate upset contrast typical hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
