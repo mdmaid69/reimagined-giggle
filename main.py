818b7e89a7b54485de558c0e8669fb430be939f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:20 +0000

    miss machine wave learn secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_density(mass, volume):
+        return mass / volume
