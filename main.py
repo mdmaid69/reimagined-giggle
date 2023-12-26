b36697120f870120840a0b9a45e4d1d9055e8bf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:24:16 +0000

    drive born already aware trick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
