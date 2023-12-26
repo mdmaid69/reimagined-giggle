6ef2bf5e6f57e7d38b9297541530adae915e78a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:32:55 +0000

    lend last community relax without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_string(s):
+        return s[::-1]
