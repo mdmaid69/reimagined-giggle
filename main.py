dac6c5ce40069fd7c348790c83a3685d9315092b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:15:29 +0000

    time persuade seat arrive fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
