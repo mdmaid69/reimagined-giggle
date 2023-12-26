89ef209bce73d285938c69d6eb624fe06cb0adef
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:53:50 +0000

    disturbing care zero somehow create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
