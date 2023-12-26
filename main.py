9d5f22973963a90109bbc28ce138a42724f7b931
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:44 +0000

    song move important appearance cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def reverse_list(lst):
+        return lst[::-1]
