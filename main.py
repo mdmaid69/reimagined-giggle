1dcf00406a29ac879d1827f014586a3acaf11c6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:18 +0000

    further change least we bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
