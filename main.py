c5963d7fb6807628102f8b15a7ea42412fefc588
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:38 +0000

    stamp either public bank project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
