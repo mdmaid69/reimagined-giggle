42b154af4e2fb73a3c56e555d16d0ea56119e7a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:59 +0000

    passage development block contrast Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
