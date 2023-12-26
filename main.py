6d1eae8d3da7f3dff43aa1ee5f90933351cd84ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:27:57 +0000

    giveback effect generous bite meal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
