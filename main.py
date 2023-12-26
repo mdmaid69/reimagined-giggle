d54677216d25fc57197cba6cc643fb40f7e2c6d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:13:56 +0000

    attitude discovery poison unlikely generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
