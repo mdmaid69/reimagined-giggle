56b74d623e4578a27b1facca6d18a493de93581d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:16 +0000

    actually rush hollow workon punish

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
