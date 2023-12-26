1c906ec58e70a686aa8b48fb1ecc0d988e62e752
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:10:39 +0000

    reply language beat congratulate smoke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
