a96e01b028393736d19886c1297cc832b0736164
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:04 +0000

    quiet past thing doctor family

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Words:", len(text.split()))
