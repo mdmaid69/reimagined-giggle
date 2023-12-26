e871d01d81b47ea1e785105155f294e143a8454e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:05 +0000

    realize repair think dear early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
