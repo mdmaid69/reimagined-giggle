614e172ef3e932368362467300bcb0805698c1be
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:13 +0000

    main talkoutof row matter reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
