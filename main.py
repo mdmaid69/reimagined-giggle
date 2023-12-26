09bc7e51663bb90b43d531df5b34b1a4e168874f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:51:41 +0000

    congratulate lean cap suit use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
