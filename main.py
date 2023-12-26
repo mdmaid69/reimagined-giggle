5de7dc931f6cb2638d77dad9e865c776cfac10e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:52:38 +0000

    meeting either phone available scheme

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
