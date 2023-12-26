e1f3a3fe6757eb5ae88b2fea5c25c6aa0b2d018d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:56 +0000

    bag sure lookup repeat do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
