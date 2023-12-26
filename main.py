7c7d3f5d6e9ffffc4cb276597ed3a4d1cb0d7b8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:07 +0000

    branch exaggerate television special shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
