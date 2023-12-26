275cebf86ced299875929acf1ef3e19e10505fbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:22:58 +0000

    flavour division treat equal explanation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
