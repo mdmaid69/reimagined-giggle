0035625a7f0e169ab6eaa2a3f433974ee83952fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:43 +0000

    set holiday soak prefer together

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import getpass
-def get_username():
-        return getpass.getuser()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
