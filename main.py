912d799a2e854dac1fc13e5f30b4216f0a59366d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:15 +0000

    stick conclusion size provide danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
