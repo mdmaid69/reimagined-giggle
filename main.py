b83aeccceb15397fcd3b87ca840e8ac319808f14
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:04 +0000

    crazy car affect born think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
