9e175ce42c251d0a340f3d067712381c09e9cd2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:00 +0000

    find scared record sharp reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
