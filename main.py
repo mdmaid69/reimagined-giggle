59e7a8f94050155c4220f46bf2b8422bd3dea428
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:11 +0000

    match more control free skin

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
