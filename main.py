25b7e413fdec52acdf18a9985da2aaafb450a6ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:14:15 +0000

    turninto knock late issue so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_array_as_set(array):
-        return set(array)
-def count_characters(sentence):
-        return len(sentence)
+def get_array_as_float(array):
+        return float(array[0])
