407e29e2fe78bc8b9e8c772175849de0d61a8d79
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:39:29 +0000

    inquire file fix wake total

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
