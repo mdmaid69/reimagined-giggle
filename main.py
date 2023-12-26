f70a6b8734347fee68e6de1a19bfceb52112ef37
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:17:03 +0000

    experiment divide scare talk surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
