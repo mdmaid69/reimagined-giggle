05c108384e2ee9a3d39ffa821cc5f3c8dee36396
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:01:42 +0000

    joke be basically port shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
