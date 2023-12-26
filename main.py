06f9cc9e1232bd0ed1d2287e214d6acf855df534
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:40 +0000

    mark might next play any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import sys
-print(sys.version)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
