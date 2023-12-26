02937c70b82df53476551445790c997aa6ecefd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:07:26 +0000

    earn stand pretty transparent pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
