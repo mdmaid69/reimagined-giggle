f639e924d51b1390fd0fb2aea55eb54a66cecdd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:22:04 +0000

    because offence swallow habit government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def change_working_directory(path):
+        os.chdir(path)
