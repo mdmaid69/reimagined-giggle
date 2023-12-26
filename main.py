194989acbf765bc3e4579c4027d20c5b50c6a26d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:07 +0000

    disapprove transparent minor activity follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
