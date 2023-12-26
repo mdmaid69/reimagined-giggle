e1646f5e3aa5681653f82039b997111b7ce43c6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:57 +0000

    authority snow dream bottom real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
