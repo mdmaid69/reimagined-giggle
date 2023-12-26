ca0aa84b8702e3d745be8e57921d55fef1ab7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:37 +0000

    vision collect awake walk size

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+i = 0
+while i < 5:
+        print(i)
+        i += 1
