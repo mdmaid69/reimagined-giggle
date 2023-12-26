9ad190582c5ac439df74d6ccc91179f30ab7c85b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:36:32 +0000

    history form length wire afternoon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
