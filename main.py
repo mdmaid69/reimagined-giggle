1c92f2eb455e6bf1d9a82591f2d45d8707b232a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:58 +0000

    side pretend smell guard possession

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
