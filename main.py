b3dffdb44ed47463ef6a7a26e6a73215d59b722d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:12 +0000

    gas house strike write after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
