bcf5218a6850bacbcd5766d9a2f6d9c1af2a83f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:52 +0000

    567enemy upset cover bath I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
