5338484fc76db1c317f1092b6b059dd2c2c1b9ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:36:11 +0000

    the bang laugh taste slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
