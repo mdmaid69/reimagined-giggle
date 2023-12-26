5971d04bcdc1b54a760c2562e39d3dae07ac6ade
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:53 +0000

    left bad compare slow often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
