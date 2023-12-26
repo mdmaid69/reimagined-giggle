1633fd9471e9d3d5a73f4d2c697bf336e3948306
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:53 +0000

    apply slip once often giveout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
