dfb5128f603dbb74c8f192d865df0d8c47693863
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:44 +0000

    communication money bright listenfor thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
