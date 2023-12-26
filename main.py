282de77086ee2fb7b3620cb2b35ea2df23bf1494
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:16:34 +0000

    gentle file today clean might

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
