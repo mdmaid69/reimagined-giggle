96221f45decbebfa4ac909d63a7d809ee2c77da7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:05:39 +0000

    healthy instruction record either during

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
