1ac59b77d569611e6792f63137e4b2c475f9179c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:05 +0000

    agree oil too start children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
