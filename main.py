08ab7b791eff00ab930c27dba5fb637c6bf32617
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:12 +0000

    rubbish tour suckme risk pump

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
