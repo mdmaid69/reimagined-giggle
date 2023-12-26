0feb420e11257f025883c2fec042c0c75405d926
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:13 +0000

    air onto image cope comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
