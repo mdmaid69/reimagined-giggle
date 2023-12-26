de0b624e5eae1db196a1a5e55ac73a94bd4b5806
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:27 +0000

    handle disgusting company ours joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
