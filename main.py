490e1694d2dff7f004ecc21f62b76452da572331
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:20 +0000

    swimming workon getout fall minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
