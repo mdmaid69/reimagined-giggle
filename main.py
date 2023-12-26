cc2ec76c8bf061cc0536f623bbb50a2b407518fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:35 +0000

    sleep some show job arrival

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def is_odd(n):
+        return n % 2 != 0
+  def convert_to_binary(n):
+        return bin(n)
