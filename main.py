7745d836fb1a8595537ae30d1804ef4f42e6b307
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:08 +0000

    offer quarter 179bite deliberate shall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
