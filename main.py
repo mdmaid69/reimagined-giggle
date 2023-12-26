81c3e96fb3ff5eb9ef6fa6173b7653fb2c81e440
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:17 +0000

    represent mad Jj stone unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_max(numbers):
+        return max(numbers)
