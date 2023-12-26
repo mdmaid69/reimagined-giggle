4cbc17e877989760d307f5627bef6f06967e7998
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:15 +0000

    combine quarter land frame ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
