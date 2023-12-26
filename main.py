d485d627c763a9b1ff325827350325e44fc8ce86
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:21 +0000

    main rate one runout escape

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
