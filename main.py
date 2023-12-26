ec1665677befc82cd20632ad541f44856350bf4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:51 +0000

    such recommend raw listen Tt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
