9ce5ab1b94e969670c435c1a8c536873c3e53404
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:30 +0000

    variety rain comeout break put

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
+def calculate_work(force, distance):
+        return force * distance
