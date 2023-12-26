653a359f6b8a2a5b7b2cb6196efd999e77829a7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:15:57 +0000

    old away compete contrast by

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
