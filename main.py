c686714c61177ccc48bc96b7d463dde1d46e0f72
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:41 +0000

    water ear work quarter comment

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
