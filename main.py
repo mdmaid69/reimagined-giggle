c32ada85980432f4cd20a6b5a91a134d1fdbddc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:06 +0000

    war blue film at depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
