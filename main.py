c38bc35885581e0dbeba9f87f5689a662ae1d0dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:11 +0000

    Qq but interested hide rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
