fa96763b73b2d31c5664b151487796be77ebe81c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:28 +0000

    experiment point friend joke present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
