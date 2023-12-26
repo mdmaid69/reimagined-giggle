bba4e7fefa36fe4040d19c0adea49d2c39c0199a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:17 +0000

    sign straight right fork suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
