fc0765aef2be3bfd5706c317399c939fcc337334
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:03 +0000

    fight noise emotion sink least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def square_number(x):
+        return x**2
