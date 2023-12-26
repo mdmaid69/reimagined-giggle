adb6818949cece6f17a0dba7e7075fc254add59d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:56 +0000

    slip make kick uniform survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_queue():
-        return collections.deque()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
