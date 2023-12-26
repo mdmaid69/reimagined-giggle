7caf3a055b10119c86e47d8c4bbdebd5d0420fef
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:47 +0000

    comment study music harm afternoon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
