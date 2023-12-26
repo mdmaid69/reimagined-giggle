ba9f206796bd82a579896cfaf580f06839819824
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:14 +0000

    section convincing grateful occur once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
