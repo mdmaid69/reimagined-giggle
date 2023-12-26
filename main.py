5799ca18a2c11d416dc4e8567e05d1d05f0f03d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:42 +0000

    slice treat 573entertainment convinced record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
