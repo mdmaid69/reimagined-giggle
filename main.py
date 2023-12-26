6019b891de6096ff984e4e8ee4e29c157ee6c533
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:19 +0000

    behave crazy pressure certainly ticket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
