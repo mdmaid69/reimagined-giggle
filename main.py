ba4606b0be5067ef4e5da90894df63571dfeb510
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:29 +0000

    scare silent trick face minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
