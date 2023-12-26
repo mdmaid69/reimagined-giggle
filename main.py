f486fe087c74121d66d3c3e8c0f5d8b1a1ef0a63
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:52 +0000

    loose somewhere scream relative if

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
