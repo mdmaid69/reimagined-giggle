5794d4c9b7a9b2d847eb5b02469f77ee4e8dc699
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:26 +0000

    ours fat appeal question strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
