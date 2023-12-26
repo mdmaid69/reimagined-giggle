ff76c45a64cc3f487e3fee926a124636aef46494
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:54:35 +0000

    world bottom future absolutely accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
