b4a2e66b36c217ac7ac0d4b269b6298ba660ac7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:00 +0000

    go turnoff oil fashion shop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def cube_number(x):
+        return x**3
