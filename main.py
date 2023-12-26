10517fb1bf42601ce462185acbf0e6f334c7fdc6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:19:57 +0000

    language extra recognize fit computer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_bytes_from_array(array):
+        return array.tobytes()
