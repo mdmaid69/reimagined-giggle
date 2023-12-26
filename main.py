3566a1edae651caeb5d7263e86159be64eb2cf46
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:30:57 +0000

    far grand future profit bored

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def check_if_array_contains_item(array, item):
+        return item in array
