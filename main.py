0fb7b56fc2cf82fad6a0c23f746207b7abec9f65
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:51 +0000

    rid try invite house school

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
