25fa4427be4a5827ddf668b018a4bea46b79f21b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:49 +0000

    unfair certain value appear spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
