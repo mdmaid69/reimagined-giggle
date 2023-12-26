c0d9421686388f7c7903c1e889fb498e53ac026e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:11:47 +0000

    belong prepare place reduce notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
