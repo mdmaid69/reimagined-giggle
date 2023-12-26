463eaddd87036d364c21181ed0f5a214379e59da
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:43 +0000

    spill agreement deaf pronunciation crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
