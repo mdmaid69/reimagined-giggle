07b28c47779958201de7cecaa472913c18c63258
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:32 +0000

    minimum possibly proof squash pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
