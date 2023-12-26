f37675ac9f4abb20902ac628b6f0f49c54f47077
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:48:24 +0000

    culture trade still tap temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
