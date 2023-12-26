60080ebf33318d004efd6e4d8ee807b205151d03
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:08 +0000

    concerned moral official side some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def square_number(x):
-        return x**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
