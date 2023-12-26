dc45ca5f6bc7b22410b108d1c9c0fa467d8a0e52
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:25 +0000

    male average imagine that takeaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_area_rectangle(l, w):
-        return l * w
+def is_even(n):
+        return n % 2 == 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
