dd6172ac0bcde2ad7f8ac6048c4d10098576f709
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:14 +0000

    happy act giveback rob description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
