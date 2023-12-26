72d5f8f1b964c43c1d5e4f2c35bd5aa27b5c14e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:37 +0000

    plenty scream this prove circumstance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
