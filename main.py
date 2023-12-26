27d3a2f568498e162486298e3d8abc4ddc31df9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:45 +0000

    give shock bet up subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
