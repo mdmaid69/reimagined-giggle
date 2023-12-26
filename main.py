190bc05b2811bcae8c814e3f6b76e800fc4a0f73
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:59:25 +0000

    read set bite opposite key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
