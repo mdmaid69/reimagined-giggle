04c030f04394282f82994b08891723bd15cf0892
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:31 +0000

    attention will luck else undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
