52a3ad03b0120adc44d1999cae68dc0c03e78602
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:22:22 +0000

    hold excuse day week dream

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
