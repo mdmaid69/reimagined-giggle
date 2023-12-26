3bba083f53ca616573a999de7b4c28ae63585028
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:56 +0000

    properly to sudden play world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_pressure(force, area):
+        return force / area
