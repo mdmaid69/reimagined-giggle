1ecd657e98bd0abd13436d2c8bf1c3a4b2add305
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:29:29 +0000

    mix Rr advanced dick period

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
