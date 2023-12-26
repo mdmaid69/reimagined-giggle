7436fc49f34a9925f2261451f666192e63272a63
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:51 +0000

    comeoff pipe giveup spot train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
