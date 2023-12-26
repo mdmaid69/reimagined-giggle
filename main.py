5409f0624061083f78d0ff9232c28401b57ca980
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:03 +0000

    actual shame present impressed pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
