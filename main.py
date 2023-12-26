ed333d2df2a400ff747f02959f2a88582e0e0163
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:34 +0000

    mass nervous mood perhaps complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
