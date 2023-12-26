7e08f3c508bc94b823e85f554002d7f716ddddd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:37 +0000

    jump understand fortunate safety emotion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def add_numbers(x, y):
-        return x + y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
