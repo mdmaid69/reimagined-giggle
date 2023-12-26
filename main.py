b5a10e3e8df8d7a10096fbe75e147d3da86191f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:28:43 +0000

    supply best success port raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
