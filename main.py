80f7cbb6c85ff41ee3178185b992f7378a625e9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:16 +0000

    collect fond tidy him present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
