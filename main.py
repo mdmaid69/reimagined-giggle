ed2533fb4665caa9a5e61cb093c0954e49f8c106
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:48 +0000

    attractive Bb film relationship drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
