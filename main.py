7ccdf961aa3427bd95826b8b8fb1f766bed2df33
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:22 +0000

    clean share expensive switch flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
