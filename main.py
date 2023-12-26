cf59a7abf7b20af57a2ab0febcc762cc54449fda
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:18 +0000

    will because release angry any

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_list_from_array(array):
+        return array.tolist()
