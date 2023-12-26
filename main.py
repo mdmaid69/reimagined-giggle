40f72015c92603a4e3f904b20e32f98965da8bd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:35:01 +0000

    straight score straight between switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
