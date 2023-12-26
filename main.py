81a61412377ec7b4cea6e04d0dd7ac191bb9c81d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:04 +0000

    often finish turn hello slow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
