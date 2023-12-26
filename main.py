1290f8d108702399b5e0c20aff5590f2b29301fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:04 +0000

    immediately track material too aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
