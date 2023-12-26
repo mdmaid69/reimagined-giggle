110e2d2b1cd4c85650144af10f65ea9ab84f9ab4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:11 +0000

    exploration tell comfort extremely hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_directory():
+        return os.getcwd()
