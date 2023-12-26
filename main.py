3101fb31f6e6e606a10b630fd7a4f355b95f2bab
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:22 +0000

    pure properly risk breakinto entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
+import array
+def get_array_item(array, i):
+        return array[i]
