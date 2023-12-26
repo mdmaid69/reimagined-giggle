f965777cd6d76c734bf0a86593e065c68817096f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:23:56 +0000

    rise most either slightly pretend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
