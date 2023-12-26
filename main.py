60b145b9fd766d8009babcf603732e8dacb4b8e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:42 +0000

    edge true number needle effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+print(math.pi)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
