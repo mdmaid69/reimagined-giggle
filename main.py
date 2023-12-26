c2896e9f9b5ff6d80c74852cb059738f1bc557f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:55 +0000

    unhappy formal telephone turninto tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
