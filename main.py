ba4d9906b88985b7b8ecc630d884f144ae5deecb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:29 +0000

    damp design over experiment uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
