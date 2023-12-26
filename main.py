49f44ab5b9f5a2d82046c6899dd42e0f09750d04
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:25 +0000

    reject politics thick report suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def subtract_numbers(x, y):
-        return x - y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
