d0a06e41a2c8c8def52b50dcc5ba895fc91cefe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:42 +0000

    count improve promise clean behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import sys
+  def get_python_version():
+        return sys.version
