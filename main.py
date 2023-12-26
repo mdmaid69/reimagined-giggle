4fa1e1390b92eb3a084fac51ea1cff8d26d0b744
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:03 +0000

    stupid everything boil myself aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
