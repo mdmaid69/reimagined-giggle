7d2f7e0d364c3fe8642037b8c431f52fd9fdeb36
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:06 +0000

    violent bite pretend wrong offend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
