c266696a4c6d61f73af94a7035c3e74f06f622ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:42:45 +0000

    yet fit everything comment holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
