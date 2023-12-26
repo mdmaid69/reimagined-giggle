a7dd0990800a9b7fcfb16c2e3bd1a978e96639c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:24 +0000

    offend finally command remove fond

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
