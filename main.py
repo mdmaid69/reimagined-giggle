a19954f6940a0fe31aa5e6b73f695dc017243d17
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:47 +0000

    past base class come this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
