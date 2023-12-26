c89a746db0d132006d49430c3bbfdcdf91532f76
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:25 +0000

    noise throughout deliberately male funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_even(n):
-        return n % 2 == 0
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
