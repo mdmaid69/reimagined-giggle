5419c627a8fad0e0bb60f1c9f04d3e0e5c189456
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:49 +0000

    holiday early morning risk turnback

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_array_as_str(array):
-        return str(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
