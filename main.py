d40af76a82055032ce79d3a3554bd70c1ce44481
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:11 +0000

    hurry what usually feeling equipment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def is_even(n):
-        return n % 2 == 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
