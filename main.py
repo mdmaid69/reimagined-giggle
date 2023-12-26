7d0b45c2e532bad589fbfe5a8877dbacf579b56a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:58:24 +0000

    suit delight forget welcome damp

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
