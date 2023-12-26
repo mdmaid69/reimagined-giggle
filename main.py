0b3d40d912f8aeaeebf863f8d665a711a56ccd60
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:42 +0000

    chase possible flow raise swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Words:", len(text.split()))
