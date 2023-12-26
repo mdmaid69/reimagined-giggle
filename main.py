26858cafe5aefd16e14cf1f8da5e549f5fd1eb1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:22 +0000

    uncomfortable lookback(on) callup bury key

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
