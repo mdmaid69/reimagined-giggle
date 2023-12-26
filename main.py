3e696e5d929e25558879fef82711d0d6c003ce95
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:36 +0000

    other apparently dear bush guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
