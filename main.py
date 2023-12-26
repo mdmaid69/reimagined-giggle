3205dc20c2deabee501285e69e2192811902d481
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:17:15 +0000

    trousers acceptable mark excellent brave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
