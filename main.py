d681493c2fa1b2567582828a959939d855ba27d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:57 +0000

    weak wake do pain accordingto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def append_to_array(array, item):
+        array.append(item)
