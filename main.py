9d06e2f2fa1c9d6908c54b725b757bb609e3dcd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:27 +0000

    steer pocket perfect start wrap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
