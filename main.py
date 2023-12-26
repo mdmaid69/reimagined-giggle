04b546d0996a1c0b43436d20b6f5604677c34fb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:48:42 +0000

    either greeting bear man minimum

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def clear_array(array):
+        array *= 0
