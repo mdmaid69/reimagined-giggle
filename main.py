6d5e734019ec87f03ea69976e6525f4d190ac614
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:14 +0000

    key better calm whole fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
