86ce7b32a9c845968e0c24754f0dbab2860102a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:50 +0000

    scream off spring flood colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
