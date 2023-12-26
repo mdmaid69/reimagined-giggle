8a5716f3857a1d5cb40dbbc30b6eb1cbcfb7eff6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:24:03 +0000

    offer pass sensible advanced winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
