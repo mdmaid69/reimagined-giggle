eb7dea54b07360be297c613a156cf3cf897f567a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:37 +0000

    next abroad sore identify splash

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
