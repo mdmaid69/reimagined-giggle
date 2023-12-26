dcdb9d3e35ec3352719dc07d7919c621da678d41
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:20 +0000

    male top cough deliberately thought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_pressure(force, area):
+        return force / area
