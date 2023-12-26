c78c2f3661358c72159311d4a1653a02749a4d88
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:29 +0000

    secret opposite contrast height market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
