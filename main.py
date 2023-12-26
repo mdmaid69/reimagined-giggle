d2a42ccd37e1342906f3e9220df505e565d61c6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:57 +0000

    course disagree underneath single passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
