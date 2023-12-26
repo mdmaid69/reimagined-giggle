0b3536dffcb61f4753bcb0fd15e11ffc43e8adc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:11 +0000

    divide weak move breakup sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
