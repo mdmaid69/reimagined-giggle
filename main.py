7d8857a68728f5a490e75f9c4952bf3cb132bac2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:11:58 +0000

    spend Cc statement nonsense turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
