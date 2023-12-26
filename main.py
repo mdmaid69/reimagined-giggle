17bd02444055d7b161983821e79ae1807aa9a713
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:12:10 +0000

    vanish undo Ee thinkthrough gather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
