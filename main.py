2f19479e29363c6e21be8cd518887ed49935f543
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:23:31 +0000

    combination switch carry understand laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
