a056f79665003bb79cbfe6f5533b5471b8c0ffa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:44 +0000

    central flat write recommend scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_even(n):
-        return n % 2 == 0
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
