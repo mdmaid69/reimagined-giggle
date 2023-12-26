e36cde24616ce84c4668581b96e6aa13794a6191
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:32 +0000

    but goon thinkover narrow will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(x, y):
-        return x + y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
