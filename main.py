b7a842f15fc8a560af5ea432f30e765863d85a2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:26 +0000

    listenfor excellent introduction story ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
