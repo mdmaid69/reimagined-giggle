fba79ed2fed41ded002cbee1f509f384c949886d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:10 +0000

    beyond sick only tone slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
