9fe9d8aadb8eee9c46915019b31812d586cbbbb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:13 +0000

    571enough note then design about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
