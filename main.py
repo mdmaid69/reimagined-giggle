c80b802db93ecaf2575d62cfeb3ac71d502e8415
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:14 +0000

    music waste war earth great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
