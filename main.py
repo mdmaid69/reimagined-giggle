ac38cdd956395f292b546944c74dfcde18ab520f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:46 +0000

    future swallow weather high office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
