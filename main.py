cf2f6183dd329f59e298c61ceb281982e48664f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:52:30 +0000

    send 573entertainment thanks deaf switch

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def convert_to_octal(n):
-        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
