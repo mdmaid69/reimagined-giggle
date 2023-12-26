5b380c1790e42998a10ea3ab645649c7b864ebb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:57 +0000

    exciting problem pull only drop

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
