94c6a54c36cdccbc5f0f5dafda648f9927f67b49
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:41 +0000

    fear test smart dear boil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_min(lst):
-        return min(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
