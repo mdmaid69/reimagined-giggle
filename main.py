089fd02021f0670d5dcaaf4d4cf4a496ccd0520a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:12 +0000

    interview 566end left past suggestion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
