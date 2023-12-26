2fece9a5f4d0296b7e46e2b3d128de3814fbbbaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:09 +0000

    stretch visit nor treat collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
