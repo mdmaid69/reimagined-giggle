7c488c571153990274d412edcaf63bc532dbce00
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:49 +0000

    whisper couple high score treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def clear_array(array):
+        array *= 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
