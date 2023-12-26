86f1cb03696b2033fbf236a66b520961c634ca05
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:06 +0000

    preparation already top watch much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
