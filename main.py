22d93f9c81c8169f7901ef363bfccb28f86ffe77
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:58:01 +0000

    dance reliable drop poor build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
