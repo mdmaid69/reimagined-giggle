4b620e371e13c69136dd408fe811fa93f9eb2b09
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:04:42 +0000

    cheek ever flight guilty fortunately

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
