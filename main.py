f0d66a709a01b028fe9fc87e2d2d2c102d298965
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:43 +0000

    without near afford constantly necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
