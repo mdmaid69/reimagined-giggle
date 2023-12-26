0bbfec95838a45b515dc811b9b50ee5e155c8479
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:36:01 +0000

    know here moment concentrate commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
