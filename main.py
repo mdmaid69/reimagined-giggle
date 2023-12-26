19ae01d04c4a14a761ccaedb6ed89483888acc6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:05 +0000

    piece remember Oo cry can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
