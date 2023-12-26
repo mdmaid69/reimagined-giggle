a1c748c294a7a2d0463e15e3ed517efb6d6483b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:29 +0000

    run bother service deep the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+print(math.pi)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
