0ee9e1edee98b311cce56323c18021ef899249d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:54 +0000

    kick slightly press chief accommodation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
