6e8bedd5446739300fba56386c583af594f2dffe
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:45:06 +0000

    well warm trust danger teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
