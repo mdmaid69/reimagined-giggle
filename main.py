d4c5209ab2dc12673ae1b57fa32ba9239909afc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:42 +0000

    whichever process let justice no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
