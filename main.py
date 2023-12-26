50fe6f5fd81f249de8d8c6699191730d356a0664
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:23 +0000

    ready relative far thinkover turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
