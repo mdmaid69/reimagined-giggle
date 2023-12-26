07ebc8f8b132431412764dc20ef8d96cf439db1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:41 +0000

    572enter serious unfair shout giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
