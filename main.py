9cc11e48af984cd7c15684243b7b9bb547ec1269
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:18 +0000

    anywayanyhow money eye eye car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
