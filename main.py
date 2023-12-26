8aea918c24fc6d41d598220db65a3f2193ecdfba
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:02 +0000

    bit carry shine plant second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
