0a84501a57b5d9ebc8e8301d3a03656b794ce520
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:17:45 +0000

    example wise own store charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
