c872f512cbb73282ce85edb0612b8896d8a53011
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:43 +0000

    unite badly add cross emotion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_to_binary(n):
+        return bin(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
