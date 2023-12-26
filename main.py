c2dad75cb15927d81ea1ee3d0bd49f27e00b4652
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:51 +0000

    question give forever sea boat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
