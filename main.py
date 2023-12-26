5ef5f89a254df8e3f62467b3e0f1929ca7662217
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:46 +0000

    eat better stick soon cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-print(os.getcwd())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_acceleration(speed, time):
+        return speed / time
