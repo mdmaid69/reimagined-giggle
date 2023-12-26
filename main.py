934e2510b3a9561db0280471d6eeb6a0d8d25213
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:22 +0000

    wrong goback(to) achieve tone phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
