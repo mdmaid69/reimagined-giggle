fd76b47c8a739bdc4cec05a0ce36ac602d6dc4fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:20 +0000

    exist fail entry underneath basically

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
