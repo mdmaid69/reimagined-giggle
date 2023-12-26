f27612ab02773676f7f5496be93751856b675582
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:08:50 +0000

    scrap heat turnback check begin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
