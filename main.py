75e6f35c6b9fc37d37925eacb3cdc925f7e6ee44
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:51 +0000

    address later reserve sentence amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
