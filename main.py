e53cdfe7611d48de5bc202548403e47eb1ebedbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:20:04 +0000

    science past slight thumb little

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def print_python_version():
+        return sys.version
