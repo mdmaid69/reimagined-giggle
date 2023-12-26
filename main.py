2abe40aa7e7eb0a452cc57bb560cd3a6f3687ae9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:19 +0000

    touch film take once point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
