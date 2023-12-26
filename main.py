24e9ec7ecd96ad8d700cbe2b634ad7d22a53d5ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:50 +0000

    hard power turndown hers habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def sort_list(lst):
+        return sorted(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
