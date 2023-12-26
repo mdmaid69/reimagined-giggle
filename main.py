6f90eb2e2adeda158ed7b0d0b534793a594f63aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:27:47 +0000

    cover enthusiastic any busy proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
