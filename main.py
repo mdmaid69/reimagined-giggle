c239218ca4fe2057427e1d0313223ca1c0abb76f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:43 +0000

    include proper half dangerous picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_int(array):
+        return int(array[0])
