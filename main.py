da027d932eedc457c7fb3a8af82cdfa83e4bd49c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:07 +0000

    breakinto region ride meet science

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
