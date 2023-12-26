685b464cc608889b57de3d8a15c3b059a229d362
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:44 +0000

    below regularly uponon boat runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
