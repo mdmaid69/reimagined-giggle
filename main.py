a7e64a9a08258611c9088ed9b6af52cc13b257b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:18 +0000

    almost everywhere either keepup extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
