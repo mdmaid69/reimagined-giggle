4477aa36bcde19a22b927cf34e72fd96f7faafb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:58 +0000

    discussion taste suggest opportunity bang

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
