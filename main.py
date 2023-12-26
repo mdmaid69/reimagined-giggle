e5bdeba5c4d717480f4ce131faeceae116a2d932
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:19 +0000

    beginning go getback book dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
