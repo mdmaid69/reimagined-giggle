d6105f1c62e4bcd5afb8ca8f0d097cefe8a75d4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:58 +0000

    length load always gooff correct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+i = 0
+while i < 5:
+        print(i)
+        i += 1
