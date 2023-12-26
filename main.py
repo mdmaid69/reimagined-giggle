7ff30e6e8356a595907bbbd896c6a191e42c9d55
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:36 +0000

    impressive workon along present praise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
