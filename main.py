86e83abca39e0e0ef71b34bd0b42aaf33a3c5076
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:13 +0000

    stretch fear use ahead base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
