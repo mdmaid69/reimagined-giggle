53ef6cef32cda4f8b639bd8e3a7c27891de9240b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:17 +0000

    close own drink town around

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_speed(distance, time):
         return distance / time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
