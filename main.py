edaee3d5a5850985d46e3f89d97703048672fca8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:10 +0000

    breakout prepare mind there nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
