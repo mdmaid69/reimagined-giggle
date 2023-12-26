98fbc2cdef12e7c332443288efc8449e9224da31
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:06:09 +0000

    care give prefer expensive result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
