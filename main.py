5ff47a254c3c1fc77c6dfc6403eb4d9ee8f22bf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:35 +0000

    excuse Qq grade coat impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
