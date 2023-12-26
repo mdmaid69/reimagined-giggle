b2eafb17942983e2b4948d99126db3b8a32afb60
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:45 +0000

    give male as play next

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
