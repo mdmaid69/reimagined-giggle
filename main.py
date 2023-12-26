c70213405d6e1c161ab5cdfb0c8bf61a18bb087a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:56:46 +0000

    score fast exam after nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def clear_array(array):
+        array *= 0
