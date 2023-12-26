bab4297d060e3130ed6cc8bb3223e59522acccac
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:41:16 +0000

    bet where quiet question unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Reversed:", text[::-1])
