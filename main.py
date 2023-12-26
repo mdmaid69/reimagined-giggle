19a3c32d63ed21914170e976dd43cf9ef1f4372f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:08:39 +0000

    hers skill movement claim fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
