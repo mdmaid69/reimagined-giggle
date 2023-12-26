6a507e329a0edea96ea6a69a054289e840ae78bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:50 +0000

    injury new reach follow calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
