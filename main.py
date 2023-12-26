0a013b4cc54b26a08529002d22e41013e96c3135
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:21:36 +0000

    cool desperate song promise serve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def is_odd(n):
-        return n % 2 != 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
