4ae2d829f6fb3a92207ba737648134cab7f4b512
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:07 +0000

    temper common continue beneath hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
