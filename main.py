dd36a0f8b76b791005f437ac22a2f4d5fb97d891
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:00:41 +0000

    dull bank real nowhere ride

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+from collections import Counter
+print(Counter("hello world"))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
