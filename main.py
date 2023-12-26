a1add81575fdf3f3f2e469e402cdba18034bbda5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:59:06 +0000

    shape through everything fair service

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
