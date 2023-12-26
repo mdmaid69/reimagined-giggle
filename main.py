d3940dcc31dc9edc74a070a428ee8652b2e01a10
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:24 +0000

    cruel smell thank first approve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
