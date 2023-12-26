78fa932c15372fe4cc7546aac10382b0be843f59
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:24 +0000

    nowhere occasional pay pay tonight

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def add_numbers(x, y):
+        return x + y
+for i in range(10): print(i)
