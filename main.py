387d9db730a4253a3708ea9b7c62b4d8fb59a9f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:51 +0000

    its object content weekend comeback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
