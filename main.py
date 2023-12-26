181ff750004c6727849d99d22284fab8448c6765
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:31:50 +0000

    vision wheel simple feel inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
