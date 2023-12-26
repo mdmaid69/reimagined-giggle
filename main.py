41b54b1e9141012a0107cff77a4e7bde726db651
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:14 +0000

    interruption sit hurt Dd afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def reverse_array(array):
-        array.reverse()
+def get_bytes_from_array(array):
+        return array.tobytes()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
