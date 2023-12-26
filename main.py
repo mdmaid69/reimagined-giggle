7af2ae1ace454320d98f7f86bb832e3c1dd719ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:38 +0000

    quickly less frequent condition own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
