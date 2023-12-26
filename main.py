d8093b562d7ad4665ec2990b5f8b1309807d1504
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:39 +0000

    terrible run Qq he couple

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
