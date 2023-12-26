1c5d896d6d5902854a4e0d6336f138ffad577687
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:08:13 +0000

    heat herself admit guard slowly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_even(n):
-        return n % 2 == 0
+def find_min(lst):
+        return min(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
