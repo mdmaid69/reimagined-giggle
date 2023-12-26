a5956b4e77a59dfaf70d271965b3073530df4cb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:27:55 +0000

    promise once level emergency tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
