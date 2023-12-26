7655b9474819ebb33978e972637112c046ff18d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:45 +0000

    copy rush agree straight guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 def add_numbers(a, b):
         return a + b
