b50ae89e72e7eefedf8e0d28d0d764e4e0da04d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:19:55 +0000

    offer shelter free fast beside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
