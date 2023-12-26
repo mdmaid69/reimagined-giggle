07f52eb533b13629a8d6f2a90b820c93f3732205
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:42 +0000

    angry grow obvious base pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_time_since_epoch():
+        return time.time()
