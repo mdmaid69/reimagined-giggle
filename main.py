5178bb14d2ae00db185bed47315faedc5f65f8d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:21:57 +0000

    round drive certain intelligent string

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def remove_duplicates(lst):
+        return list(set(lst))
