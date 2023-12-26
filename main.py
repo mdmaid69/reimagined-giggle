638042a4a735c29d7ca713bc04039475f0ce48e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:56 +0000

    among afternoon suddenly always unusual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
