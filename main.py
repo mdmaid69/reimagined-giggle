4358785d7a94130cdc84f71bde71d323b3ce91e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:21 +0000

    after drink edge touch period

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_time_since_epoch():
+        return time.time()
