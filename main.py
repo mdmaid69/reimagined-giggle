986881fac0c0505c2a7acad4ad4d70f6e3ba7eb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:53:46 +0000

    country uncomfortable handle provide traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
