0e7f81f04d2922dddfbcf5eef79b660fc3ad045c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:12 +0000

    artificial ring spoil tip escape

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-print(os.getcwd())
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
