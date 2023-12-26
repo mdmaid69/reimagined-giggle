9ab38e28b62922c234ccd5c6275e7d6f8346c4d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:49:28 +0000

    uniform giveout deaf your trousers

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_as_complex(array):
+        return complex(array[0])
+import sys
+def exit_program():
+        sys.exit()
