faefb4cf407dd8b61f2282c177fd2d67f763f596
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:36 +0000

    public married every accommodation comeround

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
