914ae9b2821bcb565ec27ccd11a3c2282c0dd9f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:20 +0000

    form butter buy plastic shoulder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
