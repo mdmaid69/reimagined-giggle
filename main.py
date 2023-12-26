90d4f3f4d29ceabfafc2444c826578485320475b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:14 +0000

    loan fit exhibition cover basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
