60aee3f61254bc67d7d3c94157ec6bc4ebbe0e2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:20 +0000

    once their light answer emergency

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
