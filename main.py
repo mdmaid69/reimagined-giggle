115debc8f229e0550ef04a69d48d4de26f67d8cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:33 +0000

    ought completely somehow over borrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
