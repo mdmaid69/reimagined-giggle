25254a8b09998e65c55b54b8dba593588a6fb10a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:30:14 +0000

    careful invite wet keepaway translate

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
