e069d15e05ca6021c0a5d1fec8cbce5ed9d84ffd
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:01 +0000

    rescue task trick fresh method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
