aedd98ae715865c7fc2c0260453d6b9a848ab066
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:22 +0000

    century class organize number she

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import array
-def get_array_from_bytes(bytes, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-def calculate_area_rectangle(l, w):
-        return l * w
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
