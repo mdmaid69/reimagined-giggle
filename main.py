f4b68c815dda15c0790b4b7bb5d0b8eaf2b39d56
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:08 +0000

    join brave noise forever how

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
