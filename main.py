e0c6c1c8a2dd34d04fda77d699f681a41ba03de2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:35 +0000

    runover with him table walk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
