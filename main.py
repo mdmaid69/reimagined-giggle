5f2a8391271ea5cc0b4daaec55096262333a2097
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:30:21 +0000

    accidental listenfor stiff report character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
