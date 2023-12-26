5305f36fcbedc291f4fa5be6035b81e5318eedc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:54 +0000

    mood trick tip unhappy support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
