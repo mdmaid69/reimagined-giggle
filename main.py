3f7c24390644732cb841180b6643bc53f9554f6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:54 +0000

    condition benefit behave on fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
