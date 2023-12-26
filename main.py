81995b807510c5ea8de9eb6c75e76123f7165ccc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:44 +0000

    house wing name rush will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
