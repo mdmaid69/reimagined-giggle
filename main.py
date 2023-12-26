20cd8a3fe1666cbe2f51d3570361a9b187c60b33
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:50:34 +0000

    part tend discuss speak rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
