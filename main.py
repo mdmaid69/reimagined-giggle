0c97c25a7446ff6b05120c89dd04d783e5d50270
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:58 +0000

    sticky next system crash pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
