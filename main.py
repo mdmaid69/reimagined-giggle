b5338460a20c1124514bf9319b1f7265237081ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:39 +0000

    now profit shallow hers already

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
