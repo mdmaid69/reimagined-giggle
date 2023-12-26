ea4ff702b1ace0260e6dc19a271b5edb7831acc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:24:05 +0000

    label possibility me respect admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-print(math.pi)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
