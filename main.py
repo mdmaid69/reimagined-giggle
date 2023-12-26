33ae71fdf1860d727abbc575b54ce0e0efea7308
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:09:26 +0000

    worried main impossible chief chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
