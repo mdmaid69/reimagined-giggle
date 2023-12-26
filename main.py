39f00ef9d44949d9f9d49f32cc65dff930c370c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:04:17 +0000

    decision decide pause arrival swimming

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
