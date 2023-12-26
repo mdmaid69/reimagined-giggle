bc1edae53e31ef043efdd02c4d8c8ddd44369342
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:49 +0000

    exit loose over under experience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
