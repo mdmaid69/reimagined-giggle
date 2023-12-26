5e350259ff0593dd0ae2ec974d5d8f079c36789d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:02 +0000

    miss taste whole push concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_work(force, distance):
+        return force * distance
