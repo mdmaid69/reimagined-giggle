60f0f53a9991c900d4df952787838697dc50a19c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:34:33 +0000

    holdout from exactly ilovedicks know

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
