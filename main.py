dd60fd4f85a7906af4093efb2e91c601ae9cac12
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:32 +0000

    ability manage shape 571enough who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
