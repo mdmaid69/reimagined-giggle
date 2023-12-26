1f192a739eb34e608592c1087c2cf6f0ae937fa5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:41 +0000

    check kind purpose flat might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(lst):
-        return max(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
