0424c0ee7aa871d787eff567f95b4572d12752a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:19 +0000

    crowd loss emphasis deliver runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
