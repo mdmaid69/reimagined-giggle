33d7acfd3354e0e385907f872c9cc8d94f0c6c8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:10 +0000

    carry move introduce hard secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_floor(x):
+        return math.floor(x)
