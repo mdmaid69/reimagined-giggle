35ed831bdfc407ab1b8866ce39c0d17ce005d1b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:47 +0000

    office our wash would centimetre

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
