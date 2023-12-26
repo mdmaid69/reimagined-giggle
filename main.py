4ecd796ea172bb350025d88f376952d812be3a64
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:26 +0000

    conversation night behaviour grow consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_error_function(x):
+        return math.erf(x)
