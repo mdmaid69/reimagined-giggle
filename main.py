dfec3b04bc65de663b03c698a999d53efeeded86
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:24:17 +0000

    train safe broad compare book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_exponential(x):
+        return math.exp(x)
