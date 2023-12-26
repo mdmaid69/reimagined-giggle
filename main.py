f65af6d3e256355d869080d6dd46dbeecff96353
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:28:03 +0000

    holdout sink on contrast high

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
