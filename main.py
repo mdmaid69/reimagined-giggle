088d25c4815d027ca9efe9b671a3288a33f6037a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:18:04 +0000

    talk translate keep miss few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
