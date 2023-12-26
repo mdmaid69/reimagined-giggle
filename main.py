09bbc09954020ca54d865c333fcba1807ce34b27
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:41 +0000

    back blame stir refer tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
