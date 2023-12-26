3cb967b50fd14cdf6188fb9cf900686a13c9b1d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:32 +0000

    aim bring shall get chase

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
