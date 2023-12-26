2e95b2c5a6709e1dcba640f55e7b5977b8a59a96
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:10 +0000

    life live person shop so

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
