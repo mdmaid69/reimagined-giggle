7868acc40ec1508ed88b77c0548b62731b6d0b87
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:48 +0000

    large describe near equal reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_bytes_from_array(array):
+        return array.tobytes()
