24b6a3d6daf1f32d577afbd4f80260df152cf820
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:30 +0000

    practical means each skill tune

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_repr(array):
-        return repr(array)
-for i in range(5):
-        print(i)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
