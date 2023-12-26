4fd015b52cc0417a83b4224ab4ae3e8e303290ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:57 +0000

    practise strength somehow Dd keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
