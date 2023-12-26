9405a39a8fcac4425091573316cb2fb35fe363c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:57:15 +0000

    wild fight price shoulder fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
