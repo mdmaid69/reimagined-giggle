29ea61ec12d4069d897e265a99a9d754f7a10a1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:41 +0000

    brain opposite scare surface summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
