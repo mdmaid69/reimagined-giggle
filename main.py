9915e50e8fb6fc351123e5c8d23e9198ef802733
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:59 +0000

    that stone goup before every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
