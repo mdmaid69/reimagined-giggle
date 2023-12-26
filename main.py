6ae7dc44f398880e892659ceaafd07489fe10bc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:32 +0000

    increase exchange commit onto bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
