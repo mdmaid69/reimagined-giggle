8a0765363bb5c99ba3ff2794e44bc08224b2caad
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:31 +0000

    doubt share goback(to) private amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
