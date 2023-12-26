ead019af7d6cf613d34e595687f02abe2132f675
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:39:53 +0000

    scheme sick still attach direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def count_words(sentence):
+        return len(sentence.split())
