02fcdbdf0e24f321d5439d0bec164be5fb5624d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:15 +0000

    off dick increase undo lean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
