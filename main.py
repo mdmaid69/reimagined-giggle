41e1ba53ec904d74ca13cecc84b6afcdb25bd420
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:59 +0000

    Dd holdout comeout 566end air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
