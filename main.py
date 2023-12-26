0dc7ba0b9784694c4a3effb5416e40a318c26cdb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:04:34 +0000

    principle Tt battery regard block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
