7fc785b230d2100335ccb292d900fb2c9d1302a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:25 +0000

    ability guess factory count hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
