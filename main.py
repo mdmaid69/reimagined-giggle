6e1287df552efb59f827410c8c4eb664282a0ad3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:08:03 +0000

    season quiet move pity concerned

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def cube_number(x):
-        return x**3
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
