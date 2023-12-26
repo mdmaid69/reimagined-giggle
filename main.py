6576e1517fcde70e329dfc29b84de3546fa77b40
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:33 +0000

    pile movement sea claim commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def reverse_string(s):
+        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
