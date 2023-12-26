0ed2e57c51d7720276df60fd6fbe1bf7c52b9107
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:49 +0000

    whoever oil expect chance comein

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
