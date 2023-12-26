d9bc4c61ef1a25360df1850d1535a5e59229db78
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:25 +0000

    majority may recognize carry official

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
