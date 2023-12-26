beb37897d2eb41ee1ff1a035c1b907db286fef5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:52 +0000

    doctor break design thumb exciting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
