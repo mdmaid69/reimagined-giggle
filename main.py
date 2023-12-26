03bb28553464d392fed9899bfef0aa8cf30628b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:34 +0000

    home charge offence cough tradition

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def print_python_version():
+        print(sys.version)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
