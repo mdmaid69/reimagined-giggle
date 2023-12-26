1f57ebf2244819e4ae9a3199abf22c996680ef6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:18 +0000

    talkoutof burn exchange right leave

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
