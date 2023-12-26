7e6667421a0f10e6615f6a5b77ecf8810b838572
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:25 +0000

    doubt calm advantage continue message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
