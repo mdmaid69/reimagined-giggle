4bc86c27708766f2d7c17f9f8875664ba1bb726d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:19:25 +0000

    band sticky line disturbing share

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
