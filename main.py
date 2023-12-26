ddb62cca3fb75640f982e9fd61f0c8f110d5e2d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:16 +0000

    transparent storm outside fence subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
