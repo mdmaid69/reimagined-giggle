753718d84454edbc89a9d909468ec8f66a648a64
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:49:36 +0000

    concerned female brief load emphasize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
