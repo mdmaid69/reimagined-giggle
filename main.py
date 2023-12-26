814449d86d3c5b170dcd9559f3eba9fde7fd663e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:36 +0000

    unfair away attract school everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
