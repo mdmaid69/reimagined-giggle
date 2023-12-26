a6992516b31dbdc6ce64be4e2bc1dfe034aa3e68
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:59 +0000

    accidental children production tool think

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
