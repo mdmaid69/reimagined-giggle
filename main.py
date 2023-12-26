0ff18773aad1aa5b30465747a85fb6c8aef325ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:40 +0000

    comedown up complaint thankyou emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
