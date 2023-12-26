642d3e8d7cb598ecdb0ab7dcbbcc20a56c7bb3cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:30 +0000

    mend through listenfor often wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
