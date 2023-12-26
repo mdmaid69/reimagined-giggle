5943cdcb137582a50098993b05b0f051d69d13e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:17 +0000

    concerned noise situation style jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
