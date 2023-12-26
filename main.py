9c968f786ff807e10f3a4f37879a3670a784775f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:50 +0000

    relative perfect rub decide warning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
