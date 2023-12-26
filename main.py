28866d871287eb0510842c83f3afe86bfa62d254
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:31 +0000

    mistake 569enjoy getout soft limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
