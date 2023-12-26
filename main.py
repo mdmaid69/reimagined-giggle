42c9db7598f618a62c3e8dfebb3cf2b6e7e3ed8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:07:46 +0000

    takeover of suck plastic through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
