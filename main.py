cf40a39e1647b967c7f206ec166938f18b34c252
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:27 +0000

    communicate destroy legal holdup ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
+import collections
+def create_user_list():
+        return collections.UserList()
