6ed9cd1055b229595c6e69d14e7482914b107658
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:19 +0000

    firm Yy leave sound burn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import sys
-print(sys.version)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
