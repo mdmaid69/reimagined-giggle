6b3a1727a68f5dd5a9a4ba359b86353f80788a1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:17 +0000

    national pen discussion taste warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
