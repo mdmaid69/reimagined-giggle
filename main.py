c4851545e500cdaaf85413a480c4635b06e54bb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:30 +0000

    convince handle hit behind difficult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tensorflow as tf
-print(tf.__version__)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
