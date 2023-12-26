21640db2c8ecf87a24413843a4be6907c5f72f6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:15 +0000

    thinkthrough familiar put rather ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
