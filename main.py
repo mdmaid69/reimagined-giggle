8c59754b936dd5f00e78c0312f3bc7acfe031212
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:04:00 +0000

    prepare formal loud serious love

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
