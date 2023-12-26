5098e810a172145e762eb85e387efbd4eeca7b05
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:30:45 +0000

    advice circle return land replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+name = "Python"
+print("Hello,", name)
