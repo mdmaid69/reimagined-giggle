b23a1a9f00c37f8b90a6399d896e091d235492a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:59 +0000

    far dress explanation cause correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
