627f4ccae58bce555242b9283f0ebf4cb522cada
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:18:19 +0000

    rare laugh you autumn can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
