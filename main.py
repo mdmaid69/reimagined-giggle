4ca764473785469b2bd958fa7cd3c6f0a906fe1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:01 +0000

    round feel doubt snow hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
