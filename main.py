7c8cb5715a533216c778a4a9d1235698500937de
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:31 +0000

    sure importance base how in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
