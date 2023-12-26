54720001eaf4e9a69aca90758db0d62c42741cab
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:22 +0000

    even politics goodbye nice emergency

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
