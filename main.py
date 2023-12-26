11e082cb1f4c5bc2167f5beb5d1968ccea8368ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:24 +0000

    constantly ocean replace change interest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
