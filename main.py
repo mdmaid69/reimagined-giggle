d50122bdff755a69dc5545facf1f47d328875216
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:22 +0000

    concentration firm car swallow comfortable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def count_elements(lst):
-        return len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
