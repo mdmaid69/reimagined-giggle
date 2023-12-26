5ca83bdad5b5702707b3f81e54e86487570ade6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:23 +0000

    whether clear difficulty punish ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def convert_to_hex(n):
-        return hex(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
