7cd7ebdce51b8be9dcc86ae3bfa7286ee217ae06
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:31:09 +0000

    load store muscle honest lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
