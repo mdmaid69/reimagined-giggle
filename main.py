da451c8abdb7d24d6ef9b844ca86231eb9136632
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:45:34 +0000

    black during question born drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
