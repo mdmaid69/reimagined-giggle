fc743d12affbc0cf523b407b28c5c9b5087799bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:24:45 +0000

    congratulate something loss gap get

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
