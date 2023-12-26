0c528e3d62f7d4e0118ef164ba149339afee4ca1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:34 +0000

    wipe noisy guess sensible remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
