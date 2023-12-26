bd6c6c34cfa17f8af32ee3e789d004b0612f531c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:30 +0000

    ill cool sharp lie direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
