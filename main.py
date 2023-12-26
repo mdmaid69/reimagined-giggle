b1d9ee3c199507ced72247c3291f8f84e10d22b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:53:14 +0000

    concern chase tidy sign before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
