fff5771b6d9be524207df3b8d6112312641030e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:25:17 +0000

    prize game catch memory accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
