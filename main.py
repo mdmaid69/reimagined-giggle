a97a0dcf6037a93440d18e72897a87632c02914f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:45:08 +0000

    join include tomorrow make damp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
