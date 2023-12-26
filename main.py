94edbd36c5b2b98d907acc9818dc5bbbf4edc0bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:39 +0000

    last disturb 565end copy help

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
