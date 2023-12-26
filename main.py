250a1d70fb161ee10427e7028ecfe29c6d5973e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:15 +0000

    mine owe outside winter ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
