de423e9ec51321e5812129ee98ec63aa24780b06
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:01 +0000

    friend murder board offer market

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
