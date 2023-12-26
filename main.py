d89696534c4265a33a31284e4566b14642936dab
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:05:42 +0000

    usual tooth next correct spring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
