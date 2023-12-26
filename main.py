19b11f9f5d59a2548cc9bf7710b418cee6473670
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:12 +0000

    mend vanish relation none else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_time_since_epoch():
+        return time.time()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
