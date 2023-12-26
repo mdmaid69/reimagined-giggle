a8dee62eb36b4f7c2b9f86e0a57bb6e7955e1a30
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:42:09 +0000

    contrast hollow hurry than concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
