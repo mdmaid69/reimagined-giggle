cbb87674025265e06ccea8cabf3673a287ea59fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:30 +0000

    show explanation ring offer might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def find_min(numbers):
+        return min(numbers)
