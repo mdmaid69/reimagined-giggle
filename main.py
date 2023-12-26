ff6119d1961c27ef3a5853ae797a61106ac4c6e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:20 +0000

    comeback though giveout progress payment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
