880cbf9907b6cb509b5eb01a8a8744cbd2bf5e8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:45 +0000

    further uncertain pupil task bag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
