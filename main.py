13c3dba0e452b4791ba107d0ecad0cf8cf37c259
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:13 +0000

    teach excited dance cruel dig

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
