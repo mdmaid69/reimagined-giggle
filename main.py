b55ec72b419a088369fe06bba7a830ae0149d113
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:18 +0000

    moment criticize expression care apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
