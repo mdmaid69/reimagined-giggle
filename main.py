be118bed5c02b28c64676f11a120440b0f440b36
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:42:52 +0000

    moon narrow copy it hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
