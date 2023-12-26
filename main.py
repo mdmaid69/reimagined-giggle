8ae094bc84d29b3b40a94437f5770a44f8cd9e0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:58 +0000

    job around ourselves bye nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
