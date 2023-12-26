645fde672b9849d8eea72939b6f534bac10849fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:29 +0000

    shopping stage hard smell colour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
