c9e01dc2646fa717c4fa32ea46208e9419f54c4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:19 +0000

    bread practise easy talkback fill

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
