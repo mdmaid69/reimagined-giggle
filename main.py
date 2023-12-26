facf0f6635ecee91a780637ebb809f7bc2bae8f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:53 +0000

    listen block fresh watch prize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
