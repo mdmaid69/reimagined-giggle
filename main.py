18a1d017e19e7bf60f1178c2394ca8fe9bff59c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:53 +0000

    bored read sort deal if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
