a675d2a6c7271193903e76062271a9636254952a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:48 +0000

    tool reply even regular lookup

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
