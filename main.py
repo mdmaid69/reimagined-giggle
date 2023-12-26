f460176926da0ab6b8c31f64df2ae1e75f73810e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:39 +0000

    stretch family sure explain tense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
