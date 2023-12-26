dfe9d5ef187a78e110dc0445ccd085ffe57961d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:49 +0000

    creature needle tragedy scream middle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+for i in range(10): print(i)
