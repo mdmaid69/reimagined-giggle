30d25abcd888ace6f2b53c48adcedd124c652279
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:26 +0000

    alive other future talkinto lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def subtract_numbers(x, y):
+        return x - y
