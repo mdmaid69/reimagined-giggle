5a5264898734fc6314e995c4a4e9299d4653ecff
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:13 +0000

    variety maximum pull pass meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
