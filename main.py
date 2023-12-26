acd7da716b6896ecee192b10954c20c9ca05439a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:47:20 +0000

    clean exploration of dangerous platform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
