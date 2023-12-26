20081c5caa435f23b3e60f4f2d837a53d6611a11
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:50 +0000

    exit interrupt turnup far flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
