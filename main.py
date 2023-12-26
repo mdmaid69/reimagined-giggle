033fb8112a3a2f2df82d27bc801cce7c62c3382d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:08:06 +0000

    proof pick worse you record

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
