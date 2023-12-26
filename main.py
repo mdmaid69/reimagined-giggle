b96215307965ce35f42b9ec36f17e43637dcd3fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:01 +0000

    might hold principle ask slide

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def reverse_array(array):
-        array.reverse()
+print("Hello, world!")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
