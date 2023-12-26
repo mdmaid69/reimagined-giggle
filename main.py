17ca5bcf7cfb1b30c9e69f6a318836bcf003643b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:09 +0000

    comment mention swim earn race

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
