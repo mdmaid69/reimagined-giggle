e027a6f8d2c44b8d60cd632171a75df08c4f5f46
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:03 +0000

    wipe rest group die sensitive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
