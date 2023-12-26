4ead2c4ee22713882dc635b3c67e78dbabf35e02
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:50 +0000

    least offer sell other callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+text = "Hello, world!"
+print("Uppercase:", text.upper())
