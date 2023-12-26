97d9f73bfb9038b495220bff3f83628769d40ab3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:19:37 +0000

    ground live corner side excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
