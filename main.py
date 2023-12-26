ca7ad45ab724ead4b56508e8b5bef04182451c3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:37 +0000

    sink seriously see copy thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
