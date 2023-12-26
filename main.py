59304f273ca0b50cd6f7dcd94c97c24e845eca09
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:17:36 +0000

    over tear furniture dream loan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
