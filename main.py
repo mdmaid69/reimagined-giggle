c726660e142241a41c39123890a5d09a209057e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:27 +0000

    report consider lookafter slide getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
