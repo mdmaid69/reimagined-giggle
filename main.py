0072b2778a6118dab456d4a4047bf5e1169b4a09
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:21:55 +0000

    accommodation dark forgive become speed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
