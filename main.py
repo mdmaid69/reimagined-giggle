bdc5acb462a49d7892acc4eb3a7529171b66434e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:35 +0000

    miss talk bang what report

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
