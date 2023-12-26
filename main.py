e83cfa87dee9126916b90a4dae9693f994c47b8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:21 +0000

    black show wish still add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
