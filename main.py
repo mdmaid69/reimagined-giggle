bf40d72517932280be75bbedf548ab55b4e029e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:58 +0000

    pain stretch key seem present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
