1ee0a5ecae4b3a2116ce8836b2b32247871c0b74
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:47 +0000

    turninto instead maybe mess soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
