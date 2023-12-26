040b9bf61967e92c67c25980f239fec808b53e83
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:22:07 +0000

    bored bang grade talk balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import sys
-def print_python_version():
-        return sys.version
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
