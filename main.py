61774ca6c5179c789fa10f92b9a56763c4f36df5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:42:00 +0000

    century quiet drink giveaway smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def calculate_mean(arr):
+        return np.mean(arr)
