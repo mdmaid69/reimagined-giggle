73f50e7ba5b323f3b530393916b6a9ad39d64805
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:36 +0000

    means deep each tight none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
