9a2f9049635f8016c1f91eea148849d277326117
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:57 +0000

    various mad smart warn shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_min(lst):
+        return min(lst)
