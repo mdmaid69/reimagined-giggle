bec8f83930c002fac50ead547c289a73fadefa71
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:39:43 +0000

    must year check feed colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-print(math.pi)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
