fc2851e73d47f98b406d2c48b7574380a264eec4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:56 +0000

    accommodation rest identify different urgent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
