ed93fb2273293047820ec8254b80c849a0d4bed0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:48 +0000

    escape naturally sad study really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
