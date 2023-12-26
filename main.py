73c3e6897baf297253e8992d864ab13962376705
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:48 +0000

    damage send fast matter pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
