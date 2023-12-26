1719928e634f970264897e95590b9be040675ba5
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:25 +0000

    extra angry bet vanish favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
