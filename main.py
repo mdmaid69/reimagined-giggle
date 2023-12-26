592d754e7ffbff2661b25efe1d9faeb4d153b4de
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:10 +0000

    opposite beyond murder pick comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
