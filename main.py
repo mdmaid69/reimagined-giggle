4846da71701c86ef02d2d84d2d47aecf17b0f114
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:02 +0000

    official intend serious loose fit

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print("Hello, world!")
+import tensorflow as tf
+print(tf.__version__)
