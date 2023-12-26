3d43cbee527eab3337e3045af3a8e32ed53c1181
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:13 +0000

    thankyou example afterwards charge precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_length(array):
+        return len(array)
