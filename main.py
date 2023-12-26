7c89629a1bbf1c02200581afbc2a5549b689b628
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:18 +0000

    throughout necessary opposite during takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
