c7f3d09031a677ea4c6f43169155cf9ea4f791bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:10 +0000

    mixture motion square action table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
