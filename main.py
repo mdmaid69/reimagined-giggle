8552f3ba3ba5c3a95d496aa85fa84e5365ede851
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:22 +0000

    ours statement custom giveout turndown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
