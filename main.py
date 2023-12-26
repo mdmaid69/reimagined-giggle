be1d81929238a4c57d13e14c673a939f970c6c74
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:00:23 +0000

    against comfortable cheek total save

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
