c4c108d0833614f57bf1c3b43b2de498a673f120
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:59 +0000

    prize rubbish influence memory no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def split_path(path):
+        return os.path.split(path)
