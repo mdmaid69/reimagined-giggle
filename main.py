89d4d7c4d84db7ed9baddd64c25190b49b5c9bd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:15 +0000

    notice concentration glad pity software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def remove_duplicates(lst):
+        return list(set(lst))
