6caa230018aa90c27b3d5b2c68068c0c11dc8337
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:32 +0000

    extraordinary best unhappy breakthrough spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
