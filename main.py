30a0514ffea4982c19e5097e7600517590160f21
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:06 +0000

    loose stir burn whatever couple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
