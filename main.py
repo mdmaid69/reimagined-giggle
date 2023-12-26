f674f1736603971bce22429994bd48612547f754
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:24 +0000

    total apparently yours together belong

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+import datetime
+print(datetime.datetime.now())
