7f9c21bc206e0e66ff5cb332e505fc27629c3e50
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:17 +0000

    press affect form steer constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sys
+  def get_python_version():
+        return sys.version
