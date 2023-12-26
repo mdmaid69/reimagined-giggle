75ae7bb0b50dafab1d5ffd1984ca026d69ab55a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:35:54 +0000

    successful matter moon plastic apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def reverse_array(array):
+        array.reverse()
