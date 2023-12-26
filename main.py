8da0cf09a05c6f86187e6db22d365a0cc5df4418
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:02 +0000

    poor pressure exploration cost worth

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
