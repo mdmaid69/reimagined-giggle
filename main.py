ebf0f10337474a86aa0ee26644592e26c01b1bbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:45 +0000

    evidence grow organization drive little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
