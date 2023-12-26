18c7ea65b436758c76314135347b9cc41d52ddaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:35 +0000

    welcome morning soft under sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
