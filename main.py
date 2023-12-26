efa4b405aa2f2916bc1c7939bb219022d982fb74
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:00 +0000

    tell such summer drawing bear

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
