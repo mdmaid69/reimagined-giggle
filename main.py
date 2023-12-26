57b78b9cfbf2b0f273ea391984951298c23b359b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:39:35 +0000

    heavy valuable direct word use

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
