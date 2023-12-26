11a7648dc3288bef54eaf0f2e555d840eae79192
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:49 +0000

    alive charge where fault slice

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
