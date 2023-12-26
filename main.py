0aa7cc28e12230b89bff046f3312f2931f8bb4a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:13 +0000

    bringback group square risk genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
