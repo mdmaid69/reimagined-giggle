dd98d450e3904e7ffffc8e8986e6d82e1a48861c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:13 +0000

    themselves upwards wide outside power

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import os
-print(os.getcwd())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
