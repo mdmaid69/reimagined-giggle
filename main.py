5d3870d36edf513e81f1cd7f563845aa28eb4cfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:14:52 +0000

    finger behind instant yet occur

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
