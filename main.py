76ca3a35f42ec93f38b5126af3e37b01c9049b04
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:30 +0000

    respect near sleep present demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   def calculate_circumference_circle(r):
         return 2 * 3.14 * r
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
