5e44c9c1ae2356f051096e527e22dd4490d71ee2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:05 +0000

    limit runaway part accommodation high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
