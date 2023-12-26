39d77aff81549e5e58df0d211a158e2434ecbf35
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:54 +0000

    measure dangerous sharp container home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_list(array):
+        return list(array)
