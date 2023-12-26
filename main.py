cd102ecca468ffd97107675c6e1d9c9c34fd5b2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:14 +0000

    giveaway Tt 568energy concern thick

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-print(sum(range(10)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
