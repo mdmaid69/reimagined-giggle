d664be5a973292aabc6e27bf229a2414bfc67d24
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:08:41 +0000

    price action modern comein on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
