8cc2e6b7b2e623373fc0c4f49e3b796c75a9083e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:33 +0000

    finger ago wise genuine stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
