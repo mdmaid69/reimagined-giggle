2a42d5cf882751eda98e8a35632daaaf49574148
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:47 +0000

    thinkover shake marry there common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-print("Hello, world!")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
