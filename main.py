fb30c20b5b049b5ca5370ddacc1037a56e1b8d82
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:44:15 +0000

    fortunate deliberate sweep quickly often

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
