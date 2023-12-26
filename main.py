00f182bcb1ed646de7291d38c6e9f49f39cbe2bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:20:08 +0000

    sometimes apparently television order persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
