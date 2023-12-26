da022afd8196c2b1e7160c8062a71d2fea57a9a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:01 +0000

    side stare unlikely adjective plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
