450ce3b0fc4ad26f763f0ff347030dbdd24b2469
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:51 +0000

    cut bear strong disappointed side

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
