0fd302c3f86ed54d17a67c10a8550d5d07d1fcb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:00:34 +0000

    dress wipe steal trade grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
