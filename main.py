0ff1c5926586857499a328db380224f4f46717fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:06 +0000

    seem since uniform see large

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
