35e0122aa52a3ccf8b96d534dace000c26bd17a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:26 +0000

    subject operate avoid alive trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_palindrome(s):
-        return s == s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
