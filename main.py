5a125660a40e4ba18a6d4df66d5cc0f085ffbaff
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:34:51 +0000

    news bother target fold risk

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
