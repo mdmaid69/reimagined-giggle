b39c807b06f8d5c85a44ac45b34c358f0c6ae94b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:21 +0000

    mainly bright clear comfortable before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
