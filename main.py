5742414ddc724671fce3e89e48a3857fb237edd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:55 +0000

    apologizeapologise point brush connection contrast

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def is_palindrome(s):
+        return s == s[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
