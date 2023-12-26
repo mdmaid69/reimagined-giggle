733a6f8c1ef343a2e5445fbc6c70fde1050b3554
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:16:05 +0000

    event runover taste greeting guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
