b09a30f7a1fd10e6b2e9c236b2c763a440c0e408
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:44 +0000

    treat search round light comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
