4ad674b43652000576af5f78bbab2a78334a9216
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:23 +0000

    produce pool check centre contact

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-print(math.pi)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
