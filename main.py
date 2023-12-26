ab05dda01a8a1603e4995f1c7fe887528591fa5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:11 +0000

    getout song issue plan tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
