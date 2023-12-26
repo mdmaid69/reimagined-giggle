7bdb259b8f1e8a7f7e25e36cd4a5e0ec021a1faf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:05:55 +0000

    uncertain broken Vv century suffer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_power(work, time):
-        return work / time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
