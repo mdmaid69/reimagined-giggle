147df79a5cf7c9a58166f9425b04d050efd6f04b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:42:04 +0000

    care to hot couple for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import datetime
+print(datetime.datetime.now())
