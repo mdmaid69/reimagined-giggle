b34d31cec2ebe863ea0e4bf62f06df2e716c241b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:03:37 +0000

    section lookaround kick circle ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
