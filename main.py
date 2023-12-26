8beafb7fdb8693dcfc2e6a2af3279722d993066c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:42 +0000

    thing thinkthrough freedom line really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
