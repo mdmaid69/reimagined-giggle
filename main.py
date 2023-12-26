6e605d46c22fa2e0e7c8fb4a531b33aaa4715aac
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:44:23 +0000

    fill legal contribute push both

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def is_odd(n):
+        return n % 2 != 0
+def is_odd(n):
+        return n % 2 != 0
