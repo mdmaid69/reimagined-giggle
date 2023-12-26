5bfc3074b57e88a949bb7bb86045310348d7aef5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:48 +0000

    keepoff false meal tense plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
