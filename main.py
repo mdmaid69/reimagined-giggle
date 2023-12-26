de26736a0efd42b4d2249f743fe35cd782406084
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:00 +0000

    half value 572enter because train

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
