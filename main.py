3109e28d1f6de60404a66e2feb1d3b862b04e010
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:10:28 +0000

    sink lookafter left giveout unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
