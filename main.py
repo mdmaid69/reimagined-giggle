5d05cf18039321c9da6cd2a783a1bad1914e4b1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:35 +0000

    past cross undo trip drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import sys
+print(sys.version)
+def is_even(n):
+        return n % 2 == 0
