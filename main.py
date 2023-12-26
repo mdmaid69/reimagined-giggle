5432d23befbb66e315375bbfc9c31b441f40a397
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:46 +0000

    combine town goon weekend favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
