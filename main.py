da0ee311c5c996a181734ea59e336bd31a6ccd86
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:58:34 +0000

    depend turninto complete system pile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def get_array_as_str(array):
+        return str(array)
