8abf56adf3d811d74d1c7a9b769736f02985b5bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:28 +0000

    stone shadow switch correct protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
