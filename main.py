d4dc6e315cbd7a44a07d66c7d9ed87b8f2a47e25
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:13 +0000

    rely comeover drive object himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
