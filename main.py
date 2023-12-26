5ad54c1af97a98868e3cd00f1c60b2ee4e8e441d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:46 +0000

    concerning reasonable precise introduce declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
