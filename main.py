ac9e2919d12d4a7892e4e86c462c9bd93881d9b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:20 +0000

    post runinto leg position everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
