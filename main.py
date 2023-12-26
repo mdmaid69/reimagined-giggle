5b074f710a1b426d422cfae3f506667e3f9fe457
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:24 +0000

    dick several intend perhaps dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
