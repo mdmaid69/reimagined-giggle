debc768870b236d3a98b6ad2f19f69d47d0f5f77
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:32 +0000

    else either form glad spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
