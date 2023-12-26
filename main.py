6cd9b4989f6d447ca2531ecaecc5713028949527
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:42 +0000

    godown cheap conversation blame weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
