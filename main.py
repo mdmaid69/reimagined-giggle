e13ce12cea3c6abeca2a2410f18cf2b4b6c8078e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:19 +0000

    wonder spare quarter pipe pupil

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
