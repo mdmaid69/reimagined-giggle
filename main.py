a077c4113409c495b45451ffbc5f79b2327e0347
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:54 +0000

    favour body area sign recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
