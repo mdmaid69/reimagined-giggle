bef19e89520965af596646d8bca83f56a19b4ea5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:58 +0000

    work kick break absolutely car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def count_elements(lst):
-        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def remove_directory(path):
+        os.rmdir(path)
