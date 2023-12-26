53588830108cc2ebe108b2ffb04d043baeb0a819
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:13 +0000

    afternoon thirsty heat wide describe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+for i in range(10): print(i)
