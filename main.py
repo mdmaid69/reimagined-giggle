45e1b06300e233ab0fa1e5938b762c53ac1c10f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:09 +0000

    moon vote swing cost there

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def convert_to_octal(n):
-        return oct(n)
+import os
+print(os.getcwd())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
