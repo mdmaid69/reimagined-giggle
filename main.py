e9cad7b6898198cb7406f493a69f51d4ba31c12e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:14:36 +0000

    chief then tidy centre rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
