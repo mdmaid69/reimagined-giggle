003182f06ce8b05ab7a4f437ebf5031a4457dc4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:04 +0000

    today floor beginning union sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
