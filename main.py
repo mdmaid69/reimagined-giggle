37db3740480c1060ec51237a78e8f691a9a4e832
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:50 +0000

    colour underneath method people guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item(array, i):
+        return array[i]
