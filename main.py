808fb65f4a994231bd17b9fce14a7222028206a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:16:08 +0000

    trial strange combine recover base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
