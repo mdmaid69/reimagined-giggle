623eb591957873ad7903fa467c87555d2049f9be
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:44:13 +0000

    her care its fasten string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
