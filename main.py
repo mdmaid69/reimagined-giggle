fc0c4f40276aff17482bdc8dfa0660ca726fc99c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:12 +0000

    write once swallow feel frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
