200c990f8f6099a0bf0652c2bab7c7c1680289fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:48 +0000

    attempt abroad record press instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
