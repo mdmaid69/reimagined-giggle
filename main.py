a062a1f0fb8695b767f5d8f55595eec0b23dd9fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:54:29 +0000

    turnout stretch spoil shall race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
