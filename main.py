d07984a81f8f3706ee8b6534d10c15588f0e6fe5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:03 +0000

    Ii agree limit date principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
