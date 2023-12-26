f20153a289376957e3d9f721fc543d494daf1317
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:45:20 +0000

    connection dare word slow card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+print(math.pi)
