c46f2a80c3e095a09f9add1d00de9c76c39a3852
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:19:51 +0000

    mark tap loss obtain Ee

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
