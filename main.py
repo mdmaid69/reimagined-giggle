dfc425bed87408fa1f2bf0b48487d0b2376cc02e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:47 +0000

    within support share towards top

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
