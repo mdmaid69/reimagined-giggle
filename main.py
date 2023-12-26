88cac2c096ca423beccf89e6253c0ddaaf09e43c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:02 +0000

    calloff block fortunately deliberate fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
