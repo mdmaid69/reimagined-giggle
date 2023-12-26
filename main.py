ace5d07cb434be2fb20fac60dc190d9d9c9e6c7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:46 +0000

    cool sale creature correct public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
