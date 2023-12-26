2164262db1d65f4c5e0f2fc9ad752cb2ad33cb45
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:45 +0000

    so desperate she accidentally size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
