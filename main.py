e5bedbd0c9d5f6d067c87923fc81177cea524f2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:33 +0000

    convince chief best crash return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def sort_numbers(numbers):
+        return sorted(numbers)
 import sys
 def print_python_version():
         return sys.version
