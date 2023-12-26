a4c9eb810f9ea5bc508a03e2e76cdbee5a17426f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:37 +0000

    impossible look by run off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def convert_to_hex(n):
+        return hex(n)
