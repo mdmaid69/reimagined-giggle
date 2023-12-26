9c0c43152adb5aadf79a9758c2003eb17d6ccc75
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:08 +0000

    unit invite alone breakfast manner

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
