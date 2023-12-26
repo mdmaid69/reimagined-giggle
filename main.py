0863aa0cfa3815e3101664052a5e663e6f776f0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:40:39 +0000

    fine him will invitation let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
