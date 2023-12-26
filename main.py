55caf3f395da1a5169c0738bb0d05b396793bc12
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:23 +0000

    doctor thirsty mother mixture herself

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
