c319ee298702151d6ea8173b3d235054f807c41e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:24 +0000

    getout small thirsty wild song

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
