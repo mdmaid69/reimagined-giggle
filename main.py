2c60d3eef6d60e1555eb2ed4a3eeca5d3f3564f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:36:07 +0000

    nigga match slice weekend maybe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
