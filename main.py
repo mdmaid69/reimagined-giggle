756b9e23e639d58ac4bf1771882613a532719b3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:46 +0000

    negative wake disgusting takeover brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
