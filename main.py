e70f8d80b8293465406ec1b6a1caa101ee58a8fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:51 +0000

    may different cross regional cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
