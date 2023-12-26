68addaf2d0732b02b5ca1c003a0d43f88429ce75
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:42 +0000

    yours total refer public produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
