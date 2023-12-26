dda6e2740aff4668a9967f80d49657b949313fd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:58:20 +0000

    manage character range call road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
