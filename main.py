62257ebb35ddad593df254f9ac01e22aebd346db
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:29:50 +0000

    answer shoulder complaint hope join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
