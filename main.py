00d8f9860269c8982af04edf79e573cbd81d25fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:24 +0000

    Ee wipe call explain back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def count_elements(lst):
-        return len(lst)
+for i in range(10): print(i)
+import array
+def get_array_as_list(array):
+        return list(array)
