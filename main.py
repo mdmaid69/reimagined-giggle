861a7798c193d111949b397f4c3b5a426933d92f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:58:52 +0000

    anxious accept ought bridge jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
