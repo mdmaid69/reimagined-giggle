6cbcffe6057f69c1030fb0fe2757c25afd603973
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:57:04 +0000

    shop surprised neck bunch takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
