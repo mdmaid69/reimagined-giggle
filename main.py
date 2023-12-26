399e42ce5d3be9a6da99c077062d0db1fa6b5071
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:16 +0000

    recognize head software guard once

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
