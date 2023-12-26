f2dd73a941c450de0886f63e0a98c55099395ae9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:46 +0000

    modern interview callat bother copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
