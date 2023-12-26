683a03e64e580a48cc1b96b18bab93e49ba21844
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:57 +0000

    wind shelter side frighten runinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
+import datetime
+print(datetime.datetime.now())
