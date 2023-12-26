ddab9b40fcbdbaf0b51cb6faa4658a077aba2d48
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:30 +0000

    music stir Gg glass punish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
