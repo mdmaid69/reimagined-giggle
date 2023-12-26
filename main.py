6b20beceee19b5f1d0ad0f4e2c66b27fef75ae71
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:42 +0000

    conclusion occasion note pile conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
