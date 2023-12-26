265cd94ffbce69d1f6211be080ab9dca93051634
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:30 +0000

    rush contain dress yes stupid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import platform
+def get_python_version():
+        return platform.python_version()
