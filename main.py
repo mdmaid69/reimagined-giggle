1fe95a334c0851bfbe41df79f13e229b3f703600
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:48 +0000

    none receive finally 571enough information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def print_python_version():
+        return sys.version
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
