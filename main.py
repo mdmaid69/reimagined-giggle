535f64e8c696cc1bd1fedb922f3c072cc2ed5302
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:24:57 +0000

    circle foreign during sink circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_list(array):
+        return list(array)
