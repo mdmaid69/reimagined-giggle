3833b8dd46c721d40bf767051fd4ac96866a63de
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:00 +0000

    similar art hurt afraid however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
