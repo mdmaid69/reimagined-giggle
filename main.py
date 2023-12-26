dd8d5546d584b8183578fd2f39d2aecc5506f07a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:27:33 +0000

    repair nothing safety unlucky border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_current_directory():
+        return os.getcwd()
