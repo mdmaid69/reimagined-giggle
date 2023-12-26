b9bd7d4e805f9d3abbaf43db5515ace93d6f2c5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:31 +0000

    traffic hurt ground brother friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
