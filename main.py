ffe243875f2d02401fa0858b4b2daf1da6aa5bb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:53 +0000

    female emphasis lead quit woman

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import datetime
-print(datetime.datetime.now())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
