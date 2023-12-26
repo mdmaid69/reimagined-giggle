4ce3f2bda5c87057f5c2018ab57b52522c93ebc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:17 +0000

    match switch guess use account

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-for i in range(5):
-        print(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
