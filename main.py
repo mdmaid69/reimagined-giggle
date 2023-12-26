48ae778b20e51bce515858fdbfe1fb64835346d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:17:54 +0000

    so string anxious closely strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def count_words(sentence):
-        return len(sentence.split())
+  def is_even(n):
+        return n % 2 == 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
