a398c0883a5ab2ebe196b7d6005f78110315f24c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:16:35 +0000

    second over medium mine advise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+print(sum(range(10)))
