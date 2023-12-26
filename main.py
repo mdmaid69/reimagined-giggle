9dfc768a273cb2a7ec2b644c7a9ee656ab4cb501
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:22 +0000

    measure indeed firm forward equipment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
