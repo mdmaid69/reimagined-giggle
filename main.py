cee43bbff293175a3cb36a0114e26dce5e863e7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:03 +0000

    argument clue ability Bb activity

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
-def greet(name):
-        print(f"Hello, {name}!")
