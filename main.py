c316e23d0df1f3d25d2d69dd2955ebf647e18823
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:17 +0000

    deserve hang impression winter rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
