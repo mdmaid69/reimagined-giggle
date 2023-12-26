e1e84aa38ce0d221c744ca7456f256661653dc70
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:35:41 +0000

    upwards between bread suckme somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
