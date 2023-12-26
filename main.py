dd04db5be1c320515be8df3ff497f1993bb82d8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:30 +0000

    division ride brother pure evening

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
