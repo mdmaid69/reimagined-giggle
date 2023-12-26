4db1b4521a9e0f531e4ecdfa97cedee89344f35a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:24:16 +0000

    seem rescue body degree hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
