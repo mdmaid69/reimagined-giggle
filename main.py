2c468321ebf4eee8f142ab8cdd96881cc58c1a34
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:30 +0000

    rent serious sleep compete squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
