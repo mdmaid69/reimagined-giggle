ddd30e1c2dc4d1c95d094acfa373d68f41461d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:35 +0000

    within delight cost gooff week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
