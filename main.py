8fa7c23bdf56e9219fea720269d9166c2fcf5968
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:00 +0000

    touch someone they mind turninto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
