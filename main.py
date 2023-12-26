2c1285448ecfcf5a963b57621f589b2f88f5a6c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:08 +0000

    used view warn bringback zero

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
