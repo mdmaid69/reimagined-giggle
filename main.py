14a50c66c5687ed296b9c68ef684008245b4a915
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:12 +0000

    central trade thinkof field reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
