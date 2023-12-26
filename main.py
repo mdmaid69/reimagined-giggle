f3e5b97f3cee8d81e894d7c19431bae9d1ff2573
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:00:48 +0000

    object buy your load tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
