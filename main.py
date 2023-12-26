64b03fccf4dbecbcfd0c741c5204cc327ea4ed86
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:08:56 +0000

    corner scale simple strike through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
