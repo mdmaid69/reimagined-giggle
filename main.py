6eba8a66c7d9ba823a952aaa302cff56780ac5b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:59 +0000

    stage Gg spray vision personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
