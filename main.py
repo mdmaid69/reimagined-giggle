8482e8d37bda80b71c70ef53ccfc78b6720a332b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:23:48 +0000

    explain obtain before definite organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
