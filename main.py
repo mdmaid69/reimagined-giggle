b749adf8d9d4bfc071f3e43cae770e847a8b9fc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:08:39 +0000

    guess wheel hot boil hardly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
