d199a18904ffb109c9579d5862b27d94713220d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:22 +0000

    top exchange pity test picture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
