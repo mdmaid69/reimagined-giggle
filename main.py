ab6073609808198431bc455aaa88306d333ff360
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:08:08 +0000

    hurt disturb anybody land problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
