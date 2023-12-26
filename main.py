15238b11fff3e3f0e773007a81177432f92bfca5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:33 +0000

    wise in since considering light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
