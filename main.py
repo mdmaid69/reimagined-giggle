a38bde8cf06a14e292f438b7d6cdf1822b933be8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:20 +0000

    murder count score insist start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
