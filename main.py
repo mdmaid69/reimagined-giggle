13c40a2f953f6a8aa4dbc74abc7193ea6d0bb773
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:14 +0000

    write brown normal interruption difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
