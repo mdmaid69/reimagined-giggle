75fbf09bdb40a7a3ed48eb541a99836454047eba
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:02:33 +0000

    plenty cry regularly design Uu

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import os
-print(os.getcwd())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
