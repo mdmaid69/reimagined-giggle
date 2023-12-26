bef5b75bce93ad79e0659f0d6ae477a2dc4fee93
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:19 +0000

    spot impossible might jump sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
