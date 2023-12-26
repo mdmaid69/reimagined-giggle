bd922d8f3920db95c2eee79a4bc85fae3784c50a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:26 +0000

    gooff object takeover defend race

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def remove_duplicates(lst):
+        return list(set(lst))
