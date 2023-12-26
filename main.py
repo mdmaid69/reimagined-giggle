f2d5609c9ec4d4633454d27925dcd96619e4e368
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:51 +0000

    hello before late now admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
