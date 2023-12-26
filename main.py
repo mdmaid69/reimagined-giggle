814ad9811ee3445acae9ebd1505ef3a5999db95e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:32 +0000

    wrap public minimum near fold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
