4598339574be22ca8ca7aa6feb99c9b2f9176195
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:39 +0000

    tread fix remain that raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
