169eaea002288f93578bb324f9965b313be74cf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:19 +0000

    Yy gate turnover favourite wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
