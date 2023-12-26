3c52ae452defe894a7210b4eadc2446bfb96f271
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:13:32 +0000

    glad joke frightened responsible cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
