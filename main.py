9c5fd8f3bcad9a8be267d28a01a1504d91a64379
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:51 +0000

    word fasten position suppose but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
