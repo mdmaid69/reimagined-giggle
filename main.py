b3f7c8004c5ac9f43c87cfa237d9ebfb510fe36b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:46 +0000

    drop learn payment curious warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+print(math.pi)
+import array
+def get_array_typecode(array):
+        return array.typecode
