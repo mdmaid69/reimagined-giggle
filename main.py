725d9da17d30ec01d8d859722e116353aef15b50
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:39:03 +0000

    measure soil slowly appear direct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
