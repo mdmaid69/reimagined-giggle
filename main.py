fa0fda64c2583375d7be7ce0599d3a09a4446c05
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:07 +0000

    here fast then workout target

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
