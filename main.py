debed0df4224dee951ed7f433d4cf7acb82fea1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:38:24 +0000

    country process direct chase quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
