2b6c1acbbca7c396de53f5d1fc1b852c4e4dc347
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:06 +0000

    spin own advanced however fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
