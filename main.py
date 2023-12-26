e8c5f6d8f6a4376fc717012199b4c46056e3452c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:34:06 +0000

    excuse influence destroy begin strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def convert_to_hex(n):
+        return hex(n)
