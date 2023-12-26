1ec2d3de073b388f03056bf29d7c6749da38d80e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:36 +0000

    free convenient glad study jump

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+import array
+def convert_array_to_list(array):
+        return array.tolist()
