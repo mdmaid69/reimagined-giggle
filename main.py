e458e1cbb140de8b383fefe480784229da4793b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:01 +0000

    thinkover long instruction hit help

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
