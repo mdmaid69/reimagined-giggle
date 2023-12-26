b59288e8f58db39f028536fe4c7bcfc32881a337
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:02:29 +0000

    rich turnover slow will natural

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
