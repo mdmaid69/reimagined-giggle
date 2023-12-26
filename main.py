620fc04292efe62fefb856daa3a0cc6c1857fbb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:10:53 +0000

    admire suck community only instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
