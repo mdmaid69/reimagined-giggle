6ce5be7534680655d6f8b7b4f522292cb8569afe
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:47 +0000

    achieve surprising takeback judge I

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
