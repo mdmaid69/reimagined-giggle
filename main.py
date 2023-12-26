93c6c89ea1f6ee89e521bbed59e833060d8ac016
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:29:04 +0000

    search store create ashamed worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+x = 10
+y = 20
+print("Sum:", x + y)
