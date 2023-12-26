486a0a7927293b7a1df17d91f6eb2619c977880d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:01 +0000

    contrast comeback real though study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def cube_number(x):
-        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
