609a0f5f8672383f6e3cc8908ccd9fe0166997df
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:03 +0000

    war thanks comfortable blow confused

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
