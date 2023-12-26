ac5ccec59ffe833e146cb167bfed7b23aec44212
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:49:51 +0000

    foreign block shout ticket development

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import datetime
+print(datetime.datetime.now())
