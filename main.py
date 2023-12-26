0264441f70c61a4ccd1d4f027c2609c6e4d8f018
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:45 +0000

    best shock floor funny part

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
