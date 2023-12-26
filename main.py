61980af672f83ffc0dfe88636e1b9124041a18bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:22:35 +0000

    along beside swing achieve weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
