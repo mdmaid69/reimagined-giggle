7bc35ccceff3b1504acee7105ca5bb235ca024f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:39 +0000

    trial shock wing likely period

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 def calculate_force(mass, acceleration):
         return mass * acceleration
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
