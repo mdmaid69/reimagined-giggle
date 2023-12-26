6b63d4deb2c8443c00461f63e469c964c03b268a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:19:07 +0000

    frighten compare reduce finger blame

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
