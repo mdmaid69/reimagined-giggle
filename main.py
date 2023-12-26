6cd37d3057397faddbada08163eeacc2996b3eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:28 +0000

    straight lesson half considering town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
