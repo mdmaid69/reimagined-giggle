bdac1b5a6a1a81099b42605faa16a071da0a5bfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:20 +0000

    typical load event goon object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
