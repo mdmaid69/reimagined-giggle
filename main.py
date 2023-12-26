3f31fa1f66ba73bc22f50e9e73a64f4111746854
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:28 +0000

    upset no dry secret love

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
