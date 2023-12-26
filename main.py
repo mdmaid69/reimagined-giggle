377f24d3b6ddaef514068ebb6f0fcc23a2b19bc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:16:52 +0000

    not connected room exciting for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
