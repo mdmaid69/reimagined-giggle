30d266a08a546a388b9fdc944cb97a2f155869a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:37 +0000

    ear either bar butter onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
