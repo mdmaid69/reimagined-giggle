66810586609d7c414a6e00e308e2d97502940d49
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:05 +0000

    fly comeround advise statement keepoff

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_int(array):
+        return int(array[0])
