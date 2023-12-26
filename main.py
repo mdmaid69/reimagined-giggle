cbff17b668485814b5d6b5b281f49d34d35c10d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:16 +0000

    prize represent no everything back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
