1bbc7ce880973affe76f183252920b07ba554839
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:04:07 +0000

    far taste meeting large introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
