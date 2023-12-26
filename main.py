0d305ed627e64b25f57e2936ddfae994c5114651
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:39:29 +0000

    pleased finally weather chase takeaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
