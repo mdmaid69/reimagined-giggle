2b81dded1bd328bd61e1794ba0ff60f9a006f5a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:29:53 +0000

    female Cc honest interview though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
