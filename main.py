29c2fe2513268cf0066abf8d3ccad5f004cb4d0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:58 +0000

    convince message neat independent part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
