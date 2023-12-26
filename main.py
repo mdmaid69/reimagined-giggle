27f8038d163d125715b1db525be031f4fbd72ba6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:31 +0000

    leave he square introduce move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
