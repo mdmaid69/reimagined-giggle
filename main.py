ce04aea15b357b42a6a4aa4b1d2bf0f56bf943f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:07:06 +0000

    pair to unusual keepup concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
