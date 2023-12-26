f6c9c16fd1722ec53da2fc68a0d3472053c4ac19
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:49:21 +0000

    wire embarrassing light born frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
