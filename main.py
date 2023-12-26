a2b1eb4d00d50e8c9134e36b08530eb957c5a9af
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:29 +0000

    soon little software chief human

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytearray(array):
+        return bytearray(array)
