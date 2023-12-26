a1eb5bf7cf74183c09846af7ccd0adf3e9731d2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:25 +0000

    glass almost maintain determined satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
