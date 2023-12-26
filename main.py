00775cf2dcdfb4ed12f092b66f0a03cf93667943
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:22 +0000

    pressure bottom wealth giveaway opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
