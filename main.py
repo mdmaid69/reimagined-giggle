2ca0f847ab6fe65982b2cc33eb452701ba0e43a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:58:06 +0000

    fond climb area sort surface

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
