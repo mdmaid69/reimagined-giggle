67dc7ee47bc0e3570b38c26c399069e0544cbd76
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:45:31 +0000

    moment disappointed search 569enjoy dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
