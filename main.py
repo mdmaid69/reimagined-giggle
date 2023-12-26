802e25398e60b0960130de443190259e2bda0161
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:18 +0000

    expect flight tragedy pupil fierce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
+def add_numbers(x, y):
+        return x + y
