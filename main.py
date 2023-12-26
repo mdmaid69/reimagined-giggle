89e8d63b6036efc64bac49f07f0e50815ec014ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:10 +0000

    divide stir respect measure over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
