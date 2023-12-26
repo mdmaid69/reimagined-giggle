129aa539079a2c4c45184a38ff0093af069e044e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:50 +0000

    world tidy fair strong runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
