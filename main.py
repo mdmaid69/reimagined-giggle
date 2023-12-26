8462371157f057c7f91f7e603b87b8b93ba7b3cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:24:23 +0000

    set Zz world condition somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
