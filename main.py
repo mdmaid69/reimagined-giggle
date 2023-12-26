f292f53b5c6b415f79d74c2689c8c7134f677f6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:24 +0000

    nigga approval address confidence protect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
