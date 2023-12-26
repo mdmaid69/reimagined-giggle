e4dc60f664e9b6d35c9e0b417799b85c0df53666
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:06:38 +0000

    no national immediately lead unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def clear_array(array):
+        array *= 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
