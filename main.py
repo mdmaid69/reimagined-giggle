4663992cbc7ac85ee7da0c13c9074dcb0f254bf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:28 +0000

    vote wing world rest service

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_priority_queue():
+        return collections.deque()
