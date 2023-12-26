07a4ccdeeb638ebdafcbc30ac30955a8db07bfc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:36 +0000

    guilty friendly fat dry 567enemy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print(sum(range(10)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import datetime
+print(datetime.datetime.now())
