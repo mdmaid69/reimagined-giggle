eef55cb254b8aca5bd3047cdc746dd15ceaa8240
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:12 +0000

    dinner share keepup stick reason

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
