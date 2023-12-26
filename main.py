c4d11d2d42f7acb7d01afdc52b0691c08915afa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:12 +0000

    now this community container man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
