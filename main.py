3b09c96a115cb9fed8c8b74e8014ab0b1885ddbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:11 +0000

    trouble service direct house tradition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
