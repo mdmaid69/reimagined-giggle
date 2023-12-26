3639d15919443ef54748d9b85ab1b64b3c8d6d88
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:23:15 +0000

    prize help each tool turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
