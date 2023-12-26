ec0177a630339d6f3c33c5d4212f55254a825d89
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:31 +0000

    concerned ahead takeoff small collection

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
