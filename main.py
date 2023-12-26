a3ff26e9bfdd7ebaa8247462064c1f7f94b6ed80
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:39 +0000

    lift winter invite callup blind

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
