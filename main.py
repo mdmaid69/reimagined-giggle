be047a86b04980b5bbbeeb570cb39832a2b2d2b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:32 +0000

    share coffee goup conduct their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
