e9dc3fc6dc0ce3e5b3cdbf5cf9111c11bfeb1d3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:15 +0000

    hole fly finally take history

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
