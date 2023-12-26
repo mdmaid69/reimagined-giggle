23cb0bd17e55919cf266cd8a927171f3d447b58b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:18:45 +0000

    comment favour reply song workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
