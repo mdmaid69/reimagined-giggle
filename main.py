fb6dcf1f8e9f07ea7fb738af1acc20a614f66941
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:01:26 +0000

    type community loan last speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
