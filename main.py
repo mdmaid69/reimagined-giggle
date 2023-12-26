5cc3abdf8c1aa37a287e2cb521f41c999d346e1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:01 +0000

    bringdown murder through understand team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
