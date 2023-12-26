f8223ca1a16482b7652beb49eb401d3876699494
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:18 +0000

    rush slightly dead congratulations burst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
