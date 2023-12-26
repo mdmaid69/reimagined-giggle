0227327a5da60f3c800f32026c19004f46072cf4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:58 +0000

    all issue book model so

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
