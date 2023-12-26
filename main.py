cf87c451f6868b0bce46f2a5c4309a8c3e8c55de
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:08 +0000

    runout embarrassed truth outside thing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
