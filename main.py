7a5dc4333bc37143f63821e58218ec4a4e78595a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:32 +0000

    nose air schedule translate mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
