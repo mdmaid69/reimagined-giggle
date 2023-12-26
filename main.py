459934f30b043b2460078d1ad9c4d6a3614ce0bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:12 +0000

    ring force complete bit instead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
