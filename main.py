8d2d5efd1e2c6a8665446ec2bbda47045ef72f39
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:08:54 +0000

    top he bright yesterday relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
