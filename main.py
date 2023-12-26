75abd8ed34cc16e5baefce38f19397a328bf46f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:37 +0000

    medium work imagine clearly clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
