6cbd04bed700e749ae46ec53f39cf7b147dd6f98
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:07:53 +0000

    worth official immediately square hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
