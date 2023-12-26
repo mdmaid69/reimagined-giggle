600dca930e2b39acc32e91355c0854b874b2580d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:45:09 +0000

    herself ability separate think principle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
