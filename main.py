6080920d92e6365521eeb62c6e8e1162c222b9ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:51:25 +0000

    behave large use direct method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
