9535fc59adf90cdf834799be3cf926c62a3cab74
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:03 +0000

    command past shop maintain almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def clear_array(array):
+        array *= 0
