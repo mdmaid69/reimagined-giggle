9244041eae0cc3d1301ee70db68c47f455153bd4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:18 +0000

    study ill 571enough touch figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import tensorflow as tf
+print(tf.__version__)
