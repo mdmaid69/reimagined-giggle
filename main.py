043c82ac66e025e3019064bb271a9f82a9fc3fce
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:50:46 +0000

    kick nor long anyone force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
