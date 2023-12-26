490ffb1347fe1c804a2f71c25e09412e24617c93
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:44:42 +0000

    thin letter strange criticism owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
