542e15a5c1414700ad34b6910b68ef367693fbb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:59 +0000

    just notice coat language check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
+import collections
+def create_user_dict():
+        return collections.UserDict()
