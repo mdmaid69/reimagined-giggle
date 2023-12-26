50ddef658d30f880f4c179f9c6aec592e2098696
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:51 +0000

    laugh possibly shut read better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
