9324e4d0329c1ed812140da1fee4a0536f8f7e47
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:12:02 +0000

    worry question tragedy stretch whom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
