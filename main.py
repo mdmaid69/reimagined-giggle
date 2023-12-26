0e5537069029b4b1c1e3acd4f6f7b2049c4f30ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:40:31 +0000

    couple scale detail getaway old

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
