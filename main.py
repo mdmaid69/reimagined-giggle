fd3b0fd8fb1cbe1cef2fef968cf08f1c3cf472e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:06 +0000

    understand doubt rub through some

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
