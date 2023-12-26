8778032c7b6d25ec5d7e2c27de2899e814799068
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:39 +0000

    vanish wave calloff hard excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
