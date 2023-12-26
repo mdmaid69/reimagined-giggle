36cc5ad3267db6ff8d1b50d6b1d01a4fc0836bf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:57 +0000

    share ready hard criticism leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
