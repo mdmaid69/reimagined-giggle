529837c44293daea2c17e9cbb54a283916460e27
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:12 +0000

    case ground sore passenger broken

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
