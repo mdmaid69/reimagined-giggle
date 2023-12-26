da41507dcc1a6f6954dc82ecc2683cfbf2eaa056
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:37 +0000

    pretend shoulder tragedy know bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
