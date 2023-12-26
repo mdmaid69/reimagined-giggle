a5037b441ed290c99618df1c4102130cb13b3b35
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:48:39 +0000

    except necessary challenge deliver start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def remove_duplicates(lst):
+        return list(set(lst))
