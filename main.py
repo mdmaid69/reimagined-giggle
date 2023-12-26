211a3127e9c7080812c60b206410941f5f86aa8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:48:53 +0000

    hear less health jump difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def reverse_array(array):
+        array.reverse()
