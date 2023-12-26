64a2cd2ad59d10093eb66d076fdf6c63f65a26d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:02:31 +0000

    study plenty Cc pause mood

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+print(sum(range(10)))
