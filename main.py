ed274a5c5ed5c12dea92ba39661c2b606e4af92f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:56 +0000

    service married path add thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
