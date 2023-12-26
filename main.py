ad51b1fad97606b9aebfabe8ba3da95d0ca35632
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:11 +0000

    wide ago passenger measure sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
