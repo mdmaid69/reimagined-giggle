5b483007045e66471f99bbcc3d77526737aa29d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:43 +0000

    twist moment edge calmly vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
