7450103a611fff8d900f9542a9abe305e58f1241
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:46 +0000

    brave noisy thanks represent once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
