62d4d69c175844752076dbb867a6b96b7447f226
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:12 +0000

    turninto closely failure remember breakthrough

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_list(array):
+        return array.tolist()
