2e9c2c2960826093e2c0128769bac3f21f581a55
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:14 +0000

    money market reach fierce Qq

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
