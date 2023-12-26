075433719f04d6dee8a6cd24c01e30ca494e5209
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:24 +0000

    attempt exaggerate address commit smooth

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def divide_numbers(x, y):
-        return x / y
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_sine(x):
+        return math.sin(x)
