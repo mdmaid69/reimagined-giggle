56391e76f40e9683958fd1029d50455b926c47eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:46:45 +0000

    please middle so exciting breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
