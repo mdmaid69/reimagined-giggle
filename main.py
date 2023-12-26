0cef0da33eeef6bb52f66e18ffdabaffec708e22
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:38 +0000

    head share protest shop giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
