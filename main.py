2daae63d587b64370d402756fa502197ae0900c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:46:14 +0000

    steady mistake butter exist shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
