f5e8916e77279a59dbcf6bf749453278aab00edd
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:49 +0000

    hurt unit whistle its square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
