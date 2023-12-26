85aa26f07236b28248b1c7d21afe280837c0eb4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:37 +0000

    edge card nervous rid light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
