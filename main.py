5e15e3346429de8474ae0e5998fb6a6f7f66874e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:52:12 +0000

    first pipe possibility defeat light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
