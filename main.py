b46227c714a4020c25873889f5e6b52b79d7ac8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:48 +0000

    sense uniform sight invite excellent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
