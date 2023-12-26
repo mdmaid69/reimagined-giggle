207f47ae406612dce7cf080bcc8619ae401319c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:19 +0000

    Itself into inquire fall agree

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_repr(array):
+        return repr(array)
