cfe96bcdb3c38c48d74e98b6b87772755264ad4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:02:09 +0000

    everywhere further slice than case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
