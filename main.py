d507bdc173c6ea6519b2fb5492a1be5adb42b3f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:30 +0000

    season trip teach funny mother

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
