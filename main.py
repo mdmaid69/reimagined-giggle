e6a7d16c64666e242168a8358422cfe134ddc7a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:32 +0000

    variety deep occasion load impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
