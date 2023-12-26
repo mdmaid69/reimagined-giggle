8aee0f7fa82ad80425f4e74db2276b5979672bc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:03:58 +0000

    tone bet hardly proper finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def convert_to_octal(n):
+        return oct(n)
