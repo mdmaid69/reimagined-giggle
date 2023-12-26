74ccc1f95e9180ff6da1cbf6e318f3df66870513
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:59 +0000

    very behaviour remember thinkthrough handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
