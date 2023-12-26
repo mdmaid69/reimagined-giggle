f770eb6a2f0125d6e85b270e4a4f58a4e8f553f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:09 +0000

    sticky almost winter goout brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
