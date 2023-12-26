78a416b7d9a51a0b1b66eacdd16e04c97c3e5cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:48 +0000

    thing task 569enjoy spoil sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
