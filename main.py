bbe89d7b2c56b1961d1a7725ae5dd5fc177976d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:09 +0000

    classroom word separate declare letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
