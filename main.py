4cb15030dcc615d19c54427ce2ac5fc48dd7f981
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:05:09 +0000

    professional punish pair throw rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
