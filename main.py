d2040aed26ece50a81692bf3c5c88765bcd4eb71
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:00 +0000

    news dinner grateful table squash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
