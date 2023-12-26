79966b9929600f0c9152747792fedebb5f589ea9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:40 +0000

    suggest equal Ll centimetre march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
