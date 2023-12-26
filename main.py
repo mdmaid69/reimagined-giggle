e6b6961d72ef1a53f189f8e5952b18a32ee669a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:39 +0000

    vanish police send steam deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   def add_numbers(x, y):
         return x + y
