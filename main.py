22edf5f82add10d17ef88351878f6c8890c98af4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:15:16 +0000

    report your road instead lot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
