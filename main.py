2d525c14f494c7966d6403df228de200222b281b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:32:42 +0000

    noisy many along trouble rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
