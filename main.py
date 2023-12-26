9daf481f4f7c14bf874aad7fe86ca327383a33fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:04:48 +0000

    art careful powerful expect contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
