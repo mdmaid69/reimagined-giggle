8cb0b61ee962454a429b1c5e2e22586ae99738d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:37 +0000

    snow pretty this wheel prepare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
