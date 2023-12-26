d9c2c03e962380c68482585f9094fe10901083b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:43 +0000

    doubt mark mine estimate late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def greet(name):
+        print(f"Hello, {name}!")
