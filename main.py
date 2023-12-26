8a9028aab5e1f58c695421845d6e42a5c009bedb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:49 +0000

    increase organize light fire Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
