fa9142890d4c7254b68a79c0715a488f8fc2fc04
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:30 +0000

    no model wall join base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
