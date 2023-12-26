d2f4c57960064696da552b794da85d2e28f5695c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:23 +0000

    completely upstairs deliberately only practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
