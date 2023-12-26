3c5097cfd307b80dc20d1940d455b3b752e6acaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:56:10 +0000

    pity completely help goaway foreign

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
