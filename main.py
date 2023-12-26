d3a34cdec905502374bb73f330bc3224b3ee8bea
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:18 +0000

    mean conclusion contents scream bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def add_numbers(x, y):
-        return x + y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
