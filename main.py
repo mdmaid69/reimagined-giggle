1713ff8241a350dc381f1b2478e37fe890c167bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:26:36 +0000

    cut question whisper edge ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
