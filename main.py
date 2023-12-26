87970052ccde73252e9d78c80db822d3107d3a4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:06 +0000

    tough telephone hello not study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
