e615612f0d33c4ef8d1e1170f762d3702d141eef
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:34 +0000

    tour lookaround stamp load breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
