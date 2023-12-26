1cefa2bcd0ef6689e89127272d9815b65eb68cfc
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:40 +0000

    niggaacademy none quiet shape police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
