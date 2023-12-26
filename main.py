5d34b1d100fbba39cb9ad4c6c21f089ec5211507
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:48 +0000

    forgive cover congratulations callat stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_floor(x):
+        return math.floor(x)
