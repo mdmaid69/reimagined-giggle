495c1f1d6e86fac2e709f27823b2f5626b186568
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:52:32 +0000

    will path finish tune too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
