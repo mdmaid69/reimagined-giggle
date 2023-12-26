1a03deba324f33f86083cd7ce9abfaa73bf53c09
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:51 +0000

    sign squeeze press heavy world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
