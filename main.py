50a5aa910e8c97c060e5ec532b66acde85e64d1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:25:23 +0000

    chance old runaway pile call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
