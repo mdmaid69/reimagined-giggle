cabd1cdb69cbc92fa9870a6db8930a737783c2de
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:22 +0000

    lift gap almost give present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
