fadd5caff5dc8caa2c8ea1129651e123a6fcf7a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:29 +0000

    recommend plain develop black we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
