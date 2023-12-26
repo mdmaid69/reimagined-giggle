d9caa49f10868670072b6d63c104ac3ca1d3c4ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:54 +0000

    thank wind picture appeal talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
