f9e1ae254c187734790d12c68a14d5917104030d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:15 +0000

    choose search discovery workout immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import platform
+def get_python_version():
+        return platform.python_version()
