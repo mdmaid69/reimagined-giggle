347d4efcd61b9faf31db4a4936f46ea9c047307b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:29 +0000

    breakin clear shine lie away

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-print(math.pi)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
