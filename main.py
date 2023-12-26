b80de897470977028dfdb8e1ae25cbada753a7ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:12 +0000

    trust ear mind improve please

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
