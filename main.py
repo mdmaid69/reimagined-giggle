181b44bdf5afe0e5ec7c35a9cafa6fbca289ed7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:00 +0000

    answer colour comefrom sensible off

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
