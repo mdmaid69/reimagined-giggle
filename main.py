d92add4544c65ab4441056d5badba023a7864673
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:19 +0000

    tear behave future manage almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+print(sum(range(10)))
