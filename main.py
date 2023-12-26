1b3b545b08bda78091a826bec40cd0a6bbedbfcb
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:43:17 +0000

    embarrassed limit everything read accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
