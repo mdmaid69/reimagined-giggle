1a923369b73e05a0f16ad5590d09a28b08aa7a9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:26 +0000

    friend land old best shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_repr(array):
+        return repr(array)
