6cf3080f88abd54ae11b83dbd2e05f07de1d4af8
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:33:01 +0000

    future watch pronunciation left represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
