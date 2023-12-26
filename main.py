f5fb4f636f3bd3bf47ae7c3e92167ced5dd0d029
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:10 +0000

    wash among after result takeout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
