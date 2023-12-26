feafda6e5bf3d416d1c4268b1aee37eb9a0aa4ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:18:16 +0000

    neat later direct intelligent board

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
