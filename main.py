bf42770503b9550f91d2bf6c49a02e47ca82c4ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:19:08 +0000

    conduct limit busy condition already

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def cube_number(x):
-        return x**3
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
