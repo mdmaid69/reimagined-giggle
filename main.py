2779f7ae364810df990447fd907a8ea4c21b67d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:49:03 +0000

    necessary us need partner 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  def sort_list(lst):
+        return sorted(lst)
