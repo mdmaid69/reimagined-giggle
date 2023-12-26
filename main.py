bf4e394e17072fbb2c818cbad6b56d23988262ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:28:08 +0000

    during slightly feel regret careless

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+print(sum(range(10)))
+import math
+print(math.pi)
