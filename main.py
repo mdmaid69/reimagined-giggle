8605c06f0ea4f4182ab108223ef0e3cab2cf3280
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:12 +0000

    trousers off mouth swimming pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def find_min(numbers):
+        return min(numbers)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
