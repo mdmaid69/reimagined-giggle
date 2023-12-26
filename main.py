d599e424d743ff523692256c7ec2e0e66d661ae1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:57:36 +0000

    free tear test to much

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_volume(length, width, height):
+        return length * width * height
