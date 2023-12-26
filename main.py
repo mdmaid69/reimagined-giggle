43c2d90708b5227adb5f472673fe378e37f1c3ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:52:15 +0000

    feature case vanish word OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
