f26e871ff0ad2f04f817cc38ad4fae98c023f9ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:01 +0000

    pay hello admire proof play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def square_number(x):
+        return x**2
