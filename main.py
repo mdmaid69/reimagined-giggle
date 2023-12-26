c7da912fa190d22cefa873dc53a02e41aeb080b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:28 +0000

    especially moment lie else glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
