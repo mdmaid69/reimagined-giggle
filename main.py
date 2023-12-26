0aa4f8d0dc5f24b37f0d7cd7044fab9ef9ccfc60
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:32 +0000

    tense arrive bar advice treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
