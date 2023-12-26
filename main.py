d948656fe96f434c3b68ccccd7336163bff6aaab
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:53 +0000

    own remove admire store scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
