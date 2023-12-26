b4a3881d305217a1ba45eb05ddd969916bfe6641
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:38 +0000

    estimate pleasant gas giveout either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
