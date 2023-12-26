c4a61f30ef5dc2e7786014c297193ad55e09c937
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:15 +0000

    guilty shall declare view discussion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def count_elements(lst):
+        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
