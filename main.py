cbb4f21320343a41c390a9997819b7e850ac8808
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:15 +0000

    spray pretty confidence comedown line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def convert_to_octal(n):
-        return oct(n)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
