d33128f18dc0d08f0100b3119ee9556604b99a2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:31 +0000

    whole easy bottom towards identify

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
