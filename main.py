4f441fa08e4e708c94d1a0f69d2e1751d62f25e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:14 +0000

    foreign disapprove search makeinto talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
