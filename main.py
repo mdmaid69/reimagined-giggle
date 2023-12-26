6340fa8856a388ed8ebfd1702946b1bc78984471
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:44 +0000

    rate mine dull 571enough second

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def square_number(x):
+        return x**2
