77e33e8e3d6bb8be8ef5e279e5cbc8d012c73934
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:53 +0000

    check line lean when spill

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_array_to_unicode(array):
+        return array.tounicode()
