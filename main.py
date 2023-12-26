55f483bf0cad4140b5b080f3b76a384e789048e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:05:09 +0000

    refer below lesson pen disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def clear_array(array):
-        array *= 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
