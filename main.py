775a6b0c45d61a15c7c628a2522a85628e928b47
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:09:52 +0000

    mainly explanation arm separate sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_counter():
-        return collections.Counter()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+print(os.getcwd())
