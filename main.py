9591e540cea2d57238637515b6a92359d8a03525
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:48:05 +0000

    but guard entry along place

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def greet(name):
+        print(f"Hello, {name}!")
