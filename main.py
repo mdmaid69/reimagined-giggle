63dbd622f4de84a5ac1f4008947856f28cf4f599
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:16 +0000

    power custom workout moment immediately

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
