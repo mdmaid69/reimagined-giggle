42a68b31d6da86f2950f632b5d8ed5c109e4e5a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:30 +0000

    us failure education commit breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
