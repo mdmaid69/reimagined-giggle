375739fe22b01edb171bedf02cde1249e6af263a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:45:57 +0000

    calmly less arrange even sentence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
