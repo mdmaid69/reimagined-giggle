3e6eeda6254fd8adcca19fce7489d82014d600b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:48 +0000

    failure immediate blind neat shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
