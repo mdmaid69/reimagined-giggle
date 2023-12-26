2c5a5d0e467ef2180c5c231babbd0a0d53aad1c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:24 +0000

    artificial contrast heat public either

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_max(lst):
-        return max(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
