2e9ca2aad868b1b080492253616499f02bba206a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:15:21 +0000

    content rate off patience answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import logging
+def log_message(message):
+        logging.info(message)
