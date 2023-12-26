b55bfb8f3db0dc15144f155d064e060c345622ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:43 +0000

    worst group lift flight appeal

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
