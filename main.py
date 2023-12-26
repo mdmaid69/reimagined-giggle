4ef06997067c9c4da55051a3475988337fe6124b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:07:41 +0000

    season important trip rather yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
