ac0ed7de958bb6c542f3c8e8fe495e7c088135bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:17 +0000

    supply Ss environment union find

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def iterate_over_array(array):
+        for item in array:
+        print(item)
