b0c73c89e33179569109283d8975e3a49c5e2cd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:30:12 +0000

    warm cause yes kill story

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
