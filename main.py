0330260aea2771fe171c388091b399162fee4cb1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:15 +0000

    considering right harm national badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
