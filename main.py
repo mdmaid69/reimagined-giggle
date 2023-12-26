4c0476fa0987d77f16b72bdc2fd50cfd50604829
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:42 +0000

    tip limit definite pretty attitude

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def convert_to_binary(n):
+        return bin(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
