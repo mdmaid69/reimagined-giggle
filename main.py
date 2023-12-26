1a4a7bf38199b7e6f217b476f92b52905d2cb885
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:30 +0000

    marry last Nn back exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import datetime
-print(datetime.datetime.now())
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_str(array):
+        return str(array)
