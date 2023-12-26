50aa985a7d53b0cefceb8c2ffa59af0fa62656c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:55:42 +0000

    sudden break abroad false proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
