9647bf15c9ea225be11498c6fddde1bcb1a68529
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:33:56 +0000

    strict unlucky base fast prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
