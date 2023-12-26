7635d569af34f7e934f40d4eea4ea4e7b0bc11b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:10 +0000

    show wash opportunity slow deaf

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
