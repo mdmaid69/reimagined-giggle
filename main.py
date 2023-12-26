95ac9d99b3a75c9430fd0f0d0dc38cb7ed4ab8df
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:55 +0000

    building adjective collect shout 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
