ee3f60a6e38dd6dc173a663f0c4bda963bba0d0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:59:44 +0000

    inquire which large bag female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
