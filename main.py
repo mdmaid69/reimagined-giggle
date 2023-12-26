6ea66e4d64752aca372705a00b018437b2ca2b89
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:57:44 +0000

    deliberately joke beside muscle limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
