5314098a82a6f76f968eefb83971956176019047
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:44 +0000

    breathe believe fold recently object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
