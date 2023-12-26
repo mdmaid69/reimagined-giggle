90cd965e5a9838c07e8d95ffb9d19fd535132326
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:13 +0000

    without scale Qq however disagree

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import numpy as np
-print(np.array([1, 2, 3]))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def convert_to_binary(n):
+        return bin(n)
