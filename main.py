ef1d87c3d4c1a95e1b05dd8142a2cbfd10f523bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:04:41 +0000

    always offence bury favour report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def cube_number(x):
+        return x**3
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
