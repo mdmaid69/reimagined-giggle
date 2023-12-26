f0826d4e4e052d6aa6f30c4b6e7152e46422273e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:35:46 +0000

    contribute Nn like blow skin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
