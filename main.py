459808f290af6cc973762a2bdeb27c2eff71524a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:17 +0000

    black or imagine period pretty

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def square_number(x):
+        return x**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
