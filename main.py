c10043d0ecbf64d45e081b8c4813692bdb34539f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:42:19 +0000

    score us judge trip will

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
