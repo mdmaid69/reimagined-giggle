a7542d2386a910f6e4a611f416fe2fedb6e60618
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:24 +0000

    upstairs legal child splash dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
