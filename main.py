4918a208764c5620c274b05b51d47211baad2f84
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:03 +0000

    winter anybody direction load wish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
