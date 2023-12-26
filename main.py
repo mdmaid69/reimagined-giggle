2a29bf1f012f643eb54d6c6e98fe50bfb225236d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:36 +0000

    flavour fork tooth breakthrough only

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
