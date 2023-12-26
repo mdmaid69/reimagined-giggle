645ccb1720e1d422945318d1105efcc27e9ecc86
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:26 +0000

    curious music embarrassed refuse violent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
