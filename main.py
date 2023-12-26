cbb3e2a60debe8218a0e880d502667063281d90b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:25:50 +0000

    great spot peace pipe comfortable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def count_elements(lst):
-        return len(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
