739db550d2809ceb3e16e754d528496018ca28ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:46:32 +0000

    brain apply file visit mind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
