fee176ee0b7eab183306f2e29ade675da68acf65
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:38 +0000

    know fine turndown order way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
