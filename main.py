7b78745361ca19323f30f0c9578f840bde7a17c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:12 +0000

    terrible anywhere around spite surprise

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
