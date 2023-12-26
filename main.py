24ac1732d5a07f438f07591a7c738aa896c6710f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:30 +0000

    mine getoff hurt train only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
