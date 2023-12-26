f2f6a0dc8b0be54b769a2c267eb49989333157ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:49 +0000

    friendly sell call glass disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
