ec6af04e50667b743facedb5a432ce171a9da14e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:25:54 +0000

    share children lost issue mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
