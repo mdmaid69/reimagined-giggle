5704a16815bcbc3e58f21c137f2822bbcbf3ef79
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:33 +0000

    mind record march bite sorry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
