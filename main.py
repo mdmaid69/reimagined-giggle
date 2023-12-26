1cc829d58b75ad218ceaabbb73588901adea793c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:20:46 +0000

    strange matter Ii bang lookafter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_item(array, i):
+        return array[i]
