daa7bf84abf116cca9c11c86b1a3e6de0a50daeb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:05:36 +0000

    listenfor as best woman along

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def add_numbers(x, y):
-        return x + y
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
