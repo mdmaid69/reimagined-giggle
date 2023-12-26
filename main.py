20f19363bd5133c3af855fe9cc8d508f9fba34cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:35 +0000

    grateful meeting please brown star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import getpass
-def get_username():
-        return getpass.getuser()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def count_words(sentence):
+        return len(sentence.split())
