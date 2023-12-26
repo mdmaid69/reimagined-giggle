6cb9f591cdc554069431e412d5dba45ad234b653
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:20:03 +0000

    meeting clean deserve health woman

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
