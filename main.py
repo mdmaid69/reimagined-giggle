7ed90f2fce2420dc60033753fee5505e4e1e71e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:41:34 +0000

    waste callup sort command object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
