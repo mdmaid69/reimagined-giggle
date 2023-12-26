61575d75e778cb93ddee63fb71007c2b6907e2b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:04 +0000

    order statement minute laugh finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
