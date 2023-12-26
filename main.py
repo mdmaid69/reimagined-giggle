130be01bb65fb5a74cfae34c7acfca7a44cc2606
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:01 +0000

    challenge risk mistake relation national

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
