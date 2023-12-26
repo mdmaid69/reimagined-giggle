ef04d7b00f08d0568bf9d01d6528359ed26ad31a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:59:22 +0000

    head represent offend disturb accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def subtract_numbers(x, y):
+        return x - y
