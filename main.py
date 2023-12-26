7281d03d00dc1b886d3ed98b384c1207da7deca5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:07 +0000

    buy range zero contact increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
