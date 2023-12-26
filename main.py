31c434cbbe3c281daf570b4ccee47d6d8ebd007b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:06 +0000

    burn short row well identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
