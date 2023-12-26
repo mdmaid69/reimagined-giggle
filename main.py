816a235707bc1b76d4494d537caf4f8c2bf080b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:10 +0000

    absolutely conduct pin doubt hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
