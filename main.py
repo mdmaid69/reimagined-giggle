29e83db85e3208f67c8213ecc19ebe8f352e4377
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:26 +0000

    complain spring shop depend slightly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def add_numbers(x, y):
-        return x + y
+def add_numbers(a, b):
+        return a + b
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
