a60bbe7c84a4dd836a2760c1fd62d2ab6681ae12
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:20 +0000

    argument nothing wander take up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
