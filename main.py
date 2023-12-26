7080c9768af0d60a083da9077eff85feba2e2062
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:31:53 +0000

    tune undo 571enough cum me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
