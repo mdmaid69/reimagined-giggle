6f657370c02e981c9c3a635439628c0987747be4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:15:08 +0000

    mean visit own brief university

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
