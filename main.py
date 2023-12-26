8969201bcebd57779c55439e201866a3bd696543
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:07 +0000

    split large deal without explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_density(mass, volume):
+        return mass / volume
