e3333ac807b135591a0270831f0c2fb6a0cea311
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:31 +0000

    estimate shoulder plain even quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
