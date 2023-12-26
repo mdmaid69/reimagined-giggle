5fba087ae2e8b3f6903a73429b05dda3e3ca2d89
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:07:51 +0000

    mark bottom kill quarter declare

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
