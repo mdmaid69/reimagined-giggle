c382551fef77b8312db0b8f4668a152e71db0c97
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:05 +0000

    production actually deliver compare pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
