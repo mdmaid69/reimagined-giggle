9e800ecaf461502514a45d16f60957e4fb052916
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:48 +0000

    steady point night line top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
