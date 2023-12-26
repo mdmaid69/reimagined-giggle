bbcebe8907e4325f6dd40a3eaab41e70bacfa10d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:40:02 +0000

    thought age shop everywhere mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
