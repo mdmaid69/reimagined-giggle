3d06828b2240edbcf59af7e3007bde4c72913928
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:06 +0000

    grow rubbish dangerous hour sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_pressure(force, area):
+        return force / area
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
