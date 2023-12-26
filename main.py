7c78aa326867d4e74684d95d9f9ed780da7aef66
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:24:23 +0000

    marry admit dull test wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
