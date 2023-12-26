5ca46ea4e6627836513668e1df12efd7274c1d8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:58 +0000

    creature comeround culture punishment slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
