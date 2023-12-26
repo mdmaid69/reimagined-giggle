4878edacb3cf53f8b07e608926a5d19d49257ef7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:53:29 +0000

    trick correct wild stop possible

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+text = "Hello, world!"
+print("Words:", len(text.split()))
