087e4c55e9f7fd0ea605f115503abc09bec0dca0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:52 +0000

    once separate point successful workon

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
