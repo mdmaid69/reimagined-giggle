b2f97318303227995e53f8506d2b0e7476f2f85b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:21:38 +0000

    wave roof choice jump claim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
