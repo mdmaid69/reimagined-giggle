7e36b8b0574dea21c2a36995c78a9f0005bde4cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:38 +0000

    black adjective angry by dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
