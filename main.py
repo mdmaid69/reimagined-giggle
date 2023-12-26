6f9365b216a0b7c2a071110ee22a50fd768f0f93
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:37 +0000

    organization happen basis divide burst

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_pressure(force, area):
+        return force / area
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
