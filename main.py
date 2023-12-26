bd9e040332a275c414930ffc0f79d39c25bdd29b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:11 +0000

    best deliberate breakin whistle young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
