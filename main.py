4b5ba767f351ff56e23981c48bcd952610f677e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:08 +0000

    suit guide properly father car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
