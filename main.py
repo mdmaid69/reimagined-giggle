e70cba40cdfdf859fd45c7789b6b28ce2bc98f51
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:13:06 +0000

    interested black question computer treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
