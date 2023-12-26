aafb4c599b13c53f270ea3e07518fd9cbc34f3ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:33 +0000

    skirt coat burst fire sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
