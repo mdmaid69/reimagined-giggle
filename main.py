8fd9c522628e05b96713542c2c921076b6ed3944
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:00 +0000

    because innocent appeal description model

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def is_even(n):
+        return n % 2 == 0
