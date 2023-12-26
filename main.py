78098ffa8332baeb7e2a2350720294527b55e381
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:23:21 +0000

    goodbye step possibility one bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
