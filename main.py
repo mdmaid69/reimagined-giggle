48d976e826fa41150bdc4fef5ae147af4cf6563c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:48 +0000

    improve otherwise desperate mouth honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
