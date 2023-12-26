7f31f72bacec65c224d9a8142354fad1139229cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:53 +0000

    plain guilty favourite discovery cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def square_number(x):
-        return x**2
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
