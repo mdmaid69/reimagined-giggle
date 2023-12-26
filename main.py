04c6183dcc4c664f64f3895662fadb4a9286f449
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:14:50 +0000

    draw warning every mean grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
