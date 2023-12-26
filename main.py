ef6d8cba3d9719b647e71547c20be150e3aa02ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:37 +0000

    official uncomfortable follow centre decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
