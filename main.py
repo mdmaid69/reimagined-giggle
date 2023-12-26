79ef06663bfe247d851628a088d3668a4dd90cc6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:18 +0000

    573entertainment prevent ago can against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_index(array, item):
+        return array.index(item)
+def is_odd(n):
+        return n % 2 != 0
