28949e891e2a12ccd2543eb55dcfd26607dbf0a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:23 +0000

    suit owe lucky cap drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
