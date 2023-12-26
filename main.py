11824e41d828f0bad9a86e3c8c963f7a11ae051b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:01 +0000

    deaf excited pause direct downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
