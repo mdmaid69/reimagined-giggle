60cab051b8f731c4da98d71bfe939bfdc248e992
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:44 +0000

    her band crisis slide remove

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
