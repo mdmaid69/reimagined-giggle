5221da152dfa5e4e748407e87f2a7185cb444ff7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:54 +0000

    wall turnback eager than relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
