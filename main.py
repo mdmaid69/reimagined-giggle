6adca1544f2d78150c70ae50cd27b343ee0b1363
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:18:35 +0000

    carry recover movement smell branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
