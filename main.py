5861a1cc9a1511ef4a158f9df80c082c32147bd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:09:43 +0000

    community study interested sad act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
