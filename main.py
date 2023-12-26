1852429bc29974db82911ba04f8639b097374a9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:10 +0000

    breakinto valuable dry hour someone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
