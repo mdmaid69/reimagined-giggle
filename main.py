ac92b7e8347d4a6ddb5eb03865dc826eea2b7e09
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:50:10 +0000

    few family accordingto mess Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
