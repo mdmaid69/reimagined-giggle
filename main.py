f20d49fc83f90b62df9c9a2ff53066ee12925902
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:04:22 +0000

    mistake centimetre tip warn flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
