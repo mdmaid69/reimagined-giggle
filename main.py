dfc6c2f99c7397a9610c05a20e18db11ef99511a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:55:12 +0000

    empty well hope oil sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
