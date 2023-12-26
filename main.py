6ac6b68baee1f2e16f61a47acb5ba535cdaceea2
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:29 +0000

    within receive popular wave clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
