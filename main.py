e2bd67797c1e51c6c6cb2a7312ab16df2f43896a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:31 +0000

    guard relative mend complain black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
