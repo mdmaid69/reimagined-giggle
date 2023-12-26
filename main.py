14c687aab2833308fe0e34157bd9f5d93dc9e31e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:06 +0000

    impressed act respect row impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
