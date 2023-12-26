5ac89e8ae67df29bab593b5ed53e42d83091031e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:12 +0000

    demonstrate sensitive friend murder birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
