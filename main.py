f3d8e94d80f501da53608b56aeff7b351e42d8ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:23 +0000

    life fail advanced total under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
