20ea1564de24dcaf4fb1b1a58327e292e87270a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:56:42 +0000

    left change even increase freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
