1031bac1677b6879a1b1db4c1037b7b994c85996
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:31 +0000

    smoke finish on switch reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
