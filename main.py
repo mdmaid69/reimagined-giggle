f7742c95023a09eae70e022ff717bc686480f0cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:23 +0000

    funny importance software drop national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
