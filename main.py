4058ba45b5cb25746c6e268ea346bf93eff1f164
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:55 +0000

    include true before label account

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
