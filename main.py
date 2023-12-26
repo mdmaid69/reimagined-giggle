1c4132f25bfc2a1d2ea7928460f944be2bb2cd11
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:45:24 +0000

    breakinto love whole meet fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_base_name(path):
+        return os.path.basename(path)
