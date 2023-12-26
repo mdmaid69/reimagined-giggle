b267ced6ec4ca9093828282fbf91c23ef3fb5055
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:46 +0000

    everywhere choice full unfair want

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+import time
+def get_current_time():
+        return time.time()
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
