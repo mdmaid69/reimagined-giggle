df53f4734650a7eca26e89bca6d03326b07f91d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:29:31 +0000

    flat hello rent must disapprove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_min(numbers):
+        return min(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
