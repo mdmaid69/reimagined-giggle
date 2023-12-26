995d6ec29e03387f0c45f52052bfaf67339ec1ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:04 +0000

    pause happy protest open further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
