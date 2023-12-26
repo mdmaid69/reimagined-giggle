2cf9a892ddc4ff49dda769128a5bfaf8fa1b0b7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:28:43 +0000

    night wind unkind behave hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
