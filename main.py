65c251e41a4e25f58ba25d57d52c2a82deb45f06
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:53:17 +0000

    welcome forget nearly claim sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def cube_number(x):
+        return x**3
