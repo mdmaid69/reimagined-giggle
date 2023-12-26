d15a17230a33dd1aeaef6acbaa2d1e999b1519dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:13 +0000

    emergency low life hurt behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_volume(length, width, height):
+        return length * width * height
