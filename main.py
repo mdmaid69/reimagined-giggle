7870b37d73456a029a37bb324096e5a5e7f2155e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:08 +0000

    squash holdup male wrap strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
