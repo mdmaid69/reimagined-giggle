0ba5b61e4994642642d323a581405e7cefc0c541
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:35 +0000

    uponon many mend ready trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Average:", sum(numbers) / len(numbers))
