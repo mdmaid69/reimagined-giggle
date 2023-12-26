fd5c9b65d9375449e0f61cb5f60f9402f83f400a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:38:55 +0000

    leave lie sound pure soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
