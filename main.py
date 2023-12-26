47600d3e801592e4e534894cf679d0f2a2d304d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:28 +0000

    565end greeting step calmly yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
