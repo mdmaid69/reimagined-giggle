365c2a44ac33fd861f760c25a534e2ca2c360452
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:18 +0000

    recover Ww century flat prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
