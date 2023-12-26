66a422300369bfe98bcf75ee04a6110b9ad1ec73
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:36 +0000

    juice cheat wind any remain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
