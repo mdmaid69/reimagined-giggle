d20f6de0255238cc48d9e015ea2146a22c334483
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:36 +0000

    concerning interfere book goup plain

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
