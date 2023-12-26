eaebf49708cfc54869c8c63a2dbaef093d9bc142
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:33 +0000

    hollow muscle shame exhibition trip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
