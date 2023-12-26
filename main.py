241239020ff7d080f160bbffc93e8f28a6de8e2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:17 +0000

    573entertainment act welcome callback brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
