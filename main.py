5a82c8bb63d2654537217a3013400a53d92cc914
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:55:43 +0000

    birthday protect ordinary principle back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_list(array):
+        return list(array)
