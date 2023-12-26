4e9d62de8f5bc69a7cdd77710d626d142abcf049
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:52 +0000

    art pain pinch fit frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
