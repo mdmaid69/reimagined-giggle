d16076f35378fe0ae1a4989c6719af9046ef651f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:32 +0000

    stretch seriously holdup account stop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
