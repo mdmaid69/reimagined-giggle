739eb9acdc6a3b28c3cc7ab578369d6e96a5fe2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:57 +0000

    talk pleasure into turnon understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
