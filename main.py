3248f8ab829dd7034756d7969ee60779d1165e68
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:40 +0000

    power perfect strange off nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
