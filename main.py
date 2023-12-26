4d2ef82c98cb41e57ade5278ae4e2181b06f5e5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:41 +0000

    combine discover strict least dish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_acceleration(speed, time):
+        return speed / time
