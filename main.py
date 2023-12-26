54f60ef94d899dbbf51a398403b60407db69c200
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:40 +0000

    black throw positive cross we

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print("Hello, world!")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
