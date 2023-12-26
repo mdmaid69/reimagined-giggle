2c8e72f85f2731f0de2ce5b971652f3f60f07f46
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:29:01 +0000

    joke go callback Yy win

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
