a11288ee7d2ead1813ff847edb15a1b773f0d85a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:11 +0000

    idea later flat upwards exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
