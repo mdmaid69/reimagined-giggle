9d8b6233d24b043525b3f033548a4b57757fe7f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:08:22 +0000

    how backwards child avoid rid

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
