d8af2fed01413289dab99fb7fd5f672c34faedaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:58:31 +0000

    genuine pretend reduce worried buy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
