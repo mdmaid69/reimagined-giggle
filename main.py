3d6b9aae154fce16b28bb2d8df91e2bbb0b81682
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:00 +0000

    tall create slip himself impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def square_number(x):
+        return x**2
