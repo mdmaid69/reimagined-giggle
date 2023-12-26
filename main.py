7d0c27b59ab80c5a98ad8a3853290686264ae9c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:59 +0000

    meanwhile brain test develop action

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
