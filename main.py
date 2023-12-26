4349ad557f9bf605a3077847764e8562ac88441c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:29:20 +0000

    meet learn apply emotion himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+print("Hello, world!")
