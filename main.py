69d3d4bba92b080e1d90fe0e3aa2c71ab3e9a2a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:47 +0000

    mouth cut sensitive over poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
