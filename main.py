52fbb84bd46b549f6ad11b1e41ddae8c396815fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:50 +0000

    strong earth central pupil because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
