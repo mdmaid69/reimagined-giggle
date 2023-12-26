c6d3d3fa00cbe504c0cf656d1438d6706cbc2d0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:33 +0000

    meanwhile again turn minute information

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
