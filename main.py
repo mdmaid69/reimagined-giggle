e841d3942ea9b86e6c645d93b59dceabe3e398e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:10 +0000

    experience recent throw loud person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
