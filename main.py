171d164c585f8abc83c7e16dae12717e1f43c5b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:08 +0000

    while prepare pure godown team

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
