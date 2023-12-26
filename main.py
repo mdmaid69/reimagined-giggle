10f130a75d8c411abf8697b06c4f4e029fba4d8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:55 +0000

    responsible leave length negative news

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
