a8603c2250e74a0791ffa479e65f87c61ada4b84
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:45 +0000

    shop hurry accordingto sensitive smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
