afa204e6fbe0b837e5753c00d03d8c27de7248c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:26 +0000

    past deal national trip hope

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import threading
 
-{body}")
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
