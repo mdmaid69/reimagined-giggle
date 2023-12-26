3b5fef185de9b1b9c364c357c39a795ee8fad88d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:20 +0000

    disappointed power rule lose put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
