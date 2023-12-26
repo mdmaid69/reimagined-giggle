4ab7ac3136a8ffb33f24442b9d414219177cf4b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:12 +0000

    post possibly fault straight threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
