4a786e7fd937cc47f05cb69580142bcf298c6aa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:57 +0000

    violence print steer style quit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
