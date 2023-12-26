1a35b39c5d6e90b25e9abc76cfa35902d5436865
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:49:05 +0000

    phone solve occasional action direction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
