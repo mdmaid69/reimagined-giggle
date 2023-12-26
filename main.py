a0bc032a13199e791358d9ed51810f900f27c912
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:10:43 +0000

    combination fast meal swallow again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
