a24e24d0c0f53d0f7353bde1d462ae06dc2f4f7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:32 +0000

    everything comeup straight typical deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
