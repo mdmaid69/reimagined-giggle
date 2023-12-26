abdcb631af4a9874896a0028a95e2791676f0302
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:16 +0000

    importance lock visit guilty real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
