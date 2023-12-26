4289e94abb657083012124178f6c1d17c66cf6ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:38 +0000

    improvement divide track effort project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
