9cc9771a6d823179ce4049a6d26ff8a048fce933
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:29 +0000

    sticky reduce speed available test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
