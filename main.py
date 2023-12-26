49f0b75dec12dcc145d8d9fd4a7e9b93add76763
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:36 +0000

    down arrival waste jump custom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_typecode(array):
+        return array.typecode
