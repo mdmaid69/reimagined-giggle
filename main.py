0415107afffa708a02c2384134f7b36eaaf067a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:30 +0000

    weather anybody scheme mistake direct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
