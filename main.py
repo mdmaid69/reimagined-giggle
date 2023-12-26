e4b77dccbee2abc20fda8d69389d686db1760bb1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:55:26 +0000

    long rescue conclusion arrival play

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
