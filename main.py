763dd72da976cc714661338011ecc6d7cfb91810
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:34 +0000

    undo thinkover lookup turnon contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
