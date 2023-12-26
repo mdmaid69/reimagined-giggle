9655277a58e29f16f3520d6aa77dc031f2801af6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:35 +0000

    choose separate worst deep comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
