5561ea1ce3ab5f6f59b96c66d654339e8586216d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:56 +0000

    hollow along then occur happen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def get_bytes_from_array(array):
+        return array.tobytes()
