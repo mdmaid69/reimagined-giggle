dc27702dc495c7d7a054bb08dbe154eb6b707ecd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:59 +0000

    quit embarrassed view thanks exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
