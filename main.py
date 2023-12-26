9b9809fdc19b1b31532a81bb78e25279e966b8a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:13 +0000

    goodbye basis determined national emergency

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
