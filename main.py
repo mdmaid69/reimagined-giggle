9abf549c42da6dfda0f5859b6c0eb075f76b6d5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:11:18 +0000

    uniform generally summer hungry home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
