868d33d58ff1bf398da2fd559fe474646af928c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:43 +0000

    constantly shadow task come bye

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+print(sum(range(10)))
