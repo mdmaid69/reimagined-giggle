8f2c458ef5f3d87d98243fed5b5c7739e13efeac
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:35:44 +0000

    promise ask understand catch wire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
