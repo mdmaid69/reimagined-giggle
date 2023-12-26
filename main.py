ef2b2407f32f4b602b0f35df35394bcb99d3b2d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:34 +0000

    repair world makeup over talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def square_number(x):
+        return x**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
