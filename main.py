37b79af9515f5f46e5d0ba06624b76df936119fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:36 +0000

    disappear flood left positive treat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-print(math.pi)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
