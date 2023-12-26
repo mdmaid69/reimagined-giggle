7deb08dd02d48d182140de65f50fa25fd10b11f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:51 +0000

    takeout majority sort marry exchange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
