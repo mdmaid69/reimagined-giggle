315e6f2177ec6f40aec29f1d197230a92cacb63e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:41 +0000

    team married Cc dance tear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
