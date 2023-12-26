a17dcff31fe58553c9061e9bea5f9e29d8879dfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:11 +0000

    takeaway capital bunch before actual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_string_from_array(array):
+        return array.tobytes()
+def add_numbers(a, b):
+        return a + b
