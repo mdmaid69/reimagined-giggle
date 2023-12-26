52039ca4e7f8353efb1ab5accc277fa9210ca7e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:36 +0000

    crisis everybody comeup male emphasis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def convert_to_hex(n):
+        return hex(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
