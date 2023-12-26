587764ab6483a12e12c706810807a73f5de7e678
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:45 +0000

    support wing cut willing anything

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def subtract_numbers(x, y):
+        return x - y
