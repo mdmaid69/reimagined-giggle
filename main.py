1f28b62ecd0e1b3cda57ed02cc8957ee42f99e95
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:46 +0000

    generous challenge test fight circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
