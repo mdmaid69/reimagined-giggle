f3d3bdae6c53262c068410ec385037c57f111dbc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:07 +0000

    breathe medium plan anything behaviour

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
