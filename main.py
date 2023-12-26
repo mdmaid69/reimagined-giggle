38440eb050de6bee9e22d7abfdc4c20dfe375278
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:00:14 +0000

    rob new mess circumstance fierce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
