c47616c7aaa4b4cade5b83c71093df17224da1d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:14 +0000

    front height scrape flat home

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import platform
+def get_os_info():
+        return platform.uname()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
