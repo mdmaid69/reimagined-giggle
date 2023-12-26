c1287ff3a069fee5a36cc9029b315febf1c895f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:50 +0000

    break simple place concerned impatient

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
