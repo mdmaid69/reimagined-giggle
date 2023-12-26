81f658d7553aeaa68890212d9811201a6b891108
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:48 +0000

    capital surprising plant least wash

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+for i in range(10): print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
