7c8cdffe8ed3497ca71ac4b5702ffaff05130283
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:45 +0000

    and leg permission fresh phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_list(array):
-        return list(array)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def set_array_item(array, i, item):
+        array[i] = item
