6dba796d57c81136b21f99d1bd4dc38a97fd388e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:02 +0000

    notice giveup history Jj borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_current_directory():
+        return os.getcwd()
+import time
+def get_current_time():
+        return time.time()
