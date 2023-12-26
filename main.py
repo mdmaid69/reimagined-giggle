f8166abfc9cafbcaed59be9a1b99a0ae9f4edefd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:38 +0000

    cross know word production gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def get_array_index(array, item):
+        return array.index(item)
