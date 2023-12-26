6c0b4f6094f5282bfb2bce6877f1c0d4b14d6a42
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:45 +0000

    reply persuade trap odd light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
