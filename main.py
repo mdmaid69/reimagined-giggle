938827531d4aa20a541a9c2ee0747ba2cfbd5c9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:16 +0000

    interview defeat simple pronounce dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
