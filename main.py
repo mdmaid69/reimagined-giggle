4e053c5790556fe7ca88bcea28950564755bbd8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:13 +0000

    custom flow age round abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
