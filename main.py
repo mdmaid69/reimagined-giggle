34fdfa5e352db14689d89e635809e5e5e4978769
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:10:08 +0000

    vision eat no comeout completely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
