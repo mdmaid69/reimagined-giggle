9b74c416d4bfef0ebbde97201bf499640237d55c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:10 +0000

    opposite happen phone offer effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_error_function(x):
+        return math.erf(x)
