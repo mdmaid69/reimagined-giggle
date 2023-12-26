7965482c51dfe88b1e03810af1b1cf9644a0466a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:10 +0000

    church scream expensive act occasion

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def divide_numbers(x, y):
+        return x / y
