ea0c059a404d6b91157f964c79dd836aad35453b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:23:23 +0000

    properly pipe attitude though post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
