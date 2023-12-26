cfeca3b68a0d1027f14094681dbfc1965289ebfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:45 +0000

    Qq destroy takeout shop near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
