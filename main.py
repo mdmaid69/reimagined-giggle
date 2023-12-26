e47944fe9a39d5da9e8f6bb5a56269ee0f53be66
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:59 +0000

    offer clothes recover land impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
