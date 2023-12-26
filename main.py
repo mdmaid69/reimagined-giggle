3e96ec61b60446e65473a5745f8ec833c72a8320
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:01 +0000

    become hate memory lower ours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
