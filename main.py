04ed7ca3e3206d3087f6dd403d21522019a40831
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:48:29 +0000

    Bb pain throw embarrassing card

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
