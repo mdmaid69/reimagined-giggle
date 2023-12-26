6f562db0c0a8f16053835a5b90b450fafeeb99a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:27 +0000

    report brother realize confidence turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
