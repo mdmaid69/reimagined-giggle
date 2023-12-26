709356680c91d6d65e208f7e981f9208c4388b1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:46 +0000

    house cold recovery you clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import shutil
 def move_file(src, dst):
         shutil.move(src, dst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
