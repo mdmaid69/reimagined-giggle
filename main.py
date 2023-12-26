433f2a654a86fa49f792930dffecadef3650fa91
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:50:03 +0000

    borrow exchange mad pleasure plan

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
