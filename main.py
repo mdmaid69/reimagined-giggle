b56abca53ee353dd7a9f82288502b53b1684853c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:59 +0000

    careless maybe pure understand period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
