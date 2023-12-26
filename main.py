70ef31892eff8d1f01497866c1fe054a03f9007a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:00:26 +0000

    marry formal partner border national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
