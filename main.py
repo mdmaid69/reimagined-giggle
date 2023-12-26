0af4304c65f4357e9211a2e13f021f7a4e85cb83
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:49 +0000

    question cost capital classroom something

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import time
+print(time.time())
