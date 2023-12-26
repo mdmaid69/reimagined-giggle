232606e0d493201f45524c7d63002cd62230f52a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:49 +0000

    concentration mad culture care crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
