9ba6c6517a179ad22efda345dadc7beba8108794
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:32:50 +0000

    pleasant than gas trap tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+print(os.getcwd())
+import os
+def get_current_working_directory():
+        return os.getcwd()
