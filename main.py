515efc9a8fea6e5d6d1c3066dc0c6dca74f1e8bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:03:10 +0000

    hang sure curious somewhere production

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
