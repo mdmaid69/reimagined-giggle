49a01c18d99c220b3a84d6d8d23419198dd39bf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:18:37 +0000

    eat Uu dish stop urgent

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
