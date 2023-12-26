7b73ae53775aa593809e4bd635875952e6edc00f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:48 +0000

    increase surface borrow several interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_set(array):
+        return set(array)
