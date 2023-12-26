3823bb6e44e555dc83f355652fa8b86a8f9600d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:16 +0000

    concentration principle for neither wash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
