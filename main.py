26cef87c221a499e95a638660d56e94458b8a41b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:49 +0000

    language 572enter rank bye true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
