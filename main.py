3c752d6cf6b73e5e9a86b4a77147e4d16f93fa51
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:23 +0000

    refer more another thirsty refer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
