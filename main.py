a74656df2fb80b135d99dcf00b80b71239cc84a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:14:12 +0000

    question comeround content happen taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
