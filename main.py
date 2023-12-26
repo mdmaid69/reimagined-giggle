799017e84ae98f4b7f40978764c79b46d2fbc880
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:47:19 +0000

    communication willing than over trip

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def subtract_numbers(x, y):
+        return x - y
