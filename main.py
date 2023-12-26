d89ad76702043677fd0f3292c01e26300c74cf97
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:40 +0000

    afford angry shout physical catch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
