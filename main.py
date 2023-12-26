f575feaa802f6c9e38b774b468e75ab9c352edbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:28 +0000

    bang platform otherwise towards comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
