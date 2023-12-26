a9f35601f45466c1f314ff77ff9faca2ce63a36e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:41 +0000

    over aim awake attach autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
