ea879b247471ae08a26771e99b10fcae0132d18f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:10 +0000

    roll steam pipe getaway smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_speed(distance, time):
+        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
