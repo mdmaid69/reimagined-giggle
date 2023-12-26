56e1355dd796821d7465640b43090e52da3ad1a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:58 +0000

    lower dream slip comeover detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
