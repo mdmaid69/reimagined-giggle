01bce8ddd78859a272f9af081b72fa77531c10d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:44 +0000

    fold brush plastic contribute record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
