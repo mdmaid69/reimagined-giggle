6df645c08d2803f1fb22d94087a13efd3da52f11
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:16 +0000

    from along call hang advantage

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
