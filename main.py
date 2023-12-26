a997315effeaf329fb41079dbc3ef063d60047de
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:16 +0000

    belong workupto may pay split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
