0c7ae6a499e79f1f6358de405d7050970266beb8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:31 +0000

    separate zero now find basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-print(sum(range(10)))
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
