7a0cd318af92ee89ab84f3021afd5c67961dda24
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:06 +0000

    show point obvious like reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
