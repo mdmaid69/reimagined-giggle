b50d67e6d3ca70561d4ceb775e457ac3dc0f6996
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:31:54 +0000

    turn maximum failure walk regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_user_list():
+        return collections.UserList()
