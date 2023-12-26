8ca4bb258246ff07bb8bb82a02915de2fe6a8d1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:29 +0000

    thirsty obvious business spray late

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
