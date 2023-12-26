3090e2b6ff23b74c61764ba906aa60db6c1e377c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:41 +0000

    moral callup decision objection Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def add_numbers(x, y):
-        return x + y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
