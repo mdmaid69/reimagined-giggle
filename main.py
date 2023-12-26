e8acee3bede98fbb508f45768d9f14672f7339bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:14 +0000

    somewhere him among Vv actual

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
