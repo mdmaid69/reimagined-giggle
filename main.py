71531c5d91fdaea4dfb5b410f3574065a0937e37
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:55 +0000

    nature listenfor try dinner shadow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def is_palindrome(s):
-        return s == s[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
