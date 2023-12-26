cb7e0692360757ae6f682240f1d7aec796adacb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:02 +0000

    roof have environment smell instrument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
