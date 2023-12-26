efe81c137681e535d009eaec25512420091c55eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:23:36 +0000

    teach translate meet by takeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
