b9136b84a5772c1f769cd653e694d11f5847fb27
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:13:30 +0000

    onto turndown modern tax die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
