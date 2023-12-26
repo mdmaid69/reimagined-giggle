27e285c6ab0d50c3775d0d80703c603e8adea3c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:33:52 +0000

    waste fasten indeed public drawing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
