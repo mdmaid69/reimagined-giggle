a10f0e4543b15e686a7d23401d975e731bfe7b49
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:41 +0000

    save exchange just situation rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
