b06531ba2cafadeaef1f2d004de095ee43e9f0e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:24 +0000

    note build stupid pollution make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
