20b4c3c0ce121731ef8817785c004f9b4949a4e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:50:35 +0000

    plan pool figure holdout old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
