287e63724f53d045ad02dd79aa561d3aef7006bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:19 +0000

    workout birthday rather replace fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
