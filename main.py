ad3dcc9f451a552f6b53ee909c87f60b4ae840b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:58:17 +0000

    container by gradually keepout complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
