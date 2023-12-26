dd487bc4d6dabed048cf4bec2919ade5ae240c5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:36:04 +0000

    argument who frame silent world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
