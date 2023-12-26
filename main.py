8b58cef36c59b37974a01b33e5cbdddc07054e3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:22 +0000

    interfere 564encourage service cinema invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
