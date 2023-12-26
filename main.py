a705bb2f2f7fc37341fd961b5e5e4bc34f379174
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:14 +0000

    dish comeup answer exciting train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
