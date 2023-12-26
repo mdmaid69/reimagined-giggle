c12f538ef02dab7e10d619271ea56002aae2029d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:46 +0000

    free recover pick lookafter practical

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pressure(force, area):
-        return force / area
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
