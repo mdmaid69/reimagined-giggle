892507c42f859cb4010dbc7e85165b26a3914e37
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:19 +0000

    exchange poor proud wind impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
