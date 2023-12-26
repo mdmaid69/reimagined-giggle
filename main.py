9e7c1e700ec14c05b404c137e23f8b75fb8d1814
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:55 +0000

    realize keepfrom example hollow shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
