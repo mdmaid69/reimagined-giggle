ed8ac5f80c2f14bb53a78e6700a67974e1fd7760
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:46:43 +0000

    change lost friendly image drink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   def square_number(x):
         return x**2
-  def add_numbers(x, y):
-        return x + y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
