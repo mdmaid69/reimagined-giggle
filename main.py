f66878176a04f9828d5af5dd7bebca1c5d82df94
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:28 +0000

    to in mood children behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
