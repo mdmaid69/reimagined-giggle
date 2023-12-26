8467b9f3889f57cdcf00fb26884ce7f2a91d9668
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:16 +0000

    light fix interested traffic Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
