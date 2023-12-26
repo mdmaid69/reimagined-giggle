638abe4164bc0ce45592cf08cb512391e5c85a27
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:44:32 +0000

    land pain warn thinkthrough bet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
