376fd93af8f80f9cba3d77af82604344991bb2ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:08 +0000

    crazy get image ordinary contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
