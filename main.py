55f54cf8b670adb74bfbe09eb087f642acb2ce02
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:16 +0000

    along forever lucky imagine keepup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
