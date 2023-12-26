a57486ec5e26dff17a50f5babacd8af30c1c9de2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:10 +0000

    declare advantage vote spill beginning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
