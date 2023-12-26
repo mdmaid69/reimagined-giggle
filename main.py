5824988fc08fda1b7fa6e31e17be6e24d30cb52b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:20:44 +0000

    small rest trade pin mind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import tensorflow as tf
+print(tf.__version__)
