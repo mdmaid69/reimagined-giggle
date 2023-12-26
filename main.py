fc9d73b14c5bc773c6c8df16e9d5f1f43cd98827
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:26:44 +0000

    criticism wind tune unlucky long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def subtract_numbers(x, y):
+        return x - y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
