21634185733641b3009276e503649c7654c49dca
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:07 +0000

    over weak impressive belief watch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
