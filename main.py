ab3e446ce59c63d0558f1813771c8be558222247
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:10:01 +0000

    slow combine shop live object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
