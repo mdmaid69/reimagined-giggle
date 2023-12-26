6d3751cc2ec26826832f4faa83e40561516b29e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:29 +0000

    traffic table exhibition whistle chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
