68c80fc73aa13c31c7f53c733250b2feb553e26a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:59 +0000

    through tough prove gas spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
