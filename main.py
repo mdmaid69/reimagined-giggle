3caf47942d5d0cf3e770d4510408b63899f40d4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:49:23 +0000

    female tour home ride discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
