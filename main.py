a6f41db9134830656e2f0896150c54055705c6d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:57:20 +0000

    idea minute paper pause pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
