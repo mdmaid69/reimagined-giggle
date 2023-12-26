e108233d2e81d5cf116cc804faca4a5157b42a1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:24 +0000

    play principle unable abroad usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
