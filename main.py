6c963a45a69636a5e59b9ad6789236796d03cf1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:34 +0000

    runinto second along blue closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import time
-print(time.time())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
