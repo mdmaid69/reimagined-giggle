422b182b1e95561eda18c2d402b85939943bd3db
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:04 +0000

    computer load closely store church

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import time
-def get_current_time():
-        return time.time()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_current_working_directory():
+        return os.getcwd()
