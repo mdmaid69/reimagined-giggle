e9b0c0f1e4d656ef7733ce6a71958a76cdeda163
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:34 +0000

    too shop move whatever conclusion

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+i = 0
+while i < 5:
+        print(i)
+        i += 1
