4cef81d3bc1b27d7b6c077d4c8c512c65182f943
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:48 +0000

    mind poison shoulder band legal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
