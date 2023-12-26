cb0ec2c287edad98bf769ecfdd85fc22d4887a0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:58 +0000

    suckme escape refer partner score

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def cube_number(x):
+        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
