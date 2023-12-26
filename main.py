283a8e58338f07335c9fddd1199860485a05e0f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:53 +0000

    exchange limit getoff zero seem

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
