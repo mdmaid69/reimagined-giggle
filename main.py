4fb3d9ff44b5da233321fa3e830e429dc246809d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:51:57 +0000

    underneath post length rub water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_directory_name(path):
+        return os.path.dirname(path)
