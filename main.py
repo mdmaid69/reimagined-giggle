568b1f640b48aa573176c6b9a205a0d431e6a1ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:33:50 +0000

    however cap decision face behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
