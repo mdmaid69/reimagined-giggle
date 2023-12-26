fb0cbb1272b6ba785139aeba4153304828e46067
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:05 +0000

    wild spill degree most last

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
