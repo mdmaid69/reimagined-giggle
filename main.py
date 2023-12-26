3f60d84bd0a2d05ccf531f9578297b54f36d5a6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:38 +0000

    kind crazy lonely usually hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sys
+def exit_program():
+        sys.exit()
