0f2bd2f940a399dcd7aea2a59be61b67933f542f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:21 +0000

    apart nigga Qq closed dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
