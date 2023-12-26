5f3312ff5024e26a89791d5202890b6fb91bd75f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:38 +0000

    concentrate welcome strange sentence string

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
