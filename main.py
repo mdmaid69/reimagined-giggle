6be3d4c07602e62a74d094ae679374f37d65b940
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:07 +0000

    dance responsibility properly computer your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
