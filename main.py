ab946842cc8f260e91516c1d5784c384a9f620ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:30:37 +0000

    underneath mention sense tread circle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+name = "Python"
+print("Hello,", name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
