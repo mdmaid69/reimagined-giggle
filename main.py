c90a1b2c5ee5eec513982936559c061e61709b82
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:27:10 +0000

    decide only hello surface progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
