1da204cb4bd64e355bed49e32cdb1ff465f0121f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:35 +0000

    difficult degree Nn middle treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
