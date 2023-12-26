0494e8182ae604f95613f22d711299d6baf3832f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:24 +0000

    nature winter quarter meet address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+print(time.time())
