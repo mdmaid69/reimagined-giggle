45b5296ec4e5d7fef7c52d09b116fa1d99df93b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:00 +0000

    whatever justice famous release fierce

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
