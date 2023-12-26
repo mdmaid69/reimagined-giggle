c889aaf03b1ad858ee2afde026bba497cc00489f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:56 +0000

    prefer language accordingto up uniform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
