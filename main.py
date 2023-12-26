908d221a71c600ee483ed400bce3f551714dd25f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:01 +0000

    bright difference forgive if deal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
