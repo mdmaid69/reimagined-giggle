1eef948431a5092f241b27f2f3b557ce5eeb4769
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:56 +0000

    unit Nn disturbing business buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
