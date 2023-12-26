7b2025639d5f40fba8763406051023eda5d3d966
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:16 +0000

    swallow believe because sweet last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
