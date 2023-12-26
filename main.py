2c4407ce670af61e71e11c3796a24dcebe48c188
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:12 +0000

    against plan boat wherever confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
