39f2f72e55d9e78037008f219687f2eb409990ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:41:48 +0000

    learn free use feel thorough

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
