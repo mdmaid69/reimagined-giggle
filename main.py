f9f2faa7d90510f8cff72bb6fd048d614b6ce3ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:54:51 +0000

    discover unit correction word touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
