7fdbf2890a84725a93b7d61a6419b51c5cc6571d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:30:41 +0000

    holiday their tie central exhibition

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
