c503725e24770d633975ccf40802cd8c36414df8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:47 +0000

    scream greeting near borrow spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
