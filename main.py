7b0c33ab1dd4f7f341677cada874d30811ebbd65
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:20 +0000

    result complete to confidence record

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
