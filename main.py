18e9c863b5a92e2570490178e72d947bf4460bee
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:57 +0000

    firm woman downstairs tough doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
