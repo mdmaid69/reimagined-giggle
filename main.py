f4806e2f772f7eb6fd7e591310a0a187fa061fd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:10:45 +0000

    mess yesterday paper hurt television

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
