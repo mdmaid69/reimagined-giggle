72045ad8e4cd778f14b92008dc24b3b1be5161e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:11 +0000

    successful imagine play scream part

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
