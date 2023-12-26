b9a387a45aaa6a30bd0aac6fe88d645186098b02
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:46 +0000

    question frame pen top doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
