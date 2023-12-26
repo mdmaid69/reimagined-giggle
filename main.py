2f35cf3815f9dcc6d2a4cca1634e950553794a2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:14 +0000

    touch cope 569enjoy arrangement certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
