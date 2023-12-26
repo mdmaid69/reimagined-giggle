5d7a0ff2d64a9a8c755c4088d728ec1e751e44a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:57:14 +0000

    store actual grade file almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
