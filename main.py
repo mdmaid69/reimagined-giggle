968ad596a26bfa136bc5dcace6bb089fa78b33ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:24:21 +0000

    few lead thick estimate practical

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
