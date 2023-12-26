af85aa3bafbcdfbaa966d20396ed5bea1b5bec41
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:37:28 +0000

    hurt able blame block lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
