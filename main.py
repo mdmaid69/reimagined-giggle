b910dfbbc9877ed2b75e387e5134a7546b487671
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:05:24 +0000

    level talkback nose unkind else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
