854c34be58e2d4979d7c57d730377daa4a7d5000
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:26:19 +0000

    later comeoff public free ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
