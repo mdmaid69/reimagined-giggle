559991751f07743f9ab8af8b4707fd7a811f67ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:25 +0000

    mind change wave occasional friend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import sys
-print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def exit_program():
+        sys.exit()
+def subtract_numbers(x, y):
+        return x - y
