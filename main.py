37a37e6080a98e6adf5a551c8e3cc739b1b9cd3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:33 +0000

    offer project nature common drive

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-print("Hello, world!")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
