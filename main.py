3189dfaec3daf39e7de38bfde898bc01a3ab9783
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:32:26 +0000

    community standard evening mine mess

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import platform
+def get_python_version():
+        return platform.python_version()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
