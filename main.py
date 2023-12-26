4d1b95e8d01f2c82aea6e75a827d4c16aad18b34
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:12 +0000

    war power trousers love insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-print(datetime.datetime.now())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from collections import Counter
+print(Counter("hello world"))
