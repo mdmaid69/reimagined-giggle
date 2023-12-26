ba55cb01a31ed109328cc4afb282fcc7110431ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:37:14 +0000

    great snow snow giveback recently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
