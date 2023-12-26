676162d79982868db50f607b000c7f3002a43c15
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:09:16 +0000

    pool degree load concentration lookafter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
