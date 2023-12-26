85bda1201818619c81e5348df1acad5e296a0e0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:08:04 +0000

    while getback except breakin balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import tensorflow as tf
+print(tf.__version__)
