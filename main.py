86385d7a8463b06e3b8542aec9f712ae4da53fc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:54 +0000

    concerned win rub sink about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
