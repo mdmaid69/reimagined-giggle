52a26ee80ad8b6f8bab0b1e8f0a1cf2595969477
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:20 +0000

    rubbish strong past approval appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
