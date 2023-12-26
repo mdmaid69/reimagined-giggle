1df76a46f01204284943a0b625aa2ce0017a4481
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:38:33 +0000

    night happen getaway block finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
