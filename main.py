a5ce0e5aa1ca9cdc6f1cfbe83e8431ffe844994b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:00:44 +0000

    fresh legal please stone great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import time
+print(time.time())
