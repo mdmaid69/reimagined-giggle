5543a0798eba2cc3152620b75b6f226fe7bbcf31
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:49:07 +0000

    dinner hot breakup already discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
