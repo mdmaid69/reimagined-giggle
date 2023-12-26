2a6672e19e6edde4ffeb75d1d7e9a835243fe978
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:21 +0000

    wherever band enthusiastic soon neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
