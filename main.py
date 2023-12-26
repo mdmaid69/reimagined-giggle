4444cab51ba21f1e5929500d3059f487be695fa4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:19 +0000

    honest hungry involve silent flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
