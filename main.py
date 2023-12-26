f10e993f92fd17264e2ea7e18c286ccad024aca3
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:45 +0000

    side uncertain freeze profit nice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
