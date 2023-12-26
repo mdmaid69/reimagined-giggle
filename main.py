fd1c209668ad9f5c15a917f74579037755d60fe9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:51 +0000

    keepaway wrong again ground because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
