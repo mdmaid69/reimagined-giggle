e3b47fed8b6c0c52504460c2cdcfa4b2bccf472d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:17 +0000

    accidentally tip Rr seriously backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
