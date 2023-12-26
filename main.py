1abc66299bf414beb082367bdd10b537be70e24f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:45 +0000

    absolutely burst second discuss lookafter

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
