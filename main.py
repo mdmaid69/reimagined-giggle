2dbb5a7cf801b3d41c08a3a1a269426a9e3c8b72
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:38 +0000

    thinkof wall section upset deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
