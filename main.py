f826fff34600b5d0cb85bf6773b69d1fd1700e04
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:21:51 +0000

    while alive difficulty shallow from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
