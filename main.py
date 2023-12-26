ace94ae436c50ed6c827f05677e36d3a7e3aa348
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:30 +0000

    belief announce cut fire whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
