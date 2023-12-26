8549331cd72c7723120efe0323da11079cfa99f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:46:02 +0000

    peaceful conclusion original correction nonsense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+def sort_list(lst):
+        return sorted(lst)
