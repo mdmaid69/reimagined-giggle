bbda6ba4b92eecf9302168e5486c7d2aa2b81400
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:10:22 +0000

    loan transport waste again us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
