cc41d5ff5fa5f9642ca9ae803ef4f5f4315dc486
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:37 +0000

    active poor broad relation forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_odd(n):
+        return n % 2 != 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
