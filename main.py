25ec96eb177c370b9db58e44304181aa837acdc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:02 +0000

    telephone set ear feel become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
