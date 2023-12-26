bd1562609aa261642be684f32a8e97151a4d7365
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:23 +0000

    politics free combination excellent pinch

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
