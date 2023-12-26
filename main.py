92bdfc35ff41af7afeaba1cac05ac205613898a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:59:25 +0000

    lookback(on) responsible square bottom how

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
-import platform
-def get_python_version():
-        return platform.python_version()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
