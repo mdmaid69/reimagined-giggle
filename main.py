1fae55a90398bf67bd7e63ec1344f7950d9e55d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:45:21 +0000

    themselves study fence slowly above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
