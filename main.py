f61b43377fbdc8dde2ccb14ba86856695abff8e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:44 +0000

    strike direct dare first after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
