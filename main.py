15ac7775db63b2341d4ed50875a31ab975d01695
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:08:35 +0000

    beneath disappointed object reasonable bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
