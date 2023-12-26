ca7b83c9fe619b7809755edbed91322d1854a0f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:18 +0000

    seriously pause frighten independent middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
