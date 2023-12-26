e371c124388d177cce9a9ccba8f2797e5a3e4c31
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:21 +0000

    rush where world its broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
