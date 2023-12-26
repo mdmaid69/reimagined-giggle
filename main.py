9a5ce2d97a03630c5a2dc18e8451a143087b8032
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:15 +0000

    swallow warm sticky easy capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
