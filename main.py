d51eb52becc473f1aca47f8b48e02b5460f00705
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:20 +0000

    future lookafter time consider scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
