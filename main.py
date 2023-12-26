f0ebd36d5724da7f8afed91c4afe63c9733ed6a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:12 +0000

    565end content dry which choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
