de80633fc0094fdae2595ad3168d6b6c2782db0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:42 +0000

    anxious left considering pleased clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
