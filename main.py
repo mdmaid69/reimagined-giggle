b30d88981f7f37eec7f1f5f8e273639e9296aad4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:50 +0000

    wrong unless getback alive attract

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
