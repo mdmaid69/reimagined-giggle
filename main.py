f95c0de95560d4a52ebe7f731b47df1be0744134
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:47 +0000

    whether rule trip water whom

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
