8b3086d03f68b72674cdf86177485f05d566876c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:41 +0000

    sort Ss crime so obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
