a424afe06e6c44fd34c3d1db6ef4f3b795b6dbe0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:56 +0000

    peaceful marry clothes home attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
