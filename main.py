94a15bf9466a6ae82f06a0dbd7e93bed8c621b6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:36:02 +0000

    too and sore tip really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
