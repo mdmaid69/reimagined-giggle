2a90f0cc326a81ac0df122668af8a3c9a3601346
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:17 +0000

    band letter neat knowledge demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
