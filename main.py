c3af0915faf29c6b3ab9ac386ef494f10124a425
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:51 +0000

    hardly amount part food wave

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
