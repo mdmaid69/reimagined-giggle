211edb38a2ba717151fe0041e08c76b6348f0d8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:16 +0000

    female trust it anybody meet

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
