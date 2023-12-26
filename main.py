0541cc377f1a8731a1db5d9f181c08dfce262488
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:49 +0000

    rough ocean appearance past unfair

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-print(sum(range(10)))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
