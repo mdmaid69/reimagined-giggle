ea7ff9467e7095bcb0cbea78d0da0cb22e0b858c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:10 +0000

    keepoff soil smell platform address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
