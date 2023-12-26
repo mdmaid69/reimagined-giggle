14fe235373821c94aa8ac01767c9bdb2db1af982
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:57 +0000

    solve even half window really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
