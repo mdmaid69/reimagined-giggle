a4fe905ad78bc992227c7000370fde61855a63e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:17 +0000

    history smell natural home variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
