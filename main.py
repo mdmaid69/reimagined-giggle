3f843f18ae16a8be876ec010d4924840a34c332a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:02:58 +0000

    that loud finally clue more

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+print(os.getcwd())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
