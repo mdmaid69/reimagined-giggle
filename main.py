24a269c779fd3dcb15ce8af4296d21bec9bd4d82
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:00 +0000

    runout bright urgent guide education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
