4095ccb300feccb3c4c2ba7d276c27f536407aa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:17:24 +0000

    strict calmly suppose rush butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
