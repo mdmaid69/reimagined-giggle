818e2b169277c3d081af3a16f974da6a81df2fc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:35 +0000

    offer piece fear best activity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
