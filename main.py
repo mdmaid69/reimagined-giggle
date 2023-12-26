48198675292cd489b0646180a5117fdbbd4f4170
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:53 +0000

    free street job charge basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
