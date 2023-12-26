38206db888858aa329bacfd59a956f317bbf0f0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:18 +0000

    mark consider complain switch control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
