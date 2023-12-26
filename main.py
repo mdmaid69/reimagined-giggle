0ac914ec82586119f5fa81af7d5339bad5ffa033
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:13 +0000

    use tax themselves university promise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
