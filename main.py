c1d710d805657fa2f446d4709ec19936695dfa16
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:49:07 +0000

    everywhere job grow by cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
