295d9632684f693e4765de3a2fe118edb18b4c1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:40 +0000

    organize juice only follow finally

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def find_max(lst):
-        return max(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_volume(length, width, height):
+        return length * width * height
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
