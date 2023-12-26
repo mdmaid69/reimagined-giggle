f3e5cba8e8b821bbbe120577231fd59b3e9fe1ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:14 +0000

    low open point search insist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
