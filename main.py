327bbb763c7419de7382156f81e8c89040f66629
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:09 +0000

    remember exactly mouth fear why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
