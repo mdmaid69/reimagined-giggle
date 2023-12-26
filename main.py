d7f86b1305a62c44b4d568d8af328c0024ebd565
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:47 +0000

    instead ourselves remember chance between

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+text = "Hello, world!"
+print("Words:", len(text.split()))
