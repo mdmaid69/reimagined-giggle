8d751a85a167f6961fd2cd2b64e5383a67f58abd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:20:48 +0000

    blue pull eye answer aware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
