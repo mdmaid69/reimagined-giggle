dc443b83d70e3d5eb71bd6aa6a15478d22939f42
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:49:21 +0000

    recognize accommodation certain explanation century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
