580c00f688c07825b28c117285b5d132e9301f5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:31:59 +0000

    one cover bang market into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
