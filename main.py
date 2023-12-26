6c91406ad31a786eb0a725ef767634601aade628
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:37:35 +0000

    similar finger fast question badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
