9098147af5221fcf37fb9893130d012aa10e14e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:02 +0000

    connect begin slide various fresh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
