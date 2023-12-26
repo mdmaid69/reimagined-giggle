2e952cb332441659dcc81e69998f29348aa1ccd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:52:04 +0000

    sound lift ticket door part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
