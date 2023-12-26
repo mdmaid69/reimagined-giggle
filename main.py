7f3fb4f758bf291caf07a8076e47c2cf336bb0cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:36:24 +0000

    journey environment thin demonstrate run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
