6aee418c97eca5104cd2337f52b13b3ea6662476
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:47 +0000

    instant Pp give ilovedicks regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
