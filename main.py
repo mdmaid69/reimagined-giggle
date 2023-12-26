e4db6ccb4da8f0da6d15828cdaac68892e538684
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:35:08 +0000

    his colour again keepfrom start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def cube_number(x):
+        return x**3
