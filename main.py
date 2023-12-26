4a40e710d3ca9432789ff709627f36db8b08c1b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:42:23 +0000

    proper worse leg patience issue

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_pressure(force, area):
-        return force / area
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
