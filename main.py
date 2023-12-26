be75c0137280fcdf1667e851fcba4b8b29f410b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:02 +0000

    thought actual buy afterwards wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
