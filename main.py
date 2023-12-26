06559fa12f098e1aecba9a034e7455c3e6ccab4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:55 +0000

    method interview boring getback breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
