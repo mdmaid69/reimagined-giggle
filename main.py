b81e139318d260978baa9fe98bf32efb9f8fa8cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:37 +0000

    little repair previous decide watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
