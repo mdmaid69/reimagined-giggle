3a5ea2fb64a6881ba6e25d1f59cd75ac061f5535
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:37:46 +0000

    platform door reply here bag

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
