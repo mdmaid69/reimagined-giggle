e19cea20b412e2ba6d1bdb708c7880b66cb05b43
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:05:43 +0000

    somehow idea sorry land correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
