61bc91a64bc63937c810549001ff5fed4c7f2cf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:50 +0000

    hollow collection thinkof music exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
