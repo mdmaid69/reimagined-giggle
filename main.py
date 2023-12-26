bbf294d7ef53ff4d70b5457ed49f3d9f586baae1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:20 +0000

    bottom and everywhere ashamed national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
