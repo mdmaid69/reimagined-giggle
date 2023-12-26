d05772f05c8620622ece331e2a5f452065539c4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:35 +0000

    hope yourself pressure activity bound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_hex(n):
-        return hex(n)
+import sys
+def exit_program():
+        sys.exit()
+  def remove_duplicates(lst):
+        return list(set(lst))
