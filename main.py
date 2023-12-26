9fe1d3f66037924ff951d668c760739077aeb3b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:17:17 +0000

    honest sing lot small instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
