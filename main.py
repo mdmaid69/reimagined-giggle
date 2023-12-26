30f8e9b6e1751022ec6121b77888cd35235ae382
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:36 +0000

    continue card down could city

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_speed(distance, time):
+        return distance / time
