02c994aa1ee01fe25784b570aad8c29fa8ac7293
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:34 +0000

    sign set goodbye strange underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
