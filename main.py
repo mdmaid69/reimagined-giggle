a7f8c24e37065d06fcf2ccd588ed60dc31589689
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:16 +0000

    container wave lot store deal

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
