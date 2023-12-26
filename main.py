e0ed9f3854cc61e61d7329d4a9916c1fdc398d00
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:39 +0000

    trust trade thick because considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
