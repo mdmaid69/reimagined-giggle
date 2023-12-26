31b59ca832e8bd71ba87b8ef3dfaf2922b88612f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:53 +0000

    group tune health downstairs spread

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
