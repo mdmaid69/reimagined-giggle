2448355d0f09625faae04587681f07c39aae3893
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:50 +0000

    play delight plate sleep today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
