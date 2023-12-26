cb66dbffa9696c7230b70b3183405dfdd0ce97da
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:58 +0000

    equal surprise expression recommend but

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
