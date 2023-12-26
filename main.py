6a88d3090938971b5133c2144ce6002fa0571f03
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:40 +0000

    union picture especially turnover long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
