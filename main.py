017509a8bf19c7d36548a9fcdc306c9f8747ee58
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:30 +0000

    regional occasion wear smile fair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
