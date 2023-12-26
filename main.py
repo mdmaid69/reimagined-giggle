ae04952a30909ef3f398987fba448279d808fab5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:52:29 +0000

    regularly build deliver mainly convincing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
