edb33db2db9827dfdb0f9261188bef3178a33f63
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:46:12 +0000

    forget mother 567enemy shout national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
