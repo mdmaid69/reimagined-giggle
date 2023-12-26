9448173b4722124559f0c0ebabaff27282857633
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:49:43 +0000

    boil change add rich comeback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
