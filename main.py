5456c71ebef5b11498c932565d8dacebcacb76c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:22 +0000

    clear reject nothing rent whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
