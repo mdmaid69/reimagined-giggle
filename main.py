03d3b28d70c38a58c438eec042bd0105292de180
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:24 +0000

    allow report communicate sea store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
