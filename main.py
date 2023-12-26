688a1db902605a40eb1a01278e2489c357e37a95
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:03:06 +0000

    570enjoyable tap little go concerning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
