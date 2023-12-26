70a47350c7b6bc0c2ba39afb97cf0bad2febd594
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:38 +0000

    storm throughout temperature hurt force

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
