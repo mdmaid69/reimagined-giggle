b038f375e83a36386adda7311e81dabad5cfce50
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:24 +0000

    form hide tread left get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
