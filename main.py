0bedb5e8e6d6c9d9cbcf1f4ecd479456956501be
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:34:40 +0000

    line ours keepup offence square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
