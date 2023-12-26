08f9f39b12878e711c99b429717a3c06ebadcf16
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:55 +0000

    combination read dress workupto little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
