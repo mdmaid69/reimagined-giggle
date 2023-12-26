268744631c480bfc58c009f1d9f1d7f8e2c6e5f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:22 +0000

    join throw cheap criticism giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
