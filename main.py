0a112f2fb00b56ffd1c623fd5dbc143763a62faf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:20 +0000

    apart priority mainly name drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
