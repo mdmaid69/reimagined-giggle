181e53f0b81a2cebd4ae0287570fc97bf1434930
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:00:47 +0000

    floor instruction right opponent throughout

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
