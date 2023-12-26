27d91816988855720bf6241e6e12306dc6323d0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:27 +0000

    attempt ignore certain concentration tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
