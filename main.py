35303e7542a4a264790a94481b3912af0bdfa520
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:42 +0000

    squeeze each strike birthday tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
