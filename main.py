170d5420abdfb13d17898dbe63fd5a750607993a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:22:04 +0000

    debt excellent funny respect food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
