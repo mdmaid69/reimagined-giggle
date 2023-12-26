a497f8c21c978371c3530c535e76334a1816ec3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:35:22 +0000

    confident heart normal meal public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_bool(array):
+        return bool(array)
