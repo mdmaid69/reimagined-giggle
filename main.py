5f60202af993cd45b84ac3613d5edff956b741db
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:14 +0000

    waste niggaacademy live matter bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
