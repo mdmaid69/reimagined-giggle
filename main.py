86a56f1767ba6223e1c7a25c02053ec1fc168e11
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:04 +0000

    dear ours disturb least poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
