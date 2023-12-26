ae697cba14c14ef232382330a909bedf3254007e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:30 +0000

    manner regard tone imagination spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
