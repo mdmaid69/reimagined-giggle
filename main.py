a57acd9ec44f56aa71b2933dd6aa7f8b3bc52e22
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:27:42 +0000

    corner rest road with valuable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def count_elements(lst):
-        return len(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def reverse_string(s):
+        return s[::-1]
