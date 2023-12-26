2447b873046cb2e16c3c4d8d6ba47e6eb2627627
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:06:08 +0000

    neck stiff effort alive race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def calculate_area_circle(r):
+        return 3.14 * r**2
