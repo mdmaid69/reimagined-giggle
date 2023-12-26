d60ffbc6e31295ef209106c7dbc8354730e9c0cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:10 +0000

    would think clothes recover chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import sys
+print(sys.version)
