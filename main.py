1280713df95f4fc61418b8448fcc893e48dc4def
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:37 +0000

    great greeting song tie occasion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import tensorflow as tf
+print(tf.__version__)
