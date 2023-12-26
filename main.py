bb69fa745edb9ee4281f5fcd04cfdb9a9b8d4f55
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:07 +0000

    old right separate mainly meaning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
