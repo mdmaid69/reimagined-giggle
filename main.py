5e29dede81c991deb9fe20138ac21281268de758
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:47:47 +0000

    borrow dick profit any yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import numpy as np
+print(np.array([1, 2, 3]))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
