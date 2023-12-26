8ffbd6215bf3d669af5a7ff942df94904dc60fe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:55 +0000

    somebody talkoutof definite favourite form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
