acc7947d85a0cb11a499f75cb95baaca16bd1af5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:49 +0000

    man rise angry shut regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-from collections import Counter
-print(Counter("hello world"))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
