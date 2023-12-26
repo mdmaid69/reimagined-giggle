3b32bab1c6b773a48baf0233d7ef50e75f84cbab
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:58:41 +0000

    breakin argument mouth listen where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import json
+print(json.dumps({"name": "John", "age": 30}))
