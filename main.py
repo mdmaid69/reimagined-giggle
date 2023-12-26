c5d2fb1d3572c55302c3804fafd4578ac96024ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:56 +0000

    advice without accident flat taste

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def count_elements(lst):
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def count_elements(lst):
         return len(lst)
