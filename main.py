13f118c4970f9abe644faa763e36544db5bab7cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:32:02 +0000

    whistle dress joke excited since

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-x = 10
-y = 20
-print("Sum:", x + y)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
