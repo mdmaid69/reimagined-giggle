19d13bbde38890a734dff2c55c82bf1df7247f1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:48 +0000

    hard everywhere variety goup uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
