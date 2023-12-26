3a8ee0a4d38c93d6c343d8bb3d76df27fef5be9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:21:57 +0000

    not several young replace talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
