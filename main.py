7a82111cddcc4a88c7af2048b841963d51c3168a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:44:19 +0000

    special let difference visit excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
