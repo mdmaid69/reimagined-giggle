77ebda32ca6492181ef2abff563829ce842b776d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:52 +0000

    recover soft sign collect hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
