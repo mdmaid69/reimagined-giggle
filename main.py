3ef533d315b7d87f552f4e70bda8bac8a692a590
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:23:52 +0000

    off direct wait shop distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import time
+print(time.time())
