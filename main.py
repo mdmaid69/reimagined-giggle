ed4dc1a05b131877906612dc58172b26726ac794
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:06 +0000

    one crowded recovery backwards national

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
