7b5053257720d71ac2db9cc487efc093d5ac2234
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:31:23 +0000

    funny lean account everybody content

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
