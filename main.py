7ba50b77b53eb8669ec0d73394f1d535b570c933
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:22:58 +0000

    send permission realize comeout easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
