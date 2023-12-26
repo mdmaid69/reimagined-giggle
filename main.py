8685d82c1a8abfe90ded71516b603d8500b8c28b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:42 +0000

    dead uncomfortable along both case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
