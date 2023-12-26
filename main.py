efdec0c51414d337b814d8457dd0146559676eee
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:33:30 +0000

    wonder worse sticky dry university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
