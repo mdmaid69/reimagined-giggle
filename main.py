f47183ad71607efb2f2dd1a39c634d50045389ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:45:07 +0000

    front remove figure room protect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
