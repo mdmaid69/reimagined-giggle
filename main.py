8067c58fee3b1b6dd1ef131dcdf9bb0d73d899bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:43:37 +0000

    attitude radio estimate mess rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
