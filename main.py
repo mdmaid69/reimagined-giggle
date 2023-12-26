cb7c51eecb11a180ecb756a2112bdb921766a131
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:33 +0000

    want Ff lose steam tap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
