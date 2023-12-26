dc189d1c0889f7d7ef25d0db8b83e1bc37d8f7f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:49 +0000

    first brush finally remark attitude

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
