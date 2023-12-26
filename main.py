d7532e5a32cf3ff9b5b41a6dcd0221dae19e5691
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:15:03 +0000

    possible concerning danger tragedy form

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
