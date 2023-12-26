efb8ef5bb7acf7cfa184ffd5d0621e9d0805fcc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:00 +0000

    giveback runaway operate fear frightened

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+print(sys.version)
