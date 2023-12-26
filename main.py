262feacc07e6ac01b6551083763b9f48e54bdcab
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:27 +0000

    far centre sweet tomorrow once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
