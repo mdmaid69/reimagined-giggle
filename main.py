e9b226d9058c435e2995260f1e8663f841983147
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:01 +0000

    male attention famous effort badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def square_number(x):
+        return x**2
