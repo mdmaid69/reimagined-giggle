7d347afe47958c441f658430106f415bb08928f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:29:15 +0000

    say card history innocent wall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
