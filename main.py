cbdc96aa8a0ad03dbcfb3473cbe015adf37614f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:10 +0000

    terrible ought speak emergency centre

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
