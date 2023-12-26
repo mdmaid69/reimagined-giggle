a7d08c9581c4e943b65c6fafd1c1796b7b605e05
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:50 +0000

    common sail content flat conduct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
