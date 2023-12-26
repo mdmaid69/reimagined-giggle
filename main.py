53c6c257013888028a8ccdc2b4c8eaab7832e79b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:03 +0000

    570enjoyable fall equal pity smoke

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import random
+def roll_die():
+        return random.randint(1, 6)
