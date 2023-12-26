35a2d206b334f4b38fad82ab6c1581ca449329af
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:55 +0000

    production fond earth movement willing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
