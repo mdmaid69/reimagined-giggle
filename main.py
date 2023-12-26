1a7db1bf2bce031696f9f73e19edafcad88bdb2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:15 +0000

    concentrate question reason stretch simple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(a, b):
+        return a + b
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
