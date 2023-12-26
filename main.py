82cf09a4413d01bb0af9ab5b712d4f60441efcd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:31 +0000

    so sorry beneath nor pure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
