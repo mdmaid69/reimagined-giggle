e758275c88c64c4f09af75f9d923ff2eaea91312
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:18 +0000

    matter passenger store occasional accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
