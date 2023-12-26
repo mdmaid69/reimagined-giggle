ec629fce29c1d2d31ee3731e468c4ddd40ad2c9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:13:45 +0000

    instead opponent hole pour exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
