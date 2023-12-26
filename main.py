437f3c2649a4512d6bc2a8dbd2ce21f8001ab737
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:15 +0000

    square look beside country let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
