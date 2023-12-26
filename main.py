633e9b7ca0c4e561fc54b21de436e1709912afa4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:20:26 +0000

    at sing body pretend hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
