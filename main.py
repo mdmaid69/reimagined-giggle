b9fc2aa59dba4c34078165c55493ee9d84f257c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:41 +0000

    yes once aware mysterious nose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
