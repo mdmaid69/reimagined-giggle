d5374bb2c9885036621e08227f0b4e88f51762a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:34:26 +0000

    after community succeed thorough time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
