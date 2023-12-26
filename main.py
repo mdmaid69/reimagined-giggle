4d17a34867c038db799bccffd5815b3c708f27ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:50 +0000

    trick admire on unusual blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
