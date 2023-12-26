c0447334114a80262f22de0a7e1cfb325c56bf95
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:28:36 +0000

    muscle common move plan middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
