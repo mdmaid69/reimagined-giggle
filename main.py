c0978e106b12a183bcda544553105362dcb0e4ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:09:09 +0000

    all lower steal easy fly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
