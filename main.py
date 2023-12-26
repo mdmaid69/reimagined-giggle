335a23d42ed810e433a9291ab523aaab8f21c82b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:05 +0000

    interfere matter hospital become early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
