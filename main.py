3b716fe0bad9b4f8489b217be10cfa68661e0a22
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:11:09 +0000

    travel there about size date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
