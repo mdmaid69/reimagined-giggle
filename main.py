673b3c708010d53569e963db5dcca0e48506ba02
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:49 +0000

    remark protection thought wheel nervous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
