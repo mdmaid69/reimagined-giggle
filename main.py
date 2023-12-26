7609093d35b507dea87d5dfd47426d56e8b01ea9
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:00 +0000

    fast acceptable temper criticism system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def multiply_numbers(x, y):
+        return x * y
