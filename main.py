0eb522669f3cc517c50a135e929ad1312c243c59
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:35:35 +0000

    weigh watch software see callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
