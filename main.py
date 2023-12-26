eed4920282ffe3e956bdb45bdcb0249d1e17d4b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:37:59 +0000

    along commit hide bored dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
