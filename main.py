44e51d47a3920725acc9784d114a6c40242a7371
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:13:44 +0000

    great keepup still bear friendly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
