c2babeaac3de25e3f50c950b383e8a8f39653cef
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:49 +0000

    feature 568energy mind container mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
