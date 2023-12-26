51119ee110c8fddaa25aa88f1279fabc04fec7da
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:07 +0000

    check moral top plenty solve

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
