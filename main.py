b14146086cec8bcb8c5dc2f27078ae72c7d58582
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:27 +0000

    approve train yes world swing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_set(array):
+        return set(array)
+print(sum(range(10)))
