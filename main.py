d29f237fa5351b5f2c8de8b9fa347419ec93bb23
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:16 +0000

    pot suggestion aware loss trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
