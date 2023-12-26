c1d8541f4f17c0d5fbbcb44dc3fb94d5b30db48d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:28:58 +0000

    open fail terrible some steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_min(lst):
+        return min(lst)
