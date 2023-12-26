b2275d0f148c7179b821d952c91782d3dab300d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:09 +0000

    upset tool instead way unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def square_number(x):
-        return x**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def is_even(n):
+        return n % 2 == 0
