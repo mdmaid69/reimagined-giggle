6f1ee218249b5624fe0a1e86830cd4e7e0409c20
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:35:41 +0000

    pair dish smile pen trap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
