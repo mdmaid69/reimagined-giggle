3db99533122318cc3ea9246142cf056f6d53b00b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:45 +0000

    form now bear band way

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_work(force, distance):
-        return force * distance
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
