84c696138580bac6f2cdaec76e92b6c078a08eed
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:55 +0000

    wide married urgent chat doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
