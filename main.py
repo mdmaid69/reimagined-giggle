480d4faaf2680b506ca380ae497b670c37718b8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:28 +0000

    day dangerous flavour tidy intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def divide_numbers(x, y):
+        return x / y
