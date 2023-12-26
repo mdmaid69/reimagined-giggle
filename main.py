1646b3e4f17026b2ebcd22ff49698f7b65a2b981
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:04 +0000

    house operation might path makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
