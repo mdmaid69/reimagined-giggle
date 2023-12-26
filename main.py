d5e8e21ed2be83b5ca983310e95d37c6c71a9147
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:21:22 +0000

    wise cruel goaway spray cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
