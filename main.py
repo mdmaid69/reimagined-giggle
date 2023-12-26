67c5f3e4358dce9ef94ab664a29732843ecdd14d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:26 +0000

    defeat admire load risk detail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
