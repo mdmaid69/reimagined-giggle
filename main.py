a10311cfb39b0848dde95be5a9c80b7109adde65
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:28:11 +0000

    comeout stand hurt represent leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
