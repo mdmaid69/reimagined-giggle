e190f4eea3dc8210ec75eea4a53d1205fe1b9e75
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:49 +0000

    expensive middle smile dream peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
