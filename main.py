0911b349c018edeba0892d9f48dd5448a13f5dd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:06 +0000

    problem mad manage important twist

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def cube_number(x):
-        return x**3
+def get_array_as_list(array):
+        return list(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
