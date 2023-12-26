afcf59609871db5eeab5e32f14935c123854a982
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:04 +0000

    apparently train but slowly earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def reverse_list(lst):
+        return lst[::-1]
