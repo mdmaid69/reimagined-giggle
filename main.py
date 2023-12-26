6372246fff8f8155f9ab7ba5ad05544add8dd6d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:18:27 +0000

    true round worry long birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def change_working_directory(path):
+        os.chdir(path)
