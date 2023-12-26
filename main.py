ea8519787e7dfa39696e54c95103afaf817eb979
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:19:12 +0000

    care size day string recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+print(sys.version)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
