ebc44ceaaa1afc70ad48daa975a1e2a7367fc877
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:49 +0000

    shop especially cause born probably

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
