ddb1c6af71510e247014e2d295e674141998266d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:34 +0000

    179bite trust drink correct fat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
