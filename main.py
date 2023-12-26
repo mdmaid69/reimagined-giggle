92647e5b795f421ea1a6d3805026924f282bbd49
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:40:14 +0000

    side it impossible cope 571enough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
