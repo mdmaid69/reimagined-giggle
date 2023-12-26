043fca81f21f0a5768c1812affa4e6ebfee30f4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:27 +0000

    quit yes advise example introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
