8173764a9f05e453ccf8b29543f67a0b54dbceec
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:05:58 +0000

    prevent bear building by information

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def divide_numbers(x, y):
-        return x / y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
