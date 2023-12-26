63d44a76b0612089e99b4e2796628f6fbf089903
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:39 +0000

    bridge off produce yesterday exhibition

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def convert_to_binary(n):
-        return bin(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_str(array):
+        return str(array)
