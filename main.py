e7ab8673d7f9d2df6a1a7e5e035ee655bcb8afc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:02 +0000

    plastic note square corner after

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-from collections import Counter
-print(Counter("hello world"))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
