e7428c3ff6c1f0ae8d539f27c801fb9e400284a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:29 +0000

    steep afraid edge science unit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
