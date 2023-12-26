8480d45f782684c70b647fe6a19dc4ca86ab9d87
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:46 +0000

    support middle ourselves somehow someone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
