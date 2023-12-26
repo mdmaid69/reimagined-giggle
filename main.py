339e8facf9da7cd09c040b479ce1b85355814952
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:27:50 +0000

    sorry market effort easy Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
