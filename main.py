3ef118f72940a5b3a6dca36a486ecb80f5d55e4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:19:43 +0000

    whoever neither likely listen television

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
