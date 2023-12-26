79da241bb2a128a1285a7ce1672c7496b71ace09
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:34 +0000

    callup thankyou finger reply minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
