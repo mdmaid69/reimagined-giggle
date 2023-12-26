cff36a57a4fa8dbdf92c73b60724fdd25249a606
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:59 +0000

    exercise continue region naturally quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def convert_to_binary(n):
+        return bin(n)
