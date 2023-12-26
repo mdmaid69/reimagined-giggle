961b9c3368f397e7860ffed4c6ea4931fac35712
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:31 +0000

    Itself closely proper hide though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
 import math
-print(math.pi)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
