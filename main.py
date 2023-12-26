cc62b289cc79eff26fda245de28b54f7e144bf65
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:48 +0000

    without wild hair plenty trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_typecode(array):
+        return array.typecode
