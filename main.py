49db5f0b7b238100235e99a438ddcfb6f2d88d13
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:21 +0000

    funny big quiet teach weak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
