61366fbe75598d43eb67f70bc6a0eb2d3a226f29
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:02 +0000

    tell funny degree soak movement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
