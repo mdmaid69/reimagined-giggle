05a6f1ecfd1112d326016c7e62770dd48cfad326
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:42 +0000

    waste calmly Nn loose instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
