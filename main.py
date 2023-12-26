106119fe5e15b31aa5ca178acb77ce8257bbbaba
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:24:11 +0000

    present lesson capital up I

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
