ce5f1c8569761aa63511ef2686188d7ad9e7f274
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:11 +0000

    score thankyou put talk remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
