c0a15adefb56b89dddc9f6fbff5db96f99027e83
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:26 +0000

    importance make delight nonsense side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
