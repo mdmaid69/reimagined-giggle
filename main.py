77426dfda2fef3af41bdf2483587b54d58eb0896
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:16 +0000

    combine important unkind scale impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
