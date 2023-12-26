1757e6198e5977da1fd38f691673c6fb23a4d3cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:35 +0000

    prefer will reduction congratulate almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
