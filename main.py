cfc824ac30555bf1e98a6c635f3f9348ea9cef05
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:21 +0000

    down usually punishment trousers full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
