3985c3fec908860f29370655085b3fc70096743d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:32 +0000

    top problem visit live real

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def convert_to_octal(n):
+        return oct(n)
