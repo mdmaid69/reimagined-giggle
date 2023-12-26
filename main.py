47d3dfed0294a466606b8abc714771768bf10071
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:30:50 +0000

    luck satisfied century contact lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
