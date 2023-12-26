1b3e288901aa43f8677b8a78f787c1e9fd8a861c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:20 +0000

    impressive opponent combination hurry female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
