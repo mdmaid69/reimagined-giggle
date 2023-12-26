2758b630f2ba120dd979af6ee8eea328055b77b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:08 +0000

    criticism target low permission no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
