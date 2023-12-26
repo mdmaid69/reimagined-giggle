b21219355662226e0dfa9c5e7d94b548613ddebe
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:40 +0000

    pencil whatever difficulty mean ball

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
