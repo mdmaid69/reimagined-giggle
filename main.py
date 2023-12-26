eb105d2e42c0ac355448e74804adc0e0f2640d9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:27:33 +0000

    decision attempt plan proper most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
