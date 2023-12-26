2d304d3d5867d90056fa90f64ed22d5c768c9418
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:54 +0000

    may manner medium rest worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
