1bc1b608ce516f9d727607e63caca43927712826
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:58 +0000

    usual spill bear mother present

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
