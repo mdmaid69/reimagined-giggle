d26dcd7e358faf11db85d6d5b5e574a54247cea2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:49:13 +0000

    load negative imagination alone pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
