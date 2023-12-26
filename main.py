149a4731f939fc479b59ef305e239c0542c33f48
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:45 +0000

    declare part tip ear steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def divide_numbers(x, y):
+        return x / y
