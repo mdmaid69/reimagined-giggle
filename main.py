108fb66f9128362afa52afa07e7facc3afd28844
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:27 +0000

    young recommend character later operation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
