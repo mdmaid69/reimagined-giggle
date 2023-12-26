a2ead2e20c612748fe094c78d7ce3e7b66409ca5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:00:01 +0000

    smell before clue pass conversation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
