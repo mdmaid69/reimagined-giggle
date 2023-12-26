1375b37abd86e340d55c2e27d26262bb1fb8b23b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:23:42 +0000

    cool position rain connected feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def find_max(lst):
-        return max(lst)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
