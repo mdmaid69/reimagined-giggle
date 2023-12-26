61fad61b966b578585226c89a595eb27fd8044dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:04 +0000

    lift relation fetch poison breathe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
