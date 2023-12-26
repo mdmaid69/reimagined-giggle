684cf49cd00f91b1e5950c45e89ea6128aa847e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:11 +0000

    basically wheel ask evidence guess

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def count_elements(lst):
+        return len(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
