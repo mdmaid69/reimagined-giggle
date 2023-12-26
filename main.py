8deb74852fc10357e0aa856397ce56deedb5c862
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:04 +0000

    mind attempt certain minority table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
