955e0bdeee99581cf75bda8ea95b776e36b8097b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:24 +0000

    over content crowd tend influence

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-print([x**2 for x in range(10)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
