097471eff5a21abf3d246ca60e87dd157442e6b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:18 +0000

    before it computer score against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
