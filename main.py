60cc37fc2fe905397530bd19505a31c4cd39d84a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:52 +0000

    try whether must people bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
