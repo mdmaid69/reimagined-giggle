4315e2bae5604b2788bade2f6c606dbc495616ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:42 +0000

    direct find heavy pick dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
