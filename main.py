16da385a74ec979e54ddfa90b9cd0bebf83bdaca
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:46:05 +0000

    vanish distance soon this book

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
