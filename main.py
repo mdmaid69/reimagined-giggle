be550f094cdcbe8ac518a0d5e85eccd83d84f4aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:34 +0000

    anywhere bend dare thorough well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
