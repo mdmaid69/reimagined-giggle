9265c8fe04d8627814a26167b1688774393645d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:13 +0000

    century bother sure upset charge

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
