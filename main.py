c924f7fff4abd6c9e25ea9163b00602667d1162c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:33 +0000

    instrument except whenever runinto keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
