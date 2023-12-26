c69cc19b418befad55df576265ca4f2f7eb50f1f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:20 +0000

    brave result hot watch food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
