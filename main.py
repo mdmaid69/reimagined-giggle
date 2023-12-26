fbcb5869bb7b2d67485f27a6210fbd1e2077b6f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:44:10 +0000

    office everybody bit sense window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Reversed:", text[::-1])
