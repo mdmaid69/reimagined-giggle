8cd70a731a7dc7ae106be2c0f0fd9b8ace55981f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:19 +0000

    tend rain scared 566end dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
