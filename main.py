17082543f137d1dedbd96b37b1983de2a97d918e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:59 +0000

    height slightly best clever last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
