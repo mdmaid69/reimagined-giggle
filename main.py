f2f4f15df0d2f5d85d1d4a2001b30750e81997ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:29 +0000

    total equal male safety repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def sort_list(lst):
-        return sorted(lst)
+  def get_base_name(path):
+        return os.path.basename(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
