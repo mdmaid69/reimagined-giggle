7116610a7dcb6ceac1f80b6ee20dc8654677ca33
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:08 +0000

    season scene save to reject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
