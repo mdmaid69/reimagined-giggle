07babf79cad8fc4256c0bd01e6854419c059bb38
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:01 +0000

    duty stare anybody experiment flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
