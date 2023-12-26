8be49e78d380268388594a6a610a67e5b96da392
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:16 +0000

    shop natural pride loud modern

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
