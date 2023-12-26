4fbb9e53c18adc15a994651abc581ce2c410a7b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:58 +0000

    apologizeapologise before operate comeround hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
