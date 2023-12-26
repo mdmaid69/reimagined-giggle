9fc8169d0ce4b27adce4ea923684340b94547cbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:46 +0000

    temperature persuade trouble 570enjoyable broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
