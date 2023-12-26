74c91a33a2c6d49df2124f58e58c8fd7248481aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:31 +0000

    design earth corner live attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
