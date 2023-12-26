ac79b8ab73bf4ec292a5eaf7b8a84f9230f6612d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:06 +0000

    reliable system exploration square design

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
