8bb555cb81cfba7098d61be234ed9f1b950fd874
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:56:52 +0000

    tie close dish broken hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
