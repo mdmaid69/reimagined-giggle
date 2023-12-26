d2b30f4f4a910d5602773fef4a649ae8eeca65d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:39 +0000

    operation wild scrape oil close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
