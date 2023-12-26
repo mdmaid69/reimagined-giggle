b6b32eed54b800b5c733d8f7b4941e0366dce48a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:59 +0000

    press tight Pp comfortable whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
