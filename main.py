92446cf2b789b8c719a651608194ce4b72fe5660
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:47 +0000

    company future send maximum upstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def cube_number(x):
+        return x**3
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
