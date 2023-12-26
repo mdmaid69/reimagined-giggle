290fbb696041c095f49b8b52aea30cd493e989dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:18 +0000

    across wing prove getback unable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
