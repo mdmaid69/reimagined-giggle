3c72db7fe6717ef121f7ac55b0e6a2b87b1ae98b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:48 +0000

    reasonable burst keep dare once

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
