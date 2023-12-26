fdd42b142b4346d4f3ef695b02db490fc12e0708
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:53 +0000

    quiet comfort butter against simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
