d0278495c5412790b8dc44bc6d8f6ba7e6181412
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:13:08 +0000

    whichever fashion wealth phone calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
