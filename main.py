2637e066a94e57e7c614d597b71f8b2f2c070a92
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:12:52 +0000

    suit Ll strict hurt crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+def add_numbers(x, y):
+        return x + y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
