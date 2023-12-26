12de45d284fe8a6b1e7fd7b676fc30d00f43e770
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:37 +0000

    prevent niggaacademy without copy curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
