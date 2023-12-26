928339c4a6777916a6a0ba2732cb172692c307d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:02 +0000

    tell speech claim me bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_set(array):
+        return set(array)
