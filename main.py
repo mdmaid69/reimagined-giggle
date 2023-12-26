b2163025b1eab21496f0c8baafc0275694de1598
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:45 +0000

    treat keepaway return war concern

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
