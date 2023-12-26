dc324188d7b73a5b09aa1783b8003ca44e96adab
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:51 +0000

    content majority they task female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
