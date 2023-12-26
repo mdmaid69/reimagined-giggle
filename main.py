ceb8068ed1918c54a280afb7e923a68bee355142
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:57:20 +0000

    last calm tool interruption bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
