d19a4131757f4c598dabb7920fe299a87dec501b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:23 +0000

    emphasize mine everything win plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
