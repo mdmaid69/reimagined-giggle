148f1a8389d1784ca1e979fd4d39bb5db02f310c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:29 +0000

    discovery explanation improvement slip Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
