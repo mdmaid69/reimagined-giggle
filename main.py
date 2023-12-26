d15477ab0383f977590b8574653a1b334fb5c673
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:03 +0000

    belong careful target everywhere wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def append_to_array(array, item):
+        array.append(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
