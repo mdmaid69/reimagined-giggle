86b13a4b2be6a85014512026b9b73c84b40ece99
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:15:31 +0000

    lie flavour sleep about characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
