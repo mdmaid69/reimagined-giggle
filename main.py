cc2e889798a7725b2cbfe7b4c41f61c9c0631b90
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:20:37 +0000

    goaway hurt method nice disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item(array, i):
+        return array[i]
