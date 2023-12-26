cb4beef600921b6616ace1916da0c6be40d62fa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:01 +0000

    taste love small tax maybe

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_repr(array):
+        return repr(array)
