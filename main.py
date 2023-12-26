c15312fef432ac8fdc99113b15a08eff21493fe6
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:19 +0000

    but otherwise 572enter transport accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
