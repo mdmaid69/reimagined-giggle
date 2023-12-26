4c7e53c80bcc915aa8a17565669d24e6c9205f22
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:55:50 +0000

    every valuable boat used weigh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
