3e4381bea2c8aa247c3d9c24c6dc89d7996da9a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:46 +0000

    promise find complete service arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_item(array, i):
+        return array[i]
