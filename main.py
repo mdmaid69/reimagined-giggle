c45eab0a87199254c28e0763852d2a42cd0f3610
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:17:38 +0000

    gentle bill such flow store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
