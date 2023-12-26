50f43c0fa322775338839d7320a808cf8d020404
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:27 +0000

    tip reply opinion meal touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
