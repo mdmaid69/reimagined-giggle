9eeba493f69e1fd7c9d699f35155a0b601d9816d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:45 +0000

    fear here laugh pure exactly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
