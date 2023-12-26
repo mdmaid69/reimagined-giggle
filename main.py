0f1aaae7b3f587da404f9245564e910f3d6b6b8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:50:01 +0000

    press admit hard friendly escape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
