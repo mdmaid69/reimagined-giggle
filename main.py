40de17db86a125746551f95d95073947fb01c891
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:13 +0000

    stop cry flavour around maybe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
