a0087625154edb8caa75185435f88240c6b1ca02
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:04:43 +0000

    cheap we into standard convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
