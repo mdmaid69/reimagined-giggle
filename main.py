90f3a9c26b20bfbdfe29183a43f7cbe11347d608
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:04 +0000

    whom give demand period software

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_str(array):
+        return str(array)
+def sort_list(lst):
+        return sorted(lst)
