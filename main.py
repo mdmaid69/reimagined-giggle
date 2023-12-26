ba0c5d4a4082b9c2981b8e43f3f38e626fb2c433
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:03:53 +0000

    cut drink depend mind print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
