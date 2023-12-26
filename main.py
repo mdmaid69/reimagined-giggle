1724aef680263ba7b46b6db0c33a1e4ab1f9802b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:19 +0000

    basic come sail deserve judge

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
