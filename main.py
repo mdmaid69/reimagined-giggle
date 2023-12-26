5fcdc20efa84833cc7bd1b13b2629ae3420f8a46
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:47 +0000

    experience open suit mind suppose

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
