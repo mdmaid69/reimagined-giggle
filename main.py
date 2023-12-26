6f1637fbba6fc8d22cb9444119754adc728754a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:45 +0000

    confident sink mistake transport continue

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
