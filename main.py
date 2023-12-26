65012f3697032cb75f19f7df84e9c2bd7e86f218
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:17 +0000

    bringforward passage oil hard sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
