b24a3fb48cfe8b7f5d455774d723f71798c8d41f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:43:36 +0000

    unlikely branch relax set big

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
