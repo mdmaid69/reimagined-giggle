f01985ceb8a8400fc62be3b618bdaf1e9e0b60fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:40:21 +0000

    manage enthusiastic opinion 572enter flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
