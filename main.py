383f0bbb23133d37748e8a036083c2f1f4c90352
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:15 +0000

    become better tradition legal length

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
