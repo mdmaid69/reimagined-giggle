efd6f2ed91cc112d05b02e0edb17ef8b9ec31e00
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:19 +0000

    common physical front heat tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
