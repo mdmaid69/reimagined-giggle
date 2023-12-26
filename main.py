a09b2445a91100bdce21c9494390b38a1c951678
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:59 +0000

    fly confused whenever expect Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-print(os.getcwd())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
