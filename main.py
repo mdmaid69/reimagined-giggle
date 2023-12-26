926a57e431b797381cc98a2490e7cb00c123e21a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:09:19 +0000

    hit include bush centre whoever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-name = "Python"
-print("Hello,", name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
