6e5d05ec3e04fc4eb5ab86bbe286b1d10d3a132f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:47 +0000

    behind spare achievement answer fit

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_current_working_directory():
+        return os.getcwd()
