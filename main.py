3cf4f6e0e6ccbe7b2c6666f443f6f68b64419846
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:43 +0000

    respect check reply perform complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_array_itemsize(array):
+        return array.itemsize
