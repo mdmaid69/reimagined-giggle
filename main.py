9dffd4f1ec6451690f5fbbfbd11df28974c4ed8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:51 +0000

    across trade shout injury himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
