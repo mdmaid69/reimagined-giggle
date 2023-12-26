5cd7549ceb76bc4d2684a71b3cb2a640321f6e7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:31 +0000

    leg talkoutof meeting refuse in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
