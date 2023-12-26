1a039c7010cfc268d889df324e8c9a4e8d2e76cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:00 +0000

    order whether far switch takeoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def get_current_working_directory():
+        return os.getcwd()
