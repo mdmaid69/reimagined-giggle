019fc1f2ee4e3899d4ea11bc6a556ecf704d84cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:39 +0000

    myself breathe sentence sit fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
