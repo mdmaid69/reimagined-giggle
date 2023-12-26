11b80a0fb9b489befef8d0db0ae4683938c14b93
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:58 +0000

    play chin next children most

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
