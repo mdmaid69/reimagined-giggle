92c823627b9fbe4cf6e39656974a9200100bfc24
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:19 +0000

    discover contents people goup turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_current_working_directory():
+        return os.getcwd()
