459a796c542b9dea0356cd9aa45f22f5b25e1495
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:08:50 +0000

    pot time large unless quiet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
