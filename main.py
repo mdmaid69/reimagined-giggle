c8a212d98c8e1134469041e50bf282bfedaffd9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:10:23 +0000

    think giveout unless inside report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def roll_die():
+        return random.randint(1, 6)
