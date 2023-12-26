f293ef4c76de7b9a8e24f8dfb5889af176d16e26
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:09 +0000

    even arm behave gradually rich

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
