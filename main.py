d995d4e74a9b39dedd08a02d623aa88547a03b23
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:20 +0000

    either familiar about approve prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
