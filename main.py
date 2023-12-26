5f78b5b3b1b4c6a91aa1ff5f2f342729a73969aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:52:19 +0000

    force limit exit below check

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
