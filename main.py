72ee336f004a2b28a691b31ce43ca4ca77f25b0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:44 +0000

    me feeling care stamp short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def reverse_list(lst):
+        return lst[::-1]
