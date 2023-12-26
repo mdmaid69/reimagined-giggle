e8fd95a1383d64c10cb979eda55a9b12f8de29d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:48 +0000

    spring tool because freedom turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_list(array):
+        return list(array)
