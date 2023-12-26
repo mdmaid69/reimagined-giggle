f9306cbcac7c403b75b35c78b91787b2f3db2748
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:03 +0000

    trip gradually warn load secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def add_numbers(x, y):
-        return x + y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
