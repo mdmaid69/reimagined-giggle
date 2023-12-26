9b6b5e73102194d67780636c404e5c203652aa36
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:11 +0000

    original smell disturb make shine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
