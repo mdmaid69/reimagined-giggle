37c3d1a6c69b4d117512221a71edaa15a10e476d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:03 +0000

    transparent describe lucky kill money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def subtract_numbers(x, y):
+        return x - y
