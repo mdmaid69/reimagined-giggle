7fabe79bf20075628fb5d29a3ce2ed491ab7931d
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:56:32 +0000

    go lie negative birth hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def greet(name):
-        print(f"Hello, {name}!")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
