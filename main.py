a93dca6762ff25d2db7f4bce8b501acf4e432fb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:53 +0000

    less affect sense later approval

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
