08baf1e702687bdc70789cd21d50394fd9be988b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:11:19 +0000

    Oo sensible ignore interesting hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
