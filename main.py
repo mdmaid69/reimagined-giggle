160ae57ef60e55b8326c3e8f183186d99cb23fff
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:37 +0000

    wish challenge yet ourselves simple

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+print(sys.version)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
