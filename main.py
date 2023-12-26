13b652d24ffdd74f3562999ad12d1237c40faf0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:01 +0000

    life exist beautiful save confident

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
