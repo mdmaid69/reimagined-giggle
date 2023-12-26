a76e07061d07b255ceb4c61e00aa4315b9b70064
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:28 +0000

    guard believe event fire consult

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def add_numbers(a, b):
-        return a + b
+def find_unique_words(sentence):
+        return set(sentence.split())
+import os
+print(os.getcwd())
