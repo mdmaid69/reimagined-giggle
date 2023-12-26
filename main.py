206a0c712ee30fff29bfbaf2cab1b330c047df0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:49:50 +0000

    information average objection forgive eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import sys
+print(sys.version)
