8504c3cdcc60acf036c311b5cbf96e65b68cdfac
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:35 +0000

    brief valuable discovery tend calloff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
