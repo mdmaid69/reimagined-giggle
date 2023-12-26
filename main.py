cec1c520b8e613ad38b13ec8082ad1112d3b85af
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:42 +0000

    maintain whatever aim able honest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print([x**2 for x in range(10)])
