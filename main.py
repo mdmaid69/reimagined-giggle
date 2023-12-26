17e6db92487766cde8cdf6d27e974de1a8f51cc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:03 +0000

    yourself now confuse them young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
