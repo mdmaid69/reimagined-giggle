13a002fe70796f2e704caf0440ac355f13aa2ed5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:16 +0000

    getoff hospital create story somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def subtract_numbers(x, y):
-        return x - y
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
