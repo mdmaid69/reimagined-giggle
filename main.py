bfdbfc70e2c8a2239d29698a6f474317ec56aae8
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:32:38 +0000

    separate whisper bag best everything

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
