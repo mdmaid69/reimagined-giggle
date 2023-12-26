168d8b292dcf9026ae932f3b462fbdc3d04de9da
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:25 +0000

    nor translate few slowly justice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+from collections import Counter
+print(Counter("hello world"))
