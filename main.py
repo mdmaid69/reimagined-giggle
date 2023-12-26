4c8c4153a4792b555d39786a3b88426446e265de
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:03 +0000

    getout direct mail closely land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
