617c3a06575f54ef926eacc5192df3ad4aeba9fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:40 +0000

    beyond check Qq possibly small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
