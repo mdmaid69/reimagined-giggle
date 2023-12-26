7f8ad9a6688ba712d9512db55c13189360574927
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:33:00 +0000

    quality relationship furniture family plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
