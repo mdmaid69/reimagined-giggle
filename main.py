004bbd6f00ecea53e2759d761ed6ab427d8107f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:39:19 +0000

    cum obvious buy freeze cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
