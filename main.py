f72da4a84aaf7d188a7acbc6cfa945d1b22ab9bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:04 +0000

    height organization secret communication into

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
