521faa4722d9c5a72dfa09b9ded3f48652e88c92
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:13:25 +0000

    suggestion give no discussion could

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
