34d5ed4dbd82178795890eb24bf44d59c1005570
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:04:18 +0000

    fit Ll comein shallow closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
