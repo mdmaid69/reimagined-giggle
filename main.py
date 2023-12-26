55d3f2356e42aa75a9b1bfceb8f7d2c2c6b74553
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:18:14 +0000

    cross any certainly unfair late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
