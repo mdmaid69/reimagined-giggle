c41dba081153fb1fa7ba34e1e05b5a6e8634e6e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:05:53 +0000

    interfere spite generally brain split

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-text = "Hello, world!"
-print("Words:", len(text.split()))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+from collections import Counter
+print(Counter("hello world"))
