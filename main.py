19895c03a9367cfb5b56e8a6fb5d999975813fe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:21 +0000

    sleep disturbing report exam powerful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
