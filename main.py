da4eaa326f3861150df265498d2d8e04f30ad45e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:46 +0000

    preparation smell praise meanwhile not

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def count_words(sentence):
+        return len(sentence.split())
