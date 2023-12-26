89a3fc5fafda04a315475b7d6fc9f92f065b2060
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:44:45 +0000

    satisfied area charge crime shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
