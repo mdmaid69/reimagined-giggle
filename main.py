0b211f436f3400d3b80a9afd55af3413c241e985
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:51:51 +0000

    deep future disapprove confidence ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
