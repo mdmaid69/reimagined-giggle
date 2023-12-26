b74e137f022f0289a6777795f3fa09d462ba57c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:01 +0000

    truth dead spend future mistake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
