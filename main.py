dbd1752bea3e6b3dd8f36109640b7fb9b56c72cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:45:31 +0000

    wonder join whole separate steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
