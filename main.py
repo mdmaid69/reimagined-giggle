6e41377ede3482d3d9e85c456aa49d16a462b1a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:12 +0000

    bury him realize smart trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import platform
+def get_os_info():
+        return platform.uname()
