6764c854bb630b9e2356450d66bcfb7c6a305155
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:33 +0000

    high be interview safety talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
