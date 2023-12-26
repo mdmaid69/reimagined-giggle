e1e44ec3ecf78aa8c87103cfa2cfc46954c618c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:16 +0000

    sail takeoff pleasant unhappy mail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
