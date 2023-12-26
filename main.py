031ed3a383705bb1abfc8e54e84fc6100822f777
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:20:40 +0000

    Ff shame replace obtain warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def is_palindrome(s):
+        return s == s[::-1]
