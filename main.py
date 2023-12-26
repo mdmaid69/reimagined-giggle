6e2b76078fdcd1ec7cf00eb605874061cd0685da
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:18 +0000

    peace flood suffer any then

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
