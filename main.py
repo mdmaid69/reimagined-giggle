0089e99050e017408915354a6e99e3a4238a1953
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:55:59 +0000

    long treat powerful Nn risk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+for i in range(5):
+        print(i)
