4b478f62ea2e083be9d3e5dd416dee1876900df5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:43 +0000

    blue voice steal label small

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
