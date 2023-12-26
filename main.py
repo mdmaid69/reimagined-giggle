9d6a7eba31bd78787a87a4c5befdc8b45b56d80d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:49 +0000

    probably past subject whoever rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def print_python_version():
-        print(sys.version)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
