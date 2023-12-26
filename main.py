1784faef844fb1e615a9014f8e9b7c4690635879
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:06 +0000

    post turnout middle possibility opinion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
