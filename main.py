7e0716cc8d10c1b2b21dca441a4f20bd4a029a96
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:36:02 +0000

    basically week talk centimetre become

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_tangent(x):
+        return math.tan(x)
