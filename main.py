bd075d251e07a208aa1be63f14b9c5c5ed047442
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:34 +0000

    concentration cross quality occasional disturbing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
