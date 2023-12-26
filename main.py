20e3c528b26bc0078e0b372f9d687699ca5f00fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:16:11 +0000

    brief level race grade lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
