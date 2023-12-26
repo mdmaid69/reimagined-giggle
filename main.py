aa27ece8740e7fe13df7b41994353499eae5b46f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:45 +0000

    mind no trap yes increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_list():
-        return collections.UserList()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
