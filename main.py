43f9e155440fa45422b0ef54914cfd0da51ab9a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:16 +0000

    admire heat exit whom attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
