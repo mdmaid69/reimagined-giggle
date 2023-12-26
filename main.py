cf0c6c4accbdd914c9266fa52e7fff31eca5ef4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:26 +0000

    comment watch loan book behind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_tuple(array):
+        return tuple(array)
