c4dfa3c6dca67ed5d06d3cac9e0c43fa1af57da3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:38 +0000

    trick importance slip custom something

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
