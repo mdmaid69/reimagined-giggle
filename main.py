c6a848e2d215b98a8ed0904e648606aa482f7adb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:20 +0000

    least deep dinner me world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
