357c41789179dcc8190ba61d3c44f0cc48cb1ad7
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:38 +0000

    joke have emergency beginning middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
