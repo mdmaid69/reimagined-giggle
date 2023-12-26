8e55e13e8311bde9067c4a069c31c9f36ec8af5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:46 +0000

    turn hand nose touch their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
