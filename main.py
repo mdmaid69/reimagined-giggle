507a24f23810c38af05131216feb263ac25f38e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:20 +0000

    pupil father mad many plan

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
