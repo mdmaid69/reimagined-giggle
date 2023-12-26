bb1e84b624d1d04fe8e0babba8a18ef32450825c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:49 +0000

    lonely book pool judge during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
