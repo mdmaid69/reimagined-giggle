f88b1aa083d48de60cd4f071d917076d4b412eaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:06 +0000

    since ready doubt still produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import logging
+def log_message(message):
+        logging.info(message)
+  def calculate_area_rectangle(l, w):
+        return l * w
