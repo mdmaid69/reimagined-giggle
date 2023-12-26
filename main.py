5c9c7386465dc0c175b79b5e21c437c2c2a9c1a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:02 +0000

    die variety arrange our chin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
