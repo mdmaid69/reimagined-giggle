29cd5eaf198643fa2ea028496b1b3c79826259b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:19:16 +0000

    disturbing pinch hole direction whisper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
