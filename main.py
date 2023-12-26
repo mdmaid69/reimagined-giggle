d352ce90370da15a6587643727791050683a5c99
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:24 +0000

    pride see quarter suppose complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_index(array, item):
+        return array.index(item)
