2890ed264f4a9b0ae675ff8afab8a7c9f6058933
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:45:31 +0000

    refuse poison amount closed professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
