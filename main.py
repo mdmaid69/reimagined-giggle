935f45845bb7831351c9b2cc3b8720ecd72ab366
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:14:31 +0000

    whole bite land permission sore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
