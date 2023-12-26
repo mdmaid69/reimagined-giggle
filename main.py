62897213f4747a2f6b44e1a8a90daabb628fc5c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:08 +0000

    argument soak seriously thing wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
