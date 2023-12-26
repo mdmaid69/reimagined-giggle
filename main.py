76a96291f968ca91a9f28e991e1a4b593185bb41
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:49 +0000

    fat Ww manner power affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
