997da3d06ab6c27618fea3e9d65e808b83729665
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:16 +0000

    past comeround cold save watch

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 def generate_random_number(start, end):
         return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
