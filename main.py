cb119421d10292e9a623df66abd48dd68000f068
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:53 +0000

    check responsible sentence escape splash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
