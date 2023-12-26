aad2197543ef370dca5d6c387069b01d91c1bb69
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:05 +0000

    office almost address job small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def convert_to_hex(n):
-        return hex(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
