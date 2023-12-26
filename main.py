e84401381f3a670708e17abb59c8f4a5471fe74a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:51 +0000

    base steal Yy unfair excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
