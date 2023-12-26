5b57601b816015913a20428559b41d71c3e795ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:52 +0000

    careless narrow moral flat gap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
