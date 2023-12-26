b77bdfe3869e49770aab8d97c2f2d9ac58d5fd89
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:41 +0000

    search bad undo concentrate land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
