d3c22f1912ad8b5f80cfdf2833b901fd41d1431c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:51:25 +0000

    strict measure string job approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
