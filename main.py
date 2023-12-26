b33a43c4c0c4b56658ed695208a93b430b4bf126
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:45 +0000

    runout scare out information than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
