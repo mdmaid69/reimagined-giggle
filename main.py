7d252bf9c9268641ab3b8bb8511c15d5dcb711bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:07 +0000

    near fork react exhibition exit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
