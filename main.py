44adede02d088dd0bcc2ef57c6af8e87d6addeb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:50 +0000

    just street tool prefer delicate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
