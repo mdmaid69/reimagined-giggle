4eb31b257433e5c5c0ac01630463bbeb115e71ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:14:40 +0000

    tongue poor makeup from anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
