c2d5a7581289d8e8044df181b0265d40bb847701
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:03 +0000

    accident Nn tune spite speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
