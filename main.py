e980761000b09b5978bba8b5276c2c3aef786c27
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:26 +0000

    office heavy name wrong become

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+for i in range(10): print(i)
+  def remove_duplicates(lst):
+        return list(set(lst))
