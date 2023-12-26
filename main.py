cf10cad7753f9aa54725be3a7f4879b04e02d125
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:22:29 +0000

    centimetre instruction as better make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
