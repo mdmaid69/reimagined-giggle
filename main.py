4a6ba3d64099e53129090aed9b177a733b7b18ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:28:25 +0000

    cheat prefer underneath really suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
