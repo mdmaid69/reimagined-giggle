08df17fd3b7f3801ba04ae09b4e6392aabade84d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:40 +0000

    persuade most set sticky secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def add_numbers(x, y):
+        return x + y
