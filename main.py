78db17793ffc01491feef7dbdb687e8edb1eb1d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:55:35 +0000

    pull themselves lose interruption leave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def count_characters(sentence):
-        return len(sentence)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
