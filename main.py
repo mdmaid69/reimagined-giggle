0d2c7397ef10b891b3a78830639dd3a52b2940c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:32:57 +0000

    rent pay impatient warn mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
