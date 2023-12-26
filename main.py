f3ab756bc249c134fcdfa6916720338ed490960c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:15 +0000

    fault chin discovery ours constantly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-print(sum(range(10)))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def convert_to_binary(n):
+        return bin(n)
