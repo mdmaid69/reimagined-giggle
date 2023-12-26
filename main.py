c185d79ec685fa4c32d3160aab8f2dc638394ae4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:29:34 +0000

    interrupt grateful hope interested occur

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
