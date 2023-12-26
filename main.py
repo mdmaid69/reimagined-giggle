d32aee1400af3a6604bb00efd21cd63359a91f25
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:10 +0000

    calmly just branch history general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
