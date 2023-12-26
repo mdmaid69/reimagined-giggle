4e7d68fc3a1aa10fa69d26a165aa0fd59f61175b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:23 +0000

    worth once key wealth sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
