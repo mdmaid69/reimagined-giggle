0fe25126293b5aedb3e0dcb5f51fedce82378927
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:09 +0000

    handle unfair wherever themselves downstairs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
