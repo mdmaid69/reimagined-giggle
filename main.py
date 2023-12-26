3ce3ffef7d8811357157cfc08e2aad775e007b0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:50 +0000

    authority hate rub aim some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
