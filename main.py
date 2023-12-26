87ba4f628cba7537d32609000fde35f615d3efa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:15:49 +0000

    while open lookaround health lot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_odd(n):
+        return n % 2 != 0
