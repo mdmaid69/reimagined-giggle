44a356cdf32c21e05c121d227d7b102b15dee2c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:46 +0000

    division world involve boil hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def count_elements(lst):
+        return len(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
