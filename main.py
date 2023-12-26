caebec6e0757a1382b2aef3ba2d947a2fe9652f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:31 +0000

    shallow takeout adjective debt course

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
