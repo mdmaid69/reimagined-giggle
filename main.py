047ea95a3ba07ca6175a3ad0fa146194d8284b67
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:27 +0000

    pause among heavy continue deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
