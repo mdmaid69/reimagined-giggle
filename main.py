4cee3a9fb0f9a378833f71ac7d11f1100282ffa0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:44 +0000

    dangerous bet rubbish follow crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
