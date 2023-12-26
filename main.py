1f90d8b496611e86aec7a0a68f9acdb0cf9e1c13
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:27 +0000

    file bar steam comeround attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def reverse_list(lst):
+        return lst[::-1]
