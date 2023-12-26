aebd7d3557aa2b06383d3bb72dae5e0ce11e7d5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:36:43 +0000

    shallow attempt enthusiastic joke correct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
