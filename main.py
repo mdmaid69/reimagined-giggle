7aa63c8b25756649b27d6296b9b53f66dec6c851
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:08 +0000

    several right rush stamp break

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
