43cb776844c06f71cba9f5cfcc956b8f4c005a6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:24 +0000

    several pot left bill consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
