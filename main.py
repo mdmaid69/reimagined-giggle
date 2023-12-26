2ae35c082f5e3a7b0847c561e419e2d20a2555a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:29 +0000

    free wall sometimes practise plate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
