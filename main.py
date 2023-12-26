83f894a610ab860badbbe0ddde675bd8bd9b9afb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:41:10 +0000

    thirsty child jump himself land

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
