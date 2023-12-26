ea650ec785cefc67556f62f5d2bd6a97892adf55
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:42 +0000

    typical smooth top along giveaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
