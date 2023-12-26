620ac0f0a590fa8ed7701f982eabdbfe22c7a8ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:48:24 +0000

    godown complaint flow kind pull

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def reverse_string(s):
-        return s[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
