a355c20fee3a71b489b8b8aa3a58b3fbe03066ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:57 +0000

    noise scale round female giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
