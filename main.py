8322fe7563192d0e501421051783dbed0b512b7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:23 +0000

    protest across fail themselves plant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
