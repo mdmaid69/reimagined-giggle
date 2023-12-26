ed14478626675b9705539346cbf63f008da2ff05
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:12 +0000

    pressure may contribute poison only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
