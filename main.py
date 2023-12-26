1163bbef67e45b3017a2f40c52a7523865a46cfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:18 +0000

    slip empty fear weigh law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
