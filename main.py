3063b220f200f82935d1c92706749c3ba64442ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:27 +0000

    mark throughout star under pause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def square_number(x):
-        return x**2
+  def convert_to_binary(n):
+        return bin(n)
+import random
+print(random.randint(0, 100))
