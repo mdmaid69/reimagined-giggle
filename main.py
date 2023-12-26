0eb3de175a3ab8fdbad6d471f66ddbf49142ec58
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:37:00 +0000

    build raise description guess breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
