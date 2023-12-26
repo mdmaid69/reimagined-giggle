f8b303583d2989cfffa51a26db5d006d5924f1f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:56 +0000

    keepfrom business rate score though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
