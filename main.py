67d3795f6923b2d3016e69ef695ba6ba5f6c9f08
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:10:03 +0000

    take keen children picture eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
