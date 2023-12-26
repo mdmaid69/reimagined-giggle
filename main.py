82bae6974179722b00540f1ac88fd899f41b7be1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:09 +0000

    building hot fall at single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
