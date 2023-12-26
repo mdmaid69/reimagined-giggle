6ee24ef8a2c719f7efab83170fbb724fee8c8677
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:33 +0000

    university tap activity to shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
