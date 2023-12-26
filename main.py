d1a3bd5be97dff97ed4d23b8dbdd79961b0c2636
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:51:39 +0000

    never rain final generous creature

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
