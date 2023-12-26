5b09a6eb84277f78239af503c3603600d2fe6949
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:33 +0000

    still study offence twist he

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
