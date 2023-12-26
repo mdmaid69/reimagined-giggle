1d70bfe59fa6293996e01d6ecfce334ed68ee863
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:37:36 +0000

    far by attempt again across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
