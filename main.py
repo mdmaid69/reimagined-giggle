19719447003a1131c11406e5ea63bfa85e8afa37
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:27 +0000

    defeat successful guard half scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
