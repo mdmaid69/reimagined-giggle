d9c010264e8d9865fbbbcc4a640065ba27fa99d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:03:50 +0000

    couple food practice regional pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
