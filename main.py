97127d86a16fee0bc32941b0ef044ccf35a5e81a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:25 +0000

    memory star estimate dry ignore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_power(work, time):
-        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
