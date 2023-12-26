998177c8e89917959fbc10d1ed8f514faa6707cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:11:17 +0000

    though train none left may

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
