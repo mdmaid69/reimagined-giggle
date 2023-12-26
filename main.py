4a036a45b7099d4455dc4edf7dad7efb68ae743c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:06 +0000

    freedom goon exercise cut unlikely

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
