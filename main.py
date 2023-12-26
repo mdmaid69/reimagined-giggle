334421d65ae6658045a2e5b8f27c04b9c1291127
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:02:47 +0000

    ring narrow naturally sentence bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def split_path(path):
+        return os.path.split(path)
