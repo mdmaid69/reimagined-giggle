a09c9be2d7f1ba82baa9b065882d0cf13c13d2ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:55:47 +0000

    once separate wind down responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
