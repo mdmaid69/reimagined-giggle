52390c42eac7392189be6d8d12581ec79c52955b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:52:14 +0000

    control rob shallow argument relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
