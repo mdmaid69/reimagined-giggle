7dd6a22266255604e153a07af6e3fe56fa7559ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:32 +0000

    comeround branch ask thought do

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+print("Hello, world!")
