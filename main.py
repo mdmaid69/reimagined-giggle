00f71838745532c36f68872ab3b15a0ea23dbcd3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:15:47 +0000

    punish set problem bound cry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+print("Hello, world!")
