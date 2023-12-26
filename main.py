685fc04a58a4797bf1037f1a3e36ba04ad4163af
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:18 +0000

    pinch full Yy talk disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
