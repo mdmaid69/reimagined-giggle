f6af987542d4c02b83369b023d90911bcb39b3c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:39:14 +0000

    bringup corner lie cruelty transport

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import os
-print(os.getcwd())
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
