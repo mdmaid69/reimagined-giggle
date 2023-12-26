dcafa6d28363a571b3c1561c06c4f79cfc9c9439
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:39 +0000

    active common reject Itself wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
