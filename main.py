9d3c054268862fe7faaa3f612cf87833704e10b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:44:22 +0000

    platform score instrument problem size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
