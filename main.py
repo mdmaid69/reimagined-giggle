42b271572eeaa69896a3a29113942ccdb78033de
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:19 +0000

    minor stop apologizeapologise mention second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_power(work, time):
+        return work / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
