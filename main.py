5dc1a214b750714c20e114cb7d0c657a7327dfdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:20 +0000

    sea often real thing extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
