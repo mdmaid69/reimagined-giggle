842fea92948fd1d5fff8454a399480c37d7a4334
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:01:58 +0000

    face feeling popular by consult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
