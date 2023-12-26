7ffd1e6f60d56e86262206424d5574034a93c010
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:59 +0000

    somewhere on past hand ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
