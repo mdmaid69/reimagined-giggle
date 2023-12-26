34adebbdeea01d304cdbed4a833dbdad0b475115
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:40 +0000

    to lost front neat but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
