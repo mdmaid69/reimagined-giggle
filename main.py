4c7800305d447a52396d2522afb04e3dadd1ecd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:42 +0000

    sound half under thank trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
