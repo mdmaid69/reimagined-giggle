a89f226fa2d9b56fb3f0e442a4b6c9f8ec482931
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:30 +0000

    hollow something place gather raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
