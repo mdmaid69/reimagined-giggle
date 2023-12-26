637346ec02523d8c21eb874cbb885b4ca1de07df
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:24 +0000

    comeoff satisfied happy birth complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
