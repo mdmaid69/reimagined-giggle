c0d89e1afff0345be5520445044e4cdc255c1508
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:51:31 +0000

    nice 567enemy couple measure praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
