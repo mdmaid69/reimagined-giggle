0ca2c15b385ec75aba499eff15d5edadddcafb33
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:22:44 +0000

    thankyou figure equipment chase callout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def convert_to_hex(n):
-        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
