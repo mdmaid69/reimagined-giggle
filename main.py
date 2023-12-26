7d9b524e47f715de0faf1a9c98ca6e392f4ade69
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:53 +0000

    turnup sound shut getout satisfied

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
