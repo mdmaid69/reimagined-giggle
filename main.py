38cc14630dc9aa821a6978d7ce259b23fb0ed19d
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:12 +0000

    passage dress swing remain OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
