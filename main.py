11354c2648bdba3e84434dae5cd61f4bda72bce8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:27 +0000

    face dry day later 571enough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_pressure(force, area):
+        return force / area
