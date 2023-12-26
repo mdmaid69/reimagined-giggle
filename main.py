7471a8e2dbc038254130caa315f8ecf22ec87d3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:47 +0000

    own whisper deserve owe bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
