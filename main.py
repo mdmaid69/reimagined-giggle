88b065f1f8365aea40405cea030399668d4ae480
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:00:09 +0000

    without see cruel direct excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
