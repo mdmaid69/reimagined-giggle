002467aefb2d24f68854457c07f2a4a4f5f65b3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:06 +0000

    test connected callup extraordinary delight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
