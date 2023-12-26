4097905155a20bf4b759d91080441e6d2e8670e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:30 +0000

    future muscle off communication benefit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
