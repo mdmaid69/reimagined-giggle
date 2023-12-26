4fad5b5ee747e2d31569af1a360c83466df99bb9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:32 +0000

    empty let authority state dry

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_length(array):
+        return len(array)
+def subtract_numbers(x, y):
+        return x - y
