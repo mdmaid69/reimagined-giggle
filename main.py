5eff5744081c1e1e5bfdc99e3f5d52a088c1950a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:46:27 +0000

    sale serious field callback case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
