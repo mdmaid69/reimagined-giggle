74413c94cc8ee98d2daca983bdc6761014775aed
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:05 +0000

    murder post walk middle Zz

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
