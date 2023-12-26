eb1b28061748857a9367ff056d5e0d17793deb93
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:16:19 +0000

    possibly over mainly circumstance like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
