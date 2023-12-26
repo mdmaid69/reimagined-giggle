439e6f4ff4fbd42d09f13066c91a8c263b1937d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:46:54 +0000

    yesterday concentration old sting underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
