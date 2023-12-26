6e81607c474cb39b4dff11ed356c63187abd2f6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:47 +0000

    onto complete wish although wrap

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
