59628ce5986b17cbe64bbc0cfcf8a50b52910672
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:08:23 +0000

    pipe offend soon wear die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
