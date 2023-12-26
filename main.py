5ad487489a34293320539f7e413bea328bd6de9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:52 +0000

    keepoff visit combination wind book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
