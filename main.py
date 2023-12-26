f0a8ec9f03a12f7c5c4926709ca8bd324cdef496
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:50 +0000

    murder base average mistake now

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
