ae7865df131755af9653687f38df584de340a22a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:03:18 +0000

    odd exercise advice close dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
