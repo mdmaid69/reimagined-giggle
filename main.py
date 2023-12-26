bd5aee909b0c18d6788f7020cd9fb402cb40876f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:13:36 +0000

    run where married safety normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
