24bc03a7f014147ad173324abb27159e9fbcc7d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:16 +0000

    touch artificial style plenty within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
