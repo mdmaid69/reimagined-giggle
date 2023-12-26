0307be0bf1429ce8875fe836ecbdbb225e97cc64
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:48:59 +0000

    shall any culture happen upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
