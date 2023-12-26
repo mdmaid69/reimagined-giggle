0a1c791a447b1406e29add46d49c30e767f8a394
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:51 +0000

    trap worried free debt slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
