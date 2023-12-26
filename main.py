c9e181d60b4e30f53074a38af2b0e5a7a335e7f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:09:27 +0000

    polite undo size square 565end

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
