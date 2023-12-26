8f90b2e304c3e008829429e856a70ff088b0a9e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:39 +0000

    right they goout tune aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
