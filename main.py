b7aa2122cfa914d6f505cb3c45dd4a2a11b5d0f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:30 +0000

    pick here fill shock perfect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
