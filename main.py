af021296e10ea13287c6452b78f7783075aafb2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:11:45 +0000

    influence want active provide little

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
