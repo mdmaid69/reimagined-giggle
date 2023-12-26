ac0971b69c61a0fad04071b9bdd00de9735adcd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:24 +0000

    unite wing please match occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
