ccf42c25d885ef92a7c627c071a22f633967459a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:51:02 +0000

    marry slightly creature target abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
