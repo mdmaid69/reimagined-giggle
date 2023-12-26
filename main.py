076bc629f2a4724e7ffb94587c2de30057ca3445
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:52:19 +0000

    straight cost contain make difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def remove_from_array(array, item):
+        array.remove(item)
