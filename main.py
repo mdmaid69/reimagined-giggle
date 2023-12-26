5f47858cda910dbf1a190cc63c26ed56f44ad3ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:22:06 +0000

    getout control underneath no worse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def sort_list(lst):
+        return sorted(lst)
