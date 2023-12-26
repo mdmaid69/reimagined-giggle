1f2c1b9634554c55bff2f71c5290c02e6201616c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:58 +0000

    event return substance mistake scrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def count_characters(sentence):
+        return len(sentence)
