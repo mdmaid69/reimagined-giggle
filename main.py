433072ba5b5300b68a4ddb2afd38b9a7ce0a12b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:55 +0000

    declare fine attempt absolutely straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
