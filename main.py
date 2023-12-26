cf40ab520f4280d712f0ff5d337b9494ef9e0c3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:08:42 +0000

    field peaceful handle present imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def print_python_version():
+        return sys.version
