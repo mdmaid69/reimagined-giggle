a1a471b9b3163c3d8c41a74498c945a7be1fd70a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:50 +0000

    love loud command beneath belief

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
