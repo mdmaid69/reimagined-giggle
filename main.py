2754fa53748c7bd427dd8b954fede9a41ba56eb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:55 +0000

    573entertainment note admire fix home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def remove_duplicates(lst):
+        return list(set(lst))
