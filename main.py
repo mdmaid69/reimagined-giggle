7a5564d534ac8b6adf2aa1cd7de842feb15fdba3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:26:04 +0000

    stamp accommodation float report undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
