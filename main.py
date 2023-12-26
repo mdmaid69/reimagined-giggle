da5348ac5df006b308b88f0bb50f3a605aa52159
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:43 +0000

    fast rest form generous foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
