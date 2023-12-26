860b4b89821b1f9f57e30321b1bd68b00295c74e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:16:27 +0000

    law dare sharp present continue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
