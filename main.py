dd220e301fa17dbd99544ac27417f055075cc6a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:19 +0000

    fond fight purpose doubt family

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
