480999e35e13261af2ac63c4cb77e2ebd22ba9ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:42 +0000

    pencil interested trip himself sign

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
