b1cf7e8b98c6f64034d058e230202107bc0258a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:24:18 +0000

    clearly chance coffee calm minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
