77bc32ace93e2e8f20246c5ae15c34854dd21b58
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:35 +0000

    goout believe type Dd hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
