64444f16a3e130bd9024e565a6548927890cc07b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:49 +0000

    platform describe talkinto normal company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_acceleration(speed, time):
+        return speed / time
