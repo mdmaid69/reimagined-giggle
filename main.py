c003536169ea2fe3fd5cbe511516b3d635a66bbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:03 +0000

    mention responsibility unusual Itself tense

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def convert_to_binary(n):
+        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
