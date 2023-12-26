8a6a29dd4a5e07089af145e5248ea2c49d3f2460
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:33 +0000

    impossible collection treatment move sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
