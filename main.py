70dd2c7bff3b2a5be3ff70b652dce81b3fc204f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:44 +0000

    protest comeover love keepaway definite

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
