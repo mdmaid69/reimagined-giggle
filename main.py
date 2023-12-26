b9ed20062a3ab153d3d32b388a41b519648b39aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:39 +0000

    callup subject care put sharp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
