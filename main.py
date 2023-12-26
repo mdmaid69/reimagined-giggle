0a4b7bac7c2cec693b7ad004143b786ed009c3d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:04:43 +0000

    develop continue notice total comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
