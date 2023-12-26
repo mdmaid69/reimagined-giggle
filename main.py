1578f29a9d7fcbc8a33a1fd22f0de7e951218db4
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:41 +0000

    union name interfere help influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
