4ce07f242a6a461037fd9440ea577e9f7e52fddc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:46 +0000

    prize Pp variety sudden check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_item_count(array, item):
+        return array.count(item)
