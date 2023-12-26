1e81adbe73b4e0e2d371411af94855c16b46d25d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:07:10 +0000

    public wear twist goodbye leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
