77213ad6df2f0ffdff1e803892c553f7340ce087
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:54 +0000

    problem support find drive bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
