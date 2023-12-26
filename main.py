e2d4e7ee9e8b746225ded2f3bde98b3c8c65ff3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:35 +0000

    avoid correct hurry organization year

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
