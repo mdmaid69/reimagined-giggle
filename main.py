0139cf88330c0d5ba98ceaaac056b5f1fada5f86
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:49 +0000

    throw contents watch disagree fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import time
-print(time.time())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
