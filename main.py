cbd31f421c03bd35d8c98e4550efcb25ef9f6969
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:05 +0000

    grand pay smile desperate completely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
