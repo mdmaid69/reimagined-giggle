fab4bf8b4a48a43d94f2b6e626210ed2ac59b8c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:55 +0000

    argue trick best future window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_min(lst):
+        return min(lst)
