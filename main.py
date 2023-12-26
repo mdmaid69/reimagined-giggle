a15e5b5dd6b0364162b44a805c9008b508984bd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:38 +0000

    talkoutof free runinto birthday port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
