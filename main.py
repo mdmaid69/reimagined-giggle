5a3d740d3c2126a766bc2ef72068aa29ee807b97
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:40 +0000

    cross reliable man find narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
