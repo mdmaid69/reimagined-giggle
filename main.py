f12b6fa41960c5b8a70fe4afd712f0c9c2b9e104
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:14:16 +0000

    Pp ride joke wide suggest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
