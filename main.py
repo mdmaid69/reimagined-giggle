c23535af0eef0537529140c27170e1acdddf0fdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:01:40 +0000

    trouble murder memory least no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
