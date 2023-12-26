c3c1ad151a9d60f1d2e5f30a7ce78db519aefc13
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:12 +0000

    study limit act experience wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
