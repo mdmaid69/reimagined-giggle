828900efa84a511ac6e526853f9aa53d2a04a00f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:05 +0000

    comefrom insist artificial know term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
