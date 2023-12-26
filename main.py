02d4a60dcec0ca250403afa24144f2405a720f64
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:39 +0000

    until wire practise sight gentle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def add_numbers(a, b):
-        return a + b
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
