1ee01773a48e481b064d3e6ec6f9445a4842696d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:29 +0000

    fast minority wake trip steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
