512d1e5e72183a394d0b9f2bde443d176aca0ae6
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:24 +0000

    lose mind turn standard form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
