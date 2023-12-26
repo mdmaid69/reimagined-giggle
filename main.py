beed5d69190e988bbb3e4f9d4bbaeb783496cb47
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:04:12 +0000

    nowhere push crash uniform picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
