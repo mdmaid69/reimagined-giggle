c213776b06089a77aefc46d4bbabbc2989aeb64a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:21 +0000

    sport mind suck makeinto soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_set(array):
+        return set(array)
