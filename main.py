1b0936deefd78172308b1c56745c80b61a3d5fc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:12 +0000

    pay sore relation country stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
