311f40683b8cf0f857748ab50c105b90f78a77db
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:58 +0000

    little gather unfair legal comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
