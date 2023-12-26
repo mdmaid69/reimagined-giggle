f9094db92055cf59539c1c8292f38eab17bf7612
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:41:36 +0000

    interfere spin family opponent improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
