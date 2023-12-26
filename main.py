d2fd8f0be6469b7714c2a34278dcdee87254bf1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:05 +0000

    inform thinkover do cheque callup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
