46472b9f4ae01dca59ab2572d8160b607690de23
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:15:34 +0000

    slowly fun grow worse plate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
