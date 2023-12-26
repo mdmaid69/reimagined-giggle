44a2af4dca203781781f0de36b29a42ad5642312
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:18 +0000

    quantity exchange ability recovery cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
