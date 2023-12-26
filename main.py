f8ad660731344988409b6a0c895c72d9092de5be
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:17:51 +0000

    involve splash reply run permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
