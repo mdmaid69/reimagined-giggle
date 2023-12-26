5a4ee2cab0a279598f2daa1db4b22a3c19ec5628
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:47:44 +0000

    yours talkinto opponent cost run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
