743f3d77673cbde33638f0d262e3eb39eddf10b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:29 +0000

    aim while forever getback real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
