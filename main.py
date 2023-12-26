253efa034863936b124f4bcb4654a4496b665f9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:34 +0000

    free behind post shoulder switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
