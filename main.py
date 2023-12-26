818a4adea58814aab1139117c247cf9819e77214
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:08 +0000

    slice crowded turnup advantage healthy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
