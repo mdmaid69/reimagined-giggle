524f08fc094d049192ad7bcdb9134eeb746c0e57
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:09:24 +0000

    through criticism anxious stamp ocean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
