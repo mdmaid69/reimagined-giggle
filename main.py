aae46bead9e9060fdb3ef8709bfa2d92644a79b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:12 +0000

    sensible uponon chief idea Dd

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
