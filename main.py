1ac0a13d79899f5d2a0a54f63f20388d15d6717c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:30 +0000

    year guard instruction start trick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def sort_list(lst):
-        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
