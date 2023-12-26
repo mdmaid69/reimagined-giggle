815af0229cfeddc6ad0b103946a9fa0234ce26aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:16 +0000

    room earth embarrassing prize study

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+print("Squared:", [n**2 for n in numbers])
