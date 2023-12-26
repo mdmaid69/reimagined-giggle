7b222e1b2a7b483043cded95607626ce1a138d16
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:15:23 +0000

    occur interesting disapprove organize creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import logging
-def log_message(message):
-        logging.info(message)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
