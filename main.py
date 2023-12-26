5e1299166533e5dd692ecac11ecd48609dfa9738
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:08:52 +0000

    effort apply back Gg no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
