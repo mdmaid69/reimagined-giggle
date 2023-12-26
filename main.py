bc0769b66cc668fcb83b8ce1572ace2b1c851f4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:17:20 +0000

    same yesterday during sort blame

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
