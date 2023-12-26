39f273fa318e0eda53e7ed6ffdac6bf89372c864
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:20 +0000

    commit wipe new stiff narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
