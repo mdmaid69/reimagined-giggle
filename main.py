d96132a354d4e9c9ebbf160e568070b8d81cd6e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:09:34 +0000

    invitation into Vv day count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
