1d4413a295e58db0e3c018bf070ab6c88834641c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:27 +0000

    steal raise keepout before ours

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
