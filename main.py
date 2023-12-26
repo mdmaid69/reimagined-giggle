296c0667acb25cda141375e68824e9e978443c67
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:13 +0000

    warning sky turnoff window invite

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
