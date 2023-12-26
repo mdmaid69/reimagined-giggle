cacb8e7260587ae0dc3f15a4f502ec54cb3521f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:08 +0000

    principle lucky opponent most use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+print(os.getcwd())
