3cfc9c94a80d179d47ada410500752eb5a29c566
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:29 +0000

    run score breakout answer comeout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
