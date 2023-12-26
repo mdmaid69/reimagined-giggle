7d3c54ec8cb789fc7c72ed7932056d99dba8b29d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:28 +0000

    stamp unless mood cheap address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
