740b814d1627e0837c440247a864a95c473bdf55
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:24 +0000

    willing dead turnup lend majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
