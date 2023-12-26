c1351eac8e23ed66952f9d2516972bb2d5acf740
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:11:19 +0000

    street side rate takeout expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
