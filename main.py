523702fe1d824d7f3595e25d3d39634a62ca4883
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:32 +0000

    entry wander break store once

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+def convert_to_binary(n):
+        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
