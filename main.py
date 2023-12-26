39a02b3af9713412873e610480b9750fe812966f
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:44:42 +0000

    well stop belong important phone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def multiply_numbers(x, y):
+        return x * y
