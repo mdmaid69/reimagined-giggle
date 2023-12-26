c82d6397a20f5b4273e6823698a2c32e192ab0b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:43:39 +0000

    fast lookback(on) bit forgive method

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
