bd24ecb1b0648ce09dba91afe0c98b08fc8fdd6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:02 +0000

    slide correct any murder Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
