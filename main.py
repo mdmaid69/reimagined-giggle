cf20f31ef4d0b9595373932f750aa836fff52f8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:00 +0000

    weak quantity definite give collect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
