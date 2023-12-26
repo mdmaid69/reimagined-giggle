efafff532f0b615cb51fdb9d7af7d98b9aa73d7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:14 +0000

    control ask mixture stop Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
