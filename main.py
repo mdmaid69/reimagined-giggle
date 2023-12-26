ddd72899d05c40f6ef4cff82fc40a386850cc866
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:59 +0000

    leave swim me underneath welcome

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
