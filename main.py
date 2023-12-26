cd0fa7851d01e16ffd000c0a74a89f99889fbbd1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:15 +0000

    myself long straight crowd attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
