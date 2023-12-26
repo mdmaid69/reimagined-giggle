9a5f9b9316b48eae3f7617b751f2b0767d7d0386
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:13 +0000

    keepaway pause get hurt walk

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
