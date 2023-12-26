e73351ddf57adeba3e02598788809c0a94a88dcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:53 +0000

    part hate quit 568energy still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
