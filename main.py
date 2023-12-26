06016de4aa5785f80f6f80cde8b60084dc631925
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:37 +0000

    strength plant possession edge often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
