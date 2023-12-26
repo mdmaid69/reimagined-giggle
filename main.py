0c454a04665a848544c636c3a30981bcb80fc07c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:46:19 +0000

    complaint kind will each comeoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import time
-print(time.time())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def print_python_version():
+        return sys.version
