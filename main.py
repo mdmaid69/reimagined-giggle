3c38df17bee401e0e352e1bd512a554a2e892b2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:23 +0000

    seat right quit evening Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
