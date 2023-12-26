494b134fbebcb1036bcc2ffdff58f8e0c534baa9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:49 +0000

    home woman they modern accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
