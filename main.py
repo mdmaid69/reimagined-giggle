871f73346ce5e9bb4016698fa63f1ae0e2509fa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:57:12 +0000

    born keepfrom fortunately fight board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
