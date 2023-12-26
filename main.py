97ab985209b84ffe4969dc139b9a528c908933a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:57:58 +0000

    train direct me distance computer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def cube_number(x):
-        return x**3
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
