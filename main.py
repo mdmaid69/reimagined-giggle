af4773be7f8593f363d21bae89bf712c5589bd00
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:07:44 +0000

    fresh language shout second see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
