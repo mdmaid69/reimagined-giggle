bc4f227c8b2e2c336871c50e708a62c83e7dba2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:22 +0000

    outside however bush male ought

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
