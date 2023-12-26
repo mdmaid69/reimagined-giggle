87c0e7fb6996340cf9b10d37680be2f3476b91de
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:50:11 +0000

    above defend instruction under talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
