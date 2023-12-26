3588ae97e0ca1064b2181c2557e3b42f98c8a0e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:40:19 +0000

    afraid experiment sister effect police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
