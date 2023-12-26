744297dfb76391b2d544724504ee8a682ae4e709
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:46:10 +0000

    state affect thinkthrough female force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
