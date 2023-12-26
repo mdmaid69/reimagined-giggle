f79a84639d124e597f4acc6ec07dfde1fb4a65ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:14 +0000

    quantity top band ever approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+print(os.getcwd())
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
