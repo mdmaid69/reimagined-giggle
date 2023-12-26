40735fbbb276df49acd9fb348aac86e9039f178b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:02 +0000

    nature taste note grade fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
