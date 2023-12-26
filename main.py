0db939055a1b8b7b3ca4ee8bb2aafda89a2891d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:07:16 +0000

    limit sharp impression forgive patience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
