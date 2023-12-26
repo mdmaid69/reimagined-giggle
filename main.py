7177a411009a0969112cd3aee44b1b9e934610ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:19 +0000

    fill middle thing platform sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
