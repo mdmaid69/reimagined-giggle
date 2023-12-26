9c8dcf5c14f1faa7acb38ba737442755f1b36a21
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:31 +0000

    flow fortunate tight claim sorry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
