082c2ebc91a939679d6ad41f6db32cdf0af4f630
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:42 +0000

    beautiful me spite climb survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
