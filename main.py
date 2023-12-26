3a8cbadde9585b38a22ed4106ed7acae0ab9d71a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:35:40 +0000

    might below wire favourite best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
