1c066c5e39e916fea9aebfdc8724411a994e7244
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:53 +0000

    maybe everything water square correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
