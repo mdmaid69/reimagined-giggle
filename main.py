b537b67a812040fbfa7c3c571a81ee18b5e010e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:15 +0000

    middle sail right succeed live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_itemsize(array):
+        return array.itemsize
