655467ea8846ee26ac1432a2cec2a4a85cdec35b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:11:54 +0000

    pen past but go contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
