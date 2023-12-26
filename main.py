e509117374505ac363e8c6f48b2d79b29f55320b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:39 +0000

    week aim nervous friendly piece

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
