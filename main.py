b0e2809c69361c64ee475be83dcd8262397bcbf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:05 +0000

    public take height consider treat

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def remove_from_array(array, item):
+        array.remove(item)
