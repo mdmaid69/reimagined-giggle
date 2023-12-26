b288322d695758cd4f5b8110f582ae5e85e3210e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:56 +0000

    visit spoil famous spoil protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
