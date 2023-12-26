73a34510c24ec6cb466e44e8e6338b45af15b1fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:22 +0000

    store move mistake roll right

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def subtract_numbers(x, y):
+        return x - y
