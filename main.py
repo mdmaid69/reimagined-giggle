4d55bc4baa4b96849c995e6473e8ea3611deba5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:36:35 +0000

    flat post keepaway under slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
