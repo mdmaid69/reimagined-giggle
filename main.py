d7c970e57856971c76bc0f0500bba5f343f58525
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:26 +0000

    course want music lookback(on) fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_rectangle(l, w):
+        return l * w
