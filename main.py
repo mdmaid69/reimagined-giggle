34b45626258b198fce25403cf658dfad1a428a83
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:39:06 +0000

    dangerous information bush call smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_work(force, distance):
+        return force * distance
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
