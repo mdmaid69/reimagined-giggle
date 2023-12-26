76b12f98d46a59755cc72919fe5f93745f3197d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:42:08 +0000

    face emergency short Jj over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
