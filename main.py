bc39f16b97fc1e5e175621a2c03de4c803f1ae87
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:44:02 +0000

    people picture bringdown small concentrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
