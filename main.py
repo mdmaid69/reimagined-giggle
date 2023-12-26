664f573b72516e5f9f0384fec0ef058e7ef7cf07
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:38:55 +0000

    die head stamp along guilty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def multiply_numbers(x, y):
+        return x * y
