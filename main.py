db92c9733382de66cee50ed4aeeab67303cc807e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:41 +0000

    cap mark them tight cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
