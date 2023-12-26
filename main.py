3dad74c6ca47ebcae27d614ece8b35e953f827ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:40 +0000

    bang serious noise brave squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
