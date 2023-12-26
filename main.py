d7fa15c579c3a2fc4aad55d687d38b198181d731
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:43:07 +0000

    environment touch pump delicate stir

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
