3dc8c6439dd0cd254f1bb337a1b562ee564a99a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:45 +0000

    delicate difference getback fire he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
