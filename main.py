570139c227e82cf00fe6414b02d97d121f056065
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:24 +0000

    power this improve bus past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
