3933c502b596cf0b8eb1d32acdfe8361b3da1eda
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:35 +0000

    uniform continue rent nearly sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
