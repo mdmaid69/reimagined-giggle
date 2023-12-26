d283ad86eef972e726a7f8e36748653f1296f1da
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:47 +0000

    mind sweet keepup able freedom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
