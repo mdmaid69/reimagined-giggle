9d7a856f9e27239d35b08114a37c4cf9b0519930
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:58 +0000

    school schedule fresh tie your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
