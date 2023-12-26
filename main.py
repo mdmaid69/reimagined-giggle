9a7d1012d5e3a7db6b2f0b48f7cdf19a2b507190
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:04 +0000

    bridge sore above state backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
