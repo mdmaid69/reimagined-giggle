0df481d29be7f114368e37c0d41ebdc363fc6015
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:11 +0000

    argue introduce Cc smell rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
