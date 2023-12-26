e1695e0b127d76c798988cadc294d58864a8794a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:30 +0000

    mine teach stick neat wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def count_words(sentence):
+        return len(sentence.split())
