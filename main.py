2c2c7d5fd535f3e00cd8282c5e1db654edd01eb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:50 +0000

    bar Uu delight age pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+print(re.match("h.*o", "hello world"))
