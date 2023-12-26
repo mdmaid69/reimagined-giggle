e4428d7653686ccba6b783a82628e03e10885713
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:51:54 +0000

    blind before hide dish choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import datetime
+def get_today_date():
+        return datetime.date.today()
