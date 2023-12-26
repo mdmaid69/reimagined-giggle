8fa999880dbfddd3b088bfa74c846a8ce6f51198
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:03:08 +0000

    Cc classroom no bush mend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
