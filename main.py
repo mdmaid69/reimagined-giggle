7f91a3f425ecdc18f7cb3f59392ad2cc354c97d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:44 +0000

    experiment hungry happy and to

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
