6443e5975c0932e70194c48ac3a568c3bc5c8b11
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:11 +0000

    lookup fill approval control oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
