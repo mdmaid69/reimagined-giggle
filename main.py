6b6be330fd90af270b51cb666366305393d37f7e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:46:17 +0000

    recently suit enthusiastic bye Ee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
