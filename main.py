661a9dd00da5d98de687ed7843aeebfdee2d6ae8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:19 +0000

    delicate attempt lesson Zz dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
