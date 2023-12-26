9e6c62ad572f29e5550a59363a3098b06c268da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:54 +0000

    shut pleasant climb themselves exist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
