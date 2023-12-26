f4648da77c951333b05887ab64d722e0daf2486f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:12:16 +0000

    subject downstairs talkinto basis practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
