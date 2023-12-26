42344ae884bc63e75e9bc9b7a46fe74a99755a5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:45 +0000

    trap recovery guess warn constantly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_power(work, time):
+        return work / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
