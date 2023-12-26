bd614c241931fc7bc98be1604d6dc5679d4683c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:24 +0000

    seat bread whom cool acceptable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_pressure(force, area):
+        return force / area
