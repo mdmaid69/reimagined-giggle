062c15b46a2c9caedd2627b6f554ba3a9d0ebb3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:27:28 +0000

    violence praise less sound taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+print(math.pi)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
