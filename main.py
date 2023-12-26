391237ceeefb642f4b71f2228a322f29ecf66544
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:16 +0000

    pot direct follow phone city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_array_to_unicode(array):
+        return array.tounicode()
