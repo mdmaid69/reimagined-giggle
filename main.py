fa439adaae319b223f6732dde865a65aff9ec7ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:23 +0000

    fight hungry busy grade mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
