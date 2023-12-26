73ddf03b0d5399b267e1de8fc5bfe9813d2ee52f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:13 +0000

    cross figure none bath respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def reverse_list(lst):
-        return lst[::-1]
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
