10491f258a357f7aa01bd3788765579cccefc849
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:21 +0000

    takeout independent coffee instrument throw

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Max:", max(numbers))
