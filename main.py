5e8dad6e026e641dd23ffd4c670a074fc578c081
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:09 +0000

    year well fire noisy goback(to)

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def subtract_numbers(x, y):
+        return x - y
