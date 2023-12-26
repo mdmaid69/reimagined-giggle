2838d34b5d556927e1eb42f7b155222fe3b6bdba
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:48 +0000

    intelligent after breakup limit around

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
