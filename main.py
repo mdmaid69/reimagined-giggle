3a890ba638aca7c091056c03b44b2ec3312ca676
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:40:08 +0000

    that lean switch tradition fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_pressure(force, area):
+        return force / area
