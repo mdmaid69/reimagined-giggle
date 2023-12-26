26506d42faa9764b1f61c7f0e536b8e52d87ec60
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:50 +0000

    meal Ll related fetch unkind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def get_array_as_complex(array):
+        return complex(array[0])
+def convert_to_binary(n):
+        return bin(n)
