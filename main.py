6976722f0b71f79090e089e965a1255f2925abb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:46 +0000

    spill once skill exchange print

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+print(sum(range(10)))
