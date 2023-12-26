5708fa39d345f0b3129632ffe6aa3165fbd4a1a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:44 +0000

    change odd without undo the

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def remove_duplicates(lst):
+        return list(set(lst))
