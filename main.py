f421d9a893eff8770f3e82d9122340fbb86ad2fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:23:09 +0000

    reply pour greeting benefit old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_exponential(x):
+        return math.exp(x)
