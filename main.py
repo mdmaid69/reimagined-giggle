09dabae8c048bdc17f3a8073cd7fb833cecf501b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:34 +0000

    region Qq rest concern measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
