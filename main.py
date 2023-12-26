2bd4132df71f5be43da5ea9c8392c8876070610d
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:42 +0000

    uponon chief sale model land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
