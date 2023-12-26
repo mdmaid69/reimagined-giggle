cdd5f053b964105c955c3565053af96e1bc2d14c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:58 +0000

    takeoff smell human medium involve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
