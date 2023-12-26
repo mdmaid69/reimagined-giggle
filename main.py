68b905d2eb96230eee621e5c69f89ce0f5e1b819
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:56 +0000

    accidentally last appeal sweep breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_elements(lst):
-        return len(lst)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
