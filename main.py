59bb0448a1f279f1e4b06eac73cbf94261c258a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:01:42 +0000

    dance butter must breakdown state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
