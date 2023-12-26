5b81f1c5d5ef71525f7896d22de0aaf350e48e45
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:06:01 +0000

    fill already breath hold music

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import random
+def roll_die():
+        return random.randint(1, 6)
