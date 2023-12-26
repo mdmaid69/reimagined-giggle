c13b51f4f713d4a64df8ba1b9c181bf8beb3648a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:00:51 +0000

    wash shelter sentence block practise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_min(lst):
+        return min(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
