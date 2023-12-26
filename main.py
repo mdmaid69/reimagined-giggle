04cb05ad754099c40cc1059d73a5c2a103bde3c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:06 +0000

    it Dd Kk strike off

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
