2f15df120f42832a20bca7e3d2e15589fe737d8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:14:25 +0000

    passage occur soak boil base

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
