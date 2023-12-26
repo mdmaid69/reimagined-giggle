36f6bd4ea1c40b658c76c5b093079b71b623942b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:03:52 +0000

    gift legal morning pay bin

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
