5ee98b0a38d119eb4c18438e4cb08ffcaa9a82d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:42:27 +0000

    cover value practise dangerous tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
