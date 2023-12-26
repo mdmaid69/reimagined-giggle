20fa4e19fb8a1c1381534e4f48e99120e10b73d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:53:58 +0000

    sound minimum community sink plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
