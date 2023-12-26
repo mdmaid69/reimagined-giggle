91e21c98df23a030ad33dbf828d2c244bbf76a0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:20:49 +0000

    comment Ss survive getaway cough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
