fe318394ca77d591033cf16fb2d2189ccef61c4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:41:12 +0000

    definite length safe produce holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
