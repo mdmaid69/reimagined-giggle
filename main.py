fbdfe771b8166501211fa21df328dadf9bf26965
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:11:30 +0000

    bag love unit letter any

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
