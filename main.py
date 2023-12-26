cc64b2ceaee457efe951201d82805d7c62d575b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:44 +0000

    mind opinion divide oil estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
