325c602fde3c7a4a047ca1ac6eea59e9fa95a982
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:34 +0000

    science pronunciation lift over mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
