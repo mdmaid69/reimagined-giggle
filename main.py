3e9284b586cb19c7014ca14b92e3eff972baee30
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:01:27 +0000

    please relationship choice over confuse

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_power(work, time):
+        return work / time
