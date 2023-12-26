0ad757ca02baf7d2c24796dc8a68a14f1ac258a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:23:12 +0000

    delight sure used wet regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
