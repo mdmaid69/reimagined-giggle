b1daf596c9524abffd2c7151f6dd9a7fc5674957
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:33 +0000

    weak drop trick thinkthrough safe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
