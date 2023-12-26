dfa4b58f6e9e8c0cc459fc2d4a5cadc79623b577
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:15 +0000

    noone minimum cook floor window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
