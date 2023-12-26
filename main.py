c3ba41e9e69fa030ce6dbf1c522c4dcf2b453d89
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:01 +0000

    off service board speed run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
