464183905d3dada17019185f279b7bebeed8888f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:26 +0000

    operation cum together heavy load

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pressure(force, area):
+        return force / area
