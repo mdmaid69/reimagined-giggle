a84f35cb70032d149d899e4e7d0616d98abb100f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:29 +0000

    command notice middle lost development

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
