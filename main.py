8971456c892330a478687fa08fca942a9de749e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:14 +0000

    aim group wear peace cry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
