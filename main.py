4922deeab3569c625b931dc58c2921fc1a584971
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:29 +0000

    key answer war transport could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
