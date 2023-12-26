5b03871c31d96cbb91760868a92fdaa7f5e779d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:54 +0000

    support question dark everyone service

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
