608ac3116aa40a92e80b85d5cd8db035061989de
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:58:21 +0000

    extraordinary priority relation this unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
