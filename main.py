20c48f5b6ef9484367dd334698e3b0a0560b6d17
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:38 +0000

    have free official exchange spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
