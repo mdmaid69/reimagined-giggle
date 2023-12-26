e8826a298a224c063f8e82f7628818ca4efc072e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:20:20 +0000

    create movement who ability dry

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
