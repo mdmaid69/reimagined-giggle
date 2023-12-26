362ee0d56efe33ae93f560045c8d3140aecb6ef7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:28 +0000

    machine direct purpose worse while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
