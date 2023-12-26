5c4ba53c54de4617447a5736a3274f241b73adc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:45 +0000

    either well bath substance model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
