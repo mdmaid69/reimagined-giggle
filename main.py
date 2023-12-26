e2d286a00bf4830ca6599e72df5cb254a94f76d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:26 +0000

    uncomfortable centre detail problem sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def sort_list(lst):
+        return sorted(lst)
