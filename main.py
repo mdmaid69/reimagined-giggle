e5d808ecefab8c04520e2321189274311b70fa43
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:17:56 +0000

    promise much except dull moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
