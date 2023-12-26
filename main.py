32db088cd5a9920e340e1ea8fedfb156398a88ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:46:48 +0000

    trust hospital cheque everything relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
