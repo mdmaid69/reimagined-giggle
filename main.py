71cfd9d123aa192200421b6686356806c796cd83
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:03 +0000

    mass emphasize dress low small

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
