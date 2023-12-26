d257add8cdd185acbb198f0be5d3a266879a78e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:00 +0000

    cost broken tradition another so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
