fd2ff963bdffe1582e0f785844aeac8d589ca0b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:52:21 +0000

    great desperate copy note country

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
