2948aca071fb5c955ae3d835f1c89a586bbf170e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:04:51 +0000

    Yy mainly keepfrom buy calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
