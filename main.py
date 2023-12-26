74aa7d643c11ed2647c6f1197845c7180a09baf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:56 +0000

    main step comment arrange makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+for i in range(5):
+        print(i)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
