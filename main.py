71295f5b30ddacb6e02a3a96bf67e878fa442752
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:48 +0000

    once smooth naturally Oo move

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
