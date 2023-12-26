9863aa505c36ee0b9b7e45797606bb52f6ea40c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:57 +0000

    march everywhere easy undo complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
