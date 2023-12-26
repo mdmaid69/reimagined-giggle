41d3a1ec86f3f9514668e9857f3fa34627d5cfe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:03 +0000

    discuss you culture whichever badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
