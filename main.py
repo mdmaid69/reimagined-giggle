6e31030d973bac609457b5f7281d483a12ccd4e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:01 +0000

    minimum shallow any money confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
