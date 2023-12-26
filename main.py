bb8e04a8096f9e350ea7ca55b4f0577a0a4c756a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:02 +0000

    thumb gap copy laugh criticism

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
