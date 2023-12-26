3bfbd5c799ef067a6b939b1bf056fd5c9a9d8475
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:06 +0000

    book congratulations spend dress basic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  def count_elements(lst):
+        return len(lst)
