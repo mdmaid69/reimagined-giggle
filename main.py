e5ec657ba347fe2957d13909ac4cbd46237c421b
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:49 +0000

    wind measure politics deliberately stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
