53b74efba0ed9ac2565fa49a85b9473f8e122ab9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:22 +0000

    time poor write urgent fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
