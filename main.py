d3318d0fd6071d9c5456ca991c142b014d4ecfb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:03 +0000

    active love dark properly inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
