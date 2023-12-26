9bd9cd5f466be7730478e55d7e23710f5fe0f134
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:14 +0000

    environment stop boring politics tip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
