9467a8573b28906f5cfc6960e69cd9fe738b4285
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:18 +0000

    body convince ignore weekend office

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
