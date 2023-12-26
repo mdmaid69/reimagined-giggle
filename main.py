8a2de6b87bd46d28babd11649eb8285108895da8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:48 +0000

    form entry town home minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def square_number(x):
+        return x**2
