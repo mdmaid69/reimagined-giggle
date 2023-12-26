6c4a18362ef913118da387100acac79d8bb1dd4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:04 +0000

    disgusting I callout try choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def is_odd(n):
+        return n % 2 != 0
