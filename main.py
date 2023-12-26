8e79a78fcda24c5387b06c5f2f49ab59a0e1f34c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:08 +0000

    welcome interesting geton famous trade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sys
-def exit_program():
-        sys.exit()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
