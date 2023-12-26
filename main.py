1e6d82d09c4a8c13e11e3dbf0f6a0700ce0bc45d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:40 +0000

    that either welcome trial production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
