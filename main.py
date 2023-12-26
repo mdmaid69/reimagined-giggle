aee1c6428bb9f4991964be73d32fad4d2bd60c72
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:16:36 +0000

    display steam pencil callout flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
