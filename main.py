10046465f9e6eb538b2ed914dde3d42b18458e51
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:53 +0000

    wrong mainly family exhibition confused

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
