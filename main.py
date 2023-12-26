a11346151b752957fa8c6bf9ea50bd2d58fcb2e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:04 +0000

    warm ilovedicks furniture estimate out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
