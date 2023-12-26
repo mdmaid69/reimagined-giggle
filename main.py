ba09d7d8cb64d4d0a1f01485c5243c2d17dd3de2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:19 +0000

    exit punishment at different them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
