6609c4cbb3d7b0ae1a2067727cea4fa0fa9bd41a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:26:31 +0000

    attention other difficult after drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
