0aaf48b9498a1ad388b17f0dd827256ec7dc5849
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:13 +0000

    hardly unkind bottom street and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
