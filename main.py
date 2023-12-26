e57ffeb493b604d68ef703548de12a0d732c3810
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:06 +0000

    rent least anything doctor land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
