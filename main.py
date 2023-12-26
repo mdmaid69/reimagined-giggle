99b6d0fa6d82b1de676a02fe5ad62b5f46a71bb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:16 +0000

    so once custom hide usual

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
