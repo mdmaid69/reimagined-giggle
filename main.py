641974945c78e25723b71934ceff6e7f9ebc8b29
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:05 +0000

    key stone weak Yy tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def reverse_string(s):
+        return s[::-1]
