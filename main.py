95d55126018f27deb4c8806157671ae57806d930
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:37 +0000

    their water replace sit suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
