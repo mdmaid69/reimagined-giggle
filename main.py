f62c4b8357196e45658efaddf986abf9492cb4e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:31:57 +0000

    quarter sea clue art swallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
