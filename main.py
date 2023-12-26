9679be7d984a0bd3f3e7f6cc57d5e3c500f512e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:47 +0000

    valuable payment rob cope match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
