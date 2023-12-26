e84b2ba3f3a8cdaf1f5a7dfe93b1103dcf5f917e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:15 +0000

    press bringback off cheque interest

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
