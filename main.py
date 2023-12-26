2f271658b1356f227ff6876823f6aab7f86de218
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:43 +0000

    upset not create natural available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_power(work, time):
+        return work / time
