9fcd378118bb60b518e9d9e9ec202078f1dbf3b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:18 +0000

    press out production holiday smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
