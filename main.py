bf0da3ef10c912d0dd2ada273b98e4aa5cc48283
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:20 +0000

    switch in offer face exhibition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def is_odd(n):
+        return n % 2 != 0
