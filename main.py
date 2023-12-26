72bc19b070cdc2f250ba14b3f56121b470838d25
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:20:37 +0000

    reduce money average objection law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
