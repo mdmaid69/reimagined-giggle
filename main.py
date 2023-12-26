0ea8e1e37a771b43ad3c373c4aaa3863f52cc1e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:50 +0000

    table regular dance willing warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
