52098a578672f1e01f9de1c1fea9bf6b7484c85a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:54 +0000

    face mistake admire affect pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
