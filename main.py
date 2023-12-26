d844d866b63dc07210070ab0fffdac1b5e66deea
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:24:47 +0000

    nearly bang shout guide similar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
