19caac0d3c49e468c4e791acdc3831459764b46f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:03 +0000

    basic improve influence list instead

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def count_elements(lst):
+        return len(lst)
