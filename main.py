d22b2c683cbaae386a56c6511de4f1eb11255665
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:30:15 +0000

    absolutely demonstrate paper pain clear

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pressure(force, area):
-        return force / area
+print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
