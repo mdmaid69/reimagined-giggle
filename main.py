4a687b12bc5ab1ae161161d81e7dc5725cbbbe46
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:29 +0000

    replace underneath play accidentally pair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(x, y):
+        return x + y
