b456e3d5b617f620fe8ed9ab01b96f5b871d0e3e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:14:10 +0000

    pool treatment task children comeup

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def count_words(sentence):
+        return len(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
