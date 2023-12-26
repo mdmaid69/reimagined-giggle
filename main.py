80ce43fdf10b83839ba675b1d5279087bdebfbde
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:31:46 +0000

    excuse generous tidy bring score

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
