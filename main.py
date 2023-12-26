58322d300b3b18c389c8c2fcfc5661c8f7de85b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:14:08 +0000

    565end cum trial notice thing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
