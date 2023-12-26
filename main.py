09735299445cda0adf1d761bf3ba57e847a2d3d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:59 +0000

    afraid education by generally scene

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
