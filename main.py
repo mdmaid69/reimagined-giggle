0dec0c17aaee6e4f499c618788074ff916856594
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:09 +0000

    extremely boat replace moon quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
