d677e3056934f55b4b567062e9a168b8beea345c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:59 +0000

    bringforward this attract cheek love

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def cube_number(x):
+        return x**3
