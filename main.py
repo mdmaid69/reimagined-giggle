81071f8459867192044d0a448f02cb65d7c3572a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:51:43 +0000

    total day everywhere repeat train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
