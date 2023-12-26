c14ae8900dafaa2dcb4b2133539f4f493f2918dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:17:06 +0000

    if possession freedom joke Vv

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_queue():
+        return collections.deque()
