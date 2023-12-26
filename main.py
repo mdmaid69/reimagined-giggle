44125bf4a61a0d0bf952f8c1654f19135cfb805f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:53:36 +0000

    study turnup from water door

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
