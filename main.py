74e22cac9883d44a976037dbe29b5f7925c17aaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:04 +0000

    fire heart desperate best peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
