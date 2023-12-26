1481f733620a762a6645b764706b324862f4c953
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:58:44 +0000

    send lock save dull performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
