c5b23b800745043dec4ba2cc7fe5dbd11ab85f34
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:37:49 +0000

    worth remember short buy getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+print(math.pi)
