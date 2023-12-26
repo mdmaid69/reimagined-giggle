8dff16059d6081ef796b4d112554653a56292ea8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:03 +0000

    fault turnup really dull pause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
