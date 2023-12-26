d2d7d6a8c3553c6ab4a41a9b427f6aae6371fbd8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:37 +0000

    mainly swing bank will board

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
