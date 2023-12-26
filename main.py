6148047ddd075fd701abae3272af5fe0a6633df1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:03 +0000

    accordingto ear belief fit friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
