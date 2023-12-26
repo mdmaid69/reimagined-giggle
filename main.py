8bb69186121a56434f69bd110849a5b794748a94
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:19 +0000

    nature substance play innocent nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-print(math.pi)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
