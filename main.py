3e49736780324b892bbe2b69c5b87fca30a745c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:06 +0000

    afraid slice stay bread break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
