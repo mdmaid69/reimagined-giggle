a18ac274586029b972f2f77af2b32eb1ab1ae028
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:26:00 +0000

    mind unlucky any violence cheque

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
