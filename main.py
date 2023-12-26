a03d03ea3c551ed6376de0ad68185df216aaa2f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:08 +0000

    permission belief fall more calmly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
