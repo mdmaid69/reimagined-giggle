97dd721f8056d4ac9a943e789e45b1416e2e895b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:20 +0000

    pay coffee move passage unhappy

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
