9157c6a4578ee3fff9d3cbe8fb8960ad6801320f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:15 +0000

    sick rubbish treat plant safe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
