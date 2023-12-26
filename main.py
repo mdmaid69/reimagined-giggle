07345a0721376d6e66dc74d47a75d251fc77a8ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:23 +0000

    mess tough desperate pocket fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def count_elements(lst):
+        return len(lst)
