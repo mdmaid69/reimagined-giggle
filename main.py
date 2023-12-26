6d0a7e6e50dcb125d0183b893afcd0e279a0f5df
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:58 +0000

    handle release most turn content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
