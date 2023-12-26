ccf5e768214485bab84259fabda3043d84f17de9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:02 +0000

    finger worried congratulations mend Dd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
