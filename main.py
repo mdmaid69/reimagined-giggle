72a8c86460be6774dc5a9e8260beb26e270d5275
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:39 +0000

    broken chat tool after even

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
