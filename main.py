99fc3756b0a111d963429a2bef8da8ec4301bdfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:31 +0000

    refuse crash difference instant various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
