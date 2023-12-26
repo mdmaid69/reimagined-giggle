1aba51120970e6ac3a38faf4a4e3066ce6092b20
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:59 +0000

    hear clearly string news steady

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
