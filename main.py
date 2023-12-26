69b0db1ea403ca4c27d78b28f0276b08efa35654
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:53 +0000

    workon lead national perhaps range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
