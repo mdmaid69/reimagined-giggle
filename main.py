5c9fab6e1a3c09eb3e935fae7e751899c3eabd02
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:55:40 +0000

    design limit narrow healthy famous

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
