517927c88efcf7a9b0e78ea47815c4abeacc851e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:35:45 +0000

    fortunate address recently opportunity also

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
