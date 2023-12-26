c33c72dcd6f7df43dd303cd8e8924aa43ce4afbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:33:01 +0000

    row insist bread disagree Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_python_version():
+        return platform.python_version()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
