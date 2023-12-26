f8a3889605d1d0eb5392b549eb3e5da1fa912f55
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:13:30 +0000

    worth ability tie point of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
