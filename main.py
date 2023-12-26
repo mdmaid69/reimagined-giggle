5e242580062dc11767cb3873fc03460e5e122e2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:30 +0000

    explain plan tax familiar confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
