325d40dc059e4df15083664ba66e83b3c8717b33
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:36:12 +0000

    odd breakdown breakout Pp friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def append_to_array(array, item):
-        array.append(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def greet(name):
+        print(f"Hello, {name}!")
