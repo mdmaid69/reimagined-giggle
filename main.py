39e587bb66b5bd614d71f9dd0b86edc5861d5e0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:32:44 +0000

    hit everyone ocean comeover slip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
