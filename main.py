52c5874a11a4ea0a3eecbb4c5012f74491c53d63
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:26 +0000

    tip 179bite goup effort colour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_min(numbers):
+        return min(numbers)
