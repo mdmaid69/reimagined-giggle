490c388f51dcac8666852133aca897106d150a30
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:33:33 +0000

    takeout string content destroy collection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
