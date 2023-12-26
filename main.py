89246beadf4abbc72c22a5ccc8234bf05accc2af
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:32:29 +0000

    grow various mixture cook burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
