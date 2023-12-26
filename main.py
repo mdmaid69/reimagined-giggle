e1a9a7e125e71f68934b4d8b0d6f1c94e717c9d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:01:22 +0000

    various exactly shut risk complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_time_since_epoch():
+        return time.time()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
