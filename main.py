28077755282396ca17d4f4f294538db71a5918cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:08 +0000

    clean forget rid pure circle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
