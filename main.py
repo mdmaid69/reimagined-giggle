2c2293766b5097b8f597fe6bce0366f748f605e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:41 +0000

    escape watch left sudden pair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
