83b4b899571c6bdf432a988b912ef285d077738c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:35 +0000

    trousers take possible attitude variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
