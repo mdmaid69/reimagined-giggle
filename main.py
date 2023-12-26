9166a447cd1db8c98dbbacb8b76675d111cc6d18
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:11:50 +0000

    your material real possibly excellent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def count_elements(lst):
+        return len(lst)
