5c5a1bbeec374ca2eda211d9513e9a65701a9292
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:29 +0000

    score copy cap lookaround plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
