2d67e77a8815daaa023e9ce84f50d535d5bc8771
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:17:53 +0000

    solve for second style announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
