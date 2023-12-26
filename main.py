90fb746e24e25d04cce87b1b6b3012cd40b4ba12
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:45:47 +0000

    doubt probably sound become door

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
