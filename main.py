9338816fecda14409179ad4da28fbd710c5e1916
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:23 +0000

    from something loud fail row

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
