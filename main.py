51ab3fbca26fe67577b37afe1a7b424ff10b6267
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:08 +0000

    trust during careful plant suit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
