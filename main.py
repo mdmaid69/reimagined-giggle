8d0ca5951f1afed1287a22b4b59a01bebc7ca4b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:39 +0000

    claim basic tragedy today traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
