46de88249f4d02e73d9956c4cdd8d562bdd0aec4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:58 +0000

    cut shopping reduce anywhere opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
