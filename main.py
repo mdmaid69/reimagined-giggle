131c38a4bf9e98ce7121e2f9f36bb3a8e7ade9e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:18:42 +0000

    bye spring calmly charge on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
