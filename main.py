d01024096a5f788040b0b96bf3d44adab7afb85e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:28:28 +0000

    bar probably any smell surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
