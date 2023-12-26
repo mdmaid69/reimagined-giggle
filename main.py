bb0c0f48390a66d3d965934bc5e841ecfd75ac03
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:12 +0000

    pronunciation claim respect little mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
