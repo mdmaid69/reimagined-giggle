cff6204090ec18c722b12ae3794c51a7114ea879
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:42:24 +0000

    own week pipe kind mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
