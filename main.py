3e30726cbcabe04ba48a0ae8bb25a725086daf7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:45:01 +0000

    bag design art lead case

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
