b33ad8ad251bb4a80eab56f122d5d51efbbcad73
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:04:05 +0000

    recently draw brush move beautiful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
