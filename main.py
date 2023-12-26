dddf36f0f4c88cc8adcb40d90f696ef21c6b48f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:29 +0000

    read gather pain unit table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
