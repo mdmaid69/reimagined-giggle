4407fdefea3783b61678d6f22bbbd4457ac6ed67
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:59 +0000

    bang listen spoil description bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
