069230207e08b627eae7550ba73c9249e96de1d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:23 +0000

    walk callat pile correct section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
