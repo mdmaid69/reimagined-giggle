cf76a927e58d81d832d985cb7e1e9b795199c923
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:52:26 +0000

    gap common of bend bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
