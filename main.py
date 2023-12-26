f56bcbb0d6ad7384cd6ab44ad23df0480a25fcaa
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:25 +0000

    remain will lower benefit feature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
