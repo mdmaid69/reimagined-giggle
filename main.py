2a043871a0a68293b78779796502b445cc7ff3ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:28 +0000

    shake protest hurt obtain mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
