bfc0300b78a9164f4d2b873ef76cbf9025f0c60a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:26 +0000

    vanish determined none law anxious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
