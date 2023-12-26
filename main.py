13a5bd427f381fda8ca48f91d70f578b2c6e5cfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:36 +0000

    print chief concentration under become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
