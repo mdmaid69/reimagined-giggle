13891ad6775d3ea9c013c59d6c077bf92e8d0434
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:19:54 +0000

    threaten rent disgusting most amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sys
+def exit_program():
+        sys.exit()
