9581ca3646fc21051bdadb452208a56984cf552a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:51:51 +0000

    thumb cut maybe clear knock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
