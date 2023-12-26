187949ecf08aa66463fbce3b258350d4c6aaccac
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:20 +0000

    style what fix surface certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
