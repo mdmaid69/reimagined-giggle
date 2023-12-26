c5fda7d66fd51eadee9432dcda9e9f15aca7f0f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:10:22 +0000

    grow century land form several

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def clear_array(array):
+        array *= 0
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
