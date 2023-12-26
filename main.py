493d49f00c0b7d6b488772ca9fc4a2ae7f0a6541
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:57:11 +0000

    natural artificial complaint find splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
