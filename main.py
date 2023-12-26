02dc58b7ab29346b1b220ffc14756ac1a948a223
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:23 +0000

    variety board judge study Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
