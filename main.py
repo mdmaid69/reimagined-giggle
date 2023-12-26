d30935b8f204aac274cfc7c584f4dc241be5e3db
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:01:37 +0000

    computer guilty public mistake visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
