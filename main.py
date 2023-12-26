0901b0072b277b37f1f7f0f8838ce8cdb8f82fd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:40 +0000

    chin side nor meal offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
