e7b0cd073d672f6bff9521f61eac091144f167a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:12 +0000

    bridge cut reaction takeoff drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
