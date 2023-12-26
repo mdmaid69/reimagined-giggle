8dd9709d70fac0d596c85a0257f1d3aeebf65784
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:47 +0000

    pour effort tape flavour back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
