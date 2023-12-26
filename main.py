87eebb858aeafcf44899af81031a7682a37ebf51
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:12:06 +0000

    follow study relief late throughout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import platform
+def get_os_info():
+        return platform.uname()
