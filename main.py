1f152075d787e80e17dee1c04f7b8c60fa8bc4f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:30:10 +0000

    coat suffer father now little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
