8d9dc6d4fa4f538d54ae58decaaeb08b1a8d5642
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:27:29 +0000

    produce afterwards design understand excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_user_string():
-        return collections.UserString()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
