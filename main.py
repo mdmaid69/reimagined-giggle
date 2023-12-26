6a99c0036f6707c416b30d728b5b0bd6f6cd646a
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:44:28 +0000

    remove always left famous criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
