c44b8310e17db85ea0fbaa4f2e0bf684bff6e680
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:55:24 +0000

    lead gate spend wash zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
