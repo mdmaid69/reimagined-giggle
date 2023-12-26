041e1a250eba0f703da52f73e04200dddd520335
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:49 +0000

    mean exchange border performance this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
