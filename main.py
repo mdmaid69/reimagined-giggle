b8230c35059ab7917fe1c8286fa8e850ffdffea8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:25 +0000

    change strike favourite at fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
