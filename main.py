2077155ed596247ab484d9763e4a4027552ee584
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:57:09 +0000

    Pp fat achievement other except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
