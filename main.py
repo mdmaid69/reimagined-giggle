22bfb5920eb4c6d050dcb244f193896b1cbe65e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:31 +0000

    572enter offend out then stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
