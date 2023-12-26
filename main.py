4b14fd3966018bed6276670e591f9ba30da43f8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:53:06 +0000

    settle bang treat arrange direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def split_path(path):
+        return os.path.split(path)
