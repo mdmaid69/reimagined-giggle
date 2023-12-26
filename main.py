6e8785837200de640340113f5398e1864d697ebb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:15:07 +0000

    ilovedicks boil comefrom agreement switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
