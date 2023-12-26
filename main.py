f921e2db4360450a927cae7d3b2771b4f93c477e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:46:54 +0000

    something face hope dick today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+text = "Hello, world!"
+print("Characters:", len(text))
