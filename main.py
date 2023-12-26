36de3be73d438e7b84e5063b956da9e09a349fcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:41 +0000

    achieve welcome storm takeoff lie

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import sys
+def exit_program():
+        sys.exit()
