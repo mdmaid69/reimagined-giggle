630c256a682396a7f4d89689be044d70b9b9f063
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:04:05 +0000

    only set meet bye introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
