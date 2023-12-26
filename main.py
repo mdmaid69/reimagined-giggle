3bb2f7c14fc55d0155aecfc5059788711ee78dda
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:00 +0000

    job post father fact heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def count_elements(lst):
+        return len(lst)
