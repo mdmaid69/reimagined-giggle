f20a232774fef09f4465fb9700304b24d39ae6b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:17:28 +0000

    perfect refuse nerve before worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
