33899273d2c9642b37fab21decf291005957f799
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:45 +0000

    Vv serious extremely within why

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
