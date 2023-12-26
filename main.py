ea3e42edfd06915f8dacfce900a2db69303406e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:57 +0000

    nonsense less organization turnback giveback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
