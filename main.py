2cb25e39a628ad24d61aaf3861926f7d47f2b66c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:59 +0000

    neither day small few involve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
