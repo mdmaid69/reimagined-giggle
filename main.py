d604f91bc6e7ace792bfaf4cdb78c3589413334a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:00 +0000

    pronunciation benefit right market hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
