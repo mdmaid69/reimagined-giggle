561e0fcb094b2671b8f530cb3b775a947d44b220
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:51 +0000

    embarrassed also apart compete unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def count_elements(lst):
-        return len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
