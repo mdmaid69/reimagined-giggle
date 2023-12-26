1f1dbf297240cdb57b48ce92e850d405ebd472c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:47:04 +0000

    reserve wind thinkover breakthrough computer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
