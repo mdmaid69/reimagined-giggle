0f3991c67d65fd554f816e0648fd283d8bac7041
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:25:03 +0000

    effect breakin succeed raise run

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def reverse_list(lst):
+        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
