37d696cb1805458fa0f49a34564a078f556fa51d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:37 +0000

    over black especially about clearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
