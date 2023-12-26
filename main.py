020b17ab74764cd4bba1a7214b1b493da6353c3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:59 +0000

    desperate slice concentration relationship whom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
