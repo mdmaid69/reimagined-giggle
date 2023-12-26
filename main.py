f4b2928a4ee6435c5ef37eacc27498c2fe6d4a71
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:47:31 +0000

    calmly accidental whatever film smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
