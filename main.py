9066f465f90468336ab886897bce475a3133e93b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:41 +0000

    cap out unfair wash mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
