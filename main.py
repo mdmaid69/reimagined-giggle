2faa93b583002abf377be8d626a04808d3043190
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:53 +0000

    variety workon similar Zz substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
