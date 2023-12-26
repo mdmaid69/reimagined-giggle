6149d5a023939dc37413d61a781fa7a207b81901
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:05 +0000

    duty empty respect thick offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
