3b145fdb221189637e5168d720b91575d924df55
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:30 +0000

    exchange message callup service grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
