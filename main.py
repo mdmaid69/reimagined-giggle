7f69bd89c1d91b399ef2277b12ba1240c80fc00b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:26:58 +0000

    care flat hit satisfactory book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
