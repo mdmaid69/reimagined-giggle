e6819857a3574731450ce7f7fbe5081413a02418
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:05:23 +0000

    partner vanish runout spot solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
