bc0392a7ffacf395956a229ab655c53212b1bc82
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:32 +0000

    dare Uu pair myself before

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_pressure(force, area):
-        return force / area
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
