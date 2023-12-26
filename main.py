f9d6b223dc32dbe8760b4ecf9e7b32c25cb9d42a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:30:45 +0000

    available cross character petrol march

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+def reverse_list(lst):
+        return lst[::-1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
