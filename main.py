5f541f98fed3a4be5db35a646b71be9b54774efa
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:42 +0000

    luck change raw spoil dish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
