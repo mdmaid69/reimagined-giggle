4f536bd055ec4a4544ea1b8b029efccdc662aec7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:19:30 +0000

    loud level coat cost organize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
