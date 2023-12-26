e189282958df430addf618788caa99be58652830
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:24 +0000

    few calm character callat Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
