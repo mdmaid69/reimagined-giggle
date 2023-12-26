449cd3b057930990f740cb3cc1c9ac186139d964
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:45 +0000

    need purpose late indeed might

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def get_array_typecode(array):
+        return array.typecode
