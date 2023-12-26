aeca71516351aefe4a97e2b4e57668ee54ab480a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:58:32 +0000

    wire embarrassing mouth talkoutof valuable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_power(work, time):
+        return work / time
