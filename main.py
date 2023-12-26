4eef9f89dfcc06d90bb50f496a04bf6eab89497f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:12 +0000

    action conduct press rely difficulty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
