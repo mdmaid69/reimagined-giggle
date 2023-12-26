2e78b2d63efd1f9b325aeeb2fa2445fb114dded3
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:11 +0000

    push stare through free put

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_pressure(force, area):
+        return force / area
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
