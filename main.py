a1dd77b3914b0c5f79d5ab0cca820549e7702a39
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:23:28 +0000

    invitation straight steam lesson record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
