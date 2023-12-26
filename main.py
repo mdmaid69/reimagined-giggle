10621c5b998fb88502b6801ef168e3686d08af30
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:41:28 +0000

    advanced judge hardly loss mad

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def split_path(path):
+        return os.path.split(path)
