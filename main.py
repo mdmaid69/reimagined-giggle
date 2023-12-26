5691b2dee748bf47da83bfb5229cce726ff5f5dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:48 +0000

    water bank conduct maximum listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
