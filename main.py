43b1272eb5a46adeee79d1454bfb4116743bcc3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:55 +0000

    Tt urgent advise whistle entry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
