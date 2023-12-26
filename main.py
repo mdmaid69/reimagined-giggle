b4e7471e314fd76a67bb27131bd2362b6d573fec
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:52:24 +0000

    afraid copy excuse used tip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
