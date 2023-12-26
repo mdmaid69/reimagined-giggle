176de76129b08b621c8fdd960e09dc688fa50749
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:06 +0000

    disappear off flight sweet sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_ceiling(x):
+        return math.ceil(x)
