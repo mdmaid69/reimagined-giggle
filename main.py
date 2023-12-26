6f70f426ef28ab9143e00479eb8c3f80f9d09f5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:43 +0000

    well major common full branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
