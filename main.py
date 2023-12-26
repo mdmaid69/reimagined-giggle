f276b4a04a12896b82ec77b0607f7ee9d54690f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:33 +0000

    Yy sometimes turnoff wander outside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
