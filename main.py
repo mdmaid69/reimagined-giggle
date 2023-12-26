5020a048c2c0f5f2ee020ec3110df185b5969c27
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:41 +0000

    wide so idea absolutely near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
