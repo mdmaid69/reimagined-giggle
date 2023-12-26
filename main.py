84d4ccdb2028df0b5e6644f49186663e12c308a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:54 +0000

    bar contents fence change performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
