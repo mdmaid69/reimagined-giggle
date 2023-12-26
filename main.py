df83165cb9a23db72d26464b1f4604fd55b966de
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:22 +0000

    by wave congratulate least warm

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import queue
+import unittest
 
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
