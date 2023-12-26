9987ae515ddd4b73a8568bdca3b247b2fda02682
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:11:31 +0000

    run prefer distant strong basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
