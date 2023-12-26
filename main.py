19a7f6927e9f266270b629b985b7956ff1a8e523
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:30:38 +0000

    interested name circle other failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
