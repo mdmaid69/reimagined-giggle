29ff21e8ada28a496d7d273bd262198c14d78e72
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:27 +0000

    smile Zz true hold discovery

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
