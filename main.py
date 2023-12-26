9cbf60c3f7dd222bb8f65f3fd3fd2176bb1a2d4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:54 +0000

    cheat stand modern communicate well

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def find_max(lst):
-        return max(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
