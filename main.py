e9a93ded8caacef67f75923e0ed121c42b632117
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:17 +0000

    steep scheme disturb ground loan

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
