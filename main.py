dea19bc4c737681e1a0e6155235c9b699b67b71b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:33 +0000

    rescue collect consist attach slightly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
