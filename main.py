188f95a776f31e8a79c864a19796718681f570bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:15 +0000

    characteristic set silent hurry only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
