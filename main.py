1a88dacf210bed8e1e9cb06fe98c2b7ad3951fd0
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:48 +0000

    bring underneath oil date suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
