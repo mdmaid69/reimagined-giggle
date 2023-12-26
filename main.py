084632540c40994d7836cf0009e2a1e82efe33bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:00 +0000

    present brown emergency hot criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_str(array):
+        return str(array)
