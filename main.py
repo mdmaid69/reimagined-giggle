4d64a4d3b0fa4fb65b789d32f818bed4c2c70e80
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:22 +0000

    up visit gradually clear light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
