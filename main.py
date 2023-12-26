446860ccadfa3d6ffa2583cd6735342b577326a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:09:59 +0000

    shout interview closed tough plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
