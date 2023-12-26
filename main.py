76c1563e1362877447a701198f1bac70de980a90
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:32 +0000

    send slice unfortunate close hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
