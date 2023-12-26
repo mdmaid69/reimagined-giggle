a64aef72b71acebef5fc839ac1b27226f6925a1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:42 +0000

    middle worse example war real

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
