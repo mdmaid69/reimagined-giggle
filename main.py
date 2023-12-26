ba292930ae01a653da0da750ce3d76fdd1f60b63
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:59 +0000

    shoot thing detail turn fly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
