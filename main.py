43461058c3583a194a6e8d706a855dedee1ed23d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:15:19 +0000

    fault exaggerate prize great shape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
