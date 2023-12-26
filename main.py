66fbb964a073e0dd7c6f9630f2a2993fd0a5f462
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:43:29 +0000

    innocent beginning forever ride how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import sys
+print(sys.version)
