8d8ae6018d9014d1eee69c3fd7392c2316ce3620
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:25:54 +0000

    pride practical proud patience tooth

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+print(sum(range(10)))
