1c50ce04ab9a37533d85c4b62dc49ed3b5ca858c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:37:05 +0000

    frequent godown sure off fit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
