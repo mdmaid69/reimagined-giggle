48296fad7ca16ae40fe7e07aa4e10e5f9f7475f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:19 +0000

    board dress no promise damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
