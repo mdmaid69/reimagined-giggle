9eb4a8d1dbc3a203e64c471ff6911393604ab7c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:25:54 +0000

    touch error room boil weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
