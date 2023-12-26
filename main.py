afe1787284e509207447ef955f47b5be9d11fdc6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:39:00 +0000

    rest become good ear before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import tensorflow as tf
+print(tf.__version__)
