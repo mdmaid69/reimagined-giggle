e7ab170c660c68e99c4d4fa5fc41475122ee47b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:05:39 +0000

    print even print promise usually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
