4450497d5884fe75379e0eb7d1486b532833a8fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:47 +0000

    try than tear season score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
