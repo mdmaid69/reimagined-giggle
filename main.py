ff556ca27415e649b2228a70e2c296eee55ec2c8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:37 +0000

    lesson street let hers praise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
