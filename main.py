81ca973a2ba73f66618da7abf54b1ff107f85e32
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:40:11 +0000

    spite depend pretty club interested

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
