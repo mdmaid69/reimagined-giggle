4515945b4868a0d323f0f05448af6eaf21a1f69f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:24:57 +0000

    Mm today watch together behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def find_min(numbers):
+        return min(numbers)
