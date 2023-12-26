9ec649004dba2f0b10e99bab5825cdf5009ebc6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:46:21 +0000

    good could train more holdout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
