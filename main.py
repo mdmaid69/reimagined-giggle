4805ee1a2e7a321c0af808f203495fb72357d70b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:38 +0000

    evening state bush live contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
