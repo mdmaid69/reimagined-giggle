b47cc73792c54d286d3662b3262e29472f64cedc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:42:21 +0000

    mainly unit hit mainly level

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
