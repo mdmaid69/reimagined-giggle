a4b853c37e77fbb63ff176559ea0e0afc957f8eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:23 +0000

    once evening he grade declare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-print(math.pi)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
