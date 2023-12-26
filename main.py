7e833d500c9cb0beba51a6beb7a8ae6c5aa9ef21
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:43 +0000

    thankyou happen count wire left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
