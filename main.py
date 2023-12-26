a1f08bc719efc641fe462d8d4636bd64c58f8234
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:12 +0000

    throw me ever married active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
