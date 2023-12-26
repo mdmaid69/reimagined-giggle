06e7deef27c629fa67a2f47807ecbe8d2c5178b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:00:51 +0000

    either intelligent secret immediately noone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
