63392038d7571bac5b475d34b5cc9e0e3086d2c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:09 +0000

    summer definite whichever rain wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
