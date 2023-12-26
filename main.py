defa9fcd1875c2d8afbdaf302e45da866490d52b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:50 +0000

    alone clean through above cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
