6cf91259c16339848ca577be8e4296bdf6e76c4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:31:05 +0000

    straight earth complicated nigga label

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
