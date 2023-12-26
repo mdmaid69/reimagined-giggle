2312e8a9552589a6247a516dfeafdd585de85949
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:24 +0000

    describe who disapprove battery soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
