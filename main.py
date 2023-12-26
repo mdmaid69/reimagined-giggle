0c00ccc2e08f0b24aa71c4468a1ea303d14fdad3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:07:00 +0000

    definite move serve off disturb

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_power(work, time):
-        return work / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
