cb9143a9f0966a1f3bfb9c928ac36c44b20269be
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:20 +0000

    really nose who lower work

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
