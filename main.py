ab2a1d5885c432c806dfc9c45b454856b7f63a33
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:42:53 +0000

    eager cost talk education bang

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
