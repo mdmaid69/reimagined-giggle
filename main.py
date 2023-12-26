c29b813ddd9eb4d3bae5b1039b6b8d40425599e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:24:08 +0000

    destroy dead obtain letter wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
