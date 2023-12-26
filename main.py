6abbd44de75e0ec4bc9e0499c3fcfcf46b788af9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:54:39 +0000

    image special judge goaway quit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import tensorflow as tf
-print(tf.__version__)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
