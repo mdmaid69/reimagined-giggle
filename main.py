d00002e74b1921f4e34f1f39d28a7c621959990b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:37:30 +0000

    body whistle shock weekend below

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
