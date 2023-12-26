5fdf4e630ca26b10ef78bdcc1937c2328223ead0
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:54:34 +0000

    replace from away face string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
