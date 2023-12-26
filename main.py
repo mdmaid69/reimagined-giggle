254fd55830dbcb9b697c5c400a1ba1726beceef8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:39 +0000

    calloff least wherever fork play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
