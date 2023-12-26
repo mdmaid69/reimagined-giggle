f1d6c855384a9ccc2cee6cdc0decbebf93f141bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:37 +0000

    holiday string crisis history necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
