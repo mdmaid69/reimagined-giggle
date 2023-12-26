2cc731d24c176982c7a9ec49b44674916ee9c025
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:32:43 +0000

    feeling dangerous fast worse table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
