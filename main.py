e062ec7cff20d5d7e2592554a04fc8d65dde6c3a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:51 +0000

    nurse protect contrast raise pressure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
