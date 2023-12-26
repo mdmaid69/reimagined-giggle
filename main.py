f76a5fdb95ee5b5fd7af3cb1fa9f3eae0c6ab864
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:09 +0000

    boring scream paper combination fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
