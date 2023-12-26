2c0f5af9950b2aa5cc01c82e461d2394ed72919c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:26 +0000

    our sport crash mysterious keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
