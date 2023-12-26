23e747b70efe5a780aa63b3d5637a7da547fe701
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:08:30 +0000

    attractive their basis basic head

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
