be94dd8b2d51373dbebf9f13f164906e9c94bc95
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:56 +0000

    snow dish tone quantity near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
