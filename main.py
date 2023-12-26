2d07571d7637a050bd927f8f4e9a33c02549a644
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:17:32 +0000

    moon acceptable imagine negative fear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
