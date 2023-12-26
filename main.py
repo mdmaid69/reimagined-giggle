70d8bee183bf6464b56e8e023780d0f768ed376b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:45:29 +0000

    happy flood obvious guess by

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
