f7a1c52ddd039e4901d048b584b33b48573d1c9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:08 +0000

    upset coffee correct aim plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
