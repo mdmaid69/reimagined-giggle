d2ce4d01d1bef1ca8721f3407028e61cc62b0ec5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:45:41 +0000

    happy set spoil much perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
