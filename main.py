343efc1e73ea46fbe4517cf8749b4aeac39c085d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:42:42 +0000

    callup comedown vote holdout steal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
