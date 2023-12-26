2700deaa7c17f511544191a56996a674e752aa5e
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:15 +0000

    teach tomorrow shout journey afford

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
