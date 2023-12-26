e5b17fde0d2848bb23aeb085b38b7a1dd4ea8f61
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:02 +0000

    burn think trousers shade meeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
