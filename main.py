6695f88f388d7d4e20df5a16c71eb5fc49d2079e
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:56 +0000

    close disturbing kind moon character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import time
-def get_time_since_epoch():
-        return time.time()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
