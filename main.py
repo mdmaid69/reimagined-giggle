11406256b4382dcb7ccdb88b28b97def574ec7cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:10:50 +0000

    action block apparently empty any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_set(array):
+        return set(array)
