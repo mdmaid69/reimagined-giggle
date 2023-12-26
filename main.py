45b7e14a21da281b2142012f017b4ee07b7d52bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:02 +0000

    gather fun curious design suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
