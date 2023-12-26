6236f6a5a2396e2352ae60f4f9b33a62c0c9bdfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:13:10 +0000

    return otherwise raise interested business

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
