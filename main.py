69c72c02fd2a80b7b44700a5ad0a5a9e7d2bc77a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:36 +0000

    education disgusting fire decision tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def is_even(n):
+        return n % 2 == 0
