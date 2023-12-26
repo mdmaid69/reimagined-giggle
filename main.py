d103615354a460c3881e3c444384839e469a1561
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:25:41 +0000

    everywhere pot settle thinkof disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
