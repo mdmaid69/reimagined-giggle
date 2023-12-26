4da42fa320cd09c387727cb5c462b050ed1872c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:37:58 +0000

    want pencil loose reserve dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
