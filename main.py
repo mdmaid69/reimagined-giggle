82c091dcb73a32d217c82446571726dcf5043f10
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:57:47 +0000

    worse swing recover ear confidence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
