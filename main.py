4bf81c9a9d6f94ae2a76ad325d8a7dc2f6920bf3
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:55:06 +0000

    oil seat clever division wash

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
