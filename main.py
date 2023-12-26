689f90d88027d3b82e9e474b1f5d6d34487d2cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:34:24 +0000

    572enter other meet fond communicate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
