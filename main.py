b6aae6b83e7a311a26debec7fa4a1d2007a1ac6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:44 +0000

    die achieve extraordinary speak blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_bytes(array):
+        return array.tobytes()
