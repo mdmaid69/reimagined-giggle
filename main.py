689175bb871b5eb17e7550cd8516904cd559c9eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:42 +0000

    niggaacademy concentrate narrow slight protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
