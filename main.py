b1cbb405c838978a0fd372f7a0c5f0b31246f8ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:34:48 +0000

    taste escape her case ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def set_array_item(array, i, item):
+        array[i] = item
