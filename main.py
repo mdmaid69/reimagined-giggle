e9fa0a0bda57430d0375f493f908031ec67a2754
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:12 +0000

    567enemy without study lie search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
