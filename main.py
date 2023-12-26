d59180b1f228637173d1a55609722ca07398bd5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:19 +0000

    introduce second pin real charge

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+for i in range(10): print(i)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
