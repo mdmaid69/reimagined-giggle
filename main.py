3ce4326d50a8f3a42518cebae2015f5585223a15
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:33:46 +0000

    disappear profit cut float underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
