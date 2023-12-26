712c5fb1d34178e30d1e5f5a4928f9dc313372b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:36 +0000

    future hello rain closed board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_min(lst):
+        return min(lst)
+import os
+def get_current_working_directory():
+        return os.getcwd()
