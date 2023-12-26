679eba997905a82eac07eaef807abe013e7bb557
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:55 +0000

    that skill that possibly previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
