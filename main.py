46e347efd341485fd870929bb33e2e25da98aed4
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:34:59 +0000

    hurry understand final event march

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
