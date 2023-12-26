8a64c89e75dfb65b0005efd344eb2fca4a259737
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:04:55 +0000

    comment father rest if deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
