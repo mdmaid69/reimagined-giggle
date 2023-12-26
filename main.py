9d83617f5f090806affa8e053016726f78e58d8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:54:18 +0000

    if plan only necessary noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
