e1eab03c94c8ad51f54ee2f12b0bbafe53e121f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:31 +0000

    expensive skirt interview prepare form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
