2a76a710a371116630014779493b7f05b9b3816b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:44 +0000

    notice stay national write flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
