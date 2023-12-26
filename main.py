5130c398d10cae04349f9dc63ca7ca56c0a3ca21
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:03 +0000

    impressive height hurry soak careful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
