c213ca4fe10aed5479748f48589c6e096a86a264
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:15 +0000

    level season sale song bring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
