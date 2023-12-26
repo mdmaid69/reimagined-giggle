c1b50ea2d116e6dec2c150a59e4cce77183d74fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:58:13 +0000

    music comment bored average mail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
