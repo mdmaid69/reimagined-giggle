b496b989acfe5356858f02485c609d9f60894a37
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:49 +0000

    course whisper discuss calm whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
