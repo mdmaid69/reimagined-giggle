23705ef390a9cc8d1978e6e51d545b444ff4276e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:44:47 +0000

    sometimes shoot shout speed bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
