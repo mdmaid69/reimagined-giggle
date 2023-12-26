4206984081212d65b890550b67e19950bbae8f8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:43 +0000

    best replace tune basically reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
