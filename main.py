058e70d77ade0648cab9fd703296f7a1e1770628
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:04 +0000

    task 572enter manage conduct value

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
