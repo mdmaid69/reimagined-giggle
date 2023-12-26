8b11ed80152e3d1910e06625b0ceab928124fc10
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:31 +0000

    hold moment reasonable spring hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
