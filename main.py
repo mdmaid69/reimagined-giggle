0feb66178dd7b8dddab36d5baca60b881400d041
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:50:26 +0000

    offer rain adjective advice raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
