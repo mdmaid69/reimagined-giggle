045da6a99a59edf925760bb377e54928ac1f6e38
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:47:41 +0000

    scheme method left march thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import os
+print(os.getcwd())
