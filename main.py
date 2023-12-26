8ef25772bf0eec4a1c6ad864f55afaf77c98a7aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:21 +0000

    whichever another weak lonely tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
