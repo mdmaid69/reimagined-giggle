bc0cb4ab9b5cfbed5d163957293e1ab5e1380fb3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:18:56 +0000

    chase nor weigh any window

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import time
-print(time.time())
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
