99d5c604e37aed5f5e9b1d101ef814a681e3b8b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:36:08 +0000

    close issue his character properly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
