7316b2676f01890da805acf94df79638eec907a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:22 +0000

    seem straight rush lookaround gooff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
