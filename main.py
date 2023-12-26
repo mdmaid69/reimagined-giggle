92ffd0f8b6c03850cacb7c6cb941cb99624292fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:54 +0000

    scheme pretty case bringdown goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
