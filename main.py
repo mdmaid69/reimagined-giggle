55bbb335739c2d4bec5bd2a3dc71cd8a413e2cc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:58 +0000

    education leg fasten illness keen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
