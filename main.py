2d667e752669f2d34c47aa27b67e52cfffcf0346
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:14 +0000

    proud build waste goup brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
