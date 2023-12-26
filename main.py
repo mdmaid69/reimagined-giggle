a91e41d93a52eb97b8cb698bb4503837f24d63c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:24 +0000

    contrast murder defeat lead reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
