3fa5ff342f0a88f60edca934307badc2bf14a30a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:34:49 +0000

    conversation catch needle release delight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
