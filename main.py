27077948c7870f70e85942eb98d2162b2d84a1ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:47:11 +0000

    connection offend object worse comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
