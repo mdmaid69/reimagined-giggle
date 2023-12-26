de34197d2a13cd695cbd7594fdae4cf531c5ca30
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:42 +0000

    limit language sign curious back

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
