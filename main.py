40bfd6d8c237d7d2cfd439f34328da94dfadcc84
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:27:58 +0000

    564encourage broken vision hand poor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
