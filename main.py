837bf5a7dd2977fa607862838aa2e7ebe082b8a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:32:53 +0000

    aim negative hope reject about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
