763c204461c944662670459c73dfcffec78651cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:00:20 +0000

    crowd connection minority relief clean

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
