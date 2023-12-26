e159c8e54ccc02e57193914a204b2c9d1320d0ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:09 +0000

    word yours giveback most will

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
