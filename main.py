0c41817dac49c36861169be38f5aeeebc05ec9ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:40:49 +0000

    sell miss meet hold file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
