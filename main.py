6c87b3da9fdddd97608adfcc3986d566602cf390
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:01:02 +0000

    water develop boring pile operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
