904297e77e910d64fbfe85b7c343640d41db9531
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:06:20 +0000

    finish apart refuse such doubt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_stack():
-        return collections.deque()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
