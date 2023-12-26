254cdd2a6ffaef942154d6af490eb0a06e384340
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:38 +0000

    mention peaceful keepaway tap substance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def count_characters(sentence):
+        return len(sentence)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
