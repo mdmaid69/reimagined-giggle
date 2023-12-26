9c37bd8db132bdbdd68cd770c353ed27bd7499ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:39 +0000

    aim today shrink little importance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import random
-print(random.randint(0, 100))
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_set(array):
+        return set(array)
