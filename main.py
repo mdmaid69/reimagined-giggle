f98f89d0c6dfb701aa3d5d2bee13c98a64c38e26
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:57 +0000

    about weekend characteristic demand present

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
