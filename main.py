b2c1282f68b5990408e18f01add86b9284a317ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:39:30 +0000

    colour offer present lesson flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
