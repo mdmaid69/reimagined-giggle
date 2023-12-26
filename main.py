d0d010ac2bd8873fe4f54ad0251d34e4ae3f8a01
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:35:55 +0000

    week save forward skin takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
