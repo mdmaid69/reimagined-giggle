dcdeb900d42a02b66ba289b8480d33991d406cdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:45 +0000

    Dd immediate post have anything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
