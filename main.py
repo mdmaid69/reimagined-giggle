a08e7cff28e96e32e955c088c2540a29fad43ca8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:26 +0000

    smile boat whistle Ll understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
