df2a8de24325f6be6327aafc88d90c406d55d0cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:04 +0000

    formal 566end shock custom let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
