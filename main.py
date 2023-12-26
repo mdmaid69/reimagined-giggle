fcfeaa3952ab7e858ef069d7bb2bab7ab005bd48
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:57 +0000

    male control line border deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def convert_to_binary(n):
+        return bin(n)
