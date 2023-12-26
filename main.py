0f14a7362080e079b024456005cbb684f343cbdc
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:08 +0000

    keen introduction empty creature sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
