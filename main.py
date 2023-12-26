e99b282e91ed2d4e70533eccf4a2f0a702602ef4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:11:39 +0000

    surface independent normal turn expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
