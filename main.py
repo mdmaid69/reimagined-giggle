d87f0f03a88cb618004ecf5e9d4bb2e5d7e263ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:41:45 +0000

    book shock break comeout turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
