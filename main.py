9654459847511713912291d1ddb1709556ccfc1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:08:25 +0000

    disagree plenty vanish waste kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        return sys.version
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
