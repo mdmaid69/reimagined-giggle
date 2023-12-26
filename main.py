b16e5ff42104934df7ca5478563913ba0d1dcde5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:29 +0000

    difficulty charge to near meet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
