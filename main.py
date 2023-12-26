7cedddfc7bc67f50bb531eacf7ce24a216dc9871
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:33 +0000

    image guess hope comeback near

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
