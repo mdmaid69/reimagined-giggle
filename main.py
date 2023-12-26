d5ea263eaadad71b7ecb40651787a921b5d3dcf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:37 +0000

    school year force lookafter swim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
