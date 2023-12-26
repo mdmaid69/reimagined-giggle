7087cff7a136cfa3d9de410b582ceeb5ba4af2c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:17:45 +0000

    piece lesson maximum detail everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def convert_to_hex(n):
+        return hex(n)
