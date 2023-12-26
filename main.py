be585661c5f81368fdad5c8d5e7a5198c709aa6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:14 +0000

    tend difference left inside method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
