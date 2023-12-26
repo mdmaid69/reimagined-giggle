4975daef91adccb860246953354302b1253a95cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:29 +0000

    low rush birth past spite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
