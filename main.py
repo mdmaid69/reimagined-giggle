3bfafcc1ec017421937cebd5c659401c584befb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:38 +0000

    easy natural software average bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_item(array, i):
-        return array[i]
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
