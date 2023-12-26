e6332ae1ad80cd3719bacb5b6e41e02e95da21cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:17 +0000

    somewhere yourself wave effect recovery

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
