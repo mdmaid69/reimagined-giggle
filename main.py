ba0c321ddb5332346ce2214bc7751f4f7614248f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:26:34 +0000

    dry job bother load attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
