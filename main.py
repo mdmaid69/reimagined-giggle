1c2bc07e70cf8eb04988b059b1500a6fefbdcc6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:50 +0000

    although beat order responsible everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
