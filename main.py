5f88c6000f9191805240a8d4af2e06095f197963
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:38:45 +0000

    emotion land improvement below perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+for i in range(10): print(i)
