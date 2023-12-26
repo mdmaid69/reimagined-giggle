147a51be356e787b02c7d93918e19eb60419235f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:13 +0000

    file favourite base hate share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
