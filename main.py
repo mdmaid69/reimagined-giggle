cabbef79dca1ff8282f1c948c2c774da5cf309ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:49:12 +0000

    secret another extra squash fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
