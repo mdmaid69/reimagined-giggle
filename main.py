2314b1ca24be7a16d7bf55e12d16573164b7f2bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:15 +0000

    the how frequent tired suffer

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_binary(n):
+        return bin(n)
