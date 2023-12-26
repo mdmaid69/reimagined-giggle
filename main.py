2f49a53e11125f3159e0bfeb44869b3ebc08c106
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:29 +0000

    education create pump contact careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
