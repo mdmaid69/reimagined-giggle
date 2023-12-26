ae1c2cb81e37789e4ea479b7432d34128f8551ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:09 +0000

    which children realize estimate morning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
