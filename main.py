b68d42d8c4a247c43731a8f288aa5da4ce3f18bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:06 +0000

    rely estimate passage book pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
