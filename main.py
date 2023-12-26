9260f824cb04c2ee8b83e75ef2b57fbb07810204
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:00:52 +0000

    most by memory door moment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+print(os.getcwd())
+for i in range(5):
+        print(i)
