d8cad99a0b0422f1937696fc854a3c355b62a25f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:00 +0000

    difficult appeal breakinto keepaway country

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
