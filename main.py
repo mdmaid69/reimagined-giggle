5cc5a2384a321718e98d6f804d3b9b1478bd3f9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:18 +0000

    hour demonstrate decide correction fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
