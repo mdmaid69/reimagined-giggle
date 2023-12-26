e16df5fda290589d70528ff13ae6afe0c410959d
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:02 +0000

    communicate develop Qq level should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
