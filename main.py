ce58709dc5f1ef39ebed79980a6076131e7f0f69
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:17:47 +0000

    money figure mark large lonely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
