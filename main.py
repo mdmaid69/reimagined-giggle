4cfeb7c0abd27dfd77051ebb934e767f28454994
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:36 +0000

    frequent always trap shoot improvement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
