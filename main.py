036a29d6771cd870ddf12656a3d9d75e2693e253
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:00 +0000

    various arrangement hear look lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_str(array):
+        return str(array)
+import tensorflow as tf
+print(tf.__version__)
