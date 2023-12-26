63d6cc3f2aceb2af23d8f287964e45ecce5481b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:12:27 +0000

    complain regularly result mail issue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
