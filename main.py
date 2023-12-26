9d152b23e4d7ceeac82532c35ba07f1d34b3a38b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:16:47 +0000

    term study nigga shout great

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def square_number(x):
+        return x**2
