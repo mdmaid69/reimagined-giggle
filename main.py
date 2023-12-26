137c609dcf6ab55e96cec94be895d8e5cb62c52c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:47:12 +0000

    pen breakinto sharp workupto simple

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_speed(distance, time):
-        return distance / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
