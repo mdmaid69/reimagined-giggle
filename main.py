9a969205ccf9db5556792931034ae8b13a7e5225
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:41:17 +0000

    571enough expect club people thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def create_user_dict():
+        return collections.UserDict()
