330553db8f7498acd71c0fe9434de968fc4de31b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:50 +0000

    rubbish under careless gooff tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+def exit_program():
+        sys.exit()
