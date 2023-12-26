e17e23d0320ff0712125ac193915cbc0c9d1c10f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:58 +0000

    blame ours yet unless brave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
