eae5a5aca1d0b92053a10010e91fc52d3e4fd2f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:59 +0000

    show control mine sort choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
