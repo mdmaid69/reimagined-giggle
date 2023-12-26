f529458a6aeb72f54f20cfeba9b6a62e57deb49e
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:24 +0000

    press tape after round tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
