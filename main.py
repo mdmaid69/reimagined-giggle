9cbeb9f034da89d9b16228f14ddd210f9cfe1e53
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:38:01 +0000

    danger break callout late limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
