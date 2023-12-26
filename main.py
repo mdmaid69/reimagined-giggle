1b0bd97bcc8bee8d197d5ec7678b743880427e6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:46 +0000

    public mass remove already open

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
