10c6859af6ae129bccb4ce15f701b6d6eb7a9722
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:30 +0000

    computer late passenger in wind

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
