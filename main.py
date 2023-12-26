961c2e4fe354fda837b5643957496bc07a1ef6bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:08:56 +0000

    breakout talkinto present write wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
