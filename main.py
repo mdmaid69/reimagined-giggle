91effdccf1b43e1712bafed051c7355e077591ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:47 +0000

    pool correct laugh keepfrom deliberate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
