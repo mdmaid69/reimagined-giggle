c37920c2284d524317d04a172c690b1068b76b36
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:19 +0000

    contrast appearance job school wealth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def count_words(sentence):
-        return len(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
