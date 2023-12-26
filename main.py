abee338f7c07775d3bf9cab5ab9b9448011f64f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:56 +0000

    damp store only straight anxious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
