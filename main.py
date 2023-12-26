ab6b4a2b01195618335348f334377fadf6fb6f6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:37:15 +0000

    father thin guilty minimum beneath

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
