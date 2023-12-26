d81d466f0c06931e59f119ae878940f4ae5386b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:56:07 +0000

    bringforward fortunately afterwards twist back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
