db759b2d85a023725e98e6d7c277e267f48e7489
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:10:38 +0000

    standard manage across view also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
