959c3f4d548b29ae70b6e0c61bc2130500453f8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:12 +0000

    hello breakfast dinner estimate skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
