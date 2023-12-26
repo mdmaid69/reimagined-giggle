4b25ab9f5ab58aa8f094b741405879dcfa0e5d96
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:10 +0000

    other dress live advanced again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
