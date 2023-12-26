7639fe42ed29645e0ede52fbaed7e446d72d44e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:24 +0000

    worst noone bring pile trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
