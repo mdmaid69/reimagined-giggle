1ca5890c6a1df8fcdc3cecb4349e757e9b5642d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:58:33 +0000

    pronunciation drink lonely run help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
