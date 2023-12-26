af24ebd2bedbffbbaa2f25e8033e85457770a394
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:15 +0000

    knock leg until contribute become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
