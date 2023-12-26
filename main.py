d906fb4bac4a9991bd2b5ee5416a261615aeecab
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:10 +0000

    disappear favour noise there lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_user_string():
+        return collections.UserString()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
