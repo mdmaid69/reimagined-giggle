30d5e9a925617a0846ace5739f36c62bb4f244a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:10 +0000

    sail lookup no consider clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
