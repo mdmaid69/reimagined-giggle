59d7a6eec528c32bbc5e34901921748d98c2c783
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:40 +0000

    commit produce convince shall union

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
