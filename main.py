fe4fbddc43212cabf4c92dedca6e3f1ecda70656
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:50:01 +0000

    cost hurt call interview rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
