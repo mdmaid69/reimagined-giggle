01c3199062ec124b6ea968fe6482ec7fff3592b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:04 +0000

    secret hers Bb lock possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
