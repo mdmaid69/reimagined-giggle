8f07ee1ee98269735649d6e8da9fd81045f88050
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:14 +0000

    spot the sticky along slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
