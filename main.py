9111daf9b4c82a97b1b3504dbf4ee576cdc29abe
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:37:55 +0000

    smooth result cough search win

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def cube_number(x):
+        return x**3
