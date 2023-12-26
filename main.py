0ab1358bcb5e75bb01506e90293944fe062fb7e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:56 +0000

    check food schedule near someone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
+  def reverse_list(lst):
+        return lst[::-1]
