f3ac51a24b75121eecb1ac2ec65557d2f6952f45
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:05 +0000

    573entertainment calloff turnup suddenly sting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
