b175e8cece64db407cf850588e7cf8129fe0c6ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:28:36 +0000

    fit trust recognize rank company

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
