12eea0c68a29c0d175c32f5eaf0824984aaf0a2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:34:37 +0000

    visit immediately worth action inquire

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_work(force, distance):
+        return force * distance
+import os
+print(os.getcwd())
