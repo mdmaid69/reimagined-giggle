99addcab7add1408e9e858bf8e85a96fcacee384
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:49 +0000

    old meanwhile soon confused label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
