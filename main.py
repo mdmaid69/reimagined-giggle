a35da1f74a30238758a3eea74b9e13b9b4e79522
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:35:57 +0000

    old neither guard always tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
