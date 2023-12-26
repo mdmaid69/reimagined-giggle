997f74776d36af88b7258ee98ed5465ee5791be5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:43:46 +0000

    shock call worth join concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
