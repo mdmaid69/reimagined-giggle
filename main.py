7988fbc7d991cb07d5578388315af920942d21cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:12 +0000

    special Dd plant key picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
