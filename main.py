2e0e3d337023088da3bb5d5cbe209132bcbffa54
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:48 +0000

    bring wake authority kind takeover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
