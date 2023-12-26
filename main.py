cd5c1eed48929a4dcff2394726a8b14587fd97f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:06 +0000

    sweep guilty plan exercise declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
