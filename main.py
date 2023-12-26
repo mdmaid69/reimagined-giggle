3bb7659746ddd09d6d7a667e1e8a14d13b9fb8c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:39:14 +0000

    bit runout copy cum general

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-print(os.getcwd())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
