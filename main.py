be443cb80d29a57b2496fdc09152cdba19bbe08d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:29 +0000

    rain point amount sad full

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+print(os.getcwd())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
