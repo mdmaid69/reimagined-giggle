2fd76c279f7683e828b304c392433ef75cd19849
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:45 +0000

    wave telephone murder patient goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
