76d8cc9bc6fe4c9e282d7a339e478f74adb13456
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:31 +0000

    its dead principle goup dead

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
