13c00a4bd62325ed27294d5410be138b04f835c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:21 +0000

    giveback stare spring reach afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
