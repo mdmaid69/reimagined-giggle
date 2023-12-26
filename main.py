48814c19fe9366be417fedba64f26f0e2350514a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:43:56 +0000

    call swing search satisfactory reply

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_power(work, time):
+        return work / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
