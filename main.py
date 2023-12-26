4ff4af54e219bce4c5ecdb4ac41bbb1897420177
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:03:48 +0000

    happy stand by catch interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
