82321030ff2a0cec86226aa0b5a2dcb673bd6b0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:28 +0000

    plenty keepaway sister container between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
