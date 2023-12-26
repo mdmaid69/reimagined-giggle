49b95ebf65195b156cf233d90bab0294e4866ade
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:55:30 +0000

    last home story ready drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def add_numbers(x, y):
+        return x + y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
