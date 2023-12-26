9dbc42198685b9ce09241ed073820a81474f09e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:56:03 +0000

    circle listen remark spare wish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import sys
+print(sys.version)
