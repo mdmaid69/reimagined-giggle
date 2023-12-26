6c82acd0588b5bf59e444503bfeaa58eb1598b00
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:41 +0000

    create freeze anybody remove original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
