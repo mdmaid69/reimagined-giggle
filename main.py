b5809fd2fdfba4681ada9b385fe98a5d02e0875e
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:06:02 +0000

    about above work determined lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import collections
+def create_user_dict():
+        return collections.UserDict()
