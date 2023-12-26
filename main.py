5466e909f08cb6094b33caab3f85b89e5cdb9df4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:10:33 +0000

    minimum congratulations pretend turnup out

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def convert_to_octal(n):
+        return oct(n)
