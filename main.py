630c8ac6539f798810054fa6005f8a80bea37108
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:15:27 +0000

    with furniture influence bury hope

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
