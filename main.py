81e0340dfde66a41cef8906c80e08cfed9cb06b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:25 +0000

    limit later instrument under neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
