8d40b811b922c9d39ba652ae550f52b35a155675
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:25 +0000

    fit plenty thinkthrough rob take

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_priority_queue():
+        return collections.deque()
+  def square_number(x):
+        return x**2
