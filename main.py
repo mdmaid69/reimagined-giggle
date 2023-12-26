e5c7e9bce380b9e735a363cefa23abe0f45be40b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:55 +0000

    contact trip mess imagination act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
