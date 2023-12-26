9f30bca804c2e9d698df8880e57d4fd94e21f68c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:24:55 +0000

    before inform time reaction read

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
