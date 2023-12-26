92ed54eeb031930f568d4f644404daf0558cca78
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:23:09 +0000

    container boil tone meet transport

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
