4ac4b4cf9f453df42a4f7aba238ea3bab2668bfe
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:18 +0000

    shut sink far means goon

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
