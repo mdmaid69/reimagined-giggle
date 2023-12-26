1a12dff42aa1d7da4906099a3f7d1d4b879aafe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:53:19 +0000

    medium mouth turninto lonely impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
