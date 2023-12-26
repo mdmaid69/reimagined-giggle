991fd462509ed712ddce2b550421d77620391d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:56 +0000

    wise activity finally sight charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
