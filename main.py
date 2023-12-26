54bb69f46eb8269ec6a3404cb02ab98552291b2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:41:04 +0000

    operation welcome weekend complaint blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_item(array, i):
+        return array[i]
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
