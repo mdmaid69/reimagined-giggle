2587696b7378f6c74c8000f5d8704d5f5181f74d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:10 +0000

    previous tend no minute fair

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def load_csv(filename):
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+        return json.load(f)
