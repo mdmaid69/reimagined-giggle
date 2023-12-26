31767d30479dbf9d519e1ae877a82c121c2602bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:40 +0000

    owe lie rest bear country

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
