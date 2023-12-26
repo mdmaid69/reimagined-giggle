c48f6c03356bd0e406c172e547fc913bf8f206c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:09:26 +0000

    row hit mind open lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
