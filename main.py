28ef77ad1d64154df315a9dbd34fed1b080bd54f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:07 +0000

    Ee judge dress improve charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
