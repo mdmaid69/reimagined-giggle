7a8b51ffa9f95033fd039db4214e9f417eedd8b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:53 +0000

    yet trousers definite term lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
