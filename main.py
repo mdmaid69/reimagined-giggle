a28b58e8074431e7200ee8982f7d41b038025239
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:09 +0000

    surface genuine aim wild general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def add_numbers(x, y):
-        return x + y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
