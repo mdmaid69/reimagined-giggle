106a7af087a56f41bcd8143bb441daf593de0574
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:54 +0000

    turn whenever skill suggestion about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
