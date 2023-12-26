01fe0cd4a385664b22e85eeef9a957faae440c28
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:16 +0000

    important concentrate exercise complete somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
