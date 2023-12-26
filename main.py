ba0406cf72cd38656a11c0fb845b5cc59982fe15
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:14:19 +0000

    off strange Bb lonely watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
