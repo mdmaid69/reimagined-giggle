ffdfe52b5ec77b7781eb8e65f5790f42627f3daa
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:01 +0000

    it software limit question contrast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import collections
+def create_queue():
+        return collections.deque()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
