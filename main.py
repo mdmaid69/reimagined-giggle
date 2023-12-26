e2a5a9cec4f31992e0ee88f7b4ab1ef664aff61a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:35 +0000

    computer definite clean recognize discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
