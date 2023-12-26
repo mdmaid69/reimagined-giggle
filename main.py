23bc5157ddab9e1ce771be09b37bb13c3474017f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:52 +0000

    meet board cruel rent strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
