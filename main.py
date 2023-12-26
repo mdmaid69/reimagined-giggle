35fef3b056e868e0785fe0ca3d5e02ae386208e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:15 +0000

    live follow ourselves well behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
