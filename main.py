81e5da60f5f440e2eb97149053d7edf04464c2f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:02 +0000

    myself thanks occur extremely wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
