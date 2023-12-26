1f914bbaef39ca618c727c031aea08a726d1375d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:04:01 +0000

    speech meeting whether opponent issue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+from collections import Counter
+print(Counter("hello world"))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
