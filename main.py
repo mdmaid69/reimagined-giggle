fbd52351f397006825584d95c5f186c45c9e382c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:57:05 +0000

    nothing ought relationship note trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
