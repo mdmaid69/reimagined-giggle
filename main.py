5948d9a5815eacd3fcab0ce2a07822c5926a28c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:45:22 +0000

    copy respect perhaps music union

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
