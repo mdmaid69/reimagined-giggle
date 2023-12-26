49dbc3405b4924d7a4f7215d3102341f22099a3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:23 +0000

    frequent trouble breakup afterwards tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
