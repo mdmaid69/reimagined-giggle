903939fb28eb13bfdc09ba3b37dd33a1a1f3155f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:30 +0000

    both money fence pattern force

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
