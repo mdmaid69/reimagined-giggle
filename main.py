a24cf4e87091c2b62dcdf72f58f8db8d16418bc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:22 +0000

    body insist allow point kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
