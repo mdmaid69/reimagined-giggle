a4a7ab859e96f54328db76b31569a5ea23eb6857
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:16 +0000

    spin mean personal city terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
