c261f95ce87c8b6f98c8088c699c5bc401a757df
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:06 +0000

    visit limit vision firm circle

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def subtract_numbers(x, y):
+        return x - y
+text = "Hello, world!"
+print("Words:", len(text.split()))
