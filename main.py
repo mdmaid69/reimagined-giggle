5f519b8124651bfd6757e2b94d929b9b1a00bee2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:29 +0000

    lock expensive comfort balance view

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
