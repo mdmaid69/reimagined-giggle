78e0c56b440d1e5bbcedbd02e52b9d90c410e5d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:36:51 +0000

    pretend cinema brain as commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
