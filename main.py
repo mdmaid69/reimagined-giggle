ff5704464884d9480e1ba9e0d70826de56d4039a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:56 +0000

    aim unkind soil sink brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_priority_queue():
+        return collections.deque()
