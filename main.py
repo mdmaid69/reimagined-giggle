5f7f6c2dd3f5a99f58b934c3c67788a258f3c322
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:03:05 +0000

    this quiet underneath in wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
