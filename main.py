5abe6dcd2782ca2fac2c19d219de97d2e9ec2b2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:46:38 +0000

    goout clue only public world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
