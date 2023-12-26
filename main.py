0dd38c5b32f24816da57ebc08b0281ba02864135
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:52 +0000

    physical school dance mainly ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
