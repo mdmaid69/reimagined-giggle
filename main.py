f5940c457db7f9c2aa0ce804a571eff439fa5505
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:26:38 +0000

    area past step float turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def count_characters(sentence):
+        return len(sentence)
