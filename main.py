916c3420fcf8ca0bda6394f4531d06fbc7ce227f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:24:10 +0000

    study possible question lot correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
