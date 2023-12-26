1b78732568507f9a7d4ee767b413514b799cc105
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:42 +0000

    speak split wear complete similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
