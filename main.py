38d02a4358ff2482e5329fee91d761f96719c863
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:46:12 +0000

    influence can callat shut immediately

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
