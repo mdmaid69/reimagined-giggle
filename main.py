bf8f1ac70e9314e7b88cbe4e13e3ee6a02c5caa3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:48 +0000

    for interested give no inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
