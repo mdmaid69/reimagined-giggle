44394d76754067f0552184048a51d09efca3a8d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:13:59 +0000

    praise down address cheap dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
