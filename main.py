c72a1f10de125b2bf2bd3363f72fb753c5e65125
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:03 +0000

    combination beat refuse represent over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
