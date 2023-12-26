0c7dcc0cdc71c275db503797635c8eab4a4f6626
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:03:51 +0000

    maximum hate lean report bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
