4046208d422485efadd913eada0583d14ba34d62
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:47:29 +0000

    grow position sister check most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
