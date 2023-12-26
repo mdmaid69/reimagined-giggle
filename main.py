1d6025d54a5c03aeda2f665038796f120bbc3c11
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:24:11 +0000

    usually outside various middle afford

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+def sort_list(lst):
+        return sorted(lst)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print(os.getcwd())
