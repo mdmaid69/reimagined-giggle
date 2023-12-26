29b63641e726683b4865c89530478ce37806e306
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:21:01 +0000

    partner closed target container pick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def is_odd(n):
+        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
