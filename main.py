af808febff34c18fe5a79f28bde241b1d3bff213
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:43 +0000

    kind lend possession repair object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def find_unique_words(sentence):
+        return set(sentence.split())
