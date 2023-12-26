f19385de779c5db162529b9eab836f88d1834035
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:21 +0000

    give alone nigga when turnon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
