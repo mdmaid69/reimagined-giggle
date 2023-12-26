23b3d84bff237309a8e45793435ae87e28b56fcf
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:50:37 +0000

    reaction keepoff consider quickly information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
