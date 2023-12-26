b1ef056baf6ce8d942dfae7ea3bdbb522bbe79d6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:48 +0000

    feel level gap traffic go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
