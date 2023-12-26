389cf15f6543dbf4add42b308bf7537e2d1d17be
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:45:48 +0000

    slightly shape yet lookaround eat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
