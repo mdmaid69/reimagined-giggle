8d49148b7ae2ece33e0b0e572e0e9cfae8cc5328
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:40 +0000

    impressed mark anywhere lead like

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_time_since_epoch():
+        return time.time()
