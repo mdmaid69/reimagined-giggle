2fbff968fe5601597e25921289b8da986d7443dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:05:18 +0000

    normal join cruelty like skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
