95373779810aa4254ed1e4cea8dfa8393a3c28ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:14 +0000

    middle crowded pick bad suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
