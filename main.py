9abaf8729da62b1d4d4a8beae52176792688a658
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:32 +0000

    fast yet sore pronounce unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
