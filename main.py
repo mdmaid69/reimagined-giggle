cb7e6106e39f0232302c90313bb4207eac36c3fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:05 +0000

    in extraordinary Zz party perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
