62f421a7b9df14abb45541a34e9d76ebc34c4deb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:53:33 +0000

    train blind takeaway letter relax

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
