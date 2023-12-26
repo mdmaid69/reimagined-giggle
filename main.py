104db66d69fb19eb659ec22d764a78c9109b4b18
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:27 +0000

    undo not pencil spread valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
