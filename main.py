9621e3bd89a1aa01f800984c9cd333a4c822e39b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:49:27 +0000

    prize town worse today boring

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
