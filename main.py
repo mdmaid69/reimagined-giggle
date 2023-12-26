cc78c44a976f149dab0165da34738ce7e5d9da73
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:41:55 +0000

    crazy he actual about relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
