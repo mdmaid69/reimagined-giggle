ff21aa0a0357899af50ef56d656764036a8ec36d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:42 +0000

    defeat clean side top forever

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
