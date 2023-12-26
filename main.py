04f67bc7a6e863e5e614bbd9186559c0befa84a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:16:28 +0000

    on shut child measure able

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
