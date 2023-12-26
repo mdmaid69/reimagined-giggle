cefc1094a436b48c699ffc0fe0c04834b4c3caca
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:52 +0000

    getback pour back slide powerful

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
