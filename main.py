10c2eb2d8e622a7bfb83fe42b064faa59dae078a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:58 +0000

    declaration circle sink moon pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
