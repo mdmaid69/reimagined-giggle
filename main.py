07b69f18cf344c0364ceae660330c66e93badb69
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:03 +0000

    visit holdon file content act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
