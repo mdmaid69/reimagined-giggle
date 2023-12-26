15c334b1eb1b7d9402f0960ff41c983c8a76b001
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:13 +0000

    stupid authority necessary which tour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import sys
+print(sys.version)
