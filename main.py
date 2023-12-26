5b0403124797586f80ebfee104f842cb43190d3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:18:38 +0000

    system fight paper you nice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
