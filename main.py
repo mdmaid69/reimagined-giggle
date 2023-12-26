e64ece7d00596c05893cdeb39a0f09c585ed1eda
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:34 +0000

    scrap tap scene cross opportunity

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
