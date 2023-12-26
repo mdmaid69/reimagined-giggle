b7ee670bf66c63ea5dfcc5b2bc0332b4a9d57563
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:38 +0000

    couple dull fall Ww what

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
