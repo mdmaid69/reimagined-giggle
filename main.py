e5725ddc47215d851198bf672beb4bfe850894fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:22:33 +0000

    house thinkthrough treat thorough honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_array_length(array):
+        return len(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
