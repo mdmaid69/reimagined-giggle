b573da4b210e202ee9519b689f6cb4664c449a0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:02:29 +0000

    spot behave announce seem responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
