37cd1d59a6ceac697d5de665f77c8d32ebb090b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:17:25 +0000

    school still favour relation shake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_pressure(force, area):
+        return force / area
