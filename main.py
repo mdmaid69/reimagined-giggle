0886e45353e234b4d1c18beeb8a3d530ce4208f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:35:21 +0000

    pain us disappear error go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
