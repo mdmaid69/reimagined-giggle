6e168bf04aedaf107a4392e3f1b7160de56b5ef7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:07:46 +0000

    move news talkback mass free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import sys
+def exit_program():
+        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
