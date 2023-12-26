566eddbd9754f7705e69d58854e56ea289f7bf51
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:29 +0000

    church deal judge along risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
