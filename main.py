b7c1eed54c025a61eb0bbbbb8f3e8a4878ef43f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:05:50 +0000

    brother stage number tread protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
