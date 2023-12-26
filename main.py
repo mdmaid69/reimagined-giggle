9af20ec0856bb11367530ffae1f14ce666df444e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:20:54 +0000

    independent deep copy 564encourage have

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
