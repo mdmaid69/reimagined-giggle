a71bba6fa7065cc4b9c1f84f99b422d287c1838f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:34:27 +0000

    fortunately travel glass strike period

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
