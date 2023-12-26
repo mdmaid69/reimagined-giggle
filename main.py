ad11fb4cc5bd55fee04621973c383b24cb044803
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:39:58 +0000

    taste set though ought joke

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
