fcb7f6e7ba6b5f705eb75375061129bf7c5d3e57
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:28 +0000

    pain familiar regret turnout meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
