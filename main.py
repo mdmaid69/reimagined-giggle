c94d749b44cce691f8e6d23284a09dbabfea575a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:06 +0000

    underneath present shut separate police

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
