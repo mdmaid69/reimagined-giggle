0f3bc7c24409ce09b7fbeb8ecf97a59365b16050
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:37:42 +0000

    stranger pollution cause child height

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+print([x**2 for x in range(10)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
