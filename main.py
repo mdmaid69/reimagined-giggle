70a2c9bed2af3972b6f4685ee94fe1e8b5fee920
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:04 +0000

    fact risk still claim favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
