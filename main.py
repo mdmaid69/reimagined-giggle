8fa649c4252aba567bd131f3c857434bda4b43c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:59:30 +0000

    expensive secret fence return niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
