a7c6332b2e6922d294460097a73633d080d8773a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:43:22 +0000

    drive weekend them juice brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_min(lst):
+        return min(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
