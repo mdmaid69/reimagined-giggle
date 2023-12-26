381c0043539f77cb1ca0e132adbb356749805323
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:12 +0000

    getup justice 573entertainment talkback style

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_current_directory():
-        return os.getcwd()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
