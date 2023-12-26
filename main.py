bd33be5dc1837c01259b07fb8f9cb9b41998df16
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:15:49 +0000

    stop rest fortunately react hurry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
