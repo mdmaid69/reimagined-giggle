d80ff28c1b4967fdb371702f2a0c03894f8def63
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:20:55 +0000

    home comedown say careful just

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+print(re.match("h.*o", "hello world"))
+def find_min(numbers):
+        return min(numbers)
