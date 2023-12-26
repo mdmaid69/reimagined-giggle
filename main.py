c534482d4918ff0963f852c3814607624850eb05
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:39 +0000

    record handle goup hope block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
