d63cf45ba728251baefc09b8fa21ba681447b74b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:12:05 +0000

    rush communication burn challenge attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tensorflow as tf
+print(tf.__version__)
