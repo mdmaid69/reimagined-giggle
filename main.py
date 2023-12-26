ead07805f0ffbccf9ea4937e42471b8b487816b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:40:14 +0000

    subject change suppose supply expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
