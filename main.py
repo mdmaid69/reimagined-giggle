977457c778162735ac914941d861d5c4663d8abf
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:47 +0000

    unlikely reply stand past become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import sys
+  def get_python_version():
+        return sys.version
