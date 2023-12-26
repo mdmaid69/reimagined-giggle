54352ca6841db9aec5800645cf2064aa45692f4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:17:02 +0000

    wonder Uu complain bother sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def find_min(numbers):
-        return min(numbers)
+import sys
+print(sys.version)
+def convert_to_hex(n):
+        return hex(n)
