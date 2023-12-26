b05f5ae4a3f5ffca82893e7fae460c1cfc0ccc4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:54 +0000

    at spot obvious hers work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(numbers):
+        return min(numbers)
