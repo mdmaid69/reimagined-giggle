6d2c611d33f2796a48cbf53125eeb2528ff6bd0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:52 +0000

    discovery view push opponent prefer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
