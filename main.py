c9c3ed5e6e7f248e1e75358c1e9b9382d092ba8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:39 +0000

    undo cap second gift fortunate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
