21e91a2b94c456b8524c787a5cfbbcceff140998
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:30 +0000

    regret sign somewhere 179bite swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
