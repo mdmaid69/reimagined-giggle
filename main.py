d1fca283e865ff3a800d2298ca26fc40cc7676da
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:03 +0000

    estimate cover risk instrument still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
