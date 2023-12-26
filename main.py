455e79d3cf65436c6f12299470a7ab69ed9dcaaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:33 +0000

    bite put grateful opponent then

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
