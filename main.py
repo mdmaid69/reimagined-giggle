4bad2b9bcf6ec9ddce819b9b5edeb337dd50eb55
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:53 +0000

    mine settle serious heart himself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
