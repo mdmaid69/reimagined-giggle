4c7679b4eb30f1a857049a92ad97c894fc359870
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:48 +0000

    trade consult rubbish row ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
