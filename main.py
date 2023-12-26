44c699161b43d1e86bc9a00cd2cc8181c2f5ec21
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:35 +0000

    behind awake kind however begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def find_min(lst):
+        return min(lst)
