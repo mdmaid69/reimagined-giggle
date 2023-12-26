186bec8c0812fd14741534d9bbc2899b804a9347
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:41:10 +0000

    ready track community comment secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
