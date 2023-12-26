88976c6348d67139fc63cdb147243f2a4c2a157c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:57 +0000

    godown commit far style save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
