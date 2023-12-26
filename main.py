ccf73863d8f0047e767be6a0982f2721a2ef94d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:27:08 +0000

    its dry favour opponent comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def sort_list(lst):
+        return sorted(lst)
