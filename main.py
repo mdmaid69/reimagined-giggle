0bf97a631267a208659e30915799c23f9d1e551c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:02 +0000

    before mistake neat reduction prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
