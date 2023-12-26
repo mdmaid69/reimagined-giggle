232cc6a20ee0b59e6f8d863c98b3bb213f3b3d58
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:39:20 +0000

    avoid design perform plain sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_arc_sine(x):
+        return math.asin(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
