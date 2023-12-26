7650dff42a48b267aae8f9c8a78e5deebcbc6196
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:13 +0000

    start bush production flight fly

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_floor(x):
+        return math.floor(x)
