5d56a92c554dd2cfcee501c2fa15708dd5fe785d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:16:45 +0000

    rich high broad constantly his

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
