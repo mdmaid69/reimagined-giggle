e4514438fdf8dcec83cefb528530da2ce53f1f1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:28 +0000

    mind bath loud roof afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
