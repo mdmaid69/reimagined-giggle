b5158e0fa893a6b05082d43063a46d7e2b12f92a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:57 +0000

    dish identify instead decision only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
