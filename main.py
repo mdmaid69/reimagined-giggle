dd45dcd9704da692a20ec3065140150d2171d7b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:43:36 +0000

    process sale place level mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
