862c65426229834656b1ae535ef29869a5c763c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:35 +0000

    pinch wheel concern concerning develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def sort_list(lst):
+        return sorted(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
