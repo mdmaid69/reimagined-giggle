02217cbd83a054744e2178d5b222df8094093a6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:55 +0000

    touch reason Gg join flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
