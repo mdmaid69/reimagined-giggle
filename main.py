eb26407a2a18be6d2082abc83bf727df12e5b271
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:57 +0000

    hold thanks instrument mention public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
