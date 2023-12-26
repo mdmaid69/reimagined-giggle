b9ddb76add1a7b2d863e4edb3e2bf50abd8f0fbb
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:41 +0000

    hurry path few flat round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
