48e1e2e0feccb3e5b58e4139c3257657220faadf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:43:58 +0000

    cum certainly swallow raise future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
