a31dfffe2c1f7772fe3d94f72304744dd6c59764
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:14:48 +0000

    deaf scared tragedy married rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
