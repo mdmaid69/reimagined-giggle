da06a11614558d954b942bededd8bbaec4cae564
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:23:39 +0000

    glad slide shock should tread

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
