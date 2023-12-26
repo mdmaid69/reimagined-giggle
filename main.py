35d6f3e53183190cb59dcdc9af987d3eaaed60d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:48 +0000

    Ss left by what touch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_power(work, time):
+        return work / time
