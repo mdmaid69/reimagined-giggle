366bf56620665d04543bb59e48cdd13eaa14730b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:28 +0000

    wet impatient feed high inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
