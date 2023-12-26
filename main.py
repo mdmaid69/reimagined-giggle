cafac05adbc4c5aa935e44b2c7ba93ddd71d0f25
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:53:11 +0000

    chase below quickly could cry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def remove_duplicates(lst):
-        return list(set(lst))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
