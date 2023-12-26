c042020cb588667c228a71f7169d81b91c6dab92
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:37 +0000

    interesting use break tax finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
