d18fcbc844a22bc024f6cac668291089deb98175
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:28 +0000

    steal physical hate love 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
