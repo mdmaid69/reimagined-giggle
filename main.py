c4717ccf748bf33a250b5f8aca39b5fc405ecf59
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:46 +0000

    deal awake breath several principle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
