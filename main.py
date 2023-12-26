82dae7af85881406bf72406b472cda5a214b6363
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:02:12 +0000

    mistake reason thing foot his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
