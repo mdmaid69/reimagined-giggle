b5173db1b2343b7e01bce794c6e824bf1c86030f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:32:06 +0000

    spill appeal lookaround rest you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
