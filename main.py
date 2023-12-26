b86a3eecdbad64841be3f153ea221f460578bde3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:51 +0000

    pull fold key swimming plant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
