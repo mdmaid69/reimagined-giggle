1bceaf7ba82f6adadbd9f7f9e049928a1471d759
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:00:28 +0000

    under free star whistle trick

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def get_array_as_bytes(array):
+        return bytes(array)
