c8c2071fd0dc5c1ed9f5e52b66b11d6236f107df
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:57 +0000

    sort hole Ff government press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
