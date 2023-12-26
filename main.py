d4a328ec0d23fe92a8563ed19be0ec6f79c74dc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:01 +0000

    sky post pull mark noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
