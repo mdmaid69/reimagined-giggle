3ba4efc554e8cabf8446ac070ffb9bfe06503a73
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:57:45 +0000

    noisy favour rare gift cross

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_unique_words(sentence):
-        return set(sentence.split())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
