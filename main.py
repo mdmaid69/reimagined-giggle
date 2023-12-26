6daa65b71b680e98cbde85ecea011d2af53a18dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:42 +0000

    punishment brain on shape quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
