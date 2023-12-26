79a0b08f07e741644f7325b8c29995c8f73ded0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:20 +0000

    179bite change anything stone less

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
