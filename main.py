9fc3040d07380644a33ea3d6526bd0dd66f34546
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:38:49 +0000

    pronunciation number used branch slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
