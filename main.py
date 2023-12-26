139cec47702935dc1946cf5f0ae9a462fe00c9ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:48:42 +0000

    work only pocket guilty bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
