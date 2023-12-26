a78167f0ab6f4911a8353b66bd42952cdb3f23a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:09 +0000

    custom rain your public empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def convert_to_binary(n):
+        return bin(n)
