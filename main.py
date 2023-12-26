00beafd0476d656051a274681f546cfd1b788b15
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:09 +0000

    possession lead depend lost stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_itemsize(array):
+        return array.itemsize
