85c88f32f945f72fb7b317cb2b38eda1e0fa1928
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:12 +0000

    workon cheque clear standard rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
