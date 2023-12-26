388ed3238df24d5b824c9435ab6809fa2b7ca022
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:21:25 +0000

    dull train concentration question scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def remove_duplicates(lst):
+        return list(set(lst))
