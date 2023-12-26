625c79fe93043231eb4e71381a7027387eb16f3c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:43 +0000

    model never majority near tie

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
