f1cf0e04dd5a18d33270e7c98ee09317d2d31306
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:02 +0000

    risk after union visit claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
