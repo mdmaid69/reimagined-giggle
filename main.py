b0a0e171516cf6b72b2367c68dcff7fa50fece1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:19 +0000

    load identify Yy indeed embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
