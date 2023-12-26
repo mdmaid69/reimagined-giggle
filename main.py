f19b24c06f6b9dc1fc0705cf71a7cb84c14a3535
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:43 +0000

    limit treat up bag edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
