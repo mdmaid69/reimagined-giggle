21f027063b535ebd91f759a20998bfd17a6dfa24
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:36 +0000

    brief characteristic top proper cry

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
