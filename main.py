f31575a98368ff96b3a33bd27448672dbdb22e95
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:05 +0000

    act such range travel calloff

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
