22447cdccfba0c4906232ba5988dbb7d63d334b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:55 +0000

    lead ought road lookafter square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
