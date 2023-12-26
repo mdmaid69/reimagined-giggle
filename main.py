9130a60b9bda166d26322f0cc75836879f74bb6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:15 +0000

    connect fire distant measure plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
