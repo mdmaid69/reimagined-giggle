bcc6fa57bec8819fb28ab8f60dfab2d8c6faa7ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:09 +0000

    journey considering past situation possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def convert_to_binary(n):
-        return bin(n)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
