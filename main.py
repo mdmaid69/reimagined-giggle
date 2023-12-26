e1b6f21e0e26200c31e3bafc8dfda1abb418093d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:26:53 +0000

    rob own usually pollution degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
