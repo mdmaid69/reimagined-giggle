c589edc44ed86c67d6c958c88dac7a05490f3260
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:29 +0000

    type attempt condition manage information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
