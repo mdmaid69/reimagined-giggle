eda32f5523d37e974f96415743d15d0aeb139760
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:25:04 +0000

    copy out address duty test

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
