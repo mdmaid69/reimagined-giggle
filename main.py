d4b22c07ce0b260d3490f6932589ea94395e99a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:03:13 +0000

    oil notice shoot occasional repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
