7c95bff3f982566aa4736d1fb2a588202da65b8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:16 +0000

    begin in still probably expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
