0353f9f080b9b4796cc27f6806321b54796e6e35
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:32 +0000

    sensible building roof share balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
