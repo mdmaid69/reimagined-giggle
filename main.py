05b759c731865a8ff470bcd8e9472ce9f0ee163a
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:51 +0000

    everybody embarrassed print quantity bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_odd(n):
-        return n % 2 != 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def create_priority_queue():
+        return collections.deque()
