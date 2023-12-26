414b0e839ec4e9f6fb5efbb74150ffc2af2f4a8d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:35 +0000

    hide begin style cross right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
