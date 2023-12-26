152b22c1bf443b434aa09215d28de04dc05bd4af
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:33 +0000

    sink interview shock Ff whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
