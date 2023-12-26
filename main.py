89110e0a4cb1e3d278c117edf5cad0e8d0308d16
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:39:45 +0000

    entry side bring building nose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+print([x**2 for x in range(10)])
