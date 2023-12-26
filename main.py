c223e726ca37ba1fc23125e6cb5aa05ff6443301
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:11:45 +0000

    statement foreign game sell true

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def greet(name):
+        print(f"Hello, {name}!")
