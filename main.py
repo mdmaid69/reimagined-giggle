0e983f91546402d903cc0d684c0db9c9fcb2aacf
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:02:58 +0000

    mystery protection goback(to) neither combination

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
