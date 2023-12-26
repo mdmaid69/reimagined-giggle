06b6d9ad084a57cea39634473e9bdd2707269e6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:51 +0000

    contrast fit steam demand lean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
