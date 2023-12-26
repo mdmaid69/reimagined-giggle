4fcd614fac74a66a5161486de786bf9b3b0e5f86
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:13:18 +0000

    570enjoyable later wheel back mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
