2e6ee8dd6aaa3c89cc2dbec0e72a80be5f8d09e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:51 +0000

    material day dick exercise piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_min(numbers):
-        return min(numbers)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
