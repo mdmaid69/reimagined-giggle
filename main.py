8c951d6449d5085c7341a41b7fc3f0a8c964c53e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:36:23 +0000

    period wherever community building go

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
