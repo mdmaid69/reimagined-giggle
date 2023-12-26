ea7f8a725a42d28d5bb90f3ac9a3c18b63b71912
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:23 +0000

    note time spot silent lunch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
