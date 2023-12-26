2621411d7f23aeca64c9b6f29ca52156e66c372f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:37:18 +0000

    shock persuade check convincing nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
