c4678f0ec2b782daa0f3087ede9947d94d08a4c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:02 +0000

    method several failure straight duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
