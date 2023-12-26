ccee4091aa20e5fd972ee3439a7e2b3185b0183c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:30:39 +0000

    door usually pleased every emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
