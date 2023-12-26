6b2868ec3b9f8e53ae2f358498ac1ee9ac6916b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:13:25 +0000

    reasonable cause common section lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
