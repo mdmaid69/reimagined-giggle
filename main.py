7f9c9c7a6046f7864a1cb22169ab7c2ae45fbd4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:41 +0000

    class hold lookback(on) future fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+name = "Python"
+print("Hello,", name)
