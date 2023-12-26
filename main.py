b181316904e800badf09a26b059ae1706991a38c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:42:45 +0000

    join Ww circumstance seat set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
