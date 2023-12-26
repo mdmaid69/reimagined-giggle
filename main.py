9bc300233c998d295e7ba58f81fb8fc12a251a66
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:21:38 +0000

    shop check shoulder market preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
