45800761aa68a27b6e48ab3443bffe8202083ce9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:16 +0000

    likely heart involve that foot

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
