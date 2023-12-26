dc47c4633a9d4bc55791d8037f73550b6780e508
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:57:45 +0000

    wave accidental imagination improvement concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
