803300d6bb9a6d43b56d724cc865348955474029
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:53 +0000

    exact file grand takeoff correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
