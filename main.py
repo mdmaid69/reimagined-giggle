b0a589cb82a44c5f93965f93aa57eb076869cc46
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:03:19 +0000

    ilovedicks handle connection difficulty trust

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+for i in range(10): print(i)
