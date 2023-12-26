68ded72aa06c0c6b6aa71302e69a61f47addc2a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:55:44 +0000

    noone suddenly child promise thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
