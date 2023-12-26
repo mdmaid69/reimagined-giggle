c114194e1abc55ddcc06b2083017c591b07beda5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:35:46 +0000

    best squash zero prevent past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
