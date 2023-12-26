aa6ae10c153498f8a7a447705355e44f0e74941f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:11:47 +0000

    work complete harm pipe judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
