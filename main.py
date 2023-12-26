3cc34447c3d6ea642dfb5313a251fcba723a5df9
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:03 +0000

    mistake let total speak feeling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
