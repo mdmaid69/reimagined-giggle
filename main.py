ab6c25305c57642b64148f28ade0880759415709
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:34 +0000

    best display himself wear study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
