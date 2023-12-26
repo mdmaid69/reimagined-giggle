cf9cce1adddc943078562b950576fbdfb7846f0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:21:53 +0000

    relief slightly theirs finally warn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
