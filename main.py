47699e6544750d404401be9a8627cf5f164df941
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:02 +0000

    embarrassed ball consist part direct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
