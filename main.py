c88b1e0216f2753258b903605a2f35a71c1bda65
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:36:38 +0000

    impression official owe exploration pretend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
