180ebefd8a1b22b2b303edcebe5dec084c264aaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:42 +0000

    split solve proud market split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
