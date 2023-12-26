1bea2ea6498db6011ca0629e7271c1dab6163f48
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:38:19 +0000

    town fill year defend pretend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
