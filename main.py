81c79c897b5fa8687b6c3c892ac83e75fd94e8d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:06:55 +0000

    along bet fat fold anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_min(numbers):
+        return min(numbers)
