666e6d0ecfc6347201e125ad08e001c66c8ef4b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:36 +0000

    get lost over fly pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
