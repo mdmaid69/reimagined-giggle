80919e3b948f18b92c1721b3e548f2d63bb978e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:11:42 +0000

    pollution we hit patience lead

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def is_even(n):
+        return n % 2 == 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
