f3e38dccb8c5ddfe6bf0db9bcce8e843a04de9c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:34 +0000

    think delicate brother office phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
