1c4bbb0363a8d23cbd71e3007dc8e1eb60b120bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:07 +0000

    story search cap study worth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def convert_to_hex(n):
+        return hex(n)
