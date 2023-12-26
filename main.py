814977c20a5caea16af79f93e320ddb3ab750d7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:06:03 +0000

    soak breathe frightened become pleased

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
