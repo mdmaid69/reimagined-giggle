0f13439f9398f809cecd9bdf64c521abeaca5b6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:24:11 +0000

    abroad comeoff neck path crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
