4eb09b6859edc0b16ffdfe02bdbe02acba5d2608
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:58:04 +0000

    refuse house myself loss announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
