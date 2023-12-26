2d0a5f38ea47289b91556b294fdab0dc2370f80a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:36:51 +0000

    block simple expensive interview sad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
