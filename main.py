a465c3a3f11723cc391b9341456dc432912cf4ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:59:25 +0000

    connect by track stare list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_counter():
-        return collections.Counter()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
