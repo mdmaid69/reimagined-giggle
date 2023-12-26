76b0cdaf1be881969210d08ba229e9f1f92a4619
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:47:21 +0000

    precise doctor either makeinto I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import platform
+def get_os_info():
+        return platform.uname()
+  def sort_list(lst):
+        return sorted(lst)
