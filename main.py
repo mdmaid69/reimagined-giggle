a1c50f5ededeb938ab4d392ccd492355b5c1d4a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:58:57 +0000

    use Ff uponon evidence bite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_work(force, distance):
+        return force * distance
