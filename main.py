cbf99b6bf6f9cba324466aa74f4e3b6c53fc5f31
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:20:54 +0000

    quantity shout middle war schedule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
