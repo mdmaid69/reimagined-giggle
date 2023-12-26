a54ecd142fb9c2e8c0d51cd566ff060d34ec30ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:16:54 +0000

    spare settle further sorry holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
