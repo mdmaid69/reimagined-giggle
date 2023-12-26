febe4be965c2a310e2f5c9bf91322b56409b6e52
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:46 +0000

    young city act relative my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
