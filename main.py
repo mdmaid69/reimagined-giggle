d45018d3c00843ecf03665d14fa9f249afcfea9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:23 +0000

    section stop schedule police consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
