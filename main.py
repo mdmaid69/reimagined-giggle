160fcee415afba354d06d4b21aa424c7903e7151
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:37 +0000

    hello best own match hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
