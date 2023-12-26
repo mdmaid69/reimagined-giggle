a6994ddf1dc8f3b0e84a66bb65972b02a58d3c0f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:58 +0000

    getaway well unlikely medium turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
