580c533f25899ff209ffe1abef6170a5e0c45890
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:43:48 +0000

    value occasion raw square trial

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
