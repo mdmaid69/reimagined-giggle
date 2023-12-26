afb75c925df793fd8d9a46ccbf7357d6813cd2a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:46 +0000

    find rich short quick sometimes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_exponential(x):
+        return math.exp(x)
