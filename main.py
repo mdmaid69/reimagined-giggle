cc38517c8f40028a79069270ac0bcf62b8d44739
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:17 +0000

    difference thick give along succeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
