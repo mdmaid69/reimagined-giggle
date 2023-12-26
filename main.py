01e703ea3374dcdcc7b63354fe14ddb56bb45aea
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:30 +0000

    comment difficulty view manage food

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
