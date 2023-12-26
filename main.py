55f21427029ed25c8b357dd63f412800b3c9efda
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:37 +0000

    interesting commit increase complaint statement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def subtract_numbers(x, y):
-        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
