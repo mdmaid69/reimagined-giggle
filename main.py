8b849c0698e4c7a438ecd31c09aed5fbb0f53a2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:53:00 +0000

    march share custom division pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
