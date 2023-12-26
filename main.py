908d0c96f41559b06f86795fc7204cb97c9fa9e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:14 +0000

    force callout at position even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
