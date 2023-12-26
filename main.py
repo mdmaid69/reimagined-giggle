fe615f6f5e7c4cadaae44801347231dbef7de85a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:04 +0000

    simple gradually branch wait put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def remove_duplicates(lst):
+        return list(set(lst))
