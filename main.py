33044cbfff59da8878f54beb4d97034b1289cda1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:40:41 +0000

    up bored fair bet cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+  def is_even(n):
+        return n % 2 == 0
