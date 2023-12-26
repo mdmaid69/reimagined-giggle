2cc0165ca186caddf5af928ec9984bab4f1f7b0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:34:45 +0000

    sad obtain hour offend angry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import datetime
+print(datetime.datetime.now())
+def calculate_pressure(force, area):
+        return force / area
