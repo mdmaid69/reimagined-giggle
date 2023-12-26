b89088bea88d88c7cf3c2916c95d5d4ec7586f09
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:27:38 +0000

    usual could degree smile dance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
