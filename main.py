dd911bb6097a79040cae3506336ab8e1debfea4a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:48 +0000

    ring loud unhappy trust workon

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def multiply_numbers(x, y):
+        return x * y
