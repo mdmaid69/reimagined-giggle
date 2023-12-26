eff3333b2b073d861a9abb403e336e773326d5e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:01:46 +0000

    recover possession wheel finger possibly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
