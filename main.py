38565777715dc7f9087a769ebb8e4b72293a7273
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:33:52 +0000

    now exam break undo original

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
