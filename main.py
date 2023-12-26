f40379957481ff4dffb5aaae757bd7a28fa31dfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:44 +0000

    exaggerate wash smell sleep pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
