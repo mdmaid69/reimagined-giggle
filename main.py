d4a4de3b0edb88e6372b8ad81ca5309a35c28a12
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:56:51 +0000

    occasion necessary correct smell flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def create_stack():
+        return collections.deque()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
