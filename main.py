a76e611bdc2cc01797fffe1b6feb16ddd05abf99
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:22 +0000

    estimate service least personal border

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_area_rectangle(l, w):
+        return l * w
