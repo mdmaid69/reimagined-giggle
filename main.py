254b68078c8bd1c7809fb9b6fe92efbc6befe7be
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:12:54 +0000

    yours giveback pity neck crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
