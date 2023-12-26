1d117735960a570f24414d7df172d15b4471154b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:27:41 +0000

    lead future foot guide dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
