17e8efb77db5a2f9a7478b7ebdf0ac4ca9bdbbe8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:41:31 +0000

    distant wash interested foreign important

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
