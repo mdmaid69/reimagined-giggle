3c2a48275208563b482fb3e649668f746e810d14
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:01:57 +0000

    appearance town fear carry neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
