c81736ddcfcb27b3172349b712513f6b109e94ea
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:27 +0000

    experiment bother swim exam eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
