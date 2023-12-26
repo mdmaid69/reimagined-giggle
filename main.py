baf42d7868d48bee730b90c0fe4f6f60f5e0af54
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:33:50 +0000

    earth record compete anything tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
