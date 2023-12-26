757737157c565f421eda545bce5f47313de341fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:32:45 +0000

    tidy experience peace temperature calloff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
