b6f6ac51fe6dc7d73253163ce24acddb4d1784fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:57 +0000

    exact development write lend subject

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
