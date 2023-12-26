b90620354e2cc760d8fc6b2ca95311117c5685d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:59:53 +0000

    spin tragedy file little complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
