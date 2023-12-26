e03c512ea5ae792748d1da3451eacaa2fd7504df
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:20 +0000

    relative comedown live onto dream

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def reverse_list(lst):
-        return lst[::-1]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
