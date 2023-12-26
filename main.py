1f918a1cd8817a82db076837e0e6d80458e339a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:23:00 +0000

    furniture organize disappointed treatment top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
