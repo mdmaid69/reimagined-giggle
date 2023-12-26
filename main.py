b18b7aac1055fc9cbbe1e60f1e7a8a60eb8f4559
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:45 +0000

    port clear machine finish early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
