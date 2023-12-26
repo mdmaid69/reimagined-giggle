d1b4772fcecf0007fd3bf3db24453b4fd0cc9d61
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:32:59 +0000

    injury innocent national slide bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
