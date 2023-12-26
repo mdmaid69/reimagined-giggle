66bbb54130990cab6c014a1906611d9307bcca23
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:23 +0000

    trick sport reach interview intend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
