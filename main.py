ae0f6126e69d747d4be8f5ca8aa76400beb95eac
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:50 +0000

    tonight scream their market record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_length(array):
+        return len(array)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def set_array_item(array, i, item):
+        array[i] = item
