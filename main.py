92dc9480dbbb0fd541ac345b2736434ff2684452
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:14:04 +0000

    crowded future strike tap like

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
