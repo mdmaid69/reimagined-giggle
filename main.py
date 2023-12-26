02de76156916d91b8887cbfd8ddb91d70f761c86
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:44 +0000

    control trip 564encourage scream together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-print(sys.version)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
