9922584203c3aacaaf127994659a4c661a22b1e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:22:42 +0000

    ball season bag grow practise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_min(lst):
+        return min(lst)
