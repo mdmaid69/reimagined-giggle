186a520ae25a311efcfbd6f695ec7ff8db8b6f2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:07 +0000

    defend reply before wet present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
