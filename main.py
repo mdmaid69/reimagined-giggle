890f8e1b3b5a322ac73e401336cec3faee38bcc5
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:27:35 +0000

    means heavy future opportunity should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
