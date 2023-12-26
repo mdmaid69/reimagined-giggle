f8481ba2495b26747c68c463a2ed7e0a1cc4664d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:16:41 +0000

    contain order error new weak

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
