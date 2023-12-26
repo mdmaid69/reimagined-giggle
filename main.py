e4e78db1efc05fca7d32fe103453934eda6c67ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:43:24 +0000

    wear apply worth that wipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
