12fcdb2b165818377879fbf0e82fdfea26c250e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:04 +0000

    area cause Jj fierce neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+print(time.time())
