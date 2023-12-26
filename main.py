0fa8214d7a3401a2fc748631f817f9a9fec8239a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:48 +0000

    deep probably another forget operation

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
