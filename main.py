cb66c6a902859da98cbf2bd3ba82d189a84cba1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:07:36 +0000

    cross shrink gap address brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+for i in range(5):
+        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
