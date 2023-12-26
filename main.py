8e0a4a6434599ab9b83fae0adae3079fd796e089
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:04 +0000

    shout question bar live frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+from collections import Counter
+print(Counter("hello world"))
