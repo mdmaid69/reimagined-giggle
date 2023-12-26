97cc241d8e4300b03cbe076b8f2e56d199ff026c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:01:54 +0000

    respect become grand study negative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+print(os.getcwd())
