40f3ce4c7fd83da45b5cd99e10097b9574c6ce23
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:20 +0000

    opponent ring frequent wheel who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
