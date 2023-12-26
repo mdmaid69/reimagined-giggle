6823d68dd1a7be76637dd35b7b9599c15def027d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:37 +0000

    exactly bin completely vision open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
