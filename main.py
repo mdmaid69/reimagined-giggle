09ceffaf081478e1355bbc393c586af68006719b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:31 +0000

    punish up we nearly various

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import tensorflow as tf
-print(tf.__version__)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
