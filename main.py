a57b911fc55fd314b4225f11653a4f7505969f63
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:43:02 +0000

    our blow turnoff match fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def split_path(path):
+        return os.path.split(path)
