3f79c69b66ec4df535e72dad43d0c57187febeab
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:27:20 +0000

    innocent pen cap false art

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
