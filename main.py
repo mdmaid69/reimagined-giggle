87293751e5e1f6cfed49ba5ef78ec82996da3ae4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:03:39 +0000

    solve holdout on unhappy powerful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
