d331300c2554c92617ecfa89b1aa1ca995e98ff9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:04 +0000

    slide general sink begin that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_array_typecode(array):
+        return array.typecode
