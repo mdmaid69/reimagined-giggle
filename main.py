65152c00f1e919abff4bc24ca5d657b56393d15c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:36 +0000

    fault straight bag invite wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+print(os.getcwd())
