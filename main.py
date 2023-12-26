9a2f0c2427effd040ba8b73e1da00b33cf18a970
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:50 +0000

    Uu why side correct relax

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
