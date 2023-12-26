5c2800c847111a6ce28f07475d3ff28b1c8885bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:29:19 +0000

    error argue contact of turnout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
