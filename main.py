5ef3e5cdbd7565b370201f0332b89e0dc472ded2
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:21:18 +0000

    lower wave bin accordingto ignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
