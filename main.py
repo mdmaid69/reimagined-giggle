1fc3f5cb7bff036a4b4fa594b4b3735e5c113bbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:28:19 +0000

    tongue judge worried swimming maintain

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
