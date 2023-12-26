b913e8ac54a4d42198899a7320d0550860be4ce3
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:34:10 +0000

    settle Ll perhaps dance look

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def find_min(numbers):
+        return min(numbers)
+def reverse_list(lst):
+        return lst[::-1]
