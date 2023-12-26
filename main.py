231dbab1e54df95d8ed7da95f78df84788aa748e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:11:29 +0000

    566end class call exact new

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
