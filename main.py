f40c6497dffeecf8ea70697f03d874e38b2402d1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:48:56 +0000

    stand create date wing part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
