0a9e408261141b8c66742090ef5138d20bbbfaae
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:25 +0000

    drop obvious Tt intelligent creature

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
