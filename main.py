7ac6b6cf960fb53012cb4e64063e89d1a1953d4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:46 +0000

    over order sweet skirt school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
