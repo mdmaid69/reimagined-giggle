ce062ac94d8f54d3a991ac1602bc9569bd21e6f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:01:21 +0000

    model this interfere come study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
