83072704826234f9dd497518e08751d37d01b810
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:54:49 +0000

    calm damp ask none snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_item(array, i):
-        return array[i]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_current_working_directory():
+        return os.getcwd()
