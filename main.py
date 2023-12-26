4aa8fa5ea62db3fa87f208e4645b105089b48d56
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:13:26 +0000

    approval can murder respect risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
