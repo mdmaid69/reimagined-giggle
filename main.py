21a81da37263243959fb2a13e9832dcbc1dd4b07
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:21 +0000

    lower error wave lock make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
