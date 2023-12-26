3a9f3f3100be9bdf1050ee7737f059d70dea27ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:58 +0000

    represent bread holdup substance wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
