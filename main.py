094ad6dbd71b55026eb0d08a189f3da7d24cab49
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:00:43 +0000

    below open environment wind brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
