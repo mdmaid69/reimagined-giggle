58e82097e68d7b0d9e0be55f5d7b83964e846da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:27 +0000

    town hold pollution follow suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
