f44c87dc42957ecbd1c5676d8b0f834fed3ef4bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:27:37 +0000

    ashamed child central there run

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def reverse_list(lst):
+        return lst[::-1]
