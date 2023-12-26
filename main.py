a468f71eb565a2c82437a7f4f876b8ffcac23ca9
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:05 +0000

    care where ear keepup image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
