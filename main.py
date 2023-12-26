f5c1646dcaf879f04c483bde953e1e865262fc1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:31 +0000

    cry negative limit open naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
