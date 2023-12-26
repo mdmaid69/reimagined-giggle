44bac424ce12cb259ba353a52253fa18d980241c
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:54 +0000

    discover wind table pleasure across

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
