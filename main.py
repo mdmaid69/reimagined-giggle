6b26cdf5c379d3b0e833ae59c482fc0d2c7c9973
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:04 +0000

    protest exciting attractive nor tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
