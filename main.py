7fb1e3006a5c39b7739af87098feaaeccb0f4352
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:55 +0000

    legal bit clearly children person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
