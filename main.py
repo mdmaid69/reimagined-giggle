f04e55c5c10aa4ee1f22fa7856ad130761fe0963
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:44 +0000

    hour whenever perform around Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
