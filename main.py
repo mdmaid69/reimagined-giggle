e3bb75b23d2193b41aa10873d4550d65e3c19e75
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:52:23 +0000

    only point worth kind hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_string(s):
-        return s[::-1]
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+print(sum(range(10)))
