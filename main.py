a37dfa2db94b6576ccc6c8d1f5d74f49b8b687a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:59:16 +0000

    themselves this difficulty geton giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
+def add_numbers(a, b):
+        return a + b
