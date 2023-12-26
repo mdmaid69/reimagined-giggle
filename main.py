c396756a4125edf2b0c4dd201fc2d95af27ebfed
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:41 +0000

    increase reject set pronunciation window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
