5cbc5ff5f510c9238fa4bcd0f8c655ccd27e7333
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:11:38 +0000

    tomorrow geton trade necessary press

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
