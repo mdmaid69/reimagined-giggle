1dc302e8aa8f218cbdc9e08e7452b653ecde6ed9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:54:40 +0000

    smoke change bath by hard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-print(math.pi)
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
