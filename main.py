cfe1379c9f6600c86c089f52f9d4c5bf14cfa8e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:04:51 +0000

    free sorry kind nose admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
