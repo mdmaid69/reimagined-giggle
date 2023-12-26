1b2f6830a676abe52b09252554292cb168009579
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:25 +0000

    careless convincing any breakthrough uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
