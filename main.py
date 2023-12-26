edf3ea55f7827fe29b8382a59b49e0fd92b2ef64
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:56 +0000

    scene interruption ride shape tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
