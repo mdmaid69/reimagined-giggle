7933b8ffb60b99ccd1013b61b1eee1d3e795fb09
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:23:33 +0000

    solve different full post goout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_typecode(array):
+        return array.typecode
