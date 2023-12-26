30ccc665a31e3d172a02a32cc3c4701e6da06ffd
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:21 +0000

    party task keep surprise business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def reverse_list(lst):
+        return lst[::-1]
