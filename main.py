ca322c043477a4d49cbe2849effcf878a1b7a400
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:34 +0000

    breakinto offence doubt shape list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
