06977d7b8fdecb5ac6733bb13238e0ddd6dd6cb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:42 +0000

    Ss mail score small refer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
