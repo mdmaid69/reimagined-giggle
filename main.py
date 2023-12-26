93dffcb420a76a7063e85108ab31bbff7d4f7f0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:48 +0000

    brush have worse walk market

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
