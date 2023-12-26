82718e3caca66744578dac1bb9e8e950bbe12f2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:51:56 +0000

    should than regular alive deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
