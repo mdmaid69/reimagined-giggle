04b963d7cf0f50affd8b0200e016840c23fd88c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:29 +0000

    persuade 573entertainment first watch develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_circle(r):
+        return 3.14 * r**2
