361f1e33ea60387fa46daaeb73fe33beb6368426
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:08:25 +0000

    standard downstairs male spot possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def reverse_list(lst):
+        return lst[::-1]
+import sys
+def exit_program():
+        sys.exit()
