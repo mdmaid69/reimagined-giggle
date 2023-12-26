b9285d8e3e996a08e54bdbe238edd67b37a53d5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:15:29 +0000

    sound please disapprove regular half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
