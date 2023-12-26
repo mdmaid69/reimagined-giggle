0e70dbe2e8078389797cca98a6d8ec3d42b32fb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:19 +0000

    manner tight pool somewhere concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
