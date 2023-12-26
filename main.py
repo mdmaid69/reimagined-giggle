2c45e1bc6cdc9816532d4a48ecdea6dc61b9fa1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:29:49 +0000

    talkinto let until occasional runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
