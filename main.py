8b45db84d149ae688e483d7bc4284d1f5a390cce
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:04 +0000

    knowledge wrap accidentally wrap human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_min(lst):
-        return min(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
