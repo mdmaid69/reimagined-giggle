b203dcf29085059582c9d56ee915c417e412841f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:18:11 +0000

    official can in sorry oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
