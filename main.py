f2b5ff809e3e454c359d425ea6cf196ffcd88c5b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:11 +0000

    new help couple character friend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
