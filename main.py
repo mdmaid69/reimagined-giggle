618f93d84992d178a0824ca983c58d104fdd4383
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:26 +0000

    exam aim precise watch wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+def calculate_sine(x):
+        return math.sin(x)
