9d09d0fcd18cbf770388ad7d31219a7e77f66534
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:24:07 +0000

    read train slice favourite consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
