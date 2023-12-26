8ed2c398ac14f325d9dee104c41103701642d8df
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:43 +0000

    mood song physical Ff square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
