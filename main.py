6fb548dcafc74a9c1285764d860285273ec1f7ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:31 +0000

    towards hollow again fit flat

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
