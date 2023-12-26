f0a33e05fa0d98635337ad0fe42a837e5e044201
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:09 +0000

    nurse fairly lead Rr walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
