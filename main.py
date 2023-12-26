fa5355d9eba164a18c80d46bc31a6fc4c562058f
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:56:14 +0000

    dream environment reach earth vision

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
