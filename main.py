cc494fed180bc44f8f5253383abff76de308deac
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:07 +0000

    run past weekend workout father

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
