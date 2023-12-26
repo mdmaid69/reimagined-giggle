5b686dbf550eff18c3070b095a0d8940ffe1d42c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:55 +0000

    shock the legal wealth pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
