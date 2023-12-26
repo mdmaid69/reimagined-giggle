a4f813b88c4c01faba9c21258f715fbc7ee7a584
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:10 +0000

    breakin callout heavy bet order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
