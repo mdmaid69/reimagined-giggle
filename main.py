74d8ba3d0a0e1a813ae7a3785d6404f29d2ddf8f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:17 +0000

    we hurt believe strength shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
