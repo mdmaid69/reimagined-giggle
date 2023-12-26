be74cb3c3def15ab48e3147619445b081ea57c00
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:14:47 +0000

    bet pause simple performance go

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
