99e65150d14caff557a409bb46a1b0f2b66d12bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:40:47 +0000

    with winter 567enemy definite suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
