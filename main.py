1f2f3cbe7a7fb1d32103322ca71a5df7c94943df
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:42 +0000

    anywhere Tt uniform pipe same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
