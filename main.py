856120e8e3574f3ed8eca2c5dc197039dbbf269b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:58:44 +0000

    notice contrast clue basically satisfied

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_directory():
+        return os.getcwd()
