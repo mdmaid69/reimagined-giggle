bc6f7f0aac61f1f97638409251131ffc87058004
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:35:10 +0000

    strict pay trip lock increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-import re
-print(re.match("h.*o", "hello world"))
