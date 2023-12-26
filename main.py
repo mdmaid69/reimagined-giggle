c94220a727df8200c4372dc5182922be30830145
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:10:00 +0000

    train anybody drawing able gate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
