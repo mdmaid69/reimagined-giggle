fd35420046d8fb6a0200b13d183d48e3ccb83943
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:03:27 +0000

    accidental name mainly Uu argue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_bytes(array):
+        return bytes(array)
