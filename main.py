bacbbf14f11104954c91a8d1528264bc4d7b6845
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:30 +0000

    threaten steer season shallow balance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+  def square_number(x):
+        return x**2
