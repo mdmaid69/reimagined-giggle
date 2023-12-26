bbd278cfac203a615867f944769cf9052e4950f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:46 +0000

    emphasis embarrassed report close unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def convert_array_to_string(array):
+        return array.tostring()
