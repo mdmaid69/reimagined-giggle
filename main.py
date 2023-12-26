4a63ac369e70eb12e97c0748a264fe695c0e2071
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:23:37 +0000

    back hot congratulate bang right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def clear_array(array):
-        array *= 0
-import sys
-def exit_program():
-        sys.exit()
+def remove_from_array(array, item):
+        array.remove(item)
