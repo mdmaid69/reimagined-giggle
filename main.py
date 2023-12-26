80d47a8e325e7108bf4b782fba0f560c588f75b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:36:13 +0000

    them anything honest rule block

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+for i in range(5):
+        print(i)
