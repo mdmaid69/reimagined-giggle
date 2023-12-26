08f185e404ffd3e7999c9b07c4c561d4439482ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:38 +0000

    godown question responsibility business explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def find_min(lst):
-        return min(lst)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
