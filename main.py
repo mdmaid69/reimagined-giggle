fa22a5783c8602c1aa3c9889df023ae75c260b13
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:11:49 +0000

    strike ignore visit squeeze tradition

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
