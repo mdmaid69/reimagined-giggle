5fce444de3f0339d35dc408527551b3d686ea077
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:23 +0000

    card quantity wind influence sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(numbers):
-        return max(numbers)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
