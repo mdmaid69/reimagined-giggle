426fabc193bee5d45bbaf1c89c249cbd30b3411b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:41:36 +0000

    workon wise under welcome food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
