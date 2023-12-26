bd7dd1edd17e684e258371e5e432c597f53ce806
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:02 +0000

    themselves rely voice lock hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
