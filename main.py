afd965d10991a347ec48f39ce096e4dbd6d76515
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:39 +0000

    dress rich would cinema 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
