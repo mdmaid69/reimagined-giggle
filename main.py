2eff631bd4d134a909e9d742fea95884e7cbc6cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:39:01 +0000

    meet replace partner escape pass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
