1407b40e7c607419ca67d0f0f90276e519c6dc0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:46:35 +0000

    keepaway criticize exchange size respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
