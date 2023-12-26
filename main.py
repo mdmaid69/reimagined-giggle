9507951c9f3b775a858adac5c45e506e53f7e533
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:08:02 +0000

    situation tape getoff skirt software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
