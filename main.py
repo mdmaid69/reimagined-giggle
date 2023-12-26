1edd7331e93e031581fc0ef5ef16d378b2dbd239
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:27:58 +0000

    nor storm left smell mark

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+print([x**2 for x in range(10)])
