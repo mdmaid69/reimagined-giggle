7e2aecf85e3408544a181b6899c11e88d4390a51
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:44:35 +0000

    used include every uniform slow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
