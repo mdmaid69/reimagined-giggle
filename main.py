d22d6f24e6c48cfeb56109d39e8e3a942445a231
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:12:38 +0000

    length serious poor shadow home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_elements(lst):
-        return len(lst)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
