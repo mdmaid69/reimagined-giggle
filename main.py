691a7be23c4f3435fc8707cfc2805fe206f53524
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:13 +0000

    tongue correct whenever completely score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
