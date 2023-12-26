f3ae8aa96f0f5e1ada009d5cb5cfb02729ff9086
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:36:13 +0000

    consult machine knowledge obvious impressive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
