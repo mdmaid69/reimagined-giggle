bf0f6271d085b42e49a359ac1cc1ec34fd650351
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:26:47 +0000

    feed guess school bush progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
