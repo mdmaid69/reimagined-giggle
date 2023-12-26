1a2f0b422bfb6d58d49bd0e6c8a001dd35d4cc46
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:57 +0000

    accidental opposite absolutely sleep do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
