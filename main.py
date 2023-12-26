5000fbcf4f5ee6ffaf88573baea79ed50c5da47c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:17:52 +0000

    mistake loss talk expensive lucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
