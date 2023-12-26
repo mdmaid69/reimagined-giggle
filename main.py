cb68aa7ad1d7792397749315717f2b3dd3415406
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:08 +0000

    tread full terrible drawing rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
