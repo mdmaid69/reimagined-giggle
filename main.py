ca44061ed566f55fcde1690ab7418fb1dd84549c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:36 +0000

    please safety drop under we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
