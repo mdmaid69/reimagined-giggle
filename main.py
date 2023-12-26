912e9dc1eeb12fd2a9fcd3269e2784d3a9c10a23
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:40 +0000

    change bother glass set ought

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
