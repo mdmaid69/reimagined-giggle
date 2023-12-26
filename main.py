83ff7abb75f120d78876e5ff665f8ca2840e7b78
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:56 +0000

    actual goaway bound goon offence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def sort_numbers(numbers):
-        return sorted(numbers)
+def add_numbers(x, y):
+        return x + y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
