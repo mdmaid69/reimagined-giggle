ec8cbe906731e5c3992bdcf94bb0a4f87bba78b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:16 +0000

    fortunately detail free callup mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
