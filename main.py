9bf76f396c355ed92da4f91061cea8d147b86fee
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:59 +0000

    tidy under far suppose shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import time
+def get_current_time():
+        return time.time()
