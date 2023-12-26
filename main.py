1dcb54523f8b9eb5c5f2fbbce637904777bb61a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:54:26 +0000

    wipe create sky little soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
