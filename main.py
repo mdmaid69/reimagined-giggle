cdaf6bd1183be7a7c1ec737eacdc3b20931a09a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:38 +0000

    drive say mark figure produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
