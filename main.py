b5e059a1a87b0cba308f43567e3dd59fa24fde3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:10:14 +0000

    attitude could low callup hear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def sort_list(lst):
+        return sorted(lst)
