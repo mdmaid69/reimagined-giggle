d31fbdc8f7026c9dd5762893837a8458964fd1de
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:05:40 +0000

    bring autumn trip fortunate calmly

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
