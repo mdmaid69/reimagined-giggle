1552feb5afbbe7d9ef6a5eb53e7833751dab9569
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:49 +0000

    giveout admire move trousers furniture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
