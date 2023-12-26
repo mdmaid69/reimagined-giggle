14b1bc8e919a11cbeda2909ab73128023217d67a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:43 +0000

    suckme drive noone include deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
