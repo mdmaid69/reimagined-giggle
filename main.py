d83379ec73910f64c972d297fc25f0a633a96336
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:45:55 +0000

    month trip ill put false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
