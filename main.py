63b290cf80d75045746a6b15ef3d7fd5ef6796dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:16:00 +0000

    scream chase organization zero blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
