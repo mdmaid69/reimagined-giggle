44c0338eb8aa6e73f2f559008276a23cd70041ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:27:43 +0000

    impression return completely not accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Words:", len(text.split()))
