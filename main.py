31581fc4aad22a23f0de665fd81481265a58bdcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:17:08 +0000

    comfort experiment bother part explain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_pythagorean_theorem(a, b):
+def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
