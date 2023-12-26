f59f387269a174baf5c9b02a04eb9b3663e64b8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:40 +0000

    why welcome press doubt paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
