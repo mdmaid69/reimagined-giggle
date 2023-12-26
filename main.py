5fe09383fa8abf3eb8d38b48fa9bb6348eec3724
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:54:34 +0000

    thanks believe song voice brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
