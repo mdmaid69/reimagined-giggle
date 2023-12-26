db11f820acf5197d9a9492d145a4b6088304825a
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:05:05 +0000

    especially hang lie cool relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
