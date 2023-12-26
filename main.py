d857289af3523bf5752546c731ed24b13f08fb41
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:00 +0000

    destroy proof letter open weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_power(work, time):
+        return work / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
