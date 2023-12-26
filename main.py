48803d6e19d75e3ab14a46c1b6a56067cd60a282
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:48 +0000

    leg notice space bringforward offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+print(time.time())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
