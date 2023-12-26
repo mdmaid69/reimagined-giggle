c029ea2b5c389ba255539f8db5eaac1a9dddfaf1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:06 +0000

    breakinto argue boil frame visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
