6d703e8c685e809ef023c70b08a1099df8bbf309
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:36:08 +0000

    snow shallow emphasize sleep this

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def count_words(sentence):
-        return len(sentence.split())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
