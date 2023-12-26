d9e16c82086c499640fa100957b233efbc0d5dbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:26 +0000

    shopping design mine answer us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def add_numbers(x, y):
+        return x + y
