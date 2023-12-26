f88fe7065f5518fd522e10dbea70149fdcbf3e1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:02:35 +0000

    round foot question would expression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
