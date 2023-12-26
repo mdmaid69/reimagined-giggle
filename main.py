b060e950b133eb9e0ad0b09207680a431d0ab37b
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:22:40 +0000

    slightly bath dare unlucky back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
