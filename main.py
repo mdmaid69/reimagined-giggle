0329d64c5f636759279f5cdce44c3ebb55844432
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:46:52 +0000

    apart mass serious connect watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
