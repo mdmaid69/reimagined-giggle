e7789c7881c5b3b0cae44e03ccdba522623c8cd9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:02 +0000

    turnoff can nonsense music team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
