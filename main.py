efd10e0bdf739e54bc1b5ba58512d2c0c10709a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:15 +0000

    thinkover seriously touch suppose lookup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
