9b70db2c39995fb17ce9d5a07f13412d9c90752b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:30:31 +0000

    around common male bottom speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
