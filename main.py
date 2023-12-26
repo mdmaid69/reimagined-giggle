eb7a7310a26d94715816c7059213ad1ef34b9edf
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:15 +0000

    bank dress music tune male

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
