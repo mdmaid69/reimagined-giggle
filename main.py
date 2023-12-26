24081bf21058058661dd6452d93ac240cebc7275
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:42:39 +0000

    pull truth all move gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
