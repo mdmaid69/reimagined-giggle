9646d0835142bba63400f52e85af27122e045a26
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:00 +0000

    backwards keen official quality cross

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
