8342d45cbc9479cb8d281fb269a7e259c4b64f98
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:19:24 +0000

    wind sweet artificial song depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
