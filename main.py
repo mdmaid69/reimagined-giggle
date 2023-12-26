c2760f044d26067282f6992a0336df532163734b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:36:30 +0000

    division he mess dish excellent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
