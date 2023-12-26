f6380fd4ab4f37d5230594c525867425c5b83ce3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:01 +0000

    pot kick secret quickly home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
