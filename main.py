ce4d78a71dceebe96b1764f1e911854c9b0659a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:21 +0000

    contents best top can lend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
