bb52d0e15a959c7d85561d1ed274f8d1ff13fd87
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:16 +0000

    increase getout connect carry will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
