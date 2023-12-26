e6ff99947c5b81b5a908d661fee3b00ac961962b
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:07:42 +0000

    soil authority bottom illness pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
