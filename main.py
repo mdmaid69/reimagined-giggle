bdd987fec503bfd196b62ca9453135ad34a3bf1b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:56:32 +0000

    ground name port artificial shoot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_work(force, distance):
+        return force * distance
