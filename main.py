8475dfbb3127bf0180983cd1d5fafa18b7dd9172
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:12:38 +0000

    pass round weigh spring against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def subtract_numbers(x, y):
+        return x - y
