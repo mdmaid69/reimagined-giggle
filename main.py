22890d4a23c0acfafea537b235a1117fa94116b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:13 +0000

    file goback(to) lend something test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
