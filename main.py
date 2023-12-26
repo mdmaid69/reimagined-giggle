9f8a5992b1e011b169b60050cf7dc0ee31c940db
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:55 +0000

    clue so estimate project private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
