21e949514d35695a0d312c1964ba4a2b7e3c5568
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:47 +0000

    count mystery body drawing store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.ctime()
