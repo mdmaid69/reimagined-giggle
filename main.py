5c3122fa507503618e7c3146c53e0a393d47fd84
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:33:23 +0000

    think whisper switch fond squash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import platform
-def get_os_info():
-        return platform.uname()
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
