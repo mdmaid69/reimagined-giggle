e261c8c46c575a87f8b45ddc7d67a73c4a121585
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:02 +0000

    brain emotion convince official after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
