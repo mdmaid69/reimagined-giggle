d4751bb730dc10b0ad4801895559902981d12f55
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:26:20 +0000

    polite discussion produce difficult drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
