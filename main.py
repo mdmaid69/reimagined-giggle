ccbcd4f8eff5c6aa6857602ba94e401e3e2c3102
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:45:33 +0000

    swallow food recent notice herself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
