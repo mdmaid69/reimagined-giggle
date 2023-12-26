5a889c464449b1eacb3a7de28d2b7224b34e0776
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:21:33 +0000

    welcome gas slightly reaction size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
