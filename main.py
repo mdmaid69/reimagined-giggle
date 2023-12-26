4eda998fc517a455ef317c021fa30b2919a037ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:24:01 +0000

    knock delight embarrassing mind float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
