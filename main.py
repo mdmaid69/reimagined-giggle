3b6a951ce756d918db4a596868006b89e6bd269c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:24:28 +0000

    runaway production talk month keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
