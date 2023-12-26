ae98223ffbe7db3a6b37460dda6cd2ea854bb525
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:08:18 +0000

    skin push big shock tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
