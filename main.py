6fe12dfeaf3093b18927f629d49da06876ea3a6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:17 +0000

    either connection some news connection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
