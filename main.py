2c0e8c1a71e80aaf4d94a8a007cac0ebd3390fff
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:32 +0000

    attention range sight by swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
