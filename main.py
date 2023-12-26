9284ad9b076658d33afb77053db59825174fbed9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:15:39 +0000

    exist promise possession show grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
