d4b7270a97518a6e1d61d8c68d79098b361f4116
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:09 +0000

    lookup regular present boil delicate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
