cf849508f9727a2dc15701eea2639f705126d39d
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:56 +0000

    basis container place scene might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
