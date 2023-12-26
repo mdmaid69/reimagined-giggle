3cad51e1cd5500c7da0d703fbc0fbdf5fa31b7f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:24:33 +0000

    make bill getoff otherwise plastic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
