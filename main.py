4b617b95cd4ab5b333f12c900d0132da305000f2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:52:54 +0000

    ground cause arrive float vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
