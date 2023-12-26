04a47c287f4c1275ca6a0e036275d107ea8f3ae9
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:01:39 +0000

    may while print on strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+import re
+print(re.match("h.*o", "hello world"))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
