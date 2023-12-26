e48dbd620983a2165124a648ce561067f98f57b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:15 +0000

    disapprove measure place letter indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
