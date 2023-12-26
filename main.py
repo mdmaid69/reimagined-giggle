b827c6940aabd7ab178828afa8da9e01dcb8d49d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:16 +0000

    movement weak cum accident kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
