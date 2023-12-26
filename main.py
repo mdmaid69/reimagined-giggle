5658d5a2e7dd9d9fe7ed40419d4a591598cb3bc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:53 +0000

    deep slow by bill turnoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
