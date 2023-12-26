f3db6e5aad96a572abeac2cb966d30ce9dd5b47b
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:33:16 +0000

    them fix accidentally separate mistake

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
