492f7d101d74a50696aaffa9485b70aa3f412f20
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:03 +0000

    blame scene pretty bury her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
