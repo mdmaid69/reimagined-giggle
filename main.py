af27c823e03f056e1c133db1172cb6480f458144
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:53:38 +0000

    rule male far warm violence

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
