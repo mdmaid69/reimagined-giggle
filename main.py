c7a0790a95bbb19fbb37adfc2441d44914dd386b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:15 +0000

    free here born remember impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
