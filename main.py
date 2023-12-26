36a51c3a1121396198737fcace69b353883b1dc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:13:11 +0000

    voice practise consider try turnoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
