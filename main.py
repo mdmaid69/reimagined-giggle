e27ed8d648b6c95e38871f497de7e196c8a03661
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:41:01 +0000

    soak crisis will smoke touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_repr(array):
+        return repr(array)
