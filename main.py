14586d0da97bd694eac3e09b9e2955eb11212aac
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:06:16 +0000

    concern record advanced almost on

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def cube_number(x):
+        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
