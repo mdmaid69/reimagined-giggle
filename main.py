27bec721cba4379fb6547ad94b9c7fa5f2fd519c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:42:33 +0000

    bite quit live pile skin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
