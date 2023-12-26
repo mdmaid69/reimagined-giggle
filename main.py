1964963665237d642c0f9cd98488e2e9d6c69da9
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:43 +0000

    settle what cause approve discover

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_acceleration(speed, time):
-        return speed / time
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
