00db8c4e2f06c10c77c1bbd1e9262dc23feb538e
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:55:46 +0000

    blame off regional business especially

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
