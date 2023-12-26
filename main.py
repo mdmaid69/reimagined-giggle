8c88d2a4ec4dd3cd94f2416821f65df24c1e5197
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:57 +0000

    government square part hair disappointed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
