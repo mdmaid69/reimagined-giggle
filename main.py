2ae4b2784666cbfce9771ed78f08b6b6ff23d7dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:39:37 +0000

    shop passenger environment immediate quick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
