254757542d1941c8833de2bc640f5178db8aa23e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:39 +0000

    cause always write any Oo

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
