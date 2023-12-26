f627f87574ff0a74482b3fc58c5ec59933ecee7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:04:43 +0000

    feed call obvious still your

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
