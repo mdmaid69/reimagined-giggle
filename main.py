aed74e6df0b58c3993578900329f0a0e91dd3bf0
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:10 +0000

    muscle top affect month discovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_power(work, time):
+        return work / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
