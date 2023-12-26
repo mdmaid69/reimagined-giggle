8895c279e2c88cfd5edf2d47b7d9a12ebd2938b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:51 +0000

    comeround artificial nervous can cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
