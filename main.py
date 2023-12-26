ef8dcb769932216da204bd9aa93f432f677168c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:51 +0000

    rubbish refuse touch drawing operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
