5f3000ac03fde0c21abea8db5ac46dbfb39a60d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:29:10 +0000

    strange wave pleasant supply square

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
