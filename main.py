306d2bafd8f15c3d9f2db046722fec6ac6db6f67
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:54:14 +0000

    appear approve translate cruel pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
