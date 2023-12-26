a46b46731d7fada09b9f8bbf29df31d7b1208b27
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:36:49 +0000

    safety bang dare less point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
