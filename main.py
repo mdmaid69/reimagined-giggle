5ddf112d622f8c3cb7caeb1a2d17c6cbbc9481a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:59:36 +0000

    system children flight now nurse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
