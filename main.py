f10695396fd78fb4337915a2116d1f1fa62e8325
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:18 +0000

    comefrom inquire quiet opinion control

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
