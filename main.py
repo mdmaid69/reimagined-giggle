e025bacfcfa1baf67fb82ddf242be1b584e7dcdf
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:20 +0000

    live sing home will curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_repr(array):
+        return repr(array)
