1657d16d8cb0b6cde94076ac8edf020281bd70a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:16 +0000

    spring from wise communicate since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(a, b):
-        return a + b
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
