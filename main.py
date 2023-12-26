354f6099d590dcae16a5823fa0d2914d2a0185b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:02:28 +0000

    tie character comein react conclusion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def subtract_numbers(x, y):
+        return x - y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
