92ba9cd19784c90728fdf0390bc49e56501bbfc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:04:26 +0000

    sure transparent discover present firm

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_user_string():
-        return collections.UserString()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
