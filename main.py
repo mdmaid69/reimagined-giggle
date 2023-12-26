263a9edccaf9e0ff342310a72b401928c2ecc37a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:40 +0000

    for under keep will production

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_set(array):
+        return set(array)
