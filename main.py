1f4a42aab20aeb895b403db850ebc8cc4a491ad9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:31 +0000

    above suck for defeat control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
