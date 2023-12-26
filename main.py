172b94b93b883b07e3a5f2c8b6f206dcc2fb572a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:11:54 +0000

    practical float contents unhappy today

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
