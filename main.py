516bc7c009b48b2006e86276ec14f48eb79f696e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:17:29 +0000

    press emotion throw demand year

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
