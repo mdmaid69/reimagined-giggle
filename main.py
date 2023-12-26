7ba50cff75d973f06525ec90f20a1e616a5b2960
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:53 +0000

    related buy joke hear seat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_characters(sentence):
-        return len(sentence)
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
