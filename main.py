5aeb0458852c14ab3dcfe38d1ac8983db5234cf5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:21 +0000

    term development store shout tread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def convert_to_hex(n):
-        return hex(n)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
