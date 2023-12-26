a30b862e2615fdb0af252f0fb1934e0f8aa83985
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:39 +0000

    copy find may stranger short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
