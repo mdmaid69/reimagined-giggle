0cbe80e8414b01cbc66c7e97e297695b12f5a415
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:22 +0000

    change shoulder fat cost practical

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_rectangle(l, w):
+        return l * w
