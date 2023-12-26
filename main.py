fc204ef04732064211314a2764b4bb46f01e1b6a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:16 +0000

    believe person post sell lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
