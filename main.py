77677988dec663364622ebb595ea2c0a3f7643dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:56 +0000

    rain brave unable secret hate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def reverse_array(array):
+        array.reverse()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
