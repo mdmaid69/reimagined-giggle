67dbc29ae1ff2b1be815f829ccdd795bdb6c0184
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:15 +0000

    work behind strange replace attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(10): print(i)
