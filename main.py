6149ac81cffa1f97ba528f6770c89fa653004075
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:10 +0000

    bring perform act argument Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
