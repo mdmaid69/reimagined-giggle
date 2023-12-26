79667b383353284d98ec3a433740d737bfabba6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:37 +0000

    strength eye yes present common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
