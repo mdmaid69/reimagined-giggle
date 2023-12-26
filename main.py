e8067ce05595d37347cf81549710e9d15bbd3d01
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:09 +0000

    edge consist life capital back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
