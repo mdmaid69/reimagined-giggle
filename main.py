74f74736455dbe693350a9c22eee1c0730af1555
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:01:58 +0000

    threaten reserve mysterious hurt provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
