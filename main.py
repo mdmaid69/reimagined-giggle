7d872310727cba8ea09d70edcb4463d556885e01
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:04:37 +0000

    argument direction calm strike fire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
