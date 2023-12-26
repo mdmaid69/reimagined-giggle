4110db7688683d99177f2acd1318344fb3de3a05
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:29:54 +0000

    range kind television previous comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_array_as_bool(array):
+        return bool(array)
