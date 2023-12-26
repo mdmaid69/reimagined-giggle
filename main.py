8e726e8fc3677d7f7502ca881e458b29943c123c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:50:25 +0000

    gas only careful progress attention

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
