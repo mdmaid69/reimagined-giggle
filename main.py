b301c6be4ec9955664e333ff190b7202e3da354b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:20:32 +0000

    rescue difference mysterious flow command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
