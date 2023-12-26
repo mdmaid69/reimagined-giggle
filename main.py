86ce611fe4b2d6b3fbdb0d5efa12ce96fbb2bbd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:14:17 +0000

    realize wherever Vv freeze similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
