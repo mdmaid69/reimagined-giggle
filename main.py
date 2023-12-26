a2aaaa39ed9ee070ded54f0ce61c595bf4fc6c79
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:34 +0000

    soil sticky middle pile wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
