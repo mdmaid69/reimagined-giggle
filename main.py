970ca42dc553cd6f27c043503e0d74a04857db45
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:31:02 +0000

    that within straight wild develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
