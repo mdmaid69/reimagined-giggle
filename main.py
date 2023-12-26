caa113e66d693377a061b0a70d66a51ce0254b64
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:06:17 +0000

    ashamed flood imagination find minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
