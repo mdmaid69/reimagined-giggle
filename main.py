ff0ff997ccade01254f1aa6984c8a4b11b803a84
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:07 +0000

    image cold key child realize

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
