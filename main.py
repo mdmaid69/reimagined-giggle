08296ec53d0345a197c9397d9ecca851bbc4b7b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:01 +0000

    together regional fence dear path

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
