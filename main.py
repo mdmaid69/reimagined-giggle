ec410fe5585194e22f469994f68e470efdc0fa78
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:20:25 +0000

    rescue house intelligent thanks wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
