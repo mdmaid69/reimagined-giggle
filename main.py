c09cf4d81a22cce7c06e66b48c2f8d05405c6bb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:02 +0000

    father wave danger ring settle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
