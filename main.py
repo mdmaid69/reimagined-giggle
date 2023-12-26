406c0507360df84c46dbc60b67fdf2b086ceb55f
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:41:57 +0000

    mess normal stamp pick clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
