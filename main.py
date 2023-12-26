347bf8e756dcd56c14790def7509bbb04e5cfa01
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:56 +0000

    ring giveup breakfast stiff listen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def change_working_directory(path):
+        os.chdir(path)
