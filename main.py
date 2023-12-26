f1f060b79251fc5fab496fc4b2f226c31129601c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:58:23 +0000

    act set and demand rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
