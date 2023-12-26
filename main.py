17984a1475d6a6f31ef6055a98f6060c8e98fc0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:48 +0000

    square last move whole people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
