00ab69c3b2d7a1be7d4903bd2bc2ddf95c0cece8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:40:34 +0000

    respect pleased swim money suggestion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+print("Hello, world!")
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
