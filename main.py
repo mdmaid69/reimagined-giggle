536c63d0bfa53ba7925895c257768eaa98dc3d9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:58:08 +0000

    could basically Cc they ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def split_path(path):
+        return os.path.split(path)
