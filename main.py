71628285daff01a0f48acc2802105f894ddcdf4d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:44:14 +0000

    rush even pin calm medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
