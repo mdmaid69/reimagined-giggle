b2fe38b3a54215693792659fa22b09cb022f7e3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:47:42 +0000

    hold drop what design hour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
