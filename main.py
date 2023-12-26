ab86c52ef358e787a5ec596df46d9df445639e86
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:13 +0000

    mark giveaway arrive actually meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
