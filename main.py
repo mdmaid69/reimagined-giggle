0d91de52d1b0df108e3443bf74ea08f68bac5cc6
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:11:31 +0000

    bar scream nose pass measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
