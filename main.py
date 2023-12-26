e8ff9db7fa395e470f2718016e8714bd66baf5d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:48 +0000

    run find at total closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
