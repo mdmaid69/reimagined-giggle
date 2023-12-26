ac7a2ce1fed128db9bf901876f1d4c0fcbf97759
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:54:02 +0000

    shout upwards badly callout joke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
