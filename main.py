eaf6d218dd9cd0e2cadd0133347a026d6902c98a
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:47:10 +0000

    thought on swallow country clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
