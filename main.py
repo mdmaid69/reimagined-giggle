e7eccc8ae0ed7cf6eb5905e14a2c5d47b2e1fa26
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:08:28 +0000

    intend poison radio round train

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-x = 10
-y = 20
-print("Sum:", x + y)
+def is_even(n):
+        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
