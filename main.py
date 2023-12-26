fe0761b824b407a82794604bc5445187963178e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:25:29 +0000

    pronounce understand wait keepup air

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
