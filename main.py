dbadc874946514d91711b268e92af26a6c54537b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:09:06 +0000

    used party squeeze love work

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
