94821c98f585202bf495b8f82948f914f7fbd2b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:14:51 +0000

    rubbish your score wrong whoever

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print([x**2 for x in range(10)])
