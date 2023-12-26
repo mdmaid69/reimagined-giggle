aef37c192d0a57d276984b8d9e6efc2a28201b69
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:33:20 +0000

    bored anywayanyhow nerve limit fold

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
