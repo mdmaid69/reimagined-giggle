f035740512a0ad55995c0f18eee7199bc7c59c0e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:35 +0000

    instead guess mouth from now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
