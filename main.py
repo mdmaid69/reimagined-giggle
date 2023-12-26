2b5e663dfc2e95b8c5ce8f228c69836c462bbbbe
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:10 +0000

    Uu speed family total pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
