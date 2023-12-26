ff36f311daa50908ee2605096a4aedd15c434146
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:55:49 +0000

    excellent appeal completely runinto complicated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
