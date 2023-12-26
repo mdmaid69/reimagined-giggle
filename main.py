a39dd737d3e9cdb341cd4a0cb44bfa80db1a6633
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:43:33 +0000

    convince comeoff confidence great quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_int(array):
+        return int(array[0])
