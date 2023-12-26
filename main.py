3e72ac8ad70e8cf08573f578601bd197dc46f78f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:04:44 +0000

    keepoff possibility underneath rather attach

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def clear_array(array):
-        array *= 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
