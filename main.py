39263090dcd3b0ddaa3b6eea98694db6246b5673
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:51:56 +0000

    be national free block sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def reverse_array(array):
+        array.reverse()
