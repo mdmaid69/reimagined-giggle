43cfe5ea1d3c9b297b7c372d62cc1543638ef06f
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:44 +0000

    mistake centre rise left future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
