b02aeabbbe83dd461a4c4867cae3ee18ca753a94
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:35 +0000

    vote deliberately use operation wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
