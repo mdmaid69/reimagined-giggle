de2bbaab59f2a767be7ee4b6d1ea4dd66942dc39
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:13:15 +0000

    aware refuse return cause even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def reverse_string(s):
+        return s[::-1]
