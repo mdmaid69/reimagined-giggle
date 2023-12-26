56d719ea656f43e1cd433cb18e2320395ee156b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:27:45 +0000

    please 564encourage play meaning beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
