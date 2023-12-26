4e2d26f1df56416cb581499392858b2687495d66
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:21:56 +0000

    consult subject contact possibility unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
