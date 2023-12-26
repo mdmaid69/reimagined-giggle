82f3fa8279992f1fdc870b7d2de7357868281b94
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:19 +0000

    earth start firm begin model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
