2f989a4c60c0281dd857cbb42688365cd9992838
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:13:24 +0000

    badly turnaway match upwards less

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
