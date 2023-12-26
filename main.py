c4d9b18222bad7bf11d3aa43314b24e7b73a831e
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:17 +0000

    lay build breath takeover expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
