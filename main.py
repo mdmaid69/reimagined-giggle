f2a37ef38f66b1227cbd66f286ae862667cc4a00
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:54 +0000

    weak notice education flood subject

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
