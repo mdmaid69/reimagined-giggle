029078d71443874b5085cf1a9b74670c3efdc3df
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:08:31 +0000

    Gg wander nose turnout fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
