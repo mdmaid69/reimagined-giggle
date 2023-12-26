8cee8ca13bc83a5e6237d7a0f63b4336a0aabd9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:13 +0000

    reject tape ocean scale warning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
