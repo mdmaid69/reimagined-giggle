457e4b27b47ed39bcbebdaf754e6a126cec79659
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:32 +0000

    suddenly turnback suppose breakin wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
