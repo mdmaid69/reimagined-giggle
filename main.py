e5bdcd5156f80347304e189ce48daeb4a4062693
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:10 +0000

    bend fill beginning news foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
