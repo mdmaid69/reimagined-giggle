563b4f568b05863e842a926429c724a9549382c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:28 +0000

    anything shake safety basically fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def cube_number(x):
+        return x**3
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_memoryview(array):
+        return memoryview(array)
