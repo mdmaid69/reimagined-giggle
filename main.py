d6fdb1dd1d38a9bfd572491dff65e624751e0e51
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:11 +0000

    breakdown apply giveback Zz getoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
