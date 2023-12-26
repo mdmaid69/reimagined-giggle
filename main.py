2b9c2691857d0c97fcedc0229976826108b23d92
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:26 +0000

    pattern some beat her moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
