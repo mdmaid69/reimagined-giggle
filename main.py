9d268658ef5077e3f9fa713ec72050d8dcecc6db
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:30:11 +0000

    clear manner song travel practical

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
