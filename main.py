2bff012679cc9fd0ec57ca255408f56b0e0afef8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:41:39 +0000

    excuse programme discover explanation pump

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
