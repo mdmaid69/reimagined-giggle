d6b8759661bcee1c9f65834971ce9012938223a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:38:15 +0000

    attention hospital reason so slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_length(array):
+        return len(array)
