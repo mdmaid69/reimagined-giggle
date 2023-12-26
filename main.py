a4bc87891a4784ccbb2c046ad0b495edce9761f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:41 +0000

    opposite godown switch original such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
