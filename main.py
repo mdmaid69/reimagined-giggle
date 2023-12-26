42174e193dd2f42c1eb19ca52558adece31f9305
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:38:23 +0000

    defend criticism serious middle name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
