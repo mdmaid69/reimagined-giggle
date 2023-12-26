044f96ecfc19e7dc2ca9188a706baaefb9ec6f74
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:30:21 +0000

    black see fairly letter pool

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
