5ce99e3d505d11d9650a13db1daf69e8106613cf
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:22 +0000

    body wall weekend among anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
