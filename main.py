5972f2caf45cc5f08ff9c7fcfba3076a8b417c94
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:16 +0000

    deliver add breakthrough choice any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
