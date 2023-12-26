89bbfb00ccb6caba3692e43ce6f4d23c0ef30b1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:53 +0000

    congratulations ball come of union

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
