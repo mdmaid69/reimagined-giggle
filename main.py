096f6d435f1c8cd218aede96e3d63dae1c7575e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:25:10 +0000

    content bring wash talkoutof approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
