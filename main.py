e74f90ce470ecdbad602aa3f1b9b10422c93a055
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:39:47 +0000

    brush offend include trick taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
