014e79e83f6533ba26527864e81ec26389d71131
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:31:03 +0000

    fight search fence extremely whichever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def convert_array_to_bytes(array):
+        return array.tobytes()
