312b89c43bf01bc69107a6e20d4c1eb06468351e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:50 +0000

    now Ii patience gradually cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def is_odd(n):
+        return n % 2 != 0
