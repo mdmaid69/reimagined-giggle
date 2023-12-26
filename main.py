d00c618bf470578b112235006cf4cf9e5f986e1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:39 +0000

    improvement commit available last disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
