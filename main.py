68e04ff6ea9fc862d7a0b3112f362b813f9bacaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:03:44 +0000

    together relief sort abroad him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_string():
-        return collections.UserString()
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+print(os.getcwd())
