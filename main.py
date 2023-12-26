26b63e8d1730d6a82656e5906d572858b3148b76
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:31 +0000

    fold proper set desperate shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
