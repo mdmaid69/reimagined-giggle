7328aeecf89b4219060b6be45a516bbf302b9c9b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:26:35 +0000

    suffer 566end slight exact drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+def add_numbers(x, y):
+        return x + y
