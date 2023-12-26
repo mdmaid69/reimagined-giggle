9f61e3a6656fd9806330aa83fc35cb60f53fd870
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:54 +0000

    promise whom interruption abroad print

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import datetime
+print(datetime.datetime.now())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
