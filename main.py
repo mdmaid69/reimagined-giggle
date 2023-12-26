e1b4f3432332ee58613dd8c65c727ed779761cbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:34 +0000

    gate use hide wind cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
