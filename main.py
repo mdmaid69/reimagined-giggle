5241e2fa7683331525dbdb8cfe4bb15aed8f3ff0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:09 +0000

    continue really lookup memory left

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
