9a505b5e6b1630def7461a40178c77e663fbfb8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:05 +0000

    whenever defend guide expression practise

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
