4880b3a542e268250ebb65a42b2acac660199c42
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:27 +0000

    extremely welcome basis meet stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
