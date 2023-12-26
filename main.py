4204f29bd035f8a39eb01fc095b9469d0225e009
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:06:56 +0000

    able noisy table here pool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
