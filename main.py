0c2eb60d67b28c28de7199f4071cfc9e150ed899
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:11:50 +0000

    impressive bet about giveback pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
+import re
+print(re.match("h.*o", "hello world"))
