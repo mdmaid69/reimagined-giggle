0e0f962ec9906903d4eebbdac37c737b616d7ab1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:48:46 +0000

    factory lookup offend holdout sale

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
