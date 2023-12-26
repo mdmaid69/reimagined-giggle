3e4ba0955e612538bd8d68a3705e1515b9998d3b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:31:10 +0000

    depend tonight ago interrupt play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
