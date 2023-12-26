09230dce0e8bc6631fee8c6e6fbd8695025a0089
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:25:20 +0000

    harm discovery lookup Ww crowded

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-print(os.getcwd())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_float(array):
+        return float(array[0])
