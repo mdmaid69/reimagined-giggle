f7df1e749e86f5cedb2fc0e4191847866ea9d90b
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:50 +0000

    bread emphasis face hole embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
