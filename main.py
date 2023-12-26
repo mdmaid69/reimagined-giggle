65f77ff6039878e4912e309cbff137b0439f0693
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:44:17 +0000

    care someone fat trial press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
