1227c45843ad9c3dac295a22b2b63e22a2c4078b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:52:59 +0000

    downstairs middle limit rest refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
