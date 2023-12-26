509829071f0abd8f8be54dbc93f276c8f0d93a48
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:41:00 +0000

    land amount hit talkback downstairs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
