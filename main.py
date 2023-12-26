bcc7278d1f17af2efadaa2a7e9983fabedfd9b53
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:26:40 +0000

    turnback narrow confused watch protect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_tangent(x):
+        return math.tan(x)
