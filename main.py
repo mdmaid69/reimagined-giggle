d33a5f4b28024f7d8255a51d5a089a9df368a32f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:25:23 +0000

    feel Mm except comeback commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def clear_array(array):
+        array *= 0
