58a573683e7214c388a9419e59305290f0a529f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:46:25 +0000

    avoid wind whatever distance upset

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def square_number(x):
+        return x**2
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
