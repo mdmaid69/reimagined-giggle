f775e78cdb5f4efea595b7c9209da107ca3268fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:37 +0000

    guess tend sweet would cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def is_odd(n):
+        return n % 2 != 0
