6a9b78e5ec4d338545a9c37ad3a1831f297c28fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:41 +0000

    frame official decide scale fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
