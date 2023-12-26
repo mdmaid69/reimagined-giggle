30bd2babd031247af75819627151e167b7560718
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:42:57 +0000

    anywayanyhow bye practical Gg comedown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
