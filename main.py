db6fc239cc66e7cc76a921b796a47d42c2b3f867
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:30:32 +0000

    positive movement him difficult lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
