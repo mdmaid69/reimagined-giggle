696141a5457481338f43f42cc3675ed323b83dce
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:45:35 +0000

    forget behind to makeup funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
