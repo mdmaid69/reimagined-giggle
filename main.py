2f4dfc50842476f270c379384bb100413b55c04b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:29 +0000

    squeeze treat trick club talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
