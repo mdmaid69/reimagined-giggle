7b572d713d6663d0b9e825b484c88c5567e0bc57
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:53 +0000

    breakin crisis height ride accidental

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
