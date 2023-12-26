ee99038f54bf4d5661a047d885a3ffe073899a51
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:04:50 +0000

    everything open mess far address

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
