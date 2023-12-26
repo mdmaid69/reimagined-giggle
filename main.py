b6dd1a3cdb51bf70563d5a7d211667f0b4f156f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:38:39 +0000

    emergency when care fun organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
