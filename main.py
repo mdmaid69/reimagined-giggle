818400ce204c4feaf67331a573a544b02d42841b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:51 +0000

    taste room when dull extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
