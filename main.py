61b6611b71b14dd4b42f123607efa049b98fec32
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:37 +0000

    late sting remove certainly injury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def multiply_numbers(x, y):
+        return x * y
+def cube_number(x):
+        return x**3
