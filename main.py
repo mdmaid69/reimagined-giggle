2427990761f80044c07953bf1f1d8907315af7c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:43 +0000

    regard bury stop arm theirs

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
