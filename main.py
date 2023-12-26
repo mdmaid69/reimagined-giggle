9756a4dd13c61e693f90626ecff3c200fa6d9ff9
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:58 +0000

    wind wish after roll visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
