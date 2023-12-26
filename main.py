1176584c1022e3fb024bc05dbe0938c9ef8caee4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:29 +0000

    present light attach slide active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
