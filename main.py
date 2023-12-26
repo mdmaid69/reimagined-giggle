ae010672a244dc1d9f9526c3496016c0053bca17
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:18:52 +0000

    communication so in closed admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def multiply_numbers(x, y):
-        return x * y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
