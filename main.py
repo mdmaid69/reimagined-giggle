9ae8ebc9ea01a157b0b848ebd4aa68b8958405b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:12 +0000

    lucky happen straight private regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
