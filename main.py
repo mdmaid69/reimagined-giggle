038a4e3a532d775ac6f21eef87cd490fd78e2841
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:22:22 +0000

    mean positive be turnup march

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
