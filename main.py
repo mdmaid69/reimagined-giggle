88dc4d841bd5b0f217b490320fc8bbf57d82f5ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:20 +0000

    instrument difficult tough guess round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
