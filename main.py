26eaf8d06949c39ed840335bcec5134031ad6407
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:57 +0000

    game stamp kick autumn history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
