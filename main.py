c48c8a89cab49266db6142044109d0bdeb5e3389
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:17 +0000

    night valuable consist active business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
