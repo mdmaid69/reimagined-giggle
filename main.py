dcaed1f88ad6fd730262528ece2d709eb126862e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:32:53 +0000

    label talk Ff but block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
