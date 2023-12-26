d59f9521fc0152389e36993c4767b1cab4b3c581
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:30:52 +0000

    leave rubbish deserve official hit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
