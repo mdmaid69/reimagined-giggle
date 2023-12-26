d59f5bf2ed4cd38446756474c1d4729d95588f85
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:12 +0000

    official thin keepoff important town

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def square_number(x):
-        return x**2
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
