c03dc4ea7fa1ef18ec857f1323c34c06eb3aef53
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:56:35 +0000

    knock whistle describe tax wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
