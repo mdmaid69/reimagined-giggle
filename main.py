b57418348827b27ae0377a4b3229dec3815801c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:55:55 +0000

    centre doctor knowledge shout respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
