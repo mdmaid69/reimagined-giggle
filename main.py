7b8b1e441665088e515655eb097dec3997e4b3b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:08:58 +0000

    Tt write reserve agree cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
