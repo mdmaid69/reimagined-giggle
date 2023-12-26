896a6c047f375de3d07a11205a50a252e3447d02
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:58:39 +0000

    female theirs check loss breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print("Hello, world!")
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
