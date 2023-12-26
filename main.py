e83a1a1ba70137d4e4f6000cdf476b7619805fac
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:43:41 +0000

    shame move extraordinary over bread

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
