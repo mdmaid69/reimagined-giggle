a80215dd4bdd3ec552aa8c1f1bbaa184a2321b9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:53:49 +0000

    spring towards no tongue public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
