9fbc3e5bf6e04e7bafebea68ebc27ebf4e0e86b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:34:07 +0000

    gather list certain pass turninto

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_density(mass, volume):
+        return mass / volume
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
