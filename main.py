ebd5c0d82f19bd9fdb8496de4e11452325533898
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:04:49 +0000

    common detail condition everywhere history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
