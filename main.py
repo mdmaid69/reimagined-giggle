64bd547f7e61c979fb304ef57327142e6a302742
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:03 +0000

    command team head go congratulations

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
