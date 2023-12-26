2305d26833bd7e3e98e9bf2aab1351a21a5ba905
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:06 +0000

    lay wonder price face meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
