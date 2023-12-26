696ab714eaa01232542ee3577d9b53792c7669b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:41 +0000

    illness 564encourage contain search its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
