1faf7ee4cd0df129624d5a128ce26f2e008472d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:26:50 +0000

    practical understand contrast tongue world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
