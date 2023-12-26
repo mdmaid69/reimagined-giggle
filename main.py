f808ff54c79e3d3247656a5d4c628dbf51d4bdf6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:56:44 +0000

    sensitive free sink sure seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
