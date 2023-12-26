ffcb43b2a96a6f24d6e9bf4712ed05afaa6b845e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:26 +0000

    drawing otherwise fix born can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
