f3c6ca5684336d876b26df87c7e2ec8575fd5a20
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:42 +0000

    remove congratulations store mainly lean

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import sys
+def exit_program():
+        sys.exit()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
