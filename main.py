eb632654d2102297eff3ce29ef3c6d77a136a61f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:41:20 +0000

    throughout quantity mess by even

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
