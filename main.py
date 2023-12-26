c132c2c6222cf75e4c439d9ea9cc54b3541b62e1
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:02:58 +0000

    actually nervous consist turnover wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
