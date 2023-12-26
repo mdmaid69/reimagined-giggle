a1189cca342c95f559f44cfafd4cac4f4a868140
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:04:04 +0000

    taste punishment yet twist influence

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
