624d4eb71b7cbc296775e4e11ab893f5e486f9a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:59:39 +0000

    hit start well interested university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
