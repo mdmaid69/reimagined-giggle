a3ba67a4294d3a0304a30328ca516601aa623035
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:17:51 +0000

    snow stir exit otherwise payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
