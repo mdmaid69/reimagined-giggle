f9350d10cbeccec2a1ef649611c9e5da8cab2ec4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:30 +0000

    them government cause attempt form

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
