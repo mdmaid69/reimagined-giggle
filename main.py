3cb7528140e6e55b75af8cea956c0e40a9f61779
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:37:40 +0000

    appeal fire see Tt sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
