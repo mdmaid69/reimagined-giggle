4fee4da8b3bcf6b02af1e417d0fc19aaa7977540
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:39:18 +0000

    active glass confident great release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
