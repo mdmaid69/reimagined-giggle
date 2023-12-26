2c9d857f2b4838a1283cfb6afd7cdda564121cea
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:34 +0000

    getup with crisis mean dish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
