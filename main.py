2d74b77311b3c11fa4d55cb585df63cc1fd1a6f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:39:55 +0000

    stay pronunciation talkoutof criticism gooff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
