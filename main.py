f2142f69da7c600ba28eff34960fe58dde415da4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:14 +0000

    sound translate stretch hard concerned

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
