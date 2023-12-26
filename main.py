0c38a97c46abc66e28082bfea3f5096fe39e35ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:45 +0000

    hour workout emphasis sound Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
