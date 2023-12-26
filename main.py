0f798df53ca755f6e3ad448e79e9ff2362484d59
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:36:45 +0000

    uncomfortable count reply hope claim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
