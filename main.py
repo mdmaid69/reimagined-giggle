96c1d22ee1cc87565fef9e52392b403ec62965da
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:04:05 +0000

    moment branch separate hers wind

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
