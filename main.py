5241397e1fe97dbbad3e87ee10f2ea4272dbb1e2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:25:23 +0000

    soon exact corner argument seem

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def convert_to_binary(n):
+        return bin(n)
