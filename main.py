a74079a7bec479e4f173cba5ee6b98525c10f07a
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:10:45 +0000

    clean passage total likely bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
