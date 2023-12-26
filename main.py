f85c90571ed2cabb64c1a0d74a595ec5006b736b
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:58:20 +0000

    comeoff target gas check damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
