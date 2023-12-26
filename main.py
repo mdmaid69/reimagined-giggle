343c250563272541b4b8a3230c095727999c41c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:44:35 +0000

    sink narrow direction same aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
