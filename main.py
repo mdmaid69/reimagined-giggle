b74b06fec16b3d1bde214616cae21e66c7249da2
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:52 +0000

    negative main present past affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
