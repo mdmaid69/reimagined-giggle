273c37d8973af222c24549ad7bdf4a52c56c0657
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:32:43 +0000

    mind temper loss comeround first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
