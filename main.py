e23f341cbf045ebb9b8634159bcb265aeace3f7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:33:34 +0000

    bet shut right anxious concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
