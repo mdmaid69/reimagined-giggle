c7e6ecb53144455888ad46e0c3317381608b989a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:46:24 +0000

    climb takeout delicate office public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
