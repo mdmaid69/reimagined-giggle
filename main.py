8b55cc8665a833da6d2d1780892119a1c6cf6d9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:13:24 +0000

    computer defend steer wild both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
