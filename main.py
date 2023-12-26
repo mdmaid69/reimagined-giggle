37c7de9bc0625b4f8a1addb655b11896f83339f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:20:02 +0000

    turn school we busy idea

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+text = "Hello, world!"
+print("Words:", len(text.split()))
