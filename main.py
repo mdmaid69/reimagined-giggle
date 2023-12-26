73ac109cdd53c23dc8a04eec713bf352c6920527
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:53 +0000

    fear born minute search funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
