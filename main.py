a7f95eb44a1c875b0788d6af7aaf5c784df45433
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:15:54 +0000

    confident picture mixture sleep minute

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def convert_to_binary(n):
+        return bin(n)
