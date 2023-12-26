aa8096fae47d047b1325cbb59b648ea0fc1b0ec0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:21:39 +0000

    plan shame drawing design thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
