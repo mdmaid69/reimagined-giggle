9aae9a0901e271a9edbc619bc4239a0ccbc2020e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:58:58 +0000

    giveback react damage old over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import getpass
+def get_username():
+        return getpass.getuser()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
