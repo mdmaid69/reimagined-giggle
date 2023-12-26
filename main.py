e31216c3b3ce214451566bf8eb04dd3aded33935
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:49 +0000

    workout score board passenger Tt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
