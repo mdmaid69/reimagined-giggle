1772e9ef4d7acabe35fc9fb74f42f3d397dac694
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:58:18 +0000

    future 564encourage building image well

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
