8522248c1ae0612dae3fa2bbfeaa97080f100b88
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:22:22 +0000

    rough give keepoff rarely no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+print(math.pi)
