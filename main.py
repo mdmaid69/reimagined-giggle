e1a4af21c391c934481151f77fadd7e403d58d70
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:45 +0000

    politics Ee gooff breakinto petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
