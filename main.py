9c57ee2d256356d793ea6f6859eb69617f6f5f1c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:14 +0000

    rescue key except subject uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
