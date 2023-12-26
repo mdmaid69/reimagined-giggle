55344a880023f2a74f2cc809b46ed8e7c89bcde8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:02 +0000

    interfere everyone I lookaround wet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
