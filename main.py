0c44ea46848b1666ce568d4fc5a602f5b30d9a5f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:46 +0000

    little age bringback light guilty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import sys
+print(sys.version)
