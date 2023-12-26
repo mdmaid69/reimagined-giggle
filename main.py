a6c715a4016e0f7946759777c067bee1b29365a1
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:17:49 +0000

    accept fight ahead she pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
