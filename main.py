9eefe703360193d1bfba0a2061743e6aeaa619fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:49 +0000

    threaten swing capital keepaway position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
