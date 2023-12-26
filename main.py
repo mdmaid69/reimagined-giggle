0947c6c39c9a756fccbbd5eb73219644f707fd7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:42:52 +0000

    nowhere give character get then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
