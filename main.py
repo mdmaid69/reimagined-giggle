6e6d152fa3905442974c2840617164475116503e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:26:49 +0000

    break pretty unite present coat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
