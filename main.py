5a9a24aca73d8f678ec510ab4796b7b6833ada85
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:11:39 +0000

    row until tense upset basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
