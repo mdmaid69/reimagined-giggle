8141b009af15e381ee42733eba2c0b2bc8eed35c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:39:55 +0000

    wonder regularly let believe lesson

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import getpass
+def get_username():
+        return getpass.getuser()
