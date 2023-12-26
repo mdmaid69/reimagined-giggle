5ca6b5171ddff1326ed6c4d85b3e15341be37350
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:08:00 +0000

    nerve appearance cost uncomfortable snow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
