45f229ff745360ca28c2539adb9e9cdff8904a2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:13 +0000

    sticky demonstrate Nn calm each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+def count_words(sentence):
+        return len(sentence.split())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
