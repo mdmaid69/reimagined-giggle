46b10ab09d7968f699d6ed335dc545b7c575c372
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:10 +0000

    compete vanish bread waste unit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
