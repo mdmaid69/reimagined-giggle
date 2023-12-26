d100aea0b93b7b578816f6c9779678d55792f9bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:42:28 +0000

    show accident steep accordingto reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
