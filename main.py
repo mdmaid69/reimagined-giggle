269f2a18d36d98a06c26292625cfca378c9b0c4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:37:44 +0000

    by account giveup suggestion us

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
