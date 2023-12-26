6a7c1769a9fe40da1ffe5c94f33a20f7c9106763
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:05 +0000

    bus temperature basic nervous store

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
