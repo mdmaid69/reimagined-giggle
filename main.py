f257324cf0f0426fa3fbadbad7b34b47c9d09535
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:30:59 +0000

    either ring pump your fly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def add_numbers(x, y):
-        return x + y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
