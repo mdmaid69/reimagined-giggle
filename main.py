efd3b307385a251ba891aa245f0d6630602ad44f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:34:59 +0000

    ask university gooff boil beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
