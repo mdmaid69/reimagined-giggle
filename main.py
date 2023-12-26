31f29add1f3e80ea667075f3ab01395353ace96a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:33:40 +0000

    interested tear finally job gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
