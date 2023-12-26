c734e5560f806c5a9c0589f87689b0cbdf62c64d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:51:52 +0000

    pollution shape only behave Ii

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
