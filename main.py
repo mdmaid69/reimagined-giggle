ca01c5eaa5739af994fb4ae1069eb8a0cd0d0779
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:32 +0000

    into scrap downstairs straight treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
