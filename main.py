517c0ab8fcb328d5736993400ff37371118588c1
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:28 +0000

    oil active trick sale count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
