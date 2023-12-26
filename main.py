56d7921b390faa708f6a7656ae2f3fadabf42f80
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:36 +0000

    stir old thirsty square pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
