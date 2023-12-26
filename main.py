8ef7ec6cb2faac157e2b3805b8a893b51a76655d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:11 +0000

    until bar movement anxious patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
