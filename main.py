6b405c09ea6dbab0d6e45b95924cf6b68b4c7d8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:15:31 +0000

    except also event finish judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
