e43935233dc85fe18f919d5b8741f99774d4d729
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:58:54 +0000

    mixture operate interest unit shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_date():
+        return datetime.date.today()
