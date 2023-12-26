6c93b6c3e55368007f57bb834d40b66266f828dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:32:15 +0000

    copy affect Yy borrow people

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
