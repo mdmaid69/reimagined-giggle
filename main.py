85f2be1dba3f5e44bbbcb4bb394e9f79ecea0dc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:29:34 +0000

    object scream practical drink protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
