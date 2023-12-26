f8a6c5c29cc9f2824207e4cc421e3ca1d2c19b78
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:51 +0000

    lower dish hers long anyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def cube_number(x):
-        return x**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
