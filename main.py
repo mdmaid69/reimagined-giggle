2d4739b8f77e419a3dd1155cfcec0543eca422e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:21:56 +0000

    male insist even town long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
