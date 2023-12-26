a54dfaf34fc40db6614d68d56f14a3d5ea851004
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:41:43 +0000

    case effort mistake behaviour first

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
