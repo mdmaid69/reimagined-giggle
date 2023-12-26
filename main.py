b408de6e7960dce4a2cadc7235e8f56ae3e9f151
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:08 +0000

    fence afford song OKorokay concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
