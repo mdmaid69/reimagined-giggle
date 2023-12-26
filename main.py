6ceff0af65546b353a9f44271332ecfc2f67d48f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:07:41 +0000

    store pronounce yet call tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def convert_to_binary(n):
+        return bin(n)
