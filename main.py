02cb161875714431f9c1f36b5dbabae59c089d77
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:50:37 +0000

    exit swimming ground giveup note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_array_to_bytes(array):
+        return array.tobytes()
