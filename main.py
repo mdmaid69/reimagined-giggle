1e02d689da02ed32716b3fbf913e096d3cf49ab2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:28 +0000

    study brush correct please warning

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
