b38b89072862e120173d87b9a65f4a71deca6a51
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:07:13 +0000

    increase react tax smell 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_current_date():
+        return datetime.date.today()
