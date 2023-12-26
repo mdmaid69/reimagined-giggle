33579dfa0ebe0e695bbb9a07d7e00c3a337e30d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:03:59 +0000

    depend child ready touch post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
