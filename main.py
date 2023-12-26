c4df9e0308a1e6c67fb00b3e3f1c8baed1f59b2d
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:27 +0000

    after plate squash anything seriously

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print([x**2 for x in range(10)])
