28ebf4205a4002941b0e73b391b1add1c2fcf22a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:42:30 +0000

    gift start board offence nurse

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
+def convert_to_hex(n):
+        return hex(n)
