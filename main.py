401c15734f73b1e0b5222bb073e736461e037943
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:19 +0000

    knock tear turnup anywayanyhow mind

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print("Hello, world!")
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
