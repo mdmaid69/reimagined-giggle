830956e28046dc65a3ce8e4648f12ab42f2e16f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:58:08 +0000

    container lead confused whichever thing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
