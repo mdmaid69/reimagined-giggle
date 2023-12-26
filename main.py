3a70939d741c42bbefca695b9ca770ace91857fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:54 +0000

    proud justice culture official translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
