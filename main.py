61322a526e52136ea5381174754e0c8fb509b938
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:42:03 +0000

    principle cheap affect arrangement head

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def square_number(x):
-        return x**2
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
