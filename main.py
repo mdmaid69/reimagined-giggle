dd34fca3d4ebc9b432e5aae6b94dea6e562846df
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:51:57 +0000

    respect press keepup instruction swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
