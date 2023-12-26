631e7eb618dadeef2ff55274c40df625ccad35cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:51 +0000

    poison belong thumb return tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
