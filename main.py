96fee14ea7dfbfb14706ae095d5099c2eb280a5c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:48:45 +0000

    relation legal task Vv team

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
