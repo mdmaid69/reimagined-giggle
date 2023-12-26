c6b7830cbcb95e23a749abe7f9196fd8a9dc7535
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:54:38 +0000

    giveaway freeze call behaviour trick

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_logarithm(base, x):
+        return math.log(x, base)
