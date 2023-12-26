98abecb34cdca127fd0f5f486734fd903b0817ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:32:30 +0000

    uncertain style either rain I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
