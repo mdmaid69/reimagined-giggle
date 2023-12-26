f410ca366221083d4dcd07101d9d548564e66c96
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:01:04 +0000

    wash century shall offend through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
+  import os
+  def split_path(path):
+        return os.path.split(path)
