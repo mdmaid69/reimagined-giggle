91aaa15c431d0bbf588c218dd9d9d3e7178ab9ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:07 +0000

    prepare colour pencil on unite

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_words(sentence):
+        return len(sentence.split())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
