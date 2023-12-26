63616a28d2fdb490459194e83eb67691e4cda27f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:08:41 +0000

    interview save below famous accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
