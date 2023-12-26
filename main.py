0819dc736245a55bbfa44768e9ffa177aebba56b
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:47:02 +0000

    window ahead spare mend heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
