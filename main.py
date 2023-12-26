a443036c2b115bcc7f698ff989694b4308158347
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:34 +0000

    person race type obvious its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
