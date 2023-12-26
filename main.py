ec9315136e06c198c4660f5ec898134bcf628e0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:59 +0000

    black pretend forgive block thought

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def multiply_numbers(x, y):
+        return x * y
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
