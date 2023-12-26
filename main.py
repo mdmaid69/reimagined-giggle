1f90fe15c17d21f8d78316a0c4ab12b25ed8d95b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:58 +0000

    look certain container until fold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def convert_to_octal(n):
+        return oct(n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
