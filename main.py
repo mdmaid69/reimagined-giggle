ea6522e1e4bfebb854384fe0b89d298d0bbcbcb6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:22:35 +0000

    sit spring basically set gooff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
