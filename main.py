ea1059f1da1bb2c044302fa2304c25a58164daa4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:33 +0000

    arrive meanwhile weather move later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
