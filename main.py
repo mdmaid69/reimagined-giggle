d50ac12c23eb35b61ed575513ddd62353f0fbec8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:00 +0000

    wish bag entrance right so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
