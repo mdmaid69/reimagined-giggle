f840e04696ee151e819b3e8c7712a2fe31fbdb57
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:02:52 +0000

    possibly brain 566end disturb rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def convert_to_octal(n):
+        return oct(n)
