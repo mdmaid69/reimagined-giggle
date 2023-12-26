dcebea8432d26dff2a0d139c04531bdc4f148da1
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:54:08 +0000

    active few its tongue even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
