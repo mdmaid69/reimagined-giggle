fe6ec3a9b0c13b2178f8cf9b6eb77a4ac0da76bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:59:18 +0000

    takeover movement like report powerful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
