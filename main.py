b1c0d6cb180f3a185419eea5ec515354399f2769
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:33:16 +0000

    gap goback(to) yesterday sign target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
