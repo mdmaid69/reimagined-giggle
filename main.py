fc6fb727b04596af6d46e8004dd14fda86a77e28
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:19 +0000

    feed work climb fast sink

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import random
+print(random.randint(0, 100))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
