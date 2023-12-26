5ded95cc4025e54b3e5bd769db0209909da400cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:32 +0000

    contrast wear move today note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
