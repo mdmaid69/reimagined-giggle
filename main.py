49d2a418f01c4049296f7563ab9d59af8e96dcb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:00:50 +0000

    pleasure getoff section afterwards makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
