edc0ed988ab9b2bb2cb4ed9c2eb8349062cf42a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:05:36 +0000

    picture female tall base patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_priority_queue():
+        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
