f104711bb07acffeb574fd016e095fba3ec64502
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:40:37 +0000

    mysterious charge Uu control hardly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
