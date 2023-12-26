94140f690e6f86dfc6a6e8de2d3dd2f96cdbfb4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:33 +0000

    table smoke glad or uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
