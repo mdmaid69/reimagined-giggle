aa6b40166b32498db86354ade8480cd8fe45f087
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:10 +0000

    vision company load forever cut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
