2021af10b1976cd473c08d7ed64341973f307bed
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:31 +0000

    guard exam where discuss sea

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
