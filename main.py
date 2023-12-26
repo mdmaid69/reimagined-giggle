dce517d4e52a4b6e1bad4b5c3b953d940ff45268
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:17:37 +0000

    maybe cause wash board last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
