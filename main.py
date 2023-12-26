505a28434704122359fa025ec70a63d65546b9d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:57 +0000

    such turnback wave cruel runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
