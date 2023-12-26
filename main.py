58d3195abf41379c167bc535e97ed886c47b58a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:38 +0000

    finish on memory mysterious challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def subtract_numbers(x, y):
+        return x - y
