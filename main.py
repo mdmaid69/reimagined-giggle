e5fe1c4d83790f61a8f9a93745ee0e0e9bd58c8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:48:55 +0000

    satisfactory stare machine comeover character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
