e748169090897882f4be063879c06d242bd6fec3
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:36 +0000

    complete regional keep right protest

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
