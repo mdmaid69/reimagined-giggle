21961f9aad10cbbbc01d499f24e6d3989e125fc7
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:26:59 +0000

    react little wind underneath decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
