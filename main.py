a91c0406bcd6fa58b1d0d00233b39420a7afb210
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:33:24 +0000

    contrast food rise bread rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
