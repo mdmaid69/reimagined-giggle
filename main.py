ac47a3426711671551ca32191ffb15e5b6116584
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:49:43 +0000

    past performance corner central exercise

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
