d810f7c3e4e42219358e12034df3b0636ecb4bb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:17:57 +0000

    skill choice tie pure start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
