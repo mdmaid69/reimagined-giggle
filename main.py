bd7c06ae558f427b7e19c6862876925f90d06b2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:20 +0000

    plan used smoke grow opinion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
