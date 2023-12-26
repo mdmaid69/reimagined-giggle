9c096dd9fbf462ecd9be0d5e27a603168557b64f
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:17 +0000

    over guess emphasize contents spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
