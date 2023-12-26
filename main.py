46b3140ad717f326df4cf93c4144a44c65bf152b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:46 +0000

    section everyone mass into run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
