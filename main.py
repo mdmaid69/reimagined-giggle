9dc312996011772e7566eaf941d6a3b3deb7df07
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:39:21 +0000

    bread stare escape ready convinced

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_typecode(array):
+        return array.typecode
