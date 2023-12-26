3cf16e67c935ccb73c96c7a11618e2a608d7c4f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:40 +0000

    content lie tidy hit increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
