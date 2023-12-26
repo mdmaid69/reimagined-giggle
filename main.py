cd712f6cebbfbdb40052a6870c378e6723783730
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:57 +0000

    sport rent trade food onto

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_max(lst):
+        return max(lst)
