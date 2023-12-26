d5fb5ff2d97377f90a6cc806034dd9e5740533e7
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:09:50 +0000

    narrow soon justice top more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_length(array):
+        return len(array)
