34255bea22e5c86c7ec74f6744c7be7908ae453f
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:28 +0000

    comeup rule so difference Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_file(file_name):
+        os.remove(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
