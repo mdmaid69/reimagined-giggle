2f60b953dbb0c82594695db2581fd4c53155fcab
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:59:36 +0000

    important good drink can our

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Powers of 2:", [2**x for x in range(n)])
