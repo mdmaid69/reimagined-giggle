d35b435d51272be31a864d979471b41ebb22da2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:07:02 +0000

    produce pencil than problem thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
