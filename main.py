052a80011c8016b2e5b50503857ff81f8ac28e51
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:44:33 +0000

    somewhere money confusing approve single

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
