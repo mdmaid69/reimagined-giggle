5ec5cff4b2a08bed111b54668f8dc9ce9b1551ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:29:36 +0000

    learn normal splash suit tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
