317cee2cdd769b6d24d96fd3645c010a7a09d2ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:43:19 +0000

    compare pocket public either conclusion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
