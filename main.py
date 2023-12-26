2a4f259ed9c503d5c209b7e9555c70c25c75a898
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:44 +0000

    lesson danger about circumstance turnaway

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
