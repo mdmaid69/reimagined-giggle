07e508efabd120476066e41c6a70d71257c6fa3f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:20 +0000

    blame agree copy excuse sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
