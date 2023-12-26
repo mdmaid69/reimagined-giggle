1877df22be904c6bddb915b7f21868c0e57cf6c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:41:00 +0000

    no memory creature uncertain rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
