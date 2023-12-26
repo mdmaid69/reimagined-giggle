6f9aaf79312b6c436b8a014441059010d8e4d50a
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:34:05 +0000

    language wheel object lookback(on) promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
