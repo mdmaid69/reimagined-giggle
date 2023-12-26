41a63b4304be7065a140641df40898bdaf17a993
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:54 +0000

    since pour relative present in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
