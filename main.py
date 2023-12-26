67131ba5549b99c27f2289914ce7553836fd3cd2
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:10 +0000

    floor comfortable bound make gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_base_name(path):
+        return os.path.basename(path)
