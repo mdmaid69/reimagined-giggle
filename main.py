f365aa2393fa9550df667b37b797b71c2c47c569
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:48 +0000

    supply lower closed persuade flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
