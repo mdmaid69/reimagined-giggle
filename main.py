ee8e809ecd479ff88de5b505cb581b825188d1f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:20 +0000

    tap tongue beneath child so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_min(lst):
+        return min(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
