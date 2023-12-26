96ce0ae4fa7bcd8310d3137514a847a633fd8b9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:59:30 +0000

    period lot Ff boring nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
