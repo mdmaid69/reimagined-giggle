544e5ae980512a1bdd6a9fee88703bef0e8a33ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:45 +0000

    question difficulty cheque stamp order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
