ab7e78d81adb7b87ff4d4a8ba084e79fe2c576a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:27:25 +0000

    imagination uncomfortable cheap dish lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
