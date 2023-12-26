961070950be51685d4c00dace47b8dcb389ff6bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:24 +0000

    concentration sleep nothing story could

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
