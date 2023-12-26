da7f26e8beecadaf233fa2bc2c236e5d37ba8643
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:05 +0000

    match go squeeze feel pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
