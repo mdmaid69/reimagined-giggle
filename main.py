64ee18b83cecae03c8476fba1e64493eb0aa0918
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:59:47 +0000

    comeout unfortunate cheque spray confused

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def remove_duplicates(lst):
-        return list(set(lst))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
