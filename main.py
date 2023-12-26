16d0fedcd620827c7182634847189ffc6ac8609c
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:14:26 +0000

    excited guide tonight fun replace

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
