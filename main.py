049938e12e8d83bf4e976477a76898f8f9865fae
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:19:35 +0000

    apparently look survive container whisper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_length(array):
-        return len(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
