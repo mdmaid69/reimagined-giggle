e5ad1a5cb6eb25c9e322599d22d8f405b39e6167
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:33 +0000

    breakfast introduction culture in interview

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
