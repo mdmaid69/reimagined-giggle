9c827c88e7816baa34efdbfd08313e34af93f782
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:40:18 +0000

    giveout deep official zero gas

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def count_words(sentence):
+        return len(sentence.split())
