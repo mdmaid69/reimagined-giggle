154eee8b918b669999070910ab3aff313bc71e4c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:09:24 +0000

    568energy sign will after reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def exit_program():
+        sys.exit()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
