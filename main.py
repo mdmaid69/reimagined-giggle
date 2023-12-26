5049dcc4ab61c456b86fb9d9353225c14f4e1453
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:17 +0000

    pay have quarter hurry down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print("Hello, world!")
