c3b81e01e658d9aa60ce20bf81160237b8bd0d2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:31:13 +0000

    situation takeback aware weather slowly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
