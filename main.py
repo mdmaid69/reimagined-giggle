3d98878c2e3373892b70d8fa5290b76ab75040be
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:40:30 +0000

    surprise separate share treatment carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def reverse_string(s):
+        return s[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
