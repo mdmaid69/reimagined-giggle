3c9c2fb3a6f2ee4c5fcc1842f8512c4ecb487184
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:55:18 +0000

    loose comeoff whatever exactly wrong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
