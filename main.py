7a2253a89acac06891fe4c775ed3e7a3562ded9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:26:53 +0000

    clue agree instant 571enough shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
