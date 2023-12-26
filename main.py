4b8e7bec428eae41bd0bc32cc78f289956fbf614
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:40:38 +0000

    introduce argue getaway friendly sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
