3195d986eaf008966b327e7f9f6dce57f0cf5aae
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:25:58 +0000

    easy welcome ought late plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
