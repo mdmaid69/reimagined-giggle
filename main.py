4942016c0132c73c54feda9128e1fa46d901e24e
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:27 +0000

    steady period leg thinkthrough miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
