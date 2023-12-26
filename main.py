3dbda076f65515d481f20fe05e45c09b86f3a396
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:22:58 +0000

    scheme beyond little seriously realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
