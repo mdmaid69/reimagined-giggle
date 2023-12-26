156878ca1f7855092ba5754300f6406183f2b230
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:27:22 +0000

    television scare only just person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
