dbba27473eb8762e59093bbcc39913689de1df97
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:23:59 +0000

    gas desperate team their wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
