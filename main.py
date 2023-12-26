4a093087dc8332b70ca9dfde3d13b1c09f1fa008
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:15:50 +0000

    plant pay organize move steer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
