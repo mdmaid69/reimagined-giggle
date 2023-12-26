ee179f0920ee04eca7eaf76b488d30ff27bfbbfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:54:08 +0000

    571enough extra accordingto correct lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
