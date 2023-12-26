0953a6852747fdf1edd2e8114e034a474d612adc
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:21:46 +0000

    pretend natural trip law early

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
