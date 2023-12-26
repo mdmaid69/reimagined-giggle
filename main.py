1016a4070f7ba21478c81a6c5bc1e1a5317a35c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:47:49 +0000

    themselves stone feeling pay theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
