3acabbf6c46cdbc153a439b4cc1e43b5be80b489
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:19:01 +0000

    tape move sister dull table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
