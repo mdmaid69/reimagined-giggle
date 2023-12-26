c10a956aae3ba3f3a2263276f5b0f5e6c54e0046
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:33 +0000

    dress science believe contrast sink

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  def cube_number(x):
-        return x**3
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
