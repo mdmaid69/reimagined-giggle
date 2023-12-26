09da45b8ea01176cb948b2bffe87dfa2ffc7d571
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:47:04 +0000

    wire second exploration and cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
