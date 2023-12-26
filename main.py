2bd69f781440d2f7785d6caefc96a42f3febb497
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:16:02 +0000

    science judge dark land fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
