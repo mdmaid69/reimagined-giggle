cc92b2462bec5d9376abaea734fdc964fbf24cd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:06 +0000

    flight children smell write spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
