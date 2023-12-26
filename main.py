7e82968c33e3e6ef947c739c2e219390d6f75eab
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:10:59 +0000

    criticize split on exploration injury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def square_number(x):
+        return x**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
