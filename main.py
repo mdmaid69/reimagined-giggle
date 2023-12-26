5ac8ba60fb8f221da6799511dde29b745f43f349
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:09:59 +0000

    thick mistake explain steep care

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
