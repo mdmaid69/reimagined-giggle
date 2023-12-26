7933d2a170d01caceb3e319363fd16f8c6d03e57
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:26:12 +0000

    giveaway finger proper why weekend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
