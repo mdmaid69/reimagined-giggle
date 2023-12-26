b6b1de2095d41206600ee0bb0e0d6525146d445b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:12:49 +0000

    window difference like heat body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
