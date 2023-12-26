4530b255606063cdccd03cb77e8fc71371223b50
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:35 +0000

    care consider left breakup likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_current_date():
+        return datetime.date.today()
