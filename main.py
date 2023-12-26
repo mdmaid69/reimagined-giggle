e7649577b7037c191fc6dccefb310f5dd3ff72d3
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:05:50 +0000

    sometimes attention meet radio surprised

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_binary(n):
+        return bin(n)
