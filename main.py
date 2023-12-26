f2c27d6f941ca06055690d3eb41502e30b4a07a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:23:19 +0000

    quality refuse Dd Ii thinkof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
