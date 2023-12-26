bf83244cd8f9857348b18b14d058054dc1ce65a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:01:19 +0000

    high complaint payment false contribute

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
