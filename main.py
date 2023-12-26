4cc3f0a471ab89e906d4231cb976cceea1c097b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:46:22 +0000

    experiment reasonable food cross frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
