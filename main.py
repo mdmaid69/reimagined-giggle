334668e62595878b80dba2e1f23b8b7ab5e3df86
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:05:16 +0000

    slice shout workon cruel cost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def roll_die():
+        return random.randint(1, 6)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
