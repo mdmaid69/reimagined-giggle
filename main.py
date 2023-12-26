c8f6932d20a358695d245b6fa6bfa382f4cd954b
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:21:23 +0000

    anything friendly again now petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
