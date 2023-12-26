ab8fe412a6eccbfde2cc04d60ea845b5457808eb
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:42:43 +0000

    fasten runinto lot motion Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
