d1c161e85af25affa9bb83d21924fdeb7cbdc460
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:05:02 +0000

    suckme neither 565end many lend

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
