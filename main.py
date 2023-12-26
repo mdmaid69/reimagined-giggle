586fb0cdf8e3a87a6cd5963e017cd821241c1d7b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:27 +0000

    print because carry disapprove light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
