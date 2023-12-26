b54c45a90642ceaeca949f52abb10d11633f1da9
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:07:36 +0000

    furniture serve ready content acceptable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
