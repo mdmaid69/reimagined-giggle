12eccf7f8ca87b5f29464b166c09b3ea88b6a181
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:31:01 +0000

    light ride swim dangerous start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
