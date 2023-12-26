af29d1f9b6c1dcf87a0212f6f616e9a4bcdf45fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:52 +0000

    course cool peace Pp spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
