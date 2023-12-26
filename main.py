ade4b4cb93697ef14236bb68036d5b45b49fc291
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:20:40 +0000

    arrangement finish post upwards mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
