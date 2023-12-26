f53b6035eb95f0c2c5aeb3a06ffbb92bdf8391fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:19:02 +0000

    concentrate section plenty degree brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
