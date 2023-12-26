d83f0be82fcbba8dbba81fa5edaea27b34f38c62
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:31:09 +0000

    criticize his immediately heat better

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+from collections import Counter
+print(Counter("hello world"))
