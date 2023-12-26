8d8f5935f4072708213bc4c58f3855af1925edbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:59:42 +0000

    square satisfactory forget serve complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_list_from_array(array):
+        return array.tolist()
