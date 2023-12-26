42b2f53f1bcd29c86403533788fcf808c61f3679
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:22:38 +0000

    that teach relationship organization available

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def sort_list(lst):
+        return sorted(lst)
