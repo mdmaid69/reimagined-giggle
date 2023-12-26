30118be39461bde583c6087554259a01f56edb48
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:45 +0000

    vote find smell soft flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
