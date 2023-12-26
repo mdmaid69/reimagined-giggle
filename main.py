e1191f924a477275200839f6d3368ad3ebd30771
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:14:18 +0000

    concerned university service favourite sell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
