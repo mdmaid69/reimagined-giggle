e837621defd8e41f407be2689067797240011a56
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:13:18 +0000

    section Kk region fault even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
