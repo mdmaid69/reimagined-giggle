8da401383e72dc4175def67d656dccef2ceddbaf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:48 +0000

    quick comefrom whatever wear borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_odd(n):
+        return n % 2 != 0
