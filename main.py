80fd7c311fc5580c43ad026eb122f2094af0e114
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:04:01 +0000

    turnback go cough future foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
