1489737ea7b4d08a861be31787ef0de5173fc9a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:25 +0000

    offer remain dish my like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
