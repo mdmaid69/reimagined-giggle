ca1d701fdc5bce54f9a38008042706da1ab2d724
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:56:05 +0000

    world float swing scene sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
