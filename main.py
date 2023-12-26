583aae4243c0cc733de9885282cb2d8bd247a0ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:01:11 +0000

    experience with warning condition suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
