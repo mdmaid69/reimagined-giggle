991fd03cf00e8caedd3f40ac2d941e3de356a56a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:50:57 +0000

    result swing accommodation song fork

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
