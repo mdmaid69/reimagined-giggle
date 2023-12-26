5fe0c45c59e5e2ce5b6c1c9bed6f4dc51e244ac1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:03:25 +0000

    behaviour university arrive form sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
