ed517e08e296bda99031689c2a4ebcc171cf1f2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:06:56 +0000

    off hollow I seriously unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
