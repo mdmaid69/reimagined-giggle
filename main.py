691b0e11aab9e6358949f26e10d69c7168bbb57b
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:52 +0000

    environment offence freedom kill refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def is_odd(n):
+        return n % 2 != 0
