a5dda7dfe865a1d1d52611b356ebe7543a57e51a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:51:13 +0000

    burst separate may prove whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def remove_duplicates(lst):
+        return list(set(lst))
