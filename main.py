19c2da8c376c5f8e4fddcf7dd209471138d4d989
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:25:37 +0000

    peaceful this workupto usually detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
