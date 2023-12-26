5a7732414977a6112e5bf4f0d92114a3b708ac6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:49:41 +0000

    572enter joke trip out interview

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
