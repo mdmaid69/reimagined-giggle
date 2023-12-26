e33bc2cd08831686f4900e76cd8568b7feb63eb7
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:50 +0000

    him desperate finally radio rain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_volume(length, width, height):
+        return length * width * height
