59b45e159eb7b8cb8574476b9b53be540632b6a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:27:18 +0000

    burn cruel worth evidence advantage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
