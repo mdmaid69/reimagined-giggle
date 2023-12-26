352f3f78ecff752128eaca765c510c752d9e4828
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:40:00 +0000

    live takeaway come left amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
