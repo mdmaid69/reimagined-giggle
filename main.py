ac57e8c8febb2348a7c9ae061a7da1a73fead2c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:52:48 +0000

    search treat chance deliberate otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
