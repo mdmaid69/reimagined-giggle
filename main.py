7dc58615af0f76f3332e96da2782a7faf91a6f80
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:52:18 +0000

    talk drawing free finger moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print("Hello, world!")
