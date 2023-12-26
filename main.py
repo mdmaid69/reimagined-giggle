ec6251cd687f0df98ecd5c8d4a20a56ef08b0e01
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:52 +0000

    noise satisfied solve fly touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
