4be91d15d4a5a8248b739287175eab634e3905ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:19:36 +0000

    professional run you on corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def subtract_numbers(x, y):
+        return x - y
