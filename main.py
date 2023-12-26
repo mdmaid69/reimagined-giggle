2b61c7fa6108d973a6a923c661b36bbdaff21aa2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:12 +0000

    separate dream hope long fight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
