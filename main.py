a35cee2a38502ae3c537883f0d061f160b662cfd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:17 +0000

    manage settle crowd accordingto takeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
