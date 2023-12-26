b7fe484bf7c3792a63af280fd09519118f1aea58
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:07:15 +0000

    doubt patient petrol born appeal

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_list_from_array(array):
+        return array.tolist()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
