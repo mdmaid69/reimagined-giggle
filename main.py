5ecb243fffebdfa1fe10b125b8c109a364f9ead8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:06:54 +0000

    fire under bottom will since

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
