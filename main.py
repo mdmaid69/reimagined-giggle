510c7bfdae95f88e4c517b1c19758a921a1eb7ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:19:51 +0000

    turnover crash rush true grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
