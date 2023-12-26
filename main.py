2ff03da7bf78cb986d9b4603abb01d6af3c93a2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:19 +0000

    argument light there moment contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def convert_to_octal(n):
+        return oct(n)
