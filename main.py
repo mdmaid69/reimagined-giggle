9ca0ce752a3bb2733463ce570420457f94d0ba21
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:44 +0000

    judge relative bang sensible unable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
