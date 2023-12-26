6b4de4b1485765755aaf6b49de08769244fbfd82
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:10:56 +0000

    aim spill tie avoid soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_item_count(array, item):
+        return array.count(item)
