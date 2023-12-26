7d05ad2b2e314fc8bc7da30474a940e811a7517a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:17:26 +0000

    need measure mark mend forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
