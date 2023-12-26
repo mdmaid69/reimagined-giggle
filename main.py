50f2c4728206f83bb2e0f0f111bbbb9f290d1e7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:35 +0000

    scare male extremely order ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
