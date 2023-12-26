b41d9705724ced9844204103135ac8e10d30e3e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:11:00 +0000

    late motion country lunch close

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
