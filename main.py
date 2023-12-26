6a554f1eb7b873f79f8ca3a643c9f0158682c478
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:13:46 +0000

    circumstance likely law piece shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_work(force, distance):
+        return force * distance
