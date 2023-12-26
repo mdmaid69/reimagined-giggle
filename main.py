6721d90261d189e2d109e872f6d740f5a07162f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:30:28 +0000

    history task circle intend embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
