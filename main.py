d10f5e38ada0d2cb4248e83829cd9b6316d2f448
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:21 +0000

    human so translate floor receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
