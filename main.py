5db5847a1f2547373766543e4ac974a29ed8d9a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:52:46 +0000

    feeling account word might if

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
