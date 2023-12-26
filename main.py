5e9df58a2cda5e0cc382881ecdf810e54db94984
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:48:29 +0000

    disappear important taste tend comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
