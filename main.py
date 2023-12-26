ca425f8bb85f1ce67fe938b6c0bb363bcecef34a
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:53:05 +0000

    length dream scream complain they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
