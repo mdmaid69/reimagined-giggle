97a207b050aa92dafc04a24696fbe8afe1889011
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:39 +0000

    remember care guess interview hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_ceiling(x):
+        return math.ceil(x)
