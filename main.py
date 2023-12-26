e8dcc87ec9e06afd5e085db94770345c12595fb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:19:23 +0000

    exciting 565end through cope discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
