c66f8013edd0e67066e54448362f9fd0b8d14273
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:40:33 +0000

    breathe copy trap piece often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
