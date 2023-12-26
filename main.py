cae074e8f188d3978859a31cc4e9195ea8153fc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:36 +0000

    here steal notice sight into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
