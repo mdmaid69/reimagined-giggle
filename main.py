1516c8a612a65a8ef45db40860e1d5e2ca442b0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:39:17 +0000

    description environment touch wonder brain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def subtract_numbers(x, y):
-        return x - y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def reverse_string(s):
+        return s[::-1]
