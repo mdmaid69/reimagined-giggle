fd728a4400d88e7f26dddd9042e750b0b65ea49d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:35 +0000

    stage speech wind stick brief

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
