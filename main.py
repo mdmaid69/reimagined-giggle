8ec1aad46c3e2e9addd7ea47f33ac3036519b212
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:23:06 +0000

    risk friendly combine little suggest

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
