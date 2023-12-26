6c2b8efadaae5b76f3bbc600d4848961d886f495
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:20:47 +0000

    keepup all manner share deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
