a9a2ac2a3b6568620d85fdd4020414d1450ec623
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:34 +0000

    score level hide reject uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
