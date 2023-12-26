3bdc71a6c9a96ab79f1a9adc49f6fd8b59d5b010
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:20:42 +0000

    hers Ss funny evening centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
