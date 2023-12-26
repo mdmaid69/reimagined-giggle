f3a01a68f9a924d525a753f2c7d82010ab21834c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:31:06 +0000

    carry add stamp workout spring

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
