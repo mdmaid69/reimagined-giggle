04c011de1b806b68c5756b795dfbc1982e7f055c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:52:14 +0000

    tip never prove rest only

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
