9d3497df1ebbd6e201245c1d364960cd87f34031
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:00:16 +0000

    Ff hold without talk information

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
