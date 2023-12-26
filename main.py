c903f90bf18e18560a1f0820986e4d43da906590
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:34:44 +0000

    consider by view permission mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def cube_number(x):
+        return x**3
