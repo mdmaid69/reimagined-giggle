4b221b6e5db048eb0f8688be348861e9d2b1d91c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:59:59 +0000

    drawing pour building either before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_min(numbers):
-        return min(numbers)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
