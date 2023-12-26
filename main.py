e5b4609f5b8b393e41a16758a0b9a0fe89cd6fa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:52:12 +0000

    match everybody obtain afford tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def reverse_array(array):
+        array.reverse()
