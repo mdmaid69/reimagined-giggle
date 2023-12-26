21e72c44a8b564f96183447ffcbafe66d8294771
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:36 +0000

    wind alive floor shape beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
