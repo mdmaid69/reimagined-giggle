8ae5c1d74fa22e1b266c4b3f86c5ec1b156253be
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:41:36 +0000

    black injury shallow handle train

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_octal(n):
+        return oct(n)
