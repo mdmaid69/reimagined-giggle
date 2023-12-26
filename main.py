337f8575032b7a54e257432aca3c924280f6bedc
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:26:21 +0000

    imagine official allow female exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
