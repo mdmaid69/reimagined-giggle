c73b72e587b7c935d8a7bec3cd56e24514a9ee2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:13:58 +0000

    fire steal visit season understand

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
