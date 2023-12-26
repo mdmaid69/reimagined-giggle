0d8835f35a7d476f06e38366b60532149732c787
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:29:08 +0000

    born shut challenge story name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def sort_list(lst):
+        return sorted(lst)
