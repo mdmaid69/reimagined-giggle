d09f1327dd437e969d73c18d44c1c61fbf9fb55d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:12:46 +0000

    bottom inside explain regional swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
