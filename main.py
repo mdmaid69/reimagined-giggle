42770e3fa8ee06bc6c92cc9cc60b39ff07e5179f
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:31:28 +0000

    about reply influence opinion treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def multiply_numbers(x, y):
-        return x * y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
