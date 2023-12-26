4619f5df0098da6a673b2f8e53396c4f79c06db3
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:02:57 +0000

    lot manner seem shallow wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
