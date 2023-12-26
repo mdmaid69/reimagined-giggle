4a80ae89d451cc30770538ef375f46852ab245dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:56 +0000

    stupid stupid choice communicate afternoon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
