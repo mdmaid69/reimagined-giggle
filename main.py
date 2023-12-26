adfedb3e90d8f8005b7c07b17069839fb2aa2f13
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:24:15 +0000

    now steep smooth brown book

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
