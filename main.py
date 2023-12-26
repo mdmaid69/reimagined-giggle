27ff16d1c40f093643cc9229dcaf81958103a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:02 +0000

    spin exciting them Pp juice

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
