9df51a7eb175a9f9736b4098747b82e5f5c6bcc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:48:23 +0000

    that suggest offence change part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
