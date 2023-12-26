59230c61b3f9f571028a84bd70ae0f2283a19005
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:57:43 +0000

    task shut different doctor measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
