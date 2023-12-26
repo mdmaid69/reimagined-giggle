7fba80281209914e58df6846543cf28ca3257fc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:42:49 +0000

    free otherwise key goaway proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
