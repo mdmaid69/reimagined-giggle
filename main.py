30985dd97aee2baeeb87094118fbc94305d5ba6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:36 +0000

    round environment would pronounce shall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+name = "Python"
+print("Hello,", name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
