603840fea7c3ff502fdf1bb1e8c562a6ed28b6e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:56 +0000

    intelligent official shock succeed pattern

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_max(lst):
+        return max(lst)
