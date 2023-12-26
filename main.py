dacc044ce3303fd6b357d8ecb33381f856dafbe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:14 +0000

    insist feed authority song test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
