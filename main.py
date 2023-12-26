e9ec70c08d6fe7ab7e96588060f6ddfe969641db
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:59:07 +0000

    damp dry common medium flow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
