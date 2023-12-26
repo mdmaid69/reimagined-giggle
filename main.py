52661db3a25b0b12d58067f14ddec362d360bd2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:29:49 +0000

    tragedy shallow their final practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
