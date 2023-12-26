c02812adf51d92b0e730a16d6b066fa254687ebe
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:08:31 +0000

    both great different unkind fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
