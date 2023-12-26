fb981d3b2e6cfb5e1e1de8dd6b1fe5d9b7038963
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:02:04 +0000

    even cheek nose treat boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
