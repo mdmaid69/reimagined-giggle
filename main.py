4525bdff98b0d50a59f3692687d7babc364cbdee
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:48:45 +0000

    mouth lonely problem talk bend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def clear_array(array):
-        array *= 0
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
