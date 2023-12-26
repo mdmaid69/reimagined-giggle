c5a218fdcf2882b5a461689f2b175b7ff700a367
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:50:57 +0000

    all image pollution wheel turn

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def clear_array(array):
+        array *= 0
