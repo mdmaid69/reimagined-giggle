5d1a0ee9f3efcac40aa69d3225ada7f5c4da92fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:51:16 +0000

    rule vanish measure me turndown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
