82b825ed9dc67c4bb2292b40f94612411b1965c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:19:00 +0000

    mean smart beneath certainly support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
