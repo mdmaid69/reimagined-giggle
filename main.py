157764e31834855d0856b47549658202463ad118
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:36:59 +0000

    autumn change reply divide section

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
