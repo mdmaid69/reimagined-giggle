a7d1d249d286ed25b12aff5df3241f5341ebd29f
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:51:39 +0000

    sail feature surprising polite adjective

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
