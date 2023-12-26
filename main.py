9f2d6d44261ea3e2182b1bb002e4c210fb7bc525
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:26 +0000

    shock equal emotion thick present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import datetime
-print(datetime.datetime.now())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import time
+def get_current_time():
+        return time.time()
