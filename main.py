d9781b6dec106816d865fb7b315b072a3aa7afb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:54:44 +0000

    illness willing deliver shoulder flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
