7ec88d8131e03ab5fe9c840345516ce4711376c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:05:06 +0000

    order middle somebody squash pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def get_array_as_float(array):
+        return float(array[0])
