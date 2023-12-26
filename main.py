1f9bb2dfaf4f2f619fa85b2cf77e79fda3a250d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:00 +0000

    confuse belief Pp earn adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+import sys
+def exit_program():
+        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
