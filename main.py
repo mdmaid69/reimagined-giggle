940345e90ba3890962cad4015244a6ff76ff14da
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:56:47 +0000

    answer floor Tt free lay

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
