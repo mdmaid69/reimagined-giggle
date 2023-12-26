f4158b8bb571d05fc7c5d08bfb266cda97d6a27a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:30:26 +0000

    he bang step mass unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_itemsize(array):
+        return array.itemsize
