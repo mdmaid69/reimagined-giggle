387bbda8bffb22a11b100e9e4ab21df11c7e04b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:40 +0000

    nearly custom unit pull stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
