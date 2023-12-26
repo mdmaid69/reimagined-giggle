649c67713b532205e7b6d47b3cfeec8afe0f20fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:47 +0000

    tip lesson mess still let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
