7e3497919c8bb8ee9b93f390b185622fa7bb3b7f
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:48 +0000

    unusual difficult back dress connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
