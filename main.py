4c4cfb69f8933dad57bd7104ff4fa06f83c92e6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:27 +0000

    form cheap balance side command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
