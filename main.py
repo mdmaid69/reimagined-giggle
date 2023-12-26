959ff679edc45ed6c62844d47726c8dacd7c7d48
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:10 +0000

    start travel shall report contents

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
