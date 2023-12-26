e957ccf89151a6ff9b5a5f66ae0ef638a39d07e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:26:45 +0000

    glad birth delight mess tell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
