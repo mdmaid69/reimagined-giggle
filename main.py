2a4025e2a857b4b4d7a848dfc97796cd13722561
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:29:10 +0000

    avoid now urgent tradition allow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
