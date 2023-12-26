d1821728daf922c4dc2c940652dd9b445acb2b36
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:23:57 +0000

    people either tone aware bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(os.getcwd())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
