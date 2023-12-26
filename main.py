b946c000052f610f31e009da1b91d9e599a8a63d
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:22:20 +0000

    negative balance loud excuse straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
