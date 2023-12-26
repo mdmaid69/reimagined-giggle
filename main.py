ecc77b0f251ca7a7aa8ecf13cca727a1a6bab46d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:49:03 +0000

    along dare after remember bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
