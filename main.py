3643b3f5673be0281d7b671fa2915ae3eddef3a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:37:59 +0000

    afterwards high violence bother breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
