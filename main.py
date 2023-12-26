a3436c5489c26854822e6c10c17769390bd2d6e0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:31:58 +0000

    damage deep window fence Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
