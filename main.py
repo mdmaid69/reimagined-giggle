2c09b934e58b2bee52008a8c2c60a1ddcc0732a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:37 +0000

    health bringdown wild direct uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
