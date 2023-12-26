3183e23bcce68bc0f2a8b26c777a547f4a0a8663
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:50:25 +0000

    favour friend once thick home

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
