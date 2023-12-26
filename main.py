455bdb400e05efe27a36eebbdb97cd4f56dfc1bb
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:42:21 +0000

    scale entry ring cry meaning

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_pressure(force, area):
+        return force / area
