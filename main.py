a388feb9b2a1679ac4d33fd2b9c03ff88bd21bc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:02:10 +0000

    love soon usually maximum importance

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def clear_array(array):
+        array *= 0
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
