c35fa10a7702cd8b2acf637aa57731acf799884e
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:21:31 +0000

    aware rest explanation aim actual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
