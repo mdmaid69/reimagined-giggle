14b532952afc95dcefb7f25efcf23ac10f5f8787
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:59:12 +0000

    pay typical organize run Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+print("Hello, world!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
