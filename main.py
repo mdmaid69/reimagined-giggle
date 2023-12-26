a14cabfa2b43814e3ccbb153a00c7e37f8f378a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:37:52 +0000

    crash dry live talkinto school

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def sort_list(lst):
+        return sorted(lst)
