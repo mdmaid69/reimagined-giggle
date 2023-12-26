316ee96783164a7aaf5c8f6b6515b982553ed4f5
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:54:10 +0000

    ago takeaway plan must meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_int(array):
+        return int(array[0])
