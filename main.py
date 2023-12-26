e59b4f8b3f1f29579af1e2ca6422a40e07738d9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:51:35 +0000

    guess inform body distant unhappy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
