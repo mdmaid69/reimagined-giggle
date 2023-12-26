1e1f6efeb053b9b177e350ffc6a76a3cbdc58d41
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:40:14 +0000

    return determined admit unusual walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
