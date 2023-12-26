cd4e2273e6d9566401ce1c742e4b420343ed5c38
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:57 +0000

    still impression approve turnup boring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
