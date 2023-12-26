6a52a54eba42e5b576e44ec57d11cf60da0cdee9
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:05 +0000

    into final urgent comfortable runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def is_odd(n):
+        return n % 2 != 0
