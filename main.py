3be536361aae9c7669aacc424d0b322e1602b9d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:55 +0000

    decision speak summer high violence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
