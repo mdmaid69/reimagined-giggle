24fec3baab3674cd9791e1ffd08550393b49bd8e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:48 +0000

    suggest turnon big however floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_list_from_array(array):
+        return array.tolist()
