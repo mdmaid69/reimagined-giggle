c89ac1ad0a1c93e4eeacb7efcd4ff6e792ca816e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:32 +0000

    badly pity favour boat treat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_max(lst):
+        return max(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
