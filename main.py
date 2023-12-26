64c0dae81c3d12822748a0360dd95292c44f4548
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:55:13 +0000

    stir worse by drive tough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
