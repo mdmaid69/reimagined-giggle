63e13fd0d5360ef3582e6a634f08fc85619f32b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:10:41 +0000

    wear habit book medium scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
