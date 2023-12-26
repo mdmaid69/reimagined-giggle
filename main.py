4a2b02a991ef5c378dd1f84d676f472c57d9b659
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:24:43 +0000

    great bus none satisfied impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
