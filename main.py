8c36c3e335571f03154ad67183bbf8c6cec93885
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:32:24 +0000

    little instead apart please calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def reverse_list(lst):
-        return lst[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def append_to_array(array, item):
+        array.append(item)
