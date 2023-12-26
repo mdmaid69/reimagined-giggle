3265f86038e0c24b41c666b4054271f25753d8ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:45:05 +0000

    general far legal plant any

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def count_elements(lst):
+        return len(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
