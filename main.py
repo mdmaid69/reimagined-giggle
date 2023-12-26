a93e04c21ca7d2d274835c3292c404bf1ec1e911
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:06:34 +0000

    sign visit thinkthrough mystery mix

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
