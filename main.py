43376ecdd92f2f98c687464f9b30d228754a6393
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:00 +0000

    moon lesson unit laugh minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
