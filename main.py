c1951d67d7560daf92d98c849428965de5e9a308
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:55:39 +0000

    shrink discover add touch spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
