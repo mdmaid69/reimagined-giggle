780c768ef52e49d2122fd731d64b9b0ac2e77143
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:05:09 +0000

    someone tap situation slow Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
