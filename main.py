0fc655ec3efb45b4c310e9c2078bb2a681548f01
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:12:40 +0000

    love might stamp measure fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def add_numbers(x, y):
+        return x + y
