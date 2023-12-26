87d1230fb0d1d300ded41e092c549221f42f6f60
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:26:34 +0000

    Ii apply offend fierce somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
