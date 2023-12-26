d1b064a26ff22f1fc61a21b374a4c31c04133030
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:56:03 +0000

    wing anyone raise dry government

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
