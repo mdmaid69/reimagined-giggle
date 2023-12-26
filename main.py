5f61aaa082f017b380d36d7d71aba435e76c6334
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:21 +0000

    backwards joke tell Ww eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
