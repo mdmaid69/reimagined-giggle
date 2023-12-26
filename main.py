fab5a576b6d7f64df4906e6a4095331389f8ca8a
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:43:56 +0000

    pull branch post scene tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
