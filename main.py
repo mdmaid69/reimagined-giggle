900b1fc4d5e32a7ac9bcaaa0d81a1b304704c6b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:24:10 +0000

    herself freeze enthusiastic food keepout

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+for i in range(10): print(i)
