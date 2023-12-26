b310677919b2bc42d5295347695cb93347c6668e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:29:18 +0000

    battery person maximum afraid discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
