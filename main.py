2648c572c6b8b688e2a78f645c4a03ab3e88a012
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:45:47 +0000

    priority purpose injury chief imagine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
