dd9ca8c9809d158feb5a1f37c8a4f985e3ee546c
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:50:36 +0000

    matter else embarrassed increase world

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
