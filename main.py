f9696dacd073c19bf4f2cd1843928813cfc24bc0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:49:06 +0000

    blame term occasional majority steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
