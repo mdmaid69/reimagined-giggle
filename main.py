8e6ef7ee8ebbb15001e6f98ed97069d9707ce9fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:25:31 +0000

    interrupt collect ahead hour accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
