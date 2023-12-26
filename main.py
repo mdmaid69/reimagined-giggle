7a47110a3d98ed8865cd3d7927a0cc04891f787b
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:59:33 +0000

    hope them intelligent genuine hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
