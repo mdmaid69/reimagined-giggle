36843153a4b3932b593fd32d68bf9c648111929b
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:19 +0000

    tend factory show attach reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
