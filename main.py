17cd9c652d73c160aa3881effab07203904ad0bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:13:06 +0000

    bend manage knowledge summer react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
