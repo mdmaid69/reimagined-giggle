97b1dc85139501d6ee92d0459ae765b0d8769756
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:51 +0000

    hardly surface maybe takeover closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
