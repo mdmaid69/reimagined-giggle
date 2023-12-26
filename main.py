40749c2e535a0e3850cdc51e703b640b05ff1bef
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:55:12 +0000

    too tomorrow intend out free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
