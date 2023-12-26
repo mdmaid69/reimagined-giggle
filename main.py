85333acc01959fe1943320d115905268d76cf3d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:19:46 +0000

    tune fire else beat new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
