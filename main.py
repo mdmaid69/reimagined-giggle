4b333d7cc5ce43d1a3d1ed1f29e1eb09daf71891
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:43:50 +0000

    accident prepare journey push any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
