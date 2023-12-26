88f8b1f8cd86458ba3cfdfd7c5818e7b83dfd5c0
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:40:42 +0000

    pile way trust careful along

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def change_working_directory(path):
+        os.chdir(path)
