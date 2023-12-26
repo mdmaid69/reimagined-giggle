5575ee448041d4d303cc93b28dfe7837c998acc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:02:08 +0000

    check now almost do load

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
