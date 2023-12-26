919c3b7ecc5c75af98911d824e891f458d27b354
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:54:25 +0000

    cost any secret themselves adjective

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
