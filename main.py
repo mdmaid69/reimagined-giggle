4907b853b9e1c0abe6d40875335cfd838b95912c
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:53:56 +0000

    raw stamp schedule concern commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def count_elements(lst):
-        return len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def count_words(sentence):
+        return len(sentence.split())
