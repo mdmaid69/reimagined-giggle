1275347ca3b4dc6e27929dec3265db0e75940f4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:00:49 +0000

    company not will that far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
