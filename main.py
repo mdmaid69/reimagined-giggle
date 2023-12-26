6aab5e3fc683c0e2fe5f5e6d0f610926b6c88d80
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:59:05 +0000

    condition just accommodation when building

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
