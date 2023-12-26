a0d04453582624ee07503c163d96422eea4c3461
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:03:41 +0000

    temper frame man Kk piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
