475e10ed0b26fde9f74178313e4261192996ed61
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:32 +0000

    dress example star scared contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
