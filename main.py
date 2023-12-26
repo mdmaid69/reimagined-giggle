ed84ae935cd24f2eb89ce69d73ded986cf5625e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:47:04 +0000

    guess why bend pride star

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
