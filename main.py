c9211f35ce029b8ef82999450d7b99bea4f8b106
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:02:44 +0000

    ground fold frequent welcome sticky

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
