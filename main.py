d3756c96ca77fe4994b226f2dd73b26993939dc4
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:59 +0000

    embarrassing shut healthy pay surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
