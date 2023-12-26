fdffbd37216d0332d92905dd7d30a00f261161a2
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:55:18 +0000

    convinced male direct satisfactory stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
