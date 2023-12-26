2f9848af433fbb62de537a315a462bf5dc42c4fc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:02:06 +0000

    ahead until could radio wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
