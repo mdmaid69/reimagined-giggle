6e78ee40e5c0bb9bc69eec6654fe865f870aec1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:53:06 +0000

    mind 566end I group second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_max(numbers):
+        return max(numbers)
