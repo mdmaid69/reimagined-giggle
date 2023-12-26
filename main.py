3b621fb230889c21af33ddf7cb88a8df98e7a322
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:18:02 +0000

    round let offer something like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
