03e0d5babc1c185d4451aa1323fb7ff828229345
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:10:02 +0000

    control for Hh forgive ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
