2d05b9d1c52900c847a8fd745b90913fdb31b051
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:49:01 +0000

    admire remove defeat project workout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
