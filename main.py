8912522956d931c49fd819975df02bd2ca01345e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:46:11 +0000

    busy contain reaction improvement spite

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
