1a1e73f8ec1d1468b00a9aca2368a42ae8ae4cd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:29 +0000

    programme hot visit poor attract

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
