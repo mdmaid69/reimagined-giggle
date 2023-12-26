071931a42e97f7b0489c0744eba3d7128f1762f0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:01:54 +0000

    lie might study board nice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
