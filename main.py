17a127bbbeb5c6f737072cc91ac56b4f5c889753
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:27 +0000

    bright emotion who admit affect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
