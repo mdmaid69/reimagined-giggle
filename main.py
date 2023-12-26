35ce38db2ffecdff68d0bb59d7994bfaad911d8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:32:49 +0000

    turninto muscle bringback future position

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
