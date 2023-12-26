b9fc4e9c71b90425fac5bb85229d33b365458757
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:48:48 +0000

    workupto interfere admire eat charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
