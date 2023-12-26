49d17d7ad8444cbcfeb34a23f7d7535e60dfc6ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:27:52 +0000

    track here still naturally track

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
