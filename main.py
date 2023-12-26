7cb13993d95a6c54f541e3d430b6b18729af9def
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:27 +0000

    previous contain rarely please scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
