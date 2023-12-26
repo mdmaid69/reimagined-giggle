1dd8a0b766653e7c65bab4212b755bfe7ea9bcde
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:00:01 +0000

    afraid across aim exaggerate unite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
