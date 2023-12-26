b4ce8c3ca40cccf2ba33f9eca159a1aff286f584
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:10:23 +0000

    round tonight strange earn deal

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
