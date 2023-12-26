d398e48f0d5563f676740481a3073c2f69ec3887
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:11 +0000

    dare flat slow slide to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import sys
-def print_python_version():
-        print(sys.version)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
