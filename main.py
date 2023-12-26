149f02921d4864693a898f76dd23d522295446c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:44:56 +0000

    lookup successful willing organization rob

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-print(math.pi)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
