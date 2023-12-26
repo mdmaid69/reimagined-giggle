38b670da28c95c3c9bb88f907f592933c693abde
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:10 +0000

    division branch dream cough physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
