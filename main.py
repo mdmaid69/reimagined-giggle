ef4b6371c6e685bc4a0109a3f0a68ba19d6cc458
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:01:06 +0000

    callat sorry sort matter suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
