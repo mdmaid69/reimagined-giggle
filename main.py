f48ffb447bcac8d0e38a2869b7f49e062abd371d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:15 +0000

    uncomfortable along around to score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
