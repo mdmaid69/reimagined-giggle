5f52e59104c36c050c86de663b161f05218d4ee0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:42:29 +0000

    word smell until violence like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
