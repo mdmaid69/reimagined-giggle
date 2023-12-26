06ffceee84e9ef97afb3c85eb629e235653c3a71
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:53:12 +0000

    dark admit church consist during

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def remove_duplicates(lst):
-        return list(set(lst))
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
