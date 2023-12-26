dab8bee14aa82dd05d4c58aaf26ba569d13369d4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:01:45 +0000

    onto replace now wonder prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_power(work, time):
+        return work / time
