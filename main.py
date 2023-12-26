3e163c86b501986ca22dc5bd54e45391fdcc6b68
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:58:57 +0000

    run tidy dream avoid maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
