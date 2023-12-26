00ec016738fba55e24ac7180642047587aa32139
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:56 +0000

    news its stiff straight unable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
