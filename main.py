8c0b792b5b90dc7d868180a848a84b163bc5a6a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:33:56 +0000

    strike drop moral copy honest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
