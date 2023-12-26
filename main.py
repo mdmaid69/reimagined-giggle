cb662720c9d26cbd25b85cbd1bd6efd0eaad72b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:07:37 +0000

    appearance somehow scheme direct cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def convert_to_hex(n):
+        return hex(n)
