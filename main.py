2f70ebd248bb94be20cc6c62ee160f77f1f79f9d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:07 +0000

    begin statement regret correction comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
