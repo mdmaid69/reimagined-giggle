cd16db754c6789d69cf79d12fc20cc228b858cf2
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:22 +0000

    glass comein sudden movement match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_ceiling(x):
+        return math.ceil(x)
