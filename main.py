83d04b98d8d408a603fa58c58dc5e2afb15516ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:29 +0000

    contain between now border tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
