75f582839747e118b8555e74de9c3f230076a64f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:43 +0000

    almost plan disturb comfortable best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
