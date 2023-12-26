3e60df5a0c348c9f974171618fb044e2d3eab636
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:33 +0000

    add help wing scream 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
