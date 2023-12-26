9220c48fa477396fa288b37c6c2f78926a93f200
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:29:51 +0000

    breath mass regard difficulty convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
