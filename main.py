9082925e3bfa06b43f61be523af95be98e4bd33c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:15:18 +0000

    improve cheque lucky bridge improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
