a6d713115570ff95c84e0e644387d25532d2a306
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:50:28 +0000

    translate every breakin trousers account

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_list(array):
-        return list(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def cube_number(x):
+        return x**3
