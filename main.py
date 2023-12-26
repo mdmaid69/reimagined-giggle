dbc583306d39807397cb1b5c0a14d9389c15f5a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:11:53 +0000

    anybody put hit about air

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
