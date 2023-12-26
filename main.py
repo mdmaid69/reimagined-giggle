279554dd3bf7e59ab348936b35abec00a1ac4f19
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:47:51 +0000

    behaviour slice thanks delight demand

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
