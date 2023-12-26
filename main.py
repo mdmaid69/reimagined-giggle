05ebd94b3fde2eece084a7d814ed21e31e3e5bca
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:49:39 +0000

    waste pay natural friend run

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_length(array):
-        return len(array)
+def find_min(lst):
+        return min(lst)
+for i in range(10): print(i)
