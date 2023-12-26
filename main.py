db050e2590e7424d59314713c319b8b5e52ceddb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:51:40 +0000

    come fall order division stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
