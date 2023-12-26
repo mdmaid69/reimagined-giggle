e23c4b891b43634bb973fe8a35c74ffcf0ead2a6
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:25:23 +0000

    tell typical shock gap broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
