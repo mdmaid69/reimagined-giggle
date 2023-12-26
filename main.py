984ed6a692acb88b19774902a5278db84d980455
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:01:06 +0000

    collection through straight total passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
