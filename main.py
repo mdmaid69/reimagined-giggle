86a8519d8a40f06a5009c149f020856c0f8e1735
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:59:31 +0000

    boat accidentally description Jj mix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_work(force, distance):
-        return force * distance
+def calculate_pressure(force, area):
+        return force / area
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
