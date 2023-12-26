a9ea2ad010b08c1d20a9a92429ad8d99eebe4edf
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:12:11 +0000

    consist confusing downstairs gas fire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+for i in range(10): print(i)
