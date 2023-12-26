ba1adc1a9d099eacd82a9b2533ec48278b20a2dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:37 +0000

    drop before opposite office design

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
