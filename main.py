b0dfbe7b915c82164cc820cb2e790adcee4a563a
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:06:38 +0000

    corner warn promise fortunate joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
