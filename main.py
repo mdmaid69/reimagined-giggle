9c654dc10110ec861338e297640dd6d949d75e90
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:06:11 +0000

    loose everybody treat past translate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
