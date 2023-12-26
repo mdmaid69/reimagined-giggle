5e8a510e3a2a64018fb86d4233b2876e47c252dc
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:46:19 +0000

    prepare agree flavour mean label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
