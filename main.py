375a22924dd6d4c18e96ed35627d7ad1bfc5736f
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:33:09 +0000

    idea content unit just tape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def remove_duplicates(lst):
+        return list(set(lst))
