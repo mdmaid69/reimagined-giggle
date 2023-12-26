8015e9ace30728401fd99dd6525f9e1b5fff6fe1
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:33:19 +0000

    tall unless alone look spring

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
