e7154b53e393ae4cd5ab84472ab357780bab9d6d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:37:14 +0000

    ability immediate sure hard have

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
