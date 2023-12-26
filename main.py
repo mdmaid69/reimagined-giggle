eb1ebb51200429a843004aa8ad8cf8788a84c889
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:46:54 +0000

    better tomorrow great search greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
