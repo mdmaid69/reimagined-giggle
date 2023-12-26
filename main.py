fd5584eaff638cc0b9d1b7b74493d3dbe98949b1
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:56:00 +0000

    patient eat tense spoil rid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
