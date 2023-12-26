b342ce1447eb149d646d6b33d64d8c4c0ff9c785
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:37:33 +0000

    mail smile welcome depend exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
