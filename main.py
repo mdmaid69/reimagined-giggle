d98b6a00d4b945a267e820f7c8bc1c169007ee72
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:08 +0000

    pay affect play whether arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
