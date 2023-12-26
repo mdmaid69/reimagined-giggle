3793946592bcec69e96fdf029863bfd55261e2ec
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:41:57 +0000

    difficulty important fire unable say

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
