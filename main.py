56c8b3fffb8fca5ff7ad5d596147c0187ccb84d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:12:58 +0000

    vote closed general back cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
