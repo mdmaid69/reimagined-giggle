a17902c70fb019dbeaeb1439cedfec8ad90e2f9c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:56:57 +0000

    steam middle that rush report

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import random
+print(random.randint(0, 100))
+def convert_to_octal(n):
+        return oct(n)
