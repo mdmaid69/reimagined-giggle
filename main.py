37d7f0b3f85ac74cb9d3d2725a3a8dc9ffe60f99
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:14 +0000

    straight better well upset pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_pressure(force, area):
+        return force / area
