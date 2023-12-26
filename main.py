de97e1099d0cdb939fb9031205252f78dca460ef
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:00:03 +0000

    object ignore raw supply comefrom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def remove_directory(path):
+        os.rmdir(path)
+print(sum(range(10)))
