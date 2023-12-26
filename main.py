d7b4d4eb268c8704d3cc479c7080379d74e12088
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:08:07 +0000

    fire accordingto chief listenfor strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
