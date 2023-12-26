dc37c9c028f65338f5c35fa395302f6d7c2e960c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:20:23 +0000

    anywhere table feature plant content

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(10): print(i)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_sine(x):
+        return math.sin(x)
