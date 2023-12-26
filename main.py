58ad98f515408d69eff1b70e149d686e71548a30
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:50 +0000

    beneath rain centre such temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
