16b37a7f9eb66c1860a0f2d9d6885068daa8c5a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:43:30 +0000

    reason public free serve himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
