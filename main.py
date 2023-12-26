1c07538c072986de68943c0c4df4821880a5edcc
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:53:24 +0000

    tap comment embarrassing repeat long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_palindrome(s):
+        return s == s[::-1]
