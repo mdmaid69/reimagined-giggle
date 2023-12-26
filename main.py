64cafb9f13a73b696cda0eb240840063418527fe
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:52 +0000

    to road post store workon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
