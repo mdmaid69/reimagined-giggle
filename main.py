58997cef94f2016e9581441010197dc812cb1acd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:28:57 +0000

    Qq talkback lend birthday general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
