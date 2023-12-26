82001510eb40a58fcc054065cfba2b20953a3999
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:31:23 +0000

    please breathe contrast explain treat

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-  def convert_to_hex(n):
-        return hex(n)
-print([x**2 for x in range(10)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
