39e18a198ef5c2daf097326030d5d9935995cb3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:22 +0000

    forever strike find surprise Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
