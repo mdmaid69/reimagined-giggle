34e400bffccd213402acff2eb8ff95e2bcc4c798
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:55:09 +0000

    passage peaceful thinkof too building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
