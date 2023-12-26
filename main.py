92762194b2ac997919eb23721cfc1acf1e9405de
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:53:40 +0000

    getback go straight main safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
