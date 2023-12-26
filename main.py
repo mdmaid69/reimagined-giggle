8ccd5cbe7ddef278fcd4f66d605f1df3843cf257
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:10 +0000

    secret pretend contrast secret responsibility

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
