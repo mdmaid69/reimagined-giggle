8d43c5e740a4c4c8992ee6c4ebca637752651ebe
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:35 +0000

    turndown polite sister fail battery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
