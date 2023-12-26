c942e1ac630004ae07f8d3eab5310aca0e5502ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:17:17 +0000

    trust gather soft help dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
