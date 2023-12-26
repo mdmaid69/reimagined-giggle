31eeb9488ffd3b3223bc9e6f4e6d0a1dd06b83a5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:16:47 +0000

    birth cheque hurt Yy age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
