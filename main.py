ff43afe515086c984930c4ed2c7d17f2b3d9ab6f
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:16:24 +0000

    spray physical mind small she

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
