3540e9b5fbc9e597f2c1dc1596718f678619d173
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:36:36 +0000

    persuade might touch sort month

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
