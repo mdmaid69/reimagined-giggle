2c47e762db4ba973b6335aed3a737fbce800fdd7
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:00:59 +0000

    land Jj throughout convince single

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def divide_numbers(x, y):
+        return x / y
