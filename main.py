5405cbdc85ccd908b583ee39e61c807518f5bca3
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:45:46 +0000

    everyone fence peaceful discover marry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
