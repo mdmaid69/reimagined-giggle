c34c3b9630dcb5a4662e994d75c93480e4a6fb9a
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:09:42 +0000

    above neither float mix mystery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def square_number(x):
+        return x**2
