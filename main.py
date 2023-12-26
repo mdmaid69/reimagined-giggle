1b975c3110e3f95c9cf36017c04677b19c672926
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:50:03 +0000

    cold borrow clear lower greeting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+print(os.getcwd())
