4211cc9483f2f4402471b9de4d8afb2cf0ee2718
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:34 +0000

    stand godown mine translate kill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
