f9f2ca34a9916c799b7b796b14e5a32fd6a82f8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:56:18 +0000

    model that in runaway sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def add_numbers(a, b):
-        return a + b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
