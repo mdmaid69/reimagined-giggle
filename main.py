cfeef103a6122372fdac08f985a766a86c27f22f
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:29:59 +0000

    hear before offend proof steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
