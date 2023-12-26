830a0b016ac4fc803c5cfc25740469132ccab0a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:48:50 +0000

    manage pollution it insist human

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def add_numbers(x, y):
+        return x + y
