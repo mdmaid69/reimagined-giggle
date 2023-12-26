cfe59cac0ad9dc69ba445b4b7cf466f634f70122
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:56:43 +0000

    explain imagination display exist welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
