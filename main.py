ea733ea5e07529799768ccc2913750792e019d5a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:15:22 +0000

    send across base public well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
