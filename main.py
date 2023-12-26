0e32bc650ec15b427595b6b873a42f0d36a4e1ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:43:34 +0000

    few keepaway snow rain move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+print("Hello, world!")
