c00ed83b8368b02a111f53f33dd404ddfd904d72
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:42:18 +0000

    connected war forget regularly would

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-print(math.pi)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
