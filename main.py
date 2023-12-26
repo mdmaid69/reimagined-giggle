1a2f894285482da2848c1e1d652a07aa70d6cd8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:37:57 +0000

    just hurry boat suit attempt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
