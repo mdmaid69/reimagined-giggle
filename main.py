e8e977c946afb0b98b857614c1c0ba67104aec8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:54 +0000

    speed thing stone various make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
