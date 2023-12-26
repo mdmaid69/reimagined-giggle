1e87ec7fea8ecbe69d6e6dce36ff8d09e5d68dae
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:16:28 +0000

    right glass bright cum strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
