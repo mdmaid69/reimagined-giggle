6e410d49cd4be94a01f546112c07f89fc9508e75
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:00:29 +0000

    me absolutely nice or classroom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
