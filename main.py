71479f89ea06c8f29c7adf5314c378890622939f
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:34:32 +0000

    even have protest my roll

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
