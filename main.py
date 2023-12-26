c1b8e4f5dfb54e2d2b5772e6f01f77f9955e30b9
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:06:22 +0000

    less fetch Nn lot could

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
