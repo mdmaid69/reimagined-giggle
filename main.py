bfddc5a1996bc5d99f317d789d66beaab60f82e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:35:33 +0000

    unpleasant what 564encourage holdout distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+name = "Python"
+print("Hello,", name)
