e34cf13ab662858a8987c4fb0d26f01eaea3e9cc
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:03:45 +0000

    mind pocket stay aim upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
