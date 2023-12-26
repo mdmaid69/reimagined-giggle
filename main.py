c806a35611e4e1bcc04d013a737fd603f586badb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:32:23 +0000

    exciting boat account secret centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
