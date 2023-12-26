9e481aa7a34cd273fba994f529e6276de8457fdb
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:30:28 +0000

    related visit crazy drop Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
