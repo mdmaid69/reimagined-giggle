cf62331abd8df97eda6b0d84cf1ea173f3c92da7
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:47:16 +0000

    fast rid fix object production

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
