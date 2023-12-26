741ae4abd1276fc2e31629943426cc6377200b5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:26:40 +0000

    neck illness study secret aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
