9c23426cb30b3a810a573d5f4709bad75d0b8b20
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:12 +0000

    project reach want share comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_counter():
+        return collections.Counter()
