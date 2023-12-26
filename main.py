e3f17906579af7e843423512ac9aa503727c0501
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:39:44 +0000

    without sweet he autumn free

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
