7f199aa0c7c319cd83115f236c5aa8f00b90f4d0
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:50:28 +0000

    side objection stay square degree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
