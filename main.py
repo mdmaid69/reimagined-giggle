c18b12bebb1c147ba5b5d2074224d6444d9aa94d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:09:06 +0000

    before goon balance welcome identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
