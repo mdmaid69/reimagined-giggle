7782a546b28e7bfe84865ec2051e018ea36fea17
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:54:53 +0000

    cross fight concern 564encourage favour

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import tensorflow as tf
+print(tf.__version__)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
