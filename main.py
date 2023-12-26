14ec21566febee2e7f510d7d5a15918550ca5031
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:29 +0000

    likely twist suckme lead Jj

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
