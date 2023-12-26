2586b3a830d1da5a052ae3f83ab55b6bd2950ed6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:29:08 +0000

    please everyone name contents not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
