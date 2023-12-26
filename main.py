b9d470f2c80e01924184a5611e336f2646db9ae5
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:45 +0000

    clever children plenty glass disapprove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_sine(x):
+        return math.sin(x)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
