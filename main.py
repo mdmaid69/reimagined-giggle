95880efc953164a0df7f7feeb986c83d825af0fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:57 +0000

    complaint standard marry warm count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
