90f734133573b0afa4c5abe4a6d8b89449c5f6a7
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:36:20 +0000

    half post wide add charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
