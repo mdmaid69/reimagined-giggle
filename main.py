78eb7218d1dfc7f277f508f127037d5b6b7a23cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:21:09 +0000

    satisfied shut modern open say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
