3efe8ae23a585c1edba29cfc991e2710c14d9272
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:58:42 +0000

    keep surface only study moral

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
