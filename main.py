e4c6e5c8b7d13375eaf9a10900b4bde5a997b5fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:37 +0000

    main middle move holiday name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import numpy as np
+print(np.array([1, 2, 3]))
