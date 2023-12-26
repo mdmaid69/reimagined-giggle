464b53e4d575bbf2a8f823eac7afb9f2566288de
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:54:09 +0000

    uncertain suck tense pronunciation lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
