0b94649783558362b85cac93b64c9aadfe1d52fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:12:17 +0000

    independent chief offence getoff turnout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
