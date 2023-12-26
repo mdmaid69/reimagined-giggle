4db509281d002615ffbde30ffce66fc0667ef6a0
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:04:55 +0000

    mouth lead car company priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
