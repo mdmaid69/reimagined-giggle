3f39ec984b1d38d0ea1f70105a4a67430675d60c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:25:24 +0000

    mixture operate worth that new

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
