23a0bb37b1faaa9eaa22dea182b4ff0f4e6ebc05
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:30 +0000

    comfort line male behind through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
