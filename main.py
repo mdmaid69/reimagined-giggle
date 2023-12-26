69f3fbe19ea5f78fd9e9ab1e9bd4e51401895f59
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:42:42 +0000

    Ww pain become rather walk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def reverse_string(s):
+        return s[::-1]
