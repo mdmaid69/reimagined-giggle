2d018943a3c2edd7b1c9f05730e061db2790177a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:25:29 +0000

    block suppose still shallow pretend

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
