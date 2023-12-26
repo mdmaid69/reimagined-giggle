5fcb5b6e3170e87e4316bacf60570528dad20b32
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:40:35 +0000

    healthy area run broken into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+  def is_odd(n):
+        return n % 2 != 0
