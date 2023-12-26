92874cb0becdf0174dca3ecfb30bf9b711ee0513
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:25:24 +0000

    final across walk couple car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
