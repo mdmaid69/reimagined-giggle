68658959fdcc908411b3ec436873fb1226998d37
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:19:39 +0000

    professional contain by regular thin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_sine(x):
+        return math.sin(x)
