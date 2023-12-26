c0cb732eb6608b8af75a508677da61df307018df
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:13 +0000

    nigga real raw motion return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
