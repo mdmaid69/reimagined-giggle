acdcbb43d1ec334d4e2267bc28f2d41116ddbbda
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:07 +0000

    drawing carry leg important since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
