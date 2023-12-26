1950fc46a5dfc630a81a418736eb87c0ccb19acb
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:15:43 +0000

    force extraordinary impatient might warn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
