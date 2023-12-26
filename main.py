2864bc5a92ee29590d27e73e88e2bbc507a32741
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:11:54 +0000

    rub Bb obtain square laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
