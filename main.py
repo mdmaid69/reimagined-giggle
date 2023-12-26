2a3394ceb0a1c39aaf8ec77c4c88becdb5e32371
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:40:28 +0000

    frighten slightly welcome walk winter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
