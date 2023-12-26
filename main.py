ae6e2f54de73edd4bd21ea94a4cf2b0bfba3a2a3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:12 +0000

    its time point front breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
