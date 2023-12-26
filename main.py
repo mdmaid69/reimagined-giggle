99c2587955a7dd27cac12d8befec8e1cea14fc59
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:37:58 +0000

    conclusion brain raise actually balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def add_numbers(a, b):
-        return a + b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
