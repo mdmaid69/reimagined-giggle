fbd1f3648dd1b76064bf84df0e2b49e05ce952f7
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:25 +0000

    awake push cry holiday anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+print(time.time())
