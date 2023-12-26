d542c700297accd71b6ba1aaa6bd09a2a5ee55ab
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:37 +0000

    Ss law month absolutely later

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+import re
+print(re.match("h.*o", "hello world"))
