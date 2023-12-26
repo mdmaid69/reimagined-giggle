f86ce356e74f266edd641c412a6b64eee928fb06
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:12:37 +0000

    take perfect answer play whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
