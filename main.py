e2ddbe41e1296f5e31fafdb9ad0ed11950915250
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:41:36 +0000

    evidence above yourself achievement emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
