6f7983fae96dfcbb2ef50a718a204accd6cf4885
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:13:36 +0000

    Jj I bringdown temperature break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
