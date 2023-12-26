48454b5b3bb726af13b47dd3940c4d87fc9b58ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:23:29 +0000

    tooth female trouble post file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
