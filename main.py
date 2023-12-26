8be496b0c124cd051102422c6fd8814eb2c6e34e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:09:40 +0000

    emotion ring half knowledge eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-print(random.randint(0, 100))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
