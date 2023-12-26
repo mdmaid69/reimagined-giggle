3bbd93a9816230b884cc022ad18e6e3ab85b0c40
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:30:02 +0000

    storm happen case shade alive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
