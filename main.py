1701924379f75408d52e5bbc3ff65d334fc45934
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:33 +0000

    cinema talkinto convincing that do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
