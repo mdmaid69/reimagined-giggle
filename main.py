fbd02d3efd1ed269b3ade78ae3db14276fe85bf4
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:02:51 +0000

    narrow window feed inquire joke

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def is_odd(n):
-        return n % 2 != 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
