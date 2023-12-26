6a2e9567efe320b086d7d28adea5245de06575c2
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:02:20 +0000

    frighten become shame breath method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
