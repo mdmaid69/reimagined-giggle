963cf733aa0f08fc03155a99a63cb95c1f5e4609
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:19:07 +0000

    mess law hole both morning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
