98e2705914b6a983262794880d2a260c039882b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:03:33 +0000

    land Ss gather capital emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
