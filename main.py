4be9da2094f751679b5f5bc1882431b0a3261584
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:31 +0000

    sky underneath juice too handle

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def cube_number(x):
+        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
