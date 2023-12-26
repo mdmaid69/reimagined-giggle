a61386a608eec1d15bbbe5f70e75fd6dccf13677
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:11:24 +0000

    valuable trap represent her manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
