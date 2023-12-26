d36ac93a89f34c22075e2981d253b0bb37da8055
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:18 +0000

    coffee gooff public artificial afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+print(sys.version)
