ef5abbef1f7c9546f9a8b4855c1a37565942df2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:09:59 +0000

    through borrow land shop major

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
