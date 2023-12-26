eaf4c9bdfe8730c01852543172114f3dff62084a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:19:59 +0000

    appearance admire coat runout save

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_density(mass, volume):
+        return mass / volume
