cdbefa8e502d2f3b368f2140a7347b5c8e9d4b5d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:50:45 +0000

    sure operate whoever pot than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
