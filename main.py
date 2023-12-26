5181fc5b020abe2821fc66a25470335a4fb75d19
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:48:29 +0000

    limit aim later dress city

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
