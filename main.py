95a29bc4be2cdee71c0ccc338b9a6b58d161fc1a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:08:07 +0000

    sound even relation middle while

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_min(numbers):
+        return min(numbers)
