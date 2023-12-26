2d9b02fa48df111490791427bf7be6ecc46ff505
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:03:31 +0000

    course getout disappear press talkinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+x = 10
+y = 20
+print("Sum:", x + y)
