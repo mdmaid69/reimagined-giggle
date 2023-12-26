9252138dde9b84ca5555e5ff39468d796c6de2ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:48:16 +0000

    lock environment cap sweep circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
