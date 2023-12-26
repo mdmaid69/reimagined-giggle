01e30a9664c6ea2cd7464a2b755d5cf8627aaf71
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:58:09 +0000

    stare mix general event original

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
