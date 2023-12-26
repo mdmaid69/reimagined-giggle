17013df4a8c96a0c6edf66fb4fed231a02a54a0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:26:06 +0000

    extraordinary the future lift mainly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
+for i in range(10): print(i)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
