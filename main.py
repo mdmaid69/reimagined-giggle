ff711d4c14f2a836a5c1e5c949d1b5e26b06fa28
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:05:52 +0000

    bottom print nurse treat generally

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
