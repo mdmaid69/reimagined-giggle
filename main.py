df0b5c55b7343730a7326e9a8d6537f2e9cb2199
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:14:07 +0000

    570enjoyable generous she moment run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
