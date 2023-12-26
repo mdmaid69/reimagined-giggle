93bc8e18839f7ca4c3533b5f4b868b38fc2e14a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:35:17 +0000

    depend paper post window talkinto

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+name = "Python"
+print("Hello,", name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
