49dc31c99018932c4ce706ac76aa0bdb68296967
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:22 +0000

    know surprise weekend possibly separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
