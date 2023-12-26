03c0830c769e10587eea576a62e045d41a3f066a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:45:16 +0000

    Ll extra serious snow dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
