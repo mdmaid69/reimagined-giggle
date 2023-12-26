4df34a7e6b6d6d78b16d6a1707a7700b05084e0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:20:32 +0000

    equal gas forgive attractive discovery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_current_directory():
+        return os.getcwd()
+import time
+print(time.time())
