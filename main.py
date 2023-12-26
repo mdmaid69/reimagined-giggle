31ff182f8c072f892940821018cbe1c0ec94104c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:50:33 +0000

    on marry discover getup visit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
