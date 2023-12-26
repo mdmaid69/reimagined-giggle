0b50fcc3e94dd9f1ef5b6ebf025243c0066312f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:27:17 +0000

    thanks family disapprove estimate business

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
