e56bf865b9f8959ae9159442b489604c07288e06
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:39:02 +0000

    pen reply tired appeal will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
