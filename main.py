a10750ec8290100b8edc21578c3fed9685cbe545
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:44:47 +0000

    drawing smoke label past exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
