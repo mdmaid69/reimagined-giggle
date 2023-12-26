1a3d9ae98d39006451a9a16f403fd6315da9ea08
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:49:42 +0000

    too store Pp wonder in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
