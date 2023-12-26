76c219b7aeaf9350faf61d9d883c750c2f56ffbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:02:45 +0000

    correction great room piece behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
