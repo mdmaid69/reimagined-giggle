1b129bdcad1236242cac3d679bc94ba175bfee13
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:11:00 +0000

    expect find appear rank grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Uppercase:", text.upper())
