27008336b636043e29c0dade3caf5c5498c4efa5
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:05:36 +0000

    whistle problem true scale breath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_max(numbers):
-        return max(numbers)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+print("Hello, world!")
