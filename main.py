f9912330f042a835014b706e6fa2db6a1ae3e243
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:57:01 +0000

    port connection little exact test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
