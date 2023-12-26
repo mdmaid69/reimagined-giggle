b4abccf6466cedb9b172b952c552e2d117983499
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:58 +0000

    opposite mark talkinto rain present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
