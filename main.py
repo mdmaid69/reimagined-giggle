6d5b0ff8f29bc6dc4b906fc77ff97416c50303e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:52:05 +0000

    clear possibly punish quickly less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
