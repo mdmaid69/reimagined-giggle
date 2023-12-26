4ea2865fd928d300711471376de13f84db82c5bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:57:10 +0000

    club dinner cry ought share

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def square_number(x):
+        return x**2
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
