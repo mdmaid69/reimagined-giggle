f247311b0fc37144bd97cbc2df18ddceacd50ebb
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:17 +0000

    as short always comeoff main

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
