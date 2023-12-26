7de38646eb5c46d8d1771eb12f41699f857cd748
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:35 +0000

    worth post process hers squeeze

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import random
+print(random.randint(0, 100))
