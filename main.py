be8e14d97656cf636b70b7eec30cbddb8c01e6d2
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:47:24 +0000

    refer run wild Nn bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
