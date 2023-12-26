2930dec5c2ee0ba86ac8b6ca9861cee9030a57fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:03 +0000

    interested against idea tooth Yy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
