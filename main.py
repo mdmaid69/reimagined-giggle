1a439c39588037d79d7dd8f590a1a7b2639dd553
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:45 +0000

    finally harm read fierce present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
