011e281644abcb0e24654ccea150dc63b5aeef74
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:21:57 +0000

    exercise offer what top bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
