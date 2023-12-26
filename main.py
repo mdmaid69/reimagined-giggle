f5f441f2100ceebb28d39220fe3cc3fc3a6edb6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:49:42 +0000

    rub scene bang odd at

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def convert_to_octal(n):
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
         return oct(n)
