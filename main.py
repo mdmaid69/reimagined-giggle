8258070f2e094fc9756bc65a697ebb301cf0e1ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:14:17 +0000

    town why ability pencil Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
