0c796120b921d65ae7d5e3f482719fa3bc4a057d
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:35 +0000

    stamp never smell force steal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_work(force, distance):
-        return force * distance
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
