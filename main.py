c78d57799ac5937ca440376f91f87d0788a81619
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:04:08 +0000

    calm seat protest recover comeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def convert_to_octal(n):
-        return oct(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
