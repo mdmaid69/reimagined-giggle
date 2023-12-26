61ccf7287a42dc076b2b5e2c97d2b2f850894427
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:07:53 +0000

    meaning situation her lunch drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
