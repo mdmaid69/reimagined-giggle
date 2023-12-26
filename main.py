cbbd14f89b173f234e634b4a9fac5848914777a8
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:34:46 +0000

    how wild book ordinary him

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
