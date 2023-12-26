a1af32dd76b01ea6c80ea703fa5abf6f9d76f0f1
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:15:40 +0000

    breakthrough comein favourite angry opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
