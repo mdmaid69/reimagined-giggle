6e742496b683c645fe3d8c72d0784e5caa0f1d72
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:48:12 +0000

    male laugh temperature scrap good

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
+for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
