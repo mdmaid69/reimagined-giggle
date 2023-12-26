b5db2a90d0f6a21e71e0839baee787b3b64650bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:35:28 +0000

    earth life watch goon finger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def is_even(n):
+        return n % 2 == 0
