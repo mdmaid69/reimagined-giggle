7b2e15573178a1a4569d2f5349d48a743753e59b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:09:03 +0000

    foot run advise star trade

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
