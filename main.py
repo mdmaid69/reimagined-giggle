fb440e07d84567fcb2960cf6062565a203154a0a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:54 +0000

    another accident target difference issue

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
