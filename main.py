bde84f1b2f69c375f8b92c849cd5aacb90030cb4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:02:11 +0000

    spoil class wide arrange square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
