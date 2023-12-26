9d6191c389ec87a0edb729659376f2074ce26969
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:52:14 +0000

    improve 573entertainment image hand rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
