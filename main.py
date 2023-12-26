31c1cac6644734ca9927c716bfb5cc370f40956f
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:16:04 +0000

    below lie circle food table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
