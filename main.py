2dad58c999b23013e3aaf6f9448d5d8c518167e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:31:31 +0000

    whisper heat his stick class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
