659d2fdf3d725dd19aca687361fba22dc78a988a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:05:43 +0000

    cruel bottom matter wish embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import time
-def get_time_since_epoch():
-        return time.time()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
