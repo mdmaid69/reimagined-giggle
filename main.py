b82a04dec0f6619b7ef9a737c85860759eeca866
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:36:41 +0000

    surprised its scare lookafter hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
