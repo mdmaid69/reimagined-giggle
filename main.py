cbf7f3445826bb83126ae027d1557df7408f0cb2
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:07:08 +0000

    combine cool runinto illness opinion

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
