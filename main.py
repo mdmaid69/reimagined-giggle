e1736ba904ebb2bf4a113d96b92ad1889c6db1a9
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:34:48 +0000

    control experiment immediately uponon mean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+print("Hello, world!")
