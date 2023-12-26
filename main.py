7603adf689b0b8bb58f24821b43d47e9f65d857a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:08:28 +0000

    tax circumstance part off who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
