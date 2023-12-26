4365a93899e2d7f04a9ce67dd316df193d4c5519
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:39:41 +0000

    future reply actually machine glad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def is_even(n):
+        return n % 2 == 0
