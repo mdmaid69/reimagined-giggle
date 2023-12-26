e5e5729725a293431a552c6fb6a7c4d7082b6849
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:45:12 +0000

    spare claim strange why shadow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
