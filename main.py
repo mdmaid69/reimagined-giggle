a9c15a40dc4cf3624fe7b742495622ad0f7a90e8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:00:09 +0000

    creature upstairs her pretty soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
