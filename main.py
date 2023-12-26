f6135b6f8277a374eb3563629393ceac3de75864
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:15 +0000

    concerned centre way new that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def get_array_typecode(array):
+        return array.typecode
