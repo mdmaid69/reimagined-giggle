17852860edd6d6911bffa715a07084452ee77a38
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:15:56 +0000

    sleep approval keepaway card thought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
