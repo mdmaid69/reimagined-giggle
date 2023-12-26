e79bbcad3fd9f79a0b6004a0253b6fc91b9ce42b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:45:51 +0000

    turnoff war consist music crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
