8b8c8a8247e4dd511f5f48ee7553cb92ac880721
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:57:05 +0000

    agree allow oil leave friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import logging
+def log_message(message):
+        logging.info(message)
