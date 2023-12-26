6ae89694df456044ba87db3b1c36e05ff339abe0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:04:06 +0000

    responsibility wing turnback listen shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def clear_array(array):
-        array *= 0
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_floor(x):
+        return math.floor(x)
