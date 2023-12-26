e4b9e11e2c7416541c3e35328a91cec0206d3a49
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:16 +0000

    real figure whenever runout different

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
