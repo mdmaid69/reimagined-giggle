b1d0a95f29cde7893f9b18e8418f6fe16aa82383
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:59:47 +0000

    worst regional interested error back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
