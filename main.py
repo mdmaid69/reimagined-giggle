5c43ec4c4d1e43ac1bd895a80a390d9513f79518
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:18:52 +0000

    bring steep prepare advanced relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
