3939ca244ea31d455f060e877aea5079e4e8b6f6
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:22:55 +0000

    house hole refer friend Zz

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
