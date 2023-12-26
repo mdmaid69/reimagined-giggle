d5bab9e4e564a901508473e0a8e2ddcc268396ce
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:59:38 +0000

    hurry record attractive music moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_even(n):
+        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
