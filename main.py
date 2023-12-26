866b115f31670071dde95e07151991ced9a64759
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:44:14 +0000

    available meeting although really mention

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
