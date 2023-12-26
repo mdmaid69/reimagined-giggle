3321b18b5aab5722ead44065e04ca1753f81bcc3
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:39:26 +0000

    drawing him shake fly willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
