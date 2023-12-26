bac30abcca9c8385bdd14195ee3a029850072c16
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:35:43 +0000

    breathe before anybody intend bad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
