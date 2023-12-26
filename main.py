125ed178eb1d2af0bfb10ce66fc4cdc1f8c59b2e
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:04:29 +0000

    welcome cover have damp line

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-def remove_duplicates(lst):
-        return list(set(lst))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
