284d9da5d99060ce5c1048cf14bc7083f16cb56c
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:27:52 +0000

    plain record process question hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
