99a34121aa42bb6d91b4efce55f316cd36653d65
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:04:37 +0000

    kind unable talkoutof deaf fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def convert_to_octal(n):
+        return oct(n)
