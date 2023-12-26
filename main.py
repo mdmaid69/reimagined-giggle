0c8149b174f2e0a88b01836fdf40d59dd4db658a
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:43:32 +0000

    instruction deal throughout fold before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_power(work, time):
-        return work / time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
