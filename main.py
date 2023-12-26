a64fe45806857e0353eed9656611cb5b7cbeed89
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:19:45 +0000

    pen operation bottom become delicate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
