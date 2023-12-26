56351fe581ab095b469eb7d38a207ca31c925fdd
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:23:34 +0000

    birthday practical run holdup least

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_set(array):
+        return set(array)
