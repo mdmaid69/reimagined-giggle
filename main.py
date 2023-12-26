9b8f02bcde08b479b770ebbd8bc5f2a5f1ff0b2b
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:49:18 +0000

    share free study thinkover have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
