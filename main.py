7389242986a920ddeeac9a5aa533e25c296163bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:58 +0000

    sail couple choose no official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
