6de2bf06e6a30b7dc55736ffeededa2e6fc79dce
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:28:34 +0000

    clear passenger bang remark long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+print(os.getcwd())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
