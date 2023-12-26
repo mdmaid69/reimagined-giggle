71b1e0cd84322732ff3dcc350c0a0a7dab24cf64
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:24:29 +0000

    lookafter sensitive reply cruelty reason

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
