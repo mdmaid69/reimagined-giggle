c15f561bf8982a1b9d3937a3ac8b4bfbd3340aa6
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:23:07 +0000

    put pick sweep principle stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
