c9cb36f6cacad76675050d55fb328e169a330b91
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:42:20 +0000

    noise spring basic once forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
