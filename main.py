50ec5e2a33fafa099139ab99dee9edb650ef5342
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:27:39 +0000

    better quantity change responsible roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def append_to_array(array, item):
+        array.append(item)
