0837d8b9546cf0823baebb24625d53b4772b06f3
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:23:56 +0000

    couple murder soil week dare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
