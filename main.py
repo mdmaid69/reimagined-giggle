87163ab979d1c99d4b9741502eb60eb900992ab6
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:29:29 +0000

    mood door else decision length

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
