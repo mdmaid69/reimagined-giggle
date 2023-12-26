db8003d4335ec469e0638a7335a066844fd5b9de
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:35 +0000

    they make exactly hit tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
