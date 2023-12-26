bc83811a98daf5de6a19943b41b7554246eaf180
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:58:20 +0000

    confused announce wave declare Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
