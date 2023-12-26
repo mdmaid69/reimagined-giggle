32a9c00597bd484a0fdb187f77133af7cad73354
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:49:12 +0000

    type chat between recognize nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
