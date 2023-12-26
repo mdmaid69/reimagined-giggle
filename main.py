6c42cf0a619510ab20a36359f6673edb7f7ba7ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:31:42 +0000

    answer negative win completely arrival

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
