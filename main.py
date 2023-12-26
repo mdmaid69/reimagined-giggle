8d4b6ade73d22a5669ef5586fa296d939c29a754
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:27:16 +0000

    strange small plate improve preparation

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_length(array):
-        return len(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
