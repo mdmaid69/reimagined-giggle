ce228dc6303dcd754175a8b18f00addd5837b842
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:28:48 +0000

    tongue identify press excuse safety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
