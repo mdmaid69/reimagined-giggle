7c430666ee601935f1056bf1dc9bf2c1d946afae
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:36:16 +0000

    anywhere exact stay breakin expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_to_hex(n):
+        return hex(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
