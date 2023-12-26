161e894cef1924762c147de9e3e0f3b7fe241b8b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:14:39 +0000

    my clothes evidence weigh healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
