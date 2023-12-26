be924e691cb49e23ec033a3afcb7c675e842c3b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:40:02 +0000

    now follow no arrival glass

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
