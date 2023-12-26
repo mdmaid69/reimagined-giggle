fd82034fa544e0a5201fdbae7ca343ca98fe3230
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:30:28 +0000

    surprising sail desperate bridge all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
