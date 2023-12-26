18b16876bd3abb1aed6a0742d7e7d920454abaac
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:23:25 +0000

    sink power hers enthusiastic proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
