905f1945543f84c83682d21895aa9c57b8dba566
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:47 +0000

    famous complete defeat involve season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import time
-def get_current_time():
-        return time.time()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
