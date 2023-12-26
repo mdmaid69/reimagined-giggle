d0144b2b401d9ec72ccb478894fe36453c3a9cd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:31:49 +0000

    arm organization exercise little not

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import getpass
+def get_username():
+        return getpass.getuser()
