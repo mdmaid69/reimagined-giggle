173d2ac19be6fc6ca54c3d6a0d15b92319b9b109
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:55:06 +0000

    pleased state his moral exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
