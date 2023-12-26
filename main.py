3c24826d131ed3568eae06d69ce8a7520d67a083
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:06:05 +0000

    insist leg advanced distant away

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
