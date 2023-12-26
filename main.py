cdc4031db7b8cf387d94e2bf00a2a469416848b0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:41 +0000

    touch sound violence getoff might

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
