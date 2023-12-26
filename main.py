e492abac045da7aa4f2d77846d0b6b874d6a7c64
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:22:26 +0000

    stone paper describe brain complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
