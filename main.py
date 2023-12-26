7ae8c58caa6399ebbdddfddf4607a7b50aaa35ff
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:58:31 +0000

    pupil organization lost react key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
