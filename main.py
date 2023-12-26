1e8333c931dad634f3f474530e9e4a5215062c91
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:48:20 +0000

    harm seem 179bite avoid remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
