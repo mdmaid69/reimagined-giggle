245caf30878db2c08e42c20bdc8b7f4d0d7d9f7c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:36:29 +0000

    separate trick beat poison when

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+def calculate_density(mass, volume):
+        return mass / volume
