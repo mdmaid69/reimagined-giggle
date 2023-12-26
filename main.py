0d7b492c05e2b56deb2672f173a8c51e169761b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:18:24 +0000

    ever achievement abroad explain slice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
