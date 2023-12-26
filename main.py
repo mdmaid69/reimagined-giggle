ded3eb3470f32f7c84823daf21fd94e0e13a4777
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:37:38 +0000

    completely guess grade reaction gradually

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
