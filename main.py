40175be8740caee7b3462822407f3e032e17dbf8
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:28:48 +0000

    workon accept present lot condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
