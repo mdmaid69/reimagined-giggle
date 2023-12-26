8800bd55c1a08c2cfb545a0f63c1e564efaa345c
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:36:21 +0000

    form shout passage bet shock

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-for i in range(10): print(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
