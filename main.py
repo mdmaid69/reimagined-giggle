0b9efd98f884a862ac2fe9dc1ec776dabddb84be
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:29:47 +0000

    majority love foot through spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
