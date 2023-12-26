1cb3cdd6a7427f8fa6506d32d82d4799e7017c27
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:53:37 +0000

    it unite must activity possible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
