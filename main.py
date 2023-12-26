47ac138fd3216f5c7973b27a6b7dcbb8846d751f
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:31:39 +0000

    excuse occasion plenty over wet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
