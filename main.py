6e7378643754604c07440f97ae11f4f370ba2aa1
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:34:58 +0000

    broad mistake mysterious region top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
