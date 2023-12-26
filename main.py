3c66a6f5084ae1b2dfd74037bfa843252ed61279
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:10:20 +0000

    pump goout butter comeoff please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
