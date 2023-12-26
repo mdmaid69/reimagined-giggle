65dd7b30aeadea33d633cd698ffde3abf71d672e
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:25:53 +0000

    chin manage rush answer bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
