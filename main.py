bc6da092794064c87e367880ddb0ae06acb3946d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:35:58 +0000

    worth impression manage trouble quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
