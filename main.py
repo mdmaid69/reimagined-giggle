f409b14243e4003bc14e99e704301d387329ae0b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:13:58 +0000

    571enough stop soil bother proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-print(time.time())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_speed(distance, time):
+        return distance / time
