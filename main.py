6d9bd469cfa0bb9adf5d327c8ebb96f7ba9966c4
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:46:52 +0000

    achievement furniture underneath bath drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
