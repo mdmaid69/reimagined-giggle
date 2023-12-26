4926c6c1d41edd812bb9fd2368bc03d7fef6c397
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:20 +0000

    command basis either nigga path

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_cosine(x):
+        return math.cos(x)
