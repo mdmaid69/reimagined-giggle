1f7fdaa3f8843b2dad65d3a9e10460fd23b0bb87
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:22:17 +0000

    geton book reach notice long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
