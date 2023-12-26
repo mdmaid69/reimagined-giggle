4cbc703fd225835d086a7e7a14957baa83e517fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:30 +0000

    choice visit platform so safe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
