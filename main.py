346182ab928d10af397a6b950bd897a7a71edcd5
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:15:35 +0000

    risk professional begin whoever cross

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
