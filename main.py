8ba215c037a004cbc20d30a30aa19a94329de0d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:35:00 +0000

    nature chance reduce hurry since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
