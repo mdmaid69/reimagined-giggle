dbb3e4da7ecf4cc46e98ccae367f3ca6d3a86157
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:35:30 +0000

    basically thumb blame thinkthrough available

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
