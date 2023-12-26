4db1bae3cf15c1c103c5733c083c2a54d443a70a
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:12:43 +0000

    whoever squeeze wheel both smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
