f7c6a405b1b16839c5f1a01e4d15831ab9c2ea47
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:44:36 +0000

    practical information play bringback hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
