3e65716c7c924083ea0e16435423e111fa9fd0c6
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:45:55 +0000

    stick deserve Bb hungry agree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-print(math.pi)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
