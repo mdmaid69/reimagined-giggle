1e43fc4ec06b8082f5039fdb875b269c2a0952c5
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:27:45 +0000

    concentration development regularly bang copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
