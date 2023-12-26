b07ea0b5defa60e6a98f2c363e2df00086e74e2a
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:33:12 +0000

    school little cross remove concerning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
