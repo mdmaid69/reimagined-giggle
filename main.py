8c513500a635fb7f563ae83966fb5ebd718f1a03
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:59 +0000

    vision open kick dry plenty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
