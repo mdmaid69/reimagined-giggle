7f1dd3a2fa9bb4384fa694152e4a0bb7963d4741
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:03:08 +0000

    idea score even Itself write

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
+import json
+print(json.dumps({"name": "John", "age": 30}))
