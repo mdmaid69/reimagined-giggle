4da7f1b723993f3c6a207e2139d1391cfe15c6c7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:17:53 +0000

    bite worry common borrow consult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_user_dict():
+        return collections.UserDict()
