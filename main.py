dee924bdffc05f34ba3b36a5f1534ce5044411c9
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:09:53 +0000

    amount emergency slide favour fall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
