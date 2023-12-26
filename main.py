d7d57686643d55dba204d3b548d5de3b16370401
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:48:38 +0000

    ours comeout charge hear attach

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
