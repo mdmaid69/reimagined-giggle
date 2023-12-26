c8543c82bc601b1be23bdb6aaf45af96df72ab2f
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:10:43 +0000

    either hot pause public juice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
