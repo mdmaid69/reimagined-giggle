303f038185fcf4fb5d1f1d1f79b5de7170da5772
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:23:12 +0000

    wave unhappy father hate comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
