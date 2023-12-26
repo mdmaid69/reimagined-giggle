b8d1f76da21595710f606053a579358b8d2a0ab4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:43:38 +0000

    plenty stiff quickly boring upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
