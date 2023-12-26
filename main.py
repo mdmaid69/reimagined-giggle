0d05208b639cfc51e4829a8d7ed04ae1584b05b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:56:23 +0000

    small lean experience hour talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
