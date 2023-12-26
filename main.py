88d35617522d25d77eb6ff05162c1c0ebc4ee5e4
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:52:51 +0000

    patience copy complete agreement half

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
