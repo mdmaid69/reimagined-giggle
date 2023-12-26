fd1839eba29aeabc62fe0159ea648ead5763f25f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:55:33 +0000

    yes tip great runover call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 def count_characters(sentence):
         return len(sentence)
+import array
+def get_array_as_repr(array):
+        return repr(array)
