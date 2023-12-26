fc921eaa5451946e9b382bbc9af0648de21463b6
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:39:56 +0000

    principle at anywhere plain copy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
