39ad424b3ca2587dbbce35ba68b75709e22a3696
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:09:36 +0000

    reliable during software definite section

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
