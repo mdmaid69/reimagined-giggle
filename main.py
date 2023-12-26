4ab305a02fbeaac937d3b173074025a833c66c33
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:02:18 +0000

    shine fine well total substance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
