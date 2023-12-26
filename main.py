fbccffac10feeb1dcfbe12dbaf029a0444f2d481
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:29:20 +0000

    waste calm hold broken so

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
