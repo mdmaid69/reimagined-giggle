bf8126c397a7acf0140849774c2bda1472d4b940
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:25:44 +0000

    dead phone side and consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
