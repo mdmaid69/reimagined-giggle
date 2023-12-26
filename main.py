be501e7a3c89c258803b9107c083723ce5cd6be9
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:39:07 +0000

    blue dull man possession funny

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
