db31961e29e676ad3bb70df664a752786cdc5eed
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:02:07 +0000

    sweet tell callback unable sister

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
