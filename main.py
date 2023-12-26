90b6e811bc63888f975f9e5197f08f8b38e10dc2
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:26:26 +0000

    anywayanyhow actually circumstance careless success

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
