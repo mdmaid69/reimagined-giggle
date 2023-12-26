1ba4c8727c76b22b895de75ad3e810527fd54931
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:14:24 +0000

    squash collect claim cruelty murder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
