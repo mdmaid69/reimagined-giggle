0d4a61e682fbeba7905b610123bd2e55a2f1cfbd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:13:06 +0000

    hour grow squash level person

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
