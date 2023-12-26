452388886e2c1d4ece2d438ffc18dd0f6b7330aa
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:53:45 +0000

    difficult rush swimming drink unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
