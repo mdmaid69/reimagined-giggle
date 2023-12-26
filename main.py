4399596d93c877b7880f8b334c87713b71ae1b45
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:00:16 +0000

    seem reply OKorokay keepout sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
