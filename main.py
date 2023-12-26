7cf60f88d157d9b8d0b856c8a4a63df2cde7bad4
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:03:08 +0000

    under avoid total transport pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def sort_list(lst):
-        return sorted(lst)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
