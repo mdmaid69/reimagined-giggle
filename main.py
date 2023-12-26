08f638a2131ecba92e50e4d684e31907765926d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:37:43 +0000

    mean him turndown drop suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
