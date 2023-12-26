0ed070d1e49f099895cf6b34ec6cc566bd5dced3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:51:01 +0000

    close escape everywhere fairly throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
