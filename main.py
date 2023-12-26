15f4de0f097d7bf17ff6f90048eaca5481c0fd63
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:48:07 +0000

    fortunate ever design carry stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
