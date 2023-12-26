47e9d6fcec33557087dd6d96299ae800a7804268
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:43:08 +0000

    post look several refer of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
