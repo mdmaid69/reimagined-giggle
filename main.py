602441b154f35caf8d43279d248e73e905b2caa0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:09:23 +0000

    open hate not comeout prize

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
