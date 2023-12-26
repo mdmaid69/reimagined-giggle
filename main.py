22ea4e716589c3340be44fa6d5d50a4ca71df938
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:46:50 +0000

    mine worst still plate convinced

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
