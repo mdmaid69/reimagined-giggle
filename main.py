4da1c440b6b68aa5aeacf90f14664c2ca25c5c51
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:58:41 +0000

    work out port fine power

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
