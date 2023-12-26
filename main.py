73c045dbcb0b349fca370792081b18a67c99f373
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:48:43 +0000

    office post obvious downstairs top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
