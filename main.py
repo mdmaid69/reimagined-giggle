375ba55cdee331f045bd7a37b80a5d32ee04416b
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:19:14 +0000

    light genuine punish give nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
