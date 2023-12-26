503a40e624bfbbed9078e1e44d68c91e1acda50e
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:59:15 +0000

    long any breakdown mix bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
