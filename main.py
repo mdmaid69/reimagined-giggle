46b58b189e9cb00249da960ba44239fb79cf3165
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:42:47 +0000

    respect loud good how value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
