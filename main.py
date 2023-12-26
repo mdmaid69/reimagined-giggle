4220081d62de5a69c27b87eea1933cd187ffee33
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:52:29 +0000

    clean rare direct book interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
