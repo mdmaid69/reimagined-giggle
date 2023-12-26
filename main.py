be555dc05f3a821f1fb8871776730f39306d6035
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:51:15 +0000

    win label speed swimming grow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
