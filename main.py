103b8c5e412ebd67f32f36d0e30470d438a4ae9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:28:40 +0000

    age possibility noone steer hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
