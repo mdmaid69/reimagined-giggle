d2a7f79faf1898ef53c052b19bbafa73249e3e9f
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:38:48 +0000

    sound 571enough genuine fly warn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
