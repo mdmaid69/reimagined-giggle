7b7d4ad9f6221df6fc81d3bd484b81e51ac00df6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:45:34 +0000

    cut hungry belief clever available

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
