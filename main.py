44d8a3f260797d6c2005f2c5f7308f2ddcdcd2ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:45:58 +0000

    original shape join classroom doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
