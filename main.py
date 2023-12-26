87e43de9d4dcda3fc0584bc0a5c5ccf9309ba61e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:57:06 +0000

    decision left shine hide search

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
