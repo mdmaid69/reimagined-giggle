9c0bd71a1e42acd1ff1579ddcd59b026394464b3
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:39:03 +0000

    pretend car fair whistle artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
