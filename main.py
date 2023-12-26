810c8e283ea6cccecd9b671fb28e0971b957dfa7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:21:52 +0000

    trap worth keepoff little family

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
