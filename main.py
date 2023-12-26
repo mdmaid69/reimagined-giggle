41f5197ae57a5b42eec91160f5beb5ef9230ff46
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:44 +0000

    discussion hollow basically bring fortunate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
