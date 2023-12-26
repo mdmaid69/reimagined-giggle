388901ad25c5b3a147098d0073e5e4df89267454
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:27:06 +0000

    anywhere convinced stay further preparation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
