df91785a8ea784b0c960cc8ca14bed29e46542b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:53:55 +0000

    pride dark centre claim inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
