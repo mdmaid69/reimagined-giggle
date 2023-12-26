265a5e8561c9b410cccdd72ab8c87c392624a30d
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:46:05 +0000

    adjective means habit neat worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
