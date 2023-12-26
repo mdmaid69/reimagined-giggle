cec8e6a500471b4aba7eb8128c8bff2902380f31
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:34:56 +0000

    heat direct explanation good fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
