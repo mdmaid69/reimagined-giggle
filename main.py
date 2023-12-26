6ff4360015f61ceff9c9bfe2133481199c478f1e
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:26 +0000

    fast downstairs sink language period

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
