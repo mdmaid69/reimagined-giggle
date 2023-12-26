1ade04db51a95c5de90cc8f2ccbd8f2dc84e50fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:36:08 +0000

    basically criticism dick past arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
