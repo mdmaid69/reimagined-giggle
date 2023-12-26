58452ad43351aae7d43080c387e767a10c879524
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:06:10 +0000

    operate backwards obtain hole quality

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+print(os.getcwd())
