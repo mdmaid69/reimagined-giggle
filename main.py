63b12784aeb85a7547a7a5a1f57de39fe115a115
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:13:56 +0000

    turndown travel weekend fast pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
