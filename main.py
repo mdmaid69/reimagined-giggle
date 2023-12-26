4ef5b2aee82ef960c76ed5b6da7a9871b476416a
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:01:51 +0000

    closed nerve pretty visit private

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import re
+print(re.match("h.*o", "hello world"))
