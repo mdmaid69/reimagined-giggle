1bc54fcf2d1cc31ff57a0646ca6570a876d80242
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:50:41 +0000

    regard refuse evidence use couple

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def remove_from_array(array, item):
+        array.remove(item)
