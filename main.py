dd0d51c9d2474fa82ec31ae10b8beb4150ca47b8
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:34:09 +0000

    must length free record mean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
