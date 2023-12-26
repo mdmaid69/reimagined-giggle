82008d8f7d9ca31da9b7be73da274c5b4044b394
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:19:29 +0000

    flow suggest as rather mistake

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
