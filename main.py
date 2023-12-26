39506455a7ea0fd59009e74c510ab6cab5f7a6dd
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:58 +0000

    sign same enthusiastic responsible relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
