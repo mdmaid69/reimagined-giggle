054051c497f607973a2b1aa37e5095d56646e87d
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:08:46 +0000

    fall wheel enthusiastic soak evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_os_info():
+        return platform.uname()
