34ed7c00b8cbddfca5e74c5945b193b08a124414
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:47:15 +0000

    possibly right away narrow between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
