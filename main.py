d3be542dfeb25ce4b9770d594bf951b22479186c
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:51:08 +0000

    mine exploration whichever real match

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def subtract_numbers(x, y):
+        return x - y
