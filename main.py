6807db5bd588b6f42e68b3aa81945c78f7aef064
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:28:14 +0000

    offence travel Nn show think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import re
+print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
