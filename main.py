194f7003362a8f8eb62bcbd95ffa05e31aed7b32
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:16:02 +0000

    pocket lot exchange measure react

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
