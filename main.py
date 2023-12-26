f4ca457d001d8678128ed19f89dc74a609fceac2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:38 +0000

    outside delicate needle difference dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
