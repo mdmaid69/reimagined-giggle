3feced45c84ecbee6358cc15f452e0728dd519e9
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:08:40 +0000

    hard instead advice distance cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
