b8b61f3afd160f7130b9304f1828fe3a7217303c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:27:30 +0000

    feel cum train impression turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
