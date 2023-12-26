c94c877876508dc4676c064f7049a41646912b03
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:14:28 +0000

    stone wish ready short exit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
