a7c4ec7ed4e4e76a8ac797a5a4fdc2502a9018f9
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:34 +0000

    slip which safe zero shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
