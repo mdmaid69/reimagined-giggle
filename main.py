8b07ea5d4267e1edf28747f40377ceaf44a0bcb0
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:58:42 +0000

    book sing trade thinkof contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
