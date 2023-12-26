1294f09ba70bee047c7e4892424cbf96c0dd58ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:46:48 +0000

    branch leave special anyone story

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def square_number(x):
-        return x**2
+import sys
+print(sys.version)
+import array
+def get_array_as_bool(array):
+        return bool(array)
