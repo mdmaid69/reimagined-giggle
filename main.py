4d50a2d746fae5291c82fa9c14944b1d6ded7ef1
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:42 +0000

    by principle comeover complain breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-print(math.pi)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
