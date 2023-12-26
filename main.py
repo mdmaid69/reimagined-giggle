68b6f64647d9cf04ff5e399007609fa2fe73a1ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:18:09 +0000

    patient proper respect blue copy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
