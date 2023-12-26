55646b3297d75f2a81bef8543f78dd0ebf7a83ee
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:23:52 +0000

    share sing talkback down sell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_min(numbers):
+        return min(numbers)
