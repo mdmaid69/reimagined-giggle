7bb62d52f77680a9dd4f81a8d3dc35b7de38f80c
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:41:56 +0000

    so risk skin tap welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
