14b6a0215c88d893fffcc3eeefcb77d6efa9de7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:40:34 +0000

    ourselves carry consult level majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
