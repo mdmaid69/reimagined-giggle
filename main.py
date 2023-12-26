1777ef64b9d16e15a9e6ac9a67e686f598894e4f
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:00:09 +0000

    finger national supply natural really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_list(array):
-        return list(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_list_from_array(array):
+        return array.tolist()
