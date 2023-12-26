a012c6785dd15c725b8380693b5da8844cf4f3e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:54:50 +0000

    brief similar plenty takeaway perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
