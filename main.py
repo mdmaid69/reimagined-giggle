fa4f83e16b802cba363002392fea14013d809b4e
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:06:58 +0000

    music holiday enthusiastic sound may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
