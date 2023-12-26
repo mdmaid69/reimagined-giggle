04b34891bd1417d82db133558ac8d8f18971885c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:56:14 +0000

    suffer strike murder bang finger

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
