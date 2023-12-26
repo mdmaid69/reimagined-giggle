8a4dd7788cc90ada7ee35318b66d0b5ddd2e6c23
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:24:58 +0000

    identify as tired feeling few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
