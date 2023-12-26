9235e6c5a0a29c11e03e08a058c68d4ba01028ac
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:56:47 +0000

    book punishment imagine dig arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        return sys.version
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
