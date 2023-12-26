740a8849189626ddaad73609a2302905439fb5fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:46:30 +0000

    complete cross closed person 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
