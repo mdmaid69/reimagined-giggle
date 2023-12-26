9162349467b03ccb7a3d8f03d50b9f3b64c4473e
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:38:27 +0000

    earth most moment some polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def remove_duplicates(lst):
+        return list(set(lst))
