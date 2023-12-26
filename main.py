c4f01fd9cda26a24fc243b0c879cfe530dc82d4b
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:38:21 +0000

    unless succeed step turnon tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
