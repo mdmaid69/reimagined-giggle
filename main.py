8406663fb93dc19f7e3c18c6b8e40dff69369ac8
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:17 +0000

    shine slightly clean lost confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
