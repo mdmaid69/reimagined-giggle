87f30acd0d375044d7eaebb7270cecc13bfaf9d8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:52:22 +0000

    could Ss react try material

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
