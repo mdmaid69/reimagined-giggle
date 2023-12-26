8f833f5507c4f298155cceff15ec90d9bb5401ca
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:37:49 +0000

    bang scream lay service doubt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
