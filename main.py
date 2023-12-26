0c3398d39a9baa900c8bc55e7f18bdfbdfc41378
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:14:13 +0000

    warn wonder light design firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
