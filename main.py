085b9d8a3fecc5900b16dc9d05184b31fe89c363
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:17:51 +0000

    behind importance major produce advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
