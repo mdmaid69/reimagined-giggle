4a0a9ba4e25ef3f40c634191feefd4fad09e037c
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:57:48 +0000

    into recognize no hollow cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
