ce5d7718a36e78236314754d0f428e04491c5f8c
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:50:53 +0000

    bringforward from wide relationship wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
