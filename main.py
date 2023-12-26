49d599598d3b0191ac693cc0c50c0ac334b26c76
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:37:43 +0000

    education say cut shock yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
