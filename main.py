908a01850a105c4295e70418725b343b1d44de6b
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:17 +0000

    purpose flight slice sudden centimetre

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
