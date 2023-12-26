effebce866a48a715c8bc59edbf223b185d6b860
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:34:09 +0000

    burn trust awake downstairs moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
