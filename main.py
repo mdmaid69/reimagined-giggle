4e24df421b5f7b2c29d914c22a4d1a4076aeb02a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:18 +0000

    fast land practical gradually mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
