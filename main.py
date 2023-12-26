988d2d3d5e3207d595442ccbb1a6bef5583ad39b
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:30:44 +0000

    rush path identify fat left

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def print_python_version():
+        print(sys.version)
