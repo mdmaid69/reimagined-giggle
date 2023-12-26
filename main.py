cc90389f7cdf00e17ec6e561321d0303f0f2a16c
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:16:27 +0000

    probably have pretty task here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
