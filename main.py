e3732ca0c39a0b5c35d2b3094b1978fdc8d33cde
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:27 +0000

    shelter popular react upwards pot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
