7e7b4295c97a0598b815f2c7dcd0f78c2fe81b57
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:55:54 +0000

    phone mysterious school ignore drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
