4ca1ef91d1904d2eae13c334764d178f1677632b
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:28:37 +0000

    tooth concerning mass however memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
