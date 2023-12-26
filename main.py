ac686fa17e6c2b9054996471b5fc444c53d998fb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:12 +0000

    file kind at remember suppose

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
