7f06ae153ab5c407edc65d5d3579d2db26af876d
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:56:58 +0000

    figure soon talk apart imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
