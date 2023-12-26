27febbe705535ffba61afe613b1811c663e14176
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:03:07 +0000

    trousers bottom phone sky right

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
