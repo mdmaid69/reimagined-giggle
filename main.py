d33757560b882f9983c613be0c97a314256202d7
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:17:35 +0000

    rid sad insist care promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
