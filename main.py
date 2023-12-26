ffbcc8e0fe3aa121e500bda6bcde6378d2bd9bfb
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:21:56 +0000

    acceptable scheme unit plant artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
