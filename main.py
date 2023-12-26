a1e60a5779a32c26c873de13278276c839416838
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:03:27 +0000

    workout popular alive television bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_typecode(array):
+        return array.typecode
