f9014961100391272e0bc8794829e392f4b823df
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:51:11 +0000

    set concentrate area blame contrast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
