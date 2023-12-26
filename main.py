af9983fe2418597f0e2ffd8ad0a697439f376b32
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:00:30 +0000

    emphasis beside long snow interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def reverse_list(lst):
+        return lst[::-1]
