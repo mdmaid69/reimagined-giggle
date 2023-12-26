a13b12094865ad6b4a942eabf35a756e9a7e0954
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:16:46 +0000

    thankyou conversation Ss face find

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
