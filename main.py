7cc8ea4b3dbf139828fc5fde18f4dd2ec9e23096
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:20:06 +0000

    interest project size ball than

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import time
-print(time.time())
+  def is_odd(n):
+        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
