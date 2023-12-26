e1b3d061298c85e80bf398236fc9c6ff3121abd6
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:30:57 +0000

    unfortunate borrow ground vanish plastic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
