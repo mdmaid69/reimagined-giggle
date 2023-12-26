975eafc78b2899ab19448bd52050fb43c3583c7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:18:07 +0000

    design cross task figure goon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
