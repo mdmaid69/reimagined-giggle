8cc47267567eb72b16699d1609a7345d5dd705db
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:19:13 +0000

    anyone greeting talkback introduction shine

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
