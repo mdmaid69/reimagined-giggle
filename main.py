bf2d47104dbaa8389510eaf59022247281c5f85d
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:15:08 +0000

    although flood laugh foot condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
