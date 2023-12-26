820b3212b9882273861ca738e941a9099fa7fce0
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:11:51 +0000

    Cc since remain exercise yours

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-for i in range(5):
-        print(i)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
