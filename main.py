fb8f487f674f56b74d837d148a82132627910ec0
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:44:26 +0000

    distance obvious cover climb reason

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import array
-def get_array_as_list(array):
-        return list(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
