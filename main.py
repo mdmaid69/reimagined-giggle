836bcdab58dd7de74cdf693713cf9fe5ca7976c3
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:08:34 +0000

    aim active area cheat frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
