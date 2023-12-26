b6d3f05837aa12c03293a57246d99dc9a9afba91
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:56:58 +0000

    objection land risk chief gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
