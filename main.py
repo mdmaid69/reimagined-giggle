91673781357285c3f7f052e91f90bb6f66e1b55d
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:12:55 +0000

    closed comeround insist ordinary hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
