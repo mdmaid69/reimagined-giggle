6791a6fce1e39960f2c12b365df69e8c223385b7
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:07:57 +0000

    mother uponon slight manage yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
