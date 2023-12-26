b1bb14394a04fe5ef707a9ef877ac569200b1ac5
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:15:59 +0000

    block speak bit yourself afternoon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
