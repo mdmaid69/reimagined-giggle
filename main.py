797c2aa8426d3edac465e74f75b6f16e22b0f922
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:47:39 +0000

    shake question matter absolutely past

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
