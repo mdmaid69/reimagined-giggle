6d8f19eb96fd5f9a76fa3d55679c0ae9aa69956e
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:51:51 +0000

    that plastic paper true downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def square_number(x):
+        return x**2
