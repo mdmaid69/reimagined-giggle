3db2037ef2fe25e7c3d6e8921393a715f7ae5a81
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:16:19 +0000

    score workupto smart she lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
