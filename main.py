ee1606addcbbcdca31403a91403dc8cc6097e95c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:32:20 +0000

    blame visit check birth careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
