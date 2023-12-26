d793c4e9c3b831170a75ebc279f2c336c07ca49e
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:53 +0000

    book exactly level everybody move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
