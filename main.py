82775773726332cb09803a96e8231b35cc403048
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:14:18 +0000

    force dull press waste past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
