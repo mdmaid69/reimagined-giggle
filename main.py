e4cf4e47eef88b007b0002f95323d7e9984e2484
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:18:57 +0000

    chance miss during position contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
