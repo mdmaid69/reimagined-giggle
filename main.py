0dde88e91537728738c23a4d6d6d9c120cec8f0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:37:10 +0000

    deep hardly pure tear black

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
