eff402a65668339c8ac3857292473e5b4aa2c585
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:16:06 +0000

    future poison return conversation treatment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-print(random.randint(0, 100))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def count_words(sentence):
+        return len(sentence.split())
