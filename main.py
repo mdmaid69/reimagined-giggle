a649d22b16626d6d0dba7fd2ae920481aa80668d
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:22:33 +0000

    help news pressure their runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
