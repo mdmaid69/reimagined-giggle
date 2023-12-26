87cbbb86e955f84735ba264c5e678609b036396e
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:18:02 +0000

    tool comeround transparent family moment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def multiply_numbers(x, y):
-        return x * y
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
