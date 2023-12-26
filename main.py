ee5bf99bea5e8876804f2a1cbe87abe23188bd56
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:53:29 +0000

    impression minor communicate discover in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
