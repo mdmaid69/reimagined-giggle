f45a5be117d33719488fc0f235082b98a6ff6a9e
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:20:23 +0000

    appeal powerful giveback stir along

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
