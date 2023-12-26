0fe969112252114be4b93df7edd43c7d5717f16c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:22:05 +0000

    fit thick block callat uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
