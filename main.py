0ac16525c79aa00c4ff6f14994b9bf7ddff2b69a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:56 +0000

    uncomfortable gas independent uponon cause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def convert_to_binary(n):
+        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
