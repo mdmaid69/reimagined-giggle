13a4799e448fed2780c3399469321b6ba611a7d5
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:35:57 +0000

    score shoot read family apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
