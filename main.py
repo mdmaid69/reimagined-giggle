e677420e04e63cec0e1329cc7f901e09568788e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:47:00 +0000

    abroad today case safety crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
