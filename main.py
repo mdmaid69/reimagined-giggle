ac4613bdb5f7b327462c7b810e1a3509c0802d1d
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:01:58 +0000

    571enough sea physical put stamp

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
