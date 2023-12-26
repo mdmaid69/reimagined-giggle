37964c67e2c6806c8a16366968a6ca239dc6115f
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:18:33 +0000

    deliberate appeal cheat visit bear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
