9535bf6caf399b35112a00e20a475283f1b79fae
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:06:27 +0000

    owe but to holdon dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
