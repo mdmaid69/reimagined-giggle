d0582bd838fa412de979472c7b2bcd8def2830e3
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:57:35 +0000

    dream everywhere type art tip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
