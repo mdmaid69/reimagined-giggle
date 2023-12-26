c2626593dd7156708b05da50156f6a96f1bb4f19
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:22:14 +0000

    sign tap translate friend exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def append_to_array(array, item):
-        array.append(item)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_list_from_array(array):
+        return array.tolist()
