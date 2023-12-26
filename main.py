8d1786d89b7078beadcbcdb2ae7d2d50fac89fbf
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:12:39 +0000

    come short dare ourselves odd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
