3d180adcd196553b19eb7f489b961e3ad06b8057
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:29:25 +0000

    float complete fun people breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
