8d9d1af8f79d6c75dddab23485d7e89156614559
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:38:11 +0000

    impossible unless occur distant embarrassing

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
