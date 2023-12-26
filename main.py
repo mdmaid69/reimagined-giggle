ac22d509341b49f7041ae3d4bfee590e4949ec77
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:33:11 +0000

    mess settle top keepfrom drive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
