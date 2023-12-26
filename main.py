806e2e7a67afebbf4f6fc08b2978630fb3f0f007
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:18:24 +0000

    interruption blue keepout personal afraid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import os
+print(os.getcwd())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
