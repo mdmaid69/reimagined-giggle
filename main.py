1222edb6c2d03cbb0590a29acdae03de2576590b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:42:51 +0000

    sure no close what strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
