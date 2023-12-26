29e2250190b69b5042492dad18a06572d2fcad44
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:28:15 +0000

    Zz takeback holdout challenge mood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
