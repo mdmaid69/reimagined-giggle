f4af0a8cbe03d31eff1e95bd080980c3a9986981
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:23:38 +0000

    understand natural murder afterwards shock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sys
+  def get_python_version():
+        return sys.version
