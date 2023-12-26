cec908d3207662bdc60c1bcfdef21c62c16ddf6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:10:32 +0000

    ill rush flavour content work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
