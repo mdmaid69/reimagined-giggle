db3feeb8a504f7672864d06f5800f3097a670199
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:35:50 +0000

    word split rain perform leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
