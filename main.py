7fb7d508005b9159825da611cb8612c50809ca82
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:38:38 +0000

    under away mix my turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
