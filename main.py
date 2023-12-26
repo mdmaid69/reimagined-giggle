caa652dfcca64402aa9fc2daf0e536f5b98d1bcd
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:47:44 +0000

    formal 571enough tip pay another

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
