11454db506f72ad4a745093e51ab914f34dea0e5
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:42:42 +0000

    neither clear roof various news

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(lst):
-        return max(lst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
