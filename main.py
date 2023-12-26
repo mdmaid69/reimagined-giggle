0a55b5ff3f020b798c0d9f7e796aa1b0007bc73b
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:51:22 +0000

    cheap ring full heat jump

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
