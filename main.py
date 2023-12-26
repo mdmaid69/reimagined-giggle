2a40b9ba72cf9dab021df0b8fb4ded36ed815524
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:20:06 +0000

    suck giveout their children organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
