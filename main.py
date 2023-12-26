6c39d365aac379c3b475b92088e64c8094a09f7d
mdmaid69 <robot@alexsyw.me>
2023-12-26 21:14:21 +0000

    replace balance effort should downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
