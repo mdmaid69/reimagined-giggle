75f518d8827efa39b2f1cf35c18f1a6bfde352cd
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:57 +0000

    rush before luck same side

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+from collections import Counter
+print(Counter("hello world"))
+  def sort_list(lst):
+        return sorted(lst)
