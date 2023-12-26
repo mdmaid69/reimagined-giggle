5f79ddc3adef52c7ee6e600a4083e33fff312f77
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:54:29 +0000

    detail chance whichever close behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+name = "Python"
+print("Hello,", name)
