b226be064ccd84a9c07a279a990d8de5191ba6ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:27:28 +0000

    smile fence 573entertainment Gg similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_repr(array):
+        return repr(array)
+for i in range(5):
+        print(i)
