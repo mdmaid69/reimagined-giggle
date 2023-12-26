aaafd777dba09a50a562afecf8e3a85a38ac070c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:51:25 +0000

    brother familiar muscle onto evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
