53d80441e83ea7120073457c61637c85d3d009f8
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:08 +0000

    everyone trap kill stiff regional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
