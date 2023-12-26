a254fe241cc769afe57447194dbd3129e7cdb192
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:28:54 +0000

    song continue temper fairly Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
