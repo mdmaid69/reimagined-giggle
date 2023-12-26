707fdc84601e2f420d917d10f0a0dbbe4a966730
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:55:05 +0000

    giveup inside ready properly guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_speed(distance, time):
+        return distance / time
