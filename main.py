8a32d0a1282bacbfa659e10f75827cb0fc7b1e82
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:15:50 +0000

    count entrance opponent air group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
