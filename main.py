8c2c86d4d5bbf9283a30688868664c7fa5a81bc1
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:22:01 +0000

    jump do smell visit effort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def reverse_list(lst):
-        return lst[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
