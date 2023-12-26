c3b6f5bbd3bd65848445e56bfc80e6b2a1d6c0cb
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:12 +0000

    factory insist sentence lesson interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+for i in range(10): print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
