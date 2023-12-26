20ec2c834af5c980c9de17548f616691e3a6e2bd
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:26:49 +0000

    clothes force used by now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 def is_odd(n):
         return n % 2 != 0
