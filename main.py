7d09050db739f365e32995a914853f22fb6ee7bc
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:54:41 +0000

    holdout study speak half message

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
