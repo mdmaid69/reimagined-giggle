eef4a5d51494485751d52810a3db4c4dc0e42f6e
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:55:24 +0000

    copy convince turnover rest discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
