24e32bd08c164f64d606a0336d3fe38bbb29a97b
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:51:01 +0000

    right one shoot computer round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
