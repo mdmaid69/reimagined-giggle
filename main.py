640c11963d8c1adc045301793fbccd7ad16404b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:10:01 +0000

    scheme occasional praise under rid

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
