2535a73825e3788a741fd6085028b2c8e1103a3d
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:16 +0000

    eager law contents as zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_density(mass, volume):
+        return mass / volume
