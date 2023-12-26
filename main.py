ae4e7a516534cbd643eeb019233c9f6358a4b3a4
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:36:27 +0000

    platform variety achieve afford star

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def is_odd(n):
+        return n % 2 != 0
