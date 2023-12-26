e9065c6fe29c9c95ac57cba918b271a5056607e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:05:38 +0000

    nearly branch positive but frame

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
