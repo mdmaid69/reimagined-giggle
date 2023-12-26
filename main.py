2b93c95a9a99b80035a2de082a539b59124870b5
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:36:50 +0000

    regional second fill lookafter spend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
