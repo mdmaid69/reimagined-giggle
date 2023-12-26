6a94074d7a3e92cbdab6dc8be5511df9b4ea9619
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:05 +0000

    splash plant last wait ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
