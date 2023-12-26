9444a685877017140f307c28ddcc26e756105c71
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:39:51 +0000

    exhibition consider method miss bush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
