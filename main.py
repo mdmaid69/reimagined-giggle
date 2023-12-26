751c82e30df6eac8976a9ed666db7a1955da869a
mdmaid69 <robot@alexsyw.me>
2023-12-26 00:54:20 +0000

    properly rise track scare fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
