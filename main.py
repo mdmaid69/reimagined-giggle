17b8c70c4ff7dfb3c386260cbde2eef3717a9e0c
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:22:06 +0000

    central guard direct among criticize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
