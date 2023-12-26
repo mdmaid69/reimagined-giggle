4d349cbc84105a5b17b4177b19b412134802b28f
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:30:08 +0000

    cum answer Ff cruel congratulations

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_typecode(array):
+        return array.typecode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
