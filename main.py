be5144872cfea991cc6672bd0bbc3eba3af770af
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:28:38 +0000

    typical back seem bad memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
