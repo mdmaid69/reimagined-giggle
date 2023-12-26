e800432263854970e0924906f7c7d3c1158feaad
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:53:18 +0000

    add congratulations performance nearly name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_binary(n):
+        return bin(n)
