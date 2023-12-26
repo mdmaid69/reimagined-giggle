dc20da28623bb0b0bcc8b547feda19892b396d96
mdmaid69 <robot@alexsyw.me>
2023-12-26 16:19:20 +0000

    stir rain room nowhere sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
