bb70fda2831a36416149270f26efe62dddd6f820
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:04:52 +0000

    advise gooff most breakinto wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import numpy as np
   def calculate_correlation(arr1, arr2):
         return np.corrcoef(arr1, arr2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
