a1ef3ce54d7dd2b6b0c1813f978451ef25a287fd
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:12:56 +0000

    quarter roll past involve favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
