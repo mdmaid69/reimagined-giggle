e7fe7d3ec5d2766becb260613979b7cedc835110
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:58 +0000

    shut continue emergency further bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+print(sum(range(10)))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
