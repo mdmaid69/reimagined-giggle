bb366ff19a65898ef3b4ec1216da71bde3bc2aea
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:34:13 +0000

    Pp sale mention open central

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
