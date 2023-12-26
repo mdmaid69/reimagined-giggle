a6c7bcaa2d7bad9b2df17bf8497c1aacae29b5d9
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:26:15 +0000

    male damage paper underneath pause

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
