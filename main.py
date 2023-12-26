02398467750c9d99c3c7459e3d1eeb7d65f79665
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:19:39 +0000

    gas behaviour whisper search Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
