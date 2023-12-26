78859e52de8d432b717a3cf3c10881a7545a37b4
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:10:26 +0000

    half undo trousers snow mysterious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
