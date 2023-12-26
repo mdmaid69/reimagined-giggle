847a58386f3e0c91be015469bcace684d0a4ba78
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:24:47 +0000

    empty drive meanwhile dangerous must

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+print(math.pi)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
