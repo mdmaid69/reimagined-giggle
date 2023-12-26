89f76708d73cc0c8099db0b627934869767df738
mdmaid69 <robot@alexsyw.me>
2023-12-26 01:12:45 +0000

    anybody force ear teach imagination

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def extend_array(array, iterable):
+        array.extend(iterable)
