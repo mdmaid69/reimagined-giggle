05a2039aea842b1d57e8d1428ff45c186fc6676c
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:46:05 +0000

    her skill holiday squeeze excellent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
