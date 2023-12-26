6da5c05144fbfc2bdc227b3de9704f7ec6271ffc
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:24:51 +0000

    interview purpose soak suggest design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
