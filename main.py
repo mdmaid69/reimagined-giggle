fbafeeab4a8d546af2d55099bbde7ab57fc8940c
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:01:11 +0000

    instant refuse majority must every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
