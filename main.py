db429a4d4da7b417e8aaf864ba53f5af9fa1b9bf
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:58:41 +0000

    niggaacademy takeaway week punish question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
