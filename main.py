c9bcc21931cfc1c98c23b8021150e85304378223
mdmaid69 <robot@alexsyw.me>
2023-12-26 09:29:03 +0000

    getoff slice fail sure amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
