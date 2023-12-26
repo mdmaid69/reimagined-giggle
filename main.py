ddf8acd9224e28fa98327de6f7f7bc0e9371e2ed
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:14:35 +0000

    snow hole dick money 572enter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
