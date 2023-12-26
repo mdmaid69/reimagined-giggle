df0b7a53657c25c06b2a37771598a9ec9214fbe4
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:15:55 +0000

    undo hot estimate happy among

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_work(force, distance):
+        return force * distance
