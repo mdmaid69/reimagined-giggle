08aa9d1518ec2a30c7360e85f2fbfcaabb34b985
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:57:10 +0000

    coffee give upstairs wise exist

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
