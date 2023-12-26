7dc91ec1dc459a42726fce4328c738d73e96be78
mdmaid69 <robot@alexsyw.me>
2023-12-26 10:53:51 +0000

    difficult far fight consult advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
