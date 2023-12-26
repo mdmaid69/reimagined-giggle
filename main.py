305e482a73b568dcdca3802f1fcc07d5fad351e6
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:32:15 +0000

    memory them tongue never bringdown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
