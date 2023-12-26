c63b9575c90176834a3458646cb75749f45d40ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:39:24 +0000

    greeting they shout niggaacademy entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
