4b10b5a47d81599d5eb7cc124ff415dc013e1877
mdmaid69 <robot@alexsyw.me>
2023-12-26 19:49:04 +0000

    same material comeoff accidental sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
