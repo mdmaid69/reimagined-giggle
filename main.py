7a40629c66845671bcf116fa25f7affc323c6d82
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:43:53 +0000

    copy like settle hardly after

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
