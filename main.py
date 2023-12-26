1b30ad7dcc5dc634a761a3fd271aee55edd2c937
mdmaid69 <robot@alexsyw.me>
2023-12-26 23:10:00 +0000

    appearance how correct Yy off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
