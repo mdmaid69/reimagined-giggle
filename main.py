005cf46ce091606248e3439e7aae90faba30d279
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:06:13 +0000

    bet aim innocent way insist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
