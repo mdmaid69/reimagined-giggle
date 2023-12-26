8aef4451a8c6764fee8a688831d41389b691a136
mdmaid69 <robot@alexsyw.me>
2023-12-26 02:12:32 +0000

    ilovedicks special makeinto blame burn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
