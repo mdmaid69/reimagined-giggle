c6d7b8acf7c80605ccef6daef7f0505fb5be6950
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:48:45 +0000

    entrance blow before interest whichever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
