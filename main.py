d0aa8fc82044fbd301295851a0989e2508a95bc8
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:34:35 +0000

    quick side likely fault negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
