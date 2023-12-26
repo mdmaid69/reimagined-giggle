7f3027da64f393c3af1f762343ee027956df433a
mdmaid69 <robot@alexsyw.me>
2023-12-26 22:15:37 +0000

    precise crime identify unable find

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
