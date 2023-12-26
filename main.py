7db387c5e9030ad201d4dcbabc473dcb1effa043
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:32:57 +0000

    note interview post hole still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
