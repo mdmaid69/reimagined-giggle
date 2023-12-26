91954ae2dfa16682daa766578493fd8ead322da6
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:43:12 +0000

    healthy fit blind properly common

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
