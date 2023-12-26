4de9bbc0e383d949d57324260216bad50fb67292
mdmaid69 <robot@alexsyw.me>
2023-12-26 07:31:27 +0000

    season question claim with identify

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
