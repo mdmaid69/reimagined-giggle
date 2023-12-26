7febda221ed47657ee03fa3674e5c8ee5b85871e
mdmaid69 <robot@alexsyw.me>
2023-12-26 20:04:55 +0000

    urgent fire Oo calloff cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def sort_list(lst):
-        return sorted(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
