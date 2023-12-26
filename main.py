7945b9c1552b1c46ec2ef92a59035c862a16b349
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:04:31 +0000

    still practise show education boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
