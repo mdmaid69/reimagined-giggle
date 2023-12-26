f45d995e2e794e61090a6affd4c227068e72b0ba
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:19:04 +0000

    land increase search Pp ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
