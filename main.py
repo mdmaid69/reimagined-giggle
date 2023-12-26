6b7c5150b85a64a5808e312db44b872b4689cd50
mdmaid69 <robot@alexsyw.me>
2023-12-26 11:30:35 +0000

    concerned deaf correction shallow Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_current_date():
+        return datetime.date.today()
