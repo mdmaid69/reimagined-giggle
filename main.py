32c44693751cc94aa3252b43e745c5d728dee66b
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:44:21 +0000

    risk ball clue winter question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
