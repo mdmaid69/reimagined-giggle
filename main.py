20ddb79b333e1a346e2b605a4cf17803b4389185
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:00:55 +0000

    giveup dish term morning flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_elements(lst):
+        return len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
