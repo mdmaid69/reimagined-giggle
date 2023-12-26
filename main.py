cd31cbcb2a38d5817d80f57e87e45200e75dc00c
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:43:37 +0000

    immediate badly out pay room

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
