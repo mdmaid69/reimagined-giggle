a94c5727c4bc5981fc056ce9fc29fb03690ae5ae
mdmaid69 <robot@alexsyw.me>
2023-12-26 18:35:43 +0000

    wrap race age show wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def square_number(x):
-        return x**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
