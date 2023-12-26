7b5ee87481dd8e4613251dc76eb6b72d957bdf6c
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:23:18 +0000

    near walk deep watch mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_binary(n):
-        return bin(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_density(mass, volume):
+        return mass / volume
