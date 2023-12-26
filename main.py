d74a25f296bace4daf2ac64a6ce72edd10bfe39b
mdmaid69 <robot@alexsyw.me>
2023-12-26 13:17:43 +0000

    increase difficulty refer march pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
