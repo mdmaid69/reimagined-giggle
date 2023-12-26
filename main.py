9b4ef09526358f394735f6622576920b9877159a
mdmaid69 <robot@alexsyw.me>
2023-12-26 08:27:18 +0000

    miss until apologizeapologise when plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def find_max(lst):
-        return max(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
