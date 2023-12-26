3081a87bf4cb00562837e0d9fd3e8eaae2ad864c
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:13:44 +0000

    dress performance lost average in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import time
+def get_current_time():
+        return time.ctime()
