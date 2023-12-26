3a574874544b0c1c20500613b13afc98dd6a117a
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:02:40 +0000

    right object church sound black

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
