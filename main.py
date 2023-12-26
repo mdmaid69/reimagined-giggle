42665079f3a59b3209965a8b64d44a19cef11b2c
mdmaid69 <robot@alexsyw.me>
2023-12-26 12:16:50 +0000

    bringforward different space eager Vv

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
