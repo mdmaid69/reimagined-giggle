3ebb1385c73d848f420b74b7d44fcad03c1b57fa
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:18:29 +0000

    straight be trip absolutely design

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import time
-def get_current_time():
-        return time.time()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
