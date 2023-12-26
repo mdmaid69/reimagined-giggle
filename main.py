844f677c79f507a48f92a3712c2c88a47df4d040
mdmaid69 <robot@alexsyw.me>
2023-12-26 05:50:52 +0000

    try similar please somewhere him

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
