789b0f1e89098e2a0ed85cfe860286f2b76cb18e
mdmaid69 <robot@alexsyw.me>
2023-12-26 15:35:30 +0000

    pile scream contact wheel design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
