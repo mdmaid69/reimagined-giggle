928b99b72cb8c42e17e2fbccbfa35384324ccb7a
mdmaid69 <robot@alexsyw.me>
2023-12-26 14:33:15 +0000

    long row further black round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
