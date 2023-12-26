df0c4256eb0f933b45ec6c621af90bab81dbb9ad
mdmaid69 <robot@alexsyw.me>
2023-12-26 03:11:41 +0000

    display spring honest new main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
