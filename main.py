2f147cfe159a16457fd362719cfc67e61a20281d
mdmaid69 <robot@alexsyw.me>
2023-12-26 06:11:58 +0000

    come score expensive trip shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
