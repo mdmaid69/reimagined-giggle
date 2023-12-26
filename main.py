71a71e33280733ee0fa3418b3c5dd2d83e3865b2
mdmaid69 <robot@alexsyw.me>
2023-12-26 17:33:45 +0000

    mainly relationship earth will trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
