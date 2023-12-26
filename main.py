abe60346a01546d6f62bc8534953dbc1f480b0f4
mdmaid69 <robot@alexsyw.me>
2023-12-26 04:58:56 +0000

    apologizeapologise better attach defend system

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
